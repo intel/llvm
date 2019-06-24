45a07038cbb34cafa925d5de8be015551ae61fc2
Sergey Semenov <sergey.semenov@intel.com>
2019-06-24 15:59:01 +0300

    [SYCL] Deduce the required alignment based on type in aligned_allocator
    Align the allocated memory in aligned_allocator by max(64, sizeof(T).
    
    Signed-off-by: Sergey Semenov <sergey.semenov@intel.com>

--- sycl/include/CL/sycl/detail/aligned_allocator.hpp
+++ sycl/include/CL/sycl/detail/aligned_allocator.hpp
@@ -10,9 +10,11 @@
 
 #include <CL/cl.h>
 #include <CL/sycl/detail/cnri.h>
+#include <CL/sycl/detail/common.hpp>
 #include <CL/sycl/detail/os_util.hpp>
 #include <CL/sycl/range.hpp>
 
+#include <algorithm>
 #include <cstring>
 #include <cstdlib>
 #include <memory>
@@ -21,8 +23,7 @@
 namespace cl {
 namespace sycl {
 namespace detail {
-template <typename T, size_t Alignment>
-class aligned_allocator {
+template <typename T> class aligned_allocator {
 public:
   using value_type = T;
   using pointer = T*;
@@ -31,10 +32,7 @@ public:
   using const_reference = const T&;
 
 public:
-  template<typename U>
-  struct rebind {
-    typedef aligned_allocator<U, Alignment> other;
-  };
+  template <typename U> struct rebind { typedef aligned_allocator<U> other; };
 
   // Construct an object
   void construct(pointer Ptr, const_reference Val) {
@@ -47,9 +45,11 @@ public:
   pointer address(reference Val) const { return &Val; }
   const_pointer address(const_reference Val) { return &Val; }
 
-  // Allocate aligned (to Alignment) memory
+  // Allocate sufficiently aligned memory
   pointer allocate(size_t Size) {
     size_t NumBytes = Size * sizeof(value_type);
+    const size_t Alignment =
+        std::max<size_t>(getNextPowerOfTwo(sizeof(value_type)), 64);
     NumBytes = ((NumBytes - 1) | (Alignment - 1)) + 1;
 
     pointer Result = reinterpret_cast<pointer>(
