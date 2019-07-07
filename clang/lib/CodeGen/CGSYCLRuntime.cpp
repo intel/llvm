f5af1d74d4d2a9a9c1c53b01c94a1fbc5bc0cc59
Konstantin S Bobrovsky <konstantin.s.bobrovsky@intel.com>
2019-07-07 13:53:29 -0700

    [SYCL] Add post-CG pass to enforce semantics of work group scope code.
    Adds a post-CodeGen pass in clang's BackendUtil.cpp which basically wraps
    all code with side-effects visible in other work items in a work group with
    if (get_linear_local_id() == 0) {...} check to make them execute only in
    the "leader" work item.
    Also adds code so that (immutable on WI scope) contents of allocas for
    compiler-generated locals in WG scope (byval parameters, lambda objects)
    are valid in every WI before calling PFWI.
    
    Signed-off-by: Konstantin S Bobrovsky <konstantin.s.bobrovsky@intel.com>

--- clang/lib/CodeGen/CGSYCLRuntime.cpp
+++ clang/lib/CodeGen/CGSYCLRuntime.cpp
@@ -19,8 +19,37 @@
 using namespace clang;
 using namespace CodeGen;
 
+/// Various utilities.
+/// TODO partially duplicates functionality from SemaSYCL.cpp, can be shared.
+class Util {
+public:
+  using DeclContextDesc = std::pair<clang::Decl::Kind, StringRef>;
+
+  /// Checks whether given clang type is declared in the given hierarchy of
+  /// declaration contexts.
+  /// \param RecTy      the clang type being checked
+  /// \param Scopes     the declaration scopes leading from the type to the
+  ///     translation unit (excluding the latter)
+  static bool matchQualifiedTypeName(const CXXRecordDecl *RecTy,
+                                     ArrayRef<Util::DeclContextDesc> Scopes);
+};
+
+static bool isPFWI(const FunctionDecl &FD) {
+  const auto *MD = dyn_cast<CXXMethodDecl>(&FD);
+  if (!MD)
+    return false;
+  static std::array<Util::DeclContextDesc, 3> Scopes = {
+      Util::DeclContextDesc{clang::Decl::Kind::Namespace, "cl"},
+      Util::DeclContextDesc{clang::Decl::Kind::Namespace, "sycl"},
+      Util::DeclContextDesc{Decl::Kind::ClassTemplateSpecialization, "group"}};
+  if (!Util::matchQualifiedTypeName(MD->getParent(), Scopes))
+    return false;
+  return FD.getName() == "parallel_for_work_item";
+}
+
 const char *WG_SCOPE_MD_ID = "work_group_scope";
 const char *WI_SCOPE_MD_ID = "work_item_scope";
+const char *PFWI_MD_ID = "parallel_for_work_item";
 
 bool CGSYCLRuntime::actOnFunctionStart(const FunctionDecl &FD,
                                        llvm::Function &F) {
@@ -33,6 +62,10 @@ bool CGSYCLRuntime::actOnFunctionStart(const FunctionDecl &FD,
     break;
   case SYCLScopeAttr::Level::WorkItem:
     F.setMetadata(WI_SCOPE_MD_ID, llvm::MDNode::get(F.getContext(), {}));
+    if (isPFWI(FD))
+      // also emit specific marker for parallel_for_work_item, as it needs to
+      // be handled specially in the SYCL lowering pass
+      F.setMetadata(PFWI_MD_ID, llvm::MDNode::get(F.getContext(), {}));
     break;
   default:
     llvm_unreachable("unknown sycl scope");
@@ -48,5 +81,52 @@ void CGSYCLRuntime::emitWorkGroupLocalVarDecl(CodeGenFunction &CGF,
 #endif // NDEBUG
   // generate global variable in the address space selected by the clang CodeGen
   // (should be local)
-  return CGF.EmitStaticVarDecl(D, llvm::GlobalValue::InternalLinkage);
+  CGF.EmitStaticVarDecl(D, llvm::GlobalValue::InternalLinkage);
+}
+
+bool CGSYCLRuntime::actOnAutoVarEmit(CodeGenFunction &CGF, const VarDecl &D,
+                                     llvm::Value *Addr) {
+  SYCLScopeAttr *Scope = D.getAttr<SYCLScopeAttr>();
+  if (!Scope)
+    return false;
+  assert(Scope->isWorkItem() && "auto var must be of work item scope");
+  auto *AI = dyn_cast<llvm::AllocaInst>(Addr);
+  assert(AI && "AllocaInst expected as local var address");
+  AI->setMetadata(WI_SCOPE_MD_ID, llvm::MDNode::get(AI->getContext(), {}));
+  return true;
+}
+
+bool Util::matchQualifiedTypeName(const CXXRecordDecl *RecTy,
+                                  ArrayRef<Util::DeclContextDesc> Scopes) {
+  // The idea: check the declaration context chain starting from the type
+  // itself. At each step check the context is of expected kind
+  // (namespace) and name.
+  if (!RecTy)
+    return false; // only classes/structs supported
+  const auto *Ctx = dyn_cast<DeclContext>(RecTy);
+  StringRef Name = "";
+
+  for (const auto &Scope : llvm::reverse(Scopes)) {
+    clang::Decl::Kind DK = Ctx->getDeclKind();
+
+    if (DK != Scope.first)
+      return false;
+
+    switch (DK) {
+    case clang::Decl::Kind::ClassTemplateSpecialization:
+      // ClassTemplateSpecializationDecl inherits from CXXRecordDecl
+    case clang::Decl::Kind::CXXRecord:
+      Name = cast<CXXRecordDecl>(Ctx)->getName();
+      break;
+    case clang::Decl::Kind::Namespace:
+      Name = cast<NamespaceDecl>(Ctx)->getName();
+      break;
+    default:
+      llvm_unreachable("matchQualifiedTypeName: decl kind not supported");
+    }
+    if (Name != Scope.second)
+      return false;
+    Ctx = Ctx->getParent();
+  }
+  return Ctx->isTranslationUnit();
 }
