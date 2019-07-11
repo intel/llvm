4c1f0693e85ec4797761a72817d718247a076157
Sindhu Chittireddy <sindhu.chittireddy@intel.com>
2019-07-11 01:05:25 -0700

    [SYCL] Removing __FILE__ from use in OCL_ERROR_REPORT
    This is to avoid displaying internal path when reporting errors using
    CHECK_OCL_CODE
    
    Signed-off-by: Sindhu Chittireddy <sindhu.chittireddy@intel.com>

--- sycl/include/CL/sycl/detail/common.hpp
+++ sycl/include/CL/sycl/detail/common.hpp
@@ -28,8 +28,9 @@ static inline std::string codeToString(cl_int code){
 }
 
 #define OCL_ERROR_REPORT                                                       \
-  "OpenCL API failed. " __FILE__                                               \
-  ":" STRINGIFY_LINE(__LINE__) ": "                                            \
+  "OpenCL API failed. " /*__FILE__*/                                           \
+  /* TODO: replace __FILE__ to report only relative path*/                     \
+  /* ":" STRINGIFY_LINE(__LINE__) ": " */                                      \
                                "OpenCL API returns: "
 
 #ifndef SYCL_SUPPRESS_OCL_ERROR_REPORT
