f6d79d78f68ab4c010a4cac2db4d01be4fa4b592
James Brodman <james.brodman@intel.com>
2019-07-16 16:47:02 -0400

    Implement depends_on inside command group handler.
    Signed-off-by: James Brodman <james.brodman@intel.com>

---
+++ sycl/test/usm/depends_on.cpp
@@ -0,0 +1,74 @@
+// RUN: %clang -std=c++11 -fsycl %s -o %t1.out -lstdc++ -lOpenCL -lsycl
+// RUN: %CPU_RUN_PLACEHOLDER %t1.out
+//==------------------- mixed.cpp - Mixed Memory test ---------------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include <CL/sycl.hpp>
+
+using namespace cl::sycl;
+
+class foo;
+int main() {
+  int *darray = nullptr;
+  int *sarray = nullptr;
+  int *harray = nullptr;
+  const int N = 4;
+  const int MAGIC_NUM = 42;
+
+  queue q;
+  auto dev = q.get_device();
+  auto ctxt = q.get_context();
+
+  darray = (int *)malloc_device(N * sizeof(int), dev, ctxt);
+  if (darray == nullptr) {
+    return -1;
+  }
+  sarray = (int *)malloc_shared(N * sizeof(int), dev, ctxt);
+
+  if (sarray == nullptr) {
+    return -1;
+  }
+
+  harray = (int *)malloc_host(N * sizeof(int), ctxt);
+  if (harray == nullptr) {
+    return -1;
+  }
+
+  auto eInit = q.submit([&](handler &cgh) {
+    cgh.single_task<class init>([=]() {
+      for (int i = 0; i < N; i++) {
+        sarray[i] = MAGIC_NUM - 1;
+        harray[i] = 1;
+      }
+    });
+  });
+
+  auto eMemset = q.memset(darray, 0, N * sizeof(int));
+
+  auto eKernel = q.submit([=](handler &cgh) {
+    cgh.depends_on({eInit, eMemset});
+    cgh.single_task<class foo>([=]() {
+      for (int i = 0; i < N; i++) {
+        sarray[i] += darray[i] + harray[i];
+      }
+    });
+  });
+
+  eKernel.wait();
+
+  for (int i = 0; i < N; i++) {
+    if (sarray[i] != MAGIC_NUM) {
+      return -1;
+    }
+  }
+  free(darray, ctxt);
+  free(sarray, ctxt);
+  free(harray, ctxt);
+
+  return 0;
+}
