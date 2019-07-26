419d9309f48b20f1fc4c32b6dd5653fc69bf3b55
Premanand M Rao <premanand.m.rao@intel.com>
2019-07-26 03:53:25 -0700

    [SYCL] Verify that -std=gnu++11 can build sycl include file cleanly.
    Signed-off-by: Premanand M Rao <premanand.m.rao@intel.com>

---
+++ sycl/test/regression/sycl-include-gnu11.cpp
@@ -0,0 +1,15 @@
+// RUN: %clangxx -std=gnu++11 -fsycl %s -o %t.out -lOpenCL
+// RUN: env SYCL_DEVICE_TYPE=HOST %t.out
+// RUN: %CPU_RUN_PLACEHOLDER %t.out
+// RUN: %GPU_RUN_PLACEHOLDER %t.out
+// RUN: %ACC_RUN_PLACEHOLDER %t.out
+
+// UNSUPPORTED: system-windows
+
+#include <CL/sycl.hpp>
+#include <iostream>
+
+int main() {
+  std::cout << "Passed" << std::endl;
+  return 0;
+}
