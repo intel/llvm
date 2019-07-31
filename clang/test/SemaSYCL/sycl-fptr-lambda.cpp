dfbaec37f211840d6161543a98f6ae9770d07545
Premanand M Rao <premanand.m.rao@intel.com>
2019-07-31 10:39:54 -0700

    [SYCL] Avoid spurious error about call through function pointer
    Signed-off-by: Premanand M Rao <premanand.m.rao@intel.com>

---
+++ clang/test/SemaSYCL/sycl-fptr-lambda.cpp
@@ -0,0 +1,14 @@
+// RUN: %clang_cc1 -fsycl-is-device -std=c++14 -verify -fsyntax-only -x c++ %s
+// expected-no-diagnostics
+
+template <typename name, typename Func>
+__attribute__((sycl_kernel)) void kernel_single_task(Func kernelFunc) {
+  kernelFunc();
+}
+
+int main() {
+  kernel_single_task<class fake_kernel>([]() {
+    auto l = [](auto f) { f(); };
+  });
+  return 0;
+}
