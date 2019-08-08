026182b4de4c5759be920943ac7f465c015bacf8
Premanand M Rao <premanand.m.rao@intel.com>
2019-08-08 01:03:08 -0700

    [SYCL] Windows MS-Compatibility changes
    Signed-off-by: Premanand M Rao <premanand.m.rao@intel.com>

--- clang/test/Preprocessor/sycl-macro.cpp
+++ clang/test/Preprocessor/sycl-macro.cpp
@@ -1,8 +1,11 @@
 // RUN: %clang_cc1 %s -E -dM | FileCheck %s
 // RUN: %clang_cc1 %s -fsycl-is-device -E -dM | FileCheck --check-prefix=CHECK-SYCL %s
 // RUN: %clang_cc1 %s -fsycl -E -dM | FileCheck --check-prefix=CHECK-ANY-SYCL %s
+// RUN: %clang_cc1 %s -fsycl-is-device -E -dM -fms-compatibility | FileCheck --check-prefix=CHECK-MSVC %s
 // CHECK-NOT:#define __SYCL_DEVICE_ONLY__ 1
 // CHECK-NOT:#define CL_SYCL_LANGUAGE_VERSION 121
 // CHECK-ANY-SYCL-NOT:#define __SYCL_DEVICE_ONLY__ 1
 // CHECK-ANY-SYCL:#define CL_SYCL_LANGUAGE_VERSION 121
 // CHECK-SYCL:#define CL_SYCL_LANGUAGE_VERSION 121
+// CHECK-MSVC-NOT: __GNUC__
+// CHECK-MSVC-NOT: __STDC__
---
+++ clang/test/SemaSYCL/delayed-parsing2.cpp
@@ -0,0 +1,4 @@
+// RUN: %clang %s -fsycl -c
+// REQUIRES: system-windows
+
+#include <windows.h>
