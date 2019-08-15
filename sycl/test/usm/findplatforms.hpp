21b59c144cbc34619d3c86836298b06e210b20db
James Brodman <james.brodman@intel.com>
2019-08-15 16:39:20 -0400

    Fix findplatforms issue due to CL weirdness for return types.  Add multictxt tests.
    Signed-off-by: James Brodman <james.brodman@intel.com>

--- sycl/test/usm/findplatforms.hpp
+++ sycl/test/usm/findplatforms.hpp
@@ -5,30 +5,31 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+#include <iostream>
+
 bool findPlatformAndDevice(cl_device_type deviceType,
                            cl_platform_id &platformOut, cl_device_id &deviceOut) {
   cl_uint numPlatforms;
   cl_int errorCode;
 
   errorCode = clGetPlatformIDs(0, nullptr, &numPlatforms);
-  if (errorCode != CL_SUCCESS) return false;
 
   std::vector<cl_platform_id> platforms(numPlatforms);
   errorCode = clGetPlatformIDs(numPlatforms, platforms.data(), nullptr);
-  if (errorCode != CL_SUCCESS) return false;
 
   for (auto platform : platforms) {
     cl_uint numDevices = 0;
     errorCode =
       clGetDeviceIDs(platform, deviceType, 0, nullptr, &numDevices);
-    if (errorCode != CL_SUCCESS) return false;
-
-    std::vector<cl_device_id> devices(numDevices);
-    errorCode = clGetDeviceIDs(platform, deviceType, numDevices,
-                               devices.data(), nullptr);
-    if (errorCode != CL_SUCCESS) return false;
+    if (!(errorCode == CL_SUCCESS || errorCode == CL_DEVICE_NOT_FOUND))
+      return false;
 
     if (numDevices) {
+      std::vector<cl_device_id> devices(numDevices);
+      errorCode = clGetDeviceIDs(platform, deviceType, numDevices,
+				 devices.data(), nullptr);
+      if (errorCode != CL_SUCCESS) return false;
+
       platformOut = platform;
       deviceOut = devices[0];
       return true;
