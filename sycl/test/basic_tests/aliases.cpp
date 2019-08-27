b1b86083adab3760fdf6f406f2561855df63aee9
Alexey Voronov <alexey.voronov@intel.com>
2019-08-27 18:12:08 +0300

    [SYCL] Align aliases with SYCL specification.
    Fixed aliases of some vectors to follow requirements from
    SYCL specification 4.10.2.2 Aliases.
    Moved to separate file(aliases.hpp).
    
    Added cl_size_t, cl_ptrdiff_t, cl_intptr_t, cl_uintptr_t
    aliases to OpenCL types.
    
    Signed-off-by: Alexey Voronov <alexey.voronov@intel.com>

--- sycl/test/basic_tests/aliases.cpp
+++ sycl/test/basic_tests/aliases.cpp
@@ -6,103 +6,65 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+
 #include <CL/sycl.hpp>
-#include <CL/sycl/detail/common.hpp>
-#include <cassert>
-#include <iostream>
+
 #include <type_traits>
 
-using namespace std;
-
-using cl_schar = cl_char;
-using cl_schar4 = cl_char4;
-
+using std::is_same;
 namespace s = cl::sycl;
 
-#define CHECK_TYPE(TYPE)                                                       \
-  static_assert(sizeof(cl_##TYPE) == sizeof(s::cl_##TYPE), "")
+// Test to verify requirements from 4.10.2.2 Aliases
 
-#define CHECK_SIZE(TYPE, SIZE) static_assert(sizeof(TYPE) == SIZE, "");
+#define ASSERT(ALIAS, TYPE, N)                                                 \
+  static_assert(is_same<ALIAS##N, s::vec<TYPE, N>>::value, "");
 
-#define CHECK_SIZE_VEC_N(TYPE, N)                                              \
-  static_assert(N * sizeof(TYPE) == sizeof(s::vec<TYPE, N>), "");
+// char, short, int, long, float, double, half
+#define PRESENTED_AS_IS(N)                                                     \
+  ASSERT(s::char, char, N)                                                     \
+  ASSERT(s::short, short, N)                                                   \
+  ASSERT(s::int, int, N)                                                       \
+  ASSERT(s::long, long, N)                                                     \
+  ASSERT(s::float, float, N)                                                   \
+  ASSERT(s::double, double, N)                                                 \
+  ASSERT(s::half, half, N);
 
-#define CHECK_SIZE_VEC_N3(TYPE)                                                \
-  static_assert(sizeof(s::vec<TYPE, 3>) == sizeof(s::vec<TYPE, 4>), "");
+// cl_char, cl_uchar, cl_short, cl_ushort, cl_int, cl_uint, cl_long, cl_ulong,
+// cl_float, cl_double and cl_half
+#define OPENCL_INTEROPERABILITY(N)                                             \
+  ASSERT(s::cl_char, s::cl_char, N)                                            \
+  ASSERT(s::cl_uchar, s::cl_uchar, N)                                          \
+  ASSERT(s::cl_short, s::cl_short, N)                                          \
+  ASSERT(s::cl_ushort, s::cl_ushort, N)                                        \
+  ASSERT(s::cl_int, s::cl_int, N)                                              \
+  ASSERT(s::cl_uint, s::cl_uint, N)                                            \
+  ASSERT(s::cl_long, s::cl_long, N)                                            \
+  ASSERT(s::cl_ulong, s::cl_ulong, N)                                          \
+  ASSERT(s::cl_float, s::cl_float, N)                                          \
+  ASSERT(s::cl_double, s::cl_double, N)                                        \
+  ASSERT(s::cl_half, s::cl_half, N)
 
-#define CHECK_SIZE_VEC(TYPE)                                                   \
-  CHECK_SIZE_VEC_N(TYPE, 2);                                                   \
-  CHECK_SIZE_VEC_N3(TYPE);                                                     \
-  CHECK_SIZE_VEC_N(TYPE, 4);                                                   \
-  CHECK_SIZE_VEC_N(TYPE, 8);                                                   \
-  CHECK_SIZE_VEC_N(TYPE, 16);
+// schar, uchar, ushort, uint, ulong, longlong and ulonglong
+#define REPRESENTED_WITH_THE_SHORT_HAND(N)                                     \
+  ASSERT(s::schar, signed char, N)                                             \
+  ASSERT(s::uchar, unsigned char, N)                                           \
+  ASSERT(s::ushort, unsigned short, N)                                         \
+  ASSERT(s::uint, unsigned int, N)                                             \
+  ASSERT(s::ulong, unsigned long, N)                                           \
+  ASSERT(s::longlong, long long, N)                                            \
+  ASSERT(s::ulonglong, unsigned long long, N)
 
-#define CHECK_SIZE_TYPE_I(TYPE, SIZE)                                          \
-  CHECK_SIZE(TYPE, SIZE)                                                       \
-  static_assert(std::is_signed<TYPE>::value, "");
-
-#define CHECK_SIZE_TYPE_UI(TYPE, SIZE)                                         \
-  CHECK_SIZE(TYPE, SIZE)                                                       \
-  static_assert(std::is_unsigned<TYPE>::value, "");
-
-#define CHECK_SIZE_TYPE_F(TYPE, SIZE)                                          \
-  CHECK_SIZE(TYPE, SIZE)                                                       \
-  static_assert(std::numeric_limits<TYPE>::is_iec559, "");
+#define CHECK_FOR_N(N)                                                         \
+  PRESENTED_AS_IS(N)                                                           \
+  OPENCL_INTEROPERABILITY(N)                                                   \
+  REPRESENTED_WITH_THE_SHORT_HAND(N)
 
 int main() {
-  CHECK_TYPE(bool);
-  CHECK_TYPE(char);
-  CHECK_TYPE(schar);
-  CHECK_TYPE(uchar);
-  CHECK_TYPE(short);
-  CHECK_TYPE(ushort);
-  CHECK_TYPE(half);
-  CHECK_TYPE(int);
-  CHECK_TYPE(uint);
-  CHECK_TYPE(long);
-  CHECK_TYPE(ulong);
-  CHECK_TYPE(float);
-  CHECK_TYPE(double);
-  CHECK_TYPE(char2);
-  CHECK_TYPE(uchar3);
-  CHECK_TYPE(short4);
-  CHECK_TYPE(ushort8);
-  CHECK_TYPE(half16);
-  CHECK_TYPE(int2);
-  CHECK_TYPE(uint3);
-  CHECK_TYPE(long4);
-  CHECK_TYPE(schar4);
-  CHECK_TYPE(ulong8);
-  CHECK_TYPE(float16);
-  CHECK_TYPE(double2);
-
-  // Table 4.93: Scalar data type aliases supported by SYCL
-  CHECK_SIZE_TYPE_UI(s::byte, 1);
-
-  CHECK_SIZE_TYPE_I(s::cl_char, 1);
-  CHECK_SIZE_TYPE_I(s::cl_short, 2);
-  CHECK_SIZE_TYPE_I(s::cl_int, 4);
-  CHECK_SIZE_TYPE_I(s::cl_long, 8);
-
-  CHECK_SIZE_TYPE_UI(s::cl_uchar, 1);
-  CHECK_SIZE_TYPE_UI(s::cl_ushort, 2);
-  CHECK_SIZE_TYPE_UI(s::cl_uint, 4);
-  CHECK_SIZE_TYPE_UI(s::cl_ulong, 8);
-
-  CHECK_SIZE_TYPE_F(s::cl_float, 4);
-  CHECK_SIZE_TYPE_F(s::cl_double, 8);
-  CHECK_SIZE(s::cl_half, 2);
-
-  CHECK_SIZE_VEC(s::cl_char);
-  CHECK_SIZE_VEC(s::cl_schar);
-  CHECK_SIZE_VEC(s::cl_uchar);
-  CHECK_SIZE_VEC(s::cl_short);
-  CHECK_SIZE_VEC(s::cl_ushort);
-  CHECK_SIZE_VEC(s::cl_half);
-  CHECK_SIZE_VEC(s::cl_int);
-  CHECK_SIZE_VEC(s::cl_uint);
-  CHECK_SIZE_VEC(s::cl_long);
-  CHECK_SIZE_VEC(s::cl_ulong);
-  CHECK_SIZE_VEC(s::cl_float);
-  CHECK_SIZE_VEC(s::cl_double);
+  // For number of elements: 2, 3, 4, 8, 16.
+  CHECK_FOR_N(2);
+  CHECK_FOR_N(3);
+  CHECK_FOR_N(4);
+  CHECK_FOR_N(8);
+  CHECK_FOR_N(16);
+  return 0;
 }
