16eda88b59b792c48b00bba9b54963c85f31723b
Sergey Dmitriev <serguei.n.dmitriev@intel.com>
2019-08-28 08:25:10 -0700

    [SYCL] Define SYCL_EXTERNAL macro as prescribed by the spec
    Signed-off-by: Sergey Dmitriev <serguei.n.dmitriev@intel.com>

--- clang/test/Preprocessor/sycl-macro.cpp
+++ clang/test/Preprocessor/sycl-macro.cpp
@@ -3,9 +3,11 @@
 // RUN: %clang_cc1 %s -fsycl -E -dM | FileCheck --check-prefix=CHECK-ANY-SYCL %s
 // RUN: %clang_cc1 %s -fsycl-is-device -E -dM -fms-compatibility | FileCheck --check-prefix=CHECK-MSVC %s
 // CHECK-NOT:#define __SYCL_DEVICE_ONLY__ 1
+// CHECK-NOT:#define SYCL_EXTERNAL
 // CHECK-NOT:#define CL_SYCL_LANGUAGE_VERSION 121
 // CHECK-ANY-SYCL-NOT:#define __SYCL_DEVICE_ONLY__ 1
 // CHECK-ANY-SYCL:#define CL_SYCL_LANGUAGE_VERSION 121
 // CHECK-SYCL:#define CL_SYCL_LANGUAGE_VERSION 121
+// CHECK-SYCL:#define SYCL_EXTERNAL __attribute__((sycl_device))
 // CHECK-MSVC-NOT: __GNUC__
 // CHECK-MSVC-NOT: __STDC__
