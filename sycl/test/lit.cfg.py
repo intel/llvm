d5faf83daefcb34730fc2ae945872bc05d2c399a
Alexander Batashev <alexander.batashev@intel.com>
2019-09-26 11:26:50 +0300

    [SYCL] Add tests for MSVC CRT logic
    Signed-off-by: Alexander Batashev <alexander.batashev@intel.com>

--- sycl/test/lit.cfg.py
+++ sycl/test/lit.cfg.py
@@ -58,6 +58,7 @@ if 'OCL_ICD_FILENAMES' in os.environ:
 
 config.substitutions.append( ('%clang_cc1', ' ' + config.clang + ' -cc1 ') )
 config.substitutions.append( ('%clangxx', ' ' + config.clangxx + ' -I'+config.opencl_include ) )
+config.substitutions.append( ('%clang_cl', ' ' + config.clang_cl + ' /I '+config.opencl_include ) )
 config.substitutions.append( ('%clang', ' ' + config.clang + ' -I'+config.opencl_include ) )
 config.substitutions.append( ('%llvm_build_libs_dir',  config.llvm_build_libs_dir ) )
 config.substitutions.append( ('%opencl_include',  config.opencl_include ) )
