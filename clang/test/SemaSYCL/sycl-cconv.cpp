77cc19b4955c2c2ec47c5b3542e1970fdf963ce4
Premanand M Rao <premanand.m.rao@intel.com>
2019-10-22 12:08:08 -0700

    [SYCL] Suppress warning about cdecl calling convention
    Signed-off-by: Premanand M Rao <premanand.m.rao@intel.com>

--- clang/test/SemaSYCL/sycl-cconv.cpp
+++ clang/test/SemaSYCL/sycl-cconv.cpp
@@ -1,14 +1,23 @@
 // RUN: %clang_cc1 -triple spir64-unknown-windows-sycldevice -aux-triple x86_64-pc-windows-msvc -fsycl-is-device -fsyntax-only -verify %s
 
-//expected-warning@+1 {{'__cdecl' calling convention is not supported for this target}}
+// expected-no-warning@+1
 __inline __cdecl int printf(char const* const _Format, ...) { return 0; }
+// expected-no-warning@+1
+__inline __cdecl __attribute__((sycl_device)) int foo() { return 0; }
+// expected-no-warning@+1
+__inline __cdecl int moo() { return 0; }
 
 void bar() {
   printf("hello\n"); // expected-no-error
 }
 
 template <typename name, typename Func>
-__attribute__((sycl_kernel)) void kernel_single_task(Func kernelFunc) {
+// expected-no-warning@+1
+__cdecl __attribute__((sycl_kernel)) void kernel_single_task(Func kernelFunc) {
+  // expected-error@+1{{SYCL kernel cannot call a variadic function}}
+  printf("cannot call from here\n");
+  // expected-no-error@+1
+  moo();
   kernelFunc();
 }
 
