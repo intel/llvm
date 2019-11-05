307da4cf37bc1805e2d7cdcd23ca4f4e7111fdb0
Vladimir Lazarev <vladimir.lazarev@intel.com>
2019-11-05 16:17:35 +0300

    [SYCL] Adopt CI scripts to use install
    The buildbot scripts were updated to use install mechanism for
    minimizing build artifacts stored in test infrastructure.
    ICD loader is added to install directory to support CI testing.
    
    Signed-off-by: Vladimir Lazarev <vladimir.lazarev@intel.com>

--- buildbot/dependency.py
+++ buildbot/dependency.py
@@ -68,13 +68,15 @@ def do_dependency(args):
     if os.path.isdir(icd_build_dir):
         shutil.rmtree(icd_build_dir)
     os.makedirs(icd_build_dir)
-
-    cmake_cmd = ["cmake", "-G", "Ninja", ".."]
+    install_dir = os.path.join(args.obj_dir, "install")
+    cmake_cmd = ["cmake", "-G", "Ninja",
+                 "-DCMAKE_INSTALL_PREFIX={}".format(install_dir),
+                 ".." ]
     subprocess.check_call(cmake_cmd, cwd=icd_build_dir)
 
     env_tmp=os.environ
     env_tmp["C_INCLUDE_PATH"] = "{}".format(ocl_header_dir)
-    subprocess.check_call(["ninja"], env=env_tmp, cwd=icd_build_dir)
+    subprocess.check_call(["ninja", "install"], env=env_tmp, cwd=icd_build_dir)
 
     ret = True
     return ret
