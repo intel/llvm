dd220d5ede3251205fcf7c32238cdc760a04ce96
Mariya Podchishchaeva <mariya.podchishchaeva@intel.com>
2019-11-06 17:35:50 +0300

    [SYCL] Add sycl_kernel attribute for accelerated code outlining
    SYCL is single source offload programming model relying on compiler to
    separate device code (i.e. offloaded to an accelerator) from the code
    executed on the host.
    
    Here is code example of the SYCL program to demonstrate compiler
    outlining work:
    
    ```
    int foo(int x) { return ++x; }
    int bar(int x) { throw std::exception("CPU code only!"); }
    ...
    using namespace cl::sycl;
    queue Q;
    buffer<int, 1> a(range<1>{1024});
    Q.submit([&](handler& cgh) {
      auto A = a.get_access<access::mode::write>(cgh);
      cgh.parallel_for<init_a>(range<1>{1024}, [=](id<1> index) {
        A[index] = index[0] + foo(42);
      });
    }
    ...
    ```
    
    SYCL device compiler must compile lambda expression passed to
    cl::sycl::handler::parallel_for method and function foo called from this
    lambda expression for an "accelerator". SYCL device compiler also must
    ignore bar function as it's not required for offloaded code execution.
    
    This patch adds the sycl_kernel attribute, which is used to mark code
    passed to cl::sycl::handler::parallel_for as "accelerated code".
    
    Attribute must be applied to function templates which parameters include
    at least "kernel name" and "kernel function object". These parameters
    will be used to establish an ABI between the host application and
    offloaded part.
    
    Reviewers: jlebar, keryell, Naghasan, ABataev, Anastasia, bader, aaron.ballman, rjmccall, rsmith
    
    Reviewed By: keryell, bader
    
    Subscribers: mgorny, OlegM, ArturGainullin, agozillon, aaron.ballman, ebevhan, Anastasia, cfe-commits
    
    Tags: #clang
    
    Differential Revision: https://reviews.llvm.org/D60455
    
    Signed-off-by: Alexey Bader <alexey.bader@intel.com>

--- clang/lib/Sema/SemaDeclAttr.cpp
+++ clang/lib/Sema/SemaDeclAttr.cpp
@@ -6754,6 +6754,45 @@ static void handleOpenCLAccessAttr(Sema &S, Decl *D, const ParsedAttr &AL) {
   D->addAttr(::new (S.Context) OpenCLAccessAttr(S.Context, AL));
 }
 
+static void handleSYCLKernelAttr(Sema &S, Decl *D, const ParsedAttr &AL) {
+  // The 'sycl_kernel' attribute applies only to function templates.
+  const auto *FD = cast<FunctionDecl>(D);
+  const FunctionTemplateDecl *FT = FD->getDescribedFunctionTemplate();
+  assert(FT && "Function template is expected");
+
+  // Function template must have at least two template parameters.
+  const TemplateParameterList *TL = FT->getTemplateParameters();
+  if (TL->size() < 2) {
+    S.Diag(FT->getLocation(), diag::warn_sycl_kernel_num_of_template_params);
+    return;
+  }
+
+  // Template parameters must be typenames.
+  for (unsigned I = 0; I < 2; ++I) {
+    const NamedDecl *TParam = TL->getParam(I);
+    if (isa<NonTypeTemplateParmDecl>(TParam)) {
+      S.Diag(FT->getLocation(),
+             diag::warn_sycl_kernel_invalid_template_param_type);
+      return;
+    }
+  }
+
+  // Function must have at least one argument.
+  if (getFunctionOrMethodNumParams(D) != 1) {
+    S.Diag(FT->getLocation(), diag::warn_sycl_kernel_num_of_function_params);
+    return;
+  }
+
+  // Function must return void.
+  QualType RetTy = getFunctionOrMethodResultType(D);
+  if (!RetTy->isVoidType()) {
+    S.Diag(FT->getLocation(), diag::warn_sycl_kernel_return_type);
+    return;
+  }
+
+  handleSimpleAttribute<SYCLKernelAttr>(S, D, AL);
+}
+
 static void handleDestroyAttr(Sema &S, Decl *D, const ParsedAttr &A) {
   if (!cast<VarDecl>(D)->hasGlobalStorage()) {
     S.Diag(D->getLocation(), diag::err_destroy_attr_on_non_static_var)
@@ -7093,7 +7132,7 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
     handleSimpleAttribute<FlattenAttr>(S, D, AL);
     break;
   case ParsedAttr::AT_SYCLKernel:
-    handleSimpleAttribute<SYCLKernelAttr>(S, D, AL);
+    handleSYCLKernelAttr(S, D, AL);
     break;
   case ParsedAttr::AT_SYCLDevice:
     handleSYCLDeviceAttr(S, D, AL);
