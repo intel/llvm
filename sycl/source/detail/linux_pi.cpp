d0a5fd2521d2e3059c96b5a2f7d152646c9d41c5
Garima Gupta <garima.gupta@intel.com>
2019-11-06 15:00:10 -0800

    [SYCL] Using a single call to Plugin, and populating a Plugin datastructure with all function pointers. Extension to this will allow us to bind multiple plugins.
    Signed-off-by: Garima Gupta <garima.gupta@intel.com>

--- sycl/source/detail/linux_pi.cpp
+++ sycl/source/detail/linux_pi.cpp
@@ -1,6 +1,11 @@
 #include <dlfcn.h>
 #include <string>
 
+namespace cl {
+namespace sycl {
+namespace detail {
+namespace pi {
+
 void *loadOsLibrary(const std::string &PluginPath) {
   // TODO: Check if the option RTLD_NOW is correct. Explore using
   // RTLD_DEEPBIND option when there are multiple plugins.
@@ -10,3 +15,8 @@ void *loadOsLibrary(const std::string &PluginPath) {
 void *getOsLibraryFuncAddress(void *Library, const std::string &FunctionName) {
   return dlsym(Library, FunctionName.c_str());
 }
+
+} // namespace pi
+} // namespace detail
+} // namespace sycl
+} // namespace cl
--- sycl/source/detail/windows_pi.cpp
+++ sycl/source/detail/windows_pi.cpp
@@ -2,6 +2,11 @@
 #include <winreg.h>
 #include <string>
 
+namespace cl {
+namespace sycl {
+namespace detail {
+namespace pi {
+
 void *loadOsLibrary(const std::string &PluginPath) {
   return (void *)LoadLibraryA(PluginPath.c_str());
 }
@@ -9,3 +14,8 @@ void *loadOsLibrary(const std::string &PluginPath) {
 void *getOsLibraryFuncAddress(void *Library, const std::string &FunctionName) {
   return GetProcAddress((HMODULE)Library, FunctionName.c_str());
 }
+
+} // namespace pi
+} // namespace detail
+} // namespace sycl
+} // namespace cl
