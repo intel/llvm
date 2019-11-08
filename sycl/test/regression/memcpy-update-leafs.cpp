99347f3fcd1894f0f01f2d44855b2fc7a41c7108
Alexey Voronov <alexey.voronov@intel.com>
2019-11-08 15:28:49 +0300

    [SYCL] Fix handler::copy [from|to] [raw|shared] pointer.
    Case 1:
        There is a call handler copy method from a pointer (used the
        accessor range and offset of 0) to an accessor(with a range
        of 4 and an offset of 2).
    
        src_ptr = [0,1,2,3,4,5,6,7] // the data that the pointer points to
        dest_acc = [x,x,x,x,x,x,x,x] // the data that the accessor points to
        handler.copy(src_ptr, dest_acc<1>{range{4}, offset{2}}
    
        Before this patch dest_acc had: "x x 2 3 4 5 x x"
        After this patch dest_acc has:  "x x 0 1 2 3 x x"
    
        The root cause: the destination offset was used for the source.
        The fix: use the source offset for the source.
    
    Case 2:
        There is a call handler copy method from an accessor(with a range
        of 4 and an offset of 2) to a pointer (used the accessor range
        and offset of 0).
    
        src_acc = [0,1,2,3,4,5,6,7] // // the data that the accessor points to
        dest_ptr = [x,x,x,x,x,x,x,x] // the data that the pointer points to
        handler.copy(src_acc<1>{range{4}, offset{2}, dest_ptr}
    
        Before this patch had dest_ptr: "0 1 2 3 x x x x"
        After this patch has dest_ptr:  "2 3 4 5 x x x x"
    
        The root cause: the destination offset was used for the source.
        The fix: use the source offset for the source.
    
    Signed-off-by: Alexey Voronov <alexey.voronov@intel.com>

--- sycl/test/regression/memcpy-update-leafs.cpp
+++ sycl/test/regression/memcpy-update-leafs.cpp
@@ -38,7 +38,7 @@ int main() {
   return 0;
 }
 
-// CHECK: PI ---> RT::piEnqueueMemBufferWrite( Queue, DstMem, CL_FALSE, DstOffset[0], DstAccessRange[0], SrcMem + DstOffset[0], DepEvents.size(), &DepEvents[0], &OutEvent)
-// CHECK-NOT: PI ---> RT::piEnqueueMemBufferWrite( Queue, DstMem, CL_FALSE, DstOffset[0], DstAccessRange[0], SrcMem + DstOffset[0], DepEvents.size(), &DepEvents[0], &OutEvent)
+// CHECK: PI ---> RT::piEnqueueMemBufferWrite( Queue, DstMem, CL_FALSE, DstOffset[0], DstAccessRange[0], SrcMem + SrcOffset[0], DepEvents.size(), &DepEvents[0], &OutEvent)
+// CHECK-NOT: PI ---> RT::piEnqueueMemBufferWrite( Queue, DstMem, CL_FALSE, DstOffset[0], DstAccessRange[0], SrcMem + SrcOffset[0], DepEvents.size(), &DepEvents[0], &OutEvent)
 // CHECK-NOT: PI ---> (MappedPtr = RT::piEnqueueMemBufferMap( Queue->getHandleRef(), pi::cast<RT::PiMem>(Mem), CL_FALSE, Flags, AccessOffset[0], AccessRange[0], DepEvents.size(), DepEvents.empty() ? nullptr : &DepEvents[0], &OutEvent, &Error), Error)
 // CHECK-NOT: PI ---> RT::piEnqueueMemUnmap( UseExclusiveQueue ? Queue->getExclusiveQueueHandleRef() : Queue->getHandleRef(), pi::cast<RT::PiMem>(Mem), MappedPtr, DepEvents.size(), DepEvents.empty() ? nullptr : &DepEvents[0], &OutEvent)
