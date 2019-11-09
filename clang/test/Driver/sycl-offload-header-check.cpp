246a6837180ffe95a11582e3314d32795dde0af0
Michael D Toguchi <michael.d.toguchi@intel.com>
2019-11-08 21:04:20 -0800

    [Driver] Update test to properly set env variable
    Signed-off-by: Michael D Toguchi <michael.d.toguchi@intel.com>

--- clang/test/Driver/sycl-offload-header-check.cpp
+++ clang/test/Driver/sycl-offload-header-check.cpp
@@ -3,10 +3,10 @@
 // actual path name and not the environment variable setting
 // RUN: mkdir -p %t_DiRnAmE
 // invoke the compiler overriding output temp location
-// RUN: TMP=%t_dirname  \
+// RUN: env TMP=%t_dirname  \
 // RUN: %clang_cl -### -fsycl %s 2>&1 | \
 // RUN: FileCheck --check-prefix=CHECK-HEADER %s
-// RUN: TMP=%t_dirname  \
+// RUN: env TMP=%t_dirname  \
 // RUN: %clang -### -fsycl %s 2>&1 | \
 // RUN: FileCheck --check-prefix=CHECK-HEADER %s
 // CHECK-HEADER: clang{{.*}} "-fsycl-int-header=[[HEADER:.+\.h]]"
