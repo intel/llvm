c8cffcc31f6c54f75193bdc3d9c244fb91a15c4e
Dounia Khaldi <dounia.khaldi@intel.com>
2019-11-21 01:51:59 -0600

    [SYCL] Add support of 1 byte type to load and store subgroup operations (#794)
    Signed-off-by: Dounia <dounia.khaldi@intel.com>

--- sycl/include/CL/sycl/intel/sub_group.hpp
+++ sycl/include/CL/sycl/intel/sub_group.hpp
@@ -280,18 +280,26 @@ struct sub_group {
 
   template <typename T, access::address_space Space>
   typename std::enable_if<(sizeof(T) == sizeof(uint32_t) ||
-                           sizeof(T) == sizeof(uint16_t)) &&
+                           sizeof(T) == sizeof(uint16_t) ||
+                           sizeof(T) == sizeof(uint8_t)) &&
                               Space == access::address_space::global_space,
                           T>::type
   load(const multi_ptr<T, Space> src) const {
+    T data;
     if (sizeof(T) == sizeof(uint32_t)) {
       uint32_t t = __spirv_SubgroupBlockReadINTEL<uint32_t>(
           (const __attribute__((ocl_global)) uint32_t *)src.get());
-      return *((T *)(&t));
+      data = *((T *)(&t));
+    } else if (sizeof(T) == sizeof(uint16_t)) {
+      uint16_t t = __spirv_SubgroupBlockReadINTEL<uint16_t>(
+          (const __attribute__((ocl_global)) uint16_t *)src.get());
+      data = *((T *)(&t));
+    } else {
+      uint8_t t = __spirv_SubgroupBlockReadINTEL<uint8_t>(
+          (const __attribute__((ocl_global)) uint8_t *)src.get());
+      data = *((T *)(&t));
     }
-    uint16_t t = __spirv_SubgroupBlockReadINTEL<uint16_t>(
-        (const __attribute__((ocl_global)) uint16_t *)src.get());
-    return *((T *)(&t));
+    return data;
   }
 
   template <int N, typename T, access::address_space Space>
@@ -322,15 +330,19 @@ struct sub_group {
   void
   store(multi_ptr<T, Space> dst,
         const typename std::enable_if<
-            (sizeof(T) == sizeof(uint32_t) || sizeof(T) == sizeof(uint16_t)) &&
+            (sizeof(T) == sizeof(uint32_t) || sizeof(T) == sizeof(uint16_t) ||
+             sizeof(T) == sizeof(uint8_t)) &&
                 Space == access::address_space::global_space,
             T>::type &x) const {
     if (sizeof(T) == sizeof(uint32_t)) {
       __spirv_SubgroupBlockWriteINTEL<uint32_t>(
           (__attribute__((ocl_global)) uint32_t *)dst.get(), *((uint32_t *)&x));
-    } else {
+    } else if (sizeof(T) == sizeof(uint16_t)) {
       __spirv_SubgroupBlockWriteINTEL<uint16_t>(
           (__attribute__((ocl_global)) uint16_t *)dst.get(), *((uint16_t *)&x));
+    } else {
+      __spirv_SubgroupBlockWriteINTEL<uint8_t>(
+          (__attribute__((ocl_global)) uint8_t *)dst.get(), *((uint8_t *)&x));
     }
   }
 
