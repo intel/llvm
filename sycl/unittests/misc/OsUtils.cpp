af853f23d22a42a9cfc84d3b60a17812bfbb4c0d
Andrew Savonichev <andrew.savonichev@intel.com>
2019-11-22 12:43:52 +0300

    Fix backslashes again
    Signed-off-by: Andrew Savonichev <andrew.savonichev@intel.com>

--- sycl/unittests/misc/OsUtils.cpp
+++ sycl/unittests/misc/OsUtils.cpp
@@ -9,7 +9,29 @@
 #include <CL/sycl/detail/os_util.hpp>
 #include <gtest/gtest.h>
 
-#ifdef __unix__
+#ifdef _WIN32
+/// Compare for string equality, but ignore difference between forward slash (/)
+/// and backward slash (\).
+///
+/// This difference can be tricky to avoid, because CMake operates with forward
+/// slashes even on Windows, and it can be problematic to convert them when
+/// CMake generator expressions are involved. It's easier to handle slashes
+/// here in the test itself.
+bool isSameDir(const char* LHS, const char* RHS) {
+  char L = 0, R = 0;
+  do {
+    L = *LHS++;
+    R = *RHS++;
+    if (L != R) {
+      if (!((L == '\\' || L == '/') && (R == '\\' || R == '/'))) {
+        return false;
+      }
+    }
+  } while (L != '\0' && R != '\0');
+  bool SameLen = (L == '\0' && R == '\0');
+  return SameLen;
+}
+#else
 #include <sys/stat.h>
 #include <stdlib.h>
 /// Check with respect to symbolic links
@@ -26,10 +48,6 @@ bool isSameDir(const char* LHS, const char* RHS) {
   }
   ino_t InodeRHS = StatBuf.st_ino;
   return InodeRHS == InodeLHS;
-}
-#else
-bool isSameDir(const char* LHS, const char* RHS) {
-  return 0 == strcmp(LHS, RHS);
 }
 #endif
 
