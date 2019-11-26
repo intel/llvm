da968d048bc7cd45b44434c700b2cbb6b59ae31a
Alexander Batashev <alexander.batashev@intel.com>
2019-11-26 08:56:27 +0300

    [SYCL] Refactor context
    Signed-off-by: Alexander Batashev <alexander.batashev@intel.com>

--- sycl/include/CL/sycl/context.hpp
+++ sycl/include/CL/sycl/context.hpp
@@ -8,11 +8,10 @@
 
 #pragma once
 #include <CL/sycl/detail/common.hpp>
-#include <CL/sycl/detail/context_impl.hpp>
 #include <CL/sycl/exception_list.hpp>
 #include <CL/sycl/info/info_desc.hpp>
+#include <CL/sycl/detail/context_impl.hpp>
 #include <CL/sycl/stl.hpp>
-#include <memory>
 #include <type_traits>
 // 4.6.2 Context class
 
@@ -21,18 +20,19 @@ namespace sycl {
 // Forward declarations
 class device;
 class platform;
+
 class context {
 public:
-  explicit context(const async_handler &asyncHandler = {});
+  explicit context(const async_handler &AsyncHandler = {});
 
-  context(const device &dev, async_handler asyncHandler = {});
+  context(const device &Device, async_handler AsyncHandler = {});
 
-  context(const platform &plt, async_handler asyncHandler = {});
+  context(const platform &Platform, async_handler AsyncHandler = {});
 
-  context(const vector_class<device> &deviceList,
-          async_handler asyncHandler = {});
+  context(const vector_class<device> &DeviceList,
+          async_handler AsyncHandler = {});
 
-  context(cl_context clContext, async_handler asyncHandler = {});
+  context(cl_context ClContext, async_handler AsyncHandler = {});
 
   template <info::context param>
   typename info::param_traits<info::context, param>::return_type
@@ -46,9 +46,9 @@ public:
 
   context &operator=(context &&rhs) = default;
 
-  bool operator==(const context &rhs) const;
+  bool operator==(const context &rhs) const { return impl == rhs.impl; }
 
-  bool operator!=(const context &rhs) const;
+  bool operator!=(const context &rhs) const { return !(*this == rhs); }
 
   cl_context get() const;
 
@@ -59,7 +59,7 @@ public:
   vector_class<device> get_devices() const;
 
 private:
-  std::shared_ptr<detail::context_impl> impl;
+  shared_ptr_class<detail::context_impl> impl;
   template <class Obj>
   friend decltype(Obj::impl) detail::getSyclObjImpl(const Obj &SyclObject);
 
@@ -74,9 +74,9 @@ private:
 
 namespace std {
 template <> struct hash<cl::sycl::context> {
-  size_t operator()(const cl::sycl::context &c) const {
-    return hash<std::shared_ptr<cl::sycl::detail::context_impl>>()(
-        cl::sycl::detail::getSyclObjImpl(c));
+  size_t operator()(const cl::sycl::context &Context) const {
+    return hash<cl::sycl::shared_ptr_class<cl::sycl::detail::context_impl>>()(
+        cl::sycl::detail::getSyclObjImpl(Context));
   }
 };
 } // namespace std
