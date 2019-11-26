bfb3f447b92a22f0edd31584d407b32565507300
Alexander Batashev <alexander.batashev@intel.com>
2019-11-26 10:14:41 +0300

    [SYCL] Document context class
    Signed-off-by: Alexander Batashev <alexander.batashev@intel.com>

--- sycl/include/CL/sycl/context.hpp
+++ sycl/include/CL/sycl/context.hpp
@@ -25,17 +25,64 @@ class context_impl;
 
 class context {
 public:
+  /// Constructs a SYCL context instance using an instance of default_selector.
+  ///
+  /// The instance of default_selector is used to select the associated platform
+  /// and device(s).
+  /// The constructed SYCL context will use the AsyncHandler parameter to handle
+  /// exceptions.
+  ///
+  /// @param AsyncHandler is an instance of async_handler.
   explicit context(const async_handler &AsyncHandler = {});
 
+  /// Constructs a SYCL context instance using the provided device.
+  ///
+  /// Newly created context is associated with the Device and the SYCL platform
+  /// that is associated with the Device.
+  /// The constructed SYCL context will use the AsyncHandler parameter to handle
+  /// exceptions.
+  ///
+  /// @param Device is an instance of SYCL device.
+  /// @param AsyncHandler is an instance of async_handler.
   context(const device &Device, async_handler AsyncHandler = {});
 
+  /// Constructs a SYCL context instance using the provided platform.
+  ///
+  /// Newly created context is associated with the Platform and with each
+  /// SYCL device that is associated with the Platform.
+  /// The constructed SYCL context will use the AsyncHandler parameter to handle
+  /// exceptions.
+  ///
+  /// @param Platform is an instance of SYCL platform.
+  /// @param AsyncHandler is an instance of async_handler.
   context(const platform &Platform, async_handler AsyncHandler = {});
 
+  /// Constructs a SYCL context instance using list of devices.
+  ///
+  /// Newly created context will be associated with each SYCL device in the
+  /// DeviceList. This requires that all SYCL devices in the list have the same
+  /// associated SYCL platform.
+  /// The constructed SYCL context will use the AsyncHandler parameter to handle
+  /// exceptions.
+  ///
+  /// @param DeviceList is a list of SYCL device instances.
+  /// @param AsyncHandler is an instance of async_handler.
   context(const vector_class<device> &DeviceList,
           async_handler AsyncHandler = {});
 
+  /// Constructs a SYCL context instance from OpenCL cl_context.
+  ///
+  /// ClContext is retained on SYCL context instantiation.
+  /// The constructed SYCL context will use the AsyncHandler parameter to handle
+  /// exceptions.
+  ///
+  /// @param ClContext is an instance of OpenCL cl_context.
+  /// @param AsyncHandler is an instance of async_handler.
   context(cl_context ClContext, async_handler AsyncHandler = {});
 
+  /// Queries this SYCL context for information.
+  ///
+  /// The return type depends on information being queried.
   template <info::context param>
   typename info::param_traits<info::context, param>::return_type
   get_info() const;
@@ -52,12 +99,26 @@ public:
 
   bool operator!=(const context &rhs) const { return !(*this == rhs); }
 
+  /// Gets OpenCL interoperability context.
+  ///
+  /// The OpenCL cl_context handle is retained on return.
+  ///
+  /// @return a valid instance of OpenCL cl_context.
   cl_context get() const;
 
+  /// Checks if this context is a SYCL host context.
+  ///
+  /// @return true if this context is a SYCL host context.
   bool is_host() const;
 
+  /// Gets platform associated with this SYCL context.
+  ///
+  /// @return a valid instance of SYCL platform.
   platform get_platform() const;
 
+  /// Gets devices associated with this SYCL context.
+  ///
+  /// @return a list of valid SYCL device instances.
   vector_class<device> get_devices() const;
 
 private:
