dbec124a7e7ffb89881c045327c5bb4d1e8b3b6e
Alexander Batashev <alexander.batashev@intel.com>
2019-11-27 13:01:32 +0300

    [SYCL] Document platform class
    Signed-off-by: Alexander Batashev <alexander.batashev@intel.com>

--- sycl/include/CL/sycl/platform.hpp
+++ sycl/include/CL/sycl/platform.hpp
@@ -26,12 +26,29 @@ class platform_impl;
 
 class platform {
 public:
+  /// Constructs a SYCL platform as a host platform.
   platform();
 
+  /// Constructs a SYCL platform instance from an OpenCL cl_platform_id.
+  ///
+  /// The provided OpenCL platform handle is retained on SYCL platform
+  /// construction.
+  ///
+  /// @param PlatformId is an OpenCL cl_platform_id instance.
   explicit platform(cl_platform_id PlatformId);
 
+  /// Constructs a SYCL platform instance using device selector.
+  ///
+  /// One of the SYCL devices that is associated with the constructed SYCL
+  /// platform instance must be the SYCL device that is produced from the
+  /// provided device selector.
+  ///
+  /// @param DeviceSelector is an instance of SYCL device_selector.
   explicit platform(const device_selector &DeviceSelector);
 
+  /// Queries this SYCL platform for info.
+  ///
+  /// The return type depends on information being queried.
   template <info::platform param>
   typename info::param_traits<info::platform, param>::return_type
   get_info() const;
@@ -48,15 +65,38 @@ public:
 
   bool operator!=(const platform &rhs) const { return !(*this == rhs); }
 
+  /// Gets an OpenCL interoperability platform.
+  ///
+  /// @return an instance of OpenCL cl_platform_id.
   cl_platform_id get() const;
 
+  /// Checks if platform support specified extension.
+  ///
+  /// @param ExtensionName is a string containing extension name.
+  /// @return true if specified extension is supported by this SYCL platform.
   bool has_extension(const string_class &ExtensionName) const;
 
+  /// Checks if this SYCL platform is a host platform.
+  ///
+  /// @return true if this SYCL platform is a host platform.
   bool is_host() const;
 
+  /// Gets all SYCL devices associated with this platform.
+  ///
+  /// If this SYCL platform is a host platform, resulting vector contains only
+  /// a single SYCL host device. If there are no devices that match given device
+  /// type, resulting vector is empty.
+  ///
+  /// @param DeviceType is a SYCL device type.
+  /// @return a vector of SYCL devices.
   vector_class<device>
   get_devices(info::device_type DeviceType = info::device_type::all) const;
 
+  /// Gets all available SYCL platforms in the system.
+  ///
+  /// The resulting vector always contains a single SYCL host platform instance.
+  ///
+  /// @return a vector of all available SYCL platforms.
   static vector_class<platform> get_platforms();
 
 private:
