afc40ed47101b78add6c90a0e3f97dbd4244acc3
Dmitry Sidorov <dmitry.sidorov@intel.com>
2019-12-01 16:20:11 +0300

    [SYCL] Fix kernel attribute codegen
    It was failed to process correctly a case of multiple attrs
    attached to SYCL kernel.
    
    Signed-off-by: Dmitry Sidorov <dmitry.sidorov@intel.com>

---
+++ clang/test/CodeGenSYCL/sycl-multi-kernel-attr.cpp
@@ -0,0 +1,21 @@
+// RUN: %clang_cc1 -std=c++11 -triple spir64-unknown-linux-sycldevice -disable-llvm-passes -fsycl-is-device -emit-llvm -o - %s | FileCheck %s
+
+class Functor {
+public:
+  [[cl::intel_reqd_sub_group_size(4), cl::reqd_work_group_size(32, 16, 16)]] void operator()() {}
+};
+
+
+template <typename name, typename Func>
+__attribute__((sycl_kernel)) void kernel(Func kernelFunc) {
+  kernelFunc();
+}
+
+void bar() {
+  Functor foo;
+  kernel<class kernel_name>(foo);
+}
+
+// CHECK: define spir_kernel void @{{.*}}kernel_name() {{.*}} !reqd_work_group_size ![[WGSIZE:[0-9]+]] !intel_reqd_sub_group_size ![[SGSIZE:[0-9]+]]
+// CHECK: ![[WGSIZE]] = !{i32 32, i32 16, i32 16}
+// CHECK: ![[SGSIZE]] = !{i32 4}
