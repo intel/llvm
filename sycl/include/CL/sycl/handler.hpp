2a24717c9eff678ff4a77ae325fd64c3b9dfe25c
Alexander Batashev <alexander.batashev@intel.com>
2019-12-05 17:39:19 +0300

    Move some handler bits to the library
    Signed-off-by: Alexander Batashev <alexander.batashev@intel.com>

--- sycl/include/CL/sycl/handler.hpp
+++ sycl/include/CL/sycl/handler.hpp
@@ -203,155 +203,15 @@ private:
   // integration header.
   void
   extractArgsAndReqsFromLambda(char *LambdaPtr, size_t KernelArgsNum,
-                               const detail::kernel_param_desc_t *KernelArgs) {
-    const bool IsKernelCreatedFromSource = false;
-    size_t IndexShift = 0;
-    for (size_t I = 0; I < KernelArgsNum; ++I) {
-      void *Ptr = LambdaPtr + KernelArgs[I].offset;
-      const detail::kernel_param_kind_t &Kind = KernelArgs[I].kind;
-      const int &Size = KernelArgs[I].info;
-      if (Kind == detail::kernel_param_kind_t::kind_accessor) {
-        // For args kind of accessor Size is information about accessor.
-        // The first 11 bits of Size encodes the accessor target.
-        const access::target AccTarget =
-            static_cast<access::target>(Size & 0x7ff);
-        if ((AccTarget == access::target::global_buffer ||
-             AccTarget == access::target::constant_buffer) ||
-            (AccTarget == access::target::image ||
-             AccTarget == access::target::image_array)) {
-          detail::AccessorBaseHost *AccBase =
-              static_cast<detail::AccessorBaseHost *>(Ptr);
-          Ptr = detail::getSyclObjImpl(*AccBase).get();
-        } else if (AccTarget == access::target::local) {
-          detail::LocalAccessorBaseHost *LocalAccBase =
-              static_cast<detail::LocalAccessorBaseHost *>(Ptr);
-          Ptr = detail::getSyclObjImpl(*LocalAccBase).get();
-        }
-      }
-      processArg(Ptr, Kind, Size, I, IndexShift, IsKernelCreatedFromSource);
-    }
-  }
+                               const detail::kernel_param_desc_t *KernelArgs);
 
   // The method extracts and prepares kernel arguments that were set
   // via set_arg(s)
-  void extractArgsAndReqs() {
-    assert(MSyclKernel && "MSyclKernel is not initialized");
-    std::vector<detail::ArgDesc> UnPreparedArgs = std::move(MArgs);
-    MArgs.clear();
-
-    std::sort(UnPreparedArgs.begin(), UnPreparedArgs.end(),
-              [](const detail::ArgDesc &first, const detail::ArgDesc &second)
-                  -> bool { return (first.MIndex < second.MIndex); });
-
-    const bool IsKernelCreatedFromSource = MSyclKernel->isCreatedFromSource();
-
-    size_t IndexShift = 0;
-    for (size_t I = 0; I < UnPreparedArgs.size(); ++I) {
-      void *Ptr = UnPreparedArgs[I].MPtr;
-      const detail::kernel_param_kind_t &Kind = UnPreparedArgs[I].MType;
-      const int &Size = UnPreparedArgs[I].MSize;
-      const int Index = UnPreparedArgs[I].MIndex;
-      processArg(Ptr, Kind, Size, Index, IndexShift, IsKernelCreatedFromSource);
-    }
-  }
+  void extractArgsAndReqs();
 
   void processArg(void *Ptr, const detail::kernel_param_kind_t &Kind,
                   const int Size, const size_t Index, size_t &IndexShift,
-                  bool IsKernelCreatedFromSource) {
-    const auto kind_std_layout = detail::kernel_param_kind_t::kind_std_layout;
-    const auto kind_accessor = detail::kernel_param_kind_t::kind_accessor;
-    const auto kind_sampler = detail::kernel_param_kind_t::kind_sampler;
-    const auto kind_pointer = detail::kernel_param_kind_t::kind_pointer;
-
-    switch (Kind) {
-    case kind_std_layout:
-    case kind_pointer: {
-      MArgs.emplace_back(Kind, Ptr, Size, Index + IndexShift);
-      break;
-    }
-    case kind_accessor: {
-      // For args kind of accessor Size is information about accessor.
-      // The first 11 bits of Size encodes the accessor target.
-      const access::target AccTarget =
-          static_cast<access::target>(Size & 0x7ff);
-      switch (AccTarget) {
-      case access::target::global_buffer:
-      case access::target::constant_buffer: {
-        detail::Requirement *AccImpl = static_cast<detail::Requirement *>(Ptr);
-        MArgs.emplace_back(Kind, AccImpl, Size, Index + IndexShift);
-        if (!IsKernelCreatedFromSource) {
-          // Dimensionality of the buffer is 1 when dimensionality of the
-          // accessor is 0.
-          const size_t SizeAccField =
-              sizeof(size_t) * (AccImpl->MDims == 0 ? 1 : AccImpl->MDims);
-          ++IndexShift;
-          MArgs.emplace_back(kind_std_layout, &AccImpl->MAccessRange[0],
-                             SizeAccField, Index + IndexShift);
-          ++IndexShift;
-          MArgs.emplace_back(kind_std_layout, &AccImpl->MMemoryRange[0],
-                             SizeAccField, Index + IndexShift);
-          ++IndexShift;
-          MArgs.emplace_back(kind_std_layout, &AccImpl->MOffset[0],
-                             SizeAccField, Index + IndexShift);
-        }
-        break;
-      }
-      case access::target::local: {
-        detail::LocalAccessorImplHost *LAcc =
-            static_cast<detail::LocalAccessorImplHost *>(Ptr);
-        // Stream implementation creates local accessor with size per work item
-        // in work group. Number of work items is not available during stream
-        // construction, that is why size of the accessor is updated here using
-        // information about number of work items in the work group.
-        if (LAcc->PerWI)
-          LAcc->resize(MNDRDesc.LocalSize.size(), MNDRDesc.GlobalSize.size());
-        range<3> &Size = LAcc->MSize;
-        const int Dims = LAcc->MDims;
-        int SizeInBytes = LAcc->MElemSize;
-        for (int I = 0; I < Dims; ++I)
-          SizeInBytes *= Size[I];
-        MArgs.emplace_back(kind_std_layout, nullptr, SizeInBytes,
-                           Index + IndexShift);
-        if (!IsKernelCreatedFromSource) {
-          ++IndexShift;
-          const size_t SizeAccField = Dims * sizeof(Size[0]);
-          MArgs.emplace_back(kind_std_layout, &Size, SizeAccField,
-                             Index + IndexShift);
-          ++IndexShift;
-          MArgs.emplace_back(kind_std_layout, &Size, SizeAccField,
-                             Index + IndexShift);
-          ++IndexShift;
-          MArgs.emplace_back(kind_std_layout, &Size, SizeAccField,
-                             Index + IndexShift);
-        }
-        break;
-      }
-      case access::target::image:
-      case access::target::image_array: {
-        detail::Requirement *AccImpl = static_cast<detail::Requirement *>(Ptr);
-        MArgs.emplace_back(Kind, AccImpl, Size, Index + IndexShift);
-        if (!IsKernelCreatedFromSource) {
-          // TODO Handle additional kernel arguments for image class
-          // if the compiler front-end adds them.
-        }
-        break;
-      }
-      case access::target::host_image:
-      case access::target::host_buffer: {
-        throw cl::sycl::invalid_parameter_error(
-            "Unsupported accessor target case.");
-        break;
-      }
-      }
-      break;
-    }
-    case kind_sampler: {
-      MArgs.emplace_back(kind_sampler, Ptr, sizeof(sampler),
-                         Index + IndexShift);
-      break;
-    }
-    }
-  }
+                  bool IsKernelCreatedFromSource);
 
   template <typename LambdaName> bool lambdaAndKernelHaveEqualName() {
     // TODO It is unclear a kernel and a lambda/functor must to be equal or not
@@ -370,70 +230,7 @@ private:
   // returns sycl::event object representing the command group.
   // It's expected that the method is the latest method executed before
   // object destruction.
-  event finalize() {
-    sycl::event EventRet;
-    std::unique_ptr<detail::CG> CommandGroup;
-    switch (MCGType) {
-    case detail::CG::KERNEL:
-    case detail::CG::RUN_ON_HOST_INTEL:
-      CommandGroup.reset(new detail::CGExecKernel(
-          std::move(MNDRDesc), std::move(MHostKernel), std::move(MSyclKernel),
-          std::move(MArgsStorage), std::move(MAccStorage),
-          std::move(MSharedPtrStorage), std::move(MRequirements),
-          std::move(MEvents), std::move(MArgs), std::move(MKernelName),
-          std::move(MOSModuleHandle), std::move(MStreamStorage), MCGType));
-      break;
-    case detail::CG::COPY_ACC_TO_PTR:
-    case detail::CG::COPY_PTR_TO_ACC:
-    case detail::CG::COPY_ACC_TO_ACC:
-      CommandGroup.reset(new detail::CGCopy(
-          MCGType, MSrcPtr, MDstPtr, std::move(MArgsStorage),
-          std::move(MAccStorage), std::move(MSharedPtrStorage),
-          std::move(MRequirements), std::move(MEvents)));
-      break;
-    case detail::CG::FILL:
-      CommandGroup.reset(new detail::CGFill(
-          std::move(MPattern), MDstPtr, std::move(MArgsStorage),
-          std::move(MAccStorage), std::move(MSharedPtrStorage),
-          std::move(MRequirements), std::move(MEvents)));
-      break;
-    case detail::CG::UPDATE_HOST:
-      CommandGroup.reset(new detail::CGUpdateHost(
-          MDstPtr, std::move(MArgsStorage), std::move(MAccStorage),
-          std::move(MSharedPtrStorage), std::move(MRequirements),
-          std::move(MEvents)));
-      break;
-    case detail::CG::COPY_USM:
-      CommandGroup.reset(new detail::CGCopyUSM(
-          MSrcPtr, MDstPtr, MLength, std::move(MArgsStorage),
-          std::move(MAccStorage), std::move(MSharedPtrStorage),
-          std::move(MRequirements), std::move(MEvents)));
-      break;
-    case detail::CG::FILL_USM:
-      CommandGroup.reset(new detail::CGFillUSM(
-          std::move(MPattern), MDstPtr, MLength, std::move(MArgsStorage),
-          std::move(MAccStorage), std::move(MSharedPtrStorage),
-          std::move(MRequirements), std::move(MEvents)));
-      break;
-    case detail::CG::PREFETCH_USM:
-      CommandGroup.reset(new detail::CGPrefetchUSM(
-          MDstPtr, MLength, std::move(MArgsStorage), std::move(MAccStorage),
-          std::move(MSharedPtrStorage), std::move(MRequirements),
-          std::move(MEvents)));
-      break;
-    case detail::CG::NONE:
-      throw runtime_error("Command group submitted without a kernel or a "
-                          "explicit memory operation.");
-    default:
-      throw runtime_error("Unhandled type of command group");
-    }
-
-    detail::EventImplPtr Event = detail::Scheduler::getInstance().addCG(
-        std::move(CommandGroup), std::move(MQueue));
-
-    EventRet = detail::createSyclObjFromImpl<event>(Event);
-    return EventRet;
-  }
+  event finalize();
 
   // Save streams associated with this handler. Streams are then forwarded to
   // command group and flushed in the scheduler.
@@ -534,6 +331,49 @@ private:
     }
     if (SyclKernel.is_host()) {
       throw invalid_object_error("Invalid kernel type, OpenCL expected");
+    }
+  }
+
+  static id<1> getDelinearizedIndex(const range<1> Range, const size_t Index) {
+    return {Index};
+  }
+
+  static id<2> getDelinearizedIndex(const range<2> Range, const size_t Index) {
+    size_t x = Index / Range[1];
+    size_t y = Index % Range[1];
+    return {x, y};
+  }
+
+  static id<3> getDelinearizedIndex(const range<3> Range, const size_t Index) {
+    size_t x = Index / (Range[1] * Range[2]);
+    size_t y = (Index / Range[2]) % Range[1];
+    size_t z = Index % Range[2];
+    return {x, y, z};
+  }
+
+  // The method stores lambda to the template-free object and initializes
+  // kernel name, list of arguments and requirements using information from
+  // integration header.
+  template <typename KernelName, typename KernelType, int Dims,
+            typename LambdaArgType = sycl::detail::lambda_arg_type<KernelType>>
+  void StoreLambda(KernelType KernelFunc) {
+    MHostKernel.reset(
+        new detail::HostKernel<KernelType, LambdaArgType, Dims>(KernelFunc));
+
+    using KI = sycl::detail::KernelInfo<KernelName>;
+    // Empty name indicates that the compilation happens without integration
+    // header, so don't perform things that require it.
+    if (KI::getName() != "") {
+      MArgs.clear();
+      extractArgsAndReqsFromLambda(MHostKernel->getPtr(), KI::getNumParams(),
+                                   &KI::getParamDesc(0));
+      MKernelName = KI::getName();
+      MOSModuleHandle = detail::OSUtil::getOSModuleHandle(KI::getName());
+    } else {
+      // In case w/o the integration header it is necessary to process
+      // accessors from the list(which are associated with this handler) as
+      // arguments.
+      MArgs = std::move(MAssociatedAccesors);
     }
   }
 
@@ -660,32 +500,6 @@ public:
   }
 
 #endif
-
-  // The method stores lambda to the template-free object and initializes
-  // kernel name, list of arguments and requirements using information from
-  // integration header.
-  template <typename KernelName, typename KernelType, int Dims,
-            typename LambdaArgType = sycl::detail::lambda_arg_type<KernelType>>
-  void StoreLambda(KernelType KernelFunc) {
-    MHostKernel.reset(
-        new detail::HostKernel<KernelType, LambdaArgType, Dims>(KernelFunc));
-
-    using KI = sycl::detail::KernelInfo<KernelName>;
-    // Empty name indicates that the compilation happens without integration
-    // header, so don't perform things that require it.
-    if (KI::getName() != "") {
-      MArgs.clear();
-      extractArgsAndReqsFromLambda(MHostKernel->getPtr(), KI::getNumParams(),
-                                   &KI::getParamDesc(0));
-      MKernelName = KI::getName();
-      MOSModuleHandle = detail::OSUtil::getOSModuleHandle(KI::getName());
-    } else {
-      // In case w/o the integration header it is necessary to process
-      // accessors from the list(which are associated with this handler) as
-      // arguments.
-      MArgs = std::move(MAssociatedAccesors);
-    }
-  }
 
   // single_task version with a kernel represented as a lambda.
   template <typename KernelName = detail::auto_name, typename KernelType>
@@ -1112,23 +926,6 @@ public:
     // Store copy of accessor to the local storage to make sure it is alive
     // until we finish
     MAccStorage.push_back(std::move(AccImpl));
-  }
-
-  static id<1> getDelinearizedIndex(const range<1> Range, const size_t Index) {
-    return {Index};
-  }
-
-  static id<2> getDelinearizedIndex(const range<2> Range, const size_t Index) {
-    size_t x = Index / Range[1];
-    size_t y = Index % Range[1];
-    return {x, y};
-  }
-
-  static id<3> getDelinearizedIndex(const range<3> Range, const size_t Index) {
-    size_t x = Index / (Range[1] * Range[2]);
-    size_t y = (Index / Range[2]) % Range[1];
-    size_t z = Index % Range[2];
-    return {x, y, z};
   }
 
   // Checks whether it is possible to copy the source shape to the destination
