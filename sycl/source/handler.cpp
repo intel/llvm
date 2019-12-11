b3e8e38dd4d7174eec41fed653e71169ab495418
Alexander Batashev <alexander.batashev@intel.com>
2019-12-11 16:19:57 +0300

    Add copyright message
    Signed-off-by: Alexander Batashev <alexander.batashev@intel.com>

--- sycl/source/handler.cpp
+++ sycl/source/handler.cpp
@@ -1,3 +1,13 @@
+//==-------- handler.cpp --- SYCL command group handler --------*- C++ -*---==//
+//
+// Copyright (C) 2019 Intel Corporation. All rights reserved.
+//
+// The information and source code contained herein is the exclusive property
+// of Intel Corporation and may not be disclosed, examined or reproduced in
+// whole or in part without explicit written authorization from the company.
+//
+// ===--------------------------------------------------------------------=== //
+
 #include <CL/sycl/event.hpp>
 #include <CL/sycl/handler.hpp>
 #include <CL/sycl/detail/common.hpp>
