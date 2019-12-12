89886da67e7e3e6683ca832c4efd834937c94ca6
Alexey Sachkov <alexey.sachkov@intel.com>
2019-12-12 16:02:13 +0300

    [SYCL] Fix diagnostic about non-globally-visible kernel name
    Moved some dead code from CodeGenSYCL/int_header1.cpp to the new test
    
    Signed-off-by: Alexey Sachkov <alexey.sachkov@intel.com>

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -329,7 +329,7 @@ public:
   ///  Signals that subsequent parameter descriptor additions will go to
   ///  the kernel with given name. Starts new kernel invocation descriptor.
   void startKernel(StringRef KernelName, QualType KernelNameType,
-                   StringRef KernelStableName);
+                   StringRef KernelStableName, SourceLocation Loc);
 
   /// Adds a kernel parameter descriptor to current kernel invocation
   /// descriptor.
@@ -367,6 +367,8 @@ private:
     /// Kernel name with stable lambda name mangling
     std::string StableName;
 
+    SourceLocation KernelLocation;
+
     /// Descriptor of kernel actual parameters.
     SmallVector<KernelParamDesc, 8> Params;
 
@@ -381,7 +383,8 @@ private:
   }
 
   /// Emits a forward declaration for given declaration.
-  void emitFwdDecl(raw_ostream &O, const Decl *D);
+  void emitFwdDecl(raw_ostream &O, const Decl *D,
+                   SourceLocation KernelLocation);
 
   /// Emits forward declarations of classes and template classes on which
   /// declaration of given type depends. See example in the comments for the
@@ -390,10 +393,14 @@ private:
   ///     stream to emit to
   /// \param T
   ///     type to emit forward declarations for
+  /// \param KernelLocation
+  ///     source location of the SYCL kernel function, used to emit nicer
+  ///     diagnostic messages if kernel name is missing
   /// \param Emitted
   ///     a set of declarations forward declrations has been emitted for already
   void emitForwardClassDecls(raw_ostream &O, QualType T,
-                             llvm::SmallPtrSetImpl<const void*> &Emitted);
+                             SourceLocation KernelLocation,
+                             llvm::SmallPtrSetImpl<const void *> &Emitted);
 
 private:
   /// Keeps invocation descriptors for each kernel invocation started by
