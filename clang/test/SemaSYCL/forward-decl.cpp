a91677a16785acb82c463226aaaf90508012ed1e
Erich Keane <erich.keane@intel.com>
2019-12-19 07:23:23 -0800

    [SYCL] Simplify forward-decl.cpp test as suggested by @bader
    Signed-off-by: Erich Keane <erich.keane@intel.com>

--- clang/test/SemaSYCL/forward-decl.cpp
+++ clang/test/SemaSYCL/forward-decl.cpp
@@ -1,5 +1,4 @@
-// RUN: %clang_cc1 -x c++ -DNOVIRTUAL -fsycl-is-device -std=c++11 -fsyntax-only -verify -pedantic %s
-// RUN: %clang_cc1 -x c++ -DVIRTUAL -fsycl-is-device -std=c++11 -fsyntax-only -verify -pedantic %s
+// RUN: %clang_cc1 -x c++ -fsycl-is-device -std=c++11 -fsyntax-only -verify -pedantic %s
 
 // expected-no-diagnostics
 template <typename name, typename Func>
@@ -8,21 +7,16 @@ __attribute__((sycl_kernel)) void kernel_single_task(Func kernelFunc) {
 }
 
 int main() {
-#ifdef NOVIRTUAL
-  kernel_single_task<class kernel_function_1>([]() {
+  kernel_single_task<class kernel_function>([]() {
       class Foo *F;
-  });
-#elif VIRTUAL
-  kernel_single_task<class kernel_function_2>([]() {
       class Boo {
       public:
         virtual int getBoo() { return 42; }
       };
   });
 
-  kernel_single_task<class kernel_function_3>([]() {
+  kernel_single_task<class kernel_function_2>([]() {
       class Boo *B;
   });
-#endif // VIRTUAL
   return 0;
 }
