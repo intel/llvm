74375584fa960fdb9adda2001c77465ae3fd1c38
Alexey Bader <alexey.bader@intel.com>
2019-12-25 15:56:29 +0300

    [SYCL] Fix compiler crash
    Signed-off-by: Alexey Bader <alexey.bader@intel.com>

---
+++ clang/test/CodeGenSYCL/kernel-name.cpp
@@ -0,0 +1,20 @@
+// RUN: %clang_cc1 -triple spir64-unknown-linux-sycldevice -std=c++11 -fsycl-is-device -S -emit-llvm -x c++ %s -o - | FileCheck %s
+
+inline namespace cl {
+  namespace sycl {
+    class kernel {};
+  }
+}
+
+using namespace cl::sycl;
+
+template <typename name, typename Func>
+__attribute__((sycl_kernel)) void kernel_single_task(Func kernelFunc) {
+  kernelFunc();
+}
+
+// CHECK: define spir_kernel {{.*}}2cl4sycl6kernel
+int main() {
+  kernel_single_task<class kernel>([]() {});
+  return 0;
+}
