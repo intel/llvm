6a23c0fe9b5744602bdea6a8f15ba126a840c798
Artur Gainullin <artur.gainullin@intel.com>
2020-01-09 13:58:27 -0800

    [SYCL] Fix requirements for aot tests
    Currently tests for aot require only existence of the tools for aot in
    the PATH. But this is not enough, for successful aot compilation
    corresponding platform must be available.
    
    Signed-off-by: Artur Gainullin <artur.gainullin@intel.com>

--- sycl/test/device-code-split/aot-gpu.cpp
+++ sycl/test/device-code-split/aot-gpu.cpp
@@ -1,4 +1,4 @@
-// REQUIRES: ocloc
+// REQUIRES: ocloc, gpu
 
 // RUN: %clangxx -fsycl -fsycl-device-code-split=per_source -fsycl-targets=spir64_gen-unknown-unknown-sycldevice -Xsycl-target-backend=spir64_gen-unknown-unknown-sycldevice "-device skl" -I %S/Inputs -o %t.out %S/split-per-source-main.cpp %S/Inputs/split-per-source-second-file.cpp
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
