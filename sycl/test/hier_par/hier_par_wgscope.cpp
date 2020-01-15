e9ce0a2c11796473637ff94438b52a5491b0cfe8
Artem Gindinson <artem.gindinson@intel.com>
2020-01-15 12:38:42 +0300

    [SYCL][NFC] Remove TODO comments from the already-fixed tests
    Signed-off-by: Artem Gindinson <artem.gindinson@intel.com>

--- sycl/test/hier_par/hier_par_wgscope.cpp
+++ sycl/test/hier_par/hier_par_wgscope.cpp
@@ -8,7 +8,6 @@
 
 // RUN: %clangxx -fsycl %s -o %t.out
 // RUN: env SYCL_DEVICE_TYPE=HOST %t.out
-// TODO: SYCL specific fail - analyze and enable on Windows
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 // RUN: %ACC_RUN_PLACEHOLDER %t.out
