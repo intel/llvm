d6830e2ecf0e0f4b97301fbfca5450e8fa295269
Mariya Podchishchaeva <mariya.podchishchaeva@intel.com>
2020-01-20 14:33:22 +0300

    Apply comment
    Signed-off-by: Mariya Podchishchaeva <mariya.podchishchaeva@intel.com>

--- clang/test/SemaSYCL/non-std-layout-param.cpp
+++ clang/test/SemaSYCL/non-std-layout-param.cpp
@@ -1,4 +1,5 @@
 // RUN: %clang_cc1 -fsycl-is-device -verify -fsyntax-only %s
+// RUN: %clang_cc1 -fsycl-is-device -fsycl-new-kernel-param-requirements -fsyntax-only %s
 
 // This test checks if compiler reports compilation error on an attempt to pass
 // non-standard layout struct object as SYCL kernel parameter.
