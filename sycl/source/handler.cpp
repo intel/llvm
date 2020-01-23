21969ef64969d36b2ecf60eb7af63b4148f40c00
Alexander Batashev <alexander.batashev@intel.com>
2020-01-23 11:12:23 +0300

    Re-run clang-format after rebase
    Signed-off-by: Alexander Batashev <alexander.batashev@intel.com>

--- sycl/source/handler.cpp
+++ sycl/source/handler.cpp
@@ -91,99 +91,97 @@ event handler::finalize() {
 void handler::processArg(void *Ptr, const detail::kernel_param_kind_t &Kind,
                          const int Size, const size_t Index, size_t &IndexShift,
                          bool IsKernelCreatedFromSource) {
-    const auto kind_std_layout = detail::kernel_param_kind_t::kind_std_layout;
-    const auto kind_accessor = detail::kernel_param_kind_t::kind_accessor;
-    const auto kind_sampler = detail::kernel_param_kind_t::kind_sampler;
-    const auto kind_pointer = detail::kernel_param_kind_t::kind_pointer;
+  const auto kind_std_layout = detail::kernel_param_kind_t::kind_std_layout;
+  const auto kind_accessor = detail::kernel_param_kind_t::kind_accessor;
+  const auto kind_sampler = detail::kernel_param_kind_t::kind_sampler;
+  const auto kind_pointer = detail::kernel_param_kind_t::kind_pointer;
 
-    switch (Kind) {
-    case kind_std_layout:
-    case kind_pointer: {
-      MArgs.emplace_back(Kind, Ptr, Size, Index + IndexShift);
-      break;
-    }
-    case kind_accessor: {
-      // For args kind of accessor Size is information about accessor.
-      // The first 11 bits of Size encodes the accessor target.
-      const access::target AccTarget =
-          static_cast<access::target>(Size & 0x7ff);
-      switch (AccTarget) {
-      case access::target::global_buffer:
-      case access::target::constant_buffer: {
-        detail::Requirement *AccImpl = static_cast<detail::Requirement *>(Ptr);
-        MArgs.emplace_back(Kind, AccImpl, Size, Index + IndexShift);
-        if (!IsKernelCreatedFromSource) {
-          // Dimensionality of the buffer is 1 when dimensionality of the
-          // accessor is 0.
-          const size_t SizeAccField =
-              sizeof(size_t) * (AccImpl->MDims == 0 ? 1 : AccImpl->MDims);
-          ++IndexShift;
-          MArgs.emplace_back(kind_std_layout, &AccImpl->MAccessRange[0],
-                             SizeAccField, Index + IndexShift);
-          ++IndexShift;
-          MArgs.emplace_back(kind_std_layout, &AccImpl->MMemoryRange[0],
-                             SizeAccField, Index + IndexShift);
-          ++IndexShift;
-          MArgs.emplace_back(kind_std_layout, &AccImpl->MOffset[0],
-                             SizeAccField, Index + IndexShift);
-        }
-        break;
-      }
-      case access::target::local: {
-        detail::LocalAccessorImplHost *LAcc =
-            static_cast<detail::LocalAccessorImplHost *>(Ptr);
-        // Stream implementation creates local accessor with size per work item
-        // in work group. Number of work items is not available during stream
-        // construction, that is why size of the accessor is updated here using
-        // information about number of work items in the work group.
-        if (LAcc->PerWI)
-          LAcc->resize(MNDRDesc.LocalSize.size(), MNDRDesc.GlobalSize.size());
-        range<3> &Size = LAcc->MSize;
-        const int Dims = LAcc->MDims;
-        int SizeInBytes = LAcc->MElemSize;
-        for (int I = 0; I < Dims; ++I)
-          SizeInBytes *= Size[I];
-        MArgs.emplace_back(kind_std_layout, nullptr, SizeInBytes,
+  switch (Kind) {
+  case kind_std_layout:
+  case kind_pointer: {
+    MArgs.emplace_back(Kind, Ptr, Size, Index + IndexShift);
+    break;
+  }
+  case kind_accessor: {
+    // For args kind of accessor Size is information about accessor.
+    // The first 11 bits of Size encodes the accessor target.
+    const access::target AccTarget = static_cast<access::target>(Size & 0x7ff);
+    switch (AccTarget) {
+    case access::target::global_buffer:
+    case access::target::constant_buffer: {
+      detail::Requirement *AccImpl = static_cast<detail::Requirement *>(Ptr);
+      MArgs.emplace_back(Kind, AccImpl, Size, Index + IndexShift);
+      if (!IsKernelCreatedFromSource) {
+        // Dimensionality of the buffer is 1 when dimensionality of the
+        // accessor is 0.
+        const size_t SizeAccField =
+            sizeof(size_t) * (AccImpl->MDims == 0 ? 1 : AccImpl->MDims);
+        ++IndexShift;
+        MArgs.emplace_back(kind_std_layout, &AccImpl->MAccessRange[0],
+                           SizeAccField, Index + IndexShift);
+        ++IndexShift;
+        MArgs.emplace_back(kind_std_layout, &AccImpl->MMemoryRange[0],
+                           SizeAccField, Index + IndexShift);
+        ++IndexShift;
+        MArgs.emplace_back(kind_std_layout, &AccImpl->MOffset[0], SizeAccField,
                            Index + IndexShift);
-        if (!IsKernelCreatedFromSource) {
-          ++IndexShift;
-          const size_t SizeAccField = Dims * sizeof(Size[0]);
-          MArgs.emplace_back(kind_std_layout, &Size, SizeAccField,
-                             Index + IndexShift);
-          ++IndexShift;
-          MArgs.emplace_back(kind_std_layout, &Size, SizeAccField,
-                             Index + IndexShift);
-          ++IndexShift;
-          MArgs.emplace_back(kind_std_layout, &Size, SizeAccField,
-                             Index + IndexShift);
-        }
-        break;
-      }
-      case access::target::image:
-      case access::target::image_array: {
-        detail::Requirement *AccImpl = static_cast<detail::Requirement *>(Ptr);
-        MArgs.emplace_back(Kind, AccImpl, Size, Index + IndexShift);
-        if (!IsKernelCreatedFromSource) {
-          // TODO Handle additional kernel arguments for image class
-          // if the compiler front-end adds them.
-        }
-        break;
-      }
-      case access::target::host_image:
-      case access::target::host_buffer: {
-        throw cl::sycl::invalid_parameter_error(
-            "Unsupported accessor target case.");
-        break;
-      }
       }
       break;
     }
-    case kind_sampler: {
-      MArgs.emplace_back(kind_sampler, Ptr, sizeof(sampler),
+    case access::target::local: {
+      detail::LocalAccessorImplHost *LAcc =
+          static_cast<detail::LocalAccessorImplHost *>(Ptr);
+      // Stream implementation creates local accessor with size per work item
+      // in work group. Number of work items is not available during stream
+      // construction, that is why size of the accessor is updated here using
+      // information about number of work items in the work group.
+      if (LAcc->PerWI)
+        LAcc->resize(MNDRDesc.LocalSize.size(), MNDRDesc.GlobalSize.size());
+      range<3> &Size = LAcc->MSize;
+      const int Dims = LAcc->MDims;
+      int SizeInBytes = LAcc->MElemSize;
+      for (int I = 0; I < Dims; ++I)
+        SizeInBytes *= Size[I];
+      MArgs.emplace_back(kind_std_layout, nullptr, SizeInBytes,
                          Index + IndexShift);
+      if (!IsKernelCreatedFromSource) {
+        ++IndexShift;
+        const size_t SizeAccField = Dims * sizeof(Size[0]);
+        MArgs.emplace_back(kind_std_layout, &Size, SizeAccField,
+                           Index + IndexShift);
+        ++IndexShift;
+        MArgs.emplace_back(kind_std_layout, &Size, SizeAccField,
+                           Index + IndexShift);
+        ++IndexShift;
+        MArgs.emplace_back(kind_std_layout, &Size, SizeAccField,
+                           Index + IndexShift);
+      }
+      break;
+    }
+    case access::target::image:
+    case access::target::image_array: {
+      detail::Requirement *AccImpl = static_cast<detail::Requirement *>(Ptr);
+      MArgs.emplace_back(Kind, AccImpl, Size, Index + IndexShift);
+      if (!IsKernelCreatedFromSource) {
+        // TODO Handle additional kernel arguments for image class
+        // if the compiler front-end adds them.
+      }
+      break;
+    }
+    case access::target::host_image:
+    case access::target::host_buffer: {
+      throw cl::sycl::invalid_parameter_error(
+          "Unsupported accessor target case.");
       break;
     }
     }
+    break;
+  }
+  case kind_sampler: {
+    MArgs.emplace_back(kind_sampler, Ptr, sizeof(sampler), Index + IndexShift);
+    break;
+  }
+  }
 }
 
 void handler::extractArgsAndReqs() {
@@ -231,11 +229,11 @@ void handler::extractArgsAndReqsFromLambda(
         detail::AccessorBaseHost *AccBase =
             static_cast<detail::AccessorBaseHost *>(Ptr);
         Ptr = detail::getSyclObjImpl(*AccBase).get();
-    } else if (AccTarget == access::target::local) {
+      } else if (AccTarget == access::target::local) {
         detail::LocalAccessorBaseHost *LocalAccBase =
             static_cast<detail::LocalAccessorBaseHost *>(Ptr);
         Ptr = detail::getSyclObjImpl(*LocalAccBase).get();
-    }
+      }
     }
     processArg(Ptr, Kind, Size, I, IndexShift, IsKernelCreatedFromSource);
   }
