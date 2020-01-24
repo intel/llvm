9c44833a56dac551f2228ffa0c31b1b1b27b0db4
Dmitry Sidorov <dmitry.sidorov@intel.com>
2020-01-24 13:31:42 +0300

    [SYCL][FPGA] Add io_pipe_id attribute
    This attirubte is applicabale to pipe storage decl.
    It's expected to be used only in SYCL headers to distinguish
    I/O pipes from ordinar pipes and from each other.
    
    Signed-off-by: Dmitry Sidorov <dmitry.sidorov@intel.com>

--- clang/test/SemaSYCL/fpga_pipes.cpp
+++ clang/test/SemaSYCL/fpga_pipes.cpp
@@ -14,3 +14,35 @@ using type4 = __attribute__((pipe(0))) const int;
 
 // expected-error@+1{{'pipe' attribute takes one argument}}
 using type5 = __attribute__((pipe)) const int;
+
+struct pipe_storage {};
+
+// no error expected
+const pipe_storage Storage1 __attribute__((io_pipe_id(1)));
+
+// expected-error@+1{{'io_pipe_id' attribute requires a non-negative integral compile time constant expression}}
+const pipe_storage Storage2 __attribute__((io_pipe_id(-11)));
+
+// expected-error@+1{{'io_pipe_id' attribute requires an integer constant}}
+const pipe_storage Storage3 __attribute__((io_pipe_id("abc")));
+
+// expected-error@+1{{'io_pipe_id' attribute only applies to SYCL pipe storage declaration}}
+int Storage4 __attribute__((io_pipe_id(5)));
+
+// expected-error@+2{{'io_pipe_id' attribute requires a non-negative integral compile time constant expression}}
+template <int N>
+pipe_storage Storage5 __attribute__((io_pipe_id(N)));
+
+template <typename name, typename Func>
+__attribute__((sycl_kernel)) void kernel_single_task(Func kernelFunc) {
+  kernelFunc();
+}
+
+void foo(pipe_storage PS) {}
+
+int main() {
+  // no error expected
+  foo(Storage5<2>);
+  // expected-note@+1{{in instantiation of variable template specialization 'Storage5' requested here}}
+  foo(Storage5<-1>);
+}
