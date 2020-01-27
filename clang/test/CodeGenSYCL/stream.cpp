a2e73ad7aeac26e008b53b1927c0d1e430ae4eea
Sergey Kanaev <sergey.kanaev@intel.com>
2020-01-27 15:38:06 +0300

    [SYCL] Improve test
    Signed-off-by: Sergey Kanaev <sergey.kanaev@intel.com>

--- clang/test/CodeGenSYCL/stream.cpp
+++ clang/test/CodeGenSYCL/stream.cpp
@@ -1,6 +1,7 @@
 // RUN: %clang_cc1 -triple spir64-unknown-unknown-sycldevice -I %S/Inputs -fsycl-is-device -disable-llvm-passes -emit-llvm %s -o %t.ll
 // RUN: FileCheck < %t.ll --enable-var-scope %s
 //
+// CHECK: define spir_kernel void @"{{.*}}StreamTester"(%"{{.*}}cl::sycl::stream"* byval(%"{{.*}}cl::sycl::stream") {{.*}}){{.*}}
 // CHECK: call spir_func void @{{.*}}__init{{.*}}(%{{.*}}cl::sycl::stream{{.*}} addrspace(4)* %{{[0-9]+}})
 // CHECK: call spir_func void @{{.*}}__finalize{{.*}}(%{{.*}}cl::sycl::stream{{.*}} addrspace(4)* %{{[0-9]+}})
 //
