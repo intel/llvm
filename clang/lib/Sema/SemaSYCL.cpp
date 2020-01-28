86241ca8ea2c15def536c56fdc73145c0a3f3d1a
Sergey Kanaev <sergey.kanaev@intel.com>
2020-01-28 17:36:46 +0300

    [SYCL] Allow for calls of builtins
    Signed-off-by: Sergey Kanaev <sergey.kanaev@intel.com>

--- clang/lib/Sema/SemaSYCL.cpp
+++ clang/lib/Sema/SemaSYCL.cpp
@@ -269,7 +269,8 @@ public:
               std::make_pair(e, e->getExprLoc()));
 
         if ((it != EvaluatabilityMap.end() && it->second) &&
-            !Callee->isDefined() && !Callee->hasAttr<SYCLDeviceAttr>())
+            (!Callee->isDefined() && !Callee->hasAttr<SYCLDeviceAttr>()) &&
+            !Callee->getBuiltinID())
           SemaRef.Diag(e->getExprLoc(), diag::err_sycl_restrict)
               << Sema::KernelCallUndefinedFunction;
       }
