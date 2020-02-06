952a3139ee44fc8cc27a63619a308d5ad3012f19
Alexander Batashev <alexander.batashev@intel.com>
2020-02-06 13:29:07 +0300

    [SYCL] Use SYCL containers in accessor_impl.hpp
    Signed-off-by: Alexander Batashev <alexander.batashev@intel.com>

--- sycl/include/CL/sycl/detail/accessor_impl.hpp
+++ sycl/include/CL/sycl/detail/accessor_impl.hpp
@@ -13,8 +13,7 @@
 #include <CL/sycl/detail/sycl_mem_obj_i.hpp>
 #include <CL/sycl/id.hpp>
 #include <CL/sycl/range.hpp>
-
-#include <memory>
+#include <CL/sycl/stl.hpp>
 
 __SYCL_INLINE namespace cl {
 namespace sycl {
@@ -100,7 +99,7 @@ public:
   Command *MBlockedCmd = nullptr;
 };
 
-using AccessorImplPtr = std::shared_ptr<AccessorImplHost>;
+using AccessorImplPtr = shared_ptr_class<AccessorImplHost>;
 
 class AccessorBaseHost {
 public:
@@ -108,9 +107,9 @@ public:
                    access::mode AccessMode, detail::SYCLMemObjI *SYCLMemObject,
                    int Dims, int ElemSize, int OffsetInBytes = 0,
                    bool IsSubBuffer = false) {
-    impl = std::make_shared<AccessorImplHost>(
+    impl = shared_ptr_class<AccessorImplHost>(new AccessorImplHost(
         Offset, AccessRange, MemoryRange, AccessMode, SYCLMemObject, Dims,
-        ElemSize, OffsetInBytes, IsSubBuffer);
+        ElemSize, OffsetInBytes, IsSubBuffer));
   }
 
 protected:
@@ -160,12 +159,13 @@ public:
   }
 };
 
-using LocalAccessorImplPtr = std::shared_ptr<LocalAccessorImplHost>;
+using LocalAccessorImplPtr = shared_ptr_class<LocalAccessorImplHost>;
 
 class LocalAccessorBaseHost {
 public:
   LocalAccessorBaseHost(sycl::range<3> Size, int Dims, int ElemSize) {
-    impl = std::make_shared<LocalAccessorImplHost>(Size, Dims, ElemSize);
+    impl = shared_ptr_class<LocalAccessorImplHost>(
+        new LocalAccessorImplHost(Size, Dims, ElemSize));
   }
   sycl::range<3> &getSize() { return impl->MSize; }
   const sycl::range<3> &getSize() const { return impl->MSize; }
@@ -181,7 +181,7 @@ protected:
   template <class Obj>
   friend decltype(Obj::impl) getSyclObjImpl(const Obj &SyclObject);
 
-  std::shared_ptr<LocalAccessorImplHost> impl;
+  shared_ptr_class<LocalAccessorImplHost> impl;
 };
 
 using Requirement = AccessorImplHost;
