6c9a3fb9ed46cc892bc3d7047107149408b922c6
Alexander Batashev <alexander.batashev@intel.com>
2020-02-16 11:37:08 +0300

    Remove constexpr magic since it does not work at all
    Signed-off-by: Alexander Batashev <alexander.batashev@intel.com>

--- sycl/include/CL/sycl/detail/sycl_assert.hpp
+++ sycl/include/CL/sycl/detail/sycl_assert.hpp
@@ -1,5 +1,14 @@
+//==----------- sycl_assert.hpp - SYCL standard header file ----------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include <CL/sycl/detail/defines.hpp>
 
+#include <cstdint>
 #include <iostream>
 #include <string>
 
@@ -7,28 +16,13 @@
 #ifndef __SYCL_DEVICE_ONLY__
 #define __SYCL_STR(X) #X
 #define __SYCL_XSTR(X) __SYCL_STR(X)
-#ifdef WIN32
-#define _SEP '\\'
-#else
-#define _SEP '/'
-#endif
-#define __SYCL_FILE__                                                          \
-  ({                                                                           \
-    static const int32_t Idx = sycl::detail::filename(__FILE__);               \
-    __FILE__ ":" __SYCL_XSTR(__LINE__) + Idx;                                  \
-  })
+#define __SYCL_FILE__ __FILE__ ":" __LINE__ 
 #define __SYCL_ASSERT(X, ...)                                                  \
   sycl::detail::sycl_assert(__SYCL_FILE__, X, __SYCL_XSTR(X), __VA_ARGS__)
 
 __SYCL_INLINE_NAMESPACE(cl) {
 namespace sycl {
 namespace detail {
-constexpr int32_t filename(const char *const Path, const int32_t Idx = 0,
-                           const int32_t SlashIdx = -1) {
-  return Path[Idx] ? (Path[Idx] == _SEP ? filename(Path, Idx + 1, Idx)
-                                       : filename(Path, Idx + 1, SlashIdx))
-                   : SlashIdx + 1;
-}
 template <typename... Args>
 void sycl_assert(const std::string &FileName, bool Cond,
                  const std::string &CondStr, Args &&... Messages) {
@@ -43,10 +37,10 @@ void sycl_assert(const std::string &FileName, bool Cond,
 } // namespace detail
 } // namespace sycl
 } // __SYCL_INLINE_NAMESPACE(cl)
-#undef _SEP
-#else
+#else // #ifdef __SYCL_DEVICE_ONLY__
 #define __SYCL_ASSERT(X, ...) assert(X)
 #endif // #ifdef __SYCL_DEVICE_ONLY__
 #else  // #ifdef __SYCL_ENABLE_ASSERTIONS__
 #define __SYCL_ASSERT(X, ...)
 #endif // #ifdef __SYCL_ENABLE_ASSERTIONS__
+
