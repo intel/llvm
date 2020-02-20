765d43bc57c589b4eb66a83300735c07be3d523b
Chris Perkins <chris.perkins@intel.com>
2020-02-20 11:34:04 -0800

    remove braces, per PR request
    Signed-off-by: Chris Perkins <chris.perkins@intel.com>

--- clang/lib/Sema/SemaType.cpp
+++ clang/lib/Sema/SemaType.cpp
@@ -2342,11 +2342,10 @@ QualType Sema::BuildArrayType(QualType T, ArrayType::ArraySizeModifier ASM,
       }
 
       // SYCL kernels reject zero length arrays
-      if (getLangOpts().SYCLIsDevice) {
+      if (getLangOpts().SYCLIsDevice) 
         SYCLDiagIfDeviceCode(ArraySize->getBeginLoc(),
                              diag::err_typecheck_zero_array_size)
             << ArraySize->getSourceRange();
-      }
     } else if (!T->isDependentType() && !T->isVariablyModifiedType() &&
                !T->isIncompleteType() && !T->isUndeducedType()) {
       // Is the array too large?
