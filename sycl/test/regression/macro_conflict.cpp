2a61c01cadfb14a25ff378b4d7b0d31da7dfc883
Byoungro So <byoungro.so@intel.com>
2020-02-26 08:25:24 -0800

    fixed regression test compiler option
    Signed-off-by: Byoungro So <byoungro.so@intel.com>

--- sycl/test/regression/macro_conflict.cpp
+++ sycl/test/regression/macro_conflict.cpp
@@ -1,12 +1,5 @@
-// RUN: %clangxx -fsycl %s -o %t.out
-// RUN: %ACC_RUN_PLACEHOLDER %t.out
+// RUN: %clangxx -fsyntax-only -Xjclang -verify %s -o %t.out
 // expected-no-diagnostics
-//
-//==-------------- macro_conflict.cpp --------------------------------------==//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
 // This test checks if the user-defined macros SUCCESS, FAIL, BLOCKED are
