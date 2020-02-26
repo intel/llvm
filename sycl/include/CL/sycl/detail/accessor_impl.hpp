e645ee2381d5bc73cbbf0cc19c4a0b5554dcab97
Alexander Batashev <alexander.batashev@intel.com>
2020-02-26 15:02:06 +0300

    [SYCL] Move host accessor details iside library
    Signed-off-by: Alexander Batashev <alexander.batashev@intel.com>

--- sycl/include/CL/sycl/detail/accessor_impl.hpp
+++ sycl/include/CL/sycl/detail/accessor_impl.hpp
@@ -37,8 +37,7 @@ public:
   range<Dims> MemRange;
 
   bool operator==(const AccessorImplDevice &Rhs) const {
-    return (Offset == Rhs.Offset &&
-            AccessRange == Rhs.AccessRange &&
+    return (Offset == Rhs.Offset && AccessRange == Rhs.AccessRange &&
             MemRange == Rhs.MemRange);
   }
 };
@@ -64,20 +63,11 @@ public:
   AccessorImplHost(id<3> Offset, range<3> AccessRange, range<3> MemoryRange,
                    access::mode AccessMode, detail::SYCLMemObjI *SYCLMemObject,
                    int Dims, int ElemSize, int OffsetInBytes = 0,
-                   bool IsSubBuffer = false)
-      : MOffset(Offset), MAccessRange(AccessRange), MMemoryRange(MemoryRange),
-        MAccessMode(AccessMode), MSYCLMemObj(SYCLMemObject), MDims(Dims),
-        MElemSize(ElemSize), MOffsetInBytes(OffsetInBytes),
-        MIsSubBuffer(IsSubBuffer) {}
+                   bool IsSubBuffer = false);
 
   ~AccessorImplHost();
 
-  AccessorImplHost(const AccessorImplHost &Other)
-      : MOffset(Other.MOffset), MAccessRange(Other.MAccessRange),
-        MMemoryRange(Other.MMemoryRange), MAccessMode(Other.MAccessMode),
-        MSYCLMemObj(Other.MSYCLMemObj), MDims(Other.MDims),
-        MElemSize(Other.MElemSize), MOffsetInBytes(Other.MOffsetInBytes),
-        MIsSubBuffer(Other.MIsSubBuffer) {}
+  AccessorImplHost(const AccessorImplHost &Other);
 
   id<3> MOffset;
   // The size of accessing region.
@@ -105,11 +95,7 @@ public:
   AccessorBaseHost(id<3> Offset, range<3> AccessRange, range<3> MemoryRange,
                    access::mode AccessMode, detail::SYCLMemObjI *SYCLMemObject,
                    int Dims, int ElemSize, int OffsetInBytes = 0,
-                   bool IsSubBuffer = false) {
-    impl = shared_ptr_class<AccessorImplHost>(new AccessorImplHost(
-        Offset, AccessRange, MemoryRange, AccessMode, SYCLMemObject, Dims,
-        ElemSize, OffsetInBytes, IsSubBuffer));
-  }
+                   bool IsSubBuffer = false);
 
 protected:
   id<3> &getOffset() { return impl->MOffset; }
@@ -131,9 +117,7 @@ protected:
 
 class LocalAccessorImplHost {
 public:
-  LocalAccessorImplHost(sycl::range<3> Size, int Dims, int ElemSize)
-      : MSize(Size), MDims(Dims), MElemSize(ElemSize),
-        MMem(Size[0] * Size[1] * Size[2] * ElemSize) {}
+  LocalAccessorImplHost(sycl::range<3> Size, int Dims, int ElemSize);
 
   sycl::range<3> MSize;
   int MDims;
@@ -143,29 +127,15 @@ public:
   bool PerWI = false;
   size_t LocalMemSize;
   size_t MaxWGSize;
-  void resize(size_t LocalSize, size_t GlobalSize) {
-    if (GlobalSize != 1 && LocalSize != 1) {
-      // If local size is not specified then work group size is chosen by
-      // runtime. That is why try to allocate based on max work group size or
-      // global size. In the worst case allocate 80% of local memory.
-      size_t MinEstWGSize = LocalSize ? LocalSize : GlobalSize;
-      MinEstWGSize = MinEstWGSize > MaxWGSize ? MaxWGSize : MinEstWGSize;
-      size_t NewSize = MinEstWGSize * MSize[0];
-      MSize[0] =
-          NewSize > 8 * LocalMemSize / 10 ? 8 * LocalMemSize / 10 : NewSize;
-      MMem.resize(NewSize * MElemSize);
-    }
-  }
+  void resize(size_t LocalSize, size_t GlobalSize);
 };
 
 using LocalAccessorImplPtr = shared_ptr_class<LocalAccessorImplHost>;
 
 class LocalAccessorBaseHost {
 public:
-  LocalAccessorBaseHost(sycl::range<3> Size, int Dims, int ElemSize) {
-    impl = shared_ptr_class<LocalAccessorImplHost>(
-        new LocalAccessorImplHost(Size, Dims, ElemSize));
-  }
+  LocalAccessorBaseHost(sycl::range<3> Size, int Dims, int ElemSize);
+
   sycl::range<3> &getSize() { return impl->MSize; }
   const sycl::range<3> &getSize() const { return impl->MSize; }
   void *getPtr() { return impl->MMem.data(); }
--- sycl/source/detail/accessor_impl.cpp
+++ sycl/source/detail/accessor_impl.cpp
@@ -13,10 +13,63 @@
 __SYCL_INLINE_NAMESPACE(cl) {
 namespace sycl {
 namespace detail {
+AccessorImplHost::AccessorImplHost(id<3> Offset, range<3> AccessRange,
+                                   range<3> MemoryRange,
+                                   access::mode AccessMode,
+                                   detail::SYCLMemObjI *SYCLMemObject, int Dims,
+                                   int ElemSize, int OffsetInBytes,
+                                   bool IsSubBuffer)
+    : MOffset(Offset), MAccessRange(AccessRange), MMemoryRange(MemoryRange),
+      MAccessMode(AccessMode), MSYCLMemObj(SYCLMemObject), MDims(Dims),
+      MElemSize(ElemSize), MOffsetInBytes(OffsetInBytes),
+      MIsSubBuffer(IsSubBuffer) {}
+
+AccessorImplHost::AccessorImplHost(const AccessorImplHost &Other)
+    : MOffset(Other.MOffset), MAccessRange(Other.MAccessRange),
+      MMemoryRange(Other.MMemoryRange), MAccessMode(Other.MAccessMode),
+      MSYCLMemObj(Other.MSYCLMemObj), MDims(Other.MDims),
+      MElemSize(Other.MElemSize), MOffsetInBytes(Other.MOffsetInBytes),
+      MIsSubBuffer(Other.MIsSubBuffer) {}
 
 AccessorImplHost::~AccessorImplHost() {
   if (MBlockedCmd)
     detail::Scheduler::getInstance().releaseHostAccessor(this);
+}
+
+AccessorBaseHost::AccessorBaseHost(id<3> Offset, range<3> AccessRange,
+                                   range<3> MemoryRange,
+                                   access::mode AccessMode,
+                                   detail::SYCLMemObjI *SYCLMemObject, int Dims,
+                                   int ElemSize, int OffsetInBytes,
+                                   bool IsSubBuffer) {
+  impl = shared_ptr_class<AccessorImplHost>(new AccessorImplHost(
+      Offset, AccessRange, MemoryRange, AccessMode, SYCLMemObject, Dims,
+      ElemSize, OffsetInBytes, IsSubBuffer));
+}
+
+LocalAccessorImplHost::LocalAccessorImplHost(sycl::range<3> Size, int Dims,
+                                             int ElemSize)
+    : MSize(Size), MDims(Dims), MElemSize(ElemSize),
+      MMem(Size[0] * Size[1] * Size[2] * ElemSize) {}
+
+void LocalAccessorImplHost::resize(size_t LocalSize, size_t GlobalSize) {
+  if (GlobalSize != 1 && LocalSize != 1) {
+    // If local size is not specified then work group size is chosen by
+    // runtime. That is why try to allocate based on max work group size or
+    // global size. In the worst case allocate 80% of local memory.
+    size_t MinEstWGSize = LocalSize ? LocalSize : GlobalSize;
+    MinEstWGSize = MinEstWGSize > MaxWGSize ? MaxWGSize : MinEstWGSize;
+    size_t NewSize = MinEstWGSize * MSize[0];
+    MSize[0] =
+        NewSize > 8 * LocalMemSize / 10 ? 8 * LocalMemSize / 10 : NewSize;
+    MMem.resize(NewSize * MElemSize);
+  }
+}
+
+LocalAccessorBaseHost::LocalAccessorBaseHost(sycl::range<3> Size, int Dims,
+                                             int ElemSize) {
+  impl = shared_ptr_class<LocalAccessorImplHost>(
+      new LocalAccessorImplHost(Size, Dims, ElemSize));
 }
 
 void addHostAccessorAndWait(Requirement *Req) {
@@ -24,7 +77,6 @@ void addHostAccessorAndWait(Requirement *Req) {
       detail::Scheduler::getInstance().addHostAccessor(Req);
   Event->wait(Event);
 }
-}
-}
-}
-
+} // namespace detail
+} // namespace sycl
+} // __SYCL_INLINE_NAMESPACE(cl)
