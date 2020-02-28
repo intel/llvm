3598dbc007bab6fa3f866c405fbd7e1d55ca25f3
Aleksander Fadeev <aleksander.fadeev@intel.com>
2020-02-28 13:49:46 +0300

    [SYCL] Forbid non-const static variable uses in device code (fix #7)
    Signed-off-by: Aleksander Fadeev <aleksander.fadeev@intel.com>

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -211,12 +211,14 @@ bool Sema::DiagnoseUseOfDecl(NamedDecl *D, ArrayRef<SourceLocation> Locs,
                              bool AvoidPartialAvailabilityChecks,
                              ObjCInterfaceDecl *ClassReceiver) {
   if (getLangOpts().SYCLIsDevice) {
-    if (auto VD = dyn_cast<VarDecl>(D))
+    if (auto VD = dyn_cast<VarDecl>(D)) {
       if (VD->getStorageClass() == SC_Static &&
           !VD->getType().isConstant(Context))
         SYCLDiagIfDeviceCode(*Locs.begin(), diag::err_sycl_restrict)
             << Sema::KernelNonConstStaticDataVariable;
+    }
   }
+
   SourceLocation Loc = Locs.front();
   if (getLangOpts().CPlusPlus && isa<FunctionDecl>(D)) {
     // If there were any diagnostics suppressed by template argument deduction,
