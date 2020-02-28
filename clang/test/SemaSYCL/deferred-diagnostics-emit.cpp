dd87de274ff803562868c6b81b8f35beff4c1fec
Chris Perkins <chris.perkins@intel.com>
2020-02-28 14:55:28 -0800

    lightening test
    Signed-off-by: Chris Perkins <chris.perkins@intel.com>

--- clang/test/SemaSYCL/deferred-diagnostics-emit.cpp
+++ clang/test/SemaSYCL/deferred-diagnostics-emit.cpp
@@ -5,30 +5,13 @@
 // testing that the deferred diagnostics work in conjunction with the SYCL namespaces.
 inline namespace cl {
 namespace sycl {
-class queue {
-public:
-  template <typename T>
-  void submit(T CGF) {}
-};
 
-template <int I>
-class id {};
+template <typename name, typename Func>
+__attribute__((sycl_kernel)) void kernel_single_task(Func kernelFunc) {
+  // expected-note@+1 2{{called by 'kernel_single_task<AName, (lambda}}
+  kernelFunc();
+}
 
-template <int I>
-class range {};
-
-class handler {
-public:
-  template <typename KernelName, typename KernelType, int Dims>
-  __attribute__((sycl_kernel)) void kernel_parallel_for(KernelType kernelFunc) {
-    // expected-note@+1 2{{called by 'kernel_parallel_for<AName, (lambda}}
-    kernelFunc(id<1>{});
-  }
-  template <typename KernelName, typename KernelType, int Dims>
-  void parallel_for(range<Dims> NWI, KernelType kernelFunc) {
-    kernel_parallel_for<KernelName, KernelType, Dims>(kernelFunc);
-  }
-};
 } // namespace sycl
 } // namespace cl
 
@@ -57,40 +40,30 @@ int calledFromKernel(int a) {
 template <typename T>
 void setup_sycl_operation(const T VA[]) {
 
-  cl::sycl::range<1> numOfItems;
-  cl::sycl::queue deviceQueue;
+  cl::sycl::kernel_single_task<class AName>([]() {
+    // FIX!!  xpected-error@+1 {{zero-length arrays are not permitted in C++}}
+    int OverlookedBadArray[0];
 
-  deviceQueue.submit([&](cl::sycl::handler &cgh) {
-    cgh.parallel_for<class AName>(numOfItems, [=](cl::sycl::id<1> wiID) {
-      // FIX!!  xpected-error@+1 {{zero-length arrays are not permitted in C++}}
-      int OverlookedBadArray[0];
-
-      // FIX!!   xpected-error@+1 {{__float128 is not supported on this target}}
-      __float128 overlookedBadFloat = 40;
-    });
+    // FIX!!   xpected-error@+1 {{__float128 is not supported on this target}}
+    __float128 overlookedBadFloat = 40;
   });
 }
 
 int main(int argc, char **argv) {
 
   // --- direct lambda testing ---
-  cl::sycl::range<1> numOfItems;
-  cl::sycl::queue deviceQueue;
+  cl::sycl::kernel_single_task<class AName>([]() {
+    // expected-error@+1 {{zero-length arrays are not permitted in C++}}
+    int BadArray[0];
 
-  deviceQueue.submit([&](cl::sycl::handler &cgh) {
-    cgh.parallel_for<class AName>(numOfItems, [=](cl::sycl::id<1> wiID) {
-      // expected-error@+1 {{zero-length arrays are not permitted in C++}}
-      int BadArray[0];
+    // expected-error@+1 {{__float128 is not supported on this target}}
+    __float128 badFloat = 40; // this SHOULD  trigger a diagnostic
 
-      // expected-error@+1 {{__float128 is not supported on this target}}
-      __float128 badFloat = 40; // this SHOULD  trigger a diagnostic
+    //expected-error@+1 {{SYCL kernel cannot call a variadic function}}
+    variadic(5);
 
-      //expected-error@+1 {{SYCL kernel cannot call a variadic function}}
-      variadic(5);
-
-      // expected-note@+1 {{called by 'operator()'}}
-      calledFromKernel(10);
-    });
+    // expected-note@+1 {{called by 'operator()'}}
+    calledFromKernel(10);
   });
 
   // --- lambda in specialized function testing ---
