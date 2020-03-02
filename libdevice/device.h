41a1b728a96f51d1309112afc7aa6674384715e1
Vyacheslav Zakharin <vyacheslav.p.zakharin@intel.com>
2020-03-02 09:43:09 -0800

    Define SYCL_EXTERNAL, if not defined.
    Signed-off-by: Vyacheslav Zakharin <vyacheslav.p.zakharin@intel.com>

--- libdevice/device.h
+++ libdevice/device.h
@@ -16,14 +16,10 @@
 #endif  // __cplusplus
 
 #ifdef CL_SYCL_LANGUAGE_VERSION
-// FIXME: we have to include CL/sycl/detail/common.hpp here,
-//        so that it undefined SYCL_EXTERNAL for host compilation.
-//        I cannot do that now, because of the conflicting
-//        definitions of _cl_motion_estimation_desc_intel in
-//        cl_ext.h from Intel OpenCL SDK and
-//        cl_ext_intel.h from Khronos. For some reason
-//        OpenCL SDK prepends SYCL headers in the include order.
-// #include "CL/sycl/detail/common.hpp"
+#ifndef SYCL_EXTERNAL
+#define SYCL_EXTERNAL
+#endif  // SYCL_EXTERNAL
+
 #define DEVICE_EXTERNAL SYCL_EXTERNAL
 #define IMPL_ENABLED __SYCL_DEVICE_ONLY__
 #else   // CL_SYCL_LANGUAGE_VERSION
