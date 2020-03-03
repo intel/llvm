e6ce6143d407e4b79a38e1c6521e01b2b690e1ff
cperkinsintel <chris.perkins@intel.com>
2020-03-03 10:59:01 -0800

    [SYCL] Reject zero length arrays (#1153)
    Signed-off-by: Chris Perkins <chris.perkins@intel.com>

--- clang/test/SemaSYCL/sycl-restrict.cpp
+++ clang/test/SemaSYCL/sycl-restrict.cpp
@@ -141,6 +141,9 @@ void usage(myFuncDef functionPtr) {
 
   // expected-error@+1 {{__float128 is not supported on this target}}
   __float128 A;
+
+  // expected-error@+1 {{zero-length arrays are not permitted in C++}}
+  int BadArray[0];
 }
 
 namespace ns {
