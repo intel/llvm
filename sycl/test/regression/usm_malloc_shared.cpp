d27268a0017b9c2a6bde42dba82010c0d832302a
Byoungro So <byoungro.so@intel.com>
2020-03-05 09:21:02 -0800

    reformat clang-format
    Signed-off-by: Byoungro So <byoungro.so@intel.com>

--- sycl/test/regression/usm_malloc_shared.cpp
+++ sycl/test/regression/usm_malloc_shared.cpp
@@ -3,12 +3,12 @@
 // RUN: env SYCL_DEVICE_TYPE=GPU %t.out
 // RUN: env SYCL_DEVICE_TYPE=HOST %t.out
 
-
-//==-------------- usm_free.cpp - SYCL USM free malloc_shared and free test -------------==//
+//==-------------- usm_free.cpp - SYCL USM free malloc_shared and free test
+//-------------==//
 //
-// This test checks if users will successfully allocate 160, 0, and -16 bytes of shared
-// memory, and also test user can call free() without worrying about nullptr or invalid
-// memory descriptor returned from malloc.
+// This test checks if users will successfully allocate 160, 0, and -16 bytes of
+// shared memory, and also test user can call free() without worrying about
+// nullptr or invalid memory descriptor returned from malloc.
 //==-------------------------------------------------------------------------------------==//
 
 #include <CL/sycl.hpp>
@@ -16,35 +16,35 @@
 #include <stdlib.h>
 using namespace cl::sycl;
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[]) {
   auto exception_handler = [](cl::sycl::exception_list exceptions) {
     for (std::exception_ptr const &e : exceptions) {
       try {
-	std::rethrow_exception(e);
-      }
-      catch (cl::sycl::exception const &e) {
-	std::cout << "Caught asynchronous SYCL "
-	"exception:\n"
-	<< e.what() << std::endl;
+        std::rethrow_exception(e);
+      } catch (cl::sycl::exception const &e) {
+        std::cout << "Caught asynchronous SYCL "
+                     "exception:\n"
+                  << e.what() << std::endl;
       }
     }
   };
 
   queue myQueue(default_selector{}, exception_handler);
   std::cout << "Device: " << myQueue.get_device().get_info<info::device::name>()
-	    << std::endl;
-  
+            << std::endl;
+
   double *ia = (double *)malloc_shared(160, myQueue);
   double *ja = (double *)malloc_shared(0, myQueue);
   double *result = (double *)malloc_shared(-16, myQueue);
 
-  std::cout << "ia : " << ia << " ja: " << ja << " result : " << result << std::endl;
+  std::cout << "ia : " << ia << " ja: " << ja << " result : " << result
+            << std::endl;
 
   // followings should not throws CL_INVALID_VALUE
-  free(ia, myQueue);
-  free(nullptr);
-  free(ja, myQueue);
-  free(result, myQueue);
+  cl::sycl::free(ia, myQueue);
+  cl::sycl::free(nullptr, myQueue);
+  cl::sycl::free(ja, myQueue);
+  cl::sycl::free(result, myQueue);
 
   return 0;
 }
