e76ab440f4f20a8ec9fef505bdead480a05e6d56
Alexey Bader <alexey.bader@intel.com>
2020-03-07 17:36:45 +0300

    [SYCL][NFC] Update copyright header in handler files
    Also update year in LICENSE.TXT.
    
    Signed-off-by: Alexey Bader <alexey.bader@intel.com>

--- sycl/source/handler.cpp
+++ sycl/source/handler.cpp
@@ -1,12 +1,10 @@
-//==-------- handler.cpp --- SYCL command group handler --------*- C++ -*---==//
+//==-------- handler.cpp --- SYCL command group handler --------------------==//
 //
-// Copyright (C) 2019 Intel Corporation. All rights reserved.
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-// The information and source code contained herein is the exclusive property
-// of Intel Corporation and may not be disclosed, examined or reproduced in
-// whole or in part without explicit written authorization from the company.
-//
-// ===--------------------------------------------------------------------=== //
+//===----------------------------------------------------------------------===//
 
 #include <CL/sycl/detail/common.hpp>
 #include <CL/sycl/detail/helpers.hpp>
