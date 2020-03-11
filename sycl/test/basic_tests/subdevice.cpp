1b78429ca0d27a491411d6d82fd81e999560d312
Bjoern Knafla <bjoern@codeplay.com>
2020-03-11 20:26:24 +0000

    [SYCL][CUDA] LIT remove exception catch
    Allow unexpected exceptions to terminate the LIT test as this outputs
    extra information.
    
    Signed-off-by: Bjoern Knafla <bjoern@codeplay.com>

--- sycl/test/basic_tests/subdevice.cpp
+++ sycl/test/basic_tests/subdevice.cpp
@@ -12,138 +12,135 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "../helpers.hpp"
 #include <CL/sycl.hpp>
 #include <algorithm>
-#include <cassert>
 #include <iostream>
 #include <utility>
 
 using namespace cl::sycl;
 
 int main() {
-  try {
-    auto devices = device::get_devices();
-    for (const auto &dev : devices) {
-      // TODO: implement subdevices creation for host device
-      if (dev.is_host())
-        continue;
+  // Not catching exceptions to make test fail instead.
 
-      assert(dev.get_info<info::device::partition_type_property>() ==
-             info::partition_property::no_partition);
+  auto devices = device::get_devices();
+  for (const auto &dev : devices) {
+    // TODO: implement subdevices creation for host device
+    if (dev.is_host())
+      continue;
 
-      size_t MaxSubDevices =
-          dev.get_info<info::device::partition_max_sub_devices>();
-      if (MaxSubDevices == 0)
-        continue;
+    CHECK(dev.get_info<info::device::partition_type_property>() ==
+          info::partition_property::no_partition);
 
-      try {
-        auto SubDevicesEq =
-            dev.create_sub_devices<info::partition_property::partition_equally>(
-                1);
-        assert(SubDevicesEq.size() == MaxSubDevices &&
-               "Requested 1 compute unit in each subdevice, expected maximum "
-               "number of subdevices in output");
-        std::cout << "Created " << SubDevicesEq.size()
-                  << " subdevices using equal partition scheme" << std::endl;
+    size_t MaxSubDevices =
+        dev.get_info<info::device::partition_max_sub_devices>();
+    if (MaxSubDevices == 0)
+      continue;
 
-        assert(
-            SubDevicesEq[0].get_info<info::device::partition_type_property>() ==
+    try {
+      auto SubDevicesEq =
+          dev.create_sub_devices<info::partition_property::partition_equally>(
+              1);
+      CHECK(SubDevicesEq.size() == MaxSubDevices &&
+            "Requested 1 compute unit in each subdevice, expected maximum "
+            "number of subdevices in output");
+      std::cout << "Created " << SubDevicesEq.size()
+                << " subdevices using equal partition scheme" << std::endl;
+
+      CHECK(SubDevicesEq[0].get_info<info::device::partition_type_property>() ==
             info::partition_property::partition_equally);
 
-        assert(SubDevicesEq[0].get_info<info::device::parent_device>().get() ==
-               dev.get());
-      } catch (feature_not_supported) {
-        // okay skip it
-      }
-
-      try {
-        vector_class<size_t> Counts(MaxSubDevices, 1);
-        auto SubDevicesByCount = dev.create_sub_devices<
-            info::partition_property::partition_by_counts>(Counts);
-        assert(SubDevicesByCount.size() == MaxSubDevices &&
-               "Maximum number of subdevices was requested with 1 compute unit "
-               "on each");
-        std::cout << "Created " << SubDevicesByCount.size()
-                  << " subdevices using partition by counts scheme."
-                  << std::endl;
-        assert(SubDevicesByCount[0]
-                   .get_info<info::device::partition_type_property>() ==
-               info::partition_property::partition_by_counts);
-      } catch (feature_not_supported) {
-        // okay skip it
-      }
-
-      try {
-        auto SubDevicesDomainNuma = dev.create_sub_devices<
-            info::partition_property::partition_by_affinity_domain>(
-            info::partition_affinity_domain::numa);
-        std::cout
-            << "Created " << SubDevicesDomainNuma.size()
-            << " subdevices using partition by numa affinity domain scheme."
-            << std::endl;
-      } catch (feature_not_supported) {
-        // okay skip it
-      }
-
-      try {
-        auto SubDevicesDomainL4 = dev.create_sub_devices<
-            info::partition_property::partition_by_affinity_domain>(
-            info::partition_affinity_domain::L4_cache);
-        std::cout << "Created " << SubDevicesDomainL4.size()
-                  << " subdevices using partition by L4 cache domain scheme."
-                  << std::endl;
-      } catch (feature_not_supported) {
-        // okay skip it
-      }
-
-      try {
-        auto SubDevicesDomainL3 = dev.create_sub_devices<
-            info::partition_property::partition_by_affinity_domain>(
-            info::partition_affinity_domain::L3_cache);
-        std::cout << "Created " << SubDevicesDomainL3.size()
-                  << " subdevices using partition by L3 cache domain scheme."
-                  << std::endl;
-      } catch (feature_not_supported) {
-        // okay skip it
-      }
-
-      try {
-        auto SubDevicesDomainL2 = dev.create_sub_devices<
-            info::partition_property::partition_by_affinity_domain>(
-            info::partition_affinity_domain::L2_cache);
-        std::cout << "Created " << SubDevicesDomainL2.size()
-                  << " subdevices using partition by L2 cache domain scheme."
-                  << std::endl;
-      } catch (feature_not_supported) {
-        // okay skip it
-      }
-
-      try {
-        auto SubDevicesDomainL1 = dev.create_sub_devices<
-            info::partition_property::partition_by_affinity_domain>(
-            info::partition_affinity_domain::L1_cache);
-        std::cout << "Created " << SubDevicesDomainL1.size()
-                  << " subdevices using partition by L1 cache domain scheme."
-                  << std::endl;
-      } catch (feature_not_supported) {
-        // okay skip it
-      }
-
-      try {
-        auto SubDevicesDomainNextPart = dev.create_sub_devices<
-            info::partition_property::partition_by_affinity_domain>(
-            info::partition_affinity_domain::next_partitionable);
-        std::cout << "Created " << SubDevicesDomainNextPart.size()
-                  << " subdevices using partition by next partitionable "
-                     "domain scheme."
-                  << std::endl;
-      } catch (feature_not_supported) {
-        // okay skip it
-      }
+      CHECK(SubDevicesEq[0].get_info<info::device::parent_device>().get() ==
+            dev.get());
+    } catch (feature_not_supported) {
+      // okay skip it
     }
-  } catch (exception e) {
-    std::cout << "SYCL exception caught: " << e.what() << std::endl;
-    return 1;
+
+    try {
+      vector_class<size_t> Counts(MaxSubDevices, 1);
+      auto SubDevicesByCount = dev.create_sub_devices<
+          info::partition_property::partition_by_counts>(Counts);
+      CHECK(SubDevicesByCount.size() == MaxSubDevices &&
+            "Maximum number of subdevices was requested with 1 compute unit "
+            "on each");
+      std::cout << "Created " << SubDevicesByCount.size()
+                << " subdevices using partition by counts scheme."
+                << std::endl;
+      CHECK(SubDevicesByCount[0]
+                .get_info<info::device::partition_type_property>() ==
+            info::partition_property::partition_by_counts);
+    } catch (feature_not_supported) {
+      // okay skip it
+    }
+
+    try {
+      auto SubDevicesDomainNuma = dev.create_sub_devices<
+          info::partition_property::partition_by_affinity_domain>(
+          info::partition_affinity_domain::numa);
+      std::cout
+          << "Created " << SubDevicesDomainNuma.size()
+          << " subdevices using partition by numa affinity domain scheme."
+          << std::endl;
+    } catch (feature_not_supported) {
+      // okay skip it
+    }
+
+    try {
+      auto SubDevicesDomainL4 = dev.create_sub_devices<
+          info::partition_property::partition_by_affinity_domain>(
+          info::partition_affinity_domain::L4_cache);
+      std::cout << "Created " << SubDevicesDomainL4.size()
+                << " subdevices using partition by L4 cache domain scheme."
+                << std::endl;
+    } catch (feature_not_supported) {
+      // okay skip it
+    }
+
+    try {
+      auto SubDevicesDomainL3 = dev.create_sub_devices<
+          info::partition_property::partition_by_affinity_domain>(
+          info::partition_affinity_domain::L3_cache);
+      std::cout << "Created " << SubDevicesDomainL3.size()
+                << " subdevices using partition by L3 cache domain scheme."
+                << std::endl;
+    } catch (feature_not_supported) {
+      // okay skip it
+    }
+
+    try {
+      auto SubDevicesDomainL2 = dev.create_sub_devices<
+          info::partition_property::partition_by_affinity_domain>(
+          info::partition_affinity_domain::L2_cache);
+      std::cout << "Created " << SubDevicesDomainL2.size()
+                << " subdevices using partition by L2 cache domain scheme."
+                << std::endl;
+    } catch (feature_not_supported) {
+      // okay skip it
+    }
+
+    try {
+      auto SubDevicesDomainL1 = dev.create_sub_devices<
+          info::partition_property::partition_by_affinity_domain>(
+          info::partition_affinity_domain::L1_cache);
+      std::cout << "Created " << SubDevicesDomainL1.size()
+                << " subdevices using partition by L1 cache domain scheme."
+                << std::endl;
+    } catch (feature_not_supported) {
+      // okay skip it
+    }
+
+    try {
+      auto SubDevicesDomainNextPart = dev.create_sub_devices<
+          info::partition_property::partition_by_affinity_domain>(
+          info::partition_affinity_domain::next_partitionable);
+      std::cout << "Created " << SubDevicesDomainNextPart.size()
+                << " subdevices using partition by next partitionable "
+                   "domain scheme."
+                << std::endl;
+    } catch (feature_not_supported) {
+      // okay skip it
+    }
   }
+
   return 0;
 }
