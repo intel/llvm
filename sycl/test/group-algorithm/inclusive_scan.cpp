3100d90d048b06517547a580ce295d7cca517eae
John Pennycook <john.pennycook@intel.com>
2020-03-11 08:32:58 -0700

    [SYCL] Remove copyright header from tests
    Signed-off-by: John Pennycook <john.pennycook@intel.com>

--- sycl/test/group-algorithm/inclusive_scan.cpp
+++ sycl/test/group-algorithm/inclusive_scan.cpp
@@ -3,13 +3,6 @@
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 // RUN: %ACC_RUN_PLACEHOLDER %t.out
-//==---- inclusive_scan.cpp - SYCL group inclusive_scan test --*- C++ -*----==//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
 
 #include <CL/sycl.hpp>
 #include <algorithm>
