1251e565f7c08d677af2a78f52f839e1a09acbdc
haonanya <haonan.yang@intel.com>
2020-03-11 10:37:28 +0800

    [SYCL] [Test] Deal with nan and inf input in is_about_FP function in math_utils.hpp
    Signed-off-by: haonanya <haonan.yang@intel.com>

--- sycl/test/devicelib/math_utils.hpp
+++ sycl/test/devicelib/math_utils.hpp
@@ -4,15 +4,19 @@
 // T must be float-point type
 template <typename T>
 bool is_about_FP(T x, T y) {
-  if (x == y)
-    return true;
+  // At least one input is nan
+  if (std::isnan(x) || std::isnan(y))
+    return std::isnan(x) && std::isnan(y);
+  // At least one input is inf 
+  else if (std::isinf(x) || std::isinf(y))
+    return (x == y);
+  // two finite 
   else {
     if (x != 0 && y != 0) {
       T max_v = std::fmax(std::abs(x), std::abs(y));
       return (std::abs(x - y) / max_v) <
-              std::numeric_limits<T>::epsilon() * 100;
-    }
-    else {
+             std::numeric_limits<T>::epsilon() * 100;
+    } else {
       if (x != 0)
         return std::abs(x) < std::numeric_limits<T>::epsilon() * 100;
       else
