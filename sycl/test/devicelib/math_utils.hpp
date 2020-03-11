9d62247fbfac4cfa310b1e8fb397d6719566385e
haonanya <haonan.yang@intel.com>
2020-03-11 13:27:20 +0800

    [SYCL] Format code
    Signed-off-by: haonanya <haonan.yang@intel.com>

--- sycl/test/devicelib/math_utils.hpp
+++ sycl/test/devicelib/math_utils.hpp
@@ -7,10 +7,12 @@ bool is_about_FP(T x, T y) {
   // At least one input is nan
   if (std::isnan(x) || std::isnan(y))
     return std::isnan(x) && std::isnan(y);
-  // At least one input is inf 
+
+  // At least one input is inf
   else if (std::isinf(x) || std::isinf(y))
     return (x == y);
-  // two finite 
+
+  // two finite
   else {
     if (x != 0 && y != 0) {
       T max_v = std::fmax(std::abs(x), std::abs(y));
