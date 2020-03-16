fd16ef2016a829f342be9d36e4c5d336d66d7d47
haonanya <haonan.yang@intel.com>
2020-03-16 17:39:06 +0800

    [SYCL] [Test] Improve readability for math_utils.hpp
    Signed-off-by: haonanya <haonan.yang@intel.com>

--- sycl/test/devicelib/math_utils.hpp
+++ sycl/test/devicelib/math_utils.hpp
@@ -8,10 +8,10 @@ bool is_about_FP(T x, T y) {
 
   // At least one input is nan
   if (std::isnan(x) || std::isnan(y))
-    ret = std::isnan(x) && std::isnan(y);
+    return std::isnan(x) && std::isnan(y);
 
   // At least one input is inf
-  else if (std::isinf(x) || std::isinf(y))
+  if (std::isinf(x) || std::isinf(y))
     ret = (x == y);
 
   // two finite
