10c938b57ca0bd3ab4af4f69e148f431f20620f0
haonanya <haonan.yang@intel.com>
2020-03-18 11:41:27 +0800

    [SYCL] Add comment for is_about_FP function in math_utils.hpp
    Signed-off-by: haonanya <haonan.yang@intel.com>

--- sycl/test/devicelib/math_utils.hpp
+++ sycl/test/devicelib/math_utils.hpp
@@ -1,7 +1,11 @@
 #ifndef MATH_UTILS
 #include <complex>
 #include <limits>
-// T must be float-point type
+
+// Since it is not proper to compare float point using operator ==, this
+// function measures whether the result of cmath function from kernel is
+// close to the reference and machine epsilon is used as threshold in this
+// function. T must be float-point type.
 template <typename T>
 bool is_about_FP(T x, T y) {
   bool ret;
@@ -19,7 +23,7 @@ bool is_about_FP(T x, T y) {
     T threshold = std::numeric_limits<T>::epsilon() * 100;
     if (x != 0 && y != 0) {
       T max_v = std::fmax(std::abs(x), std::abs(y));
-      return std::abs(x - y) < threshold*max_v;
+      return std::abs(x - y) < threshold * max_v;
     } else {
       if (x != 0)
         ret = std::abs(x) < threshold;
