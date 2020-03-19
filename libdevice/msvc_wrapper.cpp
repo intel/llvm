a083318feb5d508fecf79a317686c3ff202f8d74
vzakhari <51766749+vzakhari@users.noreply.github.com>
2020-03-19 08:30:27 -0700

    Move SPIR devicelib to top level (#1276)
    Signed-off-by: Vyacheslav Zakharin <vyacheslav.p.zakharin@intel.com>

--- libdevice/msvc_wrapper.cpp
+++ libdevice/msvc_wrapper.cpp
@@ -6,11 +6,8 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifdef __SYCL_DEVICE_ONLY__
+#include "device.h"
 #include "wrapper.h"
-
-#include <CL/__spirv/spirv_vars.hpp> // for __spirv_BuiltInGlobalInvocationId,
-                                     //     __spirv_BuiltInLocalInvocationId
 
 // Truncates a wide (16 or 32 bit) string (wstr) into an ASCII string (str).
 // Any non-ASCII characters are replaced by question mark '?'.
@@ -25,7 +22,7 @@ static void __truncate_wchar_char_str(const wchar_t *wstr, char *str,
   *str = '\0';
 }
 
-extern "C" SYCL_EXTERNAL
+DEVICE_EXTERN_C
 void _wassert(const wchar_t *wexpr, const wchar_t *wfile, unsigned line) {
   // Paths and expressions that are longer than 256 characters are going to be
   // truncated.
@@ -34,12 +31,9 @@ void _wassert(const wchar_t *wexpr, const wchar_t *wfile, unsigned line) {
   char expr[256];
   __truncate_wchar_char_str(wexpr, expr, sizeof(expr));
 
-  __devicelib_assert_fail(expr, file, line, /*func=*/nullptr,
-                          __spirv_GlobalInvocationId_x(),
-                          __spirv_GlobalInvocationId_y(),
-                          __spirv_GlobalInvocationId_z(),
-                          __spirv_LocalInvocationId_x(),
-                          __spirv_LocalInvocationId_y(),
-                          __spirv_LocalInvocationId_z());
+  __devicelib_assert_fail(
+      expr, file, line, /*func=*/nullptr, __spirv_GlobalInvocationId_x(),
+      __spirv_GlobalInvocationId_y(), __spirv_GlobalInvocationId_z(),
+      __spirv_LocalInvocationId_x(), __spirv_LocalInvocationId_y(),
+      __spirv_LocalInvocationId_z());
 }
-#endif // __SYCL_DEVICE_ONLY__
