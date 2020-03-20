55112843688e7aeca611b60c260f09eafe3fab6f
Sergey Kanaev <sergey.kanaev@intel.com>
2020-03-20 12:24:33 +0300

    [SYCL] Add test
    Signed-off-by: Sergey Kanaev <sergey.kanaev@intel.com>

--- clang/test/Driver/sycl-device.cpp
+++ clang/test/Driver/sycl-device.cpp
@@ -7,3 +7,17 @@
 // RUN:   %clang -### -fsycl-device-only %s 2>&1 \
 // RUN:   | FileCheck -check-prefix=CHECK-SYCL-DEV %s
 // CHECK-SYCL-DEV: "-fsycl-is-device"
+
+/// Check that "-Wno-sycl-strict" is set on compiler invocation with "-fsycl"
+/// or "-fsycl-device-only" or both:
+// RUN:   %clang -### -fsycl %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-SYCL-NO_STRICT1 %s
+// CHECK-SYCL-NO_STRICT1: "-Wno-sycl-strict"
+
+// RUN:   %clang -### -fsycl-device-only %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-SYCL-NO_STRICT2 %s
+// CHECK-SYCL-NO_STRICT2: "-Wno-sycl-strict"
+
+// RUN:   %clang -### -fsycl -fsycl-device-only %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-SYCL-NO_STRICT3 %s
+// CHECK-SYCL-NO_STRICT3: "-Wno-sycl-strict"
