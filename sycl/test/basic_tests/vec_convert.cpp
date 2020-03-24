6c43ecacb6d14a2452f3dbb54b3f7c30e7e9b5f7
Aleksander Fadeev <aleksander.fadeev@intel.com>
2020-03-24 13:16:33 +0300

    Origin
    Signed-off-by: Aleksander Fadeev <aleksander.fadeev@intel.com>

--- sycl/test/basic_tests/vec_convert.cpp
+++ sycl/test/basic_tests/vec_convert.cpp
@@ -1,5 +1,5 @@
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
-// RUN: env SYCL_DEVICE_TYPE=HOST %t.out
+// RUN: env SYCL_DEVICE_TYPE=CPU %t.out
 // RUNx: %CPU_RUN_PLACEHOLDER %t.out
 // RUNx: %GPU_RUN_PLACEHOLDER %t.out
 // RUNx: %ACC_RUN_PLACEHOLDER %t.out
