cd9995e90a647a810623a8069bd3642bc79698d6
Mochalova <anastasiya.mochalova@intel.com>
2020-03-25 13:36:28 +0300

    Apply comment
    Signed-off-by: Mochalova <anastasiya.mochalova@intel.com>

--- sycl/test/basic_tests/min_max_test.cpp
+++ sycl/test/basic_tests/min_max_test.cpp
@@ -1,12 +1,11 @@
 // REQUIRES: windows
-// RUN: %clangxx -fsycl %s -o %t.out
+// RUN: %clangxx -fsyntax-only -Xclang -verify %s -I %sycl_include -Xclang
+// expected-no-diagnostics
 
 #include "windows.h"
 #include "CL/sycl.hpp"
 
 int main() {
-
-  sycl::device device{sycl::default_selector()};
   int tmp = min(1, 4);
   return 0;
 }
