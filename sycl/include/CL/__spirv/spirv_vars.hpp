1f34bcd1ae62181edc75b40562c47d881b199e6a
Alexander Johnston <alexander@codeplay.com>
2020-03-25 13:53:14 +0000

    [SYCL] Ensure proper definition of spirv builtins for SYCL
    Ensures proper definition of SPIRV global and workgroup builtin variables
    for both SYCL compilation with both NVPTX and SPIRV.
    
    Signed-off-by: Alexander Johnston <alexander@codeplay.com>

--- sycl/include/CL/__spirv/spirv_vars.hpp
+++ sycl/include/CL/__spirv/spirv_vars.hpp
@@ -10,6 +10,8 @@
 
 #ifdef __SYCL_DEVICE_ONLY__
 
+#ifdef __SYCL_NVPTX__
+
 SYCL_EXTERNAL size_t __spirv_GlobalInvocationId_x();
 SYCL_EXTERNAL size_t __spirv_GlobalInvocationId_y();
 SYCL_EXTERNAL size_t __spirv_GlobalInvocationId_z();
@@ -37,6 +39,89 @@ SYCL_EXTERNAL size_t __spirv_WorkgroupId_z();
 SYCL_EXTERNAL size_t __spirv_LocalInvocationId_x();
 SYCL_EXTERNAL size_t __spirv_LocalInvocationId_y();
 SYCL_EXTERNAL size_t __spirv_LocalInvocationId_z();
+
+#else // __SYCL_NVPTX__
+
+typedef size_t size_t_vec __attribute__((ext_vector_type(3)));
+extern "C" const __attribute__((opencl_constant)) size_t_vec __spirv_BuiltInGlobalInvocationId;
+extern "C" const __attribute__((opencl_constant)) size_t_vec __spirv_BuiltInGlobalSize;
+extern "C" const __attribute__((opencl_constant)) size_t_vec __spirv_BuiltInGlobalOffset;
+extern "C" const __attribute__((opencl_constant)) size_t_vec __spirv_BuiltInNumWorkgroups;
+extern "C" const __attribute__((opencl_constant)) size_t_vec __spirv_BuiltInWorkgroupSize;
+extern "C" const __attribute__((opencl_constant)) size_t_vec __spirv_BuiltInWorkgroupId;
+extern "C" const __attribute__((opencl_constant)) size_t_vec __spirv_BuiltInLocalInvocationId;
+
+SYCL_EXTERNAL inline size_t __spirv_GlobalInvocationId_x() {
+  return __spirv_BuiltInGlobalInvocationId.x;
+}
+SYCL_EXTERNAL inline size_t __spirv_GlobalInvocationId_y() {
+  return __spirv_BuiltInGlobalInvocationId.y;
+}
+SYCL_EXTERNAL inline size_t __spirv_GlobalInvocationId_z() {
+  return __spirv_BuiltInGlobalInvocationId.z;
+}
+
+SYCL_EXTERNAL inline size_t __spirv_GlobalSize_x() {
+  return __spirv_BuiltInGlobalSize.x;
+}
+SYCL_EXTERNAL inline size_t __spirv_GlobalSize_y() {
+  return __spirv_BuiltInGlobalSize.y;
+}
+SYCL_EXTERNAL inline size_t __spirv_GlobalSize_z() {
+  return __spirv_BuiltInGlobalSize.z;
+}
+
+SYCL_EXTERNAL inline size_t __spirv_GlobalOffset_x() {
+  return __spirv_BuiltInGlobalOffset.x;
+}
+SYCL_EXTERNAL inline size_t __spirv_GlobalOffset_y() {
+  return __spirv_BuiltInGlobalOffset.y;
+}
+SYCL_EXTERNAL inline size_t __spirv_GlobalOffset_z() {
+  return __spirv_BuiltInGlobalOffset.z;
+}
+
+SYCL_EXTERNAL inline size_t __spirv_NumWorkgroups_x() {
+  return __spirv_BuiltInNumWorkgroups.x;
+}
+SYCL_EXTERNAL inline size_t __spirv_NumWorkgroups_y() {
+  return __spirv_BuiltInNumWorkgroups.y;
+}
+SYCL_EXTERNAL inline size_t __spirv_NumWorkgroups_z() {
+  return __spirv_BuiltInNumWorkgroups.z;
+}
+
+SYCL_EXTERNAL inline size_t __spirv_WorkgroupSize_x() {
+  return __spirv_BuiltInWorkgroupSize.x;
+}
+SYCL_EXTERNAL inline size_t __spirv_WorkgroupSize_y() {
+  return __spirv_BuiltInWorkgroupSize.y;
+}
+SYCL_EXTERNAL inline size_t __spirv_WorkgroupSize_z() {
+  return __spirv_BuiltInWorkgroupSize.z;
+}
+
+SYCL_EXTERNAL inline size_t __spirv_WorkgroupId_x() {
+  return __spirv_BuiltInWorkgroupId.x;
+}
+SYCL_EXTERNAL inline size_t __spirv_WorkgroupId_y() {
+  return __spirv_BuiltInWorkgroupId.y;
+}
+SYCL_EXTERNAL inline size_t __spirv_WorkgroupId_z() {
+  return __spirv_BuiltInWorkgroupId.z;
+}
+
+SYCL_EXTERNAL inline size_t __spirv_LocalInvocationId_x() {
+  return __spirv_BuiltInLocalInvocationId.x;
+}
+SYCL_EXTERNAL inline size_t __spirv_LocalInvocationId_y() {
+  return __spirv_BuiltInLocalInvocationId.y;
+}
+SYCL_EXTERNAL inline size_t __spirv_LocalInvocationId_z() {
+  return __spirv_BuiltInLocalInvocationId.z;
+}
+
+#endif // __SYCL_NVPTX__
 
 #define DEFINE_FUNC_ID_TO_XYZ_CONVERTER(POSTFIX)                               \
   template <int ID> static inline size_t get##POSTFIX();                       \
