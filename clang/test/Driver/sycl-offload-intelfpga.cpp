d5a7bb744b01eae6392b08b1173372092100bc9b
Michael D Toguchi <michael.d.toguchi@intel.com>
2020-03-25 09:33:00 -0700

    [NFC] Test update based on review
    Signed-off-by: Michael D Toguchi <michael.d.toguchi@intel.com>

--- clang/test/Driver/sycl-offload-intelfpga.cpp
+++ clang/test/Driver/sycl-offload-intelfpga.cpp
@@ -255,9 +255,9 @@
 /// -fintelfpga output dep file using -Fo<dir>
 // RUN: mkdir -p %t_dir
 // RUN: %clang_cl -### -c -fsycl -fintelfpga -Fo%t_dir/ %s 2>&1 \
-// RUN:  | FileCheck -check-prefix=CHK-FPGA-DEP-DIR %s
-// CHK-FPGA-DEP-DIR: clang{{.*}} "-dependency-file" "[[DEPFILE:.+\.d]]"
-// CHK-FPGA-DEP-DIR: clang-offload-bundler{{.*}} "-inputs={{.*}}.bc,{{.*}}.obj,[[DEPFILE]]"
+// RUN:  | FileCheck -DDEPDIR=%t_dir/ -check-prefix=CHK-FPGA-DEP-DIR %s
+// CHK-FPGA-DEP-DIR: clang{{.*}} "-dependency-file" "[[DEPDIR]][[DEPFILE:.+\.d]]"
+// CHK-FPGA-DEP-DIR: clang-offload-bundler{{.*}} "-inputs={{.*}}.bc,{{.*}}.obj,[[DEPDIR]][[DEPFILE]]"
 
 /// -fintelfpga static lib (aoco)
 // RUN:  echo "Dummy AOCO image" > %t.aoco
