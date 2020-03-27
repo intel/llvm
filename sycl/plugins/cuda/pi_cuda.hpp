9be15660abb503d4cbb296fdd86328e50ccc6983
Ruyman <ruyman@codeplay.com>
2020-03-27 19:04:57 +0000

    [SYCL][NFC] Doxygen doc for CUDA backend (#1394)
    Adding doxygen documentation to PI CUDA backend.
    Some code is re-ordered in the file to help sorting the
    doxygen.
    
    Co-Authored-By: Alexey Bader <alexey.bader@intel.com>
    Co-Authored-By: Alexander Batashev <alexbatashev@outlook.com>
    Co-Authored-By: Romanov Vlad <17316488+romanovvlad@users.noreply.github.com>
    
    Signed-off-by: Ruyman Reyes <ruyman@codeplay.com>

--- sycl/plugins/cuda/pi_cuda.hpp
+++ sycl/plugins/cuda/pi_cuda.hpp
@@ -10,8 +10,8 @@
 /// \ingroup sycl_pi
 
 /// \file pi_cuda.hpp
-/// Definition of CUDA Plugin. It is the interface between the device-agnostic
-/// SYCL runtime layer and underlying CUDA runtime.
+/// Declarations for CUDA Plugin. It is the interface between the
+/// device-agnostic SYCL runtime layer and underlying CUDA runtime.
 ///
 /// \ingroup sycl_pi_cuda
 
@@ -34,6 +34,7 @@
 
 extern "C" {
 
+/// \cond INGORE_BLOCK_IN_DOXYGEN
 pi_result cuda_piContextRetain(pi_context );
 pi_result cuda_piContextRelease(pi_context );
 pi_result cuda_piDeviceRelease(pi_device );
@@ -46,29 +47,79 @@ pi_result cuda_piMemRetain(pi_mem);
 pi_result cuda_piMemRelease(pi_mem);
 pi_result cuda_piKernelRetain(pi_kernel);
 pi_result cuda_piKernelRelease(pi_kernel);
-
-
+/// \endcond
 }
 
+/// A PI platform stores all known PI devices,
+///  in the CUDA plugin this is just a vector of
+///  available devices since initialization is done
+///  when devices are used.
+///
 struct _pi_platform {
   std::vector<std::unique_ptr<_pi_device>> devices_;
 };
 
-struct _pi_device {
+/// PI device mapping to a CUdevice.
+/// Includes an observer pointer to the platform,
+/// and implements the reference counting semantics since
+/// CUDA objects are not refcounted.
+///
+class _pi_device {
   using native_type = CUdevice;
 
   native_type cuDevice_;
   std::atomic_uint32_t refCount_;
   pi_platform platform_;
 
+public:
   _pi_device(native_type cuDevice, pi_platform platform)
       : cuDevice_(cuDevice), refCount_{1}, platform_(platform) {}
 
   native_type get() const noexcept { return cuDevice_; };
 
   pi_uint32 get_reference_count() const noexcept { return refCount_; }
+
+  pi_platform get_platform() const noexcept { return platform_; };
 };
 
+/// PI context mapping to a CUDA context object.
+///
+/// There is no direct mapping between a CUDA context and a PI context,
+/// main differences described below:
+///
+/// <b> CUDA context vs PI context </b>
+///
+/// One of the main differences between the PI API and the CUDA driver API is
+/// that the second modifies the state of the threads by assigning
+/// `CUcontext` objects to threads. `CUcontext` objects store data associated
+/// with a given device and control access to said device from the user side.
+/// PI API context are objects that are passed to functions, and not bound
+/// to threads.
+/// The _pi_context object doesn't implement this behavior, only holds the
+/// CUDA context data. The RAII object \ref ScopedContext implements the active
+/// context behavior.
+///
+/// <b> Primary vs User-defined context </b>
+///
+/// CUDA has two different types of context, the Primary context,
+/// which is usable by all threads on a given process for a given device, and
+/// the aforementioned custom contexts.
+/// CUDA documentation, and performance analysis, indicates it is recommended
+/// to use Primary context whenever possible.
+/// Primary context is used as well by the CUDA Runtime API.
+/// For PI applications to interop with CUDA Runtime API, they have to use
+/// the primary context - and make that active in the thread.
+/// The `_pi_context` object can be constructed with a `kind` parameter
+/// that allows to construct a Primary or `user-defined` context, so that
+/// the PI object interface is always the same.
+///
+///  <b> Destructor callback </b>
+///
+///  Required to implement CP023, SYCL Extended Context Destruction,
+///  the PI Context can store a number of callback functions that will be
+///  called upon destruction of the PI Context.
+///  See proposal for details.
+///
 struct _pi_context {
   using native_type = CUcontext;
 
@@ -103,8 +154,10 @@ struct _pi_context {
     destruction_callbacks_.emplace_back(std::forward<Func>(callback));
   }
 
-  _pi_device *get_device() const noexcept { return deviceId_; }
+  pi_device get_device() const noexcept { return deviceId_; }
+
   native_type get() const noexcept { return cuContext_; }
+
   bool is_primary() const noexcept { return kind_ == kind::primary; }
 
   pi_uint32 increment_reference_count() noexcept { return ++refCount_; }
@@ -112,11 +165,14 @@ struct _pi_context {
   pi_uint32 decrement_reference_count() noexcept { return --refCount_; }
 
   pi_uint32 get_reference_count() const noexcept { return refCount_; }
+
 private:
   std::mutex mutex_;
   std::vector<std::function<void(void)>> destruction_callbacks_;
 };
 
+/// PI Mem mapping to a CUDA memory allocation
+///
 struct _pi_mem {
   using native_type = CUdeviceptr;
   using pi_context = _pi_context *;
@@ -152,13 +208,15 @@ struct _pi_mem {
      }
    }
 
-  bool is_buffer() const {
-    // TODO: Adapt once images are supported.
-    return true;
-  }
-  bool is_sub_buffer() const { return (is_buffer() && (parent_ != nullptr)); }
+   /// \TODO: Adapt once images are supported.
+   bool is_buffer() const noexcept { return true; }
+
+   bool is_sub_buffer() const noexcept {
+     return (is_buffer() && (parent_ != nullptr));
+   }
 
   native_type get() const noexcept { return ptr_; }
+
   pi_context get_context() const noexcept { return context_; }
 
   pi_uint32 increment_reference_count() noexcept { return ++refCount_; }
@@ -202,6 +260,8 @@ struct _pi_mem {
   }
 };
 
+/// PI queue mapping on to CUstream objects.
+///
 struct _pi_queue {
   using native_type = CUstream;
 
@@ -268,6 +328,8 @@ private:
   void *userData_;
 };
 
+/// PI Event mapping to CUevent
+///
 class _pi_event {
 public:
   using native_type = CUevent;
@@ -425,6 +487,8 @@ private:
                         // changes.
 };
 
+/// Implementation of PI Program on CUDA Module object
+///
 struct _pi_program {
   using native_type = CUmodule;
   native_type module_;
@@ -457,22 +521,38 @@ struct _pi_program {
   pi_uint32 get_reference_count() const noexcept { return refCount_; }
 };
 
+/// Implementation of a PI Kernel for CUDA
+///
+/// PI Kernels are used to set kernel arguments,
+/// creating a state on the Kernel object for a given
+/// invocation. This is not the case of CUFunction objects,
+/// which are simply passed together with the arguments on the invocation.
+/// The PI Kernel implementation for CUDA stores the list of arguments,
+/// argument sizes and offsets to emulate the interface of PI Kernel,
+/// saving the arguments for the later dispatch.
+/// Note that in PI API, the Local memory is specified as a size per
+/// individual argument, but in CUDA only the total usage of shared
+/// memory is required since it is not passed as a parameter.
+/// A compiler pass converts the PI API local memory model into the
+/// CUDA shared model. This object simply calculates the total of
+/// shared memory, and the initial offsets of each parameter.
+///
 struct _pi_kernel {
   using native_type = CUfunction;
 
   native_type function_;
   std::string name_;
-  _pi_context *context_;
+  pi_context context_;
   pi_program program_;
   std::atomic_uint32_t refCount_;
 
-  /*
-   * Structure that holds the arguments to the kernel.
-   * Note earch argument size is known, since it comes
-   * from the kernel signature.
-   * This is not something you can query in CUDA,
-   * so error handling cannot be provided easily.
-   */
+  /// Structure that holds the arguments to the kernel.
+  /// Note earch argument size is known, since it comes
+  /// from the kernel signature.
+  /// This is not something can be queried from the CUDA API
+  /// so there is a hard-coded size (\ref MAX_PARAM_BYTES)
+  /// and a storage.
+  ///
   struct arguments {
     static constexpr size_t MAX_PARAM_BYTES = 4000u;
     using args_t = std::array<char, MAX_PARAM_BYTES>;
@@ -483,6 +563,10 @@ struct _pi_kernel {
     args_index_t indices_;
     args_size_t offsetPerIndex_;
 
+    /// Adds an argument to the kernel.
+    /// If the argument existed before, it is replaced.
+    /// Otherwise, it is added.
+    /// Gaps are filled with empty arguments.
     void add_arg(size_t index, size_t size, const void *arg,
                  size_t localSize = 0) {
       if (index + 1 > indices_.size()) {
@@ -510,7 +594,7 @@ struct _pi_kernel {
       std::fill(std::begin(offsetPerIndex_), std::end(offsetPerIndex_), 0);
     }
 
-    args_index_t get_indices() const { return indices_; }
+    args_index_t get_indices() const noexcept { return indices_; }
 
     pi_uint32 get_local_size() const {
       return std::accumulate(std::begin(offsetPerIndex_),
@@ -540,13 +624,16 @@ struct _pi_kernel {
 
   pi_uint32 get_reference_count() const noexcept { return refCount_; }
 
-  native_type get() const { return function_; };
+  native_type get() const noexcept { return function_; };
 
   pi_context get_context() const noexcept { return context_; };
 
-
   const char *get_name() const noexcept { return name_.c_str(); }
 
+  /// Returns the number of arguments.
+  /// Note this only returns the current known number of arguments, not the
+  /// real one required by the kernel, since this cannot be queried from
+  /// the CUDA Driver API
   pi_uint32 get_num_args() const noexcept { return args_.indices_.size(); }
 
   void set_kernel_arg(int index, size_t size, const void *arg) {
@@ -569,9 +656,5 @@ struct _pi_kernel {
 // -------------------------------------------------------------
 // Helper types and functions
 //
-
-// Checks a CUDA error and returns a PI error code
-// May throw
-pi_result check_error(CUresult result);
 
 #endif // PI_CUDA_HPP
