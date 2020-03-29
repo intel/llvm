14458e31271f12b55a5c4f97204f5412a6a4cc8e
Sergey Kanaev <sergey.kanaev@intel.com>
2020-03-29 11:44:03 +0300

    [SYCL] Fix assert test
    Signed-off-by: Sergey Kanaev <sergey.kanaev@intel.com>

--- sycl/test/devicelib/assert.cpp
+++ sycl/test/devicelib/assert.cpp
@@ -100,10 +100,18 @@
 // Note that the work-item that hits the assert first may vary, since the order
 // of execution is undefined. We catch only the first one (whatever id it is).
 
+#ifndef PARENT_PROCESS
 #include <CL/sycl.hpp>
+#endif
+
 #include <array>
 #include <assert.h>
 #include <stdlib.h>
+
+#ifdef PARENT_PROCESS
+#include <string.h>
+#endif
+
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
