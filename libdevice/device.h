09930dfae25be204bd2b092967f901e291a8f067
Alexander Johnston <alexander@codeplay.com>
2020-04-03 14:18:31 +0100

    Move include within header definition guard
    Signed-off-by: Alexander Johnston <alexander@codeplay.com>

--- libdevice/device.h
+++ libdevice/device.h
@@ -6,12 +6,12 @@
 //
 //===----------------------------------------------------------------------===//
 
+#ifndef __LIBDEVICE_DEVICE_H__
+#define __LIBDEVICE_DEVICE_H__
+
 // We need the following header to ensure the definition of all spirv variables
 // required by the wrapper libraries.
 #include "spirv_vars.hpp"
-
-#ifndef __LIBDEVICE_DEVICE_H__
-#define __LIBDEVICE_DEVICE_H__
 
 #ifdef __cplusplus
 #define EXTERN_C extern "C"
