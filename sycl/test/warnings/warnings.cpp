cf9d1a4bc44112a5d7e6790ab04cab88f6461d1f
Bjoern Knafla <bjoern@codeplay.com>
2020-04-09 10:22:33 +0100

    [SYCL][CUDA] LIT XFAIL/UNSUPPORTED (#1303)
    Remove XFAIL from passing tests.
    
    Change tests that should pass but fail as XFAIL, e.g., mark
    unexpectedly failing sub-buffer test to fix later for SYCL CUDA.
    
    Add `REQUIRES: opencl` for LIT tests requiring OpenCL.
    LIT tests that `REQUIRES: opencl` do not need an `UNSUPPORTED: cuda`.
    
    Mark tests of features not supported by SYCL CUDA with
    `UNSUPPORTED: cuda`.
    
    Tests that do not use the CUDA target triple do not require any
    CUDA related LIT marker.
    
    Tests that do not fail with CUDA do not require any CUDA specific
    LIT marker.
    
    Fix LIT test using `-Werror` to fail when the compiler driver warns
    about an unknown CUDA version as CUDA has otherwise nothing to do with
    the test.
    
    Adapt test to moved `pi_cuda.hpp` header.
    
    Some cleanup inside LIT configuration scripts.
    
    Signed-off-by: Bjoern Knafla <bjoern@codeplay.com>

--- sycl/test/warnings/warnings.cpp
+++ sycl/test/warnings/warnings.cpp
@@ -1,4 +1,4 @@
-// RUN: %clangxx -Wall -Wpessimizing-move -Wunused-variable -Wmismatched-tags -Wunneeded-internal-declaration -Werror -fsycl %s -o %t.out
+// RUN: %clangxx -Wall -Wpessimizing-move -Wunused-variable -Wmismatched-tags -Wunneeded-internal-declaration -Werror -Wno-unknown-cuda-version -fsycl %s -o %t.out
 
 #include <CL/sycl.hpp>
 
