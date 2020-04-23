fab13a4ab18a48a8b69e3122a927ca2ee9b247c0
Alexander Batashev <alexander.batashev@intel.com>
2020-04-23 16:33:55 +0300

    Address example comments
    Signed-off-by: Alexander Batashev <alexander.batashev@intel.com>

--- sycl/examples/simple-dpcpp-app.cpp
+++ sycl/examples/simple-dpcpp-app.cpp
@@ -1,32 +1,41 @@
 #include <CL/sycl.hpp>
 
+using namespace sycl;
+
 int main() {
-  // Creating buffer of 4 ints to be used inside the kernel code
-  cl::sycl::buffer<cl::sycl::cl_int, 1> Buffer(4);
+  // Create a buffer of 4 ints to be used inside the kernel code.
+  buffer<cl_int, 1> Buffer(4);
 
-  // Creating SYCL queue
-  cl::sycl::queue Queue;
+  // Create a simple asynchronous exception handler.
+  auto AsyncHandler = [](exception_list ExceptionList) {
+    for (auto &Exception : ExceptionList) {
+      std::rethrow_exception(Exception);
+    }
+  };
 
-  // Size of index space for kernel
-  cl::sycl::range<1> NumOfWorkItems{Buffer.get_count()};
+  // Create a SYCL queue.
+  queue Queue(AsyncHandler);
 
-  // Submitting command group(work) to queue
-  Queue.submit([&](cl::sycl::handler &cgh) {
-    // Getting write only access to the buffer on a device
-    auto Accessor = Buffer.get_access<cl::sycl::access::mode::write>(cgh);
-    // Executing kernel
-    cgh.parallel_for<class FillBuffer>(
-        NumOfWorkItems, [=](cl::sycl::id<1> WIid) {
-          // Fill buffer with indexes
-          Accessor[WIid] = (cl::sycl::cl_int)WIid.get(0);
-        });
+  // Size of index space for kernel.
+  range<1> NumOfWorkItems{Buffer.get_count()};
+
+  // Submit command group(work) to queue.
+  Queue.submit([&](handler &cgh) {
+    // Get write only access to the buffer on a device.
+    auto Accessor = Buffer.get_access<access::mode::write>(cgh);
+    // Execute kernel.
+    cgh.parallel_for<class FillBuffer>(NumOfWorkItems, [=](id<1> WIid) {
+      // Fill buffer with indices.
+      Accessor[WIid] = static_cast<cl_int>(WIid.get(0));
+    });
   });
 
-  // Getting read only access to the buffer on the host.
-  // Implicit barrier waiting for queue to complete the work.
-  const auto HostAccessor = Buffer.get_access<cl::sycl::access::mode::read>();
+  // Get read only access to the buffer on the host.
+  // This introduces an implicit barrier which blocks execution until the
+  // command group above completes.
+  const auto HostAccessor = Buffer.get_access<access::mode::read>();
 
-  // Check the results
+  // Check the results.
   bool MismatchFound = false;
   for (size_t I = 0; I < Buffer.get_count(); ++I) {
     if (HostAccessor[I] != I) {
@@ -43,4 +52,3 @@ int main() {
 
   return MismatchFound;
 }
-
