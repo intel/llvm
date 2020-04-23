7659e45221b67cc6f379c0034e0174ba1440e64a
Alexey Sotkin <alexey.sotkin@intel.com>
2020-04-23 17:42:11 +0300

    [SYCL] Add test for SYCL kernels with accessor and spec constant (#1536)
    Clang used to crash in case SYCL kernel captures a specialization constant
    before an accessor. The tests checks that the crash is fixed.
    
    Signed-off-by: Alexey Sotkin <alexey.sotkin@intel.com>

---
+++ clang/test/SemaSYCL/spec_const_and_accesor_crash.cpp
@@ -0,0 +1,19 @@
+// RUN: %clang_cc1 -fsycl -fsycl-is-device -fsyntax-only -I %S/Inputs -ast-dump %s | FileCheck %s
+// The test checks that Clang doesn't crash if a specialization constant gets
+// into the kernel capture list before an accessor
+
+#include <sycl.hpp>
+
+template <typename name, typename Func>
+__attribute__((sycl_kernel)) void kernel(Func kernelFunc) {
+  kernelFunc();
+}
+
+int main() {
+  cl::sycl::experimental::spec_constant<char, class MyInt32Const> spec_const;
+  cl::sycl::accessor<int, 1, cl::sycl::access::mode::read_write> accessor;
+  // CHECK: FieldDecl {{.*}} implicit referenced 'cl::sycl::experimental::spec_constant<char, class MyInt32Const>'
+  // CHECK: FieldDecl {{.*}} implicit referenced 'cl::sycl::accessor<int, 1, cl::sycl::access::mode::read_write>'
+  kernel<class MyKernel>([spec_const, accessor]() {});
+  return 0;
+}
