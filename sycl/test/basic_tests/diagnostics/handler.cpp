80642000543dca4ce0c918f2935786b346c4c2d5
Bjoern Knafla <bjoern@codeplay.com>
2020-04-29 18:17:47 +0100

    [SYCL][CUDA] Run more LIT tests with CUDA
    Make the backend used explicit in more LIT tests.
    These tests failed on machines with only NVIDIA OpenCL available as it
    is not supported.
    
    Signed-off-by: Bjoern Knafla <bjoern@codeplay.com>

--- sycl/test/basic_tests/diagnostics/handler.cpp
+++ sycl/test/basic_tests/diagnostics/handler.cpp
@@ -1,5 +1,8 @@
-// RUN: %clangxx -fsycl %s -o %t.out
-// RUN: %t.out | FileCheck %s
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
+// RUN: env SYCL_DEVICE_TYPE=HOST %t.out | FileCheck %s
+// RUN: %CPU_RUN_PLACEHOLDER %t.out %CPU_CHECK_PLACEHOLDER
+// RUN: %GPU_RUN_PLACEHOLDER %t.out %GPU_CHECK_PLACEHOLDER
+// RUN: %ACC_RUN_PLACEHOLDER %t.out %ACC_CHECK_PLACEHOLDER
 //==------------------- handler.cpp ----------------------------------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
--- sycl/test/basic_tests/queue.cpp
+++ sycl/test/basic_tests/queue.cpp
@@ -1,6 +1,8 @@
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
 // RUN: env SYCL_DEVICE_TYPE=HOST %t.out
-// RUN: %t.out
+// RUN: %CPU_RUN_PLACEHOLDER %t.out
+// RUN: %GPU_RUN_PLACEHOLDER %t.out
+// RUN: %ACC_RUN_PLACEHOLDER %t.out
 //==--------------- queue.cpp - SYCL queue test ----------------------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
