c470b6d87ec84ce6619cdfb862cab7538db02e32
Byoungro So <byoungro.so@intel.com>
2020-05-07 16:41:11 -0700

    match signatures of pi_opencl.cpp with pi.h Also, added regression test for checking pi* export symbols
    Signed-off-by: Byoungro So <byoungro.so@intel.com>

---
+++ sycl/test/regression/pi_opencl_symbol_check.cpp
@@ -0,0 +1,6 @@
+// RUN: env LLVM_BIN_PATH=%llvm_build_bin_dir python %sycl_tools_src_dir/abi_check.py --mode check_symbols --reference %S/pi_opencl_symbol_check.txt %sycl_libs_dir/libpi_opencl.so
+// expected-no-diagnostics
+//
+//===----------------------------------------------------------------------===//
+// This test checks if there is any change in export symbols in libpi_opencl.so
+//===----------------------------------------------------------------------===//
