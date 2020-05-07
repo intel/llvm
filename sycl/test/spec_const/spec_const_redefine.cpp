d0a68467ddb6bd90f3cf48a30c2c401a2073442f
Konstantin S Bobrovsky <konstantin.s.bobrovsky@intel.com>
2020-05-06 18:28:24 -0700

    [SQUASH] Disable CPU SC redefine test due to old CI CPU RT.

--- sycl/test/spec_const/spec_const_redefine.cpp
+++ sycl/test/spec_const/spec_const_redefine.cpp
@@ -1,9 +1,10 @@
 // RUN: %clangxx -fsycl %s -o %t.out
 // RUN: env SYCL_DEVICE_TYPE=HOST %t.out
-// RUN: env SYCL_PI_TRACE=2 %CPU_RUN_PLACEHOLDER %t.out 2>&1 %CPU_CHECK_PLACEHOLDER
+// TODO re-enable CPU device test once CI CPU OCL RT is updated:
+// RUNx: env SYCL_PI_TRACE=2 %CPU_RUN_PLACEHOLDER %t.out 2>&1 %CPU_CHECK_PLACEHOLDER
 // RUN: env SYCL_PI_TRACE=2 %GPU_RUN_PLACEHOLDER %t.out 2>&1 %GPU_CHECK_PLACEHOLDER
 // RUN: env SYCL_PI_TRACE=2 %ACC_RUN_PLACEHOLDER %t.out 2>&1 %ACC_CHECK_PLACEHOLDER
-// TODO the test currently fails on devices these devices:
+// TODO the test currently fails on these devices:
 // XFAIL: acc,cuda
 //
 //==----------- spec_const_redefine.cpp ------------------------------------==//
