feb39c27e89ebbf4887a2034335eb924c9aee5cf
Byoungro So <byoungro.so@intel.com>
2020-05-07 22:58:20 -0700

    fixed Windows LIT error
    Signed-off-by: Byoungro So <byoungro.so@intel.com>

--- sycl/test/regression/pi_opencl_symbol_check.cpp
+++ sycl/test/regression/pi_opencl_symbol_check.cpp
@@ -1,4 +1,5 @@
 // RUN: env LLVM_BIN_PATH=%llvm_build_bin_dir python %sycl_tools_src_dir/abi_check.py --mode check_symbols --reference %S/pi_opencl_symbol_check.txt %sycl_libs_dir/libpi_opencl.so
+// REQUIRES: linux
 // expected-no-diagnostics
 //
 //===----------------------------------------------------------------------===//
