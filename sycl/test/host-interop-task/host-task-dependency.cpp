168beb2babf9542a78339cafe4f2ff322a079dde
Sergey Kanaev <sergey.kanaev@intel.com>
2020-05-10 22:25:31 +0300

    [SYCL] Address review comments.
    Signed-off-by: Sergey Kanaev <sergey.kanaev@intel.com>

--- sycl/test/host-interop-task/host-task-dependency.cpp
+++ sycl/test/host-interop-task/host-task-dependency.cpp
@@ -1,7 +1,4 @@
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out %threads_lib
-// RUN: %CPU_RUN_PLACEHOLDER %t.out
-// RUN: %GPU_RUN_PLACEHOLDER %t.out
-// RUN: %ACC_RUN_PLACEHOLDER %t.out
 // RUN: %CPU_RUN_PLACEHOLDER SYCL_PI_TRACE=-1 %t.out 2>&1 %CPU_CHECK_PLACEHOLDER
 // RUN: %GPU_RUN_PLACEHOLDER SYCL_PI_TRACE=-1 %t.out 2>&1 %GPU_CHECK_PLACEHOLDER
 // RUN: %ACC_RUN_PLACEHOLDER SYCL_PI_TRACE=-1 %t.out 2>&1 %ACC_CHECK_PLACEHOLDER
