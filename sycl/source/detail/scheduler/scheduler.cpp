2aab6a16946123e41beb5dab38d30e5f218b8899
Sergey Kanaev <sergey.kanaev@intel.com>
2020-05-12 17:44:24 +0300

    [SYCL] Employ only read-lock while enqueueing recently added command.
    Signed-off-by: Sergey Kanaev <sergey.kanaev@intel.com>

--- sycl/source/detail/scheduler/scheduler.cpp
+++ sycl/source/detail/scheduler/scheduler.cpp
@@ -79,6 +79,10 @@ EventImplPtr Scheduler::addCG(std::unique_ptr<detail::CG> CommandGroup,
     default:
       NewCmd = MGraphBuilder.addCG(std::move(CommandGroup), std::move(Queue));
     }
+  }
+
+  {
+    std::shared_lock<std::shared_timed_mutex> Lock(MGraphLock);
 
     // TODO: Check if lazy mode.
     EnqueueResultT Res;
