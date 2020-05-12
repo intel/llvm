a07f0f70d5f0b56a4fa5330513e14fd968a8f482
Stuart Adams <stuart.adams@codeplay.com>
2020-05-12 13:30:09 +0100

    Implemented PR feedback, cleaned up new header files.

--- sycl/unittests/pi/BackendString.hpp
+++ sycl/unittests/pi/BackendString.hpp
@@ -1,11 +1,13 @@
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 #pragma once
 
 #include <detail/plugin.hpp>
 
-using namespace cl::sycl;
-
 namespace pi {
-inline const char *GetBackendString(backend backend) {
+inline const char *GetBackendString(cl::sycl::backend backend) {
   switch (backend) {
 #define PI_BACKEND_STR(backend_name)                                           \
   case cl::sycl::backend::backend_name:                                        \
--- sycl/unittests/pi/cuda/test_get_plugin.hpp
+++ sycl/unittests/pi/cuda/test_get_plugin.hpp
@@ -1,8 +1,9 @@
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 #pragma once
 
-#include <CL/sycl.hpp>
-#include <CL/sycl/detail/pi.hpp>
-#include <algorithm>
 #include <detail/plugin.hpp>
 
 using namespace cl::sycl;
