191be27aa2986094894ba4bbbffac4c18ee1439f
Aleksander Fadeev <aleksander.fadeev@intel.com>
2020-05-19 12:27:40 +0300

    warnings.cpp fix
    Signed-off-by: Aleksander Fadeev <aleksander.fadeev@intel.com>

--- sycl/test/warnings/warnings.cpp
+++ sycl/test/warnings/warnings.cpp
@@ -1,4 +1,4 @@
-// RUN: %clangxx -fsycl -I %source_sycl_include -Wall -Wextra -Wpessimizing-move -Wunused-variable -Wmismatched-tags -Wunneeded-internal-declaration -Werror -Wno-unknown-cuda-version  %s -o %t.out
+// RUN: %clangxx -fsycl -I %source_sycl_include -Wall -Wextra -Wno-ignored-attributes -Wpessimizing-move -Wunused-variable -Wmismatched-tags -Wunneeded-internal-declaration -Werror -Wno-unknown-cuda-version  %s -o %t.out
 
 #include <CL/sycl.hpp>
 
