ee7d0017f6a8e4bbb532dede65b4bf11c7fa1c9e
Chris Perkins <chris.perkins@intel.com>
2020-05-19 17:34:16 -0700

    DO NOT MERGE - testing CI system
    Signed-off-by: Chris Perkins <chris.perkins@intel.com>

--- sycl/source/detail/device_impl.cpp
+++ sycl/source/detail/device_impl.cpp
@@ -35,6 +35,7 @@ device_impl::device_impl(pi_native_handle InteropDeviceHandle,
                          RT::PiDevice Device, PlatformImplPtr Platform,
                          const plugin &Plugin)
     : MDevice(Device), MIsHostDevice(false) {
+  throw "yowza";
 
   bool InteroperabilityConstructor = false;
   if (Device == nullptr) {
