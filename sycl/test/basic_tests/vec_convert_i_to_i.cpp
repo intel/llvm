784952658e0dc0bc079987a8fcc2e187c07bfe3a
Aleksander Fadeev <aleksander.fadeev@intel.com>
2020-05-21 17:43:12 +0300

    vec_convert_i_to_i.cpp addition
    Signed-off-by: Aleksander Fadeev <aleksander.fadeev@intel.com>

--- sycl/test/basic_tests/vec_convert_i_to_i.cpp
+++ sycl/test/basic_tests/vec_convert_i_to_i.cpp
@@ -47,5 +47,14 @@ int main() {
   test<ulong, int, 8, rounding_mode::automatic>(
       ulong8{3000000000, 2147483647, 100, 150, 2147483648, 1000, 0, 1},
       int8{-1294967296, 2147483647, 100, 150, -2147483648, 1000, 0, 1});
+
+  test<longlong, ulonglong, 1, rounding_mode::automatic>(
+      longlong{1},
+      ulonglong{1});
+
+    test<ulonglong, longlong, 1, rounding_mode::automatic>(
+      ulonglong{1},
+      longlong{1});
+
   return 0;
 }
