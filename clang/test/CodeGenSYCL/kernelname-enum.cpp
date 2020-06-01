e939891ed74160a5e0e38df9e6f96e92c0005c51
Elizabeth Andrews <elizabeth.andrews@intel.com>
2020-06-01 08:07:53 -0700

    Add support for KernelNameType with is a template specialization class with template template arguments which in turn is templated using enum.
    Signed-off-by: Elizabeth Andrews <elizabeth.andrews@intel.com>

--- clang/test/CodeGenSYCL/kernelname-enum.cpp
+++ clang/test/CodeGenSYCL/kernelname-enum.cpp
@@ -94,6 +94,20 @@ class T2 {};
 template <typename EnumType>
 class T3 {};
 
+enum class EnumTypeOut : int { A,
+                               B,
+};
+enum class EnumValueIn : int { A,
+                               B,
+};
+template <EnumValueIn EnumValue, typename EnumTypeIn>
+class Baz;
+template <typename EnumTypeOut, template <EnumValueIn EnumValue, typename EnumTypeIn> class T>
+class dummy_functor_8 {
+public:
+  void operator()() {}
+};
+
 int main() {
 
   dummy_functor_1<no_namespace_int::val_1> f1;
@@ -104,6 +118,7 @@ int main() {
   dummy_functor_6<unscoped_enum::val_1> f6;
   dummy_functor_7<no_namespace_int> f7;
   dummy_functor_7<internal::namespace_short> f8;
+  dummy_functor_8<EnumTypeOut, Baz> f9;
 
   cl::sycl::queue q;
 
@@ -147,6 +162,10 @@ int main() {
     cgh.single_task<T1<T3<type_argument_template_enum::E>>>([=]() {});
   });
 
+  q.submit([&](cl::sycl::handler &cgh) {
+    cgh.single_task(f9);
+  });
+
   return 0;
 }
 
@@ -173,6 +192,10 @@ int main() {
 // CHECK-NEXT: }
 // CHECK: template <type_argument_template_enum::E EnumValue> class T2;
 // CHECK: template <typename T> class T1;
+// CHECK: enum class EnumTypeOut : int;
+// CHECK: enum class EnumValueIn : int;
+// CHECK: template <EnumValueIn EnumValue, typename EnumTypeIn> class Baz;
+// CHECK: template <typename EnumTypeOut, template <EnumValueIn EnumValue, typename EnumTypeIn> class T> class dummy_functor_8;
 // CHECK: Specializations of KernelInfo for kernel function types:
 // CHECK: template <> struct KernelInfo<::dummy_functor_1<(no_namespace_int)0>>
 // CHECK: template <> struct KernelInfo<::dummy_functor_2<(no_namespace_short)1>>
@@ -184,3 +207,4 @@ int main() {
 // CHECK: template <> struct KernelInfo<::dummy_functor_7<::internal::namespace_short>>
 // CHECK: template <> struct KernelInfo<::T1<::T2<(type_argument_template_enum::E)0>>>
 // CHECK: template <> struct KernelInfo<::T1<::T3<::type_argument_template_enum::E>>>
+// CHECK: template <> struct KernelInfo<::dummy_functor_8<::EnumTypeOut, Baz>>
