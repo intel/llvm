d00f58ba5f12ccc527b2d0b26dc4dadb4fb8f69c
Sergey Kanaev <sergey.kanaev@intel.com>
2020-06-02 15:19:16 +0300

    [SYCL] Add header to unittest file
    Signed-off-by: Sergey Kanaev <sergey.kanaev@intel.com>

--- sycl/unittests/scheduler/CommandsWaitForEvents.cpp
+++ sycl/unittests/scheduler/CommandsWaitForEvents.cpp
@@ -1,3 +1,11 @@
+//==-------- CommandsWaitForEvents.cpp --- Scheduler unit tests ------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "SchedulerTest.hpp"
 #include "SchedulerTestUtils.hpp"
 #include <helpers/PiMock.hpp>
