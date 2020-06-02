b7c8345fda40acdd6b1cf834089655a898219d39
Srividya Sundaram <srividya.sundaram@intel.com>
2020-06-02 15:30:17 -0700

    Fix clang-format errors

--- clang/test/CodeGenSYCL/remove-ur-inst.cpp
+++ clang/test/CodeGenSYCL/remove-ur-inst.cpp
@@ -1,9 +1,6 @@
 // RUN: %clang_cc1 -fsycl -fsycl-is-device -triple spir64-unknown-unknown-sycldevice -emit-llvm %s -o - | FileCheck %s
 
-
-SYCL_EXTERNAL void doesNotReturn ()
-     throw () __attribute__ ((__noreturn__));
-
+SYCL_EXTERNAL void doesNotReturn() throw() __attribute__((__noreturn__));
 
 template <typename name, typename Func>
 __attribute__((sycl_kernel)) void kernel(Func kernelFunc) {
