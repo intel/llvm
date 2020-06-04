016d98c969a55cbd3e70a963acb27a09c37bda3f
Premanand M Rao <premanand.m.rao@intel.com>
2020-06-04 11:00:21 -0700

    [SYCL] Do not emit unneeded static initializations in sycl device code
    Signed-off-by: Premanand M Rao <premanand.m.rao@intel.com>

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -1276,12 +1276,8 @@ void CodeGenModule::EmitCtorList(CtorList &Fns, const char *GlobalName) {
     ctor.addInt(Int32Ty, I.Priority);
     ctor.add(llvm::ConstantExpr::getBitCast(I.Initializer, CtorPFTy));
     if (I.AssociatedData)
-      if (I.AssociatedData->getType()->getPointerAddressSpace() !=
-          VoidPtrTy->getAddressSpace())
-        ctor.add(
-            llvm::ConstantExpr::getAddrSpaceCast(I.AssociatedData, VoidPtrTy));
-      else
-        ctor.add(llvm::ConstantExpr::getBitCast(I.AssociatedData, VoidPtrTy));
+      ctor.add(llvm::ConstantExpr::getPointerBitCastOrAddrSpaceCast(
+               I.AssociatedData, VoidPtrTy));
     else
       ctor.addNullPointer(VoidPtrTy);
     ctor.finishAndAddTo(ctors);
