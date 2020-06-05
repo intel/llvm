02ebca56b90350742e4d909ca2aa0a2c8513f440
Dmitry Vodopyanov <dmitry.vodopyanov@intel.com>
2020-06-05 20:42:20 +0300

    [SYCL] Fix ABI test for Windows
    Signed-off-by: Dmitry Vodopyanov <dmitry.vodopyanov@intel.com>

--- sycl/test/abi/symbol_size.cpp
+++ sycl/test/abi/symbol_size.cpp
@@ -44,7 +44,7 @@ int main() {
   check_size<event, 16>();
   check_size<gpu_selector, 8>();
 #ifdef _MSC_VER
-  check_size<handler, 528>();
+  check_size<handler, 552>();
 #else
   check_size<handler, 560>();
 #endif
