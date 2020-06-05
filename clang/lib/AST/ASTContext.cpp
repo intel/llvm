87e6240cb099bb412ddb8f7f3383e0265c6925db
schittir <49215194+schittir@users.noreply.github.com>
2020-06-05 03:38:35 -0700

    [SYCL] Matching sizeof long double of SYCL device with that of host (#1786)
    static_assert of sizeof(long double)>8 fails to compile with -fsycl
    switch on Linux x86_64 bit target because sizeof(long double) for SYCL device is
    8 bytes whereas the host is 16 bytes.
    
    This commit sets sizeof(long double) of SYCL device to match that of host

--- clang/lib/AST/ASTContext.cpp
+++ clang/lib/AST/ASTContext.cpp
@@ -2065,7 +2065,9 @@ TypeInfo ASTContext::getTypeInfoImpl(const Type *T) const {
       Align = Target->getDoubleAlign();
       break;
     case BuiltinType::LongDouble:
-      if (getLangOpts().OpenMP && getLangOpts().OpenMPIsDevice &&
+      if (((getLangOpts().SYCL && getLangOpts().SYCLIsDevice) ||
+           (getLangOpts().OpenMP && getLangOpts().OpenMPIsDevice)) &&
+          AuxTarget != nullptr &&
           (Target->getLongDoubleWidth() != AuxTarget->getLongDoubleWidth() ||
            Target->getLongDoubleAlign() != AuxTarget->getLongDoubleAlign())) {
         Width = AuxTarget->getLongDoubleWidth();
