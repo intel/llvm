01af47f938ffe883a1c02c4cf037c8c8ceb0da63
John Pennycook <john.pennycook@intel.com>
2020-06-11 09:01:05 -0700

    [SYCL] Disable atomic_ref tests on FPGA
    Signed-off-by: John Pennycook <john.pennycook@intel.com>

--- sycl/test/atomic_ref/add.cpp
+++ sycl/test/atomic_ref/add.cpp
@@ -2,7 +2,6 @@
 // RUN: env SYCL_DEVICE_TYPE=HOST %t.out
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
-// RUN: %ACC_RUN_PLACEHOLDER %t.out
 
 #include <CL/sycl.hpp>
 #include <algorithm>
--- sycl/test/atomic_ref/min.cpp
+++ sycl/test/atomic_ref/min.cpp
@@ -2,7 +2,6 @@
 // RUN: env SYCL_DEVICE_TYPE=HOST %t.out
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
-// RUN: %ACC_RUN_PLACEHOLDER %t.out
 // XFAIL: cuda
 
 #include <CL/sycl.hpp>
