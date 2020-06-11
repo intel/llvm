3ec79ba9d6f616dfcd278c9b7861616acb555550
Alexander Flegontov <alexander.flegontov@intel.com>
2020-06-11 12:49:17 +0300

    [SYCL] Fix race that occurs when submitting to single queue in parallel
    Signed-off-by: Alexander Flegontov <alexander.flegontov@intel.com>

--- sycl/source/detail/scheduler/scheduler.cpp
+++ sycl/source/detail/scheduler/scheduler.cpp
@@ -83,7 +83,8 @@ EventImplPtr Scheduler::addCG(std::unique_ptr<detail::CG> CommandGroup,
   }
 
   {
-    std::shared_lock<std::shared_timed_mutex> Lock(MGraphLock);
+    std::unique_lock<std::shared_timed_mutex> Lock(MGraphLock, std::defer_lock);
+    lockSharedTimedMutex(Lock);
 
     // TODO: Check if lazy mode.
     EnqueueResultT Res;
