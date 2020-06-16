765a23a381226aba283ab2976f0cf62a0098cc49
iburylov <ilya.burylov@intel.com>
2020-06-16 18:53:42 +0300

    a little bit more of clang-format
    Signed-off-by: iburylov <ilya.burylov@intel.com>

--- sycl/include/CL/sycl/access/access.hpp
+++ sycl/include/CL/sycl/access/access.hpp
@@ -75,12 +75,12 @@ namespace {
 
 constexpr const auto &read_only =
     sycl::detail::InlineVariableHelper<mode_tag_t<access_mode::read>>::value;
-constexpr const auto &read_write =
-    sycl::detail::InlineVariableHelper<mode_tag_t<access_mode::read_write>>::value;
+constexpr const auto &read_write = sycl::detail::InlineVariableHelper<
+    mode_tag_t<access_mode::read_write>>::value;
 constexpr const auto &write_only =
     sycl::detail::InlineVariableHelper<mode_tag_t<access_mode::write>>::value;
-constexpr const auto &read_constant =
-    sycl::detail::InlineVariableHelper<mode_target_tag_t<access_mode::read, target::constant_buffer>>::value;
+constexpr const auto &read_constant = sycl::detail::InlineVariableHelper<
+    mode_target_tag_t<access_mode::read, target::constant_buffer>>::value;
 
 } // namespace
 
