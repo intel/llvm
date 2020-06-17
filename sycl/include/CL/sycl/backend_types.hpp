8031c63271c060d018dbdeb9ba2f0e49b19acf8f
Gail Lyons <gail.lyons@intel.com>
2020-06-17 13:08:17 -0700

    This API returns the backend associated with a platform. A LIT test has also been added. Incorporated code review comments.
    Signed-off-by: Gail Lyons <gail.lyons@intel.com>

--- sycl/include/CL/sycl/backend_types.hpp
+++ sycl/include/CL/sycl/backend_types.hpp
@@ -10,12 +10,34 @@
 
 #include <CL/sycl/detail/defines.hpp>
 
+#include <iostream>
+
 __SYCL_INLINE_NAMESPACE(cl) {
 namespace sycl {
 
 enum class backend : char { host, opencl, level0, cuda };
 
 template <backend name, typename SYCLObjectT> struct interop;
+
+inline std::ostream &operator<<(std::ostream &Out, backend be) {
+  switch (be) {
+  case backend::host:
+    Out << std::string("host");
+    break;
+  case backend::opencl:
+    Out << std::string("opencl");
+    break;
+  case backend::level0:
+    Out << std::string("level0");
+    break;
+  case backend::cuda:
+    Out << std::string("cuda");
+    break;
+  default:
+    Out << std::string("unknown");
+  }
+  return Out;
+}
 
 } // namespace sycl
 } // __SYCL_INLINE_NAMESPACE(cl)
