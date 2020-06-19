3857f545064206f2c9e9650e02bd9eccb37e7298
Sindhu Chittireddy <sindhu.chittireddy@intel.com>
2020-06-19 00:36:03 -0700

    [SYCL] Fixing comment about propagation of attributes to kernel
    Also adding a test to check that the kernel does not get attributes
    from functions that are not directly called by it.

---
+++ clang/test/SemaSYCL/check-notdirect-attribute-propagation.cpp
@@ -0,0 +1,20 @@
+// RUN: %clang_cc1 -std=c++11 -fsyntax-only -fsycl -fsycl-is-device -verify %s
+
+[[intelfpga::no_global_work_offset]] void not_direct() {} //expected-warning {{'no_global_work_offset' attribute ignored}}
+
+void func() { not_direct(); }
+
+template <typename Name, typename Type>
+[[clang::sycl_kernel]] void __my_kernel__(Type bar) {
+  bar();
+  func();
+}
+
+template <typename Name, typename Type>
+void parallel_for(Type lambda) {
+  __my_kernel__<Name>(lambda);
+}
+
+void invoke_foo2() {
+  parallel_for<class KernelName>([]() {});
+}
