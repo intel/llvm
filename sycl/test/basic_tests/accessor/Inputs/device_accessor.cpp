608b3c2d387231062de9f6efe472bbfc83031cdf
iburylov <ilya.burylov@intel.com>
2020-06-23 18:31:46 +0300

    fixed filename in file header
    Signed-off-by: iburylov <ilya.burylov@intel.com>

--- sycl/test/basic_tests/accessor/Inputs/device_accessor.cpp
+++ sycl/test/basic_tests/accessor/Inputs/device_accessor.cpp
@@ -1,4 +1,4 @@
-//==----------------accessor.cpp - SYCL accessor basic test ----------------==//
+//==---------- device_accessor.cpp - SYCL accessor basic test --------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
