1ec09d06ed41fbe8928da6d0bf30f513fcf9d170
Dmitry Sidorov <dmitry.sidorov@intel.com>
2020-06-24 10:55:49 +0300

    [SYCL] Change associated numbers for global_device/host addr spaces
    Thus making SPIR AS map continuous.
    
    Signed-off-by: Dmitry Sidorov <dmitry.sidorov@intel.com>

--- clang/lib/Basic/Targets/SPIR.h
+++ clang/lib/Basic/Targets/SPIR.h
@@ -30,8 +30,8 @@ static const unsigned SPIRAddrSpaceMap[] = {
     2,  // opencl_constant
     0,  // opencl_private
     4,  // opencl_generic
-    11, // opencl_global_device
-    12, // opencl_global_host
+    5, // opencl_global_device
+    6, // opencl_global_host
     0,  // cuda_device
     0,  // cuda_constant
     0,  // cuda_shared
@@ -47,8 +47,8 @@ static const unsigned SYCLAddrSpaceMap[] = {
     2,  // opencl_constant
     0,  // opencl_private
     4,  // opencl_generic
-    11, // opencl_global_device
-    12, // opencl_global_host
+    5, // opencl_global_device
+    6, // opencl_global_host
     0,  // cuda_device
     0,  // cuda_constant
     0,  // cuda_shared
