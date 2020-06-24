f66cd1eeb0eceba848269f1e934a5f768e6bd2de
Sergey Semenov <sergey.semenov@intel.com>
2020-06-24 08:44:41 +0300

    Add a header to the unit test
    Signed-off-by: Sergey Semenov <sergey.semenov@intel.com>

--- sycl/unittests/queue/wait.cpp
+++ sycl/unittests/queue/wait.cpp
@@ -1,3 +1,11 @@
+//==----------------- wait.cpp --- queue wait unit test --------------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include <CL/sycl.hpp>
 #include <detail/context_impl.hpp>
 #include <gtest/gtest.h>
