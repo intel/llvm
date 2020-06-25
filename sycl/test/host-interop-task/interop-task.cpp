285cb5a1735a4a50e730379009506062b303993b
Sergey Kanaev <sergey.kanaev@intel.com>
2020-06-25 23:09:42 +0300

    [SYCL] Add test
    Signed-off-by: Sergey Kanaev <sergey.kanaev@intel.com>

--- sycl/test/host-interop-task/interop-task.cpp
+++ sycl/test/host-interop-task/interop-task.cpp
@@ -194,10 +194,36 @@ void test4() {
   });
 }
 
+void test5() {
+  buffer<int, 1> Buffer1{BUFFER_SIZE};
+  buffer<int, 1> Buffer2{BUFFER_SIZE};
+
+  queue Q;
+
+  Q.submit([&](handler &CGH) {
+    auto Acc = Buffer1.template get_access<mode::write>(CGH);
+
+    auto Kernel = [=](item<1> Id) { Acc[Id] = 123; };
+    CGH.parallel_for<class Test5Init>(Acc.get_count(), Kernel);
+  });
+
+  copy(Buffer1, Buffer2, Q);
+
+  {
+    auto Acc = Buffer2.get_access<mode::read>();
+
+    for (size_t Idx =0; Idx < Acc.get_count(); ++Idx) {
+      std::cout << "Second buffer [" << Idx << "] = " << Acc[Idx] << std::endl;
+      assert(Acc[Idx] == 123);
+    }
+  }
+}
+
 int main() {
   test1();
   test2();
   test3();
   test4();
+  test5();
   return 0;
 }
