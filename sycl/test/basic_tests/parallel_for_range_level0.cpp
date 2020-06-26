a5c1cf074caf3d79b630d10c1d64296671a70f03
rbegam <rehana.begam@intel.com>
2020-06-26 14:16:16 -0700

    fix the run command for the test.
    Signed-off-by: rbegam <rehana.begam@intel.com>

--- sycl/test/basic_tests/parallel_for_range_level0.cpp
+++ sycl/test/basic_tests/parallel_for_range_level0.cpp
@@ -1,8 +1,10 @@
-// RUN: %clangxx -fsycl %s -o %t.out
+// XFAIL: cuda
+// CUDA exposes broken hierarchical parallelism.
+
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple  %s -o %t.out
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 // RUN: %ACC_RUN_PLACEHOLDER %t.out
-// XFAIL: cuda
 
 #include <CL/sycl.hpp>
 
