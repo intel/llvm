09f41c58d3b0b8f3306ed684928a3e755a851c41
Wei Wu <wei.wu@intel.com>
2020-06-30 23:41:19 +0800

    [SYCL][NFC][SUBGROUPS] Separate half/double type tests from full type tests

--- sycl/test/sub_group/broadcast_fp16.cpp
+++ sycl/test/sub_group/broadcast_fp16.cpp
@@ -1,21 +1,18 @@
 // UNSUPPORTED: cuda
 // CUDA compilation and runtime do not yet support sub-groups.
-//
-// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
-// RUN: env SYCL_DEVICE_TYPE=HOST %t.out
-// RUN: %CPU_RUN_PLACEHOLDER %t.out
-// RUN: %GPU_RUN_PLACEHOLDER %t.out
-// RUN: %ACC_RUN_PLACEHOLDER %t.out
 
-//==--------------- scan.cpp - SYCL sub_group scan test --------*- C++ -*---==//
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
+// RUN: %GPU_RUN_PLACEHOLDER %t.out
+
+//==--------- broadcast_fp16.cpp - SYCL sub_group broadcast test ----*- C++ -*---==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-//===----------------------------------------------------------------------===//
+//===---------------------------------------------------------------------------===//
 
-#include "scan.hpp"
+#include "broadcast.hpp"
 
 int main() {
   queue Queue;
@@ -23,11 +20,6 @@ int main() {
     std::cout << "Skipping test\n";
     return 0;
   }
-  check<int>(Queue);
-  check<unsigned int>(Queue);
-  check<long>(Queue);
-  check<unsigned long>(Queue);
-  check<float>(Queue);
-  std::cout << "Test passed." << std::endl;
+  check<cl::sycl::half>(Queue);
   return 0;
 }
