7d6ede430ea9e498bc71533ea46a33d5edae7f4b
Victor Lomuller <victor@codeplay.com>
2020-06-30 20:20:11 +0100

    [SYCL-PTX] Add bindings with libdevice for math and relational (#2013)
    Bind the following builtins to libdevice functions:
    
    - atan2
    - copysign
    - cospi
    - fdim
    - fma
    - frexp
    - hypot
    - ilogb
    - ldexp
    - log
    - log10
    - log1p
    - log2
    - logb
    - modf
    - native_cos
    - native_log
    - native_log10
    - native_log2
    - native_powr
    - native_rsqrt
    - native_sin
    - native_sqrt
    - native_tan
    - nextafter
    - pow
    - remainer
    - round
    - rsqrt
    - sin
    - sincos
    - sinh
    - sinpi
    - tan
    - tanh
    - tgamma
    - trunc
    - isfinite
    - isinf
    - isnan
    
    Signed-off-by: Victor Lomuller <victor@codeplay.com>

--- libclc/generic/include/clcmacro.h
+++ libclc/generic/include/clcmacro.h
@@ -134,14 +134,14 @@
                               ADDR_SPACE, ARG2_TYPE)                           \
   DECLSPEC RET_TYPE##2 FUNCTION(ARG1_TYPE##2 x, ADDR_SPACE ARG2_TYPE##2 * y) { \
     return (RET_TYPE##2)(                                                      \
-        FUNCTION(x.x, (ARG2_TYPE *)y),                                         \
+        FUNCTION(x.x, (ADDR_SPACE ARG2_TYPE *)y),                              \
         FUNCTION(x.y,                                                          \
                  (ADDR_SPACE ARG2_TYPE *)((ADDR_SPACE ARG2_TYPE *)y + 1)));    \
   }                                                                            \
                                                                                \
   DECLSPEC RET_TYPE##3 FUNCTION(ARG1_TYPE##3 x, ADDR_SPACE ARG2_TYPE##3 * y) { \
     return (RET_TYPE##3)(                                                      \
-        FUNCTION(x.x, (ARG2_TYPE *)y),                                         \
+        FUNCTION(x.x, (ADDR_SPACE ARG2_TYPE *)y),                              \
         FUNCTION(x.y,                                                          \
                  (ADDR_SPACE ARG2_TYPE *)((ADDR_SPACE ARG2_TYPE *)y + 1)),     \
         FUNCTION(x.z,                                                          \
@@ -150,14 +150,14 @@
                                                                                \
   DECLSPEC RET_TYPE##4 FUNCTION(ARG1_TYPE##4 x, ADDR_SPACE ARG2_TYPE##4 * y) { \
     return (RET_TYPE##4)(                                                      \
-        FUNCTION(x.lo, (ARG2_TYPE##2 *)y),                                     \
+        FUNCTION(x.lo, (ADDR_SPACE ARG2_TYPE##2 *)y),                          \
         FUNCTION(x.hi,                                                         \
                  (ADDR_SPACE ARG2_TYPE##2 *)((ADDR_SPACE ARG2_TYPE *)y + 2))); \
   }                                                                            \
                                                                                \
   DECLSPEC RET_TYPE##8 FUNCTION(ARG1_TYPE##8 x, ADDR_SPACE ARG2_TYPE##8 * y) { \
     return (RET_TYPE##8)(                                                      \
-        FUNCTION(x.lo, (ARG2_TYPE##4 *)y),                                     \
+        FUNCTION(x.lo, (ADDR_SPACE ARG2_TYPE##4 *)y),                          \
         FUNCTION(x.hi,                                                         \
                  (ADDR_SPACE ARG2_TYPE##4 *)((ADDR_SPACE ARG2_TYPE *)y + 4))); \
   }                                                                            \
@@ -165,10 +165,19 @@
   DECLSPEC RET_TYPE##16 FUNCTION(ARG1_TYPE##16 x,                              \
                                  ADDR_SPACE ARG2_TYPE##16 * y) {               \
     return (RET_TYPE##16)(                                                     \
-        FUNCTION(x.lo, (ARG2_TYPE##8 *)y),                                     \
+        FUNCTION(x.lo, (ADDR_SPACE ARG2_TYPE##8 *)y),                          \
         FUNCTION(x.hi,                                                         \
                  (ADDR_SPACE ARG2_TYPE##8 *)((ADDR_SPACE ARG2_TYPE *)y + 8))); \
   }
+
+#define _CLC_DEFINE_TERNARY_BUILTIN(RET_TYPE, FUNCTION, BUILTIN, ARG1_TYPE,    \
+                                    ARG2_TYPE, ARG3_TYPE)                      \
+  _CLC_DEF _CLC_OVERLOAD RET_TYPE FUNCTION(ARG1_TYPE x, ARG2_TYPE y,           \
+                                           ARG3_TYPE z) {                      \
+    return BUILTIN(x, y, z);                                                   \
+  }                                                                            \
+  _CLC_TERNARY_VECTORIZE(_CLC_OVERLOAD _CLC_DEF, RET_TYPE, FUNCTION,           \
+                         ARG1_TYPE, ARG2_TYPE, ARG3_TYPE)
 
 #define _CLC_DEFINE_BINARY_BUILTIN(RET_TYPE, FUNCTION, BUILTIN, ARG1_TYPE,     \
                                    ARG2_TYPE)                                  \
