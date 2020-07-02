e472489b842cc06a05774620256a7db59ed94cb8
Srividya Sundaram <srividya.sundaram@intel.com>
2020-07-02 11:13:01 -0700

    Removing -fms-extensions option.

--- clang/test/SemaSYCL/no-prototype-func.c
+++ clang/test/SemaSYCL/no-prototype-func.c
@@ -1,3 +1,3 @@
-// RUN: %clang_cc1 -fsycl -fsycl-is-device -triple spir64-unknown-unknown-sycldevice -emit-llvm -verify -fms-extensions %s
+// RUN: %clang_cc1 -fsycl -fsycl-is-device -triple spir64-unknown-unknown-sycldevice -emit-llvm -verify %s
 // expected-no-diagnostics
 int foo();
