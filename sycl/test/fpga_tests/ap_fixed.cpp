5176f295372e0933a1f4a0106e54ccf0d9fcc513
Alexey Bader <alexey.bader@intel.com>
2020-07-07 21:35:39 +0300

    [SYCL][NFC] Make lit tests agnostic to compiler optimizations
    As of today SYCL device compiler do not apply any LLVM transformations
    to emitted LLVM code, but SYCL tests shouldn't rely on this behavior, so
    this patch explicitly set compiler option to disable any LLVM passes.

--- sycl/test/fpga_tests/ap_fixed.cpp
+++ sycl/test/fpga_tests/ap_fixed.cpp
@@ -1,4 +1,4 @@
-// RUN: %clangxx -I %sycl_include -S -emit-llvm -fsycl-device-only %s -o - | FileCheck %s
+// RUN: %clangxx -I %sycl_include -S -emit-llvm -fsycl-device-only %s -o - -Xclang -disable-llvm-passes | FileCheck %s
 //
 //==---- ap_fixed.cpp - SYCL FPGA arbitrary precision fixed point test -----==//
 //
