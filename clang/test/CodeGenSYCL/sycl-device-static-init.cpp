bde1085c73d343457e489866a9e69658971a5099
premanandrao <47116977+premanandrao@users.noreply.github.com>
2020-07-07 05:27:59 -0400

    [SYCL] Do not emit const static data members that are not const-initialized (#2019)
    Const static data members need to be either zero-initialized or
    constant-initialized.  We were emitting all const static data
    members before, which needed address space casts from constant-space
    to private-space.
    
    This change also reverts the address-space cast introduced in
    https://github.com/intel/llvm/commit/88d20313c68011cc126b55b0ca7fb58e16e9b4fa
    
    Signed-off-by: Premanand M Rao <premanand.m.rao@intel.com>

--- clang/test/CodeGenSYCL/sycl-device-static-init.cpp
+++ clang/test/CodeGenSYCL/sycl-device-static-init.cpp
@@ -1,17 +1,13 @@
 // RUN: %clang_cc1 -fsycl -fsycl-is-device -triple spir64-unknown-unknown-sycldevice -disable-llvm-passes %s -emit-llvm -o -  | FileCheck %s
 // Test that static initializers do not force the emission of globals on sycl device
 
-// CHECK: %struct._ZTS16RegisterBaseInit.RegisterBaseInit = type { i8 }
 // CHECK-NOT: $_ZN8BaseInitI12TestBaseTypeE15s_regbase_ncsdmE = comdat any
 // CHECK: $_ZN8BaseInitI12TestBaseTypeE3varE = comdat any
-// CHECK: @_ZN8BaseInitI12TestBaseTypeE9s_regbaseE = {{.*}} global %struct._ZTS16RegisterBaseInit.RegisterBaseInit
-// CHECK-NOT: @_ZN8BaseInitI12TestBaseTypeE15s_regbase_ncsdmE = weak_odr addrspace(1) global %struct._ZTS16RegisterBaseInit.RegisterBaseInit zeroinitializer, comdat, align 1
 // CHECK: @_ZN8BaseInitI12TestBaseTypeE3varE = weak_odr addrspace(1) constant i32 9, comdat, align 4
-// CHECK: @llvm.global_ctors = appending global [1 x { i32, void ()*, i8* }] [{ i32, void ()*, i8* } { i32 65535, void ()* @__cxx_global_var_init, i8* addrspacecast (i8 addrspace(1)* getelementptr inbounds (%struct._ZTS16RegisterBaseInit.RegisterBaseInit, %struct._ZTS16RegisterBaseInit.RegisterBaseInit addrspace(1)* @_ZN8BaseInitI12TestBaseTypeE9s_regbaseE, i32 0, i32 0) to i8*) }]
+// CHECK-NOT: @_ZN8BaseInitI12TestBaseTypeE15s_regbase_ncsdmE = weak_odr addrspace(1) global %struct._ZTS16RegisterBaseInit.RegisterBaseInit zeroinitializer, comdat, align 1
 // CHECK-NOT: @_ZGVN8BaseInitI12TestBaseTypeE15s_regbase_ncsdmE = weak_odr global i64 0, comdat($_ZN8BaseInitI12TestBaseTypeE9s_regbaseE), align 8
 // CHECK: define spir_kernel void @_ZTSZ4mainE11fake_kernel()
-// CHECK: call spir_func void @"_ZZ4mainENK3$_0clE16RegisterBaseInit
-// CHECK: declare spir_func void @_ZN16RegisterBaseInit3fooEv
+// CHECK: call spir_func void @"_ZZ4mainENK3$_0clEv
 
 struct TestBaseType {};
 struct RegisterBaseInit {
@@ -33,11 +29,10 @@ const int BaseInit<T>::var = 9;
 template struct BaseInit<TestBaseType>;
 template <typename name, typename Func>
 __attribute__((sycl_kernel)) void kernel_single_task(Func kernelFunc) {
-  kernelFunc(BaseInit<TestBaseType>::s_regbase);
+  kernelFunc();
 }
 int main() {
-  kernel_single_task<class fake_kernel>([=](RegisterBaseInit s) {
-    s.foo();
+  kernel_single_task<class fake_kernel>([=]() {
   });
   return 0;
 }
