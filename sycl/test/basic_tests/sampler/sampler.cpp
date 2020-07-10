15153c727da3484e0d61d84902109a368ba00b27
Alexey Bader <alexey.bader@intel.com>
2020-07-10 15:41:34 +0300

    Update sycl/test/basic_tests/sampler/sampler.cpp

--- sycl/test/basic_tests/sampler/sampler.cpp
+++ sycl/test/basic_tests/sampler/sampler.cpp
@@ -3,8 +3,6 @@
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 // RUN: %ACC_RUN_PLACEHOLDER %t.out
-// XFAIL: level0
-// "die: piextKernelSetArgSampler: not implemented"
 
 //==--------------- sampler.cpp - SYCL sampler basic test ------------------==//
 //
