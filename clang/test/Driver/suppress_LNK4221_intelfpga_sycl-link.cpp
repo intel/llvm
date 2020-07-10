6a97acb8eefacb0f655c601ca8a0cada4de918c3
gejin <ge.jin@intel.com>
2020-07-10 13:34:13 +0800

    remove unused code in test
    Signed-off-by: gejin <ge.jin@intel.com>

--- clang/test/Driver/suppress_LNK4221_intelfpga_sycl-link.cpp
+++ clang/test/Driver/suppress_LNK4221_intelfpga_sycl-link.cpp
@@ -4,6 +4,3 @@
 // RUN:  %clang_cl -target x86_64-windows-msvc-pc -fsycl -fintelfpga -fsycl-link -Xshardware %s -### 2>&1 \
 // RUN:  | FileCheck %s
 // CHECK: /IGNORE:4221
-int main() {
-  return 0;
-}
