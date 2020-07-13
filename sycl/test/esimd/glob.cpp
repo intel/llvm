59242b6994ef252d0e6872b65507d8be977f6104
Konstantin S Bobrovsky <konstantin.s.bobrovsky@intel.com>
2020-07-12 23:29:08 -0700

    [SYCL][ESIMD] Add ESIMD intrinsics/IR translation tests.
    Authors:
      Konstantin S Bobrovsky <konstantin.s.bobrovsky@intel.com>
      Denis Bakhvalov <denis.bakhvalov@intel.com>
      Gang Chen <gang.y.chen@intel.com>
    
    Signed-off-by: Konstantin S Bobrovsky <konstantin.s.bobrovsky@intel.com>

---
+++ sycl/test/esimd/glob.cpp
@@ -0,0 +1,28 @@
+// RUN: %clangxx -fsycl -fsycl-explicit-simd -c -fsycl-device-only -Xclang -emit-llvm %s -o - | \
+// RUN:  FileCheck %s
+
+// This test checks that globals with register attribute are allowed in ESIMD mode,
+// can be accessed in functions and correct LLVM IR is generated (including
+// translation of the register attribute)
+
+#include <CL/sycl.hpp>
+#include <iostream>
+#include <CL/sycl/intel/esimd.hpp>
+
+using namespace cl::sycl;
+using namespace sycl::intel::gpu;
+
+#define VL 16
+
+ESIMD_PRIVATE ESIMD_REGISTER(17) simd<int, VL> vc;
+// CHECK-DAG: @vc = {{.+}} <16 x i32> zeroinitializer, align 64 #0
+// CHECK-DAG: attributes #0 = { "genx_byte_offset"="17" "genx_volatile" }
+
+ESIMD_PRIVATE ESIMD_REGISTER(17 + VL) simd<int, VL> vc1;
+// CHECK-DAG: @vc1 = {{.+}} <16 x i32> zeroinitializer, align 64 #1
+// CHECK-DAG: attributes #1 = { "genx_byte_offset"="33" "genx_volatile" }
+
+SYCL_EXTERNAL ESIMD_NOINLINE void init_vc(int x) {
+  vc1 = vc + 1;
+  vc = x;
+}
