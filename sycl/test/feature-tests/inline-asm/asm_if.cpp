ea44cee1f50a92737a6946126ed6faeae4c03c14
Aleksander Fadeev <aleksander.fadeev@intel.com>
2020-07-14 14:19:48 +0300

    Second test of inline_asm which proves switch execution
    Signed-off-by: Aleksander Fadeev <aleksander.fadeev@intel.com>

--- sycl/test/feature-tests/inline-asm/asm_if.cpp
+++ sycl/test/feature-tests/inline-asm/asm_if.cpp
@@ -2,18 +2,13 @@
 // REQUIRES: gpu,linux
 // RUN: %clangxx -fsycl %s -DINLINE_ASM -o %t.out
 // RUN: %t.out
+// RUN: %clangxx -fsycl %s -o %t.ref.out
+// RUN: %t.ref.out
+
 
 
 #include "include/asmhelper.h"
 #include <CL/sycl.hpp>
-#include <iostream>
-#include <vector>
-
-#ifdef __SYCL_DEVICE_ONLY__
-          #define CONSTANT __attribute__((opencl_constant))
-#else
-          #define CONSTANT
-#endif
 
 using dataType = cl::sycl::cl_int;
 
@@ -22,19 +17,22 @@ struct KernelFunctor : WithOutputBuffer<T> {
   KernelFunctor(size_t problem_size) : WithOutputBuffer<T>(problem_size) {}
 
   void operator()(cl::sycl::handler &cgh) {
-    cl::sycl::stream out(1024, 256, cgh);
     auto C = this->getOutputBuffer().template get_access<cl::sycl::access::mode::write>(cgh);
     cgh.parallel_for<KernelFunctor<T>>(
         cl::sycl::range<1>{this->getOutputBufferSize()}, [=](cl::sycl::id<1> wiID) [[cl::intel_reqd_sub_group_size(8)]] {
-          int rand = 0;
-           volatile int output = 0;
+          bool switch_field = false;
+          int output = 0;
 #if defined(INLINE_ASM) && defined(__SYCL_DEVICE_ONLY__)
-          asm volatile("cmp.eq (M1_NM, 1) P1 %1(0,0)<0;1,0> 0x0:d\n"
-                       "(P1) sel (M1_NM, 1) %0(0,0)<1> 0x7:b 0x8:b"
-                       : "=rw"(output) : "rw"(rand));
+          asm volatile(".decl P1 v_type=P num_elts=1\n"
+                       "cmp.eq (M1_NM, 8) P1 %1(0,0)<0;1,0> 0x0:b\n"
+                       "(P1) sel (M1_NM, 8) %0(0,0)<1> 0x7:d 0x8:d"
+                       : "=rw"(output) : "rw"(switch_field));
           
 #else
-          output = 7;
+          if (switch_field == false)
+            output = 7;
+          else
+            output = 8;
 #endif
           C[wiID] = output;
         });
---
+++ sycl/test/feature-tests/inline-asm/asm_switch.cpp
@@ -0,0 +1,77 @@
+// UNSUPPORTED: cuda
+// REQUIRES: gpu,linux
+// RUN: %clangxx -fsycl %s -DINLINE_ASM -o %t.out
+// RUN: %t.out
+// RUN: %clangxx -fsycl %s -o %t.ref.out
+// RUN: %t.ref.out
+
+
+
+#include "include/asmhelper.h"
+#include <CL/sycl.hpp>
+
+#ifdef __SYCL_DEVICE_ONLY__
+          #define CONSTANT __attribute__((opencl_constant))
+#else
+          #define CONSTANT
+#endif
+
+using dataType = cl::sycl::cl_int;
+
+template <typename T = dataType>
+struct KernelFunctor : WithOutputBuffer<T> {
+  KernelFunctor(size_t problem_size) : WithOutputBuffer<T>(problem_size) {}
+
+  void operator()(cl::sycl::handler &cgh) {
+    auto C = this->getOutputBuffer().template get_access<cl::sycl::access::mode::write>(cgh);
+    cgh.parallel_for<KernelFunctor<T>>(
+        cl::sycl::range<1>{this->getOutputBufferSize()}, [=](cl::sycl::id<1> wiID) [[cl::intel_reqd_sub_group_size(8)]] {
+          int switch_field = 2;
+          int output = 0;
+#if defined(INLINE_ASM) && defined(__SYCL_DEVICE_ONLY__)
+          asm volatile(".decl P1 v_type=P num_elts=1\n"
+                       ".decl P2 v_type=P num_elts=1\n"
+                       ".decl P3 v_type=P num_elts=1\n"
+                       "cmp.ne (M1_NM, 8) P1 %1(0,0)<0;1,0> 0x0:d\n"
+                       "(P1) goto (M1, 1) label0\n"
+                       "mov (M1, 8) %0(0,0)<1> 0x9:d\n"
+                       "(P1) goto (M1, 1) label0\n"
+                       "label0:\n"
+                       "cmp.ne (M1_NM, 8) P2 %1(0,0)<0;1,0> 0x1:d\n"
+                       "(P2) goto (M1, 1) label1\n"
+                       "mov (M1, 8) %0(0,0)<1> 0x8:d\n"
+                       "label1:\n"
+                       "cmp.ne (M1_NM, 8) P3 %1(0,0)<0;1,0> 0x2:d\n"
+                       "(P3) goto (M1, 1) label2\n"
+                       "mov (M1, 8) %0(0,0)<1> 0x7:d\n"
+                       "label2:"
+                       : "=rw"(output) : "rw"(switch_field));
+          
+#else
+          switch(switch_field){
+            case 0:
+              output = 9;
+              break;
+            case 1:
+              output = 8;
+              break;
+            case 2:
+              output = 7;
+              break;
+          }
+#endif
+          C[wiID] = output;
+        });
+  }
+};
+
+int main() {
+  KernelFunctor<> f(DEFAULT_PROBLEM_SIZE);
+  if (!launchInlineASMTest(f))
+    return 0;
+
+  if (verify_all_the_same(f.getOutputBufferData(), 7))
+    return 0;
+
+  return 1;
+}
