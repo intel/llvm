9582684727c49b631a901a0173b1fe09d2fef169
Alexander Batashev <alexander.batashev@intel.com>
2020-07-20 16:01:59 +0300

    [SYCL][NFC] Move spec_constant_impl.hpp to source
    spec_constant_impl.hpp is not used in public interfaces. Move entire
    file to source directory as implementation detail.
    
    Signed-off-by: Alexander Batashev <alexander.batashev@intel.com>

--- sycl/source/detail/program_impl.cpp
+++ sycl/source/detail/program_impl.cpp
@@ -9,9 +9,9 @@
 #include <CL/sycl/detail/common.hpp>
 #include <CL/sycl/detail/kernel_desc.hpp>
 #include <CL/sycl/detail/pi.h>
-#include <CL/sycl/detail/spec_constant_impl.hpp>
 #include <CL/sycl/kernel.hpp>
 #include <detail/program_impl.hpp>
+#include <detail/spec_constant_impl.hpp>
 
 #include <algorithm>
 #include <fstream>
