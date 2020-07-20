0966142a1f13046f1a8b7609debe961748b9c14c
rbegam <rehana.begam@intel.com>
2020-07-20 16:34:00 -0700

    removes redundant opencl requirement and comments.
    Signed-off-by: rbegam <rehana.begam@intel.com>

--- sycl/test/inorder_queue/in_order_dmemll.cpp
+++ sycl/test/inorder_queue/in_order_dmemll.cpp
@@ -1,5 +1,3 @@
-// R-EQUIRES: opencl
-
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t1.out -L %opencl_libs_dir -lOpenCL
 // RUN: %CPU_RUN_PLACEHOLDER %t1.out
 // RUN: %GPU_RUN_PLACEHOLDER %t1.out
@@ -18,15 +16,6 @@
 using namespace cl::sycl;
 
 constexpr int numNodes = 4;
-
-/*bool getQueueOrder(cl_command_queue cq) {
-  cl_command_queue_properties reportedProps;
-  cl_int iRet = clGetCommandQueueInfo(
-      cq, CL_QUEUE_PROPERTIES, sizeof(reportedProps), &reportedProps, nullptr);
-  assert(CL_SUCCESS == iRet && "Failed to obtain queue info from ocl device");
-  return (reportedProps & CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE) ? false
-                                                                  : true;
-}*/
 
 struct Node {
   Node() : pNext(nullptr), Num(0xDEADBEEF) {}
@@ -102,16 +91,6 @@ int main() {
     }
     d_cur = h_cur.pNext;
   }
-
-  /*bool result = true;
-  cl_command_queue cq = q.get(); 
-  bool expected_result = dev.is_host() ? true : getQueueOrder(cq);
-  if (expected_result != result) {
-    std::cout << "Resulting queue order is OOO but expected order is inorder"
-              << std::endl;
-
-    return -1;
-  }*/
 
   return 0;
 }
