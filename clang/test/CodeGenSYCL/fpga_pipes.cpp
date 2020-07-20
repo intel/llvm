68e3892a41b456211d30674ea0501c233b03c827
Chris Perkins <chris.perkins@intel.com>
2020-07-20 12:51:16 -0700

    excerpts of kernel def in sycl codegen tests updated with const and ref
    Signed-off-by: Chris Perkins <chris.perkins@intel.com>

--- clang/test/CodeGenSYCL/fpga_pipes.cpp
+++ clang/test/CodeGenSYCL/fpga_pipes.cpp
@@ -45,7 +45,7 @@ private:
 };
 
 template <typename name, typename Func>
-__attribute__((sycl_kernel)) void kernel_single_task(Func kernelFunc) {
+__attribute__((sycl_kernel)) void kernel_single_task(const Func &kernelFunc) {
   kernelFunc();
 }
 
