1cb78c1a3e7d88e1c6ba1679a4bd9af5b8966111
rbegam <rehana.begam@intel.com>
2020-07-20 16:41:49 -0700

    adds a new file.
    Signed-off-by: rbegam <rehana.begam@intel.com>

--- sycl/test/basic_tests/event.cpp
+++ sycl/test/basic_tests/event.cpp
@@ -1,4 +1,4 @@
-// REQUIRES: opencl
+// REQUIRES: opencl || level0
 
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out -L %opencl_libs_dir -lOpenCL
 // RUN: env SYCL_DEVICE_TYPE=HOST %t.out
