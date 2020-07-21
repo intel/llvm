615718e573393851301ad6c34444f3666cbb9066
Vlad Romanov <vlad.romanov@intel.com>
2020-07-21 15:01:05 +0300

    fix abi test

--- sycl/test/abi/symbol_size.cpp
+++ sycl/test/abi/symbol_size.cpp
@@ -8,7 +8,6 @@
 #include <CL/sycl/detail/accessor_impl.hpp>
 #include <CL/sycl/detail/buffer_impl.hpp>
 #include <CL/sycl/detail/image_impl.hpp>
-#include <CL/sycl/detail/sampler_impl.hpp>
 #include <CL/sycl/detail/stream_impl.hpp>
 #include <CL/sycl/device.hpp>
 #include <CL/sycl/device_event.hpp>
@@ -69,13 +68,6 @@ int main() {
 #ifdef __SYCL_DEVICE_ONLY__
   check_size<private_memory<int, 1>, 4>();
   check_size<detail::sampler_impl, 8>();
-#else
-  check_size<private_memory<int, 1>, 8>();
-#ifdef _MSC_VER
-  check_size<detail::sampler_impl, 80>();
-#else
-  check_size<detail::sampler_impl, 72>();
-#endif
 #endif
   check_size<program, 16>();
   check_size<range<1>, 8>();
