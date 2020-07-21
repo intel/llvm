804f147a0fe8b12e58649d7d1f2e2ad49522563d
Soumi Manna <soumi.manna@intel.com>
2020-07-21 11:27:46 -0700

    Fix clang-format issue
    Signed-off-by: Soumi Manna <soumi.manna@intel.com>

--- clang/lib/Sema/SemaDeclAttr.cpp
+++ clang/lib/Sema/SemaDeclAttr.cpp
@@ -3018,7 +3018,7 @@ static void handleSubGroupSize(Sema &S, Decl *D, const ParsedAttr &AL) {
       IntelReqdSubGroupSizeAttr::CXX11_cl_intel_reqd_sub_group_size) {
     S.Diag(AL.getLoc(), diag::warn_attribute_spelling_deprecated) << AL;
     S.Diag(AL.getLoc(), diag::note_spelling_suggestion)
-      << "'intel::reqd_sub_group_size'";
+        << "'intel::reqd_sub_group_size'";
   }
 
   S.addIntelReqdSubGroupSizeAttr(D, AL, E);
