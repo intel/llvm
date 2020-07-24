cd6fa72c67c88761299c4dc7fe2902e5620767e4
Chris Perkins <chris.perkins@intel.com>
2020-07-23 17:43:21 -0700

    new c++17 buffers created from container calling wrong buffer_impl constructor. Added test
    Signed-off-by: Chris Perkins <chris.perkins@intel.com>

--- sycl/test/basic_tests/buffer/buffer.cpp
+++ sycl/test/basic_tests/buffer/buffer.cpp
@@ -420,6 +420,23 @@ int main() {
     }
   }
 
+  // buffer created from contiguous container copies back
+  {
+    constexpr int num = 10;
+    std::vector<int> out_data(num, -1);
+    {
+      buffer A(out_data);
+      queue Q;
+      Q.submit([&](handler &h) {
+        auto out = A.get_access<access::mode::write>(h);
+        h.parallel_for<class containerBuffer>(A.get_range(),
+                                              [out](id<1> i) { out[i] = 1; });
+      });
+    } //~buffer
+    for (int i = 0; i < num; i++)
+      assert(out_data[i] == 1);
+  }
+
   {
     std::vector<int> data1(10, -1);
     {
