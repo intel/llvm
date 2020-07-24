882b3d004f9b4ce11637a9b3acaec19ecec3e799
Sindhu Chittireddy <sindhu.chittireddy@intel.com>
2020-07-24 11:19:59 -0700

    Fix lit test failure caused by newer commits to repo

--- clang/test/SemaSYCL/check-notdirect-attribute-propagation.cpp
+++ clang/test/SemaSYCL/check-notdirect-attribute-propagation.cpp
@@ -5,7 +5,7 @@
 #ifndef TRIGGER_ERROR
 [[intelfpga::no_global_work_offset]] void not_direct_one() {} // expected-no-diagnostics
 
-[[cl::intel_reqd_sub_group_size(1)]] void func_one() {
+[[intel::reqd_sub_group_size(1)]] void func_one() {
   not_direct_one();
 }
 
