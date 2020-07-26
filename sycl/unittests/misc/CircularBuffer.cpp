849ae5bbab71406bf81bb91851e4d050983a59b7
Vlad Romanov <vlad.romanov@intel.com>
2020-07-26 17:45:03 +0300

    fix test title

--- sycl/unittests/misc/CircularBuffer.cpp
+++ sycl/unittests/misc/CircularBuffer.cpp
@@ -1,4 +1,4 @@
-//==---- OsUtils.cpp --- os_utils unit test --------------------------------==//
+//==---- CircularBuffer.cpp ------------------------------------------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
