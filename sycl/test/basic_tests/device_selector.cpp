074ada7481aa13aa4c2961523b41a90b84987f59
Byoungro So <byoungro.so@intel.com>
2020-07-26 22:13:18 -0700

    [SYCL] Implement select_device(device_type, backend)
    Extended select_device to take optional parameters device_type and backend.
    The current implementation of select_device returns different devices
    depending on the env var such as SYCL_BE and SYCL_DEVICE_TYPE, and uses heuristic.
    This extension of select_device will allow users to set the desired device type and possibly also backend if multiple devices are the same device type.
    
    Signed-off-by: Byoungro So <byoungro.so@intel.com>

--- sycl/test/basic_tests/device_selector.cpp
+++ sycl/test/basic_tests/device_selector.cpp
@@ -1,6 +1,15 @@
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
 // RUN: env SYCL_BE=%sycl_be %t.out
+// RUN: env SYCL_DEVICE_TYPE=HOST %t.out
+// RUN: env SYCL_DEVICE_TYPE=CPU %t.out
+// RUN: env SYCL_DEVICE_TYPE=GPU %t.out
+// RUN: env SYCL_DEVICE_TYPE=ACC %t.out
+// RUN: env SYCL_DEVICE_TYPE=GPU SYCL_BE=%sycl_be %t.out
+// RUN: env SYCL_DEVICE_TYPE=CPU SYCL_BE=%sycl_be %t.out
+// RUN: env SYCL_DEVICE_TYPE=HOST SYCL_BE=%sycl_be %t.out
 //
+// Checks if all different device types can be acquired from select_device
+// regardless of env var setting SYCL_BE and/or SYCL_DEVICE_TYPE
 // Checks that no device is selected when no device of desired type is
 // available.
 
@@ -8,19 +17,33 @@
 
 #include <iostream>
 
-class RejectEverything : public sycl::device_selector {
+using namespace cl::sycl;
+
+class RejectEverything : public device_selector {
 public:
-  int operator()(const sycl::device &Device) const final {
+  int operator()(const device &Device) const final {
     // Negative value means that a device must not be selected
     return -1;
   }
 };
 
 int main() {
+  default_selector ds;
+  device d = ds.select_device(info::device_type::gpu, backend::level0);
+  std::cout << "Level-zero GPU Device is found: " << std::boolalpha << d.is_gpu() << std::endl;
+  d = ds.select_device(info::device_type::gpu, backend::opencl);
+  std::cout << "OpenCL GPU Device is found: " << std::boolalpha << d.is_gpu() << std::endl;
+  d = ds.select_device(info::device_type::cpu);
+  std::cout << "CPU device is found: " << d.is_cpu() << std::endl;
+  d = ds.select_device(info::device_type::host);
+  std::cout << "HOST device is found: " << d.is_host() << std::endl;
+  d = ds.select_device(info::device_type::accelerator);
+  std::cout << "ACC device is found: " << d.is_accelerator() << std::endl;
+
   RejectEverything Selector;
   try {
     sycl::device Device(Selector);
-  } catch (sycl::runtime_error &E) {
+  } catch (runtime_error &E) {
     return 0;
   }
   std::cerr << "Error. A device is found." << std::endl;
