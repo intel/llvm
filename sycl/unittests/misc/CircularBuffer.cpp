4f86d647f068edf54a1935ce55958d33aee776e7
Romanov Vlad <17316488+romanovvlad@users.noreply.github.com>
2020-07-27 10:13:19 +0300

    [SYCL][NFC] Move circular_buffer.hpp to the source directory (#2171)

--- sycl/unittests/misc/CircularBuffer.cpp
+++ sycl/unittests/misc/CircularBuffer.cpp
@@ -1,7 +1,14 @@
-// RUN: %clangxx -fsycl %s -o %t.out
-// RUN: %t.out
+//==---- CircularBuffer.cpp ------------------------------------------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 
-#include <CL/sycl/detail/circular_buffer.hpp>
+#include <gtest/gtest.h>
+
+#include <detail/circular_buffer.hpp>
 
 #include <algorithm>
 #include <cassert>
@@ -11,21 +18,21 @@
 // This test contains basic checks for cl::sycl::detail::CircularBuffer
 void checkEquality(const cl::sycl::detail::CircularBuffer<int> &CB,
                    const std::vector<int> &V) {
-  assert(std::equal(CB.begin(), CB.end(), V.begin()));
+  ASSERT_TRUE(std::equal(CB.begin(), CB.end(), V.begin()));
 }
 
-int main() {
+TEST(CircularBufferTest, CircularBufferTest) {
   const std::size_t Capacity = 6;
   cl::sycl::detail::CircularBuffer<int> CB{Capacity};
-  assert(CB.capacity() == Capacity);
-  assert(CB.empty());
+  ASSERT_TRUE(CB.capacity() == Capacity);
+  ASSERT_TRUE(CB.empty());
 
-  int nextValue = 0;
+  size_t nextValue = 0;
   for (; nextValue < Capacity; ++nextValue) {
-    assert(CB.size() == nextValue);
+    ASSERT_TRUE(CB.size() == nextValue);
     CB.push_back(nextValue);
   }
-  assert(CB.full() && CB.size() == CB.capacity());
+  ASSERT_TRUE(CB.full() && CB.size() == CB.capacity());
   checkEquality(CB, {0, 1, 2, 3, 4, 5});
 
   CB.push_back(nextValue++);
@@ -33,8 +40,8 @@ int main() {
   CB.push_front(nextValue++);
   checkEquality(CB, {7, 1, 2, 3, 4, 5});
 
-  assert(CB.front() == 7);
-  assert(CB.back() == 5);
+  ASSERT_TRUE(CB.front() == 7);
+  ASSERT_TRUE(CB.back() == 5);
 
   CB.erase(CB.begin() + 2);
   checkEquality(CB, {7, 1, 3, 4, 5});
