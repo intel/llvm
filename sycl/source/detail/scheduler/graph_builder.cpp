a6cdc0e77848bcc0ec7cf792563400d8510c53d0
Alexander Batashev <alexander.batashev@intel.com>
2020-07-28 17:18:08 +0300

    Replace check with assert
    Signed-off-by: Alexander Batashev <alexander.batashev@intel.com>

--- sycl/source/detail/scheduler/graph_builder.cpp
+++ sycl/source/detail/scheduler/graph_builder.cpp
@@ -93,8 +93,7 @@ Scheduler::GraphBuilder::GraphBuilder() {
 }
 
 static bool markNodeAsVisited(Command *Cmd, std::vector<Command *> &Visited) {
-  if (!Cmd)
-    return false;
+  assert(Cmd && "Cmd can't be nullptr");
   if (Cmd->MMarks.MVisited)
     return false;
   Cmd->MMarks.MVisited = true;
