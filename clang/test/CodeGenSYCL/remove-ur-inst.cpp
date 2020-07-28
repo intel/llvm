c93fa93ef9b5b4a4c361ee4943a9dd79e68b9d46
Andrew Savonichev <andrew.savonichev@intel.com>
2020-07-28 15:53:32 +0300

    [SYCL] Remove noreturn function attribute (#2165)
    This is a follow-up to:
    74aa9a07 [SYCL] Remove "unreachable" instruction from LLVM IR for SYCL
    devices (#1789)
    
    Noreturn function attribute gives LLVM optimization passes an
    opportunity to re-structure code and insert an unreachable
    instruction.
    
    This happens when optimizations are turned on by
    -fsycl-enable-optimizations, so noreturn function attribute must be
    removed.
    
    Signed-off-by: Andrew Savonichev <andrew.savonichev@intel.com>

--- clang/test/CodeGenSYCL/remove-ur-inst.cpp
+++ clang/test/CodeGenSYCL/remove-ur-inst.cpp
@@ -1,4 +1,5 @@
 // RUN: %clang_cc1 -fsycl -fsycl-is-device -triple spir64-unknown-unknown-sycldevice -emit-llvm %s -o - | FileCheck %s
+// RUN: %clang_cc1 -fsycl -fsycl-is-device -fsycl-enable-optimizations -triple spir64-unknown-unknown-sycldevice -emit-llvm %s -o - | FileCheck %s
 
 SYCL_EXTERNAL void doesNotReturn() throw() __attribute__((__noreturn__));
 
@@ -11,6 +12,7 @@ int main() {
   kernel<class test>([]() {
     doesNotReturn();
     // CHECK-NOT: unreachable
+    // CHECK-NOT: noreturn
   });
   return 0;
 }
