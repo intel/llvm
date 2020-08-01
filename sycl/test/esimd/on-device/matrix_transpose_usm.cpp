8d2a00a5c86efff5c98b1e226f01363a4dc2d4cf
Konstantin S Bobrovsky <konstantin.s.bobrovsky@intel.com>
2020-07-31 22:28:14 -0700

    [SQUASH] Cleanup, reduce test size.
    Signed-off-by: Konstantin S Bobrovsky <konstantin.s.bobrovsky@intel.com>

--- sycl/test/esimd/on-device/matrix_transpose_usm.cpp
+++ sycl/test/esimd/on-device/matrix_transpose_usm.cpp
@@ -109,40 +109,6 @@ static double report_time(const string &msg, event e) {
   return elapsed;
 }
 
-// This is the class provided to SYCL runtime by the application to decide
-// on which device to run, or whether to run at all.
-// When selecting a device, SYCL runtime first takes (1) a selector provided by
-// the program or a default one and (2) the set of all available devices. Then
-// it passes each device to the '()' operator of the selector. Device, for
-// which '()' returned the highest number, is selected. If a negative number
-// was returned for all devices, then the selection process will cause an
-// exception.
-class MyDeviceSelector : public cl::sycl::device_selector {
-public:
-  MyDeviceSelector() {}
-
-  // This is the function which gives a "rating" to devices.
-  virtual int operator()(const cl::sycl::device &device) const override {
-    // The template parameter to device.get_info can be a variety of properties
-    // defined by the SYCL spec's cl::sycl::info:: enum. Properties may have
-    // different types. Here we query name which is a string.
-    const std::string name = device.get_info<cl::sycl::info::device::name>();
-    std::cout << "Trying device: " << name << "..." << std::endl;
-    std::cout << "  Vendor: "
-              << device.get_info<cl::sycl::info::device::vendor>() << std::endl;
-    if (device.is_gpu())
-      return 500;
-    if (device.is_accelerator())
-      return 400;
-    if (device.is_cpu())
-      return 300;
-    if (device.is_host())
-      return 100;
-
-    return -1;
-  }
-};
-
 // The basic idea of vecotrizing transposition can be illustrated by
 // transposing a 2 x 2 matrix as follows:
 //
@@ -414,13 +380,13 @@ int main(int argc, char *argv[]) {
   bool success = true;
   success &= runTest(MZ, 16);
   if (argc == 1) {
-    success &= runTest(1U << 10, 8);
-    success &= runTest(1U << 11, 8);
-    success &= runTest(1U << 12, 8);
+    success &= runTest(1U << 7, 8);
+    success &= runTest(1U << 8, 8);
+    success &= runTest(1U << 9, 8);
     // success &= runTest(1U << 13, 8);
-    success &= runTest(1U << 10, 16);
-    success &= runTest(1U << 11, 16);
-    success &= runTest(1U << 12, 16);
+    success &= runTest(1U << 7, 16);
+    success &= runTest(1U << 8, 16);
+    success &= runTest(1U << 9, 16);
     // success &= runTest(1U << 13, 16);
   }
 
