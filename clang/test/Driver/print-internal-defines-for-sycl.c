a293cdaef7d01bcf473bc5b5f131296c57f9602b
Alexey Bader <alexey.bader@intel.com>
2020-08-03 13:32:02 +0300

    Update test comment.

--- clang/test/Driver/print-internal-defines-for-sycl.c
+++ clang/test/Driver/print-internal-defines-for-sycl.c
@@ -1,4 +1,4 @@
-// Test if sycl is able to print internal defines
+// Test if clang is able to print internal defines in SYCL mode
 //
 // RUN: %clangxx -fsycl -dM -E -x c++ %s 2>&1 \
 // RUN: | FileCheck --check-prefix CHECK-PRINT-INTERNAL-DEFINES %s
