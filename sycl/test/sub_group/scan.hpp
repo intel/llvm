5778ebb10fa5b21ea3a8a6bc6827aeeeacc30b41
Vladimir Lazarev <vladimir.lazarev@intel.com>
2020-08-04 18:47:55 +0300

    Fix clang-format issues

--- sycl/test/sub_group/scan.hpp
+++ sycl/test/sub_group/scan.hpp
@@ -10,8 +10,7 @@
 #include <CL/sycl.hpp>
 #include <limits>
 
-template <typename T, class BinaryOperation>
-class sycl_subgr;
+template <typename T, class BinaryOperation> class sycl_subgr;
 
 using namespace cl::sycl;
 
@@ -73,8 +72,7 @@ void check_op(queue &Queue, T init, BinaryOperation op, bool skip_init = false,
   }
 }
 
-template <typename T>
-void check(queue &Queue, size_t G = 256, size_t L = 64) {
+template <typename T> void check(queue &Queue, size_t G = 256, size_t L = 64) {
   // limit data range for half to avoid rounding issues
   if (std::is_same<T, cl::sycl::half>::value) {
     G = 64;
