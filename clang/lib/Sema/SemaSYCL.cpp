6fb50b4c51dc07ee38c35d5eed3353984fbd8fa2
Soumi Manna <soumi.manna@intel.com>
2020-08-03 23:43:07 -0700

    Fix clang-format issues
    Signed-off-by: Soumi Manna <soumi.manna@intel.com>

--- clang/lib/Sema/SemaSYCL.cpp
+++ clang/lib/Sema/SemaSYCL.cpp
@@ -941,7 +941,7 @@ public:
       else if (FieldTy->isArrayType()) {
         if (KF_FOR_EACH(handleArrayType, Field, FieldTy))
           VisitArrayElements(Field, FieldTy, handlers...);
-      } else if (FieldTy->isScalarType() || FieldTy->isUnioType() ||
+      } else if (FieldTy->isScalarType() || FieldTy->isUnionType() ||
 		  FieldTy->isVectorType()) {
         KF_FOR_EACH(handleSimpleType, Field, FieldTy);
       } else
