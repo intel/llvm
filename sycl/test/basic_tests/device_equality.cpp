4bbad36b2050c37277a6b74f737668a1fb14e4f4
James Brodman <james.brodman@intel.com>
2020-08-13 16:34:13 -0400

    clang-format
    Signed-off-by: James Brodman <james.brodman@intel.com>

--- sycl/test/basic_tests/device_equality.cpp
+++ sycl/test/basic_tests/device_equality.cpp
@@ -3,7 +3,7 @@
 // RUN: %CPU_RUN_PLACEHOLDER %t1.out
 // RUN: %GPU_RUN_PLACEHOLDER %t1.out
 
-//==------- device_equality.cpp - SYCL device equality test -----------------==//
+//==------- device_equality.cpp - SYCL device equality test ----------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
