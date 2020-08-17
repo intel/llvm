e06e37a8018cbee2cef8331bd92b63f1cf59358e
Sergey Semenov <sergey.semenov@intel.com>
2020-08-17 13:42:23 +0300

    Disable the added test on CUDA due to SPIR-V usage
    Signed-off-by: Sergey Semenov <sergey.semenov@intel.com>

--- sycl/test/device_code_dae/device_code_dae.cpp
+++ sycl/test/device_code_dae/device_code_dae.cpp
@@ -1,5 +1,7 @@
 // NOTE A temporary test before this compilation flow is enabled by default in
 // driver
+// UNSUPPORTED: cuda
+// CUDA does not support SPIR-V.
 // RUN: %clangxx -fsycl-device-only -Xclang -fenable-sycl-dae -Xclang -fsycl-int-header=int_header.h %s -c -o device_code.bc -I %sycl_include -Wno-sycl-strict
 // RUN: %clangxx -include int_header.h -g -c %s -o host_code.o -I %sycl_include -Wno-sycl-strict
 // RUN: llvm-link -o=linked_device_code.bc device_code.bc
