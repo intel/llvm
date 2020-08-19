3e36141b9d9b7c6cfad96e1a981ef858fd108349
Gail Lyons <gail.lyons@intel.com>
2020-08-19 07:32:52 -0700

    Incorporating code review comments.
    Signed-off-by: Gail Lyons <gail.lyons@intel.com>

--- SYCL/Basic/regression/commandlist/gpu.cpp
+++ SYCL/Basic/regression/commandlist/gpu.cpp
@@ -1,5 +1,4 @@
-// REQUIRES: L0_plugin
 // REQUIRES: gpu
 //
-// RUN: %clangxx -fsycl %S/Inputs/FindPrimesSYCL.cpp %S/Inputs/main.cpp -o %t.out -lpthread -lOpenCL
+// RUN: %clangxx -fsycl %S/Inputs/FindPrimesSYCL.cpp %S/Inputs/main.cpp -o %t.out -lpthread
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
