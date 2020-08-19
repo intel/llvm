c1fa7d383e6f52068bd523c94813953697b9da06
Dmitry Vodopyanov <dmitry.vodopyanov@intel.com>
2020-08-19 18:16:19 +0300

    Remove unnecessary formating by clang-format

--- sycl/source/detail/scheduler/commands.cpp
+++ sycl/source/detail/scheduler/commands.cpp
@@ -2005,7 +2005,7 @@ cl_int ExecCGCommand::enqueueImp() {
     // Extract the Mem Objects for all Requirements, to ensure they are available if
     // a user ask for them inside the interop task scope
     const auto& HandlerReq = ExecInterop->MRequirements;
-    std::for_each(std::begin(HandlerReq), std::end(HandlerReq), [&](Requirement *Req) {
+    std::for_each(std::begin(HandlerReq), std::end(HandlerReq), [&](Requirement* Req) {
       AllocaCommandBase *AllocaCmd = getAllocaForReq(Req);
       auto MemArg = reinterpret_cast<pi_mem>(AllocaCmd->getMemAllocation());
       interop_handler::ReqToMem ReqToMem = std::make_pair(Req, MemArg);
