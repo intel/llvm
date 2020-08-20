d67a6f87564c37f143015c3e50035e1cd8b354ef
Erich Keane <erich.keane@intel.com>
2020-08-20 06:33:19 -0700

    Replace 'if' with 'assert' for zero size arrays

--- clang/lib/Sema/SemaSYCL.cpp
+++ clang/lib/Sema/SemaSYCL.cpp
@@ -874,13 +874,14 @@ class KernelObjVisitor {
     int64_t ElemCount = CAT->getSize().getSExtValue();
     std::initializer_list<int>{(handlers.enterArray(), 0)...};
 
-    if (ElemCount > 0) {
-      VisitFirstElement(nullptr, FD, ET, handlers...);
+
+    assert(ElemCount > 0 && "SYCL prohibits 0 sized arrays");
+    VisitFirstElement(nullptr, FD, ET, handlers...);
+    (void)std::initializer_list<int>{(handlers.nextElement(ET), 0)...};
+
+    for (int64_t Count = 1; Count < ElemCount; Count++) {
+      VisitNthElement(nullptr, FD, ET, handlers...);
       (void)std::initializer_list<int>{(handlers.nextElement(ET), 0)...};
-      for (int64_t Count = 1; Count < ElemCount; Count++) {
-        VisitNthElement(nullptr, FD, ET, handlers...);
-        (void)std::initializer_list<int>{(handlers.nextElement(ET), 0)...};
-      }
     }
 
     (void)std::initializer_list<int>{
