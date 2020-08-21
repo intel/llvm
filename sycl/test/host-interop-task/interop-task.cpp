8c69d45b439f220d174df2347d45203c4a591b5d
Alexander Batashev <alexbatashev@outlook.com>
2020-08-21 21:12:20 +0300

    Revert "[SYCL] Re-enable interop task tests (#2342)"
    This reverts commit 8f8d34c43643100e04596ecb180569245345d864.

--- sycl/test/host-interop-task/interop-task.cpp
+++ sycl/test/host-interop-task/interop-task.cpp
@@ -4,6 +4,7 @@
 // RUN: %ACC_RUN_PLACEHOLDER %t.out
 // UNSUPPORTED: level_zero, cuda
 // REQUIRES: opencl
+// REQUIRES: TEMPORARY_DISABLED
 
 #include <CL/sycl.hpp>
 #include <CL/sycl/backend/opencl.hpp>
@@ -240,7 +241,7 @@ void test6() {
     });
     Queue.wait_and_throw();
     assert(!"Expected exception was not caught");
-  } catch (const sycl::exception &ExpectedException) {
+  } catch (sycl::exception &ExpectedException) {
     assert(std::string(ExpectedException.what())
                    .find("memory object out of accessor for specified target "
                          "is not allowed") != std::string::npos &&
