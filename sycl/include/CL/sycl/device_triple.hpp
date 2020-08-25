779d304b429e72e9814932d3e31b5e82255462fa
Byoungro So <byoungro.so@intel.com>
2020-08-25 10:56:37 -0700

    Update sycl/include/CL/sycl/device_triple.hpp
    Co-authored-by: Romanov Vlad <17316488+romanovvlad@users.noreply.github.com>

--- sycl/include/CL/sycl/device_triple.hpp
+++ sycl/include/CL/sycl/device_triple.hpp
@@ -1,4 +1,4 @@
-//==-------------- triple.hpp - SYCL device triple descripter --------------==//
+//==-------------- device_triple.hpp - SYCL device triple descriptor --------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
