fa1fd6e8b39faa2d62241305dc8e4cdd5a9e3481
Byoungro So <byoungro.so@intel.com>
2020-08-25 11:18:45 -0700

    clang-format
    Signed-off-by: Byoungro So <byoungro.so@intel.com>

--- sycl/include/CL/sycl/device_triple.hpp
+++ sycl/include/CL/sycl/device_triple.hpp
@@ -1,4 +1,4 @@
-//==-------------- device_triple.hpp - SYCL device triple descriptor --------------==//
+//==---------- device_triple.hpp - SYCL device triple descriptor -----------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
