05d5cdb5b31fe04e10383061df0e761d5259c594
Mariya Podchishchaeva <mariya.podchishchaeva@intel.com>
2020-08-25 17:50:22 +0300

    Add diagnostic to SyclStrict group

--- clang/test/SemaSYCL/num-args-overflow.cpp
+++ clang/test/SemaSYCL/num-args-overflow.cpp
@@ -1,5 +1,7 @@
 // RUN: %clang_cc1 -fsycl -triple spir64_gen -DGPU -fsycl-is-device -fsyntax-only -verify %s
 // RUN: %clang_cc1 -fsycl -triple spir64 -fsycl-is-device -fsyntax-only -verify %s
+// RUN: %clang_cc1 -fsycl -triple spir64_gen -Wno-sycl-strict -fsycl-is-device -fsyntax-only -verify %s
+// RUN: %clang_cc1 -fsycl -triple spir64_gen -Werror=sycl-strict -DERROR -fsycl-is-device -fsyntax-only -verify %s
 
 template <typename Name, typename F>
 __attribute__((sycl_kernel)) void kernel(F kernelFunc) {
@@ -9,7 +11,9 @@ __attribute__((sycl_kernel)) void kernel(F kernelFunc) {
 void use() {
   int Arr[2001];
 #ifdef GPU
-  // expected-warning@+4 {{resulting number of kernel arguments 2001 is greater than maximum supported on GPU device - 2000}}
+  // expected-warning@+6 {{resulting number of kernel arguments 2001 is greater than maximum supported on GPU device - 2000}}
+#elif ERROR
+  // expected-error@+4 {{resulting number of kernel arguments 2001 is greater than maximum supported on GPU device - 2000}}
 #else
   // expected-no-diagnostics
 #endif
