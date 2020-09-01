8f7b5d250ef743f822088ec2a495f7ad7f7fefdf
rbegam <60943159+rbegam@users.noreply.github.com>
2020-09-01 06:19:56 -0700

    [SYCL] Fix incorrect data type (#2394)
    Signed-off-by: rbegam <rehana.begam@intel.com>

---
+++ sycl/test/inline-asm/negative_tests/asm_bad_operand_syntax.cpp
@@ -0,0 +1,33 @@
+// UNSUPPORTED: cuda
+// REQUIRES: gpu,linux
+// RUN: %clangxx -fsycl %s -DINLINE_ASM -o %t.out
+// RUN: not %t.out 2>&1 | FileCheck %s
+
+// CHECK: syntax error
+
+#include "../include/asmhelper.h"
+#include <CL/sycl.hpp>
+
+using dataType = cl::sycl::cl_int;
+
+template <typename T = dataType>
+struct KernelFunctor : WithOutputBuffer<T> {
+  KernelFunctor(size_t problem_size) : WithOutputBuffer<T>(problem_size) {}
+
+  void operator()(cl::sycl::handler &cgh) {
+    cgh.parallel_for<KernelFunctor<T>>(
+        cl::sycl::range<1>{this->getOutputBufferSize()}, [=](cl::sycl::id<1> wiID) [[cl::intel_reqd_sub_group_size(8)]] {          
+#if defined(INLINE_ASM) && defined(__SYCL_DEVICE_ONLY__)
+          asm volatile(".decl tmp1 v_type=G type=d num_elts=16 align=GRF\n"
+                       ".decl tmp2 v_type=G type=d num_elts=16 align=GRF\n"
+                       "mov (M1_NM, 8) tmp1(0,1)<1>:f tmp2(0,0)<1;1,0>\n");
+#endif
+        });
+  }
+};
+
+int main() {
+  KernelFunctor<> f(DEFAULT_PROBLEM_SIZE);
+  launchInlineASMTest(f);
+  return 0;
+}
