21009d6d40e892e566a42dfa09842a5f45f3ec03
James Brodman <james.brodman@intel.com>
2020-09-02 16:05:29 -0400

    Change lit invocation
    Signed-off-by: James Brodman <james.brodman@intel.com>

--- sycl/test/filter_selector/reuse.cpp
+++ sycl/test/filter_selector/reuse.cpp
@@ -1,5 +1,8 @@
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t1.out
-// RUN: %t1.out
+// RUN: env SYCL_DEVICE_TYPE=HOST %t1.out
+// RUN: %CPU_RUN_PLACEHOLDER %t1.out
+// RUN: %GPU_RUN_PLACEHOLDER %t1.out
+// RUN: %ACC_RUN_PLACEHOLDER %t1.out
 
 //==----------------- reuse.cpp - filter_selector reuse test ---------------==//
 //
--- sycl/test/filter_selector/select.cpp
+++ sycl/test/filter_selector/select.cpp
@@ -1,5 +1,8 @@
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t1.out
-// RUN: %t1.out
+0;10;1c// RUN: env SYCL_DEVICE_TYPE=HOST %t1.out
+// RUN: %CPU_RUN_PLACEHOLDER %t1.out
+// RUN: %GPU_RUN_PLACEHOLDER %t1.out
+// RUN: %ACC_RUN_PLACEHOLDER %t1.out
 
 //==------------------- select.cpp - filter_selector test ------------------==//
 //
