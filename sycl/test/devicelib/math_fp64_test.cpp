a1bca2791e9e75ab3c3556c867c1294b8c829507
gejin <ge.jin@intel.com>
2020-09-03 17:01:04 +0800

    Use correct option value for devicelib test
    Signed-off-by: gejin <ge.jin@intel.com>

--- sycl/test/devicelib/math_fp64_test.cpp
+++ sycl/test/devicelib/math_fp64_test.cpp
@@ -1,5 +1,5 @@
 // REQUIRES: cpu, linux
-// RUN: %clangxx -fsycl %s -o %t.out
+// RUN: %clangxx -fsycl -fsycl-device-lib=libm-fp64 %s -o %t.out
 // RUN: env SYCL_DEVICE_TYPE=HOST %t.out
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
 // RUN: %ACC_RUN_PLACEHOLDER %t.out
