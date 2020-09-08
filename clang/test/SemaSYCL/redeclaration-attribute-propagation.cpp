8eba1d05c1eba8865ee9bcd06002f6d321480710
Sindhu Chittireddy <sindhu.chittireddy@intel.com>
2020-09-08 02:10:02 -0700

    Change test to use kernel_single_task from Inputs/sycl.hpp
    Pass functions as lambda parameters instead of calling them from kernel
    body

--- clang/test/SemaSYCL/redeclaration-attribute-propagation.cpp
+++ clang/test/SemaSYCL/redeclaration-attribute-propagation.cpp
@@ -1,6 +1,6 @@
-// RUN: %clang_cc1 %s -I %S/Inputs -fsyntax-only -fsycl -fsycl-is-device -triple spir64 -verify
-// RUN: %clang_cc1 %s -I %S/Inputs -fsyntax-only -fsycl -fsycl-is-device -triple spir64 -DTRIGGER_ERROR -verify
-// RUN: %clang_cc1 %s -I %S/Inputs -fsyntax-only -ast-dump -fsycl -fsycl-is-device -triple spir64 | FileCheck %s
+// RUN: %clang_cc1 %s -I %S/Inputs -fsyntax-only -fsycl -fsycl-is-device -triple spir64 -Wno-sycl-2017-compat -verify
+// RUN: %clang_cc1 %s -I %S/Inputs -fsyntax-only -fsycl -fsycl-is-device -triple spir64 -DTRIGGER_ERROR -Wno-sycl-2017-compat -verify
+// RUN: %clang_cc1 %s -I %S/Inputs -fsyntax-only -ast-dump -fsycl -fsycl-is-device -triple spir64 -Wno-sycl-2017-compat | FileCheck %s
 
 #include <sycl.hpp>
 
@@ -16,7 +16,8 @@ func1();
 
 #else
 //second case - expect error
-[[intelfpga::max_work_group_size(4, 4, 4)]] void
+[[intelfpga::max_work_group_size(4, 4, 4)]] // expected-note {{conflicting attribute is here}}
+void
 func2();
 
 [[cl::reqd_work_group_size(8, 8, 8)]] // expected-note {{conflicting attribute is here}}
@@ -28,7 +29,7 @@ func2() {}
 void
 func3();
 
-[[cl::reqd_work_group_size(1, 1, 1)]] // expected-note 2 {{conflicting attribute is here}}
+[[cl::reqd_work_group_size(1, 1, 1)]] // expected-note {{conflicting attribute is here}}
 void
 // expected-warning@+1 {{attribute 'reqd_work_group_size' is already applied with different parameters}}
 func3() {} // expected-error {{'reqd_work_group_size' attribute conflicts with ''reqd_work_group_size'' attribute}}
@@ -44,32 +45,24 @@ void
 func4() {} // expected-error {{'max_work_group_size' attribute conflicts with ''max_work_group_size'' attribute}}
 #endif
 
-template <typename Name, typename Type>
-__attribute__((sycl_kernel)) void kernel(Type kernelFunc) {
-  kernelFunc();
+int main() {
 #ifndef TRIGGER_ERROR
-  func1();
-#else
-  func2();
-  func3();
-  func4();
-#endif
-}
-
-template <typename Name, typename Type>
-void parallel_for(Type kernelFunc) {
-  kernel<Name>(kernelFunc);
-}
-
-void use() {
-#ifndef TRIGGER_ERROR
-  // CHECK-LABEL:  FunctionDecl {{.*}} use 'void ()'
-  // CHECK:  `-FunctionDecl {{.*}}KernelName 'void ()'
+  // CHECK-LABEL:  FunctionDecl {{.*}} main 'int ()'
+  // CHECK:  `-FunctionDecl {{.*}}test_kernel1 'void ()'
   // CHECK:  -SYCLIntelMaxWorkGroupSizeAttr {{.*}} Inherited 4 4 4
   // CHECK:  -SYCLIntelNoGlobalWorkOffsetAttr {{.*}} Inherited Enabled
   // CHECK:  `-ReqdWorkGroupSizeAttr {{.*}} 2 2 2
-  parallel_for<class KernelName>([]() {});
+  cl::sycl::kernel_single_task<class test_kernel1>(
+      []() { func1(); });
+
 #else
-  parallel_for<class KernelName>([]() {}); // expected-error {{conflicting attributes applied to a SYCL kernel or SYCL_EXTERNAL function}}
+  cl::sycl::kernel_single_task<class test_kernel2>(
+      []() { func2(); }); // expected-error {{conflicting attributes applied to a SYCL kernel or SYCL_EXTERNAL function}}
+
+  cl::sycl::kernel_single_task<class test_kernel3>(
+      []() { func3(); });
+
+  cl::sycl::kernel_single_task<class test_kernel4>(
+      []() { func4(); });
 #endif
 }
