ba91f753f05be325c601f4f1c0dd3bf5e5d6d7c9
gejin <ge.jin@intel.com>
2020-09-09 17:28:07 +0800

    remove unnecessary restrictions for devicelib lit tests
    Signed-off-by: gejin <ge.jin@intel.com>

--- sycl/test/devicelib/math_test.cpp
+++ sycl/test/devicelib/math_test.cpp
@@ -1,7 +1,7 @@
-// REQUIRES: cpu, linux
 // RUN: %clangxx -fsycl %s -o %t.out
 // RUN: env SYCL_DEVICE_TYPE=HOST %t.out
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
+// RUN: %ACC_RUN_PLACEHOLDER %t.out
 
 #include "math_utils.hpp"
 #include <CL/sycl.hpp>
