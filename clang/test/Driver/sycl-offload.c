b0e19176bef2078e8eaf9ccc10374dc1c171f76c
Michael D Toguchi <michael.d.toguchi@intel.com>
2020-09-11 11:02:22 -0700

    [Driver][SYCL] Add defaultlib directive for sycl lib
    When compiling for Windows, add the sycl dependent library to the
    object.  If a person were to use /MTd to compile and not use /MTd
    to link, the default sycl lib would be passed to the linker instead
    of sycld.

--- clang/test/Driver/sycl-offload.c
+++ clang/test/Driver/sycl-offload.c
@@ -577,21 +577,23 @@
 
 /// Check for default linking of sycl.lib with -fsycl usage
 // RUN: %clang -fsycl -target x86_64-unknown-windows-msvc %s -o %t -### 2>&1 | FileCheck -check-prefix=CHECK-LINK-SYCL %s
-// RUN: %clang_cl -fsycl %s -o %t -### 2>&1 | FileCheck -check-prefix=CHECK-LINK-SYCL %s
-// CHECK-LINK-SYCL: "{{.*}}link{{(.exe)?}}"
+// RUN: %clang_cl -fsycl %s -o %t -### 2>&1 | FileCheck -check-prefix=CHECK-LINK-SYCL-CL %s
+// CHECK-LINK-SYCL-CL: "--dependent-lib=sycl"
+// CHECK-LINK-SYCL-CL-NOT: "-defaultlib:sycl.lib"
 // CHECK-LINK-SYCL: "-defaultlib:sycl.lib"
 
 /// Check no SYCL runtime is linked with -nolibsycl
 // RUN: %clang -fsycl -nolibsycl -target x86_64-unknown-windows-msvc %s -o %t -### 2>&1 | FileCheck -check-prefix=CHECK-LINK-NOLIBSYCL %s
 // RUN: %clang_cl -fsycl -nolibsycl %s -o %t -### 2>&1 | FileCheck -check-prefix=CHECK-LINK-NOLIBSYCL %s
+// CHECK-LINK-NOLIBSYCL-NOT: "--dependent-lib=sycl"
 // CHECK-LINK-NOLIBSYCL: "{{.*}}link{{(.exe)?}}"
 // CHECK-LINK-NOLIBSYCL-NOT: "-defaultlib:sycl.lib"
 
 /// Check sycld.lib is chosen with /MDd and /MTd
 // RUN:  %clang_cl -fsycl /MDd %s -o %t -### 2>&1 | FileCheck -check-prefix=CHECK-LINK-SYCL-DEBUG %s
 // RUN:  %clang_cl -fsycl /MTd %s -o %t -### 2>&1 | FileCheck -check-prefix=CHECK-LINK-SYCL-DEBUG %s
-// CHECK-LINK-SYCL-DEBUG: "{{.*}}link{{(.exe)?}}"
-// CHECK-LINK-SYCL-DEBUG: "-defaultlib:sycld.lib"
+// CHECK-LINK-SYCL-DEBUG: "--dependent-lib=sycld"
+// CHECK-LINK-SYCL-DEBUG-NOT: "-defaultlib:sycld.lib"
 
 /// Check "-spirv-allow-unknown-intrinsics" option is emitted for llvm-spirv tool for esimd mode
 // RUN: %clangxx %s -fsycl -fsycl-explicit-simd -### 2>&1 | FileCheck %s --check-prefix=CHK-FSYCL-ESIMD
