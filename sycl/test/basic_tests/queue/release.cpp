4d2b60379aacabc0355bd9bea0ca9136c6347c86
Chris Perkins <chris.perkins@intel.com>
2020-09-15 09:57:54 -0700

    prep
    Signed-off-by: Chris Perkins <chris.perkins@intel.com>

--- sycl/test/basic_tests/queue/release.cpp
+++ sycl/test/basic_tests/queue/release.cpp
@@ -1,6 +1,5 @@
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple  %s -o %t.out
 // RUN: env SYCL_PI_TRACE=2 %GPU_RUN_PLACEHOLDER %t.out | FileCheck %s
-// RUN: env SYCL_PI_TRACE=2 %CPU_RUN_PLACEHOLDER %t.out | FileCheck %s
 
 #include <CL/sycl.hpp>
 int main() {
