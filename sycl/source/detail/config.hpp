0a0cf635065caa715171da5ae28ecfac978553ec
Byoungro So <byoungro.so@intel.com>
2020-09-16 07:36:46 -0700

    Update sycl/source/detail/config.hpp
    Co-authored-by: vladimirlaz <vladimir.lazarev@intel.com>

--- sycl/source/detail/config.hpp
+++ sycl/source/detail/config.hpp
@@ -181,7 +181,6 @@ public:
 
     const char *ValStr = BaseT::getRawValue();
     if (ValStr) {
-      // std::string FilterString = ValStr;
       static device_filter_list DFL{ValStr};
       FilterList = &DFL;
     }
