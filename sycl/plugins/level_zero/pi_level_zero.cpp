f14fef7b58049d1249bcff307f8fea849ef1e3dc
Kieran Holland <kieran.holland@intel.com>
2020-09-22 07:52:28 -0700

    Updated level zero plugin with clang-format

--- sycl/plugins/level_zero/pi_level_zero.cpp
+++ sycl/plugins/level_zero/pi_level_zero.cpp
@@ -668,7 +668,7 @@ static pi_result getOrCreatePlatform(ze_driver_handle_t ZeDriver,
     static auto PiPlatformsCacheMutex = new std::mutex;
 
     std::lock_guard<std::mutex> Lock(*PiPlatformsCacheMutex);
-    for (const pi_platform& CachedPlatform : *PiPlatformsCache) {
+    for (const pi_platform &CachedPlatform : *PiPlatformsCache) {
       if (CachedPlatform->ZeDriver == ZeDriver) {
         Platform[0] = CachedPlatform;
         return PI_SUCCESS;
