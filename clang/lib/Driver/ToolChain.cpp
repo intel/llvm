a476a49446d4c054121a7c1f1b45ce2dd619f225
meghanathatishetti <62088706+meghanathatishetti@users.noreply.github.com>
2020-09-21 22:38:43 -0700

    [SYCL][Driver][NFC] Address klockwork issues (#2496)
    Signed-off-by: Meghana Thatishetti <meghana.thatishetti@intel.com>

--- clang/lib/Driver/ToolChain.cpp
+++ clang/lib/Driver/ToolChain.cpp
@@ -1255,6 +1255,10 @@ llvm::opt::DerivedArgList *ToolChain::TranslateOffloadTargetArgs(
         continue;
       }
     }
+
+    if (!XOffloadTargetArg)
+      continue;
+
     XOffloadTargetArg->setBaseArg(A);
     A = XOffloadTargetArg.release();
     AllocatedArgs.push_back(A);
