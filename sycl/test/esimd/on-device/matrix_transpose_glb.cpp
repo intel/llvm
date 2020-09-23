701c322e1b3777236252b4fcd474e9b990601282
Yang,Yin <yin.yang@intel.com>
2020-09-23 11:07:02 +0800

    Update sycl/test/esimd/on-device/matrix_transpose_glb.cpp
    Co-authored-by: Alexey Bader <alexey.bader@intel.com>

--- sycl/test/esimd/on-device/matrix_transpose_glb.cpp
+++ sycl/test/esimd/on-device/matrix_transpose_glb.cpp
@@ -10,7 +10,7 @@
 // REQUIRES: gpu
 // RUN: %clangxx-esimd -fsycl %s -o %t.out
 // RUN: %ESIMD_RUN_PLACEHOLDER %t.out
-// XFAIL: gpu && (level_zero || opencl) && linux
+// XFAIL: linux
 // UNSUPPORTED: cuda
 
 #include "esimd_test_utils.hpp"
