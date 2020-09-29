d5d72900fe71cefde519b2e5df96a656bcafc260
Aleksander Fadeev <aleksander.fadeev@intel.com>
2020-09-29 11:13:32 +0300

    Add test and comments
    Signed-off-by: Aleksander Fadeev <aleksander.fadeev@intel.com>

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -4012,6 +4012,12 @@ LangAS CodeGenModule::getStringLiteralAddressSpace() const {
     //   const char *getLiteral() n{
     //     return "AB";
     //   }
+    // because there is a addressspacecast to generic address space in IR,
+    // but adressspacecast from constant to generic forbitten because of 
+    // constant address space is not part of generic address space.
+    // The private adress space doesn't suit here because a IR is translated 
+    // in SPIRV in SYCLIsDevice mode, and all global Value shouldn't
+    // be private in IR for rigth translation to SPIRV.
     return LangAS::opencl_global;
   if (auto AS = getTarget().getConstantAddressSpace())
     return AS.getValue();
