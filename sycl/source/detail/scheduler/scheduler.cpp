9b94c8cd922fbe1fdb00aad5d9cbb52e5ebb22c0
Chris Perkins <chris.perkins@intel.com>
2020-09-30 12:06:22 -0700

    reversal.  Seems obvious in retrospect. But why lit tests not catching this reliably?
    Signed-off-by: Chris Perkins <chris.perkins@intel.com>

--- sycl/source/detail/scheduler/scheduler.cpp
+++ sycl/source/detail/scheduler/scheduler.cpp
@@ -99,14 +99,14 @@ EventImplPtr Scheduler::addCG(std::unique_ptr<detail::CG> CommandGroup,
       if (!Enqueued && EnqueueResultT::SyclEnqueueFailed == Res.MResult)
         throw runtime_error("Enqueue process failed.", PI_INVALID_OPERATION);
 
+      if (IsKernel)
+        Streams = ((ExecCGCommand *)NewCmd)->getStreams();
+
       if (NewCmd->MDeps.size() == 0 && NewCmd->MUsers.size() == 0) {
         NewEvent->setCommand(nullptr); // if there are no memory dependencies,
                                        // decouple and free the command
         delete NewCmd;
       }
-
-      if (IsKernel)
-        Streams = ((ExecCGCommand *)NewCmd)->getStreams();
     }
   }
 
