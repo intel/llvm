5ab80e01e7e30d65869cd8046f1e0b1721dd02dc
Srividya Sundaram <srividya.sundaram@intel.com>
2020-10-01 07:02:27 -0700

    Update implicit_kernel_type with SYCL2020 flag

--- clang/test/SemaSYCL/implicit_kernel_type.cpp
+++ clang/test/SemaSYCL/implicit_kernel_type.cpp
@@ -1,8 +1,12 @@
-// RUN: %clang_cc1 -fsycl -fsycl-is-device -internal-isystem %S/Inputs -fsycl-int-header=%t.h -fsyntax-only -verify %s -Werror=sycl-strict -DERROR
-// RUN: %clang_cc1 -fsycl -fsycl-is-device -internal-isystem %S/Inputs -fsycl-int-header=%t.h -fsyntax-only -verify %s  -Wsycl-strict -DWARN
-// RUN: %clang_cc1 -fsycl -fsycl-is-device -internal-isystem %S/Inputs -fsycl-int-header=%t.h -fsycl-unnamed-lambda -fsyntax-only -verify %s  -Werror=sycl-strict
+// RUN: %clang_cc1 -fsycl -fsycl-is-device -internal-isystem %S/Inputs -fsycl-int-header=%t.h -fsyntax-only -sycl-std=2020 -verify %s -Werror=sycl-strict -DERROR
+// RUN: %clang_cc1 -fsycl -fsycl-is-device -internal-isystem %S/Inputs -fsycl-int-header=%t.h -fsyntax-only -sycl-std=2020 -verify %s  -Wsycl-strict -DWARN
+// RUN: %clang_cc1 -fsycl -fsycl-is-device -internal-isystem %S/Inputs -fsycl-int-header=%t.h -fsycl-unnamed-lambda -fsyntax-only -sycl-std=2020 -verify %s  -Werror=sycl-strict
 
 #include "sycl.hpp"
+
+#ifdef __SYCL_UNNAMED_LAMBDA__
+// expected-no-diagnostics
+#endif
 
 using namespace cl::sycl;
 
@@ -21,67 +25,54 @@ int main() {
   queue q;
 
 #if defined(WARN)
-  // expected-warning@Inputs/sycl.hpp:211 {{Passing of kernel functions by reference is a SYCL 2020 extension}}
   // expected-error@Inputs/sycl.hpp:220 {{kernel needs to have a globally-visible name}}
-  // expected-note@+8 {{InvalidKernelName1 declared here}}
+  // expected-note@+7 {{InvalidKernelName1 declared here}}
+  // expected-note@+8 {{in instantiation of function template specialization}}
 #elif defined(ERROR)
-  // expected-error@Inputs/sycl.hpp:211 {{Passing of kernel functions by reference is a SYCL 2020 extension}}
   // expected-error@Inputs/sycl.hpp:220 {{kernel needs to have a globally-visible name}}
-  // expected-note@+4 {{InvalidKernelName1 declared here}}
-#elif defined(__SYCL_UNNAMED_LAMBDA__)
-  // expected-error@Inputs/sycl.hpp:211 {{Passing of kernel functions by reference is a SYCL 2020 extension}}
+  // expected-note@+3 {{InvalidKernelName1 declared here}}
+  // expected-note@+4 {{in instantiation of function template specialization}}
 #endif
   class InvalidKernelName1 {};
-  // expected-note@+2 {{in instantiation of function template specialization}}
   q.submit([&](handler &h) {
     h.single_task<InvalidKernelName1>([]() {});
   });
 
 #if defined(WARN)
   // expected-warning@Inputs/sycl.hpp:220 {{SYCL 1.2.1 specification requires an explicit forward declaration for a kernel type name; your program may not be portable}}
-  // expected-warning@Inputs/sycl.hpp:211 {{Passing of kernel functions by reference is a SYCL 2020 extension}}
 #elif defined(ERROR)
   // expected-error@Inputs/sycl.hpp:220 {{SYCL 1.2.1 specification requires an explicit forward declaration for a kernel type name; your program may not be portable}}
-  // expected-error@Inputs/sycl.hpp:211 {{Passing of kernel functions by reference is a SYCL 2020 extension}}
-#elif defined(__SYCL_UNNAMED_LAMBDA__)
-  // expected-error@Inputs/sycl.hpp:211 {{Passing of kernel functions by reference is a SYCL 2020 extension}}
 #endif
 
   q.submit([&](handler &h) {
 #ifndef __SYCL_UNNAMED_LAMBDA__
   // expected-note@+3 {{fake_kernel declared here}}
+  // expected-note@+2 {{in instantiation of function template specialization}}
 #endif
-    // expected-note@+1 {{in instantiation of function template specialization}}
     h.single_task<class fake_kernel>([]() { function(); });
   });
 
 #if defined(WARN)
   // expected-warning@Inputs/sycl.hpp:220 {{SYCL 1.2.1 specification requires an explicit forward declaration for a kernel type name; your program may not be portable}}
-  // expected-warning@Inputs/sycl.hpp:211 3{{Passing of kernel functions by reference is a SYCL 2020 extension}}
 #elif defined(ERROR)
   // expected-error@Inputs/sycl.hpp:220 {{SYCL 1.2.1 specification requires an explicit forward declaration for a kernel type name; your program may not be portable}}
-  // expected-error@Inputs/sycl.hpp:211 3{{Passing of kernel functions by reference is a SYCL 2020 extension}}
-#elif defined(__SYCL_UNNAMED_LAMBDA__)
-  // expected-error@Inputs/sycl.hpp:211 3{{Passing of kernel functions by reference is a SYCL 2020 extension}}
 #endif
 
   q.submit([&](handler &h) {
 #ifndef __SYCL_UNNAMED_LAMBDA__
   // expected-note@+3 {{fake_kernel2 declared here}}
+  // expected-note@+2 {{in instantiation of function template specialization}}
 #endif
-    // expected-note@+1 {{in instantiation of function template specialization}}
     h.single_task<class fake_kernel2>([]() {
       auto l = [](auto f) { f(); };
     });
   });
 
   q.submit([&](handler &h) {
-    // expected-note@+1 {{in instantiation of function template specialization}}
     h.single_task<class myWrapper>([]() { function(); });
   });
 
   q.submit([&](handler &h) {
-    // expected-note@+1 {{in instantiation of function template specialization}}
     h.single_task<class myWrapper2>([]() { function(); });
   });
   return 0;
