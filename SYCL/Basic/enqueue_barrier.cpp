8b8687d8bf09c1ff023615444718871b9975dc77
vladimirlaz <vladimir.lazarev@intel.com>
2020-10-22 16:34:56 +0300

    Disable flaky test on windows (intel/llvm-test-suite#40)

--- SYCL/Basic/enqueue_barrier.cpp
+++ SYCL/Basic/enqueue_barrier.cpp
@@ -3,7 +3,9 @@
 // RUN: env SYCL_PI_TRACE=2 %GPU_RUN_PLACEHOLDER %t.out 2>&1 %GPU_CHECK_PLACEHOLDER
 // RUN: env SYCL_PI_TRACE=2 %ACC_RUN_PLACEHOLDER %t.out 2>&1 %ACC_CHECK_PLACEHOLDER
 
-// UNSUPPORTED: cuda
+// The test is failing sporadically on Windows OpenCL RTs
+// Disabling on windows until fixed
+// UNSUPPORTED: cuda || windows
 
 #include <CL/sycl.hpp>
 #include <CL/sycl/INTEL/fpga_device_selector.hpp>
