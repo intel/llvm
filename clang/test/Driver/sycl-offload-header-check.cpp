a5091707280f41aca94115441ce47b1f9326aa64
Michael D Toguchi <michael.d.toguchi@intel.com>
2020-10-26 17:45:23 -0700

    [Driver][SYCL] Remove real_path workaround for integration header
    We applied a workaround for integration header usage on Windows that
    corrected the directory name in case it did not match expectations
    of the file system (lowercase/uppercase like things).  The workaround
    is not needed anymore as community has addressed the non-portable
    path warning directly for Windows.

--- clang/test/Driver/sycl-offload-header-check.cpp
+++ clang/test/Driver/sycl-offload-header-check.cpp
@@ -9,7 +9,8 @@
 // RUN: env TMP=%t_dirname  \
 // RUN: %clang -### -fsycl %s 2>&1 | \
 // RUN: FileCheck --check-prefix=CHECK-HEADER %s
+// CHECK-HEADER-NOT: non-portable path
 // CHECK-HEADER: clang{{.*}} "-fsycl-int-header=[[HEADER:.+\.h]]"
 // CHECK-HEADER: {{.*}} "-internal-isystem" "{{.*}}bin{{[/\\]+}}..{{[/\\]+}}include{{[/\\]+}}sycl"
 // CHECK-HEADER-NOT: clang{{.*}} "-include" "[[HEADER]]"
-// CHECK-HEADER: clang{{.*}} "-include" "{{.*}}_DiRnAmE{{.+}}.h"
+// CHECK-HEADER: clang{{.*}} "-include" "{{.*}}_dirname{{.+}}.h"
