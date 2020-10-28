4bbedbde23874a935c9e5a86ff2cc7acb88e52c4
smaslov-intel <48694368+smaslov-intel@users.noreply.github.com>
2020-10-28 15:57:53 +0700

    [SYCL] Update for the new format of sycl-ls (intel/llvm-test-suite#42)
    Signed-off-by: Sergey V Maslov <sergey.v.maslov@intel.com>

--- SYCL/Plugin/sycl-ls-gpu-level-zero.cpp
+++ SYCL/Plugin/sycl-ls-gpu-level-zero.cpp
@@ -1,12 +1,12 @@
-// REQUIRES: gpu
+// REQUIRES: gpu, level_zero
 
-// RUN: env --unset=SYCL_BE sycl-ls --verbose >%t.default.out
+// RUN: sycl-ls --verbose >%t.default.out
 // RUN: FileCheck %s --check-prefixes=CHECK-GPU-BUILTIN,CHECK-GPU-CUSTOM --input-file %t.default.out
 
 // CHECK-GPU-BUILTIN: gpu_selector(){{.*}}GPU : {{.*}}Level-Zero
 // CHECK-GPU-CUSTOM: custom_selector(gpu){{.*}}GPU : {{.*}}Level-Zero
 
-//==-- sycl-ls-gpu-default.cpp - SYCL test for default selected gpu device -==//
+//==-- sycl-ls-gpu-level-zero.cpp - Test Level-Zero selected gpu device ----==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
