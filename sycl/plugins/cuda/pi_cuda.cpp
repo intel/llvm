2c9c94642ffbb96bb3eb1cb04f26d04cd7218afa
Alexander Batashev <alexander.batashev@intel.com>
2020-11-03 14:17:18 +0300

    [SYCL][CUDA] Fix incorrectly dispatched PI methods

--- sycl/plugins/cuda/pi_cuda.cpp
+++ sycl/plugins/cuda/pi_cuda.cpp
@@ -4504,8 +4504,8 @@ pi_result piPluginInit(pi_plugin *PluginInit) {
   _PI_CL(piProgramGetBuildInfo, cuda_piProgramGetBuildInfo)
   _PI_CL(piProgramRetain, cuda_piProgramRetain)
   _PI_CL(piProgramRelease, cuda_piProgramRelease)
-  _PI_CL(piextMemGetNativeHandle, cuda_piextMemGetNativeHandle)
-  _PI_CL(piextMemCreateWithNativeHandle, cuda_piextMemCreateWithNativeHandle)
+  _PI_CL(piextProgramGetNativeHandle, cuda_piextProgramGetNativeHandle)
+  _PI_CL(piextProgramCreateWithNativeHandle, cuda_piextProgramCreateWithNativeHandle)
   // Kernel
   _PI_CL(piKernelCreate, cuda_piKernelCreate)
   _PI_CL(piKernelSetArg, cuda_piKernelSetArg)
