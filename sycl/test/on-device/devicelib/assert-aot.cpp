7b7775ece12b4f432ea19951cc7b91487ac9e112
vladimirlaz <vladimir.lazarev@intel.com>
2020-11-12 08:46:50 +0300

    [SYCL] Remove tests added to intel/llvm-test-suite (#2753)
    The tests were added in scope of https://github.com/intel/llvm-test-suite/pull/50

--- sycl/test/on-device/devicelib/assert-aot.cpp
+++
@@ -1,5 +0,0 @@
-// REQUIRES: opencl-aot, cpu, linux
-
-// RUN: %clangxx -fsycl -fsycl-targets=spir64_x86_64-unknown-unknown-sycldevice %S/assert.cpp -o %t.aot.out
-// RUN: %CPU_RUN_PLACEHOLDER %t.aot.out >%t.aot.msg
-// RUN: FileCheck %S/assert.cpp --input-file %t.aot.msg --check-prefixes=CHECK-MESSAGE
