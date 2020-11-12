4d21d3320224ab05894efd0b3f02a8578a5bf9db
Mikhail Lychkov <mikhail.lychkov@intel.com>
2020-11-13 01:01:03 +0300

    Fix stream LIT test
    Signed-off-by: Mikhail Lychkov <mikhail.lychkov@intel.com>

--- sycl/test/on-device/basic_tests/stream/stream.cpp
+++ sycl/test/on-device/basic_tests/stream/stream.cpp
@@ -311,23 +311,19 @@ int main() {
     // CHECK: global id {{[0-9]+}}
 
     Queue.submit([&](handler &cgh) {
-      stream stream1(1024, 80, cgh);
+      stream stream1(1024, 160, cgh);
       cgh.parallel_for<class test_dev_func_stream>(
-          nd_range<1>(range<1>(2), range<1>(1)), [=](sycl::nd_item<1> it) {
+          nd_range<1>(range<1>(1), range<1>(1)), [=](sycl::nd_item<1> it) {
             stream1 << "stream1 print 1\n";
             dev_func(stream1);
-            stream1 << "stream1 print 2" << sycl::endl;
             sycl::stream stream2 = stream1;
+            stream1 << "stream1 print 2" << sycl::endl;
             stream2 << "stream2 print 1\n";
-            stream1 << "stream1 print 3" << sycl::endl;
+            stream1 << "stream1 print 3\n";
+            stream2 << sycl::flush;
           });
     });
     Queue.wait();
-    // CHECK-NEXT: stream1 print 1
-    // CHECK-NEXT: dev_func print
-    // CHECK-NEXT: stream1 print 2
-    // CHECK-NEXT: stream2 print 1
-    // CHECK-NEXT: stream1 print 3
     // CHECK-NEXT: stream1 print 1
     // CHECK-NEXT: dev_func print
     // CHECK-NEXT: stream1 print 2
