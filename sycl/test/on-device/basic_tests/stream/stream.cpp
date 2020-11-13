c423a4ecd2a0f5c9bb28c1d9ea999ccea0301751
Mikhail Lychkov <mikhail.lychkov@intel.com>
2020-11-13 12:15:01 +0300

    Separate stream buffer sync LIT test
    Signed-off-by: Mikhail Lychkov <mikhail.lychkov@intel.com>

--- sycl/test/on-device/basic_tests/stream/stream.cpp
+++ sycl/test/on-device/basic_tests/stream/stream.cpp
@@ -19,8 +19,6 @@
 
 using namespace cl::sycl;
 
-void dev_func(stream out) { out << "dev_func print\n"; }
-
 int main() {
   {
     default_selector Selector;
@@ -309,26 +307,6 @@ int main() {
     // CHECK: global id {{[0-9]+}}
     // CHECK: global id {{[0-9]+}}
     // CHECK: global id {{[0-9]+}}
-
-    Queue.submit([&](handler &cgh) {
-      stream stream1(1024, 160, cgh);
-      cgh.parallel_for<class test_dev_func_stream>(
-          nd_range<1>(range<1>(1), range<1>(1)), [=](sycl::nd_item<1> it) {
-            stream1 << "stream1 print 1\n";
-            dev_func(stream1);
-            sycl::stream stream2 = stream1;
-            stream1 << "stream1 print 2" << sycl::endl;
-            stream2 << "stream2 print 1\n";
-            stream1 << "stream1 print 3\n";
-            stream2 << sycl::flush;
-          });
-    });
-    Queue.wait();
-    // CHECK-NEXT: stream1 print 1
-    // CHECK-NEXT: dev_func print
-    // CHECK-NEXT: stream1 print 2
-    // CHECK-NEXT: stream2 print 1
-    // CHECK-NEXT: stream1 print 3
   }
 
   return 0;
