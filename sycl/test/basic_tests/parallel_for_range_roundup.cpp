c340ccf649daed65af5f306a8e464d559bb6f095
rdeodhar <rajiv.deodhar@intel.com>
2020-11-17 22:55:43 -0800

    Changed runtime test.

--- sycl/test/basic_tests/parallel_for_range_roundup.cpp
+++ sycl/test/basic_tests/parallel_for_range_roundup.cpp
@@ -1,8 +1,9 @@
-// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
-// RUN: env SYCL_DEVICE_TYPE=HOST %t.out
-// RUN: %ACC_RUN_PLACEHOLDER %t.out
-// RUN: %CPU_RUN_PLACEHOLDER %t.out
-// RUN: %GPU_RUN_PLACEHOLDER %t.out
+// RUN: %clangxx -fsycl %s -o %t.out
+// RUN: env SYCL_DEVICE_TYPE=HOST SYCL_OPT_PFWGS_TRACE=1 %t.out 2> %t.errout
+// RUN: %ACC_RUN_PLACEHOLDER SYCL_OPT_PFWGS_TRACE=1 %t.out 2> %t.errout
+// RUN: %CPU_RUN_PLACEHOLDER SYCL_OPT_PFWGS_TRACE=1 %t.out 2> %t.errout
+// RUN: %GPU_RUN_PLACEHOLDER SYCL_OPT_PFWGS_TRACE=1 %t.out 2> %t.errout
+// RUN: cat %t.errout | FileCheck %s
 
 #include <CL/sycl.hpp>
 
@@ -29,41 +30,25 @@ int try_item(size_t size) {
   SizesInfo SInfo;
   range<1> Size{size};
   int Counter = 0;
-  bool OnGpu;
 
   {
     buffer<SizesInfo, 1> BufSizes(&SInfo, 1);
-    buffer<int, 1> BufCounter(&Counter, Size);
+    buffer<int, 1> BufCounter(&Counter, 1);
     queue myQueue;
-    OnGpu = myQueue.get_device().is_gpu();
 
     myQueue.submit([&](handler &cgh) {
       auto AccSizes = BufSizes.get_access<access::mode::read_write>(cgh);
       auto AccCounter = BufCounter.get_access<access::mode::atomic>(cgh);
 
       cgh.parallel_for<PF_init_item>(Size, [=](item<1> ITEM) {
-        // cgh.parallel_for<PF_init_item>(Size, [=](int ITEM) {
         AccCounter[0].fetch_add(1);
         AccSizes[0].ItemGlobalSize = ITEM.get_range(0);
-#ifdef __SYCL_DEVICE_ONLY__
-        AccSizes[0].RealGlobalSizeX = {__spirv_GlobalSize_x()};
-        AccSizes[0].RealGlobalSizeY = {__spirv_GlobalSize_y()};
-        AccSizes[0].RealGlobalSizeZ = {__spirv_GlobalSize_z()};
-#endif // __SYCL_DEVICE_ONLY__
       });
     });
   }
 
-  std::cout << std::endl;
-  if (OnGpu) {
-    std::cout << "Ran on GPU" << std::endl;
-    check("Real global size X = ", SInfo.RealGlobalSizeX.get(0), RoundedUpSize);
-    check("Real global size Y = ", SInfo.RealGlobalSizeY.get(0), 1);
-    check("Real global size Z = ", SInfo.RealGlobalSizeZ.get(0), 1);
-  }
   check("Size seen by user = ", SInfo.ItemGlobalSize.get(0), size);
   check("Counter = ", Counter, size);
-  std::cout << std::endl;
   return 0;
 }
 
@@ -76,9 +61,8 @@ int try_id(size_t size) {
 
   {
     buffer<SizesInfo, 1> BufSizes(&SInfo, 1);
-    buffer<int, 1> BufCounter(&Counter, Size);
+    buffer<int, 1> BufCounter(&Counter, 1);
     queue myQueue;
-    OnGpu = myQueue.get_device().is_gpu();
 
     myQueue.submit([&](handler &cgh) {
       auto AccSizes = BufSizes.get_access<access::mode::read_write>(cgh);
@@ -87,30 +71,16 @@ int try_id(size_t size) {
       cgh.parallel_for<class PF_init_id>(Size, [=](id<1> ID) {
         AccCounter[0].fetch_add(1);
         AccSizes[0].ItemGlobalSize = ID[0];
-#ifdef __SYCL_DEVICE_ONLY__
-        AccSizes[0].RealGlobalSizeX = {__spirv_GlobalSize_x()};
-        AccSizes[0].RealGlobalSizeY = {__spirv_GlobalSize_y()};
-        AccSizes[0].RealGlobalSizeZ = {__spirv_GlobalSize_z()};
-#endif // __SYCL_DEVICE_ONLY__
       });
     });
   }
-  std::cout << std::endl;
-  if (OnGpu) {
-    std::cout << "Ran on GPU" << std::endl;
-    check("Real global size X = ", SInfo.RealGlobalSizeX.get(0), RoundedUpSize);
-    check("Real global size Y = ", SInfo.RealGlobalSizeY.get(0), 1);
-    check("Real global size Z = ", SInfo.RealGlobalSizeZ.get(0), 1);
-  }
   check("Counter = ", Counter, size);
-  std::cout << std::endl;
 
   {
     buffer<SizesInfo, 1> BufSizes(&SInfo, 1);
-    buffer<int, 1> BufCounter(&Counter, Size);
+    buffer<int, 1> BufCounter(&Counter, 1);
     queue myQueue;
     Counter = 0;
-    OnGpu = myQueue.get_device().is_gpu();
 
     myQueue.submit([&](handler &cgh) {
       auto AccSizes = BufSizes.get_access<access::mode::read_write>(cgh);
@@ -118,23 +88,10 @@ int try_id(size_t size) {
       cgh.parallel_for<class PF_init_id1>(Size, [=](id<1> ID) {
         AccCounter[0].fetch_add(1);
         AccSizes[0].ItemGlobalSize = ID[0];
-#ifdef __SYCL_DEVICE_ONLY__
-        AccSizes[0].RealGlobalSizeX = {__spirv_GlobalSize_x()};
-        AccSizes[0].RealGlobalSizeY = {__spirv_GlobalSize_y()};
-        AccSizes[0].RealGlobalSizeZ = {__spirv_GlobalSize_z()};
-#endif // __SYCL_DEVICE_ONLY__
       });
     });
   }
-  std::cout << std::endl;
-  if (OnGpu) {
-    std::cout << "Ran on GPU" << std::endl;
-    check("Real global size X = ", SInfo.RealGlobalSizeX.get(0), RoundedUpSize);
-    check("Real global size Y = ", SInfo.RealGlobalSizeY.get(0), 1);
-    check("Real global size Z = ", SInfo.RealGlobalSizeZ.get(0), 1);
-  }
   check("Counter = ", Counter, size);
-  std::cout << std::endl;
 
   return 0;
 }
@@ -145,6 +102,9 @@ int main() {
   x = 10;
   try_item(x);
   try_id(x);
+  // CHECK: ***** Adjusted size from 10 to 32 *****
+  // CHECK: ***** Adjusted size from 10 to 32 *****
+  // CHECK: ***** Adjusted size from 10 to 32 *****
   x = 256;
   try_item(x);
   try_id(x);
