4e44e017fcd3d6ca6db1a1f4fac2a7225545885e
Viktoria Maksimova <viktoria.maksimova@intel.com>
2020-11-18 20:24:21 +0300

    Align tests with the community changes
    This patch aligns our tests to 5f12f4f and e7f3e21.
    
    They introduced suppressing of AST for inlined namespace names and template
    arguments that match default template.

--- sycl/test/abi/layout_accessors_device.cpp
+++ sycl/test/abi/layout_accessors_device.cpp
@@ -14,20 +14,20 @@ using namespace cl::sycl;
 SYCL_EXTERNAL void hostAcc(accessor<int, 1, access::mode::read, access::target::global_buffer> Acc) {
   (void)Acc.get_size();
 }
-// CHECK:  0 | class cl::sycl::accessor<int, 1, cl::sycl::access::mode::read, cl::sycl::access::target::global_buffer, cl::sycl::access::placeholder::false_t, class cl::sycl::ONEAPI::accessor_property_list<> >
-// CHECK-NEXT: 0 |   class cl::sycl::detail::accessor_common<int, 1, cl::sycl::access::mode::read, cl::sycl::access::target::global_buffer, cl::sycl::access::placeholder::false_t, class cl::sycl::ONEAPI::accessor_property_list<> > (base) (empty)
-// CHECK-NEXT: 0 |   class cl::sycl::detail::AccessorImplDevice<1> impl
-// CHECK-NEXT: 0 |     class cl::sycl::id<1> Offset
-// CHECK-NEXT: 0 |       class cl::sycl::detail::array<1> (base)
+// CHECK:      0 | class sycl::accessor<int, 1, sycl::access::mode::read, sycl::access::target::global_buffer, sycl::access::placeholder::false_t>
+// CHECK-NEXT: 0 |   class sycl::detail::accessor_common<int, 1, sycl::access::mode::read, sycl::access::target::global_buffer, sycl::access::placeholder::false_t> (base) (empty)
+// CHECK-NEXT: 0 |   class sycl::detail::AccessorImplDevice<1> impl
+// CHECK-NEXT: 0 |     class sycl::id<1> Offset
+// CHECK-NEXT: 0 |       class sycl::detail::array<1> (base)
 // CHECK-NEXT: 0 |         size_t [1] common_array
-// CHECK-NEXT: 8 |     class cl::sycl::range<1> AccessRange
-// CHECK-NEXT: 8 |       class cl::sycl::detail::array<1> (base)
+// CHECK-NEXT: 8 |     class sycl::range<1> AccessRange
+// CHECK-NEXT: 8 |       class sycl::detail::array<1> (base)
 // CHECK-NEXT: 8 |         size_t [1] common_array
-// CHECK-NEXT: 16 |     class cl::sycl::range<1> MemRange
-// CHECK-NEXT: 16 |       class cl::sycl::detail::array<1> (base)
+// CHECK-NEXT: 16 |     class sycl::range<1> MemRange
+// CHECK-NEXT: 16 |       class sycl::detail::array<1> (base)
 // CHECK-NEXT: 16 |         size_t [1] common_array
-// CHECK-NEXT: 24 |   union cl::sycl::accessor<int, 1, cl::sycl::access::mode::read, cl::sycl::access::target::global_buffer, cl::sycl::access::placeholder::false_t, class cl::sycl::ONEAPI::accessor_property_list<> > 
-// CHECK-NEXT: 24 |     cl::sycl::accessor<int, 1, cl::sycl::access::mode::read, cl::sycl::access::target::global_buffer, cl::sycl::access::placeholder::false_t, class cl::sycl::ONEAPI::accessor_property_list<> >::ConcreteASPtrType MData
+// CHECK-NEXT: 24 |   union sycl::accessor<int, 1, sycl::access::mode::read, sycl::access::target::global_buffer, sycl::access::placeholder::false_t>
+// CHECK-NEXT: 24 |     sycl::accessor<int, 1, sycl::access::mode::read, sycl::access::target::global_buffer, sycl::access::placeholder::false_t>::ConcreteASPtrType MData
 // CHECK-NEXT:     | [sizeof=32, dsize=32, align=8,
 // CHECK-NEXT:     |  nvsize=32, nvalign=8]
 
@@ -38,19 +38,19 @@ SYCL_EXTERNAL void hostAcc(accessor<int, 1, access::mode::read, access::target::
 SYCL_EXTERNAL void hostAcc(accessor<int, 1, access::mode::read_write, access::target::local> Acc) {
   (void)Acc.get_size();
 }
-// CHECK: 0 | class cl::sycl::accessor<int, 1, cl::sycl::access::mode::read_write, cl::sycl::access::target::local, cl::sycl::access::placeholder::false_t, class cl::sycl::ONEAPI::accessor_property_list<> >
-// CHECK-NEXT: 0 |   class cl::sycl::detail::accessor_common<int, 1, cl::sycl::access::mode::read_write, cl::sycl::access::target::local, cl::sycl::access::placeholder::false_t, class cl::sycl::ONEAPI::accessor_property_list<> > (base) (empty)
-// CHECK-NEXT: 0 |   class cl::sycl::detail::LocalAccessorBaseDevice<1> impl
-// CHECK-NEXT: 0 |     class cl::sycl::range<1> AccessRange
-// CHECK-NEXT: 0 |       class cl::sycl::detail::array<1> (base)
+// CHECK:      0 | class sycl::accessor<int, 1, sycl::access::mode::read_write, sycl::access::target::local, sycl::access::placeholder::false_t>
+// CHECK-NEXT: 0 |   class sycl::detail::accessor_common<int, 1, sycl::access::mode::read_write, sycl::access::target::local, sycl::access::placeholder::false_t> (base) (empty)
+// CHECK-NEXT: 0 |   class sycl::detail::LocalAccessorBaseDevice<1> impl
+// CHECK-NEXT: 0 |     class sycl::range<1> AccessRange
+// CHECK-NEXT: 0 |       class sycl::detail::array<1> (base)
 // CHECK-NEXT: 0 |         size_t [1] common_array
-// CHECK-NEXT: 8 |     class cl::sycl::range<1> MemRange
-// CHECK-NEXT: 8 |       class cl::sycl::detail::array<1> (base)
+// CHECK-NEXT: 8 |     class sycl::range<1> MemRange
+// CHECK-NEXT: 8 |       class sycl::detail::array<1> (base)
 // CHECK-NEXT: 8 |         size_t [1] common_array
-// CHECK-NEXT: 16 |     class cl::sycl::id<1> Offset
-// CHECK-NEXT: 16 |       class cl::sycl::detail::array<1> (base)
+// CHECK-NEXT: 16 |     class sycl::id<1> Offset
+// CHECK-NEXT: 16 |       class sycl::detail::array<1> (base)
 // CHECK-NEXT: 16 |         size_t [1] common_array
-// CHECK-NEXT: 24 |   cl::sycl::accessor<int, 1, cl::sycl::access::mode::read_write, cl::sycl::access::target::local, cl::sycl::access::placeholder::false_t, class cl::sycl::ONEAPI::accessor_property_list<> >::ConcreteASPtrType MData
+// CHECK-NEXT: 24 |   sycl::accessor<int, 1, sycl::access::mode::read_write, sycl::access::target::local, sycl::access::placeholder::false_t>::ConcreteASPtrType MData
 // CHECK-NEXT: | [sizeof=32, dsize=32, align=8,
 // CHECK-NEXT: |  nvsize=32, nvalign=8]
 
@@ -62,9 +62,9 @@ SYCL_EXTERNAL void hostAcc(accessor<int4, 1, access::mode::read, access::target:
   (void)Acc.get_count();
 }
 
-// CHECK: 0 | class cl::sycl::accessor<class cl::sycl::vec<int, 4>, 1, cl::sycl::access::mode::read, cl::sycl::access::target::image, cl::sycl::access::placeholder::false_t, class cl::sycl::ONEAPI::accessor_property_list<> >
-// CHECK-NEXT: 0 |   class cl::sycl::detail::image_accessor<class cl::sycl::vec<int, 4>, 1, cl::sycl::access::mode::read, cl::sycl::access::target::image, cl::sycl::access::placeholder::false_t> (base)
-// CHECK-NEXT: 0 |     cl::sycl::detail::image_accessor<class cl::sycl::vec<int, 4>, 1, cl::sycl::access::mode::read, cl::sycl::access::target::image, cl::sycl::access::placeholder::false_t>::OCLImageTy MImageObj
+// CHECK:      0 | class sycl::accessor<class sycl::vec<int, 4>, 1, sycl::access::mode::read, sycl::access::target::image, sycl::access::placeholder::false_t>
+// CHECK-NEXT: 0 |   class sycl::detail::image_accessor<class sycl::vec<int, 4>, 1, sycl::access::mode::read, sycl::access::target::image, sycl::access::placeholder::false_t> (base)
+// CHECK-NEXT: 0 |     sycl::detail::image_accessor<class sycl::vec<int, 4>, 1, sycl::access::mode::read, sycl::access::target::image, sycl::access::placeholder::false_t>::OCLImageTy MImageObj
 // CHECK-NEXT: 8 |     char [24] MPadding
 // CHECK-NEXT: | [sizeof=32, dsize=32, align=8,
 // CHECK-NEXT: |  nvsize=32, nvalign=8]
