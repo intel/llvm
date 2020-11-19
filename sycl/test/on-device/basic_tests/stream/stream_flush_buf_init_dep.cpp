de0ac8fef31e0d0c86b4007142ee8877ae6079e9
Mikhail Lychkov <mikhail.lychkov@intel.com>
2020-11-19 12:00:31 +0300

    Fix LIT test on windows
    Signed-off-by: Mikhail Lychkov <mikhail.lychkov@intel.com>

--- sycl/test/on-device/basic_tests/stream/stream_flush_buf_init_dep.cpp
+++ sycl/test/on-device/basic_tests/stream/stream_flush_buf_init_dep.cpp
@@ -6,16 +6,16 @@
 // This test is executed on host device only because flush buffer initialized
 // via a separate call group task for this device only.
 
-// CHECK: [[MAIN_CG:"0x[0-9a-f]+"]] {{.*}}EXEC CG ON HOST{{.*}}test_flush_buf_init_dep
+// CHECK: [[MAIN_CG:"[0-9a-fA-Fx]+"]] {{.*}}EXEC CG ON HOST{{.*}}test_flush_buf_init_dep
 // First 3 lines show dependencies on global buffer related commands
 // CHECK:   [[MAIN_CG]]
 // CHECK:   [[MAIN_CG]]
 // CHECK:   [[MAIN_CG]]
-// CHECK:   [[MAIN_CG]] -> [[EMPTY_NODE:"0x[0-9a-f]+"]] [ label = "Access mode: read_write\nMemObj: [[FLUSHBUF_MEMOBJ:0x[0-9a-f]+]]
+// CHECK:   [[MAIN_CG]] -> [[EMPTY_NODE:"[0-9a-fA-Fx]+"]] [ label = "Access mode: read_write\nMemObj: [[FLUSHBUF_MEMOBJ:[0-9a-fA-Fx]+]]
 // CHECK: [[EMPTY_NODE]] {{.*}}EMPTY NODE
-// CHECK:   [[EMPTY_NODE]] -> [[FILL_TASK:"0x[0-9a-f]+"]] [ label = "Access mode: discard_write\nMemObj: [[FLUSHBUF_MEMOBJ]]
+// CHECK:   [[EMPTY_NODE]] -> [[FILL_TASK:"[0-9a-fA-Fx]+"]] [ label = "Access mode: discard_write\nMemObj: [[FLUSHBUF_MEMOBJ]]
 // CHECK: [[FILL_TASK]] {{.*}}EXEC CG ON HOST\nCG type: host task
-// CHECK:   [[FILL_TASK]] -> [[ALLOC_TASK:"0x[0-9a-f]+"]] [ label = "Access mode: discard_write\nMemObj: [[FLUSHBUF_MEMOBJ]]
+// CHECK:   [[FILL_TASK]] -> [[ALLOC_TASK:"[0-9a-fA-Fx]+"]] [ label = "Access mode: discard_write\nMemObj: [[FLUSHBUF_MEMOBJ]]
 // CHECK: [[ALLOC_TASK]] {{.*}}ALLOCA ON HOST\n MemObj : [[FLUSHBUF_MEMOBJ]]
 
 #include <CL/sycl.hpp>
