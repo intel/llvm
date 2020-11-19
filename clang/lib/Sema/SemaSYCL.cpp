e1d8db73c0e751aaa00be68b14b5c274ac79c0b9
smanna12 <52675209+smanna12@users.noreply.github.com>
2020-11-19 04:38:53 -0500

    [SYCL][NFC] Fix static code analysis concerns (#2789)
    Found via a static-analysis tool:
    
    1.	Inside leaveStruct()
    
    RecordDecl *RD = Ty->getAsRecordDecl(); ---> Pointer 'RD' may be NULL
    if (!RD->hasAttr<SYCLRequiresDecompositionAttr>()) ----> will be dereferenced here
    
    2. Inside visitRecord() function, Pointer 'RecordTy->getAsRecordDecl()' returned from 
        call to function 'getAsRecordDecl' at line 
        “if (RecordTy->getAsRecordDecl()->hasAttr<SYCLRequiresDecompositionAttr>()) {“
          may be NULL and will be dereferenced at same line.
    
    
    This patch fixes null pointer dereference issues in SemaSYCL.cpp file.
    
    Signed-off-by: Soumi Manna <soumi.manna@intel.com>

--- clang/lib/Sema/SemaSYCL.cpp
+++ clang/lib/Sema/SemaSYCL.cpp
@@ -1214,7 +1214,9 @@ void KernelObjVisitor::visitRecord(const CXXRecordDecl *Owner, ParentTy &Parent,
                                    const CXXRecordDecl *Wrapper,
                                    QualType RecordTy,
                                    HandlerTys &... Handlers) {
-  if (RecordTy->getAsRecordDecl()->hasAttr<SYCLRequiresDecompositionAttr>()) {
+  RecordDecl *RD = RecordTy->getAsRecordDecl();
+  assert(RD && "should not be null.");
+  if (RD->hasAttr<SYCLRequiresDecompositionAttr>()) {
     // If this container requires decomposition, we have to visit it as
     // 'complex', so all handlers are called in this case with the 'complex'
     // case.
@@ -1583,6 +1585,7 @@ public:
   bool leaveStruct(const CXXRecordDecl *, FieldDecl *, QualType Ty) final {
     if (CollectionStack.pop_back_val()) {
       RecordDecl *RD = Ty->getAsRecordDecl();
+      assert(RD && "should not be null.");
       if (!RD->hasAttr<SYCLRequiresDecompositionAttr>())
         RD->addAttr(SYCLRequiresDecompositionAttr::CreateImplicit(
             SemaRef.getASTContext()));
@@ -1601,6 +1604,7 @@ public:
                    QualType Ty) final {
     if (CollectionStack.pop_back_val()) {
       RecordDecl *RD = Ty->getAsRecordDecl();
+      assert(RD && "should not be null.");
       if (!RD->hasAttr<SYCLRequiresDecompositionAttr>())
         RD->addAttr(SYCLRequiresDecompositionAttr::CreateImplicit(
             SemaRef.getASTContext()));
