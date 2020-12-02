1f39874cfefedef93e7a03b11008e214e688d9a8
srividya.sundaram <srividya.sundaram@intel.com>
2020-12-02 12:18:11 -0800

    Simplified test

--- clang/test/SemaSYCL/warn-constexpr-kernel-arg.cpp
+++ clang/test/SemaSYCL/warn-constexpr-kernel-arg.cpp
@@ -1,5 +1,8 @@
 // RUN: %clang_cc1 -fsycl -fsycl-is-device -internal-isystem %S/Inputs -fsyntax-only -sycl-std=2020 -verify %s
 
+// This test warns users when an ODR-use of a constexpr variable causes the kernel lambda to capture it as a
+// kernel argument
+
 #include "sycl.hpp"
 
 using namespace cl::sycl;
@@ -10,28 +13,14 @@ class LambdaKernel;
 
 int main() {
 
-  constexpr unsigned V = 16;
-  struct Id {
-    int x;
-
-    Id(int x) : x(x) {}
-
-    Id operator*(const unsigned &val) const {
-      return Id(x * val);
-    }
-
-    operator int() const {
-      return x;
-    }
-  };
-
-  Id i{5};
+  constexpr unsigned OdrUsedVar = 10;
 
   q.submit([&](handler &h) {
     // expected-note@+1 {{in instantiation of function template specialization}}
     h.single_task<LambdaKernel>(
         [=]() {
-          int A = i * V; // expected-warning {{constexpr 'V' is captured and is not a compile-time constant}}
+          // constexpr 'OdrUsedVar' is odr-used here.
+          const unsigned *ptr = &OdrUsedVar; // expected-warning {{captured constexpr 'OdrUsedVar' will be a kernel argument in device code}}
         });
   });
   return 0;
