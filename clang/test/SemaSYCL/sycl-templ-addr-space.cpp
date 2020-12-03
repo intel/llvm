113c6b13cf23fc7e90e1025eec9d079d4ff064de
Premanand M Rao <premanand.m.rao@intel.com>
2020-12-03 07:54:22 -0800

    Give variables some meaningful names

--- clang/test/SemaSYCL/sycl-templ-addr-space.cpp
+++ clang/test/SemaSYCL/sycl-templ-addr-space.cpp
@@ -1,54 +1,52 @@
 // RUN: %clang_cc1 -fsycl -fsycl-is-device -triple spir64 -verify %s
 
-// Test that an error is issued instead of an assertion for this test case.
+// Test that the compiler no longer asserts while processing this test case.
 
-template <int b>
-struct c {
-  static constexpr int d = b;
+template <int N>
+struct integral_constant {
+  static constexpr int val = N;
 };
-template <typename ab>
-ab &&l(int);
-template <typename ab>
-auto ac(l<ab>(0));
-template <typename n, typename s>
-class o {
-  template <typename ad, decltype(ac<ad>)>
+template <typename T>
+T &&convert(int);
+template <typename T>
+auto declval(convert<T>(0));
+template <typename Op1, typename Op2>
+class sub_group {
+  template <typename T, decltype(declval<T>)>
   // expected-note@+1 {{possible target for call}}
-  static c<true> p();
+  static integral_constant<true> binary_op();
   // expected-error@+1 {{reference to overloaded function could not be resolved; did you mean to call it?}}
-  decltype(p<n, s>) i;
+  decltype(binary_op<Op1, Op2>) i;
 };
 // expected-note-re@+2 {{in instantiation {{.*}} requested here}}
 template <typename n>
-struct q : o<n, int> {
+struct group : sub_group<n, int> {
 };
-template <typename ab>
-struct r {
-  typedef ab i;
+template <typename T>
+struct wrapper {
+  typedef T val;
 };
-class ah {
+class element_type {
 };
-struct ai {
-  using i = __attribute__((opencl_global)) ah;
+struct Container {
+  using __element_type = __attribute__((opencl_global)) element_type;
 };
-template <bool, class ak, class> using am = ak;
+template <bool, class Base, class> using BaseImpl = Base;
 // expected-note-re@+1 2{{candidate constructor {{.*}} not viable: {{.*}}}}
-template <int> class v {
-  template <class...> struct as;
+template <int> class id_type {
+  template <class...> struct Base;
   // expected-note-re@+1 {{in instantiation {{.*}} requested here}}
-  template <class at> struct as<at> : am<at::d, as<>, at> {};
+  template <class Der> struct Base<Der> : BaseImpl<Der::val, Base<>, Der> {};
   // expected-note-re@+1 {{in instantiation {{.*}} requested here}}
-  template <typename au> using av = r<typename as<q<au>>::d>;
+  template <typename T> using Base2 = wrapper<typename Base<group<T>>::val>;
   // expected-note-re@+3 {{in instantiation {{.*}} requested here}}
   // expected-note-re@+2 {{in instantiation {{.*}} required here}}
   // expected-note-re@+1 {{candidate template ignored: {{.*}}}}
-  template <typename au, typename = av<au>> v(au &);
+  template <typename T, typename = Base2<T>> id_type(T &);
 };
-struct x {
-  v<1> ay() {
-    ai::i *pi;
-    // expected-error@+2 {{no viable conversion from returned value of type 'ai::i' (aka '__global ah') to function return type 'v<1>'}}
-    // expected-note-re@+1 {{while substituting {{.*}}}}
-    return pi[0];
-  }
-};
+id_type<1> get() {
+  Container::__element_type *ElemPtr;
+  // expected-error@+2 {{no viable conversion from returned value of type 'Container::__element_type' (aka '__global element_type') to function return type 'id_type<1>'}}
+  // expected-note-re@+1 {{while substituting {{.*}}}}
+  return ElemPtr[0];
+}
