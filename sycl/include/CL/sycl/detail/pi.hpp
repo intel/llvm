996b49686c06e9e5c97fe4e9a4e7234bedd7f7b5
Zahira Ammarguellat <zahira.ammarguellat@intel.com>
2020-12-03 06:53:49 -0800

    Fix format of pi.hpp.

--- sycl/include/CL/sycl/detail/pi.hpp
+++ sycl/include/CL/sycl/detail/pi.hpp
@@ -176,9 +176,9 @@ template <typename T> inline void print(T val) {
   std::cout << "<unknown> : " << val << std::endl;
 }
 
-template<typename Ret, typename...Args>
-inline void print(Ret(*val)(Args...) ) {
-  std::cout <<"<FuncPtr>" << reinterpret_cast<const void*>(val) << std::endl;
+template <typename Ret, typename... Args>
+inline void print(Ret (*val)(Args...)) {
+  std::cout << "<FuncPtr>" << reinterpret_cast<const void *>(val) << std::endl;
 }
 
 template <> inline void print<>(PiPlatform val) {
