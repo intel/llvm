339dcb3705675ec29bd8216029b3040093c20680
Artur Gainullin <48139823+againull@users.noreply.github.com>
2020-12-09 11:33:59 +0300

    [SYCL] Test long kernel names with the L0 backend (intel/llvm-test-suite#70)

---
+++ SYCL/Regression/long_kernel_name.cpp
@@ -0,0 +1,27 @@
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple -I %sycl_include/.. -lze_loader %s -o %t.out
+// RUN: %GPU_RUN_PLACEHOLDER %t.out
+
+// REQUIRES: level_zero
+
+// Regression test to check that Level Zero backend doesn't fail when using a
+// long kernel name.
+
+#include <CL/sycl.hpp>
+
+int main() {
+  cl::sycl::queue Q;
+  {
+    cl::sycl::program Program(Q.get_context());
+    using SingleTask = class
+        nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn;
+
+    Program.build_with_kernel_type<SingleTask>();
+    cl::sycl::kernel Kernel = Program.get_kernel<SingleTask>();
+
+    Q.submit([&](cl::sycl::handler &CGH) {
+      CGH.single_task<SingleTask>(Kernel, [=]() {});
+    });
+  }
+
+  return 0;
+}
