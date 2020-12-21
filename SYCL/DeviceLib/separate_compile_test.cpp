3dd179ec49bca4978fbe163d16283171e3e1cd57
jinge90 <43599496+jinge90@users.noreply.github.com>
2020-12-22 00:19:43 +0800

    Remove usage of fsycl-device-lib=all/libm-fp64 (intel/llvm-test-suite#74)
    Signed-off-by: gejin <ge.jin@intel.com>

--- SYCL/DeviceLib/separate_compile_test.cpp
+++ SYCL/DeviceLib/separate_compile_test.cpp
@@ -6,7 +6,7 @@
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
 // RUN: %ACC_RUN_PLACEHOLDER %t.out
 
-// RUN: %clangxx -fsycl -fsycl-link -fsycl-device-lib=all %S/std_complex_math_fp64_test.cpp -o %t_fp64_device.o
+// RUN: %clangxx -fsycl -fsycl-link  %S/std_complex_math_fp64_test.cpp -o %t_fp64_device.o
 // RUN: %clangxx -fsycl-device-only -Xclang -fsycl-int-header=std_complex_math_fp64_test_ihdr.h %S/std_complex_math_fp64_test.cpp -I %sycl_include -Wno-sycl-strict
 // >> host compilation...
 // RUN: %clangxx -include std_complex_math_fp64_test_ihdr.h -c %S/std_complex_math_fp64_test.cpp -o %t_fp64_host.o -I %sycl_include -Wno-sycl-strict
