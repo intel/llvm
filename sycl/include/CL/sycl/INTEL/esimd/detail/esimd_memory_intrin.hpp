7f50139018742d4377a340df1e5d553ca17edc27
kychendev <kai.yu.chen@intel.com>
2020-12-24 01:16:05 -0800

    [SYCL][ESIMD] Update ESIMD support for split barrier, slm load and dp4a. (#2917)

--- sycl/include/CL/sycl/INTEL/esimd/detail/esimd_memory_intrin.hpp
+++ sycl/include/CL/sycl/INTEL/esimd/detail/esimd_memory_intrin.hpp
@@ -196,6 +196,9 @@ __esimd_flat_atomic2(sycl::INTEL::gpu::vector_type_t<uint64_t, N> addrs,
 // esimd_barrier, generic group barrier
 SYCL_EXTERNAL void __esimd_barrier();
 
+// generic work-group split barrier
+SYCL_EXTERNAL void __esimd_sbarrier(sycl::INTEL::gpu::EsimdSbarrierType flag);
+
 // slm_fence sets the SLM read/write order
 SYCL_EXTERNAL void __esimd_slm_fence(uint8_t cntl);
 
@@ -771,6 +774,8 @@ __esimd_dp4(sycl::INTEL::gpu::vector_type_t<Ty, N> v1,
 
 /// TODO
 SYCL_EXTERNAL void __esimd_barrier() {}
+
+SYCL_EXTERNAL void __esimd_sbarrier(sycl::INTEL::gpu::EsimdSbarrierType flag) {}
 
 SYCL_EXTERNAL void __esimd_slm_fence(uint8_t cntl) {}
 
