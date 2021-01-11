e49f4f4d9ee733dd1447c53e3ef0213f82296802
Vyacheslav Klochkov <34946562+v-klochkov@users.noreply.github.com>
2021-01-11 05:23:47 -0800

    Temporarily turn off stream/release_resources_test.cpp as flaky on Linux-OpenCL (intel/llvm-test-suite#89)
    Signed-off-by: Vyacheslav N Klochkov <vyacheslav.n.klochkov@intel.com>

--- SYCL/Basic/stream/release_resources_test.cpp
+++ SYCL/Basic/stream/release_resources_test.cpp
@@ -3,6 +3,9 @@
 // RUN: env SYCL_PI_TRACE=2 %GPU_RUN_PLACEHOLDER %t.out %GPU_CHECK_PLACEHOLDER
 // RUN: env SYCL_PI_TRACE=2 %ACC_RUN_PLACEHOLDER %t.out %ACC_CHECK_PLACEHOLDER
 
+// The test is flaky on Linux-OpenCL
+// UNSUPPORTED: linux && opencl
+
 // Check that buffer used by a stream object is released.
 
 #include <CL/sycl.hpp>
