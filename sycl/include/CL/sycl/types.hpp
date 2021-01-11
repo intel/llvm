e00ab98ad4bf93e5b48718a3c592dc2259a74f84
mdimakov <maxim.dimakov@intel.com>
2021-01-11 17:00:51 +0300

    [SYCL] Address review comments
    Signed-off-by: mdimakov <maxim.dimakov@intel.com>

--- sycl/include/CL/sycl/types.hpp
+++ sycl/include/CL/sycl/types.hpp
@@ -1819,6 +1819,13 @@ public:
   }
 
   template <typename asT> asT as() const {
+    static_assert((sizeof(*this) == sizeof(asT)),
+                  "The new SYCL vec type must have the same storage size in "
+                  "bytes as this SYCL swizzled vec");
+    static_assert(
+        detail::is_contained<asT, detail::gtl::vector_basic_list>::value,
+        "asT must be SYCL vec of a different element type and "
+        "number of elements specified by asT");
     // First materialize the swizzle to vec_t and then apply as() to it.
     vec_t Tmp = *this;
     return Tmp.template as<asT>();
