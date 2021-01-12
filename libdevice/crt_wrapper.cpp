2d03bdf93c19359b4be36697a16f56ae7543b235
gejin <ge.jin@intel.com>
2021-01-12 14:05:50 +0800

    Fix typo
    Signed-off-by: gejin <ge.jin@intel.com>

--- libdevice/crt_wrapper.cpp
+++ libdevice/crt_wrapper.cpp
@@ -1,4 +1,4 @@
-//==----- crt_wrapper.cpp - wrappers for Glibc internal functions ----------==//
+//==------ crt_wrapper.cpp - wrappers for libc internal functions ----------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
