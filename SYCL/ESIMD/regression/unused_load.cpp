ce5be08225849ae9cea3a1067ebe9dfceb1ffe7e
kbobrovs <Konstantin.S.Bobrovsky@intel.com>
2021-01-14 21:50:49 -0800

    [ESIMD] Add regression test for unused load. (intel/llvm-test-suite#87)
    Signed-off-by: Konstantin S Bobrovsky <konstantin.s.bobrovsky@intel.com>
    
    Co-authored-by: vladimirlaz <vladimir.lazarev@intel.com>

---
+++ SYCL/ESIMD/regression/unused_load.cpp
@@ -0,0 +1,56 @@
+//==---------- unused_load.cpp  - DPC++ ESIMD on-device test ---------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+// TODO enable on Windows
+// REQUIRES: linux && gpu
+// UNSUPPORTED: cuda
+// RUN: %clangxx-esimd -fsycl -I%S/.. %s -o %t.out
+// RUN: %ESIMD_RUN_PLACEHOLDER %t.out
+
+// This test checks that ESIMD JIT compilation does not crash on unused
+// block_load invocation.
+
+#include <CL/sycl.hpp>
+#include <CL/sycl/INTEL/esimd.hpp>
+
+#include <iostream>
+
+using namespace cl::sycl;
+
+constexpr unsigned int VL = 16;
+
+using Ty = float;
+
+int main() {
+  Ty data0[VL] = {0};
+
+  try {
+    queue q;
+
+    buffer<Ty, 1> buf0(data0, range<1>(VL));
+
+    q.submit([&](handler &cgh) {
+      std::cout << "Running on "
+                << q.get_device().get_info<cl::sycl::info::device::name>()
+                << "\n";
+
+      auto acc0 = buf0.get_access<access::mode::read_write>(cgh);
+
+      cgh.parallel_for<class Test>(
+          range<1>(1), [=](sycl::id<1> i) SYCL_ESIMD_KERNEL {
+            using namespace sycl::INTEL::gpu;
+            simd<Ty, VL> var = block_load<Ty, VL>(acc0, 0);
+          });
+    });
+    q.wait();
+  } catch (cl::sycl::exception const &e) {
+    std::cout << "SYCL exception caught: " << e.what() << '\n';
+    return 1;
+  }
+  std::cout << "Passed\n";
+  return 0;
+}
