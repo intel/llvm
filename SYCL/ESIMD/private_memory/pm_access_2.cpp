6aa4289b3b7f4ea143fcf5f87e2e06cd9e927484
Fedor Veselovskiy <fedor.veselovsky@intel.com>
2021-01-19 10:09:12 +0300

    [SYCL][ESIMD] Add ESIMD tests on TPM (intel/llvm-test-suite#67)

---
+++ SYCL/ESIMD/private_memory/pm_access_2.cpp
@@ -0,0 +1,12 @@
+//==--------------- pm_access_2.cpp - DPC++ ESIMD on-device test ----------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// TODO enable on Windows and Level Zero
+// REQUIRES: linux && gpu && opencl
+// RUN: %clangxx-esimd -fsycl -I%S/.. %S/Inputs/pm_common.cpp -o %t.out
+// RUN: %ESIMD_RUN_PLACEHOLDER %t.out 2
