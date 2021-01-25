2f56de670643c74943c3a778ca043613d38cbac1
sergei <57672082+s-kanaev@users.noreply.github.com>
2021-01-25 09:33:00 +0300

    Disable stream test (intel/llvm-test-suite#108)

--- SYCL/Basic/stream/stream.cpp
+++ SYCL/Basic/stream/stream.cpp
@@ -3,6 +3,8 @@
 // RUN: %CPU_RUN_PLACEHOLDER %t.out %CPU_CHECK_PLACEHOLDER
 // RUN: %GPU_RUN_ON_LINUX_PLACEHOLDER %t.out %GPU_CHECK_ON_LINUX_PLACEHOLDER
 // RUN: %ACC_RUN_PLACEHOLDER %t.out %ACC_CHECK_PLACEHOLDER
+// Disabled until sporadic failure is fixed
+// REQUIRES: TEMPORARY_DISABLED
 
 //==------------------ stream.cpp - SYCL stream basic test -----------------==//
 //
