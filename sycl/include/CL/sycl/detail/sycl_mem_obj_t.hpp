f2a2195e341ea5d934e107e965ffe42488088cbd
mdimakov <maxim.dimakov@intel.com>
2021-01-27 11:28:24 +0300

    [SYCL] Use unique_ptr constructor instead of make_unique
    Signed-off-by: mdimakov <maxim.dimakov@intel.com>

--- sycl/include/CL/sycl/detail/sycl_mem_obj_t.hpp
+++ sycl/include/CL/sycl/detail/sycl_mem_obj_t.hpp
@@ -200,7 +200,7 @@ public:
       // ContiguousStorage. updateHostMemory works only with pointer to
       // continuous data.
       const size_t Size = MSizeInBytes / sizeof(DestinationValueT);
-      auto ContiguousStorage = std::make_unique<bool[]>(Size);
+      std::unique_ptr<bool[]> ContiguousStorage(new bool[Size]);
       updateHostMemory(ContiguousStorage.get());
       std::copy(ContiguousStorage.get(), ContiguousStorage.get() + Size,
                 FinalData);
