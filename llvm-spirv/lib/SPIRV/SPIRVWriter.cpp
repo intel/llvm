b5b19b4dac6cf6745b5c836f4466288d3d0a9e25
Dmitry Sidorov <dmitry.sidorov@intel.com>
2021-02-04 19:10:52 +0300

    Ignore experimental_noalias_scope_decl intrinsic
    Signed-off-by: Dmitry Sidorov <dmitry.sidorov@intel.com>

--- llvm-spirv/lib/SPIRV/SPIRVWriter.cpp
+++ llvm-spirv/lib/SPIRV/SPIRVWriter.cpp
@@ -2133,6 +2133,7 @@ bool LLVMToSPIRV::isKnownIntrinsic(Intrinsic::ID Id) {
   case Intrinsic::ctlz:
   case Intrinsic::cttz:
   case Intrinsic::expect:
+  case Intrinsic::experimental_noalias_scope_decl:
   case Intrinsic::experimental_constrained_fadd:
   case Intrinsic::experimental_constrained_fsub:
   case Intrinsic::experimental_constrained_fmul:
@@ -2748,6 +2749,7 @@ SPIRVValue *LLVMToSPIRV::transIntrinsicInst(IntrinsicInst *II,
     break;
   }
   // We can just ignore/drop some intrinsics, like optimizations hint.
+  case Intrinsic::experimental_noalias_scope_decl:
   case Intrinsic::invariant_start:
   case Intrinsic::invariant_end:
   case Intrinsic::dbg_label:
