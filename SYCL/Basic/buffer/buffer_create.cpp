6c75563795b8b9fddfd3c022cd0f048c1ad7da0b
rdeodhar <rajiv.deodhar@intel.com>
2021-02-05 00:10:14 -0700

    [SYC] Test for integrated/discrete GPU buffer creation. (intel/llvm-test-suite#136)
    Signed-off-by: rdeodhar <rajiv.deodhar@intel.com>

---
+++ SYCL/Basic/buffer/buffer_create.cpp
@@ -0,0 +1,29 @@
+// REQUIRES: gpu,level_zero
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
+// RUN: env ZE_DEBUG=1 %GPU_RUN_PLACEHOLDER %t.out 2> %t1.out; cat %t1.out %GPU_CHECK_PLACEHOLDER
+
+#include <CL/sycl.hpp>
+
+using namespace cl::sycl;
+
+int main() {
+  constexpr int Size = 100;
+  queue Queue;
+  auto D = Queue.get_device();
+
+  buffer<::cl_int, 1> Buffer(Size);
+  Queue.submit([&](handler &cgh) {
+    accessor Accessor{Buffer, cgh, read_write};
+    if (D.get_info<info::device::host_unified_memory>())
+      std::cerr << "Integrated GPU should use zeMemAllocHost\n";
+    else
+      std::cerr << "Discrete GPU should use zeMemAllocDevice\n";
+    cgh.parallel_for<class CreateBuffer>(range<1>(Size), [=](id<1> ID) {});
+  });
+  Queue.wait();
+
+  return 0;
+}
+
+// CHECK: {{Integrated|Discrete}} GPU should use [[API:zeMemAllocHost|zeMemAllocDevice]]
+// CHECK: ZE ---> [[API]](
