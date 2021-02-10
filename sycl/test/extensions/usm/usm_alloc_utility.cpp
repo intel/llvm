8b28aa98b743456af4b734e074ba1f4dfadc2556
Steffen Larsen <steffen.larsen@codeplay.com>
2021-02-10 10:25:58 +0000

    Fixed typo
    Signed-off-by: Steffen Larsen <steffen.larsen@codeplay.com>

--- sycl/test/extensions/usm/usm_alloc_utility.cpp
+++ sycl/test/extensions/usm/usm_alloc_utility.cpp
@@ -26,17 +26,17 @@ int main() {
   if (dev.get_info<info::device::usm_host_allocations>()) {
     array = (int *)malloc_host(N * sizeof(int), q);
     assert((get_pointer_type(array, ctxt) == usm::alloc::host) &&
-           "Allcoation pointer should be host type");
+           "Allocation pointer should be host type");
     assert((get_pointer_device(array, ctxt) == dev) &&
-           "Allcoation pointer should be host type");
+           "Allocation pointer should be host type");
     free(array, ctxt);
 
     array =
         (int *)aligned_alloc_host(alignof(long long), N * sizeof(int), ctxt);
     assert((get_pointer_type(array, ctxt) == usm::alloc::host) &&
-           "Allcoation pointer should be host type");
+           "Allocation pointer should be host type");
     assert((get_pointer_device(array, ctxt) == dev) &&
-           "Allcoation pointer should be host type");
+           "Allocation pointer should be host type");
     free(array, ctxt);
   }
 
@@ -44,18 +44,18 @@ int main() {
     array = (int *)malloc_shared(N * sizeof(int), q);
     // host device treats all allocations as host allocations
     assert((get_pointer_type(array, ctxt) == usm::alloc::host) &&
-           "Allcoation pointer should be host type");
+           "Allocation pointer should be host type");
     assert((get_pointer_device(array, ctxt) == dev) &&
-           "Allcoation pointer should be host type");
+           "Allocation pointer should be host type");
     free(array, ctxt);
 
     array = (int *)aligned_alloc_shared(alignof(long long), N * sizeof(int),
                                         dev, ctxt);
     // host device treats all allocations as host allocations
     assert((get_pointer_type(array, ctxt) == usm::alloc::host) &&
-           "Allcoation pointer should be host type");
+           "Allocation pointer should be host type");
     assert((get_pointer_device(array, ctxt) == dev) &&
-           "Allcoation pointer should be host type");
+           "Allocation pointer should be host type");
     free(array, ctxt);
   }
 
@@ -63,18 +63,18 @@ int main() {
     array = (int *)malloc_device(N * sizeof(int), q);
     // host device treats all allocations as host allocations
     assert((get_pointer_type(array, ctxt) == usm::alloc::host) &&
-           "Allcoation pointer should be host type");
+           "Allocation pointer should be host type");
     assert((get_pointer_device(array, ctxt) == dev) &&
-           "Allcoation pointer should be host type");
+           "Allocation pointer should be host type");
     free(array, ctxt);
 
     array = (int *)aligned_alloc_device(alignof(long long), N * sizeof(int),
                                         dev, ctxt);
     // host device treats all allocations as host allocations
     assert((get_pointer_type(array, ctxt) == usm::alloc::host) &&
-           "Allcoation pointer should be host type");
+           "Allocation pointer should be host type");
     assert((get_pointer_device(array, ctxt) == dev) &&
-           "Allcoation pointer should be host type");
+           "Allocation pointer should be host type");
     free(array, ctxt);
   }
 
