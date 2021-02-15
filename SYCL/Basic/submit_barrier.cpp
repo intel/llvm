50741257741576005a4f9253301d89c40a969d4a
smaslov-intel <48694368+smaslov-intel@users.noreply.github.com>
2021-02-14 23:18:11 -0800

    [SYCL] test for barrier submission: https://github.com/intel/llvm/pul… (intel/llvm-test-suite#98)
    Signed-off-by: Sergey V Maslov <sergey.v.maslov@intel.com>

---
+++ SYCL/Basic/submit_barrier.cpp
@@ -0,0 +1,23 @@
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
+// RUN: %HOST_RUN_PLACEHOLDER %t.out
+// RUN: %CPU_RUN_PLACEHOLDER %t.out
+// RUN: %GPU_RUN_PLACEHOLDER %t.out
+
+// UNSUPPORTED: cuda
+
+#include <CL/sycl.hpp>
+#include <stdlib.h>
+
+int main() {
+
+  cl::sycl::device dev{cl::sycl::default_selector{}};
+  cl::sycl::queue q{dev};
+
+  q.submit(
+      [&](sycl::handler &cgh) { cgh.single_task<class kernel1>([]() {}); });
+
+  cl::sycl::event e = q.submit_barrier();
+  e.wait_and_throw();
+
+  return 0;
+}
