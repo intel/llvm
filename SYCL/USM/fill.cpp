ad6d39a776eee9ca163beac587af4773ac2baf1d
Steffen Larsen <56076654+steffenlarsen@users.noreply.github.com>
2021-02-18 05:52:45 +0000

    [SYCL] Extend coverage of USM memcpy, fill, and memset (intel/llvm-test-suite#144)
    Extends the coverage of the USM operations memcpy, fill, and memset by
    testing for both regular and aligned allocations for the host, shared,
    and device allocation types.
    
    This also migrates the USM allocator container test.
    
    Signed-off-by: Steffen Larsen <steffen.larsen@codeplay.com>

--- SYCL/USM/fill.cpp
+++ SYCL/USM/fill.cpp
@@ -16,24 +16,149 @@
 
 using namespace cl::sycl;
 
-constexpr int count = 100;
-constexpr int pattern = 42;
+template <typename T> class usm_device_transfer;
+template <typename T> class usm_aligned_device_transfer;
+
+static constexpr int N = 100;
+
+struct test_struct {
+  short a;
+  int b;
+  long c;
+  long long d;
+  sycl::half e;
+  float f;
+  double g;
+};
+
+bool operator==(const test_struct &lhs, const test_struct &rhs) {
+  return lhs.a == rhs.a && lhs.b == rhs.b && lhs.c == rhs.c && lhs.d == rhs.d &&
+         lhs.e == rhs.e && lhs.f == rhs.f && lhs.g == rhs.g;
+}
+
+template <typename T>
+void runHostTests(device dev, context ctxt, queue q, T val) {
+  T *array;
+
+  array = (T *)malloc_host(N * sizeof(T), q);
+  q.submit([&](handler &h) { h.fill(array, val, N); }).wait();
+  for (int i = 0; i < N; ++i) {
+    assert(array[i] == val);
+  }
+  free(array, ctxt);
+
+  array = (T *)aligned_alloc_host(alignof(long long), N * sizeof(T), ctxt);
+  q.submit([&](handler &h) { h.fill(array, val, N); }).wait();
+  for (int i = 0; i < N; ++i) {
+    assert(array[i] == val);
+  }
+  free(array, ctxt);
+}
+
+template <typename T>
+void runSharedTests(device dev, context ctxt, queue q, T val) {
+  T *array;
+
+  array = (T *)malloc_shared(N * sizeof(T), q);
+  q.submit([&](handler &h) { h.fill(array, val, N); }).wait();
+  for (int i = 0; i < N; ++i) {
+    assert(array[i] == val);
+  }
+  free(array, ctxt);
+
+  array =
+      (T *)aligned_alloc_shared(alignof(long long), N * sizeof(T), dev, ctxt);
+  q.submit([&](handler &h) { h.fill(array, val, N); }).wait();
+  for (int i = 0; i < N; ++i) {
+    assert(array[i] == val);
+  }
+  free(array, ctxt);
+}
+
+template <typename T>
+void runDeviceTests(device dev, context ctxt, queue q, T val) {
+  T *array;
+  std::vector<T> out;
+  out.resize(N);
+
+  array = (T *)malloc_device(N * sizeof(T), q);
+  q.submit([&](handler &h) { h.fill(array, val, N); }).wait();
+
+  {
+    buffer<T, 1> buf{&out[0], range<1>{N}};
+    q.submit([&](handler &h) {
+       auto acc = buf.template get_access<access::mode::write>(h);
+       h.parallel_for<usm_device_transfer<T>>(
+           range<1>(N), [=](id<1> item) { acc[item] = array[item]; });
+     }).wait();
+  }
+
+  for (int i = 0; i < N; ++i) {
+    assert(out[i] == val);
+  }
+  free(array, ctxt);
+
+  out.clear();
+  out.resize(N);
+
+  array =
+      (T *)aligned_alloc_device(alignof(long long), N * sizeof(T), dev, ctxt);
+  q.submit([&](handler &h) { h.fill(array, val, N); }).wait();
+
+  {
+    buffer<T, 1> buf{&out[0], range<1>{N}};
+    q.submit([&](handler &h) {
+       auto acc = buf.template get_access<access::mode::write>(h);
+       h.parallel_for<usm_aligned_device_transfer<T>>(
+           range<1>(N), [=](id<1> item) { acc[item] = array[item]; });
+     }).wait();
+  }
+
+  for (int i = 0; i < N; ++i) {
+    assert(out[i] == val);
+  }
+  free(array, ctxt);
+}
 
 int main() {
   queue q;
-  if (q.get_device().get_info<info::device::usm_shared_allocations>()) {
-    int *mem = malloc_shared<int>(count, q);
+  auto dev = q.get_device();
+  auto ctxt = q.get_context();
 
-    for (int i = 0; i < count; i++)
-      mem[i] = 0;
+  test_struct test_obj{4, 42, 424, 4242, 4.2f, 4.242, 4.24242};
 
-    q.fill(mem, pattern, count);
-    q.wait();
-
-    for (int i = 0; i < count; i++) {
-      assert(mem[i] == pattern);
-    }
+  if (dev.get_info<info::device::usm_host_allocations>()) {
+    runHostTests<short>(dev, ctxt, q, 4);
+    runHostTests<int>(dev, ctxt, q, 42);
+    runHostTests<long>(dev, ctxt, q, 424);
+    runHostTests<long long>(dev, ctxt, q, 4242);
+    runHostTests<sycl::half>(dev, ctxt, q, sycl::half(4.2f));
+    runHostTests<float>(dev, ctxt, q, 4.242f);
+    runHostTests<double>(dev, ctxt, q, 4.24242);
+    runHostTests<test_struct>(dev, ctxt, q, test_obj);
   }
-  std::cout << "Passed\n";
+
+  if (dev.get_info<info::device::usm_shared_allocations>()) {
+    runSharedTests<short>(dev, ctxt, q, 4);
+    runSharedTests<int>(dev, ctxt, q, 42);
+    runSharedTests<long>(dev, ctxt, q, 424);
+    runSharedTests<long long>(dev, ctxt, q, 4242);
+    runSharedTests<sycl::half>(dev, ctxt, q, sycl::half(4.2f));
+    runSharedTests<float>(dev, ctxt, q, 4.242f);
+    runSharedTests<double>(dev, ctxt, q, 4.24242);
+    runSharedTests<test_struct>(dev, ctxt, q, test_obj);
+  }
+
+  if (dev.get_info<info::device::usm_device_allocations>()) {
+    runDeviceTests<short>(dev, ctxt, q, 4);
+    runDeviceTests<int>(dev, ctxt, q, 42);
+    runDeviceTests<long>(dev, ctxt, q, 420);
+    runDeviceTests<long long>(dev, ctxt, q, 4242);
+    runDeviceTests<sycl::half>(dev, ctxt, q, sycl::half(4.2f));
+    runDeviceTests<float>(dev, ctxt, q, 4.242f);
+    runDeviceTests<double>(dev, ctxt, q, 4.24242);
+    runDeviceTests<test_struct>(dev, ctxt, q, test_obj);
+  }
+
   return 0;
 }
--- SYCL/USM/memset.cpp
+++ SYCL/USM/memset.cpp
@@ -15,47 +15,126 @@
 
 using namespace cl::sycl;
 
-static constexpr int count = 100;
+static constexpr int N = 100;
+static constexpr int VAL = 3;
 
 int main() {
   queue q([](exception_list el) {
     for (auto &e : el)
       std::rethrow_exception(e);
   });
-  if (q.get_device().get_info<info::device::usm_shared_allocations>()) {
-    uint32_t *src = (uint32_t *)malloc_shared(sizeof(uint32_t) * count,
-                                              q.get_device(), q.get_context());
+  auto dev = q.get_device();
+  auto ctxt = q.get_context();
+  char *array;
 
-    event init_copy = q.submit(
-        [&](handler &cgh) { cgh.memset(src, 0x15, sizeof(uint32_t) * count); });
-
-    q.submit([&](handler &cgh) {
-      cgh.depends_on(init_copy);
-      cgh.single_task<class double_dest>([=]() {
-        for (int i = 0; i < count; i++)
-          src[i] *= 2;
-      });
-    });
-    q.wait_and_throw();
-
-    for (int i = 0; i < count; i++) {
-      assert(src[i] == 0x2a2a2a2a);
+  if (dev.get_info<info::device::usm_host_allocations>()) {
+    // Test memset on host
+    array = (char *)malloc_host(N * sizeof(char), q);
+    q.submit([&](handler &h) {
+       h.memset(array, VAL, N * sizeof(char));
+     }).wait();
+    for (int i = 0; i < N; ++i) {
+      assert(array[i] == VAL);
     }
+    free(array, ctxt);
 
-    try {
-      // Filling to nullptr should throw.
-      q.submit([&](handler &cgh) {
-        cgh.memset(nullptr, 0, sizeof(uint32_t) * count);
+    // Test memset on aligned host
+    array =
+        (char *)aligned_alloc_host(alignof(long long), N * sizeof(char), ctxt);
+    q.submit([&](handler &h) {
+       h.memset(array, VAL, N * sizeof(char));
+     }).wait();
+    for (int i = 0; i < N; ++i) {
+      assert(array[i] == VAL);
+    }
+    free(array, ctxt);
+  }
+
+  if (dev.get_info<info::device::usm_shared_allocations>()) {
+    // Test memset on shared
+    array = (char *)malloc_shared(N * sizeof(char), q);
+    q.submit([&](handler &h) {
+       h.memset(array, VAL, N * sizeof(char));
+     }).wait();
+    for (int i = 0; i < N; ++i) {
+      assert(array[i] == VAL);
+    }
+    free(array, ctxt);
+
+    // Test memset on aligned shared
+    array = (char *)aligned_alloc_shared(alignof(long long), N * sizeof(char),
+                                         dev, ctxt);
+    q.submit([&](handler &h) {
+       h.memset(array, VAL, N * sizeof(char));
+     }).wait();
+    for (int i = 0; i < N; ++i) {
+      assert(array[i] == VAL);
+    }
+    free(array, ctxt);
+  }
+
+  if (dev.get_info<info::device::usm_device_allocations>()) {
+    std::vector<char> out;
+    out.resize(N);
+
+    // Test memset on device
+    array = (char *)malloc_device(N * sizeof(char), q);
+    q.submit([&](handler &h) {
+       h.memset(array, VAL, N * sizeof(char));
+     }).wait();
+
+    {
+      buffer<char, 1> buf{&out[0], range<1>{N}};
+      q.submit([&](handler &h) {
+        auto acc = buf.template get_access<access::mode::write>(h);
+        h.parallel_for<class usm_device_transfer>(
+            range<1>(N), [=](id<1> item) { acc[item] = array[item]; });
       });
       q.wait_and_throw();
-      assert(false && "Expected error from writing to nullptr");
-    } catch (runtime_error e) {
     }
 
-    // Filling to nullptr is skipped if the number of bytes to fill is 0.
-    q.submit([&](handler &cgh) { cgh.memset(nullptr, 0, 0); });
-    q.wait_and_throw();
+    for (int i = 0; i < N; ++i) {
+      assert(out[i] == VAL);
+    }
+    free(array, ctxt);
+
+    out.clear();
+    out.resize(N);
+
+    // Test memset on aligned device
+    array = (char *)aligned_alloc_device(alignof(long long), N * sizeof(char),
+                                         dev, ctxt);
+    q.submit([&](handler &h) {
+       h.memset(array, VAL, N * sizeof(char));
+     }).wait();
+
+    {
+      buffer<char, 1> buf{&out[0], range<1>{N}};
+      q.submit([&](handler &h) {
+        auto acc = buf.template get_access<access::mode::write>(h);
+        h.parallel_for<class usm_aligned_device_transfer>(
+            range<1>(N), [=](id<1> item) { acc[item] = array[item]; });
+      });
+      q.wait_and_throw();
+    }
+
+    for (int i = 0; i < N; ++i) {
+      assert(out[i] == VAL);
+    }
+    free(array, ctxt);
   }
-  std::cout << "Passed\n";
+
+  try {
+    // Filling to nullptr should throw.
+    q.submit([&](handler &cgh) { cgh.memset(nullptr, 0, N * sizeof(char)); });
+    q.wait_and_throw();
+    assert(false && "Expected error from writing to nullptr");
+  } catch (runtime_error e) {
+  }
+
+  // Filling to nullptr is skipped if the number of bytes to fill is 0.
+  q.submit([&](handler &cgh) { cgh.memset(nullptr, 0, 0); });
+  q.wait_and_throw();
+
   return 0;
 }
