ee590a9429cf0519e226b2dc0b35509fa1f71f3b
Sergey Semenov <sergey.semenov@intel.com>
2021-02-18 12:30:05 +0300

    [SYCL] Add support for group_local_memory_for_overwrite to headers
    Header part of the implementation of group_local_memory_for_overwrite
    from SYCL_INTEL_local_memory extension. Declares and uses
    __sycl_allocate_local_memory function the calls to which will then be
    transformed by a function pass.

---
+++ sycl/include/CL/sycl/group_local_memory.hpp
@@ -0,0 +1,46 @@
+//==----- group_local_memory.hpp --- SYCL group local memory extension -----==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+#pragma once
+
+#include <CL/sycl/detail/defines_elementary.hpp>
+#include <CL/sycl/detail/type_traits.hpp>
+#include <CL/sycl/multi_ptr.hpp>
+
+#include <cstddef>
+#include <cstdint>
+#include <type_traits>
+
+// Requests a fixed-size allocation in local address space at kernel scope.
+// This function is not implementable in SYCL headers and is left without
+// definition. Calls to it will be transformed by a pass during compilation.
+#ifdef __SYCL_DEVICE_ONLY__
+extern "C" SYCL_EXTERNAL __attribute__((opencl_local)) std::uint8_t *
+__sycl_allocate_local_memory(std::size_t Size, std::size_t Alignment);
+#endif
+
+__SYCL_INLINE_NAMESPACE(cl) {
+namespace sycl {
+
+template <typename T, typename Group>
+std::enable_if_t<std::is_trivially_destructible<T>::value &&
+                     detail::is_group<Group>::value,
+                 multi_ptr<T, access::address_space::local_space>>
+    __SYCL_ALWAYS_INLINE group_local_memory_for_overwrite(Group g) {
+#ifdef __SYCL_DEVICE_ONLY__
+  __attribute__((opencl_local)) std::uint8_t *AllocatedMem =
+      __sycl_allocate_local_memory(sizeof(T), alignof(T));
+  return reinterpret_cast<__attribute__((opencl_local)) T *>(AllocatedMem);
+#else
+  throw feature_not_supported(
+      "SYCL_INTEL_local_memory extension is not supported on host device",
+      PI_INVALID_OPERATION);
+#endif
+}
+
+} // namespace sycl
+} // __SYCL_INLINE_NAMESPACE(cl)
