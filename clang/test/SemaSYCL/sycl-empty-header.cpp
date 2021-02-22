68c4ba7e651f0dd6e893e19205273ec4a9d65033
Premanand M Rao <premanand.m.rao@intel.com>
2021-02-22 13:00:47 -0800

    Fix test

--- clang/test/SemaSYCL/sycl-empty-header.cpp
+++ clang/test/SemaSYCL/sycl-empty-header.cpp
@@ -5,12 +5,8 @@
 // RUN: %clang_cc1 -fsycl -fsycl-is-device -fsycl-int-header=%t.h -save-temps=cwd %s
 // RUN: ls %t.h
 // RUN: rm -f %t.h
-// RUN: cp %s %t_dir/foo.cpp
-// RUN: %clang_cc1 -fsycl -fsycl-is-device -fsycl-int-header=%t.h -save-temps=cwd %t_dir/foo.cpp
-// RUN: ls %t.h
-// RUN: rm -rf %t.h %t_dir
 // RUN: touch %t.fail.h
 // RUN: chmod 400 %t.fail.h
 // RUN: %clang_cc1 -fsycl -fsycl-is-device -fsycl-int-header=%t.fail.h %s 2>&1 | FileCheck %s --check-prefix=SYCL-BADFILE
-// RUN: rm %t.fail.h
+// RUN: rm -f %t.fail.h
 // SYCL-BADFILE: Error: {{[Pp]ermission}} denied when opening {{.*.fail.h}}
