0b9306c5111c52910fdd001c115cf61fc5eb3235
Michael D Toguchi <michael.d.toguchi@intel.com>
2021-02-23 07:21:10 -0800

    [NFC] address review comments

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -3728,6 +3728,20 @@ class OffloadingActionBuilder final {
     getDeviceDependences(OffloadAction::DeviceDependences &DA,
                          phases::ID CurPhase, phases::ID FinalPhase,
                          PhasesTy &Phases) override {
+      if (CurPhase == phases::Preprocess) {
+        // Do not perform the host compilation when doing preprocessing only
+        // with -fsycl-device-only.
+        bool IsPreprocessOnly =
+            Args.getLastArg(options::OPT_E) ||
+            Args.getLastArg(options::OPT__SLASH_EP, options::OPT__SLASH_P) ||
+            Args.getLastArg(options::OPT_M, options::OPT_MM);
+        if (Args.hasArg(options::OPT_fsycl_device_only) && IsPreprocessOnly) {
+          for (Action *&A : SYCLDeviceActions)
+            A = C.getDriver().ConstructPhaseAction(C, Args, CurPhase, A,
+                                                   AssociatedOffloadKind);
+          return ABRT_Ignore_Host;
+        }
+      }
 
       // FIXME: This adds the integration header generation pass before the
       // Host compilation pass so the Host can use the header generated.  This
@@ -3764,20 +3778,6 @@ class OffloadingActionBuilder final {
         // action.
         DeviceCompilerInput = nullptr;
         return SYCLDeviceOnly ? ABRT_Ignore_Host : ABRT_Success;
-      }
-
-      if (CurPhase == phases::Preprocess) {
-        // Do not perform the host compilation when doing preprocessing only
-        // with -fsycl-device-only.
-        if (Args.hasArg(options::OPT_fsycl_device_only) &&
-            (Args.getLastArg(options::OPT__SLASH_EP, options::OPT__SLASH_P) ||
-             Args.getLastArg(options::OPT_E) ||
-             Args.getLastArg(options::OPT_M, options::OPT_MM))) {
-          for (Action *&A : SYCLDeviceActions)
-            A = C.getDriver().ConstructPhaseAction(C, Args, CurPhase, A,
-                                                   AssociatedOffloadKind);
-          return ABRT_Ignore_Host;
-        }
       }
 
       // Backend/Assemble actions are obsolete for the SYCL device side
