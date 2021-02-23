1a75787b7b19bdc0555e57680d1194d39a71de04
Soumi Manna <soumi.manna@intel.com>
2021-02-23 09:05:47 -0800

    Fix errors
    Signed-off-by: Soumi Manna <soumi.manna@intel.com>

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -10219,18 +10219,14 @@ public:
   SYCLIntelNumSimdWorkItemsAttr *
   MergeSYCLIntelNumSimdWorkItemsAttr(Decl *D,
                                      const SYCLIntelNumSimdWorkItemsAttr &A);
-  void AddIntelFPGAPrivateCopiesAttr(Decl *D,
-                                     const AttributeCommonInfo &CI,
+  void AddIntelFPGAPrivateCopiesAttr(Decl *D, const AttributeCommonInfo &CI,
                                      Expr *E);
   IntelFPGAPrivateCopiesAttr *
-  MergeIntelFPGAPrivateCopiesAttr(Decl *D,
-                                  const IntelFPGAPrivateCopiesAttr &A);
-  void AddIntelFPGAMaxReplicatesAttr(Decl *D,
-                                     const AttributeCommonInfo &CI,
+  MergeIntelFPGAPrivateCopiesAttr(Decl *D, const IntelFPGAPrivateCopiesAttr &A);
+  void AddIntelFPGAMaxReplicatesAttr(Decl *D, const AttributeCommonInfo &CI,
                                      Expr *E);
   IntelFPGAMaxReplicatesAttr *
-  MergeIntelFPGAMaxReplicatesAttr(Decl *D,
-                                  const IntelFPGAMaxReplicatesAttr &A);
+  MergeIntelFPGAMaxReplicatesAttr(Decl *D, const IntelFPGAMaxReplicatesAttr &A);
 
   /// AddAlignedAttr - Adds an aligned attribute to a particular declaration.
   void AddAlignedAttr(Decl *D, const AttributeCommonInfo &CI, Expr *E,
