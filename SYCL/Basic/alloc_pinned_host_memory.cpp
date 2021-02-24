cd6888b10bf027897dabb56c81c91741d9a77fba
Alexander Batashev <alexbatashev@outlook.com>
2021-02-24 08:11:08 +0300

    [SYCL] Enable Basic/alloc_pinned_host_memory for CUDA devices (intel/llvm-test-suite#149)

--- SYCL/Basic/alloc_pinned_host_memory.cpp
+++ SYCL/Basic/alloc_pinned_host_memory.cpp
@@ -1,7 +1,7 @@
-// REQUIRES: level_zero
+// REQUIRES: level_zero || cuda
 
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t2.out
-// RUN: env SYCL_PI_TRACE=2 ZE_DEBUG=1 %GPU_RUN_PLACEHOLDER %t2.out 2>&1 %GPU_CHECK_PLACEHOLDER
+// RUN: env SYCL_PI_TRACE=2 ZE_DEBUG=1 %GPU_RUN_PLACEHOLDER %t2.out 2>&1 %GPU_L0_CHECK_PLACEHOLDER
 // RUN: %HOST_RUN_PLACEHOLDER %t2.out
 // RUN: %CPU_RUN_PLACEHOLDER %t2.out
 // RUN: %GPU_RUN_PLACEHOLDER %t2.out
