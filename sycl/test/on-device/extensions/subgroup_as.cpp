1ca5d64c64ab74e7c1c0003632ea37339a4683dd
Vladimir Lazarev <vladimir.lazarev@intel.com>
2021-02-24 12:01:07 +0300

    [SYCL] Add test

---
+++ sycl/test/on-device/extensions/subgroup_as.cpp
@@ -0,0 +1,84 @@
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple -DSYCL_USE_DECORATED_REF %s -o %t_dr.out
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
+// Sub-groups are not suported on Host
+// RUN: %GPU_RUN_PLACEHOLDER %t.out
+// RUN: %GPU_RUN_PLACEHOLDER %t_dr.out
+// Execution on CPU and FPGA takes 10000 times longer
+// RUNx: %CPU_RUN_PLACEHOLDER %t.out
+// RUNx: %ACC_RUN_PLACEHOLDER %t.out
+
+#include <CL/sycl.hpp>
+#include <cassert>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+
+int main(int argc, char *argv[]) {
+  cl::sycl::queue queue;
+  printf("Device Name = %s\n",
+         queue.get_device().get_info<cl::sycl::info::device::name>().c_str());
+
+  // Initialize some host memory
+  constexpr int N = 64;
+  int host_mem[N];
+  for (int i = 0; i < N; ++i) {
+    host_mem[i] = i*10000;
+  }
+
+  // Use the device to transform each value
+  {
+    cl::sycl::buffer<int, 1> buf(host_mem, N);
+    queue.submit([&](cl::sycl::handler &cgh) {
+      auto global =
+          buf.get_access<cl::sycl::access::mode::read_write,
+                         cl::sycl::access::target::global_buffer>(cgh);
+      sycl::accessor<int, 1, sycl::access::mode::read_write,
+                     sycl::access::target::local>
+          local(N, cgh);
+
+      cgh.parallel_for<class test>(
+          cl::sycl::nd_range<1>(N, 32), [=](cl::sycl::nd_item<1> it) {
+            int v[N] = {0,1,2,3,4,5,6,7,8,9,
+	                10,11,12,13,14,15,16,17,18,19,
+	                20,21,22,23,24,25,26,27,28,29,
+			30,31,32,33,34,35,36,37,38,39,
+			40,41,42,43,44,45,46,47,48,49,
+			50,51,52,53,54,55,56,57,58,59,
+			60,61,62,63};
+            cl::sycl::ONEAPI::sub_group sg = it.get_sub_group();
+            if (!it.get_local_id(0)) {
+	      int end = it.get_global_id(0)+it.get_local_range()[0];
+              for (int i = it.get_global_id(0); i < end; i++) {
+                local[i] = i * 100;
+              }
+            }
+            it.barrier();
+
+            int i = (it.get_global_id(0) / sg.get_max_local_range()[0]) *
+                    sg.get_max_local_range()[0];
+            // Global address space
+            auto x = sg.load(&global[i]);
+
+            // Local address space
+            auto y = sg.load(&local[i]);
+
+#if SYCL_USE_DECORATED_REF
+            int z = v[it.get_global_id(0)];
+#else
+	    auto z = sg.load(v+i);
+#endif
+            sg.store(&global[i], x + y + z);
+          });
+    });
+  }
+
+  // Print results and tidy up
+  for (int i = 0; i < N; ++i) {
+    if(i*10101 != host_mem[i]) {
+     printf("Unexpected result %06d  vs %06d\n", i*10101, host_mem[i]);
+     return 1;
+    }
+  }
+  printf("Success!\n");
+  return 0;
+}
