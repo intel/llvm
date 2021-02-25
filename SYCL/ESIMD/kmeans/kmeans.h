06cb3aec2ff59f09f0f5dd878a8afe6cf7fb0b99
Gang Chen <gang.y.chen@intel.com>
2021-02-24 21:20:46 -0800

    [SYCL][ESIMD] improved kmean (intel/llvm-test-suite#154)
    We changed the data-layout to use block-read to get better performance on GPU.
    So kernel is basically rewritten. also changed the input to use csv file
    
    Signed-off-by: Gang Y Chen <gang.y.chen@intel.com>

--- SYCL/ESIMD/kmeans/kmeans.h
+++ SYCL/ESIMD/kmeans/kmeans.h
@@ -9,17 +9,58 @@
 #ifndef KMEANS_H_INCLUDED
 #define KMEANS_H_INCLUDED
 
-#define NUM_POINTS 98304 * 8
-
-#define POINTS_PER_THREAD 256
-
-// can handle up to 160 centroids without spilling
-#define NUM_CENTROIDS 20
-
+static constexpr unsigned int NUM_POINTS = 98304 * 8;
+static constexpr unsigned int POINTS_PER_THREAD = 256;
+static constexpr unsigned int POINTS_PER_WORKITEM = 64;
+static constexpr unsigned int NUM_CENTROIDS_ACTUAL = 20;
 // round to next 16 so as to handle reading centroids easily
 // read 16 centroids at one time
-#define ROUND_TO_16_NUM_CENTROIDS (((NUM_CENTROIDS - 1) / 16 + 1) * 16)
+static constexpr unsigned int NUM_CENTROIDS_ALLOCATED =
+    ((NUM_CENTROIDS_ACTUAL + 15) / 16) * 16;
+static constexpr unsigned int SIMD_SIZE = 16;
+static constexpr unsigned int NUM_ITERATIONS = 21;
 
-#define ACCUM_REDUCTION_RATIO 1
+typedef struct {
+  float x;
+  float y;
+  int cluster;
+} Point;
+
+typedef struct {
+  float x;
+  float y;
+  int num_points;
+} Centroid;
+
+typedef struct {
+  float x_sum;
+  float y_sum;
+  int num_points;
+} Accum;
+
+// AOSOA (array of structure of arrays)
+typedef union {
+  float xyn[SIMD_SIZE + SIMD_SIZE + SIMD_SIZE];
+  struct {
+    float x[SIMD_SIZE];
+    float y[SIMD_SIZE];
+    int cluster[SIMD_SIZE];
+  };
+} Point4;
+
+typedef union {
+  float xyn[SIMD_SIZE + SIMD_SIZE + SIMD_SIZE];
+  struct {
+    float x[SIMD_SIZE];
+    float y[SIMD_SIZE];
+    int num_points[SIMD_SIZE];
+  };
+} Centroid4;
+
+typedef struct {
+  float x_sum[NUM_POINTS / POINTS_PER_THREAD];
+  float y_sum[NUM_POINTS / POINTS_PER_THREAD];
+  int num_points[NUM_POINTS / POINTS_PER_THREAD];
+} Accum4;
 
 #endif // KMEANS_H_INCLUDED
