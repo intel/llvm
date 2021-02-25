068a1f1d7c69dc335ec4c0c2c7f42469ffc38784
Sergey V Maslov <sergey.v.maslov@intel.com>
2021-02-24 16:05:29 -0800

    [SYCL] keep old version until can break ABI
    Signed-off-by: Sergey V Maslov <sergey.v.maslov@intel.com>

--- sycl/source/backend/level_zero.cpp
+++ sycl/source/backend/level_zero.cpp
@@ -47,6 +47,24 @@ __SYCL_EXPORT device make_device(const platform &Platform,
 
 //----------------------------------------------------------------------------
 // Implementation of level_zero::make<context>
+// TODO: remove this version (without ownership) when allowed to break ABI.
+__SYCL_EXPORT context make_context(const vector_class<device> &DeviceList,
+                                   pi_native_handle NativeHandle) {
+  const auto &Plugin = pi::getPlugin<backend::level_zero>();
+  // Create PI context first.
+  pi_context PiContext;
+  vector_class<pi_device> DeviceHandles;
+  for (auto Dev : DeviceList) {
+    DeviceHandles.push_back(detail::getSyclObjImpl(Dev)->getHandleRef());
+  }
+  Plugin.call<PiApiKind::piextContextCreateWithNativeHandle>(
+      NativeHandle, DeviceHandles.size(), DeviceHandles.data(), false,
+      &PiContext);
+  // Construct the SYCL context from PI context.
+  return detail::createSyclObjFromImpl<context>(
+      std::make_shared<context_impl>(PiContext, async_handler{}, Plugin));
+}
+
 __SYCL_EXPORT context make_context(const vector_class<device> &DeviceList,
                                    pi_native_handle NativeHandle,
                                    bool KeepOwnership) {
