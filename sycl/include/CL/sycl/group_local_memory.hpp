cdd809b649912d4fae7eb7a2cb731567e03f250f
Sergey Semenov <sergey.semenov@intel.com>
2021-03-01 15:02:25 +0300

    Add group_local_memory implementation

--- sycl/include/CL/sycl/group_local_memory.hpp
+++ sycl/include/CL/sycl/group_local_memory.hpp
@@ -35,5 +35,27 @@ std::enable_if_t<std::is_trivially_destructible<T>::value &&
 #endif
 }
 
+template <typename T, typename Group, typename... Args>
+std::enable_if_t<std::is_trivially_destructible<T>::value &&
+                     detail::is_group<Group>::value,
+                 multi_ptr<T, access::address_space::local_space>>
+    __SYCL_ALWAYS_INLINE group_local_memory(Group g, Args &&...args) {
+  (void)g;
+#ifdef __SYCL_DEVICE_ONLY__
+  __attribute__((opencl_local)) std::uint8_t *AllocatedMem =
+      __sycl_allocateLocalMemory(sizeof(T), alignof(T));
+
+  // TODO switch to using group::get_local_linear_id here once it's implemented
+  id<3> Id = __spirv::initLocalInvocationId<3, id<3>>();
+  if (Id == id<3>(0, 0, 0))
+    new (AllocatedMem) T(args...);
+  detail::workGroupBarrier();
+  return reinterpret_cast<__attribute__((opencl_local)) T *>(AllocatedMem);
+#else
+  throw feature_not_supported(
+      "SYCL_INTEL_local_memory extension is not supported on host device",
+      PI_INVALID_OPERATION);
+#endif
+}
 } // namespace sycl
 } // __SYCL_INLINE_NAMESPACE(cl)
