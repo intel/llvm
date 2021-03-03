573670bb056225163a16d04dfc649558e34db78e
Dmitry Sidorov <dmitry.sidorov@intel.com>
2021-03-03 23:30:51 +0300

    Test fix
    Signed-off-by: Dmitry Sidorov <dmitry.sidorov@intel.com>

--- clang/test/Driver/sycl-instrumentation.cpp
+++ clang/test/Driver/sycl-instrumentation.cpp
@@ -1,10 +1,10 @@
 /// Check that instrumentation is disabled by default:
-// RUN: %clang -### %s 2>&1 \
+// RUN: %clang -fsycl -### %s 2>&1 \
 // RUN: | FileCheck -check-prefix=CHECK-DEFAULT %s
 // CHECK-DEFAULT-NOT: "-add-instrumentation-calls"
 
 /// Check "fsycl_device_code_add_instrumentation_calls" is passed to sycl post
 /// link tool:
-// RUN: %clang -### -fsycl -fsycl-device-code-add-instrumentation-calls %s 2>&1 \
+// RUN: %clang -fsycl -### -fsycl-device-code-add-instrumentation-calls %s 2>&1 \
 // RUN: | FileCheck -check-prefix=CHECK-ENABLED %s
 // CHECK-ENABLED: sycl-post-link{{.*}}"-add-instrumentation-calls"
