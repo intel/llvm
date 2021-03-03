9bc6e8638f47b28bbae5571177732196f940a012
Vyacheslav N Klochkov <vyacheslav.n.klochkov@intel.com>
2021-03-02 21:52:42 -0800

    [SYCL] Implement SYCL-2020 reductions with read_write to reduction variable
    LIT tests: https://github.com/intel/llvm-test-suite/pull/170
    
    This patch:
    - adds SYCL-2020 property::reduction::initialize_to_identity
    - implements support for SYCL-2020 reductions for which initialize_to_identity
      is NOT used (corresponds to read_write and USM ONEAPI::reductions).
      sycl::reduction re-uses ONEAPI::reduction implementation/classes and
      automatically creates placeholder accessors for sycl::reduction called
      with sycl::buffer argument.
    - adds operator++ for reducer class defined in SYCL-2020
    - fixes 2 errors in an ONEAPI::reduction used with placeholder accessors.
    
    The attribute "initialize_to_identity" is NOT supported yet.
    The multi-dimensional reductions are NOT supported yet.
    Reductions accepting sycl::span are not supported yet.
    Reductions cannot be yet used in parallel_for() accepting sycl::range
    
    Signed-off-by: Vyacheslav N Klochkov <vyacheslav.n.klochkov@intel.com>

--- sycl/test/basic_tests/reduction_ctor.cpp
+++ sycl/test/basic_tests/reduction_ctor.cpp
@@ -49,14 +49,24 @@ void testKnown(T Identity, BinaryOperation BOp, T A, T B) {
   Q.submit([&](handler &CGH) {
     // Reduction needs a global_buffer accessor as a parameter.
     // This accessor is not really used in this test.
+    accessor<T, Dim, access::mode::read_write, access::target::global_buffer>
+        ReduRWAcc(ReduBuf, CGH);
     accessor<T, Dim, access::mode::discard_write, access::target::global_buffer>
-        ReduAcc(ReduBuf, CGH);
-    auto Redu = ONEAPI::reduction(ReduAcc, BOp);
-    assert(toBool(Redu.getIdentity() == Identity) &&
+        ReduDWAcc(ReduBuf, CGH);
+    auto ReduRW = ONEAPI::reduction(ReduRWAcc, BOp);
+    auto ReduDW = ONEAPI::reduction(ReduDWAcc, BOp);
+    auto ReduRW2020 = sycl::reduction(ReduBuf, CGH, BOp);
+    assert(toBool(ReduRW.getIdentity() == Identity) &&
+           toBool(ReduDW.getIdentity() == Identity) &&
+           toBool(ReduRW2020.getIdentity() == Identity) &&
            toBool(known_identity<BinaryOperation, T>::value == Identity) &&
            "Failed getIdentity() check().");
-    test_reducer(Redu, A, B);
-    test_reducer(Redu, Identity, BOp, A, B);
+    test_reducer(ReduRW, A, B);
+    test_reducer(ReduDW, A, B);
+    test_reducer(ReduRW2020, A, B);
+    test_reducer(ReduRW, Identity, BOp, A, B);
+    test_reducer(ReduDW, Identity, BOp, A, B);
+    test_reducer(ReduRW2020, Identity, BOp, A, B);
 
     // Command group must have at least one task in it. Use an empty one.
     CGH.single_task<SpecializationKernelName>([=]() {});
@@ -71,12 +81,20 @@ void testUnknown(T Identity, BinaryOperation BOp, T A, T B) {
   Q.submit([&](handler &CGH) {
     // Reduction needs a global_buffer accessor as a parameter.
     // This accessor is not really used in this test.
+    accessor<T, Dim, access::mode::read_write, access::target::global_buffer>
+        ReduRWAcc(ReduBuf, CGH);
     accessor<T, Dim, access::mode::discard_write, access::target::global_buffer>
-        ReduAcc(ReduBuf, CGH);
-    auto Redu = ONEAPI::reduction(ReduAcc, Identity, BOp);
-    bool IsCorrectVal = toBool(Redu.getIdentity() == Identity);
-    assert(IsCorrectVal && "Failed getIdentity() check().");
-    test_reducer(Redu, Identity, BOp, A, B);
+        ReduDWAcc(ReduBuf, CGH);
+    auto ReduRW = ONEAPI::reduction(ReduRWAcc, Identity, BOp);
+    auto ReduDW = ONEAPI::reduction(ReduDWAcc, Identity, BOp);
+    auto ReduRW2020 = sycl::reduction(ReduBuf, CGH, Identity, BOp);
+    assert(toBool(ReduRW.getIdentity() == Identity) &&
+           toBool(ReduDW.getIdentity() == Identity) &&
+           toBool(ReduRW2020.getIdentity() == Identity) &&
+           "Failed getIdentity() check().");
+    test_reducer(ReduRW, Identity, BOp, A, B);
+    test_reducer(ReduDW, Identity, BOp, A, B);
+    test_reducer(ReduRW2020, Identity, BOp, A, B);
 
     // Command group must have at least one task in it. Use an empty one.
     CGH.single_task<SpecializationKernelName>([=]() {});
