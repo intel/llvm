7c7dadcaf4c266dcbfd8dc4deee061045020f4a0
Premanand M Rao <premanand.m.rao@intel.com>
2021-03-04 10:45:25 -0800

    [SYCL] Cast to correct address space when a function object is passed
    When a function object is passed as an argument to a call,
    type mismatches may be address space mismatches and these
    cannot be bitcast away.  Check if it is an address space
    mismatch and if so, use the appropriate address space cast.
    
    Signed-off-by: Premanand M Rao <premanand.m.rao@intel.com>

--- clang/test/CodeGenSYCL/invoke-function-addrspace.cpp
+++ clang/test/CodeGenSYCL/invoke-function-addrspace.cpp
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 // RUN: %clang_cc1 -sycl-std=2020 -fsycl-is-device -fsycl-allow-func-ptr -internal-isystem %S/Inputs -disable-llvm-passes -triple spir64-unknown-unknown-sycldevice -emit-llvm -o - %s | FileCheck %s
+=======
+// RUN: %clang_cc1 -fsycl -sycl-std=2020 -fsycl-is-device -fsycl-allow-func-ptr -internal-isystem %S/Inputs -disable-llvm-passes -triple spir64-unknown-unknown-sycldevice -emit-llvm -o - %s | FileCheck %s
+>>>>>>> 27da70183e3b ([SYCL] Cast to correct address space when a function object is passed)
 
 // Test that the type of function object invoked from the kernel has
 // the right address space.
@@ -8,6 +12,7 @@
 using namespace cl::sycl;
 queue q;
 
+<<<<<<< HEAD
 // CHECK: define linkonce_odr spir_func i32 @{{.*}}invoke_function{{.*}}(i32 () addrspace(4)* %f)
 template <typename Callable>
 auto invoke_function(Callable &&f) {
@@ -22,6 +27,22 @@ auto invoke_function(Callable &&f) {
 // CHECK: define dso_local spir_func i32 @{{.*}}bar10{{.*}}()
 int bar10() { return 10; }
 
+=======
+// CHECK-DAG: define dso_local spir_func i32 @{{.*}}bar10{{.*}}()
+int bar10() { return 10; }
+
+// CHECK-DAG: define linkonce_odr spir_func i32 @{{.*}}invoke_function{{.*}}(i32 () addrspace(4)* %f)
+template <typename Callable>
+auto invoke_function(Callable &&f) {
+  // CHECK-DAG: %f.addr = alloca i32 () addrspace(4)*, align 8
+  // CHECK-DAG: %f.addr.ascast = addrspacecast i32 () addrspace(4)** %f.addr to i32 () addrspace(4)* addrspace(4)*
+  // CHECK-DAG: store i32 () addrspace(4)* %f, i32 () addrspace(4)* addrspace(4)* %f.addr.ascast, align 8
+  // CHECK-DAG: %0 = load i32 () addrspace(4)*, i32 () addrspace(4)* addrspace(4)* %f.addr.ascast, align 8
+  // CHECK-DAG: %call = call spir_func addrspace(4) i32 %0()
+  return f();
+}
+
+>>>>>>> 27da70183e3b ([SYCL] Cast to correct address space when a function object is passed)
 int main() {
   kernel_single_task<class KernelName>(
       [=]() {
