522aa715d4c5656aaddfcaf5df57b4eb99b18606
Alexander Batashev <alexander.batashev@intel.com>
2021-03-05 12:51:25 +0300

    Address reviewers feedback

--- sycl/test/basic_tests/version.cpp
+++ sycl/test/basic_tests/version.cpp
@@ -1,22 +1,16 @@
 // RUN: %clangxx -fsycl %s -o %t.out
-// RUN: %RUN_ON_HOST %t.out
 
 #include <CL/sycl.hpp>
-#include <iostream>
 
 int main() {
-#ifndef __LIBSYCL_MAJOR_VERSION
-  std::cerr << "__LIBSYCL_MAJOR_VERSION was not found\n";
-  return -1;
+#if !defined(__LIBSYCL_MAJOR_VERSION) || __LIBSYCL_MAJOR_VERSION < 0
+#error "__LIBSYCL_MAJOR_VERSION is not properly defined"
 #endif
-#ifndef __LIBSYCL_MINOR_VERSION
-  std::cerr << "__LIBSYCL_MINOR_VERSION was not found\n";
-  return -1;
+#if !defined(__LIBSYCL_MINOR_VERSION) || __LIBSYCL_MINOR_VERSION < 0
+#error "__LIBSYCL_MINOR_VERSION is not properly defined"
 #endif
-#ifndef __LIBSYCL_PATCH_VERSION
-  std::cerr << "__LIBSYCL_PATCH_VERSION was not found\n";
-  return -1;
+#if !defined(__LIBSYCL_PATCH_VERSION) || __LIBSYCL_PATCH_VERSION < 0
+#error "__LIBSYCL_PATCH_VERSION is not properly defined"
 #endif
-
   return 0;
 }
