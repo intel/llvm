ece75b5de794a904ee4a00c05fa60df37fed1216
Soumi Manna <soumi.manna@intel.com>
2021-03-09 11:51:21 -0800

    Address review comments
    Signed-off-by: Soumi Manna <soumi.manna@intel.com>

--- clang/test/SemaSYCL/intel-fpga-global-const.cpp
+++ clang/test/SemaSYCL/intel-fpga-global-const.cpp
@@ -3,21 +3,30 @@
 // Test that checks global constant variable (which allows the redeclaration) since
 // IntelFPGAConstVar is one of the subjects listed for [[intel::max_replicates()]] attribute.
 
-// Merging of different arg values.
+// Checking of duplicate argument values.
 //CHECK: VarDecl{{.*}}var_max_replicates
 //CHECK: IntelFPGAMaxReplicatesAttr
 //CHECK-NEXT: ConstantExpr
 //CHECK-NEXT: value:{{.*}}12
 //CHECK-NEXT: IntegerLiteral{{.*}}12{{$}}
-//expected-warning@+2{{attribute 'max_replicates' is already applied with different arguments}}
+//CHECK: IntelFPGAMaxReplicatesAttr
+//CHECK-NEXT: ConstantExpr
+//CHECK-NEXT: value:{{.*}}12
+//CHECK-NEXT: IntegerLiteral{{.*}}12{{$}}
 [[intel::max_replicates(12)]] extern const int var_max_replicates;
-[[intel::max_replicates(14)]] const int var_max_replicates = 0;
+[[intel::max_replicates(12)]] const int var_max_replicates = 0; // OK
+
+// Merging of different arg values.
+//expected-warning@+2{{attribute 'max_replicates' is already applied with different arguments}}
+[[intel::max_replicates(12)]] extern const int var_max_replicates_1;
+[[intel::max_replicates(14)]] const int var_max_replicates_1 = 0;
 //expected-note@-2{{previous attribute is here}}
 
 // Merging of incompatible attributes.
 // FIXME: Diagnostic order isn't correct, this isn't what we'd want here but
-// this is an upstream issue. Merge function is calling checkAttrMutualExclusion()
-// function that has backwards diagnostic behavior. This should be fixed into upstream.
+// this is an upstream issue. Merge function is calling here
+// checkAttrMutualExclusion() function that has backwards diagnostic behavior.
+// This should be fixed into upstream.
 //expected-error@+2{{'max_replicates' and 'fpga_register' attributes are not compatible}}
 //expected-note@+2{{conflicting attribute is here}}
 [[intel::max_replicates(12)]] extern const int var_max_replicates_2;
