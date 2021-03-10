4daa94183eea618e3a1e34a533c24bbfb09551dd
rdeodhar <rajiv.deodhar@intel.com>
2021-03-10 11:24:45 -0700

    Adjusts test ranges in preparation for a change in the compiler implementation (intel/llvm-test-suite#161)
    This change adjusts test ranges in preparation for a change in the compiler implementation.
    
    Signed-off-by: rdeodhar <rajiv.deodhar@intel.com>

--- SYCL/Basic/parallel_for_range_roundup.cpp
+++ SYCL/Basic/parallel_for_range_roundup.cpp
@@ -40,7 +40,7 @@ int try_item1(size_t size) {
 }
 
 void try_item2(size_t size) {
-  range<2> Size{size, size};
+  range<2> Size{size, 10};
   int Counter = 0;
   {
     buffer<range<2>, 1> BufRange(&Range2, 1);
@@ -58,11 +58,11 @@ void try_item2(size_t size) {
     myQueue.wait();
   }
   check("Size seen by user = ", Range2.get(0), size);
-  check("Counter = ", Counter, size * size);
+  check("Counter = ", Counter, size * 10);
 }
 
 void try_item3(size_t size) {
-  range<3> Size{size, size, size};
+  range<3> Size{size, 10, 10};
   int Counter = 0;
   {
     buffer<range<3>, 1> BufRange(&Range3, 1);
@@ -80,7 +80,7 @@ void try_item3(size_t size) {
     myQueue.wait();
   }
   check("Size seen by user = ", Range3.get(0), size);
-  check("Counter = ", Counter, size * size * size);
+  check("Counter = ", Counter, size * 10 * 10);
 }
 
 void try_id1(size_t size) {
@@ -105,7 +105,7 @@ void try_id1(size_t size) {
 }
 
 void try_id2(size_t size) {
-  range<2> Size{size, size};
+  range<2> Size{size, 10};
   int Counter = 0;
   {
     buffer<range<2>, 1> BufRange(&Range2, 1);
@@ -122,11 +122,11 @@ void try_id2(size_t size) {
     });
     myQueue.wait();
   }
-  check("Counter = ", Counter, size * size);
+  check("Counter = ", Counter, size * 10);
 }
 
 void try_id3(size_t size) {
-  range<3> Size{size, size, size};
+  range<3> Size{size, 10, 10};
   int Counter = 0;
   {
     buffer<range<3>, 1> BufRange(&Range3, 1);
@@ -143,13 +143,13 @@ void try_id3(size_t size) {
     });
     myQueue.wait();
   }
-  check("Counter = ", Counter, size * size * size);
+  check("Counter = ", Counter, size * 10 * 10);
 }
 
 int main() {
   int x;
 
-  x = 10;
+  x = 1500;
   try_item1(x);
   try_item2(x);
   try_item3(x);
@@ -168,27 +168,27 @@ int main() {
   return 0;
 }
 
-// CHECK:       parallel_for range adjusted from 10 to 32
-// CHECK-NEXT:  Size seen by user = 10
-// CHECK-NEXT:  Counter = 10
-// CHECK-NEXT:  parallel_for range adjusted from 10 to 32
-// CHECK-NEXT:  Size seen by user = 10
-// CHECK-NEXT:  Counter = 100
-// CHECK-NEXT:  parallel_for range adjusted from 10 to 32
-// CHECK-NEXT:  Size seen by user = 10
-// CHECK-NEXT:  Counter = 1000
-// CHECK-NEXT:  parallel_for range adjusted from 10 to 32
-// CHECK-NEXT:  Counter = 10
-// CHECK-NEXT:  parallel_for range adjusted from 10 to 32
-// CHECK-NEXT:  Counter = 100
-// CHECK-NEXT:  parallel_for range adjusted from 10 to 32
-// CHECK-NEXT:  Counter = 1000
+// CHECK:       parallel_for range adjusted from 1500 to 1504
+// CHECK-NEXT:  Size seen by user = 1500
+// CHECK-NEXT:  Counter = 1500
+// CHECK-NEXT:  parallel_for range adjusted from 1500 to 1504
+// CHECK-NEXT:  Size seen by user = 1500
+// CHECK-NEXT:  Counter = 15000
+// CHECK-NEXT:  parallel_for range adjusted from 1500 to 1504
+// CHECK-NEXT:  Size seen by user = 1500
+// CHECK-NEXT:  Counter = 150000
+// CHECK-NEXT:  parallel_for range adjusted from 1500 to 1504
+// CHECK-NEXT:  Counter = 1500
+// CHECK-NEXT:  parallel_for range adjusted from 1500 to 1504
+// CHECK-NEXT:  Counter = 15000
+// CHECK-NEXT:  parallel_for range adjusted from 1500 to 1504
+// CHECK-NEXT:  Counter = 150000
 // CHECK-NEXT:  Size seen by user = 256
 // CHECK-NEXT:  Counter = 256
 // CHECK-NEXT:  Size seen by user = 256
-// CHECK-NEXT:  Counter = 65536
+// CHECK-NEXT:  Counter = 2560
 // CHECK-NEXT:  Size seen by user = 256
-// CHECK-NEXT:  Counter = 16777216
+// CHECK-NEXT:  Counter = 25600
 // CHECK-NEXT:  Counter = 256
-// CHECK-NEXT:  Counter = 65536
-// CHECK-NEXT:  Counter = 16777216
+// CHECK-NEXT:  Counter = 2560
+// CHECK-NEXT:  Counter = 25600
