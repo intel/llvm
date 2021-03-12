d525d3cb92ea68a3275fb20b346f68f4b74f7bad
Dmitry Sidorov <dmitry.sidorov@intel.com>
2021-03-12 15:24:47 +0300

    Fix test
    Signed-off-by: Dmitry Sidorov <dmitry.sidorov@intel.com>

--- clang/test/Driver/sycl-instrumentation.c
+++ clang/test/Driver/sycl-instrumentation.c
@@ -10,5 +10,5 @@
 
 /// Check if "fsycl_instrument_device_code" usage with a non-spirv target
 /// results in an error.
-// RUN: %clang -### -fsycl-instrument-device-code -target=x86 %s 2>&1
+// RUN: %clang -### -fsycl-instrument-device-code --target=x86 %s 2>&1
 // expected-error{{unsupported option '-fsycl-instrument-device-code' for target 'x86_64-unknown-linux-gnu'}}
