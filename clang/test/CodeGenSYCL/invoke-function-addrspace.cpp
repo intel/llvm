2ebaee308ee44717c0952cd8472c4aa71e435aad
Premanand M Rao <premanand.m.rao@intel.com>
2021-03-16 14:12:40 -0700

    Modify test to use CHECK instead of CHECK-DAG

--- clang/test/CodeGenSYCL/invoke-function-addrspace.cpp
+++ clang/test/CodeGenSYCL/invoke-function-addrspace.cpp
@@ -8,19 +8,19 @@
 using namespace cl::sycl;
 queue q;
 
-// CHECK-DAG: define dso_local spir_func i32 @{{.*}}bar10{{.*}}()
-int bar10() { return 10; }
-
-// CHECK-DAG: define linkonce_odr spir_func i32 @{{.*}}invoke_function{{.*}}(i32 () addrspace(4)* %f)
+// CHECK: define linkonce_odr spir_func i32 @{{.*}}invoke_function{{.*}}(i32 () addrspace(4)* %f)
 template <typename Callable>
 auto invoke_function(Callable &&f) {
-  // CHECK-DAG: %f.addr = alloca i32 () addrspace(4)*, align 8
-  // CHECK-DAG: %f.addr.ascast = addrspacecast i32 () addrspace(4)** %f.addr to i32 () addrspace(4)* addrspace(4)*
-  // CHECK-DAG: store i32 () addrspace(4)* %f, i32 () addrspace(4)* addrspace(4)* %f.addr.ascast, align 8
-  // CHECK-DAG: %0 = load i32 () addrspace(4)*, i32 () addrspace(4)* addrspace(4)* %f.addr.ascast, align 8
-  // CHECK-DAG: %call = call spir_func addrspace(4) i32 %0()
+  // CHECK: %f.addr = alloca i32 () addrspace(4)*, align 8
+  // CHECK: %f.addr.ascast = addrspacecast i32 () addrspace(4)** %f.addr to i32 () addrspace(4)* addrspace(4)*
+  // CHECK: store i32 () addrspace(4)* %f, i32 () addrspace(4)* addrspace(4)* %f.addr.ascast, align 8
+  // CHECK: %0 = load i32 () addrspace(4)*, i32 () addrspace(4)* addrspace(4)* %f.addr.ascast, align 8
+  // CHECK: %call = call spir_func addrspace(4) i32 %0()
   return f();
 }
+
+// CHECK: define dso_local spir_func i32 @{{.*}}bar10{{.*}}()
+int bar10() { return 10; }
 
 int main() {
   kernel_single_task<class KernelName>(
