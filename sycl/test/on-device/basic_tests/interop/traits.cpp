baf4faeb09a71e377791466d9327400f159998a8
Alexander Batashev <alexander.batashev@intel.com>
2021-03-23 17:33:04 +0300

    more test fixes

--- sycl/test/on-device/basic_tests/interop/traits.cpp
+++ sycl/test/on-device/basic_tests/interop/traits.cpp
@@ -20,24 +20,23 @@ constexpr auto Backend = sycl::backend::level_zero;
 #include <sycl/sycl.hpp>
 
 int main() {
-  if constexpr (Backend == sycl::backend::opencl) {
-    static_assert(
-        std::is_same_v<sycl::backend_traits<Backend>::input_type<sycl::device>,
-                       sycl::interop<Backend, sycl::device>::type>);
-    static_assert(
-        std::is_same_v<sycl::backend_traits<Backend>::input_type<sycl::context>,
-                       sycl::interop<Backend, sycl::context>::type>);
-    static_assert(
-        std::is_same_v<sycl::backend_traits<Backend>::input_type<sycl::queue>,
-                       sycl::interop<Backend, sycl::queue>::type>);
-    static_assert(
-        std::is_same_v<
-            sycl::backend_traits<Backend>::input_type<sycl::buffer<int, 2>>,
-            sycl::interop<Backend, sycl::buffer<int, 2>>::type>);
-    static_assert(
-        std::is_same_v<sycl::backend_traits<Backend>::input_type<sycl::event>,
-                       sycl::interop<Backend, sycl::event>::type>);
-  }
+#ifdef USE_OPENCL
+  static_assert(
+      std::is_same_v<sycl::backend_traits<Backend>::input_type<sycl::device>,
+                     sycl::interop<Backend, sycl::device>::type>);
+  static_assert(
+      std::is_same_v<sycl::backend_traits<Backend>::input_type<sycl::context>,
+                     sycl::interop<Backend, sycl::context>::type>);
+  static_assert(
+      std::is_same_v<sycl::backend_traits<Backend>::input_type<sycl::queue>,
+                     sycl::interop<Backend, sycl::queue>::type>);
+  static_assert(std::is_same_v<
+                sycl::backend_traits<Backend>::input_type<sycl::buffer<int, 2>>,
+                sycl::interop<Backend, sycl::buffer<int, 2>>::type>);
+  static_assert(
+      std::is_same_v<sycl::backend_traits<Backend>::input_type<sycl::event>,
+                     sycl::interop<Backend, sycl::event>::type>);
+#endif
 
   static_assert(
       std::is_same_v<sycl::backend_traits<Backend>::input_type<sycl::platform>,
@@ -53,7 +52,7 @@ int main() {
       std::is_same_v<sycl::backend_traits<Backend>::return_type<sycl::queue>,
                      sycl::interop<Backend, sycl::queue>::type>);
   static_assert(
-      std::is_same_v<sycl::backend_traits<Backend>::input_type<sycl::platform>,
+      std::is_same_v<sycl::backend_traits<Backend>::return_type<sycl::platform>,
                      sycl::interop<Backend, sycl::platform>::type>);
 
   return 0;
