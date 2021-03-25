835fec37ee6c5f8180a92e21853fdda302fd109e
rdeodhar <rajiv.deodhar@intel.com>
2021-03-25 16:06:51 -0700

    Removed dead code.

--- sycl/plugins/level_zero/usm_allocator.hpp
+++ sycl/plugins/level_zero/usm_allocator.hpp
@@ -31,7 +31,6 @@ public:
   void *allocate(size_t size);
   void *allocate(size_t size, size_t alignment);
   void deallocate(void *ptr);
-  void printStats();
 
 private:
   std::unique_ptr<USMAllocImpl> pImpl;
