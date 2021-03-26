a7e903a36ef3a5bcf1ffcb89e448b1870d038122
smanna12 <52675209+smanna12@users.noreply.github.com>
2021-03-26 00:47:05 -0400

    [SYCL][FPGA] Add implicit memory attribute with [[intel::max_replicates()]] (#3409)
    If the declaration does not have an [[intel::fpga_memory]] attribute,
    we create one as an implicit attribute.
    
    During refactoring work with [[intel::max_replicates()]] attribute, we somehow
    dropped the implicit memory attribute.
    
    This patch fixes the problem.
    
    Signed-off-by: soumi.manna <smanna@scsel-cfl-02.sc.intel.com>

--- clang/lib/Sema/SemaDeclAttr.cpp
+++ clang/lib/Sema/SemaDeclAttr.cpp
@@ -5992,11 +5992,18 @@ void Sema::AddIntelFPGAMaxReplicatesAttr(Decl *D, const AttributeCommonInfo &CI,
         return;
       }
     }
-    // [[intel::fpga_register]] and [[intel::max_replicates()]]
-    // attributes are incompatible.
-    if (checkAttrMutualExclusion<IntelFPGARegisterAttr>(*this, D, CI))
-      return;
   }
+
+  // [[intel::fpga_register]] and [[intel::max_replicates()]]
+  // attributes are incompatible.
+  if (checkAttrMutualExclusion<IntelFPGARegisterAttr>(*this, D, CI))
+    return;
+
+  // If the declaration does not have an [[intel::fpga_memory]]
+  // attribute, this creates one as an implicit attribute.
+  if (!D->hasAttr<IntelFPGAMemoryAttr>())
+    D->addAttr(IntelFPGAMemoryAttr::CreateImplicit(
+        Context, IntelFPGAMemoryAttr::Default));
 
   D->addAttr(::new (Context) IntelFPGAMaxReplicatesAttr(Context, CI, E));
 }
