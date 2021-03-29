ffc6876cf0d0817a3fd47d9390ddd6f4633eb643
Andrzej Ratajewski <andrzej.ratajewski@intel.com>
2021-03-29 14:37:28 +0200

    Fix emission of SPIR-V friendly IR for vload/vload_half/vload_halfn/vloada_halfn OpenCL EIS instructions
    Original commit:
    https://github.com/KhronosGroup/SPIRV-LLVM-Translator/commit/f4a0a02

--- llvm-spirv/lib/SPIRV/SPIRVReader.cpp
+++ llvm-spirv/lib/SPIRV/SPIRVReader.cpp
@@ -4357,6 +4357,7 @@ Instruction *SPIRVToLLVM::transOCLBuiltinFromExtInst(SPIRVExtInst *BC,
     ArgTypes.resize(1);
   }
 
+  Type *RetTy = transType(BC->getType());
   if (BM->getDesiredBIsRepresentation() != BIsRepresentation::SPIRVFriendlyIR) {
     // Convert extended instruction into an OpenCL built-in
     if (IsPrintf) {
@@ -4366,14 +4367,14 @@ Instruction *SPIRVToLLVM::transOCLBuiltinFromExtInst(SPIRVExtInst *BC,
     }
   } else {
     MangledName = getSPIRVFriendlyIRFunctionName(
-        static_cast<OCLExtOpKind>(EntryPoint), ArgTypes);
+        static_cast<OCLExtOpKind>(EntryPoint), ArgTypes, RetTy);
   }
 
   SPIRVDBG(spvdbgs() << "[transOCLBuiltinFromExtInst] ModifiedUnmangledName: "
                      << UnmangledName << " MangledName: " << MangledName
                      << '\n');
 
-  FunctionType *FT = FunctionType::get(transType(BC->getType()), ArgTypes,
+  FunctionType *FT = FunctionType::get(RetTy, ArgTypes,
                                        /* IsVarArg */ IsPrintf);
   Function *F = M->getFunction(MangledName);
   if (!F) {
