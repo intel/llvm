0a990f83c114802e719a27e9ee53def5993edbc8
DenisBakhvalov <61807338+DenisBakhvalov@users.noreply.github.com>
2021-03-31 09:10:02 -0700

    [ESIMD] Let ESIMD attributes not depend on -fsycl-explicit-simd (#3446)
    This is one of the final patches to support mixing SYCL and ESIMD kernels.
    In the end, we want to get rid of `-fsycl-explicit-simd` option and unify the two modes: regular SYCL and ESIMD.
    This patch eliminates one of the dependencies on the `-fsycl-explicit-simd` flag, allowing to use ESIMD attributes without it.

--- clang/test/CodeGenSYCL/esimd-private-global.cpp
+++ clang/test/CodeGenSYCL/esimd-private-global.cpp
@@ -4,7 +4,7 @@
 // will be testing code generation and the second ESIMD lowering.
 //
 // RUN: %clang_cc1 -disable-llvm-passes -triple spir64-unknown-unknown-sycldevice \
-// RUN:   -fsycl-is-device -fsycl-explicit-simd -emit-llvm %s -o %t
+// RUN:   -fsycl-is-device -emit-llvm %s -o %t
 // RUN: sycl-post-link -split-esimd -lower-esimd -O0 -S %t -o %t.table
 // RUN: FileCheck %s -input-file=%t_esimd_0.ll
 
