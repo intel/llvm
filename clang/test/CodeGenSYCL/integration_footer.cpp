f25db06301b84904d6806030d0c53688241fbed4
Erich Keane <erich.keane@intel.com>
2021-04-01 13:58:54 -0700

    Fix the test, had a type in 2 places

--- clang/test/CodeGenSYCL/integration_footer.cpp
+++ clang/test/CodeGenSYCL/integration_footer.cpp
@@ -1,10 +1,10 @@
-// RUN: %clang_cc1 -fsycl-is-device -triple spir64-unknown-unknown-sycldevice -fsycl-int_footer=%t.h %s -emit-llvm -o %t.ll
+// RUN: %clang_cc1 -fsycl-is-device -triple spir64-unknown-unknown-sycldevice -fsycl-int-footer=%t.h %s -emit-llvm -o %t.ll
 // RUN: FileCheck -input-file=%t.h %s
 
 // CHECK: // Integration Footer contents to go here.
 
 #include "Inputs/sycl.hpp"
 
-nt main() {
+int main() {
   cl::sycl::kernel_single_task<class first_kernel>([]() {});
 }
