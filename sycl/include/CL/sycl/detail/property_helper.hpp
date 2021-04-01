5a05e03090bed30c3e5a8e49d1ac1331911fc726
Vyacheslav N Klochkov <vyacheslav.n.klochkov@intel.com>
2021-03-31 17:05:47 -0700

    [SYCL][NFC] - Set max number of dataless properties to 32
    Linux reserves 8 bytes and Windows reserves 4 bytes as a minimum to
    hold std::bitset. Thus, having more than 32 data-less properties,
    i.e. exceeding 32-bits on Windows, will cause ABI breaking change.
    
    Signed-off-by: Vyacheslav N Klochkov <vyacheslav.n.klochkov@intel.com>

--- sycl/include/CL/sycl/detail/property_helper.hpp
+++ sycl/include/CL/sycl/detail/property_helper.hpp
@@ -32,7 +32,8 @@ enum DataLessPropKind {
   BufferUsePinnedHostMemory = 5,
   UsePrimaryContext = 6,
   InitializeToIdentity = 7,
-  DataLessPropKindSize = 8
+  // Exceeding 32 may cause ABI breaking change on some of OSes.
+  DataLessPropKindSize = 32
 };
 
 // List of all properties with data IDs
