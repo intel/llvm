b19944f929ae9e1460638feaf48c4c4d3f739cfb
Byoungro So <byoungro.so@intel.com>
2021-03-31 22:25:38 -0700

    [SYCL] Fix SYCL_DEVICE_FILTER setting (intel/llvm-test-suite#210)
    Signed-off-by: Byoungro So <byoungro.so@intel.com>

--- SYCL/Scheduler/DataMovement.cpp
+++ SYCL/Scheduler/DataMovement.cpp
@@ -2,7 +2,6 @@
 // RUN: %HOST_RUN_PLACEHOLDER %t.out
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
-// RUN: %ACC_RUN_PLACEHOLDER %t.out
 //
 //==-------------------------- DataMovement.cpp ----------------------------==//
 //
