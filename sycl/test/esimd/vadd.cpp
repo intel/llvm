fdafd24c13f25ad11c078ea119a33a108460ea51
Thomas Applencourt <TApplencourt@users.noreply.github.com>
2021-04-01 16:28:37 -0500

    Transfer back data

--- sycl/test/esimd/vadd.cpp
+++ sycl/test/esimd/vadd.cpp
@@ -96,14 +96,13 @@ int main(void) {
             block_store<int, VL>(pC + i * VL, vc);
           });
     });
-
-    for (unsigned i = 0; i < Size; ++i) {
-      if (A[i] + B[i] != C[i]) {
-        std::cout << "failed at index " << i << ", " << C[i] << " != " << A[i]
-                  << " + " << B[i] << "\n";
-        return 1;
-      }
-    }
+  }
+  for (unsigned i = 0; i < Size; ++i) {
+     if (A[i] + B[i] != C[i]) {
+      std::cout << "failed at index " << i << ", " << C[i] << " != " << A[i]
+                << " + " << B[i] << "\n";
+      return 1;
+     }
   }
 
   std::cout << "Passed\n";
