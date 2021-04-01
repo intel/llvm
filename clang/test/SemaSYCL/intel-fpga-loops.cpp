ec8042108863e18e5ad273bb2e5a1786b5720d28
Zahira Ammarguellat <55545820+zahiraam@users.noreply.github.com>
2021-04-01 13:41:31 -0400

    [SYCL][FPGA] Allow max-concurrency attribute on functions. (#3388)
    This patch implements the support of a new FPGA function attribute ‘max_concurrency”. The attribute exists already for loops. It takes a single unsigned integer argument and has the following syntax:
    [[intel::max_concurrency(n)]]
    
    An example of it use:
    [[intel::max_concurrency(n)]]
    void foo() {
    }
    The LLVM IR representation will be function metadata:
    !max_concurrency !0
    !0 = !{!i32 n}
    
    Max_concurrency applies to functions in device code. It is not be propagated to the caller.

--- clang/test/SemaSYCL/intel-fpga-loops.cpp
+++ clang/test/SemaSYCL/intel-fpga-loops.cpp
@@ -10,7 +10,7 @@ void foo() {
   [[intel::ivdep]] int a[10];
   // expected-error@+1 {{'initiation_interval' attribute only applies to 'for', 'while', 'do' statements, and functions}}
   [[intel::initiation_interval(2)]] int c[10];
-  // expected-error@+1 {{'max_concurrency' attribute cannot be applied to a declaration}}
+  // expected-error@+1 {{'max_concurrency' attribute only applies to 'for', 'while', 'do' statements, and functions}}
   [[intel::max_concurrency(2)]] int d[10];
   // expected-error@+1 {{'disable_loop_pipelining' attribute only applies to 'for', 'while', 'do' statements, and functions}}
   [[intel::disable_loop_pipelining]] int g[10];
@@ -81,10 +81,10 @@ void boo() {
   // expected-error@+1 {{'initiation_interval' attribute takes no more than 1 argument}}
   [[intel::initiation_interval(2, 2)]] for (int i = 0; i != 10; ++i)
       a[i] = 0;
-  // expected-warning@+1 {{'max_concurrency' attribute takes at least 1 argument; attribute ignored}}
+  // expected-error@+1 {{'max_concurrency' attribute takes one argument}}
   [[intel::max_concurrency]] for (int i = 0; i != 10; ++i)
       a[i] = 0;
-  // expected-error@+1 {{'max_concurrency' attribute takes no more than 1 argument}}
+  // expected-error@+1 {{'max_concurrency' attribute takes one argument}}
   [[intel::max_concurrency(2, 2)]] for (int i = 0; i != 10; ++i)
       a[i] = 0;
 
@@ -424,7 +424,7 @@ void parse_order_error() {
   // We had a bug where we would only look at the first attribute in the group
   // when trying to determine whether to diagnose the loop attributes on an
   // incorrect subject. Test that we properly catch this situation.
-  [[clang::nomerge, intel::max_concurrency(1)]] // expected-error {{'max_concurrency' attribute only applies to 'for', 'while', and 'do' statements}}
+  [[clang::nomerge, intel::max_concurrency(1)]] // expected-error {{'max_concurrency' attribute only applies to 'for', 'while', 'do' statements, and functions}}
   if (1) { parse_order_error(); }               // Recursive call silences unrelated diagnostic about nomerge.
 
   [[clang::nomerge, intel::max_concurrency(1)]] // OK
