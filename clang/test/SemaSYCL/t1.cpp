15dc3c4a06d2e118c0b852f899bbbb92e9ba8f37
Zahira Ammarguellat <zahira.ammarguellat@intel.com>
2021-04-06 10:39:46 -0700

    Trying test case for Jessica
    Signed-off-by: Zahira Ammarguellat <zahira.ammarguellat@intel.com>

--- clang/test/SemaSYCL/t1.cpp
+++ clang/test/SemaSYCL/t1.cpp
@@ -7,9 +7,9 @@ using namespace cl::sycl;
 int main() {
   queue q;
 
-  q.submit([&](handler &h) { 
-  h.single_task<class kernel_name_3>(
-    []() [[intel::initiation_interval(4)]]{}); // expected-error{{'max_concurrency' attribute cannot be applied to types}}
+  q.submit([&](handler &h) {
+    h.single_task<class kernel_name_3>(
+        []() [[intel::initiation_interval(4)]]{}); // expected-error{{'max_concurrency' attribute cannot be applied to types}}
   });
 
   return 0;
