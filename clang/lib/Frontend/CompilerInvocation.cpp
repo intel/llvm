9e41f823ddc4bc57c5e47b6b854862384cff5c68
Erich Keane <erich.keane@intel.com>
2021-04-05 22:00:48 -0700

    [SYCL] Fix DeclareSYCLBuiltins for round-trip setting. (#3491)
    Upstream LLVMadded a round-trip functionality to the command line args so
    that we can generate a full set of command line arguments from our
    existing state.  At the moment, this is 'assert' build only, but it
    seems the intent is to make this perminent for future functionality.
    
    We will have to analyze the full set of our command line args to make
    sure we do this right (that is, that it gets set right the 2nd time),
    but this one fixes the DeclareSYCLBuiltins to unblock the pulldown's
    build.

--- clang/lib/Frontend/CompilerInvocation.cpp
+++ clang/lib/Frontend/CompilerInvocation.cpp
@@ -3496,6 +3496,9 @@ void CompilerInvocation::GenerateLangArgs(const LangOptions &Opts,
   if (Opts.getSignReturnAddressKey() ==
       LangOptions::SignReturnAddressKeyKind::BKey)
     GenerateArg(Args, OPT_msign_return_address_key_EQ, "b_key", SA);
+
+  if (Opts.DeclareSPIRVBuiltins)
+    GenerateArg(Args, OPT_fdeclare_spirv_builtins, SA);
 }
 
 bool CompilerInvocation::ParseLangArgs(LangOptions &Opts, ArgList &Args,
