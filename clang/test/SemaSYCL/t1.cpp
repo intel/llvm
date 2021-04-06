36b5eb172f05e93be839fb7ee01ea1a09f40f65a
Zahira Ammarguellat <zahira.ammarguellat@intel.com>
2021-04-06 08:34:36 -0700

    Trying test case for Jessica
    Signed-off-by: Zahira Ammarguellat <zahira.ammarguellat@intel.com>

---
+++ clang/test/SemaSYCL/t1.cpp
@@ -0,0 +1,19 @@
+// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -sycl-std=2020  -fsyntax-only -ast-dump -verify -pedantic %s | FileCheck %s
+
+#include "sycl.hpp"
+
+using namespace cl::sycl;
+
+int main() {
+  queue q;
+
+  q.submit([&](handler &h) {
+
+    h.single_task<class NT>(
+      []() [[intel::max_concurrency(NT)]]{})
+
+    });
+
+  });
+}
+
