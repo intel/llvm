fe78c896fbdf129043808233408a5b6a5eab0fa4
Alexey Bader <alexey.bader@intel.com>
2021-04-07 22:30:10 +0300

    Update group-local-memory.cpp

--- clang/test/CodeGenSYCL/group-local-memory.cpp
+++ clang/test/CodeGenSYCL/group-local-memory.cpp
@@ -1,7 +1,4 @@
 // RUN: %clang_cc1 -fsycl-is-device -triple spir64-unknown-unknown-sycldevice \
-// RUN:     -S -emit-llvm -mllvm -debug-pass=Structure -disable-llvm-passes \
-// RUN:     -o - %s 2>&1 | FileCheck %s
-// RUN: %clang_cc1 -fsycl-is-device -triple spir64-unknown-unknown-sycldevice \
 // RUN:     -S -emit-llvm -mllvm -debug-pass=Structure -o - %s 2>&1 \
 // RUN:     | FileCheck %s
 
