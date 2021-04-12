intel/llvm

commit ec9716558d8944906df5b0738c065737bcf08563

[SYCL] Persistent program cache (#3391)
First part of implementation for persistent cache of device programs following sycl/doc/KernelProgramCache.md:

Programs built for target device are stored in on-disk cache and used next time when the same application is executed.  
What is done:
 - storing and loading files when built program is not found in in-memory cache;
 - file system concurrent access management (lock file is created per cache item while data is being written, read operation does not lock the cache item);
 - thread safety for persistent device code cache is not needed as soon as build/in-memory cache part is managed by program_manager and persisten_device_code_cache does not store data shared between threads.
 - Implement support for environment variables affecting cache:
   SYCL_CACHE_DIR
   SYCL_CACHE_TRACE
   SYCL_CACHE_DISABLE_PERSISTENT
   SYCL_CACHE_MIN_DEVICE_IMAGE_SIZE
   SYCL_CACHE_MAX_DEVICE_IMAGE_SIZE
   SYCL_CACHE_MIN_DEVICE_IMAGE_SIZE
 - Errors happening during filesystem operation cause corresponding cache operation silently ends:
   - for read operation, cache miss happens (without SYCL error), message is sent to std::cerr if SYCL_CACHE_TRACE is set;
   - for write operation, no cache item stored (without SYCL error), message is sent to std::cerr if SYCL_CACHE_TRACE is set;
 - Add tests:
   - on-device tests for basic cache functionality and environment variables processing;
   - unit tests for concurrent access to filesystem and error management.
 
To be done in next patches:
 - implement rest of environment variables and extend them to both in-memory and persistent cache;
 - implement kernel eviction mechanism for on-device and in-memory caches.

sycl/source/detail/persistent_device_code_cache.cpp
@@ -1,0 +1,358 @@
+//==---------- persistent_device_code_cache.cpp -----------------*- C++-*---==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include <cstdio>
+#include <detail/device_impl.hpp>
+#include <detail/persistent_device_code_cache.hpp>
+#include <detail/plugin.hpp>
+
+#if defined(__SYCL_RT_OS_LINUX)
+#include <unistd.h>
+#else
+#include <direct.h>
+#include <io.h>
+#endif
+
+__SYCL_INLINE_NAMESPACE(cl) {
+namespace sycl {
+namespace detail {
+
+/* Lock file suffix */
+const char LockCacheItem::LockSuffix[] = ".lock";
+
+LockCacheItem::LockCacheItem(const std::string &Path)
+    : FileName(Path + LockSuffix) {
+  int fd;
+
+  /* If the lock fail is not created */
+  if ((fd = open(FileName.c_str(), O_CREAT | O_EXCL, S_IWRITE)) != -1) {
+    close(fd);
+    Owned = true;
+  } else {
+    PersistentDeviceCodeCache::trace("Failed to aquire lock file: " + FileName);
+  }
+}
+
+LockCacheItem::~LockCacheItem() {
+  if (Owned && std::remove(FileName.c_str()))
+    PersistentDeviceCodeCache::trace("Failed to release lock file: " +
+                                     FileName);
+}
+
+/* Returns true if specified image should be cached on disk. It checks if
+ * cache is enabled, image has SPIRV type and matches thresholds. */
+bool PersistentDeviceCodeCache::isImageCached(const RTDeviceBinaryImage &Img) {
+  // Cache shoould be enabled and image type should be SPIR-V
+  if (!isEnabled() || Img.getFormat() != PI_DEVICE_BINARY_TYPE_SPIRV)
+    return false;
+
+  static auto MaxImgSize = getNumParam<SYCL_CACHE_MAX_DEVICE_IMAGE_SIZE>(
+      DEFAULT_MAX_DEVICE_IMAGE_SIZE);
+  static auto MinImgSize = getNumParam<SYCL_CACHE_MIN_DEVICE_IMAGE_SIZE>(
+      DEFAULT_MIN_DEVICE_IMAGE_SIZE);
+
+  // Make sure that image size is between caching thresholds if they are set.
+  // Zero values for threshold is treated as disabled threshold.
+  if ((MaxImgSize && (Img.getSize() > MaxImgSize)) ||
+      (MinImgSize && (Img.getSize() < MinImgSize)))
+    return false;
+
+  return true;
+}
+
+/* Stores built program in persisten cache
+ */
+void PersistentDeviceCodeCache::putItemToDisc(
+    const device &Device, const RTDeviceBinaryImage &Img,
+    const SerializedObj &SpecConsts, const std::string &BuildOptionsString,
+    const RT::PiProgram &NativePrg) {
+
+  if (!isImageCached(Img))
+    return;
+
+  auto Plugin = detail::getSyclObjImpl(Device)->getPlugin();
+  std::string DirName =
+      getCacheItemPath(Device, Img, SpecConsts, BuildOptionsString);
+
+  size_t i = 0;
+  std::string FileName;
+  do {
+    FileName = DirName + "/" + std::to_string(i++);
+  } while (OSUtil::isPathPresent(FileName + ".bin"));
+
+  unsigned int DeviceNum = 0;
+
+  Plugin.call<PiApiKind::piProgramGetInfo>(
+      NativePrg, PI_PROGRAM_INFO_NUM_DEVICES, sizeof(DeviceNum), &DeviceNum,
+      nullptr);
+
+  std::vector<size_t> BinarySizes(DeviceNum);
+  Plugin.call<PiApiKind::piProgramGetInfo>(
+      NativePrg, PI_PROGRAM_INFO_BINARY_SIZES,
+      sizeof(size_t) * BinarySizes.size(), BinarySizes.data(), nullptr);
+
+  std::vector<std::vector<char>> Result;
+  std::vector<char *> Pointers;
+  for (size_t I = 0; I < BinarySizes.size(); ++I) {
+    Result.emplace_back(BinarySizes[I]);
+    Pointers.push_back(Result[I].data());
+  }
+
+  Plugin.call<PiApiKind::piProgramGetInfo>(NativePrg, PI_PROGRAM_INFO_BINARIES,
+                                           sizeof(char *) * Pointers.size(),
+                                           Pointers.data(), nullptr);
+
+  try {
+    OSUtil::makeDir(DirName.c_str());
+    LockCacheItem Lock{FileName};
+    if (Lock.isOwned()) {
+      writeBinaryDataToFile(FileName + ".bin", Result);
+      writeSourceItem(FileName + ".src", Device, Img, SpecConsts,
+                      BuildOptionsString);
+    }
+  } catch (...) {
+    // If a problem happens on storing cache item, do nothing
+  }
+}
+
+/* Program binaries built for one or more devices are read from persistent
+ * cache and returned in form of vector of programs. Each binary program is
+ * stored in vector of chars.
+ */
+std::vector<std::vector<char>> PersistentDeviceCodeCache::getItemFromDisc(
+    const device &Device, const RTDeviceBinaryImage &Img,
+    const SerializedObj &SpecConsts, const std::string &BuildOptionsString,
+    RT::PiProgram &NativePrg) {
+
+  if (!isImageCached(Img))
+    return {};
+
+  std::string Path =
+      getCacheItemPath(Device, Img, SpecConsts, BuildOptionsString);
+
+  if (!OSUtil::isPathPresent(Path))
+    return {};
+
+  int i = 0;
+
+  std::string FileName{Path + "/" + std::to_string(i)};
+  while (OSUtil::isPathPresent(FileName + ".bin") ||
+         OSUtil::isPathPresent(FileName + ".src")) {
+
+    if (!LockCacheItem::isLocked(FileName) &&
+        isCacheItemSrcEqual(FileName + ".src", Device, Img, SpecConsts,
+                            BuildOptionsString)) {
+      try {
+        return readBinaryDataFromFile(FileName + ".bin");
+      } catch (...) {
+        // If read was unsuccessfull try the next item
+      }
+    }
+    FileName = Path + "/" + std::to_string(++i);
+  }
+  return {};
+}
+
+/* Returns string value which can be used to identify different device
+ */
+std::string PersistentDeviceCodeCache::getDeviceIDString(const device &Device) {
+  return Device.get_platform().get_info<sycl::info::platform::name>() + "/" +
+         Device.get_info<sycl::info::device::name>() + "/" +
+         Device.get_info<sycl::info::device::version>() + "/" +
+         Device.get_info<sycl::info::device::driver_version>();
+}
+
+/* Write built binary to persistent cache
+ * Format: numImages, 1stImageSize, Image[, NthImageSize, NthImage...]
+ * Return on first unsuccessfull file operation
+ */
+void PersistentDeviceCodeCache::writeBinaryDataToFile(
+    const std::string &FileName, const std::vector<std::vector<char>> &Data) {
+  std::ofstream FileStream{FileName, std::ios::binary};
+
+  size_t Size = Data.size();
+  FileStream.write((char *)&Size, sizeof(Size));
+
+  for (size_t i = 0; i < Data.size(); ++i) {
+    Size = Data[i].size();
+    FileStream.write((char *)&Size, sizeof(Size));
+    FileStream.write(Data[i].data(), Size);
+  }
+  FileStream.close();
+  if (FileStream.fail())
+    trace("Failed to write binary file " + FileName);
+}
+
+/* Read built binary to persistent cache
+ * Format: numImages, 1stImageSize, Image[, NthImageSize, NthImage...]
+ */
+std::vector<std::vector<char>>
+PersistentDeviceCodeCache::readBinaryDataFromFile(const std::string &FileName) {
+  std::ifstream FileStream{FileName, std::ios::binary};
+  size_t ImgNum = 0, ImgSize = 0;
+  FileStream.read((char *)&ImgNum, sizeof(ImgNum));
+
+  std::vector<std::vector<char>> Res(ImgNum);
+  for (size_t i = 0; i < ImgNum; ++i) {
+    FileStream.read((char *)&ImgSize, sizeof(ImgSize));
+
+    std::vector<char> ImgData(ImgSize);
+    FileStream.read(ImgData.data(), ImgSize);
+
+    Res[i] = std::move(ImgData);
+  }
+  FileStream.close();
+
+  if (FileStream.fail()) {
+    trace("Failed to read binary file from " + FileName);
+    return {};
+  }
+
+  return Res;
+}
+
+/* Writing cache item key sources to be used for reliable identification
+ * Format: Four pairs of [size, value] for device, build options, specialization
+ * constant values, device code SPIR-V image.
+ */
+void PersistentDeviceCodeCache::writeSourceItem(
+    const std::string &FileName, const device &Device,
+    const RTDeviceBinaryImage &Img, const SerializedObj &SpecConsts,
+    const std::string &BuildOptionsString) {
+  std::ofstream FileStream{FileName, std::ios::binary};
+
+  std::string DeviceString{getDeviceIDString(Device)};
+  size_t Size = DeviceString.size();
+  FileStream.write((char *)&Size, sizeof(Size));
+  FileStream.write(DeviceString.data(), Size);
+
+  Size = BuildOptionsString.size();
+  FileStream.write((char *)&Size, sizeof(Size));
+  FileStream.write(BuildOptionsString.data(), Size);
+
+  Size = SpecConsts.size();
+  FileStream.write((char *)&Size, sizeof(Size));
+  FileStream.write((const char *)SpecConsts.data(), Size);
+
+  Size = Img.getSize();
+  FileStream.write((char *)&Size, sizeof(Size));
+  FileStream.write((const char *)Img.getRawData().BinaryStart, Size);
+  FileStream.close();
+
+  if (FileStream.fail()) {
+    trace("Failed to write source file to " + FileName);
+  }
+}
+
+/* Check that cache item key sources are equal to the current program.
+ * If file read operations fail cache item is treated as not equal.
+ */
+bool PersistentDeviceCodeCache::isCacheItemSrcEqual(
+    const std::string &FileName, const device &Device,
+    const RTDeviceBinaryImage &Img, const SerializedObj &SpecConsts,
+    const std::string &BuildOptionsString) {
+  std::ifstream FileStream{FileName, std::ios::binary};
+
+  std::string ImgString{(const char *)Img.getRawData().BinaryStart,
+                        Img.getSize()};
+  std::string SpecConstsString{(const char *)SpecConsts.data(),
+                               SpecConsts.size()};
+
+  size_t Size = 0;
+  FileStream.read((char *)&Size, sizeof(Size));
+  std::string res(Size, '\0');
+  FileStream.read(&res[0], Size);
+  if (getDeviceIDString(Device).compare(res))
+    return false;
+
+  FileStream.read((char *)&Size, sizeof(Size));
+  res.resize(Size);
+  FileStream.read(&res[0], Size);
+  if (BuildOptionsString.compare(0, Size, res.data()))
+    return false;
+
+  FileStream.read((char *)&Size, sizeof(Size));
+  res.resize(Size);
+  FileStream.read(&res[0], Size);
+  if (SpecConstsString.compare(res))
+    return false;
+
+  FileStream.read((char *)&Size, sizeof(Size));
+  res.resize(Size);
+  FileStream.read(&res[0], Size);
+  if (ImgString.compare(res))
+    return false;
+
+  FileStream.close();
+
+  if (FileStream.fail()) {
+    trace("Failed to read source file from " + FileName);
+  }
+
+  return true;
+}
+
+/* Returns directory name to store specific kernel image for specified
+ * device, build options and specialization constants values.
+ */
+std::string PersistentDeviceCodeCache::getCacheItemPath(
+    const device &Device, const RTDeviceBinaryImage &Img,
+    const SerializedObj &SpecConsts, const std::string &BuildOptionsString) {
+  static std::string cache_root{getRootDir()};
+
+  std::string ImgString{(const char *)Img.getRawData().BinaryStart,
+                        Img.getSize()};
+  std::string DeviceString{getDeviceIDString(Device)};
+  std::string SpecConstsString{(const char *)SpecConsts.data(),
+                               SpecConsts.size()};
+  std::hash<std::string> StringHasher{};
+
+  return cache_root + "/" + std::to_string(StringHasher(DeviceString)) + "/" +
+         std::to_string(StringHasher(ImgString)) + "/" +
+         std::to_string(StringHasher(SpecConstsString)) + "/" +
+         std::to_string(StringHasher(BuildOptionsString));
+}
+
+/* Returns true if persistent cache enabled. The cache can be disabled by
+ * setting SYCL_CACHE_EVICTION_DISABLE environmnet variable.
+ */
+bool PersistentDeviceCodeCache::isEnabled() {
+  static const char *PersistenCacheDisabled =
+      SYCLConfig<SYCL_CACHE_DISABLE_PERSISTENT>::get();
+  return !PersistenCacheDisabled;
+}
+
+/* Returns path for device code cache root directory
+ */
+std::string PersistentDeviceCodeCache::getRootDir() {
+  static const char *RootDir = SYCLConfig<SYCL_CACHE_DIR>::get();
+  if (RootDir)
+    return RootDir;
+
+  constexpr char DeviceCodeCacheDir[] = "/libsycl_cache";
+
+  // Use static to calculate directory only once per program run
+#if defined(__SYCL_RT_OS_LINUX)
+  static const char *CacheDir = std::getenv("XDG_CACHE_HOME");
+  static const char *HomeDir = std::getenv("HOME");
+  static std::string Res{
+      std::string(CacheDir
+                      ? CacheDir
+                      : (HomeDir ? std::string(HomeDir) + "/.cache" : ".")) +
+      DeviceCodeCacheDir};
+#else
+  static const char *AppDataDir = std::getenv("AppData");
+  static std::string Res{std::string(AppDataDir ? AppDataDir : ".") +
+                         DeviceCodeCacheDir};
+#endif
+  return Res;
+}
+
+} // namespace detail
+} // namespace sycl
+} // __SYCL_INLINE_NAMESPACE(cl)

commit 4e030a1793085abe78788449af7cf3fb17366c96

[SYCL][NFC] Fix post-commit failure (#3532)
Fixed post-commit regression introduced by ec9716558d8944906df5b0738c065737bcf08563.

sycl/source/detail/persistent_device_code_cache.cpp
@@ -126,8 +126,7 @@ void PersistentDeviceCodeCache::putItemToDisc(
  */
 std::vector<std::vector<char>> PersistentDeviceCodeCache::getItemFromDisc(
     const device &Device, const RTDeviceBinaryImage &Img,
-    const SerializedObj &SpecConsts, const std::string &BuildOptionsString,
-    RT::PiProgram &NativePrg) {
+    const SerializedObj &SpecConsts, const std::string &BuildOptionsString) {
 
   if (!isImageCached(Img))
     return {};

sycl/source/detail/persistent_device_code_cache.hpp
@@ -174,8 +174,7 @@ public:
   static std::vector<std::vector<char>>
   getItemFromDisc(const device &Device, const RTDeviceBinaryImage &Img,
                   const SerializedObj &SpecConsts,
-                  const std::string &BuildOptionsString,
-                  RT::PiProgram &NativePrg);
+                  const std::string &BuildOptionsString);
 
   /* Stores build program in persisten cache
    */

sycl/source/detail/program_manager/program_manager.cpp
@@ -423,7 +423,7 @@ RT::PiProgram ProgramManager::getBuiltPIProgram(OSModuleHandle M,
     RT::PiProgram NativePrg;
 
     auto BinProg = PersistentDeviceCodeCache::getItemFromDisc(
-        Device, Img, SpecConsts, CompileOpts + LinkOpts, NativePrg);
+        Device, Img, SpecConsts, CompileOpts + LinkOpts);
     if (BinProg.size()) {
       // TODO: Build for multiple devices once supported by program manager
       NativePrg = createBinaryProgram(ContextImpl, Device,

sycl/unittests/kernel-and-program/PersistentDeviceCodeCache.cpp
@@ -119,7 +119,7 @@ public:
             Dev, Img,
             sycl::vector_class<unsigned char>(
                 {'S', 'p', 'e', 'c', 'C', 'o', 'n', 's', 't', ProgramID}),
-            BuildOptions, NativeProg);
+            BuildOptions);
         for (int i = 0; i < Res.size(); ++i) {
           for (int j = 0; j < Res[i].size(); ++j) {
             assert(Res[i][j] == i &&
@@ -187,8 +187,8 @@ TEST_F(PersistenDeviceCodeCache, CorruptedCacheFiles) {
                                                    NativeProg);
   assert(!llvm::sys::fs::remove(ItemDir + "/0.bin") &&
          "Failed to remove binary file");
-  auto Res = detail::PersistentDeviceCodeCache::getItemFromDisc(
-      Dev, Img, {}, BuildOptions, NativeProg);
+  auto Res = detail::PersistentDeviceCodeCache::getItemFromDisc(Dev, Img, {},
+                                                                BuildOptions);
   assert(Res.size() == 0 && "Item with missed binary file was read");
   llvm::sys::fs::remove_directories(ItemDir);
 
@@ -197,8 +197,8 @@ TEST_F(PersistenDeviceCodeCache, CorruptedCacheFiles) {
                                                    NativeProg);
   assert(!llvm::sys::fs::remove(ItemDir + "/0.src") &&
          "Failed to remove source file");
-  Res = detail::PersistentDeviceCodeCache::getItemFromDisc(
-      Dev, Img, {}, BuildOptions, NativeProg);
+  Res = detail::PersistentDeviceCodeCache::getItemFromDisc(Dev, Img, {},
+                                                           BuildOptions);
   assert(Res.size() == 0 && "Item with missed source file was read");
   llvm::sys::fs::remove_directories(ItemDir);
 
@@ -213,8 +213,8 @@ TEST_F(PersistenDeviceCodeCache, CorruptedCacheFiles) {
   FileStream << 2 << 12 << "123456789012" << 23 << "1234";
   FileStream.close();
   assert((!FileStream.fail()) && "Failed to create trancated binary file");
-  Res = detail::PersistentDeviceCodeCache::getItemFromDisc(
-      Dev, Img, {}, BuildOptions, NativeProg);
+  Res = detail::PersistentDeviceCodeCache::getItemFromDisc(Dev, Img, {},
+                                                           BuildOptions);
   assert(Res.size() == 0 && "Item with corrupted binary file was read");
 
   llvm::sys::fs::remove_directories(ItemDir);
@@ -226,8 +226,8 @@ TEST_F(PersistenDeviceCodeCache, CorruptedCacheFiles) {
     std::ofstream FileStream(ItemDir + "/0.src",
                              std::ofstream::out | std::ofstream::trunc);
   }
-  Res = detail::PersistentDeviceCodeCache::getItemFromDisc(
-      Dev, Img, {}, BuildOptions, NativeProg);
+  Res = detail::PersistentDeviceCodeCache::getItemFromDisc(Dev, Img, {},
+                                                           BuildOptions);
   assert(Res.size() == 0 && "Item with corrupted binary file was read");
   llvm::sys::fs::remove_directories(ItemDir);
 }
@@ -256,8 +256,8 @@ TEST_F(PersistenDeviceCodeCache, LockFile) {
   { std::ofstream File{LockFile}; }
 
   // Cache item is locked, cache miss happens on read
-  auto Res = detail::PersistentDeviceCodeCache::getItemFromDisc(
-      Dev, Img, {}, BuildOptions, NativeProg);
+  auto Res = detail::PersistentDeviceCodeCache::getItemFromDisc(Dev, Img, {},
+                                                                BuildOptions);
   assert(Res.size() == 0 && "Locked item was read");
 
   // Cache item is locked - new cache item to be created
@@ -267,14 +267,14 @@ TEST_F(PersistenDeviceCodeCache, LockFile) {
 
   // Second cache item is locked, cache miss happens on read
   { std::ofstream File{ItemDir + "/1.lock"}; }
-  Res = detail::PersistentDeviceCodeCache::getItemFromDisc(
-      Dev, Img, {}, BuildOptions, NativeProg);
+  Res = detail::PersistentDeviceCodeCache::getItemFromDisc(Dev, Img, {},
+                                                           BuildOptions);
   assert(Res.size() == 0 && "Locked item was read");
 
   // First cache item was unlocked and successfully read
   std::remove(LockFile.c_str());
-  Res = detail::PersistentDeviceCodeCache::getItemFromDisc(
-      Dev, Img, {}, BuildOptions, NativeProg);
+  Res = detail::PersistentDeviceCodeCache::getItemFromDisc(Dev, Img, {},
+                                                           BuildOptions);
   for (int i = 0; i < Res.size(); ++i) {
     for (int j = 0; j < Res[i].size(); ++j) {
       assert(Res[i][j] == i && "Corrupted image loaded from persistent cache");
@@ -305,8 +305,8 @@ TEST_F(PersistenDeviceCodeCache, AccessDeniedForCacheDir) {
   assert(llvm::sys::fs::exists(ItemDir + "/1.bin") && "No file created");
 
   llvm::sys::fs::setPermissions(ItemDir + "/1.bin", llvm::sys::fs::no_perms);
-  auto Res = detail::PersistentDeviceCodeCache::getItemFromDisc(
-      Dev, Img, {}, BuildOptions, NativeProg);
+  auto Res = detail::PersistentDeviceCodeCache::getItemFromDisc(Dev, Img, {},
+                                                                BuildOptions);
 
   // No image to be read due to lack of permissions fro source file
   assert(Res.size() == 0);
@@ -314,8 +314,8 @@ TEST_F(PersistenDeviceCodeCache, AccessDeniedForCacheDir) {
   llvm::sys::fs::setPermissions(ItemDir + "/0.bin", llvm::sys::fs::all_perms);
   llvm::sys::fs::setPermissions(ItemDir + "/1.bin", llvm::sys::fs::all_perms);
 
-  Res = detail::PersistentDeviceCodeCache::getItemFromDisc(
-      Dev, Img, {}, BuildOptions, NativeProg);
+  Res = detail::PersistentDeviceCodeCache::getItemFromDisc(Dev, Img, {},
+                                                           BuildOptions);
   // Image should be successfully read
   for (int i = 0; i < Res.size(); ++i) {
     for (int j = 0; j < Res[i].size(); ++j) {