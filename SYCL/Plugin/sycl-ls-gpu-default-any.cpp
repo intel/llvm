667d447d18dfb8f1172ed34b6bba765834dce06b
Steffen Larsen <56076654+steffenlarsen@users.noreply.github.com>
2021-04-13 08:47:09 +0100

    [SYCL] Allow default selected GPU to be from any available platform (intel/llvm-test-suite#224)
    Signed-off-by: Steffen Larsen <steffen.larsen@codeplay.com>

--- SYCL/Plugin/sycl-ls-gpu-default-any.cpp
+++ SYCL/Plugin/sycl-ls-gpu-default-any.cpp
@@ -3,13 +3,18 @@
 // RUN: env --unset=SYCL_DEVICE_FILTER sycl-ls --verbose >%t.default.out
 // RUN: FileCheck %s --check-prefixes=CHECK-GPU-BUILTIN,CHECK-GPU-CUSTOM --input-file %t.default.out
 
-// CHECK-GPU-BUILTIN: gpu_selector(){{.*}}GPU : {{.*}}Level-Zero
-// CHECK-GPU-CUSTOM: custom_selector(gpu){{.*}}GPU : {{.*}}Level-Zero
+// CHECK-GPU-BUILTIN: gpu_selector(){{.*}}GPU : {{.*}}{{Level-Zero|CUDA|OpenCL}}
+// clang-format off
+// CHECK-GPU-CUSTOM: custom_selector(gpu){{.*}}GPU : {{.*}}{{Level-Zero|CUDA|OpenCL}}
+// clang-format on
 
-//==-- sycl-ls-gpu-default.cpp - SYCL test for default selected gpu device -==//
+//==--------------------- sycl-ls-gpu-default-any.cpp ----------------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+
+// This test checks that a valid GPU is returned by sycl-ls by default if one
+// is present.
---
+++ SYCL/Plugin/sycl-ls-gpu-default-level-zero.cpp
@@ -0,0 +1,18 @@
+// REQUIRES: gpu, level-zero
+
+// RUN: env --unset=SYCL_DEVICE_FILTER sycl-ls --verbose >%t.default.out
+// RUN: FileCheck %s --check-prefixes=CHECK-GPU-BUILTIN,CHECK-GPU-CUSTOM --input-file %t.default.out
+
+// CHECK-GPU-BUILTIN: gpu_selector(){{.*}}GPU : {{.*}}Level-Zero
+// CHECK-GPU-CUSTOM: custom_selector(gpu){{.*}}GPU : {{.*}}Level-Zero
+
+//==------------------ sycl-ls-gpu-default-level-zero.cpp ------------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// This test checks that, if available, a Level-Zero GPU will be selected by
+// the default GPU selector.
