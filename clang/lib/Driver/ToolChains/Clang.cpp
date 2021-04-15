3a995586491ef9da383ff71d0599a8e20b3c5b07
mdtoguchi <47896532+mdtoguchi@users.noreply.github.com>
2021-04-15 00:06:51 -0700

    [Driver][SYCL][FPGA] Use opencl-aot for AOT when doing FPGA emulation (#3440)
    When performing AOT compilation for FPGA, we call 'aoc' to perform
    the offline compilation. Update this behavior to use opencl-aot when
    we know we are performing emulation instead of running on hardware.
    
    When creating FPGA AOCX/AOCR archives when performing emulation, create
    archives which contain .spv items as opposed to using the regular aocx
    and aocr binaries.
    
    The expected input type for AOCX/AOCR were considered for hardware
    usage with FPGA. In this part, we allow for the differentiation of
    the type of archive created, whether it be emulation or hardware.
    Depending on how these archives are created, we make sure we take
    the proper flow to process the contents.
    
    Also perform some cleanup in the needed triple arch representation
    for fpga, where we didn't need to define all of the different types
    we were wrapping for FPGA specific binaries.

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -7774,7 +7774,7 @@ void OffloadBundler::ConstructJob(Compilation &C, const JobAction &JA,
     Triples += ',';
     Triples += Action::GetOffloadKindName(Action::OFK_SYCL);
     Triples += '-';
-    Triples += llvm::Triple::getArchTypeName(llvm::Triple::fpga_dep);
+    Triples += types::getTypeName(types::TY_FPGA_Dependencies);
   }
   CmdArgs.push_back(TCArgs.MakeArgString(Triples));
 
@@ -7841,7 +7841,9 @@ void OffloadBundler::ConstructJobMultipleOutputs(
   bool IsFPGADepUnbundle = JA.getType() == types::TY_FPGA_Dependencies;
   bool IsFPGADepLibUnbundle = JA.getType() == types::TY_FPGA_Dependencies_List;
 
-  if (InputType == types::TY_FPGA_AOCX || InputType == types::TY_FPGA_AOCR) {
+  if (InputType == types::TY_FPGA_AOCX || InputType == types::TY_FPGA_AOCR ||
+      InputType == types::TY_FPGA_AOCX_EMU ||
+      InputType == types::TY_FPGA_AOCR_EMU) {
     // Override type with AOCX/AOCR which will unbundle to a list containing
     // binaries with the appropriate file extension (.aocx/.aocr).
     // TODO - representation of the output file from the unbundle for these
@@ -7849,9 +7851,11 @@ void OffloadBundler::ConstructJobMultipleOutputs(
     // better in the output extension and type for improved understanding
     // of file contents and debuggability.
     if (getToolChain().getTriple().getSubArch() ==
-        llvm::Triple::SPIRSubArch_fpga)
-      TypeArg = InputType == types::TY_FPGA_AOCX ? "aocx" : "aocr";
-    else
+        llvm::Triple::SPIRSubArch_fpga) {
+      bool isAOCX = InputType == types::TY_FPGA_AOCX ||
+                    InputType == types::TY_FPGA_AOCX_EMU;
+      TypeArg = isAOCX ? "aocx" : "aocr";
+    } else
       TypeArg = "aoo";
   }
   if (InputType == types::TY_FPGA_AOCO || IsFPGADepLibUnbundle)
@@ -7919,7 +7923,7 @@ void OffloadBundler::ConstructJobMultipleOutputs(
     // file as it does not match the type being bundled.
     Triples += Action::GetOffloadKindName(Action::OFK_SYCL);
     Triples += '-';
-    Triples += llvm::Triple::getArchTypeName(llvm::Triple::fpga_dep);
+    Triples += types::getTypeName(types::TY_FPGA_Dependencies);
   }
   CmdArgs.push_back(TCArgs.MakeArgString(Triples));
 
@@ -7993,9 +7997,12 @@ void OffloadWrapper::ConstructJob(Compilation &C, const JobAction &JA,
     // to the target triple setting.
     if (TT.getSubArch() == llvm::Triple::SPIRSubArch_fpga &&
         TCArgs.hasArg(options::OPT_fsycl_link_EQ)) {
+      SmallString<16> FPGAArch("fpga_");
       auto *A = C.getInputArgs().getLastArg(options::OPT_fsycl_link_EQ);
-      TT.setArchName((A->getValue() == StringRef("early")) ? "fpga_aocr"
-                                                           : "fpga_aocx");
+      FPGAArch += A->getValue() == StringRef("early") ? "aocr" : "aocx";
+      if (C.getDriver().isFPGAEmulationMode())
+        FPGAArch += "_emu";
+      TT.setArchName(FPGAArch);
       TT.setVendorName("intel");
       TT.setEnvironment(llvm::Triple::SYCLDevice);
       TargetTripleOpt = TT.str();
@@ -8032,6 +8039,7 @@ void OffloadWrapper::ConstructJob(Compilation &C, const JobAction &JA,
     if (TC.getTriple().getSubArch() == llvm::Triple::NoSubArch) {
       // Only store compile/link opts in the image descriptor for the SPIR-V
       // target; AOT compilation has already been performed otherwise.
+      TC.AddImpliedTargetArgs(TT, TCArgs, BuildArgs);
       TC.TranslateBackendTargetArgs(TCArgs, BuildArgs);
       createArgString("-compile-opts=");
       BuildArgs.clear();
