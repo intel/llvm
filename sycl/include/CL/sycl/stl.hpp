78b61e9ba1df0608d3ef78fc4e8ce80902de4760
Chris Perkins <chris.perkins@intel.com>
2021-04-16 19:12:35 -0700

    [SYCL] add span support
    SYCL2020 spec adds span to the sycl namespace, even when not compiling C++20.  I have adapted the Clang libcxx version of span here, divorcing it from its macros and private access to implementation details, but otherwise mostly intact.
    
    Signed-off-by: Chris Perkins <chris.perkins@intel.com>

--- sycl/include/CL/sycl/stl.hpp
+++ sycl/include/CL/sycl/stl.hpp
@@ -11,6 +11,7 @@
 // 4.5 C++ Standard library classes required for the interface
 
 #include <CL/sycl/detail/defines.hpp>
+#include <CL/sycl/sycl_span.hpp>
 
 #include <exception>
 #include <functional>
