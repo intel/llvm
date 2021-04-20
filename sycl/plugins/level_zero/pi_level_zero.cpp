c8604c7a088fea6c0d8d57c4679766decf73ff8c
rdeodhar <rajiv.deodhar@intel.com>
2021-04-19 19:46:13 -0700

    Minor change.

--- sycl/plugins/level_zero/pi_level_zero.cpp
+++ sycl/plugins/level_zero/pi_level_zero.cpp
@@ -2013,9 +2013,9 @@ pi_result piDevicePartition(pi_device Device,
           return Result;
         }
         OutDevices[I] = PiSubDevice.get();
-        Platform->PiDevicesCache.push_back(std::move(PiSubDevice));
         // save pointers to sub-devices for quick retrieval in the future.
-        Device->SubDevices.push_back(OutDevices[I]);
+        Device->SubDevices.push_back(PiSubDevice.get());
+        Platform->PiDevicesCache.push_back(std::move(PiSubDevice));
       }
     }
     delete[] ZeSubdevices;
