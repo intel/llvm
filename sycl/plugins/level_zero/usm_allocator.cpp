194f6409af3ec893f1deae787c67f2ece9cd8f7f
rdeodhar <rajiv.deodhar@intel.com>
2021-04-21 22:39:54 -0700

    [SYCL] Remove unused parameter (#3592)

--- sycl/plugins/level_zero/usm_allocator.cpp
+++ sycl/plugins/level_zero/usm_allocator.cpp
@@ -281,7 +281,7 @@ public:
   void freeChunk(void *Ptr, Slab &Slab);
 
   // Free an allocation that is a full slab in this bucket.
-  void freeSlab(void *Ptr, Slab &Slab);
+  void freeSlab(Slab &Slab);
 
   SystemMemory &getMemHandle();
   USMAllocContext::USMAllocImpl &getUsmAllocCtx() { return OwnAllocCtx; }
@@ -498,7 +498,7 @@ void *Bucket::getSlab() {
   return FreeSlab;
 }
 
-void Bucket::freeSlab(void *Ptr, Slab &Slab) {
+void Bucket::freeSlab(Slab &Slab) {
   std::lock_guard<std::mutex> Lg(BucketLock);
   auto SlabIter = Slab.getIterator();
   assert(SlabIter != UnavailableSlabs.end());
@@ -671,7 +671,7 @@ void USMAllocContext::USMAllocImpl::deallocate(void *Ptr) {
       if (Bucket.getSize() <= settings::ChunkCutOff) {
         Bucket.freeChunk(Ptr, Slab);
       } else {
-        Bucket.freeSlab(Ptr, Slab);
+        Bucket.freeSlab(Slab);
       }
       return;
     }
