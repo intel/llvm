39ac038d5b5239f2ed85721535742487c4a34d90
DenisBakhvalov <61807338+DenisBakhvalov@users.noreply.github.com>
2021-04-30 13:00:23 -0700

    [ESIMD] Lower __esimd_lane_id intrinsics (#3661)
    * [ESIMD] Lower __esimd_lane_id intrinsics
    
    This patch is one of the few patches to provide a functionality to reuse
    existing scalar function implementations in a vector context.

--- llvm/lib/SYCLLowerIR/LowerESIMD.cpp
+++ llvm/lib/SYCLLowerIR/LowerESIMD.cpp
@@ -383,7 +383,7 @@ public:
         {"ssdp4a_sat", {"ssdp4a.sat", {a(0), a(1), a(2)}}},
         {"any", {"any", {ai1(0)}}},
         {"all", {"all", {ai1(0)}}},
-    };
+        {"lane_id", {"lane.id", {}}}};
   }
 
   const IntrinTable &getTable() { return Table; }
