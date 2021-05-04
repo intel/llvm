72cddd14c4a79a1ac42a4ddc9c223c314b3abe09
kbobrovs <Konstantin.S.Bobrovsky@intel.com>
2021-05-04 11:29:24 -0700

    Fix line numbers in negative tests.
    Signed-off-by: kbobrovs <Konstantin.S.Bobrovsky@intel.com>

--- sycl/test/esimd/simd_copy_to_copy_from.cpp
+++ sycl/test/esimd/simd_copy_to_copy_from.cpp
@@ -42,13 +42,13 @@ kernel3(accessor<int, 1, access::mode::read_write, access::target::local> &buf)
   simd<int, 32> v1(0, 1);
   simd<int, 32> v0;
   // expected-error@+3 {{no matching member function for call to 'copy_from'}}
-  // expected-note@CL/sycl/INTEL/esimd/esimd.hpp:513 {{}}
-  // expected-note@CL/sycl/INTEL/esimd/esimd.hpp:508 {{}}
+  // expected-note@CL/sycl/INTEL/esimd/esimd.hpp:514 {{}}
+  // expected-note@CL/sycl/INTEL/esimd/esimd.hpp:509 {{}}
   v0.copy_from(buf, 0);
   v0 = v0 + v1;
   // expected-error@+3 {{no matching member function for call to 'copy_to'}}
   // expected-note@CL/sycl/INTEL/esimd/esimd.hpp:497 {{}}
-  // expected-note@CL/sycl/INTEL/esimd/esimd.hpp:523 {{}}
+  // expected-note@CL/sycl/INTEL/esimd/esimd.hpp:525 {{}}
   v0.copy_to(buf, 0);
 }
 
@@ -58,8 +58,8 @@ SYCL_EXTERNAL void kernel4(
     SYCL_ESIMD_FUNCTION {
   simd<int, 32> v;
   // expected-error@+3 {{no matching member function for call to 'copy_from'}}
-  // expected-note@CL/sycl/INTEL/esimd/esimd.hpp:513 {{}}
-  // expected-note@CL/sycl/INTEL/esimd/esimd.hpp:508 {{}}
+  // expected-note@CL/sycl/INTEL/esimd/esimd.hpp:514 {{}}
+  // expected-note@CL/sycl/INTEL/esimd/esimd.hpp:509 {{}}
   v.copy_from(buf, 0);
 }
 
@@ -70,6 +70,6 @@ SYCL_EXTERNAL void kernel5(
   simd<int, 32> v(0, 1);
   // expected-error@+3 {{no matching member function for call to 'copy_to'}}
   // expected-note@CL/sycl/INTEL/esimd/esimd.hpp:497 {{}}
-  // expected-note@CL/sycl/INTEL/esimd/esimd.hpp:523 {{}}
+  // expected-note@CL/sycl/INTEL/esimd/esimd.hpp:525 {{}}
   v.copy_to(buf, 0);
 }
