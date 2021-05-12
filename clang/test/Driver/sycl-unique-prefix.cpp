d47e31cff4565cb575953e8b97374dc9baf58ded
Michael D Toguchi <michael.d.toguchi@intel.com>
2021-05-12 10:38:26 -0700

    Update to allow for preprocessed files to be included for the ID

--- clang/test/Driver/sycl-unique-prefix.cpp
+++ clang/test/Driver/sycl-unique-prefix.cpp
@@ -9,3 +9,10 @@
 // CHECK_PREFIX: clang{{.*}} "-triple" "spir64-unknown-unknown-sycldevice"{{.*}} "-fsycl-is-device"{{.*}} "-fsycl-unique-prefix=[[PREFIX2:([A-z0-9]){16}]]"{{.*}} "{{.*}}_file2.cpp"
 // CHECK_PREFIX: clang{{.*}} "-triple" "spir64_gen-unknown-unknown-sycldevice"{{.*}} "-fsycl-is-device"{{.*}} "-fsycl-unique-prefix=[[PREFIX2]]"{{.*}} "{{.*}}_file2.cpp"
 // CHECK_PREFIX: clang{{.*}} "-fsycl-unique-prefix=[[PREFIX2]]"{{.*}} "-fsycl-is-host"{{.*}}  "{{.*}}_file2.cpp"
+
+/// Check for prefix with preprocessed input
+// RUN: touch %t.ii
+// RUN: %clangxx -fsycl -c %t.ii -### 2>&1 \
+// RUN:  | FileCheck -check-prefix=CHECK_PREFIX_II %s
+// CHECK_PREFIX_II: clang{{.*}} "-fsycl-is-device"{{.*}} "-fsycl-unique-prefix=[[PREFIX:([A-z0-9]){16}]]"{{.*}} "{{.*}}.ii"
+// CHECK_PREFIX_II: clang{{.*}} "-fsycl-unique-prefix=[[PREFIX]]"{{.*}} "-fsycl-is-host"{{.*}} "{{.*}}.ii"
