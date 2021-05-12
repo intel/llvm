bd5cafba0fc47a3092aa4f971a05eec2090b62eb
Viktoria Maksimova <viktoria.maksimova@intel.com>
2021-05-12 18:41:36 +0300

    Reland "Fix macro"
    This reverts commit f7d6423442aa70145915a069e01d7e2f46622842.

--- llvm-spirv/lib/SPIRV/libSPIRV/SPIRVInstruction.h
+++ llvm-spirv/lib/SPIRV/libSPIRV/SPIRVInstruction.h
@@ -2916,7 +2916,7 @@ protected:
   typedef SPIRVInstTemplate<SPIRVExpectKHRInstBase, Op##x, __VA_ARGS__>        \
       SPIRV##x;
 _SPIRV_OP(ExpectKHR, true, 5)
-#undef _SPIRV_OP_INTERNAL
+#undef _SPIRV_OP
 
 class SPIRVSubgroupShuffleINTELInstBase : public SPIRVInstTemplateBase {
 protected:
