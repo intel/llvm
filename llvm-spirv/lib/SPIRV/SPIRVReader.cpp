5007a651b74d83d07405da7dd35e56a79c42bde2
Sven van Haastregt <sven.vanhaastregt@arm.com>
2021-05-13 15:52:37 +0100

    Support OpSpecConstantOp in array type length
    Original commit:
    https://github.com/KhronosGroup/SPIRV-LLVM-Translator/commit/1b1dc0e

--- llvm-spirv/lib/SPIRV/SPIRVReader.cpp
+++ llvm-spirv/lib/SPIRV/SPIRVReader.cpp
@@ -479,9 +479,14 @@ Type *SPIRVToLLVM::transType(SPIRVType *T, bool IsClassMember) {
     return mapType(T, Type::getIntNTy(*Context, T->getIntegerBitWidth()));
   case OpTypeFloat:
     return mapType(T, transFPType(T));
-  case OpTypeArray:
+  case OpTypeArray: {
+    // The length might be an OpSpecConstantOp, that needs to be specialized
+    // and evaluated before the LLVM ArrayType can be constructed.
+    auto *LenExpr = static_cast<const SPIRVTypeArray *>(T)->getLength();
+    auto *LenValue = cast<ConstantInt>(transValue(LenExpr, nullptr, nullptr));
     return mapType(T, ArrayType::get(transType(T->getArrayElementType()),
-                                     T->getArrayLength()));
+                                     LenValue->getZExtValue()));
+  }
   case internal::OpTypeTokenINTEL:
     return mapType(T, Type::getTokenTy(*Context));
   case OpTypePointer:
