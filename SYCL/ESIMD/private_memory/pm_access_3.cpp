f5181b49be4edbfd96df7eab144932b61dc3ee72
Alexey Sachkov <alexey.sachkov@intel.com>
2021-05-14 12:33:33 +0300

    [SYCL] Disable two more failing tests (intel/llvm-test-suite#282)
    ESIMD private_memory tests sporadically fail with the following error:
    "LLVM ERROR: Cannot find pointer replacement", because of regression in
    VC backend.

--- SYCL/ESIMD/private_memory/pm_access_3.cpp
+++ SYCL/ESIMD/private_memory/pm_access_3.cpp
@@ -8,6 +8,6 @@
 
 // REQUIRES: gpu
 // Sporadic failure in GPU RT 21.16.19610
-// UNSUPPORTED: cuda || (level_zero && linux)
+// UNSUPPORTED: cuda || (level_zero && linux) || (opencl && linux)
 // RUN: %clangxx -fsycl -I%S/.. %S/Inputs/pm_common.cpp -o %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out 3
