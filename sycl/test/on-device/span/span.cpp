febe2d09f28d5c8353292cd2a215accec84bcb39
Artur Gainullin <artur.gainullin@intel.com>
2021-05-14 16:20:24 -0700

    Set introduced variable for span test because it uses indirect access

--- sycl/test/on-device/span/span.cpp
+++ sycl/test/on-device/span/span.cpp
@@ -1,6 +1,6 @@
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
-// RUN: %GPU_RUN_PLACEHOLDER %t.out
+// RUN: env SYCL_PI_LEVEL_ZERO_ENABLE_INDIRECT_ACCESS_SUPPORT=1 %GPU_RUN_PLACEHOLDER %t.out
 // RUN: %ACC_RUN_PLACEHOLDER %t.out
 
 #include <numeric>
