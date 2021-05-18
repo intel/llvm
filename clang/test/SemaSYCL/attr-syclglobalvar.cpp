5c0f8aff13d060c329afc0298666d685b21fd2e6
Jeffrey T Mott <jeffrey.t.mott@intel.com>
2021-05-17 20:39:32 -0700

    Expand documentation and tests.

--- clang/test/SemaSYCL/attr-syclglobalvar.cpp
+++ clang/test/SemaSYCL/attr-syclglobalvar.cpp
@@ -4,18 +4,51 @@
 
 __attribute__((sycl_global_var)) int GlobalWithAttribute;
 
-int GlobalNoAttribute;
+__attribute__((sycl_global_var)) extern int ExternGlobalWithAttribute;
+
+namespace NS {
+    __attribute__((sycl_global_var)) int NSGlobalWithAttribute;
+}
+
+union U {
+    int InstanceMember;
+};
+
+__attribute__((sycl_global_var)) U GlobalUnion;
+
+struct S {
+    __attribute__((sycl_global_var)) static int StaticMember;
+
+    // expected-warning@+1 {{attribute only applies to global variables}}
+    __attribute__((sycl_global_var)) int InstanceMember;
+};
+int S::StaticMember = 0;
+
+__attribute__((sycl_global_var)) S GlobalStruct;
+
+__attribute__((sycl_global_var)) static S StaticGlobal;
 
 // expected-error@+1 {{attribute takes no arguments}}
 __attribute__((sycl_global_var(42))) int GlobalWithAttributeArg;
 
+int GlobalNoAttribute;
+
 // expected-warning@+1 {{attribute only applies to global variables}}
 __attribute__((sycl_global_var)) void F() {
+    __attribute__((sycl_global_var)) static int StaticLocalVar;
+
     // expected-warning@+1 {{attribute only applies to global variables}}
     __attribute__((sycl_global_var)) int Local;
 
     cl::sycl::kernel_single_task<class kernel_name>([=] () {
         (void)GlobalWithAttribute;
+        (void)ExternGlobalWithAttribute;
+        (void)NS::NSGlobalWithAttribute;
+        (void)GlobalUnion.InstanceMember;
+        (void)S::StaticMember;
+        (void)GlobalStruct.InstanceMember;
+        (void)StaticGlobal.InstanceMember; // expected-error {{SYCL kernel cannot use a non-const static data variable}}
+        (void)StaticLocalVar; // expected-error {{SYCL kernel cannot use a non-const static data variable}}
         (void)GlobalNoAttribute; // expected-error {{SYCL kernel cannot use a non-const global variable}} expected-note@Inputs/sycl.hpp:* {{called by}}
     });
 }
