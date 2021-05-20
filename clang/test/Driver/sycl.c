b6fcaff20fc968b1ce2be8ce1affc9685a7f4bb6
Viktoria Maksimova <viktoria.maksimova@intel.com>
2021-05-20 17:33:10 +0300

    Fix Driver/sycl.c test

--- clang/test/Driver/sycl.c
+++ clang/test/Driver/sycl.c
@@ -111,8 +111,8 @@
 // ID_QUERIES: "-fsycl-id-queries-fit-in-int"
 // NO_ID_QUERIES: "-fno-sycl-id-queries-fit-in-int"
 
-// RUN: %clang -### -fsycl  %s 2>&1 | FileCheck %s --check-prefix=DEFAULT
-// RUN: %clangxx -### -fsycl %s 2>&1 | FileCheck %s --check-prefix=DEFAULT
-// RUN: %clang_cl -### -fsycl -- %s 2>&1 | FileCheck %s --check-prefix=DEFAULT
+// RUN: %clang -### -fsycl  %s 2>&1 | FileCheck %s --check-prefix=DEFAULT_STD
+// RUN: %clangxx -### -fsycl %s 2>&1 | FileCheck %s --check-prefix=DEFAULT_STD
+// RUN: %clang_cl -### -fsycl -- %s 2>&1 | FileCheck %s --check-prefix=DEFAULT_STD
 
-// DEFAULT: "-sycl-std=2020"
+// DEFAULT_STD: "-sycl-std=2020"
