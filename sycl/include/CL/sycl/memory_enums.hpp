9d3fbdfcc17cc0b68a28805580eef8592d46e30b
Alexander Batashev <alexander.batashev@intel.com>
2021-05-21 18:28:37 +0300

    Update comment

--- sycl/include/CL/sycl/memory_enums.hpp
+++ sycl/include/CL/sycl/memory_enums.hpp
@@ -1,4 +1,4 @@
-//==-------------- enums.hpp --- SYCL enums --------------------------------==//
+//==-------------- memory_enums.hpp --- SYCL enums -------------------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
