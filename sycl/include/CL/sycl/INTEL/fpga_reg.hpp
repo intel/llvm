079f31ab803d81cd3a4113ab454bbb8bb5ffcf26
Kannan, Ajaykumar <ajaykumar.kannan@intel.com>
2021-05-25 10:55:38 -0400

    Change fpga_reg to use a deprecation warning for old APIs

--- sycl/include/CL/sycl/INTEL/fpga_reg.hpp
+++ sycl/include/CL/sycl/INTEL/fpga_reg.hpp
@@ -18,9 +18,22 @@ namespace INTEL {
 // Returns a registered copy of the input
 // This function is intended for FPGA users to instruct the compiler to insert
 // at least one register stage between the input and the return value.
-template <typename _T> _T fpga_reg(_T t) {
-  static_assert(std::is_trivially_copyable<_T>::value,
-                "Type is not trivially_copyable.");
+template <typename _T>
+typename std::enable_if<std::is_trivially_copyable<_T>::value, _T>::type
+fpga_reg(_T t) {
+#if __has_builtin(__builtin_intel_fpga_reg)
+  return __builtin_intel_fpga_reg(t);
+#else
+  return t;
+#endif
+}
+
+template <typename _T>
+[[deprecated("INTEL::fpga_reg will only support trivially_copyable types in a "
+             "future release. The type used here will be disallowed.")]]
+typename std::enable_if<std::is_trivially_copyable<_T>::value == false,
+                        _T>::type
+fpga_reg(_T t) {
 #if __has_builtin(__builtin_intel_fpga_reg)
   return __builtin_intel_fpga_reg(t);
 #else
@@ -31,3 +44,13 @@ template <typename _T> _T fpga_reg(_T t) {
 } // namespace INTEL
 } // namespace sycl
 } // __SYCL_INLINE_NAMESPACE(cl)
+
+// Keep it consistent with FPGA attributes like intelfpga::memory()
+// Currently clang does not support nested namespace for attributes
+namespace intelfpga {
+template <typename _T>
+[[deprecated("intelfpga::fpga_reg will be removed in a future release.")]] _T
+fpga_reg(const _T &t) {
+  return cl::sycl::INTEL::fpga_reg(t);
+}
+} // namespace intelfpga
