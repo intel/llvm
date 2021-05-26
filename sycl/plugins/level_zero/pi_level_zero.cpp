a152887907d04849f9e13d36f48d9e9ce2f3017d
Sergey V Maslov <sergey.v.maslov@intel.com>
2021-05-25 19:09:00 -0700

    Allow >4GB memory allocations
    Signed-off-by: Sergey V Maslov <sergey.v.maslov@intel.com>

--- sycl/plugins/level_zero/pi_level_zero.cpp
+++ sycl/plugins/level_zero/pi_level_zero.cpp
@@ -2296,6 +2296,14 @@ pi_result piMemBufferCreate(pi_context Context, pi_mem_flags Flags, size_t Size,
     ZeDesc.flags = 0;
     ZeDesc.ordinal = 0;
 
+    if (Size > Device->ZeDeviceProperties.maxMemAllocSize) {
+      // Tell Level-Zero to accept Size > maxMemAllocSize
+      ze_relaxed_allocation_limits_exp_desc_t RelaxedDesc = {};
+      RelaxedDesc.stype = ZE_STRUCTURE_TYPE_RELAXED_ALLOCATION_LIMITS_EXP_DESC;
+      RelaxedDesc.flags = ZE_RELAXED_ALLOCATION_LIMITS_EXP_FLAG_MAX_SIZE;
+      ZeDesc.pNext = &RelaxedDesc;
+    }
+
     ZE_CALL(
         zeMemAllocDevice(Context->ZeContext, &ZeDesc, Size, 1, ZeDevice, &Ptr));
   }
@@ -5102,6 +5110,15 @@ pi_result USMDeviceAllocImpl(void **ResultPtr, pi_context Context,
   ze_device_mem_alloc_desc_t ZeDesc = {};
   ZeDesc.flags = 0;
   ZeDesc.ordinal = 0;
+
+  if (Size > Device->ZeDeviceProperties.maxMemAllocSize) {
+    // Tell Level-Zero to accept Size > maxMemAllocSize
+    ze_relaxed_allocation_limits_exp_desc_t RelaxedDesc = {};
+    RelaxedDesc.stype = ZE_STRUCTURE_TYPE_RELAXED_ALLOCATION_LIMITS_EXP_DESC;
+    RelaxedDesc.flags = ZE_RELAXED_ALLOCATION_LIMITS_EXP_FLAG_MAX_SIZE;
+    ZeDesc.pNext = &RelaxedDesc;
+  }
+
   ZE_CALL(zeMemAllocDevice(Context->ZeContext, &ZeDesc, Size, Alignment,
                            Device->ZeDevice, ResultPtr));
 
