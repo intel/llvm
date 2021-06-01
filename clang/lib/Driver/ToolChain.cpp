61d3f98882efb82a8d64eabcd005b89972e5a8bd
malixian <1240609881@qq.com>
2021-06-01 01:48:21 +0000

    add necessary comment and format code

--- clang/lib/Driver/ToolChain.cpp
+++ clang/lib/Driver/ToolChain.cpp
@@ -1169,6 +1169,8 @@ llvm::opt::DerivedArgList *ToolChain::TranslateOffloadTargetArgs(
     // matches the current toolchain triple. If it is not present
     // at all, target and host share a toolchain.
     if (A->getOption().matches(options::OPT_m_Group)) {
+      // AMD GPU is a special case, as -mcpu is required for the device
+      // compilation.
       if (SameTripleAsHost || getTriple().getArch() == llvm::Triple::amdgcn)
         DAL->append(A);
       else
