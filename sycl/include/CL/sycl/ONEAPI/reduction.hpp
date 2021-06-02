e8caf6c32be48df5ce86b8b2c46e789a1540c929
Chris Perkins <chris.perkins@intel.com>
2021-06-02 08:50:36 -0700

    [SYCL] SYCL 2020 sub_group algorithms (#3786)
    Corresponding sub_group algorithms implemented as `ONEAPI` extensions are deprecated.
    
    Signed-off-by: Chris Perkins <chris.perkins@intel.com>

--- sycl/include/CL/sycl/ONEAPI/reduction.hpp
+++ sycl/include/CL/sycl/ONEAPI/reduction.hpp
@@ -15,6 +15,7 @@
 #include <CL/sycl/detail/tuple.hpp>
 #include <CL/sycl/handler.hpp>
 #include <CL/sycl/kernel.hpp>
+#include <CL/sycl/known_identity.hpp>
 
 #include <tuple>
 
@@ -45,178 +46,9 @@ __SYCL_EXPORT size_t reduGetMaxWGSize(shared_ptr_class<queue_impl> Queue,
 __SYCL_EXPORT size_t reduComputeWGSize(size_t NWorkItems, size_t MaxWGSize,
                                        size_t &NWorkGroups);
 
-template <typename T, class BinaryOperation>
-using IsReduPlus =
-    bool_constant<std::is_same<BinaryOperation, ONEAPI::plus<T>>::value ||
-                  std::is_same<BinaryOperation, ONEAPI::plus<void>>::value>;
 
-template <typename T, class BinaryOperation>
-using IsReduMultiplies =
-    bool_constant<std::is_same<BinaryOperation, std::multiplies<T>>::value ||
-                  std::is_same<BinaryOperation, std::multiplies<void>>::value>;
 
-template <typename T, class BinaryOperation>
-using IsReduMinimum =
-    bool_constant<std::is_same<BinaryOperation, ONEAPI::minimum<T>>::value ||
-                  std::is_same<BinaryOperation, ONEAPI::minimum<void>>::value>;
 
-template <typename T, class BinaryOperation>
-using IsReduMaximum =
-    bool_constant<std::is_same<BinaryOperation, ONEAPI::maximum<T>>::value ||
-                  std::is_same<BinaryOperation, ONEAPI::maximum<void>>::value>;
-
-template <typename T, class BinaryOperation>
-using IsReduBitOR =
-    bool_constant<std::is_same<BinaryOperation, ONEAPI::bit_or<T>>::value ||
-                  std::is_same<BinaryOperation, ONEAPI::bit_or<void>>::value>;
-
-template <typename T, class BinaryOperation>
-using IsReduBitXOR =
-    bool_constant<std::is_same<BinaryOperation, ONEAPI::bit_xor<T>>::value ||
-                  std::is_same<BinaryOperation, ONEAPI::bit_xor<void>>::value>;
-
-template <typename T, class BinaryOperation>
-using IsReduBitAND =
-    bool_constant<std::is_same<BinaryOperation, ONEAPI::bit_and<T>>::value ||
-                  std::is_same<BinaryOperation, ONEAPI::bit_and<void>>::value>;
-
-template <typename T, class BinaryOperation>
-using IsReduOptForFastAtomicFetch =
-    bool_constant<is_sgeninteger<T>::value &&
-                  sycl::detail::IsValidAtomicType<T>::value &&
-                  (IsReduPlus<T, BinaryOperation>::value ||
-                   IsReduMinimum<T, BinaryOperation>::value ||
-                   IsReduMaximum<T, BinaryOperation>::value ||
-                   IsReduBitOR<T, BinaryOperation>::value ||
-                   IsReduBitXOR<T, BinaryOperation>::value ||
-                   IsReduBitAND<T, BinaryOperation>::value)>;
-
-template <typename T, class BinaryOperation>
-using IsReduOptForFastReduce =
-    bool_constant<((is_sgeninteger<T>::value &&
-                    (sizeof(T) == 4 || sizeof(T) == 8)) ||
-                   is_sgenfloat<T>::value) &&
-                  (IsReduPlus<T, BinaryOperation>::value ||
-                   IsReduMinimum<T, BinaryOperation>::value ||
-                   IsReduMaximum<T, BinaryOperation>::value)>;
-
-// Identity = 0
-template <typename T, class BinaryOperation>
-using IsZeroIdentityOp = bool_constant<
-    (is_sgeninteger<T>::value && (IsReduPlus<T, BinaryOperation>::value ||
-                                  IsReduBitOR<T, BinaryOperation>::value ||
-                                  IsReduBitXOR<T, BinaryOperation>::value)) ||
-    (is_sgenfloat<T>::value && IsReduPlus<T, BinaryOperation>::value)>;
-
-// Identity = 1
-template <typename T, class BinaryOperation>
-using IsOneIdentityOp =
-    bool_constant<(is_sgeninteger<T>::value || is_sgenfloat<T>::value) &&
-                  IsReduMultiplies<T, BinaryOperation>::value>;
-
-// Identity = ~0
-template <typename T, class BinaryOperation>
-using IsOnesIdentityOp = bool_constant<is_sgeninteger<T>::value &&
-                                       IsReduBitAND<T, BinaryOperation>::value>;
-
-// Identity = <max possible value>
-template <typename T, class BinaryOperation>
-using IsMinimumIdentityOp =
-    bool_constant<(is_sgeninteger<T>::value || is_sgenfloat<T>::value) &&
-                  IsReduMinimum<T, BinaryOperation>::value>;
-
-// Identity = <min possible value>
-template <typename T, class BinaryOperation>
-using IsMaximumIdentityOp =
-    bool_constant<(is_sgeninteger<T>::value || is_sgenfloat<T>::value) &&
-                  IsReduMaximum<T, BinaryOperation>::value>;
-
-template <typename T, class BinaryOperation>
-using IsKnownIdentityOp =
-    bool_constant<IsZeroIdentityOp<T, BinaryOperation>::value ||
-                  IsOneIdentityOp<T, BinaryOperation>::value ||
-                  IsOnesIdentityOp<T, BinaryOperation>::value ||
-                  IsMinimumIdentityOp<T, BinaryOperation>::value ||
-                  IsMaximumIdentityOp<T, BinaryOperation>::value>;
-
-template <typename BinaryOperation, typename AccumulatorT>
-struct has_known_identity_impl
-    : std::integral_constant<
-          bool, IsKnownIdentityOp<AccumulatorT, BinaryOperation>::value> {};
-
-template <typename BinaryOperation, typename AccumulatorT, typename = void>
-struct known_identity_impl {};
-
-/// Returns zero as identity for ADD, OR, XOR operations.
-template <typename BinaryOperation, typename AccumulatorT>
-struct known_identity_impl<BinaryOperation, AccumulatorT,
-                           typename std::enable_if<IsZeroIdentityOp<
-                               AccumulatorT, BinaryOperation>::value>::type> {
-  static constexpr AccumulatorT value = 0;
-};
-
-template <typename BinaryOperation>
-struct known_identity_impl<BinaryOperation, half,
-                           typename std::enable_if<IsZeroIdentityOp<
-                               half, BinaryOperation>::value>::type> {
-  static constexpr half value =
-#ifdef __SYCL_DEVICE_ONLY__
-      0;
-#else
-      cl::sycl::detail::host_half_impl::half(static_cast<uint16_t>(0));
-#endif
-};
-
-/// Returns one as identify for MULTIPLY operations.
-template <typename BinaryOperation, typename AccumulatorT>
-struct known_identity_impl<BinaryOperation, AccumulatorT,
-                           typename std::enable_if<IsOneIdentityOp<
-                               AccumulatorT, BinaryOperation>::value>::type> {
-  static constexpr AccumulatorT value = 1;
-};
-
-template <typename BinaryOperation>
-struct known_identity_impl<BinaryOperation, half,
-                           typename std::enable_if<IsOneIdentityOp<
-                               half, BinaryOperation>::value>::type> {
-  static constexpr half value =
-#ifdef __SYCL_DEVICE_ONLY__
-      1;
-#else
-      cl::sycl::detail::host_half_impl::half(static_cast<uint16_t>(0x3C00));
-#endif
-};
-
-/// Returns bit image consisting of all ones as identity for AND operations.
-template <typename BinaryOperation, typename AccumulatorT>
-struct known_identity_impl<BinaryOperation, AccumulatorT,
-                           typename std::enable_if<IsOnesIdentityOp<
-                               AccumulatorT, BinaryOperation>::value>::type> {
-  static constexpr AccumulatorT value = ~static_cast<AccumulatorT>(0);
-};
-
-/// Returns maximal possible value as identity for MIN operations.
-template <typename BinaryOperation, typename AccumulatorT>
-struct known_identity_impl<BinaryOperation, AccumulatorT,
-                           typename std::enable_if<IsMinimumIdentityOp<
-                               AccumulatorT, BinaryOperation>::value>::type> {
-  static constexpr AccumulatorT value =
-      std::numeric_limits<AccumulatorT>::has_infinity
-          ? std::numeric_limits<AccumulatorT>::infinity()
-          : (std::numeric_limits<AccumulatorT>::max)();
-};
-
-/// Returns minimal possible value as identity for MAX operations.
-template <typename BinaryOperation, typename AccumulatorT>
-struct known_identity_impl<BinaryOperation, AccumulatorT,
-                           typename std::enable_if<IsMaximumIdentityOp<
-                               AccumulatorT, BinaryOperation>::value>::type> {
-  static constexpr AccumulatorT value =
-      std::numeric_limits<AccumulatorT>::has_infinity
-          ? static_cast<AccumulatorT>(
-                -std::numeric_limits<AccumulatorT>::infinity())
-          : std::numeric_limits<AccumulatorT>::lowest();
-};
 
 /// Class that is used to represent objects that are passed to user's lambda
 /// functions and representing users' reduction variable.
@@ -1838,25 +1670,23 @@ reduction(T *VarPtr, BinaryOperation) {
   return {VarPtr};
 }
 
+// ---- has_known_identity
 template <typename BinaryOperation, typename AccumulatorT>
-struct has_known_identity : detail::has_known_identity_impl<
-                                typename std::decay<BinaryOperation>::type,
-                                typename std::decay<AccumulatorT>::type> {};
-#if __cplusplus >= 201703L
-template <typename BinaryOperation, typename AccumulatorT>
-inline constexpr bool has_known_identity_v =
-    has_known_identity<BinaryOperation, AccumulatorT>::value;
-#endif
+struct has_known_identity
+    : sycl::has_known_identity<BinaryOperation, AccumulatorT> {};
 
 template <typename BinaryOperation, typename AccumulatorT>
-struct known_identity
-    : detail::known_identity_impl<typename std::decay<BinaryOperation>::type,
-                                  typename std::decay<AccumulatorT>::type> {};
-#if __cplusplus >= 201703L
+__SYCL_INLINE_CONSTEXPR bool has_known_identity_v =
+    has_known_identity<BinaryOperation, AccumulatorT>::value;
+
+// ---- known_identity
 template <typename BinaryOperation, typename AccumulatorT>
-inline constexpr AccumulatorT known_identity_v =
+struct known_identity : sycl::known_identity<BinaryOperation, AccumulatorT> {};
+
+template <typename BinaryOperation, typename AccumulatorT>
+__SYCL_INLINE_CONSTEXPR AccumulatorT known_identity_v =
     known_identity<BinaryOperation, AccumulatorT>::value;
-#endif
+
 } // namespace ONEAPI
 } // namespace sycl
 } // __SYCL_INLINE_NAMESPACE(cl)
