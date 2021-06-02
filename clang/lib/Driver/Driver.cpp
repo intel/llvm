7c515fab9394a64168eb766d526d79623690e50a
Aaron Ballman <aaron@aaronballman.com>
2021-06-02 14:53:51 -0400

    Silence a "local variable is initialized but not referenced" warning; NFC

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -4949,7 +4949,6 @@ public:
       return;
 
     // Let builders add host linking actions.
-    Action* HA = nullptr;
     for (DeviceActionBuilder *SB : SpecializedBuilders) {
       if (!SB->isValid())
         continue;
