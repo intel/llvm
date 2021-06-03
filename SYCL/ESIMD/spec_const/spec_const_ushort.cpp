fd6801c49046f9a0002006082fd375dac4f36a39
Alexey Sachkov <alexey.sachkov@intel.com>
2021-06-03 08:24:42 +0300

    [SYCL] Add ESIMD tests with SYCL 2020 spec constants (intel/llvm-test-suite#291)

--- SYCL/ESIMD/spec_const/spec_const_ushort.cpp
+++ SYCL/ESIMD/spec_const/spec_const_ushort.cpp
@@ -16,6 +16,8 @@
 // type size.
 // RUN: %clangxx -fsycl -I%S/.. %s -o %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
+// RUN: %clangxx -fsycl -I%S/.. -DSYCL2020 %s -o %t.2020.out
+// RUN: %GPU_RUN_PLACEHOLDER %t.2020.out
 // UNSUPPORTED: cuda
 
 #include <cstdint>
@@ -27,4 +29,8 @@
 using spec_const_t = uint16_t;
 using container_t = uint16_t;
 
+#ifndef SYCL2020
 #include "Inputs/spec_const_common.hpp"
+#else
+#include "Inputs/spec-const-2020-common.hpp"
+#endif
