d3dc212da99399c8a26dee6e22eeeddd5fdd15f2
mdtoguchi <47896532+mdtoguchi@users.noreply.github.com>
2021-06-03 11:06:17 -0700

    [Driver][SYCL] Do not imply defaultlib msvcrt for Linux based driver on Windows (#3827)
    When compiling for Windows target, we should only be adding the
    defaultlib msvcrt directive when performing MSVC compatible compilations.
    If adding msvcrt for Linux style compilations, we do not know if
    the library should be the regular library or the debug library, as that
    is determined by the use of /MD or /MDd.

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -4703,7 +4703,6 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
         if (AuxT.isWindowsMSVCEnvironment()) {
           CmdArgs.push_back("-D_MT");
           CmdArgs.push_back("-D_DLL");
-          CmdArgs.push_back("--dependent-lib=msvcrt");
         }
       }
     }
