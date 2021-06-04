e26f8f77078ae95cd0398f4dde7f9c2827804e7a
Dmitry Vodopyanov <dmitry.vodopyanov@intel.com>
2021-06-04 15:34:29 +0300

    Fix post-commit after PR #3826

--- sycl/source/detail/allowlist.cpp
+++ sycl/source/detail/allowlist.cpp
@@ -239,10 +239,12 @@ AllowListParsedT parseAllowList(const std::string &AllowListRaw) {
 // Checking if we can allow device with device description DeviceDesc
 bool deviceIsAllowed(const DeviceDescT &DeviceDesc,
                      const AllowListParsedT &AllowListParsed) {
-  for (const auto &SupportedKeyName : SupportedAllowListKeyNames)
+  for (const auto &SupportedKeyName : SupportedAllowListKeyNames) {
+    (void)SupportedKeyName;
     assert((DeviceDesc.find(SupportedKeyName) != DeviceDesc.end()) &&
            "DeviceDesc map should have all supported keys for "
            "SYCL_DEVICE_ALLOWLIST.");
+  }
   auto EqualityComp = [&](const std::string &KeyName,
                           const DeviceDescT &AllowListDeviceDesc) {
     // change to map::contains after switching DPC++ RT to C++20
