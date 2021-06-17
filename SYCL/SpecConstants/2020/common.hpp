d0fb58dbdc8a289c244107d1ba3101ff6fcd7d3a
Alexey Sachkov <alexey.sachkov@intel.com>
2021-06-17 12:56:18 +0300

    [SYCL] Add spec constants tests with kernel_bundle (intel/llvm-test-suite#292)
    * Add exception handling into test_set_and_get_on_host test

--- SYCL/SpecConstants/2020/common.hpp
+++ SYCL/SpecConstants/2020/common.hpp
@@ -29,8 +29,7 @@ inline bool operator!=(const custom_type_nested &lhs,
 
 inline std::ostream &operator<<(std::ostream &out,
                                 const custom_type_nested &v) {
-  return out << "custom_type_nested { .c = " << v.c << ", .f = " << v.f << "}"
-             << std::endl;
+  return out << "custom_type_nested { .c = " << v.c << ", .f = " << v.f << "}";
 }
 
 struct custom_type {
@@ -54,14 +53,15 @@ inline bool operator!=(const custom_type &lhs, const custom_type &rhs) {
 
 inline std::ostream &operator<<(std::ostream &out, const custom_type &v) {
   return out << "custom_type { .n = \n\t" << v.n << ",\n .ull = " << v.ull
-             << "}" << std::endl;
+             << "}";
 }
 
 template <typename T>
-bool check_value(const T &got, const T &ref, const std::string &variable_name) {
+bool check_value(const T &ref, const T &got, const std::string &variable_name) {
   if (got != ref) {
     std::cout << "Unexpected value of " << variable_name << ": " << got
               << " (got) vs " << ref << " (expected)" << std::endl;
+    return false;
   }
 
   return true;
