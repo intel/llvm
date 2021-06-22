b67428575b81bf6a465e3e053923f83d7df98674
Vyacheslav N Klochkov <vyacheslav.n.klochkov@intel.com>
2021-06-21 20:00:39 -0700

    Additional changes in LIT tests
    Signed-off-by: Vyacheslav N Klochkov <vyacheslav.n.klochkov@intel.com>

--- sycl/test/basic_tests/is_device_copyable.cpp
+++ sycl/test/basic_tests/is_device_copyable.cpp
@@ -1,70 +1,78 @@
-// RUN: not %clangxx -fsycl -fsycl-device-only -fsyntax-only -Xclang -verify \
-// RUN: %s -I %sycl_include 2>&1 | FileCheck -check-prefix=CHECK-ERR %s
+// RUN: %clangxx -fsycl -fsycl-device-only -fsyntax-only -Xclang -verify \
+// RUN: %s -I %sycl_include
 
-// This test checks if compiler reports compilation error on an attempt to pass
-// a struct with type that is not device copyable as SYCL kernel parameter.
-// It also checks that the error is not emitted if the type trait
-// is_device_copyable is specialized for such types as true_type.
+// This test checks that compiler does not report any errors
+// for the device copyable types being passed from HOST to DEVICE.
+// It also checks that the types not copyable in general, but declared as
+// device copyable (by using specialization of is_device_copyable class)
+// are allowed.
+
+// expected-no-diagnostics
 
 #include <CL/sycl.hpp>
 
 using namespace cl::sycl;
 
-struct A { int i; };
-
-struct B {
+struct A {
   int i;
-  B (int _i) : i(_i) {}
-  B (const B& x) : i(x.i) {}
 };
 
-// Identical to B, but will be declared as device copyable.
+// Not copyable type, but it will be declared as device copyable.
 struct BCopyable {
   int i;
-  BCopyable (int _i) : i(_i) {}
-  BCopyable (const BCopyable& x) : i(x.i) {}
+  BCopyable(int _i) : i(_i) {}
+  BCopyable(const BCopyable &x) : i(x.i) {}
 };
 
 struct C : A {
   const A C2;
-  C() : A{0}, C2{2}{}
+  C() : A{0}, C2{2} {}
 };
 
-struct D {
-  int i;
-  ~D();
-};
-
-// Identical to D, but will be declared as device copyable.
+// Not copyable type, but it will be declared as device copyable.
 struct DCopyable {
   int i;
   ~DCopyable();
 };
 
-template<> struct is_device_copyable<BCopyable> : std::true_type {};
-template<> struct is_device_copyable<DCopyable> : std::true_type {};
+struct FunctorA {
+  FunctorA() : b(7) {}
+  void operator()() const {
+    (void)b.i;
+    (void)i;
+  }
+
+  BCopyable b;
+  int i;
+};
+
+struct FunctorB : public DCopyable {
+  FunctorB() {}
+  void operator()() const {
+    (void)i;
+    (void)j;
+  }
+
+  int j;
+};
+
+template <> struct is_device_copyable<BCopyable> : std::true_type {};
+template <> struct is_device_copyable<DCopyable> : std::true_type {};
 
 void test() {
   A IamGood;
   IamGood.i = 0;
-  B IamBad(1);
   BCopyable IamBadButCopyable(1);
   C IamAlsoGood;
-  D IamAlsoBad{0};
   DCopyable IamAlsoBadButCopyable{0};
   queue Q;
-  Q.single_task<class kernel_capture_refs>([=] {
+  Q.single_task<class TestA>([=] {
     int A = IamGood.i;
-    int B = IamBad.i;
-
-    int C = IamBadButCopyable.i;
-    int D = IamAlsoBadButCopyable.i;
-
-    int E = IamAlsoGood.i;
-    int F = IamAlsoBad.i;
+    int B = IamBadButCopyable.i;
+    int C = IamAlsoBadButCopyable.i;
+    int D = IamAlsoGood.i;
   });
+
+  Q.single_task<class TestB>(FunctorA{});
+  Q.single_task<class TestC>(FunctorB{});
 }
-// CHECK-ERR: no expected directives found
-// CHECK-ERR-NEXT: diagnostics seen but not expected
-// CHECK-ERR-NEXT: is_device_copyable<B, void>
-// CHECK-ERR-NEXT: is_device_copyable<D, void>
