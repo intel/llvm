571b6c49eacdc7b990b82cd7f70d308875321cac
Alexander Batashev <alexbatashev@outlook.com>
2021-06-23 10:09:13 +0300

    clang-format

--- sycl/source/detail/plugin.hpp
+++ sycl/source/detail/plugin.hpp
@@ -47,7 +47,7 @@ struct array_fill_helper<Kind, Idx, T> {
 template <PiApiKind Kind, size_t Idx, typename T, typename... Args>
 struct array_fill_helper<Kind, Idx, T, Args...> {
   static void fill(unsigned char *Dst, size_t Offset, const T &&Arg,
-                   Args &&... Rest) {
+                   Args &&...Rest) {
     using ArgsTuple = typename PiApiArgTuple<Kind>::type;
     // C-style cast is required here.
     auto RealArg = (typename std::tuple_element<Idx, ArgsTuple>::type)(Arg);
