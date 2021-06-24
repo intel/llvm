619c46292ae910de4fca4539c01f39fc3b434b47
Artem Gindinson <artem.gindinson@intel.com>
2021-06-24 22:12:50 +0300

    [SYCL] Remove the __SYCL_NVPTX__ macro (#3977)
    Since the macro is identical to a combination of
    `__SYCL_DEVICE_ONLY__` and `__NVPTX__`, showing up in the same
    circumstances (SYCL device compilation with an NVPTX triple), drop
    `__SYCL_NVPTX__` as unnecessary and solely rely on the combination of
    pre-existing macros.
    
    Signed-off-by: Artem Gindinson <artem.gindinson@intel.com>

--- clang/test/Preprocessor/sycl-macro-target-specific.cpp
+++ clang/test/Preprocessor/sycl-macro-target-specific.cpp
@@ -1,3 +1,16 @@
+// RUN: %clang_cc1 %s -fsycl-is-device -triple nvptx64-nvidia-nvcl-sycldevice -E -dM \
+// RUN: | FileCheck --check-prefix=CHECK-NVPTX %s
+// RUN: %clang_cc1 %s -fsycl-is-device -triple spir64-unknown-unknown-sycldevice -E -dM \
+// RUN: | FileCheck --check-prefix=CHECK-NVPTX-NEG %s
+// RUN: %clang_cc1 %s -fsycl-is-device -triple spir64_gen-unknown-unknown-sycldevice -E -dM \
+// RUN: | FileCheck --check-prefix=CHECK-NVPTX-NEG %s
+// RUN: %clang_cc1 %s -fsycl-is-device -triple spir64_x86_64-unknown-unknown-sycldevice -E -dM \
+// RUN: | FileCheck --check-prefix=CHECK-NVPTX-NEG %s
+// RUN: %clang_cc1 %s -fsycl-is-device -triple spir64_fpga-unknown-unknown-sycldevice -E -dM \
+// RUN: | FileCheck --check-prefix=CHECK-NVPTX-NEG %s
+// CHECK-NVPTX: #define __NVPTX__
+// CHECK-NVPTX-NEG-NOT: #define __NVPTX__
+
 // RUN: %clang_cc1 %s -fsycl-is-device -triple spir64-unknown-unknown-sycldevice -E -dM \
 // RUN: | FileCheck --check-prefix=CHECK-SYCL-FP-ATOMICS %s
 // RUN: %clang_cc1 %s -fsycl-is-device -triple spir64_gen-unknown-unknown-sycldevice -E -dM \
@@ -8,6 +21,5 @@
 // RUN: | FileCheck --check-prefix=CHECK-SYCL-FP-ATOMICS-NEG %s
 // RUN: %clang_cc1 %s -fsycl-is-device -triple nvptx64-nvidia-nvcl-sycldevice -E -dM \
 // RUN: | FileCheck --check-prefix=CHECK-SYCL-FP-ATOMICS-NEG %s
-
 // CHECK-SYCL-FP-ATOMICS: #define SYCL_USE_NATIVE_FP_ATOMICS
 // CHECK-SYCL-FP-ATOMICS-NEG-NOT: #define SYCL_USE_NATIVE_FP_ATOMICS
