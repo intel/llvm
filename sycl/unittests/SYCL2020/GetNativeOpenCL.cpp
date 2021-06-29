0956fe9274decfb664c84dc6fef08ae2f9d983ab
Vlad Romanov <vlad.romanov@intel.com>
2021-06-29 14:39:23 +0300

    Address comments

--- sycl/unittests/SYCL2020/GetNativeOpenCL.cpp
+++ sycl/unittests/SYCL2020/GetNativeOpenCL.cpp
@@ -1,4 +1,4 @@
-//==----------- GetNative.cpp ---  interop unit test only for opencl -------==//
+//==----------- GetNativeOpenCL.cpp ---  interop unit test only for opencl -==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
