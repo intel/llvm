22fa8f6b037fd860369283abab926351f38b2089
rehana begam <rehana.begam@intel.com>
2021-06-30 15:19:46 -0700

    rearranged ext.
    Signed-off-by: rehana begam <rehana.begam@intel.com>

--- sycl/include/CL/sycl/info/info_desc.hpp
+++ sycl/include/CL/sycl/info/info_desc.hpp
@@ -131,8 +131,6 @@ enum class device : cl_device_info {
   usm_shared_allocations = PI_USM_SINGLE_SHARED_SUPPORT,
   usm_restricted_shared_allocations = PI_USM_CROSS_SHARED_SUPPORT,
   usm_system_allocator = PI_USM_SYSTEM_SHARED_SUPPORT,
-  // uuid extension
-  ext_intel_device_info_uuid = PI_DEVICE_INFO_UUID,
   // intel extensions
   ext_intel_pci_address = PI_DEVICE_INFO_PCI_ADDRESS,
   ext_intel_gpu_eu_count = PI_DEVICE_INFO_GPU_EU_COUNT,
@@ -142,7 +140,8 @@ enum class device : cl_device_info {
   ext_intel_gpu_eu_count_per_subslice =
       PI_DEVICE_INFO_GPU_EU_COUNT_PER_SUBSLICE,
   ext_intel_max_mem_bandwidth = PI_DEVICE_INFO_MAX_MEM_BANDWIDTH,
-  ext_intel_mem_channel = PI_MEM_PROPERTIES_CHANNEL
+  ext_intel_mem_channel = PI_MEM_PROPERTIES_CHANNEL,
+  ext_intel_device_info_uuid = PI_DEVICE_INFO_UUID
 };
 
 enum class device_type : pi_uint64 {
