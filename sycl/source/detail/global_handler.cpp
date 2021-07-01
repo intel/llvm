10f78935cd0cc77a3bb9eb58e6aa3bc76c5cdbb1
Byoungro So <byoungro.so@intel.com>
2021-07-01 12:33:59 -0700

    fix windows teardown
    Signed-off-by: Byoungro So <byoungro.so@intel.com>

--- sycl/source/detail/global_handler.cpp
+++ sycl/source/detail/global_handler.cpp
@@ -173,7 +173,7 @@ void shutdown() {
 }
 
 #ifdef _WIN32
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
+extern "C" BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
   // Perform actions based on the reason for calling.
   switch (fdwReason) {
   case DLL_PROCESS_DETACH:
