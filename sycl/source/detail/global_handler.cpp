b19f9695bc7330c27dca14e6baa9f0608e6f1dce
Byoungro So <byoungro.so@intel.com>
2021-07-02 10:23:47 -0700

    use sycl export
    Signed-off-by: Byoungro So <byoungro.so@intel.com>

--- sycl/source/detail/global_handler.cpp
+++ sycl/source/detail/global_handler.cpp
@@ -173,8 +173,9 @@ void shutdown() {
 }
 
 #ifdef _WIN32
-extern "C" __declspec(dllexport) BOOL WINAPI
-    DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
+extern "C" __SYCL_EXPORT BOOL WINAPI DllMain(HINSTANCE hinstDLL,
+                                             DWORD fdwReason,
+                                             LPVOID lpReserved) {
   // Perform actions based on the reason for calling.
   switch (fdwReason) {
   case DLL_PROCESS_DETACH:
