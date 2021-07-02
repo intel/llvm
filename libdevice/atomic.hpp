82a14e10d4eeb56805605ea795150491c13cee6e
Sergey Kanaev <sergey.kanaev@intel.com>
2021-07-02 17:21:51 +0300

    Fix style issue
    Signed-off-by: Sergey Kanaev <sergey.kanaev@intel.com>

--- libdevice/atomic.hpp
+++ libdevice/atomic.hpp
@@ -66,10 +66,9 @@ extern DEVICE_EXTERNAL int __spirv_AtomicLoad(const int __SYCL_GLOBAL__ *,
                                               __spv::Scope::Flag,
                                               __spv::MemorySemanticsMask::Flag);
 
-extern DEVICE_EXTERNAL void __spirv_AtomicStore(int __SYCL_GLOBAL__ *,
-                                                __spv::Scope::Flag,
-                                                __spv::MemorySemanticsMask::Flag,
-                                                int);
+extern DEVICE_EXTERNAL void
+__spirv_AtomicStore(int __SYCL_GLOBAL__ *, __spv::Scope::Flag,
+                    __spv::MemorySemanticsMask::Flag, int);
 
 /// Atomically set the value in *Ptr with Desired if and only if it is Expected
 /// Return the value which already was in *Ptr
@@ -88,8 +87,7 @@ static inline int atomicLoad(__SYCL_GLOBAL__ int *Ptr) {
 
 static inline void atomicStore(__SYCL_GLOBAL__ int *Ptr, int V) {
   __spirv_AtomicStore(Ptr, __spv::Scope::Device,
-                      __spv::MemorySemanticsMask::SequentiallyConsistent,
-                      V);
+                      __spv::MemorySemanticsMask::SequentiallyConsistent, V);
 }
 
 #endif // __SPIR__
