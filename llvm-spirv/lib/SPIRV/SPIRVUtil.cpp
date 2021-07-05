9d27131276134597e127b9528cb17e6119ff3786
KornevNikita <nikita.kornev@intel.com>
2021-07-05 14:56:16 +0300

    Fix using of deprecated CreateInBounds
    Enable using of CreateInBounds with element type due to deprecate
    GetElementPtrInst::CreateInBounds without element type.
    
    Original commit:
    https://github.com/KhronosGroup/SPIRV-LLVM-Translator/commit/fbb53e6

--- llvm-spirv/lib/SPIRV/SPIRVUtil.cpp
+++ llvm-spirv/lib/SPIRV/SPIRVUtil.cpp
@@ -1186,7 +1186,8 @@ Value *getScalarOrArrayConstantInt(Instruction *Pos, Type *T, unsigned Len,
     new StoreInst(CA, Alloca, Pos);
     auto Zero = ConstantInt::getNullValue(Type::getInt32Ty(T->getContext()));
     Value *Index[] = {Zero, Zero};
-    auto Ret = GetElementPtrInst::CreateInBounds(Alloca, Index, "", Pos);
+    auto *Ret =
+        GetElementPtrInst::CreateInBounds(nullptr, Alloca, Index, "", Pos);
     LLVM_DEBUG(dbgs() << "[getScalarOrArrayConstantInt] Alloca: " << *Alloca
                       << ", Return: " << *Ret << '\n');
     return Ret;
