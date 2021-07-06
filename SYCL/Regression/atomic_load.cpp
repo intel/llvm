81cfcd0af88fbf65f610e4e34c44bb5653a5cc90
Steffen Larsen <56076654+steffenlarsen@users.noreply.github.com>
2021-07-06 10:28:54 +0100

    [SYCL][CUDA] Add triple to build step and remove XFAIL: cuda (intel/llvm-test-suite#339)
    SYCL/Regression/atomic_load.cpp and SYCL/Scheduler/MemObjRemapping.cpp
    should pass for the PI CUDA backend, but are missing the corresponding
    triple in the build step. These changes add the -fsycl-targets with the
    triple to these tests and renable them for CUDA.
    
    Signed-off-by: Steffen Larsen <steffen.larsen@codeplay.com>

--- SYCL/Regression/atomic_load.cpp
+++ SYCL/Regression/atomic_load.cpp
@@ -1,9 +1,8 @@
-// RUN: %clangxx -fsycl %s -o %t.out
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
 // RUN: %HOST_RUN_PLACEHOLDER %t.out
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 // RUN: %ACC_RUN_PLACEHOLDER %t.out
-// XFAIL: cuda
 #include <CL/sycl.hpp>
 using namespace cl::sycl;
 
