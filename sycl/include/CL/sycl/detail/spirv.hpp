211f9652330398a46cd9946687e9114e5b80fa3d
Alexander Batashev <alexander.batashev@intel.com>
2021-07-07 14:40:55 +0300

    restrict template with enable_if

--- sycl/include/CL/sycl/detail/spirv.hpp
+++ sycl/include/CL/sycl/detail/spirv.hpp
@@ -15,6 +15,7 @@
 #include <CL/sycl/detail/helpers.hpp>
 #include <CL/sycl/detail/type_traits.hpp>
 #include <CL/sycl/id.hpp>
+#include <CL/sycl/memory_enums.hpp>
 
 #ifdef __SYCL_DEVICE_ONLY__
 __SYCL_INLINE_NAMESPACE(cl) {
@@ -217,7 +218,10 @@ EnableIfGenericBroadcast<T> GroupBroadcast(T x, id<Dimensions> local_id) {
 // Single happens-before means semantics should always apply to all spaces
 // Although consume is unsupported, forwarding to acquire is valid
 template <typename T>
-static inline constexpr __spv::MemorySemanticsMask::Flag
+static inline constexpr typename std::enable_if<
+    std::is_same<T, sycl::ONEAPI::memory_order>::value ||
+        std::is_same<T, sycl::memory_order>::value,
+    __spv::MemorySemanticsMask::Flag>::type
 getMemorySemanticsMask(T Order) {
   __spv::MemorySemanticsMask::Flag SpvOrder = __spv::MemorySemanticsMask::None;
   switch (Order) {
