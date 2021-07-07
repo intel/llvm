44005ae6dcf9a99bfdbeffeb814485690d19cde0
shuoniu-intel <81583996+shuoniu-intel@users.noreply.github.com>
2021-07-07 13:48:18 -0400

    Update arbitrary_template_arg.cpp
    Apply static_assert compile-time check.

--- sycl/test/fpga_tests/arbitrary_template_arg.cpp
+++ sycl/test/fpga_tests/arbitrary_template_arg.cpp
@@ -1,48 +1,38 @@
-// RUN: %clangxx -fsycl %s -o %t.out
-// RUN: %RUN_ON_HOST %t.out
+// RUN: %clangxx -fsycl -fsyntax-only -Xclang -verify %s -Xclang -verify-ignore-unexpected=note,warning
+// expected-no-diagnostics
+
 // This test performs basic check of SYCL FPGA arbitrary template argument list.
 
 #include <CL/sycl.hpp>
 #include <CL/sycl/INTEL/fpga_utils.hpp>
 #include <iostream>
 
-enum class enum_class { first, second };
+enum class enumClass { first, second };
 
-struct test_int_id;
-template <int _N> struct test_int : std::integral_constant<int, _N> {
-  using type_id = test_int_id;
+struct testIntID;
+template <int _N> struct testInt : std::integral_constant<int, _N> {
+  using type_id = testIntID;
 };
 
-struct test_enum_class_id;
-template <enum_class _N>
-struct test_enum_class : std::integral_constant<enum_class, _N> {
-  using type_id = test_enum_class_id;
+struct testEnumID;
+template <enumClass _N>
+struct testEnum : std::integral_constant<enumClass, _N> {
+  using type_id = testEnumID;
 };
 
-template <class... _Params> void func() {
-  static constexpr auto test_int_value =
-      sycl::INTEL::_GetValue<test_int<0>, _Params...>::value;
-  static constexpr auto test_enum_class_value =
-      sycl::INTEL::_GetValue<test_enum_class<enum_class::first>,
-                             _Params...>::value;
-
-  std::cout << test_int_value << " ";
-  if (test_enum_class_value == enum_class::first) {
-    std::cout << "first" << std::endl;
-  } else {
-    std::cout << "second" << std::endl;
-  }
+template <int ExpectedIntValue, enumClass ExpectedEnumValue, class... _Params>
+void func() {
+  static_assert(sycl::INTEL::_GetValue<testInt<0>, _Params...>::value ==
+                ExpectedIntValue);
+  static_assert(
+      sycl::INTEL::_GetValue<testEnum<enumClass::first>, _Params...>::value ==
+      ExpectedEnumValue);
 }
 
 int main() {
-  // CHECK: 0 first
-  func();
-  // CHECK: 1 first
-  func<test_int<1>>();
-  // CHECK: 0 second
-  func<test_enum_class<enum_class::second>>();
-  // CHECK: 1 second
-  func<test_int<1>, test_enum_class<enum_class::second>>();
-  // CHECK: 1 second
-  func<test_enum_class<enum_class::second>, test_int<1>>();
+  func<0, enumClass::first>();
+  func<1, enumClass::first, testInt<1>>();
+  func<0, enumClass::second, testEnum<enumClass::second>>();
+  func<1, enumClass::second, testInt<1>, testEnum<enumClass::second>>();
+  func<1, enumClass::second, testEnum<enumClass::second>, testInt<1>>();
 }
