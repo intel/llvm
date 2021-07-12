3fd9a983d4b264cd77693560914ac26a0ee8990d
Alexander Batashev <alexander.batashev@intel.com>
2021-07-12 15:13:41 +0300

    address feedback

--- sycl/include/CL/sycl/sub_group.hpp
+++ sycl/include/CL/sycl/sub_group.hpp
@@ -16,5 +16,16 @@ namespace sycl {
 using ext::oneapi::sub_group;
 // TODO move the entire sub_group class implementation to this file once
 // breaking changes are allowed.
+
+namespace experimental {
+inline sub_group this_sub_group() {
+#ifdef __SYCL_DEVICE_ONLY__
+  return sub_group();
+#else
+  throw runtime_error("Sub-groups are not supported on host device.",
+                      PI_INVALID_DEVICE);
+#endif
+}
+} // namespace experimental
 } // namespace sycl
 } // __SYCL_INLINE_NAMESPACE(cl)
