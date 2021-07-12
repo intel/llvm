9760e5e9c3bd487297ddd1b18713edd17bfd251a
Mikhail Nikolskii <mikhail.y.nikolsky@intel.com>
2021-07-12 13:16:11 +0300

    [SYCL] E2E test for interop_task with Level-Zero and OpenCL (intel/llvm-test-suite#252)

--- SYCL/Plugin/interop-level-zero-interop-task-mem.cpp
+++ SYCL/Plugin/interop-level-zero-interop-task-mem.cpp
@@ -2,45 +2,54 @@
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %level_zero_options %s -o %t.out
 // RUN: env SYCL_BE=PI_LEVEL_ZERO %GPU_RUN_PLACEHOLDER %t.out
 
-// Test fails on Level Zero on Linux
-// UNSUPPORTED: level_zero && linux
+// Test for Level Zero interop_task.
 
-// Test for Level Zero interop_task
-
-#include <CL/sycl.hpp>
-// clang-format off
+// Level-Zero
 #include <level_zero/ze_api.h>
-#include <CL/sycl/backend/level_zero.hpp>
-// clang-format on
 
-class my_selector : public cl::sycl::device_selector {
-public:
-  int operator()(const cl::sycl::device &dev) const override {
-    return (dev.get_platform().get_backend() == cl::sycl::backend::level_zero)
-               ? 1
-               : 0;
-  }
-};
+// SYCL
+#include <CL/sycl.hpp>
+#include <CL/sycl/backend/level_zero.hpp>
+
+using namespace sycl;
+
+constexpr size_t SIZE = 16;
 
 int main() {
-  sycl::queue sycl_queue = sycl::queue(my_selector());
+  queue queue{};
 
-  ze_context_handle_t ze_context =
-      sycl_queue.get_context().get_native<sycl::backend::level_zero>();
-  std::cout << "zeContextGetStatus = " << zeContextGetStatus(ze_context)
-            << std::endl;
+  try {
+    buffer<uint8_t, 1> buffer(SIZE);
+    image<2> image(image_channel_order::rgba, image_channel_type::fp32,
+                   {SIZE, SIZE});
 
-  auto buf = cl::sycl::buffer<uint8_t, 1>(1024);
-  sycl_queue.submit([&](cl::sycl::handler &cgh) {
-    auto acc = buf.get_access<cl::sycl::access::mode::write>(cgh);
-    cgh.interop_task([&](const cl::sycl::interop_handler &ih) {
-      void *device_ptr = ih.get_mem<sycl::backend::level_zero>(acc);
-      ze_memory_allocation_properties_t memAllocProperties{};
-      zeMemGetAllocProperties(ze_context, device_ptr, &memAllocProperties,
-                              nullptr);
-      std::cout << "Memory type = " << memAllocProperties.type << std::endl;
-    });
-  });
+    ze_context_handle_t ze_context =
+        queue.get_context().get_native<backend::level_zero>();
+
+    queue
+        .submit([&](handler &cgh) {
+          auto buffer_acc = buffer.get_access<access::mode::write>(cgh);
+          auto image_acc = image.get_access<float4, access::mode::write>(cgh);
+          cgh.interop_task([=](const interop_handler &ih) {
+            void *device_ptr = ih.get_mem<backend::level_zero>(buffer_acc);
+            ze_memory_allocation_properties_t memAllocProperties{};
+            ze_result_t res = zeMemGetAllocProperties(
+                ze_context, device_ptr, &memAllocProperties, nullptr);
+            assert(res == ZE_RESULT_SUCCESS);
+
+            ze_image_handle_t ze_image =
+                ih.get_mem<backend::level_zero>(image_acc);
+            assert(ze_image != nullptr);
+          });
+        })
+        .wait();
+  } catch (exception const &e) {
+    std::cout << "SYCL exception caught: " << e.what() << std::endl;
+    return e.get_cl_code();
+  } catch (const char *msg) {
+    std::cout << "Exception caught: " << msg << std::endl;
+    return 1;
+  }
 
   return 0;
 }
