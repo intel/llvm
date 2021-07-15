b03b5fd25038e65f00efe57b461a1fca286e1afb
vladimirlaz <vladimir.lazarev@intel.com>
2021-07-15 16:21:26 +0300

    [SYCL] Xfail tests failing after driver uplift (intel/llvm-test-suite#364)
    The tests are failing with GPU RT 30.0.100.9667 on windows

--- SYCL/DeviceCodeSplit/aot-gpu.cpp
+++ SYCL/DeviceCodeSplit/aot-gpu.cpp
@@ -2,6 +2,9 @@
 // UNSUPPORTED: cuda
 // CUDA does neither support device code splitting nor SPIR.
 //
+// The test is failing with GPU RT 30.0.100.9667
+// XFAIL: windows
+//
 // RUN: %clangxx -fsycl -fsycl-device-code-split=per_source \
 // RUN:   -fsycl-targets=spir64_gen-unknown-unknown-sycldevice \
 // RUN:   -Xsycl-target-backend=spir64_gen-unknown-unknown-sycldevice \
