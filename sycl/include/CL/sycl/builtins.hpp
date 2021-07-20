f1b00faf41882a72847108b7521e863b1f523343
gejin <ge.jin@intel.com>
2021-07-20 11:09:13 +0800

    add memcmp to builtins.hpp
    Signed-off-by: gejin <ge.jin@intel.com>

--- sycl/include/CL/sycl/builtins.hpp
+++ sycl/include/CL/sycl/builtins.hpp
@@ -1639,6 +1639,7 @@ extern SYCL_EXTERNAL double hypot(double x, double y);
 
 extern SYCL_EXTERNAL void *memcpy(void *dest, const void *src, size_t n);
 extern SYCL_EXTERNAL void *memset(void *dest, int c, size_t n);
+extern SYCL_EXTERNAL int memcmp(const void *s1, const void *s2, size_t n);
 }
 #ifdef __GLIBC__
 extern "C" {
