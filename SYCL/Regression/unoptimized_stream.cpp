0c1987ec73dfcc8429bd2bad3a495aba44d66ff8
Steffen Larsen <56076654+steffenlarsen@users.noreply.github.com>
2021-07-21 14:25:21 +0100

    [SYCL][CUDA] Unoptimized stream regression test (intel/llvm-test-suite#362)
    Previously the CUDA backend would fail due to invalid atomic memory
    orders not being optimized out. The use of these invalid memory orders
    have been removed in recent changes, so this commit adds a regression
    test to make sure they do not resurface.
    
    Signed-off-by: Steffen Larsen <steffen.larsen@codeplay.com>

---
+++ SYCL/Regression/unoptimized_stream.cpp
@@ -0,0 +1,20 @@
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -O0 -o %t.out
+// RUN: %HOST_RUN_PLACEHOLDER %t.out
+// RUN: %CPU_RUN_PLACEHOLDER %t.out
+// RUN: %GPU_RUN_PLACEHOLDER %t.out
+
+// NOTE: The libclc target used by the CUDA backend used to generate atomic load
+//       variants that were unsupported by NVPTX. Even if they were not used
+//       directly, sycl::stream and other operations would keep the invalid
+//       operations in when optimizations were disabled.
+
+#include <sycl/sycl.hpp>
+
+int main() {
+  sycl::queue q;
+  q.submit([&](sycl::handler &cgh) {
+    sycl::stream os(1024, 256, cgh);
+    cgh.single_task([=]() { os << "test"; });
+  });
+  return 0;
+}
