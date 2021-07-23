710ea04fbfd7d9fc77a76bc26bc3d097af6a047c
Viktoria Maksimova <viktoria.maksimova@intel.com>
2021-07-23 17:09:02 +0300

    [SYCL] Fix some Doxygen build warnings
    This is done as a part of https://github.com/intel/llvm/issues/4140

--- sycl/plugins/cuda/pi_cuda.cpp
+++ sycl/plugins/cuda/pi_cuda.cpp
@@ -4647,7 +4647,7 @@ pi_result cuda_piextUSMEnqueueMemAdvise(pi_queue queue, const void *ptr,
 /// \param param_name is the type of query to perform
 /// \param param_value_size is the size of the result in bytes
 /// \param param_value is the result
-/// \param param_value_ret is how many bytes were written
+/// \param param_value_size_ret is how many bytes were written
 pi_result cuda_piextUSMGetMemAllocInfo(pi_context context, const void *ptr,
                                        pi_mem_info param_name,
                                        size_t param_value_size,
