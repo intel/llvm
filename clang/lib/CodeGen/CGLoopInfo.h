cfa9a74858fbb923c4a761792f45b80ad589d48b
Soumi Manna <soumi.manna@intel.com>
2021-07-23 04:38:35 -0700

    Fix format errors
    Signed-off-by: Soumi Manna <soumi.manna@intel.com>

--- clang/lib/CodeGen/CGLoopInfo.h
+++ clang/lib/CodeGen/CGLoopInfo.h
@@ -15,11 +15,11 @@
 #define LLVM_CLANG_LIB_CODEGEN_CGLOOPINFO_H
 
 #include "llvm/ADT/ArrayRef.h"
+#include "llvm/ADT/Optional.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/IR/DebugLoc.h"
 #include "llvm/IR/Value.h"
 #include "llvm/Support/Compiler.h"
-#include "llvm/ADT/Optional.h"
 
 namespace llvm {
 class BasicBlock;
