c10875cba99b11155c682af6af2f9ad48e763810
DenisBakhvalov <61807338+DenisBakhvalov@users.noreply.github.com>
2021-07-22 22:09:16 -0700

    [ESIMD][NFC] Added tests for simd class type traits (#4146)

---
+++ sycl/test/esimd/regression/simd_wrapper.cpp
@@ -0,0 +1,28 @@
+// RUN: %clangxx -fsycl -fsyntax-only -Xclang -verify %s
+
+#include <limits>
+#include <sycl/ext/intel/experimental/esimd.hpp>
+#include <utility>
+
+// This is a regression test for simd object being non-trivial copy
+// constructible. In order to fix it, you need to provide copy constructor for
+// SimdWrapper, e.g.:
+//   SimdWrapper (const SimdWrapper& rhs) : v1(rhs.v1) {}
+
+using namespace sycl::ext::intel::experimental::esimd;
+
+struct SimdWrapper {
+  union {
+    // expected-note@+1 {{copy constructor of 'SimdWrapper' is implicitly deleted because variant field '' has a non-trivial copy constructor}}
+    struct {
+      simd<int, 4> v1;
+    };
+  };
+  SimdWrapper() {}
+};
+
+void encapsulate_simd() SYCL_ESIMD_FUNCTION {
+  SimdWrapper s1;
+  // expected-error@+1 {{call to implicitly-deleted copy constructor of 'SimdWrapper'}}
+  SimdWrapper s2(s1);
+}
