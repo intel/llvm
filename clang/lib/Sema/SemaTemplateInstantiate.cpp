6ac26add7a00aa89c27c6ae21a5925a9dd1c9d58
smanna12 <52675209+smanna12@users.noreply.github.com>
2021-07-25 04:35:13 -0400

    [SYCL][FPGA] Refactor of statement attributes (#4136)
    This patch
     1. refactors six statement attributes to align with upstream:
     
         SYCLIntelFPGALoopCountAttr 
         SYCLIntelFPGAInitiationIntervalAttr 
         SYCLIntelFPGAMaxConcurrencyAttr
         SYCLIntelFPGAMaxInterleavingAttr
         SYCLIntelFPGASpeculatedIterationsAttr 
         SYCLIntelFPGALoopCoalesceAttr 
    
     2. stores expression as ConstantExpr in Semantic Attributes
     3. removes generic function "BuildSYCLIntelFPGALoopAttr"
     4. updates codegen codes to use single variable for loop metadata and value.
    
    Signed-off-by: Soumi Manna <soumi.manna@intel.com>

--- clang/lib/Sema/SemaTemplateInstantiate.cpp
+++ clang/lib/Sema/SemaTemplateInstantiate.cpp
@@ -1541,9 +1541,8 @@ TemplateInstantiator::TransformSYCLIntelFPGAInitiationIntervalAttr(
     const SYCLIntelFPGAInitiationIntervalAttr *II) {
   Expr *TransformedExpr =
       getDerived().TransformExpr(II->getIntervalExpr()).get();
-  return getSema()
-      .BuildSYCLIntelFPGALoopAttr<SYCLIntelFPGAInitiationIntervalAttr>(
-          *II, TransformedExpr);
+  return getSema().BuildSYCLIntelFPGAInitiationIntervalAttr(*II,
+                                                            TransformedExpr);
 }
 
 const SYCLIntelFPGAMaxConcurrencyAttr *
@@ -1551,33 +1550,29 @@ TemplateInstantiator::TransformSYCLIntelFPGAMaxConcurrencyAttr(
     const SYCLIntelFPGAMaxConcurrencyAttr *MC) {
   Expr *TransformedExpr =
       getDerived().TransformExpr(MC->getNThreadsExpr()).get();
-  return getSema().BuildSYCLIntelFPGALoopAttr<SYCLIntelFPGAMaxConcurrencyAttr>(
-      *MC, TransformedExpr);
+  return getSema().BuildSYCLIntelFPGAMaxConcurrencyAttr(*MC, TransformedExpr);
 }
 
 const SYCLIntelFPGALoopCoalesceAttr *
 TemplateInstantiator::TransformSYCLIntelFPGALoopCoalesceAttr(
     const SYCLIntelFPGALoopCoalesceAttr *LC) {
   Expr *TransformedExpr = getDerived().TransformExpr(LC->getNExpr()).get();
-  return getSema().BuildSYCLIntelFPGALoopAttr<SYCLIntelFPGALoopCoalesceAttr>(
-      *LC, TransformedExpr);
+  return getSema().BuildSYCLIntelFPGALoopCoalesceAttr(*LC, TransformedExpr);
 }
 
 const SYCLIntelFPGAMaxInterleavingAttr *
 TemplateInstantiator::TransformSYCLIntelFPGAMaxInterleavingAttr(
     const SYCLIntelFPGAMaxInterleavingAttr *MI) {
   Expr *TransformedExpr = getDerived().TransformExpr(MI->getNExpr()).get();
-  return getSema().BuildSYCLIntelFPGALoopAttr<SYCLIntelFPGAMaxInterleavingAttr>(
-      *MI, TransformedExpr);
+  return getSema().BuildSYCLIntelFPGAMaxInterleavingAttr(*MI, TransformedExpr);
 }
 
 const SYCLIntelFPGASpeculatedIterationsAttr *
 TemplateInstantiator::TransformSYCLIntelFPGASpeculatedIterationsAttr(
     const SYCLIntelFPGASpeculatedIterationsAttr *SI) {
   Expr *TransformedExpr = getDerived().TransformExpr(SI->getNExpr()).get();
-  return getSema()
-      .BuildSYCLIntelFPGALoopAttr<SYCLIntelFPGASpeculatedIterationsAttr>(
-          *SI, TransformedExpr);
+  return getSema().BuildSYCLIntelFPGASpeculatedIterationsAttr(*SI,
+                                                              TransformedExpr);
 }
 
 const SYCLIntelFPGALoopCountAttr *
@@ -1585,8 +1580,7 @@ TemplateInstantiator::TransformSYCLIntelFPGALoopCountAttr(
     const SYCLIntelFPGALoopCountAttr *LCA) {
   Expr *TransformedExpr =
       getDerived().TransformExpr(LCA->getNTripCount()).get();
-  return getSema().BuildSYCLIntelFPGALoopAttr<SYCLIntelFPGALoopCountAttr>(
-      *LCA, TransformedExpr);
+  return getSema().BuildSYCLIntelFPGALoopCountAttr(*LCA, TransformedExpr);
 }
 
 const LoopUnrollHintAttr *TemplateInstantiator::TransformLoopUnrollHintAttr(
