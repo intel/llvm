6872f136069478898a9d475b675163e0d167365a
Alexander Flegontov <alexander.flegontov@intel.com>
2021-07-26 18:33:11 +0300

    [SYCL] Optimize SetKernelParamsAndLaunch
    Signed-off-by: Alexander Flegontov <alexander.flegontov@intel.com>

--- sycl/source/detail/scheduler/commands.hpp
+++ sycl/source/detail/scheduler/commands.hpp
@@ -537,7 +537,7 @@ private:
       CGExecKernel *ExecKernel,
       std::shared_ptr<device_image_impl> DeviceImageImpl, RT::PiKernel Kernel,
       NDRDescT &NDRDesc, std::vector<RT::PiEvent> &RawEvents,
-      RT::PiEvent &Event, ProgramManager::KernelArgMask EliminatedArgMask);
+      RT::PiEvent &Event, ProgramManager::KernelArgMask &EliminatedArgMask);
 
   std::unique_ptr<detail::CG> MCommandGroup;
 
