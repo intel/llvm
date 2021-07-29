19de5f41840274c5e5634a9ebf1ba8bccb5ab1e1
Byoungro So <byoungro.so@intel.com>
2021-07-28 18:22:38 -0700

    trial tests
    Signed-off-by: Byoungro So <byoungro.so@intel.com>

---
+++ sycl/test/regression/sycl-ls-unique-device-id-opencl.cpp
@@ -0,0 +1,14 @@
+// REQUIRES: opencl
+
+// RUN: env SYCL_DEVICE_FILTER=opencl sycl-ls | FileCheck %s --check-prefixes=CHECK-OPENCL
+
+// CHECK-OPENCL-COUNT-1: [opencl:{{.*}}:0]
+
+//==-- sycl-ls-unique-device-id-opencl.cpp - SYCL test for unique device id
+//--===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
