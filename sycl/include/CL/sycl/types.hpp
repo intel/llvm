94c95e76e56692ec82c866dd67afb922303d6b52
Nawrin Sultana <nawrin.sultana@intel.com>
2021-08-04 07:45:16 -0700

    Add template to get_size

--- sycl/include/CL/sycl/types.hpp
+++ sycl/include/CL/sycl/types.hpp
@@ -1405,7 +1405,9 @@ public:
   size_t size() const noexcept { return getNumElements(); }
   __SYCL2020_DEPRECATED(
       "get_size() is deprecated, please use byte_size() instead")
-  size_t get_size() const { return byte_size(); }
+  template <int Num = getNumElements()> size_t get_size() const {
+    return byte_size<Num>();
+  }
   template <int Num = getNumElements()> size_t byte_size() const noexcept {
     return sizeof(DataT) * (Num == 3 ? 4 : Num);
   }
