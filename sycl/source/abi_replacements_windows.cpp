248da2e21b09bbfe4c41fe6bacd225595e31f0a6
Dmitry Vodopyanov <dmitry.vodopyanov@intel.com>
2021-08-09 12:22:20 +0300

    [SYCL] Return some Windows symbols in library back (#4244)
    This patch returns some of the symbols of the library which
    had @ONEAPI@sycl@ before #4014 , and after that they were
    renamed to @oneapi@ext@sycl@. These symbols can't be used
    by users because the fully internal class stream_impl was exported,
    so nothing breaks. Anyway, some symbols were removed in
    comparison with Gold, and we can't change ABI yet. During the
    timeframe where we can break ABI, we can fix stream_impl by
    removing `__SYCL_EXPORT` from it.

---
+++ sycl/source/abi_replacements_windows.cpp
@@ -0,0 +1,9 @@
+#pragma comment(                                                               \
+    linker,                                                                    \
+    "/export:?accessGlobalBuf@stream_impl@detail@sycl@cl@@QEAA?AV?$accessor@D$00$0EAC@$0HNO@$0A@V?$accessor_property_list@$$V@ONEAPI@sycl@cl@@@34@AEAVhandler@34@@Z=?accessGlobalBuf@stream_impl@detail@sycl@cl@@QEAA?AV?$accessor@D$00$0EAC@$0HNO@$0A@V?$accessor_property_list@$$V@oneapi@ext@sycl@cl@@@34@AEAVhandler@34@@Z")
+#pragma comment(                                                               \
+    linker,                                                                    \
+    "/export:?accessGlobalFlushBuf@stream_impl@detail@sycl@cl@@QEAA?AV?$accessor@D$00$0EAC@$0HNO@$0A@V?$accessor_property_list@$$V@ONEAPI@sycl@cl@@@34@AEAVhandler@34@@Z=?accessGlobalFlushBuf@stream_impl@detail@sycl@cl@@QEAA?AV?$accessor@D$00$0EAC@$0HNO@$0A@V?$accessor_property_list@$$V@oneapi@ext@sycl@cl@@@34@AEAVhandler@34@@Z")
+#pragma comment(                                                               \
+    linker,                                                                    \
+    "/export:?accessGlobalOffset@stream_impl@detail@sycl@cl@@QEAA?AV?$accessor@I$00$0EAF@$0HNO@$0A@V?$accessor_property_list@$$V@ONEAPI@sycl@cl@@@34@AEAVhandler@34@@Z=?accessGlobalOffset@stream_impl@detail@sycl@cl@@QEAA?AV?$accessor@I$00$0EAF@$0HNO@$0A@V?$accessor_property_list@$$V@oneapi@ext@sycl@cl@@@34@AEAVhandler@34@@Z")
