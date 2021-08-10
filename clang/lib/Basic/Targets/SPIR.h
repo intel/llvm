8dcd7606e78d2e99cc9612ed1a940a7f0350473e
Alexey Bader <alexey.bader@intel.com>
2021-08-10 17:17:59 +0300

    Remove a work-around for SPIR target.

--- clang/lib/Basic/Targets/SPIR.h
+++ clang/lib/Basic/Targets/SPIR.h
@@ -169,12 +169,6 @@ public:
     resetDataLayout(
         "e-p:32:32-i64:64-v16:16-v24:32-v32:32-v48:64-"
         "v96:128-v192:256-v256:256-v512:512-v1024:1024-n8:16:32:64");
-
-    // TODO: This is workaround for SYCL exception_ptr class.
-    // Exceptions is not allowed in sycl device code but we should be able
-    // to parse host code. So we allow compilation of exception_ptr but
-    // if exceptions are used in device code we should emit a diagnostic.
-    MaxAtomicInlineWidth = 32;
   }
 
   void getTargetDefines(const LangOptions &Opts,
