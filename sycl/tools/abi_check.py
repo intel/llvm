89c4e3c29c2aa5230192c4a04d9e431d88918898
Dmitry Vodopyanov <dmitry.vodopyanov@intel.com>
2021-08-10 13:09:13 +0300

    Fix Windows- and Linux-specific problems

--- sycl/tools/abi_check.py
+++ sycl/tools/abi_check.py
@@ -34,7 +34,14 @@ def parse_readobj_output(output):
                         output.decode().strip())
   parsed_symbols = []
   for sym in symbols:
-    name = re.search(r"(?<=Name:\s)[^\n]+", sym)
+    # Name section on Linux has the following structure:
+    # SYMBOL_NAME (ID)
+    # regex takes SYMBOL_NAME before the space.
+    # Name section on Windows has the following structure
+    # SYMBOL_NAME
+    # and has special characters in comparison with symbols
+    # on Linux
+    name = re.search(r"(?<=Name:\s)[^ ]+", sym)
     if os.name == 'nt':
       # no additional info about the symbols on Windows in comparison with symbols on Linux below
       parsed_symbols.append(name.group())
@@ -55,7 +62,10 @@ def dump_symbols(target_path, output):
     out.write("\n################################################################################")
     out.write("\n\n# RUN: env LLVM_BIN_PATH=%llvm_build_bin_dir python")
     out.write(" %sycl_tools_src_dir/abi_check.py --mode check_symbols")
-    out.write(" --reference %s %sycl_libs_dir/")
+    if os.name == 'nt':
+      out.write(" --reference %s %llvm_build_bin_dir/")
+    else:
+      out.write(" --reference %s %sycl_libs_dir/")
     out.write(os.path.basename(target_path))
     if os.name == 'nt':
       out.write("\n# REQUIRES: windows")
@@ -127,12 +137,12 @@ def main():
   args = parser.parse_args()
 
   if args.mode == 'check_symbols':
-    if args.reference == None:
+    if args.reference is None:
       print("Please specify --reference option. Quiting.")
       sys.exit(-2)
     check_symbols(args.reference, args.target_library)
   elif args.mode == 'dump_symbols':
-    if args.output == None:
+    if args.output is None:
       print("Please specify --output option. Quiting.")
       sys.exit(-2)
     dump_symbols(args.target_library, args.output)
