ac2caf7b8308844511cb4961819b9e5f64424854
KornevNikita <33487926+KornevNikita@users.noreply.github.com>
2021-08-13 21:30:11 +0300

    Linkage type fix for global variables translation (#1148)
    This is a workaround to fix translation of linkage type of "llvm.used" and
    "llvm.compiler.used" global variables that should be "appending" instead of
    "export".
    
    Original commit:
    https://github.com/KhronosGroup/SPIRV-LLVM-Translator/commit/22629a0

--- llvm-spirv/lib/SPIRV/SPIRVReader.cpp
+++ llvm-spirv/lib/SPIRV/SPIRVReader.cpp
@@ -4157,6 +4157,9 @@ bool SPIRVToLLVM::transSourceExtension() {
 
 llvm::GlobalValue::LinkageTypes
 SPIRVToLLVM::transLinkageType(const SPIRVValue *V) {
+  std::string ValueName = V->getName();
+  if (ValueName == "llvm.used" || ValueName == "llvm.compiler.used")
+    return GlobalValue::AppendingLinkage;
   int LT = V->getLinkageType();
   switch (LT) {
   case internal::LinkageTypeInternal:
