ad3a3e3f2113077a8ddb429e17a4c454a1b7bc9e
Vyacheslav Klochkov <vyacheslav.n.klochkov@intel.com>
2021-08-17 02:15:48 -0700

    [SYCL] Enable the test reduction_nd_ext_half.cpp for opencl (intel/llvm-test-suite#406)
    The test was renewed/stabilized earlier. It also checks for aspect::fp16 before
    running the test cases using FP16 data.
    
    Signed-off-by: Vyacheslav N Klochkov <vyacheslav.n.klochkov@intel.com>

--- SYCL/Reduction/reduction_nd_ext_half.cpp
+++ SYCL/Reduction/reduction_nd_ext_half.cpp
@@ -1,12 +1,7 @@
-// Sporadic failure
-// UNSUPPORTED: opencl && windows
-
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
-
-// TODO: Enable the test for CPU/ACC when they support half type.
-// RUNx: %CPU_RUN_PLACEHOLDER %t.out
-// RUNx: %ACC_RUN_PLACEHOLDER %t.out
+// RUN: %CPU_RUN_PLACEHOLDER %t.out
+// RUN: %ACC_RUN_PLACEHOLDER %t.out
 
 // TODO: Enable the test for HOST when it supports intel::reduce() and barrier()
 // RUNx: %HOST_RUN_PLACEHOLDER %t.out
