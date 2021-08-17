306c8d697f60b3aafafdffd1c9600f884bda0be3
Vyacheslav N Klochkov <vyacheslav.n.klochkov@intel.com>
2021-08-17 14:07:40 -0700

    [SYCL][NFC] Fix duplication caused by functors defined in ext::oneapi and sycl
    The functors ext::oneapi::minimum/maximum were defined identically to
    sycl::minimum/maximum. Turn them into aliases of functors defined in sycl.
    Remove duplicated checks for ext::oneapi functors.
    
    Signed-off-by: Vyacheslav N Klochkov <vyacheslav.n.klochkov@intel.com>

--- sycl/include/sycl/ext/oneapi/functional.hpp
+++ sycl/include/sycl/ext/oneapi/functional.hpp
@@ -16,46 +16,13 @@ namespace sycl {
 namespace ext {
 namespace oneapi {
 
-template <typename T = void> struct minimum {
-  T operator()(const T &lhs, const T &rhs) const {
-    return std::less<T>()(lhs, rhs) ? lhs : rhs;
-  }
-};
-
-template <> struct minimum<void> {
-  struct is_transparent {};
-  template <typename T, typename U>
-  auto operator()(T &&lhs, U &&rhs) const ->
-      typename std::common_type<T &&, U &&>::type {
-    return std::less<>()(std::forward<const T>(lhs), std::forward<const U>(rhs))
-               ? std::forward<T>(lhs)
-               : std::forward<U>(rhs);
-  }
-};
-
-template <typename T = void> struct maximum {
-  T operator()(const T &lhs, const T &rhs) const {
-    return std::greater<T>()(lhs, rhs) ? lhs : rhs;
-  }
-};
-
-template <> struct maximum<void> {
-  struct is_transparent {};
-  template <typename T, typename U>
-  auto operator()(T &&lhs, U &&rhs) const ->
-      typename std::common_type<T &&, U &&>::type {
-    return std::greater<>()(std::forward<const T>(lhs),
-                            std::forward<const U>(rhs))
-               ? std::forward<T>(lhs)
-               : std::forward<U>(rhs);
-  }
-};
-
 template <typename T = void> using plus = std::plus<T>;
 template <typename T = void> using multiplies = std::multiplies<T>;
 template <typename T = void> using bit_or = std::bit_or<T>;
 template <typename T = void> using bit_xor = std::bit_xor<T>;
 template <typename T = void> using bit_and = std::bit_and<T>;
+template <typename T = void> using maximum = sycl::maximum<T>;
+template <typename T = void> using minimum = sycl::minimum<T>;
 
 } // namespace oneapi
 } // namespace ext
@@ -106,41 +73,29 @@ struct GroupOpTag<T, detail::enable_if_t<detail::is_sgenfloat<T>::value>> {
     return Ret;                                                                \
   }
 
-// calc for sycl minimum/maximum function objects
+// calc for sycl function objects
 __SYCL_CALC_OVERLOAD(GroupOpISigned, SMin, sycl::minimum<T>)
 __SYCL_CALC_OVERLOAD(GroupOpIUnsigned, UMin, sycl::minimum<T>)
 __SYCL_CALC_OVERLOAD(GroupOpFP, FMin, sycl::minimum<T>)
+
 __SYCL_CALC_OVERLOAD(GroupOpISigned, SMax, sycl::maximum<T>)
 __SYCL_CALC_OVERLOAD(GroupOpIUnsigned, UMax, sycl::maximum<T>)
 __SYCL_CALC_OVERLOAD(GroupOpFP, FMax, sycl::maximum<T>)
 
-// calc for oneapi function objects
-__SYCL_CALC_OVERLOAD(GroupOpISigned, SMin, ext::oneapi::minimum<T>)
-__SYCL_CALC_OVERLOAD(GroupOpIUnsigned, UMin, ext::oneapi::minimum<T>)
-__SYCL_CALC_OVERLOAD(GroupOpFP, FMin, ext::oneapi::minimum<T>)
-__SYCL_CALC_OVERLOAD(GroupOpISigned, SMax, ext::oneapi::maximum<T>)
-__SYCL_CALC_OVERLOAD(GroupOpIUnsigned, UMax, ext::oneapi::maximum<T>)
-__SYCL_CALC_OVERLOAD(GroupOpFP, FMax, ext::oneapi::maximum<T>)
-__SYCL_CALC_OVERLOAD(GroupOpISigned, IAdd, ext::oneapi::plus<T>)
-__SYCL_CALC_OVERLOAD(GroupOpIUnsigned, IAdd, ext::oneapi::plus<T>)
-__SYCL_CALC_OVERLOAD(GroupOpFP, FAdd, ext::oneapi::plus<T>)
+__SYCL_CALC_OVERLOAD(GroupOpISigned, IAdd, sycl::plus<T>)
+__SYCL_CALC_OVERLOAD(GroupOpIUnsigned, IAdd, sycl::plus<T>)
+__SYCL_CALC_OVERLOAD(GroupOpFP, FAdd, sycl::plus<T>)
 
-__SYCL_CALC_OVERLOAD(GroupOpISigned, NonUniformIMul, ext::oneapi::multiplies<T>)
-__SYCL_CALC_OVERLOAD(GroupOpIUnsigned, NonUniformIMul,
-                     ext::oneapi::multiplies<T>)
-__SYCL_CALC_OVERLOAD(GroupOpFP, NonUniformFMul, ext::oneapi::multiplies<T>)
-__SYCL_CALC_OVERLOAD(GroupOpISigned, NonUniformBitwiseOr,
-                     ext::oneapi::bit_or<T>)
-__SYCL_CALC_OVERLOAD(GroupOpIUnsigned, NonUniformBitwiseOr,
-                     ext::oneapi::bit_or<T>)
-__SYCL_CALC_OVERLOAD(GroupOpISigned, NonUniformBitwiseXor,
-                     ext::oneapi::bit_xor<T>)
-__SYCL_CALC_OVERLOAD(GroupOpIUnsigned, NonUniformBitwiseXor,
-                     ext::oneapi::bit_xor<T>)
-__SYCL_CALC_OVERLOAD(GroupOpISigned, NonUniformBitwiseAnd,
-                     ext::oneapi::bit_and<T>)
-__SYCL_CALC_OVERLOAD(GroupOpIUnsigned, NonUniformBitwiseAnd,
-                     ext::oneapi::bit_and<T>)
+__SYCL_CALC_OVERLOAD(GroupOpISigned, NonUniformIMul, sycl::multiplies<T>)
+__SYCL_CALC_OVERLOAD(GroupOpIUnsigned, NonUniformIMul, sycl::multiplies<T>)
+__SYCL_CALC_OVERLOAD(GroupOpFP, NonUniformFMul, sycl::multiplies<T>)
+
+__SYCL_CALC_OVERLOAD(GroupOpISigned, NonUniformBitwiseOr, sycl::bit_or<T>)
+__SYCL_CALC_OVERLOAD(GroupOpIUnsigned, NonUniformBitwiseOr, sycl::bit_or<T>)
+__SYCL_CALC_OVERLOAD(GroupOpISigned, NonUniformBitwiseXor, sycl::bit_xor<T>)
+__SYCL_CALC_OVERLOAD(GroupOpIUnsigned, NonUniformBitwiseXor, sycl::bit_xor<T>)
+__SYCL_CALC_OVERLOAD(GroupOpISigned, NonUniformBitwiseAnd, sycl::bit_and<T>)
+__SYCL_CALC_OVERLOAD(GroupOpIUnsigned, NonUniformBitwiseAnd, sycl::bit_and<T>)
 
 #undef __SYCL_CALC_OVERLOAD
 
