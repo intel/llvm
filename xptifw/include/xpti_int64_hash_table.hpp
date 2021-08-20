c11018c45eafc213374029dd02e4453f80d41e48
Vasanth Tovinkere <vasanth.tovinkere@intel.com>
2021-08-19 17:36:34 -0700

    [XPTI] Addresses build warnings
    Signed-off-by: Vasanth Tovinkere <vasanth.tovinkere@intel.com>

--- xptifw/include/xpti_int64_hash_table.hpp
+++ xptifw/include/xpti_int64_hash_table.hpp
@@ -162,7 +162,7 @@ public:
   using ht_lut_t = std::unordered_map<int64_t, int64_t>;
 
   Hash64x64Table(int size = 1024)
-      : MForward(size), MReverse(size), MTableSize(size) {
+      : MForward(size), MReverse(size) {
 #ifdef XPTI_STATISTICS
     MInsertions = 0;
     MRetrievals = 0;
@@ -269,7 +269,6 @@ public:
 private:
   ht_lut_t MForward;  ///< Forward lookup hash map
   ht_lut_t MReverse;  ///< Reverse lookup hash map
-  int32_t MTableSize; ///< Initial size of the hash map
   std::mutex MMutex;  ///< Mutex required to implement a double-check pattern
 #ifdef XPTI_STATISTICS
   safe_uint64_t MInsertions, ///< Thread-safe tracking of insertions
