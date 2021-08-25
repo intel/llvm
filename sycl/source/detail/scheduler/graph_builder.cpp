9e3ef859db105c9d2f59ac86439805c18666b113
JackAKirk <chezjakirk@gmail.com>
2021-08-25 16:00:21 +0100

    Corrected formating in graph_builder.cpp.
    Signed-off-by: JackAKirk <jack.kirk@codeplay.com>

--- sycl/source/detail/scheduler/graph_builder.cpp
+++ sycl/source/detail/scheduler/graph_builder.cpp
@@ -10,11 +10,11 @@
 #include <CL/sycl/access/access.hpp>
 #include <CL/sycl/detail/memory_manager.hpp>
 #include <CL/sycl/exception.hpp>
+#include <detail/backend_impl.hpp>
 #include <detail/context_impl.hpp>
 #include <detail/event_impl.hpp>
 #include <detail/queue_impl.hpp>
 #include <detail/scheduler/scheduler.hpp>
-#include <detail/backend_impl.hpp>
 
 #include <cstdlib>
 #include <cstring>
