489d51f167feed625b14cc9ccbdfaddadf90c374
sergei <sergey.kanaev@intel.com>
2021-08-31 17:13:07 +0300

    [SYCL] Disable fallback assert in spec-consts caching test (intel/llvm-test-suite#427)
    Signed-off-by: Sergey Kanaev <sergey.kanaev@intel.com>

--- SYCL/KernelAndProgram/spec_consts.cpp
+++ SYCL/KernelAndProgram/spec_consts.cpp
@@ -2,7 +2,8 @@
 // Specialization constant values are not supported on CUDA
 // REQUIRES: opencl || level_zero
 // RUN: rm -rf %t/cache_dir
-// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
+// FIXME Temporary disable fallback assert here until fixed
+// RUN: %clangxx -fsycl -DSYCL_DISABLE_FALLBACK_ASSERT=1 -fsycl-targets=%sycl_triple %s -o %t.out
 // RUN: env SYCL_CACHE_PERSISTENT=1 SYCL_CACHE_DIR=%t/cache_dir SYCL_PI_TRACE=-1 %CPU_RUN_PLACEHOLDER %t.out %CPU_CHECK_PLACEHOLDER --check-prefixes=CHECK-BUILD
 // RUN: env SYCL_CACHE_PERSISTENT=1 SYCL_CACHE_DIR=%t/cache_dir SYCL_PI_TRACE=-1 %CPU_RUN_PLACEHOLDER %t.out %CPU_CHECK_PLACEHOLDER --check-prefixes=CHECK-CACHE
 // RUN: env SYCL_CACHE_PERSISTENT=1 SYCL_CACHE_DIR=%t/cache_dir SYCL_PI_TRACE=-1 %GPU_RUN_PLACEHOLDER %t.out %GPU_CHECK_PLACEHOLDER --check-prefixes=CHECK-BUILD
