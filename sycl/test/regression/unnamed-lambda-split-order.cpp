e37ef8c062f98259b0fa6468863dcefba8db6a45
Erich Keane <erich.keane@intel.com>
2021-09-02 10:32:33 -0700

    Add comment to the regression suite test case, put it into unnamed-lambda.cpp instead as @AlexeySachkov requested offline

--- sycl/test/regression/unnamed-lambda-split-order.cpp
+++
@@ -1,8 +0,0 @@
-// RUN: %clangxx -fsycl -fsycl-unnamed-lambda %s -o %t.out
-#include <sycl.hpp>
-int main() {
-  auto w = [](auto i) {};
-  sycl::queue q;
-  q.parallel_for(10, [](auto i) {});
-  q.parallel_for(10, w);
-}
