1ffbae723a36de3cccdc3cc618417a541830d98f
Erich Keane <erich.keane@intel.com>
2021-09-02 08:39:17 -0700

    Add sycl regression test from @AlexeySachkov 's repo, hopefully sufficient for his request for this test

---
+++ sycl/test/regression/unnamed-lambda-split-order.cpp
@@ -0,0 +1,8 @@
+// RUN: %clangxx -fsycl -fsycl-unnamed-lambda %s -o %t.out
+#include <sycl.hpp>
+int main() {
+  auto w = [](auto i) {};
+  sycl::queue q;
+  q.parallel_for(10, [](auto i) {});
+  q.parallel_for(10, w);
+}
