626b9a1ce4406c4d8363394fdd5a0ba6e2a4e761
Vlad Romanov <vlad.romanov@intel.com>
2021-09-06 13:08:28 +0300

    fix header

--- sycl/include/CL/sycl/detail/defines_elementary.hpp
+++ sycl/include/CL/sycl/detail/defines_elementary.hpp
@@ -1,4 +1,4 @@
-/defines_elementary/==--- defines_elementary.hpp ---- Preprocessor directives (simplified) ---==//
+//==--- defines_elementary.hpp ---- Preprocessor directives (simplified) ---==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
