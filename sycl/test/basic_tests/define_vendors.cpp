c535d4e42c54e14fc331507f432e930651360ea3
viktor.dorokhov <vdorokho@nnlocl075.inn.intel.com>
2021-09-06 22:47:11 +0300

    [SYCL] Predefine SYCL macros with vendor strings

---
+++ sycl/test/basic_tests/define_vendors.cpp
@@ -0,0 +1,16 @@
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
+#include <CL/sycl.hpp>
+
+#if !defined(SYCL_IMPLEMENTATION_ONEAPI)
+#error SYCL_IMPLEMENTATION_ONEAPI is not defined
+#endif
+
+#if !defined(SYCL_FEATURE_SET_FULL)
+#error SYCL_FEATURE_SET_FULL is not defined
+#endif
+
+#if !defined(SYCL_IMPLEMENTATION_INTEL)
+#error SYCL_IMPLEMENTATION_INTEL is not defined
+#endif
+
+int main(void) { return 0; }
