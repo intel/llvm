d0103afb496031c14d5fc5654834bb5c73f66d7f
Artem Gindinson <artem.gindinson@intel.com>
2021-09-08 11:22:18 +0300

    Apply clang-format
    Signed-off-by: Artem Gindinson <artem.gindinson@intel.com>

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -905,17 +905,17 @@ void Driver::CreateOffloadingDeviceToolChains(Compilation &C,
   // values must happen at the level of TableGen and Arg class design, with
   // Compilation/Driver class constructors handling the driver-specific
   // diagnostic output.
-  auto checkSingleArgValidity =
-      [&](Arg *A, SmallVector<StringRef, 4> AllowedValues) {
-        if (!A)
-          return;
-        const char *ArgValue = A->getValue();
-        for (const StringRef AllowedValue : AllowedValues)
-          if (AllowedValue.equals(ArgValue))
-            return;
-        Diag(clang::diag::err_drv_invalid_value)
-            << A->getOption().getName() << ArgValue;
-      };
+  auto checkSingleArgValidity = [&](Arg *A,
+                                    SmallVector<StringRef, 4> AllowedValues) {
+    if (!A)
+      return;
+    const char *ArgValue = A->getValue();
+    for (const StringRef AllowedValue : AllowedValues)
+      if (AllowedValue.equals(ArgValue))
+        return;
+    Diag(clang::diag::err_drv_invalid_value)
+        << A->getOption().getName() << ArgValue;
+  };
   checkSingleArgValidity(SYCLLink, {"early", "image"});
   checkSingleArgValidity(
       C.getInputArgs().getLastArg(options::OPT_fsycl_device_code_split_EQ),
