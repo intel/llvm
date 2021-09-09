7d1e48132be573f60f8fce9c09920d10e6db0916
Dongkyun Ahn <dongkyun.ahn@intel.com>
2021-09-08 17:46:31 -0700

    Adding 'esimd_cpu' in ParseAllowList unit-test

--- sycl/unittests/allowlist/ParseAllowList.cpp
+++ sycl/unittests/allowlist/ParseAllowList.cpp
@@ -167,7 +167,8 @@ TEST(ParseAllowListTests, CheckAllValidBackendNameValuesAreProcessed) {
   sycl::detail::AllowListParsedT ExpectedValue{
       {{"BackendName", "host"}},       {{"BackendName", "opencl"}},
       {{"BackendName", "level_zero"}}, {{"BackendName", "cuda"}},
-      {{"BackendName", "rocm"}},       {{"BackendName", "*"}}};
+      {{"BackendName", "rocm"}},       {{"BackendName", "esimd_cpu"}},
+      {{"BackendName", "*"}}};
   EXPECT_EQ(ExpectedValue, ActualValue);
 }
 
