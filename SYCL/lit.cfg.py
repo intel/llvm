480fa55da3db7a1a462de4e4140aa872e096ff9f
Nicolas Miller <nicolas.pierre.miller@gmail.com>
2021-09-14 17:07:37 +0100

    [SYCL] Rename ROCm plugin to HIP (#426)

--- SYCL/lit.cfg.py
+++ SYCL/lit.cfg.py
@@ -157,7 +157,7 @@ if not config.sycl_be:
 # Mapping from SYCL_BE backend definition style to SYCL_DEVICE_FILTER used
 # for backward compatibility
 try:
-  config.sycl_be = { 'PI_OPENCL': 'opencl',  'PI_CUDA': 'cuda', 'PI_ROCM': 'rocm', 'PI_LEVEL_ZERO': 'level_zero'}[config.sycl_be]
+  config.sycl_be = { 'PI_OPENCL': 'opencl',  'PI_CUDA': 'cuda', 'PI_HIP': 'hip', 'PI_LEVEL_ZERO': 'level_zero'}[config.sycl_be]
 except:
   # do nothing a we expect that new format of plugin values are used
   pass
@@ -171,20 +171,24 @@ config.substitutions.append( ('%BE_RUN_PLACEHOLDER', "env SYCL_DEVICE_FILTER={SY
 if config.dump_ir_supported:
    config.available_features.add('dump_ir')
 
-if config.sycl_be not in ['host', 'opencl', 'cuda', 'rocm', 'level_zero']:
+if config.sycl_be not in ['host', 'opencl', 'cuda', 'hip', 'level_zero']:
    lit_config.error("Unknown SYCL BE specified '" +
                     config.sycl_be +
-                    "' supported values are opencl, cuda, rocm, level_zero")
+                    "' supported values are opencl, cuda, hip, level_zero")
 
-# If ROCM_PLATFORM flag is not set, default to AMD, and check if ROCM platform is supported
-supported_rocm_platforms=["AMD", "NVIDIA"]
-if config.rocm_platform == "":
-    config.rocm_platform = "AMD"
-if config.rocm_platform not in supported_rocm_platforms:
-    lit_config.error("Unknown ROCm platform '" + config.rocm_platform + "' supported platforms are " + ', '.join(supported_rocm_platforms))
+# If HIP_PLATFORM flag is not set, default to AMD, and check if HIP platform is supported
+supported_hip_platforms=["AMD", "NVIDIA"]
+if config.hip_platform == "":
+    config.hip_platform = "AMD"
+if config.hip_platform not in supported_hip_platforms:
+    lit_config.error("Unknown HIP platform '" + config.hip_platform + "' supported platforms are " + ', '.join(supported_hip_platforms))
 
-if config.sycl_be == "rocm" and config.rocm_platform == "AMD":
+if config.sycl_be == "hip" and config.hip_platform == "AMD":
+    config.available_features.add('hip_amd')
     arch_flag = '-Xsycl-target-backend=amdgcn-amd-amdhsa-sycldevice --offload-arch=' + config.amd_arch
+elif config.sycl_be == "hip" and config.hip_platform == "NVIDIA":
+    config.available_features.add('hip_nvidia')
+    arch_flag = ""
 else:
     arch_flag = ""
 
@@ -303,9 +307,9 @@ else:
 config.substitutions.append( ('%ACC_RUN_PLACEHOLDER',  acc_run_substitute) )
 config.substitutions.append( ('%ACC_CHECK_PLACEHOLDER',  acc_check_substitute) )
 
-if config.sycl_be == 'cuda' or (config.sycl_be == 'rocm' and config.rocm_platform == 'NVIDIA'):
+if config.sycl_be == 'cuda' or (config.sycl_be == 'hip' and config.hip_platform == 'NVIDIA'):
     config.substitutions.append( ('%sycl_triple',  "nvptx64-nvidia-cuda-sycldevice" ) )
-elif config.sycl_be == 'rocm' and config.rocm_platform == 'AMD':
+elif config.sycl_be == 'hip' and config.hip_platform == 'AMD':
     config.substitutions.append( ('%sycl_triple',  "amdgcn-amd-amdhsa-sycldevice" ) )
 else:
     config.substitutions.append( ('%sycl_triple',  "spir64-unknown-unknown-sycldevice" ) )
