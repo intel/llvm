b73cae4ec68294f50416bc14934bd24d508dd2d5
Byoungro So <byoungro.so@intel.com>
2021-09-16 09:33:54 -0700

    Update sycl/source/detail/plugin.hpp
    Co-authored-by: Romanov Vlad <vlad.romanov@intel.com>

--- sycl/source/detail/plugin.hpp
+++ sycl/source/detail/plugin.hpp
@@ -189,7 +189,7 @@ public:
   // return the index of PiPlatforms.
   // If not found, add it and return its index.
   int getPlatformId(RT::PiPlatform Platform) {
-    std::lock_guard<std::mutex> Guard(*PlatformIdMutex);
+// The function is expected to be called in a thread safe manner
     auto It = std::find(PiPlatforms.begin(), PiPlatforms.end(), Platform);
     if (It != PiPlatforms.end())
       return It - PiPlatforms.begin();
