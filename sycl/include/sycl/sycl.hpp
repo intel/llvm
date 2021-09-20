3d38f857e7aa05378e94f18e38c6f7a3b38259e2
Kabanov <denis.kabanov@intel.com>
2021-09-20 23:10:41 +0300

    Fix function_pointer

--- sycl/include/sycl/sycl.hpp
+++ sycl/include/sycl/sycl.hpp
@@ -55,7 +55,7 @@
 #include <sycl/ext/oneapi/atomic.hpp>
 #include <sycl/ext/oneapi/experimental/builtins.hpp>
 #include <sycl/ext/oneapi/filter_selector.hpp>
-#include <sycl/ext/oneapi/function_pointer.hpp>
+#include <sycl/ext/oneapi/__function_pointer.hpp>
 #include <sycl/ext/oneapi/group_algorithm.hpp>
 #include <sycl/ext/oneapi/matrix/matrix.hpp>
 #include <sycl/ext/oneapi/reduction.hpp>
