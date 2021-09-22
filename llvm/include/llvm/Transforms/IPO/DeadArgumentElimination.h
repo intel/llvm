69298aaffa38eac9f3edf817e4ac9f00e8006898
J Todd <joeatodd@gmail.com>
2021-09-22 09:04:44 +0000

    Nested-if idiom, readability & handle possible null

--- llvm/include/llvm/Transforms/IPO/DeadArgumentElimination.h
+++ llvm/include/llvm/Transforms/IPO/DeadArgumentElimination.h
@@ -152,33 +152,32 @@ private:
 
   void BuildNVPTXKernelSet(const Module &M) {
 
-    auto NvvmMetadata = M.getNamedMetadata("nvvm.annotations");
+    auto *NvvmMetadata = M.getNamedMetadata("nvvm.annotations");
     if(!NvvmMetadata) return;
 
-    for (auto MetadataNode : NvvmMetadata->operands()) {
+    for (auto *MetadataNode : NvvmMetadata->operands()) {
       if (MetadataNode->getNumOperands() != 3)
         continue;
 
       // NVPTX identifies kernel entry points using metadata nodes of the form:
       //   !X = !{<function>, !"kernel", i32 1}
-      auto Type = dyn_cast<MDString>(MetadataNode->getOperand(1));
+      auto *Type = dyn_cast<MDString>(MetadataNode->getOperand(1));
       // Only process kernel entry points.
       if (!Type || Type->getString() != "kernel")
         continue;
 
       // Get a pointer to the entry point function from the metadata.
-      const auto &FuncOperand = MetadataNode->getOperand(0);
-      if (!FuncOperand)
-        continue;
-      auto FuncConstant = dyn_cast<ConstantAsMetadata>(FuncOperand);
-      if (!FuncConstant)
-        continue;
-      auto Func = dyn_cast<Function>(FuncConstant->getValue());
-      if (!Func)
-        continue;
-
-      if(mdconst::dyn_extract<ConstantInt>(MetadataNode->getOperand(2))->getValue() == 1)
-        NVPTXKernelSet.insert(Func);
+      if(const auto &FuncOperand = MetadataNode->getOperand(0)){
+        if(auto *FuncConstant = dyn_cast<ConstantAsMetadata>(FuncOperand)){
+          if(auto *Func = dyn_cast<Function>(FuncConstant->getValue())){
+            if(auto *Val = mdconst::dyn_extract<ConstantInt>(MetadataNode->getOperand(2))){
+              if(Val->getValue() == 1){
+                NVPTXKernelSet.insert(Func);
+              }
+            }
+          }
+        }
+      }
     }
     return;
   }
