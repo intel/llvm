21f11fec480b3567efcf6e19e86ffe1643da76d7
Dongkyun Ahn <dongkyun.ahn@intel.com>
2021-09-22 15:37:06 -0700

    Failure fixes
    - ParseAllowLists
    
    - Removing conditional statement for including
    'esimdcpu_device_interface.cpp'

--- sycl/unittests/allowlist/ParseAllowList.cpp
+++ sycl/unittests/allowlist/ParseAllowList.cpp
@@ -167,7 +167,7 @@ TEST(ParseAllowListTests, CheckAllValidBackendNameValuesAreProcessed) {
   sycl::detail::AllowListParsedT ExpectedValue{
       {{"BackendName", "host"}},       {{"BackendName", "opencl"}},
       {{"BackendName", "level_zero"}}, {{"BackendName", "cuda"}},
-      {{"BackendName", "hip"}},        {{"BackendName", "esimd_cpu"}},
+      {{"BackendName", "esimd_cpu"}},  {{"BackendName", "hip"}},
       {{"BackendName", "*"}}};
   EXPECT_EQ(ExpectedValue, ActualValue);
 }
