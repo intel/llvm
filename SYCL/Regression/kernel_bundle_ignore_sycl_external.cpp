0602be5716b372b185d49c25345dc956ba08c71f
Steffen Larsen <steffen.larsen@intel.com>
2021-10-05 14:38:37 +0100

    [SYCL] Adds regression test for ensuring SYCL_EXTERNAL is ignored by kernel bundles (intel/llvm-test-suite#486)
    These changes add a regression test to ensure that exported device
    functions do not appear as kernels in kernel bundles.
    
    Signed-off-by: Steffen Larsen <steffen.larsen@intel.com>

---
+++ SYCL/Regression/kernel_bundle_ignore_sycl_external.cpp
@@ -0,0 +1,40 @@
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
+// RUN: %GPU_RUN_PLACEHOLDER %t.out
+// RUN: %CPU_RUN_PLACEHOLDER %t.out
+// RUN: %ACC_RUN_PLACEHOLDER %t.out
+//
+// XFAIL: cuda || hip
+
+#include <CL/sycl.hpp>
+
+class KernelName;
+
+SYCL_EXTERNAL
+int f(int a) { return a + 1; }
+
+int main() {
+  const sycl::device Dev{sycl::default_selector{}};
+  const sycl::context Ctx{Dev};
+  sycl::queue Q{Ctx, Dev};
+
+  assert(sycl::get_kernel_ids().size() == 1);
+
+  sycl::kernel_bundle EmptyKernelBundle =
+      sycl::get_kernel_bundle<sycl::bundle_state::executable>(Ctx, {Dev}, {});
+
+  assert(EmptyKernelBundle.get_kernel_ids().size() == 0);
+
+  sycl::kernel_bundle KernelBundle =
+      sycl::get_kernel_bundle<sycl::bundle_state::executable>(Ctx, {Dev});
+  sycl::kernel_id KernelID = sycl::get_kernel_id<KernelName>();
+
+  assert(KernelBundle.get_kernel_ids().size() == 1);
+  assert(KernelBundle.has_kernel(KernelID));
+
+  cl::sycl::buffer<int, 1> Buf(sycl::range<1>{1});
+  Q.submit([&](sycl::handler &CGH) {
+    auto Acc = Buf.get_access<sycl::access::mode::write>(CGH);
+    CGH.use_kernel_bundle(KernelBundle);
+    CGH.single_task<KernelName>([=]() { Acc[0] = 42; });
+  });
+}
