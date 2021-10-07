2f1ec7602911aa6b0655efd1b38f0da54f71b37b
Nikita Kornev <nikita.kornev@intel.com>
2021-10-07 10:17:45 +0300

    Fix the use of report_fatal_error
    Original commit:
    https://github.com/KhronosGroup/SPIRV-LLVM-Translator/commit/41c5608

--- llvm-spirv/lib/SPIRV/SPIRVLowerBitCastToNonStandardType.cpp
+++ llvm-spirv/lib/SPIRV/SPIRVLowerBitCastToNonStandardType.cpp
@@ -74,7 +74,9 @@ bool lowerBitCastToNonStdVec(Instruction *OldInst, Value *NewInst,
   static constexpr unsigned MaxRecursionDepth = 16;
   if (RecursionDepth++ > MaxRecursionDepth)
     report_fatal_error(
-        "The depth of recursion exceeds the maximum possible depth", false);
+        llvm::Twine(
+            "The depth of recursion exceeds the maximum possible depth"),
+        false);
 
   bool Changed = false;
   VectorType *NewVecTy = getVectorType(NewInst->getType());
@@ -169,8 +171,8 @@ public:
           uint64_t NumElemsInSrcVec = SrcVecTy->getElementCount().getValue();
           if (!isValidVectorSize(NumElemsInSrcVec))
             report_fatal_error(
-                Twine("Unsupported vector type with the size of: " +
-                      std::to_string(NumElemsInSrcVec)),
+                llvm::Twine("Unsupported vector type with the size of: " +
+                            std::to_string(NumElemsInSrcVec)),
                 false);
         }
         VectorType *DestVecTy = getVectorType(BC->getDestTy());
