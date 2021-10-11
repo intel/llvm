974c8801e1db03f570b0d5f5fce5feb06415edca
Steffen Larsen <steffen.larsen@intel.com>
2021-10-11 23:12:50 +0300

    Adhere to FE test contribution guidelines
    Signed-off-by: Steffen Larsen <steffen.larsen@intel.com>

--- clang/test/CodeGenSYCL/nontrivial_device_copyable.cpp
+++ clang/test/CodeGenSYCL/nontrivial_device_copyable.cpp
@@ -1,25 +1,28 @@
-// RUN: %clang_cc1 -triple spir64 -fsycl-is-device -disable-llvm-passes -sycl-std=2020 -emit-llvm %s -o - | FileCheck %s
+// RUN: %clang_cc1 -triple spir64 -fsycl-is-device -internal-isystem %S/Inputs -disable-llvm-passes -sycl-std=2020 -emit-llvm %s -o - | FileCheck %s
+
+// Tests that SYCL kernel arguments with non-trivially copyable types are
+// passed by-valued.
 
 struct NontriviallyCopyable {
-  int i;
-  NontriviallyCopyable(int _i) : i(_i) {}
-  NontriviallyCopyable(const NontriviallyCopyable &x) : i(x.i) {}
+  int I;
+  NontriviallyCopyable(int I) : I(I) {}
+  NontriviallyCopyable(const NontriviallyCopyable &X) : I(X.I) {}
 };
 
 template <typename name, typename Func>
-__attribute__((sycl_kernel)) void kernel(const Func &kernelFunc) {
-  kernelFunc();
+__attribute__((sycl_kernel)) void kernel(const Func &KernelFunc) {
+  KernelFunc();
 }
 
-__attribute__((sycl_device)) void device_func(NontriviallyCopyable ntc) {
-  (void)ntc;
+__attribute__((sycl_device)) void device_func(NontriviallyCopyable X) {
+  (void)X;
 }
 
 int main() {
-  NontriviallyCopyable nontrivial{10};
+  NontriviallyCopyable NontriviallyCopyableObject{10};
   kernel<class kernel_name>(
     [=]() {
-      (void)nontrivial;
+      (void)NontriviallyCopyableObject;
     });
 }
 
