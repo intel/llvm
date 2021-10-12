18c7844f23570ea8e884f0ff1526b328a838d718
Steffen Larsen <steffen.larsen@intel.com>
2021-10-12 11:59:40 +0300

    Change tests to use SYCL mock headers
    Signed-off-by: Steffen Larsen <steffen.larsen@intel.com>

--- clang/test/CodeGenSYCL/nontrivial_device_copyable.cpp
+++ clang/test/CodeGenSYCL/nontrivial_device_copyable.cpp
@@ -3,29 +3,29 @@
 // Tests that SYCL kernel arguments with non-trivially copyable types are
 // passed by-valued.
 
+#include "Inputs/sycl.hpp"
+using namespace cl::sycl;
+
 struct NontriviallyCopyable {
   int I;
   NontriviallyCopyable(int I) : I(I) {}
   NontriviallyCopyable(const NontriviallyCopyable &X) : I(X.I) {}
 };
 
-template <typename name, typename Func>
-__attribute__((sycl_kernel)) void kernel(const Func &KernelFunc) {
-  KernelFunc();
-}
-
-__attribute__((sycl_device)) void device_func(NontriviallyCopyable X) {
+void device_func(NontriviallyCopyable X) {
   (void)X;
 }
 
 int main() {
   NontriviallyCopyable NontriviallyCopyableObject{10};
-  kernel<class kernel_name>(
-    [=]() {
-      (void)NontriviallyCopyableObject;
+
+  queue Q;
+  Q.submit([&](handler &CGH) {
+    CGH.single_task<class kernel_name>([=]() {
+      device_func(NontriviallyCopyableObject);
     });
+  });
 }
 
-// SYCL kernels
 // CHECK: define {{.*}}spir_kernel void @{{.*}}kernel_name(%struct.NontriviallyCopyable* byval(%struct.NontriviallyCopyable)
-// CHECK-NOT: define {{.*}}spir_func void @{{.*}}device_func({{.*}}byval(%struct.NontriviallyCopyable)
+// CHECK-NOT: define {{.*}}spir_func void @{{.*}}device_func{{.*}}({{.*}}byval(%struct.NontriviallyCopyable)
