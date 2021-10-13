0f3df62a1d07930590ea03b21d934b5792bc9667
dongkyunahn-intel <dongkyun.ahn@intel.com>
2021-10-13 07:59:46 -0700

    Update sycl/include/sycl/ext/intel/experimental/esimd/emu/detail/esimdemu_device_interface.hpp
    Co-authored-by: kbobrovs <Konstantin.S.Bobrovsky@intel.com>

--- sycl/include/sycl/ext/intel/experimental/esimd/emu/detail/esimdemu_device_interface.hpp
+++ sycl/include/sycl/ext/intel/experimental/esimd/emu/detail/esimdemu_device_interface.hpp
@@ -1,4 +1,4 @@
-//==----- esimdemu_device_interface.hpp - DPC++ Explicit SIMD API ---------==//
+//==----- esimdemu_device_interface.hpp - DPC++ Explicit SIMD API ----------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
