a7a6a218adebc915419e2690f1e01efa25f3c395
Pavel Samolysov <pavel.samolysov@intel.com>
2021-10-13 12:18:47 +0300

    [SYCL] Fix formatting after update

--- sycl/include/CL/sycl/buffer.hpp
+++ sycl/include/CL/sycl/buffer.hpp
@@ -39,11 +39,11 @@ make_buffer_helper(pi_native_handle Handle, const context &Ctx, event Evt) {
 /// \sa sycl_api_acc
 ///
 /// \ingroup sycl_api
-template <
-    typename T, int dimensions = 1,
-    typename AllocatorT = cl::sycl::detail::aligned_allocator<char>,
-    typename __Enabled =
-        typename detail::enable_if_t<(dimensions > 0) && (dimensions <= 3)>>
+template <typename T, int dimensions = 1,
+          typename AllocatorT = cl::sycl::detail::aligned_allocator<char>,
+          typename __Enabled = typename detail::enable_if_t<(dimensions > 0) &&
+                                                            (dimensions <= 3)>>
+
 class buffer {
 public:
   using value_type = T;
