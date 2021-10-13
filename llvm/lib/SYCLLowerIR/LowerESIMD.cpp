e3a94bc89d1e092b0aa5f3dfc2d69d95a97fe693
Dongkyun Ahn <dongkyun.ahn@intel.com>
2021-10-13 11:46:53 -0700

    Removing unnecessary comment

--- llvm/lib/SYCLLowerIR/LowerESIMD.cpp
+++ llvm/lib/SYCLLowerIR/LowerESIMD.cpp
@@ -1474,7 +1474,6 @@ size_t SYCLLowerESIMDPass::runOnFunction(Function &F,
 
       // process ESIMD builtins that go through special handling instead of
       // the translation procedure
-      // TODO FIXME slm_init should be made top-level __esimd_slm_init
       if (Name.startswith("__esimd_slm_init")) {
         // tag the kernel with meta-data SLMSize, and remove this builtin
         translateSLMInit(*CI);
