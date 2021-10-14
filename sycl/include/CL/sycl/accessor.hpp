71e8d97d3a79bbb649c8527ee437a2db06ae03cc
Zahira Ammarguellat <zahira.ammarguellat@intel.com>
2021-10-14 11:27:50 -0700

    Respond to review comment
    Signed-off-by: Zahira Ammarguellat <zahira.ammarguellat@intel.com>

--- sycl/include/CL/sycl/accessor.hpp
+++ sycl/include/CL/sycl/accessor.hpp
@@ -1574,16 +1574,16 @@ public:
       const size_t LinearIndex = getLinearIndex(id<AdjustedDim>());
   return atomic<DataT, AS>(
       multi_ptr<DataT, AS>(getQualifiedPtr() + LinearIndex));
-}
+  }
 
-template <int Dims = Dimensions>
-typename detail::enable_if_t<(Dims > 0) && AccessMode == access::mode::atomic,
+  template <int Dims = Dimensions>
+  typename detail::enable_if_t<(Dims > 0) && AccessMode == access::mode::atomic,
                              atomic<DataT, AS>>
-operator[](id<Dimensions> Index) const {
-  const size_t LinearIndex = getLinearIndex(Index);
-  return atomic<DataT, AS>(
-      multi_ptr<DataT, AS>(getQualifiedPtr() + LinearIndex));
-}
+  operator[](id<Dimensions> Index) const {
+    const size_t LinearIndex = getLinearIndex(Index);
+    return atomic<DataT, AS>(
+        multi_ptr<DataT, AS>(getQualifiedPtr() + LinearIndex));
+  }
 
   template <int Dims = Dimensions>
   typename detail::enable_if_t<Dims == 1 && AccessMode == access::mode::atomic,
