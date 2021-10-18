cae1ece7f2fd0469a997adc3c9fdbabdaeddef62
Sergey Kanaev <sergey.kanaev@intel.com>
2021-10-18 20:01:48 +0300

    [SYCL] Disable submission of AssertInfoCopier for FPGA
    Signed-off-by: Sergey Kanaev <sergey.kanaev@intel.com>

--- sycl/include/CL/sycl/queue.hpp
+++ sycl/include/CL/sycl/queue.hpp
@@ -258,10 +258,11 @@ public:
       auto PostProcess = [this, &CodeLoc](bool IsKernel, bool KernelUsesAssert,
                                           event &E) {
         if (IsKernel && !device_has(aspect::ext_oneapi_native_assert) &&
-            KernelUsesAssert) {
+            KernelUsesAssert && !device_has(aspect::accelerator)) {
           // __devicelib_assert_fail isn't supported by Device-side Runtime
           // Linking against fallback impl of __devicelib_assert_fail is
           // performed by program manager class
+          // Fallback assert isn't supported for FPGA
           submitAssertCapture(*this, E, /* SecondaryQueue = */ nullptr,
                               CodeLoc);
         }
@@ -299,7 +300,7 @@ public:
       auto PostProcess = [this, &SecondaryQueue, &CodeLoc](
                              bool IsKernel, bool KernelUsesAssert, event &E) {
         if (IsKernel && !device_has(aspect::ext_oneapi_native_assert) &&
-            KernelUsesAssert) {
+            KernelUsesAssert && !device_has(aspect::accelerator)) {
           // Only secondary queues on devices need to be added to the assert
           // capture.
           // TODO: Handle case where primary queue is host but the secondary
@@ -309,6 +310,7 @@ public:
           // __devicelib_assert_fail isn't supported by Device-side Runtime
           // Linking against fallback impl of __devicelib_assert_fail is
           // performed by program manager class
+          // Fallback assert isn't supported for FPGA
           submitAssertCapture(*this, E, DeviceSecondaryQueue, CodeLoc);
         }
       };
