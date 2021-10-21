bd3b3166078845dd63eaa3ad057a13471f8ece23
Michael D Toguchi <michael.d.toguchi@intel.com>
2021-10-21 16:45:39 -0700

    [Driver][SYCL][FPGA] implied default device forces emulation
    When using -Xshardware or -Xssimulation on the command line with
    FPGA, the device AOT compilation should be performed by aoc.  This
    was not occuring due to the implied default device when one such
    object or archive was supplied on the command line.
    Make adjustments to our discovery of FPGA hardware/simulation mode
    by only checking the actual FPGA toolchain and not just the first
    offload toolchain encountered.

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -1473,18 +1473,23 @@ Compilation *Driver::BuildCompilation(ArrayRef<const char *> ArgList) {
   // Determine FPGA emulation status.
   if (C->hasOffloadToolChain<Action::OFK_SYCL>()) {
     auto SYCLTCRange = C->getOffloadToolChains<Action::OFK_SYCL>();
-    ArgStringList TargetArgs;
-    const ToolChain *TC = SYCLTCRange.first->second;
-    const toolchains::SYCLToolChain *SYCLTC =
-        static_cast<const toolchains::SYCLToolChain *>(TC);
-    SYCLTC->TranslateBackendTargetArgs(SYCLTC->getTriple(), *TranslatedArgs,
-                                       TargetArgs);
-    for (StringRef ArgString : TargetArgs) {
-      if (ArgString.equals("-hardware") || ArgString.equals("-simulation")) {
-        setFPGAEmulationMode(false);
+    for (auto TI = SYCLTCRange.first, TE = SYCLTCRange.second; TI != TE; ++TI)
+      if (TI->second->getTriple().getSubArch() ==
+          llvm::Triple::SPIRSubArch_fpga) {
+        ArgStringList TargetArgs;
+        const toolchains::SYCLToolChain *FPGATC =
+            static_cast<const toolchains::SYCLToolChain *>(TI->second);
+        FPGATC->TranslateBackendTargetArgs(FPGATC->getTriple(), *TranslatedArgs,
+                                           TargetArgs);
+        for (StringRef ArgString : TargetArgs) {
+          if (ArgString.equals("-hardware") ||
+              ArgString.equals("-simulation")) {
+            setFPGAEmulationMode(false);
+            break;
+          }
+        }
         break;
       }
-    }
   }
 
   // Construct the list of abstract actions to perform for this compilation. On
