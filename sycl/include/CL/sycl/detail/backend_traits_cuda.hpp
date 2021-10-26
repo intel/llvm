be53b408332325844cc5ce37491b80ef8c062271
Pavel Samolysov <pavel.samolysov@intel.com>
2021-10-26 10:17:13 +0300

    [SYCL][NFC] Copy TODO comments from opencl backend trait to the others
    The comments are actual for the all implemented in the project backend.

--- sycl/include/CL/sycl/detail/backend_traits_cuda.hpp
+++ sycl/include/CL/sycl/detail/backend_traits_cuda.hpp
@@ -39,7 +39,11 @@ __SYCL_INLINE_NAMESPACE(cl) {
 namespace sycl {
 namespace detail {
 
-// TODO see comments in sycl/backend/opencl.hpp
+// TODO the interops for context, device, event, platform and program
+// may be removed after removing the deprecated 'get_native()' methods
+// from the corresponding classes. The interop<backend, queue> specialization
+// is also used in the get_queue() method of the deprecated class
+// interop_handler and also can be removed after API cleanup.
 template <> struct interop<backend::ext_oneapi_cuda, context> {
   using type = CUcontext;
 };
@@ -62,6 +66,8 @@ template <> struct interop<backend::ext_oneapi_cuda, program> {
 };
 #endif
 
+// TODO the interops for accessor is used in the already deprecated class
+// interop_handler and can be removed after API cleanup.
 template <typename DataT, int Dimensions, access::mode AccessMode>
 struct interop<backend::ext_oneapi_cuda,
                accessor<DataT, Dimensions, AccessMode, access::target::device,
--- sycl/include/CL/sycl/detail/backend_traits_level_zero.hpp
+++ sycl/include/CL/sycl/detail/backend_traits_level_zero.hpp
@@ -51,7 +51,11 @@ enum class ownership { transfer, keep };
 
 namespace detail {
 
-// TODO see comments in sycl/backend/opencl.hpp
+// TODO the interops for context, device, event, platform and program
+// may be removed after removing the deprecated 'get_native()' methods
+// from the corresponding classes. The interop<backend, queue> specialization
+// is also used in the get_queue() method of the deprecated class
+// interop_handler and also can be removed after API cleanup.
 template <> struct interop<backend::ext_oneapi_level_zero, context> {
   using type = ze_context_handle_t;
 };
@@ -78,6 +82,8 @@ template <> struct interop<backend::ext_oneapi_level_zero, program> {
 };
 #endif
 
+// TODO the interops for accessor is used in the already deprecated class
+// interop_handler and can be removed after API cleanup.
 template <typename DataT, int Dimensions, access::mode AccessMode>
 struct interop<backend::ext_oneapi_level_zero,
                accessor<DataT, Dimensions, AccessMode, access::target::device,
