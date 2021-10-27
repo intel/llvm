303e0421fec9cf33688b40a58752798907df30ad
Artur Gainullin <artur.gainullin@intel.com>
2021-10-26 22:32:39 -0700

    [SYCL] Enable indirect access tracking for multi-threaded assertion tests (intel/llvm-test-suite#538)
    Memory pooling is being disabled by default:
    https://github.com/intel/llvm/pull/4799
    
    This makes necessary to enable indirect access tracking feature for
    multi-threaded assertion tests. Memory pooling was resulting in deferred
    memory release that's why this issue was not reproducing before.

--- SYCL/Assert/assert_in_simultaneously_multiple_tus.cpp
+++ SYCL/Assert/assert_in_simultaneously_multiple_tus.cpp
@@ -3,7 +3,13 @@
 // RUN: %clangxx -DSYCL_ENABLE_FALLBACK_ASSERT -fsycl -fsycl-targets=%sycl_triple -I %S/Inputs %s %S/Inputs/kernels_in_file2.cpp -o %t.out %threads_lib
 // RUN: %CPU_RUN_PLACEHOLDER %t.out &> %t.txt || true
 // RUN: %CPU_RUN_PLACEHOLDER FileCheck %s --input-file %t.txt
-// RUN: %GPU_RUN_PLACEHOLDER %t.out &> %t.txt || true
+//
+// Since this is a multi-threaded application enable memory tracking and
+// deferred release feature in the Level Zero plugin to avoid releasing memory
+// too early. This is necessary because currently SYCL RT sets indirect access
+// flag for all kernels and the Level Zero runtime doesn't support deferred
+// release yet.
+// RUN: env SYCL_PI_LEVEL_ZERO_TRACK_INDIRECT_ACCESS_MEMORY=1 %GPU_RUN_PLACEHOLDER %t.out &> %t.txt || true
 // RUN: %GPU_RUN_PLACEHOLDER FileCheck %s --input-file %t.txt
 // RUN: %ACC_RUN_PLACEHOLDER %t.out &> %t.txt || true
 // RUN: %ACC_RUN_PLACEHOLDER FileCheck %s --input-file %t.txt
