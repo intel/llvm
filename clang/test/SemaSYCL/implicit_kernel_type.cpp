f737f98c8aaad840070f1b3a409430e6fec8e6b2
Soumi Manna <soumi.manna@intel.com>
2021-11-01 13:10:37 -0700

    Remove DERROR Macro from test

--- clang/test/SemaSYCL/implicit_kernel_type.cpp
+++ clang/test/SemaSYCL/implicit_kernel_type.cpp
@@ -22,14 +22,12 @@ class myWrapper2;
 int main() {
   queue q;
 
-#if defined(ERROR)
   class InvalidKernelName1 {};
   // expected-error@#KernelSingleTask {{'InvalidKernelName1' is invalid; kernel name should be forward declarable at namespace scope}}
   // expected-note@+2 {{in instantiation of function template specialization}}
   q.submit([&](handler &h) {
     h.single_task<InvalidKernelName1>([]() {});
   });
-#endif
 
 #if defined(WARN)
   // expected-warning@#KernelSingleTask {{SYCL 1.2.1 specification requires an explicit forward declaration for a kernel type name; your program may not be portable}}
