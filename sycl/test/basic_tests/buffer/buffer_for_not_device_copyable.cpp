16051f9cd59ad56749e9913605cc3e421607b4a8
Pavel Samolysov <pavel.samolysov@intel.com>
2021-11-01 15:21:41 +0300

    [SYCL] Check if the underlying buffer type is device copyable
    The implementation should diagnose an error if the underlying type "T"
    of a buffer is not device copyable. The spec says that this is
    a requirement for all buffers. It does not make any exception for
    buffers that are used only on the host:
    
    "The underlying data type of a buffer T must be device copyable as
    defined in Section 3.13.1."
    
    (https://www.khronos.org/registry/SYCL/specs/sycl-2020/html/sycl-2020.html#subsec:buffers)

---
+++ sycl/test/basic_tests/buffer/buffer_for_not_device_copyable.cpp
@@ -0,0 +1,16 @@
+// RUN: %clangxx -fsycl -fsyntax-only -Xclang -verify %s -Xclang -verify-ignore-unexpected=note,warning
+
+#include <sycl/sycl.hpp>
+#include <string>
+
+using namespace cl::sycl;
+
+int main() {
+  std::vector<int> iv(5, 1);
+  buffer b1(iv.data(), range<1>(5));
+
+  static_assert(!sycl::is_device_copyable_v<std::string>);
+  std::vector<std::string> sv{"hello", "sycl", "world"};
+  buffer b2(sv.data(), range<1>(3));
+  //expected-error@CL/sycl/buffer.hpp:* {{"The underlying data type of a buffer 'T' must be device copyable"}}
+}
