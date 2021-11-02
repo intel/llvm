8610da40e2a68f43a166e849ebdbb49785886f09
Pavel Samolysov <pavel.samolysov@intel.com>
2021-11-02 19:08:01 +0300

    [SYCL] Workaround: mark sycl::vec as device copyable on host
    See commit https://github.com/intel/llvm/commit/43c6d7a4e1f78, the
    sycl::vec class is not trivially copyable on the host side, so it is
    not considered as device copyable. A partial specialization for the
    sycl::is_device_copyable for sycl::vec is implemented to mark the class
    as device copyable on the host side.

--- sycl/include/CL/sycl/types.hpp
+++ sycl/include/CL/sycl/types.hpp
@@ -2420,6 +2420,15 @@ struct is_device_copyable<sycl::marray<T, N>,
                           std::enable_if_t<is_device_copyable<T>::value>>
     : std::true_type {};
 
+// vec is device copyable on host if element type is device copyable,
+// on device vec is trivially copyable and therefore is device copyable too.
+#ifndef __SYCL_DEVICE_ONLY__
+template <typename T, int N>
+struct is_device_copyable<sycl::vec<T, N>,
+                          std::enable_if_t<is_device_copyable<T>::value>>
+    : std::true_type {};
+#endif
+
 namespace detail {
 template <typename T, typename = void>
 struct IsDeprecatedDeviceCopyable : std::false_type {};
