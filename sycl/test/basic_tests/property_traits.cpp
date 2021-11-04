be628bf92ea3ae9437dfca084ad2e0f264a94db7
Steffen Larsen <steffen.larsen@intel.com>
2021-11-04 12:26:16 +0300

    Add negative tests and fix host-accessor tests
    Signed-off-by: Steffen Larsen <steffen.larsen@intel.com>

--- sycl/test/basic_tests/property_traits.cpp
+++ sycl/test/basic_tests/property_traits.cpp
@@ -22,9 +22,34 @@
                 "no specialization of is_property_of_v for " #PROP             \
                 " and " #__VA_ARGS__)
 
+#define CHECK_IS_NOT_PROPERTY(PROP)                                            \
+  static_assert(!is_property<PROP>::value,                                     \
+                "no specialization of is_property for " #PROP)
+
+#define CHECK_IS_NOT_PROPERTY_V(PROP)                                          \
+  static_assert(!is_property_v<PROP>,                                          \
+                "no specialization of is_property_v for " #PROP)
+
+#define CHECK_IS_NOT_PROPERTY_OF(PROP, ...)                                    \
+  static_assert(!is_property_of<PROP, __VA_ARGS__>::value,                     \
+                "no specialization of is_property_of for " #PROP               \
+                " and " #__VA_ARGS__)
+
+#define CHECK_IS_NOT_PROPERTY_OF_V(PROP, ...)                                  \
+  static_assert(!is_property_of_v<PROP, __VA_ARGS__>,                          \
+                "no specialization of is_property_of_v for " #PROP             \
+                " and " #__VA_ARGS__)
+
+class NotAProperty {};
+class NotASYCLObject {};
+
 using namespace cl::sycl;
 
 int main() {
+  //----------------------------------------------------------------------------
+  // is_property positive tests
+  //----------------------------------------------------------------------------
+
   // Accessor is_property
   CHECK_IS_PROPERTY(property::no_init);
   CHECK_IS_PROPERTY(ext::oneapi::property::no_offset);
@@ -54,6 +79,16 @@ int main() {
   // Reduction is_property
   CHECK_IS_PROPERTY(property::reduction::initialize_to_identity);
 
+  //----------------------------------------------------------------------------
+  // is_property negative tests
+  //----------------------------------------------------------------------------
+
+  CHECK_IS_NOT_PROPERTY(NotAProperty);
+
+  //----------------------------------------------------------------------------
+  // is_property_of positive tests
+  //----------------------------------------------------------------------------
+
   // Accessor is_property_of
   CHECK_IS_PROPERTY_OF(
       property::no_init,
@@ -78,7 +113,7 @@ int main() {
 
   // Host-accessor is_property_of
   CHECK_IS_PROPERTY_OF(property::no_init,
-                       accessor<unsigned long, 2, access_mode::read>);
+                       host_accessor<unsigned long, 2, access_mode::read>);
 
   // Buffer is_property_of
   CHECK_IS_PROPERTY_OF(property::buffer::use_host_ptr, buffer<int, 2>);
@@ -101,7 +136,54 @@ int main() {
   CHECK_IS_PROPERTY_OF(property::queue::enable_profiling, queue);
   CHECK_IS_PROPERTY_OF(property::queue::cuda::use_default_stream, queue);
 
+  //----------------------------------------------------------------------------
+  // is_property_of positive tests
+  //----------------------------------------------------------------------------
+
+  // Valid properties with invalid object type
+  CHECK_IS_NOT_PROPERTY_OF(property::no_init, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF(ext::oneapi::property::no_offset, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF(ext::oneapi::property::no_alias, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF(ext::intel::property::buffer_location,
+                           NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF(property::no_init, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF(property::no_init, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF(property::buffer::use_host_ptr, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF(property::buffer::use_mutex, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF(property::buffer::context_bound, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF(property::buffer::mem_channel, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF(
+      ext::oneapi::property::buffer::use_pinned_host_memory, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF(property::context::cuda::use_primary_context,
+                           NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF(property::image::use_host_ptr, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF(property::image::use_mutex, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF(property::image::context_bound, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF(property::queue::in_order, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF(property::queue::enable_profiling, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF(property::queue::cuda::use_default_stream,
+                           NotASYCLObject);
+
+  // Invalid properties with valid object type
+  CHECK_IS_NOT_PROPERTY_OF(NotAProperty, accessor<int, 1>);
+  CHECK_IS_NOT_PROPERTY_OF(
+      NotAProperty,
+      image_accessor<float, 2, access_mode::read, target::host_buffer,
+                     access::placeholder::true_t>);
+  CHECK_IS_NOT_PROPERTY_OF(NotAProperty, host_accessor<char, 2>);
+  CHECK_IS_NOT_PROPERTY_OF(NotAProperty, buffer<int, 2>);
+  CHECK_IS_NOT_PROPERTY_OF(NotAProperty, context);
+  CHECK_IS_NOT_PROPERTY_OF(NotAProperty, image<1>);
+  CHECK_IS_NOT_PROPERTY_OF(NotAProperty, queue);
+
+  // Invalid properties with invalid object type
+  CHECK_IS_NOT_PROPERTY_OF(NotAProperty, NotASYCLObject);
+
 #if __cplusplus > 201402L
+  //----------------------------------------------------------------------------
+  // is_property_v positive tests
+  //----------------------------------------------------------------------------
+
   // Accessor is_property_v
   CHECK_IS_PROPERTY_V(property::no_init);
   CHECK_IS_PROPERTY_V(ext::oneapi::property::no_offset);
@@ -128,6 +210,16 @@ int main() {
   CHECK_IS_PROPERTY_V(property::queue::enable_profiling);
   CHECK_IS_PROPERTY_V(property::queue::cuda::use_default_stream);
 
+  //----------------------------------------------------------------------------
+  // is_property_v negative tests
+  //----------------------------------------------------------------------------
+
+  CHECK_IS_NOT_PROPERTY_V(NotAProperty);
+
+  //----------------------------------------------------------------------------
+  // is_property_of_v positive tests
+  //----------------------------------------------------------------------------
+
   // Accessor is_property_of_v
   CHECK_IS_PROPERTY_OF_V(
       property::no_init,
@@ -150,9 +242,9 @@ int main() {
       image_accessor<unsigned long, 2, access_mode::read, target::host_buffer,
                      access::placeholder::true_t>);
 
-  // Host-ccessor is_property_of_v
+  // Host-accessor is_property_of_v
   CHECK_IS_PROPERTY_OF_V(property::no_init,
-                         accessor<unsigned long, 2, access_mode::read>);
+                         host_accessor<unsigned long, 2, access_mode::read>);
 
   // Buffer is_property_of_v
   CHECK_IS_PROPERTY_OF_V(property::buffer::use_host_ptr, buffer<int, 2>);
@@ -177,5 +269,48 @@ int main() {
 
   // Reduction is_property_v
   CHECK_IS_PROPERTY_V(property::reduction::initialize_to_identity);
+
+  //----------------------------------------------------------------------------
+  // is_property_of positive tests
+  //----------------------------------------------------------------------------
+
+  // Valid properties with invalid object type
+  CHECK_IS_NOT_PROPERTY_OF_V(property::no_init, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF_V(ext::oneapi::property::no_offset, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF_V(ext::oneapi::property::no_alias, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF_V(ext::intel::property::buffer_location,
+                             NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF_V(property::no_init, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF_V(property::no_init, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF_V(property::buffer::use_host_ptr, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF_V(property::buffer::use_mutex, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF_V(property::buffer::context_bound, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF_V(property::buffer::mem_channel, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF_V(
+      ext::oneapi::property::buffer::use_pinned_host_memory, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF_V(property::context::cuda::use_primary_context,
+                             NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF_V(property::image::use_host_ptr, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF_V(property::image::use_mutex, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF_V(property::image::context_bound, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF_V(property::queue::in_order, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF_V(property::queue::enable_profiling, NotASYCLObject);
+  CHECK_IS_NOT_PROPERTY_OF_V(property::queue::cuda::use_default_stream,
+                             NotASYCLObject);
+
+  // Invalid properties with valid object type
+  CHECK_IS_NOT_PROPERTY_OF_V(NotAProperty, accessor<int, 1>);
+  CHECK_IS_NOT_PROPERTY_OF_V(
+      NotAProperty,
+      image_accessor<float, 2, access_mode::read, target::host_buffer,
+                     access::placeholder::true_t>);
+  CHECK_IS_NOT_PROPERTY_OF_V(NotAProperty, host_accessor<char, 2>);
+  CHECK_IS_NOT_PROPERTY_OF_V(NotAProperty, buffer<int, 2>);
+  CHECK_IS_NOT_PROPERTY_OF_V(NotAProperty, context);
+  CHECK_IS_NOT_PROPERTY_OF_V(NotAProperty, image<1>);
+  CHECK_IS_NOT_PROPERTY_OF_V(NotAProperty, queue);
+
+  // Invalid properties with invalid object type
+  CHECK_IS_NOT_PROPERTY_OF_V(NotAProperty, NotASYCLObject);
 #endif
 }
