4c93c786af274de2274f3e248d01a59e42742cc7
Pavel Samolysov <pavel.samolysov@intel.com>
2021-11-10 14:03:05 +0300

    [SYCL] Optimize imports in sycl::atomic_ref

--- sycl/include/CL/sycl/atomic_ref.hpp
+++ sycl/include/CL/sycl/atomic_ref.hpp
@@ -8,13 +8,12 @@
 
 #pragma once
 
-#include <CL/__spirv/spirv_ops.hpp>
 #include <CL/sycl/access/access.hpp>
 #include <CL/sycl/atomic.hpp>
 #include <CL/sycl/detail/defines.hpp>
-#include <CL/sycl/detail/spirv.hpp>
 #include <CL/sycl/memory_enums.hpp>
 #ifdef __SYCL_DEVICE_ONLY__
+#include <CL/sycl/detail/spirv.hpp>
 #include <CL/sycl/multi_ptr.hpp>
 #endif
 
