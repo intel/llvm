1bbece101a49c28953d1e8f57f92b8cbde7a6905
Denis Kabanov <denis.kabanov@intel.com>
2021-11-15 14:49:06 +0300

    Clang format for test

--- sycl/test/regression/check_vector_of_opencl_event.cpp
+++ sycl/test/regression/check_vector_of_opencl_event.cpp
@@ -2,22 +2,22 @@
 // expected-no-diagnostics
 //
 //===----------------------------------------------------------------------===//
-// This test checks that sycl::get_native<sycl::backend::opencl>(event) return 
+// This test checks that sycl::get_native<sycl::backend::opencl>(event) return
 // std::vector<cl_event> when backend = opencl, according to:
 // SYCL™ 2020 Specification (revision 3)
 //===----------------------------------------------------------------------===//
 
 #include <sycl/sycl.hpp>
 
-int main()
-{
+int main() {
 #ifdef SYCL_BACKEND_OPENCL
   sycl::queue Queue;
-  if (Queue.get_backend() == sycl::backend::opencl)
-  {
-    sycl::event event = Queue.submit([&](sycl::handler &cgh)
-                                     { cgh.single_task<class event_kernel>([]() {}); });
-    std::vector<cl_event> interopEventVec = sycl::get_native<sycl::backend::opencl>(event);
+  if (Queue.get_backend() == sycl::backend::opencl) {
+    sycl::event event = Queue.submit([&](sycl::handler &cgh) {
+      cgh.single_task<class event_kernel>([]() {});
+    });
+    std::vector<cl_event> interopEventVec =
+        sycl::get_native<sycl::backend::opencl>(event);
   }
 #endif
 }
