a42a50dcf26d126e8079950369dfaf555e901078
Michael D Toguchi <michael.d.toguchi@intel.com>
2021-11-17 15:49:26 -0800

    [SYCL] Add supplemental tool for SPIR-V to LLVM-IR conversions
    Tool name:
      spir-to-ir
    
    Function:
      Provides an interface used within the compiler driver toolchain which
      converts SPIR-V to LLVM-IR.  Using llvm-spirv as the main conversion
      tool, the purpose of this is to be able to take binaries and convert
      them to LLVM-IR.  The conversion only takes place if the input is
      SPIR-V, and if the LLVM-IR is provided it is simply copied and
      passed through.
    
      This tool is useful for handling generated fat objects/archives that
      contain SPIR-V as opposed to typical LLVM-IR.  We do not now the
      type of files within the device objects, so we use this tool to make
      sure that file consumed after unbundling is always LLVM-IR.
    
    Example usage:
      spir-to-ir input.spv -o output.bc  // creates LLVM-IR output.bc
      spir-to-ir input.bc -o output.bc   // no conversion, creates output.bc

---
+++ llvm/tools/spir-to-ir/spir-to-ir.cpp
@@ -0,0 +1,135 @@
+//===--- spir-to-ir.cpp - Utility to convert to ir if needed --------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This utility checks if the input file is SPIR based.  If so, convert to IR
+// The input can be either SPIR-V or LLVM-IR.  When LLVM-IR, copy the file to
+// the specified output.
+//
+// Uses llvm-spirv to perform the conversion if needed.
+//
+// The output file is used to allow for proper input and output flow within
+// the driver toolchain.
+//
+// Usage: spir-to-ir input.spv -o output.bc
+//
+//===----------------------------------------------------------------------===//
+
+#include "LLVMSPIRVLib.h"
+#include "llvm/BinaryFormat/Magic.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/Error.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/InitLLVM.h"
+#include "llvm/Support/Path.h"
+#include "llvm/Support/Program.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/StringSaver.h"
+
+using namespace llvm;
+
+// InputFilename - The filename to read from.
+static cl::opt<std::string> InputFilename(cl::Positional,
+                                          cl::desc("<input spv file>"),
+                                          cl::init("-"),
+                                          cl::value_desc("filename"));
+
+// Output - The filename to output to.
+static cl::opt<std::string> Output("o",
+                                   cl::desc("<output IR filename>"),
+                                   cl::value_desc("filename"));
+
+// LlvmSpirvOpts - The filename to output to.
+static cl::opt<std::string>
+    LlvmSpirvOpts("llvm-spirv-opts", cl::desc("<llvm-spirv options>"),
+                  cl::value_desc("options to pass to llvm-spirv"));
+
+static void error(const Twine &Message) {
+  llvm::errs() << "spir-to-ir: " << Message << '\n';
+  exit(1);
+}
+
+// Convert the SPIR-V to LLVM-IR.
+static int convertSPIRVToLLVMIR(const char *Argv0) {
+  // Find llvm-spirv.  It is expected this resides in the same directory
+  // as spir-to-ir.
+  StringRef ParentPath = llvm::sys::path::parent_path(Argv0);
+  llvm::ErrorOr<std::string> LlvmSpirvBinary =
+      llvm::sys::findProgramByName("llvm-spirv", ParentPath);
+  if (!LlvmSpirvBinary)
+    LlvmSpirvBinary = llvm::sys::findProgramByName("llvm-spirv");
+
+  SmallVector<StringRef, 6> LlvmSpirvArgs = {"llvm-spirv", "-r", InputFilename,
+                                             "-o", Output};
+
+  // Add any additional options specified by the user.
+  if (!LlvmSpirvOpts.empty()) {
+    SmallVector<const char *, 8> TargetArgs;
+    llvm::BumpPtrAllocator BPA;
+    llvm::StringSaver S(BPA);
+    // Tokenize the string.
+    llvm::cl::TokenizeGNUCommandLine(LlvmSpirvOpts, S, TargetArgs);
+    llvm::transform(TargetArgs, std::back_inserter(LlvmSpirvArgs),
+                    [](StringRef A) { return SmallString<32>(A); });
+  }
+  return llvm::sys::ExecuteAndWait(LlvmSpirvBinary.get(), LlvmSpirvArgs);
+}
+
+static int copyInputLLVMIRToOutput(void) {
+  // When given an output file, just copy the input to the output
+  if (!Output.empty() && !InputFilename.empty()) {
+    llvm::sys::fs::copy_file(InputFilename, Output);
+  }
+  return 0;
+}
+
+static bool isLLVMIRBinary(const std::string &File) {
+  if (File.size() < sizeof(unsigned))
+    return false;
+
+  StringRef Ext = llvm::sys::path::has_extension(File)
+                      ? llvm::sys::path::extension(File).drop_front()
+                      : "";
+  llvm::file_magic Magic;
+  llvm::identify_magic(File, Magic);
+
+  // Only .bc and bitcode files are to be considered.
+  return (Ext == "bc" || Magic == llvm::file_magic::bitcode);
+}
+
+static int checkInputFileIsAlreadyLLVM(const char *Argv0) {
+  if (InputFilename == "-")
+    return SPIRV::isSpirvBinary(InputFilename) ? convertSPIRVToLLVMIR(Argv0)
+                                               : copyInputLLVMIRToOutput();
+
+  if (isLLVMIRBinary(InputFilename))
+    return copyInputLLVMIRToOutput();
+
+  StringRef Ext = llvm::sys::path::has_extension(InputFilename)
+                      ? llvm::sys::path::extension(InputFilename).drop_front()
+                      : "";
+  if (Ext == "spv" || SPIRV::isSpirvBinary(InputFilename))
+    return convertSPIRVToLLVMIR(Argv0);
+
+  error("Could not determine input type for " + InputFilename);
+  return -1;
+}
+
+int main(int argc, char **argv) {
+  InitLLVM X(argc, argv);
+
+  LLVMContext Context;
+  cl::ParseCommandLineOptions(argc, argv, "spir-to-ir\n");
+
+  if (!llvm::sys::fs::exists(InputFilename))
+    error("Input file \'" + InputFilename + "\' not found");
+
+  if (Output.empty())
+    error("Output file not provided");
+
+  return checkInputFileIsAlreadyLLVM(argv[0]);
+}
