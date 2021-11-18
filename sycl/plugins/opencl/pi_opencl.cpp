3690165af6eed1534cc5b334f4ad085bf2feae5c
Sergey V Maslov <sergey.v.maslov@intel.com>
2021-11-18 12:40:44 -0800

    [SYCL] piextQueueCreateWithNativeHandle should not be followed by a piQueueRetain
    Signed-off-by: Sergey V Maslov <sergey.v.maslov@intel.com>

--- sycl/plugins/opencl/pi_opencl.cpp
+++ sycl/plugins/opencl/pi_opencl.cpp
@@ -389,6 +389,7 @@ pi_result piextQueueCreateWithNativeHandle(pi_native_handle nativeHandle,
   (void)ownNativeHandle;
   assert(piQueue != nullptr);
   *piQueue = reinterpret_cast<pi_queue>(nativeHandle);
+  clRetainCommandQueue(*piQueue);
   return PI_SUCCESS;
 }
 
