3aed5a2f5be0b3c8d1de4a316fac11af2a74cc1e
maximdimakov <maxim.dimakov@intel.com>
2021-11-24 10:57:24 +0300

    [SYCL] Test for event destruction (intel/llvm-test-suite#574)
    Signed-off-by: mdimakov <maxim.dimakov@intel.com>

---
+++ SYCL/Regression/event_destruction.cpp
@@ -0,0 +1,36 @@
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
+// RUN: %ACC_RUN_PLACEHOLDER %t.out
+// RUN: %HOST_RUN_PLACEHOLDER %t.out
+// RUN: %CPU_RUN_PLACEHOLDER %t.out
+// RUN: %GPU_RUN_PLACEHOLDER %t.out
+
+//==--------------- event_destruction.cpp - SYCL event test ----------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+#include <CL/sycl.hpp>
+#include <iostream>
+
+const size_t ITERS = 100000;
+
+// The test checks that that event destruction does not lead to stack overflow
+
+int main() {
+  cl::sycl::default_selector S;
+  cl::sycl::queue Q(S);
+  cl::sycl::buffer<int, 1> Buf(3000);
+  for (size_t Idx = 0; Idx < ITERS; ++Idx) {
+    auto Event = Q.submit([&](cl::sycl::handler &cgh) {
+      auto Acc = Buf.get_access<cl::sycl::access::mode::write>(cgh);
+      cgh.single_task([=]() {
+        for (size_t I = 0; I < 2000; I++) {
+          Acc[I] = I * I + 2000;
+        }
+      });
+    });
+    Event.wait();
+  }
+}
