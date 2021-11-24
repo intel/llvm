12ed2cfe683f39b1299f83ee3e4ab8b43da5a8c9
HabKaffee <nikita.naumov@intel.com>
2021-11-24 19:12:25 +0300

    hotfix

--- sycl/test/basic_tests/code_location.cpp
+++ sycl/test/basic_tests/code_location.cpp
@@ -9,21 +9,12 @@
 
 int main() {
   auto code_loc = sycl::detail::code_location::current();
-  const char *currentFunction = "main";
+  const char* funcName = "main";
 #ifdef NDEBUG
-  if (code_loc.fileName() != nullptr) {
-    return 1;
-  }
-  if (code_loc.functionName() != currentFunction) {
-    return 1;
-  }
-  if (code_loc.lineNumber() != 11) {
-    return 1;
-  }
-  if (code_loc.columnNumber() != 21) {
-    return 1;
-  }
-// std::cout << "NDEBUG asserts passed" << std::endl;
+  if (code_loc.fileName() != nullptr) return 1;
+  if (code_loc.functionName() != funcName) return 1;
+  if (code_loc.lineNumber() != 11) return 1;
+  if (code_loc.columnNumber() != 19) return 1;
 #else
   assert((code_loc.fileName() != nullptr));
   std::string str = code_loc.fileName();
@@ -31,13 +22,12 @@ int main() {
           "Filename is wrong"));
   assert((code_loc.functionName() != nullptr));
   str = code_loc.functionName();
-  assert(((str.find(currentFunction) != std::string::npos) &&
+  assert(((str.find(funcName) != std::string::npos) &&
           "Function name is wrong"));
   assert((code_loc.lineNumber() != 0));
   assert(((code_loc.lineNumber() == 11) && "Line number is wrong"));
   assert((code_loc.columnNumber() != 0));
-  assert(((code_loc.columnNumber() == 21) && "Column number is wrong"));
-// std::cout << "asserts without NDEBUG passed" << std::endl;
+  assert(((code_loc.columnNumber() == 19) && "Column number is wrong"));
 #endif
   return 0;
-}
+} 
