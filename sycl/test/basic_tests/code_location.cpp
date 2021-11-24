d7870a5c3f263426e539566145255dcbd2093781
HabKaffee <nikita.naumov@intel.com>
2021-11-24 14:56:17 +0300

    ЭLint fix

--- sycl/test/basic_tests/code_location.cpp
+++ sycl/test/basic_tests/code_location.cpp
@@ -8,34 +8,36 @@
 #include <iostream>
 
 int main() {
-    auto code_loc = sycl::detail::code_location::current();
-    const char* currentFunction = "main";
-    #ifdef NDEBUG
-    if (code_loc.fileName() != nullptr) {
-        return 1;
-    }
-    if (code_loc.functionName() != currentFunction) {
-        return 1;
-    }
-    if (code_loc.lineNumber() != 11) {
-        return 1;
-    }
-    if (code_loc.columnNumber() != 21){
-        return 1;
-    }
-    //std::cout << "NDEBUG asserts passed" << std::endl;
-    #else
-    assert((code_loc.fileName() != nullptr));
-    std::string str = code_loc.fileName();
-    assert(((str.find("code_location.cpp") != std::string::npos) && "Filename is wrong"));
-    assert((code_loc.functionName() != nullptr));
-    str = code_loc.functionName();
-    assert(((str.find(currentFunction) != std::string::npos) && "Function name is wrong"));
-    assert((code_loc.lineNumber() != 0));
-    assert(((code_loc.lineNumber() == 11) && "Line number is wrong"));
-    assert((code_loc.columnNumber() != 0));
-    assert(((code_loc.columnNumber() == 21) && "Column number is wrong"));
-    //std::cout << "asserts without NDEBUG passed" << std::endl;
-    #endif
-    return 0;
+  auto code_loc = sycl::detail::code_location::current();
+  const char *currentFunction = "main";
+#ifdef NDEBUG
+  if (code_loc.fileName() != nullptr) {
+    return 1;
+  }
+  if (code_loc.functionName() != currentFunction) {
+    return 1;
+  }
+  if (code_loc.lineNumber() != 11) {
+    return 1;
+  }
+  if (code_loc.columnNumber() != 21) {
+    return 1;
+  }
+// std::cout << "NDEBUG asserts passed" << std::endl;
+#else
+  assert((code_loc.fileName() != nullptr));
+  std::string str = code_loc.fileName();
+  assert(((str.find("code_location.cpp") != std::string::npos) &&
+          "Filename is wrong"));
+  assert((code_loc.functionName() != nullptr));
+  str = code_loc.functionName();
+  assert(((str.find(currentFunction) != std::string::npos) &&
+          "Function name is wrong"));
+  assert((code_loc.lineNumber() != 0));
+  assert(((code_loc.lineNumber() == 11) && "Line number is wrong"));
+  assert((code_loc.columnNumber() != 0));
+  assert(((code_loc.columnNumber() == 21) && "Column number is wrong"));
+// std::cout << "asserts without NDEBUG passed" << std::endl;
+#endif
+  return 0;
 }
