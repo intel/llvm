intel/llvm

commit e9f2d6409bc24f05b8e994cca1a19ed986a0ada7

[SYCL][XPTI] Enable code location info when NDEBUG is not defined (#5023)

sycl/include/CL/sycl/detail/common.hpp
@@ -24,30 +24,46 @@
 __SYCL_INLINE_NAMESPACE(cl) {
 namespace sycl {
 namespace detail {
+
+#if !defined(NDEBUG) && (_MSC_VER > 1929 || __has_builtin(__builtin_FILE))
+#define __CODELOC_FILE_NAME __builtin_FILE()
+#else
+#define __CODELOC_FILE_NAME nullptr
+#endif
+
+#if _MSC_VER > 1929 || __has_builtin(__builtin_FUNCTION)
+#define __CODELOC_FUNCTION __builtin_FUNCTION()
+#else
+#define __CODELOC_FUNCTION nullptr
+#endif
+
+#if _MSC_VER > 1929 || __has_builtin(__builtin_LINE)
+#define __CODELOC_LINE __builtin_LINE()
+#else
+#define __CODELOC_LINE 0
+#endif
+
+#if _MSC_VER > 1929 || __has_builtin(__builtin_LINE)
+#define __CODELOC_COLUMN __builtin_COLUMN()
+#else
+#define __CODELOC_COLUMN 0
+#endif
+
 // Data structure that captures the user code location information using the
 // builtin capabilities of the compiler
 struct code_location {
-#ifdef _MSC_VER
-  // Since MSVC does not support the required builtins, we
-  // implement the version with "unknown"s which is handled
-  // correctly by the instrumentation
-  static constexpr code_location current(const char *fileName = nullptr,
-                                         const char *funcName = nullptr,
-                                         unsigned long lineNo = 0,
-                                         unsigned long columnNo = 0) noexcept {
-    return code_location(fileName, funcName, lineNo, columnNo);
-  }
-#else
-  // FIXME Having a nullptr for fileName here is a short-term solution to
-  // workaround leak of full paths in builds
   static constexpr code_location
-  current(const char *fileName = nullptr,
-          const char *funcName = __builtin_FUNCTION(),
-          unsigned long lineNo = __builtin_LINE(),
-          unsigned long columnNo = 0) noexcept {
+  current(const char *fileName = __CODELOC_FILE_NAME,
+          const char *funcName = __CODELOC_FUNCTION,
+          unsigned long lineNo = __CODELOC_LINE,
+          unsigned long columnNo = __CODELOC_COLUMN) noexcept {
     return code_location(fileName, funcName, lineNo, columnNo);
   }
-#endif
+
+#undef __CODELOC_FILE_NAME
+#undef __CODELOC_FUNCTION
+#undef __CODELOC_LINE
+#undef __CODELOC_COLUMN
 
   constexpr code_location(const char *file, const char *func, int line,
                           int col) noexcept

sycl/test/basic_tests/code_location.cpp
@@ -1,0 +1,37 @@
+// RUN: %clangxx -fsycl -DNDEBUG %s -o %t.out
+// RUN: %t.out
+// RUN: %clangxx -fsycl %s -o %t.out
+// RUN: %t.out
+
+#include <CL/sycl.hpp>
+#include <cassert>
+#include <iostream>
+
+int main() {
+  auto code_loc = sycl::detail::code_location::current();
+  const char *funcName = "main";
+#ifdef NDEBUG
+  if (code_loc.fileName() != nullptr)
+    return 1;
+  if (code_loc.functionName() != funcName)
+    return 1;
+  if (code_loc.lineNumber() != 11)
+    return 1;
+  if (code_loc.columnNumber() != 19)
+    return 1;
+#else
+  assert((code_loc.fileName() != nullptr));
+  std::string str = code_loc.fileName();
+  assert(((str.find("code_location.cpp") != std::string::npos) &&
+          "Filename is wrong"));
+  assert((code_loc.functionName() != nullptr));
+  str = code_loc.functionName();
+  assert(
+      ((str.find(funcName) != std::string::npos) && "Function name is wrong"));
+  assert((code_loc.lineNumber() != 0));
+  assert(((code_loc.lineNumber() == 11) && "Line number is wrong"));
+  assert((code_loc.columnNumber() != 0));
+  assert(((code_loc.columnNumber() == 19) && "Column number is wrong"));
+#endif
+  return 0;
+}

commit 93a38405290caab1b598c2b00887855ac3e8a03a

[SYCL][XPTI] Fix incorrect ifdef breaking gcc build
This was introduced in #5023, and broke the build with recent gcc
(11.1), as gcc doesn't implement `__builtin_COLUMN` (according to
LanguageExtensions.rst).

The `ifdef` was simply checking for the wrong one, the build works fine
with this patch.

sycl/include/CL/sycl/detail/common.hpp
@@ -43,7 +43,7 @@ namespace detail {
 #define __CODELOC_LINE 0
 #endif
 
-#if _MSC_VER > 1929 || __has_builtin(__builtin_LINE)
+#if _MSC_VER > 1929 || __has_builtin(__builtin_COLUMN)
 #define __CODELOC_COLUMN __builtin_COLUMN()
 #else
 #define __CODELOC_COLUMN 0