790aa8ba7292aecf944d19e36e79e1c4edc8ff2e
shuoniu-intel <shuo.niu@intel.com>
2021-11-30 08:46:07 -0500

    [SYCL] [FPGA] Create DSP control header (#5035)
    Create header file that provides user API for FPGA DSP control feature.
    
    Related links:
    
        SPIR-V spec: https://github.com/KhronosGroup/SPIRV-Registry/blob/main/extensions/INTEL/SPV_INTEL_fpga_dsp_control.asciidoc
        SPIR-V implementation: 
    
        [SPIRV] New FPGA function attribute for DSP control KhronosGroup/SPIRV-LLVM-Translator#1046
    
    Test: intel/llvm-test-suite#591

--- sycl/include/sycl/ext/intel/fpga_extensions.hpp
+++ sycl/include/sycl/ext/intel/fpga_extensions.hpp
@@ -8,6 +8,7 @@
 
 #pragma once
 #include <sycl/ext/intel/fpga_device_selector.hpp>
+#include <sycl/ext/intel/fpga_dsp_control.hpp>
 #include <sycl/ext/intel/fpga_loop_fuse.hpp>
 #include <sycl/ext/intel/fpga_lsu.hpp>
 #include <sycl/ext/intel/fpga_reg.hpp>
