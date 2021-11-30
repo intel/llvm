bc79ab9a124efc612623def62a3bfad73f32a5ab
Mariya Podchishchaeva <mariya.podchishchaeva@intel.com>
2021-11-30 16:29:42 +0300

    Apply a suggestion and fix clang-format concern

--- clang/test/Frontend/sycl-int-header-diags.cpp
+++ clang/test/Frontend/sycl-int-header-diags.cpp
@@ -4,4 +4,3 @@
 // RUN: not %clang_cc1 -fsycl-is-device -fsycl-enable-int-header-diags %s 2>&1 | FileCheck --check-prefix=ERROR %s
 
 // ERROR: error: option '-fsycl-enable-int-header-diags' cannot be specified without '-fsycl-is-host'
-
