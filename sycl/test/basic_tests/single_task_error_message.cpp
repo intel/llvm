aaeee518a2cc229ddb10098add65e3f6af2e9eed
HabKaffee <nikita.naumov@intel.com>
2021-12-01 14:07:44 +0300

    Fix in single_task_error_message

--- sycl/test/basic_tests/single_task_error_message.cpp
+++ sycl/test/basic_tests/single_task_error_message.cpp
@@ -1,4 +1,4 @@
-// RUN: %clangxx -fsycl -fsyntax-only -Xclang -verify -Xclang -verify-ignore-unexpected=note -Xclang -verify-ignore-unexpected=error -o - %s
+// RUN: %clangxx %fsycl-host-only -fsyntax-only -Xclang -verify -Xclang -verify-ignore-unexpected=note -o - %s
 #include <CL/sycl.hpp>
 #include <iostream>
 int main(){
@@ -8,21 +8,10 @@ int main(){
         int sum  = 0;
         sycl::queue myQueue{};
         {
-            auto bufA   = sycl::buffer(&varA, sycl::range{1});
-            auto bufB   = sycl::buffer(&varB, sycl::range{1});
-            auto bufSum = sycl::buffer(&sum, sycl::range{1});
             myQueue.single_task([&](sycl::handler &cgh) {
-                // expected-error-re@CL/sycl/queue.hpp:691 {{static_assert failed due to requirement 'detail::check_fn_signature<(lambda at {{.*}}single_task_error_message.cpp:14:33), void ()>::value || detail::check_fn_signature<(lambda at {{.*}}single_task_error_message.cpp:14:33), void (sycl::kernel_handler)>::value' "sycl::queue.single_task() requires a kernel instead of command group. Use queue.submit() instead"}}
-                    auto accessorA      = sycl::accessor(bufA, cgh, sycl::read_only);
-                    // expected-error@-1 {{'sycl::buffer<int, 1, sycl::detail::aligned_allocator<char>, void> &' cannot be used as the type of a kernel parameter}}
-                    auto accessorB      = sycl::accessor(bufB, cgh, sycl::read_only);
-                    // expected-error@-1 {{'sycl::buffer<int, 1, sycl::detail::aligned_allocator<char>, void> &' cannot be used as the type of a kernel parameter}}
-                    auto accessorResult = sycl::accessor(bufSum, cgh, sycl::write_only);
-                    // expected-error@-1 {{'sycl::buffer<int, 1, sycl::detail::aligned_allocator<char>, void> &' cannot be used as the type of a kernel parameter}}
-                    cgh.single_task([=] {
-                        accessorResult[0] = accessorA[0] + accessorB[0];
-                    });
-                })
+                // expected-error-re@CL/sycl/queue.hpp:691 {{static_assert failed due to requirement '{{.*}}' "sycl::queue.single_task() requires a kernel instead of command group. Use queue.submit() instead"}}
+                // expected-error-re@CL/sycl/detail/cg_types.hpp:191 {{no matching function for call to object of type '(lambda at {{.*}}single_task_error_message.cpp:{{.*}})'}}
+            })
             .wait();
         }
     }
@@ -32,22 +21,11 @@ int main(){
         int sum  = 0;
         sycl::queue myQueue{};
         {
-            auto bufA   = sycl::buffer(&varA, sycl::range{1});
-            auto bufB   = sycl::buffer(&varB, sycl::range{1});
-            auto bufSum = sycl::buffer(&sum, sycl::range{1});
             sycl::event e {};
             myQueue.single_task(e, [&](sycl::handler &cgh) {
-                // expected-error-re@CL/sycl/queue.hpp:710 {{static_assert failed due to requirement 'detail::check_fn_signature<(lambda at {{.*}}single_task_error_message.cpp:39:36), void ()>::value || detail::check_fn_signature<(lambda at {{.*}}single_task_error_message.cpp:39:36), void (sycl::kernel_handler)>::value' "sycl::queue.single_task() requires a kernel instead of command group. Use queue.submit() instead"}}
-                    auto accessorA      = sycl::accessor(bufA, cgh, sycl::read_only);
-                    // expected-error@-1 {{'sycl::buffer<int, 1, sycl::detail::aligned_allocator<char>, void> &' cannot be used as the type of a kernel parameter}}
-                    auto accessorB      = sycl::accessor(bufB, cgh, sycl::read_only);
-                    // expected-error@-1 {{'sycl::buffer<int, 1, sycl::detail::aligned_allocator<char>, void> &' cannot be used as the type of a kernel parameter}}
-                    auto accessorResult = sycl::accessor(bufSum, cgh, sycl::write_only);
-                    // expected-error@-1 {{'sycl::buffer<int, 1, sycl::detail::aligned_allocator<char>, void> &' cannot be used as the type of a kernel parameter}}
-                    cgh.single_task([=] {
-                        accessorResult[0] = accessorA[0] + accessorB[0];
-                    });
-                })
+                // expected-error-re@CL/sycl/queue.hpp:710 {{static_assert failed due to requirement '{{.*}}' "sycl::queue.single_task() requires a kernel instead of command group. Use queue.submit() instead"}}
+                // expected-error-re@CL/sycl/detail/cg_types.hpp:191 {{no matching function for call to object of type '(lambda at {{.*}}single_task_error_message.cpp:{{.*}})'}}
+            })
             .wait();
         }
     }
@@ -57,22 +35,11 @@ int main(){
         int sum  = 0;
         sycl::queue myQueue{};
         {
-            auto bufA   = sycl::buffer(&varA, sycl::range{1});
-            auto bufB   = sycl::buffer(&varB, sycl::range{1});
-            auto bufSum = sycl::buffer(&sum, sycl::range{1});
             std::vector<sycl::event> vector_event;
             myQueue.single_task(vector_event, [&](sycl::handler &cgh) {
-                // expected-error-re@CL/sycl/queue.hpp:731 {{static_assert failed due to requirement 'detail::check_fn_signature<(lambda at {{.*}}single_task_error_message.cpp:64:47), void ()>::value || detail::check_fn_signature<(lambda at {{.*}}single_task_error_message.cpp:64:47), void (sycl::kernel_handler)>::value' "sycl::queue.single_task() requires a kernel instead of command group. Use queue.submit() instead"}}
-                    auto accessorA      = sycl::accessor(bufA, cgh, sycl::read_only);
-                    // expected-error@-1 {{'sycl::buffer<int, 1, sycl::detail::aligned_allocator<char>, void> &' cannot be used as the type of a kernel parameter}}
-                    auto accessorB      = sycl::accessor(bufB, cgh, sycl::read_only);
-                    // expected-error@-1 {{'sycl::buffer<int, 1, sycl::detail::aligned_allocator<char>, void> &' cannot be used as the type of a kernel parameter}}
-                    auto accessorResult = sycl::accessor(bufSum, cgh, sycl::write_only);
-                    // expected-error@-1 {{'sycl::buffer<int, 1, sycl::detail::aligned_allocator<char>, void> &' cannot be used as the type of a kernel parameter}}
-                    cgh.single_task([=] {
-                        accessorResult[0] = accessorA[0] + accessorB[0];
-                    });
-                })
+                // expected-error-re@CL/sycl/queue.hpp:731 {{static_assert failed due to requirement '{{.*}}' "sycl::queue.single_task() requires a kernel instead of command group. Use queue.submit() instead"}}
+                // expected-error-re@CL/sycl/detail/cg_types.hpp:191 {{no matching function for call to object of type '(lambda at {{.*}}single_task_error_message.cpp:{{.*}})'}}
+            })
             .wait();
         }
     }
