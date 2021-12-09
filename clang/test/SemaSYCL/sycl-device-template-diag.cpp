22b0d8e7a018bc7971fdbbf33e863598b79721bd
Premanand M Rao <premanand.m.rao@intel.com>
2021-12-09 08:25:14 -0800

    Address clang-format errors

--- clang/test/SemaSYCL/sycl-device-template-diag.cpp
+++ clang/test/SemaSYCL/sycl-device-template-diag.cpp
@@ -7,13 +7,13 @@
 
 using namespace cl::sycl;
 queue q;
-template <typename h> h *malloc_shared() ;
+template <typename h> h *malloc_shared();
 
 int global_value = -1;
 
-template<typename T>
+template <typename T>
 void kernel_wrapper() {
-  q.submit([&] (handler &h) {
+  q.submit([&](handler &h) {
     h.single_task([=] {
       // expected-error@+1{{SYCL kernel cannot use a non-const global variable}}
       (void)global_value;
@@ -28,7 +28,8 @@ int main() {
     h.single_task<class mykern>([=](auto g) {
       // expected-error@+1{{SYCL kernel cannot use a non-const global variable}}
       x[3] = global_value;
-    }, kh);
+    },
+                                kh);
   });
   kernel_wrapper<int>();
 }
