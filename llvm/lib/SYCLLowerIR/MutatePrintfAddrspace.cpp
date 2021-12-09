f46743494e8b05949b53a99fe3a525d544350124
Artem Gindinson <artem.gindinson@intel.com>
2021-12-09 14:34:03 +0300

    Huge refactoring
    Signed-off-by: Artem Gindinson <artem.gindinson@intel.com>

--- llvm/lib/SYCLLowerIR/MutatePrintfAddrspace.cpp
+++ llvm/lib/SYCLLowerIR/MutatePrintfAddrspace.cpp
@@ -24,8 +24,6 @@
 using namespace llvm;
 
 namespace {
-constexpr unsigned ConstantAddrspaceID = 2;
-
 // Wrapper for the pass to make it working with the old pass manager
 class SYCLMutatePrintfAddrspaceLegacyPass : public ModulePass {
 public:
@@ -57,38 +55,6 @@ Value *stripToMemorySource(Value *V) {
   return MemoryAccess->stripPointerCastsAndAliases();
 }
 
-Constant *getCASLiteral(Module *M, GlobalVariable *Literal,
-                        PointerType *CASLiteralType) {
-  StringRef CASLiteralName(Literal->getName().str() + "._AS2");
-  IRBuilder<> Builder(M->getContext());
-  Constant *Res = M->getOrInsertGlobal(CASLiteralName, CASLiteralType, [&] {
-    StringRef LiteralValue;
-    getConstantStringInfo(Literal, LiteralValue);
-    GlobalVariable *GV = Builder.CreateGlobalString(
-        LiteralValue, CASLiteralName, ConstantAddrspaceID, M);
-    GV->setLinkage(GlobalValue::LinkageTypes::InternalLinkage);
-    GV->setUnnamedAddr(GlobalValue::UnnamedAddr::None);
-    return GV;
-  });
-  // TODO: Create the literal in another way to ensure correct type instead
-  Res->mutateType(CASLiteralType);
-  return Res;
-}
-
-FunctionCallee getCASPrintfFunction(Function &GenericASPrintfFunc,
-                                    PointerType *CASLiteralType) {
-  auto *CASPrintfFuncTy = FunctionType::get(GenericASPrintfFunc.getReturnType(),
-                                            CASLiteralType, /*isVarArg=*/true);
-  FunctionCallee CASPrintfFunc =
-      GenericASPrintfFunc.getParent()->getOrInsertFunction(
-          "_Z18__spirv_ocl_printfPU3AS2Kcz", CASPrintfFuncTy,
-          GenericASPrintfFunc.getAttributes());
-  auto *Callee = cast<Function>(CASPrintfFunc.getCallee());
-  Callee->setCallingConv(CallingConv::SPIR_FUNC);
-  Callee->setDSOLocal(true);
-  return CASPrintfFunc;
-}
-
 struct CallReplacer {
   CallReplacer(CallInst *CI, Value *CASArg) : CI(CI), CASArg(CASArg) {}
 
@@ -101,69 +67,119 @@ private:
   CallInst *CI;
   Value *CASArg;
 };
-} // namespace
 
-PreservedAnalyses
-SYCLMutatePrintfAddrspacePass::run(Module &M, ModuleAnalysisManager &MAM) {
+class AddrspaceReplacer {
+public:
+  static constexpr unsigned ConstantAddrspaceID = 2;
+
+  AddrspaceReplacer() = delete;
+  AddrspaceReplacer(Module *M) : M(M) {
+    Type *Int8Ty = Type::getInt8Ty(M->getContext());
+    CASLiteralType = PointerType::get(Int8Ty, ConstantAddrspaceID);
+  }
+  ~AddrspaceReplacer() {
+    for (Function *F : FunctionsToDrop)
+      F->eraseFromParent();
+  }
+
+  void runOnFunctionDeclaration(Function *F) {
+    auto *LiteralType = F->getArg(0)->getType();
+    if (LiteralType->getPointerAddressSpace() == ConstantAddrspaceID)
+      // No need to replace the literal type and its printf users
+      return;
+    FunctionCallee CASPrintfFunc = getCASPrintfFunction(F);
+    SmallVector<CallReplacer, 16> CallsToReplace;
+    for (User *U : F->users()) {
+      if (!isa<CallInst>(U))
+        continue;
+      auto *CI = cast<CallInst>(U);
+      CallsToReplace.emplace_back(prepareCASArgForCall(CI));
+    }
+    for (CallReplacer &CR : CallsToReplace) {
+      CR.replaceWithFunction(CASPrintfFunc);
+      ++ReplacedCallsCount;
+    }
+    if (F->hasNUses(0))
+      FunctionsToDrop.emplace_back(F);
+  }
+
+  size_t getReplacedCallsCount() { return ReplacedCallsCount; }
+
+private:
+  Module *M;
+  PointerType *CASLiteralType;
   size_t ReplacedCallsCount = 0;
-
   // If the variadic version gets picked during FE compilation, we'll only have
   // 1 function to replace. However, unique declarations are emitted for each of
   // the non-variadic (variadic template) calls.
   SmallVector<Function *, 8> FunctionsToDrop;
+
+  FunctionCallee getCASPrintfFunction(Function *GenericASPrintfFunc) {
+    auto *CASPrintfFuncTy =
+        FunctionType::get(GenericASPrintfFunc->getReturnType(), CASLiteralType,
+                          /*isVarArg=*/true);
+    FunctionCallee CASPrintfFunc = M->getOrInsertFunction(
+        "_Z18__spirv_ocl_printfPU3AS2Kcz", CASPrintfFuncTy,
+        GenericASPrintfFunc->getAttributes());
+    auto *Callee = cast<Function>(CASPrintfFunc.getCallee());
+    Callee->setCallingConv(CallingConv::SPIR_FUNC);
+    Callee->setDSOLocal(true);
+    return CASPrintfFunc;
+  }
+
+  CallReplacer prepareCASArgForCall(CallInst *CI) {
+    auto getCASLiteral = [&](GlobalVariable *Literal) -> Constant * {
+      StringRef CASLiteralName(Literal->getName().str() + "._AS2");
+      IRBuilder<> Builder(M->getContext());
+      Constant *Res = M->getOrInsertGlobal(CASLiteralName, CASLiteralType, [&] {
+        StringRef LiteralValue;
+        getConstantStringInfo(Literal, LiteralValue);
+        GlobalVariable *GV = Builder.CreateGlobalString(
+            LiteralValue, CASLiteralName, ConstantAddrspaceID, M);
+        GV->setLinkage(GlobalValue::LinkageTypes::InternalLinkage);
+        GV->setUnnamedAddr(GlobalValue::UnnamedAddr::None);
+        return GV;
+      });
+      // TODO: Create the literal in another way to ensure correct type instead
+      Res->mutateType(CASLiteralType);
+      return Res;
+    };
+    Value *Stripped = stripToMemorySource(CI->getArgOperand(0));
+    Value *CASPrintfOperand = nullptr;
+    if (auto *Arg = dyn_cast<Argument>(Stripped)) {
+      Function *WrapperFunc = Arg->getParent();
+      Arg->mutateType(CASLiteralType);
+      CASPrintfOperand = Arg;
+      for (User *WrapperU : WrapperFunc->users()) {
+        auto *WrapperCI = cast<CallInst>(WrapperU);
+        Value *StrippedArg = stripToMemorySource(WrapperCI->getArgOperand(0));
+        // We're only expecting 1 level of wrappers, so cast unconditionally
+        auto *Literal = cast<GlobalVariable>(StrippedArg);
+        Constant *CASLiteral = getCASLiteral(Literal);
+        WrapperCI->setArgOperand(0, CASLiteral);
+      }
+    } else if (auto *Literal = dyn_cast<GlobalVariable>(Stripped)) {
+      CASPrintfOperand = getCASLiteral(Literal);
+    } else
+      llvm_unreachable(
+          "Unexpected literal operand type for device-side printf");
+    return {CI, CASPrintfOperand};
+  }
+};
+} // namespace
+
+PreservedAnalyses
+SYCLMutatePrintfAddrspacePass::run(Module &M, ModuleAnalysisManager &MAM) {
+  AddrspaceReplacer ASReplacer(&M);
   for (Function &F : M) {
     if (!F.isDeclaration())
       continue;
     if (!F.getName().startswith("_Z18__spirv_ocl_printf"))
       continue;
-
-    auto *LiteralType = F.getArg(0)->getType();
-    if (LiteralType->getPointerAddressSpace() == ConstantAddrspaceID)
-      // No need to replace the literal type and its printf users
-      continue;
-    auto *CASLiteralType = PointerType::get(
-        LiteralType->getPointerElementType(), ConstantAddrspaceID);
-    FunctionCallee CASPrintfFunc = getCASPrintfFunction(F, CASLiteralType);
-    SmallVector<CallReplacer, 16> CallsToReplace;
-
-    for (User *U : F.users()) {
-      if (!isa<CallInst>(U))
-        continue;
-      auto *CI = cast<CallInst>(U);
-      Value *Stripped = stripToMemorySource(CI->getArgOperand(0));
-      Value *CASPrintfOperand = nullptr;
-      if (auto *Arg = dyn_cast<Argument>(Stripped)) {
-        Function *WrapperFunc = Arg->getParent();
-        Arg->mutateType(CASLiteralType);
-        CASPrintfOperand = Arg;
-        for (User *WrapperU : WrapperFunc->users()) {
-          auto *WrapperCI = cast<CallInst>(WrapperU);
-          Value *StrippedArg = stripToMemorySource(WrapperCI->getArgOperand(0));
-          // We're only expecting 1 level of wrappers, so cast unconditionally
-          auto *Literal = cast<GlobalVariable>(StrippedArg);
-          Constant *CASLiteral = getCASLiteral(&M, Literal, CASLiteralType);
-          WrapperCI->setArgOperand(0, CASLiteral);
-        }
-      } else if (auto *Literal = dyn_cast<GlobalVariable>(Stripped)) {
-        CASPrintfOperand = getCASLiteral(&M, Literal, CASLiteralType);
-      } else
-        llvm_unreachable(
-            "Unexpected literal operand type for device-side printf");
-      CallsToReplace.emplace_back(CI, CASPrintfOperand);
-    }
-
-    for (CallReplacer &CR : CallsToReplace) {
-      CR.replaceWithFunction(CASPrintfFunc);
-      ++ReplacedCallsCount;
-    }
-    if (F.hasNUses(0))
-      FunctionsToDrop.emplace_back(&F);
+    ASReplacer.runOnFunctionDeclaration(&F);
   }
-
-  for (Function *F : FunctionsToDrop)
-    F->eraseFromParent();
-  return ReplacedCallsCount ? PreservedAnalyses::all()
-                            : PreservedAnalyses::none();
+  return ASReplacer.getReplacedCallsCount() ? PreservedAnalyses::all()
+                                            : PreservedAnalyses::none();
 }
 
 char SYCLMutatePrintfAddrspaceLegacyPass::ID = 0;
