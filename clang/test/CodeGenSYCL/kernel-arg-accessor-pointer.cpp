cca76adab07ad4208e67b057a14b193b3ee50e28
srividya sundaram <srividya.sundaram@intel.com>
2021-12-09 20:37:59 -0800

    Update test

--- clang/test/CodeGenSYCL/kernel-arg-accessor-pointer.cpp
+++ clang/test/CodeGenSYCL/kernel-arg-accessor-pointer.cpp
@@ -1,16 +1,13 @@
-// RUN: %clang_cc1 -fsycl-is-device -triple spir64-unknown-unknown -disable-llvm-passes -emit-llvm %s -o - | FileCheck %s
+// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple spir64-unknown-unknown -disable-llvm-passes -emit-llvm %s -o - | FileCheck %s
 
 // This test checks if the metadata "kernel-arg-runtime-aligned"
 // is generated if the kernel captures an accessor
 
-#include "Inputs/sycl.hpp"
+#include "sycl.hpp"
 
 using namespace cl::sycl;
 
-template <typename name, typename Func>
-__attribute__((sycl_kernel)) void a_kernel(const Func &kernelFunc) {
-  kernelFunc();
-}
+queue q;
 
 int main() {
 
@@ -18,10 +15,29 @@ int main() {
       accessor<int, 1, access::mode::read_write, access::target::global_buffer>;
   Accessor acc[2];
 
-  a_kernel<class kernel_A>(
-      [=]() {
-        acc[1].use();
-      });
+  // kernel_A parameters : int*, sycl::range<1>, sycl::range<1>, sycl::id<1>,
+  // int*, sycl::range<1>, sycl::range<1>,sycl::id<1>
+  q.submit([&](cl::sycl::handler &h) {
+    h.single_task<class kernel_A>([=]() {
+      acc[1].use();
+    });
+  });
+
+  // kernel_B parameters : none
+  q.submit([&](cl::sycl::handler &h) {
+    h.single_task<class kernel_B>([=]() {
+      int result = 5;
+    });
+  });
+
+  int a = 10;
+
+  // kernel_C parameters : int
+  q.submit([&](cl::sycl::handler &h) {
+    h.single_task<class kernel_C>([=]() {
+      int x = a;
+    });
+  });
 }
 
 // Check kernel_A parameters
@@ -36,7 +52,18 @@ int main() {
 // CHECK-SAME: %"struct.cl::sycl::id"* byval{{.*}}align 4 [[OFFSET2:%[a-zA-Z0-9_]+_8]])
 // CHECK-SAME: !kernel_arg_runtime_aligned !5
 
+// Check kernel_B parameters
+// CHECK: define {{.*}}spir_kernel void @{{.*}}kernel_B
+// CHECK-SAME: !kernel_arg_runtime_aligned !13
+
+// Check kernel_C parameters
+// CHECK: define {{.*}}spir_kernel void @{{.*}}kernel_C
+// CHECK-SAME: i32 [[MEM_ARG1:%[a-zA-Z0-9_]+]]
+// CHECK-SAME: !kernel_arg_runtime_aligned !15
+
 // Check kernel-arg-runtime-aligned metadata.
 // The value of any metadata element is 1 for any kernel arguments
 // that corresponds to the base pointer of an accessor and 0 otherwise.
 // CHECK: !5 = !{i1 true, i1 false, i1 false, i1 false, i1 true, i1 false, i1 false, i1 false}
+// CHECK: !13 = !{}
+// CHECK: !15 = !{i1 false}
