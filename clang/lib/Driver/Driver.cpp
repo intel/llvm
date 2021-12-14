0c8f897d07a306ee99ca69a572989669c7e84303
jinge90 <ge.jin@intel.com>
2021-12-14 14:48:38 +0800

    Enable online link for SYCL device library
    Signed-off-by: jinge90 <ge.jin@intel.com>

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -4612,7 +4612,7 @@ class OffloadingActionBuilder final {
         // AOT compilation.
         if (isSPIR) {
           SYCLDeviceLibLinked = addSYCLDeviceLibs(
-              TC, FullLinkObjects, true,
+              TC, FullLinkObjects, isSpirvAOT,
               C.getDefaultToolChain().getTriple().isWindowsMSVCEnvironment());
         }
 
--- sycl/source/detail/program_manager/program_manager.cpp
+++ sycl/source/detail/program_manager/program_manager.cpp
@@ -937,12 +937,7 @@ ProgramManager::ProgramPtr ProgramManager::build(
 
   bool LinkDeviceLibs = (DeviceLibReqMask != 0);
 
-  // TODO: Currently, online linking isn't implemented yet on Level Zero.
-  // To enable device libraries and unify the behaviors on all backends,
-  // online linking is disabled temporarily, all fallback device libraries
-  // will be linked offline. When Level Zero supports online linking, we need
-  // to remove the line of code below and switch back to online linking.
-  LinkDeviceLibs = false;
+  LinkDeviceLibs = true;
 
   // TODO: this is a temporary workaround for GPU tests for ESIMD compiler.
   // We do not link with other device libraries, because it may fail
