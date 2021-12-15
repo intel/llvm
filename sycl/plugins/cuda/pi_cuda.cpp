2e798df804e5cab08c22c4d3f9d408279f212d95
Dmitry Sidorov <dmitry.sidorov@intel.com>
2021-12-15 07:33:18 +0300

    [SYCL] Add number HW threads per EU query (#4901)
    The appropriate spec update: https://github.com/intel/llvm/pull/4876
    Test: https://github.com/intel/llvm-test-suite/pull/550
    
    Signed-off-by: Dmitry Sidorov <dmitry.sidorov@intel.com>

--- sycl/plugins/cuda/pi_cuda.cpp
+++ sycl/plugins/cuda/pi_cuda.cpp
@@ -1677,6 +1677,7 @@ pi_result cuda_piDeviceGetInfo(pi_device device, pi_device_info param_name,
   case PI_DEVICE_INFO_GPU_SLICES:
   case PI_DEVICE_INFO_GPU_SUBSLICES_PER_SLICE:
   case PI_DEVICE_INFO_GPU_EU_COUNT_PER_SUBSLICE:
+  case PI_DEVICE_INFO_GPU_HW_THREADS_PER_EU:
   case PI_DEVICE_INFO_MAX_MEM_BANDWIDTH:
     // TODO: Check if Intel device UUID extension is utilized for CUDA.
     // For details about this extension, see
