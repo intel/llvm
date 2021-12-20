7a13cb87389cee5151e489c7980bb0b1d2aec16b
asudarsa <arvind.sudarsanam@intel.com>
2021-12-20 14:46:23 -0500

    [SYCL][Plugin][L0] Fix queue creation test to not run on windows (intel/llvm-test-suite#621)
    Signed-off-by: Arvind Sudarsanam <arvind.sudarsanam@intel.com>

--- SYCL/Plugin/level_zero_queue_create.cpp
+++ SYCL/Plugin/level_zero_queue_create.cpp
@@ -1,6 +1,9 @@
 // REQUIRES: level_zero, level_zero_dev_kit
+// TODO: ZE_DEBUG=4 produces no output on Windows. Enable when fixed.
+// UNSUPPORTED: windows
+
 // RUN: %clangxx -fsycl -fsycl-unnamed-lambda -fsycl-targets=%sycl_triple %level_zero_options %s -o %t.out
-// RUN: env SYCL_DEVICE_FILTER=level_zero ZE_DEBUG=4 %GPU_RUN_PLACEHOLDER %t.out 2>&1 %GPU_CHECK_PLACEHOLDER
+// RUN: env ZE_DEBUG=4 %GPU_RUN_PLACEHOLDER %t.out 2>&1 %GPU_CHECK_PLACEHOLDER
 //
 // CHECK:  zeCommandQueueCreate = 1     \--->         zeCommandQueueDestroy = 1
 // The test is to check that there is only a single level zero queue created
