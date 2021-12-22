8778e38e09d47ec5b3e67a4a34724e7883c8e64e
Alexander Batashev <alexbatashev@outlook.com>
2021-12-22 22:08:52 +0300

    [SYCL] Disable failing test (intel/llvm-test-suite#658)

--- SYCL/XPTI/basic_event_collection.cpp
+++ SYCL/XPTI/basic_event_collection.cpp
@@ -1,4 +1,4 @@
-// REQUIRES: xptifw, opencl
+// REQUIRES: xptifw, opencl, TEMPORARILY_DISABLED
 // RUN: %clangxx %s -DXPTI_COLLECTOR -DXPTI_CALLBACK_API_EXPORTS %xptifw_lib %shared_lib %fPIC %cxx_std_optionc++17 -o %t_collector.dll
 // RUN: %clangxx -fsycl -fsycl-unnamed-lambda -fsycl-targets=%sycl_triple %s -o %t.out
 // RUN: env XPTI_TRACE_ENABLE=1 env XPTI_FRAMEWORK_DISPATCHER=%xptifw_dispatcher env XPTI_SUBSCRIBERS=%t_collector.dll env SYCL_DEVICE_FILTER=opencl %t.out | FileCheck %s 2>&1
