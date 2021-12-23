226679a0bfb951c7c352fbda6993d3aee51a0b22
Mariya Podchishchaeva <mariya.podchishchaeva@intel.com>
2021-12-23 19:37:43 +0300

    [SYCL] Error out instead of crash when templated global var used (#5195)
    SYCL deferred diagnostics couldn't catch the case like:
    ```
    template <typename T>
    class randomType {
     public:
      randomType() {}
      T val;
    };
    
    template <typename T>
    const randomType<T> Var;
    
    SYCL_EXTERNAL void foo() {
      (void)Var<int>;
    }
    ```
    And CodeGen was crashing due to attempt to emit invalid cast.
    That happened because use of `Var` should be diagnosed. The diagnostic
    wasn't emitted because at the places where deferred diagnostic is issued
    for this case, templated variable doesn't yet has initializer, or it has
    it but current lexical context is out of device code (in this particular
    case it will be a translation unit). This patch follows the logic
    invented by `finalizeOpenMPDelayedAnalysis` and `finalizeSYCLDelayedAnalysis`
    routines, i.e. emit immediate diagnostic when post-parsing of AST for
    deferred diagnostics happens.

--- clang/lib/Sema/SemaTemplateInstantiateDecl.cpp
+++ clang/lib/Sema/SemaTemplateInstantiateDecl.cpp
@@ -5717,10 +5717,6 @@ void Sema::InstantiateVariableInitializer(
 
   if (getLangOpts().CUDA)
     checkAllowedCUDAInitializer(Var);
-
-  if (getLangOpts().SYCLIsDevice && !checkAllowedSYCLInitializer(Var))
-    SYCLDiagIfDeviceCode(Var->getLocation(), diag::err_sycl_restrict)
-        << Sema::KernelConstStaticVariable;
 }
 
 /// Instantiate the definition of the given variable from its
