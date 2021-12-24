866749fe8d22463c0f37c6f24e89b326b588a345
Yuriy Kochetkov <yuriyx.kochetkov@intel.com>
2021-12-24 16:37:05 +0300

    [SYCL][ESIMD] Add non-const r-value reference type trait (intel/llvm-test-suite#657)
    To provide stong type guarantees for the tests by ensuring that universal
    references in the template functions are actually referring to the non-const
    r-value references

--- SYCL/ESIMD/api/functional/type_traits.hpp
+++ SYCL/ESIMD/api/functional/type_traits.hpp
@@ -26,4 +26,13 @@ template <typename T>
 inline constexpr bool is_sycl_floating_point_v{
     is_sycl_floating_point<T>::value};
 
+template <typename T>
+using is_nonconst_rvalue_reference =
+    std::bool_constant<std::is_rvalue_reference_v<T> &&
+                       !std::is_const_v<typename std::remove_reference_t<T>>>;
+
+template <typename T>
+inline constexpr bool is_nonconst_rvalue_reference_v{
+    is_nonconst_rvalue_reference<T>::value};
+
 } // namespace esimd_test::api::functional::type_traits
