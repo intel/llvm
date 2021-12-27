8f9d0d2b95af2a651e84f044ffd508c7911ec229
vladimirlaz <vladimir.lazarev@intel.com>
2021-12-27 18:47:45 +0300

    [SYCL] Add XPTI instrumentation for SYCL buffer (#5161)
    Add tracing for buffer events:
     - buffer creation;
     - allocation of BE-specific handler for the buffer;
     - release of BE-specific handler for the buffer;
     - buffer destruction.

--- sycl/source/detail/buffer_impl.cpp
+++ sycl/source/detail/buffer_impl.cpp
@@ -9,24 +9,36 @@
 #include <CL/sycl/detail/buffer_impl.hpp>
 #include <CL/sycl/detail/memory_manager.hpp>
 #include <detail/context_impl.hpp>
+#include <detail/global_handler.hpp>
 #include <detail/scheduler/scheduler.hpp>
+#include <detail/xpti_registry.hpp>
 
 __SYCL_INLINE_NAMESPACE(cl) {
 namespace sycl {
 namespace detail {
+#ifdef XPTI_ENABLE_INSTRUMENTATION
+uint8_t GBufferStreamID;
+#endif
 void *buffer_impl::allocateMem(ContextImplPtr Context, bool InitFromUserData,
-                  void *HostPtr, RT::PiEvent &OutEventToWait) {
+                               void *HostPtr, RT::PiEvent &OutEventToWait) {
   bool HostPtrReadOnly = false;
   BaseT::determineHostPtr(Context, InitFromUserData, HostPtr, HostPtrReadOnly);
 
   assert(!(nullptr == HostPtr && BaseT::useHostPtr() && Context->is_host()) &&
          "Internal error. Allocating memory on the host "
          "while having use_host_ptr property");
-
   return MemoryManager::allocateMemBuffer(
       std::move(Context), this, HostPtr, HostPtrReadOnly, BaseT::getSize(),
       BaseT::MInteropEvent, BaseT::MInteropContext, MProps, OutEventToWait);
 }
+void buffer_impl::constructorNotification(const detail::code_location &CodeLoc,
+                                          void *UserObj) {
+  XPTIRegistry::bufferConstructorNotification(UserObj, CodeLoc);
+}
+void buffer_impl::destructorNotification(void *UserObj) {
+  XPTIRegistry::bufferDestructorNotification(UserObj);
+}
+
 } // namespace detail
 } // namespace sycl
 } // __SYCL_INLINE_NAMESPACE(cl)
