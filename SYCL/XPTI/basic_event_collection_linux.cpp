a87c6f25603b2d7bed66b52d4086bdc0b36ce151
Alexander Batashev <alexbatashev@outlook.com>
2021-12-27 12:34:46 +0300

    [XPTI][SYCL] Update tests (intel/llvm-test-suite#678)

--- SYCL/XPTI/basic_event_collection_linux.cpp
+++ SYCL/XPTI/basic_event_collection_linux.cpp
@@ -1,65 +1,19 @@
-// REQUIRES: xptifw, opencl
-// RUN: %clangxx %s -DXPTI_COLLECTOR -DXPTI_CALLBACK_API_EXPORTS %xptifw_lib %shared_lib %fPIC %cxx_std_optionc++17 -o %t_collector.dll
+// REQUIRES: xptifw, opencl, cpu, linux
+// RUN: %clangxx %s -DXPTI_COLLECTOR -DXPTI_CALLBACK_API_EXPORTS %xptifw_lib -shared -fPIC -std=c++17 -o %t_collector.so
 // RUN: %clangxx -fsycl -fsycl-unnamed-lambda -fsycl-targets=%sycl_triple %s -o %t.out
-// RUN: env XPTI_TRACE_ENABLE=1 env XPTI_FRAMEWORK_DISPATCHER=%xptifw_dispatcher env XPTI_SUBSCRIBERS=%t_collector.dll env SYCL_DEVICE_FILTER=opencl %t.out | FileCheck %s 2>&1
+// RUN: env XPTI_TRACE_ENABLE=1 env XPTI_FRAMEWORK_DISPATCHER=%xptifw_dispatcher env XPTI_SUBSCRIBERS=%t_collector.so %CPU_RUN_PLACEHOLDER %t.out %CPU_CHECK_PLACEHOLDER
 
-// REQUIRES: TEMPORARY_DISABLED
-#ifdef XPTI_COLLECTOR
-
-#include "Inputs/test_collector.cpp"
-
-#else
-
-#include <sycl/sycl.hpp>
-
-int main() {
-  sycl::queue Q{sycl::default_selector{}};
-
-  auto Ptr = sycl::malloc_device<int>(1, Q);
-
-  auto Evt1 = Q.single_task([=]() { Ptr[0] = 1; });
-
-  auto Evt2 = Q.submit([&](sycl::handler &CGH) {
-    CGH.depends_on(Evt1);
-    CGH.single_task([=]() { Ptr[0]++; });
-  });
-
-  Evt2.wait();
-
-  int Res = 0;
-  Q.memcpy(&Res, Ptr, 1);
-  Q.wait();
-
-  assert(Res == 2);
-
-  return 0;
-}
-
-#endif
-
+#include "basic_event_collection.inc"
+//
 // CHECK: xptiTraceInit: Stream Name = sycl.experimental.mem_alloc
 // CHECK: xptiTraceInit: Stream Name = sycl
 // CHECK-NEXT: Graph create
 // CHECK-NEXT: xptiTraceInit: Stream Name = sycl.pi
 // CHECK-NEXT: xptiTraceInit: Stream Name = sycl.pi.debug
-// CHECK-NEXT: PI Call Begin : piPlatformsGet
-// CHECK-NEXT: PI Call Begin : piPlatformsGet
-// CHECK-NEXT: PI Call Begin : piDevicesGet
-// CHECK-NEXT: PI Call Begin : piDevicesGet
-// CHECK-NEXT: PI Call Begin : piDeviceGetInfo
-// CHECK-NEXT: PI Call Begin : piDeviceGetInfo
-// CHECK-NEXT: PI Call Begin : piDeviceGetInfo
-// CHECK-NEXT: PI Call Begin : piDeviceRetain
-// CHECK-NEXT: PI Call Begin : piDeviceGetInfo
-// CHECK-NEXT: PI Call Begin : piDeviceGetInfo
-// CHECK-NEXT: PI Call Begin : piPlatformGetInfo
-// CHECK-NEXT: PI Call Begin : piPlatformGetInfo
-// CHECK-NEXT: PI Call Begin : piDeviceRelease
+// CHECK:      PI Call Begin : piPlatformsGet
 // CHECK:      PI Call Begin : piContextCreate
 // CHECK-NEXT: PI Call Begin : piQueueCreate
-// CHECK-NEXT: PI Call Begin : piextUSMDeviceAlloc
-// CHECK-NEXT: PI Call Begin : piextDeviceSelectBinary
-// CHECK-NEXT: PI Call Begin : piDeviceGetInfo
+// CHECK:      PI Call Begin : piextDeviceSelectBinary
 // CHECK:      PI Call Begin : piKernelCreate
 // CHECK-NEXT: PI Call Begin : piKernelSetExecInfo
 // CHECK-NEXT: PI Call Begin : piextKernelSetArgPointer
@@ -112,12 +66,3 @@ int main() {
 // CHECK-NEXT: PI Call Begin : piQueueFinish
 // CHECK-NEXT: Wait end
 // CHECK-NEXT:   sycl_device : {{.*}}
-// CHECK-NEXT: PI Call Begin : piEventRelease
-// CHECK-NEXT: PI Call Begin : piEventRelease
-// CHECK-NEXT: PI Call Begin : piQueueRelease
-// CHECK-NEXT: PI Call Begin : piContextRelease
-// CHECK-NEXT: PI Call Begin : piKernelRelease
-// CHECK-NEXT: PI Call Begin : piKernelRelease
-// CHECK-NEXT: PI Call Begin : piProgramRelease
-// CHECK-NEXT: PI Call Begin : piDeviceRelease
-// CHECK-NEXT: PI Call Begin : piTearDown
---
+++ SYCL/XPTI/mem_alloc_events_linux.cpp
@@ -0,0 +1,20 @@
+// REQUIRES: xptifw, gpu, linux
+// RUN: %clangxx %s -DXPTI_COLLECTOR -DXPTI_CALLBACK_API_EXPORTS %xptifw_lib -shared -fPIC -std=c++17 -o %t_collector.so
+// RUN: %clangxx -fsycl -fsycl-unnamed-lambda -fsycl-targets=%sycl_triple %s -o %t.out
+// RUN: env XPTI_TRACE_ENABLE=1 env XPTI_FRAMEWORK_DISPATCHER=%xptifw_dispatcher env XPTI_SUBSCRIBERS=%t_collector.so %GPU_RUN_PLACEHOLDER %t.out %GPU_CHECK_PLACEHOLDER
+
+#include "mem_alloc_events.inc"
+
+// CHECK:      xptiTraceInit: Stream Name = sycl.experimental.mem_alloc
+// CHECK:      Mem Alloc Begin :   mem_obj_handle: {{.*}}
+// CHECK-NEXT:  alloc_pointer : 0
+// CHECK-NEXT:  alloc_size    : 400
+// CHECK:      Mem Alloc End :   mem_obj_handle: {{.*}}
+// CHECK-NEXT:  alloc_pointer : {{.*}}
+// CHECK-NEXT:  alloc_size    : 400
+// CHECK:      Mem Release Begin :   mem_obj_handle: {{.*}}
+// CHECK-NEXT:  alloc_pointer : {{.*}}
+// CHECK-NEXT:  alloc_size    : 0
+// CHECK:      Mem Release End :   mem_obj_handle: {{.*}}
+// CHECK-NEXT:  alloc_pointer : {{.*}}
+// CHECK-NEXT:  alloc_size    : 0
