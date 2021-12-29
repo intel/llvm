4d4558a4cccdd5672c79523a2b347a1796109f97
vladimirlaz <vladimir.lazarev@intel.com>
2021-12-29 07:34:44 +0300

    [SYCL][XPTI] Fix and enable back the test (intel/llvm-test-suite#691)

--- SYCL/XPTI/buffer/sub_buffer.cpp
+++ SYCL/XPTI/buffer/sub_buffer.cpp
@@ -1,4 +1,4 @@
-// REQUIRES: xptifw, opencl, TEMPORARY_DISABLED
+// REQUIRES: xptifw, opencl
 // RUN: %clangxx %s -DXPTI_COLLECTOR -DXPTI_CALLBACK_API_EXPORTS %xptifw_lib %shared_lib %fPIC %cxx_std_optionc++17 -o %t_collector.dll
 // RUN: %clangxx -fsycl %s -o %t.out
 // RUN: env XPTI_TRACE_ENABLE=1 XPTI_FRAMEWORK_DISPATCHER=%xptifw_dispatcher XPTI_SUBSCRIBERS=%t_collector.dll SYCL_DEVICE_FILTER=opencl %t.out | FileCheck %s 2>&1
@@ -15,13 +15,11 @@ int main() {
   bool MismatchFound = false;
   sycl::queue Queue{};
 
-  int Array[4] = {0};
   {
     sycl::range<1> NumOfWorkItems{4};
-    // CHECK:{{[0-9]+}}|Create buffer|[[#USERID1:]]|{{.*}}sub_buffer.cpp:22:26|{{.*}}sub_buffer.cpp:22:26
-    sycl::buffer<int, 1> Buffer1(Array, NumOfWorkItems,
-                                 {sycl::property::buffer::use_host_ptr()});
-    // CHECK:{{[0-9]+}}|Create buffer|[[#USERID1:]]|{{.*}}sub_buffer.cpp:25:26|{{.*}}sub_buffer.cpp:25:26
+    // CHECK:{{[0-9]+}}|Create buffer|[[#USERID1:]]|{{.*}}sub_buffer.cpp:21:26|{{.*}}sub_buffer.cpp:21:26
+    sycl::buffer<int, 1> Buffer1(NumOfWorkItems);
+    // CHECK:{{[0-9]+}}|Create buffer|[[#USERID1:]]|{{.*}}sub_buffer.cpp:23:26|{{.*}}sub_buffer.cpp:23:26
     sycl::buffer<int, 1> SubBuffer{Buffer1, sycl::range<1>{1},
                                    sycl::range<1>{2}};
 
@@ -36,15 +34,16 @@ int main() {
             Accessor1[WIid] = static_cast<int>(WIid.get(0));
           });
     });
-  }
 
-  // Check the results.
-  for (size_t I = 1; I < 3; ++I) {
-    if (Array[I] != I - 1) {
-      std::cout << "The result is incorrect for element: " << I
-                << " , expected: " << I - 1 << " , got: " << Array[I]
-                << std::endl;
-      MismatchFound = true;
+    auto Accessor1 = Buffer1.get_access<sycl::access::mode::read>();
+    // Check the results.
+    for (size_t I = 1; I < 3; ++I) {
+      if (Accessor1[I] != I - 1) {
+        std::cout << "The result is incorrect for element: " << I
+                  << " , expected: " << I - 1 << " , got: " << Accessor1[I]
+                  << std::endl;
+        MismatchFound = true;
+      }
     }
   }
 
