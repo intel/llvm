26b058f0ff4fc075eaa7af0de6d5325343492c2e
Yuriy Kochetkov <yuriyx.kochetkov@intel.com>
2022-01-12 21:55:35 +0300

    [SYCL][ESIMD] Add test proxy for move constructors (#5132)
    Because copy constructor works as the default fallback for every case with move
    constructor disabled or not provided, there is no way for a caller to
    differentiate between actual call to move constructor and copy constructor.
    Same goes for copy and move assignment operators.
    
    Therefore a test proxy is the only way left to check the move constructor and
    the move assignment works as expected.
    
    The drawback of this patch is a requirement to have `__esimd_move_test_proxy`
    explicitly stated for every user-defined move constructor and move operator.
    
    We might use default value for any constructor, the flag will be triggered as
    needed by any defaulted/implicit move constructor/operator, but user-defined
    move constructors and operators still need some explicit trigger provided by
    developer.
    
    Signed-off-by: Yuriy Kochetkov <yuriyx.kochetkov@intel.com>

--- sycl/include/sycl/ext/intel/experimental/esimd/detail/simd_obj_impl.hpp
+++ sycl/include/sycl/ext/intel/experimental/esimd/detail/simd_obj_impl.hpp
@@ -14,6 +14,7 @@
 #include <sycl/ext/intel/experimental/esimd/detail/intrin.hpp>
 #include <sycl/ext/intel/experimental/esimd/detail/memory_intrin.hpp>
 #include <sycl/ext/intel/experimental/esimd/detail/sycl_util.hpp>
+#include <sycl/ext/intel/experimental/esimd/detail/test_proxy.hpp>
 #include <sycl/ext/intel/experimental/esimd/detail/type_format.hpp>
 #include <sycl/ext/intel/experimental/esimd/simd_view.hpp>
 
@@ -729,11 +730,17 @@ public:
   __ESIMD_DEF_SIMD_OBJ_IMPL_OPASSIGN(/, /=, __ESIMD_ARITH_OP_FILTER)
 #undef __ESIMD_ARITH_OP_FILTER
 
+  // Getter for the test proxy member, if enabled
+  __ESIMD_DECLARE_TEST_PROXY_ACCESS
+
 private:
   // The underlying data for this vector.
   raw_vector_type M_data;
 
 protected:
+  // The test proxy if enabled
+  __ESIMD_DECLARE_TEST_PROXY
+
   void set(const raw_vector_type &Val) {
 #ifndef __SYCL_DEVICE_ONLY__
     M_data = Val;
