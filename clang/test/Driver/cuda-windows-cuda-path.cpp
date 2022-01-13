7e8600254b7c7b16a9ca33457f1452e8be29bd5e
Jakub Chlanda <j.chlanda@gmail.com>
2022-01-13 13:26:46 +0000

    Test for cuda path.

---
+++ clang/test/Driver/cuda-windows-cuda-path.cpp
@@ -0,0 +1,13 @@
+///
+/// Make sure that CUDA_PATH is picked up correctly when looking for CUDA
+/// instalation.
+///
+
+// REQUIRES: system-windows
+// RUN: env CUDA_PATH=%S\Inputs\CUDA_111\usr\local\cuda %clang -fsycl \
+// RUN: -fsycl-targets=nvptx64-nvidia-cuda -### -v %s 2>&1 | \
+// RUN: FileCheck %s
+
+// CHECK: Found CUDA installation: {{.*}}Inputs\CUDA_111\usr\local\cuda, version 11.1
+
+int main() {}
