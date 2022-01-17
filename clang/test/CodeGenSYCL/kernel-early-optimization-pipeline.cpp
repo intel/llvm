d4059ae2d943e68a84c976cd10e1e95210627e5f
Mikhail Lychkov <mikhail.lychkov@intel.com>
2022-01-17 14:26:46 +0300

    Fix passes order
    Signed-off-by: Mikhail Lychkov <mikhail.lychkov@intel.com>

--- clang/test/CodeGenSYCL/kernel-early-optimization-pipeline.cpp
+++ clang/test/CodeGenSYCL/kernel-early-optimization-pipeline.cpp
@@ -13,9 +13,9 @@
 // New pass manager doesn't print all passes tree, only module level.
 //
 // RUN: %clang_cc1 -O2 -fsycl-is-device -triple spir64-unknown-unknown %s -fno-legacy-pass-manager -mdebug-pass Structure -emit-llvm -o /dev/null 2>&1 | FileCheck %s --check-prefix=CHECK-NEWPM-EARLYOPT
-// CHECK-NEWPM-EARLYOPT: SYCLMutatePrintfAddrspacePass
 // CHECK-NEWPM-EARLYOPT: ConstantMergePass
+// CHECK-NEWPM-EARLYOPT: SYCLMutatePrintfAddrspacePass
 //
 // RUN: %clang_cc1 -O2 -fsycl-is-device -triple spir64-unknown-unknown %s -fno-legacy-pass-manager -mdebug-pass Structure -emit-llvm -fno-sycl-early-optimizations -o /dev/null 2>&1 | FileCheck %s --check-prefix=CHECK-NEWPM-NOEARLYOPT
-// CHECK-NEWPM-NOEARLYOPT: SYCLMutatePrintfAddrspacePass
 // CHECK-NEWPM-NOEARLYOPT-NOT: ConstantMergePass
+// CHECK-NEWPM-NOEARLYOPT: SYCLMutatePrintfAddrspacePass
