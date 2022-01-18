716407aafbeba29b8cba51f6a86f8c97eecfebbf
IgorKharchikov <95442829+IgorKharchikov@users.noreply.github.com>
2022-01-18 16:16:20 +0300

    Update ctz_namespace.cpp
    Rewritten call check.

--- sycl/test/regression/ctz_namespace.cpp
+++ sycl/test/regression/ctz_namespace.cpp
@@ -5,7 +5,5 @@ int main() {
   long long int a = -9223372034707292160ll;
   auto b = sycl::ctz(a);
   return 0;
-  // CHECK: _Z3ctzc
-  // CHECK: call spir_func i32 addrspace(1)* {{.*}}spirv_ocl_ctz{{.*}}(i32
-  // addrspace(4)*
+  // CHECK: call spir_func i32 {{.*}}__sycl_std::__invoke_ctz{{.*}} i32 addrspace(1)*
 }
