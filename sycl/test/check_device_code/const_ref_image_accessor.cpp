7465d36cebec60a0c8acf21c15ba31add8f247b6
IgorKharchikov <95442829+IgorKharchikov@users.noreply.github.com>
2022-01-18 16:43:26 +0300

    Update const_ref_image_accessor.cpp
    Reduced test file so it contains only func() function, edited commented line for test running.

--- sycl/test/check_device_code/const_ref_image_accessor.cpp
+++ sycl/test/check_device_code/const_ref_image_accessor.cpp
@@ -1,28 +1,13 @@
-// RUN: %clangxx -S -fsycl -fsycl-device-only %s
+// RUN: %clangxx -fsycl -fsycl-device-only %s -o /dev/null
 
 #include <CL/sycl.hpp>
 
-int main() {
-  const sycl::range<2> range{16, 16};
-  const std::size_t size{range.size()};
-  const sycl::float4 default_value{1.0f, 2.0f, 3.0f, 4.0f};
-  const std::vector<sycl::float4> src_data(size, default_value);
-  sycl::image<2> src_image{src_data.data(), sycl::image_channel_order::rgba,
-                           sycl::image_channel_type::fp32, range};
-  sycl::image<2> dst_image{sycl::image_channel_order::rgba,
-                           sycl::image_channel_type::fp32, range};
-  sycl::queue queue;
-
-  queue.submit([&](sycl::handler &handler) {
-    const auto src_accessor =
-        src_image.get_access<sycl::float4, sycl::access::mode::read>(handler);
-    const auto dst_accessor =
-        dst_image.get_access<sycl::float4, sycl::access::mode::write>(handler);
-    handler.parallel_for(range, [=](const sycl::item<2> item) {
-      const sycl::int2 coords{item[1], item[0]};
-      auto pixel = src_accessor.read(coords);
-      dst_accessor.write(coords, pixel);
-    });
+SYCL_EXTERNAL void
+func(sycl::handler &handler, const sycl::range<2> &range,
+     const sycl::accessor<sycl::float4, 2, sycl::access::mode::read,
+                          sycl::access::target::image> &dummy_accessor,
+  handler.parallel_for(range, [=](const sycl::item<2> item) {
+  const sycl::int2 coords{item[1], item[0]};
+  auto pixel = dummy_accessor.read(coords);
   });
-  return 0;
 }
