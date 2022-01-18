9b4be53475bd234a62a9dcaf0517bbd1340250ab
IgorKharchikov <95442829+IgorKharchikov@users.noreply.github.com>
2022-01-18 17:10:29 +0300

    Update const_ref_image_accessor.cpp
    Edited commented line for testing, corrected typos in code.

--- sycl/test/check_device_code/const_ref_image_accessor.cpp
+++ sycl/test/check_device_code/const_ref_image_accessor.cpp
@@ -1,13 +1,15 @@
-// RUN: %clangxx -fsycl -fsycl-device-only %s -o /dev/null
+// RUN: %clangxx -fsycl -fsycl-device-only -fsyntax-only -Xclang -verify -Xclang -verify-ignore-unexpected=note,warning %s
 
-#include <CL/sycl.hpp>
+// expected-no-diagnostics
+
+#include <sycl/sycl.hpp>
 
 SYCL_EXTERNAL void
 func(sycl::handler &handler, const sycl::range<2> &range,
      const sycl::accessor<sycl::float4, 2, sycl::access::mode::read,
-                          sycl::access::target::image> &dummy_accessor,
+                          sycl::access::target::image> &dummy_accessor) {
   handler.parallel_for(range, [=](const sycl::item<2> item) {
-  const sycl::int2 coords{item[1], item[0]};
-  auto pixel = dummy_accessor.read(coords);
+    const sycl::int2 coords{item[1], item[0]};
+    auto pixel = dummy_accessor.read(coords);
   });
 }
