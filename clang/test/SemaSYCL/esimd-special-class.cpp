9603dffe4e360143ef8557d06ae76c0b3caad71b
Zahira Ammarguellat <zahira.ammarguellat@intel.com>
2022-01-20 05:20:11 -0800

    Responded to reviews
    Signed-off-by: Zahira Ammarguellat <zahira.ammarguellat@intel.com>

--- clang/test/SemaSYCL/esimd-special-class.cpp
+++ clang/test/SemaSYCL/esimd-special-class.cpp
@@ -1,6 +1,4 @@
-// RUN: %clang_cc1 -fsycl-is-device -verify \
-// RUN: -internal-isystem %S/Inputs -triple spir64-unknown-unknown \
-// RUN: -fsyntax-only -disable-llvm-passes -emit-llvm %s -o - %s
+// RUN: %clang_cc1 -fsycl-is-device -verify -internal-isystem %S/Inputs -fsyntax-only %s
 
 // The test is to ensure that the use of sycl_explicit_simd attribute doesn't
 // crash when used with sampler or stream. Currently samplers/stream are not
@@ -17,12 +15,12 @@ void test() {
     cl::sycl::stream Stream(1024, 128, h);
     // expected-note@+1{{in instantiation of function template specialization}}
     h.single_task<class SamplerTester>(
-        // expected-error@+1{{type 'sampler' is not supported in 'esimd' context}}
+        // expected-error@+1{{type 'sampler' is not supported in ESIMD context}}
         [=]() [[intel::sycl_explicit_simd]] { Smplr.use(); });
 
     // expected-note@+1{{in instantiation of function template specialization}}
     h.single_task<class StreamTester>(
-        // expected-error@+1{{type 'stream' is not supported in 'esimd' context}}
+        // expected-error@+1{{type 'stream' is not supported in ESIMD context}}
         [=]() [[intel::sycl_explicit_simd]] { Stream.use(); });
   });
 }
