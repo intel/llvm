a3df097f3d75c7abd3a3eb30802c0a41edd122f7
Greg Lueck <gregory.m.lueck@intel.com>
2022-01-24 14:43:15 -0500

    Fix spelling typo
    Co-authored-by: smaslov-intel <sergey.v.maslov@intel.com>

--- sycl/plugins/level_zero/pi_level_zero.cpp
+++ sycl/plugins/level_zero/pi_level_zero.cpp
@@ -3900,7 +3900,7 @@ pi_result piProgramLink(pi_context Context, pi_uint32 NumDevices,
     ZeModuleDesc.pNext = &ZeExtModuleDesc;
     ZeModuleDesc.format = ZE_MODULE_FORMAT_IL_SPIRV;
 
-    // This works around a bug in the Level Zero driver.  When "ZEBUG=-1", the
+    // This works around a bug in the Level Zero driver.  When "ZE_DEBUG=-1", the
     // driver does validation of the API calls, and it expects "pInputModule"
     // to be non-NULL and "inputSize" to be non-zero.  This validation is wrong
     // when using the "ze_module_program_exp_desc_t" extension because those
