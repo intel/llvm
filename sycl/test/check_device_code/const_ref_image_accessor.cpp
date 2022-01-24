333ea45f174622ff02d9fcfcb8f784ffe2643399
IgorKharchikov <95442829+IgorKharchikov@users.noreply.github.com>
2022-01-24 14:36:22 +0300

    Update const_ref_image_accessor.cpp
    Removed SYCL_EXTERNAL attribute due to it's unnecessity in this test (parallel_for is used here).

--- sycl/test/check_device_code/const_ref_image_accessor.cpp
+++ sycl/test/check_device_code/const_ref_image_accessor.cpp
@@ -4,10 +4,9 @@
 
 #include <sycl/sycl.hpp>
 
-SYCL_EXTERNAL void
-func(sycl::handler &handler, const sycl::range<2> &range,
-     const sycl::accessor<sycl::float4, 2, sycl::access::mode::read,
-                          sycl::access::target::image> &dummy_accessor) {
+void func(sycl::handler &handler, const sycl::range<2> &range,
+          const sycl::accessor<sycl::float4, 2, sycl::access::mode::read,
+                               sycl::access::target::image> &dummy_accessor) {
   handler.parallel_for(range, [=](const sycl::item<2> item) {
     const sycl::int2 coords{item[1], item[0]};
     auto pixel = dummy_accessor.read(coords);
