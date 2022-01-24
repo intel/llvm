ef212f5a2c51d0091d03dfda53ce35c56c0bf84b
Artem Gindinson <artem.gindinson@intel.com>
2022-01-24 11:45:53 +0100

    [SYCL] Remove redundant branching in Printf/mixed-address-space config (intel/llvm-test-suite#761)
    Since the entire point of the test is to account for different address spaces' coexistence,
    branching the compilation/execution based on the presence of constant AS is irrelevant.

--- SYCL/Printf/mixed-address-space.cpp
+++ SYCL/Printf/mixed-address-space.cpp
@@ -9,12 +9,6 @@
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
 // RUN: %CPU_RUN_PLACEHOLDER %t.out %CPU_CHECK_PLACEHOLDER
 // RUN: %ACC_RUN_PLACEHOLDER %t.out %ACC_CHECK_PLACEHOLDER
-// FIXME: Remove dedicated constant address space testing once generic AS
-//        support is considered stable.
-// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.constant.out \
-// RUN: -DTEST_CONSTANT_AS
-// RUN: %CPU_RUN_PLACEHOLDER %t.constant.out %CPU_CHECK_PLACEHOLDER
-// RUN: %ACC_RUN_PLACEHOLDER %t.constant.out %ACC_CHECK_PLACEHOLDER
 // CHECK: Constant addrspace literal
 // CHECK: Generic addrspace literal
 
@@ -28,9 +22,7 @@ using namespace sycl;
 
 void test() {
   {
-#define TEST_CONSTANT_AS
     FORMAT_STRING(constant_literal) = "Constant addrspace literal\n";
-#undef TEST_CONSTANT_AS
     ext::oneapi::experimental::printf(constant_literal);
     ext::oneapi::experimental::printf("Generic addrspace literal\n");
   }
