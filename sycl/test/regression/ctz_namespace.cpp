6de12d2dc0c97648eb46568562d131e025e6d3b8
IgorKharchikov <95442829+IgorKharchikov@users.noreply.github.com>
2022-01-25 14:38:09 +0300

    Update ctz_namespace.cpp

--- sycl/test/regression/ctz_namespace.cpp
+++ sycl/test/regression/ctz_namespace.cpp
@@ -8,9 +8,8 @@ int main() {
 
   long long int a = -9223372034707292160ll;
   long long int b = sycl::ctz(a);
+  // expected-warning@+1 {{'ctz<long long>' is deprecated: 'sycl::ext::intel::ctz' is deprecated, use 'sycl::ctz' instead}}
   long long int c = sycl::ext::intel::ctz(a);
 
   return 0;
 }
-
-// expected-warning@+1 {{'ctz<long long>' is deprecated: 'sycl::ext::intel::ctz' is deprecated, use 'sycl::ctz' instead}}
