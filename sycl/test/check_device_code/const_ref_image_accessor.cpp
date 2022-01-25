e65067677d929f7bc6441e65f4526d0e6a26d3ec
IgorKharchikov <95442829+IgorKharchikov@users.noreply.github.com>
2022-01-25 17:13:44 +0300

    Update const_ref_image_accessor.cpp

--- sycl/test/check_device_code/const_ref_image_accessor.cpp
+++ sycl/test/check_device_code/const_ref_image_accessor.cpp
@@ -4,11 +4,9 @@
 
 #include <sycl/sycl.hpp>
 
-void func(sycl::handler &handler, const sycl::range<2> &range,
-          const sycl::accessor<sycl::float4, 2, sycl::access::mode::read,
-                               sycl::access::target::image> &dummy_accessor) {
-  handler.parallel_for(range, [=](const sycl::item<2> item) {
-    const sycl::int2 coords{item[1], item[0]};
-    auto pixel = dummy_accessor.read(coords);
-  });
+SYCL_EXTERNAL void
+func(const sycl::accessor<sycl::float4, 2, sycl::access::mode::read,
+                          sycl::access::target::image> &dummy_accessor) {
+  const sycl::int2 coords{0, 0};
+  auto pixel = dummy_accessor.read(coords);
 }
