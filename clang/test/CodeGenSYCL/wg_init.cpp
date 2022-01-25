44fa7c538de71387660c47fe4d4eb1f376cf019a
Hugh Delaney <hugh.delaney@codeplay.com>
2022-01-25 09:47:11 -0500

    Changing test to use Inputs/sycl.hpp functions

--- clang/test/CodeGenSYCL/wg_init.cpp
+++ clang/test/CodeGenSYCL/wg_init.cpp
@@ -1,32 +1,22 @@
 // RUN: %clang_cc1 -fsycl-is-device -triple spir64-unknown-unknown -disable-llvm-passes -emit-llvm %s -o - | FileCheck %s
 
-//==-----------------------------wg_init.cpp--------------------------------==//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-
 // This test checks that a local variable initialized within a
 // parallel_for_work_group scope is initialized as an UndefValue in addrspace(3)
 // in LLVM IR.
 
 #include "Inputs/sycl.hpp"
 
-template <typename KernelName, typename KernelType>
-__attribute__((sycl_kernel)) void
-kernel_parallel_for_work_group(const KernelType &KernelFunc) {
-  cl::sycl::group<1> G;
-  KernelFunc(G);
-}
+using namespace sycl;
 
 int main() {
-
-  kernel_parallel_for_work_group<class kernel>([=](cl::sycl::group<1> G) {
-    int WG_VAR = 10;
+  queue q;
+  q.submit([&](handler &h) {
+    h.parallel_for_work_group<class kernel>(
+        range<1>{1}, range<1>{1}, [=](group<1> G) {
+          int WG_VAR = 10;
+        });
   });
-  // CHECK: @{{.*}}WG_VAR = internal addrspace(3) global {{.*}} undef, {{.*}}
-
   return 0;
 }
+
+// CHECK: @{{.*}}WG_VAR = internal addrspace(3) global {{.*}} undef, {{.*}}
