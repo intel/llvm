5e7d1dae87eabb83fda94f6dabde0b9fed3e0073
Hugh Delaney <hugh.delaney@codeplay.com>
2022-01-25 04:54:12 -0500

    Adding header and description

--- clang/test/CodeGenSYCL/wg_init.cpp
+++ clang/test/CodeGenSYCL/wg_init.cpp
@@ -1,5 +1,17 @@
 // RUN: %clang_cc1 -fsycl-is-device -triple spir64-unknown-unknown -disable-llvm-passes -emit-llvm %s -o - | FileCheck %s
 
+//==-----------------------------wg_init.cpp--------------------------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// This test checks that a local variable initialized within a
+// parallel_for_work_group scope is initialized as an UndefValue in addrspace(3)
+// in LLVM IR.
+
 #include "Inputs/sycl.hpp"
 
 template <typename KernelName, typename KernelType>
