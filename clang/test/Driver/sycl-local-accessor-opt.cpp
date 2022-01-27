9b543f36c255fd2e6ea054633196a773df2c6961
aidan.belton <aidan.belton@codeplay.com>
2022-01-27 15:32:23 +0000

    fix test

--- clang/test/Driver/sycl-local-accessor-opt.cpp
+++ clang/test/Driver/sycl-local-accessor-opt.cpp
@@ -4,12 +4,11 @@
 
 // RUN:   %clang -fsycl -### %s 2>&1 \
 // RUN:   | FileCheck -check-prefix=CHECK-NO-OPT %s
-// CHECK-NO-OPT-NOT: "-sycl-enable-local-accessor"
+
+// RUN:   %clang -fsycl -S -### %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-NO-OPT %s
+// CHECK-NO-OPT: "-sycl-enable-local-accessor"
 
 // RUN:   %clang -fsycl -fsycl-targets=nvptx64-nvidia-cuda -### %s 2>&1 \
-// RUN:   | FileCheck %s
-// CHECK: "-sycl-enable-local-accessor"
-
-// RUN:   %clang -fsycl -fsycl-targets=nvptx64-nvidia-cuda -S -### %s 2>&1 \
 // RUN:   | FileCheck %s
 // CHECK: "-sycl-enable-local-accessor"
