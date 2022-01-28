c848a828fd5b651655393ea8b7fd9f24bbdde471
Sindhu Chittireddy <sindhu.chittireddy@intel.com>
2022-01-28 15:43:44 -0800

    Fix test and add more cases

--- clang/test/CodeGenSYCL/code_location.cpp
+++ clang/test/CodeGenSYCL/code_location.cpp
@@ -1,24 +1,102 @@
-// RUN: %clang_cc1 -fsycl-is-device -internal-isystem -sycl-std=2020 -fsycl-int-header=%t.h %s -o %t.out
+// RUN: %clang_cc1 -fsycl-is-device -internal-isystem -DNDEBUG -sycl-std=2020 -fsycl-int-header=%t.h %s -o %t.out
 // RUN: FileCheck -input-file=%t.h %s
-
+// lang_cc1 -fsycl-is-device -internal-isystem -sycl-std=2020 -fsycl-int-header=%t2.h %s -o %t2.out
+// ileCheck -input-file=%t2.h %s
 #include "Inputs/sycl.hpp"
 
-int main() {
+//#ifdef NDEBUG
+int test1() {
   cl::sycl::queue q;
   q.submit([&](cl::sycl::handler &h) { h.single_task([] {}); });
   q.submit([&](cl::sycl::handler &h) { h.single_task<class KernelName>([]() {}); });
   return 0;
 }
-
-// CHECK: template <> struct KernelInfoData<'_', 'Z', 'T', 'S', 'Z', 'Z', '4', 'm', 'a', 'i', 'n', 'E', 'N', 'K', 'U', 'l', 'R', 'N', '2', 'c', 'l', '4', 's', 'y', 'c', 'l', '7', 'h', 'a', 'n', 'd', 'l', 'e', 'r', 'E', 'E', '_', 'c', 'l', 'E', 'S', '2', '_', 'E', 'U', 'l', 'v', 'E', '_'> {
-// CHECK: static constexpr const char* getFileName() { return {{.*}}/clang/test/CodeGenSYCL/code_location.cpp; }
+// Specializations of KernelInfo for kernel function types:
+// CHECK: template <> struct KernelInfoData<'_', 'Z', 'T', 'S', 'Z', 'Z', '5', 't', 'e', 's', 't', '1', 'v', 'E', 'N', 'K', 'U', 'l', 'R', 'N', '2', 'c', 'l', '4', 's', 'y', 'c', 'l', '7', 'h', 'a', 'n', 'd', 'l', 'e', 'r', 'E', 'E', '_', 'c', 'l', 'E', 'S', '2', '_', 'E', 'U', 'l', 'v', 'E', '_'> {
+// CHECK: static constexpr const char* getName() { return "_ZTSZZ5test1vENKUlRN2cl4sycl7handlerEE_clES2_EUlvE_"; }
+// CHECK: static constexpr const char* getFileName() { return code_location.cpp; }
 // CHECK: static constexpr const char* getFunctionName() { return ; }
-// CHECK: static constexpr unsigned getLineNumber() { return 8; }
+// CHECK: static constexpr unsigned getLineNumber() { return 10; }
 // CHECK: static constexpr unsigned getColumnNumber() { return 54; }
 //};
+
 // CHECK: template <> struct KernelInfo<KernelName> {
-// CHECK: static constexpr const char* getFileName() { return {{.*}}/clang/test/CodeGenSYCL/code_location.cpp; }
+// CHECK: static constexpr const char* getFileName() { return code_location.cpp; }
 // CHECK: static constexpr const char* getFunctionName() { return KernelName; }
-// CHECK: static constexpr unsigned getLineNumber() { return 9; }
+// CHECK: static constexpr unsigned getLineNumber() { return 11; }
 // CHECK: static constexpr unsigned getColumnNumber() { return 72; }
 //};
+class KernelName2;
+int test2() {
+  cl::sycl::queue q;
+  q.submit([&](cl::sycl::handler &h) { h.single_task(
+                                           [] { int i = 2; }); });
+  q.submit([&](cl::sycl::handler &h) { h.single_task<KernelName2>(
+                                           [] { int i = 2; }); });
+  return 0;
+}
+
+// CHECK: template <> struct KernelInfoData<'_', 'Z', 'T', 'S', 'Z', 'Z', '5', 't', 'e', 's', 't', '2', 'v', 'E', 'N', 'K', 'U', 'l', 'R', 'N', '2', 'c', 'l', '4', 's', 'y', 'c', 'l', '7', 'h', 'a', 'n', 'd', 'l', 'e', 'r', 'E', 'E', '_', 'c', 'l', 'E', 'S', '2', '_', 'E', 'U', 'l', 'v', 'E', '_'> {
+// CHECK: static constexpr const char* getFileName() { return code_location.cpp; }
+// CHECK: static constexpr const char* getFunctionName() { return ; }
+// CHECK: static constexpr unsigned getLineNumber() { return 33; }
+// CHECK: static constexpr unsigned getColumnNumber() { return 44; }
+//};
+// CHECK: template <> struct KernelInfo<::KernelName2> {
+// CHECK: static constexpr const char* getFileName() { return code_location.cpp; }
+// CHECK: static constexpr const char* getFunctionName() { return KernelName2; }
+// CHECK: static constexpr unsigned getLineNumber() { return 35; }
+// CHECK: static constexpr unsigned getColumnNumber() { return 44; }
+//};
+
+template <typename T> class KernelName3;
+int test3() {
+  cl::sycl::queue q;
+  q.submit([&](cl::sycl::handler &h) { h.single_task<KernelName3<KernelName2>>(
+                                           [] { int i = 3; }); });
+  return 0;
+}
+
+// CHECK: template <> struct KernelInfo<::KernelName3<::KernelName2>> {
+// CHECK: static constexpr const char* getFileName() { return code_location.cpp; }
+// CHECK: static constexpr const char* getFunctionName() { return KernelName3; }
+// CHECK: static constexpr unsigned getLineNumber() { return 56; }
+// CHECK: static constexpr unsigned getColumnNumber() { return 44; }
+//};
+
+auto l4 = []() { return 4; };
+int test4() {
+  cl::sycl::queue q;
+  q.submit([=](cl::sycl::handler &h) { h.single_task<class KernelName4>(l4); });
+  return 0;
+}
+
+// CHECK: template <> struct KernelInfo<KernelName4> {
+// CHECK: static constexpr const char* getFileName() { return code_location.cpp; }
+// CHECK: static constexpr const char* getFunctionName() { return KernelName4; }
+// CHECK: static constexpr unsigned getLineNumber() { return 67; }
+// CHECK: static constexpr unsigned getColumnNumber() { return 11; }
+//};
+
+//#else
+/*
+int test2() {
+  cl::sycl::queue q;
+  q.submit([&](cl::sycl::handler &h) { h.single_task([] {}); });
+  q.submit([&](cl::sycl::handler &h) { h.single_task<class KernelName>([]() {}); });
+  return 0;
+}
+*/
+// K: template <> struct KernelInfoData<'_', 'Z', 'T', 'S', 'Z', 'Z', '4', 'm', 'a', 'i', 'n', 'E', 'N', 'K', 'U', 'l', 'R', 'N', '2', 'c', 'l', '4', 's', 'y', 'c', 'l', '7', 'h', 'a', 'n', 'd', 'l', 'e', 'r', 'E', 'E', '_', 'c', 'l', 'E', 'S', '2', '_', 'E', 'U', 'l', 'v', 'E', '_'> {
+// K: static constexpr const char* getFileName() { return ; }
+// CK: static constexpr const char* getFunctionName() { return ; }
+// HECK: static constexpr unsigned getLineNumber() { return 0; }
+// HECK: static constexpr unsigned getColumnNumber() { return 0; }
+//};
+// HECK: template <> struct KernelInfo<KernelName> {
+// HECK: static constexpr const char* getFileName() { return ; }
+// HECK: static constexpr const char* getFunctionName() { return ; }
+// HECK: static constexpr unsigned getLineNumber() { return 0; }
+// HECK: static constexpr unsigned getColumnNumber() { return 0; }
+//};
+//#endif
