c6a25c080c3aacd78f32490470132485df1dde5e
Michael D Toguchi <michael.d.toguchi@intel.com>
2022-01-31 10:48:55 -0800

    Update tests, as they are not expecting itt device objects

--- clang/test/Driver/sycl-intelfpga-static-lib.cpp
+++ clang/test/Driver/sycl-intelfpga-static-lib.cpp
@@ -10,7 +10,7 @@
 // RUN: llvm-ar cr %t.a %t1_bundle.o
 
 /// Check phases with static lib
-// RUN: %clangxx -target x86_64-unknown-linux-gnu -fsycl -fno-sycl-dead-args-optimization -fno-sycl-device-lib=all -fintelfpga %t.a -ccc-print-phases 2>&1 \
+// RUN: %clangxx -target x86_64-unknown-linux-gnu -fsycl -fno-sycl-dead-args-optimization -fno-sycl-instrument-device-code -fno-sycl-device-lib=all -fintelfpga %t.a -ccc-print-phases 2>&1 \
 // RUN:  | FileCheck -check-prefix=CHECK_PHASES %s
 // CHECK_PHASES: 0: input, "[[INPUT:.+\.a]]", object, (host-sycl)
 // CHECK_PHASES: 1: linker, {0}, image, (host-sycl)
