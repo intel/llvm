intel/llvm

commit fc0d28a10b91441be163aa6523fac71824a127b3

[SYCL][XPTI] Add missing buffer constructor data (#5259)
Extend information about created buffer:

 - Host object used to create buffer;
 - A string representing the type of buffer element;
 - Buffer element size in bytes
 - Buffer dimensions number.
 - Buffer size for each dimension.
 
The test changes available in intel/llvm-test-suite#788

xpti/include/xpti/xpti_data_types.h
@@ -168,7 +168,7 @@ struct payload_t {
   //  valid since we can potentially reconstruct the name and the source file
   //  information during post-processing step of symbol resolution; this
   //  indicates a partial but valid payload.
-  payload_t(void *codeptr) {
+  payload_t(const void *codeptr) {
     code_ptr_va = codeptr;
     name = nullptr;         ///< Invalid name string pointer
     source_file = nullptr;  ///< Invalid source file string pointer
@@ -193,7 +193,7 @@ struct payload_t {
     }
   }
 
-  payload_t(const char *func_name, void *codeptr) {
+  payload_t(const char *func_name, const void *codeptr) {
     code_ptr_va = codeptr;
     name = func_name;      ///< Invalid name string pointer
     source_file = nullptr; ///< Invalid source file string pointer
@@ -210,7 +210,7 @@ struct payload_t {
   //  on dynamic backtrace as a possibility. In this case, we send in the
   //  caller/callee information as a string in the form "caller->callee" that
   //  will be used to generate the unique ID.
-  payload_t(const char *kname, const char *caller_callee, void *codeptr) {
+  payload_t(const char *kname, const char *caller_callee, const void *codeptr) {
     if (codeptr) {
       code_ptr_va = codeptr;
       flags |= (uint64_t)payload_flag_t::CodePointerAvailable;
@@ -231,7 +231,7 @@ struct payload_t {
   //  also have the function name and source file name along with the line and
   //  column number of the trace point that forms the payload.
   payload_t(const char *kname, const char *sf, int line, int col,
-            void *codeptr) {
+            const void *codeptr) {
     code_ptr_va = codeptr;
     /// Capture the rest of the parameters
     name = kname;
@@ -520,6 +520,16 @@ struct trace_event_data_t {
 struct offload_buffer_data_t {
   /// A pointer to user level memory offload object.
   uintptr_t user_object_handle = 0;
+  /// A pointer to host memory offload object.
+  uintptr_t host_object_handle = 0;
+  /// A string representing the type of buffer element.
+  const char *element_type;
+  /// Buffer element size in bytes
+  uint32_t element_size;
+  /// Buffer dimensions number.
+  uint32_t dim = 0;
+  /// Buffer size for each dimension.
+  size_t range[3] = {0, 0, 0};
 };
 
 /// Describes offload accessor

sycl/include/CL/sycl/buffer.hpp
@@ -72,6 +72,12 @@ public:
   using EnableIfSameNonConstIterators = typename detail::enable_if_t<
       std::is_same<ItA, ItB>::value && !std::is_const<ItA>::value, ItA>;
 
+  std::array<size_t, 3> rangeToArray(range<3> &r) { return {r[0], r[1], r[2]}; }
+
+  std::array<size_t, 3> rangeToArray(range<2> &r) { return {r[0], r[1], 0}; }
+
+  std::array<size_t, 3> rangeToArray(range<1> &r) { return {r[0], 0, 0}; }
+
   buffer(const range<dimensions> &bufferRange,
          const property_list &propList = {},
          const detail::code_location CodeLoc = detail::code_location::current())
@@ -79,7 +85,9 @@ public:
     impl = std::make_shared<detail::buffer_impl>(
         size() * sizeof(T), detail::getNextPowerOfTwo(sizeof(T)), propList,
         make_unique_ptr<detail::SYCLMemObjAllocatorHolder<AllocatorT>>());
-    impl->constructorNotification(CodeLoc, (void *)impl.get());
+    impl->constructorNotification(CodeLoc, (void *)impl.get(), nullptr,
+                                  (const void *)typeid(T).name(), dimensions,
+                                  sizeof(T), rangeToArray(Range).data());
   }
 
   buffer(const range<dimensions> &bufferRange, AllocatorT allocator,
@@ -90,7 +98,9 @@ public:
         size() * sizeof(T), detail::getNextPowerOfTwo(sizeof(T)), propList,
         make_unique_ptr<detail::SYCLMemObjAllocatorHolder<AllocatorT>>(
             allocator));
-    impl->constructorNotification(CodeLoc, (void *)impl.get());
+    impl->constructorNotification(CodeLoc, (void *)impl.get(), nullptr,
+                                  (const void *)typeid(T).name(), dimensions,
+                                  sizeof(T), rangeToArray(Range).data());
   }
 
   buffer(T *hostData, const range<dimensions> &bufferRange,
@@ -101,7 +111,9 @@ public:
         hostData, size() * sizeof(T), detail::getNextPowerOfTwo(sizeof(T)),
         propList,
         make_unique_ptr<detail::SYCLMemObjAllocatorHolder<AllocatorT>>());
-    impl->constructorNotification(CodeLoc, (void *)impl.get());
+    impl->constructorNotification(CodeLoc, (void *)impl.get(), hostData,
+                                  (const void *)typeid(T).name(), dimensions,
+                                  sizeof(T), rangeToArray(Range).data());
   }
 
   buffer(T *hostData, const range<dimensions> &bufferRange,
@@ -113,7 +125,9 @@ public:
         propList,
         make_unique_ptr<detail::SYCLMemObjAllocatorHolder<AllocatorT>>(
             allocator));
-    impl->constructorNotification(CodeLoc, (void *)impl.get());
+    impl->constructorNotification(CodeLoc, (void *)impl.get(), hostData,
+                                  (const void *)typeid(T).name(), dimensions,
+                                  sizeof(T), rangeToArray(Range).data());
   }
 
   template <typename _T = T>
@@ -126,7 +140,9 @@ public:
         hostData, size() * sizeof(T), detail::getNextPowerOfTwo(sizeof(T)),
         propList,
         make_unique_ptr<detail::SYCLMemObjAllocatorHolder<AllocatorT>>());
-    impl->constructorNotification(CodeLoc, (void *)impl.get());
+    impl->constructorNotification(CodeLoc, (void *)impl.get(), hostData,
+                                  (const void *)typeid(T).name(), dimensions,
+                                  sizeof(T), rangeToArray(Range).data());
   }
 
   template <typename _T = T>
@@ -140,7 +156,9 @@ public:
         propList,
         make_unique_ptr<detail::SYCLMemObjAllocatorHolder<AllocatorT>>(
             allocator));
-    impl->constructorNotification(CodeLoc, (void *)impl.get());
+    impl->constructorNotification(CodeLoc, (void *)impl.get(), hostData,
+                                  (const void *)typeid(T).name(), dimensions,
+                                  sizeof(T), rangeToArray(Range).data());
   }
 
   buffer(const std::shared_ptr<T> &hostData,
@@ -153,7 +171,10 @@ public:
         propList,
         make_unique_ptr<detail::SYCLMemObjAllocatorHolder<AllocatorT>>(
             allocator));
-    impl->constructorNotification(CodeLoc, (void *)impl.get());
+    impl->constructorNotification(CodeLoc, (void *)impl.get(),
+                                  (void *)hostData.get(),
+                                  (const void *)typeid(T).name(), dimensions,
+                                  sizeof(T), rangeToArray(Range).data());
   }
 
   buffer(const std::shared_ptr<T[]> &hostData,
@@ -166,7 +187,10 @@ public:
         propList,
         make_unique_ptr<detail::SYCLMemObjAllocatorHolder<AllocatorT>>(
             allocator));
-    impl->constructorNotification(CodeLoc, (void *)impl.get());
+    impl->constructorNotification(CodeLoc, (void *)impl.get(),
+                                  (void *)hostData.get(),
+                                  (const void *)typeid(T).name(), dimensions,
+                                  sizeof(T), rangeToArray(Range).data());
   }
 
   buffer(const std::shared_ptr<T> &hostData,
@@ -178,7 +202,10 @@ public:
         hostData, size() * sizeof(T), detail::getNextPowerOfTwo(sizeof(T)),
         propList,
         make_unique_ptr<detail::SYCLMemObjAllocatorHolder<AllocatorT>>());
-    impl->constructorNotification(CodeLoc, (void *)impl.get());
+    impl->constructorNotification(CodeLoc, (void *)impl.get(),
+                                  (void *)hostData.get(),
+                                  (const void *)typeid(T).name(), dimensions,
+                                  sizeof(T), rangeToArray(Range).data());
   }
 
   buffer(const std::shared_ptr<T[]> &hostData,
@@ -190,7 +217,10 @@ public:
         hostData, size() * sizeof(T), detail::getNextPowerOfTwo(sizeof(T)),
         propList,
         make_unique_ptr<detail::SYCLMemObjAllocatorHolder<AllocatorT>>());
-    impl->constructorNotification(CodeLoc, (void *)impl.get());
+    impl->constructorNotification(CodeLoc, (void *)impl.get(),
+                                  (void *)hostData.get(),
+                                  (const void *)typeid(T).name(), dimensions,
+                                  sizeof(T), rangeToArray(Range).data());
   }
 
   template <class InputIterator, int N = dimensions,
@@ -205,7 +235,9 @@ public:
         propList,
         make_unique_ptr<detail::SYCLMemObjAllocatorHolder<AllocatorT>>(
             allocator));
-    impl->constructorNotification(CodeLoc, (void *)impl.get());
+    impl->constructorNotification(CodeLoc, (void *)impl.get(), &*first,
+                                  (const void *)typeid(T).name(), dimensions,
+                                  sizeof(T), {Range[0], 0, 0});
   }
 
   template <class InputIterator, int N = dimensions,
@@ -219,7 +251,10 @@ public:
         first, last, size() * sizeof(T), detail::getNextPowerOfTwo(sizeof(T)),
         propList,
         make_unique_ptr<detail::SYCLMemObjAllocatorHolder<AllocatorT>>());
-    impl->constructorNotification(CodeLoc, (void *)impl.get());
+    size_t r[3] = {Range[0], 0, 0};
+    impl->constructorNotification(CodeLoc, (void *)impl.get(), &*first,
+                                  (const void *)typeid(T).name(), dimensions,
+                                  sizeof(T), r);
   }
 
   // This constructor is a prototype for a future SYCL specification
@@ -235,7 +270,10 @@ public:
         detail::getNextPowerOfTwo(sizeof(T)), propList,
         make_unique_ptr<detail::SYCLMemObjAllocatorHolder<AllocatorT>>(
             allocator));
-    impl->constructorNotification(CodeLoc, (void *)impl.get());
+    size_t r[3] = {Range[0], 0, 0};
+    impl->constructorNotification(CodeLoc, (void *)impl.get(), container.data(),
+                                  (const void *)typeid(T).name(), dimensions,
+                                  sizeof(T), r);
   }
 
   // This constructor is a prototype for a future SYCL specification
@@ -252,7 +290,9 @@ public:
       : impl(b.impl), Range(subRange),
         OffsetInBytes(getOffsetInBytes<T>(baseIndex, b.Range)),
         IsSubBuffer(true) {
-    impl->constructorNotification(CodeLoc, (void *)impl.get());
+    impl->constructorNotification(CodeLoc, (void *)impl.get(), impl.get(),
+                                  (const void *)typeid(T).name(), dimensions,
+                                  sizeof(T), rangeToArray(Range).data());
 
     if (b.is_sub_buffer())
       throw cl::sycl::invalid_object_error(
@@ -281,7 +321,9 @@ public:
         detail::pi::cast<pi_native_handle>(MemObject), SyclContext, BufSize,
         make_unique_ptr<detail::SYCLMemObjAllocatorHolder<AllocatorT>>(),
         AvailableEvent);
-    impl->constructorNotification(CodeLoc, (void *)impl.get());
+    impl->constructorNotification(CodeLoc, (void *)impl.get(), &MemObject,
+                                  (const void *)typeid(T).name(), dimensions,
+                                  sizeof(T), rangeToArray(Range).data());
   }
 #endif
 
@@ -289,14 +331,18 @@ public:
          const detail::code_location CodeLoc = detail::code_location::current())
       : impl(rhs.impl), Range(rhs.Range), OffsetInBytes(rhs.OffsetInBytes),
         IsSubBuffer(rhs.IsSubBuffer) {
-    impl->constructorNotification(CodeLoc, (void *)impl.get());
+    impl->constructorNotification(CodeLoc, (void *)impl.get(), impl.get(),
+                                  (const void *)typeid(T).name(), dimensions,
+                                  sizeof(T), rangeToArray(Range).data());
   }
 
   buffer(buffer &&rhs,
          const detail::code_location CodeLoc = detail::code_location::current())
       : impl(std::move(rhs.impl)), Range(rhs.Range),
         OffsetInBytes(rhs.OffsetInBytes), IsSubBuffer(rhs.IsSubBuffer) {
-    impl->constructorNotification(CodeLoc, (void *)impl.get());
+    impl->constructorNotification(CodeLoc, (void *)impl.get(), impl.get(),
+                                  (const void *)typeid(T).name(), dimensions,
+                                  sizeof(T), rangeToArray(Range).data());
   }
 
   buffer &operator=(const buffer &rhs) = default;
@@ -485,7 +531,9 @@ private:
         MemObject, SyclContext, BufSize,
         make_unique_ptr<detail::SYCLMemObjAllocatorHolder<AllocatorT>>(),
         AvailableEvent);
-    impl->constructorNotification(CodeLoc, (void *)impl.get());
+    impl->constructorNotification(CodeLoc, (void *)impl.get(), &MemObject,
+                                  (const void *)typeid(T).name(), dimensions,
+                                  sizeof(T), rangeToArray(Range).data());
   }
 
   // Reinterpret contructor
@@ -495,7 +543,9 @@ private:
          const detail::code_location CodeLoc = detail::code_location::current())
       : impl(Impl), Range(reinterpretRange), OffsetInBytes(reinterpretOffset),
         IsSubBuffer(isSubBuffer) {
-    impl->constructorNotification(CodeLoc, (void *)impl.get());
+    impl->constructorNotification(CodeLoc, (void *)impl.get(), Impl.get(),
+                                  (const void *)typeid(T).name(), dimensions,
+                                  sizeof(T), rangeToArray(Range).data());
   }
 
   template <typename Type, int N>

sycl/include/CL/sycl/detail/buffer_impl.hpp
@@ -156,6 +156,11 @@ public:
   void *allocateMem(ContextImplPtr Context, bool InitFromUserData,
                     void *HostPtr, RT::PiEvent &OutEventToWait) override;
   void constructorNotification(const detail::code_location &CodeLoc,
+                               void *UserObj, const void *HostObj,
+                               const void *Type, uint32_t Dim,
+                               uint32_t ElemType, size_t Range[3]);
+  // TODO: remove once ABI break is allowed
+  void constructorNotification(const detail::code_location &CodeLoc,
                                void *UserObj);
   void destructorNotification(void *UserObj);
 

sycl/source/detail/buffer_impl.cpp
@@ -32,9 +32,19 @@ void *buffer_impl::allocateMem(ContextImplPtr Context, bool InitFromUserData,
       BaseT::MInteropEvent, BaseT::MInteropContext, MProps, OutEventToWait);
 }
 void buffer_impl::constructorNotification(const detail::code_location &CodeLoc,
-                                          void *UserObj) {
-  XPTIRegistry::bufferConstructorNotification(UserObj, CodeLoc);
+                                          void *UserObj, const void *HostObj,
+                                          const void *Type, uint32_t Dim,
+                                          uint32_t ElemSize, size_t Range[3]) {
+  XPTIRegistry::bufferConstructorNotification(UserObj, CodeLoc, HostObj, Type,
+                                              Dim, ElemSize, Range);
 }
+// TODO: remove once ABI break is allowed
+void buffer_impl::constructorNotification(const detail::code_location &CodeLoc,
+                                          void *UserObj) {
+  size_t r[3] = {0, 0, 0};
+  constructorNotification(CodeLoc, UserObj, nullptr, "", 0, 0, r);
+}
+
 void buffer_impl::destructorNotification(void *UserObj) {
   XPTIRegistry::bufferDestructorNotification(UserObj);
 }

sycl/source/detail/memory_manager.cpp
@@ -298,7 +298,6 @@ void *MemoryManager::allocateHostMemory(SYCLMemObjI *MemObj, void *UserPtr,
     return UserPtr;
 
   void *NewMem = MemObj->allocateHostMem();
-
   // Need to initialize new memory if user provides pointer to read only
   // memory.
   if (UserPtr && HostPtrReadOnly == true)

sycl/source/detail/xpti_registry.cpp
@@ -17,22 +17,15 @@ __SYCL_INLINE_NAMESPACE(cl) {
 namespace sycl {
 namespace detail {
 #ifdef XPTI_ENABLE_INSTRUMENTATION
-xpti::trace_event_data_t *
-XPTIRegistry::createTraceEvent(void *Obj, const char *ObjName, uint64_t &IId,
-                               const detail::code_location &CodeLoc,
-                               uint16_t TraceEventType) {
-  std::string Name;
-  if (CodeLoc.fileName()) {
-    Name = std::string(CodeLoc.fileName()) + ":" +
-           std::to_string(CodeLoc.lineNumber()) + ":" +
-           std::to_string(CodeLoc.columnNumber());
-  } else {
-    xpti::utils::StringHelper NG;
-    Name = NG.nameWithAddress<void *>(ObjName, Obj);
-  }
-  xpti::payload_t Payload(
-      Name.c_str(), (CodeLoc.fileName() ? CodeLoc.fileName() : ""),
-      CodeLoc.lineNumber(), CodeLoc.columnNumber(), (void *)Obj);
+xpti::trace_event_data_t *XPTIRegistry::createTraceEvent(
+    const void *Obj, const void *FuncPtr, uint64_t &IId,
+    const detail::code_location &CodeLoc, uint16_t TraceEventType) {
+  xpti::utils::StringHelper NG;
+  auto Name = NG.nameWithAddress<void *>(CodeLoc.functionName(),
+                                         const_cast<void *>(FuncPtr));
+  xpti::payload_t Payload(Name.c_str(),
+                          (CodeLoc.fileName() ? CodeLoc.fileName() : ""),
+                          CodeLoc.lineNumber(), CodeLoc.columnNumber(), Obj);
 
   // Calls could be at different user-code locations; We create a new event
   // based on the code location info and if this has been seen before, a
@@ -43,16 +36,28 @@ XPTIRegistry::createTraceEvent(void *Obj, const char *ObjName, uint64_t &IId,
 #endif // XPTI_ENABLE_INSTRUMENTATION
 
 void XPTIRegistry::bufferConstructorNotification(
-    void *UserObj, const detail::code_location &CodeLoc) {
+    const void *UserObj, const detail::code_location &CodeLoc,
+    const void *HostObj, const void *Type, uint32_t Dim, uint32_t ElemSize,
+    size_t Range[3]) {
   (void)UserObj;
   (void)CodeLoc;
+  (void)HostObj;
+  (void)Type;
+  (void)Dim;
+  (void)ElemSize;
+  (void)Range;
 #ifdef XPTI_ENABLE_INSTRUMENTATION
   GlobalHandler::instance().getXPTIRegistry().initializeFrameworkOnce();
   if (!xptiTraceEnabled())
     return;
 
   uint64_t IId;
-  xpti::offload_buffer_data_t BufConstr{(uintptr_t)UserObj};
+  xpti::offload_buffer_data_t BufConstr{(uintptr_t)UserObj,
+                                        (uintptr_t)HostObj,
+                                        (const char *)Type,
+                                        ElemSize,
+                                        Dim,
+                                        {Range[0], Range[1], Range[2]}};
 
   xpti::trace_event_data_t *TraceEvent = createTraceEvent(
       UserObj, "buffer", IId, CodeLoc, xpti::trace_offload_buffer_event);
@@ -61,7 +66,8 @@ void XPTIRegistry::bufferConstructorNotification(
 #endif
 }
 
-void XPTIRegistry::bufferAssociateNotification(void *UserObj, void *MemObj) {
+void XPTIRegistry::bufferAssociateNotification(const void *UserObj,
+                                               const void *MemObj) {
   (void)UserObj;
   (void)MemObj;
 #ifdef XPTI_ENABLE_INSTRUMENTATION
@@ -77,7 +83,8 @@ void XPTIRegistry::bufferAssociateNotification(void *UserObj, void *MemObj) {
 #endif
 }
 
-void XPTIRegistry::bufferReleaseNotification(void *UserObj, void *MemObj) {
+void XPTIRegistry::bufferReleaseNotification(const void *UserObj,
+                                             const void *MemObj) {
   (void)UserObj;
   (void)MemObj;
 #ifdef XPTI_ENABLE_INSTRUMENTATION
@@ -93,7 +100,7 @@ void XPTIRegistry::bufferReleaseNotification(void *UserObj, void *MemObj) {
 #endif
 }
 
-void XPTIRegistry::bufferDestructorNotification(void *UserObj) {
+void XPTIRegistry::bufferDestructorNotification(const void *UserObj) {
   (void)UserObj;
 #ifdef XPTI_ENABLE_INSTRUMENTATION
   if (!xptiTraceEnabled())
@@ -107,8 +114,8 @@ void XPTIRegistry::bufferDestructorNotification(void *UserObj) {
 }
 
 void XPTIRegistry::bufferAccessorNotification(
-    void *UserObj, void *AccessorObj, uint32_t Target, uint32_t Mode,
-    const detail::code_location &CodeLoc) {
+    const void *UserObj, const void *AccessorObj, uint32_t Target,
+    uint32_t Mode, const detail::code_location &CodeLoc) {
   (void)UserObj;
   (void)AccessorObj;
   (void)CodeLoc;

sycl/source/detail/xpti_registry.hpp
@@ -89,15 +89,16 @@ public:
 #endif // XPTI_ENABLE_INSTRUMENTATION
   }
 
-  static void
-  bufferConstructorNotification(void *UserObj,
-                                const detail::code_location &CodeLoc);
-  static void bufferAssociateNotification(void *UserObj, void *MemObj);
-  static void bufferReleaseNotification(void *UserObj, void *MemObj);
-  static void bufferDestructorNotification(void *UserObj);
-  static void bufferAccessorNotification(void *UserObj, void *AccessorObj,
-                                         uint32_t Target, uint32_t Mode,
-                                         const detail::code_location &CodeLoc);
+  static void bufferConstructorNotification(const void *,
+                                            const detail::code_location &,
+                                            const void *, const void *,
+                                            uint32_t, uint32_t, size_t[3]);
+  static void bufferAssociateNotification(const void *, const void *);
+  static void bufferReleaseNotification(const void *, const void *);
+  static void bufferDestructorNotification(const void *);
+  static void bufferAccessorNotification(const void *, const void *, uint32_t,
+                                         uint32_t,
+                                         const detail::code_location &);
 
 private:
   std::unordered_set<std::string> MActiveStreams;
@@ -105,7 +106,7 @@ private:
 
 #ifdef XPTI_ENABLE_INSTRUMENTATION
   static xpti::trace_event_data_t *
-  createTraceEvent(void *Obj, const char *ObjName, uint64_t &IId,
+  createTraceEvent(const void *Obj, const void *ObjName, uint64_t &IId,
                    const detail::code_location &CodeLoc,
                    uint16_t TraceEventType);
 #endif // XPTI_ENABLE_INSTRUMENTATION

sycl/test/abi/sycl_symbols_linux.dump
@@ -3834,6 +3834,7 @@ _ZN2cl4sycl6detail11SYCLMemObjTC2EmRKNS0_7contextEmNS0_5eventESt10unique_ptrINS1
 _ZN2cl4sycl6detail11buffer_impl11allocateMemESt10shared_ptrINS1_12context_implEEbPvRP9_pi_event
 _ZN2cl4sycl6detail11buffer_impl22destructorNotificationEPv
 _ZN2cl4sycl6detail11buffer_impl23constructorNotificationERKNS1_13code_locationEPv
+_ZN2cl4sycl6detail11buffer_impl23constructorNotificationERKNS1_13code_locationEPvPKvS8_jjPm
 _ZN2cl4sycl6detail11make_deviceEmNS0_7backendE
 _ZN2cl4sycl6detail11make_kernelERKNS0_7contextERKNS0_13kernel_bundleILNS0_12bundle_stateE2EEEmbNS0_7backendE
 _ZN2cl4sycl6detail11make_kernelEmRKNS0_7contextENS0_7backendE

commit c230f3c4a1eadd187dd1ed254f23da0057cde9a6

[SYCL] Fix CI failure (#5434)
Fix build issues introduced by #5259

xpti/include/xpti/xpti_data_types.h
@@ -523,9 +523,9 @@ struct offload_buffer_data_t {
   /// A pointer to host memory offload object.
   uintptr_t host_object_handle = 0;
   /// A string representing the type of buffer element.
-  const char *element_type;
+  const char *element_type = nullptr;
   /// Buffer element size in bytes
-  uint32_t element_size;
+  uint32_t element_size = 0;
   /// Buffer dimensions number.
   uint32_t dim = 0;
   /// Buffer size for each dimension.