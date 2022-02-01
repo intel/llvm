465bbe36050c544e3fd89b38f36fa779732accde
denis-kabanov <denis.kabanov@intel.com>
2022-02-01 15:19:41 +0300

    [SYCL] Disable Reduction/reduction_nd_lambda.cpp due to sporadic failure (intel/llvm-test-suite#770)

--- SYCL/Reduction/reduction_nd_lambda.cpp
+++ SYCL/Reduction/reduction_nd_lambda.cpp
@@ -1,3 +1,5 @@
+// Test disabled due to sporadic failure
+// REQUIRES: TEMPORARILY_DISABLED
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
 // RUNx: %HOST_RUN_PLACEHOLDER %t.out
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
