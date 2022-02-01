af9ba6565c45aa0d14e0ec223f22395f2c9d0683
IgorKharchikov <95442829+IgorKharchikov@users.noreply.github.com>
2022-02-01 12:36:27 +0300

    Update sycl_2020_deprecations.cpp

--- sycl/test/warnings/sycl_2020_deprecations.cpp
+++ sycl/test/warnings/sycl_2020_deprecations.cpp
@@ -179,7 +179,7 @@ int main() {
 
   cl::sycl::multi_ptr<int, cl::sycl::access::address_space::global_space> a(
       nullptr);
-  // expected-warning@+1 {{'atomic<int sycl::access::address_space::global_space>' is deprecated: sycl::atomic is deprecated since SYCL 2020}}
+  // expected-warning@+1 {{'atomic<int, sycl::access::address_space::global_space>' is deprecated: sycl::atomic is deprecated since SYCL 2020}}
   cl::sycl::atomic<int> b(a);
 
   return 0;
