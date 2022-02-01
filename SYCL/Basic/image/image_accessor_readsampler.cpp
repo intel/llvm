cd159f3c7c0aa3329a4a335ab079567b35867aae
denis-kabanov <denis.kabanov@intel.com>
2022-02-01 15:12:40 +0300

    [SYCL] Disable Basic/image/image_accessor_readsampler.cpp (intel/llvm-test-suite#790)
    Disable Basic/image/image_accessor_readsampler.cpp dut to fails of
    Jenkins/pre-ci-windows 31.01.2022

--- SYCL/Basic/image/image_accessor_readsampler.cpp
+++ SYCL/Basic/image/image_accessor_readsampler.cpp
@@ -1,4 +1,5 @@
-// UNSUPPORTED: cuda || hip
+// UNSUPPORTED: cuda || hip || (windows && level_zero)
+// unsupported on windows (level-zero) due to fail of Jenkins/pre-ci-windows
 // CUDA cannot support SYCL 1.2.1 images.
 //
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
