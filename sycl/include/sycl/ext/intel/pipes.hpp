c41ffd7137954cfc6bf148593f6d9ece5b2641ab
tyoungsc <tanner.young-schultz@intel.com>
2022-02-03 13:01:27 -0800

    Removing static asserts since the unsignedness of size_t takes care of it.

--- sycl/include/sycl/ext/intel/pipes.hpp
+++ sycl/include/sycl/ext/intel/pipes.hpp
@@ -21,7 +21,6 @@ template <class _name, class _dataT, int32_t _min_capacity = 0> class pipe {
 public:
   using value_type = _dataT;
   static constexpr int32_t min_capacity = _min_capacity;
-  static_assert(_min_capacity >= 0, "A pipe's capacity cannot be negative");
 
   // Non-blocking pipes
   // Reading from pipe is lowered to SPIR-V instruction OpReadPipe via SPIR-V
@@ -118,7 +117,6 @@ class kernel_readable_io_pipe {
 public:
   using value_type = _dataT;
   static constexpr int32_t min_capacity = _min_capacity;
-  static_assert(_min_capacity >= 0, "A pipe's capacity cannot be negative");
   
   // Non-blocking pipes
   // Reading from pipe is lowered to SPIR-V instruction OpReadPipe via SPIR-V
@@ -168,7 +166,6 @@ class kernel_writeable_io_pipe {
 public:
   using value_type = _dataT;
   static constexpr int32_t min_capacity = _min_capacity;
-  static_assert(_min_capacity >= 0, "A pipe's capacity cannot be negative");
   
   // Non-blocking pipes
   // Writing to pipe is lowered to SPIR-V instruction OpWritePipe via SPIR-V
