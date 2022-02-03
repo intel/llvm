a1033d18d212ecf58a8b788d5c1a96170dd7f629
Kseniya Tikhomirova <kseniya.tikhomirova@intel.com>
2022-02-03 11:34:03 +0300

    Disables reduction_big_data test due to flaky failures (intel/llvm-test-suite#786)
    Observed flaky failures with unrelated changes
    
    Signed-off-by: Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>

--- SYCL/Reduction/reduction_big_data.cpp
+++ SYCL/Reduction/reduction_big_data.cpp
@@ -1,3 +1,5 @@
+// REQUIRES: TEMPORARILY_DISABLED
+// Temporarily disabled due to flaky unrelated failures
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 // RUN: %ACC_RUN_PLACEHOLDER %t.out
