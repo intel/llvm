002ef854297423b78053d16d7b3aa79e12e6df5a
Alexander Batashev <alexbatashev@outlook.com>
2022-02-05 09:32:38 +0300

    add missing header

--- sycl/unittests/helpers/PiMock.cpp
+++ sycl/unittests/helpers/PiMock.cpp
@@ -1,3 +1,11 @@
+//==------------- PiMock.cpp --- Mock unit testing library -----------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "helpers/PiMock.hpp"
 #include "detail/global_handler.hpp"
 #include "detail/plugin.hpp"
