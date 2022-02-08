126693a31afb9f39463352ab23fd7079a339ecce
Steffen Larsen <steffen.larsen@intel.com>
2022-02-08 12:30:13 +0300

    Fixed property size-check and added comment
    Signed-off-by: Steffen Larsen <steffen.larsen@intel.com>

--- sycl/source/detail/program_manager/program_manager.cpp
+++ sycl/source/detail/program_manager/program_manager.cpp
@@ -1142,7 +1142,14 @@ void ProgramManager::addImages(pi_device_binaries DeviceBinary) {
 
           pi::ByteArray DeviceGlobalInfo =
               pi::DeviceBinaryProperty(DeviceGlobal).asByteArray();
-          assert(DeviceGlobalInfo.size() > 13 && "Unexpected property size");
+
+          // The supplied device_global info property is expected to contain:
+          // * 8 bytes - Size of the property.
+          // * 4 bytes - Size of the underlying type in the device_global.
+          // * 1 byte  - 0 if device_global has device_image_scope and any value
+          //             otherwise.
+          // Note: Property may be padded.
+          assert(DeviceGlobalInfo.size() >= 13 && "Unexpected property size");
           const std::uint32_t TypeSize =
               *reinterpret_cast<const std::uint32_t *>(&DeviceGlobalInfo[8]);
           const std::uint32_t DeviceImageScopeDecorated = DeviceGlobalInfo[12];
