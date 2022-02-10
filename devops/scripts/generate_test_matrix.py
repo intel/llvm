320b1f0217be7b55f071d0a54966304dee0ee924
Alexander Batashev <alexander.batashev@intel.com>
2022-02-10 14:20:50 +0300

    more python

--- devops/scripts/generate_test_matrix.py
+++ devops/scripts/generate_test_matrix.py
@@ -6,7 +6,7 @@ with open(sys.argv[1]) as f:
     config = json.load(f)
     inputs = json.loads(os.environ['GHA_INPUT'])
 
-    enabled_lts_configs = inputs.lts.slit(";")
+    enabled_lts_configs = inputs['lts'].slit(";")
 
     lts_config = []
 
