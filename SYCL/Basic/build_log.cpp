a59ab6945d10ab023f5f0756c9a33f9a75a53b12
Alexander Batashev <alexbatashev@outlook.com>
2022-02-10 11:20:37 +0300

    [SYCL] Add tests for SYCL_RT_WARNING_LEVEL (intel/llvm-test-suite#753)

---
+++ SYCL/Basic/build_log.cpp
@@ -0,0 +1,20 @@
+// REQUIRES: opencl || level_zero, gpu
+// UNSUPPORTED: gpu-intel-dg1
+//
+// RUN: %clangxx -fsycl -fsycl-targets=spir64_gen -Xsycl-target-backend=spir64_gen "-device dg1" %s -o %t.out
+// RUN: %GPU_RUN_PLACEHOLDER env SYCL_RT_WARNING_LEVEL=2 %t.out 2>&1 %GPU_CHECK_PLACEHOLDER
+
+#include <sycl/sycl.hpp>
+
+int main() {
+  sycl::queue Q{sycl::default_selector()};
+
+  auto *I = sycl::malloc_device<int>(1, Q);
+  Q.single_task([=]() { I[0] = 42; });
+
+  sycl::free(I, Q);
+
+  return 0;
+}
+
+// CHECK: The program was built for {{.*}} devices
