5f3c25782d4cc69010de03be2ac79555c8821acb
Alexander Batashev <alexander.batashev@intel.com>
2022-02-11 19:39:22 +0000

    switch to official script

---
+++ devops/scripts/generate_test_matrix.js
@@ -0,0 +1,36 @@
+module.exports = ({ core, process }) => {
+  const fs = require('fs');
+  fs.readFile('./test_configs.json', 'utf8', (err, data) => {
+    if (err) {
+      console.log(`Error reading file from disk: ${err}`);
+    } else {
+      const testConfigs = JSON.parse(data);
+      const inputs = JSON.parse(process.env.GHA_INPUTS);
+
+      const ltsConfigs = inputs.lts_config.split(';');
+
+      console.log(inputs);
+      console.log(ltsConfigs);
+
+      const enabledLTSConfigs = [];
+
+      testConfigs.lts.forEach(v => {
+        console.log(v);
+        if (ltsConfigs.includes(v.config)) {
+          console.log("BOO");
+          enabledLTSConfigs.push(v);
+        }
+      });
+
+      let ltsString = JSON.stringify(enabledLTSConfigs);
+      console.log(ltsString);
+
+      for (let [key, value] of Object.entries(inputs)) {
+        ltsString = ltsString.replaceAll("${{ inputs." + key + " }}", value);
+      }
+
+      core.setOutput('lts', ltsString);
+    }
+
+  });
+}
