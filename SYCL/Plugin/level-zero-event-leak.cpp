2c30dc5006ee58b6e4a2823144d199af4671db20
Artur Gainullin <artur.gainullin@intel.com>
2022-02-12 08:04:17 -0800

    Temporarily disable level-zero-event-leak.cpp on Windows (intel/llvm-test-suite#834)

--- SYCL/Plugin/level-zero-event-leak.cpp
+++ SYCL/Plugin/level-zero-event-leak.cpp
@@ -1,4 +1,7 @@
 // REQUIRES: level_zero, level_zero_dev_kit
+// TODO: ZE_DEBUG=4 produces no output on Windows. Enable when fixed.
+// UNSUPPORTED: windows
+//
 // RUN: %clangxx -fsycl -fsycl-unnamed-lambda -fsycl-targets=%sycl_triple %level_zero_options %s -o %t.out
 // RUN: env SYCL_DEVICE_FILTER=level_zero ZE_DEBUG=4 %GPU_RUN_PLACEHOLDER %t.out 2>&1 %GPU_CHECK_PLACEHOLDER
 //
