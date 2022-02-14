39e2cc6bfa9ea56507d535586301557690385e57
Steffen Larsen <steffen.larsen@intel.com>
2022-02-14 18:08:39 +0300

    Add test cases for redeclarations with exact duplicate attr
    Signed-off-by: Steffen Larsen <steffen.larsen@intel.com>

--- clang/test/SemaSYCL/attr-add-ir-attributes-merge.cpp
+++ clang/test/SemaSYCL/attr-add-ir-attributes-merge.cpp
@@ -2,7 +2,7 @@
 // RUN: %clang_cc1 -fsycl-is-device -std=gnu++11 -ast-dump %s | FileCheck %s
 
 // CHECK:      FunctionDecl [[FunctionRedecl1ID1:0x[0-9a-f]+]] {{.*}} FunctionRedecl1 'void ()'
-// CHECK:      FunctionDecl [[FunctionRedecl1ID2:0x[0-9a-f]+]] prev [[FunctionRedecl1ID1]] {{.*}} FunctionRedecl1 'void ()'
+// CHECK-NEXT: FunctionDecl [[FunctionRedecl1ID2:0x[0-9a-f]+]] prev [[FunctionRedecl1ID1]] {{.*}} FunctionRedecl1 'void ()'
 // CHECK-NEXT:   SYCLAddIRAttributesFunctionAttr
 // CHECK-NEXT:     ConstantExpr {{.*}} 'const char[6]' lvalue
 // CHECK-NEXT:       value: LValue
@@ -16,7 +16,21 @@
 // CHECK-NEXT:     ConstantExpr {{.*}} 'bool'
 // CHECK-NEXT:       value: Int 1
 // CHECK-NEXT:       CXXBoolLiteralExpr {{.*}} 'bool' true
-// CHECK-NEXT: FunctionDecl {{.*}} prev [[FunctionRedecl1ID2]] {{.*}} FunctionRedecl1 'void ()'
+// CHECK-NEXT: FunctionDecl [[FunctionRedecl1ID3:0x[0-9a-f]+]] prev [[FunctionRedecl1ID2]] {{.*}} FunctionRedecl1 'void ()'
+// CHECK-NEXT:   SYCLAddIRAttributesFunctionAttr
+// CHECK-NEXT:     ConstantExpr {{.*}} 'const char[6]' lvalue
+// CHECK-NEXT:       value: LValue
+// CHECK-NEXT:       StringLiteral {{.*}} 'const char[6]' lvalue "Attr1"
+// CHECK-NEXT:     ConstantExpr {{.*}} 'const char[6]' lvalue
+// CHECK-NEXT:       value: LValue
+// CHECK-NEXT:       StringLiteral {{.*}} 'const char[6]' lvalue "Attr2"
+// CHECK-NEXT:     ConstantExpr {{.*}} 'int'
+// CHECK-NEXT:       value: Int 1
+// CHECK-NEXT:       IntegerLiteral {{.*}} 'int' 1
+// CHECK-NEXT:     ConstantExpr {{.*}} 'bool'
+// CHECK-NEXT:       value: Int 1
+// CHECK-NEXT:       CXXBoolLiteralExpr {{.*}} 'bool' true
+// CHECK-NEXT: FunctionDecl {{.*}} prev [[FunctionRedecl1ID3]] {{.*}} FunctionRedecl1 'void ()'
 // CHECK-NEXT:   CompoundStmt
 // CHECK-NEXT:   SYCLAddIRAttributesFunctionAttr
 // CHECK-NEXT:     ConstantExpr {{.*}} 'const char[6]' lvalue
@@ -27,6 +41,7 @@
 // CHECK-NEXT:       CXXBoolLiteralExpr {{.*}} 'bool' false
 void FunctionRedecl1();
 [[__sycl_detail__::add_ir_attributes_function("Attr1", "Attr2", 1, true)]] void FunctionRedecl1(); // expected-note {{previous attribute is here}}
+[[__sycl_detail__::add_ir_attributes_function("Attr1", "Attr2", 1, true)]] void FunctionRedecl1(); // expected-warning {{attribute 'add_ir_attributes_function' is already applied}} // expected-note {{previous attribute is here}}
 [[__sycl_detail__::add_ir_attributes_function("Attr3", false)]] void FunctionRedecl1(){};          // expected-warning {{attribute 'add_ir_attributes_function' is already applied}}
 
 // CHECK:      CXXRecordDecl [[GlobalVarStructRedecl1ID1:0x[0-9a-f]+]] {{.*}} struct GlobalVarStructRedecl1
@@ -44,7 +59,21 @@ void FunctionRedecl1();
 // CHECK-NEXT:     ConstantExpr {{.*}} 'bool'
 // CHECK-NEXT:       value: Int 1
 // CHECK-NEXT:       CXXBoolLiteralExpr {{.*}} 'bool' true
-// CHECK-NEXT: CXXRecordDecl {{.*}} prev [[GlobalVarStructRedecl1ID2]] {{.*}} struct GlobalVarStructRedecl1 definition
+// CHECK-NEXT: CXXRecordDecl [[GlobalVarStructRedecl1ID3:0x[0-9a-f]+]] prev [[GlobalVarStructRedecl1ID2]] {{.*}} struct GlobalVarStructRedecl1
+// CHECK-NEXT:   SYCLAddIRAttributesGlobalVariableAttr
+// CHECK-NEXT:     ConstantExpr {{.*}} 'const char[6]' lvalue
+// CHECK-NEXT:       value: LValue
+// CHECK-NEXT:       StringLiteral {{.*}} 'const char[6]' lvalue "Attr1"
+// CHECK-NEXT:     ConstantExpr {{.*}} 'const char[6]' lvalue
+// CHECK-NEXT:       value: LValue
+// CHECK-NEXT:       StringLiteral {{.*}} 'const char[6]' lvalue "Attr2"
+// CHECK-NEXT:     ConstantExpr {{.*}} 'int'
+// CHECK-NEXT:       value: Int 1
+// CHECK-NEXT:       IntegerLiteral {{.*}} 'int' 1
+// CHECK-NEXT:     ConstantExpr {{.*}} 'bool'
+// CHECK-NEXT:       value: Int 1
+// CHECK-NEXT:       CXXBoolLiteralExpr {{.*}} 'bool' true
+// CHECK-NEXT: CXXRecordDecl {{.*}} prev [[GlobalVarStructRedecl1ID3]] {{.*}} struct GlobalVarStructRedecl1 definition
 // CHECK-NEXT:   DefinitionData
 // CHECK-NEXT:     DefaultConstructor
 // CHECK-NEXT:     CopyConstructor
@@ -62,6 +91,7 @@ void FunctionRedecl1();
 // CHECK-NEXT:   CXXRecordDecl {{.*}} implicit struct GlobalVarStructRedecl1
 struct GlobalVarStructRedecl1;
 struct [[__sycl_detail__::add_ir_attributes_global_variable("Attr1", "Attr2", 1, true)]] GlobalVarStructRedecl1; // expected-note {{previous attribute is here}}
+struct [[__sycl_detail__::add_ir_attributes_global_variable("Attr1", "Attr2", 1, true)]] GlobalVarStructRedecl1; // expected-warning {{attribute 'add_ir_attributes_global_variable' is already applied}} expected-note {{previous attribute is here}}
 struct [[__sycl_detail__::add_ir_attributes_global_variable("Attr3", false)]] GlobalVarStructRedecl1{};          // expected-warning {{attribute 'add_ir_attributes_global_variable' is already applied}}
 
 // CHECK:      CXXRecordDecl {{.*}} referenced struct GlobalVarStructBase definition
@@ -124,9 +154,9 @@ struct [[__sycl_detail__::add_ir_attributes_global_variable("Attr3", false)]] Gl
 // CHECK-NEXT:       CXXBoolLiteralExpr {{.*}} 'bool' false
 // CHECK-NEXT:   CXXRecordDecl {{.*}} implicit struct GlobalVarStructInherit3
 struct GlobalVarStructBase {};
-struct [[__sycl_detail__::add_ir_attributes_global_variable("Attr1", "Attr2", 1, true)]] GlobalVarStructInherit1 : GlobalVarStructBase {};
+struct [[__sycl_detail__::add_ir_attributes_global_variable("Attr1", "Attr2", 1, true)]] GlobalVarStructInherit1 : GlobalVarStructBase{};
 struct GlobalVarStructInherit2 : GlobalVarStructInherit1 {};
-struct [[__sycl_detail__::add_ir_attributes_global_variable("Attr3", false)]] GlobalVarStructInherit3 : GlobalVarStructInherit1 {};
+struct [[__sycl_detail__::add_ir_attributes_global_variable("Attr3", false)]] GlobalVarStructInherit3 : GlobalVarStructInherit1{};
 
 // CHECK:      CXXRecordDecl {{.*}} referenced struct SpecialClassStructBase definition
 // CHECK-NEXT:   DefinitionData
