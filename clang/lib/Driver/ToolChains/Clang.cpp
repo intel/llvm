f7f2faa5336c70afb5e6b20bfdb1a9a0d3507f89
Peter Jensen <peter.jensen@intel.com>
2022-02-14 16:56:21 -0800

    Use -fcuda-flush-denormals-to-zero for SYCL programs

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -3005,6 +3005,12 @@ static void RenderFloatingPointOptions(const ToolChain &TC, const Driver &D,
       RoundingMathPresent = false;
       break;
 
+    case options::OPT_fcuda_flush_denormals_to_zero:
+    case options::OPT_fgpu_flush_denormals_to_zero:
+      DenormalFPMath = llvm::DenormalMode::getPositiveZero();
+      DenormalFP32Math = llvm::DenormalMode::getPositiveZero();
+      break;
+
     case options::OPT_fdenormal_fp_math_EQ:
       DenormalFPMath = llvm::parseDenormalFPAttribute(A->getValue());
       DenormalFP32Math = DenormalFPMath;
