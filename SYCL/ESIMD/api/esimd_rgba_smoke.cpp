027f6b5722742754bdaacbeb5f15ece51dd50caf
dongkyunahn-intel <dongkyun.ahn@intel.com>
2022-02-14 17:07:39 -0800

    [SYCL][ESIMD][EMU] XFAIL for 'SYCL/ESIMD/api/esimd_rgba_smoke.cpp' (intel/llvm-test-suite#840)
    - Due to unimplemeted 'single_task()'

--- SYCL/ESIMD/api/esimd_rgba_smoke.cpp
+++ SYCL/ESIMD/api/esimd_rgba_smoke.cpp
@@ -7,6 +7,8 @@
 //===----------------------------------------------------------------------===//
 // REQUIRES: gpu
 // UNSUPPORTED: cuda || hip
+// TODO: esimd_emulator fails due to unimplemented 'single_task()' method
+// XFAIL: esimd_emulator
 // RUN: %clangxx -fsycl %s -o %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 
