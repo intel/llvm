826cba69c72a129a0c14dc3370c197be917736a7
smaslov-intel <sergey.v.maslov@intel.com>
2022-02-16 16:10:11 -0600

    [SYCL] update trace for https://github.com/intel/llvm/pull/5543 (intel/llvm-test-suite#841)

--- SYCL/Plugin/level_zero_device_scope_events.cpp
+++ SYCL/Plugin/level_zero_device_scope_events.cpp
@@ -12,7 +12,7 @@
 // MODE1-LABEL: Submitted all kernels
 // MODE1: ---> piEventsWait(
 // MODE1-NEXT:        <unknown> : 1
-// MODE1: PI ---> EventCreate(Context, true, &HostVisibleEvent)
+// MODE1: PI ---> EventCreate(Queue->Context, Queue, ForceHostVisible
 // MODE1: ZE ---> zeEventCreate(ZeEventPool, &ZeEventDesc, &ZeEvent)
 // MODE1: ZE ---> zeCommandListAppendWaitOnEvents(CommandList->first, 1,
 // &ZeEvent) MODE1-NEXT: ZE --->
@@ -22,14 +22,14 @@
 // With the SYCL_PI_LEVEL_ZERO_DEVICE_SCOPE_EVENTS=2 mode look for pattern that
 // creates host-visible event just before command-list submission.
 //
-// MODE2: PI ---> EventCreate(Context, true, &HostVisibleEvent)
+// MODE2: PI ---> EventCreate(Queue->Context, Queue, ForceHostVisible
 // MODE2: ZE ---> zeEventCreate(ZeEventPool, &ZeEventDesc, &ZeEvent)
 // MODE2: ZE ---> zeCommandListAppendSignalEvent(CommandList->first,
 // HostVisibleEvent->ZeEvent) MODE2: ZE --->
 // zeCommandListClose(CommandList->first) MODE2: ZE --->
 // zeCommandQueueExecuteCommandLists(ZeCommandQueue, 1, &ZeCommandList,
 // CommandList->second.ZeFence)
-//
+///
 #include <CL/sycl.hpp>
 
 int main(int argc, char **argv) {
