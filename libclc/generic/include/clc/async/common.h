6a3471bc78837c808367144f007105b8248c4f41
jack.kirk <jack.kirk@codeplay.com>
2022-02-18 20:32:17 +0000

    Remove subgroup async copy libclc support.
    It is not yet used by the runtime so I have removed it for the time being.

--- libclc/generic/include/clc/async/common.h
+++ libclc/generic/include/clc/async/common.h
@@ -10,17 +10,13 @@
 #define CLC_ASYNC_COMMON
 
 #define SET_GROUP_SIZE_AND_ID(SIZE, ID)                                        \
-  if (scope == Workgroup) {                                                    \
     SIZE = __spirv_WorkgroupSize_x() * __spirv_WorkgroupSize_y() *             \
            __spirv_WorkgroupSize_z();                                          \
     ID = (__spirv_WorkgroupSize_y() * __spirv_WorkgroupSize_x() *              \
           __spirv_LocalInvocationId_z()) +                                     \
          (__spirv_WorkgroupSize_x() * __spirv_LocalInvocationId_y()) +         \
-         __spirv_LocalInvocationId_x();                                        \
-  } else {                                                                     \
-    SIZE = __spirv_SubgroupSize();                                             \
-    ID = __spirv_SubgroupLocalInvocationId();                                  \
-  }
+         __spirv_LocalInvocationId_x();
+
 // Macro used by all data types, for generic and nvidia, for async copy when
 // arch < sm80
 #define STRIDED_COPY(DST_AS, SRC_AS, DST_STRIDE, SRC_STRIDE)                   \
