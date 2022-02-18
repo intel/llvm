369ae6bf9d5912123546937e9f9e503d0f0c2332
Soumi Manna <soumi.manna@intel.com>
2022-02-18 11:47:14 -0800

    Remove redundant change
    Signed-off-by: Soumi Manna <soumi.manna@intel.com>

--- clang/test/SemaSYCL/reqd-sub-group-size-host.cpp
+++ clang/test/SemaSYCL/reqd-sub-group-size-host.cpp
@@ -7,22 +7,3 @@ class Functor {
 public:
   [[intel::reqd_sub_group_size(16)]] void operator()() {}
 };
-
-// RUN: %clang_cc1 -fsycl-is-host -triple x86_64-pc-linux-gnu -fsyntax-only -verify %s -DSYCLHOST
-// RUN: %clang_cc1 -triple x86_64-pc-linux-gnu -fsyntax-only -verify %s
-
-#ifdef SYCLHOST
-// expected-no-diagnostics
-#endif
-
-void foo()
-{
-#ifndef SYCLHOST
-// expected-warning@+2 {{'reqd_sub_group_size' attribute ignored}}
-#endif
-  class Functor {
-public:
-  [[intel::reqd_sub_group_size(16)]] void operator()() {}
-};
-}
-
