581bf1f70299113fed00b6ae500424a4e4b05ddd
Soumi Manna <soumi.manna@intel.com>
2022-02-20 13:22:48 -0800

    [SYCL] Silence unknown attribut warnings on Host compilation
    The compiler emits "unknown attribute" warnings during host compilation
    for attributes:
    1. [[intel::num_simd_work_items()]]
    2. [[intel::kernel_args_restrict()]]
    3. [[intel::use_stall_enable_clusters]]
    4. [[intel::scheduler_target_fmax_mhz()]]
    5. [[intel::max_work_group_size()]]
    6. [[intel::max_global_work_dim()]]
    7. [[intel::bank_bits()]]
    
    This patch marks the attributes in Attr.td with "SilentlyIgnoreSYCLIsHost",
    so the diagnostic is not being issued on the host at all.
    
    Signed-off-by: Soumi Manna <soumi.manna@intel.com>

--- clang/test/SemaSYCL/spurious-host-warning.cpp
+++ clang/test/SemaSYCL/spurious-host-warning.cpp
@@ -1,6 +1,9 @@
 // RUN: %clang_cc1 -fsycl-is-host -triple x86_64-pc-linux-gnu -fsyntax-only -verify %s -DSYCLHOST
 // RUN: %clang_cc1 -triple x86_64-pc-linux-gnu -fsyntax-only -verify %s
 
+// Test checks the attribute is silently ignored during host compilation
+// where -fsycl-is-host is passed on cc1.
+
 #ifdef SYCLHOST
 // expected-no-diagnostics
 #endif
@@ -56,5 +59,39 @@ void foo()
 // expected-warning@+2 {{'simple_dual_port' attribute ignored}}
 #endif
   [[intel::simple_dual_port]] unsigned int v_ten[64];
-}
 
+#ifndef SYCLHOST
+// expected-warning@+2 {{'bank_bits' attribute ignored}}
+#endif
+  [[intel::bank_bits(2, 3, 4, 5)]] unsigned int v_eleven[64];
+
+#ifndef SYCLHOST
+// expected-warning@+2 {{'use_stall_enable_clusters' attribute ignored}}
+#endif
+  [[intel::use_stall_enable_clusters]] void func();
+
+#ifndef SYCLHOST
+// expected-warning@+2 {{'max_global_work_dim' attribute ignored}}
+#endif
+  [[intel::max_global_work_dim(1)]] void func1();
+
+#ifndef SYCLHOST
+// expected-warning@+2 {{'scheduler_target_fmax_mhz' attribute ignored}}
+#endif
+  [[intel::scheduler_target_fmax_mhz(3)]] void func3();
+
+#ifndef SYCLHOST
+// expected-warning@+2 {{'kernel_args_restrict' attribute ignored}}
+#endif
+  [[intel::kernel_args_restrict]] void func4();
+
+#ifndef SYCLHOST
+// expected-warning@+2 {{'num_simd_work_items' attribute ignored}}
+#endif
+  [[intel::num_simd_work_items(12)]] void func5();
+
+#ifndef SYCLHOST
+// expected-warning@+2 {{'max_work_group_size' attribute ignored}}
+#endif
+  [[intel::max_work_group_size(32, 32, 32)]] void func6();
+}
