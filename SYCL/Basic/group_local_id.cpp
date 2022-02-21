d1b89ea0ad705277ebe96e416aeb1afef7c98dd0
Arseniy Obolenskiy <gooddoog@student.su>
2022-02-21 10:53:34 +0300

    Add tests for local_id in group class on device (intel/llvm-test-suite#821)

---
+++ SYCL/Basic/group_local_id.cpp
@@ -0,0 +1,33 @@
+// REQUIRES: cpu
+
+// RUN: %clangxx -fsycl -std=c++17 -fsycl-targets=%sycl_triple %s -o %t.run
+// RUN: %CPU_RUN_PLACEHOLDER %t.run
+
+#include <CL/sycl.hpp>
+
+using namespace cl::sycl;
+
+int main() {
+  queue q(cpu_selector{});
+
+  const int sz = 16;
+  q.submit([&](handler &h) {
+    h.parallel_for(nd_range<1>{sz, sz}, [=](nd_item<1> item) {
+      assert(item.get_local_id() == item.get_group().get_local_id());
+    });
+  });
+  q.submit([&](handler &h) {
+    h.parallel_for(
+        nd_range<2>{range<2>{sz, sz}, range<2>{sz, sz}}, [=](nd_item<2> item) {
+          assert(item.get_local_id() == item.get_group().get_local_id());
+        });
+  });
+  q.submit([&](handler &h) {
+    h.parallel_for(nd_range<3>{range<3>{sz, sz, sz}, range<3>{sz, sz, sz}},
+                   [=](nd_item<3> item) {
+                     assert(item.get_local_id() ==
+                            item.get_group().get_local_id());
+                   });
+  });
+  q.wait();
+}
