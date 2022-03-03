aef34f9ed1af18321425e12d5b33aa800e73444e
Sindhu Chittireddy <sindhu.chittireddy@intel.com>
2022-03-03 14:06:55 -0800

    Add back attribute to CodeGenSYCL/Inputs/sycl.hpp after merge

--- clang/test/CodeGenSYCL/Inputs/sycl.hpp
+++ clang/test/CodeGenSYCL/Inputs/sycl.hpp
@@ -130,8 +130,10 @@ struct no_alias {
 };
 } // namespace property
 
+// device_global type decorated with attributes
 template <typename T>
-class [[__sycl_detail__::device_global]] device_global {
+class [[__sycl_detail__::device_global]]
+[[__sycl_detail__::global_variable_allowed]] device_global {
 public:
   const T &get() const noexcept { return *Data; }
   device_global() {}
