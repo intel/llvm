1b472be788f2c16cbbb1479b9085c14a1e2ac518
Alexey Bader <alexey.bader@intel.com>
2022-03-08 17:21:28 +0300

    Disable esimd_pack_unpack_mask test on OpenCL (intel/llvm-test-suite#902)
    The test was validated on old version of GPU driver.
    Newer version used in CI today exposes an issue with this test.

--- SYCL/ESIMD/api/esimd_pack_unpack_mask.cpp
+++ SYCL/ESIMD/api/esimd_pack_unpack_mask.cpp
@@ -9,6 +9,8 @@
 // UNSUPPORTED: cuda || hip
 // TODO: esimd_emulator fails due to unimplemented 'single_task()' method
 // XFAIL: esimd_emulator
+// TODO: fails on OpenCL - https://github.com/intel/llvm-test-suite/issues/901
+// UNSUPPORTED: opencl
 // RUN: %clangxx -fsycl %s -fsycl-device-code-split=per_kernel -o %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 //
