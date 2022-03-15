8a29c4412c06b1246bdcd0fa5954b70957211e36
jack.kirk <jack.kirk@codeplay.com>
2022-03-15 14:41:54 +0000

    Renamed extension to cover all bfloat16 funct.
    Removed aspect reference: can be added once the ext_oneapi_bfloat16 aspect is merged.

--- sycl/include/sycl/ext/oneapi/experimental/bfloat16.hpp
+++ sycl/include/sycl/ext/oneapi/experimental/bfloat16.hpp
@@ -17,7 +17,7 @@ namespace ext {
 namespace oneapi {
 namespace experimental {
 
-class [[sycl_detail::uses_aspects(ext_intel_bf16_conversion)]] bfloat16 {
+class bfloat16 {
   using storage_t = uint16_t;
   storage_t value;
 
