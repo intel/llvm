7b272af010de575c47d38baa4a208ff15bf6605e
maximdimakov <maxim.dimakov@intel.com>
2022-03-16 11:45:40 +0300

    [SYCL] Add checks for overridden compile and link options (#894)
    Signed-off-by: mdimakov <maxim.dimakov@intel.com>

--- SYCL/KernelAndProgram/kernel-bundle-merge-options-env.cpp
+++ SYCL/KernelAndProgram/kernel-bundle-merge-options-env.cpp
@@ -1,6 +1,9 @@
 // Disable fallback assert here so, that build process isn't affected
 // RUN: %clangxx -DSYCL_DISABLE_FALLBACK_ASSERT=1 -fsycl %s -o %t.out %debug_option
 // RUN: %GPU_RUN_PLACEHOLDER SYCL_PI_TRACE=-1 SYCL_PROGRAM_COMPILE_OPTIONS=-DENV_COMPILE_OPTS SYCL_PROGRAM_LINK_OPTIONS=-DENV_LINK_OPTS %t.out %GPU_CHECK_PLACEHOLDER
+// Check that options are overrided
+// RUN: %clangxx -DSYCL_DISABLE_FALLBACK_ASSERT=1 -fsycl -Xsycl-target-linker=spir64 -bar -Xsycl-target-frontend=spir64 -bar_compile %s -o %t.out
+// RUN: %GPU_RUN_PLACEHOLDER SYCL_PI_TRACE=-1 SYCL_PROGRAM_COMPILE_OPTIONS=-DENV_COMPILE_OPTS SYCL_PROGRAM_LINK_OPTIONS=-DENV_LINK_OPTS %t.out %GPU_CHECK_PLACEHOLDER
 // REQUIRES: gpu
 // UNSUPPORTED: cuda || hip
 // UNSUPPORTED: ze_debug-1,ze_debug4
@@ -12,16 +15,14 @@
 // CHECK-NEXT: <unknown>
 // CHECK-NEXT: <const char *>:{{.*}}-DENV_COMPILE_OPTS{{.*}}-vc-codegen
 
-// TODO: Uncomment when build options are properly passed to compile and link
-//       commands for kernel_bundle
-// xCHECK: piProgramCompile(
-// xCHECK-NEXT: <unknown>
-// xCHECK-NEXT: <unknown>
-// xCHECK-NEXT: <unknown>
-// xCHECK-NEXT: <const char *>: -DENV_COMPILE_OPTS -vc-codegen
+// CHECK: piProgramCompile(
+// CHECK-NEXT: <unknown>
+// CHECK-NEXT: <unknown>
+// CHECK-NEXT: <unknown>
+// CHECK-NEXT: <const char *>: -DENV_COMPILE_OPTS -vc-codegen
 
-// xCHECK: piProgramLink(
-// xCHECK-NEXT: <unknown>
-// xCHECK-NEXT: <unknown>
-// xCHECK-NEXT: <unknown>
-// xCHECK-NEXT: <const char *>: -DENV_LINK_OPTS -vc-codegen
+// CHECK: piProgramLink(
+// CHECK-NEXT: <unknown>
+// CHECK-NEXT: <unknown>
+// CHECK-NEXT: <unknown>
+// CHECK-NEXT: <const char *>: -DENV_LINK_OPTS
