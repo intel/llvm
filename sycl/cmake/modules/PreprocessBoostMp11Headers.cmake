1390474a72c827be46e363de990e4aa167967008
Konstantin S Bobrovsky <konstantin.s.bobrovsky@intel.com>
2022-03-16 16:14:58 -0700

    Factor out mp11 cmake code into a separate file, make it work with cmake 3.16.
    Signed-off-by: Konstantin S Bobrovsky <konstantin.s.bobrovsky@intel.com>

--- sycl/cmake/modules/PreprocessBoostMp11Headers.cmake
+++ sycl/cmake/modules/PreprocessBoostMp11Headers.cmake
@@ -4,7 +4,7 @@
 # this transformation moves all APIs from boost namespace to sycl::boost and
 # adds SYCL_ prefix to boost macros names. See more specific commetns in the
 # code below.
-# Variables which must be set by the caller to control behavior of this module: 
+# Variables which must be set by the caller to control behavior of this module:
 # - IN
 #   The source directory with mp11 headers, must contain mp11.h.
 # - OUT
@@ -15,14 +15,9 @@
 # - SRC_ID
 #   Git hash/tag or other ID identifying the original source. Used only in
 #   generated README text.
-
-set(SYCL_DERIVED_COPYRIGHT_NOTICE "\
-// -*- C++ -*-\n\
-//===----------------------------------------------------------------------===//\n\
-// Modifications Copyright Intel Corporation 2022\n\
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception\n\
-//===----------------------------------------------------------------------===//\n\
-// Auto-generated from boost/mp11 sources https://github.com/boostorg/mp11\n\n")
+#
+# Assumed to be invoked as a script:
+# ${CMAKE_COMMAND} -DIN=... -DOUT=... -DSRC_PATH=... -DSRC_ID=... -P <this file>
 
 function(preprocess_mp11_header)
   cmake_parse_arguments(
@@ -56,31 +51,56 @@ function(preprocess_mp11_header)
     "\\1sycl/detail/boost"
     FILE_CONTENTS "${FILE_CONTENTS}")
 
+  set(SYCL_DERIVED_COPYRIGHT_NOTICE "\
+  // -*- C++ -*-\n\
+  //===----------------------------------------------------------------------===//\n\
+  // Modifications Copyright Intel Corporation 2022\n\
+  // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception\n\
+  //===----------------------------------------------------------------------===//\n\
+  // Auto-generated from boost/mp11 sources https://github.com/boostorg/mp11\n\n")
+
+  # 4) add proper copyright notice atop
   string(PREPEND FILE_CONTENTS ${SYCL_DERIVED_COPYRIGHT_NOTICE})
   file(WRITE ${MP11_HDR_DST_NAME} "${FILE_CONTENTS}")
 endfunction(preprocess_mp11_header)
 
-# 1) Perform necessary preprocessing of headers.
-file(GLOB_RECURSE BOOST_MP11_SOURCES "${IN}/*")
+function(preprocess_mp11_headers)
+  cmake_parse_arguments(
+    MP11_HDRS                # prefix
+    ""                       # options
+    "IN;OUT;SRC_PATH;SRC_ID" # one value keywords
+    ""                       # multi-value keywords
+    ${ARGN})                 # arguments
 
-foreach(SRC ${BOOST_MP11_SOURCES})
-  string(REPLACE "${IN}" "${OUT}" DST "${SRC}")
-  preprocess_mp11_header(
-    SRC_NAME ${SRC}
-    DST_NAME ${DST}
-    IN_DIR ${IN}
-  )
-endforeach(SRC ${BOOST_MP11_SOURCES})
+  # 1) Perform necessary preprocessing of headers.
+  file(GLOB_RECURSE BOOST_MP11_SOURCES "${MP11_HDRS_IN}/*")
+  
+  foreach(SRC ${BOOST_MP11_SOURCES})
+    string(REPLACE "${MP11_HDRS_IN}" "${MP11_HDRS_OUT}" DST "${SRC}")
+    preprocess_mp11_header(
+      SRC_NAME ${SRC}
+      DST_NAME ${DST}
+      IN_DIR ${MP11_HDRS_IN}
+    )
+  endforeach(SRC ${BOOST_MP11_SOURCES})
+  
+  # 2) Add SYCL_README.txt to the output directory root
+  set(SYCL_README_TEXT "\
+  This directory contains boost/mp11 headers imported from\n\
+    ${MP11_HDRS_SRC_PATH} (${MP11_HDRS_SRC_ID})\n\
+  and adapted for use in SYCL headers in a way that does not conflict with\n\
+  potential use of boost in user code. Particularly, `BOOST_*` macros are\n\
+  replaced with `SYCL_DETAIL_BOOST_*`, APIs are moved into the top-level
+  `sycl::detail` namespace. For example, `sycl::detail::boost::mp11::mp_list`.\n")
+  
+  set(SYCL_README_FILE_NAME "${MP11_HDRS_OUT}/README.txt")
+  
+  file(WRITE ${SYCL_README_FILE_NAME} "${SYCL_README_TEXT}")
+endfunction(preprocess_mp11_headers)
 
-# 2) Add SYCL_README.txt to the output directory root
-set(SYCL_README_TEXT "\
-This directory contains boost/mp11 headers imported from\n\
-  ${SRC_PATH} (${SRC_ID})\n\
-and adapted for use in SYCL headers in a way that does not conflict with\n\
-potential use of boost in user code. Particularly, `BOOST_*` macros are\n\
-replaced with `SYCL_DETAIL_BOOST_*`, APIs are moved into the top-level
-`sycl::detail` namespace. For example, `sycl::detail::boost::mp11::mp_list`.\n")
-
-set(SYCL_README_FILE_NAME "${OUT}/README.txt")
-
-file(WRITE ${SYCL_README_FILE_NAME} "${SYCL_README_TEXT}")
+preprocess_mp11_headers(
+    IN ${IN}
+    OUT ${OUT}
+    SRC_PATH ${SRC_PATH}
+    SRC_ID ${SRC_ID}
+)
