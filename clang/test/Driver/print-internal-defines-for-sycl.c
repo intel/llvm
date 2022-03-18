69f2323f84ec9addbe47fdd9352811ff75849dba
Qichao Gu <qichao.gu@intel.com>
2022-03-18 00:48:36 -0700

    [Driver] Allow printing defines for sycl when input is stdin
    With normal source file as input, clang is able to print defines for sycl.
    With stdin as input, however, clang fails to do that due to errors from
    the tool append-file, which is because append-file does not allow using
    stdin as input.
    We need to allow clang to print defines for sycl when input is stdin.
    
    Signed-off-by: Qichao Gu <qichao.gu@intel.com>

--- clang/test/Driver/print-internal-defines-for-sycl.c
+++ clang/test/Driver/print-internal-defines-for-sycl.c
@@ -5,3 +5,7 @@
 // RUN: %clangxx -target x86_64-unknown-linux-gnu -fsycl -dM -E -x c++ %s 2>&1 \
 // RUN: | FileCheck --check-prefix CHECK-PRINT-INTERNAL-DEFINES %s
 // CHECK-PRINT-INTERNAL-DEFINES: #define
+
+// Printing defines also work even when input is stdin
+// RUN: %clangxx -target x86_64-unknown-linux-gnu -fsycl -dM -E - < /dev/null 2>&1 \
+// RUN: | FileCheck --check-prefix CHECK-PRINT-INTERNAL-DEFINES %s
