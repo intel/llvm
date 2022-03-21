a4c5443345b7228a1ad27cf532fa2e8db0c0fcac
Alexey Sachkov <alexey.sachkov@intel.com>
2022-03-21 11:50:35 +0300

    Apply comments

--- sycl/test/basic_tests/aspect_usage.cpp
+++ sycl/test/basic_tests/aspect_usage.cpp
@@ -13,12 +13,12 @@
 
 // RUN: %clangxx -fsycl %s -g -emit-llvm -S -o /dev/null 2>&1 | FileCheck %s --check-prefix CHECK-WARNINGS-DBG -DPATH=%s
 
-// CHECK-WARNINGS: warning: function 'func1(int)' uses aspect 'fp16' not listed in `sycl::device_has()`
+// CHECK-WARNINGS: warning: function 'func1(int)' uses aspect 'fp16' not listed in 'sycl::device_has()'
 // CHECK-WARNINGS-NEXT: note: the actual use is in func3(int, int, int), compile with '-g' to get source location
 // CHECK-WARNINGS-NEXT: note: which is called by func2(int, int), compile with '-g' to get source location
 // CHECK-WARNINGS-NEXT: note: which is called by func1(int), compile with '-g' to get source location
 //
-// CHECK-WARNINGS-DBG: warning: function 'func1(int)' uses aspect 'fp16' not listed in `sycl::device_has()`
+// CHECK-WARNINGS-DBG: warning: function 'func1(int)' uses aspect 'fp16' not listed in 'sycl::device_has()'
 // CHECK-WARNINGS-DBG-NEXT: note: the actual use is in func3(int, int, int) at [[PATH]]:36:5
 // CHECK-WARNINGS-DBG-NEXT: note: which is called by func2(int, int) at [[PATH]]:40:34
 // CHECK-WARNINGS-DBG-NEXT: note: which is called by func1(int) at [[PATH]]:42:62
