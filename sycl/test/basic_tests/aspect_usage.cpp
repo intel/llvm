70bf013c6586af4f3c567df4fe3a315d66ac2587
Maksim Sabianin <maksim.sabianin@intel.com>
2022-03-22 12:27:04 +0300

    add test checking ESIMD metadata presence

--- sycl/test/basic_tests/aspect_usage.cpp
+++ sycl/test/basic_tests/aspect_usage.cpp
@@ -19,11 +19,19 @@
 // CHECK-WARNINGS-NEXT: note: which is called by func1(int), compile with '-g' to get source location
 //
 // CHECK-WARNINGS-DBG: warning: function 'func1(int)' uses aspect 'fp16' not listed in 'sycl::device_has()'
-// CHECK-WARNINGS-DBG-NEXT: note: the actual use is in func3(int, int, int) at [[PATH]]:36:5
-// CHECK-WARNINGS-DBG-NEXT: note: which is called by func2(int, int) at [[PATH]]:40:34
-// CHECK-WARNINGS-DBG-NEXT: note: which is called by func1(int) at [[PATH]]:42:62
+// CHECK-WARNINGS-DBG-NEXT: note: the actual use is in func3(int, int, int) at [[PATH]]:44:5
+// CHECK-WARNINGS-DBG-NEXT: note: which is called by func2(int, int) at [[PATH]]:48:34
+// CHECK-WARNINGS-DBG-NEXT: note: which is called by func1(int) at [[PATH]]:50:62
+
+// Check that PropagateAspectUsage pass do not wipes out sycl_explicit_simd metadata.
+// RUN: %clangxx -fsycl %s -emit-llvm -S -o - | FileCheck %s --check-prefix CHECK-ESIMD
+
+// CHECK-ESIMD: @_Z5func4v() {{.*}} !sycl_explicit_simd {{.*}} !intel_used_aspects
+// CHECK-ESIMD: @_ZTSZ4mainEUlT_E0_() {{.*}} !sycl_explicit_simd {{.*}} !intel_used_aspects
 
 #include <CL/sycl.hpp>
+
+#include <sycl/ext/intel/esimd.hpp>
 
 using namespace cl::sycl;
 
@@ -41,7 +49,14 @@ int func2(int a, int b) { return func3(a, b, 1); }
 
 [[sycl::device_has(aspect::fp64)]] int func1(int a) { return func2(a, 1); }
 
+SYCL_ESIMD_KERNEL int func4() {
+  Struct s;
+  s.a = 1;
+  return s.a;
+}
+
 int main() {
   queue Q;
   Q.parallel_for(1, [=](auto i) { int a = func1(1); });
+  Q.parallel_for(1, [=](auto i) SYCL_ESIMD_KERNEL { int a = func4(); });
 }
