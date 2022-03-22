3d1b313fabbd8dabf70460b65102ed3fb22afcce
Alexey Sachkov <alexey.sachkov@intel.com>
2022-03-22 16:34:22 +0300

    Limit changes to __SPIRV* macro only

--- sycl/test/basic_tests/macros.cpp
+++ sycl/test/basic_tests/macros.cpp
@@ -1,12 +1,15 @@
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple -fsycl-device-only -dM -E %s -o %t.device
 // RUN: %clangxx %fsycl-host-only -dM -E %s -o %t.host
 //
+// FIXME: we should also check that we don't leak __SYCL* and SYCL* macro from
+//        our header files.
 // RUN: FileCheck %s < %t.device --check-prefixes=DEVICE,COMMON \
 // RUN:     --implicit-check-not=__SPIRV
 // RUN: FileCheck %s < %t.host --check-prefixes=COMMON \
 // RUN:      --implicit-check-not=__SPIRV
 //
 // COMMON-DAG: #define SYCL_LANGUAGE_VERSION
+// COMMON-DAG: #define __SYCL_COMPILER_VERSION
 //
 // DEVICE-DAG: #define SYCL_EXTERNAL
 // DEVICE-DAG: #define __SYCL_DEVICE_ONLY__
