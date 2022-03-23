intel/llvm

commit 644c61445dd40d01a0d1016e2df66aca161fd464

[SYCL] Add support for device_read_only shared USM allocations (#5832)

sycl/plugins/level_zero/pi_level_zero.cpp
@@ -6835,7 +6835,8 @@ static pi_result USMDeviceAllocImpl(void **ResultPtr, pi_context Context,
   PI_ASSERT(Device, PI_INVALID_DEVICE);
 
   // Check that incorrect bits are not set in the properties.
-  PI_ASSERT(!Properties || (Properties && !(*Properties & ~PI_MEM_ALLOC_FLAGS)),
+  PI_ASSERT(!Properties ||
+                (*Properties == PI_MEM_ALLOC_FLAGS && *(Properties + 2) == 0),
             PI_INVALID_VALUE);
 
   // TODO: translate PI properties to Level Zero flags
@@ -6867,10 +6868,6 @@ static pi_result USMSharedAllocImpl(void **ResultPtr, pi_context Context,
   PI_ASSERT(Context, PI_INVALID_CONTEXT);
   PI_ASSERT(Device, PI_INVALID_DEVICE);
 
-  // Check that incorrect bits are not set in the properties.
-  PI_ASSERT(!Properties || (Properties && !(*Properties & ~PI_MEM_ALLOC_FLAGS)),
-            PI_INVALID_VALUE);
-
   // TODO: translate PI properties to Level Zero flags
   ZeStruct<ze_host_mem_alloc_desc_t> ZeHostDesc;
   ZeHostDesc.flags = 0;
@@ -6901,7 +6898,8 @@ static pi_result USMHostAllocImpl(void **ResultPtr, pi_context Context,
   PI_ASSERT(Context, PI_INVALID_CONTEXT);
 
   // Check that incorrect bits are not set in the properties.
-  PI_ASSERT(!Properties || (Properties && !(*Properties & ~PI_MEM_ALLOC_FLAGS)),
+  PI_ASSERT(!Properties ||
+                (*Properties == PI_MEM_ALLOC_FLAGS && *(Properties + 2) == 0),
             PI_INVALID_VALUE);
 
   // TODO: translate PI properties to Level Zero flags
@@ -6933,6 +6931,13 @@ public:
 
 pi_result USMSharedMemoryAlloc::allocateImpl(void **ResultPtr, size_t Size,
                                              pi_uint32 Alignment) {
+  return USMSharedAllocImpl(ResultPtr, Context, Device, nullptr, Size,
+                            Alignment);
+}
+
+pi_result USMSharedReadOnlyMemoryAlloc::allocateImpl(void **ResultPtr,
+                                                     size_t Size,
+                                                     pi_uint32 Alignment) {
   return USMSharedAllocImpl(ResultPtr, Context, Device, nullptr, Size,
                             Alignment);
 }
@@ -7059,6 +7064,15 @@ pi_result piextUSMSharedAlloc(void **ResultPtr, pi_context Context,
                               pi_device Device,
                               pi_usm_mem_properties *Properties, size_t Size,
                               pi_uint32 Alignment) {
+  // See if the memory is going to be read-only on the device.
+  bool DeviceReadOnly = false;
+  // Check that incorrect bits are not set in the properties.
+  if (Properties) {
+    PI_ASSERT(*(Properties) == PI_MEM_ALLOC_FLAGS && *(Properties + 2) == 0,
+              PI_INVALID_VALUE);
+    DeviceReadOnly = *(Properties + 1) & PI_MEM_ALLOC_DEVICE_READ_ONLY;
+  }
+
   // L0 supports alignment up to 64KB and silently ignores higher values.
   // We flag alignment > 64KB as an invalid value.
   if (Alignment > 65536)
@@ -7096,11 +7110,16 @@ pi_result piextUSMSharedAlloc(void **ResultPtr, pi_context Context,
   }
 
   try {
-    auto It = Context->SharedMemAllocContexts.find(Device);
-    if (It == Context->SharedMemAllocContexts.end())
+    auto &Allocator = (DeviceReadOnly ? Context->SharedReadOnlyMemAllocContexts
+                                      : Context->SharedMemAllocContexts);
+    auto It = Allocator.find(Device);
+    if (It == Allocator.end())
       return PI_INVALID_VALUE;
 
     *ResultPtr = It->second.allocate(Size, Alignment);
+    if (DeviceReadOnly) {
+      Context->SharedReadOnlyAllocs.insert(*ResultPtr);
+    }
     if (IndirectAccessTrackingEnabled) {
       // Keep track of all memory allocations in the context
       Context->MemAllocs.emplace(std::piecewise_construct,
@@ -7229,6 +7248,9 @@ static pi_result USMFreeHelper(pi_context Context, void *Ptr) {
     return PI_SUCCESS;
   }
 
+  // Points out an allocation in SharedReadOnlyMemAllocContexts
+  auto SharedReadOnlyAllocsIterator = Context->SharedReadOnlyAllocs.end();
+
   if (!ZeDeviceHandle) {
     // The only case where it is OK not have device identified is
     // if the memory is not known to the driver. We should not ever get
@@ -7268,7 +7290,12 @@ static pi_result USMFreeHelper(pi_context Context, void *Ptr) {
 
     switch (ZeMemoryAllocationProperties.type) {
     case ZE_MEMORY_TYPE_SHARED:
-      return DeallocationHelper(Context->SharedMemAllocContexts);
+      // Distinguish device_read_only allocations since they have own pool.
+      SharedReadOnlyAllocsIterator = Context->SharedReadOnlyAllocs.find(Ptr);
+      return DeallocationHelper(SharedReadOnlyAllocsIterator !=
+                                        Context->SharedReadOnlyAllocs.end()
+                                    ? Context->SharedReadOnlyMemAllocContexts
+                                    : Context->SharedMemAllocContexts);
     case ZE_MEMORY_TYPE_DEVICE:
       return DeallocationHelper(Context->DeviceMemAllocContexts);
     default:
@@ -7278,7 +7305,9 @@ static pi_result USMFreeHelper(pi_context Context, void *Ptr) {
   }
 
   pi_result Res = USMFreeImpl(Context, Ptr);
-
+  if (SharedReadOnlyAllocsIterator != Context->SharedReadOnlyAllocs.end()) {
+    Context->SharedReadOnlyAllocs.erase(SharedReadOnlyAllocsIterator);
+  }
   if (IndirectAccessTrackingEnabled)
     PI_CALL(ContextReleaseHelper(Context));
   return Res;

sycl/doc/extensions/supported/sycl_ext_oneapi_usm_device_read_only.asciidoc
@@ -1,0 +1,98 @@
+= sycl_ext_oneapi_usm_device_read_only
+
+:source-highlighter: coderay
+:coderay-linenums-mode: table
+
+// This section needs to be after the document title.
+:doctype: book
+:toc2:
+:toc: left
+:encoding: utf-8
+:lang: en
+:dpcpp: pass:[DPC++]
+
+// Set the default source code type in this document to C++,
+// for syntax highlighting purposes.  This is needed because
+// docbook uses c++ and html5 uses cpp.
+:language: {basebackend@docbook:c++:cpp}
+
+
+== Notice
+
+[%hardbreaks]
+Copyright (C) 2022-2022 Intel Corporation.  All rights reserved.
+
+Khronos(R) is a registered trademark and SYCL(TM) and SPIR(TM) are trademarks
+of The Khronos Group Inc.  OpenCL(TM) is a trademark of Apple Inc. used by
+permission by Khronos.
+
+== Contact
+
+To report problems with this extension, please open a new issue at:
+
+https://github.com/intel/llvm/issues
+
+
+== Dependencies
+
+This extension is written against the SYCL 2020 revision 4 specification.  All
+references below to the "core SYCL specification" or to section numbers in the
+SYCL specification refer to that revision.
+
+== Status
+
+This extension is implemented and fully supported by {dpcpp}.
+
+== Overview
+
+Introduce new property notifying the SYCL runtime that the memory being
+allocated is only possibly modified from the host, but read-only in
+all device code.
+
+== Specification
+
+=== Feature test macro
+
+This extension provides a feature-test macro as described in the core SYCL
+specification.  An implementation supporting this extension must predefine
+the macro `SYCL_EXT_ONEAPI_USM_DEVICE_READ_ONLY` to one of the values defined
+in the table below.  Applications can test for the existence of this macro
+to determine if the implementation supports this feature, or applications
+can test the macro's value to determine which of the extension's features
+the implementation supports.
+
+[%header,cols="1,5"]
+|===
+|Value
+|Description
+
+|1
+|Initial version of this extension.
+|===
+
+=== API of the extension
+
+The core SYCL specification currently says this in Section 4.8.3
+"USM allocations":
+
+> This specification does not yet define any USM allocation properties.
+
+This extension adds support for a new property for all USM allocation routines
+taking properties list:
+
+```c++
+namespace sycl::ext::oneapi::property::usm {
+
+  class device_read_only {
+  public:
+    device_read_only() = default;
+  };
+
+} // namespace
+```
+The new property hints the SYCL runtime that the memory being allocated is
+only possibly modified from the host. Any device accesses to the memory
+allocated with this property must be read-only. If device code does write to
+the memory that was allocated with this property, the results
+are undefined. This hint may be used by an implementation to optimize
+allocations and and their access, if possible.

sycl/include/CL/sycl/detail/pi.h
@@ -599,6 +599,15 @@ constexpr pi_mem_properties PI_MEM_PROPERTIES_ALLOC_BUFFER_LOCATION =
 // make the translation to OpenCL transparent.
 using pi_usm_mem_properties = pi_bitfield;
 constexpr pi_usm_mem_properties PI_MEM_ALLOC_FLAGS = CL_MEM_ALLOC_FLAGS_INTEL;
+constexpr pi_usm_mem_properties PI_MEM_ALLOC_WRTITE_COMBINED =
+    CL_MEM_ALLOC_WRITE_COMBINED_INTEL;
+constexpr pi_usm_mem_properties PI_MEM_ALLOC_INITIAL_PLACEMENT_DEVICE =
+    CL_MEM_ALLOC_INITIAL_PLACEMENT_DEVICE_INTEL;
+constexpr pi_usm_mem_properties PI_MEM_ALLOC_INITIAL_PLACEMENT_HOST =
+    CL_MEM_ALLOC_INITIAL_PLACEMENT_HOST_INTEL;
+// Hints that the device/shared allocation will not be written on device.
+constexpr pi_usm_mem_properties PI_MEM_ALLOC_DEVICE_READ_ONLY = (1 << 3);
+
 constexpr pi_usm_mem_properties PI_MEM_USM_ALLOC_BUFFER_LOCATION =
     CL_MEM_ALLOC_BUFFER_LOCATION_INTEL;
 

sycl/include/CL/sycl/detail/property_helper.hpp
@@ -34,8 +34,9 @@ enum DataLessPropKind {
   InitializeToIdentity = 7,
   UseDefaultStream = 8,
   DiscardEvents = 9,
+  DeviceReadOnly = 10,
   // Indicates the last known dataless property.
-  LastKnownDataLessPropKind = 9,
+  LastKnownDataLessPropKind = 10,
   // Exceeding 32 may cause ABI breaking change on some of OSes.
   DataLessPropKindSize = 32
 };

sycl/include/CL/sycl/feature_test.hpp.in
@@ -70,6 +70,7 @@ namespace sycl {
 #define SYCL_EXT_INTEL_USM_ADDRESS_SPACES 1
 #define SYCL_EXT_INTEL_RUNTIME_BUFFER_LOCATION 1
 #define SYCL_EXT_ONEAPI_BACKEND_LEVEL_ZERO 1
+#define SYCL_EXT_ONEAPI_USM_DEVICE_READ_ONLY 1
 #cmakedefine01 SYCL_BUILD_PI_CUDA
 #if SYCL_BUILD_PI_CUDA
 #define SYCL_EXT_ONEAPI_BACKEND_CUDA 1

sycl/include/sycl/ext/intel/experimental/usm_properties.hpp
@@ -7,6 +7,19 @@
 __SYCL_INLINE_NAMESPACE(cl) {
 namespace sycl {
 namespace ext {
+
+namespace oneapi {
+namespace property {
+namespace usm {
+class device_read_only
+    : public sycl::detail::DataLessProperty<sycl::detail::DeviceReadOnly> {
+public:
+  device_read_only() = default;
+};
+} // namespace usm
+} // namespace property
+} // namespace oneapi
+
 namespace intel {
 namespace experimental {
 namespace property {
@@ -28,6 +41,10 @@ private:
 } // namespace experimental
 } // namespace intel
 } // namespace ext
+
+template <>
+struct is_property<ext::oneapi::property::usm::device_read_only>
+    : std::true_type {};
 
 template <>
 struct is_property<ext::intel::experimental::property::usm::buffer_location>

sycl/plugins/level_zero/pi_level_zero.hpp
@@ -352,6 +352,18 @@ public:
       : USMMemoryAllocBase(Ctx, Dev) {}
 };
 
+// Allocation routines for shared memory type that is only modified from host.
+class USMSharedReadOnlyMemoryAlloc : public USMMemoryAllocBase {
+protected:
+  pi_result allocateImpl(void **ResultPtr, size_t Size,
+                         pi_uint32 Alignment) override;
+  MemType getMemTypeImpl() override { return SystemMemory::SharedReadOnly; }
+
+public:
+  USMSharedReadOnlyMemoryAlloc(pi_context Ctx, pi_device Dev)
+      : USMMemoryAllocBase(Ctx, Dev) {}
+};
+
 // Allocation routines for device memory type
 class USMDeviceMemoryAlloc : public USMMemoryAllocBase {
 protected:
@@ -513,6 +525,10 @@ struct _pi_context : _pi_object {
           std::piecewise_construct, std::make_tuple(Device),
           std::make_tuple(std::unique_ptr<SystemMemory>(
               new USMSharedMemoryAlloc(this, Device))));
+      SharedReadOnlyMemAllocContexts.emplace(
+          std::piecewise_construct, std::make_tuple(Device),
+          std::make_tuple(std::unique_ptr<SystemMemory>(
+              new USMSharedReadOnlyMemoryAlloc(this, Device))));
       DeviceMemAllocContexts.emplace(
           std::piecewise_construct, std::make_tuple(Device),
           std::make_tuple(std::unique_ptr<SystemMemory>(
@@ -644,8 +660,15 @@ struct _pi_context : _pi_object {
   // Store USM allocator context(internal allocator structures)
   // for USM shared and device allocations. There is 1 allocator context
   // per each pair of (context, device) per each memory type.
-  std::unordered_map<pi_device, USMAllocContext> SharedMemAllocContexts;
   std::unordered_map<pi_device, USMAllocContext> DeviceMemAllocContexts;
+  std::unordered_map<pi_device, USMAllocContext> SharedMemAllocContexts;
+  std::unordered_map<pi_device, USMAllocContext> SharedReadOnlyMemAllocContexts;
+
+  // Since L0 native runtime does not distinguisg "shared device_read_only"
+  // vs regular "shared" allocations, we have keep track of it to use
+  // proper USMAllocContext when freeing allocations.
+  std::unordered_set<void *> SharedReadOnlyAllocs;
+
   // Store the host allocator context. It does not depend on any device.
   std::unique_ptr<USMAllocContext> HostMemAllocContext;
 

sycl/plugins/level_zero/usm_allocator.cpp
@@ -44,12 +44,23 @@
 
 namespace settings {
 
+constexpr auto operator""_B(unsigned long long x) -> size_t { return x; }
+constexpr auto operator""_KB(unsigned long long x) -> size_t {
+  return x * 1024;
+}
+constexpr auto operator""_MB(unsigned long long x) -> size_t {
+  return x * 1024 * 1024;
+}
+constexpr auto operator""_GB(unsigned long long x) -> size_t {
+  return x * 1024 * 1024 * 1024;
+}
+
 // Buckets for Host use a minimum of the cache line size of 64 bytes.
 // This prevents two separate allocations residing in the same cache line.
 // Buckets for Device and Shared allocations will use starting size of 512.
 // This is because memory compression on newer GPUs makes the
 // minimum granularity 512 bytes instead of 64.
-static constexpr size_t MinBucketSize[3] = {64, 512, 512};
+static constexpr size_t MinBucketSize[SystemMemory::All] = {64, 512, 512, 512};
 
 // The largest size which is allocated via the allocator.
 // Allocations with size > CutOff bypass the USM allocator and
@@ -63,20 +74,19 @@ static class SetLimits {
 public:
   // Minimum allocation size that will be requested from the system.
   // By default this is the minimum allocation size of each memory type.
-  // Memory types are host, device, shared.
-  size_t SlabMinSize[3] = {64 * 1024, 64 * 1024, 2 * 1024 * 1024};
+  size_t SlabMinSize[SystemMemory::All] = {};
 
   // Allocations up to this limit will be subject to chunking/pooling
-  size_t MaxPoolableSize[3] = {2 * 1024 * 1024, 4 * 1024 * 1024, 0};
+  size_t MaxPoolableSize[SystemMemory::All] = {};
 
   // When pooling, each bucket will hold a max of 4 unfreed slabs
-  size_t Capacity[3] = {4, 4, 0};
+  size_t Capacity[SystemMemory::All] = {};
 
   // Maximum memory left unfreed in pool
-  size_t MaxPoolSize = 16 * 1024 * 1024;
+  size_t MaxPoolSize = 16_MB;
 
   size_t CurPoolSize = 0;
-  size_t CurPoolSizes[3] = {0, 0, 0};
+  size_t CurPoolSizes[SystemMemory::All] = {0, 0, 0, 0};
 
   size_t EnableBuffers = 1;
 
@@ -84,6 +94,25 @@ public:
   int PoolTrace = 0;
 
   SetLimits() {
+    // Initialize default pool settings.
+    MaxPoolableSize[SystemMemory::Host] = 2_MB;
+    Capacity[SystemMemory::Host] = 4;
+    SlabMinSize[SystemMemory::Host] = 64_KB;
+
+    MaxPoolableSize[SystemMemory::Device] = 4_MB;
+    Capacity[SystemMemory::Device] = 4;
+    SlabMinSize[SystemMemory::Device] = 64_KB;
+
+    // Disable pooling of shared USM allocations.
+    MaxPoolableSize[SystemMemory::Shared] = 0;
+    Capacity[SystemMemory::Shared] = 0;
+    SlabMinSize[SystemMemory::Shared] = 2_MB;
+
+    // Allow pooling of shared allocations that are only modified on host.
+    MaxPoolableSize[SystemMemory::SharedReadOnly] = 4_MB;
+    Capacity[SystemMemory::SharedReadOnly] = 4;
+    SlabMinSize[SystemMemory::SharedReadOnly] = 64_KB;
+
     // Parse optional parameters of this form:
     // SYCL_PI_LEVEL_ZERO_USM_ALLOCATOR=[EnableBuffers][;[MaxPoolSize][;memtypelimits]...]
     //  memtypelimits: [<memtype>:]<limits>
@@ -113,15 +142,15 @@ public:
       size_t Multiplier = 1;
       if (tolower(Param[Length - 1]) == 'k') {
         Length--;
-        Multiplier = 1024;
+        Multiplier = 1_KB;
       }
       if (tolower(Param[Length - 1]) == 'm') {
         Length--;
-        Multiplier = 1024 * 1024;
+        Multiplier = 1_MB;
       }
       if (tolower(Param[Length - 1]) == 'g') {
         Length--;
-        Multiplier = 1024 * 1024 * 1024;
+        Multiplier = 1_GB;
       }
       std::string TheNumber = Param.substr(0, Length);
       if (TheNumber.find_first_not_of("0123456789") == std::string::npos)
@@ -270,7 +299,8 @@ public:
 
 using namespace settings;
 
-static const char *MemTypeNames[3] = {"Host", "Device", "Shared"};
+static const char *MemTypeNames[SystemMemory::All] = {
+    "Host", "Device", "Shared", "SharedReadOnly"};
 
 // Aligns the pointer down to the specified alignment
 // (e.g. returns 8 for Size = 13, Alignment = 8)
@@ -547,7 +577,7 @@ size_t Slab::FindFirstAvailableChunkIdx() const {
 }
 
 void *Slab::getChunk() {
-  assert(NumAllocated != Chunks.size());
+  // assert(NumAllocated != Chunks.size());
 
   const size_t ChunkIdx = FindFirstAvailableChunkIdx();
   // Free chunk must exist, otherwise we would have allocated another slab

commit 3938c3533f54591841965f67797a3d38796ea7d1

[NFC][SYCL][L0] Fix a warning about unused parameter (#5868)
This PR fixed error `error: unused parameter 'Properties' [-Werror,-Wunused-parameter]` in `sycl/plugins/level_zero/pi_level_zero.cpp` caused by https://github.com/intel/llvm/commit/644c61445dd40d01a0d1016e2df66aca161fd464

sycl/plugins/level_zero/pi_level_zero.cpp
@@ -6865,6 +6865,7 @@ static pi_result USMSharedAllocImpl(void **ResultPtr, pi_context Context,
                                     pi_device Device,
                                     pi_usm_mem_properties *Properties,
                                     size_t Size, pi_uint32 Alignment) {
+  (void)Properties;
   PI_ASSERT(Context, PI_INVALID_CONTEXT);
   PI_ASSERT(Device, PI_INVALID_DEVICE);
 