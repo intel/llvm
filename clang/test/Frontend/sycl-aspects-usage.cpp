13745b90908034e0dc5819724d4b019dca80253e
Maksim Sabianin <maksim.sabianin@intel.com>
2022-03-25 13:16:45 +0300

    fix Frontend/sycl-aspects-usage.cpp test

--- clang/test/Frontend/sycl-aspects-usage.cpp
+++ clang/test/Frontend/sycl-aspects-usage.cpp
@@ -2,7 +2,7 @@
 
 // RUN: %clang_cc1 -fsycl-is-device -triple spir64-unknown-unknown -emit-llvm %s -o /dev/null 2>&1 | FileCheck %s --check-prefix CHECK-WARNINGS
 
-// RUNx: %clang_cc1 -fsycl-is-device -triple spir64-unknown-unknown %s -debug-info-kind=constructor -dwarf-version=5 -emit-llvm -o /dev/null 2>&1 | FileCheck %s --check-prefix CHECK-WARNINGS-DBG -DPATH=%s
+// RUN: %clang_cc1 -fsycl-is-device -triple spir64-unknown-unknown %s -debug-info-kind=constructor -dwarf-version=5 -emit-llvm -o /dev/null 2>&1 | FileCheck %s --check-prefix CHECK-WARNINGS-DBG -DPATH=%s
 
 // CHECK-WARNINGS: warning: function 'func1(int)' uses aspect 'fp16' not listed in 'sycl::device_has()'
 // CHECK-WARNINGS-NEXT: note: the actual use is in func3(int, int, int), compile with '-g' to get source location
@@ -10,9 +10,9 @@
 // CHECK-WARNINGS-NEXT: note: which is called by func1(int), compile with '-g' to get source location
 //
 // CHECK-WARNINGS-DBG: warning: function 'func1(int)' uses aspect 'fp16' not listed in 'sycl::device_has()'
-// CHECK-WARNINGS-DBG-NEXT: note: the actual use is in func3(int, int, int) at [[PATH]]:36:5
-// CHECK-WARNINGS-DBG-NEXT: note: which is called by func2(int, int) at [[PATH]]:40:34
-// CHECK-WARNINGS-DBG-NEXT: note: which is called by func1(int) at [[PATH]]:42:62
+// CHECK-WARNINGS-DBG-NEXT: note: the actual use is in func3(int, int, int) at [[PATH]]:29:5
+// CHECK-WARNINGS-DBG-NEXT: note: which is called by func2(int, int) at [[PATH]]:33:34
+// CHECK-WARNINGS-DBG-NEXT: note: which is called by func1(int) at [[PATH]]:35:62
 
 #include "Inputs/sycl.hpp"
 
