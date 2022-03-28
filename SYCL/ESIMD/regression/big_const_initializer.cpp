6786ceed4b210cff7b6e550338f684bcb51bb366
dongkyunahn-intel <dongkyun.ahn@intel.com>
2022-03-28 08:18:52 -0700

    [SYCL][ESIMD][EMU]Removing XFAIL for tests enabled with PI_API debug (intel/llvm-test-suite#937)

--- SYCL/ESIMD/regression/big_const_initializer.cpp
+++ SYCL/ESIMD/regression/big_const_initializer.cpp
@@ -7,8 +7,6 @@
 //===----------------------------------------------------------------------===//
 // REQUIRES: gpu
 // UNSUPPORTED: cuda || hip
-// TODO: esimd_emulator fails due to unimplemented __esimd_scatter_scaled
-// XFAIL: esimd_emulator
 // RUN: %clangxx -fsycl -I%S/.. %s -o %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 
