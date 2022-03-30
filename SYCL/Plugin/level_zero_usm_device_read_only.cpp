35834fcc677774fa63b385d3b7f1a591cb62011a
smaslov-intel <sergey.v.maslov@intel.com>
2022-03-30 11:46:53 -0600

    [SYCL] Add support for device_read_only shared USM allocations (intel/llvm-test-suite#925)
    Signed-off-by: Sergey V Maslov <sergey.v.maslov@intel.com>

---
+++ SYCL/Plugin/level_zero_usm_device_read_only.cpp
@@ -0,0 +1,29 @@
+// REQUIRES: gpu, level_zero
+
+// RUN: %clangxx -fsycl -fsycl-unnamed-lambda -fsycl-targets=%sycl_triple %level_zero_options %s -o %t.out
+// RUN: env SYCL_PI_TRACE=2 ZE_DEBUG=1 %GPU_RUN_PLACEHOLDER %t.out 2>&1 %GPU_CHECK_PLACEHOLDER
+
+// Test that "device_read_only" shared USM allocations are pooled.
+
+#include <CL/sycl.hpp>
+
+using namespace std;
+using namespace sycl;
+
+int main(int argc, char *argv[]) {
+  queue Q;
+
+  auto ptr1 =
+      malloc_shared<int>(1, Q, ext::oneapi::property::usm::device_read_only());
+  // CHECK: ---> piextUSMSharedAlloc
+  // CHECK: ZE ---> zeMemAllocShared
+
+  auto ptr2 = aligned_alloc_shared<int>(
+      1, 1, Q, ext::oneapi::property::usm::device_read_only());
+  // CHECK: ---> piextUSMSharedAlloc
+  // CHECK-NOT: ZE ---> zeMemAllocShared
+
+  free(ptr1, Q);
+  free(ptr2, Q);
+  return 0;
+}
