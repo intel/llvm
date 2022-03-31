03512c6c3394342c859e32eb7406050fd7b338ae
kbobrovs <Konstantin.S.Bobrovsky@intel.com>
2022-03-30 22:28:37 -0700

    Disable Assert/assert_in_simultaneously_multiple_tus.cpp (intel/llvm-test-suite#959)
    * Disable Assert/assert_in_simultaneously_multiple_tus.cpp due to a failure on unrelated change

--- SYCL/Assert/assert_in_simultaneously_multiple_tus.cpp
+++ SYCL/Assert/assert_in_simultaneously_multiple_tus.cpp
@@ -1,5 +1,9 @@
 // FIXME unsupported on CUDA and HIP until fallback libdevice becomes available
 // UNSUPPORTED: cuda || hip
+// clang-format off
+// Failed on Linux on unrelated change (FileCheck error: '.../assert_in_simultaneously_multiple_tus.cpp.tmp.txt' is empty)
+// clang-format on
+// REQUIRES: TEMPORARILY_DISABLED
 // RUN: %clangxx -DSYCL_FALLBACK_ASSERT=1 -fsycl -fsycl-targets=%sycl_triple -I %S/Inputs %s %S/Inputs/kernels_in_file2.cpp -o %t.out %threads_lib
 // RUN: %CPU_RUN_PLACEHOLDER %t.out &> %t.txt || true
 // RUN: %CPU_RUN_PLACEHOLDER FileCheck %s --input-file %t.txt
