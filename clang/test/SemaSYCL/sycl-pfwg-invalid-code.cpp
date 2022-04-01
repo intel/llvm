beb49dbf0a7628e0b42f4a1d354d7777f15cb70b
Premanand M Rao <premanand.m.rao@intel.com>
2022-04-01 12:25:35 -0700

    Fix clang-format error (2)

--- clang/test/SemaSYCL/sycl-pfwg-invalid-code.cpp
+++ clang/test/SemaSYCL/sycl-pfwg-invalid-code.cpp
@@ -1,12 +1,11 @@
 // RUN: %clang_cc1 -fsycl-is-device %s -verify
 
 // Tests that an error diagnostic is issued without a crash (due to a
-// triggered assertion) when some invalid cases are encountered in 
+// triggered assertion) when some invalid cases are encountered in
 // the processing of kernel_parallel_for_work_group.
 
-template <typename, typename, typename Kernel>
-__attribute__((sycl_kernel))
-void kernel_parallel_for_work_group(const Kernel&) {
+template <typename, typename, typename K>
+__attribute__((sycl_kernel)) void kernel_parallel_for_work_group(const K &) {
   unknown(); // expected-error{{use of undeclared identifier 'unknown'}}
 }
 void foo() {
