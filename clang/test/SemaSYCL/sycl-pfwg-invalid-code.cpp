f5d3f4f7d43ccb4bf3c74fe9e690c4a5e64549d6
Premanand M Rao <premanand.m.rao@intel.com>
2022-04-04 08:00:10 -0700

    Address code-review comments

--- clang/test/SemaSYCL/sycl-pfwg-invalid-code.cpp
+++ clang/test/SemaSYCL/sycl-pfwg-invalid-code.cpp
@@ -1,9 +1,7 @@
 // RUN: %clang_cc1 -fsycl-is-device %s -verify
 
-// Tests that an error diagnostic is issued without a crash (due to a
-// triggered assertion) when some invalid cases are encountered in
-// the processing of kernel_parallel_for_work_group.
-
+// Tests that the compiler does not crash (due to a triggered assertion)
+// if definition of kernel_parallel_for_work_group is invalid.
 template <typename, typename, typename K>
 __attribute__((sycl_kernel)) void kernel_parallel_for_work_group(const K &) {
   unknown(); // expected-error{{use of undeclared identifier 'unknown'}}
