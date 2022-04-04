cf668e918290f81b66a9c75fc397df278e8a0101
Maksim Sabianin <maksim.sabianin@intel.com>
2022-04-04 12:27:09 +0300

    fix checkSeveralAspects test

--- clang/test/Frontend/sycl-aspects-usage.cpp
+++ clang/test/Frontend/sycl-aspects-usage.cpp
@@ -5,10 +5,12 @@
 // RUN: %clang_cc1 -fsycl-is-device -triple spir64-unknown-unknown %s -debug-info-kind=constructor -dwarf-version=5 -emit-llvm -o /dev/null 2>&1 | FileCheck %s --check-prefix CHECK-WARNINGS-DBG -DPATH=%s
 
 // CHECK-WARNINGS: warning: function 'checkStructUsesAspect(int)' uses aspect 'fp16' not listed in 'sycl::device_has()'
-// CHECK-WARNINGS-NEXT: note: the actual use is in checkStructUsesAspect(int), compile with '-g' to get source location
+// CHECK-WARNINGS-NEXT: note: the actual use is in funcWithStruct(int), compile with '-g' to get source location
+// CHECK-WARNINGS-NEXT: note: which is called by checkStructUsesAspect(int), compile with '-g' to get source location
 //
 // CHECK-WARNINGS-DBG: warning: function 'checkStructUsesAspect(int)' uses aspect 'fp16' not listed in 'sycl::device_has()'
-// CHECK-WARNINGS-DBG-NEXT: note: the actual use is in checkStructUsesAspect(int) at [[PATH]]:25:5
+// CHECK-WARNINGS-DBG-NEXT: note: the actual use is in funcWithStruct(int) at [[PATH]]:27:5
+// CHECK-WARNINGS-DBG-NEXT: note: which is called by checkStructUsesAspect(int) at [[PATH]]:32:10
 
 #include "Inputs/sycl.hpp"
 
@@ -20,12 +22,18 @@ struct [[__sycl_detail__::__uses_aspects__(aspect::fp16)]] Struct {
   int a = 0;
 };
 
-[[sycl::device_has(aspect::fp64)]] int checkStructUsesAspect(int) {
+int funcWithStruct(int) {
   Struct s;
   s.a = 1;
   return s.a;
 }
 
+[[sycl::device_has(aspect::fp64)]] int checkStructUsesAspect(int a) {
+  return funcWithStruct(a);
+}
+
+// RUN: %clang_cc1 -fsycl-is-device -triple spir64-unknown-unknown -emit-llvm %s -o /dev/null 2>&1 | FileCheck %s --check-prefix CHECK-EMPTY
+// CHECK-EMPTY-NOT: checkEmptyDiveceHas
 [[sycl::device_has()]] int checkEmptyDeviceHas() {
   return 0;
 }
@@ -43,14 +51,31 @@ struct [[__sycl_detail__::__uses_aspects__(aspect::fp16)]] Struct {
   return d;
 }
 
-double funcWithSeveralAspects() {
-  Struct s;
-  return static_cast<double>(s.a);
+struct [[__sycl_detail__::__uses_aspects__(aspect::cpu)]] StructWithCpu {
+  int a = 0;
+};
+
+int funcWithSeveralAspects() {
+  Struct s1;
+  StructWithCpu s2;
+  return static_cast<double>(s1.a);
 }
 
 // Check that a warning diagnostic works in a case
-// when there are several aspects present and a part
-// of them conflicts with declared in device_has().
+// when there are several aspects present which conflict
+// with declared in device_has().
+
+// Check for fp64 aspect
+// RUN: %clang_cc1 -fsycl-is-device -triple spir64-unknown-unknown -emit-llvm %s -o /dev/null 2>&1 | FileCheck %s --check-prefix CHECK-SEVERAL-FP64
+// CHECK-SEVERAL-FP64: warning: function 'checkSeveralAspects()' uses aspect 'fp64' not listed in 'sycl::device_has()'
+// CHECK-SEVERAL-FP64-NEXT: note: the actual use is in funcWithSeveralAspects(), compile with '-g' to get source location
+// CHECK-SEVERAL-FP64-NEXT: note: which is called by checkSeveralAspects(), compile with '-g' to get source location
+//
+// Check for cpu aspect
+// RUN: %clang_cc1 -fsycl-is-device -triple spir64-unknown-unknown -emit-llvm %s -o /dev/null 2>&1 | FileCheck %s --check-prefix CHECK-SEVERAL-CPU
+// CHECK-SEVERAL-CPU: warning: function 'checkSeveralAspects()' uses aspect 'cpu' not listed in 'sycl::device_has()'
+// CHECK-SEVERAL-CPU-NEXT: note: the actual use is in funcWithSeveralAspects(), compile with '-g' to get source location
+// CHECK-SEVERAL-CPU-NEXT: note: which is called by checkSeveralAspects(), compile with '-g' to get source location
 [[sycl::device_has(aspect::fp16)]] int checkSeveralAspects() {
   return funcWithSeveralAspects();
 }
