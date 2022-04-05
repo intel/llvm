42feae69af65ff0cedd9c230d78b3dd9b0e911ba
Bing1 Yu <bing1.yu@intel.com>
2022-04-06 01:29:11 +0800

    small fix again

--- sycl/include/sycl/ext/oneapi/matrix/static-query-use.hpp
+++ sycl/include/sycl/ext/oneapi/matrix/static-query-use.hpp
@@ -1,4 +1,4 @@
-//===-------------- static-query.hpp - SYCL matrix ------------*- C++ -*---===//
+//===---------- static-query-use.hpp - SYCL matrix ------------*- C++ -*---===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
