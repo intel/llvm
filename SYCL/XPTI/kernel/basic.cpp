5a03ed673d09a69e6a873716bb46766254d8af98
vladimirlaz <vladimir.lazarev@intel.com>
2022-04-12 03:32:13 +0300

    [SYCL][XPTI] Added test covering kernel info  (#949)
     - fix BE selection for buffer tests
     - kernel execution parameter (global size, local size, offset)
     - kernel argument parameters (number, size, pointer which can be
    associated with accessor/USM buffer)

---
+++ SYCL/XPTI/kernel/basic.cpp
@@ -0,0 +1,104 @@
+// REQUIRES: xptifw, opencl
+// RUN: %clangxx %s -DXPTI_COLLECTOR -DXPTI_CALLBACK_API_EXPORTS %xptifw_lib %shared_lib %fPIC %cxx_std_optionc++17 -o %t_collector.dll
+// RUN: %clangxx -fsycl %s -o %t.out
+// RUN: env XPTI_TRACE_ENABLE=1 XPTI_FRAMEWORK_DISPATCHER=%xptifw_dispatcher XPTI_SUBSCRIBERS=%t_collector.dll %BE_RUN_PLACEHOLDER %t.out | FileCheck %s 2>&1
+
+#ifdef XPTI_COLLECTOR
+
+#include "../Inputs/buffer_info_collector.cpp"
+
+#else
+
+#include <sycl/sycl.hpp>
+
+using namespace sycl::access;
+constexpr sycl::specialization_id<int> int_id(42);
+
+class Functor1 {
+public:
+  Functor1(short X_,
+           cl::sycl::accessor<int, 1, mode::read_write, target::device> &Acc_)
+      : X(X_), Acc(Acc_) {}
+
+  void operator()() const { Acc[0] += X; }
+
+private:
+  short X;
+  cl::sycl::accessor<int, 1, mode::read_write, target::device> Acc;
+};
+
+class Functor2 {
+public:
+  Functor2(short X_,
+           cl::sycl::accessor<int, 1, mode::read_write, target::device> &Acc_)
+      : X(X_), Acc(Acc_) {}
+
+  void operator()(sycl::id<1> id = 0) const { Acc[id] += X; }
+
+private:
+  short X;
+  cl::sycl::accessor<int, 1, mode::read_write, target::device> Acc;
+};
+
+int main() {
+  bool MismatchFound = false;
+  sycl::queue Queue{};
+
+  // CHECK:{{[0-9]+}}|Create buffer|[[BUFFERID:[0-9,a-f,x]+]]|0x0|{{i(nt)*}}|4|1|{5,0,0}|{{.*}}.cpp:[[# @LINE + 1]]:24
+  sycl::buffer<int, 1> Buf(5);
+  sycl::range<1> Range{Buf.size()};
+  short Val = Buf.size();
+  auto PtrDevice = sycl::malloc_device<int>(7, Queue);
+  auto PtrShared = sycl::malloc_shared<int>(8, Queue);
+  Queue
+      .submit([&](sycl::handler &cgh) {
+        // CHECK: {{[0-9]+}}|Construct accessor|[[BUFFERID]]|[[ACCID1:.+]]|2014|1026|{{.*}}.cpp:[[# @LINE + 1]]:19
+        auto A1 = Buf.get_access<mode::read_write>(cgh);
+        // CHECK: {{[0-9]+}}|Construct accessor|0x0|[[ACCID2:.*]]|2016|1026|{{.*}}.cpp:[[# @LINE + 1]]:65
+        sycl::accessor<int, 1, mode::read_write, target::local> A2(Range, cgh);
+        // CHECK:Node create|{{.*}}FillBuffer{{.*}}|{{.*}}.cpp:[[# @LINE - 6 ]]:3|{5, 1, 1}, {0, 0, 0}, {0, 0, 0}, 6
+        cgh.parallel_for<class FillBuffer>(
+            Range, [=](sycl::id<1> WIid, sycl::kernel_handler kh) {
+              // CHECK: arg0 : {1, {{[0-9,a-f,x]+}}, 2, 0}
+              int h = Val;
+              // CHECK: arg1 : {1, {{.*}}0, 20, 1}
+              A2[WIid[0]] = h;
+              // CHECK: arg2 : {0, [[ACCID1]], 4062, 2}
+              // CHECK: arg3 : {1, [[ACCID1]], 8, 3}
+              A1[WIid[0]] = A2[WIid[0]];
+              // CHECK: arg4 : {3, {{.*}}, 8, 4}
+              PtrDevice[WIid[0]] = WIid[0];
+              // CHECK: arg5 : {3, {{.*}}, 8, 5}
+              PtrShared[WIid[0]] = PtrDevice[WIid[0]];
+            });
+      })
+      .wait();
+
+  // CHECK: {{[0-9]+}}|Construct accessor|[[BUFFERID]]|[[ACCID3:.*]]|2018|1024|{{.*}}.cpp:[[# @LINE + 1]]:15
+  { auto HA = Buf.get_access<mode::read>(); }
+
+  Queue.submit([&](cl::sycl::handler &cgh) {
+    // CHECK: {{[0-9]+}}|Construct accessor|[[BUFFERID]]|[[ACCID4:.+]]|2014|1026|{{.*}}.cpp:[[# @LINE + 1]]:16
+    auto Acc = Buf.get_access<mode::read_write>(cgh);
+    Functor1 F(Val, Acc);
+    // CHECK: Node create|{{.*}}Functor1|{{.*}}.cpp:[[# @LINE - 4 ]]:3|{1, 1, 1}, {0, 0, 0}, {0, 0, 0}, 3
+    cgh.single_task(F);
+    // CHECK: arg0 : {1, {{[0-9,a-f,x]+}}, 2, 0}
+    // CHECK: arg1 : {0, [[ACCID4]], 4062, 1}
+    // CHECK: arg2 : {1, [[ACCID4]], 8, 2}
+  });
+
+  Queue.submit([&](cl::sycl::handler &cgh) {
+    // CHECK: {{[0-9]+}}|Construct accessor|[[BUFFERID]]|[[ACCID5:.+]]|2014|1026|{{.*}}.cpp:[[# @LINE + 1]]:16
+    auto Acc = Buf.get_access<mode::read_write>(cgh);
+    Functor2 F(Val, Acc);
+    // CHECK: Node create|{{.*}}Functor2|{{.*}}.cpp:[[# @LINE - 4 ]]:3|{5, 1, 1}, {0, 0, 0}, {0, 0, 0}, 3
+    cgh.parallel_for(Range, F);
+    // CHECK: arg0 : {1, {{[0-9,a-f,x]+}}, 2, 0}
+    // CHECK: arg1 : {0, [[ACCID5]], 4062, 1}
+    // CHECK: arg2 : {1, [[ACCID5]], 8, 2}
+  });
+
+  return 0;
+}
+#endif
---
+++ SYCL/XPTI/kernel/content.cpp
@@ -0,0 +1,56 @@
+// REQUIRES: xptifw, opencl
+// RUN: %clangxx %s -DXPTI_COLLECTOR -DXPTI_CALLBACK_API_EXPORTS %xptifw_lib %shared_lib %fPIC %cxx_std_optionc++17 -o %t_collector.dll
+// RUN: %clangxx -fsycl %s -o %t.out
+// RUN: env XPTI_TRACE_ENABLE=1 XPTI_FRAMEWORK_DISPATCHER=%xptifw_dispatcher XPTI_SUBSCRIBERS=%t_collector.dll %BE_RUN_PLACEHOLDER %t.out | FileCheck %s 2>&1
+
+#ifdef XPTI_COLLECTOR
+
+#include "../Inputs/buffer_info_collector.cpp"
+
+#else
+#include <array>
+#include <numeric>
+#include <sycl/sycl.hpp>
+
+using namespace sycl;
+int main() {
+  std::array<int, 1024> input;
+  std::iota(input.begin(), input.end(), 0);
+  buffer<int> valuesBuf{input};
+
+  // Buffers with just 1 element to get the reduction results
+  int sumResult = 0;
+  buffer<int> sumBuf{&sumResult, 1};
+  queue myQueue;
+  myQueue.submit([&](handler &cgh) {
+    auto inputValues = valuesBuf.get_access<access_mode::read>(cgh);
+
+    auto sumR = reduction(sumBuf, cgh, plus<>());
+    // Reduction kernel is used
+    // CHECK:Node create|{{.*}}reduction{{.*}}test1{{.*}}|{{.*}}.cpp:[[# @LINE - 5 ]]:3|{1024, 1, 1}, {{{.*}}, 1, 1}, {0, 0, 0}, 5
+    cgh.parallel_for<class test1>(
+        range<1>{1024}, sumR,
+        [=](id<1> idx, auto &sum) { sum += inputValues[idx]; });
+  });
+
+  // sumBuf contains the reduction results once the kernel completes
+  assert(sumBuf.get_host_access()[0] == 523776);
+
+  {
+    buffer<int> in_buf(input.data(), input.size());
+    buffer<int> out_buf(input.size());
+    myQueue.submit([&](handler &cgh) {
+      auto in = in_buf.template get_access<access::mode::read>(cgh);
+      auto out = out_buf.template get_access<access::mode::read_write>(cgh);
+      // CHECK:Node create|{{.*}}test2{{.*}}|{{.*}}.cpp:[[# @LINE - 3 ]]:5|{128, 4, 2}, {32, 2, 1}, {16, 1, 0}, 2
+      cgh.parallel_for<class test2>(
+          nd_range<3>({128, 4, 2}, {32, 2, 1}, {16, 1, 0}), [=](nd_item<3> it) {
+            auto sg = it.get_sub_group();
+            joint_exclusive_scan(sg, in.get_pointer(),
+                                 in.get_pointer() + sg.get_local_id(),
+                                 out.get_pointer(), std::plus<>{});
+          });
+    });
+  }
+}
+#endif
