5450d77cbf8ec875fee4a4e8e8f898c166a6a5c2
Chris Perkins <chris.perkins@intel.com>
2022-04-21 11:54:08 -0700

    control comment

--- sycl/source/detail/buffer_impl.cpp
+++ sycl/source/detail/buffer_impl.cpp
@@ -38,7 +38,7 @@ void buffer_impl::constructorNotification(const detail::code_location &CodeLoc,
   XPTIRegistry::bufferConstructorNotification(UserObj, CodeLoc, HostObj, Type,
                                               Dim, ElemSize, Range);
 }
-// TODO: remove once ABI break is allowed
+// TODO: remove once ABI breaks is allowed
 void buffer_impl::constructorNotification(const detail::code_location &CodeLoc,
                                           void *UserObj) {
   size_t r[3] = {0, 0, 0};
