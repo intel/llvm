1f5ac33b45c23ec9f3bdc7e6c4cdc116c8754444
Chris Perkins <chris.perkins@intel.com>
2022-04-26 11:09:53 -0700

    echo msg

--- devops/scripts/install_drivers.sh
+++ devops/scripts/install_drivers.sh
@@ -70,7 +70,7 @@ InstallCPURT () {
   else
     echo  $INSTALL_LOCATION/oclcpu/x64/libintelocl.so > /etc/OpenCL/vendors/intel_oclcpu.icd
   fi
-  # how's it hanging?
+  echo "how's it hanging?"
   ldd $INSTALL_LOCATION/oclcpu/x64/libintelocl.so
 }
 
