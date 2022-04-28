3465461f50071c61566d26260df7aa054c19d146
Andrei Elovikov <andrei.elovikov@intel.com>
2022-04-28 12:38:24 -0700

    Add RUN lines with -disable-llvm-passes

--- clang/test/CodeGenSYCL/dead-elim-sycl.cpp
+++ clang/test/CodeGenSYCL/dead-elim-sycl.cpp
@@ -1,7 +1,9 @@
 // RUN: %clang -cc1 -triple spir64-unknown-unknown  -fsycl-is-device  -emit-llvm-bc  -O1 -fno-legacy-pass-manager -fdebug-pass-manager -fenable-sycl-dae -o /dev/null -x c++ < %s 2>&1 | FileCheck %s
 // RUN: %clang -cc1 -triple spir64-unknown-unknown  -fsycl-is-device  -emit-llvm-bc  -O0 -fno-legacy-pass-manager -fdebug-pass-manager  -o /dev/null -x c++ < %s 2>&1 | FileCheck %s --check-prefix DISABLE
+// RUN: %clang -cc1 -triple spir64-unknown-unknown  -fsycl-is-device  -emit-llvm-bc  -O1 -fno-legacy-pass-manager -fdebug-pass-manager -fenable-sycl-dae -disable-llvm-passes -o /dev/null -x c++ < %s 2>&1 | FileCheck %s --check-prefix DISABLE
 // RUN: %clang -cc1 -triple spir64-unknown-unknown  -fsycl-is-device  -emit-llvm-bc  -O1 -flegacy-pass-manager -mllvm -debug-pass=Structure -fenable-sycl-dae -o /dev/null -x c++ < %s 2>&1 | FileCheck %s --check-prefix OLDPM
 // RUN: %clang -cc1 -triple spir64-unknown-unknown  -fsycl-is-device  -emit-llvm-bc  -O1 -flegacy-pass-manager -mllvm -debug-pass=Structure -o /dev/null -x c++ < %s 2>&1 | FileCheck %s --check-prefix DISABLE
+// RUN: %clang -cc1 -triple spir64-unknown-unknown  -fsycl-is-device  -emit-llvm-bc  -O1 -flegacy-pass-manager -mllvm -debug-pass=Structure -fenable-sycl-dae -disable-llvm-passes -o /dev/null -x c++ < %s 2>&1 | FileCheck %s --check-prefix DISABLE
 
 // Verify that Dead Arguments Elimination for SYCL kernels is/is not added to the PM.
 
