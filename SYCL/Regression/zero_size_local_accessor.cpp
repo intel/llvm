ac015da69b66ee4f1a855b295d5eaa0ae620be27
Steffen Larsen <steffen.larsen@intel.com>
2022-04-29 16:15:41 +0100

    [SYCL] Add regression test for 0-size local accessors (intel/llvm-test-suite#1001)
    This tests https://github.com/intel/llvm/pull/6032.
    
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

---
+++ SYCL/Regression/zero_size_local_accessor.cpp
@@ -0,0 +1,29 @@
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
+// RUN: %HOST_RUN_PLACEHOLDER %t.out
+// RUN: %CPU_RUN_PLACEHOLDER %t.out
+// RUN: %GPU_RUN_PLACEHOLDER %t.out
+// RUN: %ACC_RUN_PLACEHOLDER %t.out
+
+//==---- zero_size_local_accessor.cpp - SYCL 0-size local accessor test ----==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include <CL/sycl.hpp>
+
+int main() {
+  sycl::queue Q;
+  Q.submit([&](sycl::handler &CGH) {
+     sycl::accessor<uint8_t, 1, sycl::access_mode::read_write,
+                    sycl::access::target::local>
+         ZeroSizeLocalAcc(sycl::range<1>(0), CGH);
+     CGH.single_task([=]() {
+       if (ZeroSizeLocalAcc.get_range()[0])
+         ZeroSizeLocalAcc[0] = 1;
+     });
+   }).wait();
+  return 0;
+}
