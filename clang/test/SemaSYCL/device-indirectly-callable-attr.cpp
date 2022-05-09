6fe86834b9c0cebfd625aa8a9e0e0187bdbb6a1b
Soumi Manna <soumi.manna@intel.com>
2022-05-09 05:59:15 -0700

    [SYCL][NFC] Silence unknown attribut warnings on Host compilation
    This patch updates test to use a custom verify. e.g, -verify=host on
    the RUN line and then // host-warning {{'bank_bits' attribute ignored}}
    instead of using the preprocessor conditional.
    
    Fixes https://github.com/intel/llvm/issues/5667
    
    Signed-off-by: Soumi Manna <soumi.manna@intel.com>

--- clang/test/SemaSYCL/device-indirectly-callable-attr.cpp
+++ clang/test/SemaSYCL/device-indirectly-callable-attr.cpp
@@ -1,9 +1,5 @@
 // RUN: %clang_cc1 -fsycl-is-device -fsyntax-only -verify %s
 // RUN: not %clang_cc1 -fsycl-is-device -ast-dump %s | FileCheck %s
-// RUN: %clang_cc1 -verify -DNO_SYCL %s
-// RUN: %clang_cc1 -fsycl-is-host -fsyntax-only -verify -DSYCL_HOST %s
-
-#if !defined(NO_SYCL) || defined(SYCL_HOST)
 
 [[intel::device_indirectly_callable]] // expected-warning {{'device_indirectly_callable' attribute only applies to functions}}
 int N;
@@ -40,14 +36,6 @@ void helper() {}
 void foo() {
   helper();
 }
-
-#else
-
-[[intel::device_indirectly_callable]] // expected-warning {{'device_indirectly_callable' attribute ignored}}
-void
-baz() {}
-
-#endif // NO_SYCL
 
 // CHECK: FunctionDecl {{.*}} helper
 //
