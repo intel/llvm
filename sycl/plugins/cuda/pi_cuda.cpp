57eb60e13acf8bc14c84b5f3c1adbc9673467557
aidan.belton <aidan.belton@codeplay.com>
2022-05-09 10:10:09 +0100

    emit warning

--- sycl/plugins/cuda/pi_cuda.cpp
+++ sycl/plugins/cuda/pi_cuda.cpp
@@ -4765,8 +4765,11 @@ pi_result cuda_piextUSMEnqueuePrefetch(pi_queue queue, const void *ptr,
   unsigned int is_managed;
   PI_CHECK_ERROR(cuPointerGetAttribute(
       &is_managed, CU_POINTER_ATTRIBUTE_IS_MANAGED, (CUdeviceptr)ptr));
-  if (!is_managed)
-    return PI_SUCCESS;
+  if (!is_managed) {
+    setErrorMessage("Prefetch hint ignored as prefetch only works with ",
+                    "USM" PI_SUCCESS);
+    return PI_PLUGIN_SPECIFIC_ERROR;
+  }
 
   // flags is currently unused so fail if set
   if (flags != 0)
