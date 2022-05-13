5174989fa5482012550981881c451d8a79b26a10
Larsen, Steffen <steffen.larsen@intel.com>
2022-05-13 03:57:04 -0700

    Increase version size in PI HIP and PI CUDA
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- sycl/plugins/cuda/pi_cuda.cpp
+++ sycl/plugins/cuda/pi_cuda.cpp
@@ -4981,7 +4981,7 @@ pi_result piPluginInit(pi_plugin *PluginInit) {
   }
 
   // PI interface supports higher version or the same version.
-  strncpy(PluginInit->PluginVersion, SupportedVersion, 4);
+  strncpy(PluginInit->PluginVersion, SupportedVersion, 5);
 
   // Set whole function table to zero to make it easier to detect if
   // functions are not set up below.
