84d2fa884c912a622dc7689e5051ed294dbd33a3
aidan.belton <aidan.belton@codeplay.com>
2022-05-13 14:21:59 +0100

    prevent abi break

--- sycl/include/CL/sycl/half_type.hpp
+++ sycl/include/CL/sycl/half_type.hpp
@@ -278,6 +278,11 @@ public:
   }
 
   // Operator neg
+  constexpr half_v2 &operator-() {
+    Buf ^= 0x8000;
+    return *this;
+  }
+
   __SYCL_CONSTEXPR_HALF friend half_v2 operator-(half_v2 other) {
     other.Buf ^= 0x8000;
     return other;
