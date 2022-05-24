86e26b73ed1f9fc7fc9673a646b274570ef25ddd
JackAKirk <chezjakirk@gmail.com>
2022-05-24 17:09:24 +0100

    Format
    Signed-off-by: JackAKirk <jack.kirk@codeplay.com>

--- clang/test/SemaSYCL/invalid_sg_cuda.cpp
+++ clang/test/SemaSYCL/invalid_sg_cuda.cpp
@@ -7,7 +7,7 @@ int main() {
   sycl::queue Q;
 
   Q.submit([&](sycl::handler &h) {
-    h.single_task<class kernel_name>([=][[sycl::reqd_sub_group_size(8)]] { // expected-warning {{CUDA requires sub_group size 32: size 8 request will be ignored}}
+    h.single_task<class kernel_name>([=] [[sycl::reqd_sub_group_size(8)]] { // expected-warning {{CUDA requires sub_group size 32: size 8 request will be ignored}}
 
     });
   });
