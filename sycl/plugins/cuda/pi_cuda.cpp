intel/llvm

commit dd418459868a976cd2eeae367fea6b92795ea611

[SYCL][CUDA][PI] Introduce multiple streams in each queue (#6102)
So far each queue only had one underlying CUstream, making it de facto in-order. This PR introduces multiple streams in each queue. To improve opportunities for concurrent execution streams are split into two pools - one for compute (kernels) and one for memory transfers.

The streams in pools are created dynamically when first needed. When a pool is full, previously created streams are reused. By default each queue has space for up to 128 streams for compute and 64 for transfers.

This PR also removes a test for internal workings of the queue. The problem is that introducing dynamic stream creation puts more work into `_pi_queue::get()`, making it depend on some helper functions in `pi_cuda.cpp`, so I had to move the definition from header to `pi_cuda.cpp`. This, however caused problem with linking this test, as `lib_pi_cuda.so` is created using custom linking script that only exposes functions starting with "pi". This improves linking performance, but prevents any other function from being tested. Looking at other tests for internals of the plugin I noticed that other functions that other functions that would need anything from `pi_cuda.cpp` are also not tested, so I deleted this test as well.

This is not changing any user-facing interface, so there are no accompanying changes to the test suite.

sycl/plugins/cuda/pi_cuda.hpp
@@ -36,12 +36,12 @@
 extern "C" {
 
 /// \cond IGNORE_BLOCK_IN_DOXYGEN
-pi_result cuda_piContextRetain(pi_context );
-pi_result cuda_piContextRelease(pi_context );
-pi_result cuda_piDeviceRelease(pi_device );
-pi_result cuda_piDeviceRetain(pi_device );
-pi_result cuda_piProgramRetain(pi_program );
-pi_result cuda_piProgramRelease(pi_program );
+pi_result cuda_piContextRetain(pi_context);
+pi_result cuda_piContextRelease(pi_context);
+pi_result cuda_piDeviceRelease(pi_device);
+pi_result cuda_piDeviceRetain(pi_device);
+pi_result cuda_piProgramRetain(pi_program);
+pi_result cuda_piProgramRelease(pi_program);
 pi_result cuda_piQueueRelease(pi_queue);
 pi_result cuda_piQueueRetain(pi_queue);
 pi_result cuda_piMemRetain(pi_mem);
@@ -377,18 +377,33 @@ struct _pi_mem {
 ///
 struct _pi_queue {
   using native_type = CUstream;
+  static constexpr int default_num_compute_streams = 128;
+  static constexpr int default_num_transfer_streams = 64;
 
-  native_type stream_;
+  std::vector<native_type> compute_streams_;
+  std::vector<native_type> transfer_streams_;
   _pi_context *context_;
   _pi_device *device_;
   pi_queue_properties properties_;
   std::atomic_uint32_t refCount_;
   std::atomic_uint32_t eventCount_;
+  std::atomic_uint32_t compute_stream_idx_;
+  std::atomic_uint32_t transfer_stream_idx_;
+  unsigned int num_compute_streams_;
+  unsigned int num_transfer_streams_;
+  unsigned int flags_;
+  std::mutex compute_stream_mutex_;
+  std::mutex transfer_stream_mutex_;
 
-  _pi_queue(CUstream stream, _pi_context *context, _pi_device *device,
-            pi_queue_properties properties)
-      : stream_{stream}, context_{context}, device_{device},
-        properties_{properties}, refCount_{1}, eventCount_{0} {
+  _pi_queue(std::vector<CUstream> &&compute_streams,
+            std::vector<CUstream> &&transfer_streams, _pi_context *context,
+            _pi_device *device, pi_queue_properties properties,
+            unsigned int flags)
+      : compute_streams_{std::move(compute_streams)},
+        transfer_streams_{std::move(transfer_streams)}, context_{context},
+        device_{device}, properties_{properties}, refCount_{1}, eventCount_{0},
+        compute_stream_idx_{0}, transfer_stream_idx_{0},
+        num_compute_streams_{0}, num_transfer_streams_{0}, flags_(flags) {
     cuda_piContextRetain(context_);
     cuda_piDeviceRetain(device_);
   }
@@ -398,7 +413,32 @@ struct _pi_queue {
     cuda_piDeviceRelease(device_);
   }
 
-  native_type get() const noexcept { return stream_; };
+  // get_next_compute/transfer_stream() functions return streams from
+  // appropriate pools in round-robin fashion
+  native_type get_next_compute_stream();
+  native_type get_next_transfer_stream();
+  native_type get() { return get_next_compute_stream(); };
+
+  template <typename T> void for_each_stream(T &&f) {
+    {
+      std::lock_guard<std::mutex> compute_guard(compute_stream_mutex_);
+      unsigned int end =
+          std::min(static_cast<unsigned int>(compute_streams_.size()),
+                   num_compute_streams_);
+      for (unsigned int i = 0; i < end; i++) {
+        f(compute_streams_[i]);
+      }
+    }
+    {
+      std::lock_guard<std::mutex> transfer_guard(transfer_stream_mutex_);
+      unsigned int end =
+          std::min(static_cast<unsigned int>(transfer_streams_.size()),
+                   num_transfer_streams_);
+      for (unsigned int i = 0; i < end; i++) {
+        f(transfer_streams_[i]);
+      }
+    }
+  }
 
   _pi_context *get_context() const { return context_; };
 
@@ -428,6 +468,8 @@ public:
   native_type get() const noexcept { return evEnd_; };
 
   pi_queue get_queue() const noexcept { return queue_; }
+
+  CUstream get_stream() const noexcept { return stream_; }
 
   pi_command_type get_command_type() const noexcept { return commandType_; }
 
@@ -472,8 +514,9 @@ public:
   pi_uint64 get_end_time() const;
 
   // construct a native CUDA. This maps closely to the underlying CUDA event.
-  static pi_event make_native(pi_command_type type, pi_queue queue) {
-    return new _pi_event(type, queue->get_context(), queue);
+  static pi_event make_native(pi_command_type type, pi_queue queue,
+                              CUstream stream) {
+    return new _pi_event(type, queue->get_context(), queue, stream);
   }
 
   pi_result release();
@@ -483,7 +526,8 @@ public:
 private:
   // This constructor is private to force programmers to use the make_native /
   // make_user static members in order to create a pi_event for CUDA.
-  _pi_event(pi_command_type type, pi_context context, pi_queue queue);
+  _pi_event(pi_command_type type, pi_context context, pi_queue queue,
+            CUstream stream);
 
   pi_command_type commandType_; // The type of command associated with event.
 
@@ -511,6 +555,9 @@ private:
 
   pi_queue queue_; // pi_queue associated with the event. If this is a user
                    // event, this will be nullptr.
+
+  CUstream stream_; // CUstream associated with the event. If this is a user
+                    // event, this will be uninitialized.
 
   pi_context context_; // pi_context associated with the event. If this is a
                        // native event, this will be the same context associated
@@ -545,7 +592,7 @@ struct _pi_program {
 
   pi_result set_binary(const char *binary, size_t binarySizeInBytes);
 
-  pi_result build_program(const char* build_options);
+  pi_result build_program(const char *build_options);
 
   pi_context get_context() const { return context_; };
 
@@ -686,8 +733,7 @@ struct _pi_kernel {
     assert(retError == PI_SUCCESS);
   }
 
-  ~_pi_kernel()
-  {
+  ~_pi_kernel() {
     cuda_piProgramRelease(program_);
     cuda_piContextRelease(context_);
   }

sycl/unittests/pi/cuda/CMakeLists.txt
@@ -7,7 +7,6 @@ add_sycl_unittest(PiCudaTests OBJECT
   test_kernels.cpp
   test_mem_obj.cpp
   test_primary_context.cpp
-  test_queue.cpp
   test_sampler_properties.cpp
 )
 

sycl/unittests/pi/cuda/test_queue.cpp
@@ -1,172 +1,0 @@
-//==---- test_queue.cpp --- PI unit tests ----------------------------------==//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-
-#include "gtest/gtest.h"
-
-#include <cuda.h>
-
-#include "TestGetPlatforms.hpp"
-#include "TestGetPlugin.hpp"
-#include <CL/sycl.hpp>
-#include <CL/sycl/detail/cuda_definitions.hpp>
-#include <CL/sycl/detail/pi.hpp>
-#include <detail/plugin.hpp>
-#include <pi_cuda.hpp>
-
-using namespace sycl;
-
-struct CudaTestQueue : public ::testing::TestWithParam<platform> {
-
-protected:
-  std::optional<detail::plugin> plugin =
-      pi::initializeAndGet(backend::ext_oneapi_cuda);
-
-  pi_platform platform_;
-  pi_device device_;
-  pi_context context_;
-
-  void SetUp() override {
-    // skip the tests if the CUDA backend is not available
-    if (!plugin.has_value()) {
-      GTEST_SKIP();
-    }
-
-    pi_uint32 numPlatforms = 0;
-    ASSERT_EQ(plugin->getBackend(), backend::ext_oneapi_cuda);
-
-    ASSERT_EQ((plugin->call_nocheck<detail::PiApiKind::piPlatformsGet>(
-                  0, nullptr, &numPlatforms)),
-              PI_SUCCESS)
-        << "piPlatformsGet failed.\n";
-
-    ASSERT_EQ((plugin->call_nocheck<detail::PiApiKind::piPlatformsGet>(
-                  numPlatforms, &platform_, nullptr)),
-              PI_SUCCESS)
-        << "piPlatformsGet failed.\n";
-
-    ASSERT_EQ((plugin->call_nocheck<detail::PiApiKind::piDevicesGet>(
-                  platform_, PI_DEVICE_TYPE_GPU, 1, &device_, nullptr)),
-              PI_SUCCESS);
-    ASSERT_EQ((plugin->call_nocheck<detail::PiApiKind::piContextCreate>(
-                  nullptr, 1, &device_, nullptr, nullptr, &context_)),
-              PI_SUCCESS);
-    EXPECT_NE(context_, nullptr);
-  }
-
-  void TearDown() override {
-    if (plugin.has_value()) {
-      plugin->call<detail::PiApiKind::piDeviceRelease>(device_);
-      plugin->call<detail::PiApiKind::piContextRelease>(context_);
-    }
-  }
-
-  CudaTestQueue() = default;
-
-  ~CudaTestQueue() = default;
-};
-
-TEST_F(CudaTestQueue, PICreateQueueSimple) {
-  pi_queue queue;
-  ASSERT_EQ((plugin->call_nocheck<detail::PiApiKind::piQueueCreate>(
-                context_, device_, 0, &queue)),
-            PI_SUCCESS);
-  ASSERT_NE(queue, nullptr);
-  EXPECT_EQ(queue->get_context(), context_);
-
-  unsigned int flags = 0;
-  CUstream stream = queue->get();
-  cuStreamGetFlags(stream, &flags);
-  ASSERT_EQ(flags, CU_STREAM_NON_BLOCKING);
-
-  ASSERT_EQ((plugin->call_nocheck<detail::PiApiKind::piQueueRelease>(queue)),
-            PI_SUCCESS);
-}
-
-TEST_F(CudaTestQueue, PIQueueFinishSimple) {
-  pi_queue queue;
-  ASSERT_EQ((plugin->call_nocheck<detail::PiApiKind::piQueueCreate>(
-                context_, device_, 0, &queue)),
-            PI_SUCCESS);
-  ASSERT_NE(queue, nullptr);
-
-  // todo: post work on queue, ensure the results are valid and the work is
-  // complete after piQueueFinish?
-
-  ASSERT_EQ((plugin->call_nocheck<detail::PiApiKind::piQueueFinish>(queue)),
-            PI_SUCCESS);
-
-  ASSERT_EQ(cuStreamQuery(queue->get()), CUDA_SUCCESS);
-
-  ASSERT_EQ((plugin->call_nocheck<detail::PiApiKind::piQueueRelease>(queue)),
-            PI_SUCCESS);
-}
-
-TEST_F(CudaTestQueue, PICreateQueueSimpleDefault) {
-  pi_queue queue;
-  ASSERT_EQ((plugin->call_nocheck<detail::PiApiKind::piQueueCreate>(
-                context_, device_, __SYCL_PI_CUDA_USE_DEFAULT_STREAM, &queue)),
-            PI_SUCCESS);
-  ASSERT_NE(queue, nullptr);
-  EXPECT_EQ(queue->get_context(), context_);
-
-  unsigned int flags = 0;
-  CUstream stream = queue->get();
-  cuStreamGetFlags(stream, &flags);
-  ASSERT_EQ(flags, CU_STREAM_DEFAULT);
-
-  ASSERT_EQ((plugin->call_nocheck<detail::PiApiKind::piQueueRelease>(queue)),
-            PI_SUCCESS);
-}
-
-TEST_F(CudaTestQueue, PICreateQueueSyncWithDefault) {
-  pi_queue queue;
-  ASSERT_EQ((plugin->call_nocheck<detail::PiApiKind::piQueueCreate>(
-                context_, device_, __SYCL_PI_CUDA_SYNC_WITH_DEFAULT, &queue)),
-            PI_SUCCESS);
-  ASSERT_NE(queue, nullptr);
-  EXPECT_EQ(queue->get_context(), context_);
-
-  unsigned int flags = 0;
-  CUstream stream = queue->get();
-  cuStreamGetFlags(stream, &flags);
-  ASSERT_NE(flags, CU_STREAM_NON_BLOCKING);
-
-  ASSERT_EQ((plugin->call_nocheck<detail::PiApiKind::piQueueRelease>(queue)),
-            PI_SUCCESS);
-}
-
-TEST_F(CudaTestQueue, PICreateQueueInterop) {
-  pi_queue queue;
-  ASSERT_EQ((plugin->call_nocheck<detail::PiApiKind::piQueueCreate>(
-                context_, device_, 0, &queue)),
-            PI_SUCCESS);
-  ASSERT_NE(queue, nullptr);
-  EXPECT_EQ(queue->get_context(), context_);
-
-  CUstream cuStream = queue->get();
-
-  CUcontext cuCtx;
-  CUresult res = cuStreamGetCtx(cuStream, &cuCtx);
-  ASSERT_EQ(res, CUDA_SUCCESS);
-  EXPECT_EQ(cuCtx, context_->get());
-
-  ASSERT_EQ((plugin->call_nocheck<detail::PiApiKind::piQueueRelease>(queue)),
-            PI_SUCCESS);
-}
-
-TEST_P(CudaTestQueue, SYCLQueueDefaultStream) {
-  std::vector<device> CudaDevices = GetParam().get_devices();
-  auto deviceA_ = CudaDevices[0];
-  queue Queue(deviceA_, async_handler{},
-              {ext::oneapi::cuda::property::queue::use_default_stream{}});
-
-  CUstream CudaStream = get_native<backend::ext_oneapi_cuda>(Queue);
-  unsigned int flags;
-  cuStreamGetFlags(CudaStream, &flags);
-  ASSERT_EQ(flags, CU_STREAM_DEFAULT);
-}

commit 8b85a3c34c6c21758d6017e5af14547de1294d2c

[SYCL][CUDA][PI] Improve performance of piQueueFinish (#6201)
Improves performance of `piQueueFinish` and therefore of `queue::wait()` on CUDA backend by reducing the number of `cuStreamSynchronize()` calls invoked. This in most use cases fixes the slowdown to  `queue::wait()` introduced in https://github.com/intel/llvm/pull/6102.

This does not change any interface so there are no changes to the test suite.

sycl/plugins/cuda/pi_cuda.cpp
@@ -2393,7 +2393,7 @@ pi_result cuda_piQueueFinish(pi_queue command_queue) {
            nullptr); // need PI_ERROR_INVALID_EXTERNAL_HANDLE error code
     ScopedContext active(command_queue->get_context());
 
-    command_queue->for_each_stream([&result](CUstream s) {
+    command_queue->sync_streams([&result](CUstream s) {
       result = PI_CHECK_ERROR(cuStreamSynchronize(s));
     });
 

sycl/plugins/cuda/pi_cuda.hpp
@@ -391,6 +391,8 @@ struct _pi_queue {
   std::atomic_uint32_t transfer_stream_idx_;
   unsigned int num_compute_streams_;
   unsigned int num_transfer_streams_;
+  unsigned int last_sync_compute_streams_;
+  unsigned int last_sync_transfer_streams_;
   unsigned int flags_;
   std::mutex compute_stream_mutex_;
   std::mutex transfer_stream_mutex_;
@@ -403,7 +405,9 @@ struct _pi_queue {
         transfer_streams_{std::move(transfer_streams)}, context_{context},
         device_{device}, properties_{properties}, refCount_{1}, eventCount_{0},
         compute_stream_idx_{0}, transfer_stream_idx_{0},
-        num_compute_streams_{0}, num_transfer_streams_{0}, flags_(flags) {
+        num_compute_streams_{0}, num_transfer_streams_{0},
+        last_sync_compute_streams_{0}, last_sync_transfer_streams_{0},
+        flags_(flags) {
     cuda_piContextRetain(context_);
     cuda_piDeviceRetain(device_);
   }
@@ -436,6 +440,59 @@ struct _pi_queue {
                    num_transfer_streams_);
       for (unsigned int i = 0; i < end; i++) {
         f(transfer_streams_[i]);
+      }
+    }
+  }
+
+  template <typename T> void sync_streams(T &&f) {
+    auto sync = [&f](const std::vector<CUstream> &streams, unsigned int start,
+                     unsigned int stop) {
+      for (unsigned int i = start; i < stop; i++) {
+        f(streams[i]);
+      }
+    };
+    {
+      unsigned int size = static_cast<unsigned int>(compute_streams_.size());
+      std::lock_guard<std::mutex> compute_guard(compute_stream_mutex_);
+      unsigned int start = last_sync_compute_streams_;
+      unsigned int end = num_compute_streams_ < size
+                             ? num_compute_streams_
+                             : compute_stream_idx_.load();
+      last_sync_compute_streams_ = end;
+      if (end - start >= size) {
+        sync(compute_streams_, 0, size);
+      } else {
+        start %= size;
+        end %= size;
+        if (start < end) {
+          sync(compute_streams_, start, end);
+        } else {
+          sync(compute_streams_, start, size);
+          sync(compute_streams_, 0, end);
+        }
+      }
+    }
+    {
+      unsigned int size = static_cast<unsigned int>(transfer_streams_.size());
+      if (size > 0) {
+        std::lock_guard<std::mutex> transfer_guard(transfer_stream_mutex_);
+        unsigned int start = last_sync_transfer_streams_;
+        unsigned int end = num_transfer_streams_ < size
+                               ? num_transfer_streams_
+                               : transfer_stream_idx_.load();
+        last_sync_transfer_streams_ = end;
+        if (end - start >= size) {
+          sync(transfer_streams_, 0, size);
+        } else {
+          start %= size;
+          end %= size;
+          if (start < end) {
+            sync(transfer_streams_, start, end);
+          } else {
+            sync(transfer_streams_, start, size);
+            sync(transfer_streams_, 0, end);
+          }
+        }
       }
     }
   }