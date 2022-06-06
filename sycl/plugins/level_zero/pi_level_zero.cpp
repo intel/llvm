ca16a78a3bbaaf1b8d4bac932ce0027c27809176
Andrei Elovikov <andrei.elovikov@intel.com>
2022-06-06 13:44:57 -0700

    Restore proper type cast

--- sycl/plugins/level_zero/pi_level_zero.cpp
+++ sycl/plugins/level_zero/pi_level_zero.cpp
@@ -2772,15 +2772,15 @@ pi_result piDeviceGetInfo(pi_device Device, pi_device_info ParamName,
     return ReturnValue(pi_uint64{DoubleFPValue});
   }
   case PI_DEVICE_INFO_IMAGE2D_MAX_WIDTH:
-    return ReturnValue(Device->ZeDeviceImageProperties->maxImageDims2D);
+    return ReturnValue(size_t{Device->ZeDeviceImageProperties->maxImageDims2D});
   case PI_DEVICE_INFO_IMAGE2D_MAX_HEIGHT:
-    return ReturnValue(Device->ZeDeviceImageProperties->maxImageDims2D);
+    return ReturnValue(size_t{Device->ZeDeviceImageProperties->maxImageDims2D});
   case PI_DEVICE_INFO_IMAGE3D_MAX_WIDTH:
-    return ReturnValue(Device->ZeDeviceImageProperties->maxImageDims3D);
+    return ReturnValue(size_t{Device->ZeDeviceImageProperties->maxImageDims3D});
   case PI_DEVICE_INFO_IMAGE3D_MAX_HEIGHT:
-    return ReturnValue(Device->ZeDeviceImageProperties->maxImageDims3D);
+    return ReturnValue(size_t{Device->ZeDeviceImageProperties->maxImageDims3D});
   case PI_DEVICE_INFO_IMAGE3D_MAX_DEPTH:
-    return ReturnValue(Device->ZeDeviceImageProperties->maxImageDims3D);
+    return ReturnValue(size_t{Device->ZeDeviceImageProperties->maxImageDims3D});
   case PI_DEVICE_INFO_IMAGE_MAX_BUFFER_SIZE:
     return ReturnValue(
         size_t{Device->ZeDeviceImageProperties->maxImageBufferSize});
