9f61c8ee228db0d25584d282a6b376f1a131614e
Peter Colberg <peter.colberg@intel.com>
2022-06-08 11:22:34 -0400

    [SYCL] Align USM buffer location implementation for malloc_device with malloc_shared (#6269)
    Use the same call to USMDeviceAlloc with an empty property list in all
    cases, to allow for straight-forward extension with future properties.
    
    Query buffer location extension only if buffer location property is passed.
    
    This amends https://github.com/intel/llvm/pull/5634
    
    See also https://github.com/intel/llvm/pull/6220

--- sycl/plugins/level_zero/pi_level_zero.cpp
+++ sycl/plugins/level_zero/pi_level_zero.cpp
@@ -7207,7 +7207,7 @@ static pi_result USMDeviceAllocImpl(void **ResultPtr, pi_context Context,
   PI_ASSERT(Device, PI_INVALID_DEVICE);
 
   // Check that incorrect bits are not set in the properties.
-  PI_ASSERT(!Properties ||
+  PI_ASSERT(!Properties || *Properties == 0 ||
                 (*Properties == PI_MEM_ALLOC_FLAGS && *(Properties + 2) == 0),
             PI_INVALID_VALUE);
 
--- sycl/source/detail/usm/usm_impl.cpp
+++ sycl/source/detail/usm/usm_impl.cpp
@@ -129,25 +129,27 @@ void *alignedAlloc(size_t Alignment, size_t Size, const context &Ctxt,
     switch (Kind) {
     case alloc::device: {
       Id = detail::getSyclObjImpl(Dev)->getHandleRef();
-      // Parse out buffer location property
+
+      std::array<pi_usm_mem_properties, 3> Props;
+      auto PropsIter = Props.begin();
+
       // Buffer location is only supported on FPGA devices
-      bool IsBufferLocSupported =
-          Dev.has_extension("cl_intel_mem_alloc_buffer_location");
-      if (IsBufferLocSupported &&
-          PropList.has_property<cl::sycl::ext::intel::experimental::property::
-                                    usm::buffer_location>()) {
-        auto location = PropList
-                            .get_property<cl::sycl::ext::intel::experimental::
-                                              property::usm::buffer_location>()
-                            .get_buffer_location();
-        pi_usm_mem_properties props[3] = {PI_MEM_USM_ALLOC_BUFFER_LOCATION,
-                                          location, 0};
-        Error = Plugin.call_nocheck<PiApiKind::piextUSMDeviceAlloc>(
-            &RetVal, C, Id, props, Size, Alignment);
-      } else {
-        Error = Plugin.call_nocheck<PiApiKind::piextUSMDeviceAlloc>(
-            &RetVal, C, Id, nullptr, Size, Alignment);
+      if (PropList.has_property<cl::sycl::ext::intel::experimental::property::
+                                    usm::buffer_location>() &&
+          Dev.has_extension("cl_intel_mem_alloc_buffer_location")) {
+        *PropsIter++ = PI_MEM_USM_ALLOC_BUFFER_LOCATION;
+        *PropsIter++ = PropList
+                           .get_property<cl::sycl::ext::intel::experimental::
+                                             property::usm::buffer_location>()
+                           .get_buffer_location();
       }
+
+      assert(PropsIter >= Props.begin() && PropsIter < Props.end());
+      *PropsIter++ = 0; // null-terminate property list
+
+      Error = Plugin.call_nocheck<PiApiKind::piextUSMDeviceAlloc>(
+          &RetVal, C, Id, Props.data(), Size, Alignment);
+
       break;
     }
     case alloc::shared: {
