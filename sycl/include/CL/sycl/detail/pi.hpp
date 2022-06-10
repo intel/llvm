0fdae34701080bc09e05cdf3813d6c16c14f4837
Steffen Larsen <steffen.larsen@intel.com>
2022-06-10 05:45:46 +0100

    [SYCL] Implement PI cast for vectors and fix OpenCL specializations (#6278)
    This commit makes two changes related to pi::cast:
    
    Implements pi::cast between vectors by applying pi::cast to the individual elements of the input vector, storing the casted values in a new vector.
    Moves the OpenCL specializations into the backend traits file for OpenCL. Additionally, PI_OPENCL_AVAILABLE is removed as it was only defined in the pi_opencl subproject whereas pi::cast, including the moved specializations, are used by the SYCL interop headers.
    
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- sycl/include/CL/sycl/detail/pi.hpp
+++ sycl/include/CL/sycl/detail/pi.hpp
@@ -13,9 +13,6 @@
 
 #pragma once
 
-#ifdef PI_OPENCL_AVAILABLE
-#include <CL/sycl/detail/cl.h>
-#endif // PI_OPENCL_AVAILABLE
 #include <CL/sycl/backend_types.hpp>
 #include <CL/sycl/detail/export.hpp>
 #include <CL/sycl/detail/os_util.hpp>
@@ -423,38 +420,20 @@ template <class To, class From> inline To cast(From value) {
   return (To)(value);
 }
 
-template <class To, class From> inline To cast(std::vector<From> value) {
-  // Silence the unused parameter warning.
-  (void)value;
-  RT::assertion(false,
-                "Compatibility specialization, not expected to be used. "
-                "The only allowed cast using a vector of From values is "
-                "implemented in the OpenCL backend (see cl_event case).");
-  return {};
-}
+// Helper traits for identifying std::vector with arbitrary element type.
+template <typename T> struct IsStdVector : std::false_type {};
+template <typename T> struct IsStdVector<std::vector<T>> : std::true_type {};
 
-#ifdef PI_OPENCL_AVAILABLE
-
-// Cast for std::vector<cl_event>, according to the spec, make_event
-// should create one(?) event from a vector of cl_event
-template <class To> inline To cast(std::vector<cl_event> value) {
-  RT::assertion(value.size() == 1,
-                "Temporary workaround requires that the "
-                "size of the input vector for make_event be equal to one.");
-  return (To)(value[0]);
+// Overload for vectors that applies the cast to all elements. This
+// creates a new vector.
+template <class To, class FromE> To cast(std::vector<FromE> Values) {
+  static_assert(IsStdVector<To>::value, "Return type must be a vector.");
+  To ResultVec;
+  ResultVec.reserve(Values.size());
+  for (FromE &Val : Values)
+    ResultVec.push_back(cast<typename To::value_type>(Val));
+  return ResultVec;
 }
-
-// These conversions should use PI interop API.
-template <> inline pi::PiProgram cast(cl_program) {
-  RT::assertion(false, "pi::cast -> use piextCreateProgramWithNativeHandle");
-  return {};
-}
-
-template <> inline pi::PiDevice cast(cl_device_id) {
-  RT::assertion(false, "pi::cast -> use piextCreateDeviceWithNativeHandle");
-  return {};
-}
-#endif // PI_OPENCL_AVAILABLE
 
 } // namespace pi
 } // namespace detail
