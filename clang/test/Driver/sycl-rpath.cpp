426ee84d5a214d5c960cc89c7c3023f3d3acb77f
Nicolas Miller <nicolas.miller@codeplay.com>
2022-06-15 09:41:01 +0100

    [SYCL] Switch to BooleanFFlag for rpath flag and add test

---
+++ clang/test/Driver/sycl-rpath.cpp
@@ -0,0 +1,17 @@
+// RUN: %clang -### -fsycl %s 2>&1 | FileCheck --check-prefix=CHECK-DEFAULT %s
+
+// CHECK-DEFAULT: "-rpath"
+
+// RUN: %clang -### -fsycl \
+// RUN:   -fsycl-implicit-rpath %s 2>&1 | FileCheck --check-prefix=CHECK-FLAG %s
+
+// CHECK-FLAG: "-rpath"
+
+// RUN: %clang -### -fsycl \
+// RUN:   -fno-sycl-implicit-rpath %s 2>&1 | FileCheck --check-prefix=CHECK-NOFLAG %s
+
+// CHECK-NOFLAG-NOT: "-rpath"
+
+// REQUIRES: system-linux
+
+void func(){};
