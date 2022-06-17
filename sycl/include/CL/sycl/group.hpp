1d72f9803008b3435d7bbf1a4087a600ab9fa0c7
Alexey Bader <alexey.bader@intel.com>
2022-06-17 03:00:06 -0700

    [SYCL] Add group::get_linear_id(int dim) overload

--- sycl/include/CL/sycl/group.hpp
+++ sycl/include/CL/sycl/group.hpp
@@ -133,6 +133,10 @@ public:
 #endif
   }
 
+  size_t get_local_id(int dimention) const {
+    return get_local_id()[dimention];
+  }
+
   size_t get_local_linear_id() const {
     return get_local_linear_id_impl<Dimensions>();
   }
