1d8ebeaca2c1cacbe509ce2b55760983324d8c3c
gregory <gregory@gaiaplatform.io>
2022-06-21 16:04:33 -0700

    Address PR comments

--- sycl/include/sycl/ext/intel/esimd/memory.hpp
+++ sycl/include/sycl/ext/intel/esimd/memory.hpp
@@ -1354,16 +1354,13 @@ void simd_obj_impl<T, N, T1, SFINAE>::copy_to(
         Addr[NumChunks * ChunkSize] = Tmp[NumChunks * ChunkSize];
       } else if constexpr (RemN == 8 || RemN == 16) {
         if constexpr (sizeof(T) == 1 && RemN == 16) {
-          simd_mask_type<8> Pred(0);
-          Pred.template select<4, 1>() = 1;
           simd<int32_t, 8> Vals;
           Vals.template select<4, 1>() =
               Tmp.template bit_cast_view<int32_t>().template select<4, 1>(
                   NumChunks * ChunkSize);
-          simd<uint32_t, 8> Offsets(0u, sizeof(int32_t));
-          scatter<int32_t, 8>(reinterpret_cast<int32_t *>(Addr) +
-                                  (NumChunks * ChunkSize),
-                              Offsets, Vals, Pred);
+          block_store<int32_t, 8>(reinterpret_cast<int32_t *>(Addr) +
+                                      (NumChunks * ChunkSize),
+                                  Vals);
         } else {
           simd<uint32_t, RemN> Offsets(0u, sizeof(T));
           scatter<UT, RemN>(
