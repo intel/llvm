4256e4e7ac5b1d1af8fd85f9f998e0bac2e1df46
Sven van Haastregt <sven.vanhaastregt@arm.com>
2022-06-29 17:14:47 +0100

    Use Sign/ZeroExtend image operands for read/write_image
    Emit (in OCLToSPIRV) and decode (in SPIRVToOCL) the SignExtend and
    ZeroExtend Image Operands introduced by SPIR-V version 1.4 to preserve
    signedness of image read and write operations.  This allows
    distinguishing between e.g. `read_imagei` and `read_imageui` and
    enables a lossless LLVM -> SPIR-V -> LLVM conversion for those OpenCL
    builtins.
    
    Before SPIR-V 1.4, there was no way to represent the signedness of the
    image operations and llvm-spirv defaulted to signed.  This commit
    leaves that behaviour unchanged if llvm-spirv's maximum version is set
    to SPIR-V 1.3 or below.  Add the `--spirv-max-version=1.3` flag to
    some tests that rely on the old behaviour.
    
    Original commit:
    https://github.com/KhronosGroup/SPIRV-LLVM-Translator/commit/bb4ead0

--- llvm-spirv/lib/SPIRV/OCLToSPIRV.h
+++ llvm-spirv/lib/SPIRV/OCLToSPIRV.h
@@ -169,7 +169,8 @@ public:
   /// read_image(image, sampler, ...) =>
   ///   sampled_image = __spirv_SampledImage(image, sampler);
   ///   return __spirv_ImageSampleExplicitLod_R{ReturnType}(sampled_image, ...);
-  void visitCallReadImageWithSampler(CallInst *CI, StringRef MangledName);
+  void visitCallReadImageWithSampler(CallInst *CI, StringRef MangledName,
+                                     StringRef DemangledName);
 
   /// Transform read_image with msaa image arguments.
   /// Sample argument must be acoded as Image Operand.
