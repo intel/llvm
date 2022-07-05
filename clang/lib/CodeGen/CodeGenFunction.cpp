ff3551d6d5294c5b14120e2719c709a19349e834
pgorlani <paolo.gorlani@codeplay.com>
2022-07-05 07:32:42 -0700

    add missing comment

--- clang/lib/CodeGen/CodeGenFunction.cpp
+++ clang/lib/CodeGen/CodeGenFunction.cpp
@@ -1643,6 +1643,7 @@ void CodeGenFunction::GenerateCode(GlobalDecl GD, llvm::Function *Fn,
   else if (getLangOpts().CUDA && !getLangOpts().CUDAIsDevice &&
            getLangOpts().SYCLIsHost && !FD->hasAttr<CUDAHostAttr>() &&
            FD->hasAttr<CUDADeviceAttr>()) {
+    // Generate a dummy __host__ function for compiling CUDA sources in SYCL.
     Fn->setLinkage(llvm::Function::WeakODRLinkage);
     if (FD->getReturnType()->isVoidType())
       Builder.CreateRetVoid();
