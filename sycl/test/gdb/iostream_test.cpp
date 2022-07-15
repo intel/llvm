4deaffdfa9c95a79f325a8f60549f862dea407f5
Rauf, Rana <rana.rauf@intel.com>
2022-07-15 08:54:52 -0700

    Marked gdb counter test unsupported and moved test to better directory
    Signed-off-by: Rauf, Rana <rana.rauf@intel.com>

--- sycl/test/gdb/iostream_test.cpp
+++ sycl/test/gdb/iostream_test.cpp
@@ -1,6 +1,7 @@
+// UNSUPPORTED: true
 // RUN: %clangxx -fsycl -g %s -o %t.out
 // RUN: gdb %t.out -batch -x %sycl_gdb_iostream 2>&1 | FileCheck %s
-// CHECK: ignore next 9999 hits
+// CHECK: ignore next 9998 hits
 //
 // Tests if <sycl/sycl.hpp> headers include any <iostream> headers
 // Acceptable value is 1.
