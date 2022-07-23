eceed49d28bed7ba3a5d96374147d5794aa4fa10
apstasen <alexander.p.stasenko@intel.com>
2022-07-23 15:37:44 -0700

    Added AWS EC2 dynamic runner support (#2)

---
+++ devops/actions/aws-ec2/aws-ec2.js
@@ -0,0 +1,230 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const AWS = require('aws-sdk');
+
+// shortcut to reference current repo
+const repo = `${github.context.repo.owner}/${github.context.repo.repo}`;
+
+// get github registration token that allows to register new runner based on
+// GH_PERSONAL_ACCESS_TOKEN github user api key
+async function getGithubRegToken() {
+  const octokit = github.getOctokit(core.getInput("GH_PERSONAL_ACCESS_TOKEN"));
+
+  try {
+    const response = await octokit.request(
+        `POST /repos/${repo}/actions/runners/registration-token`);
+    core.info("Got Github Actions Runner registration token");
+    return response.data.token;
+  } catch (error) {
+    core.error("Error getting Github Actions Runner registration token");
+    throw error;
+  }
+}
+
+// add delay before retrying promise one more time
+function rejectDelay(reason) {
+  return new Promise(function(
+      resolve, reject) { setTimeout(reject.bind(null, reason), 10 * 1000); });
+}
+
+// starts AWS EC2 instance that will spawn Github runner for a given label
+async function start(label) {
+  const ec2 = new AWS.EC2();
+
+  // we better keep GH_PERSONAL_ACCESS_TOKEN here and do not pass it to AWS EC2
+  // userscript so it will keep secret
+  const reg_token = await getGithubRegToken();
+  const timebomb = core.getInput("aws-timebomb");
+  const raw_ec2types = JSON.parse(core.getInput("aws-type"));
+  const ec2types =
+      typeof raw_ec2types == "string" ? [ raw_ec2types ] : raw_ec2types;
+  const ec2disk = core.getInput("aws-disk");
+  const ec2spot = core.getInput("aws-spot") != "false";
+  const onejob = core.getInput("one-job") != "false";
+
+  let ec2id;      // AWS EC2 instance id
+  let last_error; // last error that ill be thrown in case all our attemps in
+                  // instance creation will fails
+  // loop for spot/ondemand instances
+  for (let spot of (ec2spot ? [ 1, 0 ] : [ 0 ])) {
+    const spot_str = spot ? "spot" : "on-demand";
+    for (let ec2type of ec2types) { // iterate for provided instance types
+      const setup_github_actions_runner = [
+        `#!/bin/bash -x`, `mkdir actions-runner`, `cd actions-runner`,
+        // we can not place runner into AMI image since it is updated often and
+        // latest version in required to connect to github
+        `export RUNNER_VERSION=$(curl -s https://api.github.com/repos/actions/runner/releases/latest | sed -n \'s,.*"tag_name": "v\\(.*\\)".*,\\1,p\')`,
+        `curl -O -L https://github.com/actions/runner/releases/download/v$RUNNER_VERSION/actions-runner-linux-x64-$RUNNER_VERSION.tar.gz || shutdown -h now`,
+        `tar xf ./actions-runner-linux-x64-$RUNNER_VERSION.tar.gz || shutdown -h now`,
+        `su gh_runner -c "./config.sh --unattended --url https://github.com/${repo} --token ${reg_token} --name ${label}_${ec2type}_${spot_str} --labels ${label} --replace || shutdown -h now"`,
+        // timebomb to avoid paying for stale AWS instances
+        `(sleep ${timebomb}; su gh_runner -c "./config.sh remove --token ${reg_token}"; shutdown -h now) &`,
+        // ephemeral runner will exit after one job so we will terminate
+        // instance sooner
+        onejob ? `su gh_runner -c "./run.sh --ephemeral"`
+               : `su gh_runner -c "./run.sh"`,
+        `su gh_runner -c "./config.sh remove --token ${reg_token}"`,
+        `shutdown -h now` // in case we launch insance with
+                          // InstanceInitiatedShutdownBehavior = "terminate" it
+                          // will terminate instance here as well
+      ];
+      try {
+        let params = {
+          ImageId : core.getInput("aws-ami"),
+          InstanceType : ec2type,
+          InstanceInitiatedShutdownBehavior : "terminate",
+          UserData : Buffer.from(setup_github_actions_runner.join('\n'))
+                           .toString('base64'),
+          MinCount : 1,
+          MaxCount : 1,
+          TagSpecifications : [ {
+            ResourceType : "instance",
+            Tags : [ {Key : "Label", Value : label} ]
+          } ]
+        };
+        if (spot)
+          params.InstanceMarketOptions = {MarketType : "spot"};
+        if (ec2disk) {
+          const items = ec2disk.split(':');
+          params.BlockDeviceMappings =
+              [ {DeviceName : items[0], Ebs : {VolumeSize : items[1]}} ];
+        }
+        const result = await ec2.runInstances(params).promise();
+        ec2id = result.Instances[0].InstanceId;
+        core.info(`Created AWS EC2 ${spot_str} instance ${ec2id} of ${ec2type} type with ${label} label`);
+        break;
+      } catch (error) {
+        core.warning(`Error creating AWS EC2 ${spot_str} instance of ${ec2type} type with ${label} label`);
+        last_error = error;
+      }
+    }
+    // we already created instance and do not need to iterate these loops
+    if (ec2id)
+      break;
+  }
+  if (last_error) {
+    core.error(`Error creating AWS EC2 instance with ${label} label`);
+    throw last_error;
+  }
+
+  // wait untill instance will be found running before continuing (spot instance
+  // can be created but never run and will be in pending state untill
+  // termination)
+  let p = ec2.waitFor("instanceRunning",
+                      {Filters : [ {Name : "tag:Label", Values : [ label ]} ]})
+              .promise();
+  for (let i = 0; i < 2; i++) {
+    p = p.catch(function() {
+      core.warning(`Error searching for running AWS EC2 spot instance ${ec2id} with ${label} label. Will retry.`);
+    }).catch(rejectDelay);
+  }
+  p = p.then(function() {
+    core.info(`Found running AWS EC2 spot instance ${ec2id} with ${label} label`);
+  }).catch(function(error) {
+    core.error(`Error searching for running AWS EC2 spot instance ${ec2id} with ${label} label`);
+    throw error;
+  });
+}
+
+// terminate (completely remove) AWS EC instances (normally one instance) with
+// given tag label and also remove all Github actions runners (normally one
+// runner) with that label
+async function stop(label) {
+  // last error that will be thrown in case something will break here
+  let last_error;
+  const ec2 = new AWS.EC2();
+
+  // find AWS EC2 instances with tag label
+  let instances;
+  try {
+    instances =
+        await ec2
+            .describeInstances(
+                {Filters : [ {Name : "tag:Label", Values : [ label ]} ]})
+            .promise();
+    core.info(`Searched for AWS EC2 instance with label ${label}`);
+  } catch (error) {
+    core.error(`Error searching for AWS EC2 instance with label ${label}`);
+    last_error = error;
+  }
+
+  // remove all found AWS EC2 instances
+  if (instances)
+    for (const reservation of instances.Reservations) {
+      for (const instance of reservation.Instances) {
+        try {
+          await ec2.terminateInstances({InstanceIds : [ instance.InstanceId ]})
+              .promise();
+          core.info(`Terminated AWS EC2 instance ${instance.InstanceId} with label ${label}`);
+        } catch (error) {
+          core.error(`Error terminating AWS EC2 instance ${instance.InstanceId} with label ${label}`);
+          last_error = error;
+        }
+      }
+    }
+
+  // find all Github action runners
+  const octokit = github.getOctokit(core.getInput("GH_PERSONAL_ACCESS_TOKEN"));
+  let runners;
+  try {
+    runners = await octokit.paginate(`GET /repos/${repo}/actions/runners`);
+    core.info(`Searched for Github action runners with label ${label}`);
+  } catch (error) {
+    core.info(`Error searching for Github action runners with label ${label}`);
+    last_error = error;
+  }
+
+  // remove Github action runners with specified label
+  if (runners)
+    for (runner of runners) {
+      let label_found = false;
+      for (label_obj of runner.labels)
+        if (label_obj.name == label) {
+          label_found = true;
+          break;
+        }
+      if (!label_found)
+        continue;
+      let p =
+          octokit.request(`DELETE /repos/${repo}/actions/runners/${runner.id}`);
+      // retry deletion up to 5 times (with 10 seconds delay) sincec Github can
+      // not remove runners still marked as active (with running job)
+      for (let i = 0; i < 5; i++) {
+        p = p.catch(function() {
+          core.warning(`Error removing Github self-hosted runner ${runner.id} with ${label}. Will retry.`);
+        }).catch(rejectDelay);
+      }
+      p = p.then(function() {
+        core.info(`Removed Github self-hosted runner ${runner.id} with ${label}`);
+      }).catch(function(error) {
+        core.error(`Error removing Github self-hosted runner ${runner.id} with ${label}`);
+        last_error = error;
+      });
+    }
+
+  if (last_error)
+    throw last_error;
+}
+
+(async function() {
+  try {
+    // provide AWS credentials
+    AWS.config.update({
+      accessKeyId : core.getInput("AWS_ACCESS_KEY"),
+      secretAccessKey : core.getInput("AWS_SECRET_KEY"),
+      region : core.getInput("aws-region")
+    });
+    // mode is start or stop
+    const mode = core.getInput("mode");
+    // label used to indentify AWS EC2 instances and Github runners
+    const label = core.getInput("label");
+    if (mode == "start") {
+      await start(label);
+    } else if (mode == "stop") {
+      await stop(label);
+    }
+  } catch (error) {
+    core.error(error);
+    core.setFailed(error.message);
+  }
+})();
