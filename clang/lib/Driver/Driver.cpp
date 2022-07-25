3f3834dc71b270248af1983abc9bf25158858722
Michael D Toguchi <michael.d.toguchi@intel.com>
2022-07-25 09:01:31 -0700

    Clang format

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -7822,8 +7822,8 @@ InputInfoList Driver::BuildJobsForActionNoCache(
         }
         std::string TmpFileName = C.getDriver().GetTemporaryPath(
             llvm::sys::path::stem(BaseInput), Ext);
-        const char *TmpFile = C.addTempFile(
-            C.getArgs().MakeArgString(TmpFileName), TI);
+        const char *TmpFile =
+            C.addTempFile(C.getArgs().MakeArgString(TmpFileName), TI);
         CurI = InputInfo(TI, TmpFile, TmpFile);
       } else {
         // Host part of the unbundled object is not used when -fsycl-link is
