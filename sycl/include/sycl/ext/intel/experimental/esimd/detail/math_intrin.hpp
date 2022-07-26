b28d750439b973d711272e3997ec32bf8acd4c2e
Ahn, Dongkyun <dongkyun.ahn@intel.com>
2022-07-26 10:28:30 -0700

    Working version for dpas.cpp
    - First argument for '__esimd_dpas_inner' is changed from 'const
    __ESIMD_DNS::vector_type_t<RT, SZ>' to 'const void *'
    
    - This is because 'std::addressof()' from caller generates 'short *
    __attribute__((ext_vector_type(16)))' that cannot be converted into
    'vector_type_t<>' while 'dpas.cpp' test is compiled.

--- sycl/include/sycl/ext/intel/experimental/esimd/detail/math_intrin.hpp
+++ sycl/include/sycl/ext/intel/experimental/esimd/detail/math_intrin.hpp
@@ -457,12 +457,13 @@ __esimd_dpas_bits_precision(__ESIMD_ENS::argument_type precisionType) {
              : 1;
 }
 
+// TODO : 'src0' argument type other than 'void*'?
 template <__ESIMD_ENS::argument_type src1_precision,
           __ESIMD_ENS::argument_type src2_precision, int systolic_depth,
           int repeat_count, typename RT, typename T1, typename T2,
           __ESIMD_NS::uint SZ, __ESIMD_NS::uint N1, __ESIMD_NS::uint N2>
 inline __ESIMD_DNS::vector_type_t<RT, SZ>
-__esimd_dpas_inner(const __ESIMD_DNS::vector_type_t<RT, SZ> *src0,
+__esimd_dpas_inner(const void *src0,
                    const __ESIMD_DNS::vector_type_t<T1, N1> &src1,
                    const __ESIMD_DNS::vector_type_t<T2, N2> &src2) {
   __ESIMD_DNS::vector_type_t<RT, SZ> retv;
@@ -575,7 +576,9 @@ __esimd_dpas_inner(const __ESIMD_DNS::vector_type_t<RT, SZ> *src0,
 
     for (uint n = 0; n < SIMDSize; n++) {
       if (src0 != nullptr) {
-        auto src0El = src0[0][r * SIMDSize + n];
+        __ESIMD_DNS::vector_type_t<RT, SZ> *src0Casted =
+            (__ESIMD_DNS::vector_type_t<RT, SZ> *)(src0);
+        auto src0El = src0Casted[0][r * SIMDSize + n];
 
         if (pvcBfDest) {
           const auto tmp = (uint32_t)(src0El) << 16;
@@ -666,7 +669,7 @@ __esimd_dpas(__ESIMD_DNS::vector_type_t<T0, N> src0,
              __ESIMD_DNS::vector_type_t<T2, N2> src2) {
   return __esimd_dpas_inner<src1_precision, src2_precision, systolic_depth,
                             repeat_count, T, T1, T2, N, N1, N2>(
-      std::addressof(src0), src1, src2);
+      (void *)std::addressof(src0), src1, src2);
 }
 
 template <__ESIMD_ENS::argument_type src1_precision,
