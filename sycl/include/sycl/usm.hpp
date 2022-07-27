acaa40243ab1df69a174d0fc8c70274bdb710848
Sergey V Maslov <sergey.v.maslov@intel.com>
2022-07-27 03:49:54 -0700

    made code_location argument configurable in template functions
    Signed-off-by: Sergey V Maslov <sergey.v.maslov@intel.com>

--- sycl/include/sycl/usm.hpp
+++ sycl/include/sycl/usm.hpp
@@ -289,16 +289,30 @@ aligned_alloc(size_t alignment, size_t size, const queue &q, usm::alloc kind,
 template <typename T>
 T *malloc_device(
     size_t Count, const device &Dev, const context &Ctxt,
-    const property_list &PropList = {},
-    const detail::code_location &CodeLoc = detail::code_location::current()) {
+    const property_list &PropList = {}
+#ifndef DISABLE_SYCL_INSTRUMENTATION_METADATA
+    ,
+    const detail::code_location &CodeLoc = detail::code_location::current()
+#endif
+) {
+#ifdef DISABLE_SYCL_INSTRUMENTATION_METADATA
+  const detail::code_location &CodeLoc = {};
+#endif
   return static_cast<T *>(aligned_alloc_device(alignof(T), Count * sizeof(T),
                                                Dev, Ctxt, PropList, CodeLoc));
 }
 
 template <typename T>
 T *malloc_device(
-    size_t Count, const queue &Q, const property_list &PropList = {},
-    const detail::code_location &CodeLoc = detail::code_location::current()) {
+    size_t Count, const queue &Q, const property_list &PropList = {}
+#ifndef DISABLE_SYCL_INSTRUMENTATION_METADATA
+    ,
+    const detail::code_location &CodeLoc = detail::code_location::current()
+#endif
+) {
+#ifdef DISABLE_SYCL_INSTRUMENTATION_METADATA
+  const detail::code_location &CodeLoc = {};
+#endif
   return malloc_device<T>(Count, Q.get_device(), Q.get_context(), PropList,
                           CodeLoc);
 }
@@ -306,8 +320,15 @@ T *malloc_device(
 template <typename T>
 T *aligned_alloc_device(
     size_t Alignment, size_t Count, const device &Dev, const context &Ctxt,
-    const property_list &PropList = {},
-    const detail::code_location &CodeLoc = detail::code_location::current()) {
+    const property_list &PropList = {}
+#ifndef DISABLE_SYCL_INSTRUMENTATION_METADATA
+    ,
+    const detail::code_location &CodeLoc = detail::code_location::current()
+#endif
+) {
+#ifdef DISABLE_SYCL_INSTRUMENTATION_METADATA
+  const detail::code_location &CodeLoc = {};
+#endif
   return static_cast<T *>(aligned_alloc_device(max(Alignment, alignof(T)),
                                                Count * sizeof(T), Dev, Ctxt,
                                                PropList, CodeLoc));
@@ -316,40 +337,75 @@ T *aligned_alloc_device(
 template <typename T>
 T *aligned_alloc_device(
     size_t Alignment, size_t Count, const queue &Q,
-    const property_list &PropList = {},
-    const detail::code_location &CodeLoc = detail::code_location::current()) {
+    const property_list &PropList = {}
+#ifndef DISABLE_SYCL_INSTRUMENTATION_METADATA
+    ,
+    const detail::code_location &CodeLoc = detail::code_location::current()
+#endif
+) {
+#ifdef DISABLE_SYCL_INSTRUMENTATION_METADATA
+  const detail::code_location &CodeLoc = {};
+#endif
   return aligned_alloc_device<T>(Alignment, Count, Q.get_device(),
                                  Q.get_context(), PropList, CodeLoc);
 }
 
 template <typename T>
 T *malloc_host(
-    size_t Count, const context &Ctxt, const property_list &PropList = {},
-    const detail::code_location &CodeLoc = detail::code_location::current()) {
+    size_t Count, const context &Ctxt, const property_list &PropList = {}
+#ifndef DISABLE_SYCL_INSTRUMENTATION_METADATA
+    ,
+    const detail::code_location &CodeLoc = detail::code_location::current()
+#endif
+) {
+#ifdef DISABLE_SYCL_INSTRUMENTATION_METADATA
+  const detail::code_location &CodeLoc = {};
+#endif
   return static_cast<T *>(aligned_alloc_host(alignof(T), Count * sizeof(T),
                                              Ctxt, PropList, CodeLoc));
 }
 
 template <typename T>
 T *malloc_host(
-    size_t Count, const queue &Q, const property_list &PropList = {},
-    const detail::code_location &CodeLoc = detail::code_location::current()) {
+    size_t Count, const queue &Q, const property_list &PropList = {}
+#ifndef DISABLE_SYCL_INSTRUMENTATION_METADATA
+    ,
+    const detail::code_location &CodeLoc = detail::code_location::current()
+#endif
+) {
+#ifdef DISABLE_SYCL_INSTRUMENTATION_METADATA
+  const detail::code_location &CodeLoc = {};
+#endif
   return malloc_host<T>(Count, Q.get_context(), PropList, CodeLoc);
 }
 
 template <typename T>
 T *malloc_shared(
     size_t Count, const device &Dev, const context &Ctxt,
-    const property_list &PropList = {},
-    const detail::code_location &CodeLoc = detail::code_location::current()) {
+    const property_list &PropList = {}
+#ifndef DISABLE_SYCL_INSTRUMENTATION_METADATA
+    ,
+    const detail::code_location &CodeLoc = detail::code_location::current()
+#endif
+) {
+#ifdef DISABLE_SYCL_INSTRUMENTATION_METADATA
+  const detail::code_location &CodeLoc = {};
+#endif
   return static_cast<T *>(aligned_alloc_shared(alignof(T), Count * sizeof(T),
                                                Dev, Ctxt, PropList, CodeLoc));
 }
 
 template <typename T>
 T *malloc_shared(
-    size_t Count, const queue &Q, const property_list &PropList = {},
-    const detail::code_location &CodeLoc = detail::code_location::current()) {
+    size_t Count, const queue &Q, const property_list &PropList = {}
+#ifndef DISABLE_SYCL_INSTRUMENTATION_METADATA
+    ,
+    const detail::code_location &CodeLoc = detail::code_location::current()
+#endif
+) {
+#ifdef DISABLE_SYCL_INSTRUMENTATION_METADATA
+  const detail::code_location &CodeLoc = {};
+#endif
   return malloc_shared<T>(Count, Q.get_device(), Q.get_context(), PropList,
                           CodeLoc);
 }
@@ -357,8 +413,15 @@ T *malloc_shared(
 template <typename T>
 T *aligned_alloc_host(
     size_t Alignment, size_t Count, const context &Ctxt,
-    const property_list &PropList = {},
-    const detail::code_location &CodeLoc = detail::code_location::current()) {
+    const property_list &PropList = {}
+#ifndef DISABLE_SYCL_INSTRUMENTATION_METADATA
+    ,
+    const detail::code_location &CodeLoc = detail::code_location::current()
+#endif
+) {
+#ifdef DISABLE_SYCL_INSTRUMENTATION_METADATA
+  const detail::code_location &CodeLoc = {};
+#endif
   return static_cast<T *>(aligned_alloc_host(std ::max(Alignment, alignof(T)),
                                              Count * sizeof(T), Ctxt, PropList,
                                              CodeLoc));
@@ -367,8 +430,15 @@ T *aligned_alloc_host(
 template <typename T>
 T *aligned_alloc_host(
     size_t Alignment, size_t Count, const queue &Q,
-    const property_list &PropList = {},
-    const detail::code_location &CodeLoc = detail::code_location::current()) {
+    const property_list &PropList = {}
+#ifndef DISABLE_SYCL_INSTRUMENTATION_METADATA
+    ,
+    const detail::code_location &CodeLoc = detail::code_location::current()
+#endif
+) {
+#ifdef DISABLE_SYCL_INSTRUMENTATION_METADATA
+  const detail::code_location &CodeLoc = {};
+#endif
   return aligned_alloc_host<T>(Alignment, Count, Q.get_context(), PropList,
                                CodeLoc);
 }
@@ -376,8 +446,15 @@ T *aligned_alloc_host(
 template <typename T>
 T *aligned_alloc_shared(
     size_t Alignment, size_t Count, const device &Dev, const context &Ctxt,
-    const property_list &PropList = {},
-    const detail::code_location &CodeLoc = detail::code_location::current()) {
+    const property_list &PropList = {}
+#ifndef DISABLE_SYCL_INSTRUMENTATION_METADATA
+    ,
+    const detail::code_location &CodeLoc = detail::code_location::current()
+#endif
+) {
+#ifdef DISABLE_SYCL_INSTRUMENTATION_METADATA
+  const detail::code_location &CodeLoc = {};
+#endif
   return static_cast<T *>(aligned_alloc_shared(max(Alignment, alignof(T)),
                                                Count * sizeof(T), Dev, Ctxt,
                                                PropList, CodeLoc));
@@ -386,8 +463,15 @@ T *aligned_alloc_shared(
 template <typename T>
 T *aligned_alloc_shared(
     size_t Alignment, size_t Count, const queue &Q,
-    const property_list &PropList = {},
-    const detail::code_location &CodeLoc = detail::code_location::current()) {
+    const property_list &PropList = {}
+#ifndef DISABLE_SYCL_INSTRUMENTATION_METADATA
+    ,
+    const detail::code_location &CodeLoc = detail::code_location::current()
+#endif
+) {
+#ifdef DISABLE_SYCL_INSTRUMENTATION_METADATA
+  const detail::code_location &CodeLoc = {};
+#endif
   return aligned_alloc_shared<T>(Alignment, Count, Q.get_device(),
                                  Q.get_context(), PropList, CodeLoc);
 }
@@ -395,8 +479,15 @@ T *aligned_alloc_shared(
 template <typename T>
 T *malloc(
     size_t Count, const device &Dev, const context &Ctxt, usm::alloc Kind,
-    const property_list &PropList = {},
-    const detail::code_location &CodeLoc = detail::code_location::current()) {
+    const property_list &PropList = {}
+#ifndef DISABLE_SYCL_INSTRUMENTATION_METADATA
+    ,
+    const detail::code_location &CodeLoc = detail::code_location::current()
+#endif
+) {
+#ifdef DISABLE_SYCL_INSTRUMENTATION_METADATA
+  const detail::code_location &CodeLoc = {};
+#endif
   return static_cast<T *>(aligned_alloc(alignof(T), Count * sizeof(T), Dev,
                                         Ctxt, Kind, PropList, CodeLoc));
 }
@@ -404,8 +495,15 @@ T *malloc(
 template <typename T>
 T *malloc(
     size_t Count, const queue &Q, usm::alloc Kind,
-    const property_list &PropList = {},
-    const detail::code_location &CodeLoc = detail::code_location::current()) {
+    const property_list &PropList = {}
+#ifndef DISABLE_SYCL_INSTRUMENTATION_METADATA
+    ,
+    const detail::code_location &CodeLoc = detail::code_location::current()
+#endif
+) {
+#ifdef DISABLE_SYCL_INSTRUMENTATION_METADATA
+  const detail::code_location &CodeLoc = {};
+#endif
   return malloc<T>(Count, Q.get_device(), Q.get_context(), Kind, PropList,
                    CodeLoc);
 }
@@ -413,8 +511,15 @@ T *malloc(
 template <typename T>
 T *aligned_alloc(
     size_t Alignment, size_t Count, const device &Dev, const context &Ctxt,
-    usm::alloc Kind, const property_list &PropList = {},
-    const detail::code_location &CodeLoc = detail::code_location::current()) {
+    usm::alloc Kind, const property_list &PropList = {}
+#ifndef DISABLE_SYCL_INSTRUMENTATION_METADATA
+    ,
+    const detail::code_location &CodeLoc = detail::code_location::current()
+#endif
+) {
+#ifdef DISABLE_SYCL_INSTRUMENTATION_METADATA
+  const detail::code_location &CodeLoc = {};
+#endif
   return static_cast<T *>(aligned_alloc(max(Alignment, alignof(T)),
                                         Count * sizeof(T), Dev, Ctxt, Kind,
                                         PropList, CodeLoc));
@@ -423,8 +528,15 @@ T *aligned_alloc(
 template <typename T>
 T *aligned_alloc(
     size_t Alignment, size_t Count, const queue &Q, usm::alloc Kind,
-    const property_list &PropList = {},
-    const detail::code_location &CodeLoc = detail::code_location::current()) {
+    const property_list &PropList = {}
+#ifndef DISABLE_SYCL_INSTRUMENTATION_METADATA
+    ,
+    const detail::code_location &CodeLoc = detail::code_location::current()
+#endif
+) {
+#ifdef DISABLE_SYCL_INSTRUMENTATION_METADATA
+  const detail::code_location &CodeLoc = {};
+#endif
   return aligned_alloc<T>(Alignment, Count, Q.get_device(), Q.get_context(),
                           Kind, PropList, CodeLoc);
 }
