05ef42981bb135c0c169d2e123f56a220a4bda5c
Andrei Elovikov <andrei.elovikov@intel.com>
2022-07-28 10:37:24 -0700

    Revert part of the change from future PR

--- sycl/include/sycl/detail/defines_elementary.hpp
+++ sycl/include/sycl/detail/defines_elementary.hpp
@@ -10,8 +10,6 @@
 
 #define __SYCL_INLINE_NAMESPACE(X) inline namespace X
 
-#define __SYCL_INLINE_VER_NAMESPACE(X) inline namespace X
-
 #ifndef __has_attribute
 #define __has_attribute(x) 0
 #endif
