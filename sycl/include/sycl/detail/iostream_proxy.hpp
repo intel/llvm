intel/llvm

commit b5705a3454adeb6ea3d8b77fe45dbdd6efe6cace

[SYCL] Remove <iostream> from header and source files (#6469)
-Removed <iostream> headers from header and source files to best conform to LLVM Coding Standards : https://llvm.org/docs/CodingStandards.html#include-iostream-is-forbidden .

-Substituted the removed headers with a proxy header which uses external linkage to resolve symbols in <iostream>

sycl/include/sycl/detail/iostream_proxy.hpp
@@ -1,0 +1,10 @@
+#pragma once
+#include <istream>
+#include <ostream>
+
+namespace std {
+extern istream cin;  /// Linked to standard input
+extern ostream cout; /// Linked to standard output
+extern ostream cerr; /// Linked to standard error (unbuffered)
+extern ostream clog; /// Linked to standard error (buffered)
+} // namespace std

sycl/include/sycl/backend_types.hpp
@@ -9,9 +9,9 @@
 #pragma once
 
 #include <sycl/detail/defines.hpp>
+#include <sycl/detail/iostream_proxy.hpp>
 
 #include <fstream>
-#include <iostream>
 #include <istream>
 #include <string>
 

sycl/include/sycl/detail/common.hpp
@@ -116,7 +116,7 @@ static inline std::string codeToString(pi_int32 code) {
                           "Native API returns: "
 
 #ifndef __SYCL_SUPPRESS_PI_ERROR_REPORT
-#include <iostream>
+#include <sycl/detail/iostream_proxy.hpp>
 // TODO: rename all names with direct use of OCL/OPENCL to be backend agnostic.
 #define __SYCL_REPORT_PI_ERR_TO_STREAM(expr)                                   \
   {                                                                            \

sycl/include/sycl/detail/device_filter.hpp
@@ -10,9 +10,9 @@
 
 #include <sycl/backend_types.hpp>
 #include <sycl/detail/defines.hpp>
+#include <sycl/detail/iostream_proxy.hpp>
 #include <sycl/info/info_desc.hpp>
 
-#include <iostream>
 #include <string>
 
 __SYCL_INLINE_NAMESPACE(cl) {

sycl/include/sycl/detail/image_accessor_util.hpp
@@ -15,12 +15,12 @@
 #include <sycl/builtins.hpp>
 #include <sycl/detail/export.hpp>
 #include <sycl/detail/generic_type_traits.hpp>
+#include <sycl/detail/iostream_proxy.hpp>
 #include <sycl/image.hpp>
 #include <sycl/sampler.hpp>
 #include <sycl/types.hpp>
 
 #include <cmath>
-#include <iostream>
 
 __SYCL_INLINE_NAMESPACE(cl) {
 namespace sycl {

sycl/include/sycl/ext/intel/esimd/simd.hpp
@@ -22,7 +22,7 @@
 #include <sycl/ext/oneapi/experimental/invoke_simd.hpp>
 
 #ifndef __SYCL_DEVICE_ONLY__
-#include <iostream>
+#include <sycl/detail/iostream_proxy.hpp>
 #endif // __SYCL_DEVICE_ONLY__
 
 __SYCL_INLINE_NAMESPACE(cl) {

sycl/include/sycl/half_type.hpp
@@ -10,10 +10,10 @@
 
 #include <sycl/detail/defines.hpp>
 #include <sycl/detail/export.hpp>
+#include <sycl/detail/iostream_proxy.hpp>
 #include <sycl/detail/type_traits.hpp>
 
 #include <functional>
-#include <iostream>
 #include <limits>
 
 #if !__has_builtin(__builtin_expect)

sycl/plugins/esimd_emulator/pi_esimd_emulator.cpp
@@ -35,6 +35,7 @@
 #include <cstdio>
 #include <cstring>
 #include <functional>
+#include <iostream>
 #include <map>
 #include <memory>
 #include <string>

commit 1ed8ad7ad1e6329e49a90f5b35fce7d068af248b

Quickfix for the build breakage after #6469

sycl/include/sycl/detail/iostream_proxy.hpp
@@ -3,8 +3,18 @@
 #include <ostream>
 
 namespace std {
-extern istream cin;  /// Linked to standard input
-extern ostream cout; /// Linked to standard output
-extern ostream cerr; /// Linked to standard error (unbuffered)
-extern ostream clog; /// Linked to standard error (buffered)
+#ifdef _WIN32
+#define __SYCL_EXTERN_STREAM_ATTRS __declspec(dllimport)
+#else
+#define __SYCL_EXTERN_STREAM_ATTRS
+#endif // _WIN32
+/// Linked to standard input
+extern __SYCL_EXTERN_STREAM_ATTRS istream cin;
+/// Linked to standard output
+extern __SYCL_EXTERN_STREAM_ATTRS ostream cout;
+/// Linked to standard error (unbuffered)
+extern __SYCL_EXTERN_STREAM_ATTRS ostream cerr;
+/// Linked to standard error (buffered)
+extern __SYCL_EXTERN_STREAM_ATTRS ostream clog;
+#undef __SYCL_EXTERN_STREAM_ATTRS
 } // namespace std