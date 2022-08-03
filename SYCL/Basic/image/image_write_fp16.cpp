5f2aadc67c44461cb4f00832c9f05c1190419a0b
Hugh Delaney <46290137+hdelan@users.noreply.github.com>
2022-08-03 18:22:02 +0100

    [SYCL] Disable failing test for CUDA (intel/llvm-test-suite#1127)

--- SYCL/Basic/image/image_write_fp16.cpp
+++ SYCL/Basic/image/image_write_fp16.cpp
@@ -1,4 +1,4 @@
-// UNSUPPORTED: hip
+// UNSUPPORTED: hip || cuda
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
 // RUN: %HOST_RUN_PLACEHOLDER %t.out
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
