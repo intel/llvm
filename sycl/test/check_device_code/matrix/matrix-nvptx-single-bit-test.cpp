48978cc8b07819fc532880467525a2b07036dc46
JackAKirk <jack.kirk@codeplay.com>
2022-08-04 09:38:06 +0100

    fix test
    Signed-off-by: JackAKirk <jack.kirk@codeplay.com>

--- sycl/test/check_device_code/matrix/matrix-nvptx-single-bit-test.cpp
+++ sycl/test/check_device_code/matrix/matrix-nvptx-single-bit-test.cpp
@@ -9,10 +9,10 @@ using namespace sycl::ext::oneapi::experimental::matrix;
 
 // M, N, (K * 32) define the sizes of dimensions of the three matrix types (a,
 // b, accumulator) used per subgroup operation.
-constexpr int M = 8; // number of rows of accumulator,
-                     // number of cols of b.
-constexpr int N = 8; // number of cols of accumulator,
-                     // number of rows of a.
+constexpr int M = 8;   // number of rows of accumulator,
+                       // number of cols of b.
+constexpr int N = 8;   // number of cols of accumulator,
+                       // number of rows of a.
 constexpr int K = 128; // number of cols of a/number of rows of b divided by 32
 
 // Each bit of each uint32_t A/B array element is an element of a single-bit
@@ -43,19 +43,19 @@ int main() {
     cgh.parallel_for<class row_col>(
         nd_range<2>({1, 32}, {1, 32}),
         [=](nd_item<2> item) [[sycl::reqd_work_group_size(1, 1, 32)]] {
-          sycl::sub_group sg = item.get_sub_group();
+      sycl::sub_group sg = item.get_sub_group();
 
-          joint_matrix<int32_t, matrix_use::accumulator, M, N,
-                       matrix_layout::row_major>
-              sub_c;
+      joint_matrix<int32_t, matrix_use::accumulator, M, N,
+                   matrix_layout::row_major>
+          sub_c;
 
-          joint_matrix<precision::b1, matrix_use::a, M, K, matrix_layout::row_major>
-              sub_a;
+      joint_matrix<precision::b1, matrix_use::a, M, K, matrix_layout::row_major>
+          sub_a;
 
-          joint_matrix<precision::b1, matrix_use::b, K, N, matrix_layout::col_major>
-              sub_b;
+      joint_matrix<precision::b1, matrix_use::b, K, N, matrix_layout::col_major>
+          sub_b;
 
-          //CHECK: tail call { i32, i32 } @llvm.nvvm.wmma.m8n8k128.load.c.row.stride.s32.p1i32(i32 addrspace(1)* %_arg_, i32 8) #{{.*}}
+          //CHECK: tail call { i32, i32 } @llvm.nvvm.wmma.m8n8k128.load.c.row.stride.s32.p1i32(i32 addrspace(1)* %_arg_accC, i32 8) #{{.*}}
           joint_matrix_load(sg, sub_c, accC.get_pointer(), N);
           //CHECK: tail call i32 @llvm.nvvm.wmma.m8n8k128.load.a.row.stride.b1.p0i32(i32* %call.ascast.i.i{{.*}}.i, i32 128) #{{.*}}
           joint_matrix_load(sg, sub_a, accA.get_pointer(), K);
@@ -67,7 +67,7 @@ int main() {
           //CHECK: tail call { i32, i32 } @llvm.nvvm.wmma.m8n8k128.mma.and.popc.row.col.b1(i32 %3, i32 %4, i32 %6, i32 %7) #{{.*}}
           sub_c = joint_matrix_bmad(sg, sub_a, sub_b, sub_c,
                                     sycl::bit_and<precision::b1>());
-          //CHECK: tail call void @llvm.nvvm.wmma.m8n8k128.store.d.row.stride.s32.p1i32(i32 addrspace(1)* %_arg_14, i32 %9, i32 %10, i32 8) #{{.*}}
+          //CHECK: tail call void @llvm.nvvm.wmma.m8n8k128.store.d.row.stride.s32.p1i32(i32 addrspace(1)* %_arg_accD, i32 %9, i32 %10, i32 8) #{{.*}}
           joint_matrix_store(sg, sub_c, accD.get_pointer(), N);
         });
   });
