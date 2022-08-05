d7df3bf09e43225597a4e3b3587d63da549aad66
Tadej Ciglarič <tadej.c@gmail.com>
2022-08-05 22:41:54 +0200

    [SYCL][CUDA] Fix compile command in gpu.cpp (intel/llvm-test-suite#1088)
    Fixes compile command in gpu.hpp, fixing the test for CUDA backend.

--- SYCL/Regression/commandlist/gpu.cpp
+++ SYCL/Regression/commandlist/gpu.cpp
@@ -1,6 +1,6 @@
 // REQUIRES: gpu, linux
-// UNSUPPORTED: cuda || hip
+// UNSUPPORTED: hip
 // UNSUPPORTED: ze_debug-1,ze_debug4
 //
-// RUN: %clangxx -fsycl %S/Inputs/FindPrimesSYCL.cpp %S/Inputs/main.cpp -o %t.out -lpthread
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %S/Inputs/FindPrimesSYCL.cpp %S/Inputs/main.cpp -o %t.out -lpthread
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
