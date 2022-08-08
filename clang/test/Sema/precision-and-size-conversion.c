96b9aac1f5e0b6ae9b99a2f5df7055f6ba69ca2d
srividya sundaram <srividya.sundaram@intel.com>
2022-08-08 12:44:49 -0700

    Fix test

--- clang/test/Sema/precision-and-size-conversion.c
+++ clang/test/Sema/precision-and-size-conversion.c
@@ -1,26 +1,20 @@
 // RUN: %clang_cc1 -fsyntax-only -verify=precision-loss,precision-gain,size-change -Wimplicit-float-conversion -Wdouble-promotion -Wimplicit-float-size-conversion \
-// RUN:   -nostdsysteminc -nobuiltininc -isystem %S/Inputs \
-// RUN:   -triple x86_64-apple-darwin %s -Wno-unreachable-code
+// RUN:   -triple x86_64-apple-darwin %s
 
 // RUN: %clang_cc1 -fsyntax-only -verify=size-only,precision-gain,size-change -Wdouble-promotion -Wimplicit-float-size-conversion \
-// RUN:   -nostdsysteminc -nobuiltininc -isystem %S/Inputs \
-// RUN:   -triple x86_64-apple-darwin %s -Wno-unreachable-code
+// RUN:   -triple x86_64-apple-darwin %s
 
 // RUN: %clang_cc1 -fsyntax-only -verify=precision-increase -Wdouble-promotion \
-// RUN:   -nostdsysteminc -nobuiltininc -isystem %S/Inputs \
-// RUN:   -triple x86_64-apple-darwin %s -Wno-unreachable-code
+// RUN:   -triple x86_64-apple-darwin %s
 
 // RUN: %clang_cc1 -fsyntax-only -verify=precision-loss,size-change -Wimplicit-float-conversion \
-// RUN:   -nostdsysteminc -nobuiltininc -isystem %S/Inputs \
-// RUN:   -triple x86_64-apple-darwin %s -Wno-unreachable-code
+// RUN:   -triple x86_64-apple-darwin %s
 
 // RUN: %clang_cc1 -fsyntax-only -verify \
-// RUN:   -nostdsysteminc -nobuiltininc -isystem %S/Inputs \
-// RUN:   -triple x86_64-apple-darwin %s -Wno-unreachable-code
+// RUN:   -triple x86_64-apple-darwin %s
 
 // This test checks that floating point conversion warnings are emitted correctly when used in conjunction.
 
-#include <conversion.h>
 // expected-no-diagnostics
 // size-only-warning@+2 {{implicit conversion between floating point types of different sizes}}
 // precision-loss-warning@+1 {{implicit conversion loses floating-point precision: 'double' to 'float'}}
