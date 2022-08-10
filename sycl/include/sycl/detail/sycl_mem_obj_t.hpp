2b6bc765ef7a27d6c7e878e0bf3c958f1a1ddb3b
Rauf, Rana <rana.rauf@intel.com>
2022-08-10 07:07:21 -0700

    formatting
    Signed-off-by: Rauf, Rana <rana.rauf@intel.com>

--- sycl/include/sycl/detail/sycl_mem_obj_t.hpp
+++ sycl/include/sycl/detail/sycl_mem_obj_t.hpp
@@ -63,7 +63,6 @@ public:
               std::unique_ptr<SYCLMemObjAllocator> Allocator)
       : SYCLMemObjT(/*SizeInBytes*/ 0, Props, std::move(Allocator)) {}
 
-
   SYCLMemObjT(pi_native_handle MemObject, const context &SyclContext,
               const size_t SizeInBytes, event AvailableEvent,
               std::unique_ptr<SYCLMemObjAllocator> Allocator);
@@ -71,7 +70,8 @@ public:
   SYCLMemObjT(cl_mem MemObject, const context &SyclContext,
               event AvailableEvent,
               std::unique_ptr<SYCLMemObjAllocator> Allocator)
-      : SYCLMemObjT(pi::cast<pi_native_handle>(MemObject), SyclContext, /*SizeInBytes*/(size_t) 0, AvailableEvent,
+      : SYCLMemObjT(pi::cast<pi_native_handle>(MemObject), SyclContext,
+                    /*SizeInBytes*/ (size_t)0, AvailableEvent,
                     std::move(Allocator)) {}
 
   SYCLMemObjT(pi_native_handle MemObject, const context &SyclContext,
@@ -280,7 +280,6 @@ public:
   __SYCL_DLL_LOCAL void setAlign(size_t RequiredAlign) {
     MAllocator->setAlignment(RequiredAlign);
   }
-
 
   static size_t getBufSizeForContext(const ContextImplPtr &Context,
                                      pi_native_handle MemObject);
