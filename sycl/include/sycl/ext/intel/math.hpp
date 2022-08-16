d9c2426868c5ad0021cd921c5f61db3cfceca09e
jinge90 <ge.jin@intel.com>
2022-08-16 12:41:59 +0800

    use compile-time if
    Signed-off-by: jinge90 <ge.jin@intel.com>

--- sycl/include/sycl/ext/intel/math.hpp
+++ sycl/include/sycl/ext/intel/math.hpp
@@ -9,11 +9,23 @@
 //===----------------------------------------------------------------------===//
 
 #pragma once
+#include <sycl/half_type.hpp>
 #include <type_traits>
+
+// _iml_half_internal is internal representation for fp16 type used in intel
+// math device library. The definition here should align with definition in
+// https://github.com/intel/llvm/blob/sycl/libdevice/imf_half.hpp
+#if defined(__SPIR__)
+typedef _Float16 _iml_half_internal;
+#else
+typedef uint16_t _iml_half_internal;
+#endif
+
 extern "C" {
 float __imf_saturatef(float);
 float __imf_copysignf(float, float);
 double __imf_copysign(double, double);
+_iml_half_internal __imf_copysignf16(_iml_half_internal, _iml_half_internal);
 };
 
 namespace sycl {
@@ -21,23 +33,35 @@ __SYCL_INLINE_VER_NAMESPACE(_V1) {
 namespace ext {
 namespace intel {
 namespace math {
+
+#if __cplusplus >= 201703L
 template <typename Tp> Tp saturate(Tp x) {
   static_assert(std::is_same<Tp, float>::value,
                 "sycl::ext::intel::math::saturate only supports fp32 version.");
-  if (std::is_same<Tp, float>::value)
+  if constexpr (std::is_same<Tp, float>::value)
     return __imf_saturatef(x);
 }
 
 template <typename Tp> Tp copysign(Tp x, Tp y) {
-  static_assert(
-      std::is_same<Tp, float>::value || std::is_same<Tp, double>::value,
-      "sycl::ext::intel::math::copysign only supports fp32, fp64 version.");
-  if (std::is_same<Tp, float>::value)
+  static_assert(std::is_same<Tp, float>::value ||
+                    std::is_same<Tp, double>::value ||
+                    std::is_same<Tp, sycl::half>::value,
+                "sycl::ext::intel::math::copysign only supports fp16, fp32, "
+                "fp64 version.");
+  if constexpr (std::is_same<Tp, float>::value)
     return __imf_copysignf(x, y);
-  if (std::is_same<Tp, double>::value)
+  if constexpr (std::is_same<Tp, double>::value)
     return __imf_copysign(x, y);
+  if constexpr (std::is_same<Tp, sycl::half>::value) {
+    static_assert(sizeof(sycl::half) == sizeof(_iml_half_internal),
+                  "sycl::half is not compatible with _iml_half_internal.");
+    _iml_half_internal xi = __builtin_bit_cast(_iml_half_internal, x);
+    _iml_half_internal yi = __builtin_bit_cast(_iml_half_internal, y);
+    return __builtin_bit_cast(sycl::half, __imf_copysignf16(xi, yi));
+  }
 }
 
+#endif
 } // namespace math
 } // namespace intel
 } // namespace ext
