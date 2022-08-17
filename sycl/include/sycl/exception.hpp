c759789c9d606d23d387eac19f0c84c4360ca16f
Steffen Larsen <steffen.larsen@intel.com>
2022-08-17 11:22:13 +0100

    [SYCL][ABI-Break] Remove sycl::exception error code member workaround (#6578)
    To avoid ABI break, sycl::exception would embed the error code into the
    error string. With the ABI-break window open, this commit promotes the
    embedded error code to a member of sycl::exception.
    
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- sycl/include/sycl/exception.hpp
+++ sycl/include/sycl/exception.hpp
@@ -102,6 +102,7 @@ private:
   std::shared_ptr<std::string> MMsg;
   pi_int32 MPIErr;
   std::shared_ptr<context> MContext;
+  std::error_code MErrC = make_error_code(sycl::errc::invalid);
 
 protected:
   // these two constructors are no longer used. Kept for ABI compatability.
