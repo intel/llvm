dc7148b8a7f6118f90ba424261880721626bb8b6
aelovikov-intel <andrei.elovikov@intel.com>
2022-08-17 17:20:59 -0700

    [SYCL] Re-enable SYCL/Reduction/ tests (intel/llvm-test-suite#1153)
    reduction_nd_lambda.cpp was supposed to be re-enabled together with
    https://github.com/intel/llvm-test-suite/pull/1134/.
    
    For reduction_big_data.cpp the logs with failure expired, not sure how
    to deal with it. I did very brief local run of it which passed. As such,
    re-enable and deal with it when/if it fails to gather some input data.

--- SYCL/Reduction/reduction_nd_lambda.cpp
+++ SYCL/Reduction/reduction_nd_lambda.cpp
@@ -1,5 +1,3 @@
-// Test disabled due to sporadic failure
-// REQUIRES: TEMPORARILY_DISABLED
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
 // RUNx: %HOST_RUN_PLACEHOLDER %t.out
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
