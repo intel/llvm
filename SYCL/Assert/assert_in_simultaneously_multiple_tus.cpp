234f14216f584f76f0a96f0e1db45efb85ee3f05
Steffen Larsen <steffen.larsen@intel.com>
2022-08-22 17:46:02 +0100

    [SYCL] Temporarily disable assert_in_simultaneously_multiple_tus for CUDA (intel/llvm-test-suite#1073)
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- SYCL/Assert/assert_in_simultaneously_multiple_tus.cpp
+++ SYCL/Assert/assert_in_simultaneously_multiple_tus.cpp
@@ -1,3 +1,6 @@
+// FIXME flaky fail on CUDA
+// UNSUPPORTED: cuda
+//
 // RUN: %clangxx -DSYCL_FALLBACK_ASSERT=1 -fsycl -fsycl-targets=%sycl_triple -I %S/Inputs %s %S/Inputs/kernels_in_file2.cpp -o %t.out %threads_lib
 // RUN: %CPU_RUN_PLACEHOLDER %t.out &> %t.txt || true
 // RUN: %CPU_RUN_PLACEHOLDER FileCheck %s --input-file %t.txt
