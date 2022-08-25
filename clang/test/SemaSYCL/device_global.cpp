35e6208245691be8daa3402e9ab2e29843dc87fa
Sindhu Chittireddy <sindhu.chittireddy@intel.com>
2022-08-24 20:13:39 -0700

    Add instantiation call for templFoo()

--- clang/test/SemaSYCL/device_global.cpp
+++ clang/test/SemaSYCL/device_global.cpp
@@ -67,25 +67,26 @@ template <typename T> void fooBar() {
 
 template <typename T> struct TS {
 private:
-  // expected-error@+1 {{'device_global' member variable 'a' should be publicly accessible from namespace scope}}
+  // expected-error@+1 2 {{'device_global' member variable 'a' should be publicly accessible from namespace scope}}
   static device_global<T> a;
-  // expected-error@+1 {{'device_global' variable must be a static data member or declared in global or namespace scope}}
+  // expected-error@+1 2 {{'device_global' variable must be a static data member or declared in global or namespace scope}}
   device_global<T> b;
   // expected-error@+2 {{'device_global' member variable 'c' should be publicly accessible from namespace scope}}
-  // expected-error@+1 {{'device_global' variable must be a static data member or declared in global or namespace scope}}
+  // expected-error@+1 2 {{'device_global' variable must be a static data member or declared in global or namespace scope}}
   device_global<int> c;
 
 public:
   static device_global<T> d;
-  // expected-error@+1 {{'device_global' variable must be a static data member or declared in global or namespace scope}}
+  // expected-error@+1 2 {{'device_global' variable must be a static data member or declared in global or namespace scope}}
   device_global<T> e;
-  // expected-error@+1 {{'device_global' variable must be a static data member or declared in global or namespace scope}}
+  // expected-error@+1 2 {{'device_global' variable must be a static data member or declared in global or namespace scope}}
   device_global<int> f;
 };
 
 // expected-note@+1 {{in instantiation of template class 'TS<int>' requested here}}
 TS<int> AAAA;
 
+//expected-note@+2 {{in instantiation of template class 'TS<char>' requested here}}
 template <typename T> void templFoo () {
   TS<T> Var;
 }
@@ -103,6 +104,9 @@ template<> void templFoo<int>();
 union [[__sycl_detail__::device_global]] [[__sycl_detail__::global_variable_allowed]] a_union;
 
 int main() {
+  // expected-note@+1{{in instantiation of function template specialization 'templFoo<char>' requested here}}
+  templFoo<char>();
+
   // expected-note@+1{{in instantiation of function template specialization 'fooBar<int>' requested here}}
   fooBar<int>();
 
