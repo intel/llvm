849ba17cc72e8a5d61e9a6d12c069cfb4d3fa146
smaslov-intel <sergey.v.maslov@intel.com>
2022-09-06 10:50:27 -0700

    [SYCL] Support query of free device memory extension (intel/llvm-test-suite#1162)

---
+++ SYCL/Plugin/level_zero_device_free_mem.cpp
@@ -0,0 +1,31 @@
+// REQUIRES: level_zero, level_zero_dev_kit
+
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %level_zero_options %s -o %t.out
+// RUN: env ZES_ENABLE_SYSMAN=1 %GPU_RUN_PLACEHOLDER %t.out 2>&1 %GPU_CHECK_PLACEHOLDER
+//
+// The test is to check that the free device memory is reported be Level Zero
+// backend
+//
+// CHECK: Free device memory
+
+#include <iostream>
+#include <sycl/sycl.hpp>
+using namespace sycl;
+
+int main() {
+
+  queue Queue;
+  auto dev = Queue.get_device();
+  std::cout << "Device: " << dev.get_info<info::device::name>() << std::endl;
+
+  if (!dev.is_host() && dev.has(aspect::ext_intel_free_memory)) {
+    auto FreeMemory = dev.get_info<info::device::ext_intel_free_memory>();
+    std::cout << "Free device memory: " << FreeMemory << std::endl;
+  } else {
+    std::cout
+        << "Query ext_intel_device_info_free_memory not supported by the device"
+        << std::endl;
+  }
+
+  return 0;
+}
