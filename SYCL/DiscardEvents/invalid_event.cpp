b344fd0c3454feab178d838d774bb51e3238fd81
Steffen Larsen <steffen.larsen@intel.com>
2022-09-06 13:46:52 +0100

    [SYCL] Remove host run and dependencies from SYCL/DiscardEvents tests (intel/llvm-test-suite#1204)
    This commit removes the host run and any assumptions and operations
    related to the host device from the tests in SYCL/DiscardEvents.
    
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>
    Co-authored-by: Sachkov, Alexey <alexey.sachkov@intel.com>

--- SYCL/DiscardEvents/invalid_event.cpp
+++ SYCL/DiscardEvents/invalid_event.cpp
@@ -2,7 +2,6 @@
 // UNSUPPORTED: hip, (opencl && accelerator)
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
 //
-// RUN: %HOST_RUN_PLACEHOLDER %t.out
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 // RUN: %ACC_RUN_PLACEHOLDER %t.out
