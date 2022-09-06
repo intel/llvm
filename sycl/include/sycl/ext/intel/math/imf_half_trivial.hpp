b770a87dd827d84d9b6c30b030d7f5891f8d60e6
jinge90 <ge.jin@intel.com>
2022-09-06 22:45:19 +0800

    Add hmax,hmax_nan,hmin,hmin_nan
    Signed-off-by: jinge90 <ge.jin@intel.com>

--- sycl/include/sycl/ext/intel/math/imf_half_trivial.hpp
+++ sycl/include/sycl/ext/intel/math/imf_half_trivial.hpp
@@ -182,96 +182,132 @@ bool hbneu2(sycl::half2 x, sycl::half2 y) {
 }
 
 sycl::half2 heq2(sycl::half2 x, sycl::half2 y) {
-  sycl::half2 r;
-  r.s0() = heq(x.s0(), y.s0()) ? 1.0f : 0.f;
-  r.s1() = heq(x.s1(), y.s1()) ? 1.0f : 0.f;
-  return r;
+  return sycl::half2{(heq(x.s0(), y.s0()) ? 1.0f : 0.f),
+                     (heq(x.s1(), y.s1()) ? 1.0f : 0.f)};
 }
 
 sycl::half2 hequ2(sycl::half2 x, sycl::half2 y) {
-  sycl::half2 r;
-  r.s0() = hequ(x.s0(), y.s0()) ? 1.0f : 0.f;
-  r.s1() = hequ(x.s1(), y.s1()) ? 1.0f : 0.f;
-  return r;
+  return sycl::half2{(hequ(x.s0(), y.s0()) ? 1.0f : 0.f),
+                     (hequ(x.s1(), y.s1()) ? 1.0f : 0.f)};
 }
 
 sycl::half2 hge2(sycl::half2 x, sycl::half2 y) {
-  sycl::half2 r;
-  r.s0() = hge(x.s0(), y.s0()) ? 1.0f : 0.f;
-  r.s1() = hge(x.s1(), y.s1()) ? 1.0f : 0.f;
-  return r;
+  return sycl::half2{(hge(x.s0(), y.s0()) ? 1.0f : 0.f),
+                     (hge(x.s1(), y.s1()) ? 1.0f : 0.f)};
 }
 
 sycl::half2 hgeu2(sycl::half2 x, sycl::half2 y) {
-  sycl::half2 r;
-  r.s0() = hgeu(x.s0(), y.s0()) ? 1.0f : 0.f;
-  r.s1() = hgeu(x.s1(), y.s1()) ? 1.0f : 0.f;
-  return r;
+  return sycl::half2{(hgeu(x.s0(), y.s0()) ? 1.0f : 0.f),
+                     (hgeu(x.s1(), y.s1()) ? 1.0f : 0.f)};
 }
 
 sycl::half2 hgt2(sycl::half2 x, sycl::half2 y) {
-  sycl::half2 r;
-  r.s0() = hgt(x.s0(), y.s0()) ? 1.0f : 0.f;
-  r.s1() = hgt(x.s1(), y.s1()) ? 1.0f : 0.f;
-  return r;
+  return sycl::half2{(hgt(x.s0(), y.s0()) ? 1.0f : 0.f),
+                     (hgt(x.s1(), y.s1()) ? 1.0f : 0.f)};
 }
 
 sycl::half2 hgtu2(sycl::half2 x, sycl::half2 y) {
-  sycl::half2 r;
-  r.s0() = hgtu(x.s0(), y.s0()) ? 1.0f : 0.f;
-  r.s1() = hgtu(x.s1(), y.s1()) ? 1.0f : 0.f;
-  return r;
+  return sycl::half2{(hgtu(x.s0(), y.s0()) ? 1.0f : 0.f),
+                     (hgtu(x.s1(), y.s1()) ? 1.0f : 0.f)};
 }
 
 sycl::half2 hle2(sycl::half2 x, sycl::half2 y) {
-  sycl::half2 r;
-  r.s0() = hle(x.s0(), y.s0()) ? 1.0f : 0.f;
-  r.s1() = hle(x.s1(), y.s1()) ? 1.0f : 0.f;
-  return r;
+  return sycl::half2{(hle(x.s0(), y.s0()) ? 1.0f : 0.f),
+                     (hle(x.s1(), y.s1()) ? 1.0f : 0.f)};
 }
 
 sycl::half2 hleu2(sycl::half2 x, sycl::half2 y) {
-  sycl::half2 r;
-  r.s0() = hleu(x.s0(), y.s0()) ? 1.0f : 0.f;
-  r.s1() = hleu(x.s1(), y.s1()) ? 1.0f : 0.f;
-  return r;
+  return sycl::half2{(hleu(x.s0(), y.s0()) ? 1.0f : 0.f),
+                     (hleu(x.s1(), y.s1()) ? 1.0f : 0.f)};
 }
 
 sycl::half2 hlt2(sycl::half2 x, sycl::half2 y) {
-  sycl::half2 r;
-  r.s0() = hlt(x.s0(), y.s0()) ? 1.0f : 0.f;
-  r.s1() = hlt(x.s1(), y.s1()) ? 1.0f : 0.f;
-  return r;
+  return sycl::half2{(hlt(x.s0(), y.s0()) ? 1.0f : 0.f),
+                     (hlt(x.s1(), y.s1()) ? 1.0f : 0.f)};
 }
 
 sycl::half2 hltu2(sycl::half2 x, sycl::half2 y) {
-  sycl::half2 r;
-  r.s0() = hltu(x.s0(), y.s0()) ? 1.0f : 0.f;
-  r.s1() = hltu(x.s1(), y.s1()) ? 1.0f : 0.f;
-  return r;
+  return sycl::half2{(hltu(x.s0(), y.s0()) ? 1.0f : 0.f),
+                     (hltu(x.s1(), y.s1()) ? 1.0f : 0.f)};
 }
 
 sycl::half2 hisnan2(sycl::half2 x) {
-  sycl::half2 r;
-  r.s0() = hisnan(x.s0()) ? 1.0f : 0.f;
-  r.s1() = hisnan(x.s1()) ? 1.0f : 0.f;
-  return r;
+  return sycl::half2{(hisnan(x.s0()) ? 1.0f : 0.f),
+                     (hisnan(x.s1()) ? 1.0f : 0.f)};
 }
 
 sycl::half2 hne2(sycl::half2 x, sycl::half2 y) {
-  sycl::half2 r;
-  r.s0() = hne(x.s0(), y.s0()) ? 1.0f : 0.f;
-  r.s1() = hne(x.s1(), y.s1()) ? 1.0f : 0.f;
-  return r;
+  return sycl::half2{(hne(x.s0(), y.s0()) ? 1.0f : 0.f),
+                     (hne(x.s1(), y.s1()) ? 1.0f : 0.f)};
 }
 
 sycl::half2 hneu2(sycl::half2 x, sycl::half2 y) {
-  sycl::half2 r;
-  r.s0() = hneu(x.s0(), y.s0()) ? 1.0f : 0.f;
-  r.s1() = hneu(x.s1(), y.s1()) ? 1.0f : 0.f;
+  return sycl::half2{(hneu(x.s0(), y.s0()) ? 1.0f : 0.f),
+                     (hneu(x.s1(), y.s1()) ? 1.0f : 0.f)};
+}
+
+sycl::half hmax(sycl::half x, sycl::half y) { return sycl::fmax(x, y); }
+
+sycl::half hmax_nan(sycl::half x, sycl::half y) {
+  if (hisnan(x) || hisnan(y))
+    return sycl::half(NAN);
+  else
+    return sycl::fmax(x, y);
+}
+
+sycl::half2 hmax2(sycl::half2 x, sycl::half2 y) {
+  return sycl::half2{hmax(x.s0(), y.s0()), hmax(x.s1(), y.s1())};
+}
+
+sycl::half2 hmax2_nan(sycl::half2 x, sycl::half2 y) {
+  return sycl::half2{hmax_nan(x.s0(), y.s0()), hmax_nan(x.s1(), y.s1())};
+}
+
+sycl::half hmin(sycl::half x, sycl::half y) { return sycl::fmin(x, y); }
+
+sycl::half hmin_nan(sycl::half x, sycl::half y) {
+  if (hisnan(x) || hisnan(y))
+    return sycl::half(NAN);
+  else
+    return sycl::fmin(x, y);
+}
+
+sycl::half2 hmin2(sycl::half2 x, sycl::half2 y) {
+  return sycl::half2{hmin(x.s0(), y.s0()), hmin(x.s1(), y.s1())};
+}
+
+sycl::half2 hmin2_nan(sycl::half2 x, sycl::half2 y) {
+  return sycl::half2{hmin_nan(x.s0(), y.s0()), hmin_nan(x.s1(), y.s1())};
+}
+
+sycl::half2 hcmadd(sycl::half2 x, sycl::half2 y, sycl::half2 z) {
+  return sycl::half2{x.s0() * y.s0() - x.s1() * y.s1() + z.s0(),
+                     x.s0() * y.s1() + x.s1() * y.s0() + z.s1()};
+}
+
+sycl::half hfma_relu(sycl::half x, sycl::half y, sycl::half z) {
+  sycl::half r = sycl::fma(x, y, z);
+  if (!hisnan(r)) {
+    if (r < 0.f)
+      return sycl::half{0.f};
+    else
+      return r;
+  }
   return r;
 }
 
+sycl::half2 hfma2_relu(sycl::half2 x, sycl::half2 y, sycl::half2 z) {
+  sycl::half2 r = sycl::fma(x, y, z);
+  if (!hisnan(r.s0()) && r.s0() < 0.f)
+    r.s0() = 0.f;
+  if (!hisnan(r.s1()) && r.s1() < 0.f)
+    r.s1() = 0.f;
+  return r;
+}
+
+sycl::half habs(sycl::half x) { return sycl::fabs(x); }
+
+sycl::half2 habs2(sycl::half x) { return sycl::fabs(x); }
 } // namespace math
 } // namespace intel
 } // namespace ext
