1b97ae51ee021d78b513a916f66488007e8d5d99
Alexey Sachkov <alexey.sachkov@intel.com>
2022-09-08 01:05:03 +0200

    Temporary disable hanging device_has_aspect L0 test (intel/llvm-test-suite#1234)

--- SYCL/AtomicRef/device_has_aspect_atomic64_level_zero.cpp
+++ SYCL/AtomicRef/device_has_aspect_atomic64_level_zero.cpp
@@ -1,4 +1,4 @@
-// REQUIRES: level_zero, level_zero_dev_kit
+// REQUIRES: level_zero, level_zero_dev_kit, TEMPORARY_DISABLED
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out %level_zero_options
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 
