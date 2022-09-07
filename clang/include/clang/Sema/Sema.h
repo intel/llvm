217493361068e43d24ea80ef2a02e5acb67ea895
Srividya Sundaram <srividya.sundaram@intel.com>
2022-09-07 09:01:04 -0700

    Update Sema.h

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -13975,7 +13975,7 @@ public:
     KernelConstStaticVariable
   };
 
-  bool isDeclAllowedInKernel(const Decl *D);
+  bool isDeclAllowedInSYCLDeviceCode(const Decl *D);
   void checkSYCLDeviceVarDecl(VarDecl *Var);
   void copySYCLKernelAttrs(const CXXRecordDecl *KernelObj);
   void ConstructOpenCLKernel(FunctionDecl *KernelCallerFunc, MangleContext &MC);
