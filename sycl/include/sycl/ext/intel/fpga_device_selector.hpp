4824689dac094e1e9c917bd1ede6f8c9ae729090
Mendell, Mark P <mark.p.mendell@intel.com>
2022-09-07 09:01:13 -0400

    Support Intel FPGA simulator device selector
    Create fpga_simulator_selector to be used to select the FPGA simulator.
    This assumes that the user has compiled their program with -Xssimulation
    (as well as -fintelfpga) to prepare for the simulator.
    
    An object of class fpga_simulator_selector must be constructed early in
    the execution of the host program in order to make the simulator
    available at runtime.
    
    The emulator and hardware FPGA device selectors will prefer the
    emulator/hardware over a simulation device if both have the same device
    name.

--- sycl/include/sycl/ext/intel/fpga_device_selector.hpp
+++ sycl/include/sycl/ext/intel/fpga_device_selector.hpp
@@ -34,6 +34,10 @@ public:
     const platform &pf = device.get_platform();
     const std::string &platform_name = pf.get_info<info::platform::name>();
     if (platform_name == device_platform_name) {
+      const std::string name = device.get_info<sycl::info::device::name>();
+      // Prefer hardware devices over simulator ones.
+      if (name.find("SimulatorDevice") != std::string::npos)
+        return 9000;
       return 10000;
     }
     return -1;
@@ -53,6 +57,37 @@ public:
 class fpga_emulator_selector : public platform_selector {
 public:
   fpga_emulator_selector() : platform_selector(EMULATION_PLATFORM_NAME) {}
+};
+
+class device_name_selector : public device_selector {
+private:
+  std::string device_name;
+
+public:
+  device_name_selector(const std::string &dev_name)
+      : device_name(dev_name) {}
+
+  int operator()(const device &device) const override {
+    if (device.get_info<sycl::info::device::name>() == device_name)
+      return 10000;
+    return -1;
+  }
+};
+
+static constexpr auto SIMULATOR_DEVICE_NAME =
+    "SimulatorDevice : Multi-process Simulator (aclmsim0)";
+
+class fpga_simulator_selector : public device_name_selector {
+public:
+  fpga_simulator_selector()
+    : device_name_selector(SIMULATOR_DEVICE_NAME) {
+    // Tell the runtime to start a simulator device
+#ifdef _WIN32
+    SetEnvironmentVariable("CL_CONTEXT_MPSIM_DEVICE_INTELFPGA", "1");
+#else
+    setenv("CL_CONTEXT_MPSIM_DEVICE_INTELFPGA", "1", 0);
+#endif
+  }
 };
 
 } // namespace intel
