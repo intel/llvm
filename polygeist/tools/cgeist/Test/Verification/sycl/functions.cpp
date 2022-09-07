619819ab31d581c02f662232431c534349e33c3d
Tiotto, Ettore <ettore.tiotto@intel.com>
2022-09-07 11:52:58 -0400

    Fix the polygeist and cgeist lit tests.
    Signed-off-by: Tiotto, Ettore <ettore.tiotto@intel.com>

--- polygeist/tools/cgeist/Test/Verification/sycl/functions.cpp
+++ polygeist/tools/cgeist/Test/Verification/sycl/functions.cpp
@@ -21,7 +21,7 @@
 // CHECK-NEXT: %0 = memref.alloca() : memref<1x!sycl_item_2_1_>
 // CHECK-NEXT: %1 = memref.cast %0 : memref<1x!sycl_item_2_1_> to memref<?x!sycl_item_2_1_>
 // CHECK-NEXT: affine.store %arg0, %0[0] : memref<1x!sycl_item_2_1_>
-// CHECK-NEXT: %2 = sycl.call(%1, %c0_i32) {Function = @get_id, MangledName = @_ZNK2cl4sycl4itemILi2ELb1EE6get_idEi, Type = @item} : (memref<?x!sycl_item_2_1_>, i32) -> i64
+// CHECK-NEXT: %2 = sycl.call(%1, %c0_i32) {Function = @get_id, MangledName = @_ZNK4sycl3_V14itemILi2ELb1EE6get_idEi, Type = @item} : (memref<?x!sycl_item_2_1_>, i32) -> i64
 // CHECK-NEXT: return
 // CHECK-NEXT: }
 
@@ -33,7 +33,7 @@ SYCL_EXTERNAL void method_1(sycl::item<2, true> item) {
 // CHECK-NEXT: %0 = memref.alloca() : memref<1x!sycl_item_2_1_>
 // CHECK-NEXT: %1 = memref.cast %0 : memref<1x!sycl_item_2_1_> to memref<?x!sycl_item_2_1_>
 // CHECK-NEXT: affine.store %arg0, %0[0] : memref<1x!sycl_item_2_1_>
-// CHECK-NEXT: %2 = sycl.call(%1, %1) {Function = @"operator==", MangledName = @_ZNK2cl4sycl4itemILi2ELb1EEeqERKS2_, Type = @item} : (memref<?x!sycl_item_2_1_>, memref<?x!sycl_item_2_1_>) -> i8
+// CHECK-NEXT: %2 = sycl.call(%1, %1) {Function = @"operator==", MangledName = @_ZNK4sycl3_V14itemILi2ELb1EEeqERKS2_, Type = @item} : (memref<?x!sycl_item_2_1_>, memref<?x!sycl_item_2_1_>) -> i8
 // CHECK-NEXT: return
 // CHECK-NEXT: }
 
@@ -48,7 +48,7 @@ SYCL_EXTERNAL void method_2(sycl::item<2, true> item) {
 // CHECK-NEXT: %3 = memref.cast %2 : memref<1x!sycl_id_2_> to memref<?x!sycl_id_2_>
 // CHECK-NEXT: affine.store %arg0, %2[0] : memref<1x!sycl_id_2_>
 // CHECK-NEXT: affine.store %arg1, %0[0] : memref<1x!sycl_id_2_>
-// CHECK-NEXT: %4 = sycl.call(%3, %1) {Function = @"operator==", MangledName = @_ZNK2cl4sycl2idILi2EEeqERKS2_, Type = @id} : (memref<?x!sycl_id_2_>, memref<?x!sycl_id_2_>) -> i8
+// CHECK-NEXT: %4 = sycl.call(%3, %1) {Function = @"operator==", MangledName = @_ZNK4sycl3_V12idILi2EEeqERKS2_, Type = @id} : (memref<?x!sycl_id_2_>, memref<?x!sycl_id_2_>) -> i8
 // CHECK-NEXT: return
 // CHECK-NEXT: }
 
@@ -63,10 +63,10 @@ SYCL_EXTERNAL void op_1(sycl::id<2> a, sycl::id<2> b) {
 // CHECK-NEXT: %1 = memref.cast %0 : memref<1x!sycl_id_2_> to memref<?x!sycl_id_2_>
 // CHECK-NEXT: affine.store %arg0, %0[0] : memref<1x!sycl_id_2_>
 // CHECK-NEXT: %2 = sycl.cast(%1) : (memref<?x!sycl_id_2_>) -> memref<?x!sycl_array_2_>
-// CHECK-NEXT: %3 = sycl.call(%2, %c0_i32) {Function = @get, MangledName = @_ZNK2cl4sycl6detail5arrayILi2EE3getEi, Type = @array} : (memref<?x!sycl_array_2_>, i32) -> i64
-// CHECK-NEXT: %4 = sycl.call(%2, %c1_i32) {Function = @get, MangledName = @_ZNK2cl4sycl6detail5arrayILi2EE3getEi, Type = @array} : (memref<?x!sycl_array_2_>, i32) -> i64
+// CHECK-NEXT: %3 = sycl.call(%2, %c0_i32) {Function = @get, MangledName = @_ZNK4sycl3_V16detail5arrayILi2EE3getEi, Type = @array} : (memref<?x!sycl_array_2_>, i32) -> i64
+// CHECK-NEXT: %4 = sycl.call(%2, %c1_i32) {Function = @get, MangledName = @_ZNK4sycl3_V16detail5arrayILi2EE3getEi, Type = @array} : (memref<?x!sycl_array_2_>, i32) -> i64
 // CHECK-NEXT: %5 = arith.addi %3, %4 : i64
-// CHECK-NEXT: %6 = sycl.call(%5) {Function = @abs, MangledName = @_ZN2cl4sycl3absImEENSt9enable_ifIXsr6detail14is_ugenintegerIT_EE5valueES3_E4typeES3_} : (i64) -> i64
+// CHECK-NEXT: %6 = sycl.call(%5) {Function = @abs, MangledName = @_ZN4sycl3_V13absImEENSt9enable_ifIXsr6detail14is_ugenintegerIT_EE5valueES3_E4typeES3_} : (i64) -> i64
 // CHECK-NEXT: return
 // CHECK-NEXT: }
 
