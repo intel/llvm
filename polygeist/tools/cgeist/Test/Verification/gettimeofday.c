80678be3d77466e0dfa3ab072cb4d60a85d5aeeb
Tsang, Whitney <whitney.tsang@intel.com>
2022-09-08 17:22:10 -0400

    add clang-format on/off
    Signed-off-by: Tsang, Whitney <whitney.tsang@intel.com>

--- polygeist/tools/cgeist/Test/Verification/gettimeofday.c
+++ polygeist/tools/cgeist/Test/Verification/gettimeofday.c
@@ -8,12 +8,13 @@ double alloc() {
   return Tp.tv_sec + Tp.tv_usec * 1.0e-6;
 }
 
+// clang-format off
 // CHECK:   func @alloc() -> f64
 // CHECK-NEXT:     %cst = arith.constant 9.9999999999999995E-7 : f64
 // CHECK-NEXT:     %0 = memref.alloca() : memref<1x2xi64>
 // CHECK-NEXT:     %1 = "polygeist.memref2pointer"(%0) : (memref<1x2xi64>) -> !llvm.ptr<struct<(i64, i64)>>
 // CHECK-NEXT:     %2 = llvm.mlir.null : !llvm.ptr<i8>
-// CHECK:          %{{.*}} = llvm.call @gettimeofday(%1, %{{.*}}) : (!llvm.ptr<struct<(i64, i64)>>, !llvm.ptr<struct<(i32, i32)>>) -> i32
+// CHECK:          %{{.*}} = llvm.call @gettimeofday(%1, %{{.*}}) : (!llvm.ptr<struct<(i64, i64)>>, {{.*}}) -> i32
 // CHECK-NEXT:     [[T5:%.*]] = affine.load %0[0, 0] : memref<1x2xi64>
 // CHECK-NEXT:     [[T6:%.*]]  = arith.sitofp [[T5]] : i64 to f64
 // CHECK-NEXT:     [[T7:%.*]]  = affine.load %0[0, 1] : memref<1x2xi64>
@@ -22,3 +23,4 @@ double alloc() {
 // CHECK-NEXT:     [[T10:%.*]]  = arith.addf [[T6]], [[T9]] : f64
 // CHECK-NEXT:     return [[T10]] : f64
 // CHECK-NEXT:   }
+// clang-format on
