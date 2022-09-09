1a63ddd692430b2b0f06bc672c6121adf3256ecf
Tsang, Whitney <whitney.tsang@intel.com>
2022-09-08 22:32:45 -0400

    dummy
    Signed-off-by: Tsang, Whitney <whitney.tsang@intel.com>

--- polygeist/tools/cgeist/Test/Verification/gettimeofday.c
+++ polygeist/tools/cgeist/Test/Verification/gettimeofday.c
@@ -8,6 +8,7 @@ double alloc() {
   return Tp.tv_sec + Tp.tv_usec * 1.0e-6;
 }
 
+
 // clang-format off
 // CHECK:   func @alloc() -> f64
 // CHECK-NEXT:     %cst = arith.constant 9.9999999999999995E-7 : f64
