ac2f98a270f98fb1fcc6479a66e5f28fb921f9b2
Vyacheslav Klochkov <vyacheslav.n.klochkov@intel.com>
2022-09-12 14:33:11 -0500

    [ESIMD] Test invoke_simd() accepting reference to var holding func ad… (intel/llvm-test-suite#1238)
    [ESIMD] Test invoke_simd() accepting reference to var holding func address
    
    Signed-off-by: Vyacheslav N Klochkov <vyacheslav.n.klochkov@intel.com>

--- SYCL/InvokeSimd/invoke_simd_smoke.cpp
+++ SYCL/InvokeSimd/invoke_simd_smoke.cpp
@@ -37,14 +37,21 @@ ESIMD_CALLEE(float *A, esimd::simd<float, VL> b, int i) SYCL_ESIMD_FUNCTION {
   return a + b;
 }
 
-// Use two functions with the same signature called via invoke_simd for better
-// testing.
+// Use two functions with the same signature but different semantics
+// called via invoke_simd for better testing.
 [[intel::device_indirectly_callable]] SYCL_EXTERNAL
     simd<float, VL> __regcall SIMD_CALLEE1(float *A, simd<float, VL> b,
-                                           int i) SYCL_ESIMD_FUNCTION;
+                                           int i) SYCL_ESIMD_FUNCTION {
+  esimd::simd<float, VL> res = ESIMD_CALLEE(A, b, i);
+  return res;
+}
+
 [[intel::device_indirectly_callable]] SYCL_EXTERNAL
     simd<float, VL> __regcall SIMD_CALLEE2(float *A, simd<float, VL> b,
-                                           int i) SYCL_ESIMD_FUNCTION;
+                                           int i) SYCL_ESIMD_FUNCTION {
+  esimd::simd<float, VL> res = ESIMD_CALLEE(A, b, i);
+  return res + res;
+}
 
 float SPMD_CALLEE(float *A, float b, int i) { return A[i] + b; }
 
@@ -81,16 +88,20 @@ inline auto createExceptionHandler() {
   };
 }
 
-int main(void) {
+// Returns true if the test passed.
+// If the template parameter use_func_directly is set to true, then this
+// test verifies the case when function is passed directly to invoke_simd().
+// Otherwise, the test verifices the case when invoke_simd accepts
+// a reference to a variable holding address of the target function.
+template <bool use_func_directly> bool test() {
   constexpr unsigned Size = 1024;
   constexpr unsigned GroupSize = 4 * VL;
 
   queue q(ESIMDSelector{}, createExceptionHandler());
 
   auto dev = q.get_device();
-  std::cout << "Running on " << dev.get_info<sycl::info::device::name>()
-            << "\n";
-  auto ctxt = q.get_context();
+  std::cout << "Running with use_func_directly = " << use_func_directly
+            << " on " << dev.get_info<sycl::info::device::name>() << "\n";
   float *A = malloc_shared<float>(Size, q);
   float *B = malloc_shared<float>(Size, q);
   float *C = malloc_shared<float>(Size, q);
@@ -108,7 +119,7 @@ int main(void) {
 
   try {
     auto e = q.submit([&](handler &cgh) {
-      cgh.parallel_for<class Test>(
+      cgh.parallel_for(
           Range, [=](nd_item<1> ndi) [[intel::reqd_sub_group_size(VL)]] {
             sub_group sg = ndi.get_sub_group();
             group<1> g = ndi.get_group();
@@ -118,13 +129,26 @@ int main(void) {
             float res = 0;
 
             if constexpr (use_invoke_simd) {
-              res = invoke_simd(sg, SIMD_CALLEE1, uniform{A}, B[wi_id],
-                                uniform{i});
-              res += invoke_simd(sg, SIMD_CALLEE2, uniform{A}, B[wi_id],
-                                 uniform{i});
-
+              if constexpr (use_func_directly) {
+                // Pass SIMD callee directly to invoke_simd.
+                res = invoke_simd(sg, SIMD_CALLEE1, uniform{A}, B[wi_id],
+                                  uniform{i});
+                res += invoke_simd(sg, SIMD_CALLEE2, uniform{A}, B[wi_id],
+                                   uniform{i});
+              } else {
+                // Pass a reference to variable holding SIMD callee address.
+                typedef simd<float, VL> __regcall (*FuncType)(
+                    float *, simd<float, VL>, int);
+                FuncType SIMD_CALLEE1_PTR = SIMD_CALLEE1;
+                FuncType SIMD_CALLEE2_PTR = SIMD_CALLEE2;
+                res = invoke_simd(sg, SIMD_CALLEE1_PTR, uniform{A}, B[wi_id],
+                                  uniform{i});
+                res += invoke_simd(sg, SIMD_CALLEE2_PTR, uniform{A}, B[wi_id],
+                                   uniform{i});
+              }
             } else {
               res = SPMD_CALLEE(A, B[wi_id], wi_id);
+              res += SPMD_CALLEE(A, B[wi_id], wi_id);
               res += SPMD_CALLEE(A, B[wi_id], wi_id);
             }
             C[wi_id] = res;
@@ -136,15 +160,15 @@ int main(void) {
     sycl::free(A, q);
     sycl::free(B, q);
     sycl::free(C, q);
-    return 1;
+    return false;
   }
 
   int err_cnt = 0;
 
   for (unsigned i = 0; i < Size; ++i) {
-    if (2 * (A[i] + B[i]) != C[i]) {
+    if (3 * (A[i] + B[i]) != C[i]) {
       if (++err_cnt < 10) {
-        std::cout << "failed at index " << i << ", " << C[i] << " != 2*("
+        std::cout << "failed at index " << i << ", " << C[i] << " != 3*("
                   << A[i] << " + " << B[i] << ")\n";
       }
     }
@@ -159,19 +183,14 @@ int main(void) {
   sycl::free(C, q);
 
   std::cout << (err_cnt > 0 ? "FAILED\n" : "Passed\n");
-  return err_cnt > 0 ? 1 : 0;
+  return err_cnt == 0;
 }
 
-SYCL_EXTERNAL
-simd<float, VL> __regcall SIMD_CALLEE1(float *A, simd<float, VL> b,
-                                       int i) SYCL_ESIMD_FUNCTION {
-  esimd::simd<float, VL> res = ESIMD_CALLEE(A, b, i);
-  return res;
-}
+int main() {
+  bool Passed = true;
+  Passed &= test<false>();
+  Passed &= test<true>();
 
-SYCL_EXTERNAL
-simd<float, VL> __regcall SIMD_CALLEE2(float *A, simd<float, VL> b,
-                                       int i) SYCL_ESIMD_FUNCTION {
-  esimd::simd<float, VL> res = ESIMD_CALLEE(A, b, i);
-  return res;
+  std::cout << (Passed ? "Passed\n" : "FAILED\n");
+  return Passed ? 0 : 1;
 }
