9fad308da81c2bf7daf1b8e98a865b878474ed6b
Tiotto, Ettore <ettore.tiotto@intel.com>
2022-09-13 14:49:57 -0400

    [SYCLToLLVM]: DialectBuilder genCall member functions should not check for function existance
    Signed-off-by: Tiotto, Ettore <ettore.tiotto@intel.com>

--- mlir-sycl/lib/Conversion/SYCLToLLVM/DialectBuilder.cpp
+++ mlir-sycl/lib/Conversion/SYCLToLLVM/DialectBuilder.cpp
@@ -96,10 +96,7 @@ func::CallOp FuncBuilder::genCall(FlatSymbolRefAttr funcSym, TypeRange resTypes,
 func::CallOp FuncBuilder::genCall(StringRef funcName, TypeRange resTypes,
                                   ValueRange operands, ModuleOp &module) const {
   assert(!funcName.contains('@') && "funcName should not contain '@'");
-  assert(module.lookupSymbol<LLVM::LLVMFuncOp>(funcName) &&
-         "Expecting to find a function declaration");
-  auto funcSym = SymbolRefAttr::get(builder.getContext(), funcName);
-  return genCall(funcSym, resTypes, operands);
+  return create<func::CallOp>(funcName, resTypes, operands);
 }
 
 //===----------------------------------------------------------------------===//
