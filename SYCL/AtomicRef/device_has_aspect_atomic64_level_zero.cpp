4802aacb21b40e202ac5086bfc6542d5ac3c971c
Kseniya Tikhomirova <kseniya.tikhomirova@intel.com>
2022-09-14 17:32:17 +0200

    [SYCL][L0] Fix has_aspect_atomic64 test and reenable it (intel/llvm-test-suite#1253)
    Fix has_aspect_atomic64 test and reenable it
    
    Signed-off-by: Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>

--- SYCL/AtomicRef/device_has_aspect_atomic64_level_zero.cpp
+++ SYCL/AtomicRef/device_has_aspect_atomic64_level_zero.cpp
@@ -1,4 +1,4 @@
-// REQUIRES: level_zero, level_zero_dev_kit, TEMPORARY_DISABLED
+// REQUIRES: level_zero, level_zero_dev_kit
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out %level_zero_options
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 
@@ -11,7 +11,7 @@ int main() {
   queue Queue;
   device Dev = Queue.get_device();
   bool Result;
-  ze_device_module_properties_t Properties;
+  ze_device_module_properties_t Properties{};
   zeDeviceGetModuleProperties(get_native<backend::ext_oneapi_level_zero>(Dev),
                               &Properties);
   if (Properties.flags & ZE_DEVICE_MODULE_FLAG_INT64_ATOMICS)
