bbc7f804fdd50fb3840311a89e9794e6f3a77cf0
Dmitry Budanov <dmitry.budanov@intel.com>
2022-09-13 18:04:52 -0700

    [ESIMD] Disable aot_mixed.cpp until get fixed 'Unsupported required sub group size' issue (intel/llvm-test-suite#1260)
    * Temporary disable ESIMD/aot_mixed.cpp untill get fixed 'Unsupported required sub group size' issue
    
    Co-authored-by: Pavel Chupin <pavel.v.chupin@intel.com>

--- SYCL/ESIMD/aot_mixed.cpp
+++ SYCL/ESIMD/aot_mixed.cpp
@@ -8,6 +8,10 @@
 // REQUIRES: gpu
 // UNSUPPORTED: cuda || hip
 // UNSUPPORTED: esimd_emulator
+// Temporary disable everywhere until "Unsupported required sub group size" is
+// fixed in some configurations.
+// UNSUPPORTED: windows
+// UNSUPPORTED: linux
 // RUN: %clangxx -fsycl -fsycl-targets=spir64_gen -Xsycl-target-backend=spir64_gen "-device gen9" -o %t.sycl.out -DENABLE_SYCL=0 %s
 // RUN: %GPU_RUN_PLACEHOLDER %t.sycl.out
 // RUN: %clangxx -fsycl -fsycl-targets=spir64_gen -Xsycl-target-backend=spir64_gen "-device gen9" -o %t.out %s
