b5fc20c8507d1020c6b548527609a6f34f902db7
Tiotto, Ettore <ettore.tiotto@intel.com>
2022-09-15 15:45:47 -0400

    [SYCLToLLVM]: Fix SubIndexOpLowering
    Signed-off-by: Tiotto, Ettore <ettore.tiotto@intel.com>

--- polygeist/tools/cgeist/Test/Verification/sycl/constructors.cpp
+++ polygeist/tools/cgeist/Test/Verification/sycl/constructors.cpp
@@ -1,5 +1,3 @@
-// Copyright (C) Codeplay Software Limited
-
 //===--- constructors.cpp -------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
@@ -8,7 +6,8 @@
 //
 //===----------------------------------------------------------------------===//
 
-// RUN: sycl-clang.py %s -S 2> /dev/null | FileCheck %s
+// RUN: sycl-clang.py %s -S 2> /dev/null | FileCheck %s --check-prefixes=CHECK
+// RUN: sycl-clang.py %s -S -emit-llvm | FileCheck %s --check-prefixes=CHECK-LLVM
 
 #include <sycl/sycl.hpp>
 
@@ -24,13 +23,25 @@
 // CHECK-NEXT:   %0 = sycl.cast(%arg0) : (memref<?x!sycl_range_1_>) -> memref<?x!sycl_array_1_>
 // clang-format on
 
-SYCL_EXTERNAL void cons_0(sycl::id<1> i, sycl::range<1> r) {
+// clang-format off
+// CHECK-LLVM: define void @cons_0([[ID_TYPE:%"class.cl::sycl::id.1"]] [[ARG0:%.*]], [[RANGE_TYPE:%"class.cl::sycl::range.1"]] [[ARG1:%.*]]) {
+// CHECK-LLVM-DAG: [[RANGE1:%.*]] = alloca [[RANGE_TYPE]]
+// CHECK-LLVM-DAG: [[ID1:%.*]] = alloca [[ID_TYPE]]
+// CHECK-LLVM-DAG: [[RANGE2:%.*]] = alloca [[RANGE_TYPE]]
+// CHECK-LLVM-DAG: [[ID2:%.*]] = alloca [[ID_TYPE]]
+// CHECK-LLVM: store [[ID_TYPE]] [[ARG0]], [[ID_TYPE]]* [[ID2]]
+// CHECK-LLVM: store [[RANGE_TYPE]] [[ARG1]], [[RANGE_TYPE]]* [[RANGE2]]
+// CHECK-LLVM: call void @_ZN4sycl3_V12idILi1EEC1ERKS2_([[ID_TYPE]]* [[ID1]], 
+// CHECK-LLVM: call void @_ZN4sycl3_V15rangeILi1EEC1ERKS2_([[RANGE_TYPE]]* [[RANGE1]],
+// clang-format on
+
+extern "C" SYCL_EXTERNAL void cons_0(sycl::id<1> i, sycl::range<1> r) {
   auto id = sycl::id<1>{i};
   auto range = sycl::range<1>{r};
 }
 
 // clang-format off
-// CHECK: func.func @_Z6cons_1v() attributes {llvm.linkage = #llvm.linkage<external>} {
+// CHECK: func.func @cons_1() attributes {llvm.linkage = #llvm.linkage<external>} {
 // CHECK-NEXT: %false = arith.constant false
 // CHECK-NEXT: %c0_i8 = arith.constant 0 : i8
 // CHECK-NEXT: %0 = memref.alloca() : memref<1x!sycl_id_2_>
@@ -49,12 +60,20 @@ SYCL_EXTERNAL void cons_0(sycl::id<1> i, sycl::range<1> r) {
 // CHECK: func.func private @_ZN4sycl3_V12idILi2EEC1Ev(memref<?x!sycl_id_2_>) attributes {llvm.linkage = #llvm.linkage<external>}
 // clang-format on
 
-SYCL_EXTERNAL void cons_1() {
+// clang-format off
+// CHECK-LLVM: define void @cons_1() {
+// CHECK-LLVM: [[ID1:%.*]] = alloca [[ID_TYPE:%"class.cl::sycl::id.2"]]
+// CHECK-LLVM: [[CAST1:%.*]] = bitcast [[ID_TYPE]]* %1 to i8*
+// CHECK-LLVM: call void @llvm.memset.p0i8.i64(i8* %2, i8 0, i64 16, i1 false)
+// CHECK-LLVM: call void @_ZN4sycl3_V12idILi2EEC1Ev([[ID_TYPE]]* [[ID1]], [[ID_TYPE]]* [[ID1]], i64 0, i64 1, i64 1)  
+// clang-format on
+
+extern "C" SYCL_EXTERNAL void cons_1() {
   auto id = sycl::id<2>{};
 }
 
 // clang-format off
-// CHECK: func.func @_Z6cons_2mm(%arg0: i64, %arg1: i64) attributes {llvm.linkage = #llvm.linkage<external>} {
+// CHECK: func.func @cons_2(%arg0: i64, %arg1: i64) attributes {llvm.linkage = #llvm.linkage<external>} {
 // CHECK-NEXT: %0 = memref.alloca() : memref<1x!sycl_id_2_>
 // CHECK-NEXT: %1 = memref.cast %0 : memref<1x!sycl_id_2_> to memref<?x!sycl_id_2_>
 // CHECK-NEXT: sycl.constructor(%1, %arg0, %arg1) {MangledName = @_ZN4sycl3_V12idILi2EEC1ILi2EEENSt9enable_ifIXeqT_Li2EEmE4typeEm, Type = @id} : (memref<?x!sycl_id_2_>, i64, i64) -> ()
@@ -62,12 +81,18 @@ SYCL_EXTERNAL void cons_1() {
 // CHECK-NEXT: }
 // clang-format on
 
-SYCL_EXTERNAL void cons_2(size_t a, size_t b) {
+// clang-format off
+// CHECK-LLVM: define void @cons_2(i64 %0, i64 %1) {
+// CHECK-LLVM: [[ID1:%.*]] = alloca [[ID_TYPE:%"class.cl::sycl::id.2"]]
+// CHECK-LLVM: call void @_ZN4sycl3_V12idILi2EEC1ILi2EEENSt9enable_ifIXeqT_Li2EEmE4typeEm([[ID_TYPE]]* [[ID1]], [[ID_TYPE]]* [[ID1]], i64 0, i64 1, i64 1, i64 %0, i64 %1)
+// clang-format on
+
+extern "C" SYCL_EXTERNAL void cons_2(size_t a, size_t b) {
   auto id = sycl::id<2>{a, b};
 }
-
+ 
 // clang-format off
-// CHECK: func.func @_Z6cons_3N4sycl3_V14itemILi2ELb1EEE(%arg0: !sycl_item_2_1_) attributes {llvm.linkage = #llvm.linkage<external>} {
+// CHECK: func.func @cons_3(%arg0: !sycl_item_2_1_) attributes {llvm.linkage = #llvm.linkage<external>} {
 // CHECK-NEXT: %0 = memref.alloca() : memref<1x!sycl_id_2_>
 // CHECK-NEXT: %1 = memref.cast %0 : memref<1x!sycl_id_2_> to memref<?x!sycl_id_2_>
 // CHECK-NEXT: %2 = memref.alloca() : memref<1x!sycl_item_2_1_>
@@ -78,12 +103,20 @@ SYCL_EXTERNAL void cons_2(size_t a, size_t b) {
 // CHECK-NEXT: }
 // clang-format on
 
-SYCL_EXTERNAL void cons_3(sycl::item<2, true> val) {
+// clang-format off
+// CHECK-LLVM: define void @cons_3([[ITEM_TYPE:%"class.cl::sycl::item.2.true"]] [[ARG0:%.*]]) {
+// CHECK-LLVM: [[ID:%.*]] = alloca [[ID_TYPE:%"class.cl::sycl::id.2"]]  
+// CHECK-LLVM: [[ITEM:%.*]] = alloca [[ITEM_TYPE]]
+// CHECK-LLVM: store [[ITEM_TYPE]] [[ARG0]], [[ITEM_TYPE]]* [[ITEM]], align 8
+// CHECK-LLVM: call void @_ZN4sycl3_V12idILi2EEC1ILi2ELb1EEERNSt9enable_ifIXeqT_Li2EEKNS0_4itemILi2EXT0_EEEE4typeE([[ID_TYPE]]* [[ID]], [[ID_TYPE]]* [[ID]], i64 0, i64 1, i64 1, [[ITEM_TYPE]]* [[ITEM]], [[ITEM_TYPE]]* [[ITEM]], i64 0, i64 1, i64 1)
+// clang-format on
+
+extern "C" SYCL_EXTERNAL void cons_3(sycl::item<2, true> val) {
   auto id = sycl::id<2>{val};
 }
 
 // clang-format off
-// CHECK: func.func @_Z6cons_4N4sycl3_V12idILi2EEE(%arg0: !sycl_id_2_) attributes {llvm.linkage = #llvm.linkage<external>} {
+// CHECK: func.func @cons_4(%arg0: !sycl_id_2_) attributes {llvm.linkage = #llvm.linkage<external>} {
 // CHECK-NEXT: %0 = memref.alloca() : memref<1x!sycl_id_2_>
 // CHECK-NEXT: %1 = memref.cast %0 : memref<1x!sycl_id_2_> to memref<?x!sycl_id_2_>
 // CHECK-NEXT: %2 = memref.alloca() : memref<1x!sycl_id_2_>
@@ -94,6 +127,14 @@ SYCL_EXTERNAL void cons_3(sycl::item<2, true> val) {
 // CHECK-NEXT: }
 // clang-format on
 
-SYCL_EXTERNAL void cons_4(sycl::id<2> val) {
+// clang-format off
+// CHECK-LLVM: define void @cons_4([[ID_TYPE:%"class.cl::sycl::id.2"]] [[ARG0:%.*]]) {
+// CHECK-LLVM: [[ID1:%.*]] = alloca [[ID_TYPE]]
+// CHECK-LLVM: [[ID2:%.*]] = alloca [[ID_TYPE]]
+// CHECK-LLVM: store [[ID_TYPE]] [[ARG0]], [[ID_TYPE]]* [[ID2]], align 8
+// CHECK-LLVM: call void @_ZN4sycl3_V12idILi2EEC1ERKS2_([[ID_TYPE]]* [[ID1]], [[ID_TYPE]]* [[ID1]], i64 0, i64 1, i64 1, [[ID_TYPE]]* [[ID2]], [[ID_TYPE]]* [[ID2]], i64 0, i64 1, i64 1)
+// clang-format on
+
+extern "C"  SYCL_EXTERNAL void cons_4(sycl::id<2> val) {
   auto id = sycl::id<2>{val};
 }
