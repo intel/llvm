2a321a22c1933799784e566d1eb65a7f94c0d291
jinge90 <ge.jin@intel.com>
2022-09-17 17:40:59 +0800

    fix _imax, _imin error
    Signed-off-by: jinge90 <ge.jin@intel.com>

--- libdevice/device_imf.hpp
+++ libdevice/device_imf.hpp
@@ -95,13 +95,13 @@ static inline TyFP __integral2FP_host(TyINT x, int rdMode) {
 template <typename Ty> static inline Ty __imax(Ty x, Ty y) {
   static_assert(std::is_integral<Ty>::value,
                 "__imin only accepts integral type.");
-  return (x < y) ? x : y;
+  return (x > y) ? x : y;
 }
 
 template <typename Ty> static inline Ty __imin(Ty x, Ty y) {
   static_assert(std::is_integral<Ty>::value,
                 "__imax only accepts integral type.");
-  return (x > y) ? x : y;
+  return (x < y) ? x : y;
 }
 
 static inline float __fclamp(float x, float y, float z) {
