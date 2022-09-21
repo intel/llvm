d6c29143cf1027b74b47397d624a24b5d1b8362b
gregory <gregory@gaiaplatform.io>
2022-09-21 13:26:40 -0700

    Minor code refactoring

--- sycl/include/sycl/ext/intel/esimd/detail/tfloat32_type_traits.hpp
+++ sycl/include/sycl/ext/intel/esimd/detail/tfloat32_type_traits.hpp
@@ -6,14 +6,14 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-// Implementation of SIMD element type traits for the sycl::half type.
+// Implementation of SIMD element type traits for the tfloat32 type.
 //===----------------------------------------------------------------------===//
 
 #pragma once
 
 #include <sycl/ext/intel/esimd/detail/elem_type_traits.hpp>
 #include <sycl/ext/intel/esimd/detail/intrin.hpp>
-#include <sycl/ext/oneapi/experimental/tfloat32.hpp>
+#include <sycl/ext/intel/experimental/esimd/tfloat32.hpp>
 
 /// @cond ESIMD_DETAIL
 
@@ -23,12 +23,10 @@ namespace ext::intel::esimd::detail {
 
 // Standalone definitions to use w/o instantiating element_type_traits.
 using tfloat32 = sycl::ext::oneapi::experimental::tfloat32;
-using tfloat32_raw_type = uint_type_t<sizeof(tfloat32)>;
-using tfloat32_enclosing_cpp_type = float;
 
 template <> struct element_type_traits<tfloat32> {
-  using RawT = tfloat32_raw_type;
-  using EnclosingCppT = tfloat32_enclosing_cpp_type;
+  using RawT = unsigned int;
+  using EnclosingCppT = float;
 
   static inline constexpr bool use_native_cpp_ops = false;
   static inline constexpr bool is_floating_point = true;
@@ -49,8 +47,8 @@ template <int N> struct vector_conversion_traits<tfloat32, N> {
 #else
     vector_type_t<RawT, N> Output = 0;
 
-    for (int i = 0; i < N; i += 1) {
-      Output[i] = tfloat32::from_float(Val[i]);
+    for (int i = 0; i < N; i++) {
+      Output[i] = sycl::bit_cast<RawT>(static_cast<tfloat32>(Val[i]));
     }
     return Output;
 #endif
@@ -58,16 +56,8 @@ template <int N> struct vector_conversion_traits<tfloat32, N> {
 
   static ESIMD_INLINE vector_type_t<StdT, N>
   convert_to_cpp(vector_type_t<RawT, N> Val) {
-#ifdef __SYCL_DEVICE_ONLY__
     vector_type_t<StdT, N> Result = sycl::bit_cast<vector_type_t<StdT, N>>(Val);
-#else
-    vector_type_t<StdT, N> Output;
-
-    for (int i = 0; i < N; i++) {
-      Output[i] = tfloat32::to_float(Val[i]);
-    }
-    return Output;
-#endif
+    return Result;
   }
 };
 
@@ -75,11 +65,11 @@ template <> struct scalar_conversion_traits<tfloat32> {
   using RawT = __raw_t<tfloat32>;
 
   static ESIMD_INLINE RawT bitcast_to_raw(tfloat32 Val) {
-    return tfloat32::from_float(Val);
+    return sycl::bit_cast<RawT>(Val);
   }
 
   static ESIMD_INLINE tfloat32 bitcast_to_wrapper(RawT Val) {
-    return tfloat32::to_float(Val);
+    return sycl::bit_cast<tfloat32>(Val);
   }
 };
 
@@ -89,12 +79,7 @@ inline std::ostream &operator<<(std::ostream &O, tfloat32 const &rhs) {
   return O;
 }
 
-inline std::istream &operator>>(std::istream &I, tfloat32 &rhs) {
-  float ValFloat = 0.0f;
-  I >> ValFloat;
-  rhs = ValFloat;
-  return I;
-}
+template <> struct is_esimd_arithmetic_type<tfloat32, void> : std::true_type {};
 
 } // namespace ext::intel::esimd::detail
 } // __SYCL_INLINE_VER_NAMESPACE(_V1)
