38fd5c7bf34a11195f90e72184781944742095e5
Artur Gainullin <artur.gainullin@intel.com>
2022-09-22 18:56:53 -0700

    [SYCL] Fix error caused by template deduction warning (#6860)

--- sycl/source/detail/queue_impl.cpp
+++ sycl/source/detail/queue_impl.cpp
@@ -71,7 +71,7 @@ event queue_impl::memset(const std::shared_ptr<detail::queue_impl> &Self,
   {
     // We need to submit command and update the last event under same lock if we
     // have in-order queue.
-    auto ScopeLock = isInOrder() ? std::unique_lock(MLastEventMtx)
+    auto ScopeLock = isInOrder() ? std::unique_lock<std::mutex>(MLastEventMtx)
                                  : std::unique_lock<std::mutex>();
     // If the last submitted command in the in-order queue is host_task then
     // wait for it before submitting usm command.
@@ -113,7 +113,7 @@ event queue_impl::memcpy(const std::shared_ptr<detail::queue_impl> &Self,
   {
     // We need to submit command and update the last event under same lock if we
     // have in-order queue.
-    auto ScopeLock = isInOrder() ? std::unique_lock(MLastEventMtx)
+    auto ScopeLock = isInOrder() ? std::unique_lock<std::mutex>(MLastEventMtx)
                                  : std::unique_lock<std::mutex>();
     // If the last submitted command in the in-order queue is host_task then
     // wait for it before submitting usm command.
@@ -156,7 +156,7 @@ event queue_impl::mem_advise(const std::shared_ptr<detail::queue_impl> &Self,
   {
     // We need to submit command and update the last event under same lock if we
     // have in-order queue.
-    auto ScopeLock = isInOrder() ? std::unique_lock(MLastEventMtx)
+    auto ScopeLock = isInOrder() ? std::unique_lock<std::mutex>(MLastEventMtx)
                                  : std::unique_lock<std::mutex>();
     // If the last submitted command in the in-order queue is host_task then
     // wait for it before submitting usm command.
