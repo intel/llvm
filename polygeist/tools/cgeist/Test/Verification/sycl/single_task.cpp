60d0b4c31a9f1860dcdfb92cd84c9926dfa2c36e
Tsang, Whitney <whitney.tsang@intel.com>
2022-09-28 16:26:26 -0400

    [SYCL-MLIR]: Add single_tast.cpp lit test
    Signed-off-by: Tsang, Whitney <whitney.tsang@intel.com>

---
+++ polygeist/tools/cgeist/Test/Verification/sycl/single_task.cpp
@@ -0,0 +1,27 @@
+// RUN: sycl-clang.py %s -S -emit-llvm -o %t.ll
+// Test that the LLVMIR generated is verifiable.
+// RUN: opt -verify -disable-output < %t.ll
+// RUN: cat %t.ll | FileCheck %s
+// XFAIL: *
+
+// Test that the kernel named `kernel_single_task` is generated.
+// CHECK: define weak_odr spir_kernel void {{.*}}kernel_single_task
+// Test that all referred sycl header functions are generated.
+// CHECK-NOT: declare {{.*}} spir_func
+
+#include <sycl/sycl.hpp>
+
+void host_single_task() {
+  auto q = sycl::queue{};
+  auto range = sycl::range<1>{1};
+
+  {
+    auto buf = sycl::buffer<int, 1>{nullptr, range};
+    q.submit([&](sycl::handler &cgh) {
+      auto A = buf.get_access<sycl::access::mode::write>(cgh);
+      cgh.single_task<class kernel_single_task>([=]() {
+        A[0] = 42;
+      });
+    });
+  }
+}
