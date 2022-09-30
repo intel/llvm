bde55ad3c0d756a12528395b19dac9816c792403
Victor Perez <victor.perez@codeplay.com>
2022-09-30 18:24:13 +0100

    Add more checks to tests

--- polygeist/tools/cgeist/Test/Verification/sycl/sycl_extern_func.cpp
+++ polygeist/tools/cgeist/Test/Verification/sycl/sycl_extern_func.cpp
@@ -13,15 +13,32 @@
 
 #include <sycl/sycl.hpp>
 
+// CHECK-MLIR: gpu.module @device_functions
+
+// CHECK-MLIR-LABEL: gpu.func @_ZTSZZ16host_single_taskvENKUlRN4sycl3_V17handlerEE_clES2_E18kernel_single_task
+// CHECK-MLIR-SAME: kernel attributes {llvm.cconv = #llvm.cconv<spir_kernelcc>, llvm.linkage = #llvm.linkage<weak_odr>, passthrough = ["norecurse", "nounwind", "convergent", "mustprogress"]} {
+
+// CHECK-MLIR-LABEL: func.func private @_ZZZ16host_single_taskvENKUlRN4sycl3_V17handlerEE_clES2_ENKUlvE_clEv
+// CHECK-MLIR-SAME: attributes {llvm.cconv = #llvm.cconv<spir_funccc>, llvm.linkage = #llvm.linkage<internal>, passthrough = ["norecurse", "nounwind", "convergent", "mustprogress"]} {
+// CHECK-MLIR: call @cons_5() : () -> ()
+
 // CHECK-MLIR-LABEL: func.func @cons_5() attributes {llvm.cconv = #llvm.cconv<spir_funccc>, llvm.linkage = #llvm.linkage<external>, passthrough = ["norecurse", "nounwind", "convergent", "mustprogress"]} {
 // CHECK-MLIR:           sycl.constructor(%{{.*}})
 // CHECK-MLIR-NEXT:      return
+
+// CHECK-LLMV-LABEL: define weak_odr spir_kernel void @_ZTSZZ16host_single_taskvENKUlRN4sycl3_V17handlerEE_clES2_E18kernel_single_task
+// CHECK-SAME: #0
+
+// CHECK-LLVM-LABEL: define internal spir_func void @_ZZZ16host_single_taskvENKUlRN4sycl3_V17handlerEE_clES2_ENKUlvE_clEv
+// CHECK-LLVM-SAME: #0
+// CHECK: call void @cons_5()
 
 // CHECK-LLVM-LABEL: define spir_func void @cons_5() #0 {
 // CHECK-LLVM-NEXT:  %{{.*}} = alloca %"class.cl::sycl::accessor.1", i64 ptrtoint (%"class.cl::sycl::accessor.1"* getelementptr (%"class.cl::sycl::accessor.1", %"class.cl::sycl::accessor.1"* null, i32 1) to i64), align 8
 // CHECK-LLVM-NEXT:  call void @_ZN4sycl3_V18accessorIiLi1ELNS0_6access4modeE1025ELNS2_6targetE2014ELNS2_11placeholderE0ENS0_3ext6oneapi22accessor_property_listIJEEEEC1Ev(%"class.cl::sycl::accessor.1"* %1, %"class.cl::sycl::accessor.1"* %1, i64 0, i64 1, i64 1)
 // CHECK-LLVM-NEXT:  ret void
 
+// CHECK-LLVM: attributes #0 = { convergent mustprogress norecurse nounwind }
 
 extern "C" SYCL_EXTERNAL void cons_5() {
   sycl::accessor<sycl::cl_int, 1, sycl::access::mode::write> accessor;
