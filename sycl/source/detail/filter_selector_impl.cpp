0a900c2c19ec6182006a7900a75dff2a82451dee
lorenc.bushi <lbushi@scsel-cfl-04.sc.intel.com>
2022-09-30 14:11:41 -0700

    Add esimd_emulator to filter_selector

--- sycl/source/detail/filter_selector_impl.cpp
+++ sycl/source/detail/filter_selector_impl.cpp
@@ -83,6 +83,9 @@ filter create_filter(const std::string &Input) {
     } else if (Token == "hip" && !Result.HasBackend) {
       Result.Backend = backend::ext_oneapi_hip;
       Result.HasBackend = true;
+    } else if (Token == "esimd_emulator" && !Result.HasBackend) { 
+      Result.Backend = backend::ext_intel_esimd_emulator;
+      Result.HasBackend = true;
     } else if (Token == "host") {
       if (!Result.HasBackend) {
         Result.Backend = backend::host;
