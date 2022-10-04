95377cd09a0f8696cf55bbcb16d2340b4aaa27a8
Sarnie, Nick <nick.sarnie@intel.com>
2022-10-04 14:33:42 -0400

    address feedback 4
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- llvm/lib/SYCLLowerIR/LowerKernelProps.cpp
+++ llvm/lib/SYCLLowerIR/LowerKernelProps.cpp
@@ -1,4 +1,4 @@
-//===---- LowerKernelProps.h - lower __sycl_set_kernel_properties ---===//
+//===---- LowerKernelProps.cpp - lower __sycl_set_kernel_properties ---===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
