69edb9174286ae4e2a5474737929551b92883223
Michael D Toguchi <michael.d.toguchi@intel.com>
2022-10-05 13:49:59 -0700

    [Driver][SYCL] Improve way sycld is pulled in for Linux based Windows driver
    Update the way sycld is pulled in by matching if msvcrtd is brought in
    via the command line.  Cmake uses '-Xclang --dependent-lib=msvcrtd' when
    there is the desire to link in with the debug libraries.  Check for this
    and use an equivalent '--dependent-lib=sycld' to pull in the SYCL runtime
    library.
    
    This is only done for the Linux based drivers on Windows.  The MSVC based
    driver behavior already pulls in the proper libraries given /MDd.

--- clang/lib/Driver/ToolChains/MSVC.cpp
+++ clang/lib/Driver/ToolChains/MSVC.cpp
@@ -124,10 +124,7 @@ void visualstudio::Linker::ConstructJob(Compilation &C, const JobAction &JA,
   if (!Args.hasArg(options::OPT_nostdlib, options::OPT_nostartfiles) &&
       !C.getDriver().IsCLMode() && !C.getDriver().IsFlangMode()) {
     if (Args.hasArg(options::OPT_fsycl) && !Args.hasArg(options::OPT_nolibsycl))
-      if (Args.hasArg(options::OPT_g_Flag))
-        CmdArgs.push_back("-defaultlib:msvcrtd");
-      else
-        CmdArgs.push_back("-defaultlib:msvcrt");
+      CmdArgs.push_back("-defaultlib:msvcrt");
     else
       CmdArgs.push_back("-defaultlib:libcmt");
     CmdArgs.push_back("-defaultlib:oldnames");
@@ -138,10 +135,7 @@ void visualstudio::Linker::ConstructJob(Compilation &C, const JobAction &JA,
       Args.hasArg(options::OPT_fsycl_host_compiler_EQ)) {
     CmdArgs.push_back(Args.MakeArgString(std::string("-libpath:") +
                                          TC.getDriver().Dir + "/../lib"));
-    if (Args.hasArg(options::OPT_g_Flag))
-      CmdArgs.push_back("-defaultlib:sycl" SYCL_MAJOR_VERSION "d.lib");
-    else
-      CmdArgs.push_back("-defaultlib:sycl" SYCL_MAJOR_VERSION ".lib");
+    CmdArgs.push_back("-defaultlib:sycl" SYCL_MAJOR_VERSION ".lib");
     CmdArgs.push_back("-defaultlib:sycl-devicelib-host.lib");
   }
 
