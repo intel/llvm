f37ed32912b921035c3818bca16e405d753c4ceb
arnamoy.bhattacharyya <arnamoyb@hds-clx-7.nh.intel.com>
2022-10-11 14:35:00 -0400

    clang-format

--- polygeist/tools/cgeist/Lib/CGExpr.cc
+++ polygeist/tools/cgeist/Lib/CGExpr.cc
@@ -1193,7 +1193,8 @@ ValueCategory MLIRScanner::VisitDeclRefExpr(DeclRefExpr *E) {
     // function, the global should be in the gpu module (which is nested inside
     // another main module).
     std::pair<mlir::memref::GlobalOp, bool> gv;
-    gv = Glob.GetOrCreateGlobal(VD, /*prefix=*/"", true, function->getParentOp() != module.get());
+    gv = Glob.GetOrCreateGlobal(VD, /*prefix=*/"", true,
+                                function->getParentOp() != module.get());
 
     auto gv2 = builder.create<memref::GetGlobalOp>(loc, gv.first.type(),
                                                    gv.first.getName());
