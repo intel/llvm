4b49e7e9598858a7bd0f2bd4bdb0ad17e2413ecd
Haonan Yang <haonan.yang@intel.com>
2022-10-12 11:13:21 +0800

    [NFC] Update tests

--- clang/test/CodeGen/ffp-model.c
+++ clang/test/CodeGen/ffp-model.c
@@ -1,19 +1,19 @@
 // REQUIRES: x86-registered-target
-// RUN: %clang -S -emit-llvm -ffp-model=fast -emit-llvm %s -o - \
-// RUN: | FileCheck %s --check-prefixes=CHECK,CHECK-FAST
+// INTEL RUN: %clang -Xclang -opaque-pointers -S -emit-llvm -ffp-model=fast -emit-llvm %s -o - \
+// INTEL RUN: | FileCheck %s --check-prefixes=CHECK,CHECK-FAST
 
-// RUN: %clang -S -emit-llvm -ffp-model=precise %s -o - \
-// RUN: | FileCheck %s --check-prefixes=CHECK,CHECK-PRECISE
+// INTEL RUN: %clang -Xclang -opaque-pointers -S -emit-llvm -ffp-model=precise %s -o - \
+// INTEL RUN: | FileCheck %s --check-prefixes=CHECK,CHECK-PRECISE
 
-// RUN: %clang -S -emit-llvm -ffp-model=strict %s -o - \
-// RUN: -target x86_64 | FileCheck %s --check-prefixes=CHECK,CHECK-STRICT
+// INTEL RUN: %clang -Xclang -opaque-pointers -S -emit-llvm -ffp-model=strict %s -o - \
+// INTEL RUN: -target x86_64 | FileCheck %s --check-prefixes=CHECK,CHECK-STRICT
 
-// RUN: %clang -S -emit-llvm -ffp-model=strict -ffast-math \
-// RUN: -target x86_64 %s -o - | FileCheck %s \
-// RUN: --check-prefixes CHECK,CHECK-STRICT-FAST
+// INTEL RUN: %clang -Xclang -opaque-pointers -S -emit-llvm -ffp-model=strict -ffast-math \
+// INTEL RUN: -target x86_64 %s -o - | FileCheck %s \
+// INTEL RUN: --check-prefixes CHECK,CHECK-STRICT-FAST
 
-// RUN: %clang -S -emit-llvm -ffp-model=precise -ffast-math \
-// RUN: %s -o - | FileCheck %s --check-prefixes CHECK,CHECK-FAST1
+// INTEL RUN: %clang -Xclang -opaque-pointers -S -emit-llvm -ffp-model=precise -ffast-math \
+// INTEL RUN: %s -o - | FileCheck %s --check-prefixes CHECK,CHECK-FAST1
 
 float mymuladd(float x, float y, float z) {
   // CHECK: define{{.*}} float @mymuladd
