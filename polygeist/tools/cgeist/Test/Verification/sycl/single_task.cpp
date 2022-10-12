39bbe6e5f951246117b39618a236793f3bd76111
Tiotto, Ettore <ettore.tiotto@intel.com>
2022-10-12 14:48:43 -0400

    Remove XFAIL on passing test
    Signed-off-by: Tiotto, Ettore <ettore.tiotto@intel.com>

--- polygeist/tools/cgeist/Test/Verification/sycl/single_task.cpp
+++ polygeist/tools/cgeist/Test/Verification/sycl/single_task.cpp
@@ -6,8 +6,6 @@
 // RUN: llvm-spirv %t.bc
 // RUN: llvm-dis %t.bc
 // RUN: cat %t.ll | FileCheck %s
-// BUG: clang++ -o doesn't redirect the output to the file specified.
-// XFAIL: *
 
 // Test that the kernel named `kernel_single_task` is generated with the correct signature.
 // CHECK: define weak_odr spir_kernel void {{.*}}kernel_single_task(i32 addrspace(1)* {{.*}}, [[RANGE_TY:%"class.sycl::_V1::range.1"]]* byval([[RANGE_TY]]) {{.*}}, [[RANGE_TY]]* byval([[RANGE_TY]]) {{.*}}, [[ID_TY:%"class.sycl::_V1::id.1"]]* byval([[ID_TY]]) {{.*}})
