39c846a2c6b537de81084be38ec1de1d06eb96f1
myl <yanliang.mu@intel.com>
2022-10-13 18:34:07 +0800

    [SYCL] disable esimd_preemption on dg2 (intel/llvm-test-suite#1159)
    disable esimd_preemption on dg2

--- SYCL/ESIMD/preemption.cpp
+++ SYCL/ESIMD/preemption.cpp
@@ -7,7 +7,7 @@
 //===----------------------------------------------------------------------===//
 // REQUIRES: gpu && linux
 // UNSUPPORTED: cuda || hip
-// UNSUPPORTED: esimd_emulator
+// UNSUPPORTED: esimd_emulator, gpu-intel-dg2
 // RUN: %clangxx -fsycl %s -o %t.out
 // RUN: %GPU_RUN_PLACEHOLDER IGC_DumpToCustomDir=%t.dump IGC_ShaderDumpEnable=1 %t.out
 // RUN: grep enablePreemption %t.dump/*.asm
