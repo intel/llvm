e58ef803aec26bdeae9f37dfb54b8940a3774013
Ettore Tiotto <ettore.tiotto@intel.com>
2022-10-17 17:02:18 -0400

    Update polygeist/tools/cgeist/Test/Verification/sycl/kernels.cpp
    Co-authored-by: Whitney Tsang <54643204+whitneywhtsang@users.noreply.github.com>

--- polygeist/tools/cgeist/Test/Verification/sycl/kernels.cpp
+++ polygeist/tools/cgeist/Test/Verification/sycl/kernels.cpp
@@ -76,7 +76,7 @@ void host_2() {
   auto q = sycl::queue{};
   auto range = sycl::range<1>{1};
 
-  {  
+  {
     auto buf = sycl::buffer<int, 1>{nullptr, range};
     q.submit([&](sycl::handler &cgh) {
       auto A = buf.get_access<sycl::access::mode::read_write>(cgh);
