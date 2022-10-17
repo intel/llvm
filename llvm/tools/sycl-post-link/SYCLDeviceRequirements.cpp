d98128c9b3fb4afee34df91fef239b1f88ca4edd
KornevNikita <nikita.kornev@intel.com>
2022-10-17 09:01:51 -0700

    [SYCL] Implement fixed target property generation
    In scope of implementation of the "SYCL/Device requirements" property
    set we need to create a "fixed_target" property if the "!sycl_fixed_targets"
    metadata is present in the IR and fill it with the corresponding
    aspects.

--- llvm/tools/sycl-post-link/SYCLDeviceRequirements.cpp
+++ llvm/tools/sycl-post-link/SYCLDeviceRequirements.cpp
@@ -16,26 +16,31 @@
 
 using namespace llvm;
 
-std::map<StringRef, std::vector<uint32_t>>
-llvm::getSYCLDeviceRequirements(const Module &M) {
-  std::map<StringRef, std::vector<uint32_t>> Result;
+void llvm::getSYCLDeviceRequirements(
+    const Module &M, std::map<StringRef, std::vector<uint32_t>> &Requirements) {
   auto ExtractIntegerFromMDNodeOperand = [=](const MDNode *N,
                                              unsigned OpNo) -> unsigned {
     Constant *C =
         cast<ConstantAsMetadata>(N->getOperand(OpNo).get())->getValue();
     return static_cast<uint32_t>(C->getUniqueInteger().getZExtValue());
   };
-  std::set<uint32_t> Aspects;
-  for (const Function &F : M) {
-    if (!F.hasMetadata("sycl_used_aspects"))
-      continue;
 
-    const MDNode *MD = F.getMetadata("sycl_used_aspects");
-    for (size_t I = 0, E = MD->getNumOperands(); I < E; ++I) {
-      Aspects.insert(ExtractIntegerFromMDNodeOperand(MD, I));
+  std::vector<std::pair<std::string, const char *>> ReqdMDs = {
+      {"sycl_used_aspects", "aspects"}, {"sycl_fixed_targets", "fixed_target"}};
+
+  for (const Function &F : M) {
+    for (const auto &MD : ReqdMDs) {
+      std::set<uint32_t> Aspects;
+      if (F.hasMetadata(MD.first)) {
+        const MDNode *MDN = F.getMetadata(MD.first);
+        for (size_t I = 0, E = MDN->getNumOperands(); I < E; ++I)
+          Aspects.insert(ExtractIntegerFromMDNodeOperand(MDN, I));
+      }
+      // We don't need the "fixed_target" property if it's empty
+      if (MD.first == "sycl_fixed_targets" && Aspects.empty())
+        continue;
+      Requirements[MD.second] =
+          std::vector<uint32_t>(Aspects.begin(), Aspects.end());
     }
   }
-
-  Result["aspects"] = std::vector<uint32_t>(Aspects.begin(), Aspects.end());
-  return Result;
 }
