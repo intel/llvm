42235ebafaeafedc2491a43dd26462f94ed42b73
Lorenc Bushi <lorenc.bushi@intel.com>
2022-10-27 13:37:08 -0700

    Remove SYCL_BE and SYCL_DEVICE_TYPE

--- sycl/source/detail/force_device.cpp
+++ sycl/source/detail/force_device.cpp
@@ -22,26 +22,6 @@ bool match_types(const info::device_type &l, const info::device_type &r) {
 }
 
 info::device_type get_forced_type() {
-  if (const char *val = std::getenv("SYCL_DEVICE_TYPE")) {
-    std::string type(val);
-    std::transform(type.begin(), type.end(), type.begin(), ::tolower);
-
-    if (type == "cpu") {
-      return info::device_type::cpu;
-    }
-    if (type == "gpu") {
-      return info::device_type::gpu;
-    }
-    if (type == "acc") {
-      return info::device_type::accelerator;
-    }
-    if (type == "host") {
-      return info::device_type::host;
-    }
-    throw sycl::runtime_error("SYCL_DEVICE_TYPE is not recognized.  Must "
-                              "be GPU, CPU, ACC or HOST.",
-                              PI_ERROR_INVALID_VALUE);
-  }
   return info::device_type::all;
 }
 
