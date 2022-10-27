9f65303e7fb54f58ef7635478c3ee0bb3451d0f2
Victor Perez <victor.perez@codeplay.com>
2022-10-27 15:42:27 +0100

    Remove attributes from function test label

--- polygeist/tools/cgeist/Test/Verification/binop.c
+++ polygeist/tools/cgeist/Test/Verification/binop.c
@@ -3,7 +3,7 @@
 // COM: Checking evaluation order of binary operations.
 // COM: Each operation must be further tested in separate files.
 
-// CHECK-LABEL: func.func @f0(%arg0: memref<?xi32>, %arg1: memref<?xi32>) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
+// CHECK-LABEL: func.func @f0(%arg0: memref<?xi32>, %arg1: memref<?xi32>)
 // CHECK-NEXT:    %0 = affine.load %arg0[0] : memref<?xi32>
 // CHECK-NEXT:    %1 = affine.load %arg1[1] : memref<?xi32>
 // CHECK-NEXT:    %2 = arith.muli %0, %1 : i32
@@ -15,7 +15,7 @@ int f0(int *x, int *y) {
   return x[i++] * y[i];
 }
 
-// CHECK-LABEL: func.func @f1(%arg0: memref<?xi32>, %arg1: memref<?xi32>) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
+// CHECK-LABEL: func.func @f1(%arg0: memref<?xi32>, %arg1: memref<?xi32>)
 // CHECK-NEXT:    %0 = affine.load %arg0[1] : memref<?xi32>
 // CHECK-NEXT:    %1 = affine.load %arg1[1] : memref<?xi32>
 // CHECK-NEXT:    %2 = arith.shli %0, %1 : i32
@@ -27,7 +27,7 @@ int f1(int *x, int *y) {
   return x[++i] << y[i];
 }
 
-// CHECK-LABEL: func.func @f2(%arg0: memref<?xi32>, %arg1: memref<?xi32>) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
+// CHECK-LABEL: func.func @f2(%arg0: memref<?xi32>, %arg1: memref<?xi32>)
 // CHECK-NEXT:    %0 = affine.load %arg0[0] : memref<?xi32>
 // CHECK-NEXT:    %1 = affine.load %arg1[1] : memref<?xi32>
 // CHECK-NEXT:    %2 = arith.remsi %0, %1 : i32
@@ -39,7 +39,7 @@ int f2(int *x, int *y) {
   return x[i] % y[++i];
 }
 
-// CHECK-LABEL: func.func @f3(%arg0: memref<?xi32>, %arg1: memref<?xi32>) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
+// CHECK-LABEL: func.func @f3(%arg0: memref<?xi32>, %arg1: memref<?xi32>)
 // CHECK-NEXT:    %0 = affine.load %arg0[0] : memref<?xi32>
 // CHECK-NEXT:    %1 = affine.load %arg1[0] : memref<?xi32>
 // CHECK-NEXT:    %2 = arith.xori %0, %1 : i32
