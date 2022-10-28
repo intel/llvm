6ba594c2e4151d6f28b81403fb8f1906dadcc7be
Hugh Delaney <hugh.delaney@codeplay.com>
2022-10-28 17:02:17 +0100

    Add more test

--- clang/test/CodeGenSYCL/nvvm-reflect.cpp
+++ clang/test/CodeGenSYCL/nvvm-reflect.cpp
@@ -3,17 +3,21 @@
 
 // Checking to see if the correct values are substituted for the nvvm_reflect
 // call when llvm passes are enabled.
-// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_50 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=NVVM_REFLECT_PASS_2
-// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_52 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=NVVM_REFLECT_PASS_3 
-// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_53 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=NVVM_REFLECT_PASS_4
-// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_60 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=NVVM_REFLECT_PASS_5
-// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_61 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=NVVM_REFLECT_PASS_6
-// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_62 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=NVVM_REFLECT_PASS_7
-// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_70 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=NVVM_REFLECT_PASS_8
-// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_72 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=NVVM_REFLECT_PASS_9
-// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_75 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=NVVM_REFLECT_PASS_10
-// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_80 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=NVVM_REFLECT_PASS_11
-// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_86 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=NVVM_REFLECT_PASS_12
+// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_50 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=ARCH_REFLECT_2
+// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_52 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=ARCH_REFLECT_3 
+// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_53 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=ARCH_REFLECT_4
+// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_60 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=ARCH_REFLECT_5
+// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_61 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=ARCH_REFLECT_6
+// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_62 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=ARCH_REFLECT_7
+// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_70 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=ARCH_REFLECT_8
+// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_72 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=ARCH_REFLECT_9
+// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_75 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=ARCH_REFLECT_10
+// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_80 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=ARCH_REFLECT_11
+// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -target-cpu sm_86 -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=ARCH_REFLECT_12
+
+// Check that -fcuda-prec-sqrt flag makes nvvm_reflect("__CUDA_PREC_SQRT") return 1
+// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -fcuda-prec-sqrt -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=PREC_SQRT_REFLECT
+// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple nvptx64-nvidia-cuda -S -emit-llvm -x c++ %s -o - | FileCheck %s --check-prefix=NO_PREC_SQRT_REFLECT
 
 #include "sycl.hpp"
 
@@ -23,19 +27,26 @@ queue q{};
 
 int main() {
   // NO_NVVM_REFLECT_PASS: call i32 @llvm.nvvm.reflect
-  // NVVM_REFLECT_PASS_2: store i32 500
-  // NVVM_REFLECT_PASS_3: store i32 520
-  // NVVM_REFLECT_PASS_4: store i32 530
-  // NVVM_REFLECT_PASS_5: store i32 600
-  // NVVM_REFLECT_PASS_6: store i32 610
-  // NVVM_REFLECT_PASS_7: store i32 620
-  // NVVM_REFLECT_PASS_8: store i32 700
-  // NVVM_REFLECT_PASS_9: store i32 720
-  // NVVM_REFLECT_PASS_10: store i32 750
-  // NVVM_REFLECT_PASS_11: store i32 800
-  // NVVM_REFLECT_PASS_12: store i32 860
+  // ARCH_REFLECT_2: store i32 500
+  // ARCH_REFLECT_3: store i32 520
+  // ARCH_REFLECT_4: store i32 530
+  // ARCH_REFLECT_5: store i32 600
+  // ARCH_REFLECT_6: store i32 610
+  // ARCH_REFLECT_7: store i32 620
+  // ARCH_REFLECT_8: store i32 700
+  // ARCH_REFLECT_9: store i32 720
+  // ARCH_REFLECT_10: store i32 750
+  // ARCH_REFLECT_11: store i32 800
+  // ARCH_REFLECT_12: store i32 860
   q.submit([&](handler &cgh) {
     cgh.single_task(
         [=]() { printf("%d", __nvvm_reflect((char *)"__CUDA_ARCH")); });
+  });
+
+  // PREC_SQRT_REFLECT: store i32 1
+  // NO_PREC_SQRT_REFLECT: store i32 0
+  q.submit([&](handler &cgh) {
+    cgh.single_task(
+        [=]() { printf("%d", __nvvm_reflect((char *)"__CUDA_PREC_SQRT")); });
   });
 }
