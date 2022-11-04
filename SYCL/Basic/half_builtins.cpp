023c6310d0f402c33edfc0b5a83a5d25f1f8e1de
Steffen Larsen <steffen.larsen@intel.com>
2022-11-04 16:54:30 +0100

    [SYCL] Prevent use of fp64 and fp16 when unsupported in more tests (#1353)
    A number of tests currently use double and sycl::half despite them not
    being supported by the used device. Although it does not necessarily
    fail to run on those devices, they are not considered supported by the
    SYCL specification and should not be used in testing on those devices.
    This commit prevent parts of a selection of tests from running on
    devices that do not support the types used.
    
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- SYCL/Basic/half_builtins.cpp
+++ SYCL/Basic/half_builtins.cpp
@@ -165,6 +165,14 @@ template <int N> bool check(vec<float, N> a, vec<float, N> b) {
 
 int main() {
   queue q;
+
+  if (!q.get_device().has(sycl::aspect::fp16)) {
+    std::cout
+        << "Test was skipped because the selected device does not support fp16"
+        << std::endl;
+    return 0;
+  }
+
   float16 a, b, c, d;
   for (int i = 0; i < SZ_max; i++) {
     a[i] = i / (float)SZ_max;
