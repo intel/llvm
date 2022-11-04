9673e9595ce5e9908bec93696e22384f3513158c
Nikita <nikita.kornev@intel.com>
2022-11-04 17:05:16 +0100

    Update clang/test/CodeGenSYCL/simple-sycl-virtual-function.cpp
    Co-authored-by: Alexey Sachkov <alexey.sachkov@intel.com>

--- clang/test/CodeGenSYCL/simple-sycl-virtual-function.cpp
+++ clang/test/CodeGenSYCL/simple-sycl-virtual-function.cpp
@@ -1,4 +1,4 @@
-// RUN: clang++ -cc1 -fsycl-allow-virtual-functions -fsycl-is-device -internal-isystem Inputs -emit-llvm %s -o - | FileCheck %s
+// RUN: %clang_cc1 -fsycl-allow-virtual-functions -fsycl-is-device -internal-isystem Inputs -emit-llvm %s -o - | FileCheck %s
 // RUN: clang++ -cc1 -fsycl-is-device -internal-isystem Inputs -emit-llvm %s -o - &> %t.txt | FileCheck %s --input-file %t.txt --check-prefix CHECK-WO-KEY
 
 // CHECK: @_ZTVN10__cxxabiv120__si_class_type_infoE = external addrspace(1) global ptr addrspace(1)
