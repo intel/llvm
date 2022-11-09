5bd23d5b447212fe60a2550995f085fb7a7f3ea4
Dounia Khaldi <dounia.khaldi@intel.com>
2022-11-09 03:13:15 -0600

    Add PVC tests and move XMX8 tests to a new folder (#1347)

--- SYCL/Matrix/XMX8/element_wise_ops.cpp
+++ SYCL/Matrix/XMX8/element_wise_ops.cpp
@@ -1,14 +1,14 @@
-//==-------- joint_matrix_half.cpp  - DPC++ joint_matrix------------ ----==//
+//==----------- element_wise_ops.cpp  - DPC++ joint_matrix------------- ----==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-// REQUIRES: matrix
+// REQUIRES: matrix-xmx8
 
 // RUN: %clangxx -fsycl %s -o %t.out
-// Only run on the GPU because half is not supported on AMX hardware
+// RUN: %CPU_RUN_PLACEHOLDER %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 
 #include <iostream>
@@ -17,6 +17,6 @@
 using namespace sycl;
 using namespace sycl::ext::oneapi::experimental::matrix;
 
-#define SG_SZ 16
+#define SG_SZ 8
 
-#include "joint_matrix_half_impl.hpp"
+#include "../element_wise_ops_impl.hpp"
