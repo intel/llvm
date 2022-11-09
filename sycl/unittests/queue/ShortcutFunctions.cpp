43aac8779e47715343b5d6f364f086e43827de35
Lamzed-Short, Andrew <andrew.lamzed-short@intel.com>
2022-11-09 09:39:50 -0800

    Fixed copyright header message

--- sycl/unittests/queue/ShortcutFunctions.cpp
+++ sycl/unittests/queue/ShortcutFunctions.cpp
@@ -1,5 +1,4 @@
-//==-------------- ShortcutFunctions.cpp --- queue unit tests
-//---------------==//
+//==-------------- ShortcutFunctions.cpp --- queue unit tests --------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
