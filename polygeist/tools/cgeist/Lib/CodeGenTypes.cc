7d6904cdae60dab5ea5e5e0bcf118c4181cb5de8
Tsang, Whitney <whitney.tsang@intel.com>
2022-11-10 12:21:01 -0500

    address review comment
    Signed-off-by: Tsang, Whitney <whitney.tsang@intel.com>

--- polygeist/tools/cgeist/Lib/CodeGenTypes.cc
+++ polygeist/tools/cgeist/Lib/CodeGenTypes.cc
@@ -1537,6 +1537,8 @@ mlir::Type CodeGenTypes::getMLIRType(clang::QualType QT, bool *ImplicitRef,
 
     if (isa<clang::VectorType>(PTT) || isa<clang::ComplexType>(PTT)) {
       if (auto VT = SubType.dyn_cast<mlir::VectorType>())
+        // FIXME: We should create memref of rank 0.
+        // Details: https://github.com/intel/llvm/issues/7354
         return mlir::MemRefType::get(Outer, SubType);
       if (SubType.isa<MemRefType>()) {
         assert(SubRef);
