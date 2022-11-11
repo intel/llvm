41a238472758fa5346a3c8cec57e8d8e39d7dce6
Tiotto, Ettore <ettore.tiotto@intel.com>
2022-11-11 12:59:37 -0500

    Address code review comments
    Signed-off-by: Tiotto, Ettore <ettore.tiotto@intel.com>

--- polygeist/tools/cgeist/Test/Verification/static.c
+++ polygeist/tools/cgeist/Test/Verification/static.c
@@ -6,7 +6,7 @@ int foo() {
   return bar[0];
 }
 
-// CHECK-DAG:   memref.global "private" @"foo@static@bar" : memref<8xi32> = dense<0> {alignment = 16 : i64}
+// CHECK: memref.global "private" @"foo@static@bar" : memref<8xi32> = dense<0> {alignment = 16 : i64}
 // CHECK-LABEL:   func @foo() -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
 // CHECK-NEXT:     %0 = memref.get_global @"foo@static@bar" : memref<8xi32>
 // CHECK-NEXT:     %1 = affine.load %0[0] : memref<8xi32>
