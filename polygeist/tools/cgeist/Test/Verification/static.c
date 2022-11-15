c61628fa16a53ae49722768dcf833caf194a79db
Ettore Tiotto <ettore.tiotto@intel.com>
2022-11-15 08:53:02 -0500

    [SYCL-MLIR]: Zero initialize global variables (#7361)
    This patch initializes tentative definition for global variable to {0}.
    
    Fixes https://github.com/intel/llvm/issues/7355
    
    Signed-off-by: Tiotto, Ettore <ettore.tiotto@intel.com>

--- polygeist/tools/cgeist/Test/Verification/static.c
+++ polygeist/tools/cgeist/Test/Verification/static.c
@@ -6,8 +6,8 @@ int foo() {
   return bar[0];
 }
 
-// CHECK:   memref.global "private" @"foo@static@bar" : memref<8xi32> = uninitialized {alignment = 16 : i64}
-// CHECK:   func @foo() -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
+// CHECK: memref.global "private" @"foo@static@bar" : memref<8xi32> = dense<0> {alignment = 16 : i64}
+// CHECK-LABEL:   func @foo() -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
 // CHECK-NEXT:     %0 = memref.get_global @"foo@static@bar" : memref<8xi32>
 // CHECK-NEXT:     %1 = affine.load %0[0] : memref<8xi32>
 // CHECK-NEXT:     return %1 : i32
