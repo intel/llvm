acca608981dbac237493ac196c351c09f68562d4
dongkyunahn-intel <dongkyun.ahn@intel.com>
2022-11-17 14:47:51 -0800

    [SYCL][ESIMD][EMU] Sub-group size info in DeviceInfo PI_API (#7439)

--- sycl/plugins/esimd_emulator/pi_esimd_emulator.cpp
+++ sycl/plugins/esimd_emulator/pi_esimd_emulator.cpp
@@ -783,10 +783,11 @@ pi_result piDeviceGetInfo(pi_device Device, pi_device_info ParamName,
   case PI_DEVICE_INFO_REFERENCE_COUNT:
     // TODO : CHECK
     return ReturnValue(pi_uint32{0});
+  case PI_DEVICE_INFO_SUB_GROUP_SIZES_INTEL:
+    return ReturnValue(size_t{1});
 
     CASE_PI_UNSUPPORTED(PI_DEVICE_INFO_MAX_NUM_SUB_GROUPS)
     CASE_PI_UNSUPPORTED(PI_DEVICE_INFO_SUB_GROUP_INDEPENDENT_FORWARD_PROGRESS)
-    CASE_PI_UNSUPPORTED(PI_DEVICE_INFO_SUB_GROUP_SIZES_INTEL)
     CASE_PI_UNSUPPORTED(PI_DEVICE_INFO_IL_VERSION)
 
     // Intel-specific extensions
