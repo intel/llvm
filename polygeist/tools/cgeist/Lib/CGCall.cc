95a50d17ad9418e2e1f9391b6b11ecd180b20dcd
Tsang, Whitney <whitney.tsang@intel.com>
2022-11-18 22:56:49 -0500

    [SYCL-MLIR] Use unsupportedFuncs list instead of supportedFuncs list
    Signed-off-by: Tsang, Whitney <whitney.tsang@intel.com>

--- polygeist/tools/cgeist/Lib/CGCall.cc
+++ polygeist/tools/cgeist/Lib/CGCall.cc
@@ -1121,7 +1121,7 @@ ValueCategory MLIRScanner::VisitCallExpr(clang::CallExpr *Expr) {
 
   std::string MangledName =
       MLIRScanner::getMangledFuncName(*Callee, Glob.getCGM());
-  if (GenerateAllSYCLFuncs || isSupportedFunctions(MangledName))
+  if (GenerateAllSYCLFuncs || !isUnsupportedFunctions(MangledName))
     ShouldEmit = true;
 
   FunctionToEmit F(*Callee, mlirclang::getInputContext(builder));
