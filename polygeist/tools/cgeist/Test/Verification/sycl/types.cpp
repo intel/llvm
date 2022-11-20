29a4a892bd9776ec5084c57d2ae40314b9d0399e
Whitney Tsang <54643204+whitneywhtsang@users.noreply.github.com>
2022-11-19 19:04:22 -0500

    [SYCL-MLIR] Run `buildO0DefaultPipeline` for `OptimizationLevel::O0` (#7452)
    The result of lit are changed because functions with `alwaysinline`
    attribute are now inlined in the `O0` default pipeline.
    
    Signed-off-by: Tsang, Whitney <whitney.tsang@intel.com>

--- polygeist/tools/cgeist/Test/Verification/sycl/types.cpp
+++ polygeist/tools/cgeist/Test/Verification/sycl/types.cpp
@@ -1,4 +1,4 @@
-// RUN: clang++ -fsycl -fsycl-device-only -emit-mlir -o - %s 2> /dev/null | FileCheck %s
+// RUN: clang++ -fsycl -fsycl-device-only -O0 -emit-mlir -o - %s 2> /dev/null | FileCheck %s
 
 #include <sycl/sycl.hpp>
 
