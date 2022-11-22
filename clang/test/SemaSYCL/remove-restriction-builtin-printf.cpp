ac156a5dee4fac79cf1d95acf1ff820555641ea9
srividya sundaram <srividya.sundaram@intel.com>
2022-11-21 21:33:34 -0800

    [SYCL] Support __builtin_printf for SYCL device

---
+++ clang/test/SemaSYCL/remove-restriction-builtin-printf.cpp
@@ -0,0 +1,20 @@
+// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -sycl-std=2020 -verify -fsyntax-only %s
+// This test checks if __builtin_printf does not throw an error when
+// called from within device code.
+
+#include "sycl.hpp"
+
+using namespace sycl;
+queue q;
+
+int main() {
+  int *Ptr[2];
+  // expected-no-diagnostics
+  q.submit([&](handler &h) {
+    h.single_task<class kernelA>([=]() {
+        __builtin_printf("hello, %d\n", 23);
+    });
+  });
+  return 0;
+}
+
