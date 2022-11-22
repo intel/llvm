a87894859cf3d1499c24a3174c9c4da1286f9182
Victor Perez <victor.perez@codeplay.com>
2022-11-22 09:48:13 +0000

    Add more types to tests

--- polygeist/tools/cgeist/Test/Verification/add.c
+++ polygeist/tools/cgeist/Test/Verification/add.c
@@ -1,48 +1,150 @@
-// RUN: cgeist %s --function=* -S | FileCheck %s
+// RUN: cgeist -O0 %s --function=* -S | FileCheck %s
 
+typedef char char_vec __attribute__((ext_vector_type(3)));
+typedef short short_vec __attribute__((ext_vector_type(3)));
 typedef int int_vec __attribute__((ext_vector_type(3)));
+typedef long long_vec __attribute__((ext_vector_type(3)));
 typedef float float_vec __attribute__((ext_vector_type(3)));
+typedef double double_vec __attribute__((ext_vector_type(3)));
 
-// CHECK-LABEL:   func.func @f0(
+// CHECK-LABEL:   func.func @add_i8(
+// CHECK-SAME:                      %[[VAL_0:.*]]: i8,
+// CHECK-SAME:                      %[[VAL_1:.*]]: i8) -> i8
+// CHECK:           %[[VAL_2:.*]] = arith.extsi %[[VAL_0]] : i8 to i32
+// CHECK:           %[[VAL_3:.*]] = arith.extsi %[[VAL_1]] : i8 to i32
+// CHECK:           %[[VAL_4:.*]] = arith.addi %[[VAL_2]], %[[VAL_3]] : i32
+// CHECK:           %[[VAL_5:.*]] = arith.trunci %[[VAL_4]] : i32 to i8
+// CHECK:           return %[[VAL_5]] : i8
+// CHECK:         }
+
+char add_i8(char a, char b) {
+  return a + b;
+}
+
+// CHECK-LABEL:   func.func @add_i16(
+// CHECK-SAME:                       %[[VAL_0:.*]]: i16,
+// CHECK-SAME:                       %[[VAL_1:.*]]: i16) -> i16
+// CHECK:           %[[VAL_2:.*]] = arith.extsi %[[VAL_0]] : i16 to i32
+// CHECK:           %[[VAL_3:.*]] = arith.extsi %[[VAL_1]] : i16 to i32
+// CHECK:           %[[VAL_4:.*]] = arith.addi %[[VAL_2]], %[[VAL_3]] : i32
+// CHECK:           %[[VAL_5:.*]] = arith.trunci %[[VAL_4]] : i32 to i16
+// CHECK:           return %[[VAL_5]] : i16
+// CHECK:         }
+
+short add_i16(short a, short b) {
+  return a + b;
+}
+
+// CHECK-LABEL:   func.func @add_i32(
 // CHECK-SAME:                  %[[VAL_0:.*]]: i32,
 // CHECK-SAME:                  %[[VAL_1:.*]]: i32) -> i32
 // CHECK:           %[[ADD:.*]] = arith.addi %[[VAL_0]], %[[VAL_1]] : i32
 // CHECK:           return %[[ADD]] : i32
 // CHECK:         }
 
-int f0(int a, int b) {
+int add_i32(int a, int b) {
   return a + b;
 }
 
-// CHECK-LABEL:   func.func @f1(
+// CHECK-LABEL:   func.func @add_i64(
+// CHECK-SAME:                       %[[VAL_0:.*]]: i64,
+// CHECK-SAME:                       %[[VAL_1:.*]]: i64) -> i64
+// CHECK:           %[[VAL_2:.*]] = arith.addi %[[VAL_0]], %[[VAL_1]] : i64
+// CHECK:           return %[[VAL_2]] : i64
+// CHECK:         }
+
+long add_i64(long a, long b) {
+  return a + b;
+}
+
+// CHECK-LABEL:   func.func @add_f32(
 // CHECK-SAME:                  %[[VAL_0:.*]]: f32,
 // CHECK-SAME:                  %[[VAL_1:.*]]: f32) -> f32
 // CHECK:           %[[ADD:.*]] = arith.addf %[[VAL_0]], %[[VAL_1]] : f32
 // CHECK:           return %[[ADD]] : f32
 // CHECK:         }
 
-float f1(float a, float b) {
+float add_f32(float a, float b) {
   return a + b;
 }
 
-// CHECK-LABEL:   func.func @f2(
+// CHECK-LABEL:   func.func @add_f64(
+// CHECK-SAME:                       %[[VAL_0:.*]]: f32,
+// CHECK-SAME:                       %[[VAL_1:.*]]: f32) -> f32
+// CHECK:           %[[VAL_2:.*]] = arith.addf %[[VAL_0]], %[[VAL_1]] : f32
+// CHECK:           return %[[VAL_2]] : f32
+// CHECK:         }
+
+float add_f64(float a, float b) {
+  return a + b;
+}
+
+// CHECK-LABEL:   func.func @add_vi8(
+// CHECK-SAME:                  %[[VAL_0:.*]]: vector<3xi8>,
+// CHECK-SAME:                  %[[VAL_1:.*]]: vector<3xi8>) -> vector<3xi8>
+// CHECK:           %[[ADD:.*]] = arith.addi %[[VAL_0]], %[[VAL_1]] : vector<3xi8>
+// CHECK:           return %[[ADD]] : vector<3xi8>
+// CHECK:         }
+
+char_vec add_vi8(char_vec a, char_vec b) {
+  return a + b;
+}
+
+// CHECK-LABEL:   func.func @add_vi16(
+// CHECK-SAME:                        %[[VAL_0:.*]]: vector<3xi16>,
+// CHECK-SAME:                        %[[VAL_1:.*]]: vector<3xi16>) -> vector<3xi16>
+// CHECK:           %[[VAL_2:.*]] = arith.addi %[[VAL_0]], %[[VAL_1]] : vector<3xi16>
+// CHECK:           return %[[VAL_2]] : vector<3xi16>
+// CHECK:         }
+
+short_vec add_vi16(short_vec a, short_vec b) {
+  return a + b;
+}
+
+// CHECK-LABEL:   func.func @add_vi32(
 // CHECK-SAME:                  %[[VAL_0:.*]]: vector<3xi32>,
 // CHECK-SAME:                  %[[VAL_1:.*]]: vector<3xi32>) -> vector<3xi32>
 // CHECK:           %[[ADD:.*]] = arith.addi %[[VAL_0]], %[[VAL_1]] : vector<3xi32>
 // CHECK:           return %[[ADD]] : vector<3xi32>
 // CHECK:         }
 
-int_vec f2(int_vec a, int_vec b) {
+int_vec add_vi32(int_vec a, int_vec b) {
   return a + b;
 }
 
-// CHECK-LABEL:   func.func @f4(
+// CHECK-LABEL:   func.func @add_vi64(
+// CHECK-SAME:                        %[[VAL_0:.*]]: memref<?xvector<3xi64>>,
+// CHECK-SAME:                        %[[VAL_1:.*]]: memref<?xvector<3xi64>>) -> vector<3xi64>
+// CHECK:           %[[VAL_2:.*]] = affine.load %[[VAL_0]][0] : memref<?xvector<3xi64>>
+// CHECK:           %[[VAL_3:.*]] = affine.load %[[VAL_1]][0] : memref<?xvector<3xi64>>
+// CHECK:           %[[VAL_4:.*]] = arith.addi %[[VAL_2]], %[[VAL_3]] : vector<3xi64>
+// CHECK:           return %[[VAL_4]] : vector<3xi64>
+// CHECK:         }
+
+long_vec add_vi64(long_vec a, long_vec b) {
+  return a + b;
+}
+
+// CHECK-LABEL:   func.func @add_vf32(
 // CHECK-SAME:                  %[[VAL_0:.*]]: vector<3xf32>,
 // CHECK-SAME:                  %[[VAL_1:.*]]: vector<3xf32>) -> vector<3xf32>
 // CHECK:           %[[ADD:.*]] = arith.addf %[[VAL_0]], %[[VAL_1]] : vector<3xf32>
 // CHECK:           return %[[ADD]] : vector<3xf32>
 // CHECK:         }
 
-float_vec f4(float_vec a, float_vec b) {
+float_vec add_vf32(float_vec a, float_vec b) {
+  return a + b;
+}
+
+// CHECK-LABEL:   func.func @add_vf64(
+// CHECK-SAME:                        %[[VAL_0:.*]]: memref<?xvector<3xf64>>,
+// CHECK-SAME:                        %[[VAL_1:.*]]: memref<?xvector<3xf64>>) -> vector<3xf64>
+// CHECK:           %[[VAL_2:.*]] = affine.load %[[VAL_0]][0] : memref<?xvector<3xf64>>
+// CHECK:           %[[VAL_3:.*]] = affine.load %[[VAL_1]][0] : memref<?xvector<3xf64>>
+// CHECK:           %[[VAL_4:.*]] = arith.addf %[[VAL_2]], %[[VAL_3]] : vector<3xf64>
+// CHECK:           return %[[VAL_4]] : vector<3xf64>
+// CHECK:         }
+
+double_vec add_vf64(double_vec a, double_vec b) {
   return a + b;
 }
--- polygeist/tools/cgeist/Test/Verification/sub.c
+++ polygeist/tools/cgeist/Test/Verification/sub.c
@@ -1,68 +1,170 @@
-// RUN: cgeist %s --function=* -S | FileCheck %s
+// RUN: cgeist -O0 %s --function=* -S | FileCheck %s
 
 #include <stddef.h>
 
+typedef char char_vec __attribute__((ext_vector_type(3)));
+typedef short short_vec __attribute__((ext_vector_type(3)));
 typedef int int_vec __attribute__((ext_vector_type(3)));
+typedef long long_vec __attribute__((ext_vector_type(3)));
 typedef float float_vec __attribute__((ext_vector_type(3)));
+typedef double double_vec __attribute__((ext_vector_type(3)));
 
-// CHECK-LABEL:   func.func @f0(
-// CHECK-SAME:                  %[[VAL_0:.*]]: i32,
-// CHECK-SAME:                  %[[VAL_1:.*]]: i32) -> i32
-// CHECK:           %[[SUB:.*]] = arith.subi %[[VAL_0]], %[[VAL_1]] : i32
-// CHECK:           return %[[SUB]] : i32
+// CHECK-LABEL:   func.func @sub_i8(
+// CHECK-SAME:                      %[[VAL_0:.*]]: i8,
+// CHECK-SAME:                      %[[VAL_1:.*]]: i8) -> i8
+// CHECK:           %[[VAL_2:.*]] = arith.extsi %[[VAL_0]] : i8 to i32
+// CHECK:           %[[VAL_3:.*]] = arith.extsi %[[VAL_1]] : i8 to i32
+// CHECK:           %[[VAL_4:.*]] = arith.subi %[[VAL_2]], %[[VAL_3]] : i32
+// CHECK:           %[[VAL_5:.*]] = arith.trunci %[[VAL_4]] : i32 to i8
+// CHECK:           return %[[VAL_5]] : i8
 // CHECK:         }
 
-int f0(int a, int b) {
+char sub_i8(char a, char b) {
   return a - b;
 }
 
-// CHECK-LABEL:   func.func @f1(
+// CHECK-LABEL:   func.func @sub_i16(
+// CHECK-SAME:                       %[[VAL_0:.*]]: i16,
+// CHECK-SAME:                       %[[VAL_1:.*]]: i16) -> i16
+// CHECK:           %[[VAL_2:.*]] = arith.extsi %[[VAL_0]] : i16 to i32
+// CHECK:           %[[VAL_3:.*]] = arith.extsi %[[VAL_1]] : i16 to i32
+// CHECK:           %[[VAL_4:.*]] = arith.subi %[[VAL_2]], %[[VAL_3]] : i32
+// CHECK:           %[[VAL_5:.*]] = arith.trunci %[[VAL_4]] : i32 to i16
+// CHECK:           return %[[VAL_5]] : i16
+// CHECK:         }
+
+short sub_i16(short a, short b) {
+  return a - b;
+}
+
+// CHECK-LABEL:   func.func @sub_i32(
+// CHECK-SAME:                       %[[VAL_0:.*]]: i32,
+// CHECK-SAME:                       %[[VAL_1:.*]]: i32) -> i32
+// CHECK:           %[[VAL_2:.*]] = arith.subi %[[VAL_0]], %[[VAL_1]] : i32
+// CHECK:           return %[[VAL_2]] : i32
+// CHECK:         }
+
+int sub_i32(int a, int b) {
+  return a - b;
+}
+
+// CHECK-LABEL:   func.func @sub_i64(
+// CHECK-SAME:                       %[[VAL_0:.*]]: i64,
+// CHECK-SAME:                       %[[VAL_1:.*]]: i64) -> i64
+// CHECK:           %[[VAL_2:.*]] = arith.subi %[[VAL_0]], %[[VAL_1]] : i64
+// CHECK:           return %[[VAL_2]] : i64
+// CHECK:         }
+
+long sub_i64(long a, long b) {
+  return a - b;
+}
+
+// CHECK-LABEL:   func.func @sub_f32(
 // CHECK-SAME:                  %[[VAL_0:.*]]: f32,
 // CHECK-SAME:                  %[[VAL_1:.*]]: f32) -> f32
 // CHECK:           %[[SUB:.*]] = arith.subf %[[VAL_0]], %[[VAL_1]] : f32
 // CHECK:           return %[[SUB]] : f32
 // CHECK:         }
 
-float f1(float a, float b) {
+float sub_f32(float a, float b) {
   return a - b;
 }
 
-// CHECK-LABEL:   func.func @f2(
-// CHECK-SAME:                  %[[VAL_0:.*]]: vector<3xi32>,
-// CHECK-SAME:                  %[[VAL_1:.*]]: vector<3xi32>) -> vector<3xi32>
-// CHECK:           %[[SUB:.*]] = arith.subi %[[VAL_0]], %[[VAL_1]] : vector<3xi32>
-// CHECK:           return %[[SUB]] : vector<3xi32>
+// CHECK-LABEL:   func.func @sub_f64(
+// CHECK-SAME:                       %[[VAL_0:.*]]: f32,
+// CHECK-SAME:                       %[[VAL_1:.*]]: f32) -> f32
+// CHECK:           %[[VAL_2:.*]] = arith.subf %[[VAL_0]], %[[VAL_1]] : f32
+// CHECK:           return %[[VAL_2]] : f32
 // CHECK:         }
 
-int_vec f2(int_vec a, int_vec b) {
+float sub_f64(float a, float b) {
   return a - b;
 }
 
-// CHECK-LABEL:   func.func @f3(
+// CHECK-LABEL:   func.func @sub_vi8(
+// CHECK-SAME:                       %[[VAL_0:.*]]: vector<3xi8>,
+// CHECK-SAME:                       %[[VAL_1:.*]]: vector<3xi8>) -> vector<3xi8>
+// CHECK:           %[[VAL_2:.*]] = arith.subi %[[VAL_0]], %[[VAL_1]] : vector<3xi8>
+// CHECK:           return %[[VAL_2]] : vector<3xi8>
+// CHECK:         }
+
+char_vec sub_vi8(char_vec a, char_vec b) {
+  return a - b;
+}
+
+// CHECK-LABEL:   func.func @sub_vi16(
+// CHECK-SAME:                        %[[VAL_0:.*]]: vector<3xi16>,
+// CHECK-SAME:                        %[[VAL_1:.*]]: vector<3xi16>) -> vector<3xi16>
+// CHECK:           %[[VAL_2:.*]] = arith.subi %[[VAL_0]], %[[VAL_1]] : vector<3xi16>
+// CHECK:           return %[[VAL_2]] : vector<3xi16>
+// CHECK:         }
+
+short_vec sub_vi16(short_vec a, short_vec b) {
+  return a - b;
+}
+
+// CHECK-LABEL:   func.func @sub_vi32(
+// CHECK-SAME:                        %[[VAL_0:.*]]: vector<3xi32>,
+// CHECK-SAME:                        %[[VAL_1:.*]]: vector<3xi32>) -> vector<3xi32>
+// CHECK:           %[[VAL_2:.*]] = arith.subi %[[VAL_0]], %[[VAL_1]] : vector<3xi32>
+// CHECK:           return %[[VAL_2]] : vector<3xi32>
+// CHECK:         }
+
+int_vec sub_vi32(int_vec a, int_vec b) {
+  return a - b;
+}
+
+// CHECK-LABEL:   func.func @sub_vi64(
+// CHECK-SAME:                        %[[VAL_0:.*]]: memref<?xvector<3xi64>>,
+// CHECK-SAME:                        %[[VAL_1:.*]]: memref<?xvector<3xi64>>) -> vector<3xi64>
+// CHECK:           %[[VAL_2:.*]] = affine.load %[[VAL_0]][0] : memref<?xvector<3xi64>>
+// CHECK:           %[[VAL_3:.*]] = affine.load %[[VAL_1]][0] : memref<?xvector<3xi64>>
+// CHECK:           %[[VAL_4:.*]] = arith.subi %[[VAL_2]], %[[VAL_3]] : vector<3xi64>
+// CHECK:           return %[[VAL_4]] : vector<3xi64>
+// CHECK:         }
+
+long_vec sub_vi64(long_vec a, long_vec b) {
+  return a - b;
+}
+
+// CHECK-LABEL:   func.func @sub_vf32(
 // CHECK-SAME:                  %[[VAL_0:.*]]: vector<3xf32>,
 // CHECK-SAME:                  %[[VAL_1:.*]]: vector<3xf32>) -> vector<3xf32>
 // CHECK:           %[[SUB:.*]] = arith.subf %[[VAL_0]], %[[VAL_1]] : vector<3xf32>
 // CHECK:           return %[[SUB]] : vector<3xf32>
 // CHECK:         }
 
-float_vec f3(float_vec a, float_vec b) {
+float_vec sub_vf32(float_vec a, float_vec b) {
   return a - b;
 }
 
-// CHECK-LABEL:   func.func @f4(
-// CHECK-SAME:                  %[[VAL_0:.*]]: !llvm.ptr<i8>,
-// CHECK-SAME:                  %[[VAL_1:.*]]: !llvm.ptr<i8>) -> i64
-// CHECK:           %[[INT_0:.*]] = llvm.ptrtoint %[[VAL_0]] : !llvm.ptr<i8> to i64
-// CHECK:           %[[INT_1:.*]] = llvm.ptrtoint %[[VAL_1]] : !llvm.ptr<i8> to i64
-// CHECK:           %[[SUB:.*]] = arith.subi %[[INT_0]], %[[INT_1]] : i64
-// CHECK:           return %[[SUB]] : i64
+// CHECK-LABEL:   func.func @sub_vf64(
+// CHECK-SAME:                        %[[VAL_0:.*]]: memref<?xvector<3xf64>>,
+// CHECK-SAME:                        %[[VAL_1:.*]]: memref<?xvector<3xf64>>) -> vector<3xf64>
+// CHECK:           %[[VAL_2:.*]] = affine.load %[[VAL_0]][0] : memref<?xvector<3xf64>>
+// CHECK:           %[[VAL_3:.*]] = affine.load %[[VAL_1]][0] : memref<?xvector<3xf64>>
+// CHECK:           %[[VAL_4:.*]] = arith.subf %[[VAL_2]], %[[VAL_3]] : vector<3xf64>
+// CHECK:           return %[[VAL_4]] : vector<3xf64>
 // CHECK:         }
 
-size_t f4(char *a, char *b) {
+double_vec sub_vf64(double_vec a, double_vec b) {
   return a - b;
 }
 
-// CHECK-LABEL:   func.func @f5(
+// CHECK-LABEL:   func.func @ptr_diff_i8(
+// CHECK-SAME:                  %[[VAL_0:.*]]: !llvm.ptr<i8>,
+// CHECK-SAME:                  %[[VAL_1:.*]]: !llvm.ptr<i8>) -> i64
+// CHECK:           %[[VAL_2:.*]] = llvm.ptrtoint %[[VAL_0]] : !llvm.ptr<i8> to i64
+// CHECK:           %[[VAL_3:.*]] = llvm.ptrtoint %[[VAL_1]] : !llvm.ptr<i8> to i64
+// CHECK:           %[[VAL_4:.*]] = arith.subi %[[VAL_2]], %[[VAL_3]] : i64
+// CHECK:           return %[[VAL_4]] : i64
+// CHECK:         }
+
+size_t ptr_diff_i8(char *a, char *b) {
+  return a - b;
+}
+
+// CHECK-LABEL:   func.func @ptr_diff_f32(
 // CHECK-SAME:                  %[[VAL_0:.*]]: memref<?xf32>,
 // CHECK-SAME:                  %[[VAL_1:.*]]: memref<?xf32>) -> i64
 // CHECK:           %[[I64_0:.*]] = arith.constant 4 : i64
@@ -75,6 +177,6 @@ size_t f4(char *a, char *b) {
 // CHECK:           return %[[SUB]] : i64
 // CHECK:         }
 
-size_t f5(float *a, float *b) {
+size_t ptr_diff_f32(float *a, float *b) {
   return a - b;
 }
