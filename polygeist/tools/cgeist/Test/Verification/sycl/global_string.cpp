8b8057fa560c6e4c04f3b0f0a5dba1d38308144d
arnamoy10 <arnamoy.bhattacharyya@intel.com>
2022-11-22 08:54:53 -0500

    Update polygeist/tools/cgeist/Test/Verification/sycl/global_string.cpp
    Co-authored-by: Whitney Tsang <54643204+whitneywhtsang@users.noreply.github.com>

--- polygeist/tools/cgeist/Test/Verification/sycl/global_string.cpp
+++ polygeist/tools/cgeist/Test/Verification/sycl/global_string.cpp
@@ -4,13 +4,13 @@
 void test_global_string(sycl::device d) {
   auto q = sycl::queue(d); 
   {
-	  q.submit([&] (sycl::handler& cgh) {
+    q.submit([&] (sycl::handler& cgh) {
        cgh.single_task<class printkernel>([=] {
           // Test that the string is generated in the gpu module
           // MLIR: gpu.module @device_functions {
           // MLIR-NEXT: llvm.mlir.global internal constant @str0("Hello\00") {addr_space = 0 : i32}
           char str1[10] = "Hello";
-        });
+       });
     });
   }
 }
