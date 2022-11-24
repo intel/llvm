c4c7da08bc54c09c0f857d256728edf56a6dd938
jinge90 <ge.jin@intel.com>
2022-11-24 16:09:17 +0800

    remove unnecessary alias for uint16_t
    Signed-off-by: jinge90 <ge.jin@intel.com>

--- sycl/include/sycl/ext/intel/math/imf_bf16.hpp
+++ sycl/include/sycl/ext/intel/math/imf_bf16.hpp
@@ -13,15 +13,14 @@
 #include <type_traits>
 
 using sycl_bfloat16 = sycl::ext::oneapi::experimental::bfloat16;
-using _iml_bfloat16_internal = uint16_t;
 
 extern "C" {
-float __imf_bfloat162float(_iml_bfloat16_internal);
-_iml_bfloat16_internal __imf_float2bfloat16(float);
-_iml_bfloat16_internal __imf_float2bfloat16_rd(float);
-_iml_bfloat16_internal __imf_float2bfloat16_rn(float);
-_iml_bfloat16_internal __imf_float2bfloat16_ru(float);
-_iml_bfloat16_internal __imf_float2bfloat16_rz(float);
+float __imf_bfloat162float(uint16_t);
+uint16_t __imf_float2bfloat16(float);
+uint16_t __imf_float2bfloat16_rd(float);
+uint16_t __imf_float2bfloat16_rn(float);
+uint16_t __imf_float2bfloat16_ru(float);
+uint16_t __imf_float2bfloat16_rz(float);
 };
 
 namespace sycl {
@@ -33,11 +32,11 @@ namespace math {
 // Need to ensure that sycl bfloat16 defined in bfloat16.hpp is compatible
 // with uint16_t in layout.
 #if __cplusplus >= 201703L
-static_assert(sizeof(sycl_bfloat16) == sizeof(_iml_bfloat16_internal),
-              "sycl bfloat16 is not compatible with _iml_bfloat16_internal.");
+static_assert(sizeof(sycl_bfloat16) == sizeof(uint16_t),
+              "sycl bfloat16 is not compatible with uint16_t.");
 
 float bfloat162float(sycl_bfloat16 b) {
-  return __imf_bfloat162float(__builtin_bit_cast(_iml_bfloat16_internal, b));
+  return __imf_bfloat162float(__builtin_bit_cast(uint16_t, b));
 }
 
 sycl_bfloat16 float2bfloat16(float f) {
@@ -60,22 +59,36 @@ sycl_bfloat16 float2bfloat16_rz(float f) {
   return __builtin_bit_cast(sycl_bfloat16, __imf_float2bfloat16_rz(f));
 }
 
-bool hisnan(sycl_bfloat b) { return sycl::isnan(bfloat162float(b)); }
+bool hisnan(sycl_bfloat16 b) { return sycl::isnan(bfloat162float(b)); }
 
-bool hisinf(sycl_bfloat b) { return sycl::isinf(bfloat162float(b)); }
+bool hisinf(sycl_bfloat16 b) { return sycl::isinf(bfloat162float(b)); }
 
 bool heq(sycl_bfloat16 b1, sycl_bfloat16 b2) {
   if (hisnan(b1) || hisnan(b2))
     return false;
-  return __builtin_bit_cast(_iml_bfloat16_internal, b1) ==
-         __builtin_bit_cast(_iml_bfloat16_internal, b2);
+  return __builtin_bit_cast(uint16_t, b1) ==
+         __builtin_bit_cast(uint16_t, b2);
 }
 
 bool hequ(sycl_bfloat16 b1, sycl_bfloat16 b2) {
   if (hisnan(b1) || hisnan(b1))
     return true;
-  return __builtin_bit_cast(_iml_bfloat16_internal, b1) ==
-         __builtin_bit_cast(_iml_bfloat16_internal, b2);
+  return __builtin_bit_cast(uint16_t, b1) ==
+         __builtin_bit_cast(uint16_t, b2);
+}
+
+bool hne(sycl_bfloat16 b1, sycl_bfloat16 b2) {
+  if (hisnan(b1) || hisnan(b2))
+    return false;
+  return __builtin_bit_cast(uint16_t, b1) !=
+         __builtin_bit_cast(uint16_t, b2);
+}
+
+bool hneu(sycl_bfloat16 b1, sycl_bfloat16 b2) {
+  if (hisnan(b1) || hisnan(b2))
+    return true;
+  return __builtin_bit_cast(uint16_t, b1) !=
+         __builtin_bit_cast(uint16_t, b2);
 }
 
 bool hge(sycl_bfloat16 b1, sycl_bfloat16 b2) {
@@ -143,60 +156,63 @@ bool hltu(sycl_bfloat16 b1, sycl_bfloat16 b2) {
 }
 
 sycl_bfloat16 hmax(sycl_bfloat16 b1, sycl_bfloat16 b2) {
-  _iml_bfloat16_internal ibi = 0x7FC0;
+  uint16_t canonical_nan = 0x7FC0;
+  uint16_t b1a = __builtin_bit_cast(uint16_t, b1);
+  uint16_t b2a = __builtin_bit_cast(uint16_t, b2);
   if (hisnan(b1) && hisnan(b2))
-    return __builtin_bit_cast(sycl_bfloat16, ibi);
+    return __builtin_bit_cast(sycl_bfloat16, canonical_nan);
   if (hisnan(b1))
     return b2;
   else if (hisnan(b2))
     return b1;
+  else if (((b1a | b2a) == 0x8000) && ((b1a & b2a) == 0x0))
+    return __builtin_bit_cast(sycl_bfloat16, static_cast<uint16_t>(0x0));
   else {
     return (hgt(b1, b2) ? b1 : b2);
   }
 }
 
 sycl_bfloat16 hmax_nan(sycl_bfloat16 b1, sycl_bfloat16 b2) {
-  _iml_bfloat16_internal ibi = 0x7FC0;
+  uint16_t canonical_nan = 0x7FC0;
+  uint16_t b1a = __builtin_bit_cast(uint16_t, b1);
+  uint16_t b2a = __builtin_bit_cast(uint16_t, b2);
   if (hisnan(b1) || hisnan(b2))
-    return __builtin_bit_cast(sycl_bfloat16, ibi);
+    return __builtin_bit_cast(sycl_bfloat16, canonical_nan);
+  else if (((b1a | b2a) == 0x8000) && ((b1a & b2a) == 0x0))
+    return __builtin_bit_cast(sycl_bfloat16, static_cast<uint16_t>(0x0));
   else
     return (hgt(b1, b2) ? b1 : b2);
 }
 
 sycl_bfloat16 hmin(sycl_bfloat16 b1, sycl_bfloat16 b2) {
-  _iml_bfloat16_internal ibi = 0x7FC0;
+  uint16_t canonical_nan = 0x7FC0;
+  uint16_t b1a = __builtin_bit_cast(uint16_t, b1);
+  uint16_t b2a = __builtin_bit_cast(uint16_t, b2);
   if (hisnan(b1) && hisnan(b2))
-    return __builtin_bit_cast(sycl_bfloat16, ibi);
+    return __builtin_bit_cast(sycl_bfloat16, canonical_nan);
   if (hisnan(b1))
     return b2;
   else if (hisnan(b2))
     return b1;
+  else if (((b1a | b2a) == 0x8000) && ((b1a & b2a) == 0x0))
+    return __builtin_bit_cast(sycl_bfloat16, static_cast<uint16_t>(0x8000));
   else {
     return (hlt(b1, b2) ? b1 : b2);
   }
 }
 
 sycl_bfloat16 hmin_nan(sycl_bfloat16 b1, sycl_bfloat16 b2) {
-  _iml_bfloat16_internal ibi = 0x7FC0;
+  uint16_t canonical_nan = 0x7FC0;
+  uint16_t b1a = __builtin_bit_cast(uint16_t, b1);
+  uint16_t b2a = __builtin_bit_cast(uint16_t, b2);
   if (hisnan(b1) || hisnan(b2))
-    return __builtin_bit_cast(sycl_bfloat16, ibi);
+    return __builtin_bit_cast(sycl_bfloat16, canonical_nan);
+  else if (((b1a | b2a) == 0x8000) && ((b1a & b2a) == 0x0))
+    return __builtin_bit_cast(sycl_bfloat16, static_cast<uint16_t>(0x8000));
   else
     return (hlt(b1, b2) ? b1 : b2);
 }
 
-bool hne(sycl_bfloat16 b1, sycl_bfloat16 b2) {
-  if (hisnan(b1) || hisnan(b2))
-    return false;
-  return __builtin_bit_cast(_iml_bfloat16_internal, b1) !=
-         __builtin_bit_cast(_iml_bfloat16_internal, b2);
-}
-
-bool hneu(sycl_bfloat16 b1, sycl_bfloat16 b2) {
-  if (hisnan(b1) || hisnan(b2))
-    return true;
-  return __builtin_bit_cast(_iml_bfloat16_internal, b1) !=
-         __builtin_bit_cast(_iml_bfloat16_internal, b2);
-}
 #endif
 } // namespace math
 } // namespace intel
