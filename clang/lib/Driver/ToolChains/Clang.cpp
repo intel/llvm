1366e7d61549b72265b9817a2cadc18efb8d896e
Sidorov, Dmitry <dmitry.sidorov@intel.com>
2022-11-24 08:30:18 -0800

    [SYCL] Enable SPV_INTEL_memory_access_aliasing extension
    Signed-off-by: Sidorov, Dmitry <dmitry.sidorov@intel.com>

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -9554,7 +9554,8 @@ void SPIRVTranslator::ConstructJob(Compilation &C, const JobAction &JA,
                 ",+SPV_INTEL_bfloat16_conversion"
                 ",+SPV_INTEL_joint_matrix"
                 ",+SPV_INTEL_hw_thread_queries"
-                ",+SPV_KHR_uniform_group_instructions";
+                ",+SPV_KHR_uniform_group_instructions"
+                ",+SPV_INTEL_memory_access_aliasing";
     TranslatorArgs.push_back(TCArgs.MakeArgString(ExtArg));
   }
   for (auto I : Inputs) {
