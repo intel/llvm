d240d5a7a55bacede93174c12c0d895b51d0b4c0
JackAKirk <chezjakirk@gmail.com>
2022-11-24 11:05:06 +0000

    Finalized initial ext: float/double only.
    Signed-off-by: JackAKirk <jack.kirk@codeplay.com>

---
+++ sycl/include/sycl/ext/oneapi/experimental/cuda/builtins.hpp
@@ -0,0 +1,82 @@
+//==--- builtins.hpp - SYCL_ONEAPI_CUDA experimental builtins  -------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#define SYCL_EXT_ONEAPI_CUDA_TEX_CACHE_READ 1
+
+#if defined(_WIN32) || defined(_WIN64)
+#define ATTRIBUTE_EXT_VEC_TYPE(N)                                              \
+  __declspec(ext_vector_type(N))
+#else
+#define ATTRIBUTE_EXT_VEC_TYPE(N)                                              \
+  __attribute__((ext_vector_type(N)))
+#endif
+
+namespace sycl {
+__SYCL_INLINE_VER_NAMESPACE(_V1) {
+namespace ext {
+namespace oneapi {
+namespace experimental {
+namespace cuda {
+
+namespace detail {
+using ldg_types = sycl::detail::type_list<float, double, sycl::float2,
+    sycl::float4, sycl::double2>;
+} // namespace detail
+
+template <typename T>
+inline __SYCL_ALWAYS_INLINE std::enable_if_t<
+    sycl::detail::is_contained<
+        T, sycl::ext::oneapi::experimental::cuda::detail::ldg_types>::value,
+    T>
+__ldg(const T *ptr) {
+#if defined(__SYCL_DEVICE_ONLY__) && defined(__NVPTX__)
+  if constexpr (std::is_same_v<T, float>) {
+    return __nvvm_ldg_f(ptr);
+  } else if constexpr (std::is_same_v<T, double>) {
+    return __nvvm_ldg_d(ptr);
+  } else if constexpr (std::is_same_v<T, sycl::float2>) {
+    // We can assume that ptr is aligned at least to float2's alignment, but the
+    // load will assume that ptr is aligned to float2's alignment.  This is only
+    // safe if alignof(f2) <= alignof(float2).
+    typedef float f2 ATTRIBUTE_EXT_VEC_TYPE(2);
+    f2 rv = __nvvm_ldg_f2(reinterpret_cast<const f2 *>(ptr));
+    sycl::float2 ret;
+    ret.x() = rv[0];
+    ret.y() = rv[1];
+    return ret;
+  } else if constexpr (std::is_same_v<T, sycl::float4>) {
+    typedef float f4 ATTRIBUTE_EXT_VEC_TYPE(4);
+    f4 rv = __nvvm_ldg_f4(reinterpret_cast<const f4 *>(ptr));
+    sycl::float4 ret;
+    ret.x() = rv[0];
+    ret.y() = rv[1];
+    ret.z() = rv[2];
+    ret.w() = rv[3];
+    return ret;
+  } else if constexpr (std::is_same_v<T, sycl::double2>) {
+    typedef double d2 ATTRIBUTE_EXT_VEC_TYPE(2);
+    d2 rv = __nvvm_ldg_d2(reinterpret_cast<const d2 *>(ptr));
+    sycl::double2 ret;
+    ret.x() = rv[0];
+    ret.y() = rv[1];
+    return ret;
+  }
+#else
+  throw runtime_error("__ldg is not supported on host device.",
+                      PI_ERROR_INVALID_DEVICE);
+#endif
+}
+
+#undef ATTRIBUTE_EXT_VEC_TYPE(N)
+
+} // namespace cuda
+} // namespace experimental
+} // namespace oneapi
+} // namespace ext
+} // __SYCL_INLINE_VER_NAMESPACE(_V1)
+} // namespace sycl
---
+++ sycl/test/check_device_code/cuda/ldg.cpp
@@ -0,0 +1,65 @@
+// REQUIRES: cuda
+
+// RUN: %clangxx -Xclang -no-opaque-pointers -fsycl-device-only -fsycl-targets=nvptx64-nvidia-cuda -S -Xclang -emit-llvm %s -o -| FileCheck %s
+// RUN: %clangxx -Xclang -opaque-pointers -fsycl-device-only -fsycl-targets=nvptx64-nvidia-cuda -S -Xclang -emit-llvm %s -o -| FileCheck %s --check-prefixes=CHECK-OPAQUE
+
+#include <sycl/sycl.hpp>
+#include <sycl/ext/oneapi/experimental/cuda/builtins.hpp>
+
+using namespace sycl;
+using namespace sycl::ext::oneapi::experimental::cuda;
+using namespace sycl::ext::oneapi::experimental;
+
+int main() {
+
+  sycl::queue q;
+
+  auto *in_f = sycl::malloc_device<float>(1, q);
+  auto *in_d = sycl::malloc_device<double>(1, q);
+
+  auto *in_f2 = sycl::malloc_device<float2>(1, q);
+  auto *in_d2 = sycl::malloc_device<double2>(1, q);
+  
+  auto *in_f4 = sycl::malloc_device<float4>(1, q);
+
+  auto *out_d = sycl::malloc_device<double>(1, q);
+
+  q.wait();
+
+  q.submit([=](sycl::handler &h) {
+    h.single_task<class ldg>([=] {
+
+      //CHECK: tail call float @llvm.nvvm.ldg.global.f.f32.p0f32(float* %0, i32 4)
+      //CHECK-OPAQUE: tail call float @llvm.nvvm.ldg.global.f.f32.p0(ptr %0, i32 4)
+      auto cached_f = __ldg(&in_f[0]);
+      //CHECK: tail call double @llvm.nvvm.ldg.global.f.f64.p0f64(double* %1, i32 8)
+      //CHECK-OPAQUE: tail call double @llvm.nvvm.ldg.global.f.f64.p0(ptr %1, i32 8)
+      auto cached_d = __ldg(&in_d[0]);
+
+      //CHECK: tail call <2 x float> @llvm.nvvm.ldg.global.f.v2f32.p0v2f32(<2 x float>* %8, i32 8)
+      //CHECK-OPAQUE: tail call <2 x float> @llvm.nvvm.ldg.global.f.v2f32.p0(ptr %2, i32 8)
+      auto cached_f2 = __ldg(&in_f2[0]);
+      //CHECK: tail call <2 x double> @llvm.nvvm.ldg.global.f.v2f64.p0v2f64(<2 x double>* %10, i32 16)
+      //CHECK-OPAQUE: tail call <2 x double> @llvm.nvvm.ldg.global.f.v2f64.p0(ptr %3, i32 16)
+      auto cached_d2 = __ldg(&in_d2[0]);
+      //CHECK: tail call <4 x float> @llvm.nvvm.ldg.global.f.v4f32.p0v4f32(<4 x float>* %12, i32 16)
+      //CHECK-OPAQUE: tail call <4 x float> @llvm.nvvm.ldg.global.f.v4f32.p0(ptr %4, i32 16)
+      auto cached_f4 = __ldg(&in_f4[0]);
+
+      out_d[0] = cached_d + cached_d2.x() + cached_f + cached_f2.x() + cached_f4.x();
+    });
+  });
+
+  q.wait();
+
+  free(in_f, q);
+  free(in_d, q);
+
+  free(in_f2, q);
+  free(in_f4, q);
+  free(in_d2, q);
+
+  free(out_d, q);
+
+  return 0;
+};
