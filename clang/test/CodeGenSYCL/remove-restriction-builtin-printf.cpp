457517e94c1b675dc580e610ac74991a7bd0a398
srividya sundaram <srividya.sundaram@intel.com>
2022-11-29 20:59:02 -0800

    Apply suggestion

--- clang/test/CodeGenSYCL/remove-restriction-builtin-printf.cpp
+++ clang/test/CodeGenSYCL/remove-restriction-builtin-printf.cpp
@@ -10,7 +10,7 @@ int main() {
   q.submit([&](handler &h) {
     // CHECK: define {{.*}}spir_kernel {{.*}}
     h.single_task<class kernelA>([=]() {
-      // CHECK: @printf{{.*}}
+      // CHECK: call {{.*}}printf(ptr noundef nonnull dereferenceable(1) @{{.*}}, i32 noundef 24)
         __builtin_printf("hello, %d\n", 24);
     });
   });
