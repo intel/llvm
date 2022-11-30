2cf9d0f08cd938ece8c2d8bbc670cea6f74b9fd7
Pablo Reble <pablo.reble@intel.com>
2022-11-29 21:26:28 -0600

    Cosmetic changes

--- sycl/source/detail/queue_impl.cpp
+++ sycl/source/detail/queue_impl.cpp
@@ -278,13 +278,11 @@ void queue_impl::wait(const detail::code_location &CodeLoc) {
   TelemetryEvent = instrumentationProlog(CodeLoc, Name, StreamID, IId);
 #endif
 
-#if 1
   if (has_property<ext::oneapi::property::queue::lazy_execution>()) {
     const detail::plugin &Plugin = getPlugin();
     if (Plugin.getBackend() == backend::ext_oneapi_level_zero)
       Plugin.call<detail::PiApiKind::piQueueFlush>(getHandleRef());
   }
-#endif
 
   std::vector<std::weak_ptr<event_impl>> WeakEvents;
   std::vector<event> SharedEvents;
