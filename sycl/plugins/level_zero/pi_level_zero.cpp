5a140b38e605746e2fb24f0001427892d30da50b
aelovikov-intel <andrei.elovikov@intel.com>
2022-12-02 12:06:42 -0800

    [SYCL][Level Zero] Don't use ZE_MEMORY_ADVICE_SET_READ_MOSTLY for read-only USM (#7601)
    There are issues in the underlying Level Zero RT causing stability
    problems. This partially reverts
    https://github.com/intel/llvm/pull/7496.

--- sycl/plugins/level_zero/pi_level_zero.cpp
+++ sycl/plugins/level_zero/pi_level_zero.cpp
@@ -7953,9 +7953,8 @@ static pi_result USMSharedAllocImpl(void **ResultPtr, pi_context Context,
   // with the same command list handle.
   std::scoped_lock<pi_mutex> Lock(Context->ImmediateCommandListMutex);
 
-  ZE_CALL(zeCommandListAppendMemAdvise,
-          (Context->ZeCommandListInit, Device->ZeDevice, *ResultPtr, Size,
-           ZE_MEMORY_ADVICE_SET_READ_MOSTLY));
+  // TODO: Underlying Level Zero runtime doesn't have a way to communicate
+  // read-only property yet.
 
   ZE_CALL(zeCommandListAppendMemAdvise,
           (Context->ZeCommandListInit, Device->ZeDevice, *ResultPtr, Size,
