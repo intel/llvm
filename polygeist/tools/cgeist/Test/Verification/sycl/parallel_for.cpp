42727a7002c94ecc2569932e4200f6c5ae115604
Whitney Tsang <54643204+whitneywhtsang@users.noreply.github.com>
2022-12-05 13:18:18 -0500

    [SYCL-MLIR] Add other forms of `parallel_for` test cases (#7625)
    Signed-off-by: Tsang, Whitney <whitney.tsang@intel.com>

--- polygeist/tools/cgeist/Test/Verification/sycl/parallel_for.cpp
+++ polygeist/tools/cgeist/Test/Verification/sycl/parallel_for.cpp
@@ -16,36 +16,73 @@
 // RUN: llvm-dis %t.bc
 // RUN: cat %t.ll | FileCheck %s --check-prefix=LLVM --implicit-check-not="declare{{.*}}spir_func"
 
-// Test that the kernel named `kernel_parallel_for` is generated with the correct signature.
-// LLVM: define weak_odr spir_kernel void {{.*}}kernel_parallel_for(
+// Test that the kernel named `kernel_parallel_for_id` is generated with the correct signature.
+// LLVM: define weak_odr spir_kernel void {{.*}}kernel_parallel_for_id(
 // LLVM-SAME:  i32 addrspace(1)* {{.*}}, [[RANGE_TY:%"class.sycl::_V1::range.1"]]* noundef byval([[RANGE_TY]]) {{.*}}, [[RANGE_TY]]* noundef byval([[RANGE_TY]]) {{.*}}, [[ID_TY:%"class.sycl::_V1::id.1"]]* noundef byval([[ID_TY]]) {{.*}})
 
 #include <sycl/sycl.hpp>
 using namespace sycl;
 static constexpr unsigned N = 8;
 
-void host_parallel_for(std::array<int, N> &A) {
-  auto q = queue{};
-  device d = q.get_device();
-  std::cout << "Using " << d.get_info<info::device::name>() << "\n";
+void parallel_for_id(std::array<int, N> &A, queue q) {
   auto range = sycl::range<1>{N};
 
   {
     auto buf = buffer<int, 1>{A.data(), range};
     q.submit([&](handler &cgh) {
       auto A = buf.get_access<access::mode::write>(cgh);
-      cgh.parallel_for<class kernel_parallel_for>(range, [=](sycl::id<1> id) {
-        A[id] = id;
+      cgh.parallel_for<class kernel_parallel_for_id>(range, [=](id<1> Id) {
+        A[Id] = Id;
       });
     });
   }
 }
 
-int main() {
-  std::array<int, N> A{0};
-  host_parallel_for(A);
-  for (unsigned i = 0; i < N; ++i) {
-    assert(A[i] == i);
+void parallel_for_item(std::array<int, N> &A, queue q) {
+  auto range = sycl::range<1>{N};
+
+  {
+    auto buf = buffer<int, 1>{A.data(), N};
+    q.submit([&](handler &cgh) {
+      auto A = buf.get_access<access::mode::write>(cgh);
+      cgh.parallel_for<class kernel_parallel_for_item>(range, [=](item<1> Item) {
+        id<1> Id = Item.get_id();
+        A[Id] = Id;
+      });
+    });
   }
+}
+
+void parallel_for_nd_item(std::array<int, N> &A, queue q) {
+  nd_range<1> ndRange(N /*globalSize*/, 2 /*localSize*/);
+
+  {
+    auto buf = buffer<int, 1>{A.data(), N};
+    q.submit([&](handler &cgh) {
+      auto A = buf.get_access<access::mode::write>(cgh);
+      cgh.parallel_for<class kernel_parallel_for_nd_item>(ndRange, [=](nd_item<1> NdItem) {
+        id<1> Id = NdItem.get_global_id();
+        A[Id] = Id;
+      });
+    });
+  }
+}
+
+template <typename F>
+static void parallel_for(F Func, queue q) {
+  std::array<int, N> A{0};
+  Func(A, q);
+  for (unsigned i = 0; i < N; ++i)
+    assert(A[i] == i);
+}
+
+int main() {
+  auto q = queue{};
+  device d = q.get_device();
+  std::cout << "Using " << d.get_info<info::device::name>() << "\n";
+
+  parallel_for(parallel_for_id, q);
+  parallel_for(parallel_for_item, q);
+  parallel_for(parallel_for_nd_item, q);
   std::cout << "Test passed" << std::endl;
 }
