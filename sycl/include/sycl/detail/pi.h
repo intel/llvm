57c763304ffa853d941dc2a9761509d9d1195e38
Andrei Elovikov <andrei.elovikov@intel.com>
2022-12-05 17:04:00 -0800

    Bump minor version

--- sycl/include/sycl/detail/pi.h
+++ sycl/include/sycl/detail/pi.h
@@ -60,9 +60,11 @@
 // PI_EXT_ONEAPI_QUEUE_PRIORITY_HIGH queue properties.
 // 11.18 Add new parameter name PI_EXT_ONEAPI_QUEUE_INFO_EMPTY to
 // _pi_queue_info.
+// 11.19 Add new PI_DEVICE_EXT_INTEL_PARTITION_BY_CSLICE piDevicePartition
+// scheme.
 
 #define _PI_H_VERSION_MAJOR 11
-#define _PI_H_VERSION_MINOR 18
+#define _PI_H_VERSION_MINOR 19
 
 #define _PI_STRING_HELPER(a) #a
 #define _PI_CONCAT(a, b) _PI_STRING_HELPER(a.b)
