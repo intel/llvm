1d302c70a656c6611a2a1193c43190c642220961
Sidorov, Dmitry <dmitry.sidorov@intel.com>
2022-12-06 07:20:34 -0800

    fix for W/a
    Signed-off-by: Sidorov, Dmitry <dmitry.sidorov@intel.com>

--- llvm-spirv/lib/SPIRV/SPIRVWriter.cpp
+++ llvm-spirv/lib/SPIRV/SPIRVWriter.cpp
@@ -1817,7 +1817,7 @@ LLVMToSPIRVBase::transValueWithoutDecoration(Value *V, SPIRVBasicBlock *BB,
 
     // TODO: it's W/A for intel/llvm to prevent not fixed SPIR-V consumers
     // from crashing. Need to remove, when we have the fixed drivers
-    // to remove: being
+    // to remove: begin
     {
       std::vector<Instruction *> GEPs;
       std::vector<Instruction *> Loads;
@@ -1826,15 +1826,14 @@ LLVMToSPIRVBase::transValueWithoutDecoration(Value *V, SPIRVBasicBlock *BB,
                         ? nullptr
                         : dyn_cast<FixedVectorType>(
                               GVTy->getNonOpaquePointerElementType());
-      auto ReplaceIfLoad = [&](User *I) -> void {
+      auto ReplaceIfLoad = [&](User *I, ConstantInt *Idx) -> void {
         auto *LD = dyn_cast<LoadInst>(I);
         if (!LD)
           return;
         Loads.push_back(LD);
         const DebugLoc &DLoc = LD->getDebugLoc();
         LoadInst *Load = new LoadInst(VecTy, GV, "", LD);
-        auto *Zero = ConstantInt::get(Type::getInt32Ty(GV->getContext()), 0);
-        ExtractElementInst *Extract = ExtractElementInst::Create(Load, Zero);
+        ExtractElementInst *Extract = ExtractElementInst::Create(Load, Idx);
         if (DLoc)
           Extract->setDebugLoc(DLoc);
         Extract->insertAfter(cast<Instruction>(Load));
@@ -1845,8 +1844,12 @@ LLVMToSPIRVBase::transValueWithoutDecoration(Value *V, SPIRVBasicBlock *BB,
           break;
         if (auto *GEP = dyn_cast<GetElementPtrInst>(UI)) {
           GEPs.push_back(GEP);
-          for (auto *GEPUser : GEP->users())
-            ReplaceIfLoad(GEPUser);
+          for (auto *GEPUser : GEP->users()) {
+            assert(GEP->getNumIndices() == 2 &&
+                   "GEP to ID vector is expected to have exactly 2 indices");
+            auto *Idx = cast<ConstantInt>(GEP->getOperand(2));
+            ReplaceIfLoad(GEPUser, Idx);
+          }
         }
       }
       auto Erase = [](std::vector<Instruction *> &ToErase) {
