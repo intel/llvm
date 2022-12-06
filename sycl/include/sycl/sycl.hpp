0d81f5785e7ca080420d677d0c5a22ee10e1f546
Romanov, Vlad <vlad.romanov@intel.com>
2022-12-06 04:43:28 -0800

    more support

--- sycl/include/sycl/sycl.hpp
+++ sycl/include/sycl/sycl.hpp
@@ -26,11 +26,11 @@
 #include <sycl/event.hpp>
 #include <sycl/exception.hpp>
 #include <sycl/ext/oneapi/atomic.hpp>
+#include <sycl/ext/oneapi/experimental/group_sort.hpp>
 #include <sycl/feature_test.hpp>
 #include <sycl/functional.hpp>
 #include <sycl/group.hpp>
 #include <sycl/group_algorithm.hpp>
-#include <sycl/ext/oneapi/experimental/group_sort.hpp>
 #include <sycl/group_local_memory.hpp>
 #include <sycl/handler.hpp>
 #include <sycl/id.hpp>
