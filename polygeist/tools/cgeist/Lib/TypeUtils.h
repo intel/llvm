107027fe09877db25dd60a26c423e6b480452163
Victor Perez <victor.perez@codeplay.com>
2022-12-08 17:59:48 +0000

    Add doc

--- polygeist/tools/cgeist/Lib/TypeUtils.h
+++ polygeist/tools/cgeist/Lib/TypeUtils.h
@@ -80,6 +80,8 @@ inline bool isAggregateType(mlir::Type Ty) {
 
 unsigned getPrimitiveSizeInBits(mlir::Type Ty);
 
+/// Return whether the input types are suitable for a function abstracted by a
+/// SYCL operation.
 bool areSuitableSYCLOpArgTypes(mlir::TypeRange Types);
 } // namespace mlirclang
 
