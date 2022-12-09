672b783778233580948ad058e914917e6cf23d54
Petr Vesely <petr.vesely@codeplay.com>
2022-12-09 12:42:07 +0000

    [ur] Implement USM 2D operations

--- source/loader/ur_libapi.cpp
+++ source/loader/ur_libapi.cpp
@@ -135,7 +135,7 @@ urContextRelease(
 ///     - ::UR_RESULT_ERROR_INVALID_NULL_HANDLE
 ///         + `NULL == hContext`
 ///     - ::UR_RESULT_ERROR_INVALID_ENUMERATION
-///         + `::UR_CONTEXT_INFO_DEVICES < ContextInfoType`
+///         + `::UR_CONTEXT_INFO_USM_MEMSET2D_SUPPORT < ContextInfoType`
 ur_result_t UR_APICALL
 urContextGetInfo(
     ur_context_handle_t hContext,                   ///< [in] handle of the context
@@ -1221,6 +1221,119 @@ urEnqueueUSMMemAdvice(
         return UR_RESULT_ERROR_UNINITIALIZED;
 
     return pfnUSMMemAdvice( hQueue, pMem, size, advice, phEvent );
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/// @brief Enqueue a command to fill 2D USM memory.
+/// 
+/// @returns
+///     - ::UR_RESULT_SUCCESS
+///     - ::UR_RESULT_ERROR_UNINITIALIZED
+///     - ::UR_RESULT_ERROR_DEVICE_LOST
+///     - ::UR_RESULT_ERROR_INVALID_NULL_HANDLE
+///         + `NULL == hQueue`
+///     - ::UR_RESULT_ERROR_INVALID_NULL_POINTER
+///         + `NULL == pMem`
+///         + `NULL == pPattern`
+///     - ::UR_RESULT_ERROR_UNSUPPORTED_FEATURE
+ur_result_t UR_APICALL
+urEnqueueUSMFill2D(
+    ur_queue_handle_t hQueue,                       ///< [in] handle of the queue to submit to.
+    void* pMem,                                     ///< [in] pointer to memory to be filled.
+    size_t pitch,                                   ///< [in] the total width of the destination memory including padding.
+    size_t patternSize,                             ///< [in] the size in bytes of the pattern.
+    const void* pPattern,                           ///< [in] pointer with the bytes of the pattern to set.
+    size_t width,                                   ///< [in] the width in bytes of each row to fill.
+    size_t height,                                  ///< [in] the height of the columns to fill.
+    uint32_t numEventsInWaitList,                   ///< [in] size of the event wait list
+    const ur_event_handle_t* phEventWaitList,       ///< [in][optional][range(0, numEventsInWaitList)] pointer to a list of
+                                                    ///< events that must be complete before the kernel execution.
+                                                    ///< If nullptr, the numEventsInWaitList must be 0, indicating that no wait
+                                                    ///< event. 
+    ur_event_handle_t* phEvent                      ///< [in,out][optional] return an event object that identifies this
+                                                    ///< particular kernel execution instance.
+    )
+{
+    auto pfnUSMFill2D = ur_lib::context->urDdiTable.Enqueue.pfnUSMFill2D;
+    if( nullptr == pfnUSMFill2D )
+        return UR_RESULT_ERROR_UNINITIALIZED;
+
+    return pfnUSMFill2D( hQueue, pMem, pitch, patternSize, pPattern, width, height, numEventsInWaitList, phEventWaitList, phEvent );
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/// @brief Enqueue a command to set 2D USM memory.
+/// 
+/// @returns
+///     - ::UR_RESULT_SUCCESS
+///     - ::UR_RESULT_ERROR_UNINITIALIZED
+///     - ::UR_RESULT_ERROR_DEVICE_LOST
+///     - ::UR_RESULT_ERROR_INVALID_NULL_HANDLE
+///         + `NULL == hQueue`
+///     - ::UR_RESULT_ERROR_INVALID_NULL_POINTER
+///         + `NULL == pMem`
+///     - ::UR_RESULT_ERROR_UNSUPPORTED_FEATURE
+ur_result_t UR_APICALL
+urEnqueueUSMMemset2D(
+    ur_queue_handle_t hQueue,                       ///< [in] handle of the queue to submit to.
+    void* pMem,                                     ///< [in] pointer to memory to be filled.
+    size_t pitch,                                   ///< [in] the total width of the destination memory including padding.
+    int value,                                      ///< [in] the value to fill into the region in pMem.
+    size_t width,                                   ///< [in] the width in bytes of each row to set.
+    size_t height,                                  ///< [in] the height of the columns to set.
+    uint32_t numEventsInWaitList,                   ///< [in] size of the event wait list
+    const ur_event_handle_t* phEventWaitList,       ///< [in][optional][range(0, numEventsInWaitList)] pointer to a list of
+                                                    ///< events that must be complete before the kernel execution.
+                                                    ///< If nullptr, the numEventsInWaitList must be 0, indicating that no wait
+                                                    ///< event. 
+    ur_event_handle_t* phEvent                      ///< [in,out][optional] return an event object that identifies this
+                                                    ///< particular kernel execution instance.
+    )
+{
+    auto pfnUSMMemset2D = ur_lib::context->urDdiTable.Enqueue.pfnUSMMemset2D;
+    if( nullptr == pfnUSMMemset2D )
+        return UR_RESULT_ERROR_UNINITIALIZED;
+
+    return pfnUSMMemset2D( hQueue, pMem, pitch, value, width, height, numEventsInWaitList, phEventWaitList, phEvent );
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/// @brief Enqueue a command to copy 2D USM memory.
+/// 
+/// @returns
+///     - ::UR_RESULT_SUCCESS
+///     - ::UR_RESULT_ERROR_UNINITIALIZED
+///     - ::UR_RESULT_ERROR_DEVICE_LOST
+///     - ::UR_RESULT_ERROR_INVALID_NULL_HANDLE
+///         + `NULL == hQueue`
+///     - ::UR_RESULT_ERROR_INVALID_NULL_POINTER
+///         + `NULL == pDst`
+///         + `NULL == pSrc`
+///     - ::UR_RESULT_ERROR_UNSUPPORTED_FEATURE
+ur_result_t UR_APICALL
+urEnqueueUSMMemcpy2D(
+    ur_queue_handle_t hQueue,                       ///< [in] handle of the queue to submit to.
+    bool blocking,                                  ///< [in] indicates if this operation should block the host.
+    void* pDst,                                     ///< [in] pointer to memory where data will be copied.
+    size_t dstPitch,                                ///< [in] the total width of the source memory including padding.
+    const void* pSrc,                               ///< [in] pointer to memory to be copied.
+    size_t srcPitch,                                ///< [in] the total width of the source memory including padding.
+    size_t width,                                   ///< [in] the width in bytes of each row to be copied.
+    size_t height,                                  ///< [in] the height of columns to be copied.
+    uint32_t numEventsInWaitList,                   ///< [in] size of the event wait list
+    const ur_event_handle_t* phEventWaitList,       ///< [in][optional][range(0, numEventsInWaitList)] pointer to a list of
+                                                    ///< events that must be complete before the kernel execution.
+                                                    ///< If nullptr, the numEventsInWaitList must be 0, indicating that no wait
+                                                    ///< event. 
+    ur_event_handle_t* phEvent                      ///< [in,out][optional] return an event object that identifies this
+                                                    ///< particular kernel execution instance.
+    )
+{
+    auto pfnUSMMemcpy2D = ur_lib::context->urDdiTable.Enqueue.pfnUSMMemcpy2D;
+    if( nullptr == pfnUSMMemcpy2D )
+        return UR_RESULT_ERROR_UNINITIALIZED;
+
+    return pfnUSMMemcpy2D( hQueue, blocking, pDst, dstPitch, pSrc, srcPitch, width, height, numEventsInWaitList, phEventWaitList, phEvent );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
