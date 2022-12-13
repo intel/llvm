ecfc6b12cab8893c3e082881fc6960565c30c2f0
Rauf, Rana <rana.rauf@intel.com>
2022-12-13 08:27:25 -0800

    Dummy
    Signed-off-by: Rauf, Rana <rana.rauf@intel.com>

--- sycl/unittests/helpers/PiMockPlugin.hpp
+++ sycl/unittests/helpers/PiMockPlugin.hpp
@@ -1054,6 +1054,7 @@ inline pi_result mock_piPluginGetLastError(char **message) {
 inline pi_result mock_piGetDeviceAndHostTimer(pi_device device,
                                               uint64_t *deviceTime,
                                               uint64_t *hostTime) {
+
   using namespace std::chrono;
   auto timeNanoseconds =
       duration_cast<nanoseconds>(steady_clock::now().time_since_epoch())
