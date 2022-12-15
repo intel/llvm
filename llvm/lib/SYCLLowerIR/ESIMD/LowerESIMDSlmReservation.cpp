29967aa9b0de16d09744135a65cb9add6beb54b6
Konstantin S Bobrovsky <konstantin.s.bobrovsky@intel.com>
2022-12-15 01:27:13 -0800

    fix comment

--- llvm/lib/SYCLLowerIR/ESIMD/LowerESIMDSlmReservation.cpp
+++ llvm/lib/SYCLLowerIR/ESIMD/LowerESIMDSlmReservation.cpp
@@ -1,4 +1,4 @@
-//===----- LowerESIMDSlmReservation - lower __esimd_slm_* -----------------===//
+//===----- LowerESIMDSlmReservation.cpp - lower __esimd_slm_* intrinsics --===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
