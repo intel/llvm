19923b97e6be71a75669f4c216e847495b2cbabb
Bing1 Yu <bing1.yu@intel.com>
2022-12-15 15:21:04 +0800

    fix some smal issues

--- sycl/include/sycl/ext/oneapi/matrix/matrix-intel.hpp
+++ sycl/include/sycl/ext/oneapi/matrix/matrix-intel.hpp
@@ -1,5 +1,4 @@
-//==------------------ matrix-jit-use.hpp - SYCL matrix ----------------*- C++
-//-*---==//
+//==------------------ matrix-intel.hpp - SYCL matrix ----------*- C++ -*---==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
