2d6a6366a39727d39914495fbb753688f83ad839
Rauf, Rana <rana.rauf@intel.com>
2022-12-19 14:29:20 -0800

    Added unittests
    Signed-off-by: Rauf, Rana <rana.rauf@intel.com>

--- sycl/source/handler.cpp
+++ sycl/source/handler.cpp
@@ -210,7 +210,8 @@ event handler::finalize() {
                               PI_ERROR_INVALID_OPERATION);
         else if (NewEvent->is_host() || NewEvent->getHandleRef() == nullptr)
           NewEvent->setComplete();
-
+        
+        NewEvent->setSubmittedQueue(MQueue);
         NewEvent->setSubmissionTime();
         MLastEvent = detail::createSyclObjFromImpl<event>(NewEvent);
       }
