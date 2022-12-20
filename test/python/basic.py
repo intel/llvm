665c9550ab89fbd407be5b311092c2c9b78386c2
patkamin <patryk.kaminski@intel.com>
2022-12-20 10:28:03 +0100

    Enable running python tests from any location
    Currently ur module is imported using the path relative to the "test"
    directory. This causes a failure when trying to run tests
    with a different working directory. This change lifts this limitation
    allowing to run python tests from any location with the usage of
    the PYTHONPATH environment variable.
    
    Co-authored-by: pbalcer <piotr.balcer@intel.com>

--- test/python/basic.py
+++ test/python/basic.py
@@ -10,10 +10,7 @@ import pytest
 import sys
 import os
 
-# not ideal, but this solution is simple and portable. Alternative is to set
-# PYTHONPATH or test on installed sources.
-sys.path.insert(1, '../include')
-import ur
+import include.ur as ur
 
 def test_ddi():
         ddi = ur.UR_DDI(ur.ur_api_version_v.CURRENT);
