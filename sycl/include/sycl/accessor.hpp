0fbf769220002d7795fc8e70f3d74b5311650373
Maronas, Marcos <marcos.maronas@intel.com>
2022-12-20 01:20:55 -0800

    Converting  local accessor LIT test into unit test.

--- sycl/include/sycl/accessor.hpp
+++ sycl/include/sycl/accessor.hpp
@@ -2405,10 +2405,12 @@ protected:
   ConcreteASPtrType MData;
 
 #else
+public:
   local_accessor_base()
       : detail::LocalAccessorBaseHost{/*Size*/ sycl::range<3>{0, 0, 0}, /*Dims*/ 0, /*ElemSize*/ sizeof(DataT)}
   {}
 
+protected:
   local_accessor_base(const detail::LocalAccessorImplPtr &Impl)
       : detail::LocalAccessorBaseHost{Impl} {}
 
