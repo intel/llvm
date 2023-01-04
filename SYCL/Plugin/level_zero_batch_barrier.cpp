c69919f0190e323c9c5639b5a332218b07193c0a
Chris Perkins <chris.perkins@intel.com>
2023-01-04 09:52:38 -0800

    [SYCL] - quick fix for level_zero_batch_barrier.cpp (intel/llvm-test-suite#1481)
    tests that include <level_zero/ze_api.h> need to require level_zero_dev_kit.

--- SYCL/Plugin/level_zero_batch_barrier.cpp
+++ SYCL/Plugin/level_zero_batch_barrier.cpp
@@ -1,4 +1,4 @@
-// REQUIRES: gpu, level_zero
+// REQUIRES: gpu, level_zero, level_zero_dev_kit
 
 // RUN: %clangxx -fsycl -fsycl-unnamed-lambda -fsycl-targets=%sycl_triple %level_zero_options %s -o %t.out
 // RUN: env SYCL_PI_TRACE=2 ZE_DEBUG=1 SYCL_PI_LEVEL_ZERO_DEVICE_SCOPE_EVENTS=2 SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=0 %GPU_RUN_PLACEHOLDER %t.out 2>&1 %GPU_CHECK_PLACEHOLDER
