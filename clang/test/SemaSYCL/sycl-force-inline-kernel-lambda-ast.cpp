9871f4684ff808e1bf9eba518403ea044d76d8c5
Lamzed-Short, Andrew <andrew.lamzed-short@intel.com>
2023-01-05 05:44:08 -0800

    Updated force inline kernel lambda test
    The previous test didn't check with and without the
    `-fno-sycl-force-inline-kernel-lambda` properly and was quite loose.
    
    The test checks, run lines, and form have been fixed with assistance. Thanks!

--- clang/test/SemaSYCL/sycl-force-inline-kernel-lambda-ast.cpp
+++ clang/test/SemaSYCL/sycl-force-inline-kernel-lambda-ast.cpp
@@ -1,33 +1,38 @@
-// RUN: %clang_cc1 -fno-sycl-force-inline-kernel-lambda -fsycl-is-device -internal-isystem %S/Inputs -disable-llvm-passes -triple spir64-unkown-unknown -ast-dump -o - %s | FileCheck %s
-// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -disable-llvm-passes -triple spir64-unkown-unknown -ast-dump -o - %s | FileCheck %s
+// RUN: %clang_cc1 -fno-sycl-force-inline-kernel-lambda -fsycl-is-device -internal-isystem %S/Inputs -disable-llvm-passes -triple spir64-unknown-unknown -ast-dump -o - %s | FileCheck %s --check-prefixes=NOINLINE,CHECK
+// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -disable-llvm-passes -triple spir64-unknown-unknown -ast-dump -o - %s | FileCheck %s --check-prefixes=INLINE,CHECK
+
+// Tests that the appropriate inlining attributes are added to kernel lambda functions,
+// with no inline attribute being added when -fno-sycl-force-inline-kernel-lambda is set
+// and attribute not explicitly provided.
 
 #include "sycl.hpp"
 
 int main() {
   sycl::queue q;
 
-  q.submit([&](sycl::handler &h) {    
-    // CHECK: LambdaExpr
-    // CHECK-NOT: AlwaysInlineAttr
-    // CHECK-NOT: NoInlineAttr
+  q.submit([&](sycl::handler &h) {
+    // CHECK: LambdaExpr{{.*}}sycl-force-inline-kernel-lambda-ast.cpp:17
+    // INLINE: AlwaysInlineAttr
+    // NOINLINE-NOT: AlwaysInlineAttr
     h.parallel_for<class KernelName>([] {});
   });
 
-  q.submit([&](sycl::handler &h) {    
-    // CHECK: LambdaExpr
+  q.submit([&](sycl::handler &h) {
+    // CHECK: LambdaExpr{{.*}}sycl-force-inline-kernel-lambda-ast.cpp:23
     // CHECK: AlwaysInlineAttr
     h.parallel_for<class KernelNameInline>([]() __attribute__((always_inline)) {});
   });
-  
-  q.submit([&](sycl::handler &h) {    
-    // CHECK: LambdaExpr
+
+  q.submit([&](sycl::handler &h) {
+    // CHECK: LambdaExpr{{.*}}sycl-force-inline-kernel-lambda-ast.cpp:30
     // CHECK: NoInlineAttr
+    // CHECK-NOT: AlwaysInlineAttr
     h.parallel_for<class KernelNameNoInline>([]() __attribute__((noinline)) {});
   });
 
   /// The flag is ignored for ESIMD kernels
   q.submit([&](sycl::handler &h) {
-    // CHECK: LambdaExpr
+    // CHECK: LambdaExpr{{.*}}sycl-force-inline-kernel-lambda-ast.cpp:39
     // CHECK: SYCLSimdAttr
     // CHECK-NOT: AlwaysInlineAttr
     // CHECK-NOT: NoInlineAttr
