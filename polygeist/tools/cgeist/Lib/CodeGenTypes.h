648ce08e24170fe4a2c617f92274b9f7d8306e98
Tiotto, Ettore <ettore.tiotto@intel.com>
2023-01-10 15:34:19 -0500

    Implement 'getMLIRType' for clang builtin types
    Signed-off-by: Tiotto, Ettore <ettore.tiotto@intel.com>

--- polygeist/tools/cgeist/Lib/CodeGenTypes.h
+++ polygeist/tools/cgeist/Lib/CodeGenTypes.h
@@ -22,11 +22,12 @@ namespace clang {
 class ASTContext;
 class CodeGenOptions;
 class FunctionDecl;
-class QualType;
+class GlobalDecl;
 class RecordType;
 class RecordDecl;
+class BuiltinType;
+class QualType;
 class Type;
-class GlobalDecl;
 
 namespace CodeGen {
 class ABIInfo;
@@ -104,6 +105,8 @@ private:
 
   bool getCPUAndFeaturesAttributes(clang::GlobalDecl GD,
                                    AttrBuilder &Attrs) const;
+
+  mlir::Type getMLIRType(const clang::BuiltinType *BT) const;
 };
 
 } // namespace CodeGen
