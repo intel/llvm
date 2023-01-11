ff8ab37a00af0625875b8d038c8a8660f7e2cc1b
Petr Vesely <petr.vesely@codeplay.com>
2023-01-11 10:46:12 +0000

    [ur] Add device testsuite

---
+++ test/conformance/device/urDeviceCreateWithNativeHandle.cpp
@@ -0,0 +1,14 @@
+// Copyright (C) 2022 Intel Corporation
+// SPDX-License-Identifier: MIT
+#include <uur/fixtures.h>
+
+using urDeviceCreateWithNativeHandleTest = uur::urPlatformTest;
+
+// TODO - Should not use nullptr for the native handle - #165
+TEST_F(urDeviceCreateWithNativeHandleTest, Success) {
+  ur_native_handle_t native_handle = nullptr;
+  ur_device_handle_t device_handle = nullptr;
+  ASSERT_SUCCESS(
+      urDeviceCreateWithNativeHandle(native_handle, platform, &device_handle));
+  ASSERT_NE(device_handle, nullptr);
+}
---
+++ test/conformance/device/urDeviceSelectBinary.cpp
@@ -0,0 +1,22 @@
+// Copyright (C) 2022 Intel Corporation
+// SPDX-License-Identifier: MIT
+#include <uur/fixtures.h>
+
+using urDeviceSelectBinaryTest = uur::urAllDevicesTest;
+
+// TODO - This is just a placeholder test until we have a better understanding
+// of the compiler requirements for UR. - See #171
+TEST_F(urDeviceSelectBinaryTest, Success) {
+  const char *binaries[] = {"binary A", "binary B"};
+  uint32_t num_binaries = 2;
+  for (auto device : devices) {
+    ASSERT_SUCCESS(urDeviceSelectBinary(device, (const uint8_t **)binaries,
+                                        num_binaries, nullptr));
+  }
+}
+
+// Re-enable this test - See #171
+TEST_F(urDeviceSelectBinaryTest, DISABLED_InvalidNullHandle) {
+  ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_NULL_HANDLE,
+                   urDeviceSelectBinary(nullptr, nullptr, 0, nullptr));
+}
