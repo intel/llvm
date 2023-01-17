be6e6533b196c9acbdb88eee8fd9cfdd995afecf
Tiotto, Ettore <ettore.tiotto@intel.com>
2023-01-17 06:03:56 -0500

    Add test
    Signed-off-by: Tiotto, Ettore <ettore.tiotto@intel.com>

--- polygeist/tools/cgeist/Test/Verification/sycl/builtin_types.cpp
+++ polygeist/tools/cgeist/Test/Verification/sycl/builtin_types.cpp
@@ -4,18 +4,53 @@
 
 using namespace sycl;
 
+// CHECK-LABEL: func.func @_Z13opencl_float2Dv2_f(
+// CHECK:         %arg0: memref<?x2xf32> {llvm.noundef})
+SYCL_EXTERNAL void opencl_float2(__cl_float2 var) {}
+
+// CHECK-LABEL: func.func @_Z13opencl_float4Dv4_f(
+// CHECK:         %arg0: memref<?x4xf32> {llvm.noundef})
+SYCL_EXTERNAL void opencl_float4(__cl_float4 var) {}
+
+// CHECK-LABEL: func.func @_Z8float128g(
+// CHECK:         %arg0: f128 {llvm.noundef})
+SYCL_EXTERNAL void float128(__float128 var) {}
+
+// CHECK-LABEL: func.func @_Z6int128n(
+// CHECK:         %arg0: i128 {llvm.noundef})
+SYCL_EXTERNAL void int128(__int128 var) {}
+
 // CHECK-LABEL: func.func @_Z19opencl_image1d_ro_t14ocl_image1d_ro(
-// CHECK:          %arg0: !llvm.ptr<struct<"opencl.image1d_ro_t", opaque>, 1>)
-// CHECK-SAME: attributes {[[SPIR_FUNCCC:llvm.cconv = #llvm.cconv<spir_funccc>]], [[LINKEXT:llvm.linkage = #llvm.linkage<external>]],
-// CHECK-SAME: [[PASSTHROUGH:passthrough = \["convergent", "mustprogress", "noinline", "norecurse", "nounwind", "optnone", \["frame-pointer", "all"\], \["no-trapping-math", "true"\], \["stack-protector-buffer-size", "8"\], \["sycl-module-id", ".*/polygeist/tools/cgeist/Test/Verification/sycl/builtin_types.cpp"\]\]]]} {
+// CHECK:         %arg0: !llvm.ptr<struct<"opencl.image1d_ro_t", opaque>, 1>)
 SYCL_EXTERNAL void opencl_image1d_ro_t(detail::opencl_image_type<1, access::mode::read, access::target::image>::type var) {}
 
 // CHECK-LABEL: func.func @_Z19opencl_image1d_wo_t14ocl_image1d_wo(
-// CHECK:          %arg0: !llvm.ptr<struct<"opencl.image1d_wo_t", opaque>, 1>)
-// CHECK-SAME: attributes {[[SPIR_FUNCCC:llvm.cconv = #llvm.cconv<spir_funccc>]], [[LINKEXT:llvm.linkage = #llvm.linkage<external>]],
-// CHECK-SAME: [[PASSTHROUGH:passthrough = \["convergent", "mustprogress", "noinline", "norecurse", "nounwind", "optnone", \["frame-pointer", "all"\], \["no-trapping-math", "true"\], \["stack-protector-buffer-size", "8"\], \["sycl-module-id", ".*/polygeist/tools/cgeist/Test/Verification/sycl/builtin_types.cpp"\]\]]]} {
+// CHECK:         %arg0: !llvm.ptr<struct<"opencl.image1d_wo_t", opaque>, 1>)
 SYCL_EXTERNAL void opencl_image1d_wo_t(detail::opencl_image_type<1, access::mode::write, access::target::image>::type var) {}
 
+// CHECK-LABEL: func.func @_Z25opencl_image1d_array_ro_t20ocl_image1d_array_ro(
+// CHECK:         %arg0: !llvm.ptr<struct<"opencl.image1d_array_ro_t", opaque>, 1>)
+SYCL_EXTERNAL void opencl_image1d_array_ro_t(detail::opencl_image_type<1, access::mode::read, access::target::image_array>::type var) {}
+
+// CHECK-LABEL: func.func @_Z25opencl_image1d_array_wo_t20ocl_image1d_array_wo(
+// CHECK:         %arg0: !llvm.ptr<struct<"opencl.image1d_array_wo_t", opaque>, 1>)
+SYCL_EXTERNAL void opencl_image1d_array_wo_t(detail::opencl_image_type<1, access::mode::write, access::target::image_array>::type var) {}
+
+// CHECK-LABEL: func.func @_Z16opencl_sampler_t11ocl_sampler(
+// CHECK:         %arg0: !llvm.ptr<struct<"opencl.sampler_t", opaque>, 2>)
+SYCL_EXTERNAL void opencl_sampler_t(__ocl_sampler_t var) {}
+
+// CHECK-LABEL: func.func @_Z33opencl_sampled_image_array1d_ro_t38__spirv_SampledImage__image1d_array_ro(
+// CHECK:         %arg0: !llvm.ptr<struct<"spirv.SampledImage.image1d_array_ro_t.0", opaque>, 1>)
+SYCL_EXTERNAL void opencl_sampled_image_array1d_ro_t(__ocl_sampled_image1d_array_ro_t var) {}
+
+// CHECK-LABEL: func.func @_Z12opencl_vec_tDv4_j(
+// CHECK:         %arg0: vector<4xi32> {llvm.noundef})
+SYCL_EXTERNAL void opencl_vec_t(__ocl_vec_t<uint32_t, 4> var) {}
+
+// CHECK-LABEL: func.func @_Z14opencl_event_t9ocl_event(
+// CHECK:         %arg0: !llvm.ptr<struct<"opencl.event_t", opaque>, 4>)
+SYCL_EXTERNAL void opencl_event_t(__ocl_event_t var) {}
 
 
 
