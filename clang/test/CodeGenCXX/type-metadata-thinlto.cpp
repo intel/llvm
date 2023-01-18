9f7fdf6e8ec9a779603fe2f0a10c3f9c7af5a2ae
KornevNikita <nikita.kornev@intel.com>
2023-01-18 02:09:29 -0800

    Revert "Fix non OpenMP/ tests"
    This reverts commit f5f6584aee6dfe5cd21dede51af03213cfb93f23.

--- clang/test/CodeGenCXX/type-metadata-thinlto.cpp
+++ clang/test/CodeGenCXX/type-metadata-thinlto.cpp
@@ -1,15 +1,6 @@
 // RUN: %clang_cc1 -flto=thin -flto-unit -fsplit-lto-unit -triple x86_64-unknown-linux -fvisibility=hidden -emit-llvm-bc -o %t %s
-// Added -opaque-pointers.
-// FIXME: Align with the community code when project is ready to enable opaque
-// pointers by default
-// Added -opaque-pointers.
-// FIXME: Align with the community code when project is ready to enable opaque
-// pointers by default
-// RUN: llvm-modextract -opaque-pointers -o - -n 1 %t | llvm-dis -opaque-pointers | FileCheck %s
-// Added -opaque-pointers.
-// FIXME: Align with the community code when project is ready to enable opaque
-// pointers by default
-// RUN: llvm-modextract -opaque-pointers -b -o - -n 1 %t | llvm-bcanalyzer -dump | FileCheck %s --check-prefix=LTOUNIT
+// RUN: llvm-modextract -o - -n 1 %t | llvm-dis | FileCheck %s
+// RUN: llvm-modextract -b -o - -n 1 %t | llvm-bcanalyzer -dump | FileCheck %s --check-prefix=LTOUNIT
 // LTOUNIT: <FLAGS op0=8/>
 
 // CHECK: @_ZTV1A = linkonce_odr
