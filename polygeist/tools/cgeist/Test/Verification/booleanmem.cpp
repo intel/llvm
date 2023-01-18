da5f48b07dcbc735cf0c61c86dd4da094f51f404
Victor Perez <victor.perez@codeplay.com>
2023-01-18 15:23:11 +0000

    Add lambda test

---
+++ polygeist/tools/cgeist/Test/Verification/booleanmem.cpp
@@ -0,0 +1,20 @@
+// RUN: cgeist %s -w -O0 --function=* -S | FileCheck %s
+
+#include <functional>
+
+// CHECK-LABEL:   func.func @_Z14lambda_capturebRb(
+// CHECK-SAME:                                     %[[VAL_0:.*]]: i1,
+// CHECK-SAME:                                     %[[VAL_1:.*]]: memref<?xi8>)
+// CHECK-NEXT:      %[[VAL_2:.*]] = arith.constant 1 : i64
+// CHECK-NEXT:      %[[VAL_3:.*]] = llvm.alloca %[[VAL_2]] x !llvm.struct<(i8, memref<?xi8>)> : (i64) -> !llvm.ptr<!llvm.struct<(i8, memref<?xi8>)>>
+// CHECK-NEXT:      %[[VAL_4:.*]] = llvm.getelementptr %[[VAL_3]][0, 0] : (!llvm.ptr<!llvm.struct<(i8, memref<?xi8>)>>) -> !llvm.ptr<i8>
+// CHECK-NEXT:      %[[VAL_5:.*]] = arith.extui %[[VAL_0]] : i1 to i8
+// CHECK-NEXT:      llvm.store %[[VAL_5]], %[[VAL_4]] : !llvm.ptr<i8>
+// CHECK-NEXT:      %[[VAL_6:.*]] = llvm.getelementptr %[[VAL_3]][0, 1] : (!llvm.ptr<!llvm.struct<(i8, memref<?xi8>)>>) -> !llvm.ptr<memref<?xi8>>
+// CHECK-NEXT:      llvm.store %[[VAL_1]], %[[VAL_6]] : !llvm.ptr<memref<?xi8>>
+// CHECK-NEXT:      return
+// CHECK-NEXT:    }
+
+void lambda_capture(bool x, bool &y) {
+  const auto f = [x, &y]() { y = !x; };
+}
