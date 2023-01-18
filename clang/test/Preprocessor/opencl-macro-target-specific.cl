89fb7e66f8e0d3b770ebbe6a58848a95be426fe8
Hugh Delaney <hugh.delaney@codeplay.com>
2023-01-18 15:02:29 +0000

    Updating preprocessor tests

---
+++ clang/test/Preprocessor/opencl-macro-target-specific.cl
@@ -0,0 +1,7 @@
+// RUN: %clang_cc1 %s -E -dM -triple amdgcn-amdhsa-amdhsa \
+// RUN: | FileCheck --check-prefix=CHECK-AMDGPU %s
+// CHECK-AMDGPU: #define __HIP_MEMORY_SCOPE_AGENT
+// CHECK-AMDGPU: #define __HIP_MEMORY_SCOPE_SINGLETHREAD
+// CHECK-AMDGPU: #define __HIP_MEMORY_SCOPE_SYSTEM
+// CHECK-AMDGPU: #define __HIP_MEMORY_SCOPE_WAVEFRONT
+// CHECK-AMDGPU: #define __HIP_MEMORY_SCOPE_WORKGROUP
--- clang/test/Preprocessor/sycl-macro-target-specific.cpp
+++ clang/test/Preprocessor/sycl-macro-target-specific.cpp
@@ -8,8 +8,25 @@
 // RUN: | FileCheck --check-prefix=CHECK-NVPTX-NEG %s
 // RUN: %clang_cc1 %s -fsycl-is-device -triple spir64_fpga-unknown-unknown -E -dM \
 // RUN: | FileCheck --check-prefix=CHECK-NVPTX-NEG %s
+// RUN: %clang_cc1 %s -fsycl-is-device -triple amdgcn-amdhsa-amdhsa -E -dM \
+// RUN: | FileCheck --check-prefix=CHECK-NVPTX-NEG %s
 // CHECK-NVPTX: #define __NVPTX__
 // CHECK-NVPTX-NEG-NOT: #define __NVPTX__
+
+// RUN: %clang_cc1 %s -fsycl-is-device -triple amdgcn-amdhsa-amdhsa -E -dM \
+// RUN: | FileCheck --check-prefix=CHECK-AMDGPU %s
+// RUN: %clang_cc1 %s -fsycl-is-device -triple nvptx64-nvidia-nvcl -E -dM \
+// RUN: | FileCheck --check-prefix=CHECK-AMDGPU-NEG %s
+// RUN: %clang_cc1 %s -fsycl-is-device -triple spir64-unknown-unknown -E -dM \
+// RUN: | FileCheck --check-prefix=CHECK-AMDGPU-NEG %s
+// RUN: %clang_cc1 %s -fsycl-is-device -triple spir64_gen-unknown-unknown -E -dM \
+// RUN: | FileCheck --check-prefix=CHECK-AMDGPU-NEG %s
+// RUN: %clang_cc1 %s -fsycl-is-device -triple spir64_x86_64-unknown-unknown -E -dM \
+// RUN: | FileCheck --check-prefix=CHECK-AMDGPU-NEG %s
+// RUN: %clang_cc1 %s -fsycl-is-device -triple spir64_fpga-unknown-unknown -E -dM \
+// RUN: | FileCheck --check-prefix=CHECK-AMDGPU-NEG %s
+// CHECK-AMDGPU: #define __AMDGPU__
+// CHECK-AMDGPU-NEG-NOT: #define __AMDGPU__
 
 // RUN: %clang_cc1 %s -fsycl-is-device -triple spir64-unknown-unknown -E -dM \
 // RUN: | FileCheck --check-prefix=CHECK-SYCL-FP-ATOMICS %s
@@ -20,6 +37,8 @@
 // RUN: %clang_cc1 %s -fsycl-is-device -triple spir64_fpga-unknown-unknown -E -dM \
 // RUN: | FileCheck --check-prefix=CHECK-SYCL-FP-ATOMICS-NEG %s
 // RUN: %clang_cc1 %s -fsycl-is-device -triple nvptx64-nvidia-nvcl -E -dM \
+// RUN: | FileCheck --check-prefix=CHECK-SYCL-FP-ATOMICS %s
+// RUN: %clang_cc1 %s -fsycl-is-device -triple amdgcn-amdhsa-amdhsa -E -dM \
 // RUN: | FileCheck --check-prefix=CHECK-SYCL-FP-ATOMICS %s
 // CHECK-SYCL-FP-ATOMICS: #define SYCL_USE_NATIVE_FP_ATOMICS
 // CHECK-SYCL-FP-ATOMICS-NEG-NOT: #define SYCL_USE_NATIVE_FP_ATOMICS
