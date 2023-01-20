6f75e179d9c277af371b5231c62ed294ea905fd3
Alexey Bader <alexey.bader@intel.com>
2023-01-19 20:25:58 -0800

    Revert "Add tests for bfloat16 isnan" (intel/llvm-test-suite#1532)
    Revert "Add tests for bfloat16 isnan (intel/llvm-test-suite#1509)"
    
    This reverts commit 87215dc93600b70b82c9f5a97cc66062f240ee99.

--- SYCL/BFloat16/bfloat16_builtins.cpp
+++ SYCL/BFloat16/bfloat16_builtins.cpp
@@ -29,8 +29,6 @@ bool check(float a, float b) {
   return fabs(2 * (a - b) / (a + b)) > bf16_eps * 2;
 }
 
-bool check(bool a, bool b) { return (a == b); }
-
 #define TEST_BUILTIN_1_SCAL_IMPL(NAME)                                         \
   {                                                                            \
     buffer<float> a_buf(&a[0], N);                                             \
@@ -48,7 +46,7 @@ bool check(bool a, bool b) { return (a == b); }
   }                                                                            \
   assert(err == 0);
 
-#define TEST_BUILTIN_1_ARR_IMPL(NAME, SZ, RETTY)                               \
+#define TEST_BUILTIN_1_ARR_IMPL(NAME, SZ)                                      \
   {                                                                            \
     buffer<float, 2> a_buf{range<2>{N / SZ, SZ}};                              \
     buffer<int> err_buf(&err, 1);                                              \
@@ -61,7 +59,7 @@ bool check(bool a, bool b) { return (a == b); }
         for (int i = 0; i < SZ; i++) {                                         \
           arg[i] = A[index][i];                                                \
         }                                                                      \
-        marray<RETTY, SZ> res = NAME(arg);                                     \
+        marray<bfloat16, SZ> res = NAME(arg);                                  \
         for (int i = 0; i < SZ; i++) {                                         \
           if (check(res[i], NAME(A[index][i]))) {                              \
             ERR[0] = 1;                                                        \
@@ -72,13 +70,13 @@ bool check(bool a, bool b) { return (a == b); }
   }                                                                            \
   assert(err == 0);
 
-#define TEST_BUILTIN_1(NAME, RETTY)                                            \
+#define TEST_BUILTIN_1(NAME)                                                   \
   TEST_BUILTIN_1_SCAL_IMPL(NAME)                                               \
-  TEST_BUILTIN_1_ARR_IMPL(NAME, 1, RETTY)                                      \
-  TEST_BUILTIN_1_ARR_IMPL(NAME, 2, RETTY)                                      \
-  TEST_BUILTIN_1_ARR_IMPL(NAME, 3, RETTY)                                      \
-  TEST_BUILTIN_1_ARR_IMPL(NAME, 4, RETTY)                                      \
-  TEST_BUILTIN_1_ARR_IMPL(NAME, 5, RETTY)
+  TEST_BUILTIN_1_ARR_IMPL(NAME, 1)                                             \
+  TEST_BUILTIN_1_ARR_IMPL(NAME, 2)                                             \
+  TEST_BUILTIN_1_ARR_IMPL(NAME, 3)                                             \
+  TEST_BUILTIN_1_ARR_IMPL(NAME, 4)                                             \
+  TEST_BUILTIN_1_ARR_IMPL(NAME, 5)
 
 #define TEST_BUILTIN_2_SCAL_IMPL(NAME)                                         \
   {                                                                            \
@@ -235,7 +233,7 @@ int main() {
       c[i] = (float)(3 * i);
     }
 
-    TEST_BUILTIN_1(fabs, bfloat16);
+    TEST_BUILTIN_1(fabs);
     TEST_BUILTIN_2(fmin);
     TEST_BUILTIN_2(fmax);
     TEST_BUILTIN_3(fma);
@@ -243,10 +241,6 @@ int main() {
     float check_nan = 0;
     TEST_BUILTIN_2_NAN(fmin);
     TEST_BUILTIN_2_NAN(fmax);
-
-    // Insert NAN value in a to test isnan
-    a[0] = a[N - 1] = NAN;
-    TEST_BUILTIN_1(isnan, bool);
   }
   return 0;
 }
