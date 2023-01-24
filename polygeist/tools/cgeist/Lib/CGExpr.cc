6a35b048091dbbc2b142166fb288b4e4247897bc
Tsang, Whitney <whitney.tsang@intel.com>
2023-01-23 20:17:32 -0500

    [SYCL-MLIR] Allow inner type to be i8 pointer
    Signed-off-by: Tsang, Whitney <whitney.tsang@intel.com>

--- polygeist/tools/cgeist/Lib/CGExpr.cc
+++ polygeist/tools/cgeist/Lib/CGExpr.cc
@@ -290,21 +290,24 @@ mlir::Attribute MLIRScanner::InitializeValueByInitListExpr(mlir::Value ToInit,
         } else {
           auto PT = ToInit.getType().cast<LLVM::LLVMPointerType>();
           auto ET = PT.getElementType();
-          mlir::Type NextType;
           if (auto ST = ET.dyn_cast<LLVM::LLVMStructType>())
-            NextType = ST.getBody()[I];
+            Next = Builder.create<LLVM::GEPOp>(
+                Loc,
+                LLVM::LLVMPointerType::get(ST.getBody()[I],
+                                           PT.getAddressSpace()),
+                ToInit, llvm::ArrayRef<mlir::LLVM::GEPArg>{0, I});
           else if (auto AT = ET.dyn_cast<LLVM::LLVMArrayType>())
-            NextType = AT.getElementType();
+            Next = Builder.create<LLVM::GEPOp>(
+                Loc,
+                LLVM::LLVMPointerType::get(AT.getElementType(),
+                                           PT.getAddressSpace()),
+                ToInit, llvm::ArrayRef<mlir::LLVM::GEPArg>{0, I});
+          else if (ET.isInteger(8))
+            Next = Builder.create<LLVM::GEPOp>(
+                Loc, LLVM::LLVMPointerType::get(ET, PT.getAddressSpace()),
+                ToInit, llvm::ArrayRef<mlir::LLVM::GEPArg>{I});
           else
             llvm_unreachable("unknown inner type");
-
-          mlir::Value Idxs[] = {
-              Builder.create<arith::ConstantIntOp>(Loc, 0, 32),
-              Builder.create<arith::ConstantIntOp>(Loc, I, 32),
-          };
-          Next = Builder.create<LLVM::GEPOp>(
-              Loc, LLVM::LLVMPointerType::get(NextType, PT.getAddressSpace()),
-              ToInit, Idxs);
         }
 
         auto Sub =
