7441dfffd4c49225925ae0a26b75f1104513f318
Tsang, Whitney <whitney.tsang@intel.com>
2023-01-24 10:17:35 -0500

    Use TypeSwitch
    Signed-off-by: Tsang, Whitney <whitney.tsang@intel.com>

--- polygeist/tools/cgeist/Lib/CGExpr.cc
+++ polygeist/tools/cgeist/Lib/CGExpr.cc
@@ -290,24 +290,27 @@ mlir::Attribute MLIRScanner::InitializeValueByInitListExpr(mlir::Value ToInit,
         } else {
           auto PT = ToInit.getType().cast<LLVM::LLVMPointerType>();
           auto ET = PT.getElementType();
-          if (auto ST = ET.dyn_cast<LLVM::LLVMStructType>())
-            Next = Builder.create<LLVM::GEPOp>(
-                Loc,
-                LLVM::LLVMPointerType::get(ST.getBody()[I],
-                                           PT.getAddressSpace()),
-                ToInit, llvm::ArrayRef<mlir::LLVM::GEPArg>{0, I});
-          else if (auto AT = ET.dyn_cast<LLVM::LLVMArrayType>())
-            Next = Builder.create<LLVM::GEPOp>(
-                Loc,
-                LLVM::LLVMPointerType::get(AT.getElementType(),
-                                           PT.getAddressSpace()),
-                ToInit, llvm::ArrayRef<mlir::LLVM::GEPArg>{0, I});
-          else if (ET.isInteger(8))
-            Next = Builder.create<LLVM::GEPOp>(
-                Loc, LLVM::LLVMPointerType::get(ET, PT.getAddressSpace()),
-                ToInit, llvm::ArrayRef<mlir::LLVM::GEPArg>{I});
-          else
-            llvm_unreachable("unknown inner type");
+          Next = TypeSwitch<mlir::Type, mlir::Value>(ET)
+                     .Case<LLVM::LLVMStructType>([=](auto ST) {
+                       return Builder.create<LLVM::GEPOp>(
+                           Loc,
+                           LLVM::LLVMPointerType::get(ST.getBody()[I],
+                                                      PT.getAddressSpace()),
+                           ToInit, llvm::ArrayRef<mlir::LLVM::GEPArg>{0, I});
+                     })
+                     .Case<LLVM::LLVMArrayType>([=](auto AT) {
+                       return Builder.create<LLVM::GEPOp>(
+                           Loc,
+                           LLVM::LLVMPointerType::get(AT.getElementType(),
+                                                      PT.getAddressSpace()),
+                           ToInit, llvm::ArrayRef<mlir::LLVM::GEPArg>{0, I});
+                     })
+                     .Case<IntegerType>([=](auto IT) {
+                       return Builder.create<LLVM::GEPOp>(
+                           Loc,
+                           LLVM::LLVMPointerType::get(IT, PT.getAddressSpace()),
+                           ToInit, llvm::ArrayRef<mlir::LLVM::GEPArg>{I});
+                     });
         }
 
         auto Sub =
