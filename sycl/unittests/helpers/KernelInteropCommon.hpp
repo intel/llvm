9167b0500329a62c95bcd087220ad075665a0b8e
Sergey Semenov <sergey.semenov@intel.com>
2023-01-26 07:17:15 -0800

    [SYCL] Fix a race condition when enqueueing an interop kernel
    piKernelSetArg isn't supposed to be called from different threads
    on the same kernel.

---
+++ sycl/unittests/helpers/KernelInteropCommon.hpp
@@ -0,0 +1,118 @@
+#include <helpers/PiMock.hpp>
+
+struct TestContext {
+
+  // SYCL RT has number of checks that all devices and contexts are consistent
+  // between kernel, kernel_bundle and other objects.
+  //
+  // To ensure that those checks pass, we intercept some PI calls to extract
+  // the exact PI handles of device and context used in queue creation to later
+  // return them when program/context/kernel info is requested.
+  pi_device deviceHandle;
+  pi_context contextHandle;
+
+  pi_program programHandle = createDummyHandle<pi_program>();
+
+  ~TestContext() { releaseDummyHandle<pi_program>(programHandle); }
+};
+
+TestContext GlobalContext;
+
+pi_result after_piContextGetInfo(pi_context context, pi_context_info param_name,
+                                 size_t param_value_size, void *param_value,
+                                 size_t *param_value_size_ret) {
+  switch (param_name) {
+  case PI_CONTEXT_INFO_DEVICES:
+    if (param_value)
+      *static_cast<pi_device *>(param_value) = GlobalContext.deviceHandle;
+    if (param_value_size_ret)
+      *param_value_size_ret = sizeof(GlobalContext.deviceHandle);
+    break;
+  default:;
+  }
+
+  return PI_SUCCESS;
+}
+
+pi_result after_piProgramGetInfo(pi_program program, pi_program_info param_name,
+                                 size_t param_value_size, void *param_value,
+                                 size_t *param_value_size_ret) {
+
+  switch (param_name) {
+  case PI_PROGRAM_INFO_DEVICES:
+    if (param_value_size_ret)
+      *param_value_size_ret = sizeof(GlobalContext.deviceHandle);
+    if (param_value)
+      *static_cast<pi_device *>(param_value) = GlobalContext.deviceHandle;
+    break;
+  default:;
+  }
+
+  return PI_SUCCESS;
+}
+
+pi_result redefined_piProgramGetBuildInfo(pi_program program, pi_device device,
+                                          _pi_program_build_info param_name,
+                                          size_t param_value_size,
+                                          void *param_value,
+                                          size_t *param_value_size_ret) {
+  switch (param_name) {
+  case PI_PROGRAM_BUILD_INFO_BINARY_TYPE:
+    if (param_value_size_ret)
+      *param_value_size_ret = sizeof(pi_program_binary_type);
+    if (param_value)
+      *static_cast<pi_program_binary_type *>(param_value) =
+          PI_PROGRAM_BINARY_TYPE_EXECUTABLE;
+    break;
+  default:;
+  }
+
+  return PI_SUCCESS;
+}
+
+pi_result after_piContextCreate(const pi_context_properties *properties,
+                                pi_uint32 num_devices, const pi_device *devices,
+                                void (*pfn_notify)(const char *errinfo,
+                                                   const void *private_info,
+                                                   size_t cb, void *user_data),
+                                void *user_data, pi_context *ret_context) {
+  if (ret_context)
+    GlobalContext.contextHandle = *ret_context;
+  GlobalContext.deviceHandle = *devices;
+  return PI_SUCCESS;
+}
+
+pi_result after_piKernelGetInfo(pi_kernel kernel, pi_kernel_info param_name,
+                                size_t param_value_size, void *param_value,
+                                size_t *param_value_size_ret) {
+  switch (param_name) {
+  case PI_KERNEL_INFO_CONTEXT:
+    if (param_value_size_ret)
+      *param_value_size_ret = sizeof(GlobalContext.contextHandle);
+    if (param_value)
+      *static_cast<pi_context *>(param_value) = GlobalContext.contextHandle;
+    break;
+  case PI_KERNEL_INFO_PROGRAM:
+    if (param_value_size_ret)
+      *param_value_size_ret = sizeof(GlobalContext.programHandle);
+    if (param_value)
+      *(pi_program *)param_value = GlobalContext.programHandle;
+    break;
+  default:;
+  }
+
+  return PI_SUCCESS;
+}
+
+void redefineMockForKernelInterop(sycl::unittest::PiMock &Mock) {
+  Mock.redefineAfter<sycl::detail::PiApiKind::piContextCreate>(
+      after_piContextCreate);
+  Mock.redefineAfter<sycl::detail::PiApiKind::piProgramGetInfo>(
+      after_piProgramGetInfo);
+  Mock.redefineAfter<sycl::detail::PiApiKind::piContextGetInfo>(
+      after_piContextGetInfo);
+  Mock.redefineAfter<sycl::detail::PiApiKind::piKernelGetInfo>(
+      after_piKernelGetInfo);
+  Mock.redefine<sycl::detail::PiApiKind::piProgramGetBuildInfo>(
+      redefined_piProgramGetBuildInfo);
+}
---
+++ sycl/unittests/thread_safety/InteropKernelEnqueue.cpp
@@ -0,0 +1,57 @@
+#include <cstddef>
+#include <gtest/gtest.h>
+#include <helpers/KernelInteropCommon.hpp>
+#include <helpers/PiMock.hpp>
+#include <sycl/sycl.hpp>
+
+#include "ThreadUtils.h"
+
+namespace {
+using namespace sycl;
+
+constexpr std::size_t NArgs = 16;
+constexpr std::size_t ThreadCount = 4;
+constexpr std::size_t LaunchCount = 8;
+
+pi_uint32 LastArgSet = -1;
+std::size_t LastThread = -1;
+pi_result redefined_piKernelSetArg(pi_kernel kernel, pi_uint32 arg_index,
+                                   size_t arg_size, const void *arg_value) {
+  EXPECT_EQ((LastArgSet + 1) % NArgs, arg_index);
+  LastArgSet = arg_index;
+  std::size_t ArgValue = *static_cast<const std::size_t *>(arg_value);
+  if (arg_index == 0)
+    LastThread = ArgValue;
+  else
+    EXPECT_EQ(LastThread, ArgValue);
+  return PI_SUCCESS;
+}
+
+TEST(KernelEnqueue, InteropKernel) {
+  unittest::PiMock Mock;
+  redefineMockForKernelInterop(Mock);
+  Mock.redefine<sycl::detail::PiApiKind::piKernelSetArg>(
+      redefined_piKernelSetArg);
+
+  platform Plt = Mock.getPlatform();
+  queue Q;
+
+  DummyHandleT Handle;
+  auto KernelCL = reinterpret_cast<typename sycl::backend_traits<
+      sycl::backend::opencl>::template input_type<sycl::kernel>>(&Handle);
+  auto Kernel =
+      sycl::make_kernel<sycl::backend::opencl>(KernelCL, Q.get_context());
+
+  auto TestLambda = [&](std::size_t ThreadId) {
+    Q.submit([&](sycl::handler &CGH) {
+       for (std::size_t I = 0; I < NArgs; ++I)
+         CGH.set_arg(I, ThreadId);
+       CGH.single_task(Kernel);
+     }).wait();
+  };
+
+  for (std::size_t I = 0; I < LaunchCount; ++I) {
+    ThreadPool Pool(ThreadCount, TestLambda);
+  }
+}
+} // namespace
