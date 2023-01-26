7b93e1d421778795c42b62100a861368ea89f797
Lamzed-Short, Andrew <andrew.lamzed-short@intel.com>
2023-01-26 03:50:21 -0800

    Add new unit test to cmakelists.

--- sycl/unittests/kernel-and-program/KernelBundle.cpp
+++ sycl/unittests/kernel-and-program/KernelBundle.cpp
@@ -1,4 +1,4 @@
-//==- KernelBuildOptions.cpp - Kernel build options processing unit test --==//
+//==--------- KernelBundle.cpp - Kernel bundle-related unit tests ---------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
