9cd44727618250caeb5ead4453920bd2886016a6
Alexey Sachkov <alexey.sachkov@intel.com>
2023-01-27 07:09:47 -0500

    fix merge conflict artifacts

--- sycl/include/sycl/aliases.hpp
+++ sycl/include/sycl/aliases.hpp
@@ -57,7 +57,6 @@ class half;
   __SYCL_MAKE_VECTOR_ALIAS(cl_half, sycl::cl_half, N)
 
 
-#define __SYCL_2020_MAKE_VECTOR_ALIASES_FOR_VECTOR_LENGTH(N)                   \
 // FIXME: schar, longlong and ulonglong aliases are not defined by SYCL 2020
 //        spec, but they are preserved in SYCL 2020 mode, because SYCL-CTS is
 //        still using them.
@@ -125,23 +124,6 @@ using cl_half = half;
 using cl_float = float;
 using cl_double = double;
 #endif
-
-namespace opencl {
-// Strictly speaking, cl_* aliases should not be defined in opencl namespace in
-// SYCL 1.2.1 mode, but we do so to simplify our implementation
-using cl_bool = bool;
-using cl_char = std::int8_t;
-using cl_uchar = std::uint8_t;
-using cl_short = std::int16_t;
-using cl_ushort = std::uint16_t;
-using cl_int = std::int32_t;
-using cl_uint = std::uint32_t;
-using cl_long = std::int64_t;
-using cl_ulong = std::uint64_t;
-using cl_half = half;
-using cl_float = float;
-using cl_double = double;
-} // namespace opencl
 
 // Vector aliases are different between SYCL 1.2.1 and SYCL 2020
 #if !defined(SYCL_LANGUAGE_VERSION) || SYCL_LANGUAGE_VERSION >= 202001
