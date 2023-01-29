5cb37f31d293564c76cc360a43535ae2fbcf853b
jinge90 <ge.jin@intel.com>
2023-01-29 11:29:10 +0800

    fix format

--- sycl/include/sycl/ext/intel/math.hpp
+++ sycl/include/sycl/ext/intel/math.hpp
@@ -138,6 +138,7 @@ std::enable_if_t<std::is_same_v<Tp, sycl::half>, sycl::half> inv(Tp x) {
 }
 
 sycl::half2 inv(sycl::half2 x) { return sycl::half2{inv(x.s0()), inv(x.s1())}; }
+
 template <typename Tp>
 std::enable_if_t<std::is_same_v<Tp, float>, float> rint(Tp x) {
   return __imf_rintf(x);
