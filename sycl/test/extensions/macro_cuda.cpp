58e43ed9558df00590cb673db03159312d9c1c7e
Alexey Sachkov <alexey.sachkov@intel.com>
2023-01-30 11:58:19 -0500

    Fix macro_cuda.cpp test

--- sycl/test/extensions/macro_cuda.cpp
+++ sycl/test/extensions/macro_cuda.cpp
@@ -2,13 +2,14 @@
 // RUN: %clangxx -fsycl -fsyntax-only %s
 // REQUIRES: cuda_be
 
+#include <sycl/sycl.hpp>
+
 #if SYCL_EXT_ONEAPI_BACKEND_CUDA == 1
 constexpr bool macro_defined = true;
 #else
 constexpr bool macro_defined = false;
 #endif
 
-#include <sycl/sycl.hpp>
 int main() {
   static_assert(macro_defined);
 
