b29ae747f715bd47b67d1796613592402a6d7234
Alexey Sachkov <alexey.sachkov@intel.com>
2023-01-31 05:46:59 -0500

    Remove outdated comment

--- sycl/test/regression/operator-names.cpp
+++ sycl/test/regression/operator-names.cpp
@@ -3,9 +3,6 @@
 //===----------------------------------------------------------------------===//
 // This test checks if any SYCL header files use C++ operator name keywords
 // e.g. and, or, not
-//
-// This test does not use -fsyntax-only because it does not appear to respect
-// the -fno-operator-names option
 //===----------------------------------------------------------------------===//
 
 #include <sycl/sycl.hpp>
