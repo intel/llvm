igchor/unified-runtime

Move UMA sources to UR-common
UMA (Unified Memory Allocation) is built as a static library.
This commit doe not include any specific provider/pool implementation.
This will be supplied later.

The goal of UMA is to unify memory allocation between different
adapters. It is supposed to be a single point of optimizations for
memory operations (allocation, and in future, data movement).

UMA exposes pool and provider abstractions. Each adapter should
implement it's own memory provider for coarse-grain allocations.
On top of memory provider there will be memory pool that will offer
fine-grain allocations. Memory pool implementation can be shared
between all adapters or customized if necessary.

source/common/unified_memory_allocation/include/uma/memory_pool.h
@@ -1,0 +1,95 @@
+/*
+ *
+ * Copyright (C) 2023 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#ifndef UMA_MEMORY_POOL_H
+#define UMA_MEMORY_POOL_H 1
+
+#include <uma/base.h>
+#include <uma/memory_pool_ops.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct uma_memory_pool_t *uma_memory_pool_handle_t;
+
+///
+/// \brief Creates new memory pool
+/// \param ops instance of uma_memory_pool_ops_t
+/// \param params pointer to pool-specific parameters
+/// \return UMA_RESULT_SUCCESS on success or appropriate error code on failure
+///
+enum uma_result_t umaPoolCreate(struct uma_memory_pool_ops_t *ops, void *params,
+                                uma_memory_pool_handle_t *hPool);
+
+///
+/// \brief Destroys memory pool
+/// \param hPool handle to the pool
+///
+void umaPoolDestroy(uma_memory_pool_handle_t hPool);
+
+///
+/// \brief Allocates size bytes of uninitialized storage of the specified hPool
+/// \param hPool specified memory hPool
+/// \param size number of bytes to allocate
+/// \return Pointer to the allocated memory
+///
+void *umaPoolMalloc(uma_memory_pool_handle_t hPool, size_t size);
+
+///
+/// \brief Allocates size bytes of uninitialized storage of the specified hPool
+/// with
+///        specified alignment
+/// \param hPool specified memory hPool
+/// \param size number of bytes to allocate
+/// \param alignment alignment of the allocation
+/// \return Pointer to the allocated memory
+///
+void *umaPoolAlignedMalloc(uma_memory_pool_handle_t hPool, size_t size,
+                           size_t alignment);
+
+///
+/// \brief Allocates memory of the specified hPool for an array of num elements
+///        of size bytes each and initializes all bytes in the allocated storage
+///        to zero
+/// \param hPool specified memory hPool
+/// \param num number of objects
+/// \param size specified size of each element
+/// \return Pointer to the allocated memory
+///
+void *umaPoolCalloc(uma_memory_pool_handle_t hPool, size_t num, size_t size);
+
+///
+/// \brief Reallocates memory of the specified hPool
+/// \param hPool specified memory hPool
+/// \param ptr pointer to the memory block to be reallocated
+/// \param size new size for the memory block in bytes
+/// \return Pointer to the allocated memory
+///
+void *umaPoolRealloc(uma_memory_pool_handle_t hPool, void *ptr, size_t size);
+
+///
+/// \brief obtain size of block of memory allocated from the pool
+/// \param hPool specified memory hPool
+/// \param ptr pointer to the allocated memory
+/// \return Number of bytes
+///
+size_t umaPoolMallocUsabeSize(uma_memory_pool_handle_t hPool, void *ptr);
+
+///
+/// \brief Free the memory space of the specified hPool pointed by ptr
+/// \param hPool specified memory hPool
+/// \param ptr pointer to the allocated memory
+///
+void umaPoolFree(uma_memory_pool_handle_t hPool, void *ptr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* UMA_MEMORY_POOL_H */

source/common/unified_memory_allocation/src/memory_pool.c
@@ -1,0 +1,69 @@
+/*
+ *
+ * Copyright (C) 2023 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include <uma/memory_pool.h>
+
+#include <stdlib.h>
+
+struct uma_memory_pool_t {
+  struct uma_memory_pool_ops_t ops;
+  void *pool_priv;
+};
+
+enum uma_result_t umaPoolCreate(struct uma_memory_pool_ops_t *ops, void *params,
+                                uma_memory_pool_handle_t *hPool) {
+  uma_memory_pool_handle_t pool = malloc(sizeof(struct uma_memory_pool_t));
+  if (!pool) {
+    return UMA_RESULT_RUNTIME_ERROR;
+  }
+
+  // TODO: handle any version mismatches
+  pool->ops = *ops;
+
+  void *pool_priv;
+  enum uma_result_t ret = ops->initialize(params, &pool_priv);
+  if (ret != UMA_RESULT_SUCCESS) {
+    return ret;
+  }
+
+  pool->pool_priv = pool_priv;
+
+  *hPool = pool;
+
+  return UMA_RESULT_SUCCESS;
+}
+
+void umaPoolDestroy(uma_memory_pool_handle_t hPool) {
+  hPool->ops.finalize(hPool->pool_priv);
+  free(hPool);
+}
+
+void *umaPoolMalloc(uma_memory_pool_handle_t hPool, size_t size) {
+  return hPool->ops.malloc(hPool->pool_priv, size);
+}
+
+void *umaPoolAlignedMalloc(uma_memory_pool_handle_t hPool, size_t size,
+                           size_t alignment) {
+  return hPool->ops.aligned_malloc(hPool->pool_priv, size, alignment);
+}
+
+void *umaPoolCalloc(uma_memory_pool_handle_t hPool, size_t num, size_t size) {
+  return hPool->ops.calloc(hPool->pool_priv, num, size);
+}
+
+void *umaPoolRealloc(uma_memory_pool_handle_t hPool, void *ptr, size_t size) {
+  return hPool->ops.realloc(hPool->pool_priv, ptr, size);
+}
+
+size_t umaPoolMallocUsabeSize(uma_memory_pool_handle_t hPool, void *ptr) {
+  return hPool->ops.malloc_usable_size(hPool->pool_priv, ptr);
+}
+
+void umaPoolFree(uma_memory_pool_handle_t hPool, void *ptr) {
+  hPool->ops.free(hPool->pool_priv, ptr);
+}

intel/llvm

source/common/unified_memory_allocation/include/uma/memory_pool.h
@@ -1,0 +1,94 @@
+/*
+ *
+ * Copyright (C) 2023 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#ifndef UMA_MEMORY_POOL_H
+#define UMA_MEMORY_POOL_H 1
+
+#include <uma/base.h>
+#include <uma/memory_pool_ops.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct uma_memory_pool_t *uma_memory_pool_handle_t;
+
+///
+/// \brief Creates new memory pool
+/// \param ops instance of uma_memory_pool_ops_t
+/// \param params pointer to pool-specific parameters
+/// \return UMA_RESULT_SUCCESS on success or appropriate error code on failure
+///
+enum uma_result_t umaPoolCreate(struct uma_memory_pool_ops_t *ops, void *params,
+                                uma_memory_pool_handle_t *hPool);
+
+///
+/// \brief Destroys memory pool
+/// \param hPool handle to the pool
+///
+void umaPoolDestroy(uma_memory_pool_handle_t hPool);
+
+///
+/// \brief Allocates size bytes of uninitialized storage of the specified hPool
+/// \param hPool specified memory hPool
+/// \param size number of bytes to allocate
+/// \return Pointer to the allocated memory
+///
+void *umaPoolMalloc(uma_memory_pool_handle_t hPool, size_t size);
+
+///
+/// \brief Allocates size bytes of uninitialized storage of the specified hPool
+/// with specified alignment
+/// \param hPool specified memory hPool
+/// \param size number of bytes to allocate
+/// \param alignment alignment of the allocation
+/// \return Pointer to the allocated memory
+///
+void *umaPoolAlignedMalloc(uma_memory_pool_handle_t hPool, size_t size,
+                           size_t alignment);
+
+///
+/// \brief Allocates memory of the specified hPool for an array of num elements
+///        of size bytes each and initializes all bytes in the allocated storage
+///        to zero
+/// \param hPool specified memory hPool
+/// \param num number of objects
+/// \param size specified size of each element
+/// \return Pointer to the allocated memory
+///
+void *umaPoolCalloc(uma_memory_pool_handle_t hPool, size_t num, size_t size);
+
+///
+/// \brief Reallocates memory of the specified hPool
+/// \param hPool specified memory hPool
+/// \param ptr pointer to the memory block to be reallocated
+/// \param size new size for the memory block in bytes
+/// \return Pointer to the allocated memory
+///
+void *umaPoolRealloc(uma_memory_pool_handle_t hPool, void *ptr, size_t size);
+
+///
+/// \brief Obtains size of block of memory allocated from the pool
+/// \param hPool specified memory hPool
+/// \param ptr pointer to the allocated memory
+/// \return Number of bytes
+///
+size_t umaPoolMallocUsableSize(uma_memory_pool_handle_t hPool, void *ptr);
+
+///
+/// \brief Frees the memory space of the specified hPool pointed by ptr
+/// \param hPool specified memory hPool
+/// \param ptr pointer to the allocated memory
+///
+void umaPoolFree(uma_memory_pool_handle_t hPool, void *ptr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* UMA_MEMORY_POOL_H */

source/common/unified_memory_allocation/src/memory_pool.c
@@ -1,0 +1,71 @@
+/*
+ *
+ * Copyright (C) 2023 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include <uma/memory_pool.h>
+
+#include <assert.h>
+#include <stdlib.h>
+
+struct uma_memory_pool_t {
+  struct uma_memory_pool_ops_t ops;
+  void *pool_priv;
+};
+
+enum uma_result_t umaPoolCreate(struct uma_memory_pool_ops_t *ops, void *params,
+                                uma_memory_pool_handle_t *hPool) {
+  uma_memory_pool_handle_t pool = malloc(sizeof(struct uma_memory_pool_t));
+  if (!pool) {
+    return UMA_RESULT_RUNTIME_ERROR;
+  }
+
+  assert(ops->version == UMA_VERSION_CURRENT);
+
+  pool->ops = *ops;
+
+  void *pool_priv;
+  enum uma_result_t ret = ops->initialize(params, &pool_priv);
+  if (ret != UMA_RESULT_SUCCESS) {
+    return ret;
+  }
+
+  pool->pool_priv = pool_priv;
+
+  *hPool = pool;
+
+  return UMA_RESULT_SUCCESS;
+}
+
+void umaPoolDestroy(uma_memory_pool_handle_t hPool) {
+  hPool->ops.finalize(hPool->pool_priv);
+  free(hPool);
+}
+
+void *umaPoolMalloc(uma_memory_pool_handle_t hPool, size_t size) {
+  return hPool->ops.malloc(hPool->pool_priv, size);
+}
+
+void *umaPoolAlignedMalloc(uma_memory_pool_handle_t hPool, size_t size,
+                           size_t alignment) {
+  return hPool->ops.aligned_malloc(hPool->pool_priv, size, alignment);
+}
+
+void *umaPoolCalloc(uma_memory_pool_handle_t hPool, size_t num, size_t size) {
+  return hPool->ops.calloc(hPool->pool_priv, num, size);
+}
+
+void *umaPoolRealloc(uma_memory_pool_handle_t hPool, void *ptr, size_t size) {
+  return hPool->ops.realloc(hPool->pool_priv, ptr, size);
+}
+
+size_t umaPoolMallocUsableSize(uma_memory_pool_handle_t hPool, void *ptr) {
+  return hPool->ops.malloc_usable_size(hPool->pool_priv, ptr);
+}
+
+void umaPoolFree(uma_memory_pool_handle_t hPool, void *ptr) {
+  hPool->ops.free(hPool->pool_priv, ptr);
+}