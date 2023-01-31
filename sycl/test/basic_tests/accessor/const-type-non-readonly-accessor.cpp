1edb94f82cc1d369e12262fb43b2a103d12b276f
M Moadeli <mahmoud.moadeli@codeplay.com>
2023-01-31 22:52:19 +0000

    Add test for const qualified DataT with non readonly accessor. Update failing tests due to adding diagnostic for const qualified DataT only allowed for non readonly accessor.

---
+++ sycl/test/basic_tests/accessor/const-type-non-readonly-accessor.cpp
@@ -0,0 +1,27 @@
+// RUN: not %clangxx -fsycl -fsycl-device-only -fsyntax-only \
+// RUN: %s -I %sycl_include 2>&1 | FileCheck %s
+
+#include <sycl/sycl.hpp>
+
+using namespace sycl;
+
+constexpr size_t dataSize = 1;
+
+int main() {
+  const int a[dataSize] = {1};
+
+  try {
+    auto defaultQueue = queue{};
+    auto bufA = buffer<const int, 1>{a, range{dataSize}};
+    defaultQueue.submit([&](handler &cgh) {
+      sycl::accessor accA{bufA, cgh, read_write};
+    });
+
+    defaultQueue.throw_asynchronous();
+  } catch (const exception &e) {
+    std::cout << "Exception caught: " << e.what() << std::endl;
+  }
+  return 0;
+}
+
+// CHECK: static assertion failed due to requirement '!isConst || IsAccessReadOnly': A const qualified DataT is only allowed for a read-only accessor
