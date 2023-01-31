c4027b3590aff6ef35ba9befb44b4ed87ccc8243
KornevNikita <nikita.kornev@intel.com>
2023-01-31 04:01:53 -0800

    Fix the build

--- llvm/tools/llvm-c-test/echo.cpp
+++ llvm/tools/llvm-c-test/echo.cpp
@@ -1385,9 +1385,6 @@ int llvm_echo(void) {
   size_t ModuleIdentLen;
   const char *ModuleName = LLVMGetModuleIdentifier(Src, &ModuleIdentLen);
   LLVMContextRef Ctx = LLVMContextCreate();
-  // FIXME: Delete once after the project is ready for opaque
-  // pointers. Original code assued that "default" value is "true".
-  LLVMContextSetOpaquePointers(Ctx, true);
   LLVMModuleRef M = LLVMModuleCreateWithNameInContext(ModuleName, Ctx);
 
   LLVMSetSourceFileName(M, SourceFileName, SourceFileLen);
