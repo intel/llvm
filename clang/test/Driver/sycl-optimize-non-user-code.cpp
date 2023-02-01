ba2fe26a621a548c1ec2a160371b0d69da58e9b4
Sabianin, Maksim <maksim.sabianin@intel.com>
2023-02-01 06:12:33 -0800

    [SYCL] adjust test to error message

--- clang/test/Driver/sycl-optimize-non-user-code.cpp
+++ clang/test/Driver/sycl-optimize-non-user-code.cpp
@@ -6,15 +6,15 @@
 
 // RUN: not %clangxx -fsycl -O1 -fsycl-optimize-non-user-code %s 2>&1  | FileCheck %s -check-prefix=CHECK-ERROR
 // RUN: not %clangxx -fsycl -fsycl-optimize-non-user-code %s 2>&1 | FileCheck %s -check-prefix=CHECK-ERROR
-// CHECK-ERROR: error: -fsycl-optimize-non-user-code option can be used only in conjunction with -O0 option
+// CHECK-ERROR: error: -fsycl-optimize-non-user-code option can be used only in conjunction with -O0
 
 // Check cases for Microsoft Windows Driver.
 // RUN: %clang_cl -fsycl -Od -### %s 2>&1 | FileCheck %s -check-prefix=NO-OPT-WIN-CHECK
 // NO-OPT-WIN-CHECK-NOT: fsycl-optimize-non-user-code
 
-// RUN: %clang_cl -fsycl -Od -fsycl-optimize-non-user-codek -### %s 2>&1 | FileCheck %s -check-prefix=OPT-WIN-CHECK
+// RUN: %clang_cl -fsycl -Od -fsycl-optimize-non-user-code -### %s 2>&1 | FileCheck %s -check-prefix=OPT-WIN-CHECK
 // OPT-WIN-CHECK: fsycl-optimize-non-user-code
 
 // RUN: not %clang_cl -fsycl -O1 -fsycl-optimize-non-user-code %s 2>&1  | FileCheck %s -check-prefix=CHECK-WIN-ERROR
 // RUN: not %clang_cl -fsycl -fsycl-optimize-non-user-code %s 2>&1 | FileCheck %s -check-prefix=CHECK-WIN-ERROR
-// CHECK-WIN-ERROR: error: -fsycl-optimize-non-user-code option can be used only in conjunction with -Od option
+// CHECK-WIN-ERROR: error: -fsycl-optimize-non-user-code option can be used only in conjunction with -Od
