db29cd8644ea3aa252e583b7fd2a1ef3e80a69e1
Chen, Brox <brox.chen@intel.com>
2023-02-02 08:02:14 -0800

    corrected typo

--- clang/test/Driver/sycl-spirv-ext.c
+++ clang/test/Driver/sycl-spirv-ext.c
@@ -45,8 +45,8 @@
 // CHECK-DEFAULT-SAME:,+SPV_INTEL_long_constant_composite
 // CHECK-DEFAULT-SAME:,+SPV_INTEL_arithmetic_fence
 // CHECK-DEFAULT-SAME:,+SPV_INTEL_token_type
-// CHECK-FPGA-HW-SAME:,+SPV_INTEL_fpga_buffer_location
-// CHECK-FPGA-HW-SAME:,+SPV_INTEL_fpga_argument_interfaces
+// CHECK-DEFAULT-SAME:,+SPV_INTEL_fpga_buffer_location
+// CHECK-DEFAULT-SAME:,+SPV_INTEL_fpga_argument_interfaces
 // CHECK-DEFAULT-SAME:,+SPV_INTEL_bfloat16_conversion
 // CHECK-DEFAULT-SAME:,+SPV_INTEL_joint_matrix
 // CHECK-DEFAULT-SAME:,+SPV_INTEL_hw_thread_queries
