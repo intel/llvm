c54f5988d7c512b6adb6fe451ee4d7614668c389
Larsen, Steffen <steffen.larsen@intel.com>
2023-02-02 07:28:07 -0800

    [SYCL] Fix test configurations when L0 and OpenCL are disabled
    This commit changes the configuration of in-tree SYCL LIT tests to
    include features for when the L0 and OpenCL plugins are enabled. This
    allows the interop backend tests to be split into backend-dependent
    tests to be run only when their backends are enabled. Additionally,
    this commit changes the fsycl-host-only LIT configuration replacement to
    ignore unset include paths, avoiding empty "-isystem" options when
    related backends are disabled.
    
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

---
+++ sycl/test/basic_tests/interop-backend-traits-level-zero.cpp
@@ -0,0 +1,24 @@
+// REQUIRES: level_zero_be
+// RUN: %clangxx %fsycl-host-only -fsyntax-only %s
+
+#include <sycl/sycl.hpp>
+#include <ze_api.h>
+
+constexpr auto Backend = sycl::backend::ext_oneapi_level_zero;
+
+int main() {
+  static_assert(
+      std::is_same_v<sycl::backend_traits<Backend>::input_type<sycl::platform>,
+                     sycl::detail::interop<Backend, sycl::platform>::type>);
+  static_assert(
+      std::is_same_v<sycl::backend_traits<Backend>::return_type<sycl::platform>,
+                     sycl::detail::interop<Backend, sycl::platform>::type>);
+  static_assert(
+      std::is_same_v<sycl::backend_traits<Backend>::return_type<sycl::device>,
+                     sycl::detail::interop<Backend, sycl::device>::type>);
+  static_assert(
+      std::is_same_v<sycl::backend_traits<Backend>::return_type<sycl::queue>,
+                     sycl::detail::interop<Backend, sycl::queue>::type>);
+
+  return 0;
+}
--- sycl/test/lit.cfg.py
+++ sycl/test/lit.cfg.py
@@ -93,7 +93,12 @@ config.substitutions.append( ('%sycl_tools_src_dir',  config.sycl_tools_src_dir 
 config.substitutions.append( ('%llvm_build_lib_dir',  config.llvm_build_lib_dir ) )
 config.substitutions.append( ('%llvm_build_bin_dir',  config.llvm_build_bin_dir ) )
 
-config.substitutions.append( ('%fsycl-host-only', '-std=c++17 -Xclang -fsycl-is-host -isystem %s -isystem %s -isystem %s -isystem %s' % (config.sycl_include, config.level_zero_include_dir, config.opencl_include_dir, config.sycl_include + '/sycl/') ) )
+sycl_host_only_options = '-std=c++17 -Xclang -fsycl-is-host'
+for include_dir in [config.sycl_include, config.level_zero_include_dir, config.opencl_include_dir, config.sycl_include + '/sycl/']:
+    if include_dir:
+        sycl_host_only_options += ' -isystem %s' % include_dir
+config.substitutions.append( ('%fsycl-host-only', sycl_host_only_options) )
+
 config.substitutions.append( ('%sycl_lib', ' -lsycl6' if platform.system() == "Windows" else '-lsycl') )
 
 llvm_config.add_tool_substitutions(['llvm-spirv'], [config.sycl_tools_dir])
@@ -112,6 +117,12 @@ if config.hip_be == "ON":
 
 if config.esimd_emulator_be == "ON":
     config.available_features.add('esimd_emulator_be')
+
+if config.opencl_be == "ON":
+    config.available_features.add('opencl_be')
+
+if config.level_zero_be == "ON":
+    config.available_features.add('level_zero_be')
 
 if triple == 'nvptx64-nvidia-cuda':
     config.available_features.add('cuda')
