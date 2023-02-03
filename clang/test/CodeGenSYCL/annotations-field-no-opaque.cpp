57a241953c6be49b3103ccb4cc9f20db3fa63d06
Mariya Podchishchaeva <mariya.podchishchaeva@intel.com>
2023-02-03 11:07:01 -0500

    Fix test
    It seems these numbers mean line of code, didn't know about that.

--- clang/test/CodeGenSYCL/annotations-field-no-opaque.cpp
+++ clang/test/CodeGenSYCL/annotations-field-no-opaque.cpp
@@ -20,18 +20,18 @@ int __attribute__((sycl_device)) foo() {
     struct foo f;
     f.v = 1;
 // CHECK: getelementptr inbounds %struct.foo, %struct.foo addrspace(4)* %{{.*}}, i32 0, i32 0
-// CHECK-NEXT: call i32 addrspace(4)* @llvm.ptr.annotation.p4i32.p1i8({{.*}}str{{.*}}str{{.*}}i32 11, i8 addrspace(1)* null)
-// CHECK-NEXT: call i32 addrspace(4)* @llvm.ptr.annotation.p4i32.p1i8({{.*}}str{{.*}}str{{.*}}i32 11, i8 addrspace(1)* null)
+// CHECK-NEXT: call i32 addrspace(4)* @llvm.ptr.annotation.p4i32.p1i8({{.*}}str{{.*}}str{{.*}}i32 14, i8 addrspace(1)* null)
+// CHECK-NEXT: call i32 addrspace(4)* @llvm.ptr.annotation.p4i32.p1i8({{.*}}str{{.*}}str{{.*}}i32 14, i8 addrspace(1)* null)
     f.w = 42;
 // CHECK: getelementptr inbounds %struct.foo, %struct.foo addrspace(4)* %{{.*}}, i32 0, i32 1
-// CHECK-NEXT: call i8 addrspace(4)* @llvm.ptr.annotation.p4i8.p1i8({{.*}}str{{.*}}str{{.*}}i32 12, i8 addrspace(1)* null)
-// CHECK-NEXT: call i8 addrspace(4)* @llvm.ptr.annotation.p4i8.p1i8({{.*}}str{{.*}}str{{.*}}i32 12, i8 addrspace(1)* null)
+// CHECK-NEXT: call i8 addrspace(4)* @llvm.ptr.annotation.p4i8.p1i8({{.*}}str{{.*}}str{{.*}}i32 15, i8 addrspace(1)* null)
+// CHECK-NEXT: call i8 addrspace(4)* @llvm.ptr.annotation.p4i8.p1i8({{.*}}str{{.*}}str{{.*}}i32 15, i8 addrspace(1)* null)
     f.f = 0;
 // CHECK: getelementptr inbounds %struct.foo, %struct.foo addrspace(4)* %{{.*}}, i32 0, i32 2
 // CHECK-NEXT: bitcast float addrspace(4)* %{{.*}} to i8 addrspace(4)*
-// CHECK-NEXT: call i8 addrspace(4)* @llvm.ptr.annotation.p4i8.p1i8({{.*}}str{{.*}}str{{.*}}i32 13, i8 addrspace(1)* null)
+// CHECK-NEXT: call i8 addrspace(4)* @llvm.ptr.annotation.p4i8.p1i8({{.*}}str{{.*}}str{{.*}}i32 16, i8 addrspace(1)* null)
 // CHECK-NEXT: bitcast i8 addrspace(4)* %{{.*}} to float addrspace(4)*
 // CHECK-NEXT: bitcast float addrspace(4)* %{{.*}} to i8 addrspace(4)*
-// CHECK-NEXT: call i8 addrspace(4)* @llvm.ptr.annotation.p4i8.p1i8({{.*}}str{{.*}}str{{.*}}i32 13, i8 addrspace(1)* null)
+// CHECK-NEXT: call i8 addrspace(4)* @llvm.ptr.annotation.p4i8.p1i8({{.*}}str{{.*}}str{{.*}}i32 16, i8 addrspace(1)* null)
     return 0;
 }
