28a604488d53f0f0ceed2c24beb72cb54ac879e1
Haroon Rasheed <haroonar32@gmail.com>
2023-02-07 19:11:50 +0500

    [SYCL][FIX] Fixed group_ballot default argument for predicate

--- sycl/include/sycl/ext/oneapi/sub_group_mask.hpp
+++ sycl/include/sycl/ext/oneapi/sub_group_mask.hpp
@@ -29,6 +29,11 @@ namespace ext::oneapi {
 #define BITS_TYPE uint32_t
 #endif
 
+// defining `group_ballot` here to make predicate default `true`
+template <typename Group>
+detail::enable_if_t<std::is_same<std::decay_t<Group>, sub_group>::value, sub_group_mask>
+group_ballot(Group g, bool predicate = true);
+
 struct sub_group_mask {
   friend class detail::Builder;
   using BitsType = BITS_TYPE;
@@ -267,7 +272,7 @@ private:
 template <typename Group>
 detail::enable_if_t<std::is_same<std::decay_t<Group>, sub_group>::value,
                     sub_group_mask>
-group_ballot(Group g, bool predicate = true) {
+group_ballot(Group g, bool predicate) {
   (void)g;
 #ifdef __SYCL_DEVICE_ONLY__
   auto res = __spirv_GroupNonUniformBallot(
