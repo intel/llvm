86ff45e7144f94ccd7854eff0eecd56ce5f0e4ef
Petr Vesely <petr.vesely@codeplay.com>
2023-02-07 13:37:08 +0000

    [ur] Add Queue testsuite

---
+++ test/conformance/queue/urQueueRelease.cpp
@@ -0,0 +1,16 @@
+// Copyright (C) 2023 Intel Corporation
+// SPDX-License-Identifier: MIT
+#include <uur/fixtures.h>
+
+using urQueueReleaseTest = uur::urQueueTest;
+UUR_INSTANTIATE_DEVICE_TEST_SUITE_P(urQueueReleaseTest);
+
+TEST_P(urQueueReleaseTest, Success) {
+    ASSERT_SUCCESS(urQueueRetain(queue));
+    ASSERT_SUCCESS(urQueueRelease(queue));
+}
+
+TEST_P(urQueueReleaseTest, InvalidNullHandleQueue) {
+    ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_NULL_HANDLE,
+                     urQueueRelease(nullptr));
+}
