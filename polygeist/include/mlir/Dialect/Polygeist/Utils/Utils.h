9ac9d0eefe68622217837aad011dfc769d33d2b0
Victor Perez <victor.perez@codeplay.com>
2023-02-09 12:43:21 +0000

    Add license

--- polygeist/include/mlir/Dialect/Polygeist/Utils/Utils.h
+++ polygeist/include/mlir/Dialect/Polygeist/Utils/Utils.h
@@ -1,5 +1,12 @@
 #ifndef MLIR_POLYGEIST_UTILS_UTILS_H_
 #define MLIR_POLYGEIST_UTILS_UTILS_H_
+//===- Utils.h - Utilities for Polygeist transformations ---------* C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 
 #include "mlir/Conversion/LLVMCommon/TypeConverter.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
