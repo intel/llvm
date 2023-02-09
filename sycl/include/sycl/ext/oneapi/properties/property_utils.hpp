e1edc32e88fe56ae2fe2f14963463f40071e6480
Agarwal, Udit <udit.agarwal@intel.com>
2023-02-09 07:20:42 -0800

    Minor formatting change.

--- sycl/include/sycl/ext/oneapi/properties/property_utils.hpp
+++ sycl/include/sycl/ext/oneapi/properties/property_utils.hpp
@@ -111,7 +111,8 @@ template <typename RHS> struct SelectNonVoid<void, RHS> {
 struct SortByPropertyId {
   template <typename T1, typename T2>
   using fn = sycl::detail::boost::mp11::mp_bool <
-             PropertyID<T1>::value<PropertyID<T2>::value>;
+             (PropertyID<T1>::value <
+              PropertyID<T2>::value)>;
 };
 template <typename... Ts> struct Sorted {
   static_assert(detail::AllPropertyValues<std::tuple<Ts...>>::value,
