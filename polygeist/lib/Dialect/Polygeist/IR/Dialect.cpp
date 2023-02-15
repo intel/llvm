e37d690edc933aea0409e7c49172bbe55684d21a
Victor Perez <victor.perez@codeplay.com>
2023-02-15 15:36:33 +0000

    [SYCL-MLIR][NFC] Restructure Polygeist dialect
    Apply following changes to the dialect to follow MLIR dialects
    conventions:
    
    - Change headers locations:
      - Dialect definition: "mlir/Dialect/Polygeist/IR/"
      - Transform passes: "mlir/Dialect/Polygeist/Transforms/"
      - Conversion passes: "mlir/Conversion/PolygeistTo<dialect>/"
      - Dialect utils: "mlir/Dialect/Polygeist/Utils/<file>.h"
    - Create a library for each conversion pass.
    - Conversion passes are defined under the `mlir` namespace.
    
    This commit does not intend to be a complete refactoring of the dialect.
    This is a work in progress, not intending to refactor any code.
    
    Signed-off-by: Victor Perez <victor.perez@codeplay.com>

---
+++ polygeist/lib/Dialect/Polygeist/IR/Dialect.cpp
@@ -0,0 +1,52 @@
+//===- PolygeistDialect.cpp - Polygeist dialect ---------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Dialect/Polygeist/IR/Ops.h"
+#include "mlir/Dialect/Polygeist/IR/Polygeist.h"
+#include "mlir/IR/DialectImplementation.h"
+#include "mlir/Transforms/InliningUtils.h"
+
+using namespace mlir;
+using namespace mlir::polygeist;
+
+//===----------------------------------------------------------------------===//
+// Polygeist Dialect Interfaces
+//===----------------------------------------------------------------------===//
+
+namespace {
+/// This class defines the interface for handling inlining Polygeist
+/// operations.
+struct PolygeistInlinerInterface : public DialectInlinerInterface {
+  using DialectInlinerInterface::DialectInlinerInterface;
+
+  //===--------------------------------------------------------------------===//
+  // Analysis Hooks
+  //===--------------------------------------------------------------------===//
+
+  // Operations in the Polygeist dialect are legal to inline.
+  bool isLegalToInline(Operation *Op, Region *Dest, bool WouldBeCloned,
+                       IRMapping &ValueMapping) const final {
+    return true;
+  }
+};
+
+} // namespace
+
+//===----------------------------------------------------------------------===//
+// Polygeist dialect.
+//===----------------------------------------------------------------------===//
+
+void PolygeistDialect::initialize() {
+  addOperations<
+#define GET_OP_LIST
+#include "mlir/Dialect/Polygeist/IR/PolygeistOps.cpp.inc"
+      >();
+  addInterfaces<PolygeistInlinerInterface>();
+}
+
+#include "mlir/Dialect/Polygeist/IR/PolygeistOpsDialect.cpp.inc"
