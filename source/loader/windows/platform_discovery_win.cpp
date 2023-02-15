6a264995e8c0af2235708c68bff061a32dc36d29
Petr Vesely <petr.vesely@codeplay.com>
2023-02-15 09:20:08 +0000

    [ur] Make known plaforms std::array

--- source/loader/windows/platform_discovery_win.cpp
+++ source/loader/windows/platform_discovery_win.cpp
@@ -11,6 +11,7 @@
 
 #include <Windows.h>
 
+#include <array>
 #include <cassert>
 #include <cfgmgr32.h>
 #include <devguid.h>
@@ -21,6 +22,8 @@
 
 namespace loader {
 
+static constexpr std::array<char *, 0> knownAdaptersNames{};
+
 std::vector<PlatformLibraryPath> discoverEnabledPlatforms() {
     std::vector<PlatformLibraryPath> enabledPlatforms;
 
@@ -28,7 +31,11 @@ std::vector<PlatformLibraryPath> discoverEnabledPlatforms() {
     char *altPlatforms = nullptr;
     _dupenv_s(&altPlatforms, NULL, "UR_ADAPTERS_FORCE_LOAD");
 
-    if (altPlatforms != nullptr) {
+    if (altPlatforms == nullptr) {
+        for (auto libName : knownAdaptersNames) {
+            enabledPlatforms.emplace_back(libName);
+        }
+    } else {
         std::stringstream ss(altPlatforms);
         while (ss.good()) {
             std::string substr;
