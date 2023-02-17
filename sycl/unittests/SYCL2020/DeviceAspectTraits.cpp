654ee58baae70fdd006a9d4ab559e1a876f8ae0c
Maronas, Marcos <marcos.maronas@intel.com>
2023-02-17 07:17:02 -0800

    Separates any_device_has and all_devices_have tests.
    Signed-off-by: Maronas, Marcos <marcos.maronas@intel.com>

--- sycl/unittests/SYCL2020/DeviceAspectTraits.cpp
+++ sycl/unittests/SYCL2020/DeviceAspectTraits.cpp
@@ -4,11 +4,17 @@
 
 #include <gtest/gtest.h>
 
-TEST(DeviceAspectTraits, AnyDeviceHasHost) {
-  std::cout << sycl::any_device_has_v<sycl::aspect::host> << std::endl;
+TEST(DeviceAspectTraits, AnyDeviceHasAspect) {
 #define __SYCL_ASPECT(ASPECT, ASPECT_VAL)                                      \
   constexpr bool CheckAnyDeviceHas##ASPECT =                                   \
-      sycl::any_device_has_v<sycl::aspect::ASPECT>;                            \
+      sycl::any_device_has_v<sycl::aspect::ASPECT>;
+
+#include <sycl/info/aspects.def>
+
+#undef __SYCL_ASPECT
+}
+TEST(DeviceAspectTraits, AllDevicesHaveAspect) {
+#define __SYCL_ASPECT(ASPECT, ASPECT_VAL)                                      \
   constexpr bool CheckAllDevicesHave##ASPECT =                                 \
       sycl::all_devices_have_v<sycl::aspect::ASPECT>;
 
