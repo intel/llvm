d36dcae70da55f804b34876407863b84641cb74b
Petr Vesely <petr.vesely@codeplay.com>
2023-02-20 15:32:14 +0000

    [ur] queue release/retain

--- test/conformance/queue/urQueueRetain.cpp
+++ test/conformance/queue/urQueueRetain.cpp
@@ -6,8 +6,20 @@ using urQueueRetainTest = uur::urQueueTest;
 UUR_INSTANTIATE_DEVICE_TEST_SUITE_P(urQueueRetainTest);
 
 TEST_P(urQueueRetainTest, Success) {
+    const auto prevRefCount = uur::urQueueGetReferenceCount(queue);
+    ASSERT_TRUE(prevRefCount.second);
+
     ASSERT_SUCCESS(urQueueRetain(queue));
+
+    const auto refCount = uur::urQueueGetReferenceCount(queue);
+    ASSERT_TRUE(refCount.second);
+
+    ASSERT_LT(prevRefCount.first, refCount.first);
+
     EXPECT_SUCCESS(urQueueRelease(queue));
 }
 
-TEST_P(urQueueRetainTest, InvalidNullHandleQueue) { ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_NULL_HANDLE, urQueueRetain(nullptr)); }
+TEST_P(urQueueRetainTest, InvalidNullHandleQueue) {
+    ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_NULL_HANDLE,
+                     urQueueRetain(nullptr));
+}
