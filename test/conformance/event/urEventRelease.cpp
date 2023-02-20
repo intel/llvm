90d174fcf5e65ee29c413c98378ff012cee1ee39
Petr Vesely <petr.vesely@codeplay.com>
2023-02-20 15:53:17 +0000

    [ur] event release/retain

--- test/conformance/event/urEventRelease.cpp
+++ test/conformance/event/urEventRelease.cpp
@@ -3,25 +3,25 @@
 
 #include "fixtures.h"
 
-using urEventReleaseTest = uur::event::urEventReferenceTest;
-
-/* Check that urEventRelease returns Success */
-TEST_P(urEventReleaseTest, Success) { ASSERT_SUCCESS(urEventRelease(event)); }
-
-/* Check that urEventRelease decrements the reference count*/
-TEST_P(urEventReleaseTest, CheckReferenceCount) {
-    ASSERT_SUCCESS(urEventRetain(event));
-    ASSERT_TRUE(checkEventReferenceCount(2));
-    ASSERT_SUCCESS(urEventRelease(event));
-    ASSERT_TRUE(checkEventReferenceCount(1));
-    ASSERT_SUCCESS(urEventRelease(event));
-}
-
-using urEventReleaseNegativeTest = uur::urQueueTest;
-
-TEST_P(urEventReleaseNegativeTest, InvalidNullHandle) {
-    ASSERT_EQ_RESULT(urEventRelease(nullptr), UR_RESULT_ERROR_INVALID_NULL_HANDLE);
-}
+using urEventReleaseTest = uur::event::urEventTest;
 
 UUR_INSTANTIATE_DEVICE_TEST_SUITE_P(urEventReleaseTest);
-UUR_INSTANTIATE_DEVICE_TEST_SUITE_P(urEventReleaseNegativeTest);
+
+TEST_P(urEventReleaseTest, Success) {
+    ASSERT_SUCCESS(urEventRetain(event));
+
+    const auto prevRefCount = uur::urEventGetReferenceCount(event);
+    ASSERT_TRUE(prevRefCount.second);
+
+    ASSERT_SUCCESS(urEventRelease(event));
+
+    const auto refCount = uur::urEventGetReferenceCount(event);
+    ASSERT_TRUE(refCount.second);
+
+    ASSERT_GT(prevRefCount.first, refCount.first);
+}
+
+TEST(urEventReleaseTest, InvalidNullHandle) {
+    ASSERT_EQ_RESULT(urEventRelease(nullptr),
+                     UR_RESULT_ERROR_INVALID_NULL_HANDLE);
+}
