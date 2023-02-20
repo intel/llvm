462e5325099c53e047c4768d4be728ae5c9f92b7
Petr Vesely <petr.vesely@codeplay.com>
2023-02-20 09:44:39 +0000

    [ur] Complete Native Handle Tests

--- test/conformance/event/urEventCreateWithNativeHandle.cpp
+++ test/conformance/event/urEventCreateWithNativeHandle.cpp
@@ -1,2 +1,14 @@
 // Copyright (C) 2023 Intel Corporation
 // SPDX-License-Identifier: MIT
+
+#include <uur/fixtures.h>
+
+using urEventCreateWithNativeHandleTest = uur::urContextTest;
+UUR_INSTANTIATE_DEVICE_TEST_SUITE_P(urEventCreateWithNativeHandleTest);
+
+TEST_P(urEventCreateWithNativeHandleTest, InvalidNullHandleNativeEvent) {
+    ur_event_handle_t event = nullptr;
+    ASSERT_EQ_RESULT(
+        UR_RESULT_ERROR_INVALID_NULL_HANDLE,
+        urEventCreateWithNativeHandle(nullptr, context, &event));
+}
