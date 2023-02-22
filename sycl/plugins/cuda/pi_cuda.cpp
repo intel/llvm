128d255fea94f5d31ec053b3a48294ac84be217c
zjin-lcf <57232910+zjin-lcf@users.noreply.github.com>
2023-02-22 10:38:55 -0500

    Update sycl/plugins/cuda/pi_cuda.cpp with the reviewer's suggestions
    Co-authored-by: Steffen Larsen <steffen.larsen@intel.com>

--- sycl/plugins/cuda/pi_cuda.cpp
+++ sycl/plugins/cuda/pi_cuda.cpp
@@ -1967,19 +1967,6 @@ pi_result cuda_piDeviceGetInfo(pi_device device, pi_device_info param_name,
   }
 
   case PI_DEVICE_INFO_MAX_MEM_BANDWIDTH: {
-    int memory_clock_khz = 0;
-    sycl::detail::pi::assertion(
-        cuDeviceGetAttribute(&memory_clock_khz, CU_DEVICE_ATTRIBUTE_MEMORY_CLOCK_RATE,
-                             device->get()) == CUDA_SUCCESS);
-
-    int memory_bus_width = 0;
-    sycl::detail::pi::assertion(
-        cuDeviceGetAttribute(&memory_bus_width,
-                             CU_DEVICE_ATTRIBUTE_GLOBAL_MEMORY_BUS_WIDTH,
-                             device->get()) == CUDA_SUCCESS);
-
-    // Reference
-    // https://github.com/jeffhammond/HPCInfo/blob/master/cuda/gpu-detect.cu
     int major = 0;
     sycl::detail::pi::assertion(
         cuDeviceGetAttribute(&major,
@@ -1992,15 +1979,30 @@ pi_result cuda_piDeviceGetInfo(pi_device device, pi_device_info param_name,
                              CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MINOR,
                              device->get()) == CUDA_SUCCESS);
 
-    // Xavier AGX override
-    if (major == 7 && minor == 2) {
-       memory_clock_khz = 2133000;
-    }
+    // Some specific devices seem to need special handling. See reference
+    // https://github.com/jeffhammond/HPCInfo/blob/master/cuda/gpu-detect.cu
+    bool is_xavier_agx = major == 7 && minor == 2;
+    bool is_orin_agx = major == 8 && minor == 7;
 
-    // Orin AGX override
-    if (major == 8 && minor == 7) {
-       memory_clock_khz = 3200000;
+    int memory_clock_khz = 0;
+    if (is_xavier_agx) {
+        memory_clock_khz = 2133000;
+    } else if (is_orin_agx) {
+        memory_clock_khz = 3200000;
+    } else {
+        sycl::detail::pi::assertion(
+            cuDeviceGetAttribute(&memory_clock_khz, CU_DEVICE_ATTRIBUTE_MEMORY_CLOCK_RATE,
+                                device->get()) == CUDA_SUCCESS);
+    }
+    
+    int memory_bus_width = 0;
+    if (is_orin_agx) {
        memory_bus_width = 256;
+    } else {
+        sycl::detail::pi::assertion(
+            cuDeviceGetAttribute(&memory_bus_width,
+                                CU_DEVICE_ATTRIBUTE_GLOBAL_MEMORY_BUS_WIDTH,
+                                device->get()) == CUDA_SUCCESS);
     }
 
     uint64_t memory_bandwidth = uint64_t(memory_clock_khz) * memory_bus_width * 250;
