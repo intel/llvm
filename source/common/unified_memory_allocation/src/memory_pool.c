1a1a5c96bb0d8f42837aca1b1f507f8b333cbf14
Igor Chorazewicz <igor.chorazewicz@intel.com>
2023-03-01 22:10:08 +0000

    Add missing '*.c' to format_src
    and reformat all *.c files

--- source/common/unified_memory_allocation/src/memory_pool.c
+++ source/common/unified_memory_allocation/src/memory_pool.c
@@ -12,65 +12,65 @@
 #include <stdlib.h>
 
 struct uma_memory_pool_t {
-  struct uma_memory_pool_ops_t ops;
-  void *pool_priv;
+    struct uma_memory_pool_ops_t ops;
+    void *pool_priv;
 };
 
 enum uma_result_t umaPoolCreate(struct uma_memory_pool_ops_t *ops, void *params,
                                 uma_memory_pool_handle_t *hPool) {
-  uma_memory_pool_handle_t pool = malloc(sizeof(struct uma_memory_pool_t));
-  if (!pool) {
-    return UMA_RESULT_ERROR_OUT_OF_HOST_MEMORY;
-  }
+    uma_memory_pool_handle_t pool = malloc(sizeof(struct uma_memory_pool_t));
+    if (!pool) {
+        return UMA_RESULT_ERROR_OUT_OF_HOST_MEMORY;
+    }
 
-  assert(ops->version == UMA_VERSION_CURRENT);
+    assert(ops->version == UMA_VERSION_CURRENT);
 
-  pool->ops = *ops;
+    pool->ops = *ops;
 
-  void *pool_priv;
-  enum uma_result_t ret = ops->initialize(params, &pool_priv);
-  if (ret != UMA_RESULT_SUCCESS) {
-    return ret;
-  }
+    void *pool_priv;
+    enum uma_result_t ret = ops->initialize(params, &pool_priv);
+    if (ret != UMA_RESULT_SUCCESS) {
+        return ret;
+    }
 
-  pool->pool_priv = pool_priv;
+    pool->pool_priv = pool_priv;
 
-  *hPool = pool;
+    *hPool = pool;
 
-  return UMA_RESULT_SUCCESS;
+    return UMA_RESULT_SUCCESS;
 }
 
 void umaPoolDestroy(uma_memory_pool_handle_t hPool) {
-  hPool->ops.finalize(hPool->pool_priv);
-  free(hPool);
+    hPool->ops.finalize(hPool->pool_priv);
+    free(hPool);
 }
 
 void *umaPoolMalloc(uma_memory_pool_handle_t hPool, size_t size) {
-  return hPool->ops.malloc(hPool->pool_priv, size);
+    return hPool->ops.malloc(hPool->pool_priv, size);
 }
 
 void *umaPoolAlignedMalloc(uma_memory_pool_handle_t hPool, size_t size,
                            size_t alignment) {
-  return hPool->ops.aligned_malloc(hPool->pool_priv, size, alignment);
+    return hPool->ops.aligned_malloc(hPool->pool_priv, size, alignment);
 }
 
 void *umaPoolCalloc(uma_memory_pool_handle_t hPool, size_t num, size_t size) {
-  return hPool->ops.calloc(hPool->pool_priv, num, size);
+    return hPool->ops.calloc(hPool->pool_priv, num, size);
 }
 
 void *umaPoolRealloc(uma_memory_pool_handle_t hPool, void *ptr, size_t size) {
-  return hPool->ops.realloc(hPool->pool_priv, ptr, size);
+    return hPool->ops.realloc(hPool->pool_priv, ptr, size);
 }
 
 size_t umaPoolMallocUsableSize(uma_memory_pool_handle_t hPool, void *ptr) {
-  return hPool->ops.malloc_usable_size(hPool->pool_priv, ptr);
+    return hPool->ops.malloc_usable_size(hPool->pool_priv, ptr);
 }
 
 void umaPoolFree(uma_memory_pool_handle_t hPool, void *ptr) {
-  hPool->ops.free(hPool->pool_priv, ptr);
+    hPool->ops.free(hPool->pool_priv, ptr);
 }
 
 enum uma_result_t
-umaPoolGetLastResult(uma_memory_pool_handle_t hPool, const char** ppMessage) {
-  return hPool->ops.get_last_result(hPool->pool_priv, ppMessage);
+umaPoolGetLastResult(uma_memory_pool_handle_t hPool, const char **ppMessage) {
+    return hPool->ops.get_last_result(hPool->pool_priv, ppMessage);
 }
