4b23429fe9618e4691ce94b5dc040d78d72f893d
Andrei Elovikov <andrei.elovikov@intel.com>
2023-03-08 10:15:00 -0800

    WIP2

--- sycl/include/sycl/ext/oneapi/experimental/group_load_store.hpp
+++ sycl/include/sycl/ext/oneapi/experimental/group_load_store.hpp
@@ -179,6 +179,7 @@ template <typename Group, typename InputIteratorT, typename OutputT, int N,
               OutputT>>>
 void group_load(Group g, InputIteratorT in_ptr, sycl::vec<OutputT, N> &out,
                 Properties properties = {}) {
+#ifdef __SYCL_DEVICE_ONLY__
   // TODO: Consider delegating to sycl::span version via sycl::bit_cast.
   constexpr bool blocked = detail::is_blocked(properties);
   auto generic = [&]() {
@@ -187,7 +188,105 @@ void group_load(Group g, InputIteratorT in_ptr, sycl::vec<OutputT, N> &out,
     });
   };
 
-  return generic();
+  using value_type = std::iterator_traits<InputIteratorT>::value_type;
+  using input_iter_no_cv = std::remove_cv_t<InputIteratorT>;
+
+  constexpr auto size = sizeof(value_type);
+  constexpr bool supported_size =
+      size == 1 || size == 2 || size == 4 || size == 8;
+  constexpr bool unsupported_vec_size = (N == 3 || (N == 16 && size != 1));
+
+  bool is_aligned = [&]() {
+    // TODO: Can we assume that the data is aligned as `alignof(decltype(out))`?
+    constexpr int reqd_read_align = 4; // bytes.
+    if constexpr (alignof(value_type) >= reqd_read_align)
+      return true;
+    else
+      return (reinterpret_cast<uintptr_t>(in_ptr) % reqd_read_align) != 0;
+  }();
+
+  if constexpr (!detail::is_spir || !supported_size || unsupported_vec_size) {
+    // TODO: Implement "unsupported_vec_size" by multiple loads followed by
+    // shuffles?
+    return generic();
+  } else if constexpr (detail::is_multi_ptr_v<InputIteratorT>) {
+    return group_load(g, in_ptr.get_decorated(), out, properties);
+  } else if constexpr (!std::is_pointer_v<input_iter_no_cv>) {
+    return generic();
+  } else {
+    // Pointer.
+    if constexpr (std::is_same_v<Group, sub_group>) {
+      constexpr auto AS = sycl::detail::deduce_AS<input_iter_no_cv>::value;
+      if constexpr (AS == access::address_space::global_space) {
+        if (!is_aligned)
+          // Not properly aligned.
+          return generic();
+
+        using BlockT = sycl::detail::sub_group::SelectBlockT<value_type>;
+        using VecT = sycl::detail::ConvertToOpenCLType_t<vec<BlockT, N>>;
+        using PtrT = sycl::detail::DecoratedType<BlockT, AS>::type *;
+
+        VecT load = __spirv_SubgroupBlockReadINTEL<VecT>(
+            reinterpret_cast<PtrT>(in_ptr));
+        out = sycl::bit_cast<vec<remove_decoration_t<value_type>, N>>(load);
+        // SPIR-V builtin assumes striped layout.
+        if constexpr (blocked) {
+          // clang-format off
+          // Data         |  0  1  2 |  3  4  5 |  6  7  8 |  9 10 11 | 12 13 14 | 15 16 17 | 18 19 20 | 21 22 23 |
+          //
+          // Was loaded as
+          // vec_idx\WI   |  0 |  1 |  2 |  3 |  4 |  5 |  6 |  7
+          //              |  8 |  9 | 10 | 11 | 12 | 13 | 14 | 15
+          //              | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23
+          //
+          // Need to shuffle as
+          //                  0        1       2       3      4       5       6       7
+          //               v[0](0)  v[0](3) v[0](6) v[1](1) v[1](4) v[1](7) v[2](2) v[2](5)
+          //               v[0](1)  v[0](4) v[0](7) v[1](2) v[1](5) v[2](0) v[2](3) v[2](6)
+          //               v[0](2)  v[0](5) v[1](0) v[1](3) v[1](6) v[2](1) v[2](4) v[2](7)
+          // clang-format on
+          vec<OutputT, N> shuffled;
+          auto lid = g.get_local_id();
+          int sg_size = g.get_local_range().size();
+          sycl::detail::dim_loop<N>([&](size_t i) {
+            shuffled[i] = select_from_group(g, out[(lid * N + i) / sg_size],
+                                            (lid * N + i) % sg_size);
+          });
+          out = shuffled;
+        }
+        return;
+      } else if constexpr (AS == access::address_space::generic_space) {
+        if (!is_aligned)
+          // Not properly aligned.
+          return generic();
+
+        if (auto global_ptr = __SYCL_GenericCastToPtrExplicit_ToGlobal<
+                remove_decoration_t<value_type>>(in_ptr))
+          return group_load(g, global_ptr, out, properties);
+
+        return generic();
+      } else {
+        return generic();
+      }
+    } else {
+        return generic();
+      // TODO: Use get_child_group from sycl_ext_oneapi_root_group extension
+      // once it is implemented instead of this free function.
+      auto ndi =
+          sycl::ext::oneapi::experimental::this_nd_item<Group::dimensions>();
+      auto sg = ndi.get_sub_group();
+      // TODO: Do we have guarantees that all SGs are of the same size? Should
+      // get_max_local_range be used instead?
+      return group_load(sg,
+                        in_ptr + sg.get_group_id() * sg.get_local_range() * N,
+                        out, properties);
+    }
+  }
+#else
+  throw sycl::exception(
+      std::error_code(PI_ERROR_INVALID_DEVICE, sycl::sycl_category()),
+      "Group loads/stoes are not supported on host.");
+#endif
 }
 
 // Store API sycl::vec overload
