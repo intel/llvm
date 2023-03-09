328f5fcedd2e44b41a6bf7290087157647fb244c
Tsang, Whitney <whitney.tsang@intel.com>
2023-03-09 17:43:17 -0500

    Revert "[MLIR] Remove LLVM typed pointer support"
    This reverts commit 02f9cf9266c35795aa44ded0c37f0c44d8c49069.

--- mlir/lib/Target/LLVMIR/TypeFromLLVM.cpp
+++ mlir/lib/Target/LLVMIR/TypeFromLLVM.cpp
@@ -97,7 +97,12 @@ private:
 
   /// Translates the given pointer type.
   Type translate(llvm::PointerType *type) {
-    return LLVM::LLVMPointerType::get(&context, type->getAddressSpace());
+    if (type->isOpaque())
+      return LLVM::LLVMPointerType::get(&context, type->getAddressSpace());
+
+    return LLVM::LLVMPointerType::get(
+        translateType(type->getNonOpaquePointerElementType()),
+        type->getAddressSpace());
   }
 
   /// Translates the given structure type.
