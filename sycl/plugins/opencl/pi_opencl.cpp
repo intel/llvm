46e315a6f37cc683e831e4e06234a33c2ac8bd71
Maronas, Marcos <marcos.maronas@intel.com>
2023-03-09 02:27:17 -0800

    Reverts involuntary change.
    Signed-off-by: Maronas, Marcos <marcos.maronas@intel.com>

--- sycl/plugins/opencl/pi_opencl.cpp
+++ sycl/plugins/opencl/pi_opencl.cpp
@@ -1,4 +1,4 @@
-//==---------- pi_ope
+//==---------- pi_opencl.cpp - OpenCL Plugin -------------------------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
