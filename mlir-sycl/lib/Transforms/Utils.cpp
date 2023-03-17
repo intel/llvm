55eff95f1c2d9936f489016dc715538fefe68f94
Victor Perez <victor.perez@codeplay.com>
2023-03-17 16:00:42 +0000

    Add copyright

--- mlir-sycl/lib/Transforms/Utils.cpp
+++ mlir-sycl/lib/Transforms/Utils.cpp
@@ -1,3 +1,11 @@
+//===- Utils.cpp - SYCL dialect transform utils -------------*- C++ -*-----===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "Utils.h"
 
 #include "mlir/Dialect/SYCL/IR/SYCLOps.h"
