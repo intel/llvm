057528abda9abe7424bc62cee9d3d92651672295
m moadeli <mahmoud.moadeli@codeplay.com>
2023-03-21 12:34:21 +0000

    Add test for get_pointer and reverts earlier change to get_multi_ptr

--- sycl/test/basic_tests/accessor/accessor_multi_ptr.cpp
+++ sycl/test/basic_tests/accessor/accessor_multi_ptr.cpp
@@ -10,27 +10,18 @@ constexpr static int size = 1;
 
 void test_get_multi_ptr(handler &cgh, buffer<int, size> &buffer) {
   using accessor_t =
-      accessor<int, size, access::mode::read_write, access::target::host_task,
+      accessor<int, size, access::mode::read_write, access::target::device,
                access::placeholder::true_t>;
   using local_accessor_t = local_accessor<int, size>;
 
-  auto ptr = buffer.get_host_access(cgh);
+  auto ptr = buffer.get_access<access_mode::read_write, target::device>(cgh);
   auto local_ptr = local_accessor<int, size>({size}, cgh);
-  auto acc_ptr = ptr.get_pointer();
   auto acc_multi_ptr = ptr.get_multi_ptr<access::decorated::yes>();
   auto local_acc_multi_ptr = local_ptr.get_multi_ptr<access::decorated::yes>();
-  auto local_acc_ptr = local_ptr.get_pointer();
-
-  static_assert(std::is_same_v<decltype(acc_ptr), std::add_pointer_t<int>>);
-
-  static_assert(
-      std::is_same_v<decltype(local_acc_ptr), std::add_pointer_t<int>>);
-
   static_assert(
       std::is_same_v<
           decltype(acc_multi_ptr),
           typename accessor_t::template accessor_ptr<access::decorated::yes>>);
-
   static_assert(std::is_same_v<decltype(local_acc_multi_ptr),
                                typename local_accessor_t::template accessor_ptr<
                                    access::decorated::yes>>);
