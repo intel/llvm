8733950734ead0d3beeb07d102b2eb0286b9e099
Larsen, Steffen <steffen.larsen@intel.com>
2023-03-22 12:25:39 -0700

    Switch to no-sycl-early-optimizations to avoid unrecognized -O0
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- sycl/test/optional_kernel_features/relaxed_fp64_propagation.cpp
+++ sycl/test/optional_kernel_features/relaxed_fp64_propagation.cpp
@@ -1,6 +1,6 @@
 // RUN: %clangxx %s -S -o %t_opt.ll -fsycl-device-only -Xclang -verify -Xclang -verify-ignore-unexpected=note
 // RUN: FileCheck %s --input-file %t_opt.ll --check-prefix=CHECK-OPT
-// RUN: %clangxx %s -S -O0 -o %t_noopt.ll -fsycl-device-only -Xclang -verify -Xclang -verify-ignore-unexpected=note
+// RUN: %clangxx %s -S -fno-sycl-early-optimizations -o %t_noopt.ll -fsycl-device-only -Xclang -verify -Xclang -verify-ignore-unexpected=note
 // RUN: FileCheck %s --input-file %t_noopt.ll --check-prefix=CHECK-NOOPT
 
 // Tests that an optimization that removes the use of double still produces a
