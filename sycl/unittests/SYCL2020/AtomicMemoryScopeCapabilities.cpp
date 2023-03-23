3cc55ded2d7f3a98c465c67e035845bb8829abfb
Marcos Maronas <maarquitos14@users.noreply.github.com>
2023-03-23 13:35:57 +0000

    [SYCL] Fixes AtomicMemoryScopeCapabilities unittest issue in Windows. (#8749)
    Signed-off-by: Maronas, Marcos <marcos.maronas@intel.com>

--- sycl/unittests/SYCL2020/AtomicMemoryScopeCapabilities.cpp
+++ sycl/unittests/SYCL2020/AtomicMemoryScopeCapabilities.cpp
@@ -16,8 +16,6 @@ namespace {
 
 thread_local bool deviceGetInfoCalled;
 
-pi_platform PiPlatform = nullptr;
-
 pi_result redefinedDeviceGetInfoAfter(pi_device device,
                                       pi_device_info param_name,
                                       size_t param_value_size,
@@ -39,10 +37,7 @@ pi_result redefinedDeviceGetInfoAfter(pi_device device,
 TEST(AtomicMemoryScopeCapabilitiesCheck, CheckAtomicMemoryScopeCapabilities) {
   sycl::unittest::PiMock Mock;
   sycl::platform Plt = Mock.getPlatform();
-
-  PiPlatform = detail::getSyclObjImpl(Plt)->getHandleRef();
-  context DefaultCtx = Plt.ext_oneapi_get_default_context();
-  device Dev = DefaultCtx.get_devices()[0];
+  device Dev = Plt.get_devices()[0];
 
   deviceGetInfoCalled = false;
 
