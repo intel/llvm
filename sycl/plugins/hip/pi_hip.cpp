45dd7620db9febe569ecd8d22d93e75c06fdaf0c
Maronas, Marcos <marcos.maronas@intel.com>
2023-03-23 07:33:44 -0700

    Addresses code review comments.
    Signed-off-by: Maronas, Marcos <marcos.maronas@intel.com>

--- sycl/plugins/hip/pi_hip.cpp
+++ sycl/plugins/hip/pi_hip.cpp
@@ -1878,7 +1878,7 @@ pi_result hip_piDeviceGetInfo(pi_device device, pi_device_info param_name,
   case PI_DEVICE_INFO_GPU_HW_THREADS_PER_EU:
   case PI_DEVICE_INFO_MAX_MEM_BANDWIDTH:
   case PI_EXT_ONEAPI_DEVICE_INFO_BFLOAT16_MATH_FUNCTIONS:
-    setErrorMessage("This backend does not support this query",
+    setErrorMessage("HIP backend does not support this query",
                     PI_ERROR_INVALID_ARG_VALUE);
     return PI_ERROR_PLUGIN_SPECIFIC_ERROR;
 
