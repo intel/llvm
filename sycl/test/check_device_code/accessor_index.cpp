b3f2a0b0846985454fc59cec52ac4b66853fd74a
Sergey Semenov <sergey.semenov@intel.com>
2023-03-23 10:56:50 -0700

    Expand test checks

--- sycl/test/check_device_code/accessor_index.cpp
+++ sycl/test/check_device_code/accessor_index.cpp
@@ -1,8 +1,9 @@
-// RUN: %clangxx -fsycl-device-only -fno-sycl-early-optimizations -S -emit-llvm -o - %s | FileCheck %s
+// RUN: %clangxx -fsycl-device-only -fno-sycl-early-optimizations -S -emit-llvm -D__SYCL_DISABLE_PARALLEL_FOR_RANGE_ROUNDING__ -o - %s | FileCheck %s
 #include <sycl/sycl.hpp>
 
 // Check that accessor index calculation is unrolled in headers.
 // CHECK-NOT: llvm.loop
+// CHECK-NOT: br i1
 using namespace sycl;
 int main() {
   queue Q;
