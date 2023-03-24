f7a7b69ba35401af4735ae357927d1344a574e4d
Georgi Mirazchiyski <georgimweb@gmail.com>
2023-03-24 19:01:25 +0000

    [SYCL][CUDA] Re-enable subgroup shuffle_fp64 test for CUDA (intel/llvm-test-suite#1663)

--- SYCL/SubGroup/shuffle_fp64.cpp
+++ SYCL/SubGroup/shuffle_fp64.cpp
@@ -3,10 +3,6 @@
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 // RUN: %ACC_RUN_PLACEHOLDER %t.out
 //
-// Temporarily disabled for CUDA due to failure reported in
-// https://github.com/intel/llvm/issues/8516
-// UNSUPPORTED: cuda
-//
 //==------- shuffle_fp64.cpp - SYCL sub_group shuffle test -----*- C++ -*---==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
