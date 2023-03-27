6eeda21c018396a8fac03a8322fb83708b0951e4
Patryk Kaminski <patryk.kaminski@intel.com>
2023-03-27 13:58:54 +0000

    Add logger test for nested path

--- test/unit/logger/logger.cpp
+++ test/unit/logger/logger.cpp
@@ -24,12 +24,12 @@ TEST_F(LoggerFromEnvVar, BasicMessage) {
 }
 
 //////////////////////////////////////////////////////////////////////////////
-TEST_F(LoggerWithFileSink, DefaultLevelNoOutput) {
+TEST_F(DefaultLoggerWithFileSink, DefaultLevelNoOutput) {
     logger->info("This should not be printed: {}", 42);
     test_msg.clear();
 }
 
-TEST_F(LoggerWithFileSink, MultipleLines) {
+TEST_F(DefaultLoggerWithFileSink, MultipleLines) {
     logger->warning("Test message: {}", "success");
     logger->debug("This should not be printed: {}", 42);
     logger->error("Test message: {}", "success");
@@ -38,31 +38,68 @@ TEST_F(LoggerWithFileSink, MultipleLines) {
                 "<test>[ERROR]: Test message: success\n";
 }
 
-TEST_F(LoggerWithFileSink, ThreeParams) {
+TEST_F(DefaultLoggerWithFileSink, ThreeParams) {
     logger->error("{} {}: {}", "Test", 42, 3.8);
     test_msg += "[ERROR]: Test 42: 3.8\n";
 }
 
-TEST_F(LoggerWithFileSink, DoubleBraces) {
+TEST_F(DefaultLoggerWithFileSink, DoubleBraces) {
     logger->error("{{}} {}: {}", "Test", 42);
     test_msg += "[ERROR]: {} Test: 42\n";
 }
 
-TEST_F(LoggerWithFileSink, DoubleBraces2) {
+TEST_F(DefaultLoggerWithFileSink, DoubleBraces2) {
     logger->error("200 {{ {}: {{{}}} 3.8", "Test", 42);
     test_msg += "[ERROR]: 200 { Test: {42} 3.8\n";
 }
 
-TEST_F(LoggerWithFileSink, DoubleBraces3) {
+TEST_F(DefaultLoggerWithFileSink, DoubleBraces3) {
     logger->error("{{ {}:}} {}}}", "Test", 42);
     test_msg += "[ERROR]: { Test:} 42}\n";
 }
 
-TEST_F(LoggerWithFileSink, NoBraces) {
+TEST_F(DefaultLoggerWithFileSink, NoBraces) {
     logger->error(" Test: 42");
     test_msg += "[ERROR]:  Test: 42\n";
 }
 
+TEST_F(DefaultLoggerWithFileSink, SetLevelDebug) {
+    auto level = logger::Level::DEBUG;
+    logger->setLevel(level);
+    logger->setFlushLevel(level);
+    logger->debug("Test message: {}", "success");
+
+    test_msg += "[DEBUG]: Test message: success\n";
+}
+
+TEST_F(DefaultLoggerWithFileSink, SetLevelInfo) {
+    auto level = logger::Level::INFO;
+    logger->setLevel(level);
+    logger->setFlushLevel(level);
+    logger->info("Test message: {}", "success");
+    logger->debug("This should not be printed: {}", 42);
+
+    test_msg += "[INFO]: Test message: success\n";
+}
+
+TEST_F(DefaultLoggerWithFileSink, SetLevelWarning) {
+    auto level = logger::Level::WARN;
+    logger->setLevel(level);
+    logger->warning("Test message: {}", "success");
+    logger->info("This should not be printed: {}", 42);
+
+    test_msg += "[WARNING]: Test message: success\n";
+}
+
+TEST_F(DefaultLoggerWithFileSink, SetLevelError) {
+    logger->setLevel(logger::Level::ERR);
+    logger->error("Test message: {}", "success");
+    logger->warning("This should not be printed: {}", 42);
+
+    test_msg += "[ERROR]: Test message: success\n";
+}
+
+//////////////////////////////////////////////////////////////////////////////
 TEST_F(LoggerWithFileSink, SetLogLevelAndFlushLevelDebugWithCtor) {
     auto level = logger::Level::DEBUG;
     logger = std::make_unique<logger::Logger>(
@@ -73,38 +110,18 @@ TEST_F(LoggerWithFileSink, SetLogLevelAndFlushLevelDebugWithCtor) {
     test_msg += "[DEBUG]: Test message: success\n";
 }
 
-TEST_F(LoggerWithFileSink, SetLevelDebug) {
-    auto level = logger::Level::DEBUG;
-    logger->setLevel(level);
-    logger->setFlushLevel(level);
-    logger->debug("Test message: {}", "success");
+TEST_F(LoggerWithFileSink, NestedFilePath) {
+    auto dir_name = "tmp_dir";
+    file_path = dir_name;
+    for (int i = 0; i < 20; ++i) {
+        file_path /= dir_name;
+    }
+    std::filesystem::create_directories(file_path);
+    file_path /= file_name;
+    logger = std::make_unique<logger::Logger>(
+        logger::Level::WARN,
+        std::make_unique<logger::FileSink>(logger_name, file_path));
 
-    test_msg += "[DEBUG]: Test message: success\n";
-}
-
-TEST_F(LoggerWithFileSink, SetLevelInfo) {
-    auto level = logger::Level::INFO;
-    logger->setLevel(level);
-    logger->setFlushLevel(level);
-    logger->info("Test message: {}", "success");
-    logger->debug("This should not be printed: {}", 42);
-
-    test_msg += "[INFO]: Test message: success\n";
-}
-
-TEST_F(LoggerWithFileSink, SetLevelWarning) {
-    auto level = logger::Level::WARN;
-    logger->setLevel(level);
     logger->warning("Test message: {}", "success");
-    logger->info("This should not be printed: {}", 42);
-
     test_msg += "[WARNING]: Test message: success\n";
-}
-
-TEST_F(LoggerWithFileSink, SetLevelError) {
-    logger->setLevel(logger::Level::ERR);
-    logger->error("Test message: {}", "success");
-    logger->warning("This should not be printed: {}", 42);
-
-    test_msg += "[ERROR]: Test message: success\n";
 }
