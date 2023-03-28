9cc7c7e3a23bb38a25be78edeb2c21df4739c944
Patryk Kaminski <patryk.kaminski@intel.com>
2023-03-28 12:26:46 +0000

    Add global logger object tests
    Add tests for validation of the global logger object setup from
    an environment variable.

--- cmake/match.cmake
+++ cmake/match.cmake
@@ -8,15 +8,25 @@
 
 find_package(Python3 COMPONENTS Interpreter)
 
-if(NOT DEFINED TEST_FILE)
-    message(FATAL_ERROR "TEST_FILE needs to be defined")
+# Process variables passed to the script
+if(NOT DEFINED MODE)
+    message(FATAL_ERROR "MODE needs to be defined. Possible values: 'stdout', 'stderr', 'file'")
+elseif(${MODE} STREQUAL "stdout" OR ${MODE} STREQUAL "stderr")
+    if(NOT DEFINED TEST_FILE)
+        message(FATAL_ERROR "TEST_FILE needs to be defined with a path to a binary to run")
+    else()
+        set(OUT_FILE "_matchtmpfile")
+    endif()
+elseif(${MODE} STREQUAL "file")
+    if(NOT DEFINED OUT_FILE)
+        message(FATAL_ERROR "OUT_FILE needs to be defined with an output file to be verified")
+    endif()
+else()
+    message(FATAL_ERROR "${MODE} mode not recognised. Possible values: 'stdout', 'stderr', 'file'")
 endif()
 if(NOT DEFINED MATCH_FILE)
     message(FATAL_ERROR "MATCH_FILE needs to be defined")
 endif()
-
-set(TEST_OUT "_matchtmpfile")
-
 if(NOT DEFINED TEST_ARGS) # easier than ifdefing the rest of the code
     set(TEST_ARGS "")
 endif()
@@ -24,23 +34,48 @@ endif()
 string(REPLACE "\"" "" TEST_ARGS "${TEST_ARGS}")
 separate_arguments(TEST_ARGS)
 
-execute_process(
-    COMMAND ${TEST_FILE} ${TEST_ARGS}
-    OUTPUT_FILE ${TEST_OUT}
-    RESULT_VARIABLE TEST_RESULT
-)
+if(EXISTS ${OUT_FILE})
+    file(REMOVE ${OUT_FILE})
+endif()
+
+# Run the test binary. Capture the output to the temporary file.
+if(${MODE} STREQUAL "stdout")
+    execute_process(
+        COMMAND ${TEST_FILE} ${TEST_ARGS}
+        OUTPUT_FILE ${OUT_FILE}
+        RESULT_VARIABLE TEST_RESULT
+    )
+elseif(${MODE} STREQUAL "stderr")
+    execute_process(
+        COMMAND ${TEST_FILE} ${TEST_ARGS}
+        ERROR_FILE ${OUT_FILE}
+        RESULT_VARIABLE TEST_RESULT
+    )
+elseif(${MODE} STREQUAL "file")
+    execute_process(
+        COMMAND ${TEST_FILE}
+        RESULT_VARIABLE TEST_RESULT
+    )
+endif()
 
 if(TEST_RESULT)
     message(FATAL_ERROR "Failed: Test ${TEST_FILE} ${TEST_ARGS} returned non-zero (${TEST_RESULT}).")
 endif()
 
+# Compare the output file contents with a match file contents
 execute_process(
-    COMMAND ${Python3_EXECUTABLE} ${CMAKE_CURRENT_LIST_DIR}/match.py ${TEST_OUT} ${MATCH_FILE}
+    COMMAND ${Python3_EXECUTABLE} ${CMAKE_CURRENT_LIST_DIR}/match.py ${OUT_FILE} ${MATCH_FILE}
     RESULT_VARIABLE TEST_RESULT
 )
 
 if(TEST_RESULT)
-    message(FATAL_ERROR "Failed: The output of ${TEST_FILE} (stored in ${TEST_OUT}) does not match ${MATCH_FILE} (${TEST_RESULT})")
+    file(READ ${OUT_FILE} OUTPUT)
+    file(READ ${MATCH_FILE} MATCH_STRING)
+    message(FATAL_ERROR "Failed: The output of ${OUT_FILE}:\n  ${OUTPUT}\n does not match ${MATCH_FILE}:\n ${MATCH_STRING} (${TEST_RESULT})")
 elseif()
-    message("Passed: The output of ${TEST_FILE} matches ${MATCH_FILE}")
+    message("Passed: The output ${OUT_FILE} matches ${MATCH_FILE}")
+endif()
+
+if(EXISTS ${OUT_FILE})
+    file(REMOVE ${OUT_FILE})
 endif()
