61f79fb6e365028dc9f5faffee39dbb18b4553ac
Abhishek Bagusetty <abagusetty@anl.gov>
2023-03-28 20:01:46 -0400

    split the tests

--- sycl/test-e2e/Basic/intel-ext-device.cpp
+++ sycl/test-e2e/Basic/intel-ext-device.cpp
@@ -1,10 +1,7 @@
 // RUN: %clangxx -fsycl %s -o %t.out
-// RUN: env ONEAPI_DEVICE_SELECTOR=level_zero:gpu %t.out
-// RUN: env ONEAPI_DEVICE_SELECTOR=opencl:gpu %t.out
+// REQUIRES: level_zero || opencl
+// RUN: %GPU_RUN_PLACEHOLDER %t.out
 //
-// REQUIRES: gpu
-// Temporarily disable on L0 due to fails in CI
-
 //==--------- intel-ext-device.cpp - SYCL device test ------------==//
 //
 // Returns the low-level device details.  These are Intel-specific extensions
@@ -134,37 +131,6 @@ int main(int argc, char **argv) {
           }
         } // SYCL_EXT_INTEL_DEVICE_INFO
       }
-
-// Check if this experimental feature is supported
-#ifdef SYCL_EXT_ONEAPI_MAX_WORK_GROUP_QUERY
-      sycl::id<1> groupD = dev.get_info<
-          sycl::ext::oneapi::experimental::info::device::max_work_groups<1>>();
-      std::cout << "Max work group size in 1D \n";
-      std::cout << "Dimension 1:" << groupD[0] << std::endl;
-
-      sycl::id<2> group2D = dev.get_info<
-          sycl::ext::oneapi::experimental::info::device::max_work_groups<2>>();
-      std::cout << "Max work group size in 2D \n";
-      std::cout << "Dimension 1:" << group2D[0] << "\n"
-                << "Dimension 2:" << group2D[1] << std::endl;
-
-      sycl::id<3> group3D = dev.get_info<
-          sycl::ext::oneapi::experimental::info::device::max_work_groups<3>>();
-      std::cout << "Max work group size in 3D \n";
-      std::cout << "Dimension 1:" << group3D[0] << "\n"
-                << "Dimension 2:" << group3D[1] << "\n"
-                << "Dimension 3:" << group3D[2] << std::endl;
-
-      size_t group_max = dev.get_info<sycl::ext::oneapi::experimental::info::
-                                          device::max_global_work_groups>();
-      std::cout << "Max global work group size:" << group_max << "\n";
-
-      assert((group3D[0] <= group_max && group3D[1] <= group_max &&
-              group3D[2] <= group_max) &&
-             "Max work-group size of each dimension must be smaller than "
-             "global work-group size");
-#endif
-
       std::cout << std::endl;
     }
   }
