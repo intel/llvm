b2a4a115ca31de05fbf6a54a7767c4a0b50a1ce1
John Pennycook <john.pennycook@intel.com>
2023-03-29 08:52:15 -0700

    Add basic tests for non-uniform groups
    Tests the ability to create an instance of each new group type,
    and the correctness of the core member functions.
    
    Signed-off-by: John Pennycook <john.pennycook@intel.com>

---
+++ sycl/test-e2e/NonUniformGroups/is_user_constructed.cpp
@@ -0,0 +1,14 @@
+// RUN: %clangxx -fsycl -fsyntax-only -fsycl-targets=%sycl_triple %s -o %t.out
+
+#include <sycl/sycl.hpp>
+namespace syclex = sycl::ext::oneapi::experimental;
+
+static_assert(
+    syclex::is_user_constructed_group_v<syclex::ballot_group<sycl::sub_group>>);
+static_assert(syclex::is_user_constructed_group_v<
+              syclex::cluster_group<1, sycl::sub_group>>);
+static_assert(syclex::is_user_constructed_group_v<
+              syclex::cluster_group<2, sycl::sub_group>>);
+static_assert(
+    syclex::is_user_constructed_group_v<syclex::tangle_group<sycl::sub_group>>);
+static_assert(syclex::is_user_constructed_group_v<syclex::opportunistic_group>);
