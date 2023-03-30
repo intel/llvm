intel/llvm

commit 992ef064289fd5f436eaf7d448fc2b6d54a647bc

[SYCL] Host pipe runtime implementation (#7468)
Disclaimer:

This work is a continuation of a previous approved Sherry's PR:
https://github.com/intel/llvm/pull/5766 and her draft work
https://github.com/intel/llvm/pull/5894.

We are implementing Hostpipes based on the Spec
[here](https://github.com/rho180/llvm/blob/rho180-pipe-design/sycl/doc/design/HostPipes.md).
OpenCL spec is
[here](https://github.com/intel-sandbox/ip-authoring-specs/blob/MJ_ChangeDocs4/Pipe/Spec/cl_intel_host_pipe_symbol.asciidoc).

The following is the outline of the design.

1. The host pipe properties need to be added to the device image
(probably similar to the previous change here
https://github.com/intel/llvm/commit/a9ad3af71cbdd5ebc7b7efeb35761aa8ea1a49d7)

2.The frontend calls the [registration of device image, maybe similar to
this
code](https://github.com/intel/llvm/blob/af858c74e4be95b2163ce6ba545ce588ff0aca4c/sycl/source/detail/program_manager/program_manager.cpp#L2014):
which is where the host pipe information is available. This is where the
mapping from host pipe name to host pipe pointer is extracted.

3. The frontend also calls `host_pipe_map::add` to register/initialize
the host pipes. This is the new function. We are not sure about the
ordering of registration of device image / registration of host pipe.
for which ever one that comes later, it need to initialize the remaining
attribute of host pipe (such as its properties).

4. The opencl runtime will need to get a cl_program object, which is
typically not available until the first kernel launch. To get a program
object early on, the host pipe name/pointer to device image mapping is
cached during registration. And when the specific host pipe is needed,
build the program and get its ocl runtime representation. This is done
in the first couple commits.

5. Since a host pipe read/write need to depend on other write operation

sycl/source/detail/program_manager/program_manager.cpp
@@ -1177,7 +1177,6 @@ bool ProgramManager::kernelUsesAssert(OSModuleHandle M,
 void ProgramManager::addImages(pi_device_binaries DeviceBinary) {
   std::lock_guard<std::mutex> Guard(Sync::getGlobalLock());
   const bool DumpImages = std::getenv("SYCL_DUMP_IMAGES") && !m_UseSpvFile;
-
   for (int I = 0; I < DeviceBinary->NumDeviceBinaries; I++) {
     pi_device_binary RawImg = &(DeviceBinary->DeviceBinaries[I]);
     OSModuleHandle M = OSUtil::getOSModuleHandle(RawImg);
@@ -1317,6 +1316,38 @@ void ProgramManager::addImages(pi_device_binaries DeviceBinary) {
                 DeviceGlobal->Name, ImgId, KSId, TypeSize,
                 DeviceImageScopeDecorated);
             m_DeviceGlobals.emplace(DeviceGlobal->Name, std::move(EntryUPtr));
+          }
+        }
+      }
+      // ... and initialize associated host_pipe information
+      {
+        std::lock_guard HostPipesGuard(m_HostPipesMutex);
+        auto HostPipes = Img->getHostPipes();
+        for (const pi_device_binary_property &HostPipe : HostPipes) {
+          ByteArray HostPipeInfo = DeviceBinaryProperty(HostPipe).asByteArray();
+
+          // The supplied host_pipe info property is expected to contain:
+          // * 8 bytes - Size of the property.
+          // * 4 bytes - Size of the underlying type in the host_pipe.
+          // Note: Property may be padded.
+
+          HostPipeInfo.dropBytes(8);
+          auto TypeSize = HostPipeInfo.consume<std::uint32_t>();
+          assert(HostPipeInfo.empty() && "Extra data left!");
+
+          auto ExistingHostPipe = m_HostPipes.find(HostPipe->Name);
+          if (ExistingHostPipe != m_HostPipes.end()) {
+            // If it has already been registered we update the information.
+            ExistingHostPipe->second->initialize(TypeSize);
+            ExistingHostPipe->second->initialize(Img.get());
+          } else {
+            // If it has not already been registered we create a new entry.
+            // Note: Pointer to the host pipe is not available here, so it
+            //       cannot be set until registration happens.
+            auto EntryUPtr =
+                std::make_unique<HostPipeMapEntry>(HostPipe->Name, TypeSize);
+            EntryUPtr->initialize(Img.get());
+            m_HostPipes.emplace(HostPipe->Name, std::move(EntryUPtr));
           }
         }
       }
@@ -1641,6 +1672,37 @@ std::vector<DeviceGlobalMapEntry *> ProgramManager::getDeviceGlobalEntries(
       FoundEntries.push_back(DeviceGlobalEntry->second.get());
   }
   return FoundEntries;
+}
+
+void ProgramManager::addOrInitHostPipeEntry(const void *HostPipePtr,
+                                            const char *UniqueId) {
+  std::lock_guard<std::mutex> HostPipesGuard(m_HostPipesMutex);
+
+  auto ExistingHostPipe = m_HostPipes.find(UniqueId);
+  if (ExistingHostPipe != m_HostPipes.end()) {
+    ExistingHostPipe->second->initialize(HostPipePtr);
+    m_Ptr2HostPipe.insert({HostPipePtr, ExistingHostPipe->second.get()});
+    return;
+  }
+
+  auto EntryUPtr = std::make_unique<HostPipeMapEntry>(UniqueId, HostPipePtr);
+  auto NewEntry = m_HostPipes.emplace(UniqueId, std::move(EntryUPtr));
+  m_Ptr2HostPipe.insert({HostPipePtr, NewEntry.first->second.get()});
+}
+
+HostPipeMapEntry *
+ProgramManager::getHostPipeEntry(const std::string &UniqueId) {
+  std::lock_guard<std::mutex> HostPipesGuard(m_HostPipesMutex);
+  auto Entry = m_HostPipes.find(UniqueId);
+  assert(Entry != m_HostPipes.end() && "Host pipe entry not found");
+  return Entry->second.get();
+}
+
+HostPipeMapEntry *ProgramManager::getHostPipeEntry(const void *HostPipePtr) {
+  std::lock_guard<std::mutex> HostPipesGuard(m_HostPipesMutex);
+  auto Entry = m_Ptr2HostPipe.find(HostPipePtr);
+  assert(Entry != m_Ptr2HostPipe.end() && "Host pipe entry not found");
+  return Entry->second;
 }
 
 device_image_plain ProgramManager::getDeviceImageFromBinaryImage(

buildbot/dependency.py
@@ -58,7 +58,7 @@ def do_dependency(args):
 
     # Checkout fixed version to avoid unexpected issues coming from upstream
     # Specific version can be uplifted as soon as such need arise
-    checkout_cmd = ["git", "checkout", "23710f1b99186065c1768fc3098ba681adc0f253"]
+    checkout_cmd = ["git", "checkout", "9ddb236e6eb3cf844f9e2f81677e1045f9bf838e"]
     subprocess.check_call(checkout_cmd, cwd=ocl_header_dir)
 
     # fetch and build OpenCL ICD loader

llvm/include/llvm/SYCLLowerIR/HostPipes.h
@@ -1,0 +1,59 @@
+//===------- HostPipes.h - get required info about FPGA Host Pipes --------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// The file contains a number of functions to extract corresponding attributes
+// of the host pipe global variables and save them as a property set for the
+// runtime.
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "llvm/ADT/MapVector.h"
+
+#include <cstdint>
+#include <vector>
+
+namespace llvm {
+
+class GlobalVariable;
+class Module;
+class StringRef;
+
+// Represents a host pipe variable - at SYCL RT level host pipe
+// variables are being represented as a byte-array.
+struct HostPipeProperty {
+  HostPipeProperty(uint32_t Size) : Size(Size) {}
+
+  // Encodes size of the underlying type T of the host pipe variable.
+  uint32_t Size;
+};
+
+using HostPipePropertyMapTy =
+    MapVector<StringRef, std::vector<HostPipeProperty>>;
+
+/// Return \c true if the variable @GV is a host pipe variable.
+///
+/// The function checks whether the variable has the LLVM IR attribute \c
+/// sycl-host-pipe
+/// @param GV [in] A variable to test.
+///
+/// @return \c true if the variable is a host pipe variable, \c false
+/// otherwise.
+bool isHostPipeVariable(const GlobalVariable &GV);
+
+/// Searches given module for occurrences of host pipe variable-specific
+/// metadata and builds "host pipe variable name" ->
+/// vector<"variable properties"> map.
+///
+/// @param M [in] LLVM Module.
+///
+/// @returns the "host pipe variable name" -> vector<"variable properties">
+/// map.
+HostPipePropertyMapTy collectHostPipeProperties(const Module &M);
+
+} // end namespace llvm

llvm/include/llvm/SYCLLowerIR/SYCLUtils.h
@@ -22,7 +22,6 @@ namespace llvm {
 namespace sycl {
 namespace utils {
 constexpr char ATTR_SYCL_MODULE_ID[] = "sycl-module-id";
-constexpr StringRef SYCL_HOST_PIPE_ATTR = "sycl-host-pipe";
 
 using CallGraphNodeAction = ::std::function<void(Function *)>;
 using CallGraphFunctionFilter =
@@ -115,10 +114,6 @@ bool collectPossibleStoredVals(
 
 inline bool isSYCLExternalFunction(const Function *F) {
   return F->hasFnAttribute(ATTR_SYCL_MODULE_ID);
-}
-
-inline bool isHostPipeVariable(const GlobalVariable &GV) {
-  return GV.hasAttribute(SYCL_HOST_PIPE_ATTR);
 }
 
 } // namespace utils

llvm/include/llvm/Support/PropertySetIO.h
@@ -197,6 +197,7 @@ public:
   static constexpr char SYCL_EXPORTED_SYMBOLS[] = "SYCL/exported symbols";
   static constexpr char SYCL_DEVICE_GLOBALS[] = "SYCL/device globals";
   static constexpr char SYCL_DEVICE_REQUIREMENTS[] = "SYCL/device requirements";
+  static constexpr char SYCL_HOST_PIPES[] = "SYCL/host pipes";
 
   // Function for bulk addition of an entire property set under given category
   // (property set name).

llvm/lib/SYCLLowerIR/CMakeLists.txt
@@ -57,6 +57,7 @@ add_llvm_component_library(LLVMSYCLLowerIR
   ESIMD/LowerESIMDVecArg.cpp
   ESIMD/LowerESIMDVLoadVStore.cpp
   ESIMD/LowerESIMDSlmReservation.cpp
+  HostPipes.cpp
   LowerInvokeSimd.cpp
   LowerKernelProps.cpp
   LowerWGLocalMemory.cpp

llvm/lib/SYCLLowerIR/CompileTimePropertiesPass.cpp
@@ -10,7 +10,7 @@
 
 #include "llvm/SYCLLowerIR/CompileTimePropertiesPass.h"
 #include "llvm/SYCLLowerIR/DeviceGlobals.h"
-#include "llvm/SYCLLowerIR/SYCLUtils.h"
+#include "llvm/SYCLLowerIR/HostPipes.h"
 
 #include "llvm/ADT/APInt.h"
 #include "llvm/ADT/StringMap.h"
@@ -343,7 +343,7 @@ PreservedAnalyses CompileTimePropertiesPass::run(Module &M,
                                               HostAccessDecorValue, VarName));
     }
 
-    if (sycl::utils::isHostPipeVariable(GV)) {
+    if (isHostPipeVariable(GV)) {
       auto VarName = getGlobalVariableUniqueId(GV);
       MDOps.push_back(buildSpirvDecorMetadata(Ctx, SPIRV_HOST_ACCESS_DECOR,
                                               SPIRV_HOST_ACCESS_DEFAULT_VALUE, 

llvm/lib/SYCLLowerIR/DeviceGlobals.cpp
@@ -72,19 +72,19 @@ bool hasDeviceImageScopeProperty(const GlobalVariable &GV) {
   return hasProperty(GV, SYCL_DEVICE_IMAGE_SCOPE_ATTR);
 }
 
-/// Returns the unique id for the device global variable.
+/// Returns the unique id for the device global or host pipe variable.
 ///
 /// The function gets this value from the LLVM IR attribute \c
 /// sycl-unique-id.
 ///
-/// @param GV [in] Device Global variable.
+/// @param GV [in] Device Global or Hostpipe variable.
 ///
-/// @returns the unique id of the device global variable represented
-/// in the LLVM IR by \c GV.
+/// @returns the unique id of the device global or hostpipe variable
+/// represented in the LLVM IR by \c GV.
 StringRef getGlobalVariableUniqueId(const GlobalVariable &GV) {
   assert(GV.hasAttribute(SYCL_UNIQUE_ID_ATTR) &&
          "a 'sycl-unique-id' string must be associated with every device "
-         "global variable");
+         "global or hostpipe variable");
   return GV.getAttribute(SYCL_UNIQUE_ID_ATTR).getValueAsString();
 }
 

commit 33bf2a70a8f1fe3c14322fc5372a0ce725a6a51b

[SYCL] Fix CTAD-related issue with std::lock_guard (#8883)
This patch fixes post-commit failure introduced in
https://github.com/intel/llvm/pull/7468:

```
/Users/runner/work/llvm/llvm/src/sycl/source/detail/program_manager/program_manager.cpp:1324:9: error: 'lock_guard' may not intend to support class template argument deduction [-Werror,-Wctad-maybe-unsupported]
        std::lock_guard HostPipesGuard(m_HostPipesMutex);
        ^
```

sycl/source/detail/program_manager/program_manager.cpp
@@ -1321,7 +1321,7 @@ void ProgramManager::addImages(pi_device_binaries DeviceBinary) {
       }
       // ... and initialize associated host_pipe information
       {
-        std::lock_guard HostPipesGuard(m_HostPipesMutex);
+        std::lock_guard<std::mutex> HostPipesGuard(m_HostPipesMutex);
         auto HostPipes = Img->getHostPipes();
         for (const pi_device_binary_property &HostPipe : HostPipes) {
           ByteArray HostPipeInfo = DeviceBinaryProperty(HostPipe).asByteArray();