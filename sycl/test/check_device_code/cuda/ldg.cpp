897981d74319ee8ad33255469ef7f244e8ec580c
Jakub Chlanda <jakub@codeplay.com>
2023-03-31 12:07:39 +0100

    sign char, vec<sign char, 2>, vec<unsigned|long, 2|4>

--- sycl/test/check_device_code/cuda/ldg.cpp
+++ sycl/test/check_device_code/cuda/ldg.cpp
@@ -15,6 +15,7 @@ int main() {
   sycl::queue q;
 
   auto *in_c = sycl::malloc_device<char>(1, q);
+  auto *in_sc = sycl::malloc_device<signed char>(1, q);
   auto *in_s = sycl::malloc_device<short>(1, q);
   auto *in_i = sycl::malloc_device<int>(1, q);
   auto *in_l = sycl::malloc_device<long>(1, q);
@@ -26,39 +27,42 @@ int main() {
   auto *in_ul = sycl::malloc_device<unsigned long>(1, q);
   auto *in_ull = sycl::malloc_device<unsigned long long>(1, q);
 
-  auto *in_c2 = sycl::malloc_device<char2>(1, q);
-  auto *in_s2 = sycl::malloc_device<short2>(1, q);
-  auto *in_i2 = sycl::malloc_device<int2>(1, q);
-  auto *in_ll2 = sycl::malloc_device<longlong2>(1, q);
-  auto *in_ll4 = sycl::malloc_device<longlong4>(1, q);
+  auto *in_c2 = sycl::malloc_device<sycl::vec<char, 2>>(1, q);
+  auto *in_sc2 = sycl::malloc_device<sycl::vec<signed char, 2>>(1, q);
+  auto *in_s2 = sycl::malloc_device<sycl::vec<short, 2>>(1, q);
+  auto *in_i2 = sycl::malloc_device<sycl::vec<int, 2>>(1, q);
+  auto *in_l2 = sycl::malloc_device<sycl::vec<long, 2>>(1, q);
+  auto *in_ll2 = sycl::malloc_device<sycl::vec<long long, 2>>(1, q);
+  auto *in_l4 = sycl::malloc_device<sycl::vec<long, 4>>(1, q);
+  auto *in_ll4 = sycl::malloc_device<sycl::vec<long long, 4>>(1, q);
 
-  auto *in_c4 = sycl::malloc_device<char4>(1, q);
-  auto *in_s4 = sycl::malloc_device<short4>(1, q);
-  auto *in_i4 = sycl::malloc_device<int4>(1, q);
+  auto *in_c4 = sycl::malloc_device<sycl::vec<char, 4>>(1, q);
+  auto *in_sc4 = sycl::malloc_device<sycl::vec<signed char, 4>>(1, q);
+  auto *in_s4 = sycl::malloc_device<sycl::vec<short, 4>>(1, q);
+  auto *in_i4 = sycl::malloc_device<sycl::vec<int, 4>>(1, q);
 
-  auto *in_uc2 = sycl::malloc_device<uchar2>(1, q);
-  auto *in_us2 = sycl::malloc_device<ushort2>(1, q);
-  auto *in_ui2 = sycl::malloc_device<uint2>(1, q);
-  auto *in_ull2 = sycl::malloc_device<ulonglong2>(1, q);
-  auto *in_ull4 = sycl::malloc_device<ulonglong4>(1, q);
+  auto *in_uc2 = sycl::malloc_device<sycl::vec<unsigned char, 2>>(1, q);
+  auto *in_us2 = sycl::malloc_device<sycl::vec<unsigned short, 2>>(1, q);
+  auto *in_ui2 = sycl::malloc_device<sycl::vec<unsigned int, 2>>(1, q);
+  auto *in_ul2 = sycl::malloc_device<sycl::vec<unsigned long, 2>>(1, q);
+  auto *in_ull2 = sycl::malloc_device<sycl::vec<unsigned long long, 2>>(1, q);
+  auto *in_ul4 = sycl::malloc_device<sycl::vec<unsigned long, 4>>(1, q);
+  auto *in_ull4 = sycl::malloc_device<sycl::vec<unsigned long long, 4>>(1, q);
 
-  auto *in_uc4 = sycl::malloc_device<uchar4>(1, q);
-  auto *in_us4 = sycl::malloc_device<ushort4>(1, q);
-  auto *in_ui4 = sycl::malloc_device<uint4>(1, q);
+  auto *in_uc4 = sycl::malloc_device<sycl::vec<unsigned char, 4>>(1, q);
+  auto *in_us4 = sycl::malloc_device<sycl::vec<unsigned short, 4>>(1, q);
+  auto *in_ui4 = sycl::malloc_device<sycl::vec<unsigned int, 4>>(1, q);
 
   auto *in_h = sycl::malloc_device<half>(1, q);
   auto *in_f = sycl::malloc_device<float>(1, q);
   auto *in_d = sycl::malloc_device<double>(1, q);
 
-  auto *in_h2 = sycl::malloc_device<half2>(1, q);
-  auto *in_h4 = sycl::malloc_device<half4>(1, q);
-  auto *in_f2 = sycl::malloc_device<float2>(1, q);
-  auto *in_d2 = sycl::malloc_device<double2>(1, q);
-  auto *in_d4 = sycl::malloc_device<double4>(1, q);
-
-  auto *in_f4 = sycl::malloc_device<float4>(1, q);
-
-  auto *out_d = sycl::malloc_device<double>(1, q);
+  auto *in_h2 = sycl::malloc_device<sycl::vec<half, 2>>(1, q);
+  auto *in_h4 = sycl::malloc_device<sycl::vec<half, 4>>(1, q);
+  auto *in_f2 = sycl::malloc_device<sycl::vec<float, 2>>(1, q);
+  auto *in_f4 = sycl::malloc_device<sycl::vec<float, 4>>(1, q);
+  auto *in_d2 = sycl::malloc_device<sycl::vec<double, 2>>(1, q);
+  auto *in_d4 = sycl::malloc_device<sycl::vec<double, 4>>(1, q);
 
   q.wait();
 
@@ -103,6 +107,9 @@ int main() {
       //CHECK: tail call i8 @llvm.nvvm.ldg.global.i.i8.p0i8(i8* %{{.*}}, i32 1)
       //CHECK-OPAQUE: tail call i8 @llvm.nvvm.ldg.global.i.i8.p0(ptr %{{.*}}, i32 1)
       auto cached_c = ldg(&in_c[0]);
+      //CHECK: tail call i8 @llvm.nvvm.ldg.global.i.i8.p0i8(i8* %{{.*}}, i32 1)
+      //CHECK-OPAQUE: tail call i8 @llvm.nvvm.ldg.global.i.i8.p0(ptr %{{.*}}, i32 1)
+      auto cached_sc = ldg(&in_sc[0]);
       //CHECK: tail call i16 @llvm.nvvm.ldg.global.i.i16.p0i16(i16* %{{.*}}, i32 2)
       //CHECK-OPAQUE: tail call i16 @llvm.nvvm.ldg.global.i.i16.p0(ptr %{{.*}}, i32 2)
       auto cached_s = ldg(&in_s[0]);
@@ -134,15 +141,26 @@ int main() {
       //CHECK: tail call <2 x i8> @llvm.nvvm.ldg.global.i.v2i8.p0v2i8(<2 x i8>* %{{.*}}, i32 2)
       //CHECK-OPAQUE: tail call <2 x i8> @llvm.nvvm.ldg.global.i.v2i8.p0(ptr %{{.*}}, i32 2)
       auto cached_c2 = ldg(&in_c2[0]);
+      //CHECK: tail call <2 x i8> @llvm.nvvm.ldg.global.i.v2i8.p0v2i8(<2 x i8>* %{{.*}}, i32 2)
+      //CHECK-OPAQUE: tail call <2 x i8> @llvm.nvvm.ldg.global.i.v2i8.p0(ptr %{{.*}}, i32 2)
+      auto cached_sc2 = ldg(&in_sc2[0]);
       //CHECK: tail call <2 x i16> @llvm.nvvm.ldg.global.i.v2i16.p0v2i16(<2 x i16>* %{{.*}}, i32 4)
       //CHECK-OPAQUE: tail call <2 x i16> @llvm.nvvm.ldg.global.i.v2i16.p0(ptr %{{.*}}, i32 4)
       auto cached_s2 = ldg(&in_s2[0]);
       //CHECK: tail call <2 x i32> @llvm.nvvm.ldg.global.i.v2i32.p0v2i32(<2 x i32>* %{{.*}}, i32 8)
       //CHECK-OPAQUE: tail call <2 x i32> @llvm.nvvm.ldg.global.i.v2i32.p0(ptr %{{.*}}, i32 8)
       auto cached_i2 = ldg(&in_i2[0]);
+      //CHECK: tail call <2 x i{{32|64}}> @llvm.nvvm.ldg.global.i.v2i{{32|64}}.p0v2i{{32|64}}(<2 x i{{32|64}}>* %{{.*}}, i32 {{8|16}})
+      //CHECK-OPAQUE: tail call <2 x i{{32|64}}> @llvm.nvvm.ldg.global.i.v2i{{32|64}}.p0(ptr %{{.*}}, i32 {{8|16}})
+      auto cached_l2 = ldg(&in_l2[0]);
       //CHECK: tail call <2 x i64> @llvm.nvvm.ldg.global.i.v2i64.p0v2i64(<2 x i64>* %{{.*}}, i32 16)
       //CHECK-OPAQUE: tail call <2 x i64> @llvm.nvvm.ldg.global.i.v2i64.p0(ptr %{{.*}}, i32 16)
       auto cached_ll2 = ldg(&in_ll2[0]);
+      //CHECK: tail call <2 x i{{32|64}}> @llvm.nvvm.ldg.global.i.v2i{{32|64}}.p0v2i{{32|64}}(<2 x i{{32|64}}>* %{{.*}}, i32 {{8|16}})
+      //CHECK: tail call <2 x i{{32|64}}> @llvm.nvvm.ldg.global.i.v2i{{32|64}}.p0v2i{{32|64}}(<2 x i{{32|64}}>* nonnull %{{.*}}, i32 {{8|16}})
+      //CHECK-OPAQUE: tail call <2 x i{{32|64}}> @llvm.nvvm.ldg.global.i.v2i{{32|64}}.p0(ptr %{{.*}}, i32 {{8|16}})
+      //CHECK-OPAQUE: tail call <2 x i{{32|64}}> @llvm.nvvm.ldg.global.i.v2i{{32|64}}.p0(ptr nonnull %{{.*}}, i32 {{8|16}})
+      auto cached_l4 = ldg(&in_l4[0]);
       //CHECK: tail call <2 x i64> @llvm.nvvm.ldg.global.i.v2i64.p0v2i64(<2 x i64>* %{{.*}}, i32 16)
       //CHECK: tail call <2 x i64> @llvm.nvvm.ldg.global.i.v2i64.p0v2i64(<2 x i64>* nonnull %{{.*}}, i32 16)
       //CHECK-OPAQUE: tail call <2 x i64> @llvm.nvvm.ldg.global.i.v2i64.p0(ptr %{{.*}}, i32 16)
@@ -157,9 +175,17 @@ int main() {
       //CHECK: tail call <2 x i32> @llvm.nvvm.ldg.global.i.v2i32.p0v2i32(<2 x i32>* %{{.*}}, i32 8)
       //CHECK-OPAQUE: tail call <2 x i32> @llvm.nvvm.ldg.global.i.v2i32.p0(ptr %{{.*}}, i32 8)
       auto cached_ui2 = ldg(&in_ui2[0]);
+      //CHECK: tail call <2 x i{{64|32}}> @llvm.nvvm.ldg.global.i.v2i{{64|32}}.p0v2i{{64|32}}(<2 x i{{64|32}}>* %{{.*}}, i32 {{8|16}})
+      //CHECK-OPAQUE: tail call <2 x i{{64|32}}> @llvm.nvvm.ldg.global.i.v2i{{64|32}}.p0(ptr %{{.*}}, i32 {{8|16}})
+      auto cached_ul2 = ldg(&in_ul2[0]);
       //CHECK: tail call <2 x i64> @llvm.nvvm.ldg.global.i.v2i64.p0v2i64(<2 x i64>* %{{.*}}, i32 16)
       //CHECK-OPAQUE: tail call <2 x i64> @llvm.nvvm.ldg.global.i.v2i64.p0(ptr %{{.*}}, i32 16)
       auto cached_ull2 = ldg(&in_ull2[0]);
+      //CHECK: tail call <2 x i{{64|32}}> @llvm.nvvm.ldg.global.i.v2i{{64|32}}.p0v2i{{64|32}}(<2 x i{{64|32}}>* %{{.*}}, i32 {{8|16}})
+      //CHECK: tail call <2 x i{{64|32}}> @llvm.nvvm.ldg.global.i.v2i{{64|32}}.p0v2i{{64|32}}(<2 x i{{64|32}}>* nonnull %{{.*}}, i32 {{8|16}})
+      //CHECK-OPAQUE: tail call <2 x i{{64|32}}> @llvm.nvvm.ldg.global.i.v2i{{64|32}}.p0(ptr %{{.*}}, i32 {{8|16}})
+      //CHECK-OPAQUE: tail call <2 x i{{64|32}}> @llvm.nvvm.ldg.global.i.v2i{{64|32}}.p0(ptr nonnull %{{.*}}, i32 {{8|16}})
+      auto cached_ul4 = ldg(&in_ul4[0]);
       //CHECK: tail call <2 x i64> @llvm.nvvm.ldg.global.i.v2i64.p0v2i64(<2 x i64>* %{{.*}}, i32 16)
       //CHECK: tail call <2 x i64> @llvm.nvvm.ldg.global.i.v2i64.p0v2i64(<2 x i64>* nonnull %{{.*}}, i32 16)
       //CHECK-OPAQUE: tail call <2 x i64> @llvm.nvvm.ldg.global.i.v2i64.p0(ptr %{{.*}}, i32 16)
@@ -169,6 +195,9 @@ int main() {
       //CHECK: tail call <4 x i8> @llvm.nvvm.ldg.global.i.v4i8.p0v4i8(<4 x i8>* %{{.*}}, i32 4)
       //CHECK-OPAQUE: tail call <4 x i8> @llvm.nvvm.ldg.global.i.v4i8.p0(ptr %{{.*}}, i32 4)
       auto cached_c4 = ldg(&in_c4[0]);
+      //CHECK: tail call <4 x i8> @llvm.nvvm.ldg.global.i.v4i8.p0v4i8(<4 x i8>* %{{.*}}, i32 4)
+      //CHECK-OPAQUE: tail call <4 x i8> @llvm.nvvm.ldg.global.i.v4i8.p0(ptr %{{.*}}, i32 4)
+      auto cached_sc4 = ldg(&in_sc4[0]);
       //CHECK: tail call <4 x i16> @llvm.nvvm.ldg.global.i.v4i16.p0v4i16(<4 x i16>* %{{.*}}, i32 8)
       //CHECK-OPAQUE: tail call <4 x i16> @llvm.nvvm.ldg.global.i.v4i16.p0(ptr %{{.*}}, i32 8)
       auto cached_s4 = ldg(&in_s4[0]);
@@ -200,6 +229,7 @@ int main() {
   free(in_d2, q);
   free(in_d4, q);
   free(in_c, q);
+  free(in_sc, q);
   free(in_s, q);
   free(in_i, q);
   free(in_l, q);
@@ -210,16 +240,22 @@ int main() {
   free(in_ul, q);
   free(in_ull, q);
   free(in_c2, q);
+  free(in_sc2, q);
   free(in_s2, q);
   free(in_i2, q);
+  free(in_l2, q);
   free(in_ll2, q);
+  free(in_l4, q);
   free(in_ll4, q);
   free(in_uc2, q);
   free(in_us2, q);
   free(in_ui2, q);
+  free(in_ul2, q);
   free(in_ull2, q);
+  free(in_ul4, q);
   free(in_ull4, q);
   free(in_c4, q);
+  free(in_sc4, q);
   free(in_s4, q);
   free(in_i4, q);
   free(in_uc4, q);
