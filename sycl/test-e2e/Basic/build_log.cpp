8aee683979ee1c85e143c2aa6274a990db28e31b
Agarwal, Udit <udit.agarwal@intel.com>
2023-04-03 12:23:31 -0700

    Disable Basic/build_log on Windows as it depends on OCLOC compiler which is not available on the Windows CI.

--- sycl/test-e2e/Basic/build_log.cpp
+++ sycl/test-e2e/Basic/build_log.cpp
@@ -1,5 +1,5 @@
 // REQUIRES: opencl || level_zero, gpu
-// UNSUPPORTED: gpu-intel-dg1
+// UNSUPPORTED: gpu-intel-dg1 || windows
 //
 // RUN: %clangxx -fsycl -fsycl-targets=spir64_gen -Xsycl-target-backend=spir64_gen "-device dg1" %s -o %t.out
 // RUN: %GPU_RUN_PLACEHOLDER env SYCL_RT_WARNING_LEVEL=2 %t.out 2>&1 %GPU_CHECK_PLACEHOLDER
