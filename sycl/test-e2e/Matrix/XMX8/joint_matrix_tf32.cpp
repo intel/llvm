1f84b046b674f21b3878d1334c416a181abe6df8
Bing1 Yu <bing1.yu@intel.com>
2023-04-03 16:26:43 +0800

    move e2e testcases from llvm-test-suite's pr

---
+++ sycl/test-e2e/Matrix/XMX8/joint_matrix_tf32.cpp
@@ -0,0 +1,27 @@
+//==---------------- joint_matrix_tf32.cpp  - DPC++ joint_matrix------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+// REQUIRES: matrix-xmx8
+
+// RUN: %clangxx -fsycl %s -o %t.out -Dsycl/test-e2e_EXT_ONEAPI_MATRIX_VERSION=4
+// RUN: %CPU_RUN_PLACEHOLDER %t.out
+// RUN: %GPU_RUN_PLACEHOLDER %t.out
+
+// XFAIL:*
+
+#include <iostream>
+#include <random>
+#include <sycl/sycl.hpp>
+
+using namespace sycl;
+using namespace sycl::ext::intel;
+using namespace sycl::ext::oneapi;
+using namespace sycl::ext::oneapi::experimental::matrix;
+
+#define SG_SZ 8
+
+#include "../joint_matrix_tf32_impl.hpp"
