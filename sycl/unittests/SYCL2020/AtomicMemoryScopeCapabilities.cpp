8841460c473ada3469bc471120f26183833d1420
Marcos Maronas <maarquitos14@users.noreply.github.com>
2023-04-06 17:38:59 +0100

    [SYCL] Add tests for `atomic_memory_order/scope` context queries. (#8770)
    Add tests for `info::context::atomic_memory_scope_capabilities` and
    `info::context::atomic_memory_order_capabilities` queries.
    
    ---------
    
    Signed-off-by: Maronas, Marcos <marcos.maronas@intel.com>

--- sycl/unittests/SYCL2020/AtomicMemoryScopeCapabilities.cpp
+++ sycl/unittests/SYCL2020/AtomicMemoryScopeCapabilities.cpp
@@ -16,6 +16,18 @@ namespace {
 
 thread_local bool deviceGetInfoCalled;
 
+pi_result redefinedDevicesGet(pi_platform platform, pi_device_type device_type,
+                              pi_uint32 num_entries, pi_device *devices,
+                              pi_uint32 *num_devices) {
+  if (num_devices)
+    *num_devices = 2;
+  if (devices && num_entries > 0) {
+    devices[0] = reinterpret_cast<pi_device>(1);
+    devices[1] = reinterpret_cast<pi_device>(2);
+  }
+  return PI_SUCCESS;
+}
+
 pi_result redefinedDeviceGetInfoAfter(pi_device device,
                                       pi_device_info param_name,
                                       size_t param_value_size,
@@ -26,15 +38,21 @@ pi_result redefinedDeviceGetInfoAfter(pi_device device,
     if (param_value) {
       auto *Result =
           reinterpret_cast<pi_memory_scope_capabilities *>(param_value);
-      *Result = PI_MEMORY_SCOPE_WORK_ITEM | PI_MEMORY_SCOPE_SUB_GROUP |
-                PI_MEMORY_SCOPE_WORK_GROUP | PI_MEMORY_SCOPE_DEVICE |
-                PI_MEMORY_SCOPE_SYSTEM;
+      if (device == reinterpret_cast<pi_device>(1)) {
+        *Result = PI_MEMORY_SCOPE_WORK_ITEM | PI_MEMORY_SCOPE_SUB_GROUP |
+                  PI_MEMORY_SCOPE_WORK_GROUP | PI_MEMORY_SCOPE_DEVICE |
+                  PI_MEMORY_SCOPE_SYSTEM;
+      }
+      if (device == reinterpret_cast<pi_device>(2)) {
+        *Result = PI_MEMORY_SCOPE_WORK_ITEM | PI_MEMORY_SCOPE_SYSTEM;
+      }
     }
   }
   return PI_SUCCESS;
 }
 
-TEST(AtomicMemoryScopeCapabilitiesCheck, CheckAtomicMemoryScopeCapabilities) {
+TEST(AtomicMemoryScopeCapabilitiesCheck,
+     CheckDeviceAtomicMemoryScopeCapabilities) {
   sycl::unittest::PiMock Mock;
   sycl::platform Plt = Mock.getPlatform();
   device Dev = Plt.get_devices()[0];
@@ -60,6 +78,34 @@ TEST(AtomicMemoryScopeCapabilitiesCheck, CheckAtomicMemoryScopeCapabilities) {
   EXPECT_FALSE(res == scope_capabilities.end());
   res = std::find(scope_capabilities.begin(), scope_capabilities.end(),
                   sycl::memory_scope::device);
+  EXPECT_FALSE(res == scope_capabilities.end());
+  res = std::find(scope_capabilities.begin(), scope_capabilities.end(),
+                  sycl::memory_scope::system);
+  EXPECT_FALSE(res == scope_capabilities.end());
+}
+
+TEST(AtomicMemoryScopeCapabilitiesCheck,
+     CheckContextAtomicMemoryScopeCapabilities) {
+  sycl::unittest::PiMock Mock;
+  sycl::platform Plt = Mock.getPlatform();
+
+  Mock.redefineAfter<detail::PiApiKind::piDeviceGetInfo>(
+      redefinedDeviceGetInfoAfter);
+  Mock.redefineAfter<detail::PiApiKind::piDevicesGet>(redefinedDevicesGet);
+
+  auto devices = Plt.get_devices();
+  context Ctx{devices};
+
+  deviceGetInfoCalled = false;
+
+  auto scope_capabilities =
+      Ctx.get_info<sycl::info::context::atomic_memory_scope_capabilities>();
+  EXPECT_TRUE(deviceGetInfoCalled);
+  size_t expectedSize = 2;
+  EXPECT_EQ(scope_capabilities.size(), expectedSize);
+
+  auto res = std::find(scope_capabilities.begin(), scope_capabilities.end(),
+                       sycl::memory_scope::work_item);
   EXPECT_FALSE(res == scope_capabilities.end());
   res = std::find(scope_capabilities.begin(), scope_capabilities.end(),
                   sycl::memory_scope::system);
