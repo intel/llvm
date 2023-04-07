0ba8964b1c17abeb2e20df7cca8ab708e30943b5
James Brodman <james.brodman@intel.com>
2023-04-07 11:50:24 -0400

    clang-format
    Signed-off-by: James Brodman <james.brodman@intel.com>

--- sycl/include/sycl/ext/oneapi/experimental/partitioned_group.hpp
+++ sycl/include/sycl/ext/oneapi/experimental/partitioned_group.hpp
@@ -1,4 +1,4 @@
-//==------ partitioned_group.hpp --- SYCL extension for non-uniform groups -----==//
+//==--- partitioned_group.hpp --- SYCL extension for non-uniform groups ----==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
