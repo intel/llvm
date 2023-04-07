af902b048fae5834186cf46540a211a95c86059c
vic <victor.perez@codeplay.com>
2023-04-07 18:47:40 +0200

    [SYCL-MLIR] Use i32 for 1D SPIRV builtin variables in SYCL lowering (#8973)
    According to [the
    spec](https://registry.khronos.org/OpenCL/specs/3.0-unified/html/OpenCL_Env.html#_built_in_variables),
    these variables should be 32 bit wide.
    
    ---------
    
    Signed-off-by: Victor Perez <victor.perez@codeplay.com>

--- mlir-sycl/lib/Conversion/SYCLToSPIRV/SYCLToSPIRV.cpp
+++ mlir-sycl/lib/Conversion/SYCLToSPIRV/SYCLToSPIRV.cpp
@@ -238,16 +238,10 @@ public:
   }
 };
 
-void rewrite1D(Operation *op, spirv::BuiltIn builtin,
-               TypeConverter &typeConverter,
-               ConversionPatternRewriter &rewriter) {
-  const auto res = ::getBuiltinVariableValue(
-      op, builtin, typeConverter.convertType(rewriter.getIndexType()),
-      rewriter);
-  rewriter.replaceOp(op, convertScalarToDtype(
-                             rewriter, op->getLoc(), res,
-                             typeConverter.convertType(op->getResultTypes()[0]),
-                             /*isUnsignedCast*/ true));
+static void rewrite1D(Operation *op, spirv::BuiltIn builtin,
+                      ConversionPatternRewriter &rewriter) {
+  rewriter.replaceOp(op, ::getBuiltinVariableValue(
+                             op, builtin, rewriter.getI32Type(), rewriter));
 }
 
 /// Converts one-dimensional operations of type \tparam OpTy to calls to a SPIRV
@@ -260,8 +254,7 @@ public:
   LogicalResult
   matchAndRewrite(OpTy op, typename OpTy::Adaptor,
                   ConversionPatternRewriter &rewriter) const final {
-    rewrite1D(op, GridOpPattern<OpTy>::spirvBuiltin,
-              *GridOpPattern<OpTy>::getTypeConverter(), rewriter);
+    rewrite1D(op, GridOpPattern<OpTy>::spirvBuiltin, rewriter);
     return success();
   }
 };
