32a5ec9b561fc98755da771f2d980194bbb1089a
gregory <gregory@gaiaplatform.io>
2023-04-18 12:31:41 -0700

    Use merge instead of multiplication

--- sycl/include/sycl/ext/intel/esimd/detail/operators.hpp
+++ sycl/include/sycl/ext/intel/esimd/detail/operators.hpp
@@ -498,7 +498,9 @@ __ESIMD_DEF_SIMD_VIEW_CMP_OP(>=, __ESIMD_DNS::is_simd_type_v<SimdT1>)
                 __ESIMD_DNS::is_simd_mask_type_v<SimdT2> &&                    \
                 (SimdT1::length == SimdT2::length) && COND>>                   \
   inline SimdT1 operator BINOP(const SimdT1 &LHS, const SimdT2 &RHS) {         \
-    return LHS BINOP SimdT1(RHS);                                              \
+    SimdT1 res(0);                                                             \
+    res.merge(LHS, RHS);                                                       \
+    return res;                                                                \
   }                                                                            \
                                                                                \
   /* simd_mask<...> BINOP simd* */                                             \
@@ -510,7 +512,9 @@ __ESIMD_DEF_SIMD_VIEW_CMP_OP(>=, __ESIMD_DNS::is_simd_type_v<SimdT1>)
                 __ESIMD_DNS::is_simd_mask_type_v<SimdT1> &&                    \
                 (SimdT2::length == SimdT1::length) && COND>>                   \
   inline SimdT2 operator BINOP(const SimdT1 &LHS, const SimdT2 &RHS) {         \
-    return SimdT2(LHS) BINOP RHS;                                              \
+    SimdT2 res(0);                                                             \
+    res.merge(RHS, LHS);                                                       \
+    return res;                                                                \
   }                                                                            \
                                                                                \
   /* SCALAR BINOP simd_mask  */                                                \
@@ -519,7 +523,9 @@ __ESIMD_DEF_SIMD_VIEW_CMP_OP(>=, __ESIMD_DNS::is_simd_type_v<SimdT1>)
             class = std::enable_if_t<                                          \
                 __ESIMD_DNS::is_simd_mask_type_v<SimdT2> && COND>>             \
   inline SimdT1 operator BINOP(T1 LHS, const SimdT2 &RHS) {                    \
-    return LHS BINOP SimdT1(RHS);                                              \
+    SimdT1 res(0);                                                             \
+    res.merge(LHS, RHS);                                                       \
+    return res;                                                                \
   }                                                                            \
                                                                                \
   /* simd_mask BINOP SCALAR */                                                 \
@@ -528,15 +534,15 @@ __ESIMD_DEF_SIMD_VIEW_CMP_OP(>=, __ESIMD_DNS::is_simd_type_v<SimdT1>)
             class = std::enable_if_t<                                          \
                 __ESIMD_DNS::is_simd_mask_type_v<SimdT1> && COND>>             \
   inline SimdT2 operator BINOP(const SimdT1 &LHS, T2 RHS) {                    \
-    return SimdT2(LHS) BINOP RHS;                                              \
+    SimdT2 res(0);                                                             \
+    res.merge(RHS, LHS);                                                       \
+    return res;                                                                \
   }
 
 #define __ESIMD_ARITH_OP_FILTER                                                \
   __ESIMD_DNS::is_valid_simd_elem_type_v<T1>                                   \
       &&__ESIMD_DNS::is_valid_simd_elem_type_v<T2>
 
-__ESIMD_DEF_SIMD_MASK_BIN_OP(+, __ESIMD_ARITH_OP_FILTER)
-__ESIMD_DEF_SIMD_MASK_BIN_OP(-, __ESIMD_ARITH_OP_FILTER)
 __ESIMD_DEF_SIMD_MASK_BIN_OP(*, __ESIMD_ARITH_OP_FILTER)
 #undef __ESIMD_ARITH_OP_FILTER
 
