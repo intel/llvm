5f5b2f134935a2dd64c026bd055bdcc7a74ea3b7
Larsen, Steffen <steffen.larsen@intel.com>
2023-04-19 02:46:41 -0700

    Remove use of context in HIP PI
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- sycl/plugins/hip/pi_hip.cpp
+++ sycl/plugins/hip/pi_hip.cpp
@@ -5562,7 +5562,6 @@ pi_result hip_piextPhysicalMemRetain(pi_physical_mem physical_mem) {
 }
 
 pi_result hip_piextPhysicalMemRelease(pi_physical_mem physical_mem) {
-  std::ignore = context;
   std::ignore = physical_mem;
 
   sycl::detail::pi::die("hip_piextPhysicalMemRelease not implemented");
