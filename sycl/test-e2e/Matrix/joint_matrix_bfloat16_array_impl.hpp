127f9bf8f93f0e40388ef4e3a2600420fb07675c
Yury Plyakhin <yury.plyakhin@intel.com>
2023-04-20 16:04:03 -0700

    [SYCL][Joint Matrix] Added test variants for XMX8.
    Also resolved clang format problems.

--- sycl/test-e2e/Matrix/joint_matrix_bfloat16_array_impl.hpp
+++ sycl/test-e2e/Matrix/joint_matrix_bfloat16_array_impl.hpp
@@ -1,4 +1,4 @@
-//==--------- ------------------------------------------------------------------==//
+//==----------------------------------------------------------------------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -13,9 +13,7 @@ static constexpr int JM_ARRAY_SZ = 2;
 
 static constexpr float BF16_EPSILON = 0.00781250;
 
-template <typename T, size_t NUM_ROWS, size_t NUM_COLS>
-struct big_matrix
-{
+template <typename T, size_t NUM_ROWS, size_t NUM_COLS> struct big_matrix {
 private:
   T *mat;
 
@@ -27,8 +25,7 @@ public:
 
 template <typename T1, typename T2, size_t M, size_t N, size_t K>
 void matrix_multiply(big_matrix<T1, M, N> &C, big_matrix<T2, M, K> &A,
-                     big_matrix<T2, K / 2, N * 2> &B)
-{
+                     big_matrix<T2, K / 2, N * 2> &B) {
   size_t NDRangeM = M / (TM * JM_ARRAY_SZ);
   size_t NDRangeN = N / TN;
   buffer<bfloat16, 2> bufA(A.get_data(), range<2>(M, K));
@@ -36,63 +33,61 @@ void matrix_multiply(big_matrix<T1, M, N> &C, big_matrix<T2, M, K> &A,
   buffer<float, 2> bufC((float *)C.get_data(), range<2>(M, N));
 
   queue q;
-  q.submit([&](handler &cgh)
-           {
-             auto accC = bufC.get_access<access::mode::read_write>(cgh);
-             auto accA = bufA.get_access<access::mode::read_write>(cgh);
-             auto accB = bufB.get_access<access::mode::read_write>(cgh);
+  q.submit([&](handler &cgh) {
+     auto accC = bufC.get_access<access::mode::read_write>(cgh);
+     auto accA = bufA.get_access<access::mode::read_write>(cgh);
+     auto accB = bufB.get_access<access::mode::read_write>(cgh);
 
-             cgh.parallel_for(
-                 nd_range<2>({NDRangeM, NDRangeN * SG_SZ}, {1, 1 * SG_SZ}),
-                 [=](nd_item<2> spmd_item) [[intel::reqd_sub_group_size(SG_SZ)]]
-                 {
-                   // The submatrix API has to be accessed by all the workitems in a
-                   // subgroup these functions will be called once by the subgroup no
-                   // code divergence between the workitems
-                   const auto global_idx = spmd_item.get_global_id(0);
-                   const auto global_idy = spmd_item.get_global_id(1);
-                   const auto sg_startx = global_idx - spmd_item.get_local_id(0);
-                   const auto sg_starty = global_idy - spmd_item.get_local_id(1);
+     cgh.parallel_for(
+         nd_range<2>({NDRangeM, NDRangeN * SG_SZ}, {1, 1 * SG_SZ}),
+         [=](nd_item<2> spmd_item) [[intel::reqd_sub_group_size(SG_SZ)]] {
+           // The submatrix API has to be accessed by all the workitems in a
+           // subgroup these functions will be called once by the subgroup no
+           // code divergence between the workitems
+           const auto global_idx = spmd_item.get_global_id(0);
+           const auto global_idy = spmd_item.get_global_id(1);
+           const auto sg_startx = global_idx - spmd_item.get_local_id(0);
+           const auto sg_starty = global_idy - spmd_item.get_local_id(1);
 
-                   sub_group sg = spmd_item.get_sub_group();
-                   joint_matrix<sub_group, bfloat16, use::a, TM, TK, layout::row_major> sub_a[JM_ARRAY_SZ];
+           sub_group sg = spmd_item.get_sub_group();
+           joint_matrix<sub_group, bfloat16, use::a, TM, TK, layout::row_major>
+               sub_a[JM_ARRAY_SZ];
 
-                   // For B, we assume B has been already VNNIed.
-                   joint_matrix<sub_group, bfloat16, use::b, TK, TN,
-                                ext::intel::experimental::matrix::layout::packed>
-                       sub_b;
-                   joint_matrix<sub_group, float, use::accumulator, TM, TN> sub_c[JM_ARRAY_SZ];
+           // For B, we assume B has been already VNNIed.
+           joint_matrix<sub_group, bfloat16, use::b, TK, TN,
+                        ext::intel::experimental::matrix::layout::packed>
+               sub_b;
+           joint_matrix<sub_group, float, use::accumulator, TM, TN>
+               sub_c[JM_ARRAY_SZ];
 
-                   for (int i = 0; i < JM_ARRAY_SZ; ++i)
-                     joint_matrix_fill(sg, sub_c[i], 1.0);
+           for (int i = 0; i < JM_ARRAY_SZ; ++i)
+             joint_matrix_fill(sg, sub_c[i], 1.0);
 
-                   for (int k = 0; k < K / TK; ++k)
-                   {
-                     joint_matrix_load(sg, sub_b,
-                                       accB.get_pointer() + (k * TK / 2) * (N * 2) +
-                                           sg_starty / SG_SZ * TN * 2,
-                                       N * 2);
+           for (int k = 0; k < K / TK; ++k) {
+             joint_matrix_load(sg, sub_b,
+                               accB.get_pointer() + (k * TK / 2) * (N * 2) +
+                                   sg_starty / SG_SZ * TN * 2,
+                               N * 2);
 
-                     for (int i = 0; i < JM_ARRAY_SZ; ++i)
-                     {
-                       joint_matrix_load(sg, sub_a[i],
-                                         accA.get_pointer() +
-                                             (sg_startx * TM * JM_ARRAY_SZ + TM * i) * K +
-                                             k * TK,
-                                         K);
-                       sub_c[i] = joint_matrix_mad(sg, sub_a[i], sub_b, sub_c[i]);
-                     }
-                   }
+             for (int i = 0; i < JM_ARRAY_SZ; ++i) {
+               joint_matrix_load(
+                   sg, sub_a[i],
+                   accA.get_pointer() +
+                       (sg_startx * TM * JM_ARRAY_SZ + TM * i) * K + k * TK,
+                   K);
+               sub_c[i] = joint_matrix_mad(sg, sub_a[i], sub_b, sub_c[i]);
+             }
+           }
 
-                   for (int i = 0; i < JM_ARRAY_SZ; ++i)
-                     joint_matrix_store(sg, sub_c[i],
-                                        accC.get_pointer() +
-                                            (sg_startx * TM * JM_ARRAY_SZ + TM * i) * N +
-                                            sg_starty / SG_SZ * TN,
-                                        N, layout::row_major);
-                 }); // parallel for
-           })
-      .wait();
+           for (int i = 0; i < JM_ARRAY_SZ; ++i)
+             joint_matrix_store(sg, sub_c[i],
+                                accC.get_pointer() +
+                                    (sg_startx * TM * JM_ARRAY_SZ + TM * i) *
+                                        N +
+                                    sg_starty / SG_SZ * TN,
+                                N, layout::row_major);
+         }); // parallel for
+   }).wait();
 }
 
 static constexpr size_t MATRIX_M = TM * 2;
@@ -105,27 +100,23 @@ bfloat16 B[MATRIX_K / 2][MATRIX_N * 2];
 float C[MATRIX_M][MATRIX_N];
 float D[MATRIX_M][MATRIX_N];
 
-float make_fp32(bfloat16 x)
-{
+float make_fp32(bfloat16 x) {
   unsigned int y = *((int *)&x);
   y = y << 16;
   float *res = reinterpret_cast<float *>(&y);
   return *res;
 }
 
-void matrix_multiply_ref(int *A_mem, int *B_mem, int *C_mem, int M, int N, int K)
-{
+void matrix_multiply_ref(int *A_mem, int *B_mem, int *C_mem, int M, int N,
+                         int K) {
   for (int m = 0; m < M; m++)
-    for (int n = 0; n < N; n++)
-    {
-      for (int k = 0; k < K; k++)
-      {
+    for (int n = 0; n < N; n++) {
+      for (int k = 0; k < K; k++) {
         // Because B was assumed VNNIed
         bfloat16 *va = (bfloat16 *)(A_mem + m * K + k);
         bfloat16 *vb = (bfloat16 *)(B_mem + k * N + n);
         float acc = *((float *)(C_mem + m * N + n));
-        for (int i = 0; i < 2; i++)
-        {
+        for (int i = 0; i < 2; i++) {
           acc += (make_fp32(va[i]) * make_fp32(vb[i]));
         }
         *((float *)(C_mem + m * N + n)) = acc;
@@ -133,26 +124,19 @@ void matrix_multiply_ref(int *A_mem, int *B_mem, int *C_mem, int M, int N, int K
     }
 }
 
-int main()
-{
-  for (int i = 0; i < MATRIX_M; i++)
-  {
-    for (int j = 0; j < MATRIX_K; j++)
-    {
+int main() {
+  for (int i = 0; i < MATRIX_M; i++) {
+    for (int j = 0; j < MATRIX_K; j++) {
       A[i][j] = bfloat16(1.0f * (i + j));
     }
   }
-  for (int i = 0; i < MATRIX_K / 2; i++)
-  {
-    for (int j = 0; j < MATRIX_N * 2; j++)
-    {
+  for (int i = 0; i < MATRIX_K / 2; i++) {
+    for (int j = 0; j < MATRIX_N * 2; j++) {
       B[i][j] = bfloat16(2.0f * i + 3.0f * j);
     }
   }
-  for (int i = 0; i < MATRIX_M; i++)
-  {
-    for (int j = 0; j < MATRIX_N; j++)
-    {
+  for (int i = 0; i < MATRIX_M; i++) {
+    for (int j = 0; j < MATRIX_N; j++) {
       C[i][j] = 1.0;
       D[i][j] = 1.0;
     }
@@ -168,11 +152,10 @@ int main()
                       MATRIX_N, MATRIX_K / 2);
 
   bool res = true;
-  for (int i = 0; i < MATRIX_M; i++)
-  {
-    for (int j = 0; j < MATRIX_N; j++)
-    {
-      // std::cout << i << "," << j << ": " << C[i][j] << ", " << D[i][j] << "\n"; // for debugging
+  for (int i = 0; i < MATRIX_M; i++) {
+    for (int j = 0; j < MATRIX_N; j++) {
+      // std::cout << i << "," << j << ": " << C[i][j] << ", " << D[i][j] <<
+      // "\n"; // for debugging
       if ((fabs(C[i][j] - D[i][j])) > BF16_EPSILON)
         res = false;
     }
