eb9b4b7a81c2f3b026c884daa6bbf23451a882b6
Jaime Arteaga <jaime.a.arteaga.molina@intel.com>
2023-04-20 17:19:13 -0700

    Port Change the default to SYCL_PI_LEVEL_ZERO_USM_RESIDENT=2
    https://github.com/intel/llvm/pull/9109
    
    Signed-off-by: Jaime Arteaga <jaime.a.arteaga.molina@intel.com>

--- sycl/plugins/unified_runtime/ur/adapters/level_zero/ur_level_zero_mem.cpp
+++ sycl/plugins/unified_runtime/ur/adapters/level_zero/ur_level_zero_mem.cpp
@@ -2397,12 +2397,12 @@ ur_result_t USMHostMemoryAlloc::allocateImpl(void **ResultPtr, size_t Size,
 }
 
 enum class USMAllocationForceResidencyType {
-  // [Default] Do not force memory residency at allocation time.
+  // Do not force memory residency at allocation time.
   None = 0,
   // Force memory resident on the device of allocation at allocation time.
   // For host allocation force residency on all devices in a context.
   Device = 1,
-  // Force memory resident on all devices in the context with P2P
+  // [Default] Force memory resident on all devices in the context with P2P
   // access to the device of allocation.
   // For host allocation force residency on all devices in a context.
   P2PDevices = 2
@@ -2412,7 +2412,7 @@ enum class USMAllocationForceResidencyType {
 static USMAllocationForceResidencyType USMAllocationForceResidency = [] {
   const auto Str = std::getenv("SYCL_PI_LEVEL_ZERO_USM_RESIDENT");
   if (!Str)
-    return USMAllocationForceResidencyType::None;
+    return USMAllocationForceResidencyType::P2PDevices;
   switch (std::atoi(Str)) {
   case 1:
     return USMAllocationForceResidencyType::Device;
