08f6486803f339d1fd4eef9f400de04b1fe2bb73
Victor Perez <victor.perez@codeplay.com>
2023-04-21 11:56:43 +0100

    clang-format files

--- mlir-sycl/lib/Dialect/SYCL/IR/SYCLAttributes.cpp
+++ mlir-sycl/lib/Dialect/SYCL/IR/SYCLAttributes.cpp
@@ -8,5 +8,5 @@
 
 #include "mlir/Dialect/SYCL/IR/SYCLAttributes.h"
 
-#include "mlir/Dialect/SYCL/IR/SYCLEnums.cpp.inc"
 #include "mlir/Dialect/SYCL/IR/SYCLAttributes.cpp.inc"
+#include "mlir/Dialect/SYCL/IR/SYCLEnums.cpp.inc"
