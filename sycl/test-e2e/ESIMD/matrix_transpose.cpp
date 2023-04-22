41b221df96e0cf2ac78906f022fd39cf5273daf5
gregory <gregory@gaiaplatform.io>
2023-04-21 18:14:37 -0700

    Permanently disable tests on simulator and add comment

--- sycl/test-e2e/ESIMD/matrix_transpose.cpp
+++ sycl/test-e2e/ESIMD/matrix_transpose.cpp
@@ -7,7 +7,7 @@
 //===----------------------------------------------------------------------===//
 // REQUIRES: gpu
 // UNSUPPORTED: cuda || hip || esimd_emulator
-// TODO: esimd_emulator fails due to unimplemented __esimd_oword_ld_unaligned
+// TODO: investigate sporadic esimd_emulator failures
 // RUN: %clangxx -fsycl %s -o %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 
--- sycl/test-e2e/ESIMD/matrix_transpose2.cpp
+++ sycl/test-e2e/ESIMD/matrix_transpose2.cpp
@@ -7,7 +7,7 @@
 //===----------------------------------------------------------------------===//
 // REQUIRES: gpu
 // UNSUPPORTED: cuda || hip || gpu-intel-pvc || esimd_emulator
-// TODO: esimd_emulator fails due to outdated __esimd_media_ld
+// TODO: investigate sporadic esimd_emulator failures
 // RUN: %clangxx -fsycl %s -o %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 
