41a0b0c3f8903964c25647296f06767665c714dc
John Pennycook <john.pennycook@intel.com>
2023-04-24 10:32:18 -0700

    Use constexpr uint32_t for sub-group size

--- sycl/test-e2e/NonUniformGroups/fixed_size_group_algorithms.cpp
+++ sycl/test-e2e/NonUniformGroups/fixed_size_group_algorithms.cpp
@@ -12,23 +12,24 @@ template <size_t PartitionSize> class TestKernel;
 template <size_t PartitionSize> void test() {
   sycl::queue Q;
 
+  constexpr uint32_t SGSize = 32;
   auto SGSizes = Q.get_device().get_info<sycl::info::device::sub_group_sizes>();
-  if (std::find(SGSizes.begin(), SGSizes.end(), 32) == SGSizes.end()) {
+  if (std::find(SGSizes.begin(), SGSizes.end(), SGSize) == SGSizes.end()) {
     std::cout << "Test skipped due to missing support for sub-group size 32."
               << std::endl;
   }
 
-  sycl::buffer<size_t, 1> TmpBuf{sycl::range{32}};
-  sycl::buffer<bool, 1> BarrierBuf{sycl::range{32}};
-  sycl::buffer<bool, 1> BroadcastBuf{sycl::range{32}};
-  sycl::buffer<bool, 1> AnyBuf{sycl::range{32}};
-  sycl::buffer<bool, 1> AllBuf{sycl::range{32}};
-  sycl::buffer<bool, 1> NoneBuf{sycl::range{32}};
-  sycl::buffer<bool, 1> ReduceBuf{sycl::range{32}};
-  sycl::buffer<bool, 1> ExScanBuf{sycl::range{32}};
-  sycl::buffer<bool, 1> IncScanBuf{sycl::range{32}};
+  sycl::buffer<size_t, 1> TmpBuf{sycl::range{SGSize}};
+  sycl::buffer<bool, 1> BarrierBuf{sycl::range{SGSize}};
+  sycl::buffer<bool, 1> BroadcastBuf{sycl::range{SGSize}};
+  sycl::buffer<bool, 1> AnyBuf{sycl::range{SGSize}};
+  sycl::buffer<bool, 1> AllBuf{sycl::range{SGSize}};
+  sycl::buffer<bool, 1> NoneBuf{sycl::range{SGSize}};
+  sycl::buffer<bool, 1> ReduceBuf{sycl::range{SGSize}};
+  sycl::buffer<bool, 1> ExScanBuf{sycl::range{SGSize}};
+  sycl::buffer<bool, 1> IncScanBuf{sycl::range{SGSize}};
 
-  const auto NDR = sycl::nd_range<1>{32, 32};
+  const auto NDR = sycl::nd_range<1>{SGSize, SGSize};
   Q.submit([&](sycl::handler &CGH) {
     sycl::accessor TmpAcc{TmpBuf, CGH, sycl::write_only};
     sycl::accessor BarrierAcc{BarrierBuf, CGH, sycl::write_only};
@@ -40,7 +41,7 @@ template <size_t PartitionSize> void test() {
     sycl::accessor ExScanAcc{ExScanBuf, CGH, sycl::write_only};
     sycl::accessor IncScanAcc{IncScanBuf, CGH, sycl::write_only};
     const auto KernelFunc =
-        [=](sycl::nd_item<1> item) [[sycl::reqd_sub_group_size(32)]] {
+        [=](sycl::nd_item<1> item) [[sycl::reqd_sub_group_size(SGSize)]] {
           auto WI = item.get_global_id();
           auto SG = item.get_sub_group();
 
@@ -51,7 +52,7 @@ template <size_t PartitionSize> void test() {
           TmpAcc[WI] = 1;
           sycl::group_barrier(Partition);
           size_t Visible = 0;
-          for (size_t Other = 0; Other < 32; ++Other) {
+          for (size_t Other = 0; Other < SGSize; ++Other) {
             if ((WI / PartitionSize) == (Other / PartitionSize)) {
               Visible += TmpAcc[Other];
             }
@@ -109,7 +110,7 @@ template <size_t PartitionSize> void test() {
   sycl::host_accessor ReduceAcc{ReduceBuf, sycl::read_only};
   sycl::host_accessor ExScanAcc{ExScanBuf, sycl::read_only};
   sycl::host_accessor IncScanAcc{IncScanBuf, sycl::read_only};
-  for (int WI = 0; WI < 32; ++WI) {
+  for (int WI = 0; WI < SGSize; ++WI) {
     assert(BarrierAcc[WI] == true);
     assert(BroadcastAcc[WI] == true);
     assert(AnyAcc[WI] == true);
