5bd42eb6a7dff86ab0ae4c85a1ef4b6cd1ad57c6
Chunyang Dai <chunyang.dai@intel.com>
2023-04-24 04:53:55 -0700

    [SYCL] Set compiled program for hostpipe (#9060)
    HostPipe implementation should pass the compiled program to backend when
    invoke the read / write operation.
    
    There are two scenarios for HostPipe read / write:
       * Invoke device kernel before HostPipe read / write
       * Invoke HostPipe read / write before device kernel.
    
    For the 1st scenario, the program should be compiled before HostPipe
    read / write. We get the program from Cache for HostPipe entry.
    
    For the 2nd scenario, the program should NOT be compiled. We need to
    compile the program first for HostPipe entry. The compiled program will
    be cached for future device kernel.

--- sycl/source/detail/scheduler/commands.cpp
+++ sycl/source/detail/scheduler/commands.cpp
@@ -2343,9 +2343,24 @@ pi_int32 enqueueReadWriteHostPipe(const QueueImplPtr &Queue,
   detail::HostPipeMapEntry *hostPipeEntry =
       ProgramManager::getInstance().getHostPipeEntry(PipeName);
 
-  RT::PiProgram Program = ProgramManager::getInstance().createPIProgram(
-      *(hostPipeEntry->mDeviceImage), Queue->get_context(),
-      Queue->get_device());
+  RT::PiProgram Program = nullptr;
+  device Device = Queue->get_device();
+  ContextImplPtr ContextImpl = Queue->getContextImplPtr();
+  std::optional<RT::PiProgram> CachedProgram =
+      ContextImpl->getProgramForHostPipe(Device, hostPipeEntry);
+  if (CachedProgram)
+    Program = *CachedProgram;
+  else {
+    // If there was no cached program, build one.
+    device_image_plain devImgPlain =
+        ProgramManager::getInstance().getDeviceImageFromBinaryImage(
+            hostPipeEntry->getDevBinImage(), Queue->get_context(),
+            Queue->get_device());
+    device_image_plain BuiltImage =
+        ProgramManager::getInstance().build(devImgPlain, {Device}, {});
+    Program = getSyclObjImpl(BuiltImage)->get_program_ref();
+  }
+  assert(Program && "Program for this hostpipe is not compiled.");
 
   // Get plugin for calling opencl functions
   const detail::plugin &Plugin = Queue->getPlugin();
