be6dfcb75201e768e31adbd976bb1fb7ac70ff9b
fineg74 <61437305+fineg74@users.noreply.github.com>
2023-04-24 18:03:25 -0700

    Update sycl/test-e2e/ESIMD/dword_atomic_accessor_smoke.cpp
    Co-authored-by: Nick Sarnie <sarnex@users.noreply.github.com>

--- sycl/test-e2e/ESIMD/dword_atomic_accessor_smoke.cpp
+++ sycl/test-e2e/ESIMD/dword_atomic_accessor_smoke.cpp
@@ -1,4 +1,4 @@
-//==------ dword_atomic_accessor__smoke.cpp  - DPC++ ESIMD on-device test --==//
+//==------ dword_atomic_accessor_smoke.cpp  - DPC++ ESIMD on-device test --==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
