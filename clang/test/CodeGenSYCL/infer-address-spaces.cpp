af1cf61e39aae98444280ee7c27d910128f7fbc1
Freddy Ye <freddy.ye@intel.com>
2023-04-26 20:29:26 +0800

    Update infer-address-spaces.cpp (#9216)

--- clang/test/CodeGenSYCL/infer-address-spaces.cpp
+++ clang/test/CodeGenSYCL/infer-address-spaces.cpp
@@ -17,5 +17,5 @@ void foo(const float *usm_in, float* usm_out) {
 
 // No addrspacecast before loading and storing values
 // CHECK-NOT: addrspacecast
-// CHECK:  %0 = load float, ptr addrspace(1)
-// CHECK:  store float %0, ptr addrspace(1)
+// CHECK:  {{%[^ ]*}} = load float, ptr addrspace(1)
+// CHECK:  store float {{%[^ ]*}}, ptr addrspace(1)
