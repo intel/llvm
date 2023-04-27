ca141b810af82a01280d13812bed31ed84972c5a
Steffen Larsen <steffen.larsen@intel.com>
2023-04-27 19:04:10 +0100

    [SYCL][NFC] Ignore unused argument in copy from host (#9239)
    Fixes post-commit failure after https://github.com/intel/llvm/pull/9215.
    
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- sycl/source/detail/context_impl.cpp
+++ sycl/source/detail/context_impl.cpp
@@ -417,8 +417,9 @@ void context_impl::memcpyToHostOnlyDeviceGlobal(
 
 void context_impl::memcpyFromHostOnlyDeviceGlobal(
     const std::shared_ptr<device_impl> &DeviceImpl, void *Dest,
-    const void *DeviceGlobalPtr, size_t DeviceGlobalTSize,
-    bool IsDeviceImageScoped, size_t NumBytes, size_t Offset) {
+    const void *DeviceGlobalPtr, size_t, bool IsDeviceImageScoped,
+    size_t NumBytes, size_t Offset) {
+
   std::optional<RT::PiDevice> KeyDevice = std::nullopt;
   if (IsDeviceImageScoped)
     KeyDevice = DeviceImpl->getHandleRef();
