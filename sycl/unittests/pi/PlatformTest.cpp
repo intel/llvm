259a812ff76d934545cdcf1e2e9056d2ec3a5983
Sergey V Maslov <sergey.v.maslov@intel.com>
2023-04-27 19:24:59 -0700

    address test failures
    Signed-off-by: Sergey V Maslov <sergey.v.maslov@intel.com>

--- sycl/unittests/pi/PlatformTest.cpp
+++ sycl/unittests/pi/PlatformTest.cpp
@@ -66,7 +66,7 @@ INSTANTIATE_TEST_SUITE_P(
     PlatformTestImpl, PlatformTest,
     testing::ValuesIn(pi::initializeAndRemoveInvalid()),
     [](const testing::TestParamInfo<PlatformTest::ParamType> &info) {
-      return pi::GetBackendString(info.param).c_str();
+      return pi::GetBackendString(info.param);
     });
 
 TEST_P(PlatformTest, piPlatformsGet) {
