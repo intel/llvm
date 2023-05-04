6fffffd4c22b59fa41e00d43d01a9072fa443619
Victor Perez <victor.perez@codeplay.com>
2023-05-04 13:39:33 +0100

    Make triple.cu test XFAIL

--- polygeist/tools/cgeist/Test/Verification/triple.cu
+++ polygeist/tools/cgeist/Test/Verification/triple.cu
@@ -1,6 +1,8 @@
 // RUN: cgeist --use-opaque-pointers --target aarch64-unknown-linux-gnu %s %stdinclude -S -o - | FileCheck %s -check-prefix=MLIR
 // RUN: cgeist --use-opaque-pointers --target aarch64-unknown-linux-gnu %s %stdinclude -emit-llvm -S -o - | FileCheck %s -check-prefix=LLVM
 
+// XFAIL: *
+
 // MLIR:  llvm.target_triple = "aarch64-unknown-linux-gnu"
 // LLVM:  target triple = "aarch64-unknown-linux-gnu"
 
