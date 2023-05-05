54343339ce7d59bb50992b0c1e5118bde330a023
gregory <gregory@gaiaplatform.io>
2023-05-04 19:28:56 -0700

    Add more test cases

--- sycl/test-e2e/ESIMD/api/mask_math.cpp
+++ sycl/test-e2e/ESIMD/api/mask_math.cpp
@@ -9,65 +9,150 @@
 //===----------------------------------------------------------------------===//
 // REQUIRES: gpu
 // UNSUPPORTED: cuda || hip
+// UNSUPPORTED: gpu-intel-gen9 && windows
 // RUN: %clangxx -fsycl %s -o %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 
+#include "../esimd_test_utils.hpp"
 #include <sycl/ext/intel/esimd.hpp>
 #include <sycl/sycl.hpp>
 
 namespace esimd = sycl::ext::intel::esimd;
+// Result type of a scalar binary Op
+constexpr unsigned Size = 2;
+template <class T1, class T2, class OpClass>
+using scalar_comp_t =
+    std::conditional_t<std::is_same_v<OpClass, esimd_test::CmpOp>,
+                       typename esimd::simd_mask<Size>::element_type,
+                       __ESIMD_DNS::computation_type_t<T1, T2>>;
 
-int main(int, char **) {
-  constexpr unsigned Size = 16;
-  sycl::queue q;
-  float *Input = sycl::malloc_shared<float>(Size, q);
-  float *Output1 = sycl::malloc_shared<float>(Size, q);
-  float *Output2 = sycl::malloc_shared<float>(Size, q);
-  float *Output3 = sycl::malloc_shared<float>(Size, q);
-  float *Output4 = sycl::malloc_shared<float>(Size, q);
+// Result type of a vector binary Op
+template <class T1, class T2, class OpClass, int N = 0>
+using comp_t = std::conditional_t<
+    N == 0, scalar_comp_t<T1, T2, OpClass>,
+    std::conditional_t<
+        std::is_same_v<OpClass, esimd_test::CmpOp>, esimd::simd_mask<N>,
+        esimd::simd<__ESIMD_DNS::computation_type_t<T1, T2>, N>>>;
 
+template <class T, class OpClass, class Ops>
+bool test(Ops ops, queue &q, int expectedValues[]) {
+  T *Input = sycl::malloc_shared<T>(Size, q);
+  constexpr int NumOps = (int)Ops::size;
+  int CSize = NumOps * Size;
+  T *Output = sycl::malloc_shared<T>(CSize * 7, q);
   for (int i = 0; i < Size; ++i)
     Input[i] = i;
+  constexpr T TestValue = 2;
 
-  q.submit([&](sycl::handler &cgh) {
-     cgh.single_task<class Kernel>([=]() SYCL_ESIMD_KERNEL {
-       esimd::simd<float, Size> x = 2;
-       esimd::simd<float, Size> InputVector(Input);
-       esimd::simd<float, Size> OutputVector1 = x * (InputVector < 8);
-       esimd::simd<float, Size> OutputVector2 = (InputVector < 8) * x;
-       esimd::simd<float, Size> OutputVector3 = 2 * (InputVector < 8);
-       esimd::simd<float, Size> OutputVector4 = (InputVector < 8) * 2;
-       OutputVector1.copy_to(Output1);
-       OutputVector2.copy_to(Output2);
-       OutputVector3.copy_to(Output3);
-       OutputVector4.copy_to(Output4);
-     });
-   }).wait();
+  try {
+    q.submit([&](sycl::handler &cgh) {
+       cgh.single_task([=]() SYCL_ESIMD_KERNEL {
+         esimd::simd<T, Size> x = TestValue;
+         esimd::simd<T, Size> InputVector(Input);
 
-  bool Passed = true;
-  float ExpectedValue = 0;
-  for (int i = 0; i < Size; ++i) {
-    if (i < 8) {
-      ExpectedValue = 2;
-    } else {
-      ExpectedValue = 0;
-    }
+         esimd_test::apply_ops(
+             ops, x, InputVector < (Size / 2),
+             [=](comp_t<T, T, OpClass, Size> res, OpClass op, unsigned op_num) {
+               unsigned res_off = op_num * Size;
+               res.copy_to(Output + res_off);
+             });
+         esimd_test::apply_ops(
+             ops, InputVector < (Size / 2), x,
+             [=](comp_t<T, T, OpClass, Size> res, OpClass op, unsigned op_num) {
+               unsigned res_off = CSize + op_num * Size;
+               res.copy_to(Output + res_off);
+             });
+         esimd_test::apply_ops(
+             ops, TestValue, InputVector < (Size / 2),
+             [=](comp_t<T, T, OpClass, Size> res, OpClass op, unsigned op_num) {
+               unsigned res_off = 2 * CSize + op_num * Size;
+               res.copy_to(Output + res_off);
+             });
+         esimd_test::apply_ops(
+             ops, InputVector < (Size / 2), TestValue,
+             [=](comp_t<T, T, OpClass, Size> res, OpClass op, unsigned op_num) {
+               unsigned res_off = 3 * CSize + op_num * Size;
+               res.copy_to(Output + res_off);
+             });
+         esimd_test::apply_ops(
+             ops, InputVector < (Size / 2), InputVector < (Size / 2),
+             [=](comp_t<T, T, OpClass, Size> res, OpClass op, unsigned op_num) {
+               unsigned res_off = 4 * CSize + op_num * Size;
+               res.copy_to(Output + res_off);
+             });
+         esimd_test::apply_ops(
+             ops, x.template bit_cast_view<T>(), InputVector < (Size / 2),
+             [=](comp_t<T, T, OpClass, Size> res, OpClass op, unsigned op_num) {
+               unsigned res_off = 5 * CSize + op_num * Size;
+               res.copy_to(Output + res_off);
+             });
+         esimd_test::apply_ops(
+             ops, InputVector < (Size / 2), x.template bit_cast_view<T>(),
+             [=](comp_t<T, T, OpClass, Size> res, OpClass op, unsigned op_num) {
+               unsigned res_off = 6 * CSize + op_num * Size;
+               res.copy_to(Output + res_off);
+             });
+       });
+     }).wait();
+  } catch (sycl::exception const &e) {
+    std::cout << "SYCL exception caught: " << e.what() << '\n';
+    sycl::free(Input, q);
+    sycl::free(Output, q);
+    return false;
+  }
+  bool Result = true;
+  int err_cnt = 0;
 
-    if (Output1[i] != ExpectedValue || Output2[i] != ExpectedValue ||
-        Output3[i] != ExpectedValue || Output4[i] != ExpectedValue) {
-      Passed = false;
+  for (int i = 0; i < CSize * 7; ++i) {
+    if (Output[i] != expectedValues[i]) {
+      Result = false;
+      if (++err_cnt < 10) {
+        std::cout << "  failed at index " << i
+                  << " Expected Value: " << expectedValues[i]
+                  << " Result: " << Output[i] << ".\n";
+      }
     }
   }
+  sycl::free(Input, q);
+  sycl::free(Output, q);
+  return Result;
+}
+
+int main(int, char **) {
+
+  sycl::queue q;
+  int BinOpExpectedValues[] = {3, 2, 1, 2, 2, 0, 3,  2,  -1, -2, 2,  0,  3, 2,
+                               1, 2, 2, 0, 3, 2, -1, -2, 2,  0,  2,  0,  0, 0,
+                               1, 0, 3, 2, 1, 2, 2,  0,  3,  2,  -1, -2, 2, 0};
+  int CmpOpExpectedValues[] = {
+      0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+      1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0,
+      0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0};
+  int IntBinOpExpectedValues[] = {
+      3, 2, 1, 2, 2, 0, 3, 2, 0, 0, 3, 2,  3,  2, -1, -2, 2, 0,  3,  2, 0,
+      0, 3, 2, 3, 2, 1, 2, 2, 0, 3, 2, 0,  0,  3, 2,  3,  2, -1, -2, 2, 0,
+      3, 2, 0, 0, 3, 2, 2, 0, 0, 0, 1, 0,  1,  0, 1,  0,  0, 0,  3,  2, 1,
+      2, 2, 0, 3, 2, 0, 0, 3, 2, 3, 2, -1, -2, 2, 0,  3,  2, 0,  0,  3, 2};
+  bool Passed = true;
+  auto arith_ops = esimd_test::ArithBinaryOpsNoDiv;
+  Passed &=
+      test<float, esimd_test::BinaryOp>(arith_ops, q, BinOpExpectedValues);
+  Passed &=
+      test<uint32_t, esimd_test::BinaryOp>(arith_ops, q, BinOpExpectedValues);
+
+  auto int_arith_ops = esimd_test::IntBinaryOpsNoShiftNoDivRem;
+
+  Passed &= test<int32_t, esimd_test::BinaryOp>(int_arith_ops, q,
+                                                IntBinOpExpectedValues);
+  auto cmp_ops = esimd_test::CmpOps;
+  Passed &= test<uint16_t, esimd_test::CmpOp>(cmp_ops, q, CmpOpExpectedValues);
+
   if (!Passed) {
     std::cout << "Test failed." << std::endl;
   } else {
     std::cout << "Test passed." << std::endl;
   }
-  free(Input, q);
-  free(Output1, q);
-  free(Output2, q);
-  free(Output3, q);
-  free(Output4, q);
 
   return Passed ? 0 : 1;
 }
