7902d6b3534867294e7176901b78d1f078eaa367
Evgeniy <evgeniy.tyurin@intel.com>
2023-05-05 06:42:30 -0700

    [SYCL][ESIMD] Adjust static restriction checks for block_2d APIs (#9295)
    Patch fixes several bugs in the restrictions checks, alignes the checks
    with documentation requirements, and removes duplicating checks from the
    test.

--- sycl/test/esimd/check_lsc.cpp
+++ sycl/test/esimd/check_lsc.cpp
@@ -2,7 +2,7 @@
 // RUN: not %clangxx %fsycl-host-only -fsyntax-only -Wno-unused-command-line-argument %s 2>&1 | FileCheck %s --implicit-check-not="error:"
 
 // This test checks that both host and device compilers can:
-// - successfully compile lsc_load2d/lsc_store2d APIs
+// - successfully compile lsc_load_2d/lsc_store_2d APIs
 // - emit an error if some of the restrictions on template parameters are
 //   violated
 
@@ -23,9 +23,9 @@ template <class T, int BLOCK_WIDTH, int BLOCK_HEIGHT, int NUM_BLOCKS,
               T, NUM_BLOCKS, BLOCK_HEIGHT, BLOCK_WIDTH, TRANSPOSE, TRANSFORM>()>
 SYCL_EXTERNAL auto test_load(T *ptr, int width, int height,
                              int pitch) SYCL_ESIMD_FUNCTION {
-  return lsc_load2d<T, BLOCK_WIDTH, BLOCK_HEIGHT, NUM_BLOCKS, TRANSPOSE,
-                    TRANSFORM, L1H, L3H>(ptr, width * sizeof(T) - 1, height - 1,
-                                         pitch * sizeof(T) - 1, 0, 0);
+  return lsc_load_2d<T, BLOCK_WIDTH, BLOCK_HEIGHT, NUM_BLOCKS, TRANSPOSE,
+                     TRANSFORM, L1H, L3H>(
+      ptr, width * sizeof(T) - 1, height - 1, pitch * sizeof(T) - 1, 0, 0);
 }
 
 template <class T, int BLOCK_WIDTH, int BLOCK_HEIGHT, int NUM_BLOCKS,
@@ -34,7 +34,7 @@ template <class T, int BLOCK_WIDTH, int BLOCK_HEIGHT, int NUM_BLOCKS,
               T, NUM_BLOCKS, BLOCK_HEIGHT, BLOCK_WIDTH, false, false>()>
 SYCL_EXTERNAL void test_store(T *ptr, simd<T, N> v, int width, int height,
                               int pitch) SYCL_ESIMD_FUNCTION {
-  lsc_store2d<T, BLOCK_WIDTH, BLOCK_HEIGHT, L1H, L3H>(
+  lsc_store_2d<T, BLOCK_WIDTH, BLOCK_HEIGHT, L1H, L3H>(
       ptr, width * sizeof(T) - 1, height - 1, pitch * sizeof(T) - 1, 0, 0, v);
 }
 
@@ -45,8 +45,8 @@ test_load<float, 16, 16, 1, false, false, cache_hint::none, cache_hint::none>(
     float *, int, int, int) SYCL_ESIMD_FUNCTION;
 
 constexpr int N16_8 =
-    __ESIMD_EDNS::get_lsc_block_2d_data_size<float, 1, 16, 8, false, false>();
-template void test_store<float, 8, 16, 1, cache_hint::none, cache_hint::none>(
+    __ESIMD_EDNS::get_lsc_block_2d_data_size<float, 1, 8, 16, false, false>();
+template void test_store<float, 16, 8, 1, cache_hint::none, cache_hint::none>(
     float *, simd<float, N16_8>, int, int, int) SYCL_ESIMD_FUNCTION;
 
 // --- Negative tests.
@@ -62,3 +62,4 @@ template void test_store<float, 16, 16, 1, cache_hint::none, cache_hint::none>(
     float *, simd<float, N16_16>, int, int, int) SYCL_ESIMD_FUNCTION;
 // CHECK: {{.*}}error: {{.*}}Unsupported block width
 // CHECK: {{.*}}error: {{.*}}2D store supports 512 bytes max
+// CHECK: {{.*}}error: {{.*}}Unsupported block height for store
