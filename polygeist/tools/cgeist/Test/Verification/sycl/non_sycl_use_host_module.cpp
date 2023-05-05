4ec35b41ad6e5424d0cb08f0145461bd10aa6192
Victor Perez <victor.perez@codeplay.com>
2023-05-05 09:37:55 +0100

    Remove empty line

--- polygeist/tools/cgeist/Test/Verification/sycl/non_sycl_use_host_module.cpp
+++ polygeist/tools/cgeist/Test/Verification/sycl/non_sycl_use_host_module.cpp
@@ -1,4 +1,3 @@
-
 // RUN: not cgeist -sycl-use-host-module=%S/host_module.mlir -O0 -w -o - %s 2>&1 | FileCheck %s
 
 // CHECK: "-sycl-use-host-module" can only be used during SYCL device compilation
