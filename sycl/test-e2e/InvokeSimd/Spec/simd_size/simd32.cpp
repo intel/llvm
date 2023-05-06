5df3bd415743bbf2d5cebe5ee8cdf7356f141dc7
aelovikov-intel <andrei.elovikov@intel.com>
2023-05-06 12:05:06 -0700

    [SYCL][Test E2E] Use %{build}/%{run} in InvokeSimd tests (#9329)

--- sycl/test-e2e/InvokeSimd/Spec/simd_size/simd32.cpp
+++ sycl/test-e2e/InvokeSimd/Spec/simd_size/simd32.cpp
@@ -4,11 +4,11 @@
 // UNSUPPORTED: cuda || hip
 //
 // Check that full compilation works:
-// RUN: %clangxx -fsycl -fno-sycl-device-code-split-esimd -Xclang -fsycl-allow-func-ptr %s -o %t.out
-// RUN: env IGC_VCSaveStackCallLinkage=1 IGC_VCDirectCallsOnly=1 %GPU_RUN_PLACEHOLDER %t.out
+// RUN: %{build} -fno-sycl-device-code-split-esimd -Xclang -fsycl-allow-func-ptr -o %t.out
+// RUN: env IGC_VCSaveStackCallLinkage=1 IGC_VCDirectCallsOnly=1 %{run} %t.out
 //
 // VISALTO enable run
-// RUN: env IGC_VISALTO=63 IGC_VCSaveStackCallLinkage=1 IGC_VCDirectCallsOnly=1 %GPU_RUN_PLACEHOLDER %t.out
+// RUN: env IGC_VISALTO=63 IGC_VCSaveStackCallLinkage=1 IGC_VCDirectCallsOnly=1 %{run} %t.out
 
 /* Tests invoke_simd support in the compiler/headers
  * Test checks support for different subgroup sizes in combination with
