1208df513a70d8cba86370bfd9ce8df5b1afbf19
Lukas Sommer <lukas.sommer@codeplay.com>
2023-05-08 15:29:54 +0100

    Update tests
    Signed-off-by: Lukas Sommer <lukas.sommer@codeplay.com>

--- polygeist/tools/cgeist/Test/Verification/base_with_virt.cpp
+++ polygeist/tools/cgeist/Test/Verification/base_with_virt.cpp
@@ -35,16 +35,16 @@ void a() {
 
 // CHECK-LABEL:   func.func @_Z1av() attributes {llvm.linkage = #llvm.linkage<external>} {
 // CHECK-NEXT:      %[[VAL_0:.*]] = arith.constant 1 : i64
-// CHECK-NEXT:      %[[VAL_1:.*]] = llvm.alloca %[[VAL_0]] x !llvm.struct<(struct<packed (ptr<ptr<func<i32 (...)>>>, i32, array<4 x i8>)>, struct<(ptr<i8>)>)> : (i64) -> !llvm.ptr
+// CHECK-NEXT:      %[[VAL_1:.*]] = llvm.alloca %[[VAL_0]] x !llvm.struct<(struct<packed (ptr, i32, array<4 x i8>)>, struct<(ptr)>)> : (i64) -> !llvm.ptr
 // CHECK-NEXT:      call @_ZN16mbasic_stringbufC1Ev(%[[VAL_1]]) : (!llvm.ptr) -> ()
 // CHECK-NEXT:      return
 // CHECK-NEXT:    }
 
 // CHECK-LABEL:   func.func @_ZN16mbasic_stringbufC1Ev(
 // CHECK-SAME:                                         %[[VAL_0:.*]]: !llvm.ptr) attributes {llvm.linkage = #llvm.linkage<linkonce_odr>} {
-// CHECK-NEXT:      %[[VAL_1:.*]] = llvm.getelementptr inbounds %[[VAL_0]][0, 0] : (!llvm.ptr) -> !llvm.ptr, !llvm.struct<(struct<packed (ptr<ptr<func<i32 (...)>>>, i32, array<4 x i8>)>, struct<(ptr<i8>)>)>
+// CHECK-NEXT:      %[[VAL_1:.*]] = llvm.getelementptr inbounds %[[VAL_0]][0, 0] : (!llvm.ptr) -> !llvm.ptr, !llvm.struct<(struct<packed (ptr, i32, array<4 x i8>)>, struct<(ptr)>)>
 // CHECK-NEXT:      call @_ZN1AC1Ev(%[[VAL_1]]) : (!llvm.ptr) -> ()
-// CHECK-NEXT:      %[[VAL_2:.*]] = llvm.getelementptr inbounds %[[VAL_0]][0, 1] : (!llvm.ptr) -> !llvm.ptr, !llvm.struct<(struct<packed (ptr<ptr<func<i32 (...)>>>, i32, array<4 x i8>)>, struct<(ptr<i8>)>)>
+// CHECK-NEXT:      %[[VAL_2:.*]] = llvm.getelementptr inbounds %[[VAL_0]][0, 1] : (!llvm.ptr) -> !llvm.ptr, !llvm.struct<(struct<packed (ptr, i32, array<4 x i8>)>, struct<(ptr)>)>
 // CHECK-NEXT:      call @_ZN12_Alloc_hiderC1Ev(%[[VAL_2]]) : (!llvm.ptr) -> ()
 // CHECK-NEXT:      return
 // CHECK-NEXT:    }
@@ -52,7 +52,7 @@ void a() {
 // CHECK-LABEL:   func.func @_ZN1AC1Ev(
 // CHECK-SAME:                         %[[VAL_0:.*]]: !llvm.ptr) attributes {llvm.linkage = #llvm.linkage<linkonce_odr>} {
 // CHECK-NEXT:      %[[VAL_1:.*]] = arith.constant 3 : i32
-// CHECK-NEXT:      %[[VAL_2:.*]] = llvm.getelementptr inbounds %[[VAL_0]][0, 1] : (!llvm.ptr) -> !llvm.ptr, !llvm.struct<packed (ptr<ptr<func<i32 (...)>>>, i32, array<4 x i8>)>
+// CHECK-NEXT:      %[[VAL_2:.*]] = llvm.getelementptr inbounds %[[VAL_0]][0, 1] : (!llvm.ptr) -> !llvm.ptr, !llvm.struct<packed (ptr, i32, array<4 x i8>)>
 // CHECK-NEXT:      llvm.store %[[VAL_1]], %[[VAL_2]] : i32, !llvm.ptr
 // CHECK-NEXT:      return
 // CHECK-NEXT:    }
