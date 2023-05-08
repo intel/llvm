fe27231eefb7bd04f967e094be4624f183505ce0
Sarnie, Nick <nick.sarnie@intel.com>
2023-05-08 12:32:40 -0700

    [SYCL][ESIMD][E2E] Mark ESIMD InlineAsm tests unsupported on gen9 win
    All ESIMD tests already don't support gen9 win but I forgot to add it when making these tests.
    
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- sycl/test-e2e/ESIMD/InlineAsm/asm_vadd.cpp
+++ sycl/test-e2e/ESIMD/InlineAsm/asm_vadd.cpp
@@ -8,6 +8,7 @@
 //===----------------------------------------------------------------------===//
 // REQUIRES: gpu
 // UNSUPPORTED: cuda || hip
+// UNSUPPORTED: gpu-intel-gen9 && windows
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 
