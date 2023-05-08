d5494f933b1cde58c18bd4e9470eadc6de95a8f5
aelovikov-intel <andrei.elovikov@intel.com>
2023-05-08 07:16:26 -0700

    [SYCL][Test E2E] Use %{build}/%{run} in Sampler tests (#9323)

--- sycl/test-e2e/Sampler/basic-rw-float.cpp
+++ sycl/test-e2e/Sampler/basic-rw-float.cpp
@@ -1,8 +1,7 @@
 // REQUIRES: aspect-ext_intel_legacy_image
 // UNSUPPORTED: hip
-// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
-// RUN: %CPU_RUN_PLACEHOLDER %t.out
-// RUN: %GPU_RUN_PLACEHOLDER %t.out
+// RUN: %{build} -o %t.out
+// RUN: %{run} %t.out
 
 #include "common.hpp"
 #include <sycl/sycl.hpp>
