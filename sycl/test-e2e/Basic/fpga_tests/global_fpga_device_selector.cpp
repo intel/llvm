7383af5578ae03a2dd248f9771bf8f73ab2b2b4f
Artur Gainullin <artur.gainullin@intel.com>
2023-05-09 14:02:33 -0700

    [SYCL] Fix compilation line in fpga tests (#9379)
    Current version results in "The option -fsycl-targets= conflicts with
    -fintelfpga" error.

--- sycl/test-e2e/Basic/fpga_tests/global_fpga_device_selector.cpp
+++ sycl/test-e2e/Basic/fpga_tests/global_fpga_device_selector.cpp
@@ -1,6 +1,6 @@
 // REQUIRES: opencl-aot, accelerator
 
-// RUN: %{build} -fintelfpga -o %t.out
+// RUN: %clangxx -fsycl -fintelfpga %s -o %t.out
 // RUN: %{run} %t.out
 
 #include <sycl/ext/intel/fpga_extensions.hpp>
