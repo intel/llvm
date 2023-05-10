0faa9fa2d3b8b7b01776ad5a7dcc60a2357734d5
Bing1 Yu <bing1.yu@intel.com>
2023-05-10 14:29:11 +0800

    remove -O1

--- sycl/test-e2e/ESIMD/regression/dgetrf.cpp
+++ sycl/test-e2e/ESIMD/regression/dgetrf.cpp
@@ -9,7 +9,7 @@
 // REQUIRES: gpu
 // UNSUPPORTED: gpu-intel-gen9 && windows
 // UNSUPPORTED: cuda || hip
-// RUN: %{build} -I%S/.. -O1 -fno-inline-functions -o %t.out
+// RUN: %{build} -I%S/.. -fno-inline-functions -o %t.out
 // RUN: %{run} %t.out 3 2 1
 //
 // This test checks the correctness of ESIMD program for batched LU
