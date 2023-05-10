d8062ff3e9b58a96965292ca3f1eab550d8b997d
jinge90 <ge.jin@intel.com>
2023-05-10 13:12:18 +0800

    add float2bfloat16 wrappers

--- sycl/include/sycl/ext/intel/math.hpp
+++ sycl/include/sycl/ext/intel/math.hpp
@@ -1,4 +1,4 @@
-//==------------- math.hpp - Intel specific math API -----------------------==//
+//==------------- math.hpp - Intel specific math API-----------------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -12,6 +12,7 @@
 #include <sycl/builtins.hpp>
 #include <sycl/ext/intel/math/imf_half_trivial.hpp>
 #include <sycl/ext/intel/math/imf_simd.hpp>
+#include <sycl/ext/oneapi/bfloat16.hpp>
 #include <sycl/half_type.hpp>
 #include <type_traits>
 
@@ -50,6 +51,66 @@ _iml_half_internal __imf_rsqrtf16(_iml_half_internal);
 float __imf_truncf(float);
 double __imf_trunc(double);
 _iml_half_internal __imf_truncf16(_iml_half_internal);
+// bfloat16 conversions using software emulation
+float __imf_bfloat162float(uint16_t x);
+unsigned int __imf_bfloat162uint_rd(uint16_t);
+unsigned int __imf_bfloat162uint_rn(uint16_t);
+unsigned int __imf_bfloat162uint_ru(uint16_t);
+unsigned int __imf_bfloat162uint_rz(uint16_t);
+unsigned short __imf_bfloat162ushort_rd(uint16_t);
+unsigned short __imf_bfloat162ushort_rn(uint16_t);
+unsigned short __imf_bfloat162ushort_ru(uint16_t);
+unsigned short __imf_bfloat162ushort_rz(uint16_t);
+unsigned long long __imf_bfloat162ull_rd(uint16_t);
+unsigned long long __imf_bfloat162ull_rn(uint16_t);
+unsigned long long __imf_bfloat162ull_ru(uint16_t);
+unsigned long long __imf_bfloat162ull_rz(uint16_t);
+int __imf_bfloat162int_rd(uint16_t);
+int __imf_bfloat162int_rn(uint16_t);
+int __imf_bfloat162int_ru(uint16_t);
+int __imf_bfloat162int_rz(uint16_t);
+short __imf_bfloat162short_rd(uint16_t);
+short __imf_bfloat162short_rn(uint16_t);
+short __imf_bfloat162short_ru(uint16_t);
+short __imf_bfloat162short_rz(uint16_t);
+long long __imf_bfloat162ll_rd(uint16_t);
+long long __imf_bfloat162ll_rn(uint16_t);
+long long __imf_bfloat162ll_ru(uint16_t);
+long long __imf_bfloat162ll_rz(uint16_t);
+uint16_t __imf_float2bfloat16(float);
+uint16_t __imf_float2bfloat16_rd(float);
+uint16_t __imf_float2bfloat16_rn(float);
+uint16_t __imf_float2bfloat16_ru(float);
+uint16_t __imf_float2bfloat16_rz(float);
+uint16_t __imf_ushort2bfloat16_rd(unsigned short);
+uint16_t __imf_ushort2bfloat16_rn(unsigned short);
+uint16_t __imf_ushort2bfloat16_ru(unsigned short);
+uint16_t __imf_ushort2bfloat16_rz(unsigned short);
+uint16_t __imf_uint2bfloat16_rd(unsigned int);
+uint16_t __imf_uint2bfloat16_rn(unsigned int);
+uint16_t __imf_uint2bfloat16_ru(unsigned int);
+uint16_t __imf_uint2bfloat16_rz(unsigned int);
+uint16_t __imf_ull2bfloat16_rd(unsigned long long);
+uint16_t __imf_ull2bfloat16_rn(unsigned long long);
+uint16_t __imf_ull2bfloat16_ru(unsigned long long);
+uint16_t __imf_ull2bfloat16_rz(unsigned long long);
+uint16_t __imf_short2bfloat16_rd(short);
+uint16_t __imf_short2bfloat16_rn(short);
+uint16_t __imf_short2bfloat16_ru(short);
+uint16_t __imf_short2bfloat16_rz(short);
+uint16_t __imf_int2bfloat16_rd(int);
+uint16_t __imf_int2bfloat16_rn(int);
+uint16_t __imf_int2bfloat16_ru(int);
+uint16_t __imf_int2bfloat16_rz(int);
+uint16_t __imf_ll2bfloat16_rd(long long);
+uint16_t __imf_ll2bfloat16_rn(long long);
+uint16_t __imf_ll2bfloat16_ru(long long);
+uint16_t __imf_ll2bfloat16_rz(long long);
+uint16_t __imf_double2bfloat16(double);
+short __imf_bfloat16_as_short(uint16_t);
+unsigned short __imf_bfloat16_as_ushort(uint16_t);
+uint16_t __imf_short_as_bfloat16(short);
+uint16_t __imf_ushort_as_bfloat16(unsigned short);
 };
 
 namespace sycl {
@@ -227,6 +288,93 @@ std::enable_if_t<std::is_same_v<Tp, sycl::half>, sycl::half> trunc(Tp x) {
 template <typename Tp>
 std::enable_if_t<std::is_same_v<Tp, sycl::half2>, sycl::half2> trunc(Tp x) {
   return sycl::half2{trunc(x.s0()), trunc(x.s1())};
+}
+
+template <typename To, typename From>
+static std::enable_if_t<std::is_same_v<To, sycl::ext::oneapi::bfloat16>,
+                        sycl::ext::oneapi::bfloat16>
+__internal_fp_convert_rd(
+    std::enable_if_t<std::is_same_v<From, float>, float> x) {
+  return __builtin_bit_cast(sycl::ext::oneapi::bfloat16,
+                            __imf_float2bfloat16_rd(x));
+}
+
+template <typename To, typename From>
+static std::enable_if_t<std::is_same_v<To, sycl::ext::oneapi::bfloat16>,
+                        sycl::ext::oneapi::bfloat16>
+__internal_fp_convert_rn(
+    std::enable_if_t<std::is_same_v<From, float>, float> x) {
+  return __builtin_bit_cast(sycl::ext::oneapi::bfloat16,
+                            __imf_float2bfloat16_rn(x));
+}
+
+template <typename To, typename From>
+static std::enable_if_t<std::is_same_v<To, sycl::ext::oneapi::bfloat16>,
+                        sycl::ext::oneapi::bfloat16>
+__internal_fp_convert_ru(
+    std::enable_if_t<std::is_same_v<From, float>, float> x) {
+  return __builtin_bit_cast(sycl::ext::oneapi::bfloat16,
+                            __imf_float2bfloat16_ru(x));
+}
+
+template <typename To, typename From>
+static std::enable_if_t<std::is_same_v<To, sycl::ext::oneapi::bfloat16>,
+                        sycl::ext::oneapi::bfloat16>
+__internal_fp_convert_rz(
+    std::enable_if_t<std::is_same_v<From, float>, float> x) {
+  return __builtin_bit_cast(sycl::ext::oneapi::bfloat16,
+                            __imf_float2bfloat16_rz(x));
+}
+
+float bfloat162float(sycl::ext::oneapi::bfloat16 x) {
+  return __imf_bfloat162float(__builtin_bit_cast(uint16_t, x));
+}
+
+template <typename Tp = sycl::ext::oneapi::bfloat16>
+Tp float2bfloat16(float x) {
+  return __internal_fp_convert_rn<
+      std::enable_if_t<std::is_same_v<Tp, sycl::ext::oneapi::bfloat16>,
+                       sycl::ext::oneapi::bfloat16>,
+      float>(x);
+}
+
+template <typename Tp = sycl::ext::oneapi::bfloat16>
+Tp float2bfloat16_rd(float x) {
+  return __internal_fp_convert_rd<
+      std::enable_if_t<std::is_same_v<Tp, sycl::ext::oneapi::bfloat16>,
+                       sycl::ext::oneapi::bfloat16>,
+      float>(x);
+}
+
+template <typename Tp = sycl::ext::oneapi::bfloat16>
+Tp float2bfloat16_rn(float x) {
+  return __internal_fp_convert_rn<
+      std::enable_if_t<std::is_same_v<Tp, sycl::ext::oneapi::bfloat16>,
+                       sycl::ext::oneapi::bfloat16>,
+      float>(x);
+}
+
+template <typename Tp = sycl::ext::oneapi::bfloat16>
+Tp float2bfloat16_ru(float x) {
+  return __internal_fp_convert_ru<
+      std::enable_if_t<std::is_same_v<Tp, sycl::ext::oneapi::bfloat16>,
+                       sycl::ext::oneapi::bfloat16>,
+      float>(x);
+}
+
+template <typename Tp = sycl::ext::oneapi::bfloat16>
+Tp float2bfloat16_rz(float x) {
+  return __internal_fp_convert_rz<
+      std::enable_if_t<std::is_same_v<Tp, sycl::ext::oneapi::bfloat16>,
+                       sycl::ext::oneapi::bfloat16>,
+      float>(x);
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, sycl::ext::oneapi::bfloat16>,
+                 sycl::ext::oneapi::bfloat16>
+float2bfloat16_rz(float x) {
+  return __internal_fp_convert_rz<sycl::ext::oneapi::bfloat16, float>(x);
 }
 
 } // namespace ext::intel::math
