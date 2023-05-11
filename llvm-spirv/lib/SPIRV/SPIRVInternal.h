19035db9c16714b42356c9c41fa6763ff329853a
Nick Sarnie <sarnex@users.noreply.github.com>
2023-05-11 19:45:29 -0400

    Support the spirv.BufferSurfaceINTEL target extension type (#1995)
    This target extension type is created here: https://github.com/intel/vc-intrinsics/blob/master/GenXIntrinsics/lib/GenXIntrinsics/GenXSPIRVWriterAdaptor.cpp#L245
    
    As with other target extension types, reverse translation is not yet supported.
    
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>
    Co-authored-by: Victor Mustya <victor.mustya@intel.com>
    
    Original commit:
    https://github.com/KhronosGroup/SPIRV-LLVM-Translator/commit/60746d5

--- llvm-spirv/lib/SPIRV/SPIRVInternal.h
+++ llvm-spirv/lib/SPIRV/SPIRVInternal.h
@@ -311,6 +311,7 @@ const static char PipeStorage[] = "PipeStorage";
 const static char ConstantPipeStorage[] = "ConstantPipeStorage";
 const static char VmeImageINTEL[] = "VmeImageINTEL";
 const static char JointMatrixINTEL[] = "JointMatrixINTEL";
+const static char BufferSurfaceINTEL[] = "BufferSurfaceINTEL";
 } // namespace kSPIRVTypeName
 
 namespace kSPR2TypeName {
@@ -954,6 +955,7 @@ template <> inline void SPIRVMap<std::string, Op, SPIRVOpaqueType>::init() {
   _SPIRV_OP(AvcRefResultINTEL)
   _SPIRV_OP(AvcSicResultINTEL)
   _SPIRV_OP(VmeImageINTEL)
+  _SPIRV_OP(BufferSurfaceINTEL)
 #undef _SPIRV_OP
   add("JointMatrixINTEL", internal::OpTypeJointMatrixINTEL);
 }
