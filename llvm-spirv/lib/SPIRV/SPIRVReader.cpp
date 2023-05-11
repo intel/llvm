77b1d845e0d0919c5138b3151904a8ea2346d661
Nick Sarnie <sarnex@users.noreply.github.com>
2023-05-11 08:14:22 -0400

    Add option to control builtin format for reverse translation (#1986)
    Currently, we always convert SPIR-V bultins to globals for forward translation and to functions for reverse translation.
    
    I have a use case where I want to keep them as globals for reverse translation, so I added this mode.
    
    Implementations for both cases already existed, I just consolidated them and added the option.
    
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>
    
    Original commit:
    https://github.com/KhronosGroup/SPIRV-LLVM-Translator/commit/730eaf0

--- llvm-spirv/lib/SPIRV/SPIRVReader.cpp
+++ llvm-spirv/lib/SPIRV/SPIRVReader.cpp
@@ -3341,14 +3341,7 @@ bool SPIRVToLLVM::translate() {
   if (!transSourceExtension())
     return false;
   transGeneratorMD();
-  // TODO: add an option to control the builtin format in SPV-IR.
-  // The primary format should be function calls:
-  //   e.g. call spir_func i32 @_Z29__spirv_BuiltInGlobalLinearIdv()
-  // The secondary format should be global variables:
-  //   e.g. load i32, i32* @__spirv_BuiltInGlobalLinearId, align 4
-  // If the desired format is global variables, we don't have to lower them
-  // as calls.
-  if (!lowerBuiltinVariablesToCalls(M))
+  if (!lowerBuiltins(BM, M))
     return false;
   if (BM->getDesiredBIsRepresentation() == BIsRepresentation::SPIRVFriendlyIR) {
     SPIRVWord SrcLangVer = 0;
