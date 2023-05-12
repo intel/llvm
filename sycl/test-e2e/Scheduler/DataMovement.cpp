09caf67ee81c8a782bff03a9381290bf99f670f1
aelovikov-intel <andrei.elovikov@intel.com>
2023-05-11 21:35:37 -0700

    [SYCL][Test E2E] Remove some UNSUPPORTED: accelerator directives (#9426)
    I tested those tests locally on `opencl:acc` device (which we don't have
    in GitHub CI, only in internal testing) on both Windows/Linux.

--- sycl/test-e2e/Scheduler/DataMovement.cpp
+++ sycl/test-e2e/Scheduler/DataMovement.cpp
@@ -1,8 +1,6 @@
 // RUN: %{build} -o %t.out %debug_option
 // RUN: %{run} %t.out
 //
-// UNSUPPORTED: accelerator
-//
 //==-------------------------- DataMovement.cpp ----------------------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
