bced7f577827c3395d65441e94ba2154fc9b24f7
Elizabeth Andrews <elizabeth.andrews@intel.com>
2023-05-19 14:35:38 -0700

    Add more tests

--- clang/test/SemaSYCL/virtual-function.cpp
+++ clang/test/SemaSYCL/virtual-function.cpp
@@ -1,10 +1,7 @@
 // RUN: %clang_cc1 -fsycl-is-device -fsycl-allow-virtual-functions -internal-isystem %S/Inputs -verify -fsyntax-only %s
-// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -verify=novirtualfunction -fsyntax-only %s
 
 // This test verifies that pure virtual functions are not
 // diagnosed if undefined in base class.
-
-// expected-no-diagnostics
 
 #include "sycl.hpp"
 
@@ -12,19 +9,35 @@ sycl::queue deviceQueue;
 
 class AbstractClass {
   public:
-  virtual void testVF() = 0;
+  virtual void testVF() = 0; //expected-note{{unimplemented pure virtual method 'testVF' in 'Derived2'}}
 };
 
-class Derived : public AbstractClass {
+class Derived1 : public AbstractClass {
     void testVF() {}
+};
+
+class Derived2 : public AbstractClass {
+};
+
+class Derived3 : public AbstractClass {
+    void testVF();
 };
 
 void foo() {
   deviceQueue.submit([&](sycl::handler &h) {
     h.single_task<class CallToUndefinedFnTester>([]() {
-      Derived d;
-      AbstractClass* obj = &d;
-      obj->testVF(); // novirtualfunction-error {{SYCL kernel cannot call a virtual function}}
+      Derived1 d1;
+      AbstractClass* obj1 = &d1;
+      obj1->testVF(); // No diagnostic
+
+      Derived2 d2; // expected-error{{variable type 'Derived2' is an abstract class}}
+
+      Derived3 d3;
+      AbstractClass* obj2 = &d3;
+      // Ideally we would want to diagnose the missing SYCL_EXTERNAL macro here since
+      // there is no definition for this function in this TU. However it is expensive
+      // to do so in the frontend, and a diagnostic is not currently emitted.
+      obj2->testVF();
     });
   });
 }
