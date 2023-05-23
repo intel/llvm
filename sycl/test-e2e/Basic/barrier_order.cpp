6718a9d60c4d2c6316dfd1f229e087539996d97c
aelovikov-intel <andrei.elovikov@intel.com>
2023-05-23 08:17:51 -0700

    Update sycl/test-e2e/Basic/barrier_order.cpp

--- sycl/test-e2e/Basic/barrier_order.cpp
+++ sycl/test-e2e/Basic/barrier_order.cpp
@@ -2,7 +2,8 @@
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 
-// Hangs on that platform (at least on gpu-intel-pvc), https://github.com/intel/llvm/issues/7330.
+// Hangs on that platform (at least on gpu-intel-pvc),
+// https://github.com/intel/llvm/issues/7330.
 // UNSUPPORTED: opencl && gpu
 
 #include <iostream>
