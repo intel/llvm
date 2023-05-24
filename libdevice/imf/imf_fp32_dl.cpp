97126c4ee6c559c5a15adca0419e9bb1d8e4dce1
jinge90 <ge.jin@intel.com>
2023-05-24 21:02:41 +0800

    adjust file header
    Signed-off-by: jinge90 <ge.jin@intel.com>

--- libdevice/imf/imf_fp32_dl.cpp
+++ libdevice/imf/imf_fp32_dl.cpp
@@ -1,16 +1,16 @@
-//==---------------------- imf_fp32_dl.cpp - subset of fp32 math functions
-// required by deep learning frameworks --------------------==//
+//===----------- imf_fp32_dl.cpp - fp32 functions required by DL ----------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+/// Subset of intel math functions is required by deep learning frameworks and
+/// we decide to keep all functions required in a separate file and build a new
+/// spirv module using this file. By this way, we can reduce unnecessary jit
+/// overhead in these deep learning frameworks.
+//===----------------------------------------------------------------------===//
 
-// Subset of intel math functions is required by deep learning frameworks and we
-// decide to keep all functions required in a separate file and build a new
-// spirv module using this file. By this way, we can reduce unnecessary jit
-// overhead in these deep learning frameworks.
 #include "../device_imf.hpp"
 #ifdef __LIBDEVICE_IMF_ENABLED__
 
