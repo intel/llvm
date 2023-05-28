19be7ab78079fac5224c09758f347a59d8e4e5fe
Arvind Sudarsanam <arvind.sudarsanam@intel.com>
2023-05-28 08:55:16 -0700

    Fix windows test fail
    Signed-off-by: Arvind Sudarsanam <arvind.sudarsanam@intel.com>

--- clang/test/Driver/clang-offload-bundler.c
+++ clang/test/Driver/clang-offload-bundler.c
@@ -573,12 +573,6 @@
 // RUN: llvm-ar t %T/hip-openmp_906.a | FileCheck -check-prefix=OPENMPHIPCOMPAT %s
 // OPENMPHIPCOMPAT: hip_bundle1-hip-amdgcn-amd-amdhsa--gfx906
 
-// Check working of bundler before and after standardization
-// RUN: clang-offload-bundler -type=o -targets=host-x86_64-unknown-linux-gnu,sycl-spir64-unknown-unknown -input=%S/Inputs/bundles/bundle_bef_standardization_of_target_triple.o -output=test-host-x86_64-unknown-linux-gnu.o -output=test-sycl-spir64-unknown-unknown.o -unbundle 2>&1 | FileCheck %s -check-prefix=CHECK-STD-OLD --allow-empty
-// CHECK-STD-OLD-NOT: error: Can't find bundles for
-// RUN: clang-offload-bundler -type=o -targets=host-x86_64-unknown-linux-gnu,sycl-spir64-unknown-unknown -input=%S/Inputs/bundles/bundle_aft_standardization_of_target_triple.o -output=test-host-x86_64-unknown-linux-gnu.o -output=test-sycl-spir64-unknown-unknown.o -unbundle 2>&1 | FileCheck %s -check-prefix=CHECK-STD-NEW --allow-empty
-// CHECK-STD-NEW-NOT: error: Can't find bundles for
-
 // Some code so that we can create a binary out of this file.
 int A = 0;
 void test_func(void) {
