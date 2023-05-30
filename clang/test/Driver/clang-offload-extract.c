d9ade5b75bb0f53b877356de8aed4e0c9eebc17d
Dirk MG Seynhaeve <dirk.seynhaeve@intel.com>
2023-05-30 10:37:04 -0700

    Update expected help message

--- clang/test/Driver/clang-offload-extract.c
+++ clang/test/Driver/clang-offload-extract.c
@@ -5,17 +5,29 @@
 //
 // RUN: clang-offload-extract --help | FileCheck %s --check-prefix CHECK-HELP
 
-// CHECK-HELP: OVERVIEW: A tool for extracting target images from the linked fat offload binary.
+// CHECK-HELP: OVERVIEW:
+// CHECK-HELP: 
+// CHECK-HELP: A utility to extract all the target images from a
+// CHECK-HELP: linked fat binary, and store them in separate files.
+// CHECK-HELP: 
 // CHECK-HELP: USAGE: clang-offload-extract [options] <input file>
+// CHECK-HELP: 
 // CHECK-HELP: OPTIONS:
+// CHECK-HELP: 
 // CHECK-HELP: Generic Options:
-// CHECK-HELP:   --help            - Display available options (--help-hidden for more)
-// CHECK-HELP:   --help-list       - Display list of available options (--help-list-hidden for more)
-// CHECK-HELP:   --version         - Display the version of this program
-// CHECK-HELP: clang-offload-extract options:
-// CHECK-HELP:   --output=<string> - Specifies prefix for the output file(s). Output file name
-// CHECK-HELP:                       is composed from this prefix and the sequential number
-// CHECK-HELP:                       of extracted image appended to the prefix.
+// CHECK-HELP: 
+// CHECK-HELP:   --help          - Display available options (--help-hidden for more)
+// CHECK-HELP:   --help-list     - Display list of available options (--help-list-hidden for more)
+// CHECK-HELP:   --version       - Display the version of this program
+// CHECK-HELP: 
+// CHECK-HELP: Utility-specific options:
+// CHECK-HELP: 
+// CHECK-HELP:   --stem=<string> - Specifies the stem for the output file(s).
+// CHECK-HELP:                     The default stem when not specified is "target.bin".
+// CHECK-HELP:                     The Output file name is composed from this stem and
+// CHECK-HELP:                     the sequential number of each extracted image appended
+// CHECK-HELP:                     to the stem:
+// CHECK-HELP:                       <stem>.<index>
 
 //
 // Create fat offload binary with two embedded target images.
