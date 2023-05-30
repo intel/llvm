35b6c0c56c82d981cbc5662c0c9543e781a5b6e3
Sabianin, Maksim <maksim.sabianin@intel.com>
2023-05-30 04:39:04 -0700

    [SYCL] add fatlib.a in Inputs folder

--- clang/test/Driver/clang-offload-bundler-exclude.c
+++ clang/test/Driver/clang-offload-bundler-exclude.c
@@ -4,34 +4,33 @@
 
 // UNSUPPORTED: system-windows
 
-// RUN: echo "DUMMY IR FILE" > %t-device
-// RUN: echo "DUMMY IR2 FILE" > %t-device2
-// RUN: echo "DUMMY AOCO FILE" > %t-aoco
-// RUN: echo "DUMMY HOST FILE" > %t-host
-// RUN: echo "DUMMY HOST2 FILE" > %t-host2
+// The test uses assembled archive file fatlib.a.
+// The assembly algorithm is the following:
+// echo "DUMMY IR FILE" > device
+// echo "DUMMY IR2 FILE" > device2
+// echo "DUMMY AOCO FILE" > aoco
+// echo "DUMMY HOST FILE" > host
+// echo "DUMMY HOST2 FILE" > host2
+// # Wrap and compile objects
+// clang-offload-wrapper -o=device.bc -host=x86_64-unknown-linux-gnu -target=spir64 -kind=sycl device
+// clang-offload-wrapper -o=device2.bc -host=x86_64-unknown-linux-gnu -target=spir64 -kind=sycl device2
+// clang-offload-wrapper -o=aoco.bc -host=x86_64-unknown-linux-gnu -target=spir64 -kind=sycl aoco
+// clang-offload-wrapper -o=host.bc -host=x86_64-unknown-linux-gnu -target=spir64 -kind=sycl host
+// clang-offload-wrapper -o=host2.bc -host=x86_64-unknown-linux-gnu -target=spir64 -kind=sycl host2
+// llc -filetype=obj -o device.o device.bc
+// llc -filetype=obj -o device2.o device2.bc
+// llc -filetype=obj -o aoco.o aoco.bc
+// llc -filetype=obj -o host.o host.bc
+// llc -filetype=obj -o host2.o host2.bc
+// # Bundle the objects
+// clang-offload-bundler -input=device.o -input=host.o -output=fat_device.o -targets=sycl-spir64_fpga-unknown-unknown,host-x86_64-unknown-linux-gnu -type=o
+// clang-offload-bundler -input=device2.o -input=aoco.o -input=host2.o -output=fat_device_aoco.o -targets=sycl-spir64_fpga-unknown-unknown,sycl-fpga_aoco-intel-unknown,host-x86_64-unknown-linux-gnu -type=o
+// # Create the archive
+// ar cr fatlib.a fat_device.o fat_device_aoco.o
 
-// Wrap and compile objects
-// RUN: clang-offload-wrapper -o=%t-device.bc -host=x86_64-unknown-linux-gnu -target=spir64 -kind=sycl %t-device
-// RUN: clang-offload-wrapper -o=%t-device2.bc -host=x86_64-unknown-linux-gnu -target=spir64 -kind=sycl %t-device2
-// RUN: clang-offload-wrapper -o=%t-aoco.bc -host=x86_64-unknown-linux-gnu -target=spir64 -kind=sycl %t-aoco
-// RUN: clang-offload-wrapper -o=%t-host.bc -host=x86_64-unknown-linux-gnu -target=spir64 -kind=sycl %t-host
-// RUN: clang-offload-wrapper -o=%t-host2.bc -host=x86_64-unknown-linux-gnu -target=spir64 -kind=sycl %t-host2
 
-// RUN: llc -filetype=obj -o %t-device.o %t-device.bc
-// RUN: llc -filetype=obj -o %t-device2.o %t-device2.bc
-// RUN: llc -filetype=obj -o %t-aoco.o %t-aoco.bc
-// RUN: llc -filetype=obj -o %t-host.o %t-host.bc
-// RUN: llc -filetype=obj -o %t-host2.o %t-host2.bc
-
-// Bundle the objects
-// RUN: clang-offload-bundler -input=%t-device.o -input=%t-host.o -output=%t-fat_device.o -targets=sycl-spir64_fpga-unknown-unknown,host-x86_64-unknown-linux-gnu -type=o
-
-// RUN: clang-offload-bundler -input=%t-device2.o -input=%t-aoco.o -input=%t-host2.o -output=%t-fat_device_aoco.o -targets=sycl-spir64_fpga-unknown-unknown,sycl-fpga_aoco-intel-unknown,host-x86_64-unknown-linux-gnu -type=o
-
-// Create the archive
-// RUN: ar cr %t-fatlib.a %t-fat_device.o %t-fat_device_aoco.o
-
-// RUN: clang-offload-bundler -type=aoo -excluded-targets=sycl-fpga_aoco-intel-unknown -targets=sycl-spir64_fpga-unknown-unknown -input=%t-fatlib.a -output=%t-my_output.txt -unbundle -allow-missing-bundles
+// Unbundle archive
+// RUN: clang-offload-bundler -type=aoo -excluded-targets=sycl-fpga_aoco-intel-unknown -targets=sycl-spir64_fpga-unknown-unknown -input=%S/Inputs/clang-offload-bundler-exclude/fatlib.a -output=%t-my_output.txt -unbundle -allow-missing-bundles
 
 // Check that output of unbundling doesn't contain content of device2
 // RUN: cat %t-my_output.txt | xargs cat | strings | not grep "DUMMY IR2"
