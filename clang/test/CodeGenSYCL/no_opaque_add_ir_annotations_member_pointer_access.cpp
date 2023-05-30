b64adeb0cab7c869d7706a13eb0e9b4e10b95c2f
Chen, Brox <brox.chen@intel.com>
2023-05-30 11:58:52 -0700

    Fix lit test and add non-opqaue pointer lit test

--- clang/test/CodeGenSYCL/no_opaque_add_ir_annotations_member_pointer_access.cpp
+++ clang/test/CodeGenSYCL/no_opaque_add_ir_annotations_member_pointer_access.cpp
@@ -1,9 +1,6 @@
 // RUN: %clang_cc1 -internal-isystem %S/Inputs -disable-llvm-passes \
 // RUN:    -triple spir64-unknown-unknown -fsycl-is-device -S \
-// RUN:    -opaque-pointers -emit-llvm %s -o - | FileCheck %s
-// RUN: %clang_cc1 -internal-isystem %S/Inputs -disable-llvm-passes \
-// RUN:    -triple spir64-unknown-unknown -fsycl-is-device -DTEST_SCALAR -S \
-// RUN:    -opaque-pointers -emit-llvm %s -o - | FileCheck %s
+// RUN:    -no-opaque-pointers -emit-llvm %s -o - | FileCheck %s
 
 // Tests the generation of IR annotation calls from
 // __sycl_detail__::add_ir_annotations_member attributes.
@@ -29,7 +26,7 @@ public:
 
 int main() {
   sycl::queue q;
-  g<prop1, prop2, prop3, prop4, prop5, prop6, prop7, prop8> a;
+  g<prop1, prop2, prop3, prop4, prop5, prop6, prop7> a;
   q.submit([&](sycl::handler &h) {
     h.single_task<class test_kernel1>(
         [=]() {
@@ -48,16 +45,18 @@ int main() {
 // CHECK-DAG: @[[Prop5Name:.*]] = private unnamed_addr addrspace(1) constant [6 x i8] c"Prop5\00", section "llvm.metadata"
 // CHECK-DAG: @[[Prop6Name:.*]] = private unnamed_addr addrspace(1) constant [6 x i8] c"Prop6\00", section "llvm.metadata"
 // CHECK-DAG: @[[Prop7Name:.*]] = private unnamed_addr addrspace(1) constant [6 x i8] c"Prop7\00", section "llvm.metadata"
-// CHECK-DAG: @[[Prop8Name:.*]] = private unnamed_addr addrspace(1) constant [6 x i8] c"Prop8\00", section "llvm.metadata"
 
 // CHECK-DAG: @[[Prop1Value:.*]] = private unnamed_addr addrspace(1) constant [16 x i8] c"Property string\00", section "llvm.metadata"
 // CHECK-DAG: @[[Prop2_7_14Value:.*]] = private unnamed_addr addrspace(1) constant [2 x i8] c"1\00", section "llvm.metadata"
 // CHECK-DAG: @[[Prop3Value:.*]] = private unnamed_addr addrspace(1) constant [5 x i8] c"true\00", section "llvm.metadata"
 // CHECK-DAG: @[[Prop4_12_17Value:.*]] = private unnamed_addr addrspace(1) constant [2 x i8] c"2\00", section "llvm.metadata"
-// CHECK-DAG: @[[Prop8_18Value:.*]] = private unnamed_addr addrspace(1) constant [9 x i8] c"Property\00", section "llvm.metadata"
 
-// CHECK-DAG: @[[GArgs:.*]] = private unnamed_addr addrspace(1) constant { ptr addrspace(1), ptr addrspace(1), ptr addrspace(1), ptr addrspace(1), ptr addrspace(1), ptr addrspace(1), ptr addrspace(1), ptr addrspace(1), ptr addrspace(1), ptr addrspace(1), ptr addrspace(1), ptr addrspace(1), ptr addrspace(1), ptr addrspace(1), ptr addrspace(1), ptr addrspace(1) } { ptr addrspace(1) @[[Prop1Name]], ptr addrspace(1) @[[Prop1Value]], ptr addrspace(1) @[[Prop2Name]], ptr addrspace(1) @[[Prop2_7_14Value]], ptr addrspace(1) @[[Prop3Name]], ptr addrspace(1) @[[Prop3Value]], ptr addrspace(1) @[[Prop4Name]], ptr addrspace(1) @[[Prop4_12_17Value]], ptr addrspace(1) @[[Prop5Name]], ptr addrspace(1) null, ptr addrspace(1) @[[Prop6Name]], ptr addrspace(1) null, ptr addrspace(1) @[[Prop7Name]], ptr addrspace(1) @[[Prop2_7_14Value]], ptr addrspace(1) @[[Prop8Name]], ptr addrspace(1) @[[Prop8_18Value]] }, section "llvm.metadata"
 
-// CHECK: %[[PtrToPtr:.*]] = call ptr addrspace(4) @llvm.ptr.annotation.p4.p1(ptr {{.*}}, ptr addrspace(1) @[[AnnotName]], {{.*}}, i32 {{.*}}, ptr addrspace(1) @[[GArgs]])
-// CHECK: %[[Ptr:.*]]      = load ptr addrspace(4), ptr addrspace(4) %[[PtrToPtr]], {{.*}}
-// CHECK: store i32 1, ptr addrspace(4) %[[Ptr]], {{.*}}
+// CHECK-DAG: @[[GArgs:.*]] = private unnamed_addr addrspace(1) constant { [6 x i8] addrspace(1)*, [16 x i8] addrspace(1)*, [6 x i8] addrspace(1)*, [2 x i8] addrspace(1)*, [6 x i8] addrspace(1)*, [5 x i8] addrspace(1)*, [6 x i8] addrspace(1)*, [2 x i8] addrspace(1)*, [6 x i8] addrspace(1)*, i8 addrspace(1)*, [6 x i8] addrspace(1)*, i8 addrspace(1)*, [6 x i8] addrspace(1)*, [2 x i8] addrspace(1)* } { [6 x i8] addrspace(1)* @[[Prop1Name]], [16 x i8] addrspace(1)* @[[Prop1Value]], [6 x i8] addrspace(1)* @[[Prop2Name]], [2 x i8] addrspace(1)* @[[Prop2_7_14Value]], [6 x i8] addrspace(1)* @[[Prop3Name]], [5 x i8] addrspace(1)* @[[Prop3Value]], [6 x i8] addrspace(1)* @[[Prop4Name]], [2 x i8] addrspace(1)* @[[Prop4_12_17Value]], [6 x i8] addrspace(1)* @[[Prop5Name]], i8 addrspace(1)* null, [6 x i8] addrspace(1)* @[[Prop6Name]], i8 addrspace(1)* null, [6 x i8] addrspace(1)* @[[Prop7Name]], [2 x i8] addrspace(1)* @[[Prop2_7_14Value]] }, section "llvm.metadata"
+
+
+// CHECK: %[[AnnPtr:.*]] = call i8 addrspace(4)* @llvm.ptr.annotation.p4i8.p1i8(i8 {{.*}}, i8 addrspace(1)* getelementptr inbounds ([16 x i8], [16 x i8] addrspace(1)* @[[AnnotName]], i32 0, i32 0), i8 addrspace(1)* getelementptr inbounds {{.*}}, i32 {{.*}}, i8 addrspace(1)* bitcast ({ [6 x i8] addrspace(1)*, [16 x i8] addrspace(1)*, [6 x i8] addrspace(1)*, [2 x i8] addrspace(1)*, [6 x i8] addrspace(1)*, [5 x i8] addrspace(1)*, [6 x i8] addrspace(1)*, [2 x i8] addrspace(1)*, [6 x i8] addrspace(1)*, i8 addrspace(1)*, [6 x i8] addrspace(1)*, i8 addrspace(1)*, [6 x i8] addrspace(1)*, [2 x i8] addrspace(1)* } addrspace(1)* @[[GArgs]] to i8 addrspace(1)*))
+// CHECK: %[[PtrToPtr:.*]]   = bitcast i8 addrspace(4)* %[[AnnPtr]] to i32 addrspace(4)* addrspace(4)*
+// CHECK: %[[Ptr:.*]]        = load i32 addrspace(4)*, i32 addrspace(4)* addrspace(4)* %[[PtrToPtr]], {{.*}}
+// CHECK: store i32 1, i32 addrspace(4)* %[[Ptr]], {{.*}}
+
