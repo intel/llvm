2d2cd06489a5c53f5e2d1fc2ddb4973b50d8e267
Petr Vesely <petr.vesely@codeplay.com>
2023-05-30 11:54:30 +0100

    [UR] Verify kernel GetInfo

--- source/loader/layers/validation/ur_valddi.cpp
+++ source/loader/layers/validation/ur_valddi.cpp
@@ -2423,8 +2423,20 @@ __urdlllocal ur_result_t UR_APICALL urKernelGetInfo(
             return UR_RESULT_ERROR_INVALID_NULL_HANDLE;
         }
 
+        if (propSize != 0 && pPropValue == NULL) {
+            return UR_RESULT_ERROR_INVALID_NULL_POINTER;
+        }
+
+        if (pPropValue == NULL && pPropSizeRet == NULL) {
+            return UR_RESULT_ERROR_INVALID_NULL_POINTER;
+        }
+
         if (UR_KERNEL_INFO_NUM_REGS < propName) {
             return UR_RESULT_ERROR_INVALID_ENUMERATION;
+        }
+
+        if (propSize == 0 && pPropValue != NULL) {
+            return UR_RESULT_ERROR_INVALID_SIZE;
         }
     }
 
