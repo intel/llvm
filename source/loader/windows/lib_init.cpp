fdde92f5dfcd029db4af5923b4d7a9cfc7859f54
Omar Ahmed <omar.ahmed@codeplay.com>
2023-05-31 12:08:52 +0100

    Change mit license to apache 2.0 with llvm exceptions

--- source/loader/windows/lib_init.cpp
+++ source/loader/windows/lib_init.cpp
@@ -2,7 +2,9 @@
  *
  * Copyright (C) 2021-2023 Intel Corporation
  *
- * SPDX-License-Identifier: MIT
+ * Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM Exceptions.
+ * See LICENSE.TXT
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
  *
  */
 
