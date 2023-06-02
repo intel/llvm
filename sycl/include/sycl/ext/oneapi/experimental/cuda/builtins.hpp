d8c4f82f4a0d52ff6388f887e120a7ad77a640b7
Jakub Chlanda <jakub@codeplay.com>
2023-06-02 09:27:51 +0100

    Vec 3

--- sycl/include/sycl/ext/oneapi/experimental/cuda/builtins.hpp
+++ sycl/include/sycl/ext/oneapi/experimental/cuda/builtins.hpp
@@ -27,17 +27,23 @@ namespace cuda {
 
 namespace detail {
 using ldg_vector_types = sycl::detail::type_list<
-    sycl::vec<char, 2>, sycl::vec<char, 4>, sycl::vec<signed char, 2>,
-    sycl::vec<signed char, 4>, sycl::vec<short, 2>, sycl::vec<short, 4>,
-    sycl::vec<int, 2>, sycl::vec<int, 4>, sycl::vec<long, 2>,
-    sycl::vec<long, 4>, sycl::vec<long long, 2>, sycl::vec<long long, 4>,
-    sycl::vec<unsigned char, 2>, sycl::vec<unsigned char, 4>,
-    sycl::vec<unsigned short, 2>, sycl::vec<unsigned short, 4>,
-    sycl::vec<unsigned int, 2>, sycl::vec<unsigned int, 4>,
-    sycl::vec<unsigned long, 2>, sycl::vec<unsigned long, 4>,
-    sycl::vec<unsigned long long, 2>, sycl::vec<unsigned long long, 4>,
-    sycl::vec<half, 2>, sycl::vec<half, 4>, sycl::vec<float, 2>,
-    sycl::vec<float, 4>, sycl::vec<double, 2>, sycl::vec<double, 4>>;
+    sycl::vec<char, 2>, sycl::vec<char, 3>, sycl::vec<char, 4>,
+    sycl::vec<signed char, 2>, sycl::vec<signed char, 3>,
+    sycl::vec<signed char, 4>, sycl::vec<short, 2>, sycl::vec<short, 3>,
+    sycl::vec<short, 4>, sycl::vec<int, 2>, sycl::vec<int, 3>,
+    sycl::vec<int, 4>, sycl::vec<long, 2>, sycl::vec<long, 3>,
+    sycl::vec<long, 4>, sycl::vec<long long, 2>, sycl::vec<long long, 3>,
+    sycl::vec<long long, 4>, sycl::vec<unsigned char, 2>,
+    sycl::vec<unsigned char, 3>, sycl::vec<unsigned char, 4>,
+    sycl::vec<unsigned short, 2>, sycl::vec<unsigned short, 3>,
+    sycl::vec<unsigned short, 4>, sycl::vec<unsigned int, 2>,
+    sycl::vec<unsigned int, 3>, sycl::vec<unsigned int, 4>,
+    sycl::vec<unsigned long, 2>, sycl::vec<unsigned long, 3>,
+    sycl::vec<unsigned long, 4>, sycl::vec<unsigned long long, 2>,
+    sycl::vec<unsigned long long, 3>, sycl::vec<unsigned long long, 4>,
+    sycl::vec<half, 2>, sycl::vec<half, 3>, sycl::vec<half, 4>,
+    sycl::vec<float, 2>, sycl::vec<float, 3>, sycl::vec<float, 4>,
+    sycl::vec<double, 2>, sycl::vec<double, 3>, sycl::vec<double, 4>>;
 
 using ldg_types =
     sycl::detail::type_list<ldg_vector_types,
@@ -92,6 +98,16 @@ ldg(const T *ptr) {
     ret.x() = rv[0];
     ret.y() = rv[1];
     return ret;
+  } else if constexpr (std::is_same_v<T, sycl::vec<char, 3>>) {
+    typedef char c2 ATTRIBUTE_EXT_VEC_TYPE(2);
+    c2 rv_2 = __nvvm_ldg_c2(reinterpret_cast<const c2 *>(ptr));
+    char rv = __nvvm_ldg_c(reinterpret_cast<const char *>(
+        std::next(reinterpret_cast<const c2 *>(ptr))));
+    sycl::vec<char, 3> ret;
+    ret.x() = rv_2[0];
+    ret.y() = rv_2[1];
+    ret.z() = rv;
+    return ret;
   } else if constexpr (std::is_same_v<T, sycl::vec<char, 4>>) {
     typedef char c4 ATTRIBUTE_EXT_VEC_TYPE(4);
     c4 rv = __nvvm_ldg_c4(reinterpret_cast<const c4 *>(ptr));
@@ -107,6 +123,16 @@ ldg(const T *ptr) {
     sycl::vec<signed char, 2> ret;
     ret.x() = rv[0];
     ret.y() = rv[1];
+    return ret;
+  } else if constexpr (std::is_same_v<T, sycl::vec<signed char, 3>>) {
+    typedef signed char sc2 ATTRIBUTE_EXT_VEC_TYPE(2);
+    sc2 rv_2 = __nvvm_ldg_sc2(reinterpret_cast<const sc2 *>(ptr));
+    signed char rv = __nvvm_ldg_sc(reinterpret_cast<const signed char *>(
+        std::next(reinterpret_cast<const sc2 *>(ptr))));
+    sycl::vec<signed char, 3> ret;
+    ret.x() = rv_2[0];
+    ret.y() = rv_2[1];
+    ret.z() = rv;
     return ret;
   } else if constexpr (std::is_same_v<T, sycl::vec<signed char, 4>>) {
     typedef signed char sc4 ATTRIBUTE_EXT_VEC_TYPE(4);
@@ -124,6 +150,16 @@ ldg(const T *ptr) {
     ret.x() = rv[0];
     ret.y() = rv[1];
     return ret;
+  } else if constexpr (std::is_same_v<T, sycl::vec<short, 3>>) {
+    typedef short s2 ATTRIBUTE_EXT_VEC_TYPE(2);
+    s2 rv_2 = __nvvm_ldg_s2(reinterpret_cast<const s2 *>(ptr));
+    short rv = __nvvm_ldg_s(reinterpret_cast<const short *>(
+        std::next(reinterpret_cast<const s2 *>(ptr))));
+    sycl::vec<short, 3> ret;
+    ret.x() = rv_2[0];
+    ret.y() = rv_2[1];
+    ret.z() = rv;
+    return ret;
   } else if constexpr (std::is_same_v<T, sycl::vec<short, 4>>) {
     typedef short s4 ATTRIBUTE_EXT_VEC_TYPE(4);
     s4 rv = __nvvm_ldg_s4(reinterpret_cast<const s4 *>(ptr));
@@ -140,6 +176,16 @@ ldg(const T *ptr) {
     ret.x() = rv[0];
     ret.y() = rv[1];
     return ret;
+  } else if constexpr (std::is_same_v<T, sycl::vec<int, 3>>) {
+    typedef int i2 ATTRIBUTE_EXT_VEC_TYPE(2);
+    i2 rv_2 = __nvvm_ldg_i2(reinterpret_cast<const i2 *>(ptr));
+    int rv = __nvvm_ldg_i(reinterpret_cast<const int *>(
+        std::next(reinterpret_cast<const i2 *>(ptr))));
+    sycl::vec<int, 3> ret;
+    ret.x() = rv_2[0];
+    ret.y() = rv_2[1];
+    ret.z() = rv;
+    return ret;
   } else if constexpr (std::is_same_v<T, sycl::vec<int, 4>>) {
     typedef int i4 ATTRIBUTE_EXT_VEC_TYPE(4);
     i4 rv = __nvvm_ldg_i4(reinterpret_cast<const i4 *>(ptr));
@@ -155,6 +201,16 @@ ldg(const T *ptr) {
     sycl::vec<long, 2> ret;
     ret.x() = rv[0];
     ret.y() = rv[1];
+    return ret;
+  } else if constexpr (std::is_same_v<T, sycl::vec<long, 3>>) {
+    typedef long l2 ATTRIBUTE_EXT_VEC_TYPE(2);
+    l2 rv_2 = __nvvm_ldg_l2(reinterpret_cast<const l2 *>(ptr));
+    long rv = __nvvm_ldg_l(reinterpret_cast<const long *>(
+        std::next(reinterpret_cast<const l2 *>(ptr))));
+    sycl::vec<long, 3> ret;
+    ret.x() = rv_2[0];
+    ret.y() = rv_2[1];
+    ret.z() = rv;
     return ret;
   } else if constexpr (std::is_same_v<T, sycl::vec<long, 4>>) {
     typedef long l2 ATTRIBUTE_EXT_VEC_TYPE(2);
@@ -173,6 +229,16 @@ ldg(const T *ptr) {
     ret.x() = rv[0];
     ret.y() = rv[1];
     return ret;
+  } else if constexpr (std::is_same_v<T, sycl::vec<long long, 3>>) {
+    typedef long long ll2 ATTRIBUTE_EXT_VEC_TYPE(2);
+    ll2 rv_2 = __nvvm_ldg_ll2(reinterpret_cast<const ll2 *>(ptr));
+    long long rv = __nvvm_ldg_ll(reinterpret_cast<const long long *>(
+        std::next(reinterpret_cast<const ll2 *>(ptr))));
+    sycl::vec<long long, 3> ret;
+    ret.x() = rv_2[0];
+    ret.y() = rv_2[1];
+    ret.z() = rv;
+    return ret;
   } else if constexpr (std::is_same_v<T, sycl::vec<long long, 4>>) {
     typedef long long ll2 ATTRIBUTE_EXT_VEC_TYPE(2);
     ll2 rv1 = __nvvm_ldg_ll2(reinterpret_cast<const ll2 *>(ptr));
@@ -190,6 +256,16 @@ ldg(const T *ptr) {
     ret.x() = rv[0];
     ret.y() = rv[1];
     return ret;
+  } else if constexpr (std::is_same_v<T, sycl::vec<unsigned char, 3>>) {
+    typedef unsigned char uc2 ATTRIBUTE_EXT_VEC_TYPE(2);
+    uc2 rv_2 = __nvvm_ldg_uc2(reinterpret_cast<const uc2 *>(ptr));
+    unsigned char rv = __nvvm_ldg_uc(reinterpret_cast<const unsigned char *>(
+        std::next(reinterpret_cast<const uc2 *>(ptr))));
+    sycl::vec<unsigned char, 3> ret;
+    ret.x() = rv_2[0];
+    ret.y() = rv_2[1];
+    ret.z() = rv;
+    return ret;
   } else if constexpr (std::is_same_v<T, sycl::vec<unsigned char, 4>>) {
     typedef unsigned char uc4 ATTRIBUTE_EXT_VEC_TYPE(4);
     uc4 rv = __nvvm_ldg_uc4(reinterpret_cast<const uc4 *>(ptr));
@@ -205,6 +281,16 @@ ldg(const T *ptr) {
     sycl::vec<unsigned short, 2> ret;
     ret.x() = rv[0];
     ret.y() = rv[1];
+    return ret;
+  } else if constexpr (std::is_same_v<T, sycl::vec<unsigned short, 3>>) {
+    typedef unsigned short us2 ATTRIBUTE_EXT_VEC_TYPE(2);
+    us2 rv_2 = __nvvm_ldg_us2(reinterpret_cast<const us2 *>(ptr));
+    unsigned short rv = __nvvm_ldg_us(reinterpret_cast<const unsigned short *>(
+        std::next(reinterpret_cast<const us2 *>(ptr))));
+    sycl::vec<unsigned short, 3> ret;
+    ret.x() = rv_2[0];
+    ret.y() = rv_2[1];
+    ret.z() = rv;
     return ret;
   } else if constexpr (std::is_same_v<T, sycl::vec<unsigned short, 4>>) {
     typedef unsigned short us4 ATTRIBUTE_EXT_VEC_TYPE(4);
@@ -222,6 +308,16 @@ ldg(const T *ptr) {
     ret.x() = rv[0];
     ret.y() = rv[1];
     return ret;
+  } else if constexpr (std::is_same_v<T, sycl::vec<unsigned int, 3>>) {
+    typedef unsigned int ui2 ATTRIBUTE_EXT_VEC_TYPE(2);
+    ui2 rv_2 = __nvvm_ldg_ui2(reinterpret_cast<const ui2 *>(ptr));
+    unsigned int rv = __nvvm_ldg_ui(reinterpret_cast<const unsigned int *>(
+        std::next(reinterpret_cast<const ui2 *>(ptr))));
+    sycl::vec<unsigned int, 3> ret;
+    ret.x() = rv_2[0];
+    ret.y() = rv_2[1];
+    ret.z() = rv;
+    return ret;
   } else if constexpr (std::is_same_v<T, sycl::vec<unsigned int, 4>>) {
     typedef unsigned int ui4 ATTRIBUTE_EXT_VEC_TYPE(4);
     ui4 rv = __nvvm_ldg_ui4(reinterpret_cast<const ui4 *>(ptr));
@@ -237,6 +333,16 @@ ldg(const T *ptr) {
     sycl::vec<unsigned long, 2> ret;
     ret.x() = rv[0];
     ret.y() = rv[1];
+    return ret;
+  } else if constexpr (std::is_same_v<T, sycl::vec<unsigned long, 3>>) {
+    typedef unsigned long ul2 ATTRIBUTE_EXT_VEC_TYPE(2);
+    ul2 rv_2 = __nvvm_ldg_ul2(reinterpret_cast<const ul2 *>(ptr));
+    unsigned long rv = __nvvm_ldg_ul(reinterpret_cast<const unsigned long *>(
+        std::next(reinterpret_cast<const ul2 *>(ptr))));
+    sycl::vec<unsigned long, 3> ret;
+    ret.x() = rv_2[0];
+    ret.y() = rv_2[1];
+    ret.z() = rv;
     return ret;
   } else if constexpr (std::is_same_v<T, sycl::vec<unsigned long, 4>>) {
     typedef unsigned long ul2 ATTRIBUTE_EXT_VEC_TYPE(2);
@@ -255,6 +361,17 @@ ldg(const T *ptr) {
     ret.x() = rv[0];
     ret.y() = rv[1];
     return ret;
+  } else if constexpr (std::is_same_v<T, sycl::vec<unsigned long long, 3>>) {
+    typedef unsigned long long ull2 ATTRIBUTE_EXT_VEC_TYPE(2);
+    ull2 rv_2 = __nvvm_ldg_ull2(reinterpret_cast<const ull2 *>(ptr));
+    unsigned long long rv =
+        __nvvm_ldg_ull(reinterpret_cast<const unsigned long long *>(
+            std::next(reinterpret_cast<const ull2 *>(ptr))));
+    sycl::vec<unsigned long long, 3> ret;
+    ret.x() = rv_2[0];
+    ret.y() = rv_2[1];
+    ret.z() = rv;
+    return ret;
   } else if constexpr (std::is_same_v<T, sycl::vec<unsigned long long, 4>>) {
     typedef unsigned long long ull2 ATTRIBUTE_EXT_VEC_TYPE(2);
     ull2 rv1 = __nvvm_ldg_ull2(reinterpret_cast<const ull2 *>(ptr));
@@ -271,6 +388,16 @@ ldg(const T *ptr) {
     sycl::vec<half, 2> ret;
     ret.x() = rv[0];
     ret.y() = rv[1];
+    return ret;
+  } else if constexpr (std::is_same_v<T, sycl::vec<half, 3>>) {
+    typedef __fp16 h2 ATTRIBUTE_EXT_VEC_TYPE(2);
+    h2 rv_2 = __nvvm_ldg_h2(reinterpret_cast<const h2 *>(ptr));
+    auto rv = __nvvm_ldg_h(reinterpret_cast<const __fp16 *>(
+        std::next(reinterpret_cast<const h2 *>(ptr))));
+    sycl::vec<half, 3> ret;
+    ret.x() = rv_2[0];
+    ret.y() = rv_2[1];
+    ret.z() = rv;
     return ret;
   } else if constexpr (std::is_same_v<T, sycl::vec<half, 4>>) {
     typedef __fp16 h2 ATTRIBUTE_EXT_VEC_TYPE(2);
@@ -289,6 +416,16 @@ ldg(const T *ptr) {
     ret.x() = rv[0];
     ret.y() = rv[1];
     return ret;
+  } else if constexpr (std::is_same_v<T, sycl::vec<float, 3>>) {
+    typedef float f2 ATTRIBUTE_EXT_VEC_TYPE(2);
+    f2 rv_2 = __nvvm_ldg_f2(reinterpret_cast<const f2 *>(ptr));
+    float rv = __nvvm_ldg_f(reinterpret_cast<const float *>(
+        std::next(reinterpret_cast<const f2 *>(ptr))));
+    sycl::vec<float, 3> ret;
+    ret.x() = rv_2[0];
+    ret.y() = rv_2[1];
+    ret.z() = rv;
+    return ret;
   } else if constexpr (std::is_same_v<T, sycl::vec<float, 4>>) {
     typedef float f4 ATTRIBUTE_EXT_VEC_TYPE(4);
     f4 rv = __nvvm_ldg_f4(reinterpret_cast<const f4 *>(ptr));
@@ -304,6 +441,16 @@ ldg(const T *ptr) {
     sycl::vec<double, 2> ret;
     ret.x() = rv[0];
     ret.y() = rv[1];
+    return ret;
+  } else if constexpr (std::is_same_v<T, sycl::vec<double, 3>>) {
+    typedef double d2 ATTRIBUTE_EXT_VEC_TYPE(2);
+    d2 rv_2 = __nvvm_ldg_d2(reinterpret_cast<const d2 *>(ptr));
+    double rv = __nvvm_ldg_d(reinterpret_cast<const double *>(
+        std::next(reinterpret_cast<const d2 *>(ptr))));
+    sycl::vec<double, 3> ret;
+    ret.x() = rv_2[0];
+    ret.y() = rv_2[1];
+    ret.z() = rv;
     return ret;
   } else if constexpr (std::is_same_v<T, sycl::vec<double, 4>>) {
     typedef double d2 ATTRIBUTE_EXT_VEC_TYPE(2);
