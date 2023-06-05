b8449772fa599f5507ea24955e16a44f074f0b0c
Callum Fare <callum@codeplay.com>
2023-06-05 10:40:02 +0100

    [ur] Make hArgValue optional again in urKernelSetMemObj

--- source/loader/ur_ldrddi.cpp
+++ source/loader/ur_ldrddi.cpp
@@ -2560,7 +2560,7 @@ __urdlllocal ur_result_t UR_APICALL urKernelSetArgSampler(
 __urdlllocal ur_result_t UR_APICALL urKernelSetArgMemObj(
     ur_kernel_handle_t hKernel, ///< [in] handle of the kernel object
     uint32_t argIndex, ///< [in] argument index in range [0, num args - 1]
-    ur_mem_handle_t hArgValue ///< [in] handle of Memory object.
+    ur_mem_handle_t hArgValue ///< [in][optional] handle of Memory object.
 ) {
     ur_result_t result = UR_RESULT_SUCCESS;
 
@@ -2575,7 +2575,9 @@ __urdlllocal ur_result_t UR_APICALL urKernelSetArgMemObj(
     hKernel = reinterpret_cast<ur_kernel_object_t *>(hKernel)->handle;
 
     // convert loader handle to platform handle
-    hArgValue = reinterpret_cast<ur_mem_object_t *>(hArgValue)->handle;
+    hArgValue = (hArgValue)
+                    ? reinterpret_cast<ur_mem_object_t *>(hArgValue)->handle
+                    : nullptr;
 
     // forward to device-platform
     result = pfnSetArgMemObj(hKernel, argIndex, hArgValue);
