ab43cd4168b6ecb4b32404acd510d0d453c72fbd
pbalcer <piotr.balcer@intel.com>
2023-06-05 12:10:44 +0200

    [level-zero] fix build script and failing tests

--- test/loader/platforms/platforms.cpp
+++ test/loader/platforms/platforms.cpp
@@ -17,32 +17,32 @@ using namespace logger;
 
 //////////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[]) {
-    logger::init("TEST");
+    auto out = create_logger("TEST");
 
     ur_result_t status;
 
     // Initialize the platform
     status = urInit(0);
     if (status != UR_RESULT_SUCCESS) {
-        error("urInit failed with return code: {}", status);
+        out.error("urInit failed with return code: {}", status);
         return 1;
     }
-    info("urInit succeeded.");
+    out.info("urInit succeeded.");
 
     uint32_t platformCount = 0;
     std::vector<ur_platform_handle_t> platforms;
 
     status = urPlatformGet(1, nullptr, &platformCount);
     if (status != UR_RESULT_SUCCESS) {
-        error("urPlatformGet failed with return code: {}", status);
+        out.error("urPlatformGet failed with return code: {}", status);
         goto out;
     }
-    info("urPlatformGet found {} platforms", platformCount);
+    out.info("urPlatformGet found {} platforms", platformCount);
 
     platforms.resize(platformCount);
     status = urPlatformGet(platformCount, platforms.data(), nullptr);
     if (status != UR_RESULT_SUCCESS) {
-        error("urPlatformGet failed with return code: {}", status);
+        out.error("urPlatformGet failed with return code: {}", status);
         goto out;
     }
 
@@ -51,7 +51,7 @@ int main(int argc, char *argv[]) {
         status =
             urPlatformGetInfo(p, UR_PLATFORM_INFO_NAME, 0, nullptr, &name_len);
         if (status != UR_RESULT_SUCCESS) {
-            error("urPlatformGetInfo failed with return code: {}", status);
+            out.error("urPlatformGetInfo failed with return code: {}", status);
             goto out;
         }
 
@@ -61,11 +61,11 @@ int main(int argc, char *argv[]) {
         status = urPlatformGetInfo(p, UR_PLATFORM_INFO_NAME, name_len, name,
                                    nullptr);
         if (status != UR_RESULT_SUCCESS) {
-            error("urPlatformGetInfo failed with return code: {}", status);
+            out.error("urPlatformGetInfo failed with return code: {}", status);
             free(name);
             goto out;
         }
-        info("Found {} ", name);
+        out.info("Found {} ", name);
 
         free(name);
     }
