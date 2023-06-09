eea1d9ab0d66354034a3872936032ada47ee57c8
JackAKirk <chezjakirk@gmail.com>
2023-06-09 21:25:17 +0100

    Added missing types.
    Refactored impl.
    
    Signed-off-by: JackAKirk <jack.kirk@codeplay.com>

--- sycl/include/sycl/ext/oneapi/experimental/cuda/non_uniform_algorithms.hpp
+++ sycl/include/sycl/ext/oneapi/experimental/cuda/non_uniform_algorithms.hpp
@@ -94,58 +94,42 @@ masked_reduction_cuda_sm80(Group g, T x, BinaryOperation binary_op,
 
 //// Shuffle based masked reduction impls
 
-// fixed_size_group group reduction using shfls, T = double
+// fixed_size_group group reduction using shfls
 template <typename Group, typename T, class BinaryOperation>
 inline __SYCL_ALWAYS_INLINE
-    std::enable_if_t<ext::oneapi::experimental::is_fixed_size_group_v<Group> &&
-                         std::is_same_v<T, double>,
-                     T>
+    std::enable_if_t<ext::oneapi::experimental::is_fixed_size_group_v<Group>, T>
     masked_reduction_cuda_shfls(Group g, T x, BinaryOperation binary_op,
                                 const uint32_t MemberMask) {
   for (int i = g.get_local_range()[0] / 2; i > 0; i /= 2) {
-    int x_a, x_b;
-    asm volatile("mov.b64 {%0,%1},%2; \n\t" : "=r"(x_a), "=r"(x_b) : "l"(x));
-
-    auto tmp_a = __nvvm_shfl_sync_bfly_i32(MemberMask, x_a, -1, i);
-    auto tmp_b = __nvvm_shfl_sync_bfly_i32(MemberMask, x_b, -1, i);
-    double tmp;
-    asm volatile("mov.b64 %0,{%1,%2}; \n\t"
-                 : "=l"(tmp)
-                 : "r"(tmp_a), "r"(tmp_b));
-    x = binary_op(x, tmp);
-  }
-
-  return x;
-}
-
-// fixed_size_group group reduction using shfls, T = float
-template <typename Group, typename T, class BinaryOperation>
-inline __SYCL_ALWAYS_INLINE
-    std::enable_if_t<ext::oneapi::experimental::is_fixed_size_group_v<Group> &&
-                         std::is_same_v<T, float>,
-                     T>
-    masked_reduction_cuda_shfls(Group g, T x, BinaryOperation binary_op,
-                                const uint32_t MemberMask) {
-
-  for (int i = g.get_local_range()[0] / 2; i > 0; i /= 2) {
-    auto tmp =
-        __nvvm_shfl_sync_bfly_i32(MemberMask, __nvvm_bitcast_f2i(x), -1, i);
-    x = binary_op(x, __nvvm_bitcast_i2f(tmp));
-  }
-  return x;
-}
-
-// fixed_size_group group reduction using shfls, std::is_integral_v<T>
-template <typename Group, typename T, class BinaryOperation>
-inline __SYCL_ALWAYS_INLINE
-    std::enable_if_t<ext::oneapi::experimental::is_fixed_size_group_v<Group> &&
-                         std::is_integral_v<T>,
-                     T>
-    masked_reduction_cuda_shfls(Group g, T x, BinaryOperation binary_op,
-                                const uint32_t MemberMask) {
-
-  for (int i = g.get_local_range()[0] / 2; i > 0; i /= 2) {
-    auto tmp = __nvvm_shfl_sync_bfly_i32(MemberMask, x, -1, i);
+    T tmp;
+    if constexpr (std::is_same_v<T, double>) {
+      int x_a, x_b;
+      asm volatile("mov.b64 {%0,%1},%2;" : "=r"(x_a), "=r"(x_b) : "d"(x));
+      auto tmp_a = __nvvm_shfl_sync_bfly_i32(MemberMask, x_a, -1, i);
+      auto tmp_b = __nvvm_shfl_sync_bfly_i32(MemberMask, x_b, -1, i);
+      asm volatile("mov.b64 %0,{%1,%2};" : "=d"(tmp) : "r"(tmp_a), "r"(tmp_b));
+    } else if constexpr (std::is_same_v<T, long> ||
+                         std::is_same_v<T, unsigned long>) {
+      int x_a, x_b;
+      asm volatile("mov.b64 {%0,%1},%2;" : "=r"(x_a), "=r"(x_b) : "l"(x));
+      auto tmp_a = __nvvm_shfl_sync_bfly_i32(MemberMask, x_a, -1, i);
+      auto tmp_b = __nvvm_shfl_sync_bfly_i32(MemberMask, x_b, -1, i);
+      asm volatile("mov.b64 %0,{%1,%2};" : "=l"(tmp) : "r"(tmp_a), "r"(tmp_b));
+    } else if constexpr (std::is_same_v<T, half>) {
+      short tmp_b16;
+      asm volatile("mov.b16 %0,%1;" : "=h"(tmp_b16) : "h"(x));
+      auto tmp_b32 = __nvvm_shfl_sync_bfly_i32(
+          MemberMask, static_cast<int>(tmp_b16), -1, i);
+      asm volatile("mov.b16 %0,%1;"
+                   : "=h"(tmp)
+                   : "h"(static_cast<short>(tmp_b32)));
+    } else if constexpr (std::is_same_v<T, float>) {
+      auto tmp_b32 =
+          __nvvm_shfl_sync_bfly_i32(MemberMask, __nvvm_bitcast_f2i(x), -1, i);
+      tmp = __nvvm_bitcast_i2f(tmp_b32);
+    } else {
+      tmp = __nvvm_shfl_sync_bfly_i32(MemberMask, x, -1, i);
+    }
     x = binary_op(x, tmp);
   }
   return x;
@@ -169,17 +153,31 @@ non_uniform_shfl(Group g, const uint32_t MemberMask, T x, int shfl_param) {
   T res;
   if constexpr (std::is_same_v<T, double>) {
     int x_a, x_b;
-    asm volatile("mov.b64 {%0,%1},%2; \n\t" : "=r"(x_a), "=r"(x_b) : "l"(x));
-
+    asm volatile("mov.b64 {%0,%1},%2;" : "=r"(x_a), "=r"(x_b) : "d"(x));
     auto tmp_a = non_uniform_shfl_T<Group>(MemberMask, x_a, shfl_param);
     auto tmp_b = non_uniform_shfl_T<Group>(MemberMask, x_b, shfl_param);
-    asm volatile("mov.b64 %0,{%1,%2}; \n\t"
-                 : "=l"(res)
-                 : "r"(tmp_a), "r"(tmp_b));
+    asm volatile("mov.b64 %0,{%1,%2};" : "=d"(res) : "r"(tmp_a), "r"(tmp_b));
+  } else if constexpr (std::is_same_v<T, long> ||
+                       std::is_same_v<T, unsigned long>) {
+    int x_a, x_b;
+    asm volatile("mov.b64 {%0,%1},%2;" : "=r"(x_a), "=r"(x_b) : "l"(x));
+    auto tmp_a = non_uniform_shfl_T<Group>(MemberMask, x_a, shfl_param);
+    auto tmp_b = non_uniform_shfl_T<Group>(MemberMask, x_b, shfl_param);
+    asm volatile("mov.b64 %0,{%1,%2};" : "=l"(res) : "r"(tmp_a), "r"(tmp_b));
+  } else if constexpr (std::is_same_v<T, half>) {
+    short tmp_b16;
+    asm volatile("mov.b16 %0,%1;" : "=h"(tmp_b16) : "h"(x));
+    auto tmp_b32 = non_uniform_shfl_T<Group>(
+        MemberMask, static_cast<int>(tmp_b16), shfl_param);
+    asm volatile("mov.b16 %0,%1;"
+                 : "=h"(res)
+                 : "h"(static_cast<short>(tmp_b32)));
+  } else if constexpr (std::is_same_v<T, float>) {
+    auto tmp_b32 = non_uniform_shfl_T<Group>(MemberMask, __nvvm_bitcast_f2i(x),
+                                             shfl_param);
+    res = __nvvm_bitcast_i2f(tmp_b32);
   } else {
-    auto input = std::is_same_v<T, float> ? __nvvm_bitcast_f2i(x) : x;
-    auto tmp_b32 = non_uniform_shfl_T<Group>(MemberMask, input, shfl_param);
-    res = std::is_same_v<T, float> ? __nvvm_bitcast_i2f(tmp_b32) : tmp_b32;
+    res = non_uniform_shfl_T<Group>(MemberMask, x, shfl_param);
   }
   return res;
 }
@@ -192,27 +190,18 @@ inline __SYCL_ALWAYS_INLINE std::enable_if_t<
     T>
 masked_reduction_cuda_shfls(Group g, T x, BinaryOperation binary_op,
                             const uint32_t MemberMask) {
-  if (MemberMask == 0xffffffff) {
-    for (int i = 16; i > 0; i /= 2) {
-      auto tmp = __nvvm_shfl_sync_bfly_i32(MemberMask, x, -1, i);
-      x = binary_op(x, tmp);
-    }
-    return x;
-  }
 
   unsigned localSetBit = g.get_local_id()[0] + 1;
 
   // number of elements requiring binary operations each loop iteration
   auto opRange = g.get_local_range()[0];
 
-  // remainder that won't have a binary partner each loop iteration
-  int remainder;
+  // stride between local_ids forming a binary op
+  unsigned stride = opRange / 2;
+  while (stride >= 1) {
 
-  while (opRange / 2 >= 1) {
-    remainder = opRange % 2;
-
-    // stride between local_ids forming a binary op
-    int stride = opRange / 2;
+    // if (remainder == 1), there is a WI without a binary op partner
+    unsigned remainder = opRange % 2;
 
     // unfolded position of set bit in mask of shfl src lane
     int unfoldedSrcSetBit = localSetBit + stride;
@@ -226,12 +215,15 @@ masked_reduction_cuda_shfls(Group g, T x, BinaryOperation binary_op,
       x = binary_op(x, tmp);
     }
 
-    opRange = std::ceil((float)opRange / 2.0f);
+    opRange = stride + remainder;
+    stride = opRange / 2;
   }
-  int broadID;
-  int maskRev;
-  asm volatile("brev.b32 %0, %1;" : "=r"(maskRev) : "r"(MemberMask));
-  asm volatile("clz.b32 %0, %1;" : "=r"(broadID) : "r"(maskRev));
+  unsigned broadID;
+  asm volatile(".reg .u32 rev;\n\t"
+               "brev.b32 rev, %1;\n\t" // reverse mask bits
+               "clz.b32 %0, rev;"
+               : "=r"(broadID)
+               : "r"(MemberMask));
 
   return non_uniform_shfl(g, MemberMask, x, broadID);
 }
@@ -279,28 +271,7 @@ inline __SYCL_ALWAYS_INLINE
   inline __SYCL_ALWAYS_INLINE                                                  \
       std::enable_if_t<OP_CHECK<T, BinaryOperation>::value, T>                 \
       get_identity() {                                                         \
-    if constexpr (std::is_same_v<T, char>) {                                   \
-      return std::numeric_limits<char>::OP();                                  \
-    } else if constexpr (std::is_same_v<T, unsigned char>) {                   \
-      return std::numeric_limits<unsigned char>::OP();                         \
-    } else if constexpr (std::is_same_v<T, short>) {                           \
-      return std::numeric_limits<short>::OP();                                 \
-    } else if constexpr (std::is_same_v<T, unsigned short>) {                  \
-      return std::numeric_limits<unsigned short>::OP();                        \
-    } else if constexpr (std::is_same_v<T, int>) {                             \
-      return std::numeric_limits<int>::OP();                                   \
-    } else if constexpr (std::is_same_v<T, unsigned int>) {                    \
-      return std::numeric_limits<unsigned int>::OP();                          \
-    } else if constexpr (std::is_same_v<T, long>) {                            \
-      return std::numeric_limits<int>::OP();                                   \
-    } else if constexpr (std::is_same_v<T, unsigned long>) {                   \
-      return std::numeric_limits<unsigned int>::OP();                          \
-    } else if constexpr (std::is_same_v<T, float>) {                           \
-      return std::numeric_limits<float>::OP();                                 \
-    } else if constexpr (std::is_same_v<T, double>) {                          \
-      return std::numeric_limits<double>::OP();                                \
-    }                                                                          \
-    return 0;                                                                  \
+    return std::numeric_limits<T>::OP();                                       \
   }
 
 GET_ID(IsMinimum, max)
@@ -317,15 +288,16 @@ inline __SYCL_ALWAYS_INLINE
     std::enable_if_t<ext::oneapi::experimental::is_fixed_size_group_v<Group>, T>
     masked_scan_cuda_shfls(Group g, T x, BinaryOperation binary_op,
                            const uint32_t MemberMask) {
+  unsigned localIdVal = g.get_local_id()[0];
   for (int i = 1; i < g.get_local_range()[0]; i *= 2) {
     auto tmp = non_uniform_shfl(g, MemberMask, x, i);
-    if (g.get_local_id()[0] >= i)
+    if (localIdVal >= i)
       x = binary_op(x, tmp);
   }
   if constexpr (Op == __spv::GroupOperation::ExclusiveScan) {
 
     x = non_uniform_shfl(g, MemberMask, x, 1);
-    if (g.get_local_id()[0] == 0) {
+    if (localIdVal == 0) {
       return get_identity<T, BinaryOperation>();
     }
   }
@@ -340,8 +312,8 @@ inline __SYCL_ALWAYS_INLINE std::enable_if_t<
     T>
 masked_scan_cuda_shfls(Group g, T x, BinaryOperation binary_op,
                        const uint32_t MemberMask) {
-  int localIdVal = g.get_local_id()[0];
-  int localSetBit = localIdVal + 1;
+  unsigned localIdVal = g.get_local_id()[0];
+  unsigned localSetBit = localIdVal + 1;
 
   for (int i = 1; i < g.get_local_range()[0]; i *= 2) {
     int unfoldedSrcSetBit = localSetBit - i;
