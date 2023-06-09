a5f95646143be03d73cccff50a9fa4f0a315c673
Michael Aziz <michael.aziz@intel.com>
2023-06-09 10:33:42 -0700

    [SYCL] Update test case to use `sycl::multi_ptr`
    Signed-off-by: Michael Aziz <michael.aziz@intel.com>

--- sycl/test-e2e/Basic/built-ins/host_math.cpp
+++ sycl/test-e2e/Basic/built-ins/host_math.cpp
@@ -37,14 +37,19 @@ void testCospi() {
 void testRemquo() {
   {
     int quo = 0;
-    float rem = sycl::remquo(86.0f, 10.0f, &quo);
+    sycl::multi_ptr<int, sycl::access::address_space::global_space> x(&quo);
+    float rem = sycl::remquo(
+        86.0f, 10.0f,
+        sycl::multi_ptr<int, sycl::access::address_space::global_space>{&quo});
     assert(quo == 9);
     assert(rem == -4);
   }
 
   {
     int quo = 0;
-    float rem = sycl::remquo(-10.0, 3.0, &quo);
+    float rem = sycl::remquo(
+        -10.0, 3.0,
+        sycl::multi_ptr<int, sycl::access::address_space::global_space>{&quo});
     assert(quo == -3);
     assert(rem == -1);
   }
