028fb349cbbf853b50932de30d478b091fa06c09
Dounia <dounia.khaldi@intel.com>
2023-06-13 08:11:39 -0700

    Remove duplicated code

--- sycl/test-e2e/Matrix/joint_matrix_out_bounds_impl.hpp
+++ sycl/test-e2e/Matrix/joint_matrix_out_bounds_impl.hpp
@@ -1,25 +1,3 @@
-//==-------- joint_matrix_bf16.cpp  - DPC++ joint_matrix--------------- ----==//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-// REQUIRES: matrix
-
-// RUN: %clangxx -fsycl %s -o %t.out -DSYCL_EXT_ONEAPI_MATRIX_VERSION=1
-// RUN: %CPU_RUN_PLACEHOLDER %t.out
-// RUN: %GPU_RUN_PLACEHOLDER %t.out
-
-#include <iostream>
-#include <sycl/sycl.hpp>
-
-using namespace sycl;
-using namespace sycl::ext::oneapi::experimental::matrix;
-using bfloat16 = sycl::ext::oneapi::bfloat16;
-
-#define SG_SZ 16
-
 #define TM 8
 #define TN SG_SZ
 #define TK 16
