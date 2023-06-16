6a2ead96307c7c8b78a3961379d7ccc49e93eb8e
Julian Oppermann <julian.oppermann@codeplay.com>
2023-06-16 10:11:05 +0100

    Much nicer test using `SYCL_EXTERNAL`.
    Signed-off-by: Julian Oppermann <julian.oppermann@codeplay.com>

--- polygeist/tools/cgeist/Test/Verification/sycl/math_funcs.cpp
+++ polygeist/tools/cgeist/Test/Verification/sycl/math_funcs.cpp
@@ -1,86 +1,77 @@
-// RUN: clang++ -Xcgeist --use-opaque-pointers=1 -fsycl -fsycl-targets=spir64-unknown-unknown-syclmlir -O0 -w %s -o %t.O0.out 2>&1 | FileCheck %s --allow-empty --implicit-check-not="{{error|Error}}:"
-// RUN: clang++ -Xcgeist --use-opaque-pointers=1 -fsycl -fsycl-targets=spir64-unknown-unknown-syclmlir -O1 -w %s -o %t.O1.out 2>&1 | FileCheck %s --allow-empty --implicit-check-not="{{error|Error}}:"
-// RUN: clang++ -Xcgeist --use-opaque-pointers=1 -fsycl -fsycl-targets=spir64-unknown-unknown-syclmlir -O2 -w %s -o %t.O2.out 2>&1 | FileCheck %s --allow-empty --implicit-check-not="{{error|Error}}:"
-// RUN: clang++ -Xcgeist --use-opaque-pointers=1 -fsycl -fsycl-targets=spir64-unknown-unknown-syclmlir -O3 -w %s -o %t.O3.out 2>&1 | FileCheck %s --allow-empty --implicit-check-not="{{error|Error}}:"
-// RUN: clang++ -Xcgeist --use-opaque-pointers=1 -fsycl -fsycl-targets=spir64-unknown-unknown-syclmlir -Ofast -w %s -o %t.Ofast.out 2>&1 | FileCheck %s --allow-empty --implicit-check-not="{{error|Error}}:"
+// Test that `sycl.math.*` operations are constructed ...
+// RUN: clang++ -Xcgeist --use-opaque-pointers=1 -fsycl -fsycl-device-only -fsycl-targets=spir64-unknown-unknown-syclmlir \
+// RUN:     -O0 -w -emit-mlir -S -o - %s | FileCheck %s --check-prefixes=BOTH,MLIR
 
-// RUN: clang++ -Xcgeist --use-opaque-pointers=1 -fsycl -fsycl-device-only -O0 -w -emit-llvm -fsycl-targets=spir64-unknown-unknown-syclmlir %s -o %t.bc
+// ... and lowered to the corresponding LLVM intrinsics.
+// RUN: clang++ -Xcgeist --use-opaque-pointers=1 -fsycl -fsycl-device-only -fsycl-targets=spir64-unknown-unknown-syclmlir \
+// RUN:     -O0 -w -emit-llvm -S -o %t.ll %s && FileCheck %s --check-prefixes=BOTH,LLVM < %t.ll
 
 // Test that the LLVMIR generated is verifiable.
-// RUN: opt -passes=verify -disable-output < %t.bc
+// RUN: opt -passes=verify -disable-output %t.ll
 
 // Verify that LLVMIR generated is translatable to SPIRV.
-// RUN: llvm-spirv %t.bc
-
-// Test that the SYCL math functions are lowered to llvm intrinsics.
-// RUN: llvm-dis %t.bc
-// RUN: cat %t.ll | FileCheck %s --check-prefix=LLVM
-
-#include <climits>
-#include <cmath>
+// RUN: llvm-as %t.ll && llvm-spirv %t.bc
 
 #include <sycl/sycl.hpp>
-using namespace sycl;
 
-void host_math_funcs(std::array<float, 1> &A) {
-  auto q = queue{};
-  device d = q.get_device();
-  std::cout << "Using " << d.get_info<info::device::name>() << "\n";
-
-  {
-    auto buf = buffer<float, 1>{A.data(), 1};
-    q.submit([&](handler &cgh) {
-      auto A = buf.get_access<access::mode::read_write>(cgh);
-      cgh.single_task<class kernel_math_funcs>([=]() {
-        // LLVM: define internal spir_func void @_ZZZ15host_math_funcsRSt5arrayIfLm1EEENKUlRN4sycl3_V17handlerEE_clES5_ENKUlvE_clEv
-        float a = A[0], b, c;
-        // LLVM: call float @llvm.ceil.f32
-        a += b = sycl::ceil(a);
-        // LLVM: call float @llvm.copysign.f32
-        a += c = sycl::copysign(a, b);
-        // LLVM: call float @llvm.cos.f32
-        a += sycl::cos(a);
-        // LLVM: call float @llvm.exp.f32
-        a += sycl::exp(a);
-        // LLVM: call float @llvm.exp2.f32
-        a += sycl::exp2(a);
-        // LLVM: %[[exp:.*]] = call float @llvm.exp.f32
-        // LLVM-NEXT: fsub float %[[exp]], 1.000000e+00
-        a += sycl::expm1(a);
-        // LLVM: call float @llvm.fabs.f32
-        a += sycl::fabs(a);
-        // LLVM: call float @llvm.floor.f32
-        a += sycl::floor(a);
-        // LLVM: call float @llvm.fma.f32
-        a += sycl::fma(a, b, c);
-        // LLVM: call float @llvm.log.f32
-        a += sycl::log(a);
-        // LLVM: call float @llvm.log10.f32
-        a += sycl::log10(a);
-        // LLVM: call float @llvm.log2.f32
-        a += sycl::log2(a);
-        // LLVM: call float @llvm.pow.f32
-        a += sycl::pow(a, a);
-        // LLVM: call float @llvm.round.f32
-        a += sycl::round(a);
-        // LLVM: %[[sqrt:.*]] = call float @llvm.sqrt.f32
-        // LLVM-NEXT: fdiv float 1.000000e+00, %[[sqrt]]
-        a += sycl::rsqrt(a);
-        // LLVM: call float @llvm.sin.f32
-        a += sycl::sin(a);
-        // LLVM: call float @llvm.sqrt.f32
-        a += sycl::sqrt(a);
-        // LLVM: call float @llvm.trunc.f32
-        a += sycl::trunc(a);
-        A[0] = a;
-      });
-    });
-  }
-}
-
-int main() {
-  std::array<float, 1> A = {0.0f};
-  host_math_funcs(A);
-  assert(std::fabs(A[0] - std::sqrt(2.0f)) < std::numeric_limits<float>::epsilon() );
-  std::cout << "Test passed" << std::endl;
+// BOTH-LABEL: math_funcs_float
+SYCL_EXTERNAL float math_funcs_float(float a, float b, float c) {
+  // MLIR: sycl.math.ceil
+  // LLVM: call float @llvm.ceil.f32
+  a += sycl::ceil(a);
+  // MLIR: sycl.math.copysign
+  // LLVM: call float @llvm.copysign.f32
+  a += sycl::copysign(a, b);
+  // MLIR: sycl.math.cos
+  // LLVM: call float @llvm.cos.f32
+  a += sycl::cos(a);
+  // MLIR: sycl.math.exp
+  // LLVM: call float @llvm.exp.f32
+  a += sycl::exp(a);
+  // MLIR: sycl.math.exp2
+  // LLVM: call float @llvm.exp2.f32
+  a += sycl::exp2(a);
+  // MLIR: sycl.math.expm1
+  // LLVM: %[[exp:.*]] = call float @llvm.exp.f32
+  // LLVM-NEXT: fsub float %[[exp]], 1.000000e+00
+  a += sycl::expm1(a);
+  // MLIR: sycl.math.fabs
+  // LLVM: call float @llvm.fabs.f32
+  a += sycl::fabs(a);
+  // MLIR: sycl.math.floor
+  // LLVM: call float @llvm.floor.f32
+  a += sycl::floor(a);
+  // MLIR: sycl.math.fma
+  // LLVM: call float @llvm.fma.f32
+  a += sycl::fma(a, b, c);
+  // MLIR: sycl.math.log
+  // LLVM: call float @llvm.log.f32
+  a += sycl::log(a);
+  // MLIR: sycl.math.log10
+  // LLVM: call float @llvm.log10.f32
+  a += sycl::log10(a);
+  // MLIR: sycl.math.log2
+  // LLVM: call float @llvm.log2.f32
+  a += sycl::log2(a);
+  // MLIR: sycl.math.pow
+  // LLVM: call float @llvm.pow.f32
+  a += sycl::pow(a, a);
+  // MLIR: sycl.math.round
+  // LLVM: call float @llvm.round.f32
+  a += sycl::round(a);
+  // MLIR: sycl.math.rsqrt
+  // LLVM: %[[sqrt:.*]] = call float @llvm.sqrt.f32
+  // LLVM-NEXT: fdiv float 1.000000e+00, %[[sqrt]]
+  a += sycl::rsqrt(a);
+  // MLIR: sycl.math.sin
+  // LLVM: call float @llvm.sin.f32
+  a += sycl::sin(a);
+  // MLIR: sycl.math.sqrt
+  // LLVM: call float @llvm.sqrt.f32
+  a += sycl::sqrt(a);
+  // MLIR: sycl.math.trunc
+  // LLVM: call float @llvm.trunc.f32
+  a += sycl::trunc(a);
+  
+  return a;
 }
