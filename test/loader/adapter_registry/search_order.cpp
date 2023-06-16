107d166144246e091638adc0c108a9d849cc6445
Krzysztof Swiecicki <krzysztof.swiecicki@intel.com>
2023-06-16 13:46:00 +0200

    Fix adapter discovery behavior change in tests

--- test/loader/adapter_registry/search_order.cpp
+++ test/loader/adapter_registry/search_order.cpp
@@ -5,28 +5,29 @@
 
 #include "fixtures.hpp"
 
+template <typename P>
+void assertRegistryPathSequence(std::vector<fs::path> testAdapterPaths,
+                                P predicate) {
+    static size_t assertIndex = 0;
+
+    auto pathIt = std::find_if(testAdapterPaths.cbegin(),
+                               testAdapterPaths.cend(), predicate);
+    size_t index = std::distance(testAdapterPaths.cbegin(), pathIt);
+    ASSERT_EQ(index, assertIndex++);
+}
+
 TEST_F(adapterRegSearchTest, testSearchOrder) {
     // Adapter search order:
     // 1. Every path from UR_ADAPTERS_SEARCH_PATH.
-    // 2. OS search paths.
+    // 2. OS search paths (disabled on Windows).
     // 3. Loader library directory.
-
     auto it = std::find_if(registry.cbegin(), registry.cend(), hasTestLibName);
     ASSERT_NE(it, registry.end());
 
     auto testAdapterPaths = *it;
-    auto pathIt = std::find_if(testAdapterPaths.cbegin(),
-                               testAdapterPaths.cend(), isTestEnvPath);
-    std::size_t index = std::distance(testAdapterPaths.cbegin(), pathIt);
-    ASSERT_EQ(index, 0);
-
-    pathIt = std::find_if(testAdapterPaths.cbegin(), testAdapterPaths.cend(),
-                          isTestLibName);
-    index = std::distance(testAdapterPaths.cbegin(), pathIt);
-    ASSERT_EQ(index, 1);
-
-    pathIt = std::find_if(testAdapterPaths.cbegin(), testAdapterPaths.cend(),
-                          isCurPath);
-    index = std::distance(testAdapterPaths.cbegin(), pathIt);
-    ASSERT_EQ(index, 2);
+    assertRegistryPathSequence(testAdapterPaths, isTestEnvPath);
+#ifndef _WIN32
+    assertRegistryPathSequence(testAdapterPaths, isTestLibName);
+#endif
+    assertRegistryPathSequence(testAdapterPaths, isCurPath);
 }
