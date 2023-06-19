d80109e3a59c452fb1c3c9c31c4da5944dab343f
Alberto Cabrera <alberto.cabrera@codeplay.com>
2023-06-19 15:54:13 +0100

    [SYCL][COMPAT] Dim3 and macro definitions added.
    Co-Authored-By: Gordon Brown <gordon@codeplay.com>
    Co-Authored-By: Joe Todd <joe.todd@codeplay.com>
    Co-Authored-By: Pietro Ghiglio <pietro.ghiglio@codeplay.com>
    Co-Authored-By: Ruyman Reyes <ruyman@codeplay.com>

---
+++ sycl/cmake/modules/AddSYCLLibraryUnitTest.cmake
@@ -0,0 +1,53 @@
+# add_sycl_library_unittest(test_suite_name sycl_extra_flags 
+#                           file1.cpp file2.cpp ...)
+#
+# sycl_extra_flags: Clang extra compiler flags, e.g. 
+#                   "-fsycl-unnamed-lambdas;-fsycl-device-code-split"
+#
+# Will compile the list of files together using clang.
+# Produces a binary names `basename(test_suite_name)`.
+macro(add_sycl_library_unittest test_suite_name sycl_extra_flags)
+
+  add_custom_target(check-sycl-${test_suite_name}
+    ${CMAKE_COMMAND} -E env
+    LD_LIBRARY_PATH="${CMAKE_BINARY_DIR}/lib"
+    LLVM_PROFILE_FILE="${SYCL_COVERAGE_PATH}/${test_dirname}.profraw"
+    env SYCL_CONFIG_FILE_NAME=null.cfg
+    env SYCL_DEVICELIB_NO_FALLBACK=1
+    env SYCL_CACHE_DIR="${CMAKE_BINARY_DIR}/sycl_cache"
+    ${CMAKE_CURRENT_BINARY_DIR}/${test_suite_name}
+  )
+
+  add_dependencies(check-sycl-unittests-libs check-sycl-${test_suite_name})
+
+  if (SYCL_ENABLE_COVERAGE)
+    list(APPEND sycl_extra_flags "-fprofile-instr-generate")
+    list(APPEND sycl_extra_flags "-fcoverage-mapping")
+  endif()
+  
+  get_target_property(GTEST_INCLUDES llvm_gtest INCLUDE_DIRECTORIES)
+  foreach(_dir ${GTEST_INCLUDES})
+    # Avoid -I when _dir contains an empty generator expression.
+    list(APPEND INCLUDE_COMPILER_STRING "$<$<BOOL:${_dir}>:-I${_dir}>")
+  endforeach()
+
+  add_sycl_executable(
+    ${test_suite_name}
+    OPTIONS
+      ${sycl_extra_flags}
+      ${INCLUDE_COMPILER_STRING}
+    LIBRARIES
+      llvm_gtest_main
+      llvm_gtest
+      LLVMSupport
+      LLVMDemangle
+      pthread
+      dl
+      ncurses
+    SOURCES
+      ${ARGN}
+    DEPENDANTS
+      check-sycl-${test_suite_name})
+
+endmacro()
+
