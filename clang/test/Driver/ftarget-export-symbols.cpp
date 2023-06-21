29a70c72763b0f9537999eb08b78892e428d8b94
Michael D Toguchi <michael.d.toguchi@intel.com>
2023-06-21 15:43:47 -0700

    Update test to check for unused diagnostic when appropriate

--- clang/test/Driver/ftarget-export-symbols.cpp
+++ clang/test/Driver/ftarget-export-symbols.cpp
@@ -14,3 +14,16 @@
 // TARGET_EXPORT: ocloc{{.*}} "-output"
 // TARGET_EXPORT: "-options" "-library-compilation"
 // NO_TARGET_EXPORT-NOT: "-library-compilation"
+
+// 'unused' for non-spir64_gen targets
+// RUN: %clang -### -target x86_64-unknown-linux-gnu -fsycl \
+// RUN:    -fsycl-targets=spir64 -ftarget-export-symbols %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=UNUSED %s
+// RUN: %clang -### -target x86_64-unknown-linux-gnu -fsycl \
+// RUN:    -fsycl-targets=spir64_x86_64 -ftarget-export-symbols %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=UNUSED %s
+// RUN: %clang -### -target x86_64-unknown-linux-gnu -fsycl \
+// RUN:    -fsycl-targets=spir64_fpga -ftarget-export-symbols %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=UNUSED %s
+
+// UNUSED: argument unused during compilation: '-ftarget-export-symbols'
