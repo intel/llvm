4098f4ae6b2e68155cd5133040085fd23cfe6680
Andrei Elovikov <andrei.elovikov@intel.com>
2023-06-21 10:10:03 -0700

    Test lint

--- sycl/source/accessor.cpp
+++ sycl/source/accessor.cpp
@@ -13,7 +13,7 @@ namespace sycl {
 __SYCL_INLINE_VER_NAMESPACE(_V1) {
 namespace detail {
 device getDeviceFromHandler(handler &CommandGroupHandlerRef) {
-  return CommandGroupHandlerRef.MQueue->get_device();
+    return CommandGroupHandlerRef.MQueue->get_device();
 }
 
 AccessorBaseHost::AccessorBaseHost(id<3> Offset, range<3> AccessRange,
