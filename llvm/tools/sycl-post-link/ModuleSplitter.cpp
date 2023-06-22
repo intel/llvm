b6fed48548e77c775a9e4711bb1301cba7c60e5c
Lu, John <john.lu@intel.com>
2023-06-21 22:11:26 -0700

    Apply clang-format
    Signed-off-by: Lu, John <john.lu@intel.com>

--- llvm/tools/sycl-post-link/ModuleSplitter.cpp
+++ llvm/tools/sycl-post-link/ModuleSplitter.cpp
@@ -134,7 +134,8 @@ bool isESIMDFunction(const Function &F) {
 // Predicate for Internalize pass.
 bool mustPreserveGV(const GlobalValue &GV) {
   if (const Function *F = dyn_cast<Function>(&GV))
-    return F->getCallingConv() == CallingConv::SPIR_KERNEL || F->isDeclaration() || F->hasFnAttribute("sycl-module-id");
+    return F->getCallingConv() == CallingConv::SPIR_KERNEL ||
+           F->isDeclaration() || F->hasFnAttribute("sycl-module-id");
 
   GV.removeDeadConstantUsers();
   return !GV.use_empty();
