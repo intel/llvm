01e168f398c199782c2864ce249dd094dc46691c
Dmitry Vodoypanov <dmitry.vodopyanov@intel.com>
2023-06-22 08:34:36 -0400

    Add missing ABI symbols and improve E2E test

--- sycl/test-e2e/DeviceArchitecture/device_architecture_on_host.cpp
+++ sycl/test-e2e/DeviceArchitecture/device_architecture_on_host.cpp
@@ -1,7 +1,7 @@
 // UNSUPPORTED: cuda, hip, esimd_emulator
 
 // RUN: %{build} -o %t.out
-// RUN: env SYCL_PI_TRACE=-1 %{run} %t.out 2>&1 | FileCheck %s
+// RUN: %{run} %t.out
 
 #include <sycl/sycl.hpp>
 
@@ -11,10 +11,8 @@ int main() {
 
   sycl::ext::oneapi::experimental::architecture arch = dev.get_info<
       sycl::ext::oneapi::experimental::info::device::architecture>();
-  // CHECK: piDeviceGetInfo
 
-  bool is_arch_bdw = dev.ext_oneapi_architecture_is(
-      sycl::ext::oneapi::experimental::architecture::intel_gpu_bdw);
-  // CHECK: piDeviceGetInfo
+  assert(dev.ext_oneapi_architecture_is(arch));
+
   return 0;
 }
