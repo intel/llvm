7b6312448692837fe884936c3eaa3dc8b016845c
Teresa Johnson <tejohnson@google.com>
2023-06-23 11:01:22 -0700

    [LTO][GlobalDCE] Use pass parameter instead of module flag for LTO phase
    D63932 added a module flag to indicate that we are executing the regular
    LTO post merge pipeline, so that GlobalDCE could perform more aggressive
    optimization for Dead Virtual Function Elimination. This caused issues
    trying to reuse bitcode that had already been through the LTO pipeline
    (see context in D139816).
    
    Instead support this by passing down a parameter flag to the
    GlobalDCEPass constructor, which is the more usual way for indicating
    this information.
    
    Most test changes are to remove incidental uses of this flag. Of the 2
    real uses, llvm/test/LTO/ARM/lto-linking-metadata.ll is now obsolete and
    removed in this patch, and the virtual-functions-visibility-post-lto.ll
    test is updated to use the regular LTO default pipeline where this
    parameter is set to true.
    
    Differential Revision: https://reviews.llvm.org/D153655

--- llvm/lib/Passes/PassBuilder.cpp
+++ llvm/lib/Passes/PassBuilder.cpp
@@ -682,6 +682,10 @@ Expected<bool> parseSinglePassOption(StringRef Params, StringRef OptionName,
   return Result;
 }
 
+Expected<bool> parseGlobalDCEPassOptions(StringRef Params) {
+  return parseSinglePassOption(Params, "vfe-linkage-unit-visibility", "GlobalDCE");
+}
+
 Expected<bool> parseInlinerPassOptions(StringRef Params) {
   return parseSinglePassOption(Params, "only-mandatory", "InlinerPass");
 }
