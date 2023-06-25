c6cd4ebd9d3eb6d8f1ba3ff44cb4bc3bd61e23e9
Takuya Shimizu <shimizu2486@gmail.com>
2023-06-26 00:27:15 +0900

    [clang][Diagnostics] Provide parameter source range to arity-mismatch notes
    Consider the following piece of code:
    ```
    void func( int aa,
               int bb,
               int cc) {}
    
    void arity_mismatch() {
      func(2, 4);
    }
    ```
    BEFORE:
    ```
    source.cpp:6:3: error: no matching function for call to 'func'
        6 |   func(2, 4);
          |   ^~~~
    source.cpp:1:6: note: candidate function not viable: requires 3 arguments, but 2 were provided
        1 | void func( int aa,
          |      ^
    ```
    AFTER:
    ```
    source.cpp:6:3: error: no matching function for call to 'func'
        6 |   func(2, 4);
          |   ^~~~
    source.cpp:1:6: note: candidate function not viable: requires 3 arguments, but 2 were provided
        1 | void func( int aa,
          |      ^     ~~~~~~~
        2 |            int bb,
          |            ~~~~~~~
        3 |            int cc) {}
          |            ~~~~~~
    ```
    
    Reviewed By: cjdb, aaron.ballman
    
    Differential Revision: https://reviews.llvm.org/D153267

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -6543,7 +6543,8 @@ Sema::ConvertArgumentsForCall(CallExpr *Call, Expr *Fn,
 
       // Emit the location of the prototype.
       if (!TC && FDecl && !FDecl->getBuiltinID() && !IsExecConfig)
-        Diag(FDecl->getLocation(), diag::note_callee_decl) << FDecl;
+        Diag(FDecl->getLocation(), diag::note_callee_decl)
+            << FDecl << FDecl->getParametersSourceRange();
 
       return true;
     }
@@ -6588,7 +6589,8 @@ Sema::ConvertArgumentsForCall(CallExpr *Call, Expr *Fn,
 
       // Emit the location of the prototype.
       if (!TC && FDecl && !FDecl->getBuiltinID() && !IsExecConfig)
-        Diag(FDecl->getLocation(), diag::note_callee_decl) << FDecl;
+        Diag(FDecl->getLocation(), diag::note_callee_decl)
+            << FDecl << FDecl->getParametersSourceRange();
 
       // This deletes the extra arguments.
       Call->shrinkNumArgs(NumParams);
