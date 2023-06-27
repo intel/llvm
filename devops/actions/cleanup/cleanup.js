beb95fd3dc29bdab473637b9a598174729ecac05
Andrei Elovikov <andrei.elovikov@intel.com>
2023-06-27 09:21:17 -0700

    [CI] Generalize hidden files removal in actions/cleanup

--- devops/actions/cleanup/cleanup.js
+++ devops/actions/cleanup/cleanup.js
@@ -1,4 +1,3 @@
-const path = process.env.GITHUB_WORKSPACE + '/*';
+const path = process.env.GITHUB_WORKSPACE + '/{*,.*}';
 console.log('Cleaning ' + path)
 require('child_process').execSync('rm -rf ' + path);
-require('child_process').execSync('rm -rf ' + process.env.GITHUB_WORKSPACE + '/.git');
