6ff72653a5feb5d15f5aee3d6b16d923001489e8
Petr Vesely <petr.vesely@codeplay.com>
2023-06-28 12:06:46 +0100

    [UR][CTS] Adds CTS coverage for urVirtualMemGranularityGetInfo

--- source/loader/layers/validation/ur_valddi.cpp
+++ source/loader/layers/validation/ur_valddi.cpp
@@ -1890,8 +1890,20 @@ __urdlllocal ur_result_t UR_APICALL urVirtualMemGranularityGetInfo(
             return UR_RESULT_ERROR_INVALID_NULL_HANDLE;
         }
 
+        if (propSize != 0 && pPropValue == NULL) {
+            return UR_RESULT_ERROR_INVALID_NULL_POINTER;
+        }
+
+        if (pPropValue == NULL && pPropSizeRet == NULL) {
+            return UR_RESULT_ERROR_INVALID_NULL_POINTER;
+        }
+
         if (UR_VIRTUAL_MEM_GRANULARITY_INFO_RECOMMENDED < propName) {
             return UR_RESULT_ERROR_INVALID_ENUMERATION;
+        }
+
+        if (propSize == 0 && pPropValue != NULL) {
+            return UR_RESULT_ERROR_INVALID_SIZE;
         }
     }
 
