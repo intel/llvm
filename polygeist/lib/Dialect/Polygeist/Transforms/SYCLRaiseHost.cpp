08e22540a3e0568fc20aa35971c3fe6e709b71d4
Lukas Sommer <lukas.sommer@codeplay.com>
2023-06-28 11:28:31 +0100

    Add pattern to raise sycl::buffer constructor
    Signed-off-by: Lukas Sommer <lukas.sommer@codeplay.com>

--- polygeist/lib/Dialect/Polygeist/Transforms/SYCLRaiseHost.cpp
+++ polygeist/lib/Dialect/Polygeist/Transforms/SYCLRaiseHost.cpp
@@ -21,7 +21,9 @@
 #include "mlir/Dialect/Polygeist/Utils/Utils.h"
 #include "mlir/Dialect/SYCL/IR/SYCLOps.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+#include "llvm/Demangle/Demangle.h"
 #include "llvm/Support/Debug.h"
+#include "llvm/Support/Regex.h"
 
 namespace mlir {
 namespace polygeist {
@@ -45,6 +47,90 @@ public:
 };
 
 } // anonymous namespace
+
+//===----------------------------------------------------------------------===//
+// Helper
+//===----------------------------------------------------------------------===//
+
+namespace {
+
+bool isConstructor(CallOpInterface call, LLVM::AllocaOp alloc) {
+  if (call->getOperand(0) != alloc) {
+    // Allocation ('this*') must be first argument for constructor call.
+    return false;
+  }
+
+  CallInterfaceCallable callableOp = call.getCallableForCallee();
+  StringRef funcName = callableOp.get<SymbolRefAttr>().getLeafReference();
+
+  llvm::ItaniumPartialDemangler Demangler;
+  Demangler.partialDemangle(funcName.data());
+  if (!Demangler.isCtorOrDtor())
+    return false;
+
+  char *demangled = Demangler.finishDemangle(nullptr, 0);
+  if (!demangled)
+    // Demangling failed
+    return false;
+
+  llvm::StringRef demangledName{demangled};
+  bool isDestructor = demangledName.contains('~');
+  free(demangled);
+  return !isDestructor;
+}
+
+bool anyUsesBetween(LLVM::AllocaOp alloc, Operation *end) {
+  assert(alloc->getBlock() && end->getBlock() &&
+         "Expecting operations to be aprt of a block");
+
+  if (alloc->getBlock() != end->getBlock())
+    return true;
+
+  llvm::SmallPtrSet<Operation *, 8> uses(alloc->user_begin(),
+                                         alloc->user_end());
+
+  bool started = false;
+  for (auto &op : *alloc->getBlock()) {
+    if (&op == alloc)
+      started = true;
+
+    if (started) {
+      if (&op == end)
+        return false;
+
+      if (uses.contains(&op))
+        return true;
+    }
+  }
+  llvm_unreachable("'end' operation before allocation");
+}
+
+sycl::BufferType getBufferTypeFromConstructor(CallOpInterface constructor) {
+  CallInterfaceCallable callableOp = constructor.getCallableForCallee();
+  StringRef constructorName =
+      callableOp.get<SymbolRefAttr>().getLeafReference();
+
+  auto demangledName = llvm::demangle(constructorName);
+
+  // Try to determine the dimensions of the buffer by parsing the template
+  // parameter from the demangled name of the constructor.
+  llvm::Regex bufferTemplate("buffer<.*, ([0-9]+)");
+  llvm::SmallVector<StringRef> matches;
+  bool regexMatch = bufferTemplate.match(demangledName, &matches);
+  unsigned dimensions = 1;
+  if (regexMatch)
+    std::stoul(matches[1].str());
+
+  // FIXME: There's currently no good way to obtain the element type of the
+  // buffer from the constructor call (or allocation). Parsing it from the
+  // demangled name, as done for 'dimensions' above, would require translation
+  // from C++ types to MLIR types, which is not available here.
+  Type elemTy = LLVM::LLVMVoidType::get(constructor->getContext());
+
+  return sycl::BufferType::get(constructor->getContext(), elemTy, dimensions);
+}
+
+} // namespace
 
 //===----------------------------------------------------------------------===//
 // Pattern
@@ -107,6 +193,54 @@ private:
                                        : std::nullopt;
   }
 };
+
+class BufferConstructorPattern : public OpRewritePattern<LLVM::InvokeOp> {
+
+public:
+  using OpRewritePattern<LLVM::InvokeOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(LLVM::InvokeOp invoke,
+                                PatternRewriter &rewriter) const final {
+
+    if (invoke.getArgOperands().empty())
+      return failure();
+
+    // 'this*' is the first argument to the constructor call, if it is a
+    // constructor.
+    auto alloc = dyn_cast_or_null<LLVM::AllocaOp>(
+        invoke.getArgOperands().front().getDefiningOp());
+
+    if (!alloc)
+      return failure();
+
+    assert(alloc.getElemType().has_value() &&
+           "Expecting element type attribute for opaque alloca");
+
+    auto allocTy = *alloc.getElemType();
+    auto structAllocTy = dyn_cast<LLVM::LLVMStructType>(allocTy);
+    if (!structAllocTy || structAllocTy.getName() != "class.sycl::_V1::buffer")
+      return failure();
+
+    if (invoke.getNumResults())
+      // Constructor should not return anything.
+      return failure();
+
+    if (!isConstructor(invoke, alloc))
+      // Invoke is not a constructor call.
+      return failure();
+
+    rewriter.create<sycl::SYCLHostConstructorOp>(
+        invoke->getLoc(), invoke.getArgOperands().front(),
+        invoke.getArgOperands().drop_front(1),
+        TypeAttr::get(getBufferTypeFromConstructor(invoke)));
+
+    rewriter.create<LLVM::BrOp>(invoke->getLoc(),
+                                invoke.getNormalDestOperands(),
+                                invoke.getNormalDest());
+    rewriter.eraseOp(invoke);
+    return success();
+  }
+};
 } // namespace
 
 //===----------------------------------------------------------------------===//
@@ -118,7 +252,7 @@ void SYCLRaiseHostConstructsPass::runOnOperation() {
   MLIRContext *context = &getContext();
 
   RewritePatternSet rewritePatterns{context};
-  rewritePatterns.add<RaiseKernelName>(context);
+  rewritePatterns.add<RaiseKernelName, BufferConstructorPattern>(context);
   FrozenRewritePatternSet frozen(std::move(rewritePatterns));
 
   if (failed(applyPatternsAndFoldGreedily(scopeOp, frozen)))
