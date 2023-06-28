7f8bff7a28e47997c3922c735d79192b51082634
Dmitry Vodopyanov <dmitry.vodopyanov@intel.com>
2023-06-28 18:04:00 +0200

    Update accessor_local.cpp

--- sycl/test-e2e/ESIMD/accessor_local.cpp
+++ sycl/test-e2e/ESIMD/accessor_local.cpp
@@ -4,6 +4,9 @@
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 
+// TODO: Enable the test when GPU driver is ready/fixed.
+// XFAIL: gpu && windows
+
 // This test verifies usage of local_accessor methods operator[]
 // and get_pointer().
 
