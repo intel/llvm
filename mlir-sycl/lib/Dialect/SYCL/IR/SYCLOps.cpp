4e06ff0bcbe55fe9f3886087f34395a086394826
Julian Oppermann <julian.oppermann@codeplay.com>
2023-07-04 10:26:41 +0200

    [SYCL-MLIR] Add wrap, unwrap ops. (#10139)
    The `!sycl_half` type technically just wrap an `f16` value. I'm
    proposing to add two new cast-like operations to the dialect in order to
    provide a bridge to and from the upstream dialects, e.g.
    
    ```mlir
    %res = sycl.math.sin %0 : !sycl_half
    
    // ... lowers to ...
    
    %1 = sycl.mlir.unwrap %0 : !sycl_half to f16
    %2 = math.sin %1 : f16
    %res = sycl.mlir.wrap %2 : f16 to !sycl_half
    ```
    
    Folders are provided to elide redundant (un)wrappings in operation
    chains.
    
    Notes:
    - Converting `!sycl_half` to `f16` everywhere in `PolygeistToLLVM` seems
    impractical at this point, as I believe this would involve rewriting
    functions signatures etc.
    - I plan to lower these ops to `llvm.insertvalue` and
    `llvm.extractvalue`, when the SYCL-types are mapped to LLVM structs.
    - Adding support for SYCL vector types as well would be straightforward,
    but is not part of this PR to keep it focused.
    
    ---------
    
    Signed-off-by: Julian Oppermann <julian.oppermann@codeplay.com>

--- mlir-sycl/lib/Dialect/SYCL/IR/SYCLOps.cpp
+++ mlir-sycl/lib/Dialect/SYCL/IR/SYCLOps.cpp
@@ -259,6 +259,46 @@ LogicalResult SYCLIDConstructorOp::verify() {
       "expects a different signature. Check documentation for details");
 }
 
+static Type getBodyType(Type type) {
+  auto *ctx = type.getContext();
+  return TypeSwitch<Type, Type>(type)
+      .Case<HalfType>([&](auto) { return Float16Type::get(ctx); })
+      .Default({});
+}
+
+bool SYCLWrapOp::areCastCompatible(TypeRange inputs, TypeRange outputs) {
+  if (inputs.size() != 1 || outputs.size() != 1)
+    return false;
+
+  Type sourceType = inputs.front();
+  Type resultType = outputs.front();
+  Type bodyType = getBodyType(resultType);
+
+  return bodyType && sourceType == bodyType;
+}
+
+OpFoldResult SYCLWrapOp::fold(FoldAdaptor adaptor) {
+  if (auto unwrap = dyn_cast_or_null<SYCLUnwrapOp>(getSource().getDefiningOp()))
+    return unwrap.getSource();
+  return nullptr;
+}
+
+bool SYCLUnwrapOp::areCastCompatible(TypeRange inputs, TypeRange outputs) {
+  if (inputs.size() != 1 || outputs.size() != 1)
+    return false;
+
+  Type sourceType = inputs.front();
+  Type resultType = outputs.front();
+  Type bodyType = getBodyType(sourceType);
+  return bodyType && resultType == bodyType;
+}
+
+OpFoldResult SYCLUnwrapOp::fold(FoldAdaptor adaptor) {
+  if (auto wrap = dyn_cast_or_null<SYCLWrapOp>(getSource().getDefiningOp()))
+    return wrap.getSource();
+  return nullptr;
+}
+
 static LogicalResult verifyReferencesKernel(SymbolUserOpInterface user,
                                             SymbolTableCollection &symbolTable,
                                             SymbolRefAttr symbol) {
