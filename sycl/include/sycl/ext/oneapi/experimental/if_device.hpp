6050fe4739c3a097942cf3a9087707cd304f89e1
Andrei Elovikov <andrei.elovikov@intel.com>
2023-07-05 08:43:02 -0700

    Add licence header

--- sycl/include/sycl/ext/oneapi/experimental/if_device.hpp
+++ sycl/include/sycl/ext/oneapi/experimental/if_device.hpp
@@ -1,3 +1,11 @@
+//==------ if_device.hpp --- SYCL ext header file -----------=--*- C++ -*---==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #pragma once
 
 #include <sycl/detail/common.hpp>
