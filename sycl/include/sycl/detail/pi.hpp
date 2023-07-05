intel/llvm

commit b2d0837e96449958e9062350a5b5c82d6df0cfda

[SYCLNATIVECPU] Initial Native CPU plug-in implementation (#9635)
This PR contains the initial implementation of the SYCL Native CPU
Plug-in.
This work is still in progress, so only basic SYCL features are
supported right now. You can refer to the tests in
`sycl/test/native_cpu` for examples of supported SYCL applications;
additional information can be found in `sycl/doc/SYCLNativeCPU.md`.
Currently there are no performance optimizations, but they will be added
in subsequent PRs, along with the support for other SYCL features.

---------

Co-authored-by: Uwe Dolinsky <uwe@codeplay.com>
Co-authored-by: Alexey Sachkov <alexey.sachkov@intel.com>
Co-authored-by: Steffen Larsen <steffen.larsen@intel.com>

sycl/include/sycl/detail/pi.hpp
@@ -72,6 +72,7 @@ bool trace(TraceLevel level);
 #define __SYCL_ESIMD_EMULATOR_PLUGIN_NAME "pi_esimd_emulator.dll"
 #define __SYCL_HIP_PLUGIN_NAME "libpi_hip.dll"
 #define __SYCL_UR_PLUGIN_NAME "pi_unified_runtime.dll"
+#define __SYCL_NATIVE_CPU_PLUGIN_NAME "pi_native_cpu.dll"
 #else
 #define __SYCL_OPENCL_PLUGIN_NAME "libpi_opencl.dll"
 #define __SYCL_LEVEL_ZERO_PLUGIN_NAME "libpi_level_zero.dll"
@@ -79,6 +80,7 @@ bool trace(TraceLevel level);
 #define __SYCL_ESIMD_EMULATOR_PLUGIN_NAME "libpi_esimd_emulator.dll"
 #define __SYCL_HIP_PLUGIN_NAME "libpi_hip.dll"
 #define __SYCL_UR_PLUGIN_NAME "libpi_unified_runtime.dll"
+#define __SYCL_NATIVE_CPU_PLUGIN_NAME "libpi_native_cpu.dll"
 #endif
 #elif defined(__SYCL_RT_OS_LINUX)
 #define __SYCL_OPENCL_PLUGIN_NAME "libpi_opencl.so"
@@ -87,6 +89,7 @@ bool trace(TraceLevel level);
 #define __SYCL_ESIMD_EMULATOR_PLUGIN_NAME "libpi_esimd_emulator.so"
 #define __SYCL_HIP_PLUGIN_NAME "libpi_hip.so"
 #define __SYCL_UR_PLUGIN_NAME "libpi_unified_runtime.so"
+#define __SYCL_NATIVE_CPU_PLUGIN_NAME "libpi_native_cpu.so"
 #elif defined(__SYCL_RT_OS_DARWIN)
 #define __SYCL_OPENCL_PLUGIN_NAME "libpi_opencl.dylib"
 #define __SYCL_LEVEL_ZERO_PLUGIN_NAME "libpi_level_zero.dylib"

commit 8852c79c3817652e6eec744e083b8ea6dd3a8fa3

Add __SYCL_NATIVE_CPU_PLUGIN_NAME for MacOS (#10207)
This PR fixes a post commit failure introduced by
https://github.com/intel/llvm/pull/9635

sycl/include/sycl/detail/pi.hpp
@@ -97,6 +97,7 @@ bool trace(TraceLevel level);
 #define __SYCL_ESIMD_EMULATOR_PLUGIN_NAME "libpi_esimd_emulator.dylib"
 #define __SYCL_HIP_PLUGIN_NAME "libpi_hip.dylib"
 #define __SYCL_UR_PLUGIN_NAME "libpi_unified_runtime.dylib"
+#define __SYCL_NATIVE_CPU_PLUGIN_NAME "libpi_native_cpu.dylib"
 #else
 #error "Unsupported OS"
 #endif