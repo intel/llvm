3cd46aaaa2b4b36589825d9395c653a59c76bfd6
Victor Perez <victor.perez@codeplay.com>
2023-07-05 10:35:10 +0100

    Check more patterns

--- polygeist/tools/cgeist/Test/Verification/sycl/host_raising.cpp
+++ polygeist/tools/cgeist/Test/Verification/sycl/host_raising.cpp
@@ -8,10 +8,23 @@ std::vector<float> init(std::size_t);
 
 class KernelName;
 
-// COM: Check we can detect kernel assignment to a sycl::handler:
-
 // CHECK-DAG: gpu.func @_ZTS10KernelName
 // CHECK-DAG: gpu.func @_ZTSN4sycl3_V16detail19__pf_kernel_wrapperI10KernelNameEE
+
+// COM: Check we can detect buffers contruction
+
+// CHECK-LABEL: llvm.func @main
+// CHECK:         sycl.host.constructor({{.*}}) {type = !sycl.buffer<[1, !llvm.void]>}  : (!llvm.ptr, !llvm.ptr, i64, !llvm.ptr, !llvm.ptr) -> ()
+// CHECK:         sycl.host.constructor({{.*}}) {type = !sycl.buffer<[1, !llvm.void]>}  : (!llvm.ptr, !llvm.ptr, i64, !llvm.ptr, !llvm.ptr) -> ()
+// CHECK:         sycl.host.constructor({{.*}}) {type = !sycl.buffer<[1, !llvm.void]>}  : (!llvm.ptr, !llvm.ptr, i64, !llvm.ptr, !llvm.ptr) -> ()
+
+// COM: Check we can detect accessors construction
+
+// CHECK-LABEL: llvm.func internal @_ZNSt17_Function_handlerIFvRN4sycl3_V17handlerEEZ4mainEUlS3_E_E9_M_invokeERKSt9_Any_dataS3_
+// CHECK:          sycl.host.constructor({{.*}}) {type = !sycl_accessor_1_21llvm2Evoid_r_gb} : (!llvm.ptr, !llvm.ptr, !llvm.ptr, !llvm.ptr, !llvm.ptr) -> ()
+// CHECK:          sycl.host.constructor({{.*}}) {type = !sycl_accessor_1_21llvm2Evoid_r_gb} : (!llvm.ptr, !llvm.ptr, !llvm.ptr, !llvm.ptr, !llvm.ptr) -> ()
+
+// COM: Check we can detect kernel assignment to a sycl::handler:
 
 // CHECK-DAG: sycl.host.handler.set_kernel %{{.*}} -> @device_functions::@_ZTSN4sycl3_V16detail19__pf_kernel_wrapperI10KernelNameEE : !llvm.ptr
 // CHECK-DAG: sycl.host.handler.set_kernel %{{.*}} -> @device_functions::@_ZTS10KernelName : !llvm.ptr
