c9ed0caaf4ea013d03ee313174b5fdc649429839
Isaac Ault <isaac.ault@codeplay.com>
2023-07-05 10:30:09 +0100

    Add _EXP

--- source/common/ur_params.hpp
+++ source/common/ur_params.hpp
@@ -2574,8 +2574,8 @@ inline std::ostream &operator<<(std::ostream &os, enum ur_device_info_t value) {
         os << "UR_DEVICE_INFO_BINDLESS_IMAGES_SUPPORT_EXP";
         break;
 
-    case UR_DEVICE_INFO_BINDLESS_IMAGES_SHARED_USM_SUPPORT:
-        os << "UR_DEVICE_INFO_BINDLESS_IMAGES_SHARED_USM_SUPPORT";
+    case UR_DEVICE_INFO_BINDLESS_IMAGES_SHARED_USM_SUPPORT_EXP:
+        os << "UR_DEVICE_INFO_BINDLESS_IMAGES_SHARED_USM_SUPPORT_EXP";
         break;
 
     case UR_DEVICE_INFO_BINDLESS_IMAGES_1D_USM_SUPPORT_EXP:
@@ -4208,7 +4208,7 @@ inline void serializeTagged(std::ostream &os, const void *ptr,
         os << ")";
     } break;
 
-    case UR_DEVICE_INFO_BINDLESS_IMAGES_SHARED_USM_SUPPORT: {
+    case UR_DEVICE_INFO_BINDLESS_IMAGES_SHARED_USM_SUPPORT_EXP: {
         const ur_bool_t *tptr = (const ur_bool_t *)ptr;
         if (sizeof(ur_bool_t) > size) {
             os << "invalid size (is: " << size
