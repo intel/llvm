45ab8bc532d3514fa07d3e9484b33ea85304161d
Lukas Sommer <lukas.sommer@codeplay.com>
2023-07-07 13:04:04 +0100

    Change memory effect to read and write
    Signed-off-by: Lukas Sommer <lukas.sommer@codeplay.com>

--- mlir-sycl/lib/Dialect/SYCL/IR/SYCLHostOps.cpp
+++ mlir-sycl/lib/Dialect/SYCL/IR/SYCLHostOps.cpp
@@ -14,14 +14,18 @@ LogicalResult SYCLHostConstructorOp::verify() {
 void SYCLHostConstructorOp::getEffects(
     SmallVectorImpl<SideEffects::EffectInstance<MemoryEffects::Effect>>
         &effects) {
-  // NOTE: This definition conservatively assumes that all (pointer) arguments
-  // are written to. This is definitely true for the first argument ('this'
-  // pointer), but a pessimistic assumption for the remaining arguments.
+  // NOTE: This definition assumes only the first (`this`) argument is written
+  // to and the remaining arguments, if they are memory arguments, are both read
+  // and written. The latter is a conservative assumption about the behavior of
+  // the constructors and could be changed to enable better analysis.
   auto *defaultResource = SideEffects::DefaultResource::get();
   // The `this` argument will always be written to
   effects.emplace_back(MemoryEffects::Write::get(), getDst(), defaultResource);
-  // For the remaining non-scalar arguments we also assume they are written.
-  for (auto value : getArgs())
-    if (isa<MemRefType, LLVM::LLVMPointerType>(value.getType()))
+  // The rest of the arguments will be scalar or read and write.
+  for (auto value : getArgs()) {
+    if (isa<MemRefType, LLVM::LLVMPointerType>(value.getType())) {
+      effects.emplace_back(MemoryEffects::Read::get(), value, defaultResource);
       effects.emplace_back(MemoryEffects::Write::get(), value, defaultResource);
+    }
+  }
 }
