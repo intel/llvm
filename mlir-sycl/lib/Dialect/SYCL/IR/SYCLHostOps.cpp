61d994ad3ec0682bdae722541ad2eac737da3e00
Lukas Sommer <lukas.sommer@codeplay.com>
2023-07-07 14:49:18 +0100

    Drop write effect
    Signed-off-by: Lukas Sommer <lukas.sommer@codeplay.com>

--- mlir-sycl/lib/Dialect/SYCL/IR/SYCLHostOps.cpp
+++ mlir-sycl/lib/Dialect/SYCL/IR/SYCLHostOps.cpp
@@ -15,17 +15,14 @@ void SYCLHostConstructorOp::getEffects(
     SmallVectorImpl<SideEffects::EffectInstance<MemoryEffects::Effect>>
         &effects) {
   // NOTE: This definition assumes only the first (`this`) argument is written
-  // to and the remaining arguments, if they are memory arguments, are both read
-  // and written. The latter is a conservative assumption about the behavior of
-  // the constructors and could be changed to enable better analysis.
+  // to and the remaining arguments, if they are memory arguments, are only read
+  // from. This is optimistic assumption, but enables better analysis and is
+  // true for the types and properties we are most interested in right now.
   auto *defaultResource = SideEffects::DefaultResource::get();
   // The `this` argument will always be written to
   effects.emplace_back(MemoryEffects::Write::get(), getDst(), defaultResource);
-  // The rest of the arguments will be scalar or read and write.
-  for (auto value : getArgs()) {
-    if (isa<MemRefType, LLVM::LLVMPointerType>(value.getType())) {
+  // The rest of the arguments will be scalar or read from
+  for (auto value : getArgs())
+    if (isa<MemRefType, LLVM::LLVMPointerType>(value.getType()))
       effects.emplace_back(MemoryEffects::Read::get(), value, defaultResource);
-      effects.emplace_back(MemoryEffects::Write::get(), value, defaultResource);
-    }
-  }
 }
