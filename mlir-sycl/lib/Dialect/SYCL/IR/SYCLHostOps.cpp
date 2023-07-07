5dc3b02c4818c6efeb06cff750e0cddaa99d5704
Lukas Sommer <lukas.sommer@codeplay.com>
2023-07-07 07:29:17 +0100

    [SYCL-MLIR] Define mem effects of host constructor
    Define the memory effects of the `sycl.host.constructor` operation.
    Conservatively, it has to be assumed that any memory (pointer or memref) argument will be written.
    
    Signed-off-by: Lukas Sommer <lukas.sommer@codeplay.com>

--- mlir-sycl/lib/Dialect/SYCL/IR/SYCLHostOps.cpp
+++ mlir-sycl/lib/Dialect/SYCL/IR/SYCLHostOps.cpp
@@ -10,3 +10,20 @@ LogicalResult SYCLHostConstructorOp::verify() {
            << type;
   return success();
 }
+
+void SYCLHostConstructorOp::getEffects(
+    SmallVectorImpl<SideEffects::EffectInstance<MemoryEffects::Effect>>
+        &effects) {
+  // NOTE: This definition conservatively assumes that all (pointer) arguments
+  // are written to. This is definitely true for the first argument ('this'
+  // pointer), but a pessimistic assumption for the remaining arguments.
+  auto *defaultResource = SideEffects::DefaultResource::get();
+  // The `this` argument will always be written to
+  effects.emplace_back(MemoryEffects::Write::get(), getDst(), defaultResource);
+  // For the remaining non-scalar arguments we also assume they are written.
+  for (auto value : getArgs()) {
+    if (isa<MemRefType, LLVM::LLVMPointerType>(value.getType())) {
+      effects.emplace_back(MemoryEffects::Write::get(), value, defaultResource);
+    }
+  }
+}
