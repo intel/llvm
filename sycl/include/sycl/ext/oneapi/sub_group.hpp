e4a755ed60cab534f710435ec6b945b74699938d
KornevNikita <nikita.kornev@intel.com>
2023-07-11 08:37:47 -0700

    Apply suggestions

--- sycl/include/sycl/ext/oneapi/sub_group.hpp
+++ sycl/include/sycl/ext/oneapi/sub_group.hpp
@@ -10,6 +10,7 @@
 
 #include <sycl/group.hpp>
 #include <sycl/sub_group.hpp>
+#include <tuple>
 
 namespace sycl {
 __SYCL_INLINE_VER_NAMESPACE(_V1) {
@@ -22,14 +23,9 @@ struct __SYCL_DEPRECATED("use sycl::sub_group() instead") sub_group
   //    sycl::ext::oneapi::experimental::this_sub_group();
   // ...
   // sycl::ext::oneapi::sub_group sg = item.get_sub_group();
-  sub_group(const sycl::sub_group &sg) {
-#ifdef __SYCL_DEVICE_ONLY__
-    sub_group();
-#else
-    throw runtime_error("Sub-groups are not supported on host device.",
-                        PI_ERROR_INVALID_DEVICE);
-#endif
-  }
+  // Note: this constructor is used for implicit conversion. Since the
+  // sub_group class doesn't have any members, just ignore the arg.
+  sub_group(const sycl::sub_group &sg) : sub_group() { std::ignore = sg; }
 
 private:
   sub_group() = default;
