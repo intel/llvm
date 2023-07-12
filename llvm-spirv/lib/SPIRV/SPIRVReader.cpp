313ad0ad5246ae1ce6ef2456280ee829aac4098a
Joshua Cranmer <joshua.cranmer@intel.com>
2023-07-12 09:48:02 -0700

    Emit target extension types for SPIR-V friendly IR.
    The ABI name mangling of target extension types is tweaked to be like a
    pointer-to-a-struct, which maintains somewhat better compatibility with
    the typed pointer representation for name mangling.
    
    Original commit:
    https://github.com/KhronosGroup/SPIRV-LLVM-Translator/commit/15e0aa9

--- llvm-spirv/lib/SPIRV/SPIRVReader.cpp
+++ llvm-spirv/lib/SPIRV/SPIRVReader.cpp
@@ -59,6 +59,7 @@
 #include "llvm/ADT/StringExtras.h"
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/BinaryFormat/Dwarf.h"
+#include "llvm/IR/AttributeMask.h"
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/Dominators.h"
@@ -3179,6 +3180,8 @@ SPIRVToLLVM::SPIRVToLLVM(Module *LLVMModule, SPIRVModule *TheSPIRVModule)
   assert(M && "Initialization without an LLVM module is not allowed");
   initialize(*M);
   Context = &M->getContext();
+  if (BM->getDesiredBIsRepresentation() == BIsRepresentation::SPIRVFriendlyIR)
+    UseTargetTypes = !Context->supportsTypedPointers();
   DbgTran.reset(new SPIRVToLLVMDbgTran(TheSPIRVModule, LLVMModule, this));
 }
 
