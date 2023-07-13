d2b7a5bac89e45f7f1655010e7b3fb42bbfde1e5
Khatavkar, Yashasvi <yashasvi.khatavkar@intel.com>
2023-07-13 07:31:36 -0700

    apply clang-format

--- sycl/test-e2e/Basic/get_info_aspect.cpp
+++ sycl/test-e2e/Basic/get_info_aspect.cpp
@@ -1,5 +1,5 @@
-/* This test checks that get_info checks its aspect and throws an invalid object error
-   when ext::intel::info::device::free_memory is missing on L0*/
+/* This test checks that get_info checks its aspect and throws an invalid object
+   error when ext::intel::info::device::free_memory is missing on L0*/
 
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
@@ -8,12 +8,12 @@
 int main() {
   sycl::queue q;
   bool failed = true;
-  try{
-      sycl:: device d(sycl::default_selector_v);
-      size_t mem_free = d.get_info<sycl::ext::intel::info::device::free_memory>();
-  } catch(const sycl::invalid_object_error &e) {
-        std::cout << "Expected exception encountered: "<<e.what()<< std::endl;
-	failed = false;
-  }
-  return failed;
+  try {
+    sycl::device d(sycl::default_selector_v);
+    size_t mem_free = d.get_info<sycl::ext::intel::info::device::free_memory>();
+  } catch (const sycl::invalid_object_error &e) {
+    std::cout << "Expected exception encountered: " << e.what() << std::endl;
+    failed = false;
+   }
+   return failed;
 } 
