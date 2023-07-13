73a7ce5ba3c2ca8824322813d36185187574f07e
Khatavkar, Yashasvi <yashasvi.khatavkar@intel.com>
2023-07-13 12:45:45 -0700

    make test specific to L0

--- sycl/test-e2e/Basic/get_info_aspect.cpp
+++ sycl/test-e2e/Basic/get_info_aspect.cpp
@@ -1,6 +1,7 @@
 /* This test checks that get_info checks its aspect and throws an invalid object
    error when ext::intel::info::device::free_memory is missing on L0*/
 
+// REQUIRES: gpu, level_zero
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 
