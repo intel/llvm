6ac95136e99b80772f444fc970b7fc1e9079499d
Lukas Sommer <lukas.sommer@codeplay.com>
2023-07-13 15:44:38 +0100

    Accessor analysis
    Signed-off-by: Lukas Sommer <lukas.sommer@codeplay.com>

---
+++ polygeist/include/mlir/Dialect/Polygeist/Analysis/SYCLAccessorAnalysis.h
@@ -0,0 +1,130 @@
+//===- SYCLAccessorAnalysis.h - Analysis for sycl::accessor -----*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains an analysis to derive interesting properties about
+// sycl::accessor in SYCL host code.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_DIALECT_POLYGEIST_ANALYSIS_SYCLACCESSORANALYSIS_H
+#define MLIR_DIALECT_POLYGEIST_ANALYSIS_SYCLACCESSORANALYSIS_H
+
+#include "mlir/Analysis/DataFlowFramework.h"
+#include "mlir/Dialect/Polygeist/Analysis/ReachingDefinitionAnalysis.h"
+#include "mlir/Dialect/Polygeist/Analysis/SYCLBufferAnalysis.h"
+#include "mlir/Dialect/Polygeist/Analysis/SYCLIDAndRangeAnalysis.h"
+#include "mlir/Dialect/SYCL/Analysis/AliasAnalysis.h"
+#include "mlir/Pass/AnalysisManager.h"
+
+namespace mlir {
+namespace polygeist {
+
+/// YES indicates that the buffer is definitely a sub-buffer
+/// NO indicates that the buffer is definitely not a sub-buffer.
+/// MAYBE indicates that there are multiple constructions with different
+/// sub-buffer property or insufficient information is available.
+enum class SYCLAcessorAliasResult { YES, NO, MAYBE };
+
+/// Represents information about a `sycl::accessor` gathered from its
+/// construction.
+class AccessorInformation {
+public:
+  AccessorInformation() {}
+
+  AccessorInformation(Value buf, std::optional<BufferInformation> bufInfo,
+                      bool hasRange, llvm::ArrayRef<size_t> constRange,
+                      bool hasOffset, llvm::ArrayRef<size_t> constOffset)
+      : buffer{buf}, bufferInfo{bufInfo}, needRange{hasRange},
+        constantRange{constRange}, needOffset{hasOffset}, constantOffset{
+                                                              constOffset} {}
+
+  bool hasKnownBuffer() const { return buffer != nullptr; }
+
+  Value getBuffer() const { return buffer; }
+
+  bool hasBufferInformation() const { return bufferInfo.has_value(); }
+
+  const BufferInformation &getBufferInfo() const { return *bufferInfo; }
+
+  bool needsRange() const { return needRange; }
+
+  bool hasConstantRange() const { return !constantRange.empty(); }
+
+  const llvm::SmallVector<size_t, 3> &getConstantRange() const {
+    assert(hasConstantRange() && "Range not constant");
+    return constantRange;
+  }
+
+  bool needsOffset() const { return needOffset; }
+
+  bool hasConstantOffset() const { return !constantOffset.empty(); }
+
+  const llvm::SmallVector<size_t, 3> &getConstantOffset() const {
+    assert(hasConstantOffset() && "Offset not constant");
+    return constantOffset;
+  }
+
+  const AccessorInformation join(const AccessorInformation &other,
+                                 AliasAnalysis &aliasAnalysis) const;
+
+private:
+  Value buffer;
+
+  std::optional<BufferInformation> bufferInfo;
+
+  bool needRange;
+
+  llvm::SmallVector<size_t, 3> constantRange;
+
+  bool needOffset;
+
+  llvm::SmallVector<size_t, 3> constantOffset;
+
+  friend raw_ostream &operator<<(raw_ostream &, const AccessorInformation &);
+};
+
+/// Analysis to determine properties of interest about a `sycl::accessor` from
+/// its construction.
+class SYCLAccessorAnalysis {
+public:
+  SYCLAccessorAnalysis(Operation *op, AnalysisManager &am);
+
+  /// Consumers of the analysis must call this member function immediately after
+  /// construction and before requesting any information from the analysis.
+  SYCLAccessorAnalysis &initialize(bool useRelaxedAliasing = false);
+
+  std::optional<AccessorInformation>
+  getAccessorInformationFromConstruction(Operation *op, Value operand);
+
+private:
+  void initialize();
+
+  bool isConstructor(const Definition &def);
+
+  AccessorInformation getInformation(const Definition &def);
+
+  bool isHandler(Value value) const;
+
+  bool isAccessTag(Value value) const;
+
+  Operation *operation;
+
+  AnalysisManager &am;
+
+  DataFlowSolver solver;
+
+  bool initialized = false;
+
+  AliasAnalysis *aliasAnalysis;
+
+  SYCLIDAndRangeAnalysis idRangeAnalysis;
+};
+} // namespace polygeist
+} // namespace mlir
+
+#endif // MLIR_DIALECT_POLYGEIST_ANALYSIS_SYCLACCESSORANALYSIS_H
---
+++ polygeist/lib/Dialect/Polygeist/Analysis/SYCLAccessorAnalysis.cpp
@@ -0,0 +1,207 @@
+//===- SYCLAccessorAnalysis.h - Analysis for sycl::buffer
+//-------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Dialect/Polygeist/Analysis/SYCLAccessorAnalysis.h"
+
+#include "mlir/Analysis/DataFlow/ConstantPropagationAnalysis.h"
+#include "mlir/Analysis/DataFlow/DeadCodeAnalysis.h"
+#include "mlir/Dialect/Polygeist/Analysis/ReachingDefinitionAnalysis.h"
+#include "mlir/Dialect/SYCL/IR/SYCLDialect.h"
+#include "mlir/Dialect/SYCL/IR/SYCLOps.h"
+#include "mlir/Dialect/SYCL/IR/SYCLTypes.h"
+
+#define DEBUG_TYPE "sycl-accessor-analysis"
+
+namespace mlir {
+namespace polygeist {
+
+//===----------------------------------------------------------------------===//
+// AccessorInformation
+//===----------------------------------------------------------------------===//
+
+raw_ostream &operator<<(raw_ostream &os, const AccessorInformation &info) {
+  // TODO
+  return os;
+}
+
+const AccessorInformation
+AccessorInformation::join(const AccessorInformation &other,
+                          AliasAnalysis &aliasAnalysis) const {
+  // TODO
+}
+
+//===----------------------------------------------------------------------===//
+// SYCLAccessorAnalysis
+//===----------------------------------------------------------------------===//
+
+SYCLAccessorAnalysis::SYCLAccessorAnalysis(Operation *op, AnalysisManager &mgr)
+    : operation(op), am(mgr), idRangeAnalysis(op, mgr) {}
+
+SYCLAccessorAnalysis &
+SYCLAccessorAnalysis::initialize(bool useRelaxedAliasing) {
+
+  // Initialize the dataflow solver
+  aliasAnalysis = &am.getAnalysis<mlir::AliasAnalysis>();
+  aliasAnalysis->addAnalysisImplementation(
+      sycl::AliasAnalysis(useRelaxedAliasing));
+
+  // Populate the solver and run the analyses needed by this analysis.
+  solver.load<dataflow::DeadCodeAnalysis>();
+  solver.load<dataflow::SparseConstantPropagation>();
+  solver.load<ReachingDefinitionAnalysis>(*aliasAnalysis);
+
+  if (failed(solver.initializeAndRun(operation))) {
+    operation->emitError("Failed to run required dataflow analyses");
+    return *this;
+  }
+
+  idRangeAnalysis.initialize(useRelaxedAliasing);
+
+  initialized = true;
+
+  return *this;
+}
+
+std::optional<AccessorInformation>
+SYCLAccessorAnalysis::getAccessorInformationFromConstruction(Operation *op,
+                                                             Value operand) {
+  assert(initialized &&
+         "Analysis only available after successful initialization");
+  assert(isa<LLVM::LLVMPointerType>(operand.getType()) &&
+         "Expecting an LLVM pointer");
+
+  const polygeist::ReachingDefinition *reachingDef =
+      solver.lookupState<polygeist::ReachingDefinition>(op);
+  assert(reachingDef && "expected a reaching definition");
+
+  auto mods = reachingDef->getModifiers(operand);
+  if (!mods || mods->empty())
+    return std::nullopt;
+
+  if (!llvm::all_of(*mods,
+                    [&](const Definition &def) { return isConstructor(def); }))
+    return std::nullopt;
+
+  auto pMods = reachingDef->getPotentialModifiers(operand);
+  if (pMods) {
+    if (!llvm::all_of(
+            *pMods, [&](const Definition &def) { return isConstructor(def); }))
+      return std::nullopt;
+  }
+
+  bool first = true;
+  AccessorInformation info;
+  for (const Definition &def : *mods) {
+    if (first) {
+      info = getInformation(def);
+      first = false;
+    } else {
+      info = info.join(getInformation(def), *aliasAnalysis);
+      if (false /* TODO */)
+        // Early return: As soon as joining of the different information has led
+        // to an info with no fixed size and no definitive sub-buffer
+        // information, we can end the processing.
+        return info;
+    }
+  }
+
+  if (pMods) {
+    for (const Definition &def : *pMods) {
+      info = info.join(getInformation(def), *aliasAnalysis);
+      if (false /* TODO */)
+        // Early return: As soon as joining of the different information has led
+        // to an info with no fixed size and no definitive sub-buffer
+        // information, we can end the processing.
+        return info;
+    }
+  }
+
+  return info;
+}
+
+bool SYCLAccessorAnalysis::isConstructor(const Definition &def) {
+  if (!def.isOperation())
+    return false;
+
+  auto constructor = dyn_cast<sycl::SYCLHostConstructorOp>(def.getOperation());
+  if (!constructor)
+    return false;
+
+  return isa<sycl::AccessorType>(constructor.getType().getValue());
+}
+
+AccessorInformation
+SYCLAccessorAnalysis::getInformation(const Definition &def) {
+  assert(def.isOperation() && "Expecting operation");
+
+  auto constructor = cast<sycl::SYCLHostConstructorOp>(def.getOperation());
+
+  // The default is true because we conservatively need to assume that the
+  // accessor uses those two fields, unless we can infer otherwise.
+  bool needsRange = true;
+  bool needsOffset = true;
+  SmallVector<size_t, 3> constRange;
+  SmallVector<size_t, 3> constOffset;
+  Value buffer = (constructor->getNumOperands() > 2)
+                     ? constructor->getOperand(1)
+                     : nullptr;
+
+  // Deduct two for the this pointer and the DPC++-specific code location
+  // argument.
+  switch (constructor->getNumOperands() - 2) {
+  case 0:
+  case 2:
+    needsRange = false;
+    needsOffset = false;
+    break;
+  case 3: {
+    needsOffset = false;
+    // The second parameter can either be a handler, a tag or a range.
+    Value secondArg = constructor->getOperand(2);
+    needsRange = !(isHandler(secondArg) || isAccessTag(secondArg));
+    if (needsRange) {
+      auto rangeInfo =
+          idRangeAnalysis
+              .getIDRangeInformationFromConstruction<sycl::RangeType>(
+                  constructor, secondArg);
+      if (rangeInfo && rangeInfo->isConstant())
+        constRange = rangeInfo->getConstantValues();
+    }
+    break;
+  }
+  case 4: {
+    // The second parameter can either be a handler or a range.
+    Value secondArg = constructor->getOperand(2);
+    // The third parameter can either be a tag, a range or an id.
+    Value thirdArg = constructor.getOperand(3);
+    if (isHandler(secondArg)) {
+      needsOffset = false;
+      if (isAccessTag(thirdArg)) {
+        needsRange = false;
+        break;
+      }
+      auto rangeInfo =
+          idRangeAnalysis
+              .getIDRangeInformationFromConstruction<sycl::RangeType>(
+                  constructor, thirdArg);
+      if (rangeInfo && rangeInfo->isConstant())
+        constRange = rangeInfo->getConstantValues();
+      break;
+    }
+  }
+  }
+  // TODO
+}
+
+bool SYCLAccessorAnalysis::isHandler(Value value) const { return false; }
+
+bool SYCLAccessorAnalysis::isAccessTag(Value value) const { return false; }
+
+} // namespace polygeist
+} // namespace mlir
