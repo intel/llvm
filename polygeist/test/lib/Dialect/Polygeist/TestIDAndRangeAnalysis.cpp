3864dcd3e9ca72d1337d5fd8a298ef3efff15c19
Lukas Sommer <lukas.sommer@codeplay.com>
2023-07-13 17:31:24 +0200

    [SYCL-MLIR] SYCL 'id' and 'range' analysis (#10298)
    MLIR analysis to infer properties of interest about instances of
    `sycl::id` or `sycl::range` from their construction in host code.
    
    ---------
    
    Signed-off-by: Lukas Sommer <lukas.sommer@codeplay.com>

---
+++ polygeist/test/lib/Dialect/Polygeist/TestIDAndRangeAnalysis.cpp
@@ -0,0 +1,67 @@
+//===----------- TestIDAndRangeAnalysis.cpp -------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Dialect/Polygeist/Analysis/SYCLIDAndRangeAnalysis.h"
+#include "mlir/Dialect/SYCL/IR/SYCLTypes.h"
+#include "mlir/Pass/Pass.h"
+
+using namespace mlir;
+
+namespace {
+
+struct TestIDAndRangeAnalysisPass
+    : public PassWrapper<TestIDAndRangeAnalysisPass, OperationPass<>> {
+  MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(TestIDAndRangeAnalysisPass)
+
+  StringRef getArgument() const override { return "test-id-range-analysis"; }
+
+  void runOnOperation() override {
+    Operation *op = getOperation();
+    bool relaxedAliasing = true;
+    auto &IDRangeAnalysis =
+        getAnalysis<polygeist::SYCLIDAndRangeAnalysis>().initialize(
+            relaxedAliasing);
+
+    op->walk([&](Operation *op) {
+      auto tag = op->getAttrOfType<StringAttr>("tag");
+      if (!tag)
+        return WalkResult::skip();
+
+      llvm::errs() << "test_tag: " << tag.getValue() << ":\n";
+      for (auto [index, operand] : llvm::enumerate(op->getOperands())) {
+        llvm::errs().indent(2) << "operand #" << index << "\n";
+        auto idResult =
+            IDRangeAnalysis.getIDRangeInformationFromConstruction<sycl::IDType>(
+                op, operand);
+        if (idResult) {
+          llvm::errs().indent(2) << "id:\n";
+          llvm::errs() << *idResult;
+        }
+        auto rangeResult =
+            IDRangeAnalysis
+                .getIDRangeInformationFromConstruction<sycl::RangeType>(
+                    op, operand);
+        if (rangeResult) {
+          llvm::errs().indent(2) << "range:\n";
+          llvm::errs() << *rangeResult;
+        }
+      }
+      return WalkResult::advance();
+    });
+  };
+};
+
+} // namespace
+
+namespace mlir {
+namespace test {
+void registerTestIDAndRangeAnalysisPass() {
+  PassRegistration<TestIDAndRangeAnalysisPass>();
+}
+} // end namespace test
+} // end namespace mlir
