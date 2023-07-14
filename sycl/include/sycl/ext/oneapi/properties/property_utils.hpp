82f8239ab7071a03e171ccedebae2beabbaabd85
Alexey Bader <alexey.bader@intel.com>
2023-07-13 22:19:18 -0700

    [SYCL][NFC] Don't include the whole mp11 library
    Include only what you use i.e. algorithm in our case.

--- sycl/include/sycl/ext/oneapi/properties/property_utils.hpp
+++ sycl/include/sycl/ext/oneapi/properties/property_utils.hpp
@@ -10,7 +10,7 @@
 
 #include <sycl/detail/property_helper.hpp>
 #include <sycl/ext/oneapi/properties/property.hpp>
-#include <sycl/detail/boost/mp11.hpp>
+#include <sycl/detail/boost/mp11/algorithm.hpp>
 
 #include <tuple>
 
