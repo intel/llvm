5476fe52fdb13ad9a5903240ebe4dcfdd6b64f70
vic <victor.perez@codeplay.com>
2023-07-14 11:57:49 +0200

    [SYCL-MLIR] Raise `nd_range` constructor (#10345)
    Raise `nd_range` constructor to `sycl.host.constructor`. This may
    involve allocating each argument of the constructor if the `range` and
    `id` instances are optimized-away.
    
    ---------
    
    Signed-off-by: Victor Perez <victor.perez@codeplay.com>

---
+++ polygeist/tools/cgeist/Test/Verification/sycl/raising/host_raising_constructors.cpp
@@ -0,0 +1,175 @@
+// RUN: clang++ -O1 %s -S -emit-mlir -o - -fsycl -fsycl-raise-host -Xclang -opaque-pointers -w | FileCheck %s
+// RUN: clang++ -O2 %s -S -emit-mlir -o - -fsycl -fsycl-raise-host -Xclang -opaque-pointers -w | FileCheck %s
+// RUN: clang++ -O3 %s -S -emit-mlir -o - -fsycl -fsycl-raise-host -Xclang -opaque-pointers -w | FileCheck %s
+
+#include <sycl/sycl.hpp>
+
+template <typename... Args>
+void keep(Args&&...);
+
+template <int Dimensions>
+void nd_range(sycl::range<Dimensions> globalSize,
+              sycl::range<Dimensions> localSize) {
+  sycl::nd_range<Dimensions> ndr(globalSize, localSize);
+  keep(ndr);
+}
+
+template <int Dimensions>
+void nd_range_offset(sycl::range<Dimensions> globalSize,
+                     sycl::range<Dimensions> localSize,
+                     sycl::id<Dimensions> offset) {
+  sycl::nd_range<Dimensions> ndr(globalSize, localSize, offset);
+  keep(ndr);
+}
+
+template <int Dimensions>
+void nd_range_copy(const sycl::nd_range<Dimensions> &other) {
+  sycl::nd_range<Dimensions> ndr(other);
+  keep(ndr);
+}
+
+template <int Dimensions>
+void nd_range_move(sycl::nd_range<Dimensions> &&other) {
+  sycl::nd_range<Dimensions> ndr(std::move(other));
+  keep(ndr);
+}
+
+// CHECK-LABEL:   llvm.func weak_odr @_Z8nd_rangeILi1EEvN4sycl3_V15rangeIXT_EEES3_(
+// CHECK-SAME:                                                                     %[[VAL_0:.*]]: i64,
+// CHECK-SAME:                                                                     %[[VAL_1:.*]]: i64)
+// CHECK-DAG:       %[[VAL_2:.*]] = llvm.mlir.constant(1 : i32) : i32
+// CHECK-DAG:       %[[VAL_3:.*]] = llvm.mlir.constant(0 : i64) : i64
+// CHECK-NEXT:      %[[VAL_5:.*]] = llvm.alloca %[[VAL_2]] x !llvm.struct<[[RANGE1:.*]]> : (i32) -> !llvm.ptr
+// CHECK-NEXT:      sycl.host.constructor(%[[VAL_5]], %[[VAL_0]]) {type = !sycl_range_1_} : (!llvm.ptr, i64) -> ()
+// CHECK-NEXT:      %[[VAL_6:.*]] = llvm.alloca %[[VAL_2]] x !llvm.struct<[[RANGE1]]> : (i32) -> !llvm.ptr
+// CHECK-NEXT:      sycl.host.constructor(%[[VAL_6]], %[[VAL_1]]) {type = !sycl_range_1_} : (!llvm.ptr, i64) -> ()
+// CHECK-NEXT:      %[[VAL_7:.*]] = llvm.alloca %[[VAL_2]] x !llvm.struct<[[ID1:.*]]> : (i32) -> !llvm.ptr
+// CHECK-NEXT:      sycl.host.constructor(%[[VAL_7]], %[[VAL_3]]) {type = !sycl_id_1_} : (!llvm.ptr, i64) -> ()
+// CHECK-NEXT:      %[[VAL_8:.*]] = llvm.alloca %[[VAL_2]] x !llvm.struct<[[ND1:.*]]> {alignment = 8 : i64} : (i32) -> !llvm.ptr
+// CHECK-NEXT:      llvm.intr.lifetime.start 24, %[[VAL_8]] : !llvm.ptr
+// CHECK-NEXT:      sycl.host.constructor(%[[VAL_8]], %[[VAL_5]], %[[VAL_6]], %[[VAL_7]]) {type = !sycl_nd_range_1_} : (!llvm.ptr, !llvm.ptr, !llvm.ptr, !llvm.ptr) -> ()
+// CHECK-NEXT:      llvm.call @_Z4keepIJRN4sycl3_V18nd_rangeILi1EEEEEvDpOT_(%[[VAL_8]]) : (!llvm.ptr) -> ()
+// CHECK-NEXT:      llvm.intr.lifetime.end 24, %[[VAL_8]] : !llvm.ptr
+// CHECK-NEXT:      llvm.return
+// CHECK-NEXT:    }
+template void nd_range(sycl::range<1>, sycl::range<1>);
+
+// CHECK-LABEL:   llvm.func weak_odr @_Z8nd_rangeILi3EEvN4sycl3_V15rangeIXT_EEES3_(
+// CHECK-SAME:                                                                     %[[VAL_0:.*]]: !llvm.ptr {{{.*}}},
+// CHECK-SAME:                                                                     %[[VAL_1:.*]]: !llvm.ptr {{{.*}}})
+// CHECK-DAG:       %[[VAL_2:.*]] = llvm.mlir.constant(1 : i32) : i32
+// CHECK-DAG:       %[[VAL_3:.*]] = llvm.mlir.constant(24 : i64) : i64
+// CHECK-DAG:       %[[VAL_4:.*]] = llvm.mlir.constant(false) : i1
+// CHECK-DAG:       %[[VAL_5:.*]] = llvm.mlir.constant(0 : i8) : i8
+// CHECK-NEXT:      %[[VAL_6:.*]] = llvm.alloca %[[VAL_2]] x !llvm.struct<[[ND3:.*]]> {alignment = 8 : i64} : (i32) -> !llvm.ptr
+// CHECK-NEXT:      llvm.intr.lifetime.start 72, %[[VAL_6]] : !llvm.ptr
+// CHECK-NEXT:      sycl.host.constructor(%[[VAL_6]], %[[VAL_0]], %[[VAL_1]]) {type = !sycl_nd_range_3_} : (!llvm.ptr, !llvm.ptr, !llvm.ptr) -> ()
+// CHECK-NEXT:      %[[VAL_7:.*]] = llvm.getelementptr inbounds %[[VAL_6]][0, 2] : (!llvm.ptr) -> !llvm.ptr, !llvm.struct<[[ND3]]>
+// CHECK-NEXT:      "llvm.intr.memset"(%[[VAL_7]], %[[VAL_5]], %[[VAL_3]], %[[VAL_4]]) : (!llvm.ptr, i8, i64, i1) -> ()
+// CHECK-NEXT:      llvm.call @_Z4keepIJRN4sycl3_V18nd_rangeILi3EEEEEvDpOT_(%[[VAL_6]]) : (!llvm.ptr) -> ()
+// CHECK-NEXT:      llvm.intr.lifetime.end 72, %[[VAL_6]] : !llvm.ptr
+// CHECK-NEXT:      llvm.return
+// CHECK-NEXT:    }
+template void nd_range(sycl::range<3>, sycl::range<3>);
+
+// CHECK-LABEL:   llvm.func weak_odr @_Z15nd_range_offsetILi1EEvN4sycl3_V15rangeIXT_EEES3_NS1_2idIXT_EEE(
+// CHECK-SAME:                                                                                           %[[VAL_0:.*]]: i64, %[[VAL_1:.*]]: i64, %[[VAL_2:.*]]: i64)
+// CHECK-DAG:       %[[VAL_3:.*]] = llvm.mlir.constant(1 : i32) : i32
+// CHECK-NEXT:      %[[VAL_5:.*]] = llvm.alloca %[[VAL_3]] x !llvm.struct<[[RANGE1]]> : (i32) -> !llvm.ptr
+// CHECK-NEXT:      sycl.host.constructor(%[[VAL_5]], %[[VAL_0]]) {type = !sycl_range_1_} : (!llvm.ptr, i64) -> ()
+// CHECK-NEXT:      %[[VAL_6:.*]] = llvm.alloca %[[VAL_3]] x !llvm.struct<[[RANGE1]]> : (i32) -> !llvm.ptr
+// CHECK-NEXT:      sycl.host.constructor(%[[VAL_6]], %[[VAL_1]]) {type = !sycl_range_1_} : (!llvm.ptr, i64) -> ()
+// CHECK-NEXT:      %[[VAL_7:.*]] = llvm.alloca %[[VAL_3]] x !llvm.struct<[[ID1]]> : (i32) -> !llvm.ptr
+// CHECK-NEXT:      sycl.host.constructor(%[[VAL_7]], %[[VAL_2]]) {type = !sycl_id_1_} : (!llvm.ptr, i64) -> ()
+// CHECK-NEXT:      %[[VAL_8:.*]] = llvm.alloca %[[VAL_3]] x !llvm.struct<[[ND1]]> {alignment = 8 : i64} : (i32) -> !llvm.ptr
+// CHECK-NEXT:      llvm.intr.lifetime.start 24, %[[VAL_8]] : !llvm.ptr
+// CHECK-NEXT:      sycl.host.constructor(%[[VAL_8]], %[[VAL_5]], %[[VAL_6]], %[[VAL_7]]) {type = !sycl_nd_range_1_} : (!llvm.ptr, !llvm.ptr, !llvm.ptr, !llvm.ptr) -> ()
+// CHECK-NEXT:      llvm.call @_Z4keepIJRN4sycl3_V18nd_rangeILi1EEEEEvDpOT_(%[[VAL_8]]) : (!llvm.ptr) -> ()
+// CHECK-NEXT:      llvm.intr.lifetime.end 24, %[[VAL_8]] : !llvm.ptr
+// CHECK-NEXT:      llvm.return
+// CHECK-NEXT:    }
+template void nd_range_offset(sycl::range<1>, sycl::range<1>, sycl::id<1>);
+
+// CHECK-LABEL:   llvm.func weak_odr @_Z15nd_range_offsetILi3EEvN4sycl3_V15rangeIXT_EEES3_NS1_2idIXT_EEE(
+// CHECK-SAME:                                                                                           %[[VAL_0:.*]]: !llvm.ptr {{{.*}}}, %[[VAL_1:.*]]: !llvm.ptr {{{.*}}}, %[[VAL_2:.*]]: !llvm.ptr {{{.*}}})
+// CHECK-NEXT:      %[[VAL_3:.*]] = llvm.mlir.constant(1 : i32) : i32
+// CHECK-NEXT:      %[[VAL_4:.*]] = llvm.alloca %[[VAL_3]] x !llvm.struct<[[ND3]]> {alignment = 8 : i64} : (i32) -> !llvm.ptr
+// CHECK-NEXT:      llvm.intr.lifetime.start 72, %[[VAL_4]] : !llvm.ptr
+// CHECK-NEXT:      sycl.host.constructor(%[[VAL_4]], %[[VAL_0]], %[[VAL_1]], %[[VAL_2]]) {type = !sycl_nd_range_3_} : (!llvm.ptr, !llvm.ptr, !llvm.ptr, !llvm.ptr) -> ()
+// CHECK-NEXT:      llvm.call @_Z4keepIJRN4sycl3_V18nd_rangeILi3EEEEEvDpOT_(%[[VAL_4]]) : (!llvm.ptr) -> ()
+// CHECK-NEXT:      llvm.intr.lifetime.end 72, %[[VAL_4]] : !llvm.ptr
+// CHECK-NEXT:      llvm.return
+// CHECK-NEXT:    }
+template void nd_range_offset(sycl::range<3>, sycl::range<3>, sycl::id<3>);
+
+// CHECK-LABEL:   llvm.func weak_odr @_Z13nd_range_copyILi1EEvRKN4sycl3_V18nd_rangeIXT_EEE(
+// CHECK-SAME:                                                                             %[[VAL_0:.*]]: !llvm.ptr {{{.*}}})
+// CHECK-NEXT:      %[[VAL_1:.*]] = llvm.mlir.constant(1 : i32) : i32
+// CHECK-NEXT:      %[[VAL_2:.*]] = llvm.alloca %[[VAL_1]] x !llvm.struct<[[ND1]]> {alignment = 8 : i64} : (i32) -> !llvm.ptr
+// CHECK-NEXT:      llvm.intr.lifetime.start 24, %[[VAL_2]] : !llvm.ptr
+// CHECK-NEXT:      sycl.host.constructor(%[[VAL_2]], %[[VAL_0]]) {type = !sycl_nd_range_1_} : (!llvm.ptr, !llvm.ptr) -> ()
+// CHECK-NEXT:      llvm.call @_Z4keepIJRN4sycl3_V18nd_rangeILi1EEEEEvDpOT_(%[[VAL_2]]) : (!llvm.ptr) -> ()
+// CHECK-NEXT:      llvm.intr.lifetime.end 24, %[[VAL_2]] : !llvm.ptr
+// CHECK-NEXT:      llvm.return
+// CHECK-NEXT:    }
+template void nd_range_copy(const sycl::nd_range<1> &);
+
+// CHECK-LABEL:   llvm.func weak_odr @_Z13nd_range_copyILi2EEvRKN4sycl3_V18nd_rangeIXT_EEE(
+// CHECK-SAME:                                                                             %[[VAL_0:.*]]: !llvm.ptr {{{.*}}})
+// CHECK-NEXT:      %[[VAL_1:.*]] = llvm.mlir.constant(1 : i32) : i32
+// CHECK-NEXT:      %[[VAL_2:.*]] = llvm.alloca %[[VAL_1]] x !llvm.struct<[[ND2:.*]]> {alignment = 8 : i64} : (i32) -> !llvm.ptr
+// CHECK-NEXT:      llvm.intr.lifetime.start 48, %[[VAL_2]] : !llvm.ptr
+// CHECK-NEXT:      sycl.host.constructor(%[[VAL_2]], %[[VAL_0]]) {type = !sycl_nd_range_2_} : (!llvm.ptr, !llvm.ptr) -> ()
+// CHECK-NEXT:      llvm.call @_Z4keepIJRN4sycl3_V18nd_rangeILi2EEEEEvDpOT_(%[[VAL_2]]) : (!llvm.ptr) -> ()
+// CHECK-NEXT:      llvm.intr.lifetime.end 48, %[[VAL_2]] : !llvm.ptr
+// CHECK-NEXT:      llvm.return
+// CHECK-NEXT:    }
+template void nd_range_copy(const sycl::nd_range<2> &);
+
+// CHECK-LABEL:   llvm.func weak_odr @_Z13nd_range_copyILi3EEvRKN4sycl3_V18nd_rangeIXT_EEE(
+// CHECK-SAME:                                                                             %[[VAL_0:.*]]: !llvm.ptr {{{.*}}})
+// CHECK-NEXT:      %[[VAL_1:.*]] = llvm.mlir.constant(1 : i32) : i32
+// CHECK-NEXT:      %[[VAL_2:.*]] = llvm.alloca %[[VAL_1]] x !llvm.struct<[[ND3]]> {alignment = 8 : i64} : (i32) -> !llvm.ptr
+// CHECK-NEXT:      llvm.intr.lifetime.start 72, %[[VAL_2]] : !llvm.ptr
+// CHECK-NEXT:      sycl.host.constructor(%[[VAL_2]], %[[VAL_0]]) {type = !sycl_nd_range_3_} : (!llvm.ptr, !llvm.ptr) -> ()
+// CHECK-NEXT:      llvm.call @_Z4keepIJRN4sycl3_V18nd_rangeILi3EEEEEvDpOT_(%[[VAL_2]]) : (!llvm.ptr) -> ()
+// CHECK-NEXT:      llvm.intr.lifetime.end 72, %[[VAL_2]] : !llvm.ptr
+// CHECK-NEXT:      llvm.return
+// CHECK-NEXT:    }
+template void nd_range_copy(const sycl::nd_range<3> &);
+
+// CHECK-LABEL:   llvm.func weak_odr @_Z13nd_range_moveILi1EEvON4sycl3_V18nd_rangeIXT_EEE(
+// CHECK-SAME:                                                                            %[[VAL_0:.*]]: !llvm.ptr {{{.*}}})
+// CHECK-NEXT:      %[[VAL_1:.*]] = llvm.mlir.constant(1 : i32) : i32
+// CHECK-NEXT:      %[[VAL_2:.*]] = llvm.alloca %[[VAL_1]] x !llvm.struct<[[ND1]]> {alignment = 8 : i64} : (i32) -> !llvm.ptr
+// CHECK-NEXT:      llvm.intr.lifetime.start 24, %[[VAL_2]] : !llvm.ptr
+// CHECK-NEXT:      sycl.host.constructor(%[[VAL_2]], %[[VAL_0]]) {type = !sycl_nd_range_1_} : (!llvm.ptr, !llvm.ptr) -> ()
+// CHECK-NEXT:      llvm.call @_Z4keepIJRN4sycl3_V18nd_rangeILi1EEEEEvDpOT_(%[[VAL_2]]) : (!llvm.ptr) -> ()
+// CHECK-NEXT:      llvm.intr.lifetime.end 24, %[[VAL_2]] : !llvm.ptr
+// CHECK-NEXT:      llvm.return
+// CHECK-NEXT:    }
+template void nd_range_move(sycl::nd_range<1> &&);
+
+// CHECK-LABEL:   llvm.func weak_odr @_Z13nd_range_moveILi2EEvON4sycl3_V18nd_rangeIXT_EEE(
+// CHECK-SAME:                                                                            %[[VAL_0:.*]]: !llvm.ptr {{{.*}}})
+// CHECK-NEXT:      %[[VAL_1:.*]] = llvm.mlir.constant(1 : i32) : i32
+// CHECK-NEXT:      %[[VAL_2:.*]] = llvm.alloca %[[VAL_1]] x !llvm.struct<[[ND2]]> {alignment = 8 : i64} : (i32) -> !llvm.ptr
+// CHECK-NEXT:      llvm.intr.lifetime.start 48, %[[VAL_2]] : !llvm.ptr
+// CHECK-NEXT:      sycl.host.constructor(%[[VAL_2]], %[[VAL_0]]) {type = !sycl_nd_range_2_} : (!llvm.ptr, !llvm.ptr) -> ()
+// CHECK-NEXT:      llvm.call @_Z4keepIJRN4sycl3_V18nd_rangeILi2EEEEEvDpOT_(%[[VAL_2]]) : (!llvm.ptr) -> ()
+// CHECK-NEXT:      llvm.intr.lifetime.end 48, %[[VAL_2]] : !llvm.ptr
+// CHECK-NEXT:      llvm.return
+// CHECK-NEXT:    }
+template void nd_range_move(sycl::nd_range<2> &&);
+
+// CHECK-LABEL:   llvm.func weak_odr @_Z13nd_range_moveILi3EEvON4sycl3_V18nd_rangeIXT_EEE(
+// CHECK-SAME:                                                                            %[[VAL_0:.*]]: !llvm.ptr {{{.*}}})
+// CHECK-NEXT:      %[[VAL_1:.*]] = llvm.mlir.constant(1 : i32) : i32
+// CHECK-NEXT:      %[[VAL_2:.*]] = llvm.alloca %[[VAL_1]] x !llvm.struct<[[ND3]]> {alignment = 8 : i64} : (i32) -> !llvm.ptr
+// CHECK-NEXT:      llvm.intr.lifetime.start 72, %[[VAL_2]] : !llvm.ptr
+// CHECK-NEXT:      sycl.host.constructor(%[[VAL_2]], %[[VAL_0]]) {type = !sycl_nd_range_3_} : (!llvm.ptr, !llvm.ptr) -> ()
+// CHECK-NEXT:      llvm.call @_Z4keepIJRN4sycl3_V18nd_rangeILi3EEEEEvDpOT_(%[[VAL_2]]) : (!llvm.ptr) -> ()
+// CHECK-NEXT:      llvm.intr.lifetime.end 72, %[[VAL_2]] : !llvm.ptr
+// CHECK-NEXT:      llvm.return
+// CHECK-NEXT:    }
+template void nd_range_move(sycl::nd_range<3> &&);
