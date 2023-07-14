3696b20e3ff2d1764fc90314c44725b257cf0fde
Piotr Balcer <piotr.balcer@intel.com>
2023-07-14 16:32:44 +0200

    [SYCL][CUDA] Remove SYCL dependencies from CUDA UR adapter (#10135)
    This was preventing out-of-tree build of the adapter for standalone use
    with unified runtime.
    
    Signed-off-by: Piotr Balcer <piotr.balcer@intel.com>

--- sampler.cpp
+++ sampler.cpp
@@ -69,7 +69,7 @@ UR_APIEXPORT ur_result_t UR_APICALL
 urSamplerRelease(ur_sampler_handle_t hSampler) {
   // double delete or someone is messing with the ref count.
   // either way, cannot safely proceed.
-  sycl::detail::ur::assertion(
+  detail::ur::assertion(
       hSampler->getReferenceCount() != 0,
       "Reference count overflow detected in urSamplerRelease.");
 
