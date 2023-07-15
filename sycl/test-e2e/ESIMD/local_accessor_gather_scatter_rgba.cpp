5e0aa8d617224ee4dc664533ceebb578cee081fa
gregory <gregory@gaiaplatform.io>
2023-07-14 20:12:19 -0700

    Disable tests on Gen12

--- sycl/test-e2e/ESIMD/local_accessor_gather_scatter_rgba.cpp
+++ sycl/test-e2e/ESIMD/local_accessor_gather_scatter_rgba.cpp
@@ -8,7 +8,7 @@
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 // TODO: Enable the test when GPU driver is ready/fixed.
-// XFAIL: opencl || windows || gpu-intel-pvc
+// XFAIL: opencl || windows || gpu-intel-pvc || gpu-intel-gen12
 // TODO: add support for local_accessors to esimd_emulator.
 // UNSUPPORTED: esimd_emulator
 // The test checks functionality of the gather_rgba/scatter_rgba local
