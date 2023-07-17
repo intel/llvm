b78844b374eebdee72971d669582ba126689e326
Ewan Crawford <ewan@codeplay.com>
2023-07-17 16:01:20 +0100

    [SYCL] Disable failing Windows tests
    CI has discovered some failing Graphs E2E tests on Windows.
    Either mark them XFAIL or skip them until we can investigate
    and fix.

--- sycl/test-e2e/Graph/RecordReplay/valid_no_end.cpp
+++ sycl/test-e2e/Graph/RecordReplay/valid_no_end.cpp
@@ -1,4 +1,8 @@
 // REQUIRES: level_zero, gpu
+//
+// A non-zero exit code is returned on Windows
+// XFAIL: windows
+//
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 // RUN: %if ext_oneapi_level_zero %{env ZE_DEBUG=4 %{run} %t.out 2>&1 | FileCheck %s %}
