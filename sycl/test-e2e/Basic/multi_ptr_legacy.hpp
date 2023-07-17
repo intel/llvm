b4f02f80e78d286321d48760a692ad7d8a8045fd
m moadeli <mahmoud.moadeli@codeplay.com>
2023-07-17 11:56:36 +0100

    Test cover for generic_space legacy multi_ptr constructed using local_accessor.

--- sycl/test-e2e/Basic/multi_ptr_legacy.hpp
+++ sycl/test-e2e/Basic/multi_ptr_legacy.hpp
@@ -88,6 +88,10 @@ template <typename T> void testMultPtr() {
                 multi_ptr<const void, access::address_space::local_space,
                           access::decorated::legacy>(localAccessor);
 
+            auto local_ptr5 =
+                multi_ptr<T, access::address_space::generic_space,
+                          access::decorated::legacy>(localAccessor);
+
             // Construct extension pointer from accessors.
             auto dev_ptr =
                 multi_ptr<const T,
