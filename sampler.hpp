a9ff0edaffb2bbbca1925c3a85e29e19d37e95b0
Isaac Ault <isaac.ault@codeplay.com>
2023-07-18 16:53:30 +0100

    [SYCL][Bindless][2/4] Add experimental implementation of SYCL bindless images extension (#10112)
    # Experimental Implementation of SYCL Bindless Images Extension
    
    This commit stands as the second commit of four to make code review
    easier, implementing revision 4 of the [bindless images extension
    proposal](https://github.com/intel/llvm/pull/9842).
    
    ## Scope
    
    This PR covers changes made to the PI and the UR. This includes
    
    - Extending PI with extension functions
    - Updating UR FetchContent commit and implementing [UR bindless images
    experimental
    features](https://oneapi-src.github.io/unified-runtime/core/EXP-BINDLESS-IMAGES.html)
    on the CUDA adaptor
    
    ## Following Split PRs
    
    - [3/4] Implement the user-facing SYCL extension
    - [4/4] Add tests
    
    ## Authors
    
    Co-authored-by: Isaac Ault <isaac.ault@codeplay.com>
    Co-authored-by: Hugh Bird <hugh.bird@codeplay.com>
    Co-authored-by: Duncan Brawley <duncan.brawley@codeplay.com>
    Co-authored-by: Przemek Malon <przemek.malon@codeplay.com>
    Co-authored-by: Chedy Najjar <chedy.najjar@codeplay.com>
    Co-authored-by: Sean Stirling <sean.stirling@codeplay.com>
    Co-authored-by: Peter Zuzek <peter@codeplay.com>

--- sampler.hpp
+++ sampler.hpp
@@ -11,19 +11,44 @@
 /// Implementation of samplers for CUDA
 ///
 /// Sampler property layout:
-/// | 31 30 ... 6 5 |      4 3 2      |     1      |         0        |
-/// |      N/A      | addressing mode | fiter mode | normalize coords |
+/// |     <bits>     | <usage>
+/// -----------------------------------
+/// |  31 30 ... 6   | N/A
+/// |       5        | mip filter mode
+/// |     4 3 2      | addressing mode
+/// |       1        | filter mode
+/// |       0        | normalize coords
 struct ur_sampler_handle_t_ {
   std::atomic_uint32_t RefCount;
   uint32_t Props;
+  float MinMipmapLevelClamp;
+  float MaxMipmapLevelClamp;
+  float MaxAnisotropy;
   ur_context_handle_t Context;
 
   ur_sampler_handle_t_(ur_context_handle_t Context)
-      : RefCount(1), Props(0), Context(Context) {}
+      : RefCount(1), Props(0), MinMipmapLevelClamp(0.0f),
+        MaxMipmapLevelClamp(0.0f), MaxAnisotropy(0.0f), Context(Context) {}
 
   uint32_t incrementReferenceCount() noexcept { return ++RefCount; }
 
   uint32_t decrementReferenceCount() noexcept { return --RefCount; }
 
   uint32_t getReferenceCount() const noexcept { return RefCount; }
+
+  ur_bool_t isNormalizedCoords() const noexcept {
+    return static_cast<ur_bool_t>(Props & 0b1);
+  }
+
+  ur_sampler_filter_mode_t getFilterMode() const noexcept {
+    return static_cast<ur_sampler_filter_mode_t>((Props >> 1) & 0b1);
+  }
+
+  ur_sampler_addressing_mode_t getAddressingMode() const noexcept {
+    return static_cast<ur_sampler_addressing_mode_t>((Props >> 2) & 0b111);
+  }
+
+  ur_sampler_filter_mode_t getMipFilterMode() const noexcept {
+    return static_cast<ur_sampler_filter_mode_t>((Props >> 5) & 0b1);
+  }
 };
