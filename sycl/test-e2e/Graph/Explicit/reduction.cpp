760c672bb29c188cf2b6a4a19fa325e0d423877c
Ben Tracy <ben.tracy@codeplay.com>
2023-07-19 16:01:17 +0100

    [SYCL][Graph] Update test comments

--- sycl/test-e2e/Graph/Explicit/reduction.cpp
+++ sycl/test-e2e/Graph/Explicit/reduction.cpp
@@ -5,6 +5,7 @@
 //
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
+// Extra run to check for leaks in Level Zero using ZE_DEBUG
 // RUN: %if ext_oneapi_level_zero %{env ZE_DEBUG=4 %{run} %t.out 2>&1 | FileCheck %s %}
 //
 // CHECK-NOT: LEAK
--- sycl/test-e2e/Graph/Threading/update.cpp
+++ sycl/test-e2e/Graph/Threading/update.cpp
@@ -3,6 +3,7 @@
 
 // RUN: %clangxx -pthread -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
 // RUN: %{run} %t.out
+// Extra run to check for leaks in Level Zero using ZE_DEBUG
 // RUN: %if ext_oneapi_level_zero %{env ZE_DEBUG=4 %{run} %t.out 2>&1 | FileCheck %s %}
 //
 // CHECK-NOT: LEAK
