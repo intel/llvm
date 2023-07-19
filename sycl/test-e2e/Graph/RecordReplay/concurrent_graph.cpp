c7fd2193433d0555c8f400cd6afec8293b7d96ce
Ewan Crawford <ewan@codeplay.com>
2023-07-19 07:39:19 +0100

    [SYCL] Disable failing Windows tests (#270)
    Cherry-pick commit https://github.com/intel/llvm/pull/10216/commits/b78844b374eebdee72971d669582ba126689e326
    from `sycl-graph-patch-4` back to `sycl-graph-develop` until
    these Windows fails are investigated and resolved
    
    * https://github.com/reble/llvm/issues/267
    * https://github.com/reble/llvm/issues/268
    * https://github.com/reble/llvm/issues/269

--- sycl/test-e2e/Graph/RecordReplay/concurrent_graph.cpp
+++ sycl/test-e2e/Graph/RecordReplay/concurrent_graph.cpp
@@ -1,4 +1,6 @@
-// REQUIRES: level_zero, gpu
+// Test unexpectedly passes on Windows, require Linux until fixed
+// REQUIRES: level_zero, gpu, linux
+//
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 // RUN: %if ext_oneapi_level_zero %{env ZE_DEBUG=4 %{run} %t.out 2>&1 | FileCheck %s %}
