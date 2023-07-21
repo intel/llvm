48417af8032b4dd731651d07d9783e6ce8cc04d1
srividya sundaram <srividya.sundaram@intel.com>
2023-07-21 15:32:30 -0700

    Update test.

--- clang/test/Driver/sycl-specific-args-diagnostics.cpp
+++ clang/test/Driver/sycl-specific-args-diagnostics.cpp
@@ -18,17 +18,15 @@
 
 // Warning should be emitted when using -fsycl-dead-args-optimization without -fsycl
 // RUN:   %clang -### -fsycl-dead-args-optimization  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-DEAD-ARGS -DOPT=-fsycl-dead-args-optimization %s
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-dead-args-optimization %s
 // RUN:   %clang_cl -### -fsycl-dead-args-optimization  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-DEAD-ARGS -DOPT=-fsycl-dead-args-optimization %s
-// WARNING-DEAD-ARGS: warning: argument unused during compilation: '[[OPT]]' [-Wunused-command-line-argument]
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-dead-args-optimization %s
 
 // Warning should be emitted when using -fsycl-device-lib-jit-link without -fsycl
 // RUN:   %clang -### -fsycl-device-lib-jit-link  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-JIT-LINK -DOPT=-fsycl-device-lib-jit-link %s
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-device-lib-jit-link %s
 // RUN:   %clang_cl -### -fsycl-device-lib-jit-link  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-JIT-LINK -DOPT=-fsycl-device-lib-jit-link %s
-// WARNING-JIT-LINK: warning: argument unused during compilation: '[[OPT]]' [-Wunused-command-line-argument]
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-device-lib-jit-link %s
 
 // Warning should be emitted when using -fsycl-default-sub-group-size= without -fsycl
 // RUN:   %clang -### -fsycl-default-sub-group-size=10  %s 2>&1 \
@@ -40,8 +38,7 @@
 
 // Warning should be emitted when using -fsycl-device-code-split-esimd without -fsycl
 // RUN:   %clang -### -fsycl-device-code-split-esimd  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-DCSE -DOPT=-fsycl-device-code-split-esimd %s
-// WARNING-DCSE: warning: argument unused during compilation: '[[OPT]]' [-Wunused-command-line-argument]
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-device-code-split-esimd %s
 
 // Warning should be emitted when using -fsycl-device-lib=libc without -fsycl
 // RUN:   %clang -### -fsycl-device-lib=libc  %s 2>&1 \
@@ -57,41 +54,36 @@
 // RUN:   | FileCheck -check-prefix=WARNING-DEVICE-OBJ %s
 // WARNING-DEVICE-OBJ: warning: argument unused during compilation: '-fsycl-device-obj=spirv' [-Wunused-command-line-argument]
 
-// Warning should be emitted when using -fsycl-device-only without -fsycl
+// Warning should not be emitted when using -fsycl-device-only without -fsycl
 // RUN:   %clang -### -fsycl-device-only  %s 2>&1 \
 // RUN:   | FileCheck -check-prefix=WARNING-DEVICE-ONLY -DOPT=-fsycl-device-only %s
-// WARNING-DEVICE-ONLY-NOT: warning: argument unused during compilation: '[[OPT]]' [-Wunused-command-line-argument]
 // RUN:   %clang_cl -### -fsycl-device-only  %s 2>&1 \
 // RUN:   | FileCheck -check-prefix=WARNING-DEVICE-ONLY -DOPT=-fsycl-device-only %s
 // WARNING-DEVICE-ONLY-NOT: warning: argument unused during compilation: '[[OPT]]' [-Wunused-command-line-argument]
 
 // Warning should be emitted when using -fsycl-early-optimizations without -fsycl
 // RUN:   %clang -### -fsycl-early-optimizations  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-EARLY-OPT -DOPT=-fsycl-early-optimizations %s
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-early-optimizations %s
 // RUN:   %clang_cl -### -fsycl-early-optimizations  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-EARLY-OPT -DOPT=-fsycl-early-optimizations %s
-// WARNING-EARLY-OPT: warning: argument unused during compilation: '[[OPT]]' [-Wunused-command-line-argument]
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-early-optimizations %s
 
 // Warning should be emitted when using -fsycl-embed-ir without -fsycl
 // RUN:   %clang -### -fsycl-embed-ir  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-EMBED-IR -DOPT=-fsycl-embed-ir %s
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-embed-ir %s
 // RUN:   %clang_cl -### -fsycl-embed-ir  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-EMBED-IR -DOPT=-fsycl-embed-ir %s
-// WARNING-EMBED-IR: warning: argument unused during compilation: '[[OPT]]' [-Wunused-command-line-argument]
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-embed-ir %s
 
 // Warning should be emitted when using -fsycl-esimd-force-stateless-mem without -fsycl
 // RUN:   %clang -### -fsycl-esimd-force-stateless-mem  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-FSM -DOPT=-fsycl-esimd-force-stateless-mem %s
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-esimd-force-stateless-mem %s
 // RUN:   %clang_cl -### -fsycl-esimd-force-stateless-mem  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-FSM -DOPT=-fsycl-esimd-force-stateless-mem %s
-// WARNING-FSM: warning: argument unused during compilation: '-fsycl-esimd-force-stateless-mem' [-Wunused-command-line-argument]
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-esimd-force-stateless-mem %s
 
 // Warning should be emitted when using -fsycl-force-inline-kernel-lambda without -fsycl
 // RUN:   %clang -### -fsycl-force-inline-kernel-lambda  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-FORCE-INLINE -DOPT=-fsycl-force-inline-kernel-lambda %s
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-force-inline-kernel-lambda %s
 // RUN:   %clang_cl -### -fsycl-force-inline-kernel-lambda  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-FORCE-INLINE -DOPT=-fsycl-force-inline-kernel-lambda %s
-// WARNING-FORCE-INLINE: warning: argument unused during compilation: '[[OPT]]' [-Wunused-command-line-argument]
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-force-inline-kernel-lambda %s
 
 // Warning should be emitted when using -fsycl-fp32-prec-sqrt without -fsycl
 // RUN:   %clang -### -fsycl-fp32-prec-sqrt  %s 2>&1 \
@@ -103,17 +95,15 @@
 
 // Warning should be emitted when using -fsycl-id-queries-fit-in-int without -fsycl
 // RUN:   %clang -### -fsycl-id-queries-fit-in-int  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-ID -DOPT=-fsycl-id-queries-fit-in-int %s
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-id-queries-fit-in-int %s
 // RUN:   %clang_cl -### -fsycl-id-queries-fit-in-int  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-ID -DOPT=-fsycl-id-queries-fit-in-int %s
-// WARNING-ID: warning: argument unused during compilation: '[[OPT]]' [-Wunused-command-line-argument]
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-id-queries-fit-in-int %s
 
 // Warning should be emitted when using -fsycl-instrument-device-code without -fsycl
 // RUN:   %clang -### -fsycl-instrument-device-code  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-INS-DEV -DOPT=-fsycl-instrument-device-code %s
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-instrument-device-code %s
 // RUN:   %clang_cl -### -fsycl-instrument-device-code  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-INS-DEV -DOPT=-fsycl-instrument-device-code %s
-// WARNING-INS-DEV: warning: argument unused during compilation: '[[OPT]]' [-Wunused-command-line-argument]
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-instrument-device-code %s
 
 // Warning should be emitted when using -fsycl-libspirv-path without -fsycl
 // RUN:   %clang -### -fsycl-libspirv-path=libspirv.bc  %s 2>&1 \
@@ -131,21 +121,20 @@
 
 // Warning should be emitted when using -fsycl-optimize-non-user-code without -fsycl
 // RUN:   %clang -### -fsycl-optimize-non-user-code  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-OPTIMIZE -DOPT=-fsycl-optimize-non-user-code %s
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-optimize-non-user-code %s
 // RUN:   %clang_cl -### -fsycl-optimize-non-user-code  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-OPTIMIZE -DOPT=-fsycl-optimize-non-user-code %s
-// WARNING-OPTIMIZE: warning: argument unused during compilation: '[[OPT]]' [-Wunused-command-line-argument]
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-optimize-non-user-code %s
 
 // Warning should be emitted when using -fsycl-unnamed-lambda without -fsycl
 // RUN:   %clang -### -fsycl-unnamed-lambda  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-UNNAMED-LAMBDA -DOPT=-fsycl-unnamed-lambda %s
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-unnamed-lambda %s
 // RUN:   %clang_cl -### -fsycl-unnamed-lambda  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-UNNAMED-LAMBDA -DOPT=-fsycl-unnamed-lambda %s
-// WARNING-UNNAMED-LAMBDA: warning: argument unused during compilation: '[[OPT]]' [-Wunused-command-line-argument]
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-unnamed-lambda %s
 
 // Warning should be emitted when using -fsycl-use-bitcode without -fsycl
 // RUN:   %clang -### -fsycl-use-bitcode  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-USE-BITCODE -DOPT=-fsycl-use-bitcode %s
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-use-bitcode %s
 // RUN:   %clang_cl -### -fsycl-use-bitcode  %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=WARNING-USE-BITCODE -DOPT=-fsycl-use-bitcode %s
-// WARNING-USE-BITCODE: warning: argument unused during compilation: '[[OPT]]' [-Wunused-command-line-argument]
+// RUN:   | FileCheck -check-prefix=WARNING-UNUSED-ARG -DOPT=-fsycl-use-bitcode %s
+
+// WARNING-UNUSED-ARG: warning: argument unused during compilation: '[[OPT]]' [-Wunused-command-line-argument]
