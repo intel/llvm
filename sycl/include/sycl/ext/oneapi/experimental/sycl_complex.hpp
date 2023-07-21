b4c97be3910a9b5b892e394bc925377a3fc74530
Alexey Bader <alexey.bader@intel.com>
2023-07-21 13:29:03 -0700

    [SYCL][NFC] Add missing license comments to DPC++ headers (#10527)

--- sycl/include/sycl/ext/oneapi/experimental/sycl_complex.hpp
+++ sycl/include/sycl/ext/oneapi/experimental/sycl_complex.hpp
@@ -1,8 +1,7 @@
-// -*- C++ -*-
-//===----------------------------------------------------------------------===//
+//===- sycl_complex.hpp ---------------------------------------------------===//
 //
-// Adapted from the LLVM Project, under the Apache License v2.0 with LLVM
-// Exceptions. See https://llvm.org/LICENSE.txt for license information.
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
