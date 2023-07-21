80b7de1d63f4152a5bfcc62906d2527e84105b4c
Brox Chen <brox.chen@intel.com>
2023-07-21 07:51:18 -0700

    [SYCL] Reorganize annotated_arg/ptr folder (#10420)
    Reorganize annotated_ptr/arg folder
    
    sycl/ext
    ----oneapi/
    --------experimental
    --------------annotated_ptr
    --------------------annotated_ptr.hpp
    --------------annotated_arg
    --------------------annotated_arg.hpp
    --------------common_annotated_properties
    --------------------properties.hpp
    ----intel/
    --------experimental
    --------------fpga_annotated_properties.hpp
    --------------fpga_kernel_properties.hpp
    
    ---------
    
    Co-authored-by: Dmitry Vodopyanov <dmitry.vodopyanov@intel.com>

--- sycl/include/sycl/ext/oneapi/experimental/annotated_ptr/annotated_ptr.hpp
+++ sycl/include/sycl/ext/oneapi/experimental/annotated_ptr/annotated_ptr.hpp
@@ -13,7 +13,8 @@
 
 #include <sycl/detail/stl_type_traits.hpp>
 #include <sycl/exception.hpp>
-#include <sycl/ext/oneapi/annotated_arg/properties.hpp>
+#include <sycl/ext/intel/experimental/fpga_annotated_properties.hpp>
+#include <sycl/ext/oneapi/experimental/common_annotated_properties/properties.hpp>
 #include <sycl/ext/oneapi/properties/properties.hpp>
 
 namespace sycl {
@@ -21,6 +22,41 @@ __SYCL_INLINE_VER_NAMESPACE(_V1) {
 namespace ext {
 namespace oneapi {
 namespace experimental {
+
+//===----------------------------------------------------------------------===//
+//        Specific properties of annotated_ptr
+//===----------------------------------------------------------------------===//
+struct alignment_key {
+  template <int K>
+  using value_t = property_value<alignment_key, std::integral_constant<int, K>>;
+};
+
+template <int K> inline constexpr alignment_key::value_t<K> alignment;
+
+template <> struct is_property_key<alignment_key> : std::true_type {};
+
+template <typename T, int W>
+struct is_valid_property<T, alignment_key::value_t<W>>
+    : std::bool_constant<std::is_pointer<T>::value> {};
+
+template <typename T, typename PropertyListT>
+struct is_property_key_of<alignment_key, annotated_ptr<T, PropertyListT>>
+    : std::true_type {};
+
+namespace detail {
+
+template <> struct PropertyToKind<alignment_key> {
+  static constexpr PropKind Kind = PropKind::Alignment;
+};
+
+template <> struct IsCompileTimeProperty<alignment_key> : std::true_type {};
+
+template <int N> struct PropertyMetaInfo<alignment_key::value_t<N>> {
+  static constexpr const char *name = "sycl-alignment";
+  static constexpr int value = N;
+};
+
+} // namespace detail
 
 namespace {
 #define PROPAGATE_OP(op)                                                       \
@@ -135,7 +171,8 @@ __SYCL_TYPE(annotated_ptr) annotated_ptr<T, detail::properties_t<Props...>> {
 
   // buffer_location and alignment are allowed for annotated_ref
   using allowed_properties =
-      std::tuple<decltype(buffer_location<0>), decltype(alignment<0>)>;
+      std::tuple<decltype(ext::intel::experimental::buffer_location<0>),
+                 decltype(ext::oneapi::experimental::alignment<0>)>;
   using filtered_properties =
       typename PropertiesFilter<allowed_properties, Props...>::tuple;
 
