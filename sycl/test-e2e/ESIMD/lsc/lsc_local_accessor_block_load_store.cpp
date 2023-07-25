1629b5b5b6078b88f9d312e4e68efb4eb8e92dcc
gregory <gregory@gaiaplatform.io>
2023-07-24 17:54:07 -0700

    Update test comments to address PR comments

--- sycl/test-e2e/ESIMD/lsc/lsc_local_accessor_block_load_store.cpp
+++ sycl/test-e2e/ESIMD/lsc/lsc_local_accessor_block_load_store.cpp
@@ -11,7 +11,6 @@
 //
 // TODO: Enable the test when GPU driver is ready/fixed.
 // XFAIL: opencl || windows || gpu-intel-pvc
-// TODO: add support for local_accessors to esimd_emulator.
 // UNSUPPORTED: esimd_emulator
 // This test verifies usage of block_load/block_store for local_accessor.
 
