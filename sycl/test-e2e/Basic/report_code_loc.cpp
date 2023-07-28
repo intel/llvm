aa14a97ca402fde823249ad6bb5c4bdca9809ce5
Khatavkar, Yashasvi <yashasvi.khatavkar@intel.com>
2023-07-28 06:29:41 -0700

    [SYCL] add test

---
+++ sycl/test-e2e/Basic/report_code_loc.cpp
@@ -0,0 +1,23 @@
+/*This test checks that source information where an exception occured is
+ * reported*/
+
+// RUN: %{build} -o %t.out
+// RUN: env ZE_DEBUG=1 %{run} %t.out 2>&1 | FileCheck  %s
+
+#include <sycl/sycl.hpp>
+using namespace sycl;
+#define XFLOAT float
+#define mdlXYZ 1000
+
+int main() {
+  bool failed = true;
+  XFLOAT *mdlImag;
+  queue q{{property::queue::enable_profiling()}};
+  mdlImag = sycl::malloc_device<XFLOAT>(mdlXYZ, q);
+  const char *funcName = "main";
+  try {
+    q.memcpy(mdlImag, 0, sizeof(XFLOAT));
+  } catch (...) {
+    // CHECK: Exception caught at File: {{.*}}report_code_loc.cpp | Function: main | Line: 19 | Column: 5
+  }
+}
