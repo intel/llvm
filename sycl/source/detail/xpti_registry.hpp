5475615c52d8b2655e63a040dfe162087d04490a
Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>
2023-08-01 06:29:57 -0700

    [XPTI][SYCL] remove  filtered XPTI enabling check from XPTIScope
    Signed-off-by: Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>

--- sycl/source/detail/xpti_registry.hpp
+++ sycl/source/detail/xpti_registry.hpp
@@ -222,9 +222,8 @@ public:
   /// @brief Method that emits begin/end trace notifications
   /// @return Current class
   XPTIScope &scopedNotify(uint16_t TraceType) {
-    TraceType &= 0xfffe;
-    if (xptiCheckTraceEnabled(MStreamID, TraceType) && MTP) {
-      MTraceType = TraceType;
+    if (xptiTraceEnabled() && MTP) {
+      MTraceType = TraceType & 0xfffe;
       MScopedNotify = true;
       xptiNotifySubscribers(MStreamID, MTraceType, nullptr, MTraceEvent,
                             MInstanceID, static_cast<const void *>(MUserData));
@@ -232,7 +231,7 @@ public:
     return *this;
   }
   ~XPTIScope() {
-    if (xptiCheckTraceEnabled(MStreamID, MTraceType) && MTP && MScopedNotify) {
+    if (xptiTraceEnabled() && MTP && MScopedNotify) {
       if (MTraceType == (uint16_t)xpti::trace_point_type_t::signal ||
           MTraceType == (uint16_t)xpti::trace_point_type_t::graph_create ||
           MTraceType == (uint16_t)xpti::trace_point_type_t::node_create ||
