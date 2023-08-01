ca91dc97611bc8abe542e681539be36eb07f854c
Petr Vesely <petr.vesely@codeplay.com>
2023-08-01 12:52:48 +0100

    [UR] Add license to missing files

--- test/conformance/adapters/cuda/cuda_urEventGetNativeHandle.cpp
+++ test/conformance/adapters/cuda/cuda_urEventGetNativeHandle.cpp
@@ -1,3 +1,8 @@
+// Copyright (C) 2022-2023 Intel Corporation
+// Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM Exceptions.
+// See LICENSE.TXT
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 #include "cuda_fixtures.h"
 
 using urCudaEventGetNativeHandleTest = uur::urQueueTest;
