9febf28decac41f3c2f2b5c81cd197757ef51f32
Sarnie, Nick <nick.sarnie@intel.com>
2023-08-04 08:22:53 -0700

    Address LIT test feedback
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- sycl/test/esimd/genx_func_attr.cpp
+++ sycl/test/esimd/genx_func_attr.cpp
@@ -16,24 +16,24 @@ __attribute__((sycl_kernel)) void kernel(Func kernelFunc) {
   kernelFunc();
 }
 
-SYCL_ESIMD_FUNCTION SYCL_EXTERNAL ESIMD_NOINLINE void callee() {
+SYCL_ESIMD_FUNCTION SYCL_EXTERNAL ESIMD_NOINLINE void callee(int x) {
   slm_allocator<1234> alloc;
   named_barrier_init<13>();
 }
 
 // inherits SLMSize and NBarrierCount from callee
-void caller_abc() {
-  kernel<class kernel_abc>([=]() SYCL_ESIMD_KERNEL { callee(); });
-  // CHECK: define dso_local spir_kernel void @_ZTSZ10caller_abcvE10kernel_abc() local_unnamed_addr #2
+void caller_abc(int x) {
+  kernel<class kernel_abc>([=]() SYCL_ESIMD_KERNEL { callee(x); });
+  // CHECK: define dso_local spir_kernel void @_ZTSZ10caller_abciE10kernel_abc() local_unnamed_addr #2
 }
 
 // inherits only NBarrierCount from callee
-void caller_xyz() {
+void caller_xyz(int x) {
   kernel<class kernel_xyz>([=]() SYCL_ESIMD_KERNEL {
     slm_init(1235); // also works in non-O0
-    callee();
+    callee(x);
   });
-  // CHECK: define dso_local spir_kernel void @_ZTSZ10caller_xyzvE10kernel_xyz() local_unnamed_addr #2
+  // CHECK: define dso_local spir_kernel void @_ZTSZ10caller_xyziE10kernel_xyz() local_unnamed_addr #2
 }
 
 // CHECK: attributes #2 = { {{.*}} "VCNamedBarrierCount"="13" "VCSLMSize"="2469"
