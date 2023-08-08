a3485c98a19cbfb92f0e7921a09eac55e167c45b
jinge90 <ge.jin@intel.com>
2023-08-08 16:16:04 +0800

    fix inline _V1 namespace break
    Signed-off-by: jinge90 <ge.jin@intel.com>

--- sycl/include/sycl/ext/intel/math/imf_fp_conversions.hpp
+++ sycl/include/sycl/ext/intel/math/imf_fp_conversions.hpp
@@ -169,7 +169,7 @@ uint16_t __imf_ushort_as_bfloat16(unsigned short);
 };
 
 namespace sycl {
-__SYCL_INLINE_VER_NAMESPACE(_V1) {
+inline namespace _V1 {
 namespace ext::intel::math {
 
 template <typename To = int, typename From = float> To float2int_rd(From x) {
@@ -961,5 +961,5 @@ To bfloat162ull_rz(From x) {
 }
 
 } // namespace ext::intel::math
-} // __SYCL_INLINE_VER_NAMESPACE(_V1)
+} // namespace _V1
 } // namespace sycl
