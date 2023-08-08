4d9dff47c13f459f55c0ff9d1e3ebcab42723d69
Dounia <dounia.khaldi@intel.com>
2023-08-08 08:48:34 -0700

    Address Yury's comments

---
+++ sycl/test-e2e/Matrix/common.hpp
@@ -0,0 +1,18 @@
+using bfloat16 = sycl::ext::oneapi::bfloat16;
+
+template <typename T, size_t NUM_ROWS, size_t NUM_COLS> struct big_matrix {
+public:
+  T *mat;
+
+public:
+  T *get_data() { return mat; }
+  void set_data(T *data) { mat = data; }
+  big_matrix(T *data) : mat(data) {}
+};
+
+float make_fp32(bfloat16 x) {
+  unsigned int y = *((int *)&x);
+  y = y << 16;
+  float *res = reinterpret_cast<float *>(&y);
+  return *res;
+}
