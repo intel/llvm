12acd6fe864e3aacae354bcf1469a279169bc9bf
Cai, Justin <justin.cai@intel.com>
2023-08-09 15:12:34 +0000

    Use numeric_limits

--- sycl/test-e2e/OptionalKernelFeatures/large-reqd-work-group-size.cpp
+++ sycl/test-e2e/OptionalKernelFeatures/large-reqd-work-group-size.cpp
@@ -34,26 +34,26 @@ void throws_kernel_not_supported(const char *test_name, FunctorT f) {
 
 int main(int argc, char *argv[]) {
   throws_kernel_not_supported("nd_range<1>", [] {
-    constexpr uint32_t N = 4294967295;
+    constexpr uint32_t N = std::numeric_limits<uint32_t>::max();
     q.parallel_for<class K0>(nd_range<1>(N, N),
                              [=](auto) [[sycl::reqd_work_group_size(N)]] {});
   });
 
   throws_kernel_not_supported("nd_range<2>", [] {
-    constexpr uint32_t N = 4294967295;
+    constexpr uint32_t N = std::numeric_limits<uint32_t>::max();
     q.parallel_for<class K1>(nd_range<2>({N, N}, {N, N}),
                              [=](auto) [[sycl::reqd_work_group_size(N, N)]] {});
   });
 
   throws_kernel_not_supported("nd_range<3>", [] {
-    constexpr uint32_t N = 4294967295;
+    constexpr uint32_t N = std::numeric_limits<uint32_t>::max();
     q.parallel_for<class K2>(nd_range<3>({N, N, N}, {N, N, N}),
                              [=](auto)
                                  [[sycl::reqd_work_group_size(N, N, N)]] {});
   });
 
   throws_kernel_not_supported("uint32_max+2", [] {
-    constexpr uint64_t N = 4294967297;
+    constexpr uint64_t N = std::numeric_limits<uint32_t>::max() + uint64_t(2);
     q.parallel_for<class K3>(nd_range<1>(N, N),
                              [=](auto) [[sycl::reqd_work_group_size(N)]] {});
   });
