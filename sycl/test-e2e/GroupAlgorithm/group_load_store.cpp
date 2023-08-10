ac132ebb0be311157ca257fc0c75d1d6ed602c16
Khatavkar, Yashasvi <yashasvi.khatavkar@intel.com>
2023-08-10 11:05:39 -0700

    [SYCL] update test

--- sycl/test-e2e/GroupAlgorithm/group_load_store.cpp
+++ sycl/test-e2e/GroupAlgorithm/group_load_store.cpp
@@ -1,5 +1,5 @@
 // Use per-kernel split as a workaroud for a miscompilation bug in IGC.
-// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple -fsycl-device-code-split=per_kernel %s -o %t.out
+// RUN: %clangxx -fsycl -fsycl-targets=%{sycl_triple} -fsycl-device-code-split=per_kernel %s -o %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
 // RUN: %ACC_RUN_PLACEHOLDER %t.out
