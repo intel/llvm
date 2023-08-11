2e648df213b5001612619d97b13cc9c3e1596d73
Georgi Mirazchiyski <georgi.mirazchiyski@codeplay.com>
2023-08-11 16:45:14 +0100

    Enable some debug logging and tracing wrt AMDGPU failures

--- sycl/test-e2e/AtomicRef/or.cpp
+++ sycl/test-e2e/AtomicRef/or.cpp
@@ -1,5 +1,6 @@
-// RUN: %{build} -o %t.out
-// RUN: %{run} %t.out
+// REQUIRES: hip_amd
+// RUN: %{build} -mllvm -debug-only="regalloc,si-insert-waitcnts,si-fix-sgpr-copies,si-fix-vgpr-copies,si-instr-info" -o %t.out
+// RUN: AMD_LOG_LEVEL=3 %{run} %t.out
 
 #include "or.h"
 
