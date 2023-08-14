48638439597d77fac380558d9831d83df3406db7
Georgi Mirazchiyski <georgi.mirazchiyski@codeplay.com>
2023-08-14 10:10:17 +0100

    Try to fix debug logging

--- sycl/test-e2e/AtomicRef/or.cpp
+++ sycl/test-e2e/AtomicRef/or.cpp
@@ -1,6 +1,6 @@
 // REQUIRES: hip_amd
-// RUN: %{build} -mllvm -debug-only="regalloc,si-insert-waitcnts,si-fix-sgpr-copies,si-fix-vgpr-copies,si-instr-info" -o %t.out
-// RUN: AMD_LOG_LEVEL=3 %{run} %t.out
+// RUN: %{build} -mllvm -debug-only="regalloc" -o %t.out
+// RUN: env AMD_LOG_LEVEL=3 %{run} %t.out
 
 #include "or.h"
 
--- sycl/test-e2e/Basic/parallel_for_user_types.cpp
+++ sycl/test-e2e/Basic/parallel_for_user_types.cpp
@@ -1,6 +1,6 @@
 // REQUIRES: hip_amd
-// RUN: %{build} -mllvm -debug-only="regalloc,si-insert-waitcnts,si-fix-sgpr-copies,si-fix-vgpr-copies,si-instr-info" -o %t.out
-// RUN: AMD_LOG_LEVEL=3 %{run} %t.out
+// RUN: %{build} -mllvm -debug-only="regalloc" -o %t.out
+// RUN: env AMD_LOG_LEVEL=3 %{run} %t.out
 
 // This test performs basic check of supporting user defined class that are
 // implicitly converted from sycl::item/sycl::nd_item in parallel_for.
