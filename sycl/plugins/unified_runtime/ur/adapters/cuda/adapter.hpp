2f5940cc98d487ecd7133fd9d77532450e7bfaf5
Petr Vesely <veselypeta@gmail.com>
2023-08-15 10:07:27 +0100

    [UR][CUDA][HIP][L0] Cleanup licence header
    Update sycl/plugins/unified_runtime/ur/adapters/hip/memory.cpp
    
    Co-authored-by: Nicolas Miller <nicolas.pierre.miller@gmail.com>
    
    Update sycl/plugins/unified_runtime/ur/adapters/hip/memory.hpp
    
    Co-authored-by: Nicolas Miller <nicolas.pierre.miller@gmail.com>
    
    Update sycl/plugins/unified_runtime/ur/adapters/hip/memory.cpp
    
    Co-authored-by: Alexey Bader <alexey.bader@intel.com>

--- sycl/plugins/unified_runtime/ur/adapters/cuda/adapter.hpp
+++ sycl/plugins/unified_runtime/ur/adapters/cuda/adapter.hpp
@@ -1,4 +1,4 @@
-//===--------- adapter.cpp - CUDA Adapter ----------------------------===//
+//===--------- adapter.hpp - CUDA Adapter ----------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
