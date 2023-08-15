70026a0ae3984e8bfa87a42fb0ec1f21de22f7d5
Dounia <dounia.khaldi@intel.com>
2023-08-15 07:05:28 -0700

    format

--- sycl/test-e2e/Matrix/joint_matrix_down_convert.cpp
+++ sycl/test-e2e/Matrix/joint_matrix_down_convert.cpp
@@ -36,8 +36,8 @@ void matrix_copy(big_matrix<T1, M, N> &C, big_matrix<T2, M, K> &A) {
      auto accA = bufA.get_access<access::mode::write>(cgh);
 
      cgh.parallel_for(
-         nd_range<2>({NDRangeM, NDRangeN * SG_SZ}, {1, 1 * SG_SZ}), [=
-     ](nd_item<2> spmd_item) [[intel::reqd_sub_group_size(SG_SZ)]] {
+         nd_range<2>({NDRangeM, NDRangeN * SG_SZ}, {1, 1 * SG_SZ}),
+         [=](nd_item<2> spmd_item) [[intel::reqd_sub_group_size(SG_SZ)]] {
            // The submatrix API has to be accessed by all the workitems in a
            // subgroup these functions will be called once by the subgroup no
            // code divergence between the workitems
