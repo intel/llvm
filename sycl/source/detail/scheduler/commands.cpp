9bb9812bfbd022b8035bddb990d723dd7c7f0a94
smaslov <sergey.v.maslov@intel.com>
2023-08-15 11:30:01 -0700

    [SYCL] Avoid std::string(0)
    Signed-off-by: smaslov <sergey.v.maslov@intel.com>

--- sycl/source/detail/scheduler/commands.cpp
+++ sycl/source/detail/scheduler/commands.cpp
@@ -2037,10 +2037,11 @@ void emitKernelInstrumentationData(
 
   xpti_td *CmdTraceEvent = nullptr;
   uint64_t InstanceID = -1;
-  instrumentationFillCommonData(KernelName, CodeLoc.fileName(),
-                                CodeLoc.lineNumber(), CodeLoc.columnNumber(),
-                                Address, Queue, FromSource, InstanceID,
-                                CmdTraceEvent);
+  std::string FileName =
+      CodeLoc.fileName() ? CodeLoc.fileName() : std::string();
+  instrumentationFillCommonData(KernelName, FileName, CodeLoc.lineNumber(),
+                                CodeLoc.columnNumber(), Address, Queue,
+                                FromSource, InstanceID, CmdTraceEvent);
 
   if (CmdTraceEvent) {
     instrumentationAddExtraKernelMetadata(CmdTraceEvent, NDRDesc,
