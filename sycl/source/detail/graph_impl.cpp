a7379f7a7f9d71424789c7abcaa86d96e5eac405
Byoungro So <byoungro.so@intel.com>
2023-08-15 19:25:29 -0700

    [SYCL] Fix coverity issues
    Signed-off-by: Byoungro So <byoungro.so@intel.com>

--- sycl/source/detail/graph_impl.cpp
+++ sycl/source/detail/graph_impl.cpp
@@ -14,6 +14,7 @@
 #include <detail/scheduler/commands.hpp>
 #include <sycl/feature_test.hpp>
 #include <sycl/queue.hpp>
+#include <random>
 
 // Developer switch to use emulation mode on all backends, even those that
 // report native support, this is useful for debugging.
@@ -37,12 +38,12 @@ void connectToExitNodes(
     std::shared_ptr<node_impl> CurrentNode,
     const std::vector<std::shared_ptr<node_impl>> &NewInputs) {
   if (CurrentNode->MSuccessors.size() > 0) {
-    for (auto Successor : CurrentNode->MSuccessors) {
+    for (auto &Successor : CurrentNode->MSuccessors) {
       connectToExitNodes(Successor, NewInputs);
     }
 
   } else {
-    for (auto Input : NewInputs) {
+    for (auto &Input : NewInputs) {
       CurrentNode->registerSuccessor(Input, CurrentNode);
     }
   }
@@ -75,7 +76,7 @@ bool checkForRequirement(sycl::detail::AccessorImplHost *Req,
 
 void exec_graph_impl::schedule() {
   if (MSchedule.empty()) {
-    for (auto Node : MGraphImpl->MRoots) {
+    for (auto &Node : MGraphImpl->MRoots) {
       Node->sortTopological(Node, MSchedule);
     }
   }
@@ -97,7 +98,7 @@ std::shared_ptr<node_impl> graph_impl::addSubgraphNodes(
 
   // Recursively walk the graph to find exit nodes and connect up the inputs
   // TODO: Consider caching exit nodes so we don't have to do this
-  for (auto NodeImpl : MRoots) {
+  for (auto &NodeImpl : MRoots) {
     connectToExitNodes(NodeImpl, Inputs);
   }
 
@@ -118,7 +119,7 @@ graph_impl::add(const std::vector<std::shared_ptr<node_impl>> &Dep) {
 
   // TODO: Encapsulate in separate function to avoid duplication
   if (!Dep.empty()) {
-    for (auto N : Dep) {
+    for (auto &N : Dep) {
       N->registerSuccessor(NodeImpl, N); // register successor
       this->removeRoot(NodeImpl);        // remove receiver from root node
                                          // list
@@ -180,13 +181,13 @@ graph_impl::add(sycl::detail::CG::CGTYPE CGType,
   const auto &Requirements = CommandGroup->getRequirements();
   for (auto &Req : Requirements) {
     // Look through the graph for nodes which share this requirement
-    for (auto NodePtr : MRoots) {
+    for (auto &NodePtr : MRoots) {
       checkForRequirement(Req, NodePtr, UniqueDeps);
     }
   }
 
   // Add any nodes specified by event dependencies into the dependency list
-  for (auto Dep : CommandGroup->getEvents()) {
+  for (auto &Dep : CommandGroup->getEvents()) {
     if (auto NodeImpl = MEventsMap.find(Dep); NodeImpl != MEventsMap.end()) {
       if (UniqueDeps.find(NodeImpl->second) == UniqueDeps.end()) {
         UniqueDeps.insert(NodeImpl->second);
@@ -204,7 +205,7 @@ graph_impl::add(sycl::detail::CG::CGTYPE CGType,
   const std::shared_ptr<node_impl> &NodeImpl =
       std::make_shared<node_impl>(CGType, std::move(CommandGroup));
   if (!Deps.empty()) {
-    for (auto N : Deps) {
+    for (auto &N : Deps) {
       N->registerSuccessor(NodeImpl, N); // register successor
       this->removeRoot(NodeImpl);        // remove receiver from root node
                                          // list
@@ -218,8 +219,10 @@ graph_impl::add(sycl::detail::CG::CGTYPE CGType,
 bool graph_impl::clearQueues() {
   bool AnyQueuesCleared = false;
   for (auto &Queue : MRecordingQueues) {
-    Queue->setCommandGraph(nullptr);
-    AnyQueuesCleared = true;
+    if (Queue) {
+      Queue->setCommandGraph(nullptr);
+      AnyQueuesCleared = true;
+    }
   }
   MRecordingQueues.clear();
 
@@ -507,27 +510,28 @@ modifiable_command_graph::finalize(const sycl::property_list &) const {
 
 bool modifiable_command_graph::begin_recording(queue &RecordingQueue) {
   auto QueueImpl = sycl::detail::getSyclObjImpl(RecordingQueue);
+  if (QueueImpl) {
+    if (QueueImpl->get_context() != impl->getContext()) {
+      throw sycl::exception(sycl::make_error_code(errc::invalid),
+                            "begin_recording called for a queue whose context "
+                            "differs from the graph context.");
+    }
+    if (QueueImpl->get_device() != impl->getDevice()) {
+      throw sycl::exception(sycl::make_error_code(errc::invalid),
+                            "begin_recording called for a queue whose device "
+                            "differs from the graph device.");
+    }
 
-  if (QueueImpl->get_context() != impl->getContext()) {
-    throw sycl::exception(sycl::make_error_code(errc::invalid),
-                          "begin_recording called for a queue whose context "
-                          "differs from the graph context.");
-  }
-  if (QueueImpl->get_device() != impl->getDevice()) {
-    throw sycl::exception(sycl::make_error_code(errc::invalid),
-                          "begin_recording called for a queue whose device "
-                          "differs from the graph device.");
-  }
-
-  if (QueueImpl->getCommandGraph() == nullptr) {
-    QueueImpl->setCommandGraph(impl);
-    impl->addQueue(QueueImpl);
-    return true;
-  }
-  if (QueueImpl->getCommandGraph() != impl) {
-    throw sycl::exception(sycl::make_error_code(errc::invalid),
-                          "begin_recording called for a queue which is already "
-                          "recording to a different graph.");
+    if (QueueImpl->getCommandGraph() == nullptr) {
+      QueueImpl->setCommandGraph(impl);
+      impl->addQueue(QueueImpl);
+      return true;
+    }
+    if (QueueImpl->getCommandGraph() != impl) {
+      throw sycl::exception(sycl::make_error_code(errc::invalid),
+                            "begin_recording called for a queue which is already "
+                            "recording to a different graph.");
+    }
   }
   // Queue was already recording to this graph.
   return false;
@@ -546,7 +550,7 @@ bool modifiable_command_graph::end_recording() { return impl->clearQueues(); }
 
 bool modifiable_command_graph::end_recording(queue &RecordingQueue) {
   auto QueueImpl = sycl::detail::getSyclObjImpl(RecordingQueue);
-  if (QueueImpl->getCommandGraph() == impl) {
+  if (QueueImpl && QueueImpl->getCommandGraph() == impl) {
     QueueImpl->setCommandGraph(nullptr);
     impl->removeQueue(QueueImpl);
     return true;
@@ -572,8 +576,10 @@ bool modifiable_command_graph::end_recording(
 
 executable_command_graph::executable_command_graph(
     const std::shared_ptr<detail::graph_impl> &Graph, const sycl::context &Ctx)
-    : MTag(rand()),
-      impl(std::make_shared<detail::exec_graph_impl>(Ctx, Graph)) {
+    : impl(std::make_shared<detail::exec_graph_impl>(Ctx, Graph)) {
+  std::default_random_engine randEngine;
+  std::uniform_int_distribution<int> dist(0, RAND_MAX);
+  MTag = dist(randEngine);
   finalizeImpl(); // Create backend representation for executable graph
 }
 
