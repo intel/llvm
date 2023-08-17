98ebdf5d313c2cc2536b52e9181e92e3738ebfb8
Sarnie, Nick <nick.sarnie@intel.com>
2023-08-17 08:23:23 -0700

    Rework change to not use experimental infrastructure
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- source/common/ur_params.hpp
+++ source/common/ur_params.hpp
@@ -2772,6 +2772,10 @@ inline std::ostream &operator<<(std::ostream &os, enum ur_device_info_t value) {
         os << "UR_DEVICE_INFO_VIRTUAL_MEMORY_SUPPORT";
         break;
 
+    case UR_DEVICE_INFO_ESIMD_SUPPORT:
+        os << "UR_DEVICE_INFO_ESIMD_SUPPORT";
+        break;
+
     case UR_DEVICE_INFO_BINDLESS_IMAGES_SUPPORT_EXP:
         os << "UR_DEVICE_INFO_BINDLESS_IMAGES_SUPPORT_EXP";
         break;
@@ -2836,9 +2840,6 @@ inline std::ostream &operator<<(std::ostream &os, enum ur_device_info_t value) {
         os << "UR_DEVICE_INFO_INTEROP_SEMAPHORE_EXPORT_SUPPORT_EXP";
         break;
 
-    case UR_DEVICE_INFO_ESIMD_SUPPORT_EXP:
-        os << "UR_DEVICE_INFO_ESIMD_SUPPORT_EXP";
-        break;
     default:
         os << "unknown enumerator";
         break;
@@ -4414,6 +4415,20 @@ inline void serializeTagged(std::ostream &os, const void *ptr,
         os << ")";
     } break;
 
+    case UR_DEVICE_INFO_ESIMD_SUPPORT: {
+        const ur_bool_t *tptr = (const ur_bool_t *)ptr;
+        if (sizeof(ur_bool_t) > size) {
+            os << "invalid size (is: " << size
+               << ", expected: >=" << sizeof(ur_bool_t) << ")";
+            return;
+        }
+        os << (void *)(tptr) << " (";
+
+        os << *tptr;
+
+        os << ")";
+    } break;
+
     case UR_DEVICE_INFO_BINDLESS_IMAGES_SUPPORT_EXP: {
         const ur_bool_t *tptr = (const ur_bool_t *)ptr;
         if (sizeof(ur_bool_t) > size) {
@@ -4625,20 +4640,6 @@ inline void serializeTagged(std::ostream &os, const void *ptr,
     } break;
 
     case UR_DEVICE_INFO_INTEROP_SEMAPHORE_EXPORT_SUPPORT_EXP: {
-        const ur_bool_t *tptr = (const ur_bool_t *)ptr;
-        if (sizeof(ur_bool_t) > size) {
-            os << "invalid size (is: " << size
-               << ", expected: >=" << sizeof(ur_bool_t) << ")";
-            return;
-        }
-        os << (void *)(tptr) << " (";
-
-        os << *tptr;
-
-        os << ")";
-    } break;
-
-    case UR_DEVICE_INFO_ESIMD_SUPPORT_EXP: {
         const ur_bool_t *tptr = (const ur_bool_t *)ptr;
         if (sizeof(ur_bool_t) > size) {
             os << "invalid size (is: " << size
