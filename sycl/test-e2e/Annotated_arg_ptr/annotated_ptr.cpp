09b66f465727fa78550c95acb3a8a7417a0242f3
Roland Schulz <roland.schulz@intel.com>
2023-08-17 13:46:58 -0700

    Fix test

--- sycl/test-e2e/Annotated_arg_ptr/annotated_ptr.cpp
+++ sycl/test-e2e/Annotated_arg_ptr/annotated_ptr.cpp
@@ -1,15 +1,6 @@
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 //
-// The HIP and OpenCL GPU backends do not currently support device_global
-// backend calls.
-// UNSUPPORTED: hip || (opencl && gpu)
-//
-// Temporarily disabled for OpenCL CPU while we wait for CPU driver bump. Same
-// applies to the FPGA emulator.
-// UNSUPPORTED: opencl
-//
-// Tests operator-> on annotated_arg.
 
 #include "common.hpp"
 
@@ -21,21 +12,21 @@ struct test {
 int main() {
   queue Q;
 
-  auto *a = malloc_device<int>(8, Q);
+  auto *a = malloc_shared<int>(8, Q);
   auto a_ptr = annotated_ptr{a, properties{buffer_location<0>}};
   for (int i = 0; i < 8; i++)
     a_ptr[i] = i;
 
-  auto *b = malloc_device<int>(4, Q);
+  auto *b = malloc_shared<int>(4, Q);
   auto b_ptr = annotated_ptr{b, properties{buffer_location<1>, alignment<16>}};
 
-  auto *c = malloc_device<test>(1, Q);
-  c->b = malloc_device<int>(1, Q);
+  auto *c = malloc_shared<test>(1, Q);
+  c->b = malloc_shared<int>(1, Q);
   auto c_ptr = annotated_ptr{c, properties{buffer_location<2>, alignment<16>}};
   c->a = 0;
   c->b[0] = 0;
 
-  auto *d = malloc_device<int>(4, Q);
+  auto *d = malloc_shared<int>(4, Q);
   auto d_ptr = annotated_ptr{d, properties{buffer_location<3>}};
   for (int i = 0; i < 4; i++)
     d_ptr[i] = i;
