ccfe1f6e83594c9019bcd463f0d99e65e037eef8
Hugh Delaney <hugh.delaney@codeplay.com>
2023-08-18 11:19:27 +0100

    Add more known adapters to list

--- source/loader/ur_adapter_registry.hpp
+++ source/loader/ur_adapter_registry.hpp
@@ -113,8 +113,10 @@ class AdapterRegistry {
     // to load the adapter.
     std::vector<std::vector<fs::path>> adaptersLoadPaths;
 
-    static constexpr std::array<const char *, 2> knownAdapterNames{
+    static constexpr std::array<const char *, 4> knownAdapterNames{
         MAKE_LIBRARY_NAME("ur_adapter_level_zero", "0"),
+        MAKE_LIBRARY_NAME("ur_adapter_hip", "0"),
+        MAKE_LIBRARY_NAME("ur_adapter_opencl", "0"),
         MAKE_LIBRARY_NAME("ur_adapter_cuda", "0")};
 
     std::optional<std::vector<fs::path>> getEnvAdapterSearchPaths() {
