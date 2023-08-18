ffbb1eae95f49b6424138b114f8aac600c92d3e3
Chen, Brox <brox.chen@intel.com>
2023-08-18 10:06:56 -0700

    remove comment

--- sycl/test-e2e/Annotated_arg_ptr/annotated_arg.cpp
+++ sycl/test-e2e/Annotated_arg_ptr/annotated_arg.cpp
@@ -1,15 +1,6 @@
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 //
-// The HIP and OpenCL GPU backends do not currently support device_global
-// backend calls.
-// UNSUPPORTED: hip || (opencl && gpu)
-//
-// Temporarily disabled for OpenCL CPU while we wait for CPU driver bump. Same
-// applies to the FPGA emulator.
-// UNSUPPORTED: opencl
-//
-// Tests operator-> on annotated_arg.
 
 #include "common.hpp"
 
