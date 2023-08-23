3fa4db3a9734caaeae62850363bfae1fa864a742
Georgi Mirazchiyski <georgi.mirazchiyski@codeplay.com>
2023-08-23 16:35:38 +0100

    Make the driver test independent of sycl mode.#

--- clang/test/Driver/sycl-clang-offload-bundler-opaque-pointers.cpp
+++ clang/test/Driver/sycl-clang-offload-bundler-opaque-pointers.cpp
@@ -3,42 +3,42 @@
 /// to the bundle targets and clang-offload-bundler is able to read the bitcode.
 ///
 
+// REQUIRES: x86-registered-target
 // UNSUPPORTED: system-windows
 
-#include "sycl/detail/defines_elementary.hpp"
-SYCL_EXTERNAL void foo(int *p) {
-    *p = 1;
+void foo(int *p) {
+  *p = 1;
 }
 
 //
 // Generate inputs for clang-offload-bundler.
 //
-// 
-// RUN: %clangxx -fsycl -fsycl-targets=spir64 -fsycl-device-only -Xclang -no-opaque-pointers %s -S -emit-llvm -o %s.spv.ll
-// RUN: %clangxx -fsycl -fsycl-targets=nvptx-nvidia-cuda -fsycl-device-only %s -S -emit-llvm -o %s.nv.ll
-// RUN: %clangxx -fsycl -target x86_64-unknown-linux-gnu %s -c -o %s.host.o
+// RUN: %clangxx -target openmp-x86_64-pc-linux-gnu -Xclang -no-opaque-pointers %s -S -emit-llvm -o %s.tgt1.ll
+// RUN: %clangxx -target openmp-x86_64-pc-linux-gnu -Xclang -no-opaque-pointers %s -c -emit-llvm -o %s.tgt1.bc
+// RUN: %clangxx -target openmp-nvptx64-nvidia-cuda %s -S -emit-llvm -o %s.tgt2.ll
+// RUN: %clangxx -target openmp-nvptx64-nvidia-cuda %s -c -emit-llvm -o %s.tgt2.bc
+// RUN: %clangxx -target x86_64-unknown-linux-gnu %s -c -o %s.host.o
 
 //
-// Check clang-offload-bundler obj for opaque pointers support error when spir64
-// is the first bc input, followed by an opaque pointers module bc, i.e. nvptx.
+// Check clang-offload-bundler obj for opaque pointers support error when typed
+// pointer module is the first input target, followed by an opaque pointers one.
 //
-// RUN: llvm-as %s.spv.ll -o %s.spv.bc | llvm-as %s.nv.ll -o %s.nv.bc \
-// RUN: | clang-offload-bundler -type=o -targets=sycl-spir64-unknown-unknown,sycl-nvptx64-nvidia-cuda-sm_50,host-x86_64-unknown-linux-gpu -output=%s.o -input=%s.spv.bc -input=%s.nv.bc -input=%s.host.o 2>&1 \
+// RUN: clang-offload-bundler -type=o -targets=openmp-x86_64-pc-linux-gnu,openmp-nvptx64-nvidia-cuda,host-x86_64-unknown-linux-gnu -output=%s.bundle.o -input=%s.tgt1.bc -input=%s.tgt2.bc -input=%s.host.o 2>&1 \
 // RUN: | FileCheck %s -check-prefix=CHECK-STD --allow-empty
 // CHECK-STD-NOT: error: Opaque pointers are only supported in -opaque-pointers mode
 
 //
-// Verify the spir64 module contains typed pointers (set -no-opaque-pointers).
+// Verify the target 1 module contains typed pointers (set -no-opaque-pointers).
 //
-// RUN: FileCheck %s --check-prefix CHECK_OPAQUE_POINTERS_SPIRV --input-file=%s.spv.ll
-// CHECK_OPAQUE_POINTERS_SPIRV: target triple = "spir64-unknown-unknown"
-// CHECK_OPAQUE_POINTERS_SPIRV: i32 addrspace(4)*
-// CHECK_OPAQUE_POINTERS_SPIRV-NOT: ptr addrspace(2)
+// RUN: FileCheck %s --check-prefix CHECK-TYPED_POINTERS_TARGET --input-file=%s.tgt1.ll
+// CHECK-TYPED_POINTERS_TARGET: target triple = "x86_64-pc-linux-gnu-openmp"
+// CHECK-TYPED_POINTERS_TARGET: i32* noundef
+// CHECK-TYPED_POINTERS_TARGET-NOT: ptr noundef
 
 //
-// Verify the nvptx module contains opaque pointers.
+// Verify the target 2 module contains opaque pointers.
 //
-// RUN: FileCheck %s --check-prefix CHECK_OPAQUE_POINTERS_NVPTX --input-file=%s.nv.ll
-// CHECK_OPAQUE_POINTERS_NVPTX: target triple = "nvptx-nvidia-cuda"
-// CHECK_OPAQUE_POINTERS_NVPTX: ptr addrspace(2)
-// CHECK_OPAQUE_POINTERS_NVPTX-NOT: i32 addrspace(4)*
+// RUN: FileCheck %s --check-prefix CHECK-OPAQUE_POINTERS_TARGET --input-file=%s.tgt2.ll
+// CHECK-OPAQUE_POINTERS_TARGET: target triple = "nvptx64-nvidia-cuda-openmp"
+// CHECK-OPAQUE_POINTERS_TARGET: ptr noundef
+// CHECK-OPAQUE_POINTERS_TARGET-NOT: i32* noundef
