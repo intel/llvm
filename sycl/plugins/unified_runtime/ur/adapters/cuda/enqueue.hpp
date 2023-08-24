c753b8ea2dc0cbcf2ef81f99047a386d0074d4ad
Maxime France-Pillois <maxime.francepillois@codeplay.com>
2023-08-24 11:03:15 +0100

    [SYCL][Graph] Implement graph enqueue for CUDA backend
    Implements urCommandBufferAppendKernelLaunchExp function to append kernel to a command buffer.
    Implements urCommandBufferAppendKernelLaunchExp function to enqueue a command buffer to a cuda stream.
    Enable all tests supported by the current state of the Cuda backend.
    
    Sets a ScopedContext before enqueuing command buffer.
    
    * [SYCL][Graph] Adds missing exception catch

--- sycl/plugins/unified_runtime/ur/adapters/cuda/enqueue.hpp
+++ sycl/plugins/unified_runtime/ur/adapters/cuda/enqueue.hpp
@@ -14,3 +14,20 @@
 ur_result_t enqueueEventsWait(ur_queue_handle_t CommandQueue, CUstream Stream,
                               uint32_t NumEventsInWaitList,
                               const ur_event_handle_t *EventWaitList);
+
+void guessLocalWorkSize(ur_device_handle_t Device, size_t *ThreadsPerBlock,
+                        const size_t *GlobalWorkSize, const uint32_t WorkDim,
+                        const size_t MaxThreadsPerBlock[3],
+                        ur_kernel_handle_t Kernel, uint32_t LocalSize);
+
+bool hasExceededMaxRegistersPerBlock(ur_device_handle_t Device,
+                                     ur_kernel_handle_t Kernel,
+                                     size_t BlockSize);
+
+ur_result_t
+setKernelParams(const ur_context_handle_t Context,
+                const ur_device_handle_t Device, const uint32_t WorkDim,
+                const size_t *GlobalWorkOffset, const size_t *GlobalWorkSize,
+                const size_t *LocalWorkSize, ur_kernel_handle_t &Kernel,
+                CUfunction &CuFunc, size_t (&ThreadsPerBlock)[3],
+                size_t (&BlocksPerGrid)[3]);
