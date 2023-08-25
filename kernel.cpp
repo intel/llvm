4fb000460a1f0739a0821a71695e48032b45b96a
Petr Vesely <22935437+veselypeta@users.noreply.github.com>
2023-08-25 13:28:40 +0100

    [UR][CUDA][HIP] Fix Set Arg Local (#10710)
    In the CUDA/HIP adapters `urKernelSetArgValue` was being used to
    implement both `urKernelSetArgValue` & `urKernelSetArgLocal`. However,
    if the validation layer is enabled in UR then the path to set local arg
    is never taken since it includes a check that `pArgValue` is not null.
    
    This PR:
     * Implements `urKernelSetArgLocal` for CUDA/HIP adapters
    * Changes `pi2ur` to call `urKernelSetArgLocal` when `arg_value` is
    `nullptr`
    * Implements `urKernelSetArgLocal` for L0 adapter - this just calls back
    into `urKernelSetArgValue`.

--- kernel.cpp
+++ kernel.cpp
@@ -419,12 +419,11 @@ UR_APIEXPORT ur_result_t UR_APICALL urKernelSetArgLocal(
     const ur_kernel_arg_local_properties_t
         *Properties ///< [in][optional] argument properties
 ) {
-  std::ignore = Kernel;
-  std::ignore = ArgIndex;
   std::ignore = Properties;
-  std::ignore = ArgSize;
-  urPrint("[UR][L0] %s function not implemented!\n", __FUNCTION__);
-  return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
+
+  UR_CALL(urKernelSetArgValue(Kernel, ArgIndex, ArgSize, nullptr, nullptr));
+
+  return UR_RESULT_SUCCESS;
 }
 
 UR_APIEXPORT ur_result_t UR_APICALL urKernelGetInfo(
