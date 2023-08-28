072f5b3be5f33094a0c032ca2a3b9f09de30044d
Petr Vesely <22935437+veselypeta@users.noreply.github.com>
2023-08-28 14:28:41 +0100

    [UR][CUDA][HIP][L0] Cleanup licence header (#10824)
    Fix the license headers at the top of each source file in the unified
    runtime directory.
    
    ---------
    
    Co-authored-by: Alexey Bader <alexey.bader@intel.com>

--- context.cpp
+++ context.cpp
@@ -1,10 +1,10 @@
-//===--------- context.cpp - CUDA Adapter ----------------------------===//
+//===--------- context.cpp - CUDA Adapter ---------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-//===-----------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 
 #include "context.hpp"
 #include "usm.hpp"
