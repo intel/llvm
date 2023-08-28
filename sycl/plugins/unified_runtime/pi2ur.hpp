7fd3df7f1c7c235f577b6647a6792c5a35c3eb8d
Petr Vesely <22935437+veselypeta@users.noreply.github.com>
2023-08-28 14:28:41 +0100

    [UR][CUDA][HIP][L0] Cleanup licence header (#10824)
    Fix the license headers at the top of each source file in the unified
    runtime directory.
    
    ---------
    
    Co-authored-by: Alexey Bader <alexey.bader@intel.com>

--- sycl/plugins/unified_runtime/pi2ur.hpp
+++ sycl/plugins/unified_runtime/pi2ur.hpp
@@ -1,10 +1,10 @@
-//===---------------- pi2ur.hpp - PI API to UR API  --------------------==//
+//===---------------- pi2ur.hpp - PI API to UR API  ------------------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-//===------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 #pragma once
 
 #include "ur_api.h"
