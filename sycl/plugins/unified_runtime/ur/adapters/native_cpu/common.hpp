440c9d5ed39fe0eef112faa2e923292078bebb9e
Petr Vesely <22935437+veselypeta@users.noreply.github.com>
2023-08-28 14:28:41 +0100

    [UR][CUDA][HIP][L0] Cleanup licence header (#10824)
    Fix the license headers at the top of each source file in the unified
    runtime directory.
    
    ---------
    
    Co-authored-by: Alexey Bader <alexey.bader@intel.com>

--- sycl/plugins/unified_runtime/ur/adapters/native_cpu/common.hpp
+++ sycl/plugins/unified_runtime/ur/adapters/native_cpu/common.hpp
@@ -1,4 +1,4 @@
-//===----------- common.cpp - Native CPU Adapter ---------------------===//
+//===----------- common.hpp - Native CPU Adapter ---------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- sycl/plugins/unified_runtime/ur/adapters/native_cpu/usm_p2p.cpp
+++ sycl/plugins/unified_runtime/ur/adapters/native_cpu/usm_p2p.cpp
@@ -1,10 +1,10 @@
-//===--------- usm_p2p.cpp - Native CPU Adapter ------------------------===//
+//===--------- usm_p2p.cpp - Native CPU Adapter ---------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-//===-------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 
 #include "common.hpp"
 
