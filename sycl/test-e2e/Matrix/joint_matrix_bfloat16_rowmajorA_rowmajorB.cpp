2dccd7722f07df0fb3fd389c64628c33966771d1
YuriPlyakhin <yury.plyakhin@intel.com>
2023-08-28 12:01:34 -0700

    [SYCL][Joint Matrix] Add tests for sub-group size = 32 (#10666)
    Added tests for sub group size = 32 to SYCL Joint Matrix

--- sycl/test-e2e/Matrix/joint_matrix_bfloat16_rowmajorA_rowmajorB.cpp
+++ sycl/test-e2e/Matrix/joint_matrix_bfloat16_rowmajorA_rowmajorB.cpp
@@ -21,5 +21,6 @@ using namespace sycl::ext::oneapi::experimental::matrix;
 using bfloat16 = sycl::ext::oneapi::bfloat16;
 
 #define SG_SZ 16
+constexpr size_t TN = 16;
 
 #include "joint_matrix_bfloat16_rowmajorA_rowmajorB_impl.hpp"
