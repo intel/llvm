710e8b5f8af1d3698807e0716223ea2b9b156de5
Petr Vesely <22935437+veselypeta@users.noreply.github.com>
2023-08-28 14:28:41 +0100

    [UR][CUDA][HIP][L0] Cleanup licence header (#10824)
    Fix the license headers at the top of each source file in the unified
    runtime directory.
    
    ---------
    
    Co-authored-by: Alexey Bader <alexey.bader@intel.com>

--- kernel.cpp
+++ kernel.cpp
@@ -1,10 +1,10 @@
-//===--------- kernel.cpp - CUDA Adapter ---------------------------===//
+//===--------- kernel.cpp - CUDA Adapter ----------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-//===-----------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 
 #include "kernel.hpp"
 #include "memory.hpp"
