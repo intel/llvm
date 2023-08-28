7773e3d2f9e84b7d3422c5639098d5cada642feb
uwedolinsky <40493844+uwedolinsky@users.noreply.github.com>
2023-08-28 14:29:18 +0100

    [SYCL][NATIVECPU] more ur adapter implementations (#10972)
    Adding more UR adapter functionality to pass more tests related to
    buffers and subbuffers, and to enable `sycl-ls --verbose`.

--- sycl/plugins/unified_runtime/ur/adapters/native_cpu/memory.hpp
+++ sycl/plugins/unified_runtime/ur/adapters/native_cpu/memory.hpp
@@ -12,17 +12,23 @@
 #include <cstdlib>
 #include <cstring>
 
-struct ur_mem_handle_t_ {
-  ur_mem_handle_t_(size_t Size)
-      : _mem{static_cast<char *>(malloc(Size))}, _ownsMem{true}, _refCount{1} {}
+#include "common.hpp"
+#include "context.hpp"
 
-  ur_mem_handle_t_(void *HostPtr, size_t Size)
-      : _mem{static_cast<char *>(malloc(Size))}, _ownsMem{true}, _refCount{1} {
+struct ur_mem_handle_t_ : _ur_object {
+  ur_mem_handle_t_(size_t Size, bool _IsImage)
+      : _mem{static_cast<char *>(malloc(Size))}, _ownsMem{true},
+        IsImage{_IsImage} {}
+
+  ur_mem_handle_t_(void *HostPtr, size_t Size, bool _IsImage)
+      : _mem{static_cast<char *>(malloc(Size))}, _ownsMem{true},
+        IsImage{_IsImage} {
     memcpy(_mem, HostPtr, Size);
   }
 
-  ur_mem_handle_t_(void *HostPtr)
-      : _mem{static_cast<char *>(HostPtr)}, _ownsMem{false}, _refCount{1} {}
+  ur_mem_handle_t_(void *HostPtr, bool _IsImage)
+      : _mem{static_cast<char *>(HostPtr)}, _ownsMem{false}, IsImage{_IsImage} {
+  }
 
   ~ur_mem_handle_t_() {
     if (_ownsMem) {
@@ -32,7 +38,36 @@ struct ur_mem_handle_t_ {
 
   void decrementRefCount() noexcept { _refCount--; }
 
+  // Method to get type of the derived object (image or buffer)
+  bool isImage() const { return this->IsImage; }
+
   char *_mem;
   bool _ownsMem;
-  std::atomic_uint32_t _refCount;
+  std::atomic_uint32_t _refCount = {1};
+
+private:
+  const bool IsImage;
+};
+
+struct _ur_buffer final : ur_mem_handle_t_ {
+  // Buffer constructor
+  _ur_buffer(ur_context_handle_t /* Context*/, void *HostPtr)
+      : ur_mem_handle_t_(HostPtr, false) {}
+  _ur_buffer(ur_context_handle_t /* Context*/, void *HostPtr, size_t Size)
+      : ur_mem_handle_t_(HostPtr, Size, false) {}
+  _ur_buffer(ur_context_handle_t /* Context*/, size_t Size)
+      : ur_mem_handle_t_(Size, false) {}
+  _ur_buffer(_ur_buffer *b, size_t Offset, size_t Size)
+      : ur_mem_handle_t_(b->_mem + Offset, false), SubBuffer(b) {
+    SubBuffer.Origin = Offset;
+  }
+
+  bool isSubBuffer() const { return SubBuffer.Parent != nullptr; }
+
+  struct BB {
+    BB(_ur_buffer *b) : Parent(b) {}
+    BB() : BB(nullptr) {}
+    _ur_buffer *const Parent;
+    size_t Origin; // only valid if Parent != nullptr
+  } SubBuffer;
 };
