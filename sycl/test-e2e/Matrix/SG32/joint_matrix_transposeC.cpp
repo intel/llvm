cb5ae2c0232e6b2c0f08ec94e0130e51838b3a39
YuriPlyakhin <yury.plyakhin@intel.com>
2023-09-01 13:15:35 -0700

    [SYCL][Joint Matrix] Add missing tests for SG=32 (#11040)
    These tests were added for SG=16, but not for SG=32. Fixing that:
    joint_matrix_colA_rowB_colC.cpp,
    joint_matrix_down_convert.cpp,
    joint_matrix_out_bounds.cpp,
    joint_matrix_transposeC.cpp,
    joint_matrix_unaligned_k.cpp

--- sycl/test-e2e/Matrix/SG32/joint_matrix_transposeC.cpp
+++ sycl/test-e2e/Matrix/SG32/joint_matrix_transposeC.cpp
@@ -1,4 +1,4 @@
-//==-------- joint_matrix_down_convert.cpp  - DPC++ joint_matrix------------==//
+//==----------- joint_matrix_transposeC.cpp  - DPC++ joint_matrix-----------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -10,8 +10,11 @@
 // RUN: %{build} -o %t.out -DSYCL_EXT_ONEAPI_MATRIX_VERSION=4
 // RUN: %{run} %t.out
 
-#include "common.hpp"
+// XFAIL:*
 
-constexpr size_t SG_SZ = 16;
+#include "../common.hpp"
 
-#include "joint_matrix_down_convert_impl.hpp"
+constexpr size_t SG_SZ = 32;
+constexpr size_t TN = 16;
+
+#include "../joint_matrix_transposeC_impl.hpp"
