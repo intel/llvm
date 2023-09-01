bda9f68232afa8589c41770f62dc7920a8c3962f
Lu, John <john.lu@intel.com>
2023-09-01 08:32:59 -0700

    Test unbundling multiple targets
    Signed-off-by: Lu, John <john.lu@intel.com>

--- clang/test/Driver/clang-offload-bundler-bc-archive-support.cpp
+++ clang/test/Driver/clang-offload-bundler-bc-archive-support.cpp
@@ -82,6 +82,13 @@
 // RUN: cmp %t2.bc           `cat %t_list4.txt`
 // RUN: cmp %t3.bc           `cat %t_list5.txt`
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Test archive unbundling for multiple targets
+// RUN: clang-offload-bundler -unbundle -type=aoo -input=%t_bundled.a -targets=sycl-spir64-unknown-unknown,host-spir64_gen -output=%t_listA.txt -output=%t_listB.txt
+
+// RUN: cmp %t_unbundled_A.o `cat %t_listA.txt`
+// RUN: cmp %t2.bc           `cat %t_listB.txt`
+
 #include <sycl/sycl.hpp>
 #include <iostream>
 
