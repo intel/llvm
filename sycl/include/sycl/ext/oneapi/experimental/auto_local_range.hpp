e6e12aedf15c2bda0839fc5e57e97a3437c495e3
0x12CC <68250218+0x12CC@users.noreply.github.com>
2023-09-06 11:58:11 -0400

    Update sycl/include/sycl/ext/oneapi/experimental/auto_local_range.hpp
    Co-authored-by: John Pennycook <john.pennycook@intel.com>

--- sycl/include/sycl/ext/oneapi/experimental/auto_local_range.hpp
+++ sycl/include/sycl/ext/oneapi/experimental/auto_local_range.hpp
@@ -1,4 +1,4 @@
-//==--- auto_local_range.hpp --- SYCL extension for root groups ------------==//
+//==--- auto_local_range.hpp --- SYCL extension for auto range -------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
