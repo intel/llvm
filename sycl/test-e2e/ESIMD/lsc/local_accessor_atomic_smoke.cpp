bd2e6b5b876636a6447b5b221b1274bffad713a2
Gregory Fine <gregory.fine@intel.com>
2023-09-05 18:45:23 -0700

    Introduce driver version checks to enable the tests

--- sycl/test-e2e/ESIMD/lsc/local_accessor_atomic_smoke.cpp
+++ sycl/test-e2e/ESIMD/lsc/local_accessor_atomic_smoke.cpp
@@ -11,9 +11,6 @@
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 //
-// TODO: Enable the test when GPU driver is ready/fixed.
-// XFAIL: opencl || windows || gpu-intel-pvc
-// UNSUPPORTED: esimd_emulator
 
 #include "../esimd_test_utils.hpp"
 
@@ -626,6 +623,11 @@ int main(void) {
 
   auto dev = q.get_device();
   std::cout << "Running on " << dev.get_info<info::device::name>() << "\n";
+  if (!isGPUDriverGE(q, esimd_test::GPUDriverOS::LinuxAndWindows, "26690",
+                     "101.4576")) {
+    std::cout << "Skipped. The test requires GPU driver 1.3.26690 or newer.\n";
+    return 0;
+  }
 
   bool passed = true;
 #ifndef CMPXCHG_TEST
