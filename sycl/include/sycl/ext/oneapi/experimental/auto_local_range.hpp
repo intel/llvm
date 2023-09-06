e99c13df4b229dd939cb2cd5b04d7859dd0020ac
Michael Aziz <michael.aziz@intel.com>
2023-09-06 08:49:26 -0700

    [SYCL] Implement sycl_ext_oneapi_auto_local_range
    Add the `auto_range` definition specified in the
    sycl_ext_oneapi_auto_local_range extension. The runtime already uses
    `piKernelGetGroupInfo` to determine the local size when it's not
    specified, so no other changes are needed.
    
    Signed-off-by: Michael Aziz <michael.aziz@intel.com>

---
+++ sycl/include/sycl/ext/oneapi/experimental/auto_local_range.hpp
@@ -0,0 +1,31 @@
+//==--- auto_local_range.hpp --- SYCL extension for root groups ------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <sycl/range.hpp>
+
+#define SYCL_EXT_ONEAPI_AUTO_LOCAL_RANGE 1
+
+namespace sycl {
+namespace ext {
+namespace oneapi {
+namespace experimental {
+
+template <int Dimensions> static const inline range<Dimensions> auto_range;
+
+template <> static const inline range<3> auto_range<3>{0, 0, 0};
+
+template <> static const inline range<2> auto_range<2>{0, 0};
+
+template <> static const inline range<1> auto_range<1>{0};
+
+} // namespace experimental
+} // namespace oneapi
+} // namespace ext
+} // namespace sycl
