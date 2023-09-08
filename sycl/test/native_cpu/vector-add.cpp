b99304adab191279ad3ceb0f30dfb579355e779b
pietro.ghiglio <pietro.ghiglio@codeplay.com>
2023-09-08 13:54:56 +0100

    update tests

--- sycl/test/native_cpu/vector-add.cpp
+++ sycl/test/native_cpu/vector-add.cpp
@@ -1,6 +1,10 @@
 // REQUIRES: native_cpu_be
-// RUN: %clangxx -fsycl -fsycl-targets=native_cpu -g %s -o %t
+// RUN: %clangxx -fsycl -fsycl-targets=native_cpu %s -o %t
 // RUN: env ONEAPI_DEVICE_SELECTOR="native_cpu:cpu" %t
+
+// Same test but with -g 
+// RUN: %clangxx -fsycl -fsycl-targets=native_cpu %s -g -o %t-debug
+// RUN: env ONEAPI_DEVICE_SELECTOR="native_cpu:cpu" %t-debug
 
 #include <sycl/sycl.hpp>
 
