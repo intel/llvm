d4c793a575cba5441113e8d32b326520508c77b7
Jinsong Ji <jinsong.ji@intel.com>
2023-09-08 11:28:26 -0700

    Remove OPAQUEPOINTER_READY in TableGen.cmake

--- llvm/cmake/modules/TableGen.cmake
+++ llvm/cmake/modules/TableGen.cmake
@@ -4,8 +4,6 @@
 # Adds the name of the generated file to TABLEGEN_OUTPUT.
 include(LLVMDistributionSupport)
 
-list(APPEND LLVM_TABLEGEN_FLAGS "-DINTEL_SYCL_OPAQUEPOINTER_READY")
-
 function(tablegen project ofn)
   cmake_parse_arguments(ARG "" "" "DEPENDS;EXTRA_INCLUDES" ${ARGN})
 
