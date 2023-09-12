58953f08a91d3d0d5ffbb3f90ae57dc96aa18946
Artem Radzikhovskyy <artem.radzikhovskyy@intel.com>
2023-09-12 09:35:43 -0700

    Scope test

--- sycl/test/check_device_code/fpga_mem_scope.cpp
+++ sycl/test/check_device_code/fpga_mem_scope.cpp
@@ -8,11 +8,13 @@ using namespace sycl;
 namespace intel = sycl::ext::intel::experimental; // for fpga_mem
 namespace oneapi = sycl::ext::oneapi::experimental; // for properties
 
-// %[[fpga_mem:.*fpga_mem.*]] = type { %[[fpga_mem_base:.*fpga_mem_base.*]] }
-// %[[fpga_mem_base]] = type { [2 x i32] }
+// CHECK: %[[fpga_mem:.*fpga_mem.*]] = type { %[[fpga_mem_base:.*fpga_mem_base.*]] }
+// CHECK: %[[fpga_mem_base]] = type { [2 x i32] }
 
 constexpr intel::fpga_mem<int[2], decltype(oneapi::properties(oneapi::num_banks<4>))> global {9, 14};
 // CHECK: @{{.*}}global = internal addrspace(1) constant { [2 x i32] } { [2 x i32] [i32 9, i32 14] }, align 4, !spirv.Decorations ![[GlobalProps:[0-9]+]]
+
+// CHECK: @[[str:.*]] = private unnamed_addr addrspace(1) constant [27 x i8] c"{5826:\22DEFAULT\22}{5827:\222\22}\00"
 
 struct foo {
   int f;
@@ -24,25 +26,26 @@ int main() {
   int f = 0;
   foo b {2, 5.4f};
 
-  constexpr intel::fpga_mem<int[2], decltype(oneapi::properties(oneapi::num_banks<8>))> host {3, -9};
+  // FIXME: Artem
+  // constexpr intel::fpga_mem<int[2], decltype(oneapi::properties(oneapi::num_banks<8>))> host {3, -9};
 
   Q.single_task([=]() {
     constexpr intel::fpga_mem<int[2], decltype(oneapi::properties(oneapi::num_banks<2>))> kernel {7, -1298};
-  // %kernel.i = alloca %[[fpga_mem]], align 8
-  // %kernel.ascast.i = addrspacecast ptr %kernel.i to ptr addrspace(4)
-  // %6 = call dereferenceable(8) ptr addrspace(4) @llvm.ptr.annotation.p4.p1(ptr addrspace(4) %kernel.ascast.i, ptr addrspace(1) @.str.12, ptr addrspace(1) @.str.1, i32 35, ptr addrspace(1) null)
-  // %arrayidx9.i = getelementptr inbounds [2 x i32], ptr addrspace(4) %6, i64 0, i64 %idxprom.i
+  // CHECK: %[[kernel:kernel.*]] = alloca %[[fpga_mem]], align 8
+  // CHECK: %[[kernel_acast:.*]] = addrspacecast ptr %[[kernel]] to ptr addrspace(4)
+  // CHECK: %[[kernel_annot:.*]] = call dereferenceable(8) ptr addrspace(4) @llvm.ptr.annotation{{.*}}(ptr addrspace(4) %[[kernel_acast]], ptr addrspace(1) @[[str]]
+  // CHECK: getelementptr inbounds [2 x i32], ptr addrspace(4) %[[kernel_annot]]
     
     // Validate values of fpga_mem at various scopes
     static_assert(global[0] == 9);
     static_assert(global[1] == 14);
-    static_assert(host[0] == 3);
-    static_assert(host[1] == -9);
+    // static_assert(host[0] == 3); // FIXME: Artem
+    // static_assert(host[1] == -9); // FIXME: Artem
     static_assert(kernel[0] == 7);
     static_assert(kernel[1] == -1298);
 
 
-    volatile int ReadVal = global[f] + host[f] + kernel[f] + b.f;
+    volatile int ReadVal = global[f] /*+ host[f]*/ + kernel[f] + b.f;
   });
   return 0;
 }
