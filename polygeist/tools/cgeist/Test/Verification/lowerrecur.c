de564584fcc633369e6a2656f9ef8f963c079014
Lukas Sommer <lukas.sommer@codeplay.com>
2023-09-14 13:58:50 +0100

    Update test checks and actually file-check them
    Signed-off-by: Lukas Sommer <lukas.sommer@codeplay.com>

--- polygeist/tools/cgeist/Test/Verification/lowerrecur.c
+++ polygeist/tools/cgeist/Test/Verification/lowerrecur.c
@@ -1,9 +1,5 @@
 // RUN: cgeist %s --function=* -emit-llvm -S | FileCheck %s
 
-// TODO: This test case does not yet work with opaque pointers,
-// as it requires the Polygeist transformations to work correctly
-// with opaque pointers.
-
 struct X{
  double* a;
  double* b;
