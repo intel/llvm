1a8071f72c503fc8616b36eadb91003b0b7ace33
Yury Plyakhin <yury.plyakhin@intel.com>
2023-09-15 20:04:00 -0700

    updated SG_SZ

--- sycl/test-e2e/Matrix/SG32/joint_matrix_bfloat16.cpp
+++ sycl/test-e2e/Matrix/SG32/joint_matrix_bfloat16.cpp
@@ -17,7 +17,7 @@ using namespace sycl;
 using namespace sycl::ext::oneapi::experimental::matrix;
 using bfloat16 = sycl::ext::oneapi::bfloat16;
 
-constexpr size_t SG_SZ = 32;
+#define SG_SZ 32
 constexpr size_t TN = 16;
 
 #include "../joint_matrix_bfloat16_impl.hpp"
--- sycl/test-e2e/Matrix/joint_matrix_bfloat16.cpp
+++ sycl/test-e2e/Matrix/joint_matrix_bfloat16.cpp
@@ -15,7 +15,6 @@
 
 using namespace sycl::ext::oneapi::experimental::matrix;
 
-#define SG_SZ 16
 constexpr size_t TN = 16;
 
 #include "joint_matrix_bfloat16_impl.hpp"
