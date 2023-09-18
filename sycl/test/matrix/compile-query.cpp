7afef97391db8c8e43457b9d91626f0a1b93f8d5
Dounia <dounia.khaldi@intel.com>
2023-09-18 07:00:35 -0700

    Address Greg's comments and remove ATS from the test case because for names, dg2 is used

--- sycl/test/matrix/compile-query.cpp
+++ sycl/test/matrix/compile-query.cpp
@@ -39,52 +39,18 @@ void query_amx_spr() {
   return;
 }
 
-void query_xmx_dg() {
+void query_xmx_dg2() {
 
   // generates combination assert
-  // using myparams = matrix_params<architecture::intel_gpu_dg1, int, int, int,
-  // 2, 8, 32>;
+  // using myparams = matrix_params<architecture::intel_gpu_dg2_g10, int, int, int, 2, 8, 32>;
 
   // generate combination of type assert
-  // using myparams = matrix_params<architecture::intel_gpu_dg1, int, int, int>;
+  // using myparams = matrix_params<architecture::intel_gpu_dg2_g10, int, int, int>;
 
   // tells whether a combination is valid or not, if valid, those will be set as
   // default
-  using myparams = matrix_params<architecture::intel_gpu_dg1, int8_t, int8_t,
+  using myparams = matrix_params<architecture::intel_gpu_dg2_g10, int8_t, int8_t,
                                  int, int, 2, 8, 32>;
-
-  size_t dmsize = myparams::M;
-  size_t dnsize = myparams::N;
-  size_t dksize = myparams::K;
-  std::cout << "sizes of Intel XMX of architecture::intel_gpu_dg1 "
-               "matrix_params chosen by the user are: M "
-            << dmsize << " N " << dnsize << " K " << dksize << std::endl;
-
-  // sizes-only query: types are given, generate default sizes
-  using myparams2 =
-      matrix_params<architecture::intel_gpu_dg1, int8_t, int8_t, int>;
-  dmsize = myparams2::M;
-  dnsize = myparams2::N;
-  dksize = myparams2::K;
-  std::cout << "Default Intel XMX of architecture::intel_gpu_dg1 sizes  are: M "
-            << dmsize << " N " << dnsize << " K " << dksize << std::endl;
-  return;
-}
-
-void query_xmx_ats() {
-
-  // generates combination assert
-  // using myparams = matrix_params<architecture::intel_gpu_dg2_g10, int, int,
-  // int, // 2, 8, 32>;
-
-  // generate combination of type assert
-  // using myparams = matrix_params<architecture::intel_gpu_dg2_g10, int, int,
-  // int>;
-
-  // tells whether a combination is valid or not, if valid, those will be set as
-  // default
-  using myparams = matrix_params<architecture::intel_gpu_dg2_g10, int8_t,
-                                 int8_t, int, int, 2, 8, 32>;
 
   size_t dmsize = myparams::M;
   size_t dnsize = myparams::N;
@@ -99,9 +65,8 @@ void query_xmx_ats() {
   dmsize = myparams2::M;
   dnsize = myparams2::N;
   dksize = myparams2::K;
-  std::cout
-      << "Default Intel XMX of architecture::intel_gpu_dg2_g10 sizes  are: M "
-      << dmsize << " N " << dnsize << " K " << dksize << std::endl;
+  std::cout << "Default Intel XMX of architecture::intel_gpu_dg2_g10 sizes  are: M "
+            << dmsize << " N " << dnsize << " K " << dksize << std::endl;
   return;
 }
 
@@ -139,8 +104,7 @@ void query_xmx_pvc() {
 
 int main() {
   query_amx_spr();
-  query_xmx_dg();
-  query_xmx_ats();
+  query_xmx_dg2();
   query_xmx_pvc();
   return 0;
 }
