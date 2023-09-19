3be8f3d34bc8e606f12f088e470c7ce3a46e0ebe
jinge90 <ge.jin@intel.com>
2023-09-19 12:11:23 +0800

    add tests
    Signed-off-by: jinge90 <ge.jin@intel.com>

---
+++ sycl/test-e2e/DeviceLib/imf_fp32_rounding_test.cpp
@@ -0,0 +1,113 @@
+// INTEL_CUSTOMIZATION
+// RUN: %{build} -o %t.out
+// RUN: %{run} %t.out
+
+// RUN: %{build} -fno-builtin -fsycl-device-lib-jit-link -o %t.out
+// RUN: %{run} %t.out
+//
+// UNSUPPORTED: cuda || hip
+// end INTEL_CUSTOMIZATION
+
+#include "imf_utils.hpp"
+
+extern "C" {
+float __imf_fadd_rz(float, float);
+float __imf_fadd_rn(float, float);
+float __imf_fadd_ru(float, float);
+float __imf_fadd_rd(float, float);
+float __imf_fsub_rz(float, float);
+float __imf_fsub_rn(float, float);
+float __imf_fsub_ru(float, float);
+float __imf_fsub_rd(float, float);
+float __imf_fmul_rz(float, float);
+float __imf_fmul_rn(float, float);
+float __imf_fmul_ru(float, float);
+float __imf_fmul_rd(float, float);
+}
+
+int main(int, char **) {
+  sycl::queue device_queue(sycl::default_selector_v);
+
+  {
+    std::initializer_list<float> input_vals1 = {0x1p-1, 0x1.8bd054p+6,
+                                                0x1.fcd686p+0, -0x1.7f9abp+3};
+    std::initializer_list<float> input_vals2 = {-0x1.a8p+2, -0x1.674a3cp+5,
+                                                0x1.f3d6aep+10, 0x1.d6bf48p+10};
+    std::initializer_list<unsigned> ref_vals_rd = {0xc0c40000, 0x42582b36,
+                                                   0x44fa2af1, 0x44e9e009};
+    std::initializer_list<unsigned> ref_vals_rn = {0xc0c40000, 0x42582b36,
+                                                   0x44fa2af2, 0x44e9e009};
+    std::initializer_list<unsigned> ref_vals_ru = {0xc0c40000, 0x42582b36,
+                                                   0x44fa2af2, 0x44e9e00a};
+    std::initializer_list<unsigned> ref_vals_rz = {0xc0c40000, 0x42582b36,
+                                                   0x44fa2af1, 0x44e9e009};
+    test2(device_queue, input_vals1, input_vals2, ref_vals_rd,
+          F2T(unsigned, __imf_fadd_rd));
+    std::cout << "__imf_fadd_rd passes." << std::endl;
+    test2(device_queue, input_vals1, input_vals2, ref_vals_rn,
+          F2T(unsigned, __imf_fadd_rn));
+    std::cout << "__imf_fadd_rn passes." << std::endl;
+    test2(device_queue, input_vals1, input_vals2, ref_vals_ru,
+          F2T(unsigned, __imf_fadd_ru));
+    std::cout << "__imf_fadd_ru passes." << std::endl;
+    test2(device_queue, input_vals1, input_vals2, ref_vals_rz,
+          F2T(unsigned, __imf_fadd_rz));
+    std::cout << "__imf_fadd_rz passes." << std::endl;
+  }
+
+  {
+    std::initializer_list<float> input_vals1 = {0x1p-1, 0x1.8bd054p+6,
+                                                0x1.fcd686p+0, -0x1.7f9abp+3};
+    std::initializer_list<float> input_vals2 = {-0x1.a8p+2, -0x1.674a3cp+5,
+                                                0x1.f3d6aep+10, 0x1.d6bf48p+10};
+    std::initializer_list<unsigned> ref_vals_rd = {0x40e40000, 0x430fdd5c,
+                                                   0xc4f9abbd, 0xc4ecdf3f};
+    std::initializer_list<unsigned> ref_vals_rn = {0x40e40000, 0x430fdd5c,
+                                                   0xc4f9abbc, 0xc4ecdf3f};
+    std::initializer_list<unsigned> ref_vals_ru = {0x40e40000, 0x430fdd5d,
+                                                   0xc4f9abbc, 0xc4ecdf3e};
+    std::initializer_list<unsigned> ref_vals_rz = {0x40e40000, 0x430fdd5c,
+                                                   0xc4f9abbc, 0xc4ecdf3e};
+    test2(device_queue, input_vals1, input_vals2, ref_vals_rd,
+          F2T(unsigned, __imf_fsub_rd));
+    std::cout << "__imf_fsub_rd passes." << std::endl;
+    test2(device_queue, input_vals1, input_vals2, ref_vals_rn,
+          F2T(unsigned, __imf_fsub_rn));
+    std::cout << "__imf_fsub_rn passes." << std::endl;
+    test2(device_queue, input_vals1, input_vals2, ref_vals_ru,
+          F2T(unsigned, __imf_fsub_ru));
+    std::cout << "__imf_fsub_ru passes." << std::endl;
+    test2(device_queue, input_vals1, input_vals2, ref_vals_rz,
+          F2T(unsigned, __imf_fsub_rz));
+    std::cout << "__imf_fsub_rz passes." << std::endl;
+  }
+
+  {
+    std::initializer_list<float> input_vals1 = {0x1p-1, 0x1.8bd054p+6,
+                                                0x1.fcd686p+0, -0x1.7f9abp+3};
+    std::initializer_list<float> input_vals2 = {-0x1.a8p+2, -0x1.674a3cp+5,
+                                                0x1.f3d6aep+10, 0x1.d6bf48p+10};
+    std::initializer_list<unsigned> ref_vals_rd = {0xc0540000, 0xc58ae0fc,
+                                                   0x45786037, 0xc6b05928};
+    std::initializer_list<unsigned> ref_vals_rn = {0xc0540000, 0xc58ae0fb,
+                                                   0x45786037, 0xc6b05928};
+    std::initializer_list<unsigned> ref_vals_ru = {0xc0540000, 0xc58ae0fb,
+                                                   0x45786038, 0xc6b05927};
+    std::initializer_list<unsigned> ref_vals_rz = {0xc0540000, 0xc58ae0fb,
+                                                   0x45786037, 0xc6b05927};
+    test2(device_queue, input_vals1, input_vals2, ref_vals_rd,
+          F2T(unsigned, __imf_fmul_rd));
+    std::cout << "__imf_fmul_rd passes." << std::endl;
+    test2(device_queue, input_vals1, input_vals2, ref_vals_rn,
+          F2T(unsigned, __imf_fmul_rn));
+    std::cout << "__imf_fmul_rn passes." << std::endl;
+    test2(device_queue, input_vals1, input_vals2, ref_vals_ru,
+          F2T(unsigned, __imf_fmul_ru));
+    std::cout << "__imf_fmul_ru passes." << std::endl;
+    test2(device_queue, input_vals1, input_vals2, ref_vals_rz,
+          F2T(unsigned, __imf_fmul_rz));
+    std::cout << "__imf_fmul_rz passes." << std::endl;
+  }
+
+  return 0;
+}
---
+++ sycl/test-e2e/DeviceLib/imf_fp64_rounding_test.cpp
@@ -0,0 +1,131 @@
+// INTEL_CUSTOMIZATION
+// RUN: %{build} -o %t.out
+// RUN: %{run} %t.out
+
+// RUN: %{build} -fno-builtin -fsycl-device-lib-jit-link -o %t.out
+// RUN: %{run} %t.out
+//
+// UNSUPPORTED: cuda || hip
+// end INTEL_CUSTOMIZATION
+
+#include "imf_utils.hpp"
+
+extern "C" {
+double __imf_dadd_rz(double, double);
+double __imf_dadd_rn(double, double);
+double __imf_dadd_ru(double, double);
+double __imf_dadd_rd(double, double);
+double __imf_dsub_rz(double, double);
+double __imf_dsub_rn(double, double);
+double __imf_dsub_ru(double, double);
+double __imf_dsub_rd(double, double);
+double __imf_dmul_rz(double, double);
+double __imf_dmul_rn(double, double);
+double __imf_dmul_ru(double, double);
+double __imf_dmul_rd(double, double);
+}
+
+int main(int, char **) {
+  sycl::queue device_queue(sycl::default_selector_v);
+
+  {
+    std::initializer_list<double> input_vals1 = {
+        0x1.5ef3da7bf609ap+4, 0x1.fbd37afb0f8edp-1, 0x1.9238e38e38e35p+6,
+        0x1.7p+3};
+    std::initializer_list<double> input_vals2 = {
+        -0x1.bc7db6de6d33fp+9, 0x1.2f638fa4e71a6p+10, 0x1.08e38e38e38e3p+4,
+        -0x1.94p+3};
+    std::initializer_list<unsigned long long> ref_vals_rd = {
+        0xc08b186180a8d83b, 0x4092fa30a14467c5, 0x405d471c71c71c6d,
+        0xbff2000000000000};
+    std::initializer_list<unsigned long long> ref_vals_rn = {
+        0xc08b186180a8d83a, 0x4092fa30a14467c5, 0x405d471c71c71c6e,
+        0xbff2000000000000};
+    std::initializer_list<unsigned long long> ref_vals_ru = {
+        0xc08b186180a8d83a, 0x4092fa30a14467c6, 0x405d471c71c71c6e,
+        0xbff2000000000000};
+    std::initializer_list<unsigned long long> ref_vals_rz = {
+        0xc08b186180a8d83a, 0x4092fa30a14467c5, 0x405d471c71c71c6d,
+        0xbff2000000000000};
+    test2(device_queue, input_vals1, input_vals2, ref_vals_rd,
+          F2T(unsigned long long, __imf_dadd_rd));
+    std::cout << "__imf_dadd_rd passes." << std::endl;
+    test2(device_queue, input_vals1, input_vals2, ref_vals_rn,
+          F2T(unsigned long long, __imf_dadd_rn));
+    std::cout << "__imf_dadd_rn passes." << std::endl;
+    test2(device_queue, input_vals1, input_vals2, ref_vals_ru,
+          F2T(unsigned long long, __imf_dadd_ru));
+    std::cout << "__imf_dadd_ru passes." << std::endl;
+    test2(device_queue, input_vals1, input_vals2, ref_vals_rz,
+          F2T(unsigned long long, __imf_dadd_rz));
+    std::cout << "__imf_dadd_rz passes." << std::endl;
+  }
+
+  {
+    std::initializer_list<double> input_vals1 = {
+        0x1.5ef3da7bf609ap+4, 0x1.fbd37afb0f8edp-1, 0x1.9238e38e38e35p+6,
+        0x1.7p+3};
+    std::initializer_list<double> input_vals2 = {
+        -0x1.bc7db6de6d33fp+9, 0x1.2f638fa4e71a6p+10, 0x1.08e38e38e38e3p+4,
+        -0x1.94p+3};
+    std::initializer_list<unsigned long long> ref_vals_rd = {
+        0x408c77555b24ce43, 0xc092f24153587b87, 0x4054fffffffffffc,
+        0x4038200000000000};
+    std::initializer_list<unsigned long long> ref_vals_rn = {
+        0x408c77555b24ce44, 0xc092f24153587b87, 0x4054fffffffffffc,
+        0x4038200000000000};
+    std::initializer_list<unsigned long long> ref_vals_ru = {
+        0x408c77555b24ce44, 0xc092f24153587b86, 0x4054fffffffffffd,
+        0x4038200000000000};
+    std::initializer_list<unsigned long long> ref_vals_rz = {
+        0x408c77555b24ce43, 0xc092f24153587b86, 0x4054fffffffffffc,
+        0x4038200000000000};
+    test2(device_queue, input_vals1, input_vals2, ref_vals_rd,
+          F2T(unsigned long long, __imf_dsub_rd));
+    std::cout << "__imf_dsub_rd passes." << std::endl;
+    test2(device_queue, input_vals1, input_vals2, ref_vals_rn,
+          F2T(unsigned long long, __imf_dsub_rn));
+    std::cout << "__imf_dsub_rn passes." << std::endl;
+    test2(device_queue, input_vals1, input_vals2, ref_vals_ru,
+          F2T(unsigned long long, __imf_dsub_ru));
+    std::cout << "__imf_dsub_ru passes." << std::endl;
+    test2(device_queue, input_vals1, input_vals2, ref_vals_rz,
+          F2T(unsigned long long, __imf_dsub_rz));
+    std::cout << "__imf_dsub_rz passes." << std::endl;
+  }
+
+  {
+    std::initializer_list<double> input_vals1 = {
+        0x1.5ef3da7bf609ap+4, 0x1.fbd37afb0f8edp-1, 0x1.9238e38e38e35p+6,
+        0x1.7p+3};
+    std::initializer_list<double> input_vals2 = {
+        -0x1.bc7db6de6d33fp+9, 0x1.2f638fa4e71a6p+10, 0x1.08e38e38e38e3p+4,
+        -0x1.94p+3};
+    std::initializer_list<unsigned long long> ref_vals_rd = {
+        0xc0d30ada3597be04, 0x4092cea6724fb0f1, 0x409a030329161f95,
+        0xc062260000000000};
+    std::initializer_list<unsigned long long> ref_vals_rn = {
+        0xc0d30ada3597be04, 0x4092cea6724fb0f2, 0x409a030329161f96,
+        0xc062260000000000};
+    std::initializer_list<unsigned long long> ref_vals_ru = {
+        0xc0d30ada3597be03, 0x4092cea6724fb0f2, 0x409a030329161f96,
+        0xc062260000000000};
+    std::initializer_list<unsigned long long> ref_vals_rz = {
+        0xc0d30ada3597be03, 0x4092cea6724fb0f1, 0x409a030329161f95,
+        0xc062260000000000};
+    test2(device_queue, input_vals1, input_vals2, ref_vals_rd,
+          F2T(unsigned long long, __imf_dmul_rd));
+    std::cout << "__imf_dmul_rd passes." << std::endl;
+    test2(device_queue, input_vals1, input_vals2, ref_vals_rn,
+          F2T(unsigned long long, __imf_dmul_rn));
+    std::cout << "__imf_dmul_rn passes." << std::endl;
+    test2(device_queue, input_vals1, input_vals2, ref_vals_ru,
+          F2T(unsigned long long, __imf_dmul_ru));
+    std::cout << "__imf_dmul_ru passes." << std::endl;
+    test2(device_queue, input_vals1, input_vals2, ref_vals_rz,
+          F2T(unsigned long long, __imf_dmul_rz));
+    std::cout << "__imf_dmul_rz passes." << std::endl;
+  }
+
+  return 0;
+}
