1ff102d60c261aba8e153531c2e7cebc79d58592
Maxime France-Pillois <maxime.francepillois@codeplay.com>
2023-09-20 11:21:50 +0100

    [SYCL][Graph] Throw an exception if multiple concurrent submissions (#321)
    * [SYCL][Graph] Throw an exception if the graph is resubmitted while the previous submission has not completed.
    
    Adds a check for previous submission completion before submitting a graph to backend.
    Throws an invalid exception if the previous submission has not completed.
    Adds a test to verify the exception throwing.
    Updates e2e tests to conform to this behaviour (required by the specification).
    
    * [SYCL][Graph] Add missing test update and change function name

--- sycl/test-e2e/Graph/Inputs/basic_buffer.cpp
+++ sycl/test-e2e/Graph/Inputs/basic_buffer.cpp
@@ -35,14 +35,13 @@ int main() {
 
       auto GraphExec = Graph.finalize();
 
-      event Event;
-      for (unsigned n = 0; n < Iterations; n++) {
-        Event = Queue.submit([&](handler &CGH) {
-          CGH.depends_on(Event);
-          CGH.ext_oneapi_graph(GraphExec);
-        });
-      }
-      Queue.wait_and_throw();
+    event Event;
+    for (unsigned n = 0; n < Iterations; n++) {
+      Event = Queue.submit([&](handler &CGH) {
+        CGH.depends_on(Event);
+        CGH.ext_oneapi_graph(GraphExec);
+      });
+      Event.wait();
     }
 
     host_accessor HostAccA(BufferA);
