fe57bece1a200275d22434776dd0bcdd6d551d80
Dounia <dounia.khaldi@intel.com>
2023-09-19 18:24:41 -0700

    [SYCL][Matrix tests] Fix the performance test so it compiles on both CPU and GPU

--- sycl/test-e2e/Matrix/joint_matrix_bf16_fill_k_cache_unroll_init.cpp
+++ sycl/test-e2e/Matrix/joint_matrix_bf16_fill_k_cache_unroll_init.cpp
@@ -7,8 +7,11 @@
 //===----------------------------------------------------------------------===//
 // REQUIRES: matrix
 
-// RUN: %{build} -mllvm -inline-threshold=2000 -o %t.out -DSYCL_EXT_ONEAPI_MATRIX_VERSION=4 -DINIT_LIST -DMANUAL_UNROLL
-// RUN: %{run} %t.out
+// RUN: %if gpu %{ %{build} -mllvm -inline-threshold=2000 -o %t_gpu.out -DSYCL_EXT_ONEAPI_MATRIX_VERSION=4 -DINIT_LIST -DMANUAL_UNROLL %}
+// RUN: %if gpu %{ %{run} %t_gpu.out %}
+
+// RUN: %if cpu %{ %{build} -mllvm -inline-threshold=2000 -o %t_cpu.out -DSYCL_EXT_ONEAPI_MATRIX_VERSION=4 -DINIT_LIST -DMANUAL_UNROLL -DtM=16 -DtK=32 -DNCACHE1=32 -DKCACHE1=32
+// RUN: %if cpu %{ %{run} %t_cpu.out %}
 
 // -mllvm -inline-threshold=2000 added as a workaround,
 // since IGC doesn't support some variants of IR for Joint Matrix currently
