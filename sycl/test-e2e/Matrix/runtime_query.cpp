830dea408f94404f48f55c815955eb5368249675
Dmitry Vodoypanov <dmitry.vodopyanov@intel.com>
2023-09-21 06:26:13 -0700

    Apply missing clang-format

--- sycl/test-e2e/Matrix/runtime_query.cpp
+++ sycl/test-e2e/Matrix/runtime_query.cpp
@@ -6,12 +6,12 @@
 
 using namespace sycl::ext::oneapi::experimental::matrix;
 
-bool compare(const combination& lhs, const combination& rhs) {
-    return (lhs.max_msize == rhs.max_msize && lhs.max_nsize == rhs.max_nsize &&
-            lhs.max_ksize == rhs.max_ksize && lhs.msize == rhs.msize &&
-            lhs.nsize == rhs.nsize && lhs.ksize == rhs.ksize &&
-            lhs.atype == rhs.atype && lhs.btype == rhs.btype &&
-            lhs.ctype == rhs.ctype && lhs.dtype == rhs.dtype);
+bool compare(const combination &lhs, const combination &rhs) {
+  return (lhs.max_msize == rhs.max_msize && lhs.max_nsize == rhs.max_nsize &&
+          lhs.max_ksize == rhs.max_ksize && lhs.msize == rhs.msize &&
+          lhs.nsize == rhs.nsize && lhs.ksize == rhs.ksize &&
+          lhs.atype == rhs.atype && lhs.btype == rhs.btype &&
+          lhs.ctype == rhs.ctype && lhs.dtype == rhs.dtype);
 }
 
 int main() {
@@ -30,7 +30,7 @@ int main() {
        matrix_type::fp32, matrix_type::fp32},
   };
 
-    std::vector<combination> aaa_combinations = {
+  std::vector<combination> aaa_combinations = {
       {8, 0, 0, 0, 16, 32, matrix_type::uint8, matrix_type::uint8,
        matrix_type::sint32, matrix_type::sint32},
       {8, 0, 0, 0, 16, 32, matrix_type::uint8, matrix_type::sint8,
@@ -51,10 +51,12 @@ int main() {
           .get_info<sycl::ext::oneapi::experimental::info::device::
                         matrix_combinations>();
 
-  assert(actual_combinations.size() == expected_combinations.size() && "Number of combinations is not equal.");
+  assert(actual_combinations.size() == expected_combinations.size() &&
+         "Number of combinations is not equal.");
 
   for (int i = 0; i < actual_combinations.size(); i++) {
-    assert(compare(actual_combinations[i], expected_combinations[i]) && "Some values in matrix runtime query for PVC are not expected.");
+    assert(compare(actual_combinations[i], expected_combinations[i]) &&
+           "Some values in matrix runtime query for PVC are not expected.");
   }
 
   return 0;
