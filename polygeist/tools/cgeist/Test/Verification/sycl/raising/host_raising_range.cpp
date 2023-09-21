a392a93c3a82bd1bb5fc0438da80de7d20ed72e3
Tsang, Whitney <whitney.tsang@intel.com>
2023-09-21 14:19:58 -0700

    [SYCL-MLIR] Fix merge
    Signed-off-by: Tsang, Whitney <whitney.tsang@intel.com>

--- polygeist/tools/cgeist/Test/Verification/sycl/raising/host_raising_range.cpp
+++ polygeist/tools/cgeist/Test/Verification/sycl/raising/host_raising_range.cpp
@@ -14,7 +14,7 @@ class KernelName;
 // CHECK-DAG: gpu.func @_ZTS10KernelName
 // CHECK-DAG: gpu.func @_ZTSN4sycl3_V16detail19__pf_kernel_wrapperI10KernelNameEE
 
-// COM: Check we can detect buffers contruction
+// COM: Check we can detect buffers construction
 
 // CHECK-LABEL: llvm.func local_unnamed_addr @main
 // CHECK:         sycl.host.constructor({{.*}}) {type = !sycl.buffer<[1, !llvm.void]>}  : (!llvm.ptr, !llvm.ptr, i64, !llvm.ptr, !llvm.ptr) -> ()
@@ -24,7 +24,6 @@ class KernelName;
 // COM: Check we can detect accessors construction
 
 // CHECK-LABEL: llvm.func internal @_ZNSt17_Function_handlerIFvRN4sycl3_V17handlerEEZ4mainEUlS3_E_E9_M_invokeERKSt9_Any_dataS3_
-// CHECK:          %[[N:.*]] = llvm.mlir.constant(1024 : i64) : i64
 // CHECK:          sycl.host.constructor({{.*}}) {type = !sycl_accessor_1_21llvm2Evoid_r_gb} : (!llvm.ptr, !llvm.ptr, !llvm.ptr, !llvm.ptr, !llvm.ptr) -> ()
 // CHECK:          sycl.host.constructor({{.*}}) {type = !sycl_accessor_1_21llvm2Evoid_r_gb} : (!llvm.ptr, !llvm.ptr, !llvm.ptr, !llvm.ptr, !llvm.ptr) -> ()
 // CHECK:          sycl.host.constructor({{.*}}) {type = !sycl_accessor_1_21llvm2Evoid_w_gb} : (!llvm.ptr, !llvm.ptr, !llvm.ptr, !llvm.ptr, !llvm.ptr) -> ()
