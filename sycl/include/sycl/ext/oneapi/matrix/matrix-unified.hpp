95df3b18379e8993958bc8b6b800e345ae9099d3
Bing1 Yu <bing1.yu@intel.com>
2023-09-22 16:40:38 +0800

    lint

--- sycl/include/sycl/ext/oneapi/matrix/matrix-unified.hpp
+++ sycl/include/sycl/ext/oneapi/matrix/matrix-unified.hpp
@@ -40,7 +40,8 @@ struct joint_matrix {
 
 #if defined(__SYCL_DEVICE_ONLY__)
 #if defined(__NVPTX__)
-  mutable sycl::ext::oneapi::detail::joint_matrix_cuda<T, Use, Rows, Cols, Layout>
+  mutable sycl::ext::oneapi::detail::joint_matrix_cuda<T, Use, Rows, Cols,
+                                                       Layout>
       cuda_impl;
 #elif defined(__SPIR__)
   __spv::__spirv_JointMatrixINTEL<
