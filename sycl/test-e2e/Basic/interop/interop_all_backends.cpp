aec00f0ef61f2d383e997379cf90f82bb67c481d
JackAKirk <jack.kirk@codeplay.com>
2023-09-25 06:49:44 -0400

    Added test for queue, device, event interop.
    All backends currently support these three types of interop, but a
    complete e2e test did not previously exist.
    
    Signed-off-by: JackAKirk <jack.kirk@codeplay.com>

---
+++ sycl/test-e2e/Basic/interop/interop_all_backends.cpp
@@ -0,0 +1,62 @@
+// RUN: %{build} %if ext_oneapi_hip %{ -DSYCL_EXT_ONEAPI_BACKEND_HIP %} %elif ext_oneapi_cuda %{ -DSYCL_EXT_ONEAPI_BACKEND_CUDA_EXPERIMENTAL %} %elif ext_oneapi_level_zero %{ -DSYCL_EXT_ONEAPI_BACKEND_L0 %} -o test.out RUN:
+// %{run} test.out
+
+#include <sycl/sycl.hpp>
+using namespace sycl;
+
+#ifdef SYCL_EXT_ONEAPI_BACKEND_CUDA_EXPERIMENTAL
+#include <sycl/ext/oneapi/experimental/backend/cuda.hpp>
+constexpr auto BACKEND = backend::ext_oneapi_cuda;
+#elif defined(SYCL_EXT_ONEAPI_BACKEND_HIP)
+#include <sycl/ext/oneapi/backend/hip.hpp>
+constexpr auto BACKEND = backend::ext_oneapi_hip;
+#elif defined(SYCL_EXT_ONEAPI_BACKEND_L0)
+constexpr auto BACKEND = backend::ext_oneapi_level_zero;
+#else
+constexpr auto BACKEND = backend::opencl;
+#endif
+
+constexpr int N = 100;
+constexpr int VAL = 3;
+
+int main() {
+
+  device Device;
+  auto NativeDevice = get_native<BACKEND>(Device);
+  auto IDevice = make_device<BACKEND>(NativeDevice);
+
+  context Context(IDevice);
+
+  // Create queue with device created from a native device.
+  queue Queue(IDevice, {sycl::property::queue::in_order()});
+  auto NativeQueue = get_native<BACKEND>(Queue);
+  auto IQueue = make_queue<BACKEND>(NativeQueue, Context);
+
+  auto A = (int *)malloc_device(N * sizeof(int), IQueue);
+  std::vector<int> vec(N, 0);
+
+  auto Event = Queue.submit([&](handler &h) {
+    h.parallel_for<class kern1>(range<1>(N),
+                                [=](id<1> item) { A[item] = VAL; });
+  });
+
+  auto NativeEvent = get_native<BACKEND>(Event);
+  event IEvent = make_event<BACKEND>(NativeEvent, Context);
+
+  // depends_on event created from a native event.
+  auto Event2 = IQueue.submit([&](handler &h) {
+    h.depends_on(IEvent);
+    h.parallel_for<class kern2>(range<1>(N), [=](id<1> item) { A[item]++; });
+  });
+
+  auto Event3 = IQueue.memcpy(&vec[0], A, N * sizeof(int), Event2);
+  Event3.wait();
+
+  free(A, IQueue);
+
+  for (const auto &val : vec) {
+    assert(val == VAL + 1);
+  }
+
+  return 0;
+}
