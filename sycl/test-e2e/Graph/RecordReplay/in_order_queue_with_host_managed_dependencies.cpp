b365b2a5211b493f9e36cc74ecabc5862520c2d9
Maxime France-Pillois <maxime.francepillois@codeplay.com>
2023-09-26 14:19:55 +0100

    [SYCL][Graph] Fix bug when host-task is submitted to in-order queue (#322)
    * [SYCL][Graph] Fix bug when host-task is submitted to in-order queue
    
    When a host-task is submitted to in-order queue, dependency between this host-task and the successor is explicitly handled.
    However, when we record an in-order queue, the recorded CG are not part of the regular in-order queue execution sequence.
    But inter-CG dependancies are managed by the graph implementation.
    This PR implements this point and ensures that recording an in-order does not impact the normal execution sequence.
    Tests (e2e and unitest) have been added to check it.
    
     Adds and Renames tests (e2e and unitests)

---
+++ sycl/test-e2e/Graph/RecordReplay/in_order_queue_with_host_managed_dependencies.cpp
@@ -0,0 +1,65 @@
+// REQUIRES: cuda || level_zero, gpu
+// RUN: %{build} -o %t.out
+// RUN: %{run} %t.out
+
+// Tests submitting an host kernel to an in-order queue before recording
+// commands from it.
+
+#include "../graph_common.hpp"
+
+int main() {
+  using T = int;
+
+  queue Queue{{sycl::ext::intel::property::queue::no_immediate_command_list{},
+               sycl::property::queue::in_order{}}};
+
+  // Check if device has usm shared allocation
+  if (!Queue.get_device().has(sycl::aspect::usm_shared_allocations))
+    return 0;
+
+  T *TestData = sycl::malloc_shared<T>(Size, Queue);
+
+  ext::oneapi::experimental::command_graph Graph{Queue.get_context(),
+                                                 Queue.get_device()};
+
+  Queue.submit([&](handler &CGH) {
+    CGH.host_task([=]() {
+      for (size_t i = 0; i < Size; i++) {
+        TestData[i] = static_cast<T>(i);
+      }
+    });
+  });
+
+  Graph.begin_recording(Queue);
+
+  auto GraphEvent = Queue.submit([&](handler &CGH) {
+    CGH.single_task<class TestKernel2>([=]() {
+      for (size_t i = 0; i < Size; i++) {
+        TestData[i] += static_cast<T>(i);
+      }
+    });
+  });
+
+  Graph.end_recording(Queue);
+
+  auto GraphExec = Graph.finalize();
+  Queue.submit([&](handler &CGH) { CGH.ext_oneapi_graph(GraphExec); });
+
+  Queue.submit([&](handler &CGH) {
+    CGH.single_task<class TestKernel3>([=]() {
+      for (size_t i = 0; i < Size; i++) {
+        TestData[i] *= static_cast<T>(i);
+      }
+    });
+  });
+
+  Queue.wait_and_throw();
+
+  for (size_t i = 0; i < Size; i++) {
+    assert(TestData[i] == ((i + i) * i));
+  }
+
+  sycl::free(TestData, Queue);
+
+  return 0;
+}
