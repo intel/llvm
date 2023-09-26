643270af11df069517642dfb524a303b3a475ef8
JackAKirk <jack.kirk@codeplay.com>
2023-09-26 08:40:12 -0400

    Switch naming to Interop*.
    Also tried to fix llvm testing instructions.
    
    Signed-off-by: JackAKirk <jack.kirk@codeplay.com>

--- sycl/test-e2e/Basic/interop/interop_all_backends.cpp
+++ sycl/test-e2e/Basic/interop/interop_all_backends.cpp
@@ -1,4 +1,4 @@
-// RUN: %{build} %if ext_oneapi_hip %{ -DSYCL_EXT_ONEAPI_BACKEND_HIP %} %elif ext_oneapi_cuda %{ -DSYCL_EXT_ONEAPI_BACKEND_CUDA_EXPERIMENTAL %} %elif ext_oneapi_level_zero %{ -DSYCL_EXT_ONEAPI_BACKEND_L0 %} -o test.out
+// RUN: %{build} %if ext_oneapi_hip %{ -DSYCL_EXT_ONEAPI_BACKEND_HIP %} %else %if ext_oneapi_cuda %{ -DSYCL_EXT_ONEAPI_BACKEND_CUDA_EXPERIMENTAL %} %else %if ext_oneapi_level_zero %{ -DSYCL_EXT_ONEAPI_BACKEND_L0 %} -o test.out
 // RUN: %{run} test.out
 
 #include <sycl/sycl.hpp>
@@ -23,16 +23,17 @@ int main() {
 
   device Device;
   auto NativeDevice = get_native<BACKEND>(Device);
-  auto IDevice = make_device<BACKEND>(NativeDevice);
+  // Create sycl device with a native device.
+  auto InteropDevice = make_device<BACKEND>(NativeDevice);
 
-  context Context(IDevice);
+  context Context(InteropDevice);
 
-  // Create queue with device created from a native device.
-  queue Queue(IDevice, {sycl::property::queue::in_order()});
+  // Create sycl queue with device created from a native device.
+  queue Queue(InteropDevice, {sycl::property::queue::in_order()});
   auto NativeQueue = get_native<BACKEND>(Queue);
-  auto IQueue = make_queue<BACKEND>(NativeQueue, Context);
+  auto InteropQueue = make_queue<BACKEND>(NativeQueue, Context);
 
-  auto A = (int *)malloc_device(N * sizeof(int), IQueue);
+  auto A = (int *)malloc_device(N * sizeof(int), InteropQueue);
   std::vector<int> vec(N, 0);
 
   auto Event = Queue.submit([&](handler &h) {
@@ -41,18 +42,19 @@ int main() {
   });
 
   auto NativeEvent = get_native<BACKEND>(Event);
-  event IEvent = make_event<BACKEND>(NativeEvent, Context);
+  // Create sycl event with a native event.
+  event InteropEvent = make_event<BACKEND>(NativeEvent, Context);
 
-  // depends_on event created from a native event.
-  auto Event2 = IQueue.submit([&](handler &h) {
-    h.depends_on(IEvent);
+  // depends_on sycl event created from a native event.
+  auto Event2 = InteropQueue.submit([&](handler &h) {
+    h.depends_on(InteropEvent);
     h.parallel_for<class kern2>(range<1>(N), [=](id<1> item) { A[item]++; });
   });
 
-  auto Event3 = IQueue.memcpy(&vec[0], A, N * sizeof(int), Event2);
+  auto Event3 = InteropQueue.memcpy(&vec[0], A, N * sizeof(int), Event2);
   Event3.wait();
 
-  free(A, IQueue);
+  free(A, InteropQueue);
 
   for (const auto &val : vec) {
     assert(val == VAL + 1);
