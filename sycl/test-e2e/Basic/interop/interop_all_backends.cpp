08c8bc2a97d82ab3959ba9250d86d5158ac76740
JackAKirk <chezjakirk@gmail.com>
2023-09-29 14:21:54 +0100

    Try interop e2e test on L0 and opencl CI.
    Signed-off-by: JackAKirk <jack.kirk@codeplay.com>

--- sycl/test-e2e/Basic/interop/interop_all_backends.cpp
+++ sycl/test-e2e/Basic/interop/interop_all_backends.cpp
@@ -1,4 +1,3 @@
-// REQUIRES: CUDA || HIP
 // RUN: %{build} %if hip %{ -DSYCL_EXT_ONEAPI_BACKEND_HIP %} %else %{ %if cuda %{ -DSYCL_EXT_ONEAPI_BACKEND_CUDA_EXPERIMENTAL %} %else %{ %if level_zero %{ -DSYCL_EXT_ONEAPI_BACKEND_L0 %} %} %} -o %t.out
 
 #include <sycl/sycl.hpp>
