d46b7b7fddf752f49da1ffc6ee45c0c8531dd4db
Roland Schulz <roland.schulz@intel.com>
2023-09-30 18:30:00 -0700

    Fix test

--- sycl/include/sycl/detail/type_list.hpp
+++ sycl/include/sycl/detail/type_list.hpp
@@ -27,7 +27,7 @@ using is_empty_type_list = std::is_same<T, empty_type_list>;
 
 // is_contained
 template <typename T, typename TypeList>
-using is_contained = boost::mp11::mp_contains<TypeList, T>;
+using is_contained = boost::mp11::mp_contains<TypeList, std::remove_cv_t<T>>;
 // unclear whether TypeList can contain duplicates. If so mp_set_contains can be
 // used.
 
