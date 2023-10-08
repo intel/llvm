60806adf13e6353f139c16e47d788331f8c7ae13
Roland Schulz <roland.schulz@intel.com>
2023-10-07 18:57:14 -0700

    fix test

--- sycl/test/basic_tests/no_iostream_include.cpp
+++ sycl/test/basic_tests/no_iostream_include.cpp
@@ -5,8 +5,8 @@
 
 void t(std::istream &i, std::ostream &o) {
   char c;
-  i >> c; // expected-error {{invalid operands to binary expression}}
-  o << o; // expected-error {{invalid operands to binary expression}}
-  std::cout
-      << "\n"; // expected-error {{no member named 'cout' in namespace 'std'}}
+  i >> c;   // expected-error {{invalid operands to binary expression}}
+  o << o;   // expected-error {{invalid operands to binary expression}}
+  std::cout // expected-error {{no member named 'cout' in namespace 'std'}}
+      << "\n";
 }
