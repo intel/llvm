1ac3520c462e595ac63495b08399a45dffcf27d8
Victor Lomuller <victor@codeplay.com>
2023-10-09 20:22:59 +0100

    Make clang driver test work when libdevice is not built

--- clang/test/Driver/sycl-embed-ir.cpp
+++ clang/test/Driver/sycl-embed-ir.cpp
@@ -6,9 +6,7 @@
 // RUN: FileCheck -check-prefix=CHECK-NV %s
 
 // CHECK-NV: [[IR:[0-9]+]]: compiler, {4}, ir, (device-sycl, sm_80)
-// CHECK-NV: [[LINK1:[0-9]+]]: linker, {[[IR]]}, ir, (device-sycl, sm_80)
-// CHECK-NV: [[LINK2:[0-9]+]]: linker, {[[LINK1]], {{.*}}}, ir, (device-sycl, sm_80)
-// CHECK-NV: [[POSTLINK:[0-9]+]]: sycl-post-link, {[[LINK2]]}, ir, (device-sycl, sm_80)
+// CHECK-NV: [[POSTLINK:[0-9]+]]: sycl-post-link, {{{.*}}}, ir, (device-sycl, sm_80)
 // CHECK-NV: [[WRAP:[0-9]+]]: clang-offload-wrapper, {[[POSTLINK]]}, object, (device-sycl, sm_80)
 // CHECK-NV: offload, "host-sycl (x86_64-unknown-linux-gnu)" {{{.*}}}, "device-sycl (nvptx64-nvidia-cuda:sm_80)" {[[WRAP]]}, "device-sycl (nvptx64-nvidia-cuda:sm_80)" {{{.*}}}, image
 
@@ -16,7 +14,6 @@
 // RUN: FileCheck -check-prefix=CHECK-AMD %s
 
 // CHECK-AMD: [[IR:[0-9]+]]: compiler, {4}, ir, (device-sycl, gfx1010)
-// CHECK-AMD: [[LINK1:[0-9]+]]: linker, {[[IR]]}, ir, (device-sycl, gfx1010)
-// CHECK-AMD: [[POSTLINK:[0-9]+]]: sycl-post-link, {[[LINK1]]}, ir, (device-sycl, gfx1010)
+// CHECK-AMD: [[POSTLINK:[0-9]+]]: sycl-post-link, {{{.*}}}, ir, (device-sycl, gfx1010)
 // CHECK-AMD: [[WRAP:[0-9]+]]: clang-offload-wrapper, {[[POSTLINK]]}, object, (device-sycl, gfx1010)
 // CHECK-AMD: offload, "host-sycl (x86_64-unknown-linux-gnu)" {{{.*}}}, "device-sycl (amdgcn-amd-amdhsa:gfx1010)" {[[WRAP]]}, "device-sycl (amdgcn-amd-amdhsa:gfx1010)" {{{.*}}}, image
