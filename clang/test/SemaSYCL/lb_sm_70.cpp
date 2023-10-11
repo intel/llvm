a97bc95db26ce5b8526e67287f12690800a0d0b8
Jakub Chlanda <jakub@codeplay.com>
2023-10-11 13:33:04 +0100

    Move test to clang/test/SemaSYCL

--- clang/test/SemaSYCL/lb_sm_70.cpp
+++ clang/test/SemaSYCL/lb_sm_70.cpp
@@ -1,62 +1,53 @@
-//  REQUIRES: cuda
+// REQUIRES: cuda
 
-// RUN: not %clangxx -ferror-limit=100 -fsycl -fsycl-targets=nvptx64-nvidia-cuda -Xsycl-target-backend --cuda-gpu-arch=sm_70 -fsyntax-only %s -o - 2>&1 | FileCheck %s
-
-// NOTE: we can not use the `-verify` run alongside
-// `expected-error`/`expected-warnings` as the diagnostics come from the device
-// compilation, which happen in temporary files, while `expected-...` are
-// placed in the main file, causing clang to complain at the file mismatch
+// RUN: %clangxx -ferror-limit=100 -fsycl -fsycl-targets=nvptx64-nvidia-cuda -Xsycl-target-backend --cuda-gpu-arch=sm_70 -fsycl-device-only -fsyntax-only -Xclang -verify %s
 
 #include <sycl/sycl.hpp>
 
 template <int N1, int N2, int N3> class Functor {
 public:
+  // expected-warning@+2 {{'maxclusterrank' requires sm_90 or higher, CUDA arch provided: sm_70, ignoring 'max_work_groups_per_mp' attribute}}
   [[intel::max_work_group_size(1, 1, N1), intel::min_work_groups_per_cu(N2),
     intel::max_work_groups_per_mp(N3)]] void
-  // CHECK: maxclusterrank requires sm_90 or higher, CUDA arch provided: sm_70, ignoring 'max_work_groups_per_mp' attribute
   operator()() const {}
 };
 
 int main() {
   sycl::queue Q{};
 
-  sycl::range<1> Gws(32);
-  sycl::range<1> Lws(32);
-
   Q.submit([&](sycl::handler &cgh) {
+     // expected-warning@+5 {{'maxclusterrank' requires sm_90 or higher, CUDA arch provided: sm_70, ignoring 'max_work_groups_per_mp' attribute}}
      cgh.single_task<class T1>(
-         sycl::nd_range<1>(Gws, Lws),
          [=]()
              [[intel::max_work_group_size(1, 1, 256),
                intel::min_work_groups_per_cu(2),
-               // CHECK: maxclusterrank requires sm_90 or higher, CUDA arch provided: sm_70, ignoring 'max_work_groups_per_mp' attribute
                intel::max_work_groups_per_mp(4)]] { volatile int A = 42; });
 
      constexpr float A = 2.0;
+     // expected-error@+5 {{'min_work_groups_per_cu' attribute requires parameter 0 to be an integer constant}}
+     // expected-warning@+5 {{'maxclusterrank' requires sm_90 or higher, CUDA arch provided: sm_70, ignoring 'max_work_groups_per_mp' attribute}}
      cgh.single_task<class T2>(
          [=]()
              [[intel::max_work_group_size(1, 1, 256),
                intel::min_work_groups_per_cu(A),
-               // CHECK: 'min_work_groups_per_cu' attribute requires parameter 0 to be an integer constant
                intel::max_work_groups_per_mp(4)]] { volatile int A = 42; });
-     // CHECK: maxclusterrank requires sm_90 or higher, CUDA arch provided: sm_70, ignoring 'max_work_groups_per_mp' attribute
 
+     // expected-error@+3 {{'min_work_groups_per_cu' attribute requires parameter 0 to be an integer constant}}
      cgh.single_task<class T3>(
          [=]() [[intel::max_work_group_size(1, 1, 256),
                  intel::min_work_groups_per_cu(2147483647 + 1)]]
-         // CHECK: 'min_work_groups_per_cu' attribute requires parameter 0 to be an integer constant
          { volatile int A = 42; });
 
+     // expected-warning@+4 {{attribute 'min_work_groups_per_cu' is already applied with different arguments}}
+     // expected-note@+2 {{previous attribute is here}}
      cgh.single_task<class T4>([=]() [[intel::max_work_group_size(1, 1, 256),
                                        intel::min_work_groups_per_cu(4),
                                        intel::min_work_groups_per_cu(8)]] {
-       // CHECK: attribute 'min_work_groups_per_cu' is already applied with different arguments
-       // CHECK: note: previous attribute is here
        volatile int A = 42;
      });
 
+     // expected-error@+1 {{'min_work_groups_per_cu' attribute must be greater than 0}}
      cgh.single_task<class T5>([=]() [[intel::min_work_groups_per_cu(-8)]] {
-       // CHECK: 'min_work_groups_per_cu' attribute must be greater than 0
        volatile int A = 42;
      });
    }).wait_and_throw();
