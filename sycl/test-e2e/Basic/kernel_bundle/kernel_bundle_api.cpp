9ace836ba3315aec65ad3eda08fd08a2fa090499
JackAKirk <chezjakirk@gmail.com>
2023-10-11 16:34:15 +0100

    Swap !cpu for ext_oneapi_cuda || ext_oneapi_hip
    Signed-off-by: JackAKirk <jack.kirk@codeplay.com>

--- sycl/test-e2e/Basic/kernel_bundle/kernel_bundle_api.cpp
+++ sycl/test-e2e/Basic/kernel_bundle/kernel_bundle_api.cpp
@@ -3,8 +3,8 @@
 // Use of per-kernel device code split and linking the bundle with all images
 // involved leads to multiple definition of AssertHappened structure due each
 // device image is statically linked against fallback libdevice.
-// RUN: %{build} %if cpu %{ -DSYCL_DISABLE_FALLBACK_ASSERT=1 %} %if !ext_oneapi_hip %{ -DENABLE_ONLINE_COMPILE_CHECKS %} -fsycl-device-code-split=per_kernel -o %t.out
-// RUN: %if !cpu %{ %{run} %t.out %}
+// RUN: %{build} %if cpu %{ -DSYCL_DISABLE_FALLBACK_ASSERT=1 %} %if ext_oneapi_cuda || cpu %{ -DENABLE_ONLINE_COMPILE_CHECKS %} -fsycl-device-code-split=per_kernel -o %t.out
+// RUN: %if ext_oneapi_hip || ext_oneapi_cuda %{ %{run} %t.out %}
 // RUN: %if cpu %{ env SYCL_PI_TRACE=2 %{run} %t.out | FileCheck %s %}
 
 #include <iostream>
