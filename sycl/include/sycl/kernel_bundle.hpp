e15820b9b72a3d88aca3abcc0e89c2aa7fd70003
Chris Perkins <chris.perkins@intel.com>
2023-10-12 12:46:00 -0700

    clang-format humiliation

--- sycl/include/sycl/kernel_bundle.hpp
+++ sycl/include/sycl/kernel_bundle.hpp
@@ -824,7 +824,8 @@ template <> struct PropertyToKind<syclex::build_options_key> {
   static constexpr PropKind Kind = PropKind::BuildOptions;
 };
 
-template <> struct IsRuntimeProperty<syclex::build_options_key> : std::true_type {};
+template <>
+struct IsRuntimeProperty<syclex::build_options_key> : std::true_type {};
 
 template <>
 struct IsCompileTimeProperty<syclex::build_options_key> : std::false_type {};
@@ -853,8 +854,7 @@ template <> struct PropertyToKind<syclex::build_log_key> {
   static constexpr PropKind Kind = PropKind::BuildLog;
 };
 
-template <>
-struct IsRuntimeProperty<syclex::build_log_key> : std::true_type {};
+template <> struct IsRuntimeProperty<syclex::build_log_key> : std::true_type {};
 
 template <>
 struct IsCompileTimeProperty<syclex::build_log_key> : std::false_type {};
