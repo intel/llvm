2d3b9f4dd2bf086d9d7bee239bda03b2f69f877a
Khatavkar, Yashasvi <yashasvi.khatavkar@intel.com>
2023-10-19 11:32:35 -0700

    [SYCL] apply clang-format

--- sycl/source/handler.cpp
+++ sycl/source/handler.cpp
@@ -806,10 +806,11 @@ void handler::extractArgsAndReqsFromLambda(
 // As this is impossible in header, there's a function that calls necessary
 // method inside the library and returns the result.
 const char *handler::getKernelName() {
-  const std::string& kernelName = MKernel->get_info<info::kernel::function_name>();
-    char* kernelNameCStr = new char[kernelName.length() + 1];
-    strcpy(kernelNameCStr, kernelName.c_str());
-    return kernelNameCStr;
+  const std::string &kernelName =
+      MKernel->get_info<info::kernel::function_name>();
+  char *kernelNameCStr = new char[kernelName.length() + 1];
+  strcpy(kernelNameCStr, kernelName.c_str());
+  return kernelNameCStr;
 }
 
 void handler::verifyUsedKernelBundle(const std::string &KernelName) {
