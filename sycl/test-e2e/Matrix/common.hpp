6ad41e9b65d09c9b2a6effce7ac48fd94c6fe170
Yury Plyakhin <yury.plyakhin@intel.com>
2023-10-20 04:00:35 -0700

    [SYCL][Joint Matrix] Fix for flaky joint_matrix_all_sizes (#11608)
    Resolves https://github.com/intel/llvm/issues/11603

--- sycl/test-e2e/Matrix/common.hpp
+++ sycl/test-e2e/Matrix/common.hpp
@@ -5,8 +5,14 @@
 
 using bfloat16 = sycl::ext::oneapi::bfloat16;
 
-constexpr float BF16_EPSILON = 10e-2;
-constexpr float FLOAT_EPSILON = 10e-3;
+// Most of the time, failures related to floating-point calculations (both float
+// and bfloat16) are caused by accumulation errors rather than the algorithm
+// itself. If it is an algorithm issue, the calculated result gap from the
+// reference would be much bigger. To avoid flaky test results while catching
+// algorithm errors, we are increasing the accuracy threshold.
+// Something like this should be good enough to catch algorithm errors:
+// fabs(ref[i] - val[i])/max(fabs(ref)) < 10e-2
+constexpr float FLOAT_EPSILON = 10e-2;
 
 template <typename T, size_t NUM_ROWS, size_t NUM_COLS> struct big_matrix {
 public:
@@ -103,11 +109,11 @@ bool matrix_compare(unsigned int rows, unsigned int cols, T1 *src, T2 *ref) {
     for (int j = 0; j < cols; j++) {
       if constexpr (std::is_same_v<T1, float> || std::is_same_v<T1, bfloat16>) {
         float diff = std::fabs(src[i * cols + j] - (T1)ref[i * cols + j]);
-        if (std::is_same_v<T1, float> && diff > FLOAT_EPSILON ||
-            std::is_same_v<T1, bfloat16> && diff > BF16_EPSILON) {
+        if (diff > FLOAT_EPSILON) {
           std::cout << "Incorrect result in matrix. Ref: "
                     << (T1)ref[i * cols + j] << ", Val: " << src[i * cols + j]
-                    << ", Diff: " << diff << "\n";
+                    << ", Diff: " << diff << ", Epsilon: " << FLOAT_EPSILON
+                    << "\n";
           return false;
         }
       } else if constexpr (std::is_same_v<T1, int32_t>) {
