a365c0f21c904b77f7733718865553149e2ed1d7
Justin Rosner <justin.rosner@intel.com>
2023-10-20 10:59:18 -0700

    Update for clang-format

--- sycl/test/check_device_code/device_global_const_eval_use.cpp
+++ sycl/test/check_device_code/device_global_const_eval_use.cpp
@@ -9,16 +9,16 @@ using namespace sycl;
 using namespace sycl::ext::oneapi::experimental;
 
 // Constant int and array of ints device_globals
-constexpr device_global<int, decltype(properties(device_image_scope))> 
-   dg_int{5};
+constexpr device_global<int, decltype(properties(device_image_scope))> dg_int{
+    5};
 // CHECK: @{{[A-Za-z0-9_]*}}dg_int = internal addrspace(1) constant { i32 } { i32 5 }, align 4, !spirv.Decorations
 constexpr device_global<int[3], decltype(properties(device_image_scope))>
-   dg_int_arr{5, 2, 3};
+    dg_int_arr{5, 2, 3};
 // CHECK: @{{[A-Za-z0-9_]*}}dg_int_arr = internal addrspace(1) constant { [3 x i32] } { [3 x i32] [i32 5, i32 2, i32 3] }, align 4, !spirv.Decorations
 
 // Constant char and array of char device_globals
-constexpr device_global<char, decltype(properties(device_image_scope))>
-    dg_char{'f'};
+constexpr device_global<char, decltype(properties(device_image_scope))> dg_char{
+    'f'};
 // CHECK: @{{[A-Za-z0-9_]*}}dg_char = internal addrspace(1) constant { i8 } { i8 102 }, align 1, !spirv.Decorations
 constexpr device_global<char[3], decltype(properties(device_image_scope))>
     dg_char_arr{'d', '4', 'S'};
@@ -31,26 +31,26 @@ constexpr device_global<int[3][2], decltype(properties(device_image_scope))>
 
 // Constant float and array of float device_globals
 constexpr device_global<float, decltype(properties(device_image_scope))>
-   dg_float{4.5};
+    dg_float{4.5};
 // CHECK: @{{[A-Za-z0-9_]*}}g_float = internal addrspace(1) constant { float } { float 4.500000e+00 }, align 4, !spirv.Decorations
 constexpr device_global<float[6], decltype(properties(device_image_scope))>
-   dg_float_arr{4.5, 2.1, 3.5, 9.33, 2.33, 2.1};
+    dg_float_arr{4.5, 2.1, 3.5, 9.33, 2.33, 2.1};
 // CHECK: @{{[A-Za-z0-9_]*}}dg_float_arr = internal addrspace(1) constant { [6 x float] } { [6 x float] [float 4.500000e+00, float 0x4000CCCCC0000000, float 3.500000e+00, float 0x4022A8F5C0000000, float 0x4002A3D700000000, float 0x4000CCCCC0000000] }, align 4, !spirv.Decorations
-   
+
 // Constant double and array of double device_globals
 constexpr device_global<double, decltype(properties(device_image_scope))>
-   dg_double{3.56543};
-// CHECK: @{{[A-Za-z0-9_]*}}dg_double = internal addrspace(1) constant { double } { double 3.565430e+00 }, align 8, !spirv.Decorations
+    dg_double{3.56543};
+// CHECK: @{{[A-Za-z0-9_]*}}dg_double = internal addrspace(1) constant { double  } { double 3.565430e+00 }, align 8, !spirv.Decorations
 constexpr device_global<double[3], decltype(properties(device_image_scope))>
-   dg_double_arr{2.2341234, 233.23423, 236.52321};
+    dg_double_arr{2.2341234, 233.23423, 236.52321};
 // CHECK: @{{[A-Za-z0-9_]*}}dg_double_arr = internal addrspace(1) constant { [3 x double] } { [3 x double] [double 0x4001DF7C16D1D39D, double 0x406D277ECFE9B7BF, double 0x406D90BE22E5DE16] }, align 8, !spirv.Decorations
 
 // Constant bool and array of bool device_globals
-constexpr device_global<bool, decltype(properties(device_image_scope))>
-   dg_bool{true};
+constexpr device_global<bool, decltype(properties(device_image_scope))> dg_bool{
+    true};
 // CHECK: @{{[A-Za-z0-9_]*}}dg_bool = internal addrspace(1) constant { i8 } { i8 1 }, align 1, !spirv.Decorations
 constexpr device_global<bool[3], decltype(properties(device_image_scope))>
-   dg_bool_arr{true, false, true};
+    dg_bool_arr{true, false, true};
 // CHECK: @{{[A-Za-z0-9_]*}}dg_bool_arr = internal addrspace(1) constant { [3 x i8] } { [3 x i8] c"\01\00\01" }, align 1, !spirv.Decorations
 
 // Constant struct and array of struct device_globals
@@ -64,25 +64,25 @@ constexpr TestStruct TS1(5, true, 2.1, {1, 2, 3, 4});
 constexpr TestStruct TS2(7, false, 2.4, {1, 2, 3, 4});
 constexpr TestStruct TS3(6, false, 4.34534, {5, 6, 7, 8});
 constexpr device_global<TestStruct, decltype(properties(device_image_scope))>
-   dg_struct{TS3};
+    dg_struct{TS3};
 // CHECK: @{{[A-Za-z0-9_]*}}dg_struct = internal addrspace(1) constant { %struct.TestStruct } { %struct.TestStruct { i32 6, i8 0, float 0x401161A0C0000000, [4 x i32] [i32 5, i32 6, i32 7, i32 8] } }, align 4, !spirv.Decorations
 constexpr device_global<TestStruct[2], decltype(properties(device_image_scope))>
-   dg_struct_arr{TS1, TS2};
+    dg_struct_arr{TS1, TS2};
 // CHECK: @{{[A-Za-z0-9_]*}}dg_struct_arr = internal addrspace(1) constant { [2 x %struct.TestStruct] } { [2 x %struct.TestStruct] [%struct.TestStruct { i32 5, i8 1, float 0x4000CCCCC0000000, [4 x i32] [i32 1, i32 2, i32 3, i32 4] }, %struct.TestStruct { i32 7, i8 0, float 0x4003333340000000, [4 x i32] [i32 1, i32 2, i32 3, i32 4] }] }, align 4, !spirv.Decorations
 
 // Struct with constexpr constructor
 struct TestStruct2 {
-   int value;
-   constexpr TestStruct2(int val) : value(val) {}; 
+  int value;
+  constexpr TestStruct2(int val) : value(val){};
 };
 constexpr TestStruct2 TS4{4};
 constexpr device_global<TestStruct2, decltype(properties(device_image_scope))>
     dg_constexpr_constructor_struct{TS4};
 // CHECK: @{{[A-Za-z0-9_]*}}dg_constexpr_constructor_struct = internal addrspace(1) constant { %struct.TestStruct2 } { %struct.TestStruct2 { i32 4 } }, align 4, !spirv.Decorations
 
-int main () {
+int main() {
   sycl::queue Q;
-  Q.submit([&](sycl::handler& h) {
+  Q.submit([&](sycl::handler &h) {
     // Simple kernel that just copies over the values from the device_globals so
     // that we can observe the GlobalVariables that are created to represent
     // them in the IR
@@ -107,13 +107,13 @@ int main () {
       std::ignore = dg_double_arr[0];
 
       // Bool and array of bools
-      std::ignore= dg_bool;
+      std::ignore = dg_bool;
       std::ignore = dg_bool_arr[0];
 
       // Struct and array of structs
       std::ignore = dg_struct.get().field1;
       std::ignore = dg_struct_arr[0];
-    
+
       // Struct with constexpr constructor
       std::ignore = dg_constexpr_constructor_struct.get().value;
     });
--- sycl/test/extensions/device_global/device_global_const_eval.cpp
+++ sycl/test/extensions/device_global/device_global_const_eval.cpp
@@ -10,18 +10,18 @@ using namespace sycl::ext::oneapi::experimental;
 
 // Constant int and array of ints device_globals
 constexpr int int_var = 5;
-constexpr device_global<int, decltype(properties(device_image_scope))> 
-   dg_int{int_var};
+constexpr device_global<int, decltype(properties(device_image_scope))> dg_int{
+    int_var};
 constexpr device_global<int[3], decltype(properties(device_image_scope))>
-   dg_int_arr{5, 2, 3};
+    dg_int_arr{5, 2, 3};
 static_assert(dg_int == 5);
 static_assert(dg_int_arr[0] == 5);
 static_assert(dg_int_arr[1] == 2);
 static_assert(dg_int_arr[2] == 3);
 
 // Constant char and array of char device_globals
-constexpr device_global<char, decltype(properties(device_image_scope))>
-    dg_char{'f'};
+constexpr device_global<char, decltype(properties(device_image_scope))> dg_char{
+    'f'};
 constexpr device_global<char[3], decltype(properties(device_image_scope))>
     dg_char_arr{'d', '4', 'S'};
 static_assert(dg_char == 'f');
@@ -41,9 +41,9 @@ static_assert(dg_multi_dim_arr[2][1] == 8);
 
 // Constant float and array of float device_globals
 constexpr device_global<float, decltype(properties(device_image_scope))>
-   dg_float{4.5};
+    dg_float{4.5};
 constexpr device_global<float[6], decltype(properties(device_image_scope))>
-   dg_float_arr{4.5, 2.1, 3.5, 9.33, 2.33, 2.1};
+    dg_float_arr{4.5, 2.1, 3.5, 9.33, 2.33, 2.1};
 static_assert(dg_float == 4.5);
 static_assert(dg_float_arr[0] == float(4.5));
 static_assert(dg_float_arr[1] == float(2.1));
@@ -51,22 +51,22 @@ static_assert(dg_float_arr[2] == float(3.5));
 static_assert(dg_float_arr[3] == float(9.33));
 static_assert(dg_float_arr[4] == float(2.33));
 static_assert(dg_float_arr[5] == float(2.1));
-   
+
 // Constant double and array of double device_globals
 constexpr device_global<double, decltype(properties(device_image_scope))>
-   dg_double{3.56543};
+    dg_double{3.56543};
 constexpr device_global<double[3], decltype(properties(device_image_scope))>
-   dg_double_arr{2.2341234, 233.23423, 236.52321};
+    dg_double_arr{2.2341234, 233.23423, 236.52321};
 static_assert(dg_double == double(3.56543));
 static_assert(dg_double_arr[0] == double(2.2341234));
 static_assert(dg_double_arr[1] == double(233.23423));
 static_assert(dg_double_arr[2] == double(236.52321));
 
 // Constant bool and array of bool device_globals
-constexpr device_global<bool, decltype(properties(device_image_scope))>
-   dg_bool{true};
+constexpr device_global<bool, decltype(properties(device_image_scope))> dg_bool{
+    true};
 constexpr device_global<bool[3], decltype(properties(device_image_scope))>
-   dg_bool_arr{true, false, true};
+    dg_bool_arr{true, false, true};
 static_assert(dg_bool == true);
 static_assert(dg_bool_arr[0] == true);
 static_assert(dg_bool_arr[1] == false);
@@ -82,9 +82,9 @@ struct TestStruct {
 constexpr TestStruct TS1(5, true, 2.1, {1, 2, 3, 4});
 constexpr TestStruct TS2(7, false, 2.4, {1, 2, 3, 4});
 constexpr device_global<TestStruct, decltype(properties(device_image_scope))>
-   dg_struct{TS1};
+    dg_struct{TS1};
 constexpr device_global<TestStruct[2], decltype(properties(device_image_scope))>
-   dg_struct_arr{TS1, TS2};
+    dg_struct_arr{TS1, TS2};
 static_assert(dg_struct.get().field1 == 5);
 static_assert(dg_struct.get().field2 == true);
 static_assert(dg_struct.get().field3 == float(2.1));
@@ -102,8 +102,8 @@ static_assert(dg_struct_arr[1].field4[3] == 4);
 
 // Test struct with constexpr constructor
 struct TestStruct2 {
-   int value;
-   constexpr TestStruct2(int val) : value(val) {}; 
+  int value;
+  constexpr TestStruct2(int val) : value(val){};
 };
 constexpr TestStruct2 TS3{4};
 constexpr device_global<TestStruct2, decltype(properties(device_image_scope))>
