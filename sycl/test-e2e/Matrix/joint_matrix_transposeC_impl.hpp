53630b8205c494d13a9e91cb9b1527d7e998805c
Bing1 Yu <bing1.yu@intel.com>
2023-10-25 11:38:59 +0800

    address comments: 1.use address_space_cast 2.remove A&B 3.let C carry random value 4.introduce helper function matrix_copy to copy C to D

--- sycl/test-e2e/Matrix/joint_matrix_transposeC_impl.hpp
+++ sycl/test-e2e/Matrix/joint_matrix_transposeC_impl.hpp
@@ -7,20 +7,16 @@ using namespace sycl::ext::oneapi::experimental::matrix;
 constexpr size_t TM = 8;
 constexpr size_t TK = 16;
 
-template <typename T1, typename T2, size_t NUM_ROWS_A, size_t NUM_COLS_A,
-          size_t NUM_ROWS_B, size_t NUM_COLS_B, size_t NUM_ROWS_C,
-          size_t NUM_COLS_C>
-void matrix_multiply(T1 *C, T2 *A, T2 *B, queue q, unsigned int vnniFactor) {
+template <typename T1, size_t NUM_ROWS_C, size_t NUM_COLS_C>
+void matrix_load_store(T1 *C, queue q) {
   size_t M = NUM_ROWS_C;
   size_t N = NUM_COLS_C;
-  size_t K = NUM_COLS_A;
 
   size_t NDRangeM = M / TM;
   size_t NDRangeN = N / TN;
 
-  auto pA = multi_ptr<T2, sycl::access::address_space::global_space>(A);
-  auto pB = multi_ptr<T2, sycl::access::address_space::global_space>(B);
-  auto pC = multi_ptr<T1, sycl::access::address_space::global_space>(C);
+  auto pC = address_space_cast<sycl::access::address_space::global_space,
+                               sycl::access::decorated::no>(C);
 
   q.submit([&](handler &cgh) {
      cgh.parallel_for(
@@ -28,9 +24,9 @@ void matrix_multiply(T1 *C, T2 *A, T2 *B, queue q, unsigned int vnniFactor) {
          [=](nd_item<2> spmd_item) [[intel::reqd_sub_group_size(SG_SZ)]]
 
          {
-           // The submatrix API has to be accessed by all the workitems in a
-           // subgroup these functions will be called once by the subgroup no
-           // code divergence between the workitems
+           // The submatrix API has to be accessed by all the workitems in
+           // a subgroup these functions will be called once by the
+           // subgroup no code divergence between the workitems
            const auto global_idx = spmd_item.get_global_id(0);
            const auto global_idy = spmd_item.get_global_id(1);
            const auto sg_startx = global_idx - spmd_item.get_local_id(0);
@@ -57,24 +53,15 @@ void matrix_multiply(T1 *C, T2 *A, T2 *B, queue q, unsigned int vnniFactor) {
 int main() {
   static constexpr size_t MATRIX_M = 1024;
   static constexpr size_t MATRIX_N = 1024;
-  static constexpr size_t MATRIX_K = 1024;
-  static constexpr unsigned int vnniFactor = 2;
+
   queue q;
-  bfloat16 *A = malloc_shared<bfloat16>(MATRIX_M * MATRIX_K, q);
-  bfloat16 *B = malloc_shared<bfloat16>(MATRIX_K * MATRIX_N, q);
-  bfloat16 *vnniB = malloc_shared<bfloat16>(MATRIX_K * MATRIX_N, q);
   float *C = malloc_shared<float>(MATRIX_M * MATRIX_N, q);
   float *D = malloc_shared<float>(MATRIX_M * MATRIX_N, q);
 
-  matrix_rand(MATRIX_M, MATRIX_K, A, (bfloat16)5);
-  matrix_rand(MATRIX_K, MATRIX_N, B, (bfloat16)5);
-  matrix_fill(MATRIX_M, MATRIX_N, C, (float)1.0);
-  matrix_fill(MATRIX_M, MATRIX_N, D, (float)1.0);
+  matrix_rand(MATRIX_M, MATRIX_N, C, (float)5.0);
+  matrix_copy(MATRIX_M, MATRIX_N, C, D);
 
-  matrix_vnni<bfloat16>(MATRIX_K, MATRIX_N, B, vnniB, vnniFactor);
-  matrix_multiply<float, bfloat16, MATRIX_M, MATRIX_K, MATRIX_K / vnniFactor,
-                  MATRIX_N * vnniFactor, MATRIX_M, MATRIX_N>(C, A, vnniB, q,
-                                                             vnniFactor);
+  matrix_load_store<float, MATRIX_M, MATRIX_N>(C, q);
 
   bool res = matrix_compare(MATRIX_M, MATRIX_N, C, D);
 
