a9a6e3d053d71bf6c21f119c0658abae36fcd312
Vyacheslav N Klochkov <vyacheslav.n.klochkov@intel.com>
2023-10-24 18:15:31 -0700

    [ESIMD] Implement unified memory API - part 3 - slm_block_load()
    This patch automatically implements support for 8- and 16-bit data types
    in slm_block_load().
    
    Signed-off-by: Klochkov, Vyacheslav N <vyacheslav.n.klochkov@intel.com>

--- sycl/test/esimd/memory_properties.cpp
+++ sycl/test/esimd/memory_properties.cpp
@@ -11,18 +11,21 @@
 using namespace sycl::ext::intel::esimd;
 
 using AccType = sycl::accessor<uint8_t, 1, sycl::access::mode::read_write>;
+using LocalAccType = sycl::local_accessor<double, 1>;
 
-SYCL_ESIMD_FUNCTION SYCL_EXTERNAL void
-foo(AccType &, float *, int byte_offset32, size_t byte_offset64);
+SYCL_ESIMD_FUNCTION SYCL_EXTERNAL void foo(AccType &, LocalAccType &, float *,
+                                           int byte_offset32,
+                                           size_t byte_offset64);
 
 class EsimdFunctor {
 public:
   AccType acc;
+  LocalAccType local_acc;
   float *ptr;
   int byte_offset32;
   size_t byte_offset64;
   void operator()() __attribute__((sycl_explicit_simd)) {
-    foo(acc, ptr, byte_offset32, byte_offset64);
+    foo(acc, local_acc, ptr, byte_offset32, byte_offset64);
   }
 };
 
@@ -31,13 +34,16 @@ __attribute__((sycl_kernel)) void kernel(Func kernelFunc) {
   kernelFunc();
 }
 
-void bar(AccType &acc, float *ptr, int byte_offset32, size_t byte_offset64) {
-  EsimdFunctor esimdf{acc, ptr, byte_offset32, byte_offset64};
+void bar(AccType &acc, LocalAccType &local_acc, float *ptr, int byte_offset32,
+         size_t byte_offset64) {
+  EsimdFunctor esimdf{acc, local_acc, ptr, byte_offset32, byte_offset64};
   kernel<class kernel_esimd>(esimdf);
 }
 
-SYCL_ESIMD_FUNCTION SYCL_EXTERNAL void
-foo(AccType &acc, float *ptrf, int byte_offset32, size_t byte_offset64) {
+SYCL_ESIMD_FUNCTION SYCL_EXTERNAL void foo(AccType &acc,
+                                           LocalAccType &local_acc, float *ptrf,
+                                           int byte_offset32,
+                                           size_t byte_offset64) {
   properties props_a{cache_hint_L1<cache_hint::streaming>,
                      cache_hint_L2<cache_hint::cached>, alignment<16>};
   static_assert(props_a.has_property<cache_hint_L1_key>(), "Missing L1 hint");
@@ -143,4 +149,42 @@ foo(AccType &acc, float *ptrf, int byte_offset32, size_t byte_offset64) {
 
   // CHECK: call <16 x float> @llvm.genx.oword.ld.v16f32(i32 0, i32 {{[^)]+}}, i32 {{[^)]+}})
   auto z3 = block_load<float, 16>(acc, byte_offset64, props_c16);
+
+  // Now try SLM block_load() with and without cache hints that are ignored.
+
+  // CHECK: load <11 x double>, ptr addrspace(3) {{[^)]+}}, align 16
+  auto slm_bl1 = slm_block_load<double, 11>(byte_offset32, props_a);
+
+  // CHECK: call <8 x double> @llvm.genx.lsc.load.slm.v8f64.v1i1.v1i32(<1 x i1> {{[^)]+}}, i8 0, i8 0, i8 0, i16 1, i32 0, i8 4, i8 5, i8 2, i8 0, <1 x i32> {{[^)]+}}, i32 0)
+  auto slm_bl2 = slm_block_load<double, 8>(byte_offset32, mask, props_c16);
+
+  simd<double, 8> pass_thrud = 2.0;
+  // CHECK: call <8 x double> @llvm.genx.lsc.load.merge.slm.v8f64.v1i1.v1i32(<1 x i1> {{[^)]+}}, i8 0, i8 0, i8 0, i16 1, i32 0, i8 4, i8 5, i8 2, i8 0, <1 x i32> {{[^)]+}}, i32 0, <8 x double> {{[^)]+}})
+  auto slm_bl3 =
+      slm_block_load<double, 8>(byte_offset32, mask, pass_thrud, props_c16);
+
+  // Now try block_load() accepting local accessor.
+
+  // CHECK: load <2 x double>, ptr addrspace(3) {{[^)]+}}, align 16
+  auto lacc_bl1 = block_load<double, 2>(local_acc, props_a);
+
+  // CHECK: load <5 x double>, ptr addrspace(3) {{[^)]+}}, align 8
+  auto lacc_bl2 = block_load<double, 5>(local_acc, byte_offset32, props_b);
+
+  // CHECK: call <8 x double> @llvm.genx.lsc.load.slm.v8f64.v1i1.v1i32(<1 x i1> {{[^)]+}}, i8 0, i8 0, i8 0, i16 1, i32 0, i8 4, i8 5, i8 2, i8 0, <1 x i32> {{[^)]+}}, i32 0)
+  auto lacc_bl3 = block_load<double, 8>(local_acc, mask, props_a);
+
+  // CHECK: call <16 x double> @llvm.genx.lsc.load.merge.slm.v16f64.v1i1.v1i32(<1 x i1> {{[^)]+}}, i8 0, i8 0, i8 0, i16 1, i32 0, i8 4, i8 6, i8 2, i8 0, <1 x i32> {{[^)]+}}, i32 0, <16 x double> {{[^)]+}})
+  simd<double, 16> pass_thrud16 = 2.0;
+  auto lacc_bl4 =
+      block_load<double, 16>(local_acc, mask, pass_thrud16, props_b);
+
+  // CHECK: call <32 x double> @llvm.genx.lsc.load.slm.v32f64.v1i1.v1i32(<1 x i1> {{[^)]+}}, i8 0, i8 0, i8 0, i16 1, i32 0, i8 4, i8 7, i8 2, i8 0, <1 x i32> {{[^)]+}}, i32 0)
+  auto lacc_bl5 =
+      block_load<double, 32>(local_acc, byte_offset32, mask, props_a);
+
+  // CHECK: call <4 x double> @llvm.genx.lsc.load.merge.slm.v4f64.v1i1.v1i32(<1 x i1> {{[^)]+}}, i8 0, i8 0, i8 0, i16 1, i32 0, i8 4, i8 4, i8 2, i8 0, <1 x i32> {{[^)]+}}, i32 0, <4 x double> {{[^)]+}})
+  simd<double, 4> pass_thrud4 = 2.0;
+  auto lacc_bl6 = block_load<double, 4>(local_acc, byte_offset32, mask,
+                                        pass_thrud4, props_a);
 }
