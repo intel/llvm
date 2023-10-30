6cce24565bd95340eed528203cff3437077c4ea6
Lu, John <john.lu@intel.com>
2023-10-30 12:43:05 -0700

    Add more severe test case.  Test more libraries.  Libraries with more kernels.  Kernels provided by source (i.e. not a library).  Splitting per-kernel.
    Signed-off-by: Lu, John <john.lu@intel.com>

---
+++ sycl/test-e2e/AOT/Inputs/fpga_sub_x.cpp
@@ -0,0 +1,25 @@
+#include "sycl/sycl.hpp"
+
+void sub_x_1(sycl::queue q, int *result, int a, int b) {
+  q.single_task<class sub_x_1_dummy>([=] { *result = a - b - 1; });
+}
+
+void sub_x_2(sycl::queue q, int *result, int a, int b) {
+  q.single_task<class sub_x_2_dummy>([=] { *result = a - b - 2; });
+}
+
+void sub_x_3(sycl::queue q, int *result, int a, int b) {
+  q.single_task<class sub_x_3_dummy>([=] { *result = a - b - 3; });
+}
+
+void sub_x_4(sycl::queue q, int *result, int a, int b) {
+  q.single_task<class sub_x_4_dummy>([=] { *result = a - b - 4; });
+}
+
+void sub_x_5(sycl::queue q, int *result, int a, int b) {
+  q.single_task<class sub_x_5_dummy>([=] { *result = a - b - 5; });
+}
+
+void sub_x_6(sycl::queue q, int *result, int a, int b) {
+  q.single_task<class sub_x_6_dummy>([=] { *result = a - b - 6; });
+}
