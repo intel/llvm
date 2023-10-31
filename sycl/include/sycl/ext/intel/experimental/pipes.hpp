ec9b92871988bd6007cd3e6d7cdecc9714586ce9
Niu, Shuo <shuo.niu@intel.com>
2023-10-31 12:07:50 -0700

    Revert "[SYCL] [FPGA] Leverage `add_ir_annotations_member` in latency controls (#11554)"
    This reverts commit b60828d9d6cc5968209cdd94086a22e6d5088602.

--- sycl/include/sycl/ext/intel/experimental/pipes.hpp
+++ sycl/include/sycl/ext/intel/experimental/pipes.hpp
@@ -136,19 +136,36 @@ public:
   template <typename _functionPropertiesT>
   static _dataT read(bool &Success, _functionPropertiesT) {
 #ifdef __SYCL_DEVICE_ONLY__
+    // Get latency control properties
+    using _latency_anchor_id_prop = typename detail::GetOrDefaultValT<
+        _functionPropertiesT, latency_anchor_id_key,
+        detail::defaultLatencyAnchorIdProperty>::type;
+    using _latency_constraint_prop = typename detail::GetOrDefaultValT<
+        _functionPropertiesT, latency_constraint_key,
+        detail::defaultLatencyConstraintProperty>::type;
+
+    // Get latency control property values
+    static constexpr int32_t _anchor_id = _latency_anchor_id_prop::value;
+    static constexpr int32_t _target_anchor = _latency_constraint_prop::target;
+    static constexpr latency_control_type _control_type =
+        _latency_constraint_prop::type;
+    static constexpr int32_t _relative_cycle = _latency_constraint_prop::cycle;
+
+    int32_t _control_type_code = 0; // latency_control_type::none is default
+    if constexpr (_control_type == latency_control_type::exact) {
+      _control_type_code = 1;
+    } else if constexpr (_control_type == latency_control_type::max) {
+      _control_type_code = 2;
+    } else if constexpr (_control_type == latency_control_type::min) {
+      _control_type_code = 3;
+    }
+
     __ocl_RPipeTy<_dataT> _RPipe =
         __spirv_CreatePipeFromPipeStorage_read<_dataT>(&m_Storage);
     _dataT TempData;
-    if constexpr (std::is_same_v<_functionPropertiesT,
-                                 oneapi::experimental::empty_properties_t>) {
-      Success = !static_cast<bool>(
-          __spirv_ReadPipe(_RPipe, &TempData, m_Size, m_Alignment));
-    } else {
-      detail::AnnotatedMemberValue<__ocl_RPipeTy<_dataT>, _functionPropertiesT>
-          annotated_wrapper(_RPipe);
-      Success = !static_cast<bool>(__spirv_ReadPipe(
-          annotated_wrapper.MValue, &TempData, m_Size, m_Alignment));
-    }
+    Success = !static_cast<bool>(__latency_control_nb_read_wrapper(
+        _RPipe, &TempData, _anchor_id, _target_anchor, _control_type_code,
+        _relative_cycle));
     return TempData;
 #else
     (void)Success;
@@ -168,18 +185,35 @@ public:
   template <typename _functionPropertiesT>
   static void write(const _dataT &Data, bool &Success, _functionPropertiesT) {
 #ifdef __SYCL_DEVICE_ONLY__
+    // Get latency control properties
+    using _latency_anchor_id_prop = typename detail::GetOrDefaultValT<
+        _functionPropertiesT, latency_anchor_id_key,
+        detail::defaultLatencyAnchorIdProperty>::type;
+    using _latency_constraint_prop = typename detail::GetOrDefaultValT<
+        _functionPropertiesT, latency_constraint_key,
+        detail::defaultLatencyConstraintProperty>::type;
+
+    // Get latency control property values
+    static constexpr int32_t _anchor_id = _latency_anchor_id_prop::value;
+    static constexpr int32_t _target_anchor = _latency_constraint_prop::target;
+    static constexpr latency_control_type _control_type =
+        _latency_constraint_prop::type;
+    static constexpr int32_t _relative_cycle = _latency_constraint_prop::cycle;
+
+    int32_t _control_type_code = 0; // latency_control_type::none is default
+    if constexpr (_control_type == latency_control_type::exact) {
+      _control_type_code = 1;
+    } else if constexpr (_control_type == latency_control_type::max) {
+      _control_type_code = 2;
+    } else if constexpr (_control_type == latency_control_type::min) {
+      _control_type_code = 3;
+    }
+
     __ocl_WPipeTy<_dataT> _WPipe =
         __spirv_CreatePipeFromPipeStorage_write<_dataT>(&m_Storage);
-    if constexpr (std::is_same_v<_functionPropertiesT,
-                                 oneapi::experimental::empty_properties_t>) {
-      Success = !static_cast<bool>(
-          __spirv_WritePipe(_WPipe, &Data, m_Size, m_Alignment));
-    } else {
-      detail::AnnotatedMemberValue<__ocl_WPipeTy<_dataT>, _functionPropertiesT>
-          annotated_wrapper(_WPipe);
-      Success = !static_cast<bool>(__spirv_WritePipe(
-          annotated_wrapper.MValue, &Data, m_Size, m_Alignment));
-    }
+    Success = !static_cast<bool>(__latency_control_nb_write_wrapper(
+        _WPipe, &Data, _anchor_id, _target_anchor, _control_type_code,
+        _relative_cycle));
 #else
     (void)Success;
     (void)Data;
@@ -247,18 +281,36 @@ public:
   template <typename _functionPropertiesT>
   static _dataT read(_functionPropertiesT) {
 #ifdef __SYCL_DEVICE_ONLY__
+    // Get latency control properties
+    using _latency_anchor_id_prop = typename detail::GetOrDefaultValT<
+        _functionPropertiesT, latency_anchor_id_key,
+        detail::defaultLatencyAnchorIdProperty>::type;
+    using _latency_constraint_prop = typename detail::GetOrDefaultValT<
+        _functionPropertiesT, latency_constraint_key,
+        detail::defaultLatencyConstraintProperty>::type;
+
+    // Get latency control property values
+    static constexpr int32_t _anchor_id = _latency_anchor_id_prop::value;
+    static constexpr int32_t _target_anchor = _latency_constraint_prop::target;
+    static constexpr latency_control_type _control_type =
+        _latency_constraint_prop::type;
+    static constexpr int32_t _relative_cycle = _latency_constraint_prop::cycle;
+
+    int32_t _control_type_code = 0; // latency_control_type::none is default
+    if constexpr (_control_type == latency_control_type::exact) {
+      _control_type_code = 1;
+    } else if constexpr (_control_type == latency_control_type::max) {
+      _control_type_code = 2;
+    } else if constexpr (_control_type == latency_control_type::min) {
+      _control_type_code = 3;
+    }
+
     __ocl_RPipeTy<_dataT> _RPipe =
         __spirv_CreatePipeFromPipeStorage_read<_dataT>(&m_Storage);
     _dataT TempData;
-    if constexpr (std::is_same_v<_functionPropertiesT,
-                                 oneapi::experimental::empty_properties_t>) {
-      __spirv_ReadPipeBlockingINTEL(_RPipe, &TempData, m_Size, m_Alignment);
-    } else {
-      detail::AnnotatedMemberValue<__ocl_RPipeTy<_dataT>, _functionPropertiesT>
-          annotated_wrapper(_RPipe);
-      __spirv_ReadPipeBlockingINTEL(annotated_wrapper.MValue, &TempData, m_Size,
-                                    m_Alignment);
-    }
+    __latency_control_bl_read_wrapper(_RPipe, &TempData, _anchor_id,
+                                      _target_anchor, _control_type_code,
+                                      _relative_cycle);
     return TempData;
 #else
     throw sycl::exception(
@@ -275,17 +327,35 @@ public:
   template <typename _functionPropertiesT>
   static void write(const _dataT &Data, _functionPropertiesT) {
 #ifdef __SYCL_DEVICE_ONLY__
+    // Get latency control properties
+    using _latency_anchor_id_prop = typename detail::GetOrDefaultValT<
+        _functionPropertiesT, latency_anchor_id_key,
+        detail::defaultLatencyAnchorIdProperty>::type;
+    using _latency_constraint_prop = typename detail::GetOrDefaultValT<
+        _functionPropertiesT, latency_constraint_key,
+        detail::defaultLatencyConstraintProperty>::type;
+
+    // Get latency control property values
+    static constexpr int32_t _anchor_id = _latency_anchor_id_prop::value;
+    static constexpr int32_t _target_anchor = _latency_constraint_prop::target;
+    static constexpr latency_control_type _control_type =
+        _latency_constraint_prop::type;
+    static constexpr int32_t _relative_cycle = _latency_constraint_prop::cycle;
+
+    int32_t _control_type_code = 0; // latency_control_type::none is default
+    if constexpr (_control_type == latency_control_type::exact) {
+      _control_type_code = 1;
+    } else if constexpr (_control_type == latency_control_type::max) {
+      _control_type_code = 2;
+    } else if constexpr (_control_type == latency_control_type::min) {
+      _control_type_code = 3;
+    }
+
     __ocl_WPipeTy<_dataT> _WPipe =
         __spirv_CreatePipeFromPipeStorage_write<_dataT>(&m_Storage);
-    if constexpr (std::is_same_v<_functionPropertiesT,
-                                 oneapi::experimental::empty_properties_t>) {
-      __spirv_WritePipeBlockingINTEL(_WPipe, &Data, m_Size, m_Alignment);
-    } else {
-      detail::AnnotatedMemberValue<__ocl_WPipeTy<_dataT>, _functionPropertiesT>
-          annotated_wrapper(_WPipe);
-      __spirv_WritePipeBlockingINTEL(annotated_wrapper.MValue, &Data, m_Size,
-                                     m_Alignment);
-    }
+    __latency_control_bl_write_wrapper(_WPipe, &Data, _anchor_id,
+                                       _target_anchor, _control_type_code,
+                                       _relative_cycle);
 #else
     (void)Data;
     throw sycl::exception(
@@ -331,6 +401,45 @@ public:
       m_uses_valid,
       m_first_symbol_in_high_order_bits,
       m_protocol};
+
+#ifdef __SYCL_DEVICE_ONLY__
+private:
+  // FPGA BE will recognize this function and extract its arguments.
+  // TODO: Pass latency control parameters via the __spirv_* builtin when ready.
+  template <typename _T>
+  static int32_t __latency_control_nb_read_wrapper(
+      __ocl_RPipeTy<_T> Pipe, _T *Data, int32_t /* AnchorID */,
+      int32_t /* TargetAnchor */, int32_t /* Type */, int32_t /* Cycle */) {
+    return __spirv_ReadPipe(Pipe, Data, m_Size, m_Alignment);
+  }
+
+  // FPGA BE will recognize this function and extract its arguments.
+  // TODO: Pass latency control parameters via the __spirv_* builtin when ready.
+  template <typename _T>
+  static int32_t __latency_control_nb_write_wrapper(
+      __ocl_WPipeTy<_T> Pipe, const _T *Data, int32_t /* AnchorID */,
+      int32_t /* TargetAnchor */, int32_t /* Type */, int32_t /* Cycle */) {
+    return __spirv_WritePipe(Pipe, Data, m_Size, m_Alignment);
+  }
+
+  // FPGA BE will recognize this function and extract its arguments.
+  // TODO: Pass latency control parameters via the __spirv_* builtin when ready.
+  template <typename _T>
+  static void __latency_control_bl_read_wrapper(
+      __ocl_RPipeTy<_T> Pipe, _T *Data, int32_t /* AnchorID */,
+      int32_t /* TargetAnchor */, int32_t /* Type */, int32_t /* Cycle */) {
+    return __spirv_ReadPipeBlockingINTEL(Pipe, Data, m_Size, m_Alignment);
+  }
+
+  // FPGA BE will recognize this function and extract its arguments.
+  // TODO: Pass latency control parameters via the __spirv_* builtin when ready.
+  template <typename _T>
+  static void __latency_control_bl_write_wrapper(
+      __ocl_WPipeTy<_T> Pipe, const _T *Data, int32_t /* AnchorID*/,
+      int32_t /* TargetAnchor */, int32_t /* Type */, int32_t /* Cycle */) {
+    return __spirv_WritePipeBlockingINTEL(Pipe, Data, m_Size, m_Alignment);
+  }
+#endif // __SYCL_DEVICE_ONLY__
 };
 
 } // namespace experimental
