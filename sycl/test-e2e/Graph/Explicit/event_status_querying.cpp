f281cd63b66bfacaff73ed223ff3fe8fc6cc5236
Ewan Crawford <ewan@codeplay.com>
2023-11-01 14:36:15 +0000

    Disable buffer Graph tests for CUDA
    This is a temporary workaround to unblock the UR
    merge queue. We will investigate and fix as follow-up work.

--- sycl/test-e2e/Graph/Explicit/event_status_querying.cpp
+++ sycl/test-e2e/Graph/Explicit/event_status_querying.cpp
@@ -1,8 +1,11 @@
-// REQUIRES: cuda || level_zero, gpu
+// REQUIRES: level_zero, gpu
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out 2>&1 | FileCheck %s
 //
 // CHECK: complete
+//
+// TODO enable cuda once buffer issue investigated and fixed
+// UNSUPPORTED: cuda
 
 #define GRAPH_E2E_EXPLICIT
 
