b2bf1534f65b505425ee0ec275f03934cf88fec8
Wu, Yingcong <yingcong.wu@intel.com>
2023-11-01 18:20:12 -0700

    change to vector

--- sycl/test-e2e/ESIMD/linear/bitmap_helpers.h
+++ sycl/test-e2e/ESIMD/linear/bitmap_helpers.h
@@ -23,6 +23,7 @@
 #include <memory>
 #include <string>
 #include <sycl/detail/defines.hpp>
+#include <vector>
 
 namespace sycl {
 inline namespace _V1 {
@@ -258,8 +259,10 @@ namespace bitmap {
       width = std::abs(*(short *)&header_out[18]);
       height = std::abs(*(short *)&header_out[22]);
 
-      img_out = (unsigned char *)std::malloc(width * height * 3);
-      img_gold = (unsigned char *)std::malloc(width * height * 3);
+      auto img_out_vector = std::vector<unsigned char>(width * height * 3);
+      auto img_gold_vector = std::vector<unsigned char>(width * height * 3);
+      img_out = img_out_vector.data();
+      img_gold = img_gold_vector.data();
 
       if (fread(img_out, 1, width * height * 3, f_out) != width * height * 3) {
         perror(f_out_str);
@@ -275,17 +278,13 @@ namespace bitmap {
       fclose(f_out);
       fclose(f_gold);
 
-      bool check_res = true;
       for (i = 0; i < width * height * 3; i++) {
         if (std::abs(img_out[i] - img_gold[i]) > tolerance) {
-          check_res = false;
-          break;
+          return false;
         }
       }
 
-      std::free(img_out);
-      std::free(img_gold);
-      return check_res;
+      return true;
     }
   };
 
