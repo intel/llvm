f76912df229c27507a95208cfc92dd2fa9e3a326
Chen, Brox <brox.chen@intel.com>
2023-11-06 11:13:27 -0800

    clang-format

--- sycl/test-e2e/Annotated_arg_ptr/annotated_ptr.cpp
+++ sycl/test-e2e/Annotated_arg_ptr/annotated_ptr.cpp
@@ -29,7 +29,7 @@ int main() {
   auto *d = malloc_shared<int>(4, Q);
   auto d_ptr = annotated_ptr{d};
 
-  volatile int* e = malloc_shared<int>(1, Q);
+  volatile int *e = malloc_shared<int>(1, Q);
   *e = 0;
   auto e_ptr = annotated_ptr{e};
 
@@ -66,7 +66,7 @@ int main() {
 
      d_ptr[3] = func(d_ptr[0], d_ptr[1], d_ptr[2]);
 
-	 e_ptr[0] = 5;
+     e_ptr[0] = 5;
    }).wait();
 
   assert(a_ptr[0] == -1 && "a_ptr[0] value does not match.");
