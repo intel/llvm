89f22b9e042a49f56c1a08e03d6a5135db3affa2
Maxime France-Pillois <maxime.francepillois@codeplay.com>
2023-11-07 12:24:37 +0000

    Adds tolerance on node link order + checks output only on linux

--- sycl/test-e2e/Graph/Explicit/debug_print_graph_verbose.cpp
+++ sycl/test-e2e/Graph/Explicit/debug_print_graph_verbose.cpp
@@ -1,6 +1,11 @@
 // REQUIRES: level_zero, gpu
 // RUN: %{build} -o %t.out
-// RUN: %{run} %t.out ; FileCheck %s --input-file graph_verbose.dot
+// RUN: %if linux %{ %{run} %t.out ; FileCheck %s --input-file graph_verbose.dot %}
+// RUN: %if windows %{ %{run} %t.out %}
+// Windows output format differs from linux format.
+// The filecheck-based output checking is suited to linux standards.
+// On Windows, we only test that printing takes place correctly and does not
+// trigger errors or throw execeptions.
 //
 // CHECK: digraph dot {
 // CHECK-NEXT: "0x[[#%x,NODE1:]]"
@@ -21,12 +26,12 @@
 // CHECK-SAME: [style=bold, label="ID = 0x[[#NODE4]]\nTYPE = CGExecKernel \nNAME = _ZTSZZ11add_kernelsItEN4sycl3_V13ext6oneapi12experimental4nodeENS4_13command_graphILNS4_11graph_stateE0EEEmNS1_6bufferIT_Li1ENS1_6detail17aligned_allocatorINSt12remove_constISA_E4typeEEEvEESH_SH_ENKUlRNS1_7handlerEE2_clESJ_EUlNS1_4itemILi1ELb1EEEE_\n
 // CHECK-SAME: ARGS = \n0) Type: Accessor Ptr: 0x[[#%x,ADDR18:]]\n1) Type: STD_Layout Ptr: 0x[[#%x,ADDR19:]]\n2) Type: STD_Layout Ptr: 0x[[#%x,ADDR20:]]\n
 // CHECK-SAME: 3) Type: STD_Layout Ptr: 0x[[#ADDR18]]\n4) Type: Accessor Ptr: 0x[[#%x,ADDR21:]]\n5) Type: STD_Layout Ptr: 0x[[#%x,ADDR22:]]\n6) Type: STD_Layout Ptr: 0x[[#%x,ADDR23:]]\n7) Type: STD_Layout Ptr: 0x[[#%x,ADDR24:]]\n"];
-// CHECK-NEXT: "0x[[#NODE3]]" -> "0x[[#NODE4]]"
-// CHECK-NEXT: "0x[[#NODE2]]" -> "0x[[#NODE4]]"
+// CHECK-DAG: "0x[[#NODE3]]" -> "0x[[#NODE4]]"
+// CHECK-DAG: "0x[[#NODE2]]" -> "0x[[#NODE4]]"
 // CHECK-NEXT: "0x[[#%x,NODE5:]]"
 // CHECK-SAME: [style=bold, label="ID = 0x[[#NODE5]]\nTYPE = CGCopy Device-to-Device \nSrc: 0x[[#%x,ADDR25:]] Dst: 0x[[#%x,ADDR26:]]\n"];
-// CHECK-NEXT: "0x[[#NODE3]]" -> "0x[[#NODE5]]"
-// CHECK-NEXT: "0x[[#NODE4]]" -> "0x[[#NODE5]]
+// CHECK-DAG: "0x[[#NODE3]]" -> "0x[[#NODE5]]"
+// CHECK-DAG: "0x[[#NODE4]]" -> "0x[[#NODE5]]
 // CHECK-NEXT: "0x[[#%x,NODE6:]]"
 // CHECK-SAME: [style=bold, label="ID = 0x[[#NODE6]]\nTYPE = CGCopy Device-to-Host \nSrc: 0x[[#%x,ADDR27:]] Dst: 0x[[#%x,ADDR28:]]\n"];
 // CHECK-NEXT: "0x[[#%x,NODE7:]]"
