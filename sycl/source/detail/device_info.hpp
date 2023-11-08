946b1f2361670c7ac24e342ee77b1f467778cf94
Dmitry Vodopyanov <dmitry.vodopyanov@intel.com>
2023-11-08 17:00:42 +0100

    [SYCL][Joint Matrix] Implement opt kernel feature for joint_matrix and joint_matrix_mad (#11738)
    This patch adds two new properties `joint_matrix` and `joint_matrix_mad`
    to device requirements in
    sycl-post-link.
    SYCL RT reads these properties and throws exception if objects of
    `joint_matrix` type or `joint_matrix_mad` functions are not supported by
    the current device. "Unsupported" means matrix type and sizes provided
    by user are not compatible with the list of all supported matrix types
    and sizes from the runtime query in
    `get_info<...matrix_combinations>`.

--- sycl/source/detail/device_info.hpp
+++ sycl/source/detail/device_info.hpp
@@ -759,7 +759,7 @@ struct get_device_info_impl<
            matrix_type::sint32, matrix_type::sint32},
           {8, 0, 0, 0, 8, 32, matrix_type::sint8, matrix_type::uint8,
            matrix_type::sint32, matrix_type::sint32},
-          {8, 0, 0, 0, 8, 16, matrix_type::sint8, matrix_type::sint8,
+          {8, 0, 0, 0, 8, 32, matrix_type::sint8, matrix_type::sint8,
            matrix_type::sint32, matrix_type::sint32},
           {8, 0, 0, 0, 8, 16, matrix_type::fp16, matrix_type::fp16,
            matrix_type::fp32, matrix_type::fp32},
