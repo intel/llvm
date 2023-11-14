dc73e9652518cbec221f293b744ca1d42593dd01
Abhishek Tiwari <abhishek2.tiwari@intel.com>
2023-11-14 10:55:03 -0800

    clang-format

--- sycl/include/sycl/ext/oneapi/experimental/annotated_arg/annotated_arg.hpp
+++ sycl/include/sycl/ext/oneapi/experimental/annotated_arg/annotated_arg.hpp
@@ -72,8 +72,8 @@ __SYCL_TYPE(annotated_arg) annotated_arg<T *, detail::properties_t<Props...>> {
 #ifdef __SYCL_DEVICE_ONLY__
   void __init([[__sycl_detail__::add_ir_attributes_kernel_parameter(
       detail::PropertyMetaInfo<Props>::name...,
-      detail::PropertyMetaInfo<
-          Props>::value...)]] typename decorated_global_ptr<T>::pointer _obj) {
+      detail::PropertyMetaInfo<Props>::value...)]]
+              typename decorated_global_ptr<T>::pointer _obj) {
     obj = _obj;
   }
 #endif
