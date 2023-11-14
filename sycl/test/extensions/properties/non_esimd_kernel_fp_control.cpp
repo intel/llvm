95bea0055f0242b94a5920f2480f802a08c7754f
Artur Gainullin <artur.gainullin@intel.com>
2023-11-14 07:54:18 -0800

    Add static assert for non-ESIMD kernel

--- sycl/test/extensions/properties/non_esimd_kernel_fp_control.cpp
+++ sycl/test/extensions/properties/non_esimd_kernel_fp_control.cpp
@@ -1,6 +1,5 @@
-// RUN: %clangxx -fsycl -fsycl-device-only -fsycl-targets=%sycl_triple -Xclang -verify -Xclang -verify-ignore-unexpected=note,warning %s
+// RUN: %clangxx %fsycl-host-only -fsyntax-only -Xclang -verify -Xclang -verify-ignore-unexpected=note,warning %s
 
-#include <iostream>
 #include <sycl/ext/intel/esimd.hpp>
 #include <sycl/sycl.hpp>
 
@@ -13,7 +12,7 @@ struct ESIMDKernel {
 
   void operator()(id<1> i) const {}
 
-  auto get(sycl::ext::oneapi::experimental::properties_tag) {
+  auto get(sycl::ext::oneapi::experimental::properties_tag) const {
     return sycl::ext::oneapi::experimental::properties{
         intelex::fp_control<intelex::fp_mode::round_downward |
                             intelex::fp_mode::denorm_ftz>};
@@ -22,7 +21,7 @@ struct ESIMDKernel {
 
 int main(void) {
   queue q;
-  // expected-error@* {{SYCL fp_control control property is supported only for ESIMD kernels}}
+  // expected-error-re@sycl/handler.hpp:* {{static assertion failed due to requirement {{.+}}: Floating point control property is supported for ESIMD kernels only.}}
   syclex::properties properties7{
       intelex::fp_control<intelex::fp_mode::round_toward_zero |
                           intelex::fp_mode::denorm_ftz>};
@@ -30,7 +29,7 @@ int main(void) {
     cgh.single_task<class TestKernel7>(properties7, [=]() {});
   });
 
-  // expected-error@* {{SYCL fp_control control property is supported only for ESIMD kernels}}
+  // expected-error-re@sycl/handler.hpp:* {{static assertion failed due to requirement {{.+}}: Floating point control property is supported for ESIMD kernels only.}}
   ESIMDKernel Kern;
   q.submit([&](handler &cgh) { cgh.parallel_for(range<1>(1), Kern); });
 
