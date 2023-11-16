8a0567490681893306a641889147bfc8ecb57fa9
Fábio Mestre <fabio.mestre@codeplay.com>
2023-11-16 14:56:00 +0000

    [CI] Add support for OpenCL CI on CPU

--- test/conformance/source/environment.cpp
+++ test/conformance/source/environment.cpp
@@ -179,6 +179,16 @@ PlatformEnvironment::parsePlatformOptions(int argc, char **argv) {
                 std::string(&arg[std::strlen("--platform=")]);
         }
     }
+
+    /* If a platform was not provided using the --platform command line option,
+     * check if environment variable is set to use as a fallback. */
+    if (options.platform_name.empty()) {
+        const char *env_platform = std::getenv("UR_CTS_ADAPTER_PLATFORM");
+        if (env_platform) {
+            options.platform_name = env_platform;
+        }
+    }
+
     return options;
 }
 
