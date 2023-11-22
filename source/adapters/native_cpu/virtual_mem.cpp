e9f16eed5a958e08bf9fb95445fef06ba24844c6
Larsen, Steffen <steffen.larsen@intel.com>
2023-11-22 00:01:24 -0800

    Avoid death
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- source/adapters/native_cpu/virtual_mem.cpp
+++ source/adapters/native_cpu/virtual_mem.cpp
@@ -16,45 +16,33 @@
 UR_APIEXPORT ur_result_t UR_APICALL urVirtualMemGranularityGetInfo(
     ur_context_handle_t, ur_device_handle_t, ur_virtual_mem_granularity_info_t,
     size_t, void *, size_t *) {
-  detail::ur::die("Virtual memory extension is not currently implemented for "
-                  "NATIVE CPU adapter.");
   return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
 }
 
 UR_APIEXPORT ur_result_t UR_APICALL urVirtualMemReserve(ur_context_handle_t,
                                                         const void *, size_t,
                                                         void **) {
-  detail::ur::die("Virtual memory extension is not currently implemented for "
-                  "NATIVE CPU adapter.");
   return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
 }
 
 UR_APIEXPORT ur_result_t UR_APICALL urVirtualMemFree(ur_context_handle_t,
                                                      const void *, size_t) {
-  detail::ur::die("Virtual memory extension is not currently implemented for "
-                  "NATIVE CPU adapter.");
   return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
 }
 
 UR_APIEXPORT ur_result_t UR_APICALL urVirtualMemSetAccess(
     ur_context_handle_t, const void *, size_t, ur_virtual_mem_access_flags_t) {
-  detail::ur::die("Virtual memory extension is not currently implemented for "
-                  "NATIVE CPU adapter.");
   return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
 }
 
 UR_APIEXPORT ur_result_t UR_APICALL urVirtualMemMap(
     ur_context_handle_t, const void *, size_t, ur_physical_mem_handle_t, size_t,
     ur_virtual_mem_access_flags_t) {
-  detail::ur::die("Virtual memory extension is not currently implemented for "
-                  "NATIVE CPU adapter.");
   return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
 }
 
 UR_APIEXPORT ur_result_t UR_APICALL urVirtualMemUnmap(ur_context_handle_t,
                                                       const void *, size_t) {
-  detail::ur::die("Virtual memory extension is not currently implemented for "
-                  "NATIVE CPU adapter.");
   return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
 }
 
@@ -63,7 +51,5 @@ UR_APIEXPORT ur_result_t UR_APICALL urVirtualMemGetInfo(ur_context_handle_t,
                                                         ur_virtual_mem_info_t,
                                                         size_t, void *,
                                                         size_t *) {
-  detail::ur::die("Virtual memory extension is not currently implemented for "
-                  "NATIVE CPU adapter.");
   return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
 }
