intel/llvm

commit 096676e8d4d87475860723ed8a4d8c256bcd98c2

[SYCL] ext kernel compiler (#11517)
This is the implementation for the new "Kernel Compiler" which will
replace the Online Compiler.
Its pending spec is here: https://github.com/intel/llvm/pull/11331

sycl/source/detail/common.cpp
@@ -74,18 +74,14 @@ const char *stringifyErrorCode(pi_int32 error) {
 std::vector<std::string> split_string(const std::string &str, char delimeter) {
   std::vector<std::string> result;
   size_t beg = 0;
-  size_t length = 0;
-  for (const auto &x : str) {
-    if (x == delimeter) {
-      result.push_back(str.substr(beg, length));
-      beg += length + 1;
-      length = 0;
-      continue;
-    }
-    length++;
+  size_t end = 0;
+  while ((end = str.find(delimeter, beg)) != std::string::npos) {
+    result.push_back(str.substr(beg, end - beg));
+    beg = end + 1;
   }
-  if (length != 0) {
-    result.push_back(str.substr(beg, length));
+  end = str.find('\0');
+  if (beg < end) {
+    result.push_back(str.substr(beg, end - beg));
   }
   return result;
 }

sycl/doc/extensions/experimental/sycl_ext_oneapi_kernel_compiler.asciidoc
@@ -49,14 +49,12 @@ This extension also depends on the following other SYCL extensions:
 
 == Status
 
-This is a proposed extension specification, intended to gather community
-feedback.
-Interfaces defined in this specification may not be implemented yet or may be
-in a preliminary state.
-The specification itself may also change in incompatible ways before it is
-finalized.
-*Shipping software products should not rely on APIs defined in this
-specification.*
+This is an experimental extension specification, intended to provide early 
+access to features and gather community feedback. Interfaces defined in 
+this specification are implemented in DPC++, but they are not finalized 
+and may change incompatibly in future versions of DPC++ without prior notice. 
+*Shipping software products should not rely on APIs defined in 
+this specification.*
 
 
 == Overview

sycl/doc/extensions/experimental/sycl_ext_oneapi_kernel_compiler_opencl.asciidoc
@@ -49,14 +49,12 @@ This extension also depends on the following other SYCL extensions:
 
 == Status
 
-This is a proposed extension specification, intended to gather community
-feedback.
-Interfaces defined in this specification may not be implemented yet or may be
-in a preliminary state.
-The specification itself may also change in incompatible ways before it is
-finalized.
-*Shipping software products should not rely on APIs defined in this
-specification.*
+This is an experimental extension specification, intended to provide early 
+access to features and gather community feedback. Interfaces defined in 
+this specification are implemented in DPC++, but they are not finalized 
+and may change incompatibly in future versions of DPC++ without prior notice. 
+*Shipping software products should not rely on APIs defined in 
+this specification.*
 
 
 == Overview

sycl/include/sycl/ext/intel/experimental/online_compiler.hpp
@@ -75,7 +75,11 @@ enum class source_language { opencl_c = 0, cm = 1 };
 
 /// Represents an online compiler for the language given as template
 /// parameter.
-template <source_language Lang> class online_compiler {
+template <source_language Lang>
+class __SYCL2020_DEPRECATED(
+    "experimental online_compiler is being deprecated. See "
+    "'sycl_ext_oneapi_kernel_compiler.asciidoc' instead for new kernel "
+    "compiler extension to kernel_bundle implementation.") online_compiler {
 public:
   /// Constructs online compiler which can target any device and produces
   /// given compiled code format. Produces 64-bit device code.

sycl/include/sycl/ext/oneapi/properties/properties.hpp
@@ -241,6 +241,30 @@ struct ValueOrDefault<
   }
 };
 
+template <typename SyclT, typename PropertiesT> struct all_props_are_keys_of;
+
+template <typename SyclT, typename PropertiesT>
+struct all_props_are_keys_of : std::true_type {};
+
+template <typename SyclT>
+struct all_props_are_keys_of<
+    SyclT, ext::oneapi::experimental::detail::empty_properties_t>
+    : std::true_type {};
+
+template <typename SyclT, typename PropT>
+struct all_props_are_keys_of<
+    SyclT, ext::oneapi::experimental::properties<std::tuple<PropT>>>
+    : std::bool_constant<
+          ext::oneapi::experimental::is_property_key_of<PropT, SyclT>::value> {
+};
+
+template <typename SyclT, typename PropT, typename... PropTs>
+struct all_props_are_keys_of<
+    SyclT, ext::oneapi::experimental::properties<std::tuple<PropT, PropTs...>>>
+    : std::bool_constant<
+          ext::oneapi::experimental::is_property_key_of<PropT, SyclT>::value &&
+          all_props_are_keys_of<SyclT, PropTs...>()> {};
+
 } // namespace detail
 } // namespace ext::oneapi::experimental
 

sycl/include/sycl/ext/oneapi/properties/property.hpp
@@ -218,8 +218,10 @@ enum PropKind : uint32_t {
   CacheControlReadHint = 48,
   CacheControlReadAssertion = 49,
   CacheControlWrite = 50,
+  BuildOptions = 51,
+  BuildLog = 52,
   // PropKindSize must always be the last value.
-  PropKindSize = 51,
+  PropKindSize = 53,
 };
 
 // This trait must be specialized for all properties and must have a unique

sycl/include/sycl/kernel_bundle.hpp
@@ -20,6 +20,10 @@
 #include <sycl/kernel_bundle_enums.hpp>    // for bundle_state
 #include <sycl/property_list.hpp>          // for property_list
 
+#include <sycl/ext/oneapi/properties/properties.hpp>     // PropertyT
+#include <sycl/ext/oneapi/properties/property.hpp>       // build_options
+#include <sycl/ext/oneapi/properties/property_value.hpp> // and log
+
 #include <array>       // for array
 #include <cstring>     // for size_t, memcpy
 #include <functional>  // for function
@@ -41,6 +45,7 @@ auto get_native(const kernel_bundle<State> &Obj)
 
 namespace detail {
 class kernel_id_impl;
+class kernel_impl;
 }
 
 template <typename KernelName> kernel_id get_kernel_id();
@@ -176,6 +181,10 @@ public:
 
   bool native_specialization_constant() const noexcept;
 
+  bool ext_oneapi_has_kernel(const std::string &name);
+
+  kernel ext_oneapi_get_kernel(const std::string &name);
+
 protected:
   // \returns a kernel object which represents the kernel identified by
   // kernel_id passed
@@ -215,7 +224,12 @@ public:
   kernel_bundle() = delete;
 
   /// \returns true if the kernel_bundles contains no device images
-  bool empty() const noexcept { return kernel_bundle_plain::empty(); }
+  template <
+      bundle_state _State = State,
+      typename = std::enable_if_t<_State != bundle_state::ext_oneapi_source>>
+  bool empty() const noexcept {
+    return kernel_bundle_plain::empty();
+  }
 
   /// \returns the backend associated with the kernel bundle
   backend get_backend() const noexcept {
@@ -234,6 +248,9 @@ public:
 
   /// \returns true if the kernel_bundle contains the kernel identified by
   /// kernel_id passed
+  template <
+      bundle_state _State = State,
+      typename = std::enable_if_t<_State != bundle_state::ext_oneapi_source>>
   bool has_kernel(const kernel_id &KernelID) const noexcept {
     return kernel_bundle_plain::has_kernel(KernelID);
   }
@@ -241,36 +258,53 @@ public:
   /// \returns true if the kernel_bundle contains the kernel identified by
   /// kernel_id passed and if this kernel is compatible with the device
   /// specified
+  template <
+      bundle_state _State = State,
+      typename = std::enable_if_t<_State != bundle_state::ext_oneapi_source>>
   bool has_kernel(const kernel_id &KernelID, const device &Dev) const noexcept {
     return kernel_bundle_plain::has_kernel(KernelID, Dev);
   }
 
   /// \returns true only if the kernel bundle contains the kernel identified by
   /// KernelName.
-  template <typename KernelName> bool has_kernel() const noexcept {
+  template <
+      typename KernelName, bundle_state _State = State,
+      typename = std::enable_if_t<_State != bundle_state::ext_oneapi_source>>
+  bool has_kernel() const noexcept {
     return has_kernel(get_kernel_id<KernelName>());
   }
 
   /// \returns true only if the kernel bundle contains the kernel identified by
   /// KernelName and if that kernel is compatible with the device Dev.
-  template <typename KernelName>
+  template <
+      typename KernelName, bundle_state _State = State,
+      typename = std::enable_if_t<_State != bundle_state::ext_oneapi_source>>
   bool has_kernel(const device &Dev) const noexcept {
     return has_kernel(get_kernel_id<KernelName>(), Dev);
   }
 
   /// \returns a vector of kernel_id's that contained in the kernel_bundle
+  template <
+      bundle_state _State = State,
+      typename = std::enable_if_t<_State != bundle_state::ext_oneapi_source>>
   std::vector<kernel_id> get_kernel_ids() const {
     return kernel_bundle_plain::get_kernel_ids();
   }
 
   /// \returns true if the kernel_bundle contains at least one device image
   /// which uses specialization constants
+  template <
+      bundle_state _State = State,
+      typename = std::enable_if_t<_State != bundle_state::ext_oneapi_source>>
   bool contains_specialization_constants() const noexcept {
     return kernel_bundle_plain::contains_specialization_constants();
   }
 
   /// \returns true if all specialization constants which are used in the
   /// kernel_bundle are "native specialization constants in all device images
+  template <
+      bundle_state _State = State,
+      typename = std::enable_if_t<_State != bundle_state::ext_oneapi_source>>
   bool native_specialization_constant() const noexcept {
     return kernel_bundle_plain::native_specialization_constant();
   }
@@ -293,7 +327,10 @@ public:
 
   /// \returns true if any device image in the kernel_bundle uses specialization
   /// constant whose address is SpecName
-  template <auto &SpecName> bool has_specialization_constant() const noexcept {
+  template <
+      auto &SpecName, bundle_state _State = State,
+      typename = std::enable_if_t<_State != bundle_state::ext_oneapi_source>>
+  bool has_specialization_constant() const noexcept {
     const char *SpecSymName = detail::get_spec_constant_symbolic_ID<SpecName>();
     return has_specialization_constant_impl(SpecSymName);
   }
@@ -312,7 +349,9 @@ public:
 
   /// \returns the value of the specialization constant whose address is
   /// SpecName for this kernel bundle.
-  template <auto &SpecName>
+  template <
+      auto &SpecName, bundle_state _State = State,
+      typename = std::enable_if_t<_State != bundle_state::ext_oneapi_source>>
   typename std::remove_reference_t<decltype(SpecName)>::value_type
   get_specialization_constant() const {
     using SCType =
@@ -331,14 +370,40 @@ public:
   }
 
   /// \returns an iterator to the first device image kernel_bundle contains
+  template <
+      bundle_state _State = State,
+      typename = std::enable_if_t<_State != bundle_state::ext_oneapi_source>>
   device_image_iterator begin() const {
     return reinterpret_cast<device_image_iterator>(
         kernel_bundle_plain::begin());
   }
 
   /// \returns an iterator to the last device image kernel_bundle contains
+  template <
+      bundle_state _State = State,
+      typename = std::enable_if_t<_State != bundle_state::ext_oneapi_source>>
   device_image_iterator end() const {
     return reinterpret_cast<device_image_iterator>(kernel_bundle_plain::end());
+  }
+
+  /////////////////////////
+  // ext_oneapi_has_kernel
+  //  only true if created from source and has this kernel
+  /////////////////////////
+  template <bundle_state _State = State,
+            typename = std::enable_if_t<_State == bundle_state::executable>>
+  bool ext_oneapi_has_kernel(const std::string &name) {
+    return detail::kernel_bundle_plain::ext_oneapi_has_kernel(name);
+  }
+
+  /////////////////////////
+  // ext_oneapi_get_kernel
+  //  kernel_bundle must be created from source, throws if not present
+  /////////////////////////
+  template <bundle_state _State = State,
+            typename = std::enable_if_t<_State == bundle_state::executable>>
+  kernel ext_oneapi_get_kernel(const std::string &name) {
+    return detail::kernel_bundle_plain::ext_oneapi_get_kernel(name);
   }
 
 private:
@@ -742,6 +807,138 @@ build(const kernel_bundle<bundle_state::input> &InputBundle,
       const property_list &PropList = {}) {
   return build(InputBundle, InputBundle.get_devices(), PropList);
 }
+
+namespace ext::oneapi::experimental {
+
+/////////////////////////
+// PropertyT syclex::build_options
+/////////////////////////
+struct build_options {
+  std::vector<std::string> opts;
+  build_options(const std::string &optsArg) : opts{optsArg} {}
+  build_options(const std::vector<std::string> &optsArg) : opts(optsArg) {}
+};
+using build_options_key = build_options;
+
+template <> struct is_property_key<build_options_key> : std::true_type {};
+
+template <>
+struct is_property_key_of<build_options_key,
+                          sycl::kernel_bundle<bundle_state::ext_oneapi_source>>
+    : std::true_type {};
+
+namespace detail {
+
+template <>
+struct PropertyToKind<sycl::ext::oneapi::experimental::build_options_key> {
+  static constexpr PropKind Kind = PropKind::BuildOptions;
+};
+
+template <>
+struct IsRuntimeProperty<sycl::ext::oneapi::experimental::build_options_key>
+    : std::true_type {};
+
+template <>
+struct IsCompileTimeProperty<sycl::ext::oneapi::experimental::build_options_key>
+    : std::false_type {};
+
+} // namespace detail
+
+/////////////////////////
+// PropertyT syclex::save_log
+/////////////////////////
+struct save_log {
+  std::string *log;
+  save_log(std::string *logArg) : log(logArg) {}
+};
+using save_log_key = save_log;
+
+template <> struct is_property_key<save_log_key> : std::true_type {};
+
+template <>
+struct is_property_key_of<save_log_key,
+                          sycl::kernel_bundle<bundle_state::ext_oneapi_source>>
+    : std::true_type {};
+
+namespace detail {
+
+template <>
+struct PropertyToKind<sycl::ext::oneapi::experimental::save_log_key> {
+  static constexpr PropKind Kind = PropKind::BuildLog;
+};
+
+template <>
+struct IsRuntimeProperty<sycl::ext::oneapi::experimental::save_log_key>
+    : std::true_type {};
+
+template <>
+struct IsCompileTimeProperty<sycl::ext::oneapi::experimental::save_log_key>
+    : std::false_type {};
+
+} // namespace detail
+
+/////////////////////////
+// syclex::is_source_kernel_bundle_supported
+/////////////////////////
+__SYCL_EXPORT bool is_source_kernel_bundle_supported(backend BE,
+                                                     source_language Language);
+
+/////////////////////////
+// syclex::create_kernel_bundle_from_source
+/////////////////////////
+__SYCL_EXPORT kernel_bundle<bundle_state::ext_oneapi_source>
+create_kernel_bundle_from_source(
+    const context &SyclContext,
+    sycl::ext::oneapi::experimental::source_language Language,
+    const std::string &Source);
+
+/////////////////////////
+// syclex::build(source_kb) => exe_kb
+/////////////////////////
+namespace detail {
+// forward decl
+__SYCL_EXPORT kernel_bundle<bundle_state::executable>
+build_from_source(kernel_bundle<bundle_state::ext_oneapi_source> &SourceKB,
+                  const std::vector<device> &Devices,
+                  const std::vector<std::string> &BuildOptions,
+                  std::string *LogPtr);
+
+} // namespace detail
+
+template <typename PropertyListT = detail::empty_properties_t,
+          typename = std::enable_if_t<
+              is_property_list_v<PropertyListT> &&
+              detail::all_props_are_keys_of<
+                  kernel_bundle<bundle_state::ext_oneapi_source>,
+                  PropertyListT>::value>>
+
+kernel_bundle<bundle_state::executable>
+build(kernel_bundle<bundle_state::ext_oneapi_source> &SourceKB,
+      const std::vector<device> &Devices, PropertyListT props = {}) {
+  std::vector<std::string> BuildOptionsVec;
+  std::string *LogPtr = nullptr;
+  if constexpr (props.template has_property<build_options>()) {
+    BuildOptionsVec = props.template get_property<build_options>().opts;
+  }
+  if constexpr (props.template has_property<save_log>()) {
+    LogPtr = props.template get_property<save_log>().log;
+  }
+  return detail::build_from_source(SourceKB, Devices, BuildOptionsVec, LogPtr);
+}
+
+template <typename PropertyListT = detail::empty_properties_t,
+          typename = std::enable_if_t<
+              is_property_list_v<PropertyListT> &&
+              detail::all_props_are_keys_of<
+                  kernel_bundle<bundle_state::ext_oneapi_source>,
+                  PropertyListT>::value>>
+kernel_bundle<bundle_state::executable>
+build(kernel_bundle<bundle_state::ext_oneapi_source> &SourceKB,
+      PropertyListT props = {}) {
+  return build<PropertyListT>(SourceKB, SourceKB.get_devices(), props);
+}
+
+} // namespace ext::oneapi::experimental
 
 } // namespace _V1
 } // namespace sycl

sycl/include/sycl/kernel_bundle_enums.hpp
@@ -11,7 +11,18 @@
 namespace sycl {
 inline namespace _V1 {
 
-enum class bundle_state : char { input = 0, object = 1, executable = 2 };
+enum class bundle_state : char {
+  input = 0,
+  object = 1,
+  executable = 2,
+  ext_oneapi_source = 3
+};
 
-}
+namespace ext::oneapi::experimental {
+
+enum class source_language : int { opencl = 0 /* sycl , spir-v, cuda */ };
+
+} // namespace ext::oneapi::experimental
+
+} // namespace _V1
 } // namespace sycl

commit 670861009076ddf0b1ff3245b6d28fb45c847c27

[SYCL] Fix regression introduced in https://github.com/intel/llvm/pull/11517 (#12002)
This patch fixes a regression caused by refactoring of `split_string`
function in https://github.com/intel/llvm/pull/11517.
The regression is the following: the delimiter is a space, input string
is `"A B C "` - which has a space at the end.
Before https://github.com/intel/llvm/pull/11517 `split_string` returned
`{"A", "B", "C"}` but with https://github.com/intel/llvm/pull/11517 it
returns `{"A", "B", "C", ""}`.
This regression affects multiple CTS tests, verified the fix locally,
and also unit tests for `split_string` function were added to this
patch. This log shows that one of the unit tests failed before applying
the fix:
https://github.com/intel/llvm/actions/runs/6974175043/job/18979466747#step:11:372.
With the fix it passes.

Fixes https://github.com/intel/llvm/issues/11996

sycl/source/detail/common.cpp
@@ -72,18 +72,24 @@ const char *stringifyErrorCode(pi_int32 error) {
 }
 
 std::vector<std::string> split_string(const std::string &str, char delimeter) {
-  std::vector<std::string> result;
-  size_t beg = 0;
-  size_t end = 0;
-  while ((end = str.find(delimeter, beg)) != std::string::npos) {
-    result.push_back(str.substr(beg, end - beg));
-    beg = end + 1;
+  std::vector<std::string> Result;
+  size_t Start = 0;
+  size_t End = 0;
+  while ((End = str.find(delimeter, Start)) != std::string::npos) {
+    Result.push_back(str.substr(Start, End - Start));
+    Start = End + 1;
   }
-  end = str.find('\0');
-  if (beg < end) {
-    result.push_back(str.substr(beg, end - beg));
+  // Get the last substring and ignore the null character so we wouldn't get
+  // double null characters \0\0 at the end of the substring
+  End = str.find('\0');
+  if (Start < End) {
+    std::string LastSubStr(str.substr(Start, End - Start));
+    // In case str has a delimeter at the end, the substring will be empty, so
+    // we shouldn't add it to the final vector
+    if (!LastSubStr.empty())
+      Result.push_back(LastSubStr);
   }
-  return result;
+  return Result;
 }
 
 } // namespace detail

sycl/unittests/kernel-and-program/DeviceInfo.cpp
@@ -235,3 +235,28 @@ TEST_F(DeviceInfoNegativeTest, TestAspectNotSupported) {
   EXPECT_EQ(Dev.has(aspect::ext_intel_memory_clock_rate), false);
   EXPECT_EQ(Dev.has(aspect::ext_intel_memory_bus_width), false);
 }
+
+TEST_F(DeviceInfoTest, SplitStringDelimeterSpace) {
+  std::string InputString("V1 V2 V3");
+  std::vector<std::string> Expected{"V1", "V2", "V3"};
+  EXPECT_EQ(detail::split_string(InputString, ' '), Expected);
+}
+
+TEST_F(DeviceInfoTest, SplitStringDelimeterSpaceAtTheEnd) {
+  std::string InputString("V1 V2 V3 ");
+  std::vector<std::string> Expected{"V1", "V2", "V3"};
+  EXPECT_EQ(detail::split_string(InputString, ' '), Expected);
+}
+
+TEST_F(DeviceInfoTest, SplitStringDelimeterSemicolon) {
+  std::string InputString("V1;V2;V3");
+  std::vector<std::string> Expected{"V1", "V2", "V3"};
+  EXPECT_EQ(detail::split_string(InputString, ';'), Expected);
+}
+
+TEST_F(DeviceInfoTest, SplitStringCheckNoDoubleNullCharacters) {
+  std::string InputString("V1;V23");
+  std::vector<std::string> Result = detail::split_string(InputString, ';');
+  EXPECT_EQ(Result[0].length(), (unsigned)2);
+  EXPECT_EQ(Result[1].length(), (unsigned)3);
+}