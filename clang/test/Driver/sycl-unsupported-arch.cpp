3d1639046fe9fe4e061c6beb3d00d215545712ca
Andrey Alekseenko <al42and@gmail.com>
2023-11-28 12:00:37 +0100

    Use AMDGCN target instead of NVPTX for testing
    Seems more stable

--- clang/test/Driver/sycl-unsupported-arch.cpp
+++ clang/test/Driver/sycl-unsupported-arch.cpp
@@ -1,8 +1,8 @@
 /// Verify that compiler passes are correctly determined
-// RUN: %clangxx -fsycl -fsycl-targets=nvptx64-nvidia-cuda -Xsycl-target-backend=nvptx64-nvidia-cuda --offload-arch=sm_30 -c -ccc-print-phases %s 2>&1 | FileCheck %s --check-prefix=CHECK-PHASES
-// CHECK-PHASES: offload, "device-sycl (nvptx64-nvidia-cuda:sm_30)"
+// RUN: %clangxx -fsycl -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend=amdgcn-amd-amdhsa --offload-arch=gfx600 -c -ccc-print-phases %s 2>&1 | FileCheck %s --check-prefix=CHECK-PHASES
+// CHECK-PHASES: offload, "device-sycl (amdgcn-amd-amdhsa:gfx600)"
 
 /// Verify that preprocessor works (#8112)
-// RUN: %clangxx -fsycl -fsycl-targets=nvptx64-nvidia-cuda -Xsycl-target-backend=nvptx64-nvidia-cuda --offload-arch=sm_30 -E %s 2>&1 | FileCheck %s --check-prefix=CHECK-PREPROCESSOR
-// CHECK-PREPROCESSOR: // __CLANG_OFFLOAD_BUNDLE____START__ sycl-nvptx64-nvidia-cuda-sm_30
+// RUN: %clangxx -fsycl -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend=amdgcn-amd-amdhsa --offload-arch=gfx600 -E %s 2>&1 | FileCheck %s --check-prefix=CHECK-PREPROCESSOR
+// CHECK-PREPROCESSOR: // __CLANG_OFFLOAD_BUNDLE____START__ sycl-amdgcn-amd-amdhsa-gfx600
 
