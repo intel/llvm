fca071220430a563cbafd0bc5b738899bbacb4a0
Artem Radzikhovskyy <artem.radzikhovskyy@intel.com>
2023-11-30 12:33:45 -0800

    Can apply MemoryINTEL annotation multiple times on same variable (#2230)
    According to SPIRV spec only one instance of a specific decoration can be on a SPIRV-Id, unless the decoration explicitly allows it.
    
    LLVM IR has no such limitation.
    
    MemoryINTEL can have two annotations on the same pointer in LLVM IR. When it comes to translating this to SPIRV, we can filter out the less precise annotation (DEFAULT) and only keep one that will be translated to a SPIRV decoration.
    
    Original commit:
    https://github.com/KhronosGroup/SPIRV-LLVM-Translator/commit/ff48393

--- llvm-spirv/lib/SPIRV/SPIRVWriter.cpp
+++ llvm-spirv/lib/SPIRV/SPIRVWriter.cpp
@@ -3242,6 +3242,28 @@ AnnotationDecorations tryParseAnnotationString(SPIRVModule *BM,
                        internal::DecorationCacheControlLoadINTEL) {
           Decorates.CacheControlVec.emplace_back(
               static_cast<Decoration>(DecorationKind), std::move(DecValues));
+        } else if (DecorationKind == DecorationMemoryINTEL) {
+          // SPIRV doesn't allow the same Decoration to be applied multiple
+          // times on a single SPIRVEntry, unless explicitly allowed by the
+          // language spec. Filter out the less specific MemoryINTEL
+          // decorations, if applied multiple times
+          auto CanFilterOut = [](auto &Val) {
+            if (!Val.second.empty())
+              return (Val.second[0] == "DEFAULT");
+            return false;
+          };
+          auto It = std::find_if(DecorationsVec.begin(), DecorationsVec.end(),
+                                 CanFilterOut);
+
+          if (It != DecorationsVec.end()) {
+            // replace the less specific decoration
+            *It = {static_cast<Decoration>(DecorationKind),
+                   std::move(DecValues)};
+          } else {
+            // add new decoration
+            DecorationsVec.emplace_back(static_cast<Decoration>(DecorationKind),
+                                        std::move(DecValues));
+          }
         } else {
           DecorationsVec.emplace_back(static_cast<Decoration>(DecorationKind),
                                       std::move(DecValues));
