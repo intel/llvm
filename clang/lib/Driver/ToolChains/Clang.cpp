1b89ba213b46583ac8f9cd783d0b5d7c5befa27e
Sudarsanam, Arvind <arvind.sudarsanam@intel.com>
2023-12-02 06:23:33 -0800

    [sycl-post-link][Driver] Pass in -emit-only-kernels-as-entry-point option by default
    Signed-off-by: Sudarsanam, Arvind <arvind.sudarsanam@intel.com>

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -10324,7 +10324,8 @@ void SYCLPostLink::ConstructJob(Compilation &C, const JobAction &JA,
   // On FPGA target we don't need non-kernel functions as entry points, because
   // it only increases amount of code for device compiler to handle, without any
   // actual benefits.
-  if (T.getArchName() == "spir64_fpga")
+  if ((T.getArchName() == "spir64_fpga") ||
+      !TCArgs.getLastArg(options::OPT_femit_sycl_external_funcs_as_entry_points))
     addArgs(CmdArgs, TCArgs, {"-emit-only-kernels-as-entry-points"});
 
   // OPT_fsycl_device_code_split is not checked as it is an alias to
--- clang/test/Driver/sycl-offload.cpp
+++ clang/test/Driver/sycl-offload.cpp
@@ -170,3 +170,11 @@
 /// Check if the clang with fsycl adds C++ libraries to the link line
 //  RUN:  %clang -### -target x86_64-unknown-linux-gnu -fsycl %s 2>&1 | FileCheck -check-prefix=CHECK-FSYCL-WITH-CLANG %s
 // CHECK-FSYCL-WITH-CLANG: "-lstdc++"
+
+/// Check selective passing of -emit-only-kernels-as-entry-points to sycl-post-link tool
+// RUN: %clang -### -target spir64_fpga -fsycl %s 2>&1 | FileCheck -check-prefix=CHECK_SYCL_POST_LINK_OPT_PASS %s
+// RUN: %clang -### -target spir64_gen -fsycl %s 2>&1 | FileCheck -check-prefix=CHECK_SYCL_POST_LINK_OPT_PASS %s
+// CHECK_SYCL_POST_LINK_OPT_PASS: sycl-post-link{{.*}}emit-only-kernels-as-entry-points
+// RUN: %clang -### -target spir64_gen -femit-sycl-external-funcs-as-entry-points -fsycl %s 2>&1 | FileCheck -check-prefix=CHECK_SYCL_POST_LINK_OPT_NO_PASS %s
+// CHECK_SYCL_POST_LINK_OPT_NO_PASS-NOT: sycl-post-link{{.*}}emit-only-kernels-as-entry-points
+
