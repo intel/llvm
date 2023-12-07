a1924847f8f6c2e2eae5d63f695a1e51dab8d9d5
Zhao, Yang2 <yang2.zhao@intel.com>
2023-12-06 18:51:27 -0800

    add sanitizer test

---
+++ examples/sanitizer/helpers.cpp
@@ -0,0 +1,21 @@
+/*
+ *
+ * Copyright (C) 2023 Intel Corporation
+ *
+ * Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM Exceptions.
+ * See LICENSE.TXT
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
+#include "helpers.h"
+
+#include <fstream>
+#include <sstream>
+
+std::string generate_plus_one_spv() {
+    std::ifstream spv("../../examples/sanitizer/asan.spv");
+    std::stringstream buffer;
+    buffer << spv.rdbuf();
+
+    return buffer.str();
+}
