a314dd8cc8f573d7937adef74f7da71cb8379166
Igor Chorazewicz <igor.chorazewicz@intel.com>
2023-12-08 20:51:12 +0100

    [UR][L0] Fix tracing param in disjoint_pool_parser
    The disjoint_pool implementation distinguishes between
    a few different levels of logging (0-3). The argument
    should be an int, not bool.

--- source/common/umf_pools/disjoint_pool_config_parser.hpp
+++ source/common/umf_pools/disjoint_pool_config_parser.hpp
@@ -51,7 +51,7 @@ class DisjointPoolAllConfigs {
 // Example of usage:
 // "1;32M;host:1M,4,64K;device:1M,4,64K;shared:0,0,2M"
 DisjointPoolAllConfigs parseDisjointPoolConfig(const std::string &config,
-                                               bool trace = 1);
+                                               int trace = 1);
 } // namespace usm
 
 #endif
