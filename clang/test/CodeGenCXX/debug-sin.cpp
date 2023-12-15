eccdd8b8d83844a8f666cbe0d7a5665531b4eef5
Lu, John <john.lu@intel.com>
2023-12-15 13:17:05 -0800

    Windows does not generate debug info for sin.  Adapt test to this.
    Signed-off-by: Lu, John <john.lu@intel.com>

--- clang/test/CodeGenCXX/debug-sin.cpp
+++ clang/test/CodeGenCXX/debug-sin.cpp
@@ -8,7 +8,18 @@
 // RUN: %clang                   -emit-llvm -S -g %s     -o %t.default.ll
 // RUN: %clang -fno-system-debug -emit-llvm -S -g %s     -o %t.no_system_debug.ll
 
-// RUN: FileCheck --check-prefix=CHECK-DEFAULT         %s < %t.default.ll
+//// FIXME
+// We want to run this command on both Windows and Linux
+//
+//    FileCheck --check-prefix=CHECK-DEFAULT         %s < %t.default.ll
+//
+// but debug info for "sin" is not generated on Windows.  Use the following
+// line to workaround Windows.  If Windows compiler is updated to be the
+// same as Linux this check will need to be updated.
+//
+// RUN: FileCheck %if system-windows %{ --check-prefix=CHECK-NO-SYSTEM-DEBUG %} %else %{ --check-prefix=CHECK-DEFAULT %} %s < %t.default.ll
+
+// No debug information for "sin" should be generated with -fno-system-debug
 // RUN: FileCheck --check-prefix=CHECK-NO-SYSTEM-DEBUG %s < %t.no_system_debug.ll
 
 // CHECK-DEFAULT:             DISubprogram(name: "sin",
