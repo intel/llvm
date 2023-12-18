778602538fc8f81dae81d4aac0a7aba0ebb7de36
PietroGhg <pietro.ghiglio@codeplay.com>
2023-12-18 08:58:32 +0000

    Update string output for backend type

--- sycl/include/sycl/backend_types.hpp
+++ sycl/include/sycl/backend_types.hpp
@@ -57,7 +57,7 @@ inline std::ostream &operator<<(std::ostream &Out, backend be) {
     Out << "ext_oneapi_hip";
     break;
   case backend::ext_oneapi_native_cpu:
-    Out << "ext_native_cpu";
+    Out << "ext_oneapi_native_cpu";
     break;
   case backend::all:
     Out << "all";
