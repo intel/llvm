4d7365765fce48d09a61b28cb340befad477786d
JackAKirk <jack.kirk@codeplay.com>
2024-01-03 06:34:09 -0500

    USM-P2P extension implementation for hip.
    Uses the latest non deprecated hip APIs.
    
    fully tested and passing via DPC++ using Crusher.
    
    Signed-off-by: JackAKirk <jack.kirk@codeplay.com>

--- source/adapters/hip/usm_p2p.cpp
+++ source/adapters/hip/usm_p2p.cpp
@@ -9,25 +9,59 @@
 //===----------------------------------------------------------------------===//
 
 #include "common.hpp"
+#include "context.hpp"
 
-UR_APIEXPORT ur_result_t UR_APICALL
-urUsmP2PEnablePeerAccessExp(ur_device_handle_t, ur_device_handle_t) {
-  detail::ur::die(
-      "urUsmP2PEnablePeerAccessExp is not implemented for HIP adapter.");
-  return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
+UR_APIEXPORT ur_result_t UR_APICALL urUsmP2PEnablePeerAccessExp(
+    ur_device_handle_t commandDevice, ur_device_handle_t peerDevice) {
+  ur_result_t result = UR_RESULT_SUCCESS;
+  try {
+    ScopedContext active(commandDevice);
+    UR_CHECK_ERROR(hipDeviceEnablePeerAccess(peerDevice->get(), 0));
+  } catch (ur_result_t err) {
+    result = err;
+  }
+  return result;
 }
 
-UR_APIEXPORT ur_result_t UR_APICALL
-urUsmP2PDisablePeerAccessExp(ur_device_handle_t, ur_device_handle_t) {
-  detail::ur::die(
-      "urUsmP2PDisablePeerAccessExp is not implemented for HIP adapter.");
-  return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
+UR_APIEXPORT ur_result_t UR_APICALL urUsmP2PDisablePeerAccessExp(
+    ur_device_handle_t commandDevice, ur_device_handle_t peerDevice) {
+  ur_result_t result = UR_RESULT_SUCCESS;
+  try {
+    ScopedContext active(commandDevice);
+    UR_CHECK_ERROR(hipDeviceDisablePeerAccess(peerDevice->get()));
+  } catch (ur_result_t err) {
+    result = err;
+  }
+  return result;
 }
 
 UR_APIEXPORT ur_result_t UR_APICALL urUsmP2PPeerAccessGetInfoExp(
-    ur_device_handle_t, ur_device_handle_t, ur_exp_peer_info_t, size_t propSize,
-    void *pPropValue, size_t *pPropSizeRet) {
+    ur_device_handle_t commandDevice, ur_device_handle_t peerDevice,
+    ur_exp_peer_info_t propName, size_t propSize, void *pPropValue,
+    size_t *pPropSizeRet) {
   UrReturnHelper ReturnValue(propSize, pPropValue, pPropSizeRet);
-  // Zero return value indicates that all of the queries currently return false.
-  return ReturnValue(uint32_t{0});
+
+  int value;
+  hipDeviceP2PAttr hip_attr;
+  try {
+    ScopedContext active(commandDevice);
+    switch (propName) {
+    case UR_EXP_PEER_INFO_UR_PEER_ACCESS_SUPPORTED: {
+      hip_attr = hipDevP2PAttrAccessSupported;
+      break;
+    }
+    case UR_EXP_PEER_INFO_UR_PEER_ATOMICS_SUPPORTED: {
+      hip_attr = hipDevP2PAttrNativeAtomicSupported;
+      break;
+    }
+    default: {
+      return UR_RESULT_ERROR_INVALID_ENUMERATION;
+    }
+    }
+    UR_CHECK_ERROR(hipDeviceGetP2PAttribute(
+        &value, hip_attr, commandDevice->get(), peerDevice->get()));
+  } catch (ur_result_t err) {
+    return err;
+  }
+  return ReturnValue(value);
 }
