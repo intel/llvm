3a05a4bd34e8e39f22d3f35a93120954a8d304cf
Dan Holmes <daniel.john.holmes@intel.com>
2024-01-04 15:44:14 +0000

    Adding tests for ODS function urDeviceGetSelected

--- test/conformance/device/urDeviceGetSelected.cpp
+++ test/conformance/device/urDeviceGetSelected.cpp
@@ -4,28 +4,28 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <uur/fixtures.h>
+#include "C:\Users\holmesda\git-repos\oneAPI-src\unified-runtime\test\unit\helpers.h"
 
 using urDeviceGetSelectedTest = uur::urPlatformTest;
 
+/* adpater agnostic tests -- none assume the existence or support of any specific adapter */
+
 TEST_F(urDeviceGetSelectedTest, Success) {
+    unsetenv("ONEAPI_DEVICE_SELECTOR");
     uint32_t count = 0;
-    ur_result_t res1 =
-        urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count);
-    ASSERT_EQ_RESULT(res1, UR_RESULT_SUCCESS);
+    ASSERT_SUCCESS(
+        urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count));
     ASSERT_NE(count, 0);
     std::vector<ur_device_handle_t> devices(count);
-    ASSERT_NE(devices.size(), 0);
-    ASSERT_NE(devices.data(), nullptr);
-    //FAIL() << "devices.size() = " << devices.size() << " whereas count = " << count;
-    ur_result_t res2 = urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, count,
-                                           devices.data(), nullptr);
-    ASSERT_EQ_RESULT(res2, UR_RESULT_SUCCESS);
+    ASSERT_SUCCESS(urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, count,
+                                       devices.data(), nullptr));
     for (auto &device : devices) {
         ASSERT_NE(nullptr, device);
     }
 }
 
 TEST_F(urDeviceGetSelectedTest, SuccessSubsetOfDevices) {
+    unsetenv("ONEAPI_DEVICE_SELECTOR");
     uint32_t count = 0;
     ASSERT_SUCCESS(
         urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count));
@@ -41,7 +41,96 @@ TEST_F(urDeviceGetSelectedTest, SuccessSubsetOfDevices) {
     }
 }
 
+TEST_F(urDeviceGetSelectedTest, SuccessSelected_StarColonStar) {
+    setenv("ONEAPI_DEVICE_SELECTOR", "*:*", 1);
+    uint32_t count = 0;
+    ASSERT_SUCCESS(
+        urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count));
+    ASSERT_NE(count, 0);
+    std::vector<ur_device_handle_t> devices(count);
+    ASSERT_SUCCESS(urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, count,
+                                       devices.data(), nullptr));
+    for (auto &device : devices) {
+        ASSERT_NE(nullptr, device);
+    }
+
+    uint32_t countAll = 0;
+    ASSERT_SUCCESS(
+        urDeviceGet(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &countAll));
+    ASSERT_NE(countAll, 0);
+    ASSERT_EQ(countAll, count);
+    std::vector<ur_device_handle_t> devicesAll(countAll);
+    ASSERT_SUCCESS(urDeviceGet(platform, UR_DEVICE_TYPE_ALL, countAll,
+                                       devicesAll.data(), nullptr));
+    for (auto &device : devicesAll) {
+        ASSERT_NE(nullptr, device);
+    }
+
+    for (size_t i = 0; i < count; ++i) {
+        ASSERT_EQ(devices[i], devicesAll[i]);
+    }
+}
+
+TEST_F(urDeviceGetSelectedTest, SuccessSelected_StarColonZero) {
+    setenv("ONEAPI_DEVICE_SELECTOR", "*:0", 1);
+    uint32_t count = 0;
+    ASSERT_SUCCESS(
+        urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count));
+    ASSERT_NE(count, 0);
+    std::vector<ur_device_handle_t> devices(count);
+    ASSERT_SUCCESS(urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, count,
+                                       devices.data(), nullptr));
+    for (auto &device : devices) {
+        ASSERT_NE(nullptr, device);
+    }
+}
+
+TEST_F(urDeviceGetSelectedTest, SuccessSelected_StarColonZeroCommaStar) {
+    setenv("ONEAPI_DEVICE_SELECTOR", "*:0,*", 1);
+    uint32_t count = 0;
+    ASSERT_SUCCESS(
+        urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count));
+    ASSERT_NE(count, 0);
+    std::vector<ur_device_handle_t> devices(count);
+    ASSERT_SUCCESS(urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, count,
+                                       devices.data(), nullptr));
+    for (auto &device : devices) {
+        ASSERT_NE(nullptr, device);
+    }
+}
+
+TEST_F(urDeviceGetSelectedTest, SuccessSelected_DiscardStarColonStar) {
+    setenv("ONEAPI_DEVICE_SELECTOR", "!*:*", 1);
+    uint32_t count = 0;
+    ASSERT_SUCCESS(
+        urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count));
+    ASSERT_EQ(count, 0);
+}
+
+TEST_F(urDeviceGetSelectedTest, SuccessSelected_SelectAndDiscard) {
+    setenv("ONEAPI_DEVICE_SELECTOR", "*:0;!*:*", 1);
+    uint32_t count = 0;
+    ASSERT_SUCCESS(
+        urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count));
+    ASSERT_EQ(count, 0);
+}
+
+TEST_F(urDeviceGetSelectedTest, SuccessSelected_SelectSomethingAndDiscardSomethingElse) {
+    setenv("ONEAPI_DEVICE_SELECTOR", "*:0;!*:1", 1);
+    uint32_t count = 0;
+    ASSERT_SUCCESS(
+        urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count));
+    ASSERT_NE(count, 0);
+    std::vector<ur_device_handle_t> devices(count);
+    ASSERT_SUCCESS(urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, count,
+                                       devices.data(), nullptr));
+    for (auto &device : devices) {
+        ASSERT_NE(nullptr, device);
+    }
+}
+
 TEST_F(urDeviceGetSelectedTest, InvalidNullHandlePlatform) {
+    unsetenv("ONEAPI_DEVICE_SELECTOR");
     uint32_t count = 0;
     ASSERT_EQ_RESULT(
         UR_RESULT_ERROR_INVALID_NULL_HANDLE,
@@ -49,6 +138,7 @@ TEST_F(urDeviceGetSelectedTest, InvalidNullHandlePlatform) {
 }
 
 TEST_F(urDeviceGetSelectedTest, InvalidEnumerationDevicesType) {
+    unsetenv("ONEAPI_DEVICE_SELECTOR");
     uint32_t count = 0;
     ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_ENUMERATION,
                      urDeviceGetSelected(platform, UR_DEVICE_TYPE_FORCE_UINT32,
@@ -56,6 +146,7 @@ TEST_F(urDeviceGetSelectedTest, InvalidEnumerationDevicesType) {
 }
 
 TEST_F(urDeviceGetSelectedTest, InvalidValueNumEntries) {
+    unsetenv("ONEAPI_DEVICE_SELECTOR");
     uint32_t count = 0;
     ASSERT_SUCCESS(
         urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count));
@@ -64,4 +155,93 @@ TEST_F(urDeviceGetSelectedTest, InvalidValueNumEntries) {
     ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_SIZE,
                      urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0,
                                          devices.data(), nullptr));
+}
+
+TEST_F(urDeviceGetSelectedTest, InvalidMissingBackend) {
+    setenv("ONEAPI_DEVICE_SELECTOR", ":garbage", 1);
+    uint32_t count = 0;
+    ASSERT_EQ_RESULT(
+        UR_RESULT_ERROR_UNKNOWN,
+        urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count));
+    ASSERT_EQ(count, 0);
+}
+
+TEST_F(urDeviceGetSelectedTest, InvalidGarbageBackendString) {
+    setenv("ONEAPI_DEVICE_SELECTOR", "garbage:0", 1);
+    uint32_t count = 0;
+    ASSERT_EQ_RESULT(
+        UR_RESULT_ERROR_INVALID_VALUE,
+        urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count));
+    ASSERT_EQ(count, 0);
+}
+
+TEST_F(urDeviceGetSelectedTest, InvalidMissingFilterStrings) {
+    setenv("ONEAPI_DEVICE_SELECTOR", "*", 1);
+    uint32_t count = 0;
+    ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_VALUE,
+        urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count));
+    ASSERT_EQ(count, 0);
+    setenv("ONEAPI_DEVICE_SELECTOR", "*:", 1);
+    uint32_t count2 = 0;
+    ASSERT_EQ_RESULT(
+        UR_RESULT_ERROR_INVALID_VALUE,
+        urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count2));
+    ASSERT_EQ(count2, 0);
+}
+
+TEST_F(urDeviceGetSelectedTest, InvalidMissingFilterString) {
+    setenv("ONEAPI_DEVICE_SELECTOR", "*:0,,2", 1);
+    uint32_t count = 0;
+    ASSERT_EQ_RESULT(
+        UR_RESULT_ERROR_UNKNOWN,
+        urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count));
+    ASSERT_EQ(count, 0);
+}
+
+TEST_F(urDeviceGetSelectedTest, InvalidTooManyDotsInFilterString) {
+    setenv("ONEAPI_DEVICE_SELECTOR", "*:0.1.2.3", 1);
+    uint32_t count = 0;
+    ASSERT_EQ_RESULT(
+        UR_RESULT_ERROR_INVALID_VALUE,
+        urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count));
+    ASSERT_EQ(count, 0);
+}
+
+TEST_F(urDeviceGetSelectedTest, InvalidBadWildardInFilterString) {
+    setenv("ONEAPI_DEVICE_SELECTOR", "*:*.", 1);
+    uint32_t count = 0;
+    ASSERT_EQ_RESULT(
+        UR_RESULT_ERROR_INVALID_VALUE,
+        urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count));
+    ASSERT_EQ(count, 0);
+    setenv("ONEAPI_DEVICE_SELECTOR", "*:*.0", 1);
+    uint32_t count2 = 0;
+    ASSERT_EQ_RESULT(
+        UR_RESULT_ERROR_INVALID_VALUE,
+        urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count2));
+    ASSERT_EQ(count2, 0);
+}
+
+TEST_F(urDeviceGetSelectedTest, InvalidSelectingNonexistentDevice) {
+    setenv("ONEAPI_DEVICE_SELECTOR", "*:4321", 1);
+    uint32_t count = 0;
+    ASSERT_SUCCESS(
+        urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count));
+    ASSERT_EQ(count, 0);
+}
+
+TEST_F(urDeviceGetSelectedTest, InvalidSelectingNonexistentSubDevice) {
+    setenv("ONEAPI_DEVICE_SELECTOR", "*:0.4321", 1);
+    uint32_t count = 0;
+    ASSERT_SUCCESS(
+        urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count));
+    ASSERT_EQ(count, 0);
+}
+
+TEST_F(urDeviceGetSelectedTest, InvalidSelectingNonexistentSubSubDevice) {
+    setenv("ONEAPI_DEVICE_SELECTOR", "*:0.0.4321", 1);
+    uint32_t count = 0;
+    ASSERT_SUCCESS(
+        urDeviceGetSelected(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &count));
+    ASSERT_EQ(count, 0);
 }
