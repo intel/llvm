d28865ad07f17309d87c07ab29e53ba3e9e7e7b0
Vyacheslav Levytskyy <89994100+VyacheslavLevytskyy@users.noreply.github.com>
2024-01-04 04:24:33 -0800

    Remove CoordX and CoordY arguments of OpCooperativeMatrixPrefetchINTEL
    According to a new specification, CoordX and CoordY parameters are not needed for prefetch in CooperativeMatrixPrefetchINTEL(), only offset pointer is enough. This PR is to fix support for joint_matrix_prefetch in SPIRV according to the new specification.
    
    Original commit:
    https://github.com/KhronosGroup/SPIRV-LLVM-Translator/commit/c0e19d6

--- llvm-spirv/lib/SPIRV/libSPIRV/SPIRVInstruction.h
+++ llvm-spirv/lib/SPIRV/libSPIRV/SPIRVInstruction.h
@@ -3438,7 +3438,7 @@ protected:
   typedef SPIRVInstTemplate<SPIRVCooperativeMatrixPrefetchINTELInstBase,       \
                             internal::Op##x##INTEL, __VA_ARGS__>               \
       SPIRV##x##INTEL;
-_SPIRV_OP(CooperativeMatrixPrefetch, false, 8, true, 5)
+_SPIRV_OP(CooperativeMatrixPrefetch, false, 6, true, 3)
 #undef _SPIRV_OP
 
 class SPIRVCooperativeMatrixInvocationInstructionsINTELInstBase
