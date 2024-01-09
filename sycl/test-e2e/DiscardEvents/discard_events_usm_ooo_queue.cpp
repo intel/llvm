5033ef2ee4aeb16c856d43c1d2dd23a4ce46c86a
Georgi Mirazchiyski <georgi.mirazchiyski@codeplay.com>
2024-01-09 12:41:08 +0000

    Clarify why the -996 errc is an acceptable result.

--- sycl/test-e2e/DiscardEvents/discard_events_usm_ooo_queue.cpp
+++ sycl/test-e2e/DiscardEvents/discard_events_usm_ooo_queue.cpp
@@ -5,10 +5,11 @@
 // The test checks that the last parameter is not `nullptr` for all PI calls
 // that should discard events.
 // {{0|0000000000000000}} is required for various output on Linux and Windows.
-// NOTE: piextUSMEnqueuePrefetch in the CUDA backend may return a warning
-//       result on Windows with error-code -996
-//       (PI_ERROR_PLUGIN_SPECIFIC_ERROR). Since it is a warning it is safe to
-//       ignore for this test.
+// NOTE: piextUSMEnqueuePrefetch and piextUSMEnqueueMemAdvise in the CUDA and
+//       HIP backends may return a warning result on Windows with error-code
+//       -996 (PI_ERROR_PLUGIN_SPECIFIC_ERROR) if USM managed memory is not
+//       supported or if unsupported advice flags are used for the latter API.
+//       Since it is a warning it is safe to ignore for this test.
 //
 // Everything that follows TestQueueOperations()
 // CHECK: ---> piextUSMEnqueueMemset(
