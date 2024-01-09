4cae6f2c59d097b26c2a07c8d24b366207a11060
Ewan Crawford <ewan@codeplay.com>
2024-01-09 14:45:55 +0000

    Use `FileCheck --implicit-check-not=LEAK`

--- sycl/test-e2e/Graph/RecordReplay/usm_memset.cpp
+++ sycl/test-e2e/Graph/RecordReplay/usm_memset.cpp
@@ -1,9 +1,7 @@
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 // Extra run to check for leaks in Level Zero using UR_L0_LEAKS_DEBUG
-// RUN: %if level_zero %{env UR_L0_LEAKS_DEBUG=1 %{run} %t.out 2>&1 | FileCheck %s %}
-//
-// CHECK-NOT: LEAK
+// RUN: %if level_zero %{env UR_L0_LEAKS_DEBUG=1 %{run} %t.out 2>&1 | FileCheck --implicit-check-not=LEAK %s %}
 
 // USM memset command not supported for OpenCL
 // UNSUPPORTED: opencl
