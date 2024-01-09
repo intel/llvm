d04dcaa9c48006c8ad328c7f26e3656a2ab11239
Chris Perkins <chris.perkins@intel.com>
2024-01-09 11:43:46 -0800

    using sycl::bit_cast when possible

--- sycl/include/sycl/types.hpp
+++ sycl/include/sycl/types.hpp
@@ -157,7 +157,10 @@ template <> struct vec_helper<sycl::ext::oneapi::bfloat16> {
   using RetType = sycl::ext::oneapi::bfloat16;
   using BFloat16StorageT = sycl::ext::oneapi::detail::Bfloat16StorageT;
   static constexpr RetType get(BFloat16StorageT value) {
-    // awkward workaround. sycl::bit_cast isn't constexpr on Win.
+#if defined(__SYCL_BITCAST_IS_CONSTEXPR)
+    return sycl::bit_cast<RetType>(value);
+#else
+    // awkward workaround. sycl::bit_cast isn't constexpr in older GCC
     // C++20 will give us both std::bit_cast and constexpr reinterpet for void*
     // but neither available yet.
     union {
@@ -166,17 +169,22 @@ template <> struct vec_helper<sycl::ext::oneapi::bfloat16> {
     } result = {};
     result.storage = value;
     return result.bf16;
+#endif
   }
 
   static constexpr RetType get(RetType value) { return value; }
 
   static constexpr BFloat16StorageT set(RetType value) {
+#if defined(__SYCL_BITCAST_IS_CONSTEXPR)
+    return sycl::bit_cast<BFloat16StorageT>(value);
+#else
     union {
       sycl::ext::oneapi::bfloat16 bf16;
       sycl::ext::oneapi::detail::Bfloat16StorageT storage;
     } result = {};
     result.bf16 = value;
     return result.storage;
+#endif
   }
 };
 
