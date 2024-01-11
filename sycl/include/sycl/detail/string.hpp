20448f715cc5f249f3e6abf499d12a8f9401654a
Byoungro So <byoungro.so@intel.com>
2024-01-10 18:08:20 -0800

    added comments
    Signed-off-by: Byoungro So <byoungro.so@intel.com>

--- sycl/include/sycl/detail/string.hpp
+++ sycl/include/sycl/detail/string.hpp
@@ -12,6 +12,19 @@ namespace sycl {
 inline namespace _V1 {
 namespace detail {
 
+// This class is intended to support different ABIs between libsycl and
+// the user program.
+// One most important issue is different ABIs for std::string.
+// To address this issue we define sycl::detail::string class.
+// There are two occasions that crosses the ABI boundaries.
+// Once from the user program to the libsycl side, where we need to
+// pass the existing std::string to libsycl. This one uses the member 'str'.
+// Another occasion is returning std::string from libsycl to the user program.
+// In this case, sycl::detail::string provides a placeholder pointer, ret_str.
+// libsycl will allocated memory and assign the pointer to it.
+// These two boundary crossing can happen in one place.
+// For example, an API can pass a std::string as a parameter and returns
+// a std::string. That's why we need two separate members, 'str' and 'ret_str'.
 class string {
   const char *str;         // used to send existing std::string to libsycl
   char *ret_str = nullptr; // set from libsycl
@@ -23,10 +36,13 @@ public:
 
   bool operator==(const char *st) { return strcmp(str, st) == 0; }
 
+  // Once libsycl passes ret_str, we need to reconstruct std::string
+  // to return to the user program.
   std::string marshall() { return std::string(ret_str); }
 
   std::string marshall() const { return std::string(ret_str); }
 
+  // Libsycl calls this method to reconstruct std::string from 'str'
   void unmarshall(std::string &strn) { strcpy(ret_str, strn.c_str()); }
 
   void allocate(int size) { ret_str = new char[size]; }
