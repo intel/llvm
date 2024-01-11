95f48f8c2ead08814af9387934d7700fcafc5e5b
Byoungro So <byoungro.so@intel.com>
2024-01-10 18:24:37 -0800

    adjusted comment
    Signed-off-by: Byoungro So <byoungro.so@intel.com>

--- sycl/include/sycl/detail/string.hpp
+++ sycl/include/sycl/detail/string.hpp
@@ -14,20 +14,21 @@ namespace detail {
 
 // This class is intended to support different ABIs between libsycl and
 // the user program.
-// One most important issue is different ABIs for std::string.
-// To address this issue we define sycl::detail::string class.
-// There are two occasions that crosses the ABI boundaries.
+// One most important issue is different ABIs for std::string before
+// C++11-ABI and after C++11-ABI.
+// To address this issue, we define sycl::detail::string class.
+// There are two occasions that std::string crosses the ABI boundaries.
 // Once from the user program to the libsycl side, where we need to
 // pass the existing std::string to libsycl. This one uses the member 'str'.
 // Another occasion is returning std::string from libsycl to the user program.
 // In this case, sycl::detail::string provides a placeholder pointer, ret_str.
 // libsycl will allocated memory and assign the pointer to it.
-// These two boundary crossing can happen in one place.
-// For example, an API can pass a std::string as a parameter and returns
+// These two boundary crossing cases can happen in one place.
+// For example, an API can pass a std::string as a parameter and return
 // a std::string. That's why we need two separate members, 'str' and 'ret_str'.
 class string {
-  const char *str;         // used to send existing std::string to libsycl
-  char *ret_str = nullptr; // set from libsycl
+  const char *str;         // used to send user's owing std::string to libsycl
+  char *ret_str = nullptr; // set from libsycl to return a std::string to user
 
 public:
   string() : str(nullptr), ret_str(nullptr) {}
