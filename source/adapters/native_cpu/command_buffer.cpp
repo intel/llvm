348215f43c62e32e67591be3b579dc5a6efcf271
PietroGhg <pietro.ghiglio@codeplay.com>
2024-01-12 14:06:47 +0000

    Empty implementation for command buffer fill

--- source/adapters/native_cpu/command_buffer.cpp
+++ source/adapters/native_cpu/command_buffer.cpp
@@ -133,3 +133,26 @@ UR_APIEXPORT ur_result_t UR_APICALL urCommandBufferEnqueueExp(
                   "implemented for the NativeCPU adapter.");
   return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
 }
+
+UR_APIEXPORT ur_result_t UR_APICALL urCommandBufferAppendMemBufferFillExp(
+    ur_exp_command_buffer_handle_t, ur_mem_handle_t,
+    const void *, size_t, size_t, size_t,
+    uint32_t,
+    const ur_exp_command_buffer_sync_point_t *,
+    ur_exp_command_buffer_sync_point_t *) {
+  detail::ur::die("Experimental Command-buffer feature is not "
+                  "implemented for the NativeCPU adapter.");
+  return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
+}
+
+UR_APIEXPORT ur_result_t UR_APICALL urCommandBufferAppendUSMFillExp(
+     ur_exp_command_buffer_handle_t,
+     void *,  const void *,
+     size_t,  size_t,
+     uint32_t,
+     const ur_exp_command_buffer_sync_point_t *,
+     ur_exp_command_buffer_sync_point_t *) {
+  detail::ur::die("Experimental Command-buffer feature is not "
+                  "implemented for the NativeCPU adapter.");
+  return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
+}
