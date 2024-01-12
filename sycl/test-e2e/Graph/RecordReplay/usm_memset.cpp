d414e3881cb7773b1c5852fe45504e32fcd88eca
Maxime France-Pillois <maxime.francepillois@codeplay.com>
2024-01-12 15:28:20 +0000

    Adds tests buffer fill and usm fill with imm-cmd-list

--- sycl/test-e2e/Graph/RecordReplay/usm_memset.cpp
+++ sycl/test-e2e/Graph/RecordReplay/usm_memset.cpp
@@ -2,6 +2,8 @@
 // RUN: %{run} %t.out
 // Extra run to check for leaks in Level Zero using UR_L0_LEAKS_DEBUG
 // RUN: %if level_zero %{env UR_L0_LEAKS_DEBUG=1 %{run} %t.out 2>&1 | FileCheck --implicit-check-not=LEAK %s %}
+// Extra run to check for immediate-command-list in Level Zero
+// RUN: %if level_zero && linux %{env SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=1 UR_L0_LEAKS_DEBUG=1 %{run} %t.out 2>&1 | FileCheck %s --implicit-check-not=LEAK %}
 
 // USM memset command not supported for OpenCL
 // UNSUPPORTED: opencl
