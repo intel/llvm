3177d1b1f43cddb6a757d44474134764b4821768
Hugh Delaney <hugh.delaney@codeplay.com>
2024-01-16 15:04:24 +0000

    add_native_events second basic test working
    Able to call event.wait() and queue.wait() but cannot use plugin
    dependencies directly

--- sycl/source/detail/event_impl.hpp
+++ sycl/source/detail/event_impl.hpp
@@ -336,10 +336,10 @@ public:
     return MEventFromSubmittedExecCommandBuffer;
   }
   void addHostTaskPiEvent(detail::pi::PiEvent Event) {
-    HostTaskPiEvents.push_back(Event);
+    MHostTaskPiEvents.push_back(Event);
   }
 
-  bool backendSet() const { return HostTaskPiEvents.size() > 0; }
+  bool backendSet() const { return MHostTaskPiEvents.size() > 0; }
 
   void setProfilingEnabled(bool Value) { MIsProfilingEnabled = Value; }
 
@@ -395,7 +395,7 @@ protected:
 
   // Used to hold pi_events for native events that are stored with
   // interop_handle::add_native_events
-  std::vector<sycl::detail::pi::PiEvent> HostTaskPiEvents;
+  std::vector<sycl::detail::pi::PiEvent> MHostTaskPiEvents;
 
   /// Dependency events prepared for waiting by backend.
   std::vector<EventImplPtr> MPreparedDepsEvents;
