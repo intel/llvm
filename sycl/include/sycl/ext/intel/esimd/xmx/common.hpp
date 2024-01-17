32e7858418e41996be80da8cfa5b1fa47d703cf1
Klochkov, Vyacheslav N <vyacheslav.n.klochkov@intel.com>
2024-01-16 17:11:57 -0800

    Fix clang-format
    Signed-off-by: Klochkov, Vyacheslav N <vyacheslav.n.klochkov@intel.com>

--- sycl/include/sycl/ext/intel/esimd/xmx/common.hpp
+++ sycl/include/sycl/ext/intel/esimd/xmx/common.hpp
@@ -23,15 +23,15 @@ enum class dpas_argument_type {
   Invalid = 0,
   u1 __SYCL_DEPRECATED("u1 is reserved/unsupported") = 1, // unsigned 1 bit
   s1 __SYCL_DEPRECATED("s1 is reserved/unsupported") = 2, // signed 1 bit
-  u2 = 3,    // unsigned 2 bits
-  s2 = 4,    // signed 2 bits
-  u4 = 5,    // unsigned 4 bits
-  s4 = 6,    // signed 4 bits
-  u8 = 7,    // unsigned 8 bits
-  s8 = 8,    // signed 8 bits
-  bf16 = 9,  // bfloat 16
-  fp16 = 10, // half float
-  tf32 = 12, // tensorfloat 32
+  u2 = 3,                                                 // unsigned 2 bits
+  s2 = 4,                                                 // signed 2 bits
+  u4 = 5,                                                 // unsigned 4 bits
+  s4 = 6,                                                 // signed 4 bits
+  u8 = 7,                                                 // unsigned 8 bits
+  s8 = 8,                                                 // signed 8 bits
+  bf16 = 9,                                               // bfloat 16
+  fp16 = 10,                                              // half float
+  tf32 = 12,                                              // tensorfloat 32
 };
 
 } // namespace ext::intel::esimd::xmx
