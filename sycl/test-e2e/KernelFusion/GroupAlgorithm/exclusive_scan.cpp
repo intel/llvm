1a3ee2d061d0cadab35bff4545cbad4e6761e125
Victor Perez <victor.perez@codeplay.com>
2024-01-17 09:40:07 +0000

    Avoid weird kernel names

--- sycl/test-e2e/KernelFusion/GroupAlgorithm/exclusive_scan.cpp
+++ sycl/test-e2e/KernelFusion/GroupAlgorithm/exclusive_scan.cpp
@@ -18,17 +18,18 @@ using namespace sycl;
 template <class SpecializationKernelName, int TestNumber>
 class exclusive_scan_kernel;
 
-template <typename SpecializationKernelName, typename InputContainer,
-          typename OutputContainer, class BinaryOperation>
+template <typename BinaryOperation> class K0;
+template <typename BinaryOperation> class K1;
+template <typename BinaryOperation> class K2;
+template <typename BinaryOperation> class K3;
+
+template <typename InputContainer, typename OutputContainer,
+          class BinaryOperation>
 void test(queue q, InputContainer input, OutputContainer output,
           BinaryOperation binary_op,
           typename OutputContainer::value_type identity) {
   typedef typename InputContainer::value_type InputT;
   typedef typename OutputContainer::value_type OutputT;
-  typedef class exclusive_scan_kernel<SpecializationKernelName, 0> kernel_name0;
-  typedef class exclusive_scan_kernel<SpecializationKernelName, 1> kernel_name1;
-  typedef class exclusive_scan_kernel<SpecializationKernelName, 2> kernel_name2;
-  typedef class exclusive_scan_kernel<SpecializationKernelName, 3> kernel_name3;
   OutputT init = 42;
   size_t N = input.size();
   size_t G = 64;
@@ -44,11 +45,12 @@ void test(queue q, InputContainer input, OutputContainer output,
     q.submit([&](handler &cgh) {
       accessor in{in_buf, cgh, sycl::read_only};
       accessor out{out_buf, cgh, sycl::write_only, sycl::no_init};
-      cgh.parallel_for<kernel_name0>(nd_range<1>(G, G), [=](nd_item<1> it) {
-        group<1> g = it.get_group();
-        int lid = it.get_local_id(0);
-        out[lid] = exclusive_scan_over_group(g, in[lid], binary_op);
-      });
+      cgh.parallel_for<K0<BinaryOperation>>(
+          nd_range<1>(G, G), [=](nd_item<1> it) {
+            group<1> g = it.get_group();
+            int lid = it.get_local_id(0);
+            out[lid] = exclusive_scan_over_group(g, in[lid], binary_op);
+          });
     });
 
     complete_fusion_with_check(
@@ -73,11 +75,12 @@ void test(queue q, InputContainer input, OutputContainer output,
     q.submit([&](handler &cgh) {
       accessor in{in_buf, cgh, sycl::read_only};
       accessor out{out_buf, cgh, sycl::write_only, sycl::no_init};
-      cgh.parallel_for<kernel_name1>(nd_range<1>(G, G), [=](nd_item<1> it) {
-        group<1> g = it.get_group();
-        int lid = it.get_local_id(0);
-        out[lid] = exclusive_scan_over_group(g, in[lid], init, binary_op);
-      });
+      cgh.parallel_for<K1<BinaryOperation>>(
+          nd_range<1>(G, G), [=](nd_item<1> it) {
+            group<1> g = it.get_group();
+            int lid = it.get_local_id(0);
+            out[lid] = exclusive_scan_over_group(g, in[lid], init, binary_op);
+          });
     });
 
     complete_fusion_with_check(
@@ -102,13 +105,14 @@ void test(queue q, InputContainer input, OutputContainer output,
     q.submit([&](handler &cgh) {
       accessor in{in_buf, cgh, sycl::read_only};
       accessor out{out_buf, cgh, sycl::write_only, sycl::no_init};
-      cgh.parallel_for<kernel_name2>(nd_range<1>(G, G), [=](nd_item<1> it) {
-        group<1> g = it.get_group();
-        joint_exclusive_scan(
-            g, in.template get_multi_ptr<access::decorated::no>(),
-            in.template get_multi_ptr<access::decorated::no>() + N,
-            out.template get_multi_ptr<access::decorated::no>(), binary_op);
-      });
+      cgh.parallel_for<K2<BinaryOperation>>(
+          nd_range<1>(G, G), [=](nd_item<1> it) {
+            group<1> g = it.get_group();
+            joint_exclusive_scan(
+                g, in.template get_multi_ptr<access::decorated::no>(),
+                in.template get_multi_ptr<access::decorated::no>() + N,
+                out.template get_multi_ptr<access::decorated::no>(), binary_op);
+          });
     });
     complete_fusion_with_check(
         fw, ext::codeplay::experimental::property::no_barriers{});
@@ -131,14 +135,15 @@ void test(queue q, InputContainer input, OutputContainer output,
     q.submit([&](handler &cgh) {
       accessor in{in_buf, cgh, sycl::read_only};
       accessor out{out_buf, cgh, sycl::write_only, sycl::no_init};
-      cgh.parallel_for<kernel_name3>(nd_range<1>(G, G), [=](nd_item<1> it) {
-        group<1> g = it.get_group();
-        joint_exclusive_scan(
-            g, in.template get_multi_ptr<access::decorated::no>(),
-            in.template get_multi_ptr<access::decorated::no>() + N,
-            out.template get_multi_ptr<access::decorated::no>(), init,
-            binary_op);
-      });
+      cgh.parallel_for<K3<BinaryOperation>>(
+          nd_range<1>(G, G), [=](nd_item<1> it) {
+            group<1> g = it.get_group();
+            joint_exclusive_scan(
+                g, in.template get_multi_ptr<access::decorated::no>(),
+                in.template get_multi_ptr<access::decorated::no>() + N,
+                out.template get_multi_ptr<access::decorated::no>(), init,
+                binary_op);
+          });
     });
     complete_fusion_with_check(
         fw, ext::codeplay::experimental::property::no_barriers{});
@@ -160,24 +165,17 @@ int main() {
   std::array<int, N> output;
   std::fill(output.begin(), output.end(), 0);
 
-  test<class KernelNamePlusV>(q, input, output, sycl::plus<>(), 0);
-  test<class KernelNameMinimumV>(q, input, output, sycl::minimum<>(),
-                                 std::numeric_limits<int>::max());
-  test<class KernelNameMaximumV>(q, input, output, sycl::maximum<>(),
-                                 std::numeric_limits<int>::lowest());
-
-  test<class KernelNamePlusI>(q, input, output, sycl::plus<int>(), 0);
-  test<class KernelNameMinimumI>(q, input, output, sycl::minimum<int>(),
-                                 std::numeric_limits<int>::max());
-  test<class KernelNameMaximumI>(q, input, output, sycl::maximum<int>(),
-                                 std::numeric_limits<int>::lowest());
-  test<class KernelName_VzAPutpBRRJrQPB>(q, input, output,
-                                         sycl::multiplies<int>(), 1);
-  test<class KernelName_UXdGbr>(q, input, output, sycl::bit_or<int>(), 0);
-  test<class KernelName_saYaodNyJknrPW>(q, input, output, sycl::bit_xor<int>(),
-                                        0);
-  test<class KernelName_GPcuAlvAOjrDyP>(q, input, output, sycl::bit_and<int>(),
-                                        ~0);
+  test(q, input, output, sycl::plus<>(), 0);
+  test(q, input, output, sycl::minimum<>(), std::numeric_limits<int>::max());
+  test(q, input, output, sycl::maximum<>(), std::numeric_limits<int>::lowest());
+  test(q, input, output, sycl::plus<int>(), 0);
+  test(q, input, output, sycl::minimum<int>(), std::numeric_limits<int>::max());
+  test(q, input, output, sycl::maximum<int>(),
+       std::numeric_limits<int>::lowest());
+  test(q, input, output, sycl::multiplies<int>(), 1);
+  test(q, input, output, sycl::bit_or<int>(), 0);
+  test(q, input, output, sycl::bit_xor<int>(), 0);
+  test(q, input, output, sycl::bit_and<int>(), ~0);
 
   std::cout << "Test passed." << std::endl;
 }
