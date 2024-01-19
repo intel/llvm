7e215a3c0caec3d05d4e20eaea4fc9d228810680
Maxime France-Pillois <maxime.francepillois@codeplay.com>
2024-01-19 14:58:01 +0000

    Update sycl/unittests/Extensions/CommandGraph.cpp
    Co-authored-by: Marcos Maronas <maarquitos14@users.noreply.github.com>

--- sycl/unittests/Extensions/CommandGraph.cpp
+++ sycl/unittests/Extensions/CommandGraph.cpp
@@ -1350,8 +1350,8 @@ TEST_F(CommandGraphTest, InOrderQueueMemcpyAndGraph) {
       InOrderQueue.memcpy(TestData, TestDataHost.data(), Size * sizeof(int));
   auto EventLastImpl = sycl::detail::getSyclObjImpl(EventLast);
   auto EventLastWaitList = EventLastImpl->getWaitList();
-  // Previous task is not an host task. In Order queue dependency are managed by
-  // the backend for non-host kernels
+  // Previous task is not a host task. In Order queue dependency is managed by
+  // the backend for non-host kernels.
   ASSERT_EQ(EventLastWaitList.size(), 0lu);
 }
 
