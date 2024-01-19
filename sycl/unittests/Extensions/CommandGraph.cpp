d70ac0ca117e1af27f722ba3cd252cfff0defe95
Maxime France-Pillois <maxime.francepillois@codeplay.com>
2024-01-19 14:59:39 +0000

    Update sycl/unittests/Extensions/CommandGraph.cpp
    Co-authored-by: Marcos Maronas <maarquitos14@users.noreply.github.com>

--- sycl/unittests/Extensions/CommandGraph.cpp
+++ sycl/unittests/Extensions/CommandGraph.cpp
@@ -1220,7 +1220,7 @@ TEST_F(CommandGraphTest, InOrderQueueMemsetAndGraph) {
   auto EventInitial = InOrderQueue.memset(TestData, 1, Size * sizeof(int));
   auto EventInitialImpl = sycl::detail::getSyclObjImpl(EventInitial);
 
-  // Record in-order queue with three nodes
+  // Record in-order queue with three nodes.
   InOrderGraph.begin_recording(InOrderQueue);
   auto Node1Graph = InOrderQueue.submit(
       [&](sycl::handler &cgh) { cgh.single_task<TestKernel<>>([]() {}); });
