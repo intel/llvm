88e2c31a4ee16a2ba8988a8f44a93e7e8d9bf4ae
Lorenc Bushi <lorenc.bushi@intel.com>
2024-01-18 16:44:04 -0800

    Throw when USM not supported!

--- sycl/source/detail/usm/usm_impl.cpp
+++ sycl/source/detail/usm/usm_impl.cpp
@@ -253,9 +253,9 @@ void *alignedAlloc(size_t Alignment, size_t Size, const context &Ctxt,
       (uint16_t)xpti::trace_point_type_t::mem_alloc_begin);
 #endif
   if (Kind == alloc::device &&
-    !Dev.has(sycl::aspect::usm_device_allocations)) {
-        throw sycl::exception(sycl::errc::feature_not_supported,
-            "Device does not support Unified Shared Memory!");
+        !Dev.has(sycl::aspect::usm_device_allocations)) {
+            throw sycl::exception(sycl::errc::feature_not_supported,
+                "Device does not support Unified Shared Memory!");
   }
   void *RetVal =
       alignedAllocInternal(Alignment, Size, getSyclObjImpl(Ctxt).get(),
