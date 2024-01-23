cee972ca6fc48d4903c82dcdab75252bf4ab2205
Lu, John <john.lu@intel.com>
2024-01-23 12:49:39 -0800

    Test typedefs in stdint.h which is available on Windows
    Signed-off-by: Lu, John <john.lu@intel.com>

--- clang/test/CodeGen/debug-info-system-typedef.c
+++ clang/test/CodeGen/debug-info-system-typedef.c
@@ -4,22 +4,31 @@
 // typedef-ed in system headers.  Thus, the increased debuggability
 // is worth the small extra cost.
 
-// Windows does not have <unistd.h>
-// UNSUPPORTED: system-windows
-
 // RUN: %clang -fno-system-debug -emit-llvm -S -g %s -o %t.ll
 
 // RUN: FileCheck %s < %t.ll
 
-// CHECK: !DIDerivedType(tag: DW_TAG_typedef, name: "gid_t",
-// CHECK: !DIDerivedType(tag: DW_TAG_typedef, name: "__gid_t",
-// CHECK: !DIDerivedType(tag: DW_TAG_typedef, name: "uid_t",
-// CHECK: !DIDerivedType(tag: DW_TAG_typedef, name: "__uid_t",
-  
-#include <unistd.h>
+// CHECK-DAG: !DIDerivedType(tag: DW_TAG_typedef, name: "int_fast8_t",
+// CHECK-DAG: !DIDerivedType(tag: DW_TAG_typedef, name: "int_fast16_t",
+// CHECK-DAG: !DIDerivedType(tag: DW_TAG_typedef, name: "int_fast32_t",
+// CHECK-DAG: !DIDerivedType(tag: DW_TAG_typedef, name: "int_fast64_t",
 
-uid_t xuid;
-gid_t xgid;
+// CHECK-DAG: !DIDerivedType(tag: DW_TAG_typedef, name: "uint_fast8_t",
+// CHECK-DAG: !DIDerivedType(tag: DW_TAG_typedef, name: "uint_fast16_t",
+// CHECK-DAG: !DIDerivedType(tag: DW_TAG_typedef, name: "uint_fast32_t",
+// CHECK-DAG: !DIDerivedType(tag: DW_TAG_typedef, name: "uint_fast64_t",
+  
+#include <stdint.h>
+
+int_fast8_t   f8;
+int_fast16_t  f16;
+int_fast32_t  f32;
+int_fast64_t  f64;
+
+uint_fast8_t   uf8;
+uint_fast16_t  uf16;
+uint_fast32_t  uf32;
+uint_fast64_t  uf64;
 
 int
 main (void)
