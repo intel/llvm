c53de1d03a050454adab4e6c30ebeada755500ad
Zahira Ammarguellat <zahira.ammarguellat@intel.com>
2024-01-23 07:31:15 -0800

    Edited LIT test as suggested by reviewer.

--- sycl/test/check_device_code/fp-accuracy.cpp
+++ sycl/test/check_device_code/fp-accuracy.cpp
@@ -1,47 +1,29 @@
-// RUN: %clangxx -fsycl %s -ffp-accuracy=high -fno-math-errno \
-// RUN: -S -emit-llvm -o- | FileCheck %s
+// DEFINE: %{common_opts} = -fsycl -fsycl-device-only -fno-math-errno \
+// DEFINE: -ffp-accuracy=high -S -emit-llvm -o - %s
 
-// RUN: %clangxx -fsycl %s -ffp-accuracy=high -ffp-accuracy=low:exp \
-// RUN: -fno-math-errno -S -emit-llvm -o- | FileCheck --check-prefix=CHECK-F1 %s
+// RUN: %clangxx %{common_opts} | FileCheck %s
+
+// RUN: %clangxx %{common_opts} -ffp-accuracy=low:exp \
+// RUN: | FileCheck %s --check-prefix=CHECK-LOW-EXP
 
 #include <sycl/sycl.hpp>
-using namespace sycl;
 
-#define sz 2
-double input[sz];
-double output[sz];
-
-int main() {
-  queue deviceQueue;
-  double Value = 5.;
-
-  range<1> size{sz};
-
-  buffer<double, 1> in(input, size);
-  buffer<double, 1> out(output, size);
-
-  deviceQueue.submit([&](handler &cgh) {
-    accessor in_vals{in, cgh, read_only};
-    accessor out_vals{out, cgh, write_only};
-    cgh.single_task<class Kernel1>([=]() {
-      for (int i = 0; i < sz; i++)
-        out_vals[i] = cos(exp(log(in_vals[i])));
-    });
-  });
-  return 0;
+SYCL_EXTERNAL auto foo(double x) {
+  using namespace sycl;
+  return cos(exp(log(x)));
 }
 
-// CHECK-LABEL: define {{.*}}spir_kernel void @{{.*}}Kernel1
-// CHECK: tail call double @llvm.fpbuiltin.log.f64(double %2) #[[ATTR_HIGH:[0-9]+]]
-// CHECK: tail call double @llvm.fpbuiltin.exp.f64(double %3) #[[ATTR_HIGH]]
-// CHECK: tail call double @llvm.fpbuiltin.cos.f64(double %4) #[[ATTR_HIGH]]
+// CHECK-LABEL: define {{.*}}food
+// CHECK: tail call double @llvm.fpbuiltin.log.f64(double {{.*}}) #[[ATTR_HIGH:[0-9]+]]
+// CHECK: tail call double @llvm.fpbuiltin.exp.f64(double {{.*}}) #[[ATTR_HIGH]]
+// CHECK: tail call double @llvm.fpbuiltin.cos.f64(double {{.*}}) #[[ATTR_HIGH]]
 
 // CHECK: attributes #[[ATTR_HIGH]] = {{.*}}"fpbuiltin-max-error"="1.0"
 
-// CHECK-F1-LABEL: define {{.*}}spir_kernel void @{{.*}}Kernel1
-// CHECK-F1: tail call double @llvm.fpbuiltin.log.f64(double {{.*}}) #[[ATTR_F1_HIGH:[0-9]+]]
-// CHECK-F1: tail call double @llvm.fpbuiltin.exp.f64(double {{.*}}) #[[ATTR_F1_LOW:[0-9]+]]
-// CHECK-F1: tail call double @llvm.fpbuiltin.cos.f64(double {{.*}}) #[[ATTR_F1_HIGH]]
+// CHECK-LOW-EXP-LABEL: define {{.*}}food
+// CHECK-LOW-EXP: tail call double @llvm.fpbuiltin.log.f64(double {{.*}}) #[[ATTR_F1_HIGH:[0-9]+]]
+// CHECK-LOW-EXP: tail call double @llvm.fpbuiltin.exp.f64(double {{.*}}) #[[ATTR_F1_LOW:[0-9]+]]
+// CHECK-LOW-EXP: tail call double @llvm.fpbuiltin.cos.f64(double {{.*}}) #[[ATTR_F1_HIGH]]
 
 // CHECK-F1: attributes #[[ATTR_F1_HIGH]] = {{.*}}"fpbuiltin-max-error"="1.0"
 // CHECK-F1: attributes #[[ATTR_F1_LOW]] = {{.*}}"fpbuiltin-max-error"="67108864.0"
