9abed8156fe25cd7ece890689ac128d42e3710dc
Michael Aziz <michael.aziz@intel.com>
2024-01-24 10:43:51 -0800

    Update interface loaders
    Signed-off-by: Michael Aziz <michael.aziz@intel.com>

--- source/adapters/opencl/ur_interface_loader.cpp
+++ source/adapters/opencl/ur_interface_loader.cpp
@@ -390,7 +390,8 @@ UR_DLLEXPORT ur_result_t UR_APICALL urGetEnqueueExpProcAddrTable(
     return result;
   }
 
-  pDdiTable->pfnCooperativeKernelLaunchExp = nullptr;
+  pDdiTable->pfnCooperativeKernelLaunchExp =
+      urEnqueueCooperativeKernelLaunchExp;
 
   return UR_RESULT_SUCCESS;
 }
@@ -402,7 +403,8 @@ UR_DLLEXPORT ur_result_t UR_APICALL urGetKernelExpProcAddrTable(
     return result;
   }
 
-  pDdiTable->pfnSuggestMaxCooperativeGroupCountExp = nullptr;
+  pDdiTable->pfnSuggestMaxCooperativeGroupCountExp =
+      urKernelSuggestMaxCooperativeGroupCountExp;
 
   return UR_RESULT_SUCCESS;
 }
