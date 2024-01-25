b318ee2afbda9c95e449402ae08dbe12b92c09ce
Kenneth Benzie (Benie) <k.benzie@codeplay.com>
2024-01-25 15:35:30 +0000

    [CTS] Fix resource leaks
    This patch introduces the `uur::raii::Wrapper<T>` object and per handle
    aliases (`uur::raii::Context` for `ur_context_handle_t` etc.) to replace
    instances of the following pattern in test bodies:
    
    ```cpp
    TEST(Context, Example) {
        ur_context_handle_t context = nullptr;
        ASSERT_SUCCESS(urContextCreate(1, &device, nullptr, &context));
    
        // potential resource leak as `ASSERT_*` returns from this function
        ASSERT_TRUE(might_be_false);
    
        urContextRelease(context);
    }
    ```
    
    With the following:
    
    ```cpp
    TEST(Context, Example) {
        uur::raii::Context context = nullptr;
        ASSERT_SUCCESS(urContextCreate(1, &device, nullptr, context.ptr()));
    
        // no leak because the destructor releases the context
        ASSERT_TRUE(might_be_false);
    }
    ```

--- test/conformance/virtual_memory/urPhysicalMemCreate.cpp
+++ test/conformance/virtual_memory/urPhysicalMemCreate.cpp
@@ -2,7 +2,8 @@
 // Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM Exceptions.
 // See LICENSE.TXT
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-#include <uur/fixtures.h>
+
+#include "uur/fixtures.h"
 
 struct urPhysicalMemCreateTest
     : uur::urVirtualMemGranularityTestWithParam<size_t> {
@@ -13,29 +14,33 @@ struct urPhysicalMemCreateTest
         size = getParam() * granularity;
     }
 
+    void TearDown() override {
+        if (physical_mem) {
+            ASSERT_SUCCESS(urPhysicalMemRelease(physical_mem));
+        }
+        uur::urVirtualMemGranularityTestWithParam<size_t>::TearDown();
+    }
+
     size_t size;
+    ur_physical_mem_handle_t physical_mem = nullptr;
 };
 
 UUR_TEST_SUITE_P(urPhysicalMemCreateTest, ::testing::Values(1, 2, 3, 7, 12, 44),
                  uur::deviceTestWithParamPrinter<size_t>);
 
 TEST_P(urPhysicalMemCreateTest, Success) {
-    ur_physical_mem_handle_t physical_mem = nullptr;
     ASSERT_SUCCESS(
         urPhysicalMemCreate(context, device, size, nullptr, &physical_mem));
     ASSERT_NE(physical_mem, nullptr);
-    EXPECT_SUCCESS(urPhysicalMemRelease(physical_mem));
 }
 
 TEST_P(urPhysicalMemCreateTest, InvalidNullHandleContext) {
-    ur_physical_mem_handle_t physical_mem = nullptr;
     ASSERT_EQ_RESULT(
         urPhysicalMemCreate(nullptr, device, size, nullptr, &physical_mem),
         UR_RESULT_ERROR_INVALID_NULL_HANDLE);
 }
 
 TEST_P(urPhysicalMemCreateTest, InvalidNullHandleDevice) {
-    ur_physical_mem_handle_t physical_mem = nullptr;
     ASSERT_EQ_RESULT(
         urPhysicalMemCreate(context, nullptr, size, nullptr, &physical_mem),
         UR_RESULT_ERROR_INVALID_NULL_HANDLE);
@@ -52,7 +57,6 @@ TEST_P(urPhysicalMemCreateTest, InvalidSize) {
         GTEST_SKIP()
             << "A granularity of 1 means that any size will be accepted.";
     }
-    ur_physical_mem_handle_t physical_mem = nullptr;
     size_t invalid_size = size - 1;
     ASSERT_EQ_RESULT(urPhysicalMemCreate(context, device, invalid_size, nullptr,
                                          &physical_mem),
