7cac7c850eeda7c912ff7b8d1c53a28f3c09cafa
Zhao, Yang2 <yang2.zhao@intel.com>
2024-01-25 03:36:34 -0800

    add license

--- source/loader/layers/sanitizer/linux/san_utils.cpp
+++ source/loader/layers/sanitizer/linux/san_utils.cpp
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+/*
+ *
+ * Copyright (C) 2023 Intel Corporation
+ *
+ * Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM Exceptions.
+ * See LICENSE.TXT
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ *
+ * @file san_utils.cpp
+ *
+ */
+
 #include "common.hpp"
 #include <sys/mman.h>
 #include <asm/param.h>
