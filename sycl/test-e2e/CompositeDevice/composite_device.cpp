b25e46e3658e5fe9f11612cc60b80cb8363d2f43
Maronas, Marcos <marcos.maronas@intel.com>
2024-01-25 09:58:31 -0800

    Update failing tests from unsupported to expected fail.
    Signed-off-by: Maronas, Marcos <marcos.maronas@intel.com>

--- sycl/test-e2e/CompositeDevice/composite_device.cpp
+++ sycl/test-e2e/CompositeDevice/composite_device.cpp
@@ -2,7 +2,7 @@
 // RUN: env ZE_FLAT_DEVICE_HIERARCHY=COMBINED %{run} %t.out
 // RUN: env ZE_FLAT_DEVICE_HIERARCHY=COMPOSITE %{run} %t.out
 // RUN: env ZE_FLAT_DEVICE_HIERARCHY=FLAT %{run} %t.out
-// UNSUPPORTED: (windows && level_zero)
+// XFAIL: (windows && level_zero)
 
 #include <sycl/sycl.hpp>
 
