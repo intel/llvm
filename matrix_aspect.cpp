8d47336f319b511bd02d24c63878f701a92bcfda
Lorenc Bushi <lorenc.bushi@intel.com>
2024-01-26 10:05:07 -0800

    Add support for ext_intel_matrix aspect

--- matrix_aspect.cpp
+++
@@ -1,9 +0,0 @@
-#include <sycl/sycl.hpp>
-
-using namespace sycl;
-int main() {
-   queue q;
-   auto d = q.get_device();
-   std::cout << std::boolalpha << d.has(aspect::ext_intel_matrix);
-   return 0;
-}
