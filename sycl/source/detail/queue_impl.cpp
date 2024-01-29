0f6996f7bbf2238ec8e143fa5350cb96f3690473
Sergey Semenov <sergey.semenov@intel.com>
2024-01-29 09:36:53 -0800

    Appease clang-format

--- sycl/source/detail/queue_impl.cpp
+++ sycl/source/detail/queue_impl.cpp
@@ -321,10 +321,9 @@ areEventsSafeForSchedulerBypass(const std::vector<sycl::event> &DepEvents,
     return SyclEventImplPtr->getHandleRef() != nullptr;
   };
 
-  return std::all_of(DepEvents.begin(), DepEvents.end(),
-                     [&CheckEvent](const sycl::event &Event) {
-                       return CheckEvent(Event);
-                     });
+  return std::all_of(
+      DepEvents.begin(), DepEvents.end(),
+      [&CheckEvent](const sycl::event &Event) { return CheckEvent(Event); });
 }
 
 template <typename HandlerFuncT>
