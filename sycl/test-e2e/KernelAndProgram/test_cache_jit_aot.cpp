908427f014a091df19a1dc28de61d4e746a9e3e0
Andrei Elovikov <andrei.elovikov@intel.com>
2024-02-02 15:38:48 -0800

    ocloc must be present to support AOT

--- sycl/test-e2e/KernelAndProgram/test_cache_jit_aot.cpp
+++ sycl/test-e2e/KernelAndProgram/test_cache_jit_aot.cpp
@@ -1,7 +1,7 @@
 // Don't use normal %{run} as we need to control cache directory removal and
 // cannot do that reliably when number of devices is unknown.
 //
-// REQUIRES: level_zero
+// REQUIRES: level_zero, ocloc
 //
 // DEFINE: %{cache_vars} = env SYCL_CACHE_PERSISTENT=1 SYCL_CACHE_TRACE=1 SYCL_CACHE_DIR=%t/cache_dir
 // DEFINE: %{build_cmd} = %{build}
