intel/llvm

commit 7e9819da0e095f2e41061eb8be9b51a4caae048c

[SYCL] Refactor builtins implementation (#11956)
See `builtins_preview.hpp` for the outline of the new design. This PR
changes the implementation under `-fpreview-breaking-changes` and
removes reliance on a python builtins generator script.

Suggested reading/review order: `builtins_preview.hpp`,
`helper_macros.hpp`,
`host_helper_macros.hpp`, then headers implementing user-visible side
with
the library implementation `sycl/source/builtins/*_functions.cpp` last.

sycl/include/sycl/detail/builtins/relational_functions.inc
@@ -1,0 +1,264 @@
+//==------------------- relational_functions.hpp ---------------------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// Intentionally insufficient set of includes and no "#pragma once".
+
+#include <sycl/detail/builtins/helper_macros.hpp>
+
+namespace sycl {
+inline namespace _V1 {
+namespace detail {
+template <typename T>
+struct rel_ret_traits
+    : std::conditional<is_scalar_arithmetic_v<T>, bool,
+                       std::conditional_t<
+                           is_marray_v<T>, marray<bool, num_elements<T>::value>,
+                           same_size_signed_int_t<simplify_if_swizzle_t<T>>>> {
+};
+} // namespace detail
+BUILTIN_CREATE_ENABLER(builtin_enable_rel, rel_ret_traits, fp_elem_type,
+                       non_scalar_only, same_elem_type)
+
+namespace detail {
+#ifdef __SYCL_DEVICE_ONLY__
+template <typename FuncTy, typename... Ts>
+auto builtin_device_rel_impl(FuncTy F, const Ts &...xs) {
+  using T = typename first_type<Ts...>::type;
+  if constexpr (detail::is_vec_or_swizzle_v<T>) {
+    // decltype(ret) is signed char ext_vector_type(N). Convert it to
+    // sycl::vec<signed char, N> first and then to the required return type of
+    // the relation builtin (vector of int16_t/int32_t/int64_t depending on the
+    // arguments' element type).
+    auto ret = F(builtins::convert_arg(xs)...);
+    vec<signed char, num_elements<T>::value> tmp{ret};
+    using res_elem_type =
+        make_type_t<get_elem_type_t<T>, type_list<int16_t, int32_t, int64_t>>;
+    static_assert(is_scalar_arithmetic_v<res_elem_type>);
+    return tmp.template convert<res_elem_type>();
+  } else if constexpr (std::is_same_v<T, half>) {
+    return bool{F(builtins::convert_arg(xs)...)};
+  } else {
+    static_assert(!detail::is_swizzle_v<T>);
+    return F(builtins::convert_arg(xs)...);
+  }
+}
+#endif
+
+template <typename FuncTy, typename... Ts>
+auto builtin_delegate_rel_impl(FuncTy F, const Ts &...x) {
+  using T = typename first_type<Ts...>::type;
+  if constexpr ((... || is_swizzle_v<Ts>)) {
+    return F(simplify_if_swizzle_t<T>{x}...);
+  } else if constexpr (is_vec_v<T>) {
+    // TODO: using Res{} to avoid Werror. Not sure if ok.
+    vec<same_size_signed_int_t<get_elem_type_t<T>>, T::size()> Res{};
+    detail::loop<T::size()>(
+        [&](auto idx) { Res[idx] = F(x[idx]...) ? -1 : 0; });
+    return Res;
+  } else {
+    // marray.
+    marray<bool, T::size()> Res;
+    // TODO: Can we optimize this? Note that using vector version isn't
+    // straightforward as it doesn't return booleans.
+    detail::loop<T::size()>([&](auto idx) { Res[idx] = F(x[idx]...); });
+    return Res;
+  }
+}
+} // namespace detail
+
+#ifdef __SYCL_DEVICE_ONLY__
+#define BUILTIN_REL(NUM_ARGS, NAME, SPIRV_IMPL)                                \
+  bool NAME(NUM_ARGS##_TYPE_ARG(float)) { return SPIRV_IMPL(NUM_ARGS##_ARG); } \
+  bool NAME(NUM_ARGS##_TYPE_ARG(double)) {                                     \
+    return SPIRV_IMPL(NUM_ARGS##_ARG);                                         \
+  }                                                                            \
+  bool NAME(NUM_ARGS##_TYPE_ARG(half)) {                                       \
+    return SPIRV_IMPL(NUM_ARGS##_CONVERTED_ARG);                               \
+  }                                                                            \
+  template <NUM_ARGS##_TYPENAME_TYPE>                                          \
+  detail::builtin_enable_rel_t<NUM_ARGS##_TEMPLATE_TYPE> NAME(                 \
+      NUM_ARGS##_TEMPLATE_TYPE_ARG) {                                          \
+    if constexpr (detail::is_marray_v<T0>)                                     \
+      return detail::builtin_delegate_rel_impl(                                \
+          [&](NUM_ARGS##_AUTO_ARG) { return NAME(NUM_ARGS##_ARG); },           \
+          NUM_ARGS##_ARG);                                                     \
+    else                                                                       \
+      return detail::builtin_device_rel_impl(                                  \
+          [&](NUM_ARGS##_AUTO_ARG) { return SPIRV_IMPL(NUM_ARGS##_ARG); },     \
+          NUM_ARGS##_ARG);                                                     \
+  }
+#else
+#define BUILTIN_REL(NUM_ARGS, NAME, SPIRV_IMPL)                                \
+  FOR_EACH3(HOST_IMPL_SCALAR_RET_TYPE, NUM_ARGS, NAME, bool, FP_TYPES)         \
+  HOST_IMPL_TEMPLATE_CUSTOM_DELEGATOR(NUM_ARGS, NAME, builtin_enable_rel_t,    \
+                                      rel, rel_ret_traits,                     \
+                                      builtin_delegate_rel_impl)
+#endif
+
+BUILTIN_REL(TWO_ARGS, isequal, __spirv_FOrdEqual)
+BUILTIN_REL(TWO_ARGS, isnotequal, __spirv_FUnordNotEqual)
+BUILTIN_REL(TWO_ARGS, isgreater, __spirv_FOrdGreaterThan)
+BUILTIN_REL(TWO_ARGS, isgreaterequal, __spirv_FOrdGreaterThanEqual)
+BUILTIN_REL(TWO_ARGS, isless, __spirv_FOrdLessThan)
+BUILTIN_REL(TWO_ARGS, islessequal, __spirv_FOrdLessThanEqual)
+BUILTIN_REL(TWO_ARGS, islessgreater, __spirv_FOrdNotEqual)
+BUILTIN_REL(ONE_ARG, isfinite, __spirv_IsFinite)
+BUILTIN_REL(ONE_ARG, isinf, __spirv_IsInf)
+BUILTIN_REL(ONE_ARG, isnan, __spirv_IsNan)
+BUILTIN_REL(ONE_ARG, isnormal, __spirv_IsNormal)
+BUILTIN_REL(TWO_ARGS, isordered, __spirv_Ordered)
+BUILTIN_REL(TWO_ARGS, isunordered, __spirv_Unordered)
+BUILTIN_REL(ONE_ARG, signbit, __spirv_SignBitSet)
+
+#undef BUILTIN_REL
+
+#ifdef __SYCL_DEVICE_ONLY__
+DEVICE_IMPL_TEMPLATE(THREE_ARGS, bitselect, builtin_enable_generic_t,
+                     __spirv_ocl_bitselect)
+#else
+HOST_IMPL_TEMPLATE(THREE_ARGS, bitselect, builtin_enable_generic_t, rel,
+                   default_ret_type)
+#endif
+
+namespace detail {
+template <typename T>
+struct builtin_enable_rel_all_any
+    : std::enable_if<(is_marray_v<T> &&
+                      std::is_same_v<get_elem_type_t<T>, bool>) ||
+                         (is_vec_or_swizzle_v<T> &&
+                          check_type_in_v<get_elem_type_t<T>, int8_t, int16_t,
+                                          int32_t, int64_t>),
+                     std::conditional_t<is_marray_v<T>, bool, int>> {};
+
+template <typename T>
+struct builtin_enable_rel_all_any_deprecated
+    : std::enable_if<((is_scalar_arithmetic_v<T> || is_marray_v<T>)) &&
+                         check_type_in_v<get_elem_type_t<T>, signed char, short,
+                                         int, long, long long>,
+                     bool> {};
+} // namespace detail
+
+template <typename T>
+typename detail::builtin_enable_rel_all_any<T>::type any(T x) {
+  if constexpr (detail::is_marray_v<T>) {
+    return std::any_of(x.begin(), x.end(), [](bool x) { return x; });
+  } else {
+    for (size_t i = 0; i < detail::num_elements<T>::value; ++i)
+      if (detail::msbIsSet(x[i]))
+        return true;
+    return false;
+  }
+}
+
+template <typename T>
+__SYCL_DEPRECATED("This overload is deprecated in SYCL 2020.")
+typename detail::builtin_enable_rel_all_any_deprecated<T>::type any(T x) {
+  if constexpr (detail::is_marray_v<T>) {
+    return std::any_of(x.begin(), x.end(),
+                       [](auto x) { return detail::msbIsSet(x); });
+  } else {
+    return detail::msbIsSet(x);
+  }
+}
+
+template <typename T>
+typename detail::builtin_enable_rel_all_any<T>::type all(T x) {
+  if constexpr (detail::is_marray_v<T>) {
+    return std::all_of(x.begin(), x.end(), [](bool x) { return x; });
+  } else {
+    for (size_t i = 0; i < detail::num_elements<T>::value; ++i)
+      if (!detail::msbIsSet(x[i]))
+        return false;
+    return true;
+  }
+}
+
+template <typename T>
+__SYCL_DEPRECATED("This overload is deprecated in SYCL 2020.")
+typename detail::builtin_enable_rel_all_any_deprecated<T>::type all(T x) {
+  if constexpr (detail::is_marray_v<T>) {
+    return std::all_of(x.begin(), x.end(),
+                       [](auto x) { return detail::msbIsSet(x); });
+  } else {
+    return detail::msbIsSet(x);
+  }
+}
+namespace detail {
+template <typename T>
+inline constexpr bool is_rel_generic_scalar_v =
+    check_type_in_v<T, char, signed char, short, int, long, long long,
+                    unsigned char, unsigned short, unsigned int, unsigned long,
+                    unsigned long long, float, double, half>;
+template <typename T>
+inline constexpr bool is_rel_vector_elem_type_v =
+    check_type_in_v<T, int8_t, int16_t, int32_t, int64_t, uint8_t, uint16_t,
+                    uint32_t, uint64_t, float, double, half>;
+
+template <typename T0, typename T1, typename T2>
+struct rel_enable_select_marray_helper {
+  static constexpr bool check_T0 =
+      is_marray_v<T0> && is_rel_generic_scalar_v<get_elem_type_t<T0>>;
+  static constexpr bool check_T1 = std::is_same_v<T0, T1>;
+  static constexpr bool check_T2 =
+      is_marray_v<T2> && std::is_same_v<get_elem_type_t<T2>, bool> &&
+      num_elements<T0>::value == num_elements<T2>::value;
+
+  static constexpr bool value = check_T0 && check_T1 && check_T2;
+};
+
+template <typename T0, typename T1, typename T2>
+struct rel_enable_select_vec_helper {
+  using T0_simplified = simplify_if_swizzle_t<T0>;
+  using T1_simplified = simplify_if_swizzle_t<T1>;
+  using T2_simplified = simplify_if_swizzle_t<T2>;
+  using T0_elem_type = get_elem_type_t<T0>;
+  using T2_elem_type = get_elem_type_t<T2>;
+
+  static constexpr bool check_T0 =
+      is_vec_or_swizzle_v<T0> && is_rel_vector_elem_type_v<get_elem_type_t<T0>>;
+  static constexpr bool check_T1 = std::is_same_v<T0_simplified, T1_simplified>;
+  static constexpr bool check_T2 =
+      is_vec_or_swizzle_v<T2> &&
+      num_elements<T0>::value == num_elements<T2>::value &&
+      std ::is_integral_v<T2_elem_type> &&
+      sizeof(T0_elem_type) == sizeof(T2_elem_type);
+  static constexpr bool value = check_T0 && check_T1 && check_T2;
+};
+
+template <typename T0, typename T1, typename T2>
+inline constexpr bool rel_enable_select_v =
+    detail::rel_enable_select_marray_helper<T0, T1, T2>::value ||
+    detail::rel_enable_select_vec_helper<T0, T1, T2>::value;
+} // namespace detail
+
+// __spirv_ocl_select doesn't behave as required by SYCL/OpenCL spec for vector
+// data types (MSB-related stuff).
+template <typename T>
+std::enable_if_t<detail::is_rel_generic_scalar_v<T>, T> select(T a, T b,
+                                                               bool c) {
+  return (c ? b : a);
+}
+
+template <typename T0, typename T1, typename T2>
+std::enable_if_t<detail::rel_enable_select_v<T0, T1, T2>,
+                 detail::simplify_if_swizzle_t<T0>>
+select(T0 a, T1 b, T2 c) {
+  if constexpr (detail::is_marray_v<T0>) {
+    T0 ret;
+    for (size_t i = 0; i < T0::size(); ++i)
+      ret[i] = (c[i] ? b[i] : a[i]);
+    return ret;
+  } else {
+    detail::simplify_if_swizzle_t<T0> ret;
+    for (size_t i = 0; i < ret.size(); ++i)
+      ret[i] = (detail::msbIsSet(c[i]) ? b[i] : a[i]);
+    return ret;
+  }
+}
+} // namespace _V1
+} // namespace sycl

sycl/include/sycl/builtins.hpp
@@ -12,10 +12,7 @@
 
 #ifdef __INTEL_PREVIEW_BREAKING_CHANGES
 
-// Include the generated builtins.
-#include <sycl/builtins_marray_gen.hpp>
-#include <sycl/builtins_scalar_gen.hpp>
-#include <sycl/builtins_vector_gen.hpp>
+#include <sycl/builtins_preview.hpp>
 
 #else // __INTEL_PREVIEW_BREAKING_CHANGES
 

commit dc6c7376471b50f3fc559be7e1e39e77eb0763db

[SYCL] Fix bitselect builtin for integer types
This regressed after #11956 as return type wasn't correctly converted
from SPIR-V intrinsic back to SYCL types. This PR fixes that.

In addition, I'm also adding tests for `sycl::select` builtin that was
left unaffected only because we couldn't use SPIR-V intrinsic for its
implementation.

sycl/include/sycl/detail/builtins/relational_functions.inc
@@ -118,8 +118,12 @@ BUILTIN_REL(ONE_ARG, signbit, __spirv_SignBitSet)
 #undef BUILTIN_REL
 
 #ifdef __SYCL_DEVICE_ONLY__
-DEVICE_IMPL_TEMPLATE(THREE_ARGS, bitselect, builtin_enable_generic_t,
-                     __spirv_ocl_bitselect)
+DEVICE_IMPL_TEMPLATE(
+    THREE_ARGS, bitselect, builtin_enable_generic_t, [](auto... xs) {
+      using ret_ty = detail::builtin_enable_generic_t<THREE_ARGS_TEMPLATE_TYPE>;
+      using detail::builtins::convert_result;
+      return convert_result<ret_ty>(__spirv_ocl_bitselect(xs...));
+    })
 #else
 HOST_IMPL_TEMPLATE(THREE_ARGS, bitselect, builtin_enable_generic_t, rel,
                    default_ret_type)

sycl/test-e2e/Basic/built-ins/marray_relational.cpp
@@ -111,6 +111,61 @@ int main() {
   TEST2(sycl::any, int, EXPECTED(bool, false), 3, ma7);
   TEST(sycl::bitselect, float, EXPECTED(float, 1.0, 1.0), 2, ma8, ma9, ma10);
   TEST(sycl::select, float, EXPECTED(float, 1.0, 2.0, 8.0), 3, ma5, ma6, c);
+  {
+    // Extra tests for select/bitselect due to special handling required for
+    // integer return types.
+
+    auto Test = [&](auto F, auto Expected, auto... Args) {
+      std::tuple ArgsTuple{Args...};
+      auto Result = std::apply(F, ArgsTuple);
+      static_assert(std::is_same_v<decltype(Expected), decltype(Result)>);
+
+      // Note: operator==(vec, vec) return vec.
+      auto Equal = [](auto x, auto y) {
+        return std::equal(x.begin(), x.end(), y.begin());
+      };
+
+      assert(Equal(Result, Expected));
+
+      sycl::buffer<bool, 1> ResultBuf{1};
+      deviceQueue.submit([&](sycl::handler &cgh) {
+        sycl::accessor Result{ResultBuf, cgh};
+        cgh.single_task([=]() {
+          auto R = std::apply(F, ArgsTuple);
+          static_assert(std::is_same_v<decltype(Expected), decltype(R)>);
+          Result[0] = Equal(R, Expected);
+        });
+      });
+      assert(sycl::host_accessor{ResultBuf}[0]);
+    };
+
+    sycl::marray<char, 2> a{0b1100, 0b0011};
+    sycl::marray<char, 2> b{0b0011, 0b1100};
+    sycl::marray<char, 2> c{0b1010, 0b1010};
+    sycl::marray<char, 2> r{0b0110, 0b1001};
+
+    auto BitSelect = [](auto... xs) { return sycl::bitselect(xs...); };
+    Test(BitSelect, r, a, b, c);
+    // Input values/results above are positive, so use the same values for
+    // signed/unsigned char tests.
+    [&](auto... xs) {
+      Test(BitSelect, sycl::marray<signed char, 2>{xs}...);
+    }(r, a, b, c);
+    [&](auto... xs) {
+      Test(BitSelect, sycl::marray<unsigned char, 2>{xs}...);
+    }(r, a, b, c);
+
+    auto Select = [](auto... xs) { return sycl::select(xs...); };
+    sycl::marray<bool, 2> c2{false, true};
+    sycl::marray<char, 2> r2{a[0], b[1]};
+    Test(Select, r2, a, b, c2);
+    [&](auto... xs) {
+      Test(Select, sycl::marray<signed char, 2>{xs}..., c2);
+    }(r2, a, b);
+    [&](auto... xs) {
+      Test(Select, sycl::marray<unsigned char, 2>{xs}..., c2);
+    }(r2, a, b);
+  }
 
   return 0;
 }

sycl/test-e2e/Basic/built-ins/scalar_relational.cpp
@@ -1,0 +1,60 @@
+// RUN: %{build} -o %t.out
+// RUN: %{run} %t.out
+// RUN: %if preview-breaking-changes-supported %{ %{build} -fpreview-breaking-changes -o %t_preview.out %}
+// RUN: %if preview-breaking-changes-supported %{ %{run} %t_preview.out%}
+
+#include <sycl/sycl.hpp>
+
+template <typename... Ts, typename FuncTy>
+void TestTypes(FuncTy F) {
+  (F(Ts{}), ...);
+}
+
+int main() {
+  sycl::queue q;
+
+  auto Test = [&](auto F, auto Expected, auto... Args) {
+#if defined(__GNUC__) || defined(__clang__)
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+#endif
+    std::tuple ArgsTuple{Args...};
+    auto Result = std::apply(F, ArgsTuple);
+    static_assert(std::is_same_v<decltype(Expected), decltype(Result)>);
+    assert(Expected == Result);
+
+    sycl::buffer<bool, 1> ResultBuf{1};
+    q.submit([&](sycl::handler &cgh) {
+      sycl::accessor Result{ResultBuf, cgh};
+      cgh.single_task([=]() {
+        auto R = std::apply(F, ArgsTuple);
+        static_assert(std::is_same_v<decltype(Expected), decltype(R)>);
+        Result[0] = Expected == R;
+      });
+    });
+    assert(sycl::host_accessor{ResultBuf}[0]);
+  };
+
+  auto TestBitSelect = [&](auto type_val) {
+    using T = decltype(type_val);
+    auto BitSelect = [](auto... xs) { return sycl::bitselect(xs...); };
+
+    static_assert(std::is_integral_v<T>,
+                  "Only integer test is implemented here!");
+    Test(BitSelect, T{0b0110}, T{0b1100}, T{0b0011}, T{0b1010});
+  };
+
+  TestTypes<signed char, unsigned char, char, long, long long, unsigned long,
+            unsigned long long>(TestBitSelect);
+
+  auto TestSelect = [&](auto type_val) {
+    using T = decltype(type_val);
+    auto Select = [](auto... xs) { return sycl::select(xs...); };
+
+    Test(Select, T{0}, T{1}, T{0}, true);
+    Test(Select, T{1}, T{1}, T{0}, false);
+  };
+
+  TestTypes<signed char, unsigned char, char>(TestSelect);
+
+  return 0;
+}

sycl/test-e2e/Basic/built-ins/vec_relational.cpp
@@ -88,6 +88,70 @@ int main() {
   TEST2(sycl::any, int, EXPECTED(int32_t, 0), 3, va7);
   TEST(sycl::bitselect, float, EXPECTED(float, 1.0, 1.0), 2, va8, va9, va10);
   TEST(sycl::select, float, EXPECTED(float, 1.0, 2.0, 8.0), 3, va5, va6, c1);
+  {
+    // Extra tests for select/bitselect due to special handling required for
+    // integer return types.
+
+    auto Test = [&](auto F, auto Expected, auto... Args) {
+      std::tuple ArgsTuple{Args...};
+      auto Result = std::apply(F, ArgsTuple);
+      static_assert(std::is_same_v<decltype(Expected), decltype(Result)>);
+
+      // Note: operator==(vec, vec) return vec.
+      auto Equal = [](auto x, auto y) {
+        for (size_t i = 0; i < x.size(); ++i)
+          if (x[i] != y[i])
+            return false;
+
+        return true;
+      };
+
+      assert(Equal(Result, Expected));
+
+      sycl::buffer<bool, 1> ResultBuf{1};
+      deviceQueue.submit([&](sycl::handler &cgh) {
+        sycl::accessor Result{ResultBuf, cgh};
+        cgh.single_task([=]() {
+          auto R = std::apply(F, ArgsTuple);
+          static_assert(std::is_same_v<decltype(Expected), decltype(R)>);
+          Result[0] = Equal(R, Expected);
+        });
+      });
+      assert(sycl::host_accessor{ResultBuf}[0]);
+    };
+
+    // Note that only int8_t/uint8_t are supported by the bitselect/select
+    // builtins and not all three char data types. Also, use positive numbers
+    // for the values below so that we could use the same for both
+    // signed/unsigned tests.
+    sycl::vec<uint8_t, 2> a{0b1100, 0b0011};
+    sycl::vec<uint8_t, 2> b{0b0011, 0b1100};
+    sycl::vec<uint8_t, 2> c{0b1010, 0b1010};
+    sycl::vec<uint8_t, 2> r{0b0110, 0b1001};
+
+    auto BitSelect = [](auto... xs) { return sycl::bitselect(xs...); };
+    Test(BitSelect, r, a, b, c);
+    [&](auto... xs) {
+      Test(BitSelect, xs.template as<sycl::vec<int8_t, 2>>()...);
+    }(r, a, b, c);
+
+    auto Select = [](auto... xs) { return sycl::select(xs...); };
+    sycl::vec<uint8_t, 2> c2{0x7F, 0xFF};
+    sycl::vec<uint8_t, 2> r2{a[0], b[1]};
+
+    Test(Select, r2, a, b, c2);
+    [&](auto... xs) {
+      Test(Select, xs.template as<sycl::vec<int8_t, 2>>()..., c2);
+    }(r2, a, b);
+
+    // Assume that MSB of a signed data type is the leftmost bit (signbit).
+    auto c3 = c2.template as<sycl::vec<int8_t, 2>>();
+
+    Test(Select, r2, a, b, c3);
+    [&](auto... xs) {
+      Test(Select, xs.template as<sycl::vec<int8_t, 2>>()..., c3);
+    }(r2, a, b);
+  }
 
 #ifdef __INTEL_PREVIEW_BREAKING_CHANGES
   TEST(sycl::isequal, int32_t, EXPECTED(int32_t, 1, 1), 2, va11.swizzle<0, 1>(),