17ce514bfe0838fa9793d1337e1ccbfa4291d86d
Agarwal, Udit <udit.agarwal@intel.com>
2024-02-02 11:19:27 -0800

    More revert

--- sycl/test-e2e/OneapiDeviceSelector/illegal_input.cpp
+++ sycl/test-e2e/OneapiDeviceSelector/illegal_input.cpp
@@ -9,7 +9,6 @@
 // RUN: env ONEAPI_DEVICE_SELECTOR="level_zero:" %{run-unfiltered-devices} %t.out
 // RUN: env ONEAPI_DEVICE_SELECTOR="level_zero:::gpu" %{run-unfiltered-devices} %t.out
 // RUN: env ONEAPI_DEVICE_SELECTOR="level_zero:.1" %{run-unfiltered-devices} %t.out
-// RUN: env ONEAPI_DEVICE_SELECTOR="*:acc" %{run-unfiltered-devices} %t.out
 // XFAIL: *
 
 // Calling ONEAPI_DEVICE_SELECTOR with an illegal input should result in an
