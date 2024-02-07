eead9892edfe2bf9e7a8f501ff86e545d1f786e4
Maxime France-Pillois <maxime.francepillois@codeplay.com>
2024-02-07 11:40:38 +0000

    [SYCL][Graph] Enable Graphs with L0 immediate command-list (#12279)
    Updates e2e tests to test L0 immediate command-lists. 
    Improves filecheck usage in graph E2E tests
    Removes test that checks immediate command-list exception. 
    Updates the spec.
    
    Addresses Issue: #10467
    
    ---------
    
    Co-authored-by: Ben Tracy <ben.tracy@codeplay.com>

--- sycl/test-e2e/Graph/Explicit/depends_on.cpp
+++ sycl/test-e2e/Graph/Explicit/depends_on.cpp
@@ -1,9 +1,10 @@
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 // Extra run to check for leaks in Level Zero using UR_L0_LEAKS_DEBUG
-// RUN: %if level_zero %{env UR_L0_LEAKS_DEBUG=1 %{run} %t.out 2>&1 | FileCheck %s %}
+// RUN: %if level_zero %{env SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=0 UR_L0_LEAKS_DEBUG=1 %{run} %t.out 2>&1 | FileCheck %s --implicit-check-not=LEAK %}
+// Extra run to check for immediate-command-list in Level Zero
+// RUN: %if level_zero && linux %{env SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=1 UR_L0_LEAKS_DEBUG=1 %{run} %t.out 2>&1 | FileCheck %s --implicit-check-not=LEAK %}
 //
-// CHECK-NOT: LEAK
 
 // Tests that an event returned from adding a graph node using the queue
 // recording API can be passed to `handler::depends_on` inside a node
@@ -12,7 +13,7 @@
 #include "../graph_common.hpp"
 
 int main() {
-  queue Queue{{sycl::ext::intel::property::queue::no_immediate_command_list{}}};
+  queue Queue{};
 
   if (!are_graphs_supported(Queue)) {
     return 0;
