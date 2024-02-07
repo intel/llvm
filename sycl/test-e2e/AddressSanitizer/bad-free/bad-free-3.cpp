0fd1844cab1b3a9da8a42745eff0cf0029ba7e9c
Zhao, Yang2 <yang2.zhao@intel.com>
2024-02-06 18:27:09 -0800

    add tests

---
+++ sycl/test-e2e/AddressSanitizer/bad-free/bad-free-3.cpp
@@ -0,0 +1,13 @@
+// REQUIRES: linux, cpu
+// RUN: %{build} %device_sanitizer_flags -DMALLOC_DEVICE -O1 -g -o %t
+// RUN: env SYCL_PREFER_UR=1 ONEAPI_DEVICE_SELECTOR=opencl:cpu %{run-unfiltered-devices} not %t &> %t.txt ; FileCheck --check-prefixes CHECK,CHECK-DEVICE --input-file %t.txt %s
+#include <sycl/sycl.hpp>
+
+constexpr size_t N = 64;
+
+int main() {
+  sycl::queue Q;
+  auto *data = new int[N];
+  sycl::free(data - 1, Q);
+  return 0;
+}
