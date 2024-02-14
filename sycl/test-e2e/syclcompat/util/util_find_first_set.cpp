3975176718091d7b4040039bc80d23394cd29db0
Lorenc Bushi <lorenc.bushi@intel.com>
2024-02-14 13:13:39 -0800

    Rewrite syclcompat tests

--- sycl/test-e2e/syclcompat/util/util_find_first_set.cpp
+++ sycl/test-e2e/syclcompat/util/util_find_first_set.cpp
@@ -30,7 +30,6 @@
 //
 // ===----------------------------------------------------------------------===//
 
-// REQUIRES: usm_shared_allocations
 // RUN: %clangxx -fsycl -fsycl-targets=%{sycl_triple} %s -o %t.out
 // RUN: %{run} %t.out
 
@@ -105,7 +104,7 @@ void test_find_first_set() {
   sycl::queue q_ct1 = *dev_ct1.default_queue();
   int *test_result, host_test_result = 0;
 
-  test_result = sycl::malloc_shared<int>(sizeof(int), q_ct1);
+  test_result = sycl::malloc_device<int>(1, q_ct1);
   *test_result = 0;
 
   q_ct1.parallel_for(
@@ -113,8 +112,8 @@ void test_find_first_set() {
       [=](sycl::nd_item<3> item_ct1) { find_first_set_test(test_result); });
 
   dev_ct1.queues_wait_and_throw();
+  q_ct1.copy(&host_test_result, test_result, sizeof(int)).wait();
   find_first_set_test(&host_test_result);
-  assert(*test_result == 0);
   assert(host_test_result == 0);
 
   sycl::free(test_result, q_ct1);
