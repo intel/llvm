2145b66d5fff13ac4218fde5afb146c22797144c
Agarwal, Udit <udit.agarwal@intel.com>
2024-02-20 12:53:44 -0800

    Fix test case

--- sycl/test-e2e/USM/usm_allocator_zero_allocation.cpp
+++ sycl/test-e2e/USM/usm_allocator_zero_allocation.cpp
@@ -4,21 +4,27 @@
 #include <sycl/sycl.hpp>
 
 using namespace sycl;
-#define ALLOC_SIZE 0
+
+template <usm::alloc alloc_kind> void test(queue &q) {
+  sycl::usm_allocator<int, alloc_kind> ua(q);
+  int *p = ua.allocate(0);
+
+  assert(!p && "Our implementation of usm_allocator is expected to return a "
+               "null pointer when allocation "
+               "size is zero.");
+
+  ua.deallocate(p, 0);
+}
 
 int main() {
   queue q;
   auto dev = q.get_device();
-  auto ctxt = q.get_context();
 
   if (dev.get_info<info::device::usm_host_allocations>()) {
-    sycl::usm_allocator<int, sycl::usm::alloc::host> ua{ctxt, dev};
-    int *p = ua.allocate(ALLOC_SIZE);
-
-    assert(!p && "usm_allocator should return a null pointer when allocation "
-                 "size is zero.");
-
-    ua.deallocate(p, ALLOC_SIZE);
+    test<usm::alloc::host>(q);
+  }
+  if (dev.get_info<info::device::usm_shared_allocations>()) {
+    test<usm::alloc::shared>(q);
   }
 
   return 0;
