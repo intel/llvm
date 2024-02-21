9487121179a307d084b880c868f7ac3fbce39620
Lorenc Bushi <lorenc.bushi@intel.com>
2024-02-20 16:30:40 -0800

    Refactor even more

--- sycl/include/sycl/half_type.hpp
+++ sycl/include/sycl/half_type.hpp
@@ -11,8 +11,8 @@
 #include <sycl/bit_cast.hpp>              // for bit_cast
 #include <sycl/detail/export.hpp>         // for __SYCL_EXPORT
 #include <sycl/detail/iostream_proxy.hpp> // for istream, ostream
-#include <sycl/detail/vector_traits.hpp> // for vector_alignment
-
+#include <sycl/detail/memcpy.hpp>         // for bit_cast
+#include <sycl/detail/vector_traits.hpp>  // for vector_alignment
 #ifdef __SYCL_DEVICE_ONLY__
 #include <sycl/aspects.hpp>
 #endif
@@ -156,6 +156,11 @@ public:
   __SYCL_CONSTEXPR_HALF half(const float &rhs) : Buf(float2Half(rhs)) {}
 
   constexpr half &operator=(const half &rhs) = default;
+
+  constexpr volatile half &operator=(const half &rhs) volatile {
+    Buf = rhs.Buf;
+    return *this;
+  }
 
   // Operator +=, -=, *=, /=
   __SYCL_CONSTEXPR_HALF half &operator+=(const half &rhs) {
@@ -306,6 +311,11 @@ public:
   __SYCL_CONSTEXPR_HALF half(const float &rhs) : Data(rhs) {}
 
   constexpr half &operator=(const half &rhs) = default;
+
+  constexpr volatile half &operator=(const half &rhs) volatile {
+    std::ignore = (Data = rhs.Data); // std::ignore to avoid compiler warnings
+    return *this;
+  }
 
 #ifndef __SYCL_DEVICE_ONLY__
   // Since StorageT and BIsRepresentationT are different on host, these two
@@ -593,6 +603,22 @@ inline float cast_if_host_half(half_impl::half val) {
 }
 
 } // namespace detail
+
+// An overload of the bit_cast function template that is defined in bit_cast.hpp
+// to provide a customized version in the case that From == sycl::half. It is
+// needed because that version of bit_cast requires the From type to be
+// trivially copyable which sycl::half is not. Therefore, we provide this
+// implementation which does not have that requirement.
+template <typename To>
+std::enable_if_t<sizeof(To) == sizeof(half), To>
+bit_cast(const half &from) noexcept {
+  static_assert(std::is_trivially_default_constructible<To>::value,
+                "To must be trivially default constructible");
+  To to;
+  detail::memcpy(&to, &from, sizeof(To));
+  return to;
+}
+
 } // namespace _V1
 } // namespace sycl
 
