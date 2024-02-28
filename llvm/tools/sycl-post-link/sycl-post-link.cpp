201f92a4e6db3c3763e92e783d5143a935ea8c94
Maosu Zhao <maosu.zhao@intel.com>
2024-02-28 10:55:15 +0800

    Add a new pass SanitizeDeviceGlobal

--- llvm/tools/sycl-post-link/sycl-post-link.cpp
+++ llvm/tools/sycl-post-link/sycl-post-link.cpp
@@ -41,6 +41,7 @@
 #include "llvm/SYCLLowerIR/LowerInvokeSimd.h"
 #include "llvm/SYCLLowerIR/ModuleSplitter.h"
 #include "llvm/SYCLLowerIR/SYCLUtils.h"
+#include "llvm/SYCLLowerIR/SanitizeDeviceGlobal.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/FileSystem.h"
 #include "llvm/Support/InitLLVM.h"
@@ -881,110 +882,6 @@ static bool removeDeviceGlobalFromCompilerUsed(Module &M) {
   return true;
 }
 
-// Add extra red zone to each image scope device globals if the module has been
-// instrumented by sanitizer pass. And record their infomation like size, red
-// zone size, beginning address.
-static bool instrumentDeviceGlobal(Module &M) {
-  auto &DL = M.getDataLayout();
-  IRBuilder<> IRB(M.getContext());
-  SmallVector<GlobalVariable *, 8> GlobalsToRemove;
-  SmallVector<GlobalVariable *, 8> NewDeviceGlobals;
-  SmallVector<Constant *, 8> DeviceGlobalMetadata;
-
-  constexpr uint64_t MaxRZ = 1 << 18;
-  constexpr uint64_t MinRZ = 32;
-
-  Type *IntTy = Type::getIntNTy(M.getContext(), DL.getPointerSizeInBits());
-
-  // Device global meta data is described by a structure
-  //  size_t device_global_size
-  //  size_t device_global_size_with_red_zone
-  //  size_t beginning address of the device global
-  StructType *StructTy = StructType::get(IntTy, IntTy, IntTy);
-
-  for (auto &G : M.globals()) {
-    // Non image scope device globals are implemented by device USM, and the
-    // out-of-bounds check for them will be done by sanitizer USM part. So we
-    // exclude them here.
-    if (!isDeviceGlobalVariable(G) || !hasDeviceImageScopeProperty(G))
-      continue;
-
-    Type *Ty = G.getValueType();
-    const uint64_t SizeInBytes = DL.getTypeAllocSize(Ty);
-    const uint64_t RightRedzoneSize = [&] {
-      uint64_t RZ = 0;
-      if (SizeInBytes <= MinRZ / 2) {
-        // Reduce redzone size for small size objects, e.g. int, char[1].
-        // Optimize when SizeInBytes is less than or equal to half of MinRZ.
-        RZ = MinRZ - SizeInBytes;
-      } else {
-        // Calculate RZ, where MinRZ <= RZ <= MaxRZ, and RZ ~ 1/4 *
-        // SizeInBytes.
-        RZ = std::clamp((SizeInBytes / MinRZ / 4) * MinRZ, MinRZ, MaxRZ);
-
-        // Round up to multiple of MinRZ.
-        if (SizeInBytes % MinRZ)
-          RZ += MinRZ - (SizeInBytes % MinRZ);
-      }
-
-      assert((RZ + SizeInBytes) % MinRZ == 0);
-      return RZ;
-    }();
-    Type *RightRedZoneTy = ArrayType::get(IRB.getInt8Ty(), RightRedzoneSize);
-    StructType *NewTy = StructType::get(Ty, RightRedZoneTy);
-    Constant *NewInitializer = ConstantStruct::get(
-        NewTy, G.getInitializer(), Constant::getNullValue(RightRedZoneTy));
-
-    // Create a new global variable with enough space for a redzone.
-    GlobalVariable *NewGlobal = new GlobalVariable(
-        M, NewTy, G.isConstant(), G.getLinkage(), NewInitializer, "", &G,
-        G.getThreadLocalMode(), G.getAddressSpace());
-    NewGlobal->copyAttributesFrom(&G);
-    NewGlobal->setComdat(G.getComdat());
-    NewGlobal->setAlignment(Align(MinRZ));
-    NewGlobal->copyMetadata(&G, 0);
-
-    Value *Indices2[2];
-    Indices2[0] = IRB.getInt32(0);
-    Indices2[1] = IRB.getInt32(0);
-
-    G.replaceAllUsesWith(
-        ConstantExpr::getGetElementPtr(NewTy, NewGlobal, Indices2, true));
-    NewGlobal->takeName(&G);
-    GlobalsToRemove.push_back(&G);
-    NewDeviceGlobals.push_back(NewGlobal);
-    DeviceGlobalMetadata.push_back(ConstantStruct::get(
-        StructTy, ConstantInt::get(IntTy, SizeInBytes),
-        ConstantInt::get(IntTy, SizeInBytes + RightRedzoneSize),
-        ConstantExpr::getPointerCast(NewGlobal, IntTy)));
-  }
-
-  if (GlobalsToRemove.empty())
-    return false;
-
-  // Create global to record number of device globals
-  GlobalVariable *NumOfDeviceGlobals = new GlobalVariable(
-      M, IntTy, false, GlobalValue::ExternalLinkage,
-      ConstantInt::get(IntTy, NewDeviceGlobals.size()),
-      "__AsanDeviceGlobalCount", nullptr, GlobalValue::NotThreadLocal, 1);
-  NumOfDeviceGlobals->setUnnamedAddr(GlobalValue::UnnamedAddr::Local);
-
-  // Create meta data global to record device globals' information
-  ArrayType *ArrayTy = ArrayType::get(StructTy, NewDeviceGlobals.size());
-  Constant *MetadataInitializer =
-      ConstantArray::get(ArrayTy, DeviceGlobalMetadata);
-  GlobalVariable *AsanDeviceGlobalMetadata = new GlobalVariable(
-      M, MetadataInitializer->getType(), false, GlobalValue::ExternalLinkage,
-      MetadataInitializer, "__AsanDeviceGlobalMetadata", nullptr,
-      GlobalValue::NotThreadLocal, 1);
-  AsanDeviceGlobalMetadata->setUnnamedAddr(GlobalValue::UnnamedAddr::Local);
-
-  for (auto *G : GlobalsToRemove)
-    G->eraseFromParent();
-
-  return true;
-}
-
 SmallVector<module_split::ModuleDesc, 2>
 handleESIMD(module_split::ModuleDesc &&MDesc, bool &Modified,
             bool &SplitOccurred) {
@@ -1085,7 +982,7 @@ processInputModule(std::unique_ptr<Module> M) {
   // Instrument each image scope device globals if the module has been
   // instrumented by sanitizer pass.
   if (isModuleUsingAsan(*M))
-    Modified |= instrumentDeviceGlobal(*M.get());
+    Modified |= runModulePass<SanitizeDeviceGlobalPass>(*M);
 
   // Do invoke_simd processing before splitting because this:
   // - saves processing time (the pass is run once, even though on larger IR)
