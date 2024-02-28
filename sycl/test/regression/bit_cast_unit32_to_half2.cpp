09443b86ce29569d73aae08f910a4723bdbf3abd
Lorenc Bushi <113361374+lbushi25@users.noreply.github.com>
2024-02-28 13:24:11 -0500

    Update bit_cast_unit32_to_half2.cpp

--- sycl/test/regression/bit_cast_unit32_to_half2.cpp
+++ sycl/test/regression/bit_cast_unit32_to_half2.cpp
@@ -5,3 +5,6 @@ SYCL_EXTERNAL uint32_t test(uint32_t a) {
    sycl::half2 ah=sycl::bit_cast<sycl::half2, uint32_t>(a);
    return sycl::bit_cast<uint32_t, sycl::half2>(ah);
 }                                                                    
+int main() {
+   return 0;
+}
