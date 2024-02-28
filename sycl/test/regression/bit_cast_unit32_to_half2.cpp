ccf3a5c923955fed6b397a959ee8138888501597
Lorenc Bushi <113361374+lbushi25@users.noreply.github.com>
2024-02-28 13:49:37 -0500

    Update sycl/test/regression/bit_cast_unit32_to_half2.cpp
    Co-authored-by: Alexey Bader <alexey.bader@intel.com>

--- sycl/test/regression/bit_cast_unit32_to_half2.cpp
+++ sycl/test/regression/bit_cast_unit32_to_half2.cpp
@@ -1,10 +1,7 @@
-// RUN: %clangxx -fsycl -fpreview-breaking-changes %s -o %t.out
+// RUN: %clangxx -fsycl -fpreview-breaking-changes -c %s -o %t.out
 #include <sycl/sycl.hpp>
 
 SYCL_EXTERNAL uint32_t test(uint32_t a) {
    sycl::half2 ah=sycl::bit_cast<sycl::half2, uint32_t>(a);
    return sycl::bit_cast<uint32_t, sycl::half2>(ah);
 }                                                                    
-int main() {
-   return 0;
-}
