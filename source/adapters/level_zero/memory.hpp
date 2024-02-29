515477a7ffb367035d4a1e5de87be8c593df18f6
He, Wenju <wenju.he@intel.com>
2024-02-28 19:41:43 -0800

    MipmapFree, createUrMemFromZeImage, urMemRelease

--- source/adapters/level_zero/memory.hpp
+++ source/adapters/level_zero/memory.hpp
@@ -212,3 +212,20 @@ struct _ur_image final : ur_mem_handle_t_ {
   // Level Zero image handle.
   ze_image_handle_t ZeImage;
 };
+
+template <typename T>
+ur_result_t
+createUrMemFromZeImage(ur_context_handle_t Context, ze_image_handle_t ZeImage,
+                       bool OwnZeMemHandle,
+                       const ZeStruct<ze_image_desc_t> &ZeImageDesc, T *UrMem) {
+  try {
+    auto UrImage = new _ur_image(Context, ZeImage, OwnZeMemHandle);
+    UrImage->ZeImageDesc = ZeImageDesc;
+    *UrMem = reinterpret_cast<T>(UrImage);
+  } catch (const std::bad_alloc &) {
+    return UR_RESULT_ERROR_OUT_OF_HOST_MEMORY;
+  } catch (...) {
+    return UR_RESULT_ERROR_UNKNOWN;
+  }
+  return UR_RESULT_SUCCESS;
+}
