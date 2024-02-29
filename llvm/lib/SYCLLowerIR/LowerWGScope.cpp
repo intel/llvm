f6e70d20c730fb210e6a50d7b93796a38f6ddad0
PietroGhg <pietro.ghiglio@codeplay.com>
2024-02-29 11:16:16 +0000

    Avoid evaluating unnecessary condition

--- llvm/lib/SYCLLowerIR/LowerWGScope.cpp
+++ llvm/lib/SYCLLowerIR/LowerWGScope.cpp
@@ -899,8 +899,7 @@ GlobalVariable *spirv::createWGLocalVariable(Module &M, Type *T,
 // Return a value equals to 0 if and only if the local linear id is 0.
 Value *spirv::genPseudoLocalID(Instruction &Before, const Triple &TT) {
   Module &M = *Before.getModule();
-  const bool IsNativeCPU = sycl::utils::isSYCLNativeCPU(M);
-  if (TT.isNVPTX() || TT.isAMDGCN() || IsNativeCPU) {
+  if (TT.isNVPTX() || TT.isAMDGCN() || sycl::utils::isSYCLNativeCPU(M)) {
     LLVMContext &Ctx = Before.getContext();
     Type *RetTy = getSizeTTy(M);
 
