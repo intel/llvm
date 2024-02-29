oneapi-src/unified-runtime

Add enqueue timestamp recording extension
This commit adds a new extension feature for recording timestamps into
events, the information from which can be queried using the existing
profiling queries.

Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

include/ur_ddi.h
@@ -236,6 +236,15 @@ typedef ur_result_t(UR_APICALL *ur_pfnEventSetCallback_t)(
     void *);
 
 ///////////////////////////////////////////////////////////////////////////////
+/// @brief Function-pointer for urEnqueueTimestampRecordingExp
+typedef ur_result_t(UR_APICALL *ur_pfnEnqueueTimestampRecordingExp_t)(
+    ur_queue_handle_t,
+    bool,
+    uint32_t,
+    const ur_event_handle_t *,
+    ur_event_handle_t *);
+
+///////////////////////////////////////////////////////////////////////////////
 /// @brief Table of Event functions pointers
 typedef struct ur_event_dditable_t {
     ur_pfnEventGetInfo_t pfnGetInfo;
@@ -246,6 +255,7 @@ typedef struct ur_event_dditable_t {
     ur_pfnEventGetNativeHandle_t pfnGetNativeHandle;
     ur_pfnEventCreateWithNativeHandle_t pfnCreateWithNativeHandle;
     ur_pfnEventSetCallback_t pfnSetCallback;
+    ur_pfnEnqueueTimestampRecordingExp_t pfnEnqueueTimestampRecordingExp;
 } ur_event_dditable_t;
 
 ///////////////////////////////////////////////////////////////////////////////

source/adapters/level_zero/event.cpp
@@ -461,8 +461,9 @@ UR_APIEXPORT ur_result_t UR_APICALL urEventGetProfilingInfo(
 ) {
   std::shared_lock<ur_shared_mutex> EventLock(Event->Mutex);
 
-  if (Event->UrQueue &&
-      (Event->UrQueue->Properties & UR_QUEUE_FLAG_PROFILING_ENABLE) == 0) {
+  // The event must either have profiling enabled or be recording timestamps.
+  bool isTimestampedEvent = Event->isTimestamped();
+  if (!Event->isProfilingEnabled() && !isTimestampedEvent) {
     return UR_RESULT_ERROR_PROFILING_INFO_NOT_AVAILABLE;
   }
 
@@ -474,6 +475,62 @@ UR_APIEXPORT ur_result_t UR_APICALL urEventGetProfilingInfo(
       ((1ULL << Device->ZeDeviceProperties->kernelTimestampValidBits) - 1ULL);
 
   UrReturnHelper ReturnValue(PropValueSize, PropValue, PropValueSizeRet);
+
+  // For timestamped events we have the timestamps ready directly on the event
+  // handle, so we short-circuit the return.
+  if (isTimestampedEvent) {
+    uint64_t ContextStartTime = Event->RecordEventStartTimestamp;
+    switch (PropName) {
+    case UR_PROFILING_INFO_COMMAND_QUEUED:
+    case UR_PROFILING_INFO_COMMAND_SUBMIT:
+      return ReturnValue(ContextStartTime);
+    case UR_PROFILING_INFO_COMMAND_END:
+    case UR_PROFILING_INFO_COMMAND_START: {
+      // If RecordEventEndTimestamp on the event is non-zero it means it has
+      // collected the result of the queue already. In that case it has been
+      // adjusted and is ready for immediate return.
+      if (Event->RecordEventEndTimestamp)
+        return ReturnValue(Event->RecordEventEndTimestamp);
+
+      // Otherwise we need to collect it from the queue.
+      auto Entry = Event->UrQueue->EndTimeRecordings.find(Event);
+
+      // Unexpected state if there is no end-time record.
+      if (Entry == Event->UrQueue->EndTimeRecordings.end())
+        return UR_RESULT_ERROR_UNKNOWN;
+      auto &EndTimeRecording = Entry->second;
+
+      // End time needs to be adjusted for resolution and valid bits.
+      uint64_t ContextEndTime =
+          ((*EndTimeRecording.RecordEventEndTimestamp) & TimestampMaxValue) *
+          ZeTimerResolution;
+
+      // If the result is 0, we have not yet gotten results back and so we just
+      // return it.
+      if (ContextEndTime == 0)
+        return ReturnValue(ContextEndTime);
+
+      // Handle a possible wrap-around (the underlying HW counter is < 64-bit).
+      // Note, it will not report correct time if there were multiple wrap
+      // arounds, and the longer term plan is to enlarge the capacity of the
+      // HW timestamps.
+      if (ContextEndTime < ContextStartTime)
+        ContextEndTime += TimestampMaxValue * ZeTimerResolution;
+
+      // Now that we have the result, there is no need to keep it in the queue
+      // anymore, so we cache it on the event and evict the record from the
+      // queue.
+      Event->RecordEventEndTimestamp = ContextEndTime;
+      free(EndTimeRecording.RecordEventEndTimestamp);
+      Event->UrQueue->EndTimeRecordings.erase(Entry);
+
+      return ReturnValue(ContextEndTime);
+    }
+    default:
+      urPrint("urEventGetProfilingInfo: not supported ParamName\n");
+      return UR_RESULT_ERROR_INVALID_VALUE;
+    }
+  }
 
   ze_kernel_timestamp_result_t tsResult;
 
@@ -579,6 +636,62 @@ UR_APIEXPORT ur_result_t UR_APICALL urEventGetProfilingInfo(
     urPrint("urEventGetProfilingInfo: not supported ParamName\n");
     return UR_RESULT_ERROR_INVALID_VALUE;
   }
+
+  return UR_RESULT_SUCCESS;
+}
+
+UR_APIEXPORT ur_result_t UR_APICALL urEnqueueTimestampRecordingExp(
+    ur_queue_handle_t Queue,      ///< [in] handle of the queue object
+    bool Blocking,                ///< [in] blocking or non-blocking enqueue
+    uint32_t NumEventsInWaitList, ///< [in] size of the event wait list
+    const ur_event_handle_t
+        *EventWaitList, ///< [in][optional][range(0, numEventsInWaitList)]
+                        ///< pointer to a list of events that must be complete
+                        ///< before this command can be executed. If nullptr,
+                        ///< the numEventsInWaitList must be 0, indicating
+                        ///< that this command does not wait on any event to
+                        ///< complete.
+    ur_event_handle_t
+        *OutEvent ///< [in,out] return an event object that identifies
+                  ///< this particular command instance.
+) {
+  // Lock automatically releases when this goes out of scope.
+  std::scoped_lock<ur_shared_mutex> lock(Queue->Mutex);
+
+  ur_device_handle_t Device = Queue->Device;
+
+  uint64_t DeviceStartTimestamp = 0;
+  UR_CALL(urDeviceGetGlobalTimestamps(Device, &DeviceStartTimestamp, nullptr));
+
+  bool UseCopyEngine = false;
+  _ur_ze_event_list_t TmpWaitList;
+  UR_CALL(TmpWaitList.createAndRetainUrZeEventList(
+      NumEventsInWaitList, EventWaitList, Queue, UseCopyEngine));
+
+  // Get a new command list to be used on this call
+  ur_command_list_ptr_t CommandList{};
+  UR_CALL(Queue->Context->getAvailableCommandList(
+      Queue, CommandList, UseCopyEngine, /* AllowBatching */ false));
+
+  UR_CALL(createEventAndAssociateQueue(
+      Queue, OutEvent, UR_EXT_COMMAND_TYPE_USER, CommandList,
+      /* IsInternal */ false, /* HostVisible */ true));
+  ze_event_handle_t ZeEvent = (*OutEvent)->ZeEvent;
+  (*OutEvent)->WaitList = TmpWaitList;
+  (*OutEvent)->RecordEventStartTimestamp = DeviceStartTimestamp;
+
+  // Allocate new entry in the queue's recordings.
+  uint64_t *EndTimestampPtr = (uint64_t *)malloc(sizeof(uint64_t));
+  *EndTimestampPtr = 0;
+  Queue->EndTimeRecordings[*OutEvent] = ur_queue_handle_t_::end_time_recording{
+      EndTimestampPtr, /*EventHasDied=*/false};
+
+  ZE2UR_CALL(zeCommandListAppendWriteGlobalTimestamp,
+             (CommandList->first, EndTimestampPtr, ZeEvent,
+              (*OutEvent)->WaitList.Length, (*OutEvent)->WaitList.ZeEventList));
+
+  UR_CALL(
+      Queue->executeCommandList(CommandList, Blocking, /* OkToBatch */ false));
 
   return UR_RESULT_SUCCESS;
 }
@@ -874,6 +987,24 @@ ur_result_t urEventReleaseInternal(ur_event_handle_t Event) {
     delete Event;
   } else {
     Event->Context->addEventToContextCache(Event);
+  }
+
+  // If the event was a timestamp recording, we try to evict its entry in the
+  // queue.
+  if (Event->isTimestamped()) {
+    auto Entry = Queue->EndTimeRecordings.find(Event);
+    if (Entry != Queue->EndTimeRecordings.end()) {
+      auto &EndTimeRecording = Entry->second;
+      if ((EndTimeRecording.RecordEventEndTimestamp) == 0) {
+        // If the end time recording has not finished, we tell the queue that
+        // the event is no longer alive to avoid invalid write-backs.
+        EndTimeRecording.EventHasDied = true;
+      } else {
+        // Otherwise we evict the entry.
+        free(EndTimeRecording.RecordEventEndTimestamp);
+        Event->UrQueue->EndTimeRecordings.erase(Entry);
+      }
+    }
   }
 
   // We intentionally incremented the reference counter when an event is
@@ -1393,4 +1524,13 @@ ur_result_t _ur_ze_event_list_t::collectEventsForReleaseAndDestroyUrZeEventList(
 bool ur_event_handle_t_::isProfilingEnabled() const {
   return !UrQueue || // tentatively assume user events are profiling enabled
          (UrQueue->Properties & UR_QUEUE_FLAG_PROFILING_ENABLE) != 0;
+}
+
+// Tells if this event was created as a timestamp event, allowing profiling
+// info even if profiling is not enabled.
+bool ur_event_handle_t_::isTimestamped() const {
+  // If we are recording, the start time of the event will be non-zero. The
+  // end time might still be missing, depending on whether the corresponding
+  // enqueue is still running.
+  return RecordEventStartTimestamp != 0;
 }

source/adapters/opencl/ur_interface_loader.cpp
@@ -77,6 +77,7 @@ UR_DLLEXPORT ur_result_t UR_APICALL urGetEventProcAddrTable(
   pDdiTable->pfnRetain = urEventRetain;
   pDdiTable->pfnSetCallback = urEventSetCallback;
   pDdiTable->pfnWait = urEventWait;
+  pDdiTable->pfnEnqueueTimestampRecordingExp = urEnqueueTimestampRecordingExp;
   return UR_RESULT_SUCCESS;
 }
 

intel/llvm

include/ur_ddi.h
@@ -1439,9 +1439,19 @@ typedef ur_result_t(UR_APICALL *ur_pfnEnqueueCooperativeKernelLaunchExp_t)(
     ur_event_handle_t *);
 
 ///////////////////////////////////////////////////////////////////////////////
+/// @brief Function-pointer for urEnqueueTimestampRecordingExp
+typedef ur_result_t(UR_APICALL *ur_pfnEnqueueTimestampRecordingExp_t)(
+    ur_queue_handle_t,
+    bool,
+    uint32_t,
+    const ur_event_handle_t *,
+    ur_event_handle_t *);
+
+///////////////////////////////////////////////////////////////////////////////
 /// @brief Table of EnqueueExp functions pointers
 typedef struct ur_enqueue_exp_dditable_t {
     ur_pfnEnqueueCooperativeKernelLaunchExp_t pfnCooperativeKernelLaunchExp;
+    ur_pfnEnqueueTimestampRecordingExp_t pfnTimestampRecordingExp;
 } ur_enqueue_exp_dditable_t;
 
 ///////////////////////////////////////////////////////////////////////////////

source/adapters/level_zero/event.cpp
@@ -461,8 +461,9 @@ UR_APIEXPORT ur_result_t UR_APICALL urEventGetProfilingInfo(
 ) {
   std::shared_lock<ur_shared_mutex> EventLock(Event->Mutex);
 
-  if (Event->UrQueue &&
-      (Event->UrQueue->Properties & UR_QUEUE_FLAG_PROFILING_ENABLE) == 0) {
+  // The event must either have profiling enabled or be recording timestamps.
+  bool isTimestampedEvent = Event->isTimestamped();
+  if (!Event->isProfilingEnabled() && !isTimestampedEvent) {
     return UR_RESULT_ERROR_PROFILING_INFO_NOT_AVAILABLE;
   }
 
@@ -474,6 +475,62 @@ UR_APIEXPORT ur_result_t UR_APICALL urEventGetProfilingInfo(
       ((1ULL << Device->ZeDeviceProperties->kernelTimestampValidBits) - 1ULL);
 
   UrReturnHelper ReturnValue(PropValueSize, PropValue, PropValueSizeRet);
+
+  // For timestamped events we have the timestamps ready directly on the event
+  // handle, so we short-circuit the return.
+  if (isTimestampedEvent) {
+    uint64_t ContextStartTime = Event->RecordEventStartTimestamp;
+    switch (PropName) {
+    case UR_PROFILING_INFO_COMMAND_QUEUED:
+    case UR_PROFILING_INFO_COMMAND_SUBMIT:
+      return ReturnValue(ContextStartTime);
+    case UR_PROFILING_INFO_COMMAND_END:
+    case UR_PROFILING_INFO_COMMAND_START: {
+      // If RecordEventEndTimestamp on the event is non-zero it means it has
+      // collected the result of the queue already. In that case it has been
+      // adjusted and is ready for immediate return.
+      if (Event->RecordEventEndTimestamp)
+        return ReturnValue(Event->RecordEventEndTimestamp);
+
+      // Otherwise we need to collect it from the queue.
+      auto Entry = Event->UrQueue->EndTimeRecordings.find(Event);
+
+      // Unexpected state if there is no end-time record.
+      if (Entry == Event->UrQueue->EndTimeRecordings.end())
+        return UR_RESULT_ERROR_UNKNOWN;
+      auto &EndTimeRecording = Entry->second;
+
+      // End time needs to be adjusted for resolution and valid bits.
+      uint64_t ContextEndTime =
+          ((*EndTimeRecording.RecordEventEndTimestamp) & TimestampMaxValue) *
+          ZeTimerResolution;
+
+      // If the result is 0, we have not yet gotten results back and so we just
+      // return it.
+      if (ContextEndTime == 0)
+        return ReturnValue(ContextEndTime);
+
+      // Handle a possible wrap-around (the underlying HW counter is < 64-bit).
+      // Note, it will not report correct time if there were multiple wrap
+      // arounds, and the longer term plan is to enlarge the capacity of the
+      // HW timestamps.
+      if (ContextEndTime < ContextStartTime)
+        ContextEndTime += TimestampMaxValue * ZeTimerResolution;
+
+      // Now that we have the result, there is no need to keep it in the queue
+      // anymore, so we cache it on the event and evict the record from the
+      // queue.
+      Event->RecordEventEndTimestamp = ContextEndTime;
+      free(EndTimeRecording.RecordEventEndTimestamp);
+      Event->UrQueue->EndTimeRecordings.erase(Entry);
+
+      return ReturnValue(ContextEndTime);
+    }
+    default:
+      urPrint("urEventGetProfilingInfo: not supported ParamName\n");
+      return UR_RESULT_ERROR_INVALID_VALUE;
+    }
+  }
 
   ze_kernel_timestamp_result_t tsResult;
 
@@ -579,6 +636,62 @@ UR_APIEXPORT ur_result_t UR_APICALL urEventGetProfilingInfo(
     urPrint("urEventGetProfilingInfo: not supported ParamName\n");
     return UR_RESULT_ERROR_INVALID_VALUE;
   }
+
+  return UR_RESULT_SUCCESS;
+}
+
+UR_APIEXPORT ur_result_t UR_APICALL urEnqueueTimestampRecordingExp(
+    ur_queue_handle_t Queue,      ///< [in] handle of the queue object
+    bool Blocking,                ///< [in] blocking or non-blocking enqueue
+    uint32_t NumEventsInWaitList, ///< [in] size of the event wait list
+    const ur_event_handle_t
+        *EventWaitList, ///< [in][optional][range(0, numEventsInWaitList)]
+                        ///< pointer to a list of events that must be complete
+                        ///< before this command can be executed. If nullptr,
+                        ///< the numEventsInWaitList must be 0, indicating
+                        ///< that this command does not wait on any event to
+                        ///< complete.
+    ur_event_handle_t
+        *OutEvent ///< [in,out] return an event object that identifies
+                  ///< this particular command instance.
+) {
+  // Lock automatically releases when this goes out of scope.
+  std::scoped_lock<ur_shared_mutex> lock(Queue->Mutex);
+
+  ur_device_handle_t Device = Queue->Device;
+
+  uint64_t DeviceStartTimestamp = 0;
+  UR_CALL(urDeviceGetGlobalTimestamps(Device, &DeviceStartTimestamp, nullptr));
+
+  bool UseCopyEngine = false;
+  _ur_ze_event_list_t TmpWaitList;
+  UR_CALL(TmpWaitList.createAndRetainUrZeEventList(
+      NumEventsInWaitList, EventWaitList, Queue, UseCopyEngine));
+
+  // Get a new command list to be used on this call
+  ur_command_list_ptr_t CommandList{};
+  UR_CALL(Queue->Context->getAvailableCommandList(
+      Queue, CommandList, UseCopyEngine, /* AllowBatching */ false));
+
+  UR_CALL(createEventAndAssociateQueue(
+      Queue, OutEvent, UR_COMMAND_TIMESTAMP_RECORDING_EXP, CommandList,
+      /* IsInternal */ false, /* HostVisible */ true));
+  ze_event_handle_t ZeEvent = (*OutEvent)->ZeEvent;
+  (*OutEvent)->WaitList = TmpWaitList;
+  (*OutEvent)->RecordEventStartTimestamp = DeviceStartTimestamp;
+
+  // Allocate new entry in the queue's recordings.
+  uint64_t *EndTimestampPtr = (uint64_t *)malloc(sizeof(uint64_t));
+  *EndTimestampPtr = 0;
+  Queue->EndTimeRecordings[*OutEvent] = ur_queue_handle_t_::end_time_recording{
+      EndTimestampPtr, /*EventHasDied=*/false};
+
+  ZE2UR_CALL(zeCommandListAppendWriteGlobalTimestamp,
+             (CommandList->first, EndTimestampPtr, ZeEvent,
+              (*OutEvent)->WaitList.Length, (*OutEvent)->WaitList.ZeEventList));
+
+  UR_CALL(
+      Queue->executeCommandList(CommandList, Blocking, /* OkToBatch */ false));
 
   return UR_RESULT_SUCCESS;
 }
@@ -874,6 +987,24 @@ ur_result_t urEventReleaseInternal(ur_event_handle_t Event) {
     delete Event;
   } else {
     Event->Context->addEventToContextCache(Event);
+  }
+
+  // If the event was a timestamp recording, we try to evict its entry in the
+  // queue.
+  if (Event->isTimestamped()) {
+    auto Entry = Queue->EndTimeRecordings.find(Event);
+    if (Entry != Queue->EndTimeRecordings.end()) {
+      auto &EndTimeRecording = Entry->second;
+      if ((EndTimeRecording.RecordEventEndTimestamp) == 0) {
+        // If the end time recording has not finished, we tell the queue that
+        // the event is no longer alive to avoid invalid write-backs.
+        EndTimeRecording.EventHasDied = true;
+      } else {
+        // Otherwise we evict the entry.
+        free(EndTimeRecording.RecordEventEndTimestamp);
+        Event->UrQueue->EndTimeRecordings.erase(Entry);
+      }
+    }
   }
 
   // We intentionally incremented the reference counter when an event is
@@ -1393,4 +1524,13 @@ ur_result_t _ur_ze_event_list_t::collectEventsForReleaseAndDestroyUrZeEventList(
 bool ur_event_handle_t_::isProfilingEnabled() const {
   return !UrQueue || // tentatively assume user events are profiling enabled
          (UrQueue->Properties & UR_QUEUE_FLAG_PROFILING_ENABLE) != 0;
+}
+
+// Tells if this event was created as a timestamp event, allowing profiling
+// info even if profiling is not enabled.
+bool ur_event_handle_t_::isTimestamped() const {
+  // If we are recording, the start time of the event will be non-zero. The
+  // end time might still be missing, depending on whether the corresponding
+  // enqueue is still running.
+  return RecordEventStartTimestamp != 0;
 }

source/adapters/opencl/ur_interface_loader.cpp
@@ -397,6 +397,7 @@ UR_DLLEXPORT ur_result_t UR_APICALL urGetEnqueueExpProcAddrTable(
 
   pDdiTable->pfnCooperativeKernelLaunchExp =
       urEnqueueCooperativeKernelLaunchExp;
+  pDdiTable->pfnTimestampRecordingExp = urEnqueueTimestampRecordingExp;
 
   return UR_RESULT_SUCCESS;
 }