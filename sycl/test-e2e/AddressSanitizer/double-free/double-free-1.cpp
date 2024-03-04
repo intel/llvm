4843366fcbb6361043311c6f2db54ee234cd4ded
Zhao, Yang2 <yang2.zhao@intel.com>
2024-03-03 22:59:49 -0800

    update lit tests

--- sycl/test-e2e/AddressSanitizer/double-free/double-free-1.cpp
+++ sycl/test-e2e/AddressSanitizer/double-free/double-free-1.cpp
@@ -1,10 +1,10 @@
 // REQUIRES: linux, cpu
 // RUN: %{build} %device_sanitizer_flags -DMALLOC_DEVICE -O0 -g -o %t
-// RUN: env SYCL_PREFER_UR=1 UR_ENABLE_LAYERS=UR_LAYER_ASAN ONEAPI_DEVICE_SELECTOR=opencl:cpu %{run-unfiltered-devices} not %t &> %t.txt ; FileCheck --check-prefixes CHECK,CHECK-DEVICE --input-file %t.txt %s
+// RUN: env SYCL_PREFER_UR=1 UR_ENABLE_LAYERS=UR_LAYER_ASAN ONEAPI_DEVICE_SELECTOR=opencl:cpu UR_LAYER_ASAN_OPTIONS=quarantine_size_mb:1 %{run-unfiltered-devices} not %t &> %t.txt ; FileCheck --check-prefixes CHECK,CHECK-DEVICE --input-file %t.txt %s
 // RUN: %{build} %device_sanitizer_flags -DMALLOC_HOST -O0 -g -o %t
-// RUN: env SYCL_PREFER_UR=1 UR_ENABLE_LAYERS=UR_LAYER_ASAN ONEAPI_DEVICE_SELECTOR=opencl:cpu %{run-unfiltered-devices} not %t &> %t.txt ; FileCheck --check-prefixes CHECK,CHECK-HOST --input-file %t.txt %s
+// RUN: env SYCL_PREFER_UR=1 UR_ENABLE_LAYERS=UR_LAYER_ASAN ONEAPI_DEVICE_SELECTOR=opencl:cpu UR_LAYER_ASAN_OPTIONS=quarantine_size_mb:1 %{run-unfiltered-devices} not %t &> %t.txt ; FileCheck --check-prefixes CHECK,CHECK-HOST --input-file %t.txt %s
 // RUN: %{build} %device_sanitizer_flags -DMALLOC_SHARED -O0 -g -o %t
-// RUN: env SYCL_PREFER_UR=1 UR_ENABLE_LAYERS=UR_LAYER_ASAN ONEAPI_DEVICE_SELECTOR=opencl:cpu %{run-unfiltered-devices} not %t &> %t.txt ; FileCheck --check-prefixes CHECK,CHECK-SHARED --input-file %t.txt %s
+// RUN: env SYCL_PREFER_UR=1 UR_ENABLE_LAYERS=UR_LAYER_ASAN ONEAPI_DEVICE_SELECTOR=opencl:cpu UR_LAYER_ASAN_OPTIONS=quarantine_size_mb:1 %{run-unfiltered-devices} not %t &> %t.txt ; FileCheck --check-prefixes CHECK,CHECK-SHARED --input-file %t.txt %s
 #include <sycl/sycl.hpp>
 
 constexpr size_t N = 64;
--- sycl/test-e2e/AddressSanitizer/use-after-free/use-after-free-3.cpp
+++ sycl/test-e2e/AddressSanitizer/use-after-free/use-after-free-3.cpp
@@ -1,32 +1,32 @@
 // REQUIRES: linux, cpu
 // RUN: %{build} %device_sanitizer_flags -DMALLOC_DEVICE -O0 -g -o %t
-// RUN: env SYCL_PREFER_UR=1 ONEAPI_DEVICE_SELECTOR=opencl:cpu UR_ENABLE_LAYERS=UR_LAYER_ASAN UR_LAYER_ASAN_OPTIONS=quarantine_size_mb:5 UR_LOG_SANITIZER=level:debug %{run-unfiltered-devices} not %t &> %t.txt ; FileCheck --input-file %t.txt %s
+// RUN: env SYCL_PREFER_UR=1 ONEAPI_DEVICE_SELECTOR=opencl:cpu UR_ENABLE_LAYERS=UR_LAYER_ASAN UR_LAYER_ASAN_OPTIONS=quarantine_size_mb:5 UR_LOG_SANITIZER=level:info %{run-unfiltered-devices} not %t &> %t.txt ; FileCheck --input-file %t.txt %s
 #include <sycl/sycl.hpp>
 
 int main() {
   sycl::queue Q;
   auto *array =
       sycl::malloc_device<char>(1024, Q); // allocated size: 1280 <= 5120
+  // CHECK: AllocBegin=[[ADDR1:0x[0-9a-f]+]]
   sycl::free(array, Q);
-  // CHECK: <SANITIZER>[DEBUG]: ==== urUSMFree: [[ADDR1:0x.*]]
 
   auto *temp =
       sycl::malloc_device<char>(1024, Q); // allocated size: 1280*2 <= 5120
+  // CHECK: AllocBegin=[[ADDR2:0x[0-9a-f]+]]
   sycl::free(temp, Q);
-  // CHECK: <SANITIZER>[DEBUG]: ==== urUSMFree: [[ADDR2:0x.*]]
 
   temp = sycl::malloc_device<char>(1024, Q); // allocated size: 1280*3 <= 5120
+  // CHECK: AllocBegin=[[ADDR3:0x[0-9a-f]+]]
   sycl::free(temp, Q);
-  // CHECK: <SANITIZER>[DEBUG]: ==== urUSMFree: [[ADDR3:0x.*]]
 
   temp = sycl::malloc_device<char>(1024, Q); // allocated size: 1280*4 <= 5120
+  // CHECK: AllocBegin=[[ADDR4:0x[0-9a-f]+]]
   sycl::free(temp, Q);
-  // CHECK: <SANITIZER>[DEBUG]: ==== urUSMFree: [[ADDR4:0x.*]]
 
   temp = sycl::malloc_device<char>(1024, Q); // allocated size: 1280*5 > 5120
+  // CHECK: AllocBegin=[[ADDR5:0x[0-9a-f]+]]
   sycl::free(temp, Q);
-  // CHECK: <SANITIZER>[DEBUG]: ==== urUSMFree: [[ADDR5:0x.*]]
-  // CHECK: <SANITIZER>[INFO]: Quarantine Free: [[ADDR1]]
+  // CHECK: Quarantine Free: [[ADDR1]]
 
   return 0;
 }
