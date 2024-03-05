37833fd74750d141206452efef0fe40c58a74fbf
Byoungro So <byoungro.so@intel.com>
2024-03-04 17:31:48 -0800

    [SYCL] ABI Neutral Tests
    Signed-off-by: Byoungro So <byoungro.so@intel.com>

---
+++ sycl/test-e2e/AbiNeutral/catch-exception.cpp
@@ -0,0 +1,51 @@
+// RUN: %{build} -o %t.out -fpreview-breaking-changes -D_GLIBCXX_USE_CXX11_ABI=0
+// RUN: %{run} %t.out
+
+#include <sycl/sycl.hpp>
+
+#include <iostream>
+
+int main() {
+#ifdef _GLIBCXX_USE_CXX11_ABI
+  std::cout << "is_cxx11_abi: " << (_GLIBCXX_USE_CXX11_ABI != 0) << std::endl;
+  ;
+#else
+  std::cout << "is_cxx11_abi: 1" << std::endl;
+#endif
+  std::vector<sycl::device> root_devices;
+  auto platform_list = sycl::platform::get_platforms();
+  // Enumerated GPU devices from GPU platform firstly.
+  for (const auto &platform : platform_list) {
+    if (platform.get_backend() != sycl::backend::ext_oneapi_level_zero) {
+      continue;
+    }
+    auto device_list = platform.get_devices();
+    for (const auto &device : device_list) {
+      if (device.is_gpu()) {
+        root_devices.push_back(device);
+      }
+    }
+  }
+  assert(root_devices.size() > 0);
+  std::cout << "gpu number: " << root_devices.size() << std::endl;
+  constexpr sycl::info::partition_property partition_by_affinity =
+      sycl::info::partition_property::partition_by_affinity_domain;
+  constexpr sycl::info::partition_affinity_domain next_partitionable =
+      sycl::info::partition_affinity_domain::next_partitionable;
+  for (const auto &root_device : root_devices) {
+    try {
+      auto sub_devices = root_device.create_sub_devices<partition_by_affinity>(
+          next_partitionable);
+      std::cout << "tile partition is supported!" << std::endl;
+    } catch (sycl::exception &e) {
+      if (e.code() != sycl::errc::feature_not_supported &&
+          e.code() != sycl::errc::invalid) {
+        throw std::runtime_error(
+            std::string("Failed to apply tile partition: ") + e.what());
+      } else {
+        std::cout << "tile partition is UNSUPPORTED!" << std::endl;
+      }
+    }
+  }
+  std::cout << "pass!" << std::endl;
+}
---
+++ sycl/test-e2e/AbiNeutral/submit-kernel.cpp
@@ -0,0 +1,27 @@
+// RUN: %{build} -o %t.out -fpreview-breaking-changes -D_GLIBCXX_USE_CXX11_ABI=0
+// RUN: %{run} %t.out
+
+#include <sycl/sycl.hpp>
+
+#include <iostream>
+
+int main() {
+  try {
+    auto cgf = [&](sycl::handler &cgh) { cgh.single_task([=]() {}); };
+    sycl::queue queue;
+    for (auto i = 0; i < 25; i++) {
+      sycl::malloc_device(1024, queue);
+    }
+    auto event = queue.submit(cgf);
+    event.wait_and_throw();
+  } catch (const sycl::exception &ep) {
+    const std::string_view err_msg(ep.what());
+    if (err_msg.find("PI_ERROR_OUT_OF_RESOURCES") != std::string::npos) {
+      std::cout << "Allocation is out of device memory on the current platform."
+                << std::endl;
+    } else {
+      throw ep;
+    }
+  }
+  std::cout << "pass!" << std::endl;
+}
