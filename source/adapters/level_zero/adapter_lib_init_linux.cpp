aa690790dbdbef57b545f6dd68cd5c46d37107ed
Neil R. Spruit <neil.r.spruit@intel.com>
2024-03-07 11:23:50 -0800

    Fix license format
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>

--- source/adapters/level_zero/adapter_lib_init_linux.cpp
+++ source/adapters/level_zero/adapter_lib_init_linux.cpp
@@ -1,14 +1,12 @@
-/*
-*
-* Copyright (C) 2024 Intel Corporation
-*
-* Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM Exceptions.
-* See LICENSE.TXT
-* SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-*
-* @file adapter_lib_init_linux.cpp
-*
-*/
+//===--------- adapter_lib_init_linux.cpp - Level Zero Adapter ------------===//
+//
+// Copyright (C) 2023 Intel Corporation
+//
+// Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM
+// Exceptions. See LICENSE.TXT
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 
 #include "adapter.hpp"
 #include "ur_level_zero.hpp"
