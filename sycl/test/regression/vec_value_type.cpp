94eac25390d75f42567ddbd548bece69b90fd918
Lorenc Bushi <113361374+lbushi25@users.noreply.github.com>
2024-03-08 15:54:57 -0500

    Include type_traits

--- sycl/test/regression/vec_value_type.cpp
+++ sycl/test/regression/vec_value_type.cpp
@@ -1,6 +1,6 @@
 // RUN: %clangxx -fsycl -fsyntax-only %s
 #include <sycl/sycl.hpp>
-
+#include <type_traits>
 using namespace std;
 int main() {
   static_assert(is_same_v<sycl::vec<int, 1>::value_type, int>);
