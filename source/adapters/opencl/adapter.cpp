2aa884fcd953cd98a7761c9033f91cd92c888823
Maosu Zhao <maosu.zhao@intel.com>
2024-03-12 15:48:36 +0800

    [OCL] Gracefully tear down adapter in case that some globals have been released
    Sometimes the adapter may exit before sycl lib, if sycl lib call
    urAdapterRelease later it may cause segment fault since some globals
    already have been released.

--- source/adapters/opencl/adapter.cpp
+++ source/adapters/opencl/adapter.cpp
@@ -10,23 +10,40 @@
 
 #include "common.hpp"
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 struct ur_adapter_handle_t_ {
   std::atomic<uint32_t> RefCount = 0;
   std::mutex Mutex;
 };
 
-ur_adapter_handle_t_ adapter{};
+static ur_adapter_handle_t_ *adapter = new ur_adapter_handle_t_();
+
+static void globalAdapterShutdown() {
+  if (cl_ext::ExtFuncPtrCache) {
+    delete cl_ext::ExtFuncPtrCache;
+    cl_ext::ExtFuncPtrCache = nullptr;
+  }
+  if (adapter) {
+    delete adapter;
+    adapter = nullptr;
+  }
+}
 
 UR_APIEXPORT ur_result_t UR_APICALL
 urAdapterGet(uint32_t NumEntries, ur_adapter_handle_t *phAdapters,
              uint32_t *pNumAdapters) {
   if (NumEntries > 0 && phAdapters) {
-    std::lock_guard<std::mutex> Lock{adapter.Mutex};
-    if (adapter.RefCount++ == 0) {
-      cl_ext::ExtFuncPtrCache = std::make_unique<cl_ext::ExtFuncPtrCacheT>();
+    std::lock_guard<std::mutex> Lock{adapter->Mutex};
+    if (adapter->RefCount++ == 0) {
+      cl_ext::ExtFuncPtrCache = new cl_ext::ExtFuncPtrCacheT();
     }
 
-    *phAdapters = &adapter;
+    *phAdapters = adapter;
+
+    atexit(globalAdapterShutdown);
   }
 
   if (pNumAdapters) {
@@ -37,14 +54,20 @@ urAdapterGet(uint32_t NumEntries, ur_adapter_handle_t *phAdapters,
 }
 
 UR_APIEXPORT ur_result_t UR_APICALL urAdapterRetain(ur_adapter_handle_t) {
-  ++adapter.RefCount;
+  ++adapter->RefCount;
   return UR_RESULT_SUCCESS;
 }
 
 UR_APIEXPORT ur_result_t UR_APICALL urAdapterRelease(ur_adapter_handle_t) {
-  std::lock_guard<std::mutex> Lock{adapter.Mutex};
-  if (--adapter.RefCount == 0) {
-    cl_ext::ExtFuncPtrCache.reset();
+  // Check first if the adapter is valid pointer
+  if (adapter) {
+    std::lock_guard<std::mutex> Lock{adapter->Mutex};
+    if (--adapter->RefCount == 0) {
+      if (cl_ext::ExtFuncPtrCache) {
+        delete cl_ext::ExtFuncPtrCache;
+        cl_ext::ExtFuncPtrCache = nullptr;
+      }
+    }
   }
   return UR_RESULT_SUCCESS;
 }
@@ -68,7 +91,7 @@ UR_APIEXPORT ur_result_t UR_APICALL urAdapterGetInfo(ur_adapter_handle_t,
   case UR_ADAPTER_INFO_BACKEND:
     return ReturnValue(UR_ADAPTER_BACKEND_OPENCL);
   case UR_ADAPTER_INFO_REFERENCE_COUNT:
-    return ReturnValue(adapter.RefCount.load());
+    return ReturnValue(adapter->RefCount.load());
   default:
     return UR_RESULT_ERROR_INVALID_ENUMERATION;
   }
