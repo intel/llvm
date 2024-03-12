7ad31b6d2d098d36086916dd3a5605e6846366b8
Wu, Yingcong <yingcong.wu@intel.com>
2024-03-11 22:17:09 -0700

    address comment

--- source/ur_api.cpp
+++ source/ur_api.cpp
@@ -3425,8 +3425,8 @@ ur_result_t UR_APICALL urKernelCreateWithNativeHandle(
 ///        implementation.
 ///
 /// @details
-///     - Query a suggested group size for a kernel given a global size for each
-///       dimension.
+///     - Query a suggested local work size for a kernel given a global size for
+///       each dimension.
 ///     - The application may call this function from simultaneous threads for
 ///       the same context.
 ///     - The implementation of this function should be thread-safe.
@@ -3459,7 +3459,7 @@ ur_result_t UR_APICALL urKernelGetSuggestedLocalWorkSize(
     ///< kernel function
     size_t *
         pSuggestedLocalWorkSize ///< [out] pointer to an array of workDim unsigned values that specify
-    ///< suggested local work size that shall be used when executing the kernel
+    ///< suggested local work size that will contain the result of the query
 ) {
     ur_result_t result = UR_RESULT_SUCCESS;
     return result;
