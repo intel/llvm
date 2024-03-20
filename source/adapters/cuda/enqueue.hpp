5da3e60f266d4e2ef062273b318891599434b0c2
Wu, Yingcong <yingcong.wu@intel.com>
2024-03-19 22:58:22 -0700

    fix head error(is this a bug for other pr?)

--- source/adapters/cuda/enqueue.hpp
+++ source/adapters/cuda/enqueue.hpp
@@ -20,7 +20,7 @@ ur_result_t enqueueEventsWait(ur_queue_handle_t CommandQueue, CUstream Stream,
 void guessLocalWorkSize(ur_device_handle_t Device, size_t *ThreadsPerBlock,
                         const size_t *GlobalWorkSize, const uint32_t WorkDim,
                         const size_t MaxThreadsPerBlock[3],
-                        ur_kernel_handle_t Kernel, uint32_t LocalSize);
+                        ur_kernel_handle_t Kernel);
 
 bool hasExceededMaxRegistersPerBlock(ur_device_handle_t Device,
                                      ur_kernel_handle_t Kernel,
