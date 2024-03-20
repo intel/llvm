d36444eda861b5b9ecf171a0c48f7055d32c2e92
Lorenc Bushi <113361374+lbushi25@users.noreply.github.com>
2024-03-20 15:27:47 -0400

    Update sycl/test/regression/vector_const_data.cpp
    Co-authored-by: aelovikov-intel <andrei.elovikov@intel.com>

--- sycl/test/regression/vector_const_data.cpp
+++ sycl/test/regression/vector_const_data.cpp
@@ -8,7 +8,6 @@
 // succeeds for the function template member load of sycl::vec when instantiated
 // with a const-qualified type.
 int main() {
-  sycl::queue Q;
   sycl::vec<int, 16> my_vec{};
   sycl::vec<const int, 16> my_const_vec{};
   return 0;
