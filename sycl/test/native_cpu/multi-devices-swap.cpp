cc7c381e5f441376e34b98c8bfb14dcb9510d538
PietroGhg <pietro.ghiglio@codeplay.com>
2024-03-21 10:28:02 +0000

    re-enable native cpu run

--- sycl/test/native_cpu/multi-devices-swap.cpp
+++ sycl/test/native_cpu/multi-devices-swap.cpp
@@ -1,6 +1,7 @@
 // REQUIRES: native_cpu_be
 // REQUIRES: opencl_be
 // RUN: %clangxx -fsycl -fsycl-targets=native_cpu,spir64 %s -o %t
+// RUN: env ONEAPI_DEVICE_SELECTOR="native_cpu:cpu" %t
 
 #include <sycl/sycl.hpp>
 
--- sycl/test/native_cpu/multi-devices.cpp
+++ sycl/test/native_cpu/multi-devices.cpp
@@ -1,6 +1,7 @@
 // REQUIRES: native_cpu_be
 // REQUIRES: opencl_be
 // RUN: %clangxx -fsycl -fsycl-targets=spir64,native_cpu %s -o %t
+// RUN: env ONEAPI_DEVICE_SELECTOR="native_cpu:cpu" %t
 
 #include <sycl/sycl.hpp>
 
