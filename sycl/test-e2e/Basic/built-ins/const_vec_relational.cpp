c8a93a37d32d1ad8521264e42d6115a7073ad80a
Larsen, Steffen <steffen.larsen@intel.com>
2024-03-22 08:40:50 -0700

    [SYCL][E2E] Add builtin tests for const vec and const swizzles
    This commit copies the vec math builtin tests to check that they also
    work for const vec and the constant swizzles produced from const vec.
    
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- sycl/test-e2e/Basic/built-ins/const_vec_relational.cpp
+++ sycl/test-e2e/Basic/built-ins/const_vec_relational.cpp
@@ -3,9 +3,13 @@
 // RUN: %if preview-breaking-changes-supported %{ %{build} -fpreview-breaking-changes -o %t2.out %}
 // RUN: %if preview-breaking-changes-supported %{ %{run} %t2.out %}
 
-#include "vec_common.hpp"
+#include "vec_relational.hpp"
+
+// Alias is needed to const-qualify vec without template args.
+template <typename T, int NumElems>
+using ConstVec = const sycl::vec<T, NumElems>;
 
 int main() {
-  run_test<sycl::vec>();
+  run_test<ConstVec>();
   return 0;
 }
