b2d8f3d3fd319d0127a5db269f5d87e670c8bae4
Dounia <dounia.khaldi@intel.com>
2024-03-26 11:01:56 -0700

    added missing Requires

--- sycl/test-e2e/Matrix/joint_matrix_apply_two_matrices.cpp
+++ sycl/test-e2e/Matrix/joint_matrix_apply_two_matrices.cpp
@@ -5,9 +5,10 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+// REQUIRES: aspect-ext_intel_matrix
+
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 
 #include "common.hpp"
-
 #include "joint_matrix_apply_two_matrices_impl.hpp"
