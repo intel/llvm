58f331fbab7311984f42e3587e3b6dfb2ee330e5
Sergey Semenov <sergey.semenov@intel.com>
2024-03-27 11:07:08 -0700

    Update comment

--- sycl/source/detail/event_impl.hpp
+++ sycl/source/detail/event_impl.hpp
@@ -75,9 +75,10 @@ public:
   /// Self is needed in order to pass shared_ptr to Scheduler.
   ///
   /// \param Self is a pointer to this event.
-  /// \param Success is an optional parameter that indicates that the wait
-  ///        could fail (e.g., in case of a non-blocking read from an empty
-  ///        pipe) and is set to true iff the wait succeeded.
+  /// \param Success is an optional parameter that, when set to a non-null
+  ///        pointer, indicates that failure is a valid outcome for this wait
+  ///        (e.g., in case of a non-blocking read from a pipe), and the value
+  ///        it's pointing to is then set according to the outcome.
   void wait(std::shared_ptr<sycl::detail::event_impl> Self,
             bool *Success = nullptr);
 
@@ -112,9 +113,10 @@ public:
   ~event_impl();
 
   /// Waits for the event with respect to device type.
-  /// \param Success is an optional parameter that indicates that the wait
-  ///        could fail (e.g., in case of a non-blocking read from an empty
-  ///        pipe) and is set to true iff the wait succeeded.
+  /// \param Success is an optional parameter that, when set to a non-null
+  ///        pointer, indicates that failure is a valid outcome for this wait
+  ///        (e.g., in case of a non-blocking read from a pipe), and the value
+  ///        it's pointing to is then set according to the outcome.
   void waitInternal(bool *Success = nullptr);
 
   /// Marks this event as completed.
--- sycl/source/detail/scheduler/scheduler.hpp
+++ sycl/source/detail/scheduler/scheduler.hpp
@@ -396,9 +396,10 @@ public:
   /// corresponding function of device API.
   ///
   /// \param Event is a pointer to event to wait on.
-  /// \param Success is an optional parameter that indicates that the wait
-  ///        could fail (e.g., in case of a non-blocking read from an empty
-  ///        pipe) and is set to true iff the wait succeeded.
+  /// \param Success is an optional parameter that, when set to a non-null
+  ///        pointer, indicates that failure is a valid outcome for this wait
+  ///        (e.g., in case of a non-blocking read from a pipe), and the value
+  ///        it's pointing to is then set according to the outcome.
 
   void waitForEvent(const EventImplPtr &Event, bool *Success = nullptr);
 
@@ -890,9 +891,10 @@ protected:
     /// \param GraphReadLock read-lock which is already acquired for reading
     /// \param ToCleanUp container for commands that can be cleaned up.
     /// \param LockTheLock selects if graph lock should be locked upon return
-    /// \param Success is an optional parameter that indicates that the wait
-    ///        could fail (e.g., in case of a non-blocking read from an empty
-    ///        pipe) and is set to true iff the wait succeeded.
+    /// \param Success is an optional parameter that, when set to a non-null
+    ///        pointer, indicates that failure is a valid outcome for this wait
+    ///        (e.g., in case of a non-blocking read from a pipe), and the value
+    ///        it's pointing to is then set according to the outcome.
     ///
     /// The function may unlock and lock GraphReadLock as needed. Upon return
     /// the lock is left in locked state if and only if LockTheLock is true.
