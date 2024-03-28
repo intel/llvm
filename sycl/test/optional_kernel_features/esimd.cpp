091ace54131e109829948d00335aeff1be59ed46
Cai, Justin <justin.cai@intel.com>
2024-03-28 13:31:04 -0700

    Fix esimd.cpp test

--- sycl/test/optional_kernel_features/esimd.cpp
+++ sycl/test/optional_kernel_features/esimd.cpp
@@ -4,7 +4,6 @@
 // CHECK: !sycl_types_that_use_aspects = !{![[#MDNUM1:]], ![[#MDNUM2:]]}
 // CHECK: ![[#MDNUM1]] = !{!"class.sycl::_V1::ext::intel::esimd::detail::simd_obj_impl", i32 [[#ASPECT_NUM:]]}
 // CHECK: ![[#MDNUM2]] = !{!"class.sycl::_V1::ext::intel::esimd::detail::simd_view_impl", i32 [[#ASPECT_NUM]]}
-// CHECK: !{{.*}} = !{!"ext_intel_esimd", i32 [[#ASPECT_NUM]]}
 
 #include <sycl/ext/intel/esimd.hpp>
 #include <sycl/sycl.hpp>
