88d40663c2f74429f8049f46beda5fd839eecdc3
Wu, Yingcong <yingcong.wu@intel.com>
2024-03-28 14:49:14 +0800

    update header

--- source/adapters/cuda/enqueue.hpp
+++ source/adapters/cuda/enqueue.hpp
@@ -19,7 +19,6 @@ ur_result_t enqueueEventsWait(ur_queue_handle_t CommandQueue, CUstream Stream,
 
 void guessLocalWorkSize(ur_device_handle_t Device, size_t *ThreadsPerBlock,
                         const size_t *GlobalWorkSize, const uint32_t WorkDim,
-                        const size_t MaxThreadsPerBlock[3],
                         ur_kernel_handle_t Kernel);
 
 bool hasExceededMaxRegistersPerBlock(ur_device_handle_t Device,
