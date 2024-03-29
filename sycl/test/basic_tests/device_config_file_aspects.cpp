afa3b17bbc898084c56de23b1c78084dd466006f
Cai, Justin <justin.cai@intel.com>
2024-03-29 09:36:48 -0700

    Refactor device_config_file_aspects.cpp

--- sycl/test/basic_tests/device_config_file_aspects.cpp
+++ sycl/test/basic_tests/device_config_file_aspects.cpp
@@ -1,54 +1,48 @@
+// This test checks if DeviceConfigFile.td and aspects.def are in sync.
 // RUN: %clangxx -fsycl %s -o %t.out -I %llvm_main_include_dir
 // RUN: %t.out
-//
+
+#include <iostream>
 #include <map>
 
 #include <llvm/ADT/StringRef.h>
 #include <llvm/SYCLLowerIR/DeviceConfigFile.hpp>
 #include <sycl/sycl.hpp>
 
-#define __SYCL_ASPECT_DEPRECATED_ALIAS(ASPECT, ID, MESSAGE)                    \
-  __SYCL_ASPECT_DEPRECATED(ASPECT, ID, MESSAGE)
+void check(const char *aspect_name, int aspect_val, int &n_fail) {
+  const auto &aspectTable = DeviceConfigFile::AspectTable;
+  auto res = aspectTable.find(aspect_name);
+  if (res == aspectTable.end()) {
+    std::cout << "Aspect " << aspect_name
+              << " was not found in the device config file!\n";
+    ++n_fail;
+    return;
+  }
+  if (res->second != aspect_val) {
+    std::cout << "Aspect " << aspect_name << " has value " << res->second
+              << " in the device config file but has value " << aspect_val
+              << " in aspects.def!\n";
+    ++n_fail;
+    return;
+  }
+}
 
 int main() {
-  auto testAspects = DeviceConfigFile::TargetTable.find("__TestAspectList");
-  assert(testAspects != DeviceConfigFile::TargetTable.end());
-  auto aspectsList = testAspects->second.aspects;
+  int n_fail = 0;
 
-#define __SYCL_ASPECT(ASPECT, ASPECT_VAL)                                      \
-  llvm::StringRef s##ASPECT(#ASPECT);                                          \
-  assert(std::find(aspectsList.begin(), aspectsList.end(), s##ASPECT) !=       \
-         aspectsList.end());
-
-#include <sycl/info/aspects.def>
-
-#undef __SYCL_ASPECT
-
-  auto testDeprecatedAspects =
-      DeviceConfigFile::TargetTable.find("__TestDeprecatedAspectList");
-  assert(testDeprecatedAspects != DeviceConfigFile::TargetTable.end());
-  auto deprecatedAspectsList = testDeprecatedAspects->second.aspects;
-#define __SYCL_ASPECT_DEPRECATED(ASPECT, ASPECT_VAL, MSG)                      \
-  llvm::StringRef s##ASPECT(#ASPECT);                                          \
-  assert(std::find(deprecatedAspectsList.begin(), deprecatedAspectsList.end(), \
-                   s##ASPECT) != deprecatedAspectsList.end());
-
-#include <sycl/info/aspects_deprecated.def>
-
-#undef __SYCL_ASPECT_DEPRECATED
-
-  const auto &aspectTable = DeviceConfigFile::AspectTable;
-#define __SYCL_ASPECT(ASPECT, ASPECT_VAL)                                      \
-  {auto res = aspectTable.find(#ASPECT);                                       \
-  assert(res != aspectTable.end() && #ASPECT);                                 \
-  assert(res->second == ASPECT_VAL && #ASPECT);                                \
-  }
+#define __SYCL_ASPECT(ASPECT, ASPECT_VAL) check(ASPECT, ASPECT_VAL, n_fail)
 #define __SYCL_ASPECT_DEPRECATED(ASPECT, ASPECT_VAL, MSG)                      \
   __SYCL_ASPECT(ASPECT, ASPECT_VAL)
 #define __SYCL_ASPECT_DEPRECATED_ALIAS(ASPECT, ID, MESSAGE)
 #include <sycl/info/aspects.def>
 #include <sycl/info/aspects_deprecated.def>
-#undef __SYCL_ASPECT
-}
-
 #undef __SYCL_ASPECT_DEPRECATED_ALIAS
+#undef __SYCL_ASPECT_DEPRECATED
+#undef __SYCL_ASPECT
+
+  if (n_fail > 0) {
+    std::cout << "Errors detected, DeviceConfigFile.td and aspects.def are out "
+                 "of sync!\n";
+  }
+  return n_fail;
+}
