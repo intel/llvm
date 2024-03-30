e8850985333b5a5bbf5bf6dc6292ab6f9b6db382
sys_ce_bb <sys_ce_bb@intel.com>
2024-03-30 11:42:41 -0700

    fix range

--- llvm-spirv/lib/SPIRV/SPIRVRegularizeLLVM.cpp
+++ llvm-spirv/lib/SPIRV/SPIRVRegularizeLLVM.cpp
@@ -365,7 +365,7 @@ static void simplifyBuiltinVarAccesses(GlobalValue *GV) {
 
   Type *Int32Ty = Type::getInt32Ty(GV->getContext());
   auto GetGep = [&](unsigned Offset) {
-    llvm::ConstantRange InRange(llvm::APInt(32, Offset, true),
+    llvm::ConstantRange InRange(llvm::APInt(32, -Offset, true),
                                 llvm::APInt(32, Offset, true));
     return ConstantExpr::getGetElementPtr(
         Ty, GV,
