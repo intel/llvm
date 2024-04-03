d98d82eb009aff84f3e072c7dbd78b3c4f1f7cd3
Konrad Kusiak <konrad.kusiak@codeplay.com>
2024-04-03 10:26:06 +0100

    Updated graph usm fill tests and Command Graph docs

--- sycl/test-e2e/Graph/RecordReplay/usm_memset_shortcut.cpp
+++ sycl/test-e2e/Graph/RecordReplay/usm_memset_shortcut.cpp
@@ -2,6 +2,8 @@
 // RUN: %{run} %t.out
 // Extra run to check for leaks in Level Zero using UR_L0_LEAKS_DEBUG
 // RUN: %if level_zero %{ %{l0_leak_check} %{run} %t.out 2>&1 | FileCheck %s --implicit-check-not=LEAK %}
+// USM fill command not supported for OpenCL
+// UNSUPPORTED: opencl
 //
 // Tests adding a USM memset queue shortcut operation as a graph node.
 
