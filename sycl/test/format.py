88fe960ebb608adeac816979e8e92ba98eae9758
Alexey Sachkov <alexey.sachkov@intel.com>
2024-04-04 09:31:19 -0700

    format python code

--- sycl/test/format.py
+++ sycl/test/format.py
@@ -4,38 +4,60 @@ import lit.formats
 
 import os
 
+
 class CustomFormat(lit.formats.TestFormat):
-    def getTestsForPath(self, testSuite, path_in_suite, filepath, litConfig, localConfig):
+    def getTestsForPath(
+        self, testSuite, path_in_suite, filepath, litConfig, localConfig
+    ):
         yield lit.Test.Test(testSuite, path_in_suite, localConfig, file_path=filepath)
 
     def getTestsInDirectory(self, testSuite, path_in_suite, litConfig, localConfig):
         # We traverse build/sycl/include/sycl directory
-        source_path = os.path.join(localConfig.sycl_include, 'sycl')
+        source_path = os.path.join(localConfig.sycl_include, "sycl")
         for dirpath, _, filenames in os.walk(source_path):
-            relative_dirpath = dirpath[len(localConfig.sycl_include) + 1:]
+            relative_dirpath = dirpath[len(localConfig.sycl_include) + 1 :]
             for filename in filenames:
-                if not filename.endswith('.hpp'):
+                if not filename.endswith(".hpp"):
                     continue
                 filepath = os.path.join(dirpath, filename)
-                for t in self.getTestsForPath(testSuite, path_in_suite + (relative_dirpath, filename,), filepath, litConfig, localConfig):
+                for t in self.getTestsForPath(
+                    testSuite, path_in_suite
+                    + (
+                        relative_dirpath, filename,
+                    ),
+                    filepath,
+                    litConfig,
+                    localConfig,
+                ):
                     yield t
 
     def execute(self, test, litConfig):
-        command = [test.config.clang, '-fsycl', '-fsyntax-only', '-include', test.file_path, os.path.join(test.suite.getSourcePath(('self-contained-headers')), 'Inputs', 'test.cpp')]
+        command = [
+            test.config.clang,
+            "-fsycl",
+            "-fsyntax-only",
+            "-include",
+            test.file_path,
+            os.path.join(
+                test.suite.getSourcePath(("self-contained-headers",)),
+                "Inputs",
+                "test.cpp"
+            ),
+        ]
         try:
             out, err, exitCode = lit.util.executeCommand(
                 command,
                 # cwd=???,
                 env=test.config.environment,
                 timeout=litConfig.maxIndividualTestTime,
-                )
+            )
             status = lit.Test.PASS if exitCode == 0 else lit.Test.FAIL
             timeoutInfo = None
         except lit.util.ExecuteCommandTimeoutException as e:
             out, err, exitCode, timeoutInfo = e.out, e.err, e.exitCode, e.msg
             status = lit.Test.TIMEOUT
 
-        output = """Running command: %s\n""" % (' '.join(command),)
+        output = """Running command: %s\n""" % (" ".join(command),)
         output += f"Exit Code: {exitCode}\n"
         if timeoutInfo is not None:
             output += """Timeout: %s\n""" % (timeoutInfo,)
@@ -48,4 +70,3 @@ class CustomFormat(lit.formats.TestFormat):
             output += """Command Output (stderr):\n--\n%s\n--\n""" % (err,)
 
         return lit.Test.Result(status, output)
-
