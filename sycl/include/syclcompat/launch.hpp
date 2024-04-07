09b854d76734231e111897096f5acf571314458a
atharva.dubey <atharva.dubey@codeplay.com>
2024-04-07 17:55:38 +0100

    add support for selecting sub group size in syclcompat launch

--- sycl/include/syclcompat/launch.hpp
+++ sycl/include/syclcompat/launch.hpp
@@ -87,6 +87,43 @@ sycl::event launch(const sycl::nd_range<3> &range, size_t mem_size,
   });
 }
 
+template <int SubgroupSize, auto F, typename... Args>
+std::enable_if_t<std::is_invocable_v<decltype(F), Args...>, sycl::event>
+launch(const sycl::nd_range<3> &range, sycl::queue q, Args... args) {
+  static_assert(detail::getArgumentCount(F) == sizeof...(args),
+                "Wrong number of arguments to SYCL kernel");
+  static_assert(
+      std::is_same<std::invoke_result_t<decltype(F), Args...>, void>::value,
+      "SYCL kernels should return void");
+
+  return q.parallel_for(range, [=](sycl::nd_item<3>)
+                                   [[sycl::reqd_sub_group_size(SubgroupSize)]] {
+                                     [[clang::always_inline]] F(args...);
+                                   });
+}
+
+template <int SubgroupSize, auto F, typename... Args>
+sycl::event launch(const sycl::nd_range<3> &range, size_t mem_size,
+                   sycl::queue q, Args... args) {
+  static_assert(detail::getArgumentCount(F) == sizeof...(args) + 1,
+                "Wrong number of arguments to SYCL kernel");
+
+  using F_t = decltype(F);
+  using f_return_t = typename std::invoke_result_t<F_t, Args..., char *>;
+  static_assert(std::is_same<f_return_t, void>::value,
+                "SYCL kernels should return void");
+
+  return q.submit([&](sycl::handler &cgh) {
+    auto local_acc = sycl::local_accessor<char, 1>(mem_size, cgh);
+    cgh.parallel_for(range,
+                     [=](sycl::nd_item<3>)
+                         [[sycl::reqd_sub_group_size(SubgroupSize)]] {
+                           auto local_mem = local_acc.get_pointer();
+                           [[clang::always_inline]] F(args..., local_mem);
+                         });
+  });
+}
+
 } // namespace detail
 
 template <int Dim>
@@ -135,6 +172,33 @@ template <auto F, typename... Args>
 std::enable_if_t<std::is_invocable_v<decltype(F), Args...>, sycl::event>
 launch(const dim3 &grid, const dim3 &threads, Args... args) {
   return launch<F>(grid, threads, get_default_queue(), args...);
+}
+
+template <int SubgroupSize, auto F, int Dim, typename... Args>
+std::enable_if_t<std::is_invocable_v<decltype(F), Args...>, sycl::event>
+launch(const sycl::nd_range<Dim> &range, sycl::queue q, Args... args) {
+  return detail::launch<SubgroupSize, F>(detail::transform_nd_range<Dim>(range),
+                                         q, args...);
+}
+
+template <int SubgroupSize, auto F, int Dim, typename... Args>
+std::enable_if_t<std::is_invocable_v<decltype(F), Args...>, sycl::event>
+launch(const sycl::nd_range<Dim> &range, Args... args) {
+  return launch<SubgroupSize, F>(range, get_default_queue(), args...);
+}
+
+// Alternative launch through dim3 objects
+template <int SubgroupSize, auto F, typename... Args>
+std::enable_if_t<std::is_invocable_v<decltype(F), Args...>, sycl::event>
+launch(const dim3 &grid, const dim3 &threads, sycl::queue q, Args... args) {
+  return launch<SubgroupSize, F>(sycl::nd_range<3>{grid * threads, threads}, q,
+                                 args...);
+}
+
+template <int SubgroupSize, auto F, typename... Args>
+std::enable_if_t<std::is_invocable_v<decltype(F), Args...>, sycl::event>
+launch(const dim3 &grid, const dim3 &threads, Args... args) {
+  return launch<SubgroupSize, F>(grid, threads, get_default_queue(), args...);
 }
 
 /// Launches a kernel with the templated F param and arguments on a
@@ -218,6 +282,99 @@ template <auto F, typename... Args>
 sycl::event launch(const dim3 &grid, const dim3 &threads, size_t mem_size,
                    Args... args) {
   return launch<F>(grid, threads, mem_size, get_default_queue(), args...);
+}
+
+/// Launches a kernel with the requested sub group size SubgroupSize, templated
+/// F param and arguments on a device specified by the given nd_range and SYCL
+/// queue.
+
+/// @tparam SubgroupSize The subgroup size to be used by the kernel.
+/// @tparam F SYCL kernel to be executed, expects signature F(T* local_mem,
+/// Args... args).
+/// @tparam Dim nd_range dimension number.
+/// @tparam Args Types of the arguments to be passed to the kernel.
+/// @param range Nd_range specifying the work group and global sizes for the
+/// kernel.
+/// @param q The SYCL queue on which to execute the kernel.
+/// @param mem_size The size, in number of bytes, of the local
+/// memory to be allocated for kernel.
+/// @param args The arguments to be passed to the kernel.
+/// @return A SYCL event object that can be used to synchronize with the
+/// kernel's execution.
+template <int SubgroupSize, auto F, int Dim, typename... Args>
+sycl::event launch(const sycl::nd_range<Dim> &range, size_t mem_size,
+                   sycl::queue q, Args... args) {
+  return detail::launch<SubgroupSize, F>(detail::transform_nd_range<Dim>(range),
+                                         mem_size, q, args...);
+}
+
+/// Launches a kernel with the requested sub group size SubgroupSize, templated
+/// F param and arguments on a device specified by the given nd_range using
+/// theSYCL default queue.
+/// @tparam SubgroupSize The subgroup size to be used by the kernel.
+/// @tparam F SYCL kernel to be executed, expects signature F(T* local_mem,
+/// Args... args).
+/// @tparam Dim nd_range dimension number.
+/// @tparam Args Types of the arguments to be passed to the kernel.
+/// @param range Nd_range specifying the work group and global sizes for the
+/// kernel.
+/// @param mem_size The size, in number of bytes, of the local
+/// memory to be allocated for kernel.
+/// @param args The arguments to be passed to the kernel.
+/// @return A SYCL event object that can be used to synchronize with the
+/// kernel's execution.
+template <int SubgroupSize, auto F, int Dim, typename... Args>
+sycl::event launch(const sycl::nd_range<Dim> &range, size_t mem_size,
+                   Args... args) {
+  return launch<SubgroupSize, F>(range, mem_size, get_default_queue(), args...);
+}
+
+/// Launches a kernel with the requested sub group size SubgroupSize, templated
+/// F param and arguments on a device with a user-specified grid and block
+/// dimensions following the standard of other programming models using a
+/// user-defined SYCL queue
+/// @tparam SubgroupSize The subgroup size to be used by the kernel.
+/// @tparam F SYCL kernel to be executed, expects signature F(T* local_mem,
+/// Args... args).
+/// @tparam Dim nd_range dimension number.
+/// @tparam Args Types of the arguments to be passed to the kernel.
+/// @param grid Grid dimensions represented with an (x, y, z) iteration space.
+/// @param threads Block dimensions represented with an (x, y, z) iteration
+/// space.
+/// @param mem_size The size, in number of bytes, of the local
+/// memory to be allocated for kernel.
+/// @param args The arguments to be passed to the kernel.
+/// @return A SYCL event object that can be used to synchronize with the
+/// kernel's execution.
+template <int SubgroupSize, auto F, typename... Args>
+sycl::event launch(const dim3 &grid, const dim3 &threads, size_t mem_size,
+                   sycl::queue q, Args... args) {
+  return launch<F>(sycl::nd_range<3>{grid * threads, threads}, mem_size, q,
+                   args...);
+}
+
+/// Launches a kernel with the requested sub group size SubgroupSize, templated
+/// F param and arguments on a device with a user-specified grid and block
+/// dimensions following the standard of other programming models using the
+/// default SYCL queue.
+/// @tparam SubgroupSize The subgroup size to be used by the kernel
+/// @tparam F SYCL kernel to be executed, expects signature F(T* local_mem,
+/// Args... args).
+/// @tparam Dim nd_range dimension number.
+/// @tparam Args Types of the arguments to be passed to the kernel.
+/// @param grid Grid dimensions represented with an (x, y, z) iteration space.
+/// @param threads Block dimensions represented with an (x, y, z) iteration
+/// space.
+/// @param mem_size The size, in number of bytes, of the
+/// local memory to be allocated.
+/// @param args The arguments to be passed to the kernel.
+/// @return A SYCL event object that can be used to synchronize with the
+/// kernel's execution.
+template <int SubgroupSize, auto F, typename... Args>
+sycl::event launch(const dim3 &grid, const dim3 &threads, size_t mem_size,
+                   Args... args) {
+  return launch<SubgroupSize, F>(grid, threads, mem_size, get_default_queue(),
+                                 args...);
 }
 
 } // namespace syclcompat
