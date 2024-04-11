0496e804cc90e1339158da8d78f6287567528b9a
chedy.najjar <chedy.najjar@codeplay.com>
2024-04-11 18:19:20 +0100

    fix odd formatting

--- source/adapters/cuda/image.cpp
+++ source/adapters/cuda/image.cpp
@@ -898,8 +898,7 @@ UR_APIEXPORT ur_result_t UR_APICALL urBindlessImagesImageCopyExp(
         UR_CHECK_ERROR(cuMemcpy3DAsync(&cpy_desc, Stream));
       }
       // Synchronization is required here to handle the case of copying data
-      // from
-      // host to device, then device to device and finally device to host.
+      // from host to device, then device to device and finally device to host.
       // Without it, there is a risk of the copies not being executed in the
       // intended order.
       cuStreamSynchronize(Stream);
