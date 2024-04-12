23be3fc0e92c20fd74ddb78e4c28d58e7b8acffb
Aaron Greig <aaron.greig@codeplay.com>
2024-04-12 14:21:58 +0100

    [ABI-Break][SYCL] Remove deprecated property use_primary_context

--- sycl/include/sycl/properties/all_properties.hpp
+++ sycl/include/sycl/properties/all_properties.hpp
@@ -11,7 +11,6 @@
 #include <sycl/ext/codeplay/experimental/fusion_properties.hpp>
 #include <sycl/properties/accessor_properties.hpp>
 #include <sycl/properties/buffer_properties.hpp>
-#include <sycl/properties/context_properties.hpp>
 #include <sycl/properties/image_properties.hpp>
 #include <sycl/properties/queue_properties.hpp>
 #include <sycl/properties/reduction_properties.hpp>
