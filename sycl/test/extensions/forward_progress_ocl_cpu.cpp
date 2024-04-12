17e6ac24766d5bfc9fe268017fa1f2c5c56776ab
Lorenc Bushi <lorenc.bushi@intel.com>
2024-04-12 16:34:06 -0700

    Fix formatting and warnings

--- sycl/test/extensions/forward_progress_ocl_cpu.cpp
+++ sycl/test/extensions/forward_progress_ocl_cpu.cpp
@@ -8,22 +8,15 @@ using namespace sycl::ext::oneapi::experimental;
 std::vector<forward_progress_guarantee> ref1 = {
     forward_progress_guarantee::weakly_parallel};
 std::vector<forward_progress_guarantee> ref2 = {
-    forward_progress_guarantee::parallel,
-    forward_progress_guarantee::concurrent};
-std::vector<forward_progress_guarantee> ref3 = {
     forward_progress_guarantee::weakly_parallel,
     forward_progress_guarantee::parallel};
-std::vector<forward_progress_guarantee> ref4 = {
-    forward_progress_guarantee::weakly_parallel,
-    forward_progress_guarantee::parallel,
-    forward_progress_guarantee::concurrent};
 
 int main() {
   sycl::queue q;
   sycl::device d = q.get_device();
   auto guarantees = d.get_info<info::device::work_group_progress_capabilities<
       execution_scope::root_group>>();
-  assert(guarantees == ref3);
+  assert(guarantees == ref2);
   guarantees = d.get_info<info::device::sub_group_progress_capabilities<
       execution_scope::root_group>>();
   assert(guarantees == ref1);
