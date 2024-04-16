9063aa70b2123b6b558c908bbff83cdeee3c4896
Lorenc Bushi <lorenc.bushi@intel.com>
2024-04-16 09:24:11 -0700

    Refactor tests for readability

--- sycl/test/extensions/forward_progress_L0_gpu.cpp
+++ sycl/test/extensions/forward_progress_L0_gpu.cpp
@@ -15,22 +15,31 @@ std::vector<forward_progress_guarantee> ref2 = {
 int main() {
   sycl::queue q;
   sycl::device d = q.get_device();
+
+  // Check guarantees for execution_scope::root_group
   auto guarantees = d.get_info<info::device::work_group_progress_capabilities<
       execution_scope::root_group>>();
   assert(guarantees == ref2);
+
   guarantees = d.get_info<info::device::sub_group_progress_capabilities<
       execution_scope::root_group>>();
   assert(guarantees == ref2);
-  guarantees = d.get_info<info::device::sub_group_progress_capabilities<
-      execution_scope::work_group>>();
-  assert(guarantees == ref2);
+
   guarantees = d.get_info<info::device::work_item_progress_capabilities<
       execution_scope::root_group>>();
   assert(guarantees == ref1);
+
+  // Check guarantees for execution_scope::work_group
+  guarantees = d.get_info<info::device::sub_group_progress_capabilities<
+      execution_scope::work_group>>();
+  assert(guarantees == ref2);
+
+  guarantees = d.get_info<info::device::work_item_progress_capabilities<
+      execution_scope::work_group>>();
+  assert(guarantees == ref1);
+
+  // Check guarantees for execution_scope::sub_group
   guarantees = d.get_info<info::device::work_item_progress_capabilities<
       execution_scope::sub_group>>();
-  assert(guarantees == ref1);
-  guarantees = d.get_info<info::device::work_item_progress_capabilities<
-      execution_scope::work_group>>();
   assert(guarantees == ref1);
 }
--- sycl/test/kernel_param/forward_progress.cpp
+++ sycl/test/kernel_param/forward_progress.cpp
@@ -6,6 +6,7 @@ using namespace sycl::ext::oneapi::experimental;
 
 template <forward_progress_guarantee guarantee>
 void check_props(sycl::queue &q) {
+  // Check properties at execution_scope::root_group coordination level
   q.single_task(
       properties{work_group_progress<guarantee, execution_scope::root_group>},
       [=]() {});
@@ -13,14 +14,18 @@ void check_props(sycl::queue &q) {
       properties{sub_group_progress<guarantee, execution_scope::root_group>},
       [=]() {});
   q.single_task(
-      properties{sub_group_progress<guarantee, execution_scope::work_group>},
-      [=]() {});
-  q.single_task(
       properties{work_item_progress<guarantee, execution_scope::root_group>},
+      [=]() {});
+
+  // Check properties at execution_scope::work_group coordination level
+  q.single_task(
+      properties{sub_group_progress<guarantee, execution_scope::work_group>},
       [=]() {});
   q.single_task(
       properties{work_item_progress<guarantee, execution_scope::work_group>},
       [=]() {});
+
+  // Check properties at execution_scope::sub_group coordination level 
   q.single_task(
       properties{work_item_progress<guarantee, execution_scope::sub_group>},
       [=]() {});
