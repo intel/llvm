056731070be46069ae96d7783278d7ed25807026
Cai, Justin <justin.cai@intel.com>
2024-04-16 11:22:35 -0700

    Add comment

--- sycl/test-e2e/Regression/default-constructed-local-accessor.cpp
+++ sycl/test-e2e/Regression/default-constructed-local-accessor.cpp
@@ -1,3 +1,5 @@
+// -O0 is necessary; on higher levels of optimization, an error
+// would not occur because of dead argument elimination of the local_accessor.
 // RUN: %{build} -o %t.out -O0
 // RUN: %{run} %t.out
 
