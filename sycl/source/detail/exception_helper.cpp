995deaa01aedd2e68c96d1350bfecbcbfafb8245
Byoungro So <byoungro.so@intel.com>
2024-04-17 15:02:26 -0700

    export
    Signed-off-by: Byoungro So <byoungro.so@intel.com>

--- sycl/source/detail/exception_helper.cpp
+++ sycl/source/detail/exception_helper.cpp
@@ -3,7 +3,8 @@
 namespace sycl {
 inline namespace _V1 {
 namespace detail {
-void throw_invalid_parameter(const char *Msg, const pi_int32 PIErr) {
+__SYCL_EXPORT void throw_invalid_parameter(const char *Msg,
+                                           const pi_int32 PIErr) {
   throw sycl::invalid_parameter_error(Msg, PIErr);
 }
 } // namespace detail
