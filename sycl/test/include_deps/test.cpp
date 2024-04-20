d912fe4aec5b0ca6c412887a563a4125972f0b19
Byoungro So <byoungro.so@intel.com>
2024-04-20 01:03:20 -0700

    clang-format
    Signed-off-by: Byoungro So <byoungro.so@intel.com>

--- sycl/test/include_deps/test.cpp
+++ sycl/test/include_deps/test.cpp
@@ -1,6 +1,7 @@
 // Use update_test.sh to (re-)generate the checks
 // REQUIRES: linux
 // RUN: bash %S/deps_known.sh | FileCheck %s
+
 // CHECK-LABEL: Dependencies for <sycl/detail/defines_elementary.hpp>:
 // CHECK-NEXT: /dev/null: /dev/null
 // CHECK-NEXT: detail/defines_elementary.hpp
