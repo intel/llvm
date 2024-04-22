f2af7ab14e87be3ed042caccdb9f9259c79d1ac3
Zhao, Yang2 <yang2.zhao@intel.com>
2024-04-21 20:30:02 -0700

    fix tests

--- sycl/test-e2e/AddressSanitizer/out-of-bounds/local/local_accessor_multiargs.cpp
+++ sycl/test-e2e/AddressSanitizer/out-of-bounds/local/local_accessor_multiargs.cpp
@@ -1,9 +1,9 @@
 // REQUIRES: linux
-// RUN: %{build} %device_sanitizer_flags -g -O0 -o %t.out
+// RUN: %{build} %device_asan_flags -g -O0 -o %t.out
 // RUN: env SYCL_PREFER_UR=1 %{run} not %t.out 2>&1 | FileCheck %s
-// RUN: %{build} %device_sanitizer_flags -g -O1 -o %t.out
+// RUN: %{build} %device_asan_flags -g -O1 -o %t.out
 // RUN: env SYCL_PREFER_UR=1 %{run} not %t.out 2>&1 | FileCheck %s
-// RUN: %{build} %device_sanitizer_flags -g -O2 -o %t.out
+// RUN: %{build} %device_asan_flags -g -O2 -o %t.out
 // RUN: env SYCL_PREFER_UR=1 %{run} not %t.out 2>&1 | FileCheck %s
 #include <sycl/sycl.hpp>
 #include <vector>
