3ff6955969aa6a671c63394f289bf5616ac3ae44
Hu, Peisen <peisen.hu@intel.com>
2024-04-22 12:40:21 -0700

    [SYCL] Restore correct changes that were reverted
    Signed-off-by: Hu, Peisen <peisen.hu@intel.com>

--- clang/test/Frontend/sycl-unnamed-lambdas-default.cpp
+++ clang/test/Frontend/sycl-unnamed-lambdas-default.cpp
@@ -1,12 +1,9 @@
 // RUN: %clang_cc1 -fsycl-is-device %s -verify -DUNNAMED_LAMBDAS
 // RUN: %clang_cc1 -fsycl-is-host %s -verify -DUNNAMED_LAMBDAS
 //
-// RUN: %clang_cc1 -fsycl-is-host -sycl-std=2017 %s -verify
 // RUN: %clang_cc1 -fsycl-is-host -sycl-std=2020 %s -verify -DUNNAMED_LAMBDAS
 //
 // RUN: %clang_cc1 -fsycl-is-host -fno-sycl-unnamed-lambda %s -verify
-// RUN: %clang_cc1 -fsycl-is-host -sycl-std=2017 -fno-sycl-unnamed-lambda %s -verify
-// RUN: %clang_cc1 -fsycl-is-host -sycl-std=2017 -fsycl-unnamed-lambda %s -verify -DUNNAMED_LAMBDAS
 
 // expected-no-diagnostics
 #if defined(UNNAMED_LAMBDAS) && !defined(__SYCL_UNNAMED_LAMBDA__)
