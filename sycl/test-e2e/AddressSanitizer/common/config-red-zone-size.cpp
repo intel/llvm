eff963834bd948677853b908ea609190522cc42f
Maosu Zhao <maosu.zhao@intel.com>
2024-04-22 14:53:33 +0800

    [DeviceSanitizer] Add tests to check customizing red zone size

---
+++ sycl/test-e2e/AddressSanitizer/common/config-red-zone-size.cpp
@@ -0,0 +1,25 @@
+// REQUIRES: linux
+// RUN: %{build} %device_sanitizer_flags -O0 -g -o %t
+// RUN: env SYCL_PREFER_UR=1 UR_LAYER_ASAN_OPTIONS=redzone:64 %{run} not %t 2>&1 | FileCheck %s
+// RUN: env SYCL_PREFER_UR=1 UR_LOG_SANITIZER=level:debug UR_LAYER_ASAN_OPTIONS=redzone:8 %{run} %t 2>&1 | FileCheck --check-prefixes CHECK-MIN %s
+// RUN: env SYCL_PREFER_UR=1 UR_LOG_SANITIZER=level:debug UR_LAYER_ASAN_OPTIONS=max_redzone:4096 %{run} %t 2>&1 | FileCheck --check-prefixes CHECK-MAX %s
+
+#include "sycl/sycl.hpp"
+
+int main() {
+  sycl::queue q;
+  constexpr std::size_t N = 8;
+  auto *array = sycl::malloc_device<char>(N, q);
+
+  q.submit([&](sycl::handler &h) {
+     h.single_task<class Test>([=]() { ++array[N + 24]; });
+   }).wait();
+  // CHECK: ERROR: DeviceSanitizer: out-of-bounds-access on Device USM
+  // CHECK: {{READ of size 1 at kernel <.*Test> LID\(0, 0, 0\) GID\(0, 0, 0\)}}
+  // CHECK: {{  #0 .* .*config-red-zone-size.cpp:}}[[@LINE-4]]
+  // CHECK-MIN: Trying to set redzone size to a value less than 16 is ignored
+  // CHECK-MAX: Trying to set max redzone size to a value greater than 2048 is ignored
+
+  sycl::free(array, q);
+  return 0;
+}
