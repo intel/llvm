3b8846baa06db426d499d47da9ee41f4d5d66f3f
Michael D Toguchi <michael.d.toguchi@intel.com>
2024-04-22 15:18:58 -0700

    clang format

--- clang/lib/Driver/ToolChains/SYCL.cpp
+++ clang/lib/Driver/ToolChains/SYCL.cpp
@@ -364,19 +364,35 @@ SYCL::getDeviceLibraries(const Compilation &C, const llvm::Triple &TargetTriple,
 // The list should match pre-built SYCL device library files located in
 // compiler package. Once we add or remove any SYCL device library files,
 // the list should be updated accordingly.
-static llvm::SmallVector<StringRef, 16> SYCLDeviceLibList {
-  "bfloat16", "crt", "cmath", "cmath-fp64", "complex", "complex-fp64",
+static llvm::SmallVector<StringRef, 16> SYCLDeviceLibList{
+    "bfloat16",
+    "crt",
+    "cmath",
+    "cmath-fp64",
+    "complex",
+    "complex-fp64",
 #if defined(_WIN32)
-      "msvc-math",
+    "msvc-math",
 #else
-      "sanitizer",
+    "sanitizer",
 #endif
-      "imf", "imf-fp64", "imf-bf16", "itt-compiler-wrappers", "itt-stubs",
-      "itt-user-wrappers", "fallback-cassert", "fallback-cstring",
-      "fallback-cmath", "fallback-cmath-fp64", "fallback-complex",
-      "fallback-complex-fp64", "fallback-imf", "fallback-imf-fp64",
-      "fallback-imf-bf16", "fallback-bfloat16", "native-bfloat16"
-};
+    "imf",
+    "imf-fp64",
+    "imf-bf16",
+    "itt-compiler-wrappers",
+    "itt-stubs",
+    "itt-user-wrappers",
+    "fallback-cassert",
+    "fallback-cstring",
+    "fallback-cmath",
+    "fallback-cmath-fp64",
+    "fallback-complex",
+    "fallback-complex-fp64",
+    "fallback-imf",
+    "fallback-imf-fp64",
+    "fallback-imf-bf16",
+    "fallback-bfloat16",
+    "native-bfloat16"};
 
 const char *SYCL::Linker::constructLLVMLinkCommand(
     Compilation &C, const JobAction &JA, const InputInfo &Output,
