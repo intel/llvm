47503b7d695db92e3f77c1995c47c676bd53ebd3
Fábio Mestre <fabio.mestre@codeplay.com>
2024-04-23 17:10:14 +0100

    Address review comments

--- sycl/test-e2e/Graph/RecordReplay/kernel_bundle_spirv.cpp
+++ sycl/test-e2e/Graph/RecordReplay/kernel_bundle_spirv.cpp
@@ -1,5 +1,11 @@
 // RUN: %{build} -o %t.out
-// RUN: %if level_zero %{%{run} %t.out %S/../Inputs/Kernels/kernels.spv %}
+// RUN: %{run} %t.out %S/../Inputs/Kernels/kernels.spv
+// Extra run to check for leaks in Level Zero using UR_L0_LEAKS_DEBUG
+// RUN: %if level_zero %{env SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=0 %{l0_leak_check} %{run} %t.out %S/../Inputs/Kernels/kernels.spv 2>&1 | FileCheck %s --implicit-check-not=LEAK %}
+// Extra run to check for immediate-command-list in Level Zero
+// RUN: %if level_zero && linux %{env SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=1 %{l0_leak_check} %{run} %t.out %S/../Inputs/Kernels/kernels.spv 2>&1 | FileCheck %s --implicit-check-not=LEAK %}
+
+// REQUIRES: level_zero
 
 #define GRAPH_E2E_RECORD_REPLAY
 
