e01a75662253982537b60d84f1ea2f38c5156718
Hu, Peisen <peisen.hu@intel.com>
2024-04-23 23:15:06 -0700

    [SYCL] Restore vec-type-hint.cpp and vec-type-hint-2.cpp
    Signed-off-by: Hu, Peisen <peisen.hu@intel.com>

--- clang/test/SemaSYCL/vec-type-hint.cpp
+++ clang/test/SemaSYCL/vec-type-hint.cpp
@@ -1,26 +1,8 @@
-// RUN: %clang_cc1 -fsycl-is-device -sycl-std=2020 -internal-isystem %S/Inputs -fsyntax-only -verify %s
+// RUN: %clang_cc1 -fsycl-is-device -sycl-std=2020 -fsyntax-only -DKERNEL= -verify=sycl-2020 %s
+// RUN: %clang_cc1 -fsyntax-only -DKERNEL=kernel -verify=opencl -x cl %s
 
-// Test which verifies [[sycl::vec_type_hint()]] is accepted
-// with non-conforming lambda syntax.
+// opencl-no-diagnostics
 
-// NOTE: This attribute is not supported in the SYCL backends.
-// To be minimally conformant with SYCL2020, attribute is
-// accepted by the Clang FE with a warning. No additional
-// semantic handling or IR generation is done for this
-// attribute.
-
-#include "sycl.hpp"
-
-struct test {};
-
-using namespace sycl;
-queue q;
-
-void bar() {
-  q.submit([&](handler &h) {
-    h.single_task<class kernelname>(
-        // expected-warning@+1 {{attribute 'vec_type_hint' is deprecated; attribute ignored}}
-        []() [[sycl::vec_type_hint(test)]] {});
-  });
-}
-
+// __attribute__((vec_type_hint)) is deprecated without replacement in SYCL
+// 2020 mode, but is allowed in OpenCL modes.
+KERNEL __attribute__((vec_type_hint(int))) void foo() {} // sycl-2020-warning {{attribute 'vec_type_hint' is deprecated; attribute ignored}}
