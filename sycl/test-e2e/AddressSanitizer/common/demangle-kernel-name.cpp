81045680d1b9949456fedcc7d7869528521b3de0
Zhao, Yang2 <yang2.zhao@intel.com>
2024-04-23 20:51:18 -0700

    add aspect-usm_device_allocations

--- sycl/test-e2e/AddressSanitizer/common/demangle-kernel-name.cpp
+++ sycl/test-e2e/AddressSanitizer/common/demangle-kernel-name.cpp
@@ -1,4 +1,4 @@
-// REQUIRES: linux, cpu
+// REQUIRES: linux, cpu, aspect-usm_device_allocations
 // RUN: %{build} %device_asan_flags -O2 -g -o %t
 // RUN: env SYCL_PREFER_UR=1 %{run} not %t &> %t.txt ; FileCheck --input-file %t.txt %s
 #include <sycl/detail/core.hpp>
--- sycl/test-e2e/AddressSanitizer/common/kernel-debug.cpp
+++ sycl/test-e2e/AddressSanitizer/common/kernel-debug.cpp
@@ -1,4 +1,4 @@
-// REQUIRES: linux, cpu
+// REQUIRES: linux, cpu, aspect-usm_device_allocations
 // RUN: %{build} %device_asan_flags -O2 -g -o %t
 // RUN: env SYCL_PREFER_UR=1 UR_LAYER_ASAN_OPTIONS=debug:1 %{run} %t 2>&1 | FileCheck --check-prefixes CHECK-DEBUG %s
 // RUN: env SYCL_PREFER_UR=1 UR_LAYER_ASAN_OPTIONS=debug:0 %{run} %t 2>&1 | FileCheck %s
