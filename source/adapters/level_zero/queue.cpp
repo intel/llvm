738622923c03e2cf1db8e7bb1127c2825ad52f9a
Lorenc Bushi <lorenc.bushi@intel.com>
2024-04-24 11:58:56 -0700

    Implement Queue Flush operation

--- source/adapters/level_zero/queue.cpp
+++ source/adapters/level_zero/queue.cpp
@@ -902,10 +902,8 @@ UR_APIEXPORT ur_result_t UR_APICALL urQueueFinish(
 UR_APIEXPORT ur_result_t UR_APICALL urQueueFlush(
     ur_queue_handle_t Queue ///< [in] handle of the queue to be flushed.
 ) {
-  // Flushing cross-queue dependencies is covered by
-  // createAndRetainUrZeEventList, so this can be left as a no-op.
-  std::ignore = Queue;
-  return UR_RESULT_SUCCESS;
+  std::scoped_lock<ur_shared_mutex> Lock(Queue->Mutex);
+  return Queue->executeAllOpenCommandLists();
 }
 
 // Configuration of the command-list batching.
