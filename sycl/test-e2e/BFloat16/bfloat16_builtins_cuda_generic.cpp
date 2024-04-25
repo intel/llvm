7ce174aa86ebeaa49dbccbdf7e1b0c6cc0fdcab3
Fraser Cormack <fraser@codeplay.com>
2024-04-25 15:32:48 +0100

    feedback: remove aspect; unsupported cpu

--- sycl/test-e2e/BFloat16/bfloat16_builtins_cuda_generic.cpp
+++ sycl/test-e2e/BFloat16/bfloat16_builtins_cuda_generic.cpp
@@ -5,14 +5,14 @@
 // + below sm_80 always uses generic impls
 
 // DEFINE: %{mathflags} = %if cl_options %{/clang:-fno-fast-math%} %else %{-fno-fast-math%}
-// REQUIRES: aspect-ext_oneapi_bfloat16_math_functions
 
 // If CUDA, test "new" again for sm_75/generic
 // RUN:  %if any-device-is-cuda %{ %if preview-breaking-changes-supported %{  %clangxx -fsycl -fpreview-breaking-changes -fsycl-targets=%{sycl_triple}  -Xsycl-target-backend --cuda-gpu-arch=sm_75  %s -o %t3.out %{mathflags} %} %}
 // RUN:  %if any-device-is-cuda %{ %if preview-breaking-changes-supported %{  %{run} %t3.out  %} %}
 
 // Currently the feature isn't supported on FPGA.
-// UNSUPPORTED: accelerator
+// FIXME: enable opaque pointers support on CPU.
+// UNSUPPORTED: accelerator, cpu
 #include "bfloat16_builtins.hpp"
 
 int main() {
