0bd148672bd66d87ba26733ec7f787bc2465f827
Lorenc Bushi <lorenc.bushi@intel.com>
2024-04-26 12:10:23 -0700

    Add uniot test

---
+++ sycl/unittests/Extensions/OneAPIProd.cpp
@@ -0,0 +1,29 @@
+//==------------ OneAPIProd.cpp --- sycl_ext_oneapi_prod unit tests
+//----------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include <helpers/PiMock.hpp>
+
+using namespace sycl;
+
+static bool QueueFlushed = false;
+
+static pi_result redefinedQueueFlush(pi_queue Queue) {
+  QueueFlushed = true;
+  return PI_SUCCESS;
+}
+
+TEST(OneAPIProdTest, PiQueueFlush) {
+  sycl::unittest::PiMock Mock;
+  sycl::platform Plt = Mock.getPlatform();
+  Mock.redefine<detail::PiApiKind::piQueueFlush>(redefinedQueueFlush);
+  context Ctx{Plt};
+  queue Queue{Ctx};
+  queue.ext_oneapi_prod();
+  EXPECT_TRUE(QueueFlushed);
+}
