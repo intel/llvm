df577ccbac7908561ae036c3fd0766f22e3f5a27
Georgi Mirazchiyski <georgi.mirazchiyski@codeplay.com>
2024-05-02 11:19:04 +0100

    Update AMDGCN and NVPTX tests

--- clang/test/Driver/sycl-device-traits-macros-amdgcn.cpp
+++ clang/test/Driver/sycl-device-traits-macros-amdgcn.cpp
@@ -1,9 +1,17 @@
-// REQUIRES: amdgpu-registered-target
+// Check device traits macros are defined if sycl is enabled:
+// Compiling for a specific HIP target passing the device to '-fsycl-targets'.
+// RUN: %clangxx -nogpulib -fsycl -fsycl-targets=amd_gpu_gfx906 \
+// RUN:   -fsycl-libspirv-path=%S/Inputs/SYCL/libspirv.bc -### %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-SYCL-AMDGCN-AMD-AMDHSA-DEVICE-TRIPLE %s
+// CHECK-SYCL-AMDGCN-AMD-AMDHSA-DEVICE-TRIPLE-NOT: "-D__SYCL_ANY_DEVICE_HAS_ANY_ASPECT__=1"
+// CHECK-SYCL-AMDGCN-AMD-AMDHSA-DEVICE-TRIPLE: "-D__SYCL_ANY_DEVICE_HAS_{{.*}}__=1"
+// CHECK-SYCL-AMDGCN-AMD-AMDHSA-DEVICE-TRIPLE: "{{(-D__SYCL_ALL_DEVICES_HAVE_)?}}{{.*}}{{(__=1)?}}"
 
 // Check device traits macros are defined if sycl is enabled:
-// RUN:   %clangxx -fsycl -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx906 -### %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=CHECK-SYCL-AMDGCN-AMD-AMDHSA %s
-// CHECK-SYCL-AMDGCN-AMD-AMDHSA-NOT: "-D__SYCL_ANY_DEVICE_HAS_ANY_ASPECT__=1"
-// CHECK-SYCL-AMDGCN-AMD-AMDHSA: "-D__SYCL_ANY_DEVICE_HAS_{{.*}}__=1"
-// CHECK-SYCL-AMDGCN-AMD-AMDHSA: "{{(-D__SYCL_ALL_DEVICES_HAVE_)?}}{{.*}}{{(__=1)?}}"
-
+// Compiling for a HIP target passing the device arch to '--offload-arch'.
+// RUN: %clangxx -nogpulib -fsycl -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx906 \
+// RUN:   -fsycl-libspirv-path=%S/Inputs/SYCL/libspirv.bc -### %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-SYCL-AMDGCN-AMD-AMDHSA-OFFLOAD-ARCH %s
+// CHECK-SYCL-AMDGCN-AMD-AMDHSA-OFFLOAD-ARCH-NOT: "-D__SYCL_ANY_DEVICE_HAS_ANY_ASPECT__=1"
+// CHECK-SYCL-AMDGCN-AMD-AMDHSA-OFFLOAD-ARCH: "-D__SYCL_ANY_DEVICE_HAS_{{.*}}__=1"
+// CHECK-SYCL-AMDGCN-AMD-AMDHSA-OFFLOAD-ARCH: "{{(-D__SYCL_ALL_DEVICES_HAVE_)?}}{{.*}}{{(__=1)?}}"
--- clang/test/Driver/sycl-device-traits-macros-nvptx.cpp
+++ clang/test/Driver/sycl-device-traits-macros-nvptx.cpp
@@ -1,9 +1,20 @@
-// REQUIRES: nvptx-registered-target
+// Check device traits macros are defined if sycl is enabled:
+// Compiling for the default CUDA target passing the triple to '-fsycl-targets'.
+// RUN: %clangxx -nocudalib -fsycl -fsycl-targets=nvptx64-nvidia-cuda -### %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-SYCL-NVPTX-NVIDIA-CUDA-DEFAULT-TRIPLE %s
+// CHECK-SYCL-NVPTX-NVIDIA-CUDA-DEFAULT-TRIPLE-COUNT-2: "-D__SYCL_ANY_DEVICE_HAS_ANY_ASPECT__=1"
+// CHECK-SYCL-NVPTX-NVIDIA-CUDA-DEFAULT-TRIPLE: "{{(-D__SYCL_ALL_DEVICES_HAVE_)?}}{{.*}}{{(__=1)?}}"
 
 // Check device traits macros are defined if sycl is enabled:
-// RUN:   %clangxx -fsycl -fsycl-targets=nvptx64-nvidia-cuda -### %s 2>&1 \
-// RUN:   | FileCheck -check-prefix=CHECK-SYCL-NVPTX-NVIDIA-CUDA %s
-// CHECK-SYCL-NVPTX-NVIDIA-CUDA-COUNT-2: "-D__SYCL_ANY_DEVICE_HAS_ANY_ASPECT__=1"
-// CHECK-SYCL-NVPTX-NVIDIA-CUDA: "-D__SYCL_ANY_DEVICE_HAS_{{.*}}__=1"
-// CHECK-SYCL-NVPTX-NVIDIA-CUDA: "{{(-D__SYCL_ALL_DEVICES_HAVE_)?}}{{.*}}{{(__=1)?}}"
+// Compiling for a specific CUDA target passing the device to '-fsycl-targets'.
+// RUN: %clangxx -nocudalib -fsycl -fsycl-targets=nvidia_gpu_sm_80 -### %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-SYCL-NVPTX-NVIDIA-CUDA-DEVICE-TRIPLE %s
+// CHECK-SYCL-NVPTX-NVIDIA-CUDA-DEVICE-TRIPLE-COUNT-2: "-D__SYCL_ANY_DEVICE_HAS_ANY_ASPECT__=1"
+// CHECK-SYCL-NVPTX-NVIDIA-CUDA-DEVICE-TRIPLE: "{{(-D__SYCL_ALL_DEVICES_HAVE_)?}}{{.*}}{{(__=1)?}}"
 
+// Check device traits macros are defined if sycl is enabled:
+// Compiling for a CUDA target passing the device arch to '--offload-arch'.
+// RUN: %clangxx -nocudalib -fsycl -fsycl-targets=nvptx64-nvidia-cuda -Xsycl-target-backend --offload-arch=sm_80 -### %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-SYCL-NVPTX-NVIDIA-CUDA-OFFLOAD-ARCH %s
+// CHECK-SYCL-NVPTX-NVIDIA-CUDA-OFFLOAD-ARCH-COUNT-2: "-D__SYCL_ANY_DEVICE_HAS_ANY_ASPECT__=1"
+// CHECK-SYCL-NVPTX-NVIDIA-CUDA-OFFLOAD-ARCH: "{{(-D__SYCL_ALL_DEVICES_HAVE_)?}}{{.*}}{{(__=1)?}}"
