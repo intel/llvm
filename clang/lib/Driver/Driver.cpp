c17eb92d5942595881b262c036d9fbb2562b4bc7
Michael D Toguchi <michael.d.toguchi@intel.com>
2024-05-07 16:35:39 -0700

    [Driver][SYCL][FPGA] Use bundled device libraries for FPGA targets
    The change that introduced the ability to use device libraries as IR
    files does not play well with the FPGA target.  The IR generated with
    spir64_fpga is different, causing issues as the bitcode based device
    libraries are built with spir64.
    
    Update the compilation flow specific to FPGA to use the bundled device
    libraries as opposed to the bitcode files.

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -5905,7 +5905,10 @@ class OffloadingActionBuilder final {
             ++NumOfDeviceLibLinked;
             Arg *InputArg = MakeInputArg(Args, C.getDriver().getOpts(),
                                          Args.MakeArgString(LibName));
-            if (TC->getTriple().isNVPTX()) {
+            if (TC->getTriple().isNVPTX() ||
+                (TC->getTriple().isSPIR() &&
+                 TC->getTriple().getSubArch() ==
+                     llvm::Triple::SPIRSubArch_fpga)) {
               auto *SYCLDeviceLibsInputAction =
                   C.MakeAction<InputAction>(*InputArg, types::TY_Object);
               auto *SYCLDeviceLibsUnbundleAction =
