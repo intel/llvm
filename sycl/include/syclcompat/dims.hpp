6ba2ad88a3691f59ecd161b60f6f6bff0bba3ae2
Alejandro Acosta <alejandro.acosta@codeplay.com>
2024-05-09 10:48:40 +0100

    Set default values to zero

--- sycl/include/syclcompat/dims.hpp
+++ sycl/include/syclcompat/dims.hpp
@@ -38,7 +38,7 @@ public:
 
   dim3(const sycl::range<1> &r) : x(r[0]), y(1), z(1) {}
 
-  constexpr dim3() : x(1), y(1), z(1) {}
+  constexpr dim3() : x(0), y(0), z(0) {}
 
   constexpr dim3(size_t x, size_t y = 1, size_t z = 1) : x(x), y(y), z(z) {}
 
