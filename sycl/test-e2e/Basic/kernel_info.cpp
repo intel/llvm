b758dda07d9c32e9659d9062435a73eaf4a411a6
Nick Sarnie <sarnex@users.noreply.github.com>
2024-05-13 18:02:33 +0000

    [SYCL][E2E] Fix two XPASSes on OCL CPU (#13749)
    They are passing on CPU in the nightlies.
    
    Closes: https://github.com/intel/llvm/issues/13735
    
    ---------
    
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- sycl/test-e2e/Basic/kernel_info.cpp
+++ sycl/test-e2e/Basic/kernel_info.cpp
@@ -4,8 +4,8 @@
 // Fail is flaky for level_zero, enable when fixed.
 // UNSUPPORTED: level_zero
 //
-// Consistently fails with opencl non-fpga, enable when fixed.
-// XFAIL: opencl && !accelerator
+// Consistently fails with opencl gpu, enable when fixed.
+// XFAIL: opencl && gpu
 
 //==--- kernel_info.cpp - SYCL kernel info test ----------------------------==//
 //
