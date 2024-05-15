f08e9815a05e0cd1005cbdfa24d78522d638216f
Lorenc Bushi <lorenc.bushi@intel.com>
2024-05-15 15:03:53 -0700

    Fix unused variable warning

--- sycl/source/detail/device_impl.cpp
+++ sycl/source/detail/device_impl.cpp
@@ -859,11 +859,13 @@ bool device_impl::extOneapiCanCompile(
 // threads created at threadScope from a coordination scope given by
 // coordinationScope
 sycl::ext::oneapi::experimental::forward_progress_guarantee
-    device_impl::getHostProgressGuarantee(
-        ext::oneapi::experimental::execution_scope, // threadScope
-        ext::oneapi::experimental::execution_scope) // coordinationScope {
-    return sycl::ext::oneapi::experimental::forward_progress_guarantee::
-        weakly_parallel;
+device_impl::getHostProgressGuarantee(
+    ext::oneapi::experimental::execution_scope threadScope,
+    ext::oneapi::experimental::execution_scope coordinationScope) {
+  (void)threadScope;
+  (void)coordinationScope;
+  return sycl::ext::oneapi::experimental::forward_progress_guarantee::
+      weakly_parallel;
 }
 
 // Returns the strongest progress guarantee that can be provided by this device
