3c1e42491bcd42cb04be01cc6bf9c2101fda9dc5
Sidorov, Dmitry <dmitry.sidorov@intel.com>
2024-05-16 06:54:41 -0700

    Remove unused vars
    Signed-off-by: Sidorov, Dmitry <dmitry.sidorov@intel.com>

--- sycl/include/sycl/ext/oneapi/matrix/matrix-unified.hpp
+++ sycl/include/sycl/ext/oneapi/matrix/matrix-unified.hpp
@@ -557,9 +557,6 @@ joint_matrix_prefetch(Group sg, T *Ptr, size_t stride,
 #else
   std::ignore = sg;
   auto prop = properties.template get_property<prefetch_hint_key>();
-  // Will be removed once SPIRV implementation also uses offsetpointer
-  size_t coordX = 0;
-  size_t coordY = 0;
   __spirv_CooperativeMatrixPrefetchINTEL<T>(
       Ptr, NumRows, NumCols, detail::PropertyMetaInfo<decltype(prop)>::value,
       sycl::detail::joint_matrix_layout_to_spv(Layout), stride);
