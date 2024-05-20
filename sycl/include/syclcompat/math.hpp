3f2bba257605893faa30be2585bd8ac9664fcdf9
Joe Todd <joe.todd@codeplay.com>
2024-05-20 08:54:10 +0100

    Catch an out-of-bounds case in bfe (FIXME)

--- sycl/include/syclcompat/math.hpp
+++ sycl/include/syclcompat/math.hpp
@@ -137,7 +137,9 @@ inline bool isnan(const sycl::ext::oneapi::bfloat16 a) {
 template <typename T>
 inline std::enable_if_t<std::is_unsigned_v<T>, T>
 bfe(const T source, const uint32_t bit_start, const uint32_t num_bits) {
-  const T mask = (T{1} << num_bits) - 1;
+  //FIXME(jtodd): This ternary was added to catch a case which may be
+  //undefined anyway. Consider that we are losing perf here.
+  const T mask = num_bits >= CHAR_BIT * sizeof(T) ? T{-1} : ((T{1} << num_bits) - 1);
   return (source >> bit_start) & mask;
 }
 
@@ -192,7 +194,9 @@ bfe_safe(const T source, const uint32_t bit_start, const uint32_t num_bits) {
   const uint32_t pos = std::min(bit_start, bit_width);
   const uint32_t len = std::min(pos + num_bits, bit_width) - pos;
   if constexpr (std::is_signed_v<T>) {
-    const T mask = (T{1} << len) - 1;
+    //FIXME(jtodd): As above, catching a case whose result is undefined
+    //and likely losing perf.
+    const T mask = len >= bit_width ? T{-1} : (T{1} << len) - 1;
 
     // Find the sign-bit, the result is padded with the sign bit of the
     // extracted field.
