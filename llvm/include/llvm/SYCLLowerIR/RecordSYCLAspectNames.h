7d6af99df2670403a4f717f9820a2859b95e9338
Cai, Justin <justin.cai@intel.com>
2024-05-21 14:46:15 +0000

    Update comment

--- llvm/include/llvm/SYCLLowerIR/RecordSYCLAspectNames.h
+++ llvm/include/llvm/SYCLLowerIR/RecordSYCLAspectNames.h
@@ -11,8 +11,11 @@
 // apsects a function uses. The format of this metadata initially just an
 // integer value corresponding to the enum value in C++. The !sycl_aspects
 // named metadata contains the associations from aspect values to aspect names.
-// This pass takes the associations from !sycl_aspects and then updates all the
-// !sycl_used_aspects metadata to include the aspect names.
+// These associations are needed later in sycl-post-link, but we drop
+// !sycl_aspects before that to avoid LLVM IR bloat, so this pass takes
+// the associations from !sycl_aspects and then updates all the
+// !sycl_used_aspects metadata to include the aspect names, which allows us
+// to preserve these associations.
 //===----------------------------------------------------------------------===//
 //
 #ifndef LLVM_RECORD_SYCL_ASPECT_NAMES
