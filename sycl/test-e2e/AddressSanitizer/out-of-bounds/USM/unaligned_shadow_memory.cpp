5a9f7dac48d3657dfc37159967eb518e34291dc2
Maosu Zhao <maosu.zhao@intel.com>
2024-05-21 15:25:14 +0800

    format code

--- sycl/test-e2e/AddressSanitizer/out-of-bounds/USM/unaligned_shadow_memory.cpp
+++ sycl/test-e2e/AddressSanitizer/out-of-bounds/USM/unaligned_shadow_memory.cpp
@@ -20,22 +20,19 @@ typedef unsigned _BitInt(128) TestType;
 int main() {
   sycl::queue Q;
   constexpr std::size_t size = 128 + (ASAN_SHADOW_GRANULARITY - 1);
-  TestType *array =
-      (TestType *)sycl::malloc_device<char>(size, Q);
+  TestType *array = (TestType *)sycl::malloc_device<char>(size, Q);
 
   Q.submit([&](sycl::handler &h) {
     h.parallel_for<class MyKernelR_4>(
         sycl::nd_range<1>(size / sizeof(TestType) + 1, 1),
-        [=](sycl::nd_item<1> item) {
-          ++array[item.get_global_id(0)];
-        });
+        [=](sycl::nd_item<1> item) { ++array[item.get_global_id(0)]; });
   });
   Q.wait();
   // CHECK: ERROR: DeviceSanitizer: out-of-bounds-access on Device USM
   // CHECK1: {{READ of size 8 at kernel <.*MyKernelR_4> LID\(0, 0, 0\) GID\(17, 0, 0\)}}
-  // CHECK1: {{  #0 .* .*unaligned_shadow_memory.cpp:}}[[@LINE-6]]
+  // CHECK1: {{  #0 .* .*unaligned_shadow_memory.cpp:}}[[@LINE-5]]
   // CHECK2: {{READ of size 16 at kernel <.*MyKernelR_4> LID\(0, 0, 0\) GID\(8, 0, 0\)}}
-  // CHECK2: {{  #0 .* .*unaligned_shadow_memory.cpp:}}[[@LINE-8]]
+  // CHECK2: {{  #0 .* .*unaligned_shadow_memory.cpp:}}[[@LINE-7]]
 
   return 0;
 }
