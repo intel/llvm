55f015ade963133d685a3f02dff92c79fb83b1e8
Larsen, Steffen <steffen.larsen@intel.com>
2024-05-23 05:33:20 -0700

    Add native_cpu PI interfaces
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- sycl/plugins/native_cpu/pi_native_cpu.cpp
+++ sycl/plugins/native_cpu/pi_native_cpu.cpp
@@ -1292,6 +1292,69 @@ pi_result piextKernelSuggestMaxCooperativeGroupCount(
   return PI_ERROR_UNSUPPORTED_FEATURE;
 }
 
+pi_result
+piextVirtualMemGranularityGetInfo(pi_context context, pi_device device,
+                                  pi_virtual_mem_granularity_info param_name,
+                                  size_t param_value_size, void *param_value,
+                                  size_t *param_value_size_ret) {
+  return pi2ur::piextVirtualMemGranularityGetInfo(context, device, param_name,
+                                                  param_value_size, param_value,
+                                                  param_value_size_ret);
+}
+
+pi_result piextPhysicalMemCreate(pi_context context, pi_device device,
+                                 size_t mem_size,
+                                 pi_physical_mem *ret_physical_mem) {
+  return pi2ur::piextPhysicalMemCreate(context, device, mem_size,
+                                       ret_physical_mem);
+}
+
+pi_result piextPhysicalMemRetain(pi_physical_mem physical_mem) {
+  return pi2ur::piextPhysicalMemRetain(physical_mem);
+}
+
+pi_result piextPhysicalMemRelease(pi_physical_mem physical_mem) {
+  return pi2ur::piextPhysicalMemRelease(physical_mem);
+}
+
+pi_result piextVirtualMemReserve(pi_context context, const void *start,
+                                 size_t range_size, void **ret_ptr) {
+  return pi2ur::piextVirtualMemReserve(context, start, range_size, ret_ptr);
+}
+
+pi_result piextVirtualMemFree(pi_context context, const void *ptr,
+                              size_t range_size) {
+  return pi2ur::piextVirtualMemFree(context, ptr, range_size);
+}
+
+pi_result piextVirtualMemMap(pi_context context, const void *ptr,
+                             size_t range_size, pi_physical_mem physical_mem,
+                             size_t offset, pi_virtual_access_flags flags) {
+  return pi2ur::piextVirtualMemMap(context, ptr, range_size, physical_mem,
+                                   offset, flags);
+}
+
+pi_result piextVirtualMemUnmap(pi_context context, const void *ptr,
+                               size_t range_size) {
+  return pi2ur::piextVirtualMemUnmap(context, ptr, range_size);
+}
+
+pi_result piextVirtualMemSetAccess(pi_context context, const void *ptr,
+                                   size_t range_size,
+                                   pi_virtual_access_flags flags) {
+  return pi2ur::piextVirtualMemSetAccess(context, ptr, range_size, flags);
+}
+
+pi_result piextVirtualMemGetInfo(pi_context context, const void *ptr,
+                                 size_t range_size,
+                                 pi_virtual_mem_info param_name,
+                                 size_t param_value_size, void *param_value,
+                                 size_t *param_value_size_ret) {
+  return pi2ur::piextVirtualMemGetInfo(context, ptr, range_size, param_name,
+                                       param_value_size, param_value,
+                                       param_value_size_ret);
+}
+
 // Initialize function table with stubs.
 #define _PI_API(api)                                                           \
   (PluginInit->PiFunctionTable).api = (decltype(&::api))(&api);
