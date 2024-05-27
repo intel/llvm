d02831948b15e38124fdfc903479bac48c8309e7
Lorenc Bushi <113361374+lbushi25@users.noreply.github.com>
2024-05-27 10:10:06 -0400

    Update sycl/test-e2e/USM/align.cpp
    Co-authored-by: ldrumm <ldrumm@rtps.co>

--- sycl/test-e2e/USM/align.cpp
+++ sycl/test-e2e/USM/align.cpp
@@ -48,7 +48,7 @@ template <typename T> void testAlign(sycl::queue &q, unsigned align) {
     if (dev.has(aspect::usm_shared_allocations)) {
       return aligned_alloc_shared<T>(align, N, args...);
     }
-    return (T *)nullptr;
+    return nullptr;
   };
   auto ATAnnotated = [&](size_t align, auto... args) {
     return aligned_alloc<T>(align, N, args...);
