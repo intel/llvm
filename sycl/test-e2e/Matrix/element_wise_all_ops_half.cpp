8b0e59debf7835de7ed26ae06419edd90a57f938
Artem Radzikhovskyy <artem.radzikhovskyy@intel.com>
2024-05-27 10:34:42 -0700

    CPU works element_wise_all_ops_half

--- sycl/test-e2e/Matrix/element_wise_all_ops_half.cpp
+++ sycl/test-e2e/Matrix/element_wise_all_ops_half.cpp
@@ -5,6 +5,7 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+// REQUIRES: matrix-fp16
 // REQUIRES: aspect-fp16
 // REQUIRES: aspect-ext_intel_matrix
 
