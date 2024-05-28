37e64b1290bfa33c2ded3baa594bedba42063a7a
Agarwal, Udit <udit.agarwal@intel.com>
2024-05-28 08:57:04 -0700

    Augment test case for testing discard filters

--- sycl/test-e2e/OneapiDeviceSelector/discard_filters.cpp
+++ sycl/test-e2e/OneapiDeviceSelector/discard_filters.cpp
@@ -1,6 +1,26 @@
-// REQUIRES: gpu
-// RUN: %clangxx -fsycl -fsycl-targets=%{sycl_triple} %S/Inputs/trivial.cpp -o %t.out
+// RUN: %{build} -o %t.out
 
 // Test discard filters in ONEAPI_DEVICE_SELECTOR.
-// RUN: env ONEAPI_DEVICE_SELECTOR="!*:gpu" %t.out | FileCheck %s --allow-empty
-// CHECK-NOT: {{.*}}Device:{{.*}}{{gpu|GPU|Gpu}}{{.*}}
+// RUN: env ONEAPI_DEVICE_SELECTOR="!*:gpu" %t.out | FileCheck %s --allow-empty --implicit-check-not="[{{.*}}{{gpu|GPU|Gpu}}{{.*}}]:[{{.*}}]"
+// RUN: env ONEAPI_DEVICE_SELECTOR="!*:cpu" %t.out | FileCheck %s --allow-empty --implicit-check-not="[{{.*}}{{cpu|CPU|cpu}}{{.*}}]:[{{.*}}]"
+// RUN: env ONEAPI_DEVICE_SELECTOR="!*:cpu,gpu" %t.out | FileCheck %s --allow-empty --implicit-check-not="[{{.*}}{{cpu|CPU|cpu|gpu|GPU|Gpu}}{{.*}}]:[{{.*}}]"
+
+// RUN: env ONEAPI_DEVICE_SELECTOR="!opencl:*" %t.out | FileCheck %s --allow-empty --implicit-check-not="[{{.*}}]:[{{.*}}{{OpenCL|opencl|Opencl}}{{.*}}]"
+// RUN: env ONEAPI_DEVICE_SELECTOR="!level_zero:*" %t.out | FileCheck %s --allow-empty --implicit-check-not="[{{.*}}]:[{{.*}}{{Level-Zero}}{{.*}}]"
+
+// RUN: env ONEAPI_DEVICE_SELECTOR="!level_zero:cpu" %t.out | FileCheck %s --allow-empty --implicit-check-not="[{{.*}}{{cpu|CPU|Cpu}}{{.*}}]:[{{.*}}{{Level-Zero}}{{.*}}]"
+
+#include <iostream>
+#include <sycl/detail/core.hpp>
+using namespace sycl;
+
+int main() {
+  for (auto &d : device::get_devices()) {
+    // Get device name and backend name.
+    std::string device_name = d.get_info<info::device::name>();
+    std::string be_name = d.get_platform().get_info<info::platform::name>();
+
+    std::cout << "[" << device_name << "]:[" << be_name << "]" << std::endl;
+  }
+  return 0;
+}
