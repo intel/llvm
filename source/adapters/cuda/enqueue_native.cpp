799303e7b5d0e7a1f089e2d1431fe183d2034e3a
Hugh Delaney <hugh.delaney@codeplay.com>
2024-05-28 12:31:32 +0100

    Add basic CUDA impl using ScopedStream
    Use ScopedStream to return the same stream during the lifetime of the
    RAII object. This allows us to create events outside a user submitted
    func, and submit work within the user submitted func, since the stream
    given to the user from urQueueGetNativeHandle is guaranteed to be the
    same stream that we record events on.

--- source/adapters/cuda/enqueue_native.cpp
+++ source/adapters/cuda/enqueue_native.cpp
@@ -1,4 +1,4 @@
-//===--------- native_enqueue.cpp - CUDA Adapter --------------------------===//
+//===--------- enqueue_native.cpp - CUDA Adapter --------------------------===//
 //
 // Copyright (C) 2024 Intel Corporation
 //
@@ -10,9 +10,44 @@
 
 #include <ur_api.h>
 
-ur_result_t urNativeEnqueueExp(ur_queue_handle_t,
-                               ur_exp_enqueue_native_command_function_t, void *,
-                               uint32_t, const ur_event_handle_t *,
-                               ur_event_handle_t *) {
-  return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
+#include "context.hpp"
+#include "event.hpp"
+#include "queue.hpp"
+
+UR_APIEXPORT ur_result_t UR_APICALL urEnqueueNativeCommandExp(
+    ur_queue_handle_t hQueue,
+    ur_exp_enqueue_native_command_function_t pfnNativeEnqueue, void *data,
+    const ur_exp_enqueue_native_command_properties_t *,
+    uint32_t NumEventsInWaitList, const ur_event_handle_t *phEventWaitList,
+    ur_event_handle_t *phEvent) {
+  // TODO: how should mem migration work across a context here?
+  // Perhaps we will need to add a phMemObjArgs so that we are able to make
+  // sure memory migration happens across devices in the same context
+
+  try {
+    ScopedContext ActiveContext(hQueue->getDevice());
+    ScopedStream ActiveStream(hQueue, NumEventsInWaitList, phEventWaitList);
+    std::unique_ptr<ur_event_handle_t_> RetImplEvent{nullptr};
+
+    if (phEvent) {
+      RetImplEvent =
+          std::unique_ptr<ur_event_handle_t_>(ur_event_handle_t_::makeNative(
+              UR_COMMAND_ENQUEUE_NATIVE_EXP, hQueue, ActiveStream.getStream()));
+      UR_CHECK_ERROR(RetImplEvent->start());
+    }
+
+    pfnNativeEnqueue(hQueue, data); // This is using urQueueGetNativeHandle to
+                                    // get the CUDA stream. It must be the
+                                    // same stream as is used before and after
+    if (phEvent) {
+      UR_CHECK_ERROR(RetImplEvent->record());
+      *phEvent = RetImplEvent.release();
+    }
+
+  } catch (ur_result_t Err) {
+    return Err;
+  } catch (CUresult CuErr) {
+    return mapErrorUR(CuErr);
+  }
+  return UR_RESULT_SUCCESS;
 }
