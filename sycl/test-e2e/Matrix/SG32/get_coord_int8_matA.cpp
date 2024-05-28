f79c3c2b9c552f5726e24b54b6e61898181259c0
Artem Radzikhovskyy <artem.radzikhovskyy@intel.com>
2024-05-28 14:42:56 -0700

    get_coord_int8_matA

--- sycl/test-e2e/Matrix/SG32/get_coord_int8_matA.cpp
+++ sycl/test-e2e/Matrix/SG32/get_coord_int8_matA.cpp
@@ -5,19 +5,11 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-// REQUIRES: matrix
+// REQUIRES: aspect-ext_intel_matrix
 // REQUIRES-INTEL-DRIVER: lin: 27501, win: 101.4943
 
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 
 #include "../common.hpp"
-#include <iostream>
-
-using namespace sycl;
-using namespace sycl::ext::oneapi::experimental::matrix;
-
-#define SG_SZ 32
-constexpr size_t TN = 16;
-
 #include "../get_coord_int8_matA_impl.hpp"
