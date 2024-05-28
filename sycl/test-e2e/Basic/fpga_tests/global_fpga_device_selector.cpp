e5083cd021debf4fe6f2f30adbdf3701179b6e73
Garcia Orozco, David <david.garcia.orozco@intel.com>
2024-05-28 12:57:38 -0700

    clang-format global fpga selector test

--- sycl/test-e2e/Basic/fpga_tests/global_fpga_device_selector.cpp
+++ sycl/test-e2e/Basic/fpga_tests/global_fpga_device_selector.cpp
@@ -9,6 +9,7 @@
 // Check that FPGA emulator device is found if we try to initialize inline
 // global variable using fpga_emulator_selector parameter.
 
-inline sycl::queue fpga_emu_queue_inlined{sycl::ext::intel::fpga_emulator_selector_v };
+inline sycl::queue fpga_emu_queue_inlined{
+    sycl::ext::intel::fpga_emulator_selector_v};
 
 int main() { return 0; }
