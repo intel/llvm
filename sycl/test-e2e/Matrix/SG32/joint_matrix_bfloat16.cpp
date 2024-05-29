b0da58443efa71cd4d7231ff083034a06dc2a89e
Artem Radzikhovskyy <artem.radzikhovskyy@intel.com>
2024-05-29 09:47:05 -0700

    joint_matrix_apply_bf16, joint_matrix_bfloat16

--- sycl/test-e2e/Matrix/SG32/joint_matrix_bfloat16.cpp
+++ sycl/test-e2e/Matrix/SG32/joint_matrix_bfloat16.cpp
@@ -5,22 +5,14 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-// REQUIRES: matrix
+// REQUIRES: aspect-ext_intel_matrix
 // REQUIRES-INTEL-DRIVER: lin: 27501, win: 101.4943
 
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 
 #include "../common.hpp"
-#include <iostream>
-#include <sycl/detail/core.hpp>
-#include <sycl/ext/oneapi/matrix/matrix.hpp>
-
-using namespace sycl;
-using namespace sycl::ext::oneapi::experimental::matrix;
-using bfloat16 = sycl::ext::oneapi::bfloat16;
 
 #define SG_SZ 32
-constexpr size_t TN = 16;
 
 #include "../joint_matrix_bfloat16_impl.hpp"
