64bc25e290b9ea338e324824b220c80e7c6e03db
Arvind Sudarsanam <arvind.sudarsanam@intel.com>
2024-05-28 19:55:09 -0700

    Temporarily disable E2E test
    Signed-off-by: Arvind Sudarsanam <arvind.sudarsanam@intel.com>

--- sycl/test-e2e/OptionalKernelFeatures/fp64-conv-emu.cpp
+++ sycl/test-e2e/OptionalKernelFeatures/fp64-conv-emu.cpp
@@ -1,19 +1,14 @@
-// REQUIRES: ocloc
-// RUN: %clangxx -fsycl -fsycl-targets=spir64_gen -fsycl-fp64-conv-emu -O0 %s -o %t_opt.out
-// RUN: %{run} %t_opt.out
+// REQUIRES: ocloc, gpu
+// UNSUPPORTED: cuda, hip
 
-// Tests that aspect::fp64 is emitted correctly when -fsycl-fp64-conv-emu flag
+// RUN: %clangxx -fsycl -fsycl-targets=spir64_gen  -Xsycl-target-backend "-device *" -fsycl-fp64-conv-emu -O0 %s -o %t_opt.out
+// To be enabled soon.
+// RUNx: %{run} %t_opt.out
+
+// Tests that aspect::fp64 is not emitted correctly when -fsycl-fp64-conv-emu flag
 // is used.
 
 #include <sycl/detail/core.hpp>
-
-template <typename t, typename Func>
-__attribute__((sycl_kernel)) void kernel_compute(const Func &func) {
-  double a[3] = {1, 2, 3};
-  double b[3] = {1, 2, 3};
-  int i = 1;
-  func(a, b, i);
-}
 
 template <typename t, typename Func>
 __attribute__((sycl_kernel)) void kernel_convert(const Func &func) {
@@ -21,7 +16,6 @@ __attribute__((sycl_kernel)) void kernel_convert(const Func &func) {
   double b[3] = {1, 2, 3};
   int i = 1;
   func(a, b, i);
-  ;
 }
 
 extern "C" {
@@ -31,8 +25,6 @@ void __sycl_unregister_lib(void *) {}
 }
 
 int main() {
-  kernel_compute<class fake_kernel_compute>(
-      [](double *a, double *b, int i) { b[i] = a[i] + 1.0; });
   kernel_convert<class fake_kernel_convert>(
       [](double *a, double *b, int i) { b[i] = (double)((float)(a[i])); });
   return 0;
