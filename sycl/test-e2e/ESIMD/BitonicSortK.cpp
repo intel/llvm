7a29f962ba84e98bc15ceb9830c60fc2d217fa24
jason1.li <jasonli@ubuntu-server.jf.intel.com>
2024-05-29 10:41:38 -0700

    [SYCL][ESIMD] Added instruction count test to BitonicSortK.cpp

--- sycl/test-e2e/ESIMD/BitonicSortK.cpp
+++ sycl/test-e2e/ESIMD/BitonicSortK.cpp
@@ -8,4 +8,10 @@
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 
+
+// RUN: mkdir -p %t.dir && %{build} -o %t.dir/exec.out
+// RUN: env IGC_DumpToCustomDir=%t.dir IGC_ShaderDumpEnable=1 %{run} %t.dir/exec.out
+// RUN: python3 %S/instruction_count.py %t.dir 2108 VC_asmfc04983569d0d4c9__ZTSZZN11BitonicSort5SolveEPjS0_jENKUlRN4sycl3_V17handlerEE0_clES4_E5Merge.asm
+// RUN: rm -rf %t.dir
+
 #include "BitonicSortK.hpp"
