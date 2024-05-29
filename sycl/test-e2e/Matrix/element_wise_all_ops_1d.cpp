dec11466876481d0140b75ac37ccaabbe68cf324
Yury Plyakhin <yury.plyakhin@intel.com>
2024-05-29 07:29:50 -0700

    [SYCL][Joint Matrix][E2E] Add few load/store tests for different JointMatrixLoadStoreOpt flag setting (#13873)
    This PR adds the following tests:
    - element_wise_all_ops verification for
    IGC_JointMatrixLoadStoreOpt=2,1,0. It is split to 3 different tests,
    since test runtime became too big, so splitting allows to run tests
    faster in several threads
    - big combinations, B row-major and annotated_ptr tests with different
    flag setting (IGC_JointMatrixLoadStoreOpt=0,1) in accordance with the
    loads/stores implemented in IGC for those flag settings.

--- sycl/test-e2e/Matrix/element_wise_all_ops_1d.cpp
+++ sycl/test-e2e/Matrix/element_wise_all_ops_1d.cpp
@@ -1,19 +1,14 @@
-//==-------- joint_matrix_bf16_vnni.cpp  - DPC++ joint_matrix---------------==//
+//==-------- element_wise_all_ops_1d.cpp - DPC++ joint_matrix ---------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-// REQUIRES: matrix
+// REQUIRES: aspect-ext_intel_matrix, gpu
 
 // RUN: %{build} -o %t.out
-// RUN: %{run} %t.out
-// RUN: %if gpu %{ env IGC_JointMatrixLoadStoreOpt=0 %{run} %t.out %}
-// RUN: %if gpu %{ env IGC_JointMatrixLoadStoreOpt=1 %{run} %t.out %}
+// RUN: env IGC_JointMatrixLoadStoreOpt=1 %{run} %t.out
 
 #include "common.hpp"
-
-constexpr size_t TN = 16;
-
-#include "joint_matrix_int8_vnni_impl.hpp"
+#include "element_wise_all_ops_impl.hpp"
