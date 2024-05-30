a1e1e73d5fee32db1a164e0df2037c1da992d0e4
jason1.li <jasonli@ubuntu-server.jf.intel.com>
2024-05-30 08:35:41 -0700

    moved instruction count tests to their own folder

--- sycl/test-e2e/ESIMD/BitonicSortK.cpp
+++ sycl/test-e2e/ESIMD/BitonicSortK.cpp
@@ -8,11 +8,4 @@
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 
-// DEFINE: %{instcount} = %if gpu-intel-dg2 %{ 3452 } %else %{ %if gpu-intel-gen12 %{ 4745 %} %}
-// RUN: echo %{instcount}
-// RUN: mkdir -p %t.dir && %{build} -o %t.dir/exec.out
-// RUN: env IGC_DumpToCustomDir=%t.dir IGC_ShaderDumpEnable=1 %{run} %t.dir/exec.out
-// RUN: python3 %S/instruction_count.py %t.dir %{instcount} VC_asmfc04983569d0d4c9__ZTSZZN11BitonicSort5SolveEPjS0_jENKUlRN4sycl3_V17handlerEE0_clES4_E5Merge.asm
-// RUN: rm -rf %t.dir
-
 #include "BitonicSortK.hpp"
