46571e36d503e46b5d3bb58637e0ea0c6ed42a7f
Steffen Larsen <steffen.larsen@intel.com>
2024-05-31 08:29:25 +0200

    [SYCL][E2E] Disable profiling_queue.hpp for FPGA (#13971)
    Due to a problem in the reported timings of the fallback implementation
    on the FPGA emulator, the profiling_queue.hpp test fails. This disables
    the test for those devices.
    
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- sycl/test-e2e/ProfilingTag/profiling_queue.cpp
+++ sycl/test-e2e/ProfilingTag/profiling_queue.cpp
@@ -16,6 +16,9 @@
 // https://github.com/intel/llvm/issues/12904
 // UNSUPPORTED: hip
 
+// FPGA emulator seems to return unexpected start time for the fallback barrier.
+// UNSUPPORTED: accelerator
+
 #include "common.hpp"
 
 int main() {
