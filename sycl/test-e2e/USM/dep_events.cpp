db73a88bf731a3ed3d176271a2d930ebe37d1e55
M Moadeli <mahmoud.moadeli@codeplay.com>
2024-06-03 11:06:34 +0100

    Disallow dep_events.cpp test built for CUDA backend on Windows.

--- sycl/test-e2e/USM/dep_events.cpp
+++ sycl/test-e2e/USM/dep_events.cpp
@@ -6,8 +6,9 @@
 //
 //===----------------------------------------------------------------------===//
 // REQUIRES: aspect-usm_shared_allocations
+// UNSUPPORTED: cuda && windows
 // RUN: %{build} -o %t1.out
-// RUN: %if !(windows && cuda) %{ %{run} %t1.out %}
+// RUN: %{run} %t1.out
 
 #include <iostream>
 #include <sycl/detail/core.hpp>
