1b69c052b9375bcf122b8f1005fbdcf03d00c865
jason1.li <jasonli@ubuntu-server.jf.intel.com>
2024-06-05 09:24:24 -0700

    fix div by 0 error

--- sycl/test-e2e/ESIMD/PerformanceTests/BitonicSortKv2.cpp
+++ sycl/test-e2e/ESIMD/PerformanceTests/BitonicSortKv2.cpp
@@ -9,7 +9,7 @@
 
 // RUN: mkdir -p %t.dir && %{build} -o %t.dir/exec.out
 // RUN: env IGC_DumpToCustomDir=%t.dir IGC_ShaderDumpEnable=1 %{run} %t.dir/exec.out
-// RUN: python3 %S/instruction_count.py %t.dir 0 ZTSZZN11BitonicSort5SolveEPjS0_jENKUlRN4sycl3_V17handlerEE0_clES4_E5Merge.asm
+// RUN: python3 %S/instruction_count.py %t.dir 1 ZTSZZN11BitonicSort5SolveEPjS0_jENKUlRN4sycl3_V17handlerEE0_clES4_E5Merge.asm
 // RUN: echo "Baseline from driver version 1.3.29138"
 
 #include "../BitonicSortKv2.cpp"
