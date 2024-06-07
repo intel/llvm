62b6040739785618127f6b7ab23e2aab3899c6bd
JackAKirk <jack.kirk@codeplay.com>
2024-06-07 05:38:12 -0700

    Add duplicate advise on command device.
    Covers case where command device outside the ur Context.
    
    Signed-off-by: JackAKirk <jack.kirk@codeplay.com>

--- source/adapters/cuda/usm.cpp
+++ source/adapters/cuda/usm.cpp
@@ -151,18 +151,22 @@ ur_result_t USMDeviceAllocImpl(void **ResultPtr, ur_context_handle_t,
 }
 
 ur_result_t USMSharedAllocImpl(void **ResultPtr, ur_context_handle_t Context,
-                               ur_device_handle_t Device,
+                               ur_device_handle_t CommandDevice,
                                ur_usm_host_mem_flags_t,
                                ur_usm_device_mem_flags_t, size_t Size,
                                uint32_t Alignment) {
   try {
-    ScopedContext Active(Device);
+    ScopedContext Active(CommandDevice);
     UR_CHECK_ERROR(cuMemAllocManaged((CUdeviceptr *)ResultPtr, Size,
                                      CU_MEM_ATTACH_GLOBAL));
-    for (const auto &device : Context->getDevices()) {
-      if (getAttribute(device, CU_DEVICE_ATTRIBUTE_CONCURRENT_MANAGED_ACCESS)) {
+    if (getAttribute(CommandDevice, CU_DEVICE_ATTRIBUTE_CONCURRENT_MANAGED_ACCESS)) {
         UR_CHECK_ERROR(cuMemAdvise((CUdeviceptr)*ResultPtr, Size,
-                                   CU_MEM_ADVISE_SET_ACCESSED_BY, device->get()));
+                                   CU_MEM_ADVISE_SET_ACCESSED_BY, CommandDevice->get()));
+      }
+    for (const auto &Dev : Context->getDevices()) {
+      if (getAttribute(Dev, CU_DEVICE_ATTRIBUTE_CONCURRENT_MANAGED_ACCESS)) {
+        UR_CHECK_ERROR(cuMemAdvise((CUdeviceptr)*ResultPtr, Size,
+                                   CU_MEM_ADVISE_SET_ACCESSED_BY, Dev->get()));
       }
     }
   } catch (ur_result_t Err) {
