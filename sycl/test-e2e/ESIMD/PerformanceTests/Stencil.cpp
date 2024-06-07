6d591f19dc2ffb1321f790f46776280032b3c9e6
Jason Li <jasonlizhengjian@gmail.com>
2024-06-07 10:22:37 -0400

    [SYCL][ESIMD] Instruction count performance test  (#14033)
    (fixed issues with post commit testing)
    
    ---------
    
    Co-authored-by: jason1.li <jasonli@ubuntu-server.jf.intel.com>

--- sycl/test-e2e/ESIMD/PerformanceTests/Stencil.cpp
+++ sycl/test-e2e/ESIMD/PerformanceTests/Stencil.cpp
@@ -1,4 +1,4 @@
-//==---------------- BitonicSortK.cpp  - DPC++ ESIMD on-device test --------==//
+//==---------------- Stencil.cpp  - DPC++ ESIMD on-device test ------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -9,7 +9,7 @@
 
 // RUN: mkdir -p %t.dir && %{build} -o %t.dir/exec.out
 // RUN: env IGC_DumpToCustomDir=%t.dir IGC_ShaderDumpEnable=1 %{run} %t.dir/exec.out
-// RUN: python3 %S/instruction_count.py %t.dir 3452 ZTSZZN11BitonicSort5SolveEPjS0_jENKUlRN4sycl3_V17handlerEE0_clES4_E5Merge.asm
+// RUN: python3 %S/instruction_count.py %t.dir 1699 ZTSZZ4mainENKUlRN4sycl3_V17handlerEE_clES2_E14Stencil_kernel.asm
 // RUN: echo "Baseline from driver version 1.3.29138"
 
-#include "../BitonicSortK.cpp"
+#include "../Stencil.cpp"
