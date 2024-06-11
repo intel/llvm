4055c1fc9688665af03715e8d2a0d1f758426b0a
Hugh Delaney <hugh.delaney@codeplay.com>
2024-06-11 10:08:52 +0100

    Remove some multi dev ctx shenanigans
    As is done in #1711, we don't need to wait on events that are not given
    directly to UR by the user through the phEventWaitList param.

--- source/adapters/cuda/enqueue_native.cpp
+++ source/adapters/cuda/enqueue_native.cpp
@@ -23,67 +23,34 @@ UR_APIEXPORT ur_result_t UR_APICALL urEnqueueNativeCommandExp(
     uint32_t NumEventsInWaitList, const ur_event_handle_t *phEventWaitList,
     ur_event_handle_t *phEvent) {
 
-  std::vector<ur_event_handle_t> MemMigrationEvents;
-  std::vector<std::pair<ur_mem_handle_t, ur_lock>> MemMigrationLocks;
-
-  // phEventWaitList only contains events that are handed to UR by the SYCL
-  // runtime. However since UR handles memory dependencies within a context
-  // we may need to add more events to our dependent events list if the UR
-  // context contains multiple devices
-  if (NumMemsInMemList > 0 && hQueue->getContext()->Devices.size() > 1) {
-    for (auto i = 0u; i < NumMemsInMemList; ++i) {
-      auto Mem = phMemList[i];
-      if (auto MemDepEvent = Mem->LastEventWritingToMemObj;
-          MemDepEvent &&
-          std::find(MemMigrationEvents.begin(), MemMigrationEvents.end(),
-                    MemDepEvent) == MemMigrationEvents.end()) {
-        MemMigrationEvents.push_back(MemDepEvent);
-        MemMigrationLocks.emplace_back(
-            std::pair{Mem, ur_lock{Mem->MemoryMigrationMutex}});
-      }
-    }
-  }
-
   try {
     ScopedContext ActiveContext(hQueue->getDevice());
     ScopedStream ActiveStream(hQueue, NumEventsInWaitList, phEventWaitList);
     std::unique_ptr<ur_event_handle_t_> RetImplEvent{nullptr};
 
-    if (phEvent || MemMigrationEvents.size()) {
+    if (phEvent) {
       RetImplEvent =
           std::unique_ptr<ur_event_handle_t_>(ur_event_handle_t_::makeNative(
               UR_COMMAND_ENQUEUE_NATIVE_EXP, hQueue, ActiveStream.getStream()));
       UR_CHECK_ERROR(RetImplEvent->start());
     }
 
-    if (MemMigrationEvents.size()) {
-      UR_CHECK_ERROR(
-          urEnqueueEventsWaitWithBarrier(hQueue, MemMigrationEvents.size(),
-                                         MemMigrationEvents.data(), nullptr));
+    if (hQueue->getContext()->getDevices().size() > 1) {
       for (auto i = 0u; i < NumMemsInMemList; ++i) {
-        auto Mem = phMemList[i];
-        migrateMemoryToDeviceIfNeeded(Mem, hQueue->getDevice());
-        Mem->setLastEventWritingToMemObj(RetImplEvent.get());
+        // FIXME: Update to enqueueMigrateMemory and also using
+        // setLastQueueWritingToMemObj when #1711 has merged
+        migrateMemoryToDeviceIfNeeded(phMemList[i], hQueue->getDevice());
+        phMemList[i]->setLastEventWritingToMemObj(RetImplEvent.get());
       }
-      MemMigrationLocks.clear();
     }
 
     pfnNativeEnqueue(hQueue, data); // This is using urQueueGetNativeHandle to
                                     // get the CUDA stream. It must be the
                                     // same stream as is used before and after
 
-    if (phEvent || MemMigrationEvents.size()) {
+    if (phEvent) {
       UR_CHECK_ERROR(RetImplEvent->record());
-      if (phEvent) {
-        *phEvent = RetImplEvent.release();
-      } else {
-        // Give ownership of the event to the mem
-        for (auto i = 0u; i < NumMemsInMemList; ++i) {
-          auto Mem = phMemList[i];
-          migrateMemoryToDeviceIfNeeded(Mem, hQueue->getDevice());
-          Mem->setLastEventWritingToMemObj(RetImplEvent.release());
-        }
-      }
+      *phEvent = RetImplEvent.release();
     }
   } catch (ur_result_t Err) {
     return Err;
