1aa0304beeb2895138d19a5c22931dadd19cd7cd
Agarwal, Udit <udit.agarwal@intel.com>
2024-06-14 14:24:43 -0700

    Re-enable e2e BF16 vec test for older vec implementation.

--- sycl/test-e2e/BFloat16/bfloat16_vec.cpp
+++ sycl/test-e2e/BFloat16/bfloat16_vec.cpp
@@ -10,8 +10,8 @@
 // TODO enable opaque pointers support on CPU.
 // UNSUPPORTED: cpu || accelerator
 
-// UN: %{build} -o %t.out
-// UN: %{run} %t.out
+// RUN: %{build} -o %t.out
+// RUN: %{run} %t.out
 // RUN: %if preview-breaking-changes-supported %{  %{build} -fpreview-breaking-changes -o %t2.out   %}
 // RUN: %if preview-breaking-changes-supported %{ %{run} %t2.out  %}
 
