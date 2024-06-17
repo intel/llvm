d574d16a09e33e9c8c4b00351736cb00205c166e
Hugh Delaney <hugh.delaney@codeplay.com>
2024-06-17 10:55:50 +0100

    Don't use DIE_NO_IMPLEMENTATION

--- source/adapters/native_cpu/enqueue.cpp
+++ source/adapters/native_cpu/enqueue.cpp
@@ -716,5 +716,5 @@ UR_APIEXPORT ur_result_t UR_APICALL urEnqueueNativeCommandExp(
     uint32_t, const ur_mem_handle_t *,
     const ur_exp_enqueue_native_command_properties_t *, uint32_t,
     const ur_event_handle_t *, ur_event_handle_t *) {
-  DIE_NO_IMPLEMENTATION;
+  return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
 }
