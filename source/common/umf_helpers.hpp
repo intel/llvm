73df8f939d8a8aaef69f2f5e53695101fbf2eb11
Krzysztof Swiecicki <krzysztof.swiecicki@intel.com>
2024-06-21 10:56:13 +0000

    [UR][L0][CUDA][HIP] Update the copyright dates

--- source/common/umf_helpers.hpp
+++ source/common/umf_helpers.hpp
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (C) 2023 Intel Corporation
+ * Copyright (C) 2023-2024 Intel Corporation
  *
  * Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM Exceptions.
  * See LICENSE.TXT
