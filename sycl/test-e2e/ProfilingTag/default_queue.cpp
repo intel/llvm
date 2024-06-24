b9908a6e3616a4629337a762ba24363ccaba3029
Larsen, Steffen <steffen.larsen@intel.com>
2024-06-24 08:44:19 -0700

    [SYCL][E2E] Reenable profiling tag tests on HIP
    https://github.com/oneapi-src/unified-runtime/pull/1634 is believed to
    have fixed the issues for HIP in the profiling tag extension. This
    commit reenables the tests for HIP.
    
    Closes https://github.com/intel/llvm/issues/12904.
    
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- sycl/test-e2e/ProfilingTag/default_queue.cpp
+++ sycl/test-e2e/ProfilingTag/default_queue.cpp
@@ -4,10 +4,6 @@
 
 // Tests the get_profiling_tag extension function on an out-of-order queue.
 
-// HIP backend currently returns invalid values for submission time queries.
-// https://github.com/intel/llvm/issues/12904
-// UNSUPPORTED: hip
-
 // CUDA backend seems to fail sporadically for expected profiling tag time
 // query orderings.
 // https://github.com/intel/llvm/issues/14053
