3661d3ba940071f8cb58d9549289ab420cb2177c
Lorenc Bushi <113361374+lbushi25@users.noreply.github.com>
2024-06-26 16:15:55 -0400

    Update helpers.hpp

--- sycl/test-e2e/Basic/built-ins/helpers.hpp
+++ sycl/test-e2e/Basic/built-ins/helpers.hpp
@@ -50,7 +50,8 @@ template <typename FuncTy, typename ExpectedTy, typename... ArgTys>
 void test(FuncTy F, ExpectedTy Expected, ArgTys... Args) {
   test(true /*CheckDevice*/, 0.0 /*delta*/, F, Expected, Args...);
 }
-template <typename FuncTy, typename ExpectedTy, typename... ArgTys>
+template <typename FuncTy, typename ExpectedTy, 
+          typename... ArgTys>
 void test(bool CheckDevice, FuncTy F, ExpectedTy Expected, ArgTys... Args) {
   test(CheckDevice, 0.0 /*delta*/, F, Expected, Args...);
 }
