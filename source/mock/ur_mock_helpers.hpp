53acffb7305cd6677c1749d19121459cf5e5dcf5
Aaron Greig <aaron.greig@codeplay.com>
2024-06-26 16:45:53 +0100

    Add storage stuff back to dummy handles.

--- source/mock/ur_mock_helpers.hpp
+++ source/mock/ur_mock_helpers.hpp
@@ -13,24 +13,31 @@
 #pragma once
 
 #include "ur_api.h"
+
 #include <atomic>
 #include <cstddef>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 namespace mock {
 
 struct dummy_handle_t_ {
-    dummy_handle_t_() {}
-    std::atomic<size_t> refCounter = 1;
+    dummy_handle_t_(size_t DataSize = 0)
+        : MStorage(DataSize), MData(MStorage.data()) {}
+    dummy_handle_t_(unsigned char *Data) : MData(Data) {}
+    std::atomic<size_t> MRefCounter = 1;
+    std::vector<unsigned char> MStorage;
+    unsigned char *MData = nullptr;
 };
 
 using dummy_handle_t = dummy_handle_t_ *;
 
-// Allocates a dummy handle of type T with support for reference counting.
-// The handle has to be deallocated using 'releaseDummyHandle'.
-template <class T> inline T createDummyHandle() {
-    dummy_handle_t DummyHandlePtr = new dummy_handle_t_();
+// Allocates a dummy handle of type T with support of reference counting.
+// Takes optional 'Size' parameter which can be used to allocate additional
+// memory. The handle has to be deallocated using 'releaseDummyHandle'.
+template <class T> inline T createDummyHandle(size_t Size = 0) {
+    dummy_handle_t DummyHandlePtr = new dummy_handle_t_(Size);
     return reinterpret_cast<T>(DummyHandlePtr);
 }
 
@@ -38,7 +45,7 @@ template <class T> inline T createDummyHandle() {
 // reference counter becomes zero
 template <class T> inline void releaseDummyHandle(T Handle) {
     auto DummyHandlePtr = reinterpret_cast<dummy_handle_t>(Handle);
-    const size_t NewValue = --DummyHandlePtr->refCounter;
+    const size_t NewValue = --DummyHandlePtr->MRefCounter;
     if (NewValue == 0) {
         delete DummyHandlePtr;
     }
@@ -47,7 +54,7 @@ template <class T> inline void releaseDummyHandle(T Handle) {
 // Increment reference counter for the handle
 template <class T> inline void retainDummyHandle(T Handle) {
     auto DummyHandlePtr = reinterpret_cast<dummy_handle_t>(Handle);
-    ++DummyHandlePtr->refCounter;
+    ++DummyHandlePtr->MRefCounter;
 }
 
 struct callbacks_t {
