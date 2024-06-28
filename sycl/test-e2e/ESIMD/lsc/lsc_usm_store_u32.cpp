00c1b44d8f54d15dce1709379b024d6a9edeb51e
Gregory Fine <gregory.fine@intel.com>
2024-06-28 12:25:09 -0400

    Address PR comments

--- sycl/test-e2e/ESIMD/lsc/lsc_usm_store_u32.cpp
+++ sycl/test-e2e/ESIMD/lsc/lsc_usm_store_u32.cpp
@@ -10,6 +10,13 @@
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 
+// TODO: Windows compiler causes this test to fail due to handling of floating
+// point arithmetics. Fixing requires using -Qfp-speculation=safe option to
+// disable some floating point optimizations to produce corect resut. icx
+// compiler supports that option while clang-cl does not which causes test to
+// fail when running as part of a test suite. Therefore the test is disabled for
+// windows until equivalent option is found.
+
 #include "Inputs/lsc_usm_store.hpp"
 
 constexpr uint32_t seed = 299;
