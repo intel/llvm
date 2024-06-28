686cf44d8d648c5c6ee33084fe683eac0364a879
Artur Gainullin <artur.gainullin@intel.com>
2024-06-28 14:02:02 -0700

    [UR][L0] Fix undefined behaviour caused by shifting more than bits count

--- source/adapters/level_zero/queue.cpp
+++ source/adapters/level_zero/queue.cpp
@@ -1550,8 +1550,7 @@ ur_result_t ur_queue_handle_legacy_t_::active_barriers::clear() {
 
 void ur_queue_handle_legacy_t_::clearEndTimeRecordings() {
   uint64_t ZeTimerResolution = Device->ZeDeviceProperties->timerResolution;
-  const uint64_t TimestampMaxValue =
-      ((1ULL << Device->ZeDeviceProperties->kernelTimestampValidBits) - 1ULL);
+  const uint64_t TimestampMaxValue = Device->getTimestampMask();
 
   for (auto Entry : EndTimeRecordings) {
     auto &Event = Entry.first;
