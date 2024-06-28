891fc0d387ea11c90f98ca47ff838d11db812553
Lorenc Bushi <113361374+lbushi25@users.noreply.github.com>
2024-06-27 22:43:08 -0400

    Update helpers.hpp

--- sycl/test-e2e/Basic/built-ins/helpers.hpp
+++ sycl/test-e2e/Basic/built-ins/helpers.hpp
@@ -43,13 +43,12 @@ void test(bool CheckDevice, double delta, FuncTy F, ExpectedTy Expected,
       cgh.single_task([=]() {
         auto R = F(Args...);
         static_assert(std::is_same_v<decltype(Expected), decltype(R)>);
-        Success[0] = equal(
-            R, Expected,
-            delta); // use double precision error tolerance when fp64 supported
+        // use double precision error tolerance when fp64 supported
+        Success[0] = equal(R, Expected,delta); 
       });
     } else {
-      sycl::detail::get_elem_type_t<ExpectedTy> d(
-          delta); // downscale the error tolerance when fp64 is not supported
+      // downscale the error tolerance when fp64 is not supported
+      sycl::detail::get_elem_type_t<ExpectedTy> d(delta);
       cgh.single_task([=]() {
         auto R = F(Args...);
         static_assert(std::is_same_v<decltype(Expected), decltype(R)>);
