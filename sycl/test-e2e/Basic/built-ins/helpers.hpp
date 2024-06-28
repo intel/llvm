070dec62068d5de153970d09d78948df8d51fc41
Lorenc Bushi <113361374+lbushi25@users.noreply.github.com>
2024-06-27 22:41:50 -0400

    Update helpers.hpp

--- sycl/test-e2e/Basic/built-ins/helpers.hpp
+++ sycl/test-e2e/Basic/built-ins/helpers.hpp
@@ -60,12 +60,12 @@ void test(bool CheckDevice, double delta, FuncTy F, ExpectedTy Expected,
   assert(sycl::host_accessor{SuccessBuf}[0]);
 }
 
-template <typename FuncTy, typename ExpectedTy,
-          typename... ArgTys>
+template <typename FuncTy, typename ExpectedTy, typename... ArgTys>
 void test(FuncTy F, ExpectedTy Expected, ArgTys... Args) {
   test(true /*CheckDevice*/, 0.0 /*delta*/, F, Expected, Args...);
 }
-template <typename FuncTy, typename ExpectedTy, typename... ArgTys>
+template <typename FuncTy, typename ExpectedTy, 
+          typename... ArgTys>
 void test(bool CheckDevice, FuncTy F, ExpectedTy Expected, ArgTys... Args) {
   test(CheckDevice, 0.0 /*delta*/, F, Expected, Args...);
 }
