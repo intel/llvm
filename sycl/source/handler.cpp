47fc2749f74d5bb57da80bebd45876bde07ce2be
Larsen, Steffen <steffen.larsen@intel.com>
2024-06-28 01:48:26 -0700

    Revert "Remove unused APIs"
    This reverts commit b070d526a3f584be9d4bb77de72ede09106979d9.

--- sycl/source/handler.cpp
+++ sycl/source/handler.cpp
@@ -80,6 +80,18 @@ void *getValueFromDynamicParameter(
 
 } // namespace detail
 
+/// TODO: Unused. Remove with ABI break.
+handler::handler(std::shared_ptr<detail::queue_impl> Queue, bool IsHost)
+    : handler(Queue, IsHost, /*CallerNeedsEvent=*/true) {}
+
+/// TODO: Unused. Remove with ABI break.
+handler::handler(std::shared_ptr<detail::queue_impl> Queue,
+                 std::shared_ptr<detail::queue_impl> PrimaryQueue,
+                 std::shared_ptr<detail::queue_impl> SecondaryQueue,
+                 bool IsHost)
+    : handler(Queue, PrimaryQueue, SecondaryQueue, IsHost,
+              /*CallerNeedsEvent=*/true) {}
+
 handler::handler(std::shared_ptr<detail::queue_impl> Queue, bool IsHost,
                  bool CallerNeedsEvent)
     : handler(Queue, Queue, nullptr, IsHost, CallerNeedsEvent) {}
