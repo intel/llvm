1d4507584379b614e3ce58775458c04645c77cee
PietroGhg <pietro.ghiglio@codeplay.com>
2024-07-01 13:10:37 +0100

    Schedule optimization passes in Native CPU pipeline

--- llvm/lib/SYCLLowerIR/PipelineSYCLNativeCPU.cpp
+++ llvm/lib/SYCLLowerIR/PipelineSYCLNativeCPU.cpp
@@ -22,7 +22,21 @@
 #include "compiler/utils/prepare_barriers_pass.h"
 #include "compiler/utils/sub_group_analysis.h"
 #include "compiler/utils/work_item_loops_pass.h"
+#include "llvm/IR/PassManager.h"
 #include "llvm/Transforms/IPO/AlwaysInliner.h"
+#include "llvm/Transforms/AggressiveInstCombine/AggressiveInstCombine.h"
+#include "llvm/Transforms/IPO/Inliner.h"
+#include "llvm/Transforms/IPO/SCCP.h"
+#include "llvm/Transforms/InstCombine/InstCombine.h"
+#include "llvm/Transforms/Scalar/ADCE.h"
+#include "llvm/Transforms/Scalar/DCE.h"
+#include "llvm/Transforms/Scalar/EarlyCSE.h"
+#include "llvm/Transforms/Scalar/FlattenCFG.h"
+#include "llvm/Transforms/Scalar/GVN.h"
+#include "llvm/Transforms/Scalar/LowerExpectIntrinsic.h"
+#include "llvm/Transforms/Scalar/SROA.h"
+#include "llvm/Transforms/Scalar/SimplifyCFG.h"
+#include "llvm/Transforms/Scalar/Sink.h"
 #include "vecz/pass.h"
 #include "vecz/vecz_target_info.h"
 #endif
@@ -54,6 +68,7 @@ void llvm::sycl::utils::addSYCLNativeCPUBackendPasses(
     OptimizationLevel OptLevel) {
   MPM.addPass(ConvertToMuxBuiltinsSYCLNativeCPUPass());
 #ifdef NATIVECPU_USE_OCK
+  MPM.addPass(compiler::utils::PrepareBarriersPass());
   // Always enable vectorizer, unless explictly disabled or -O0 is set.
   if (OptLevel != OptimizationLevel::O0 && !SYCLNativeCPUNoVecz) {
     MAM.registerPass([] { return vecz::TargetInfoAnalysis(); });
@@ -73,13 +88,24 @@ void llvm::sycl::utils::addSYCLNativeCPUBackendPasses(
     MAM.registerPass(
         [QueryFunc] { return vecz::VeczPassOptionsAnalysis(QueryFunc); });
     MPM.addPass(vecz::RunVeczPass());
+    FunctionPassManager FPM;
+    FPM.addPass(LowerExpectIntrinsicPass());
+    FPM.addPass(SimplifyCFGPass());
+    FPM.addPass(SROAPass(SROAOptions::ModifyCFG));
+    FPM.addPass(EarlyCSEPass());
+    FPM.addPass(AggressiveInstCombinePass());
+    FPM.addPass(GVNPass(GVNOptions().setMemDep(true)));
+    FPM.addPass(AggressiveInstCombinePass());
+    FPM.addPass(ADCEPass());
+    FPM.addPass(DCEPass());
+    FPM.addPass(SimplifyCFGPass());
+    MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
   }
   compiler::utils::WorkItemLoopsPassOptions Opts;
   Opts.IsDebug = IsDebug;
   Opts.ForceNoTail = ForceNoTail;
   MAM.registerPass([] { return compiler::utils::BuiltinInfoAnalysis(); });
   MAM.registerPass([] { return compiler::utils::SubgroupAnalysis(); });
-  MPM.addPass(compiler::utils::PrepareBarriersPass());
   MPM.addPass(compiler::utils::WorkItemLoopsPass(Opts));
   MPM.addPass(AlwaysInlinerPass());
 #endif
