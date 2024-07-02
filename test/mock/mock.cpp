bc05606826fc3b6123b5d0b2c50601f2b52a6a8b
Aaron Greig <aaron.greig@codeplay.com>
2024-07-02 12:23:40 +0100

    Re-do docs and testing

--- test/mock/mock.cpp
+++ test/mock/mock.cpp
@@ -14,41 +14,17 @@
 #include <gtest/gtest.h>
 #include <ur_api.h>
 
-TEST(Mock, NullProperties) {
-    uur::raii::LoaderConfig loader_config;
-    ASSERT_EQ(urLoaderConfigCreate(loader_config.ptr()), UR_RESULT_SUCCESS);
-    ASSERT_EQ(urLoaderConfigSetMockCallbacks(loader_config, nullptr),
-              UR_RESULT_ERROR_INVALID_NULL_POINTER);
-}
-
-TEST(Mock, NullCallback) {
-    uur::raii::LoaderConfig loader_config;
-    ASSERT_EQ(urLoaderConfigCreate(loader_config.ptr()), UR_RESULT_SUCCESS);
-
-    ur_mock_callback_properties_t callback_properties = {
-        UR_STRUCTURE_TYPE_MOCK_CALLBACK_PROPERTIES, nullptr, "urAdapterGet",
-        UR_CALLBACK_OVERRIDE_MODE_REPLACE, nullptr};
-
-    ASSERT_EQ(
-        urLoaderConfigSetMockCallbacks(loader_config, &callback_properties),
-        UR_RESULT_ERROR_INVALID_NULL_POINTER);
-}
-
-ur_result_t generic_callback(void *) { return UR_RESULT_SUCCESS; }
+#include <ur_mock_helpers.hpp>
 
 TEST(Mock, NullHandle) {
-    ur_mock_callback_properties_t callback_properties = {
-        UR_STRUCTURE_TYPE_MOCK_CALLBACK_PROPERTIES, nullptr, "urAdapterGet",
-        UR_CALLBACK_OVERRIDE_MODE_REPLACE, &generic_callback};
-
-    ASSERT_EQ(urLoaderConfigSetMockCallbacks(nullptr, &callback_properties),
+    ASSERT_EQ(urLoaderConfigSetMockingEnabled(nullptr, true),
               UR_RESULT_ERROR_INVALID_NULL_HANDLE);
 }
 
 TEST(Mock, DefaultBehavior) {
     uur::raii::LoaderConfig loader_config;
     ASSERT_EQ(urLoaderConfigCreate(loader_config.ptr()), UR_RESULT_SUCCESS);
-    ASSERT_EQ(urLoaderConfigEnableLayer(loader_config, "UR_LAYER_MOCK"),
+    ASSERT_EQ(urLoaderConfigSetMockingEnabled(loader_config, true),
               UR_RESULT_SUCCESS);
     ASSERT_EQ(urLoaderInit(0, loader_config), UR_RESULT_SUCCESS);
 
@@ -108,32 +84,20 @@ ur_result_t afterUrAdapterGet(void *pParams) {
 TEST(Mock, Callbacks) {
     uur::raii::LoaderConfig loader_config;
     ASSERT_EQ(urLoaderConfigCreate(loader_config.ptr()), UR_RESULT_SUCCESS);
+    ASSERT_EQ(urLoaderConfigSetMockingEnabled(loader_config, true),
+              UR_RESULT_SUCCESS);
+    ASSERT_EQ(urLoaderInit(0, loader_config), UR_RESULT_SUCCESS);
 
     // This callback is set up to check *phAdapters is still the pre-call
     // init value we set below
-    ur_mock_callback_properties_t adapterGetBeforeProperties = {
-        UR_STRUCTURE_TYPE_MOCK_CALLBACK_PROPERTIES, nullptr, "urAdapterGet",
-        UR_CALLBACK_OVERRIDE_MODE_BEFORE, &beforeUrAdapterGet};
+    mock::callbacks.set_before_callback("urAdapterGet", &beforeUrAdapterGet);
 
     // This callback is set up to return a distinct test value in phAdapters
     // rather than the default generic handle
-    ur_mock_callback_properties_t adapterGetReplaceProperties = {
-        UR_STRUCTURE_TYPE_MOCK_CALLBACK_PROPERTIES, &adapterGetBeforeProperties,
-        "urAdapterGet", UR_CALLBACK_OVERRIDE_MODE_REPLACE,
-        &replaceUrAdapterGet};
+    mock::callbacks.set_replace_callback("urAdapterGet", &replaceUrAdapterGet);
 
     // This callback is set up to check our replace callback did its job
-    ur_mock_callback_properties_t adapterGetAfterProperties = {
-        UR_STRUCTURE_TYPE_MOCK_CALLBACK_PROPERTIES,
-        &adapterGetReplaceProperties, "urAdapterGet",
-        UR_CALLBACK_OVERRIDE_MODE_AFTER, &afterUrAdapterGet};
-
-    ASSERT_EQ(urLoaderConfigSetMockCallbacks(loader_config,
-                                             &adapterGetAfterProperties),
-              UR_RESULT_SUCCESS);
-    ASSERT_EQ(urLoaderConfigEnableLayer(loader_config, "UR_LAYER_MOCK"),
-              UR_RESULT_SUCCESS);
-    ASSERT_EQ(urLoaderInit(0, loader_config), UR_RESULT_SUCCESS);
+    mock::callbacks.set_after_callback("urAdapterGet", &afterUrAdapterGet);
 
     ur_adapter_handle_t adapter =
         reinterpret_cast<ur_adapter_handle_t>(0xF00DCAFE);
