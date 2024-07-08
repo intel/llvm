b8736c3a3c83fb5a80fa1898547cc3e4e1f65044
Sarnie, Nick <nick.sarnie@intel.com>
2024-07-08 11:36:54 -0700

    [SYCL][ESIMD][ABI Break] Remove predec atomic op
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- sycl/test-e2e/ESIMD/lsc/atomic_smoke.cpp
+++ sycl/test-e2e/ESIMD/lsc/atomic_smoke.cpp
@@ -120,8 +120,6 @@ const char *to_string(DWORDAtomicOp op) {
     return "load";
   case DWORDAtomicOp::store:
     return "store";
-  case DWORDAtomicOp::predec:
-    return "predec";
   }
   return "<unknown>";
 }
