c2420c2695831d4650dd5bb3dc17675f027f714e
Ewan Crawford <ewan@codeplay.com>
2024-07-09 12:06:06 +0100

    Refactor L0 command-buffer cleanup.
    Coverity has reported that there is the possibility of the
    command-buffer destructor throwing an exception from its calls
    to `CleanupCompletedEvent`.
    
    This is from an underlying `throw` in
    [GetQueue](https://github.com/oneapi-src/unified-runtime/blob/main/source/adapters/level_zero/queue.hpp#L890)
    when a `dynamic_cast` doesn't behave as expected. I initially tried
    changing this to an assert but the V2 Level Zero adapter is triggering
    this case while in it's developmental state, so an assert brings down the
    whole test suite rather than just being reported as a fail (the UR
    loader catches exceptions and returns them as error codes).
    
    Therefore this PR addresses the issue by moving the calls to
    `CleanupCompletedEvent()` outside of the command-buffer destructor. This allows
    us us to report an error from command-buffer release by doing cleanup prior to
    calling the destructor, without the risk of throwing an exception.

--- source/adapters/level_zero/command_buffer.cpp
+++ source/adapters/level_zero/command_buffer.cpp
@@ -424,9 +424,7 @@ ur_exp_command_buffer_handle_t_::ur_exp_command_buffer_handle_t_(
   urDeviceRetain(Device);
 }
 
-// The ur_exp_command_buffer_handle_t_ destructor releases all the memory
-// objects allocated for command_buffer management.
-ur_exp_command_buffer_handle_t_::~ur_exp_command_buffer_handle_t_() {
+void ur_exp_command_buffer_handle_t_::cleanupCommandBufferResources() {
   // Release the memory allocated to the Context stored in the command_buffer
   urContextRelease(Context);
 
@@ -703,6 +701,7 @@ urCommandBufferReleaseExp(ur_exp_command_buffer_handle_t CommandBuffer) {
   if (!CommandBuffer->RefCount.decrementAndTest())
     return UR_RESULT_SUCCESS;
 
+  CommandBuffer->cleanupCommandBufferResources();
   delete CommandBuffer;
   return UR_RESULT_SUCCESS;
 }
