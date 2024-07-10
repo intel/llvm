350efbdd463c288d80b866d784b2257d47c0830a
Byoungro So <byoungro.so@intel.com>
2024-07-10 10:04:44 -0700

    fix error code of grep

--- sycl/test/regression/pre-cpp11-abi-test.cpp
+++ sycl/test/regression/pre-cpp11-abi-test.cpp
@@ -1,3 +1,3 @@
-// RUN: grep __cxx11 %sycl_source_dir/../test/abi/sycl_symbols_linux.dump | FileCheck %s --allow-empty
+// RUN: not grep __cxx11 %sycl_source_dir/../test/abi/sycl_symbols_linux.dump | FileCheck %s --allow-empty
 // CHECK-NOT: {{.}}
 // REQUIRES: linux
