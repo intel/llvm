oneapi-src/unified-runtime

[L0] Create v2::context
that inherits from the old context and extends it with v2 functionality.
urContextCreate will now always create v2::context. All legacy
functions will continue to operate as normal and the optimized
code path can levarage additional features implemented in v2.

source/adapters/level_zero/v2/context.hpp
@@ -1,0 +1,29 @@
+//===--------- context.hpp - Level Zero Adapter --------------------------===//
+//
+// Copyright (C) 2024 Intel Corporation
+//
+// Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM
+// Exceptions. See LICENSE.TXT
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "command_list_cache.hpp"
+
+#include "../context.hpp"
+
+namespace v2 {
+
+struct ur_context_handle_t_;
+using ur_context_handle_t = ur_context_handle_t_ *;
+
+struct ur_context_handle_t_ : public ::ur_context_handle_t_ {
+  ur_context_handle_t_(ze_context_handle_t ZeContext, uint32_t NumDevices,
+                       const ur_device_handle_t *Devs, bool OwnZeContext);
+
+  command_list_cache_t CommandListCache;
+};
+
+} // namespace v2

intel/llvm

source/adapters/level_zero/v2/context.hpp
@@ -1,0 +1,29 @@
+//===--------- context.hpp - Level Zero Adapter --------------------------===//
+//
+// Copyright (C) 2024 Intel Corporation
+//
+// Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM
+// Exceptions. See LICENSE.TXT
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "command_list_cache.hpp"
+
+#include "../context.hpp"
+
+namespace v2 {
+
+struct ur_context_handle_t_;
+using ur_context_handle_t = ur_context_handle_t_ *;
+
+struct ur_context_handle_t_ : public ::ur_context_handle_t_ {
+  ur_context_handle_t_(ze_context_handle_t hContext, uint32_t numDevices,
+                       const ur_device_handle_t *phDevices, bool ownZeContext);
+
+  command_list_cache_t commandListCache;
+};
+
+} // namespace v2