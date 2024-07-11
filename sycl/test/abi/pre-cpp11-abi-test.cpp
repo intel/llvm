11bb072824e237a16a23652e9b48b825fb40de14
Artur Gainullin <artur.gainullin@intel.com>
2024-07-11 10:56:14 -0700

    Add exclude list to ABI test

---
+++ sycl/test/abi/pre-cpp11-abi-test.cpp
@@ -0,0 +1,5 @@
+// REQUIRES: linux
+// At first exclude known symbols which needs to be fixed and then check that cxx11 is not matched.
+// RUN: grep -v -f %S/cxx11_abi_exclude_list.txt %S/sycl_symbols_linux.dump | FileCheck %s
+// CHECK-NOT: {{cxx11}}
+
--- sycl/test/regression/pre-cpp11-abi-test.cpp
+++
@@ -1,3 +0,0 @@
-// RUN: not grep __cxx11 %sycl_source_dir/../test/abi/sycl_symbols_linux.dump | FileCheck %s --allow-empty
-// CHECK-NOT: {{.}}
-// REQUIRES: linux
