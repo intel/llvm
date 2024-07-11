1a4ae12f5153052fbe0f25a7f59c4a39e0d30734
Wu, Yingcong <yingcong.wu@intel.com>
2024-07-10 19:33:49 -0700

    reduce range

--- sycl/test-e2e/GroupAlgorithm/inclusive_scan_sycl2020.cpp
+++ sycl/test-e2e/GroupAlgorithm/inclusive_scan_sycl2020.cpp
@@ -41,11 +41,12 @@ void test(const InputContainer &input, BinaryOperation binary_op,
     q.submit([&](handler &cgh) {
       accessor in{in_buf, cgh, sycl::read_only};
       accessor out{out_buf, cgh, sycl::write_only, sycl::no_init};
-      cgh.parallel_for<kernel_name0>(nd_range<1>(G, G), [=](nd_item<1> it) {
-        group<1> g = it.get_group();
-        int lid = it.get_local_id(0);
-        out[lid] = inclusive_scan_over_group(g, in[lid], binary_op);
-      });
+      cgh.parallel_for<kernel_name0>(
+          nd_range<1>(confirmRange, confirmRange), [=](nd_item<1> it) {
+            group<1> g = it.get_group();
+            int lid = it.get_local_id(0);
+            out[lid] = inclusive_scan_over_group(g, in[lid], binary_op);
+          });
     });
   }
   emu::inclusive_scan(input.begin(), input.begin() + confirmRange,
@@ -66,11 +67,12 @@ void test(const InputContainer &input, BinaryOperation binary_op,
     q.submit([&](handler &cgh) {
       accessor in{in_buf, cgh, sycl::read_only};
       accessor out{out_buf, cgh, sycl::write_only, sycl::no_init};
-      cgh.parallel_for<kernel_name1>(nd_range<1>(G, G), [=](nd_item<1> it) {
-        group<1> g = it.get_group();
-        int lid = it.get_local_id(0);
-        out[lid] = inclusive_scan_over_group(g, in[lid], binary_op, init);
-      });
+      cgh.parallel_for<kernel_name1>(
+          nd_range<1>(confirmRange, confirmRange), [=](nd_item<1> it) {
+            group<1> g = it.get_group();
+            int lid = it.get_local_id(0);
+            out[lid] = inclusive_scan_over_group(g, in[lid], binary_op, init);
+          });
     });
   }
   emu::inclusive_scan(input.begin(), input.begin() + confirmRange,
@@ -92,12 +94,14 @@ void test(const InputContainer &input, BinaryOperation binary_op,
     q.submit([&](handler &cgh) {
       accessor in{in_buf, cgh, sycl::read_only};
       accessor out{out_buf, cgh, sycl::write_only, sycl::no_init};
-      cgh.parallel_for<kernel_name2>(nd_range<1>(G, G), [=](nd_item<1> it) {
-        group<1> g = it.get_group();
-        joint_inclusive_scan(
-            g, global_ptr<const InputT>(in), global_ptr<const InputT>(in) + N,
-            out.template get_multi_ptr<access::decorated::no>(), binary_op);
-      });
+      cgh.parallel_for<kernel_name2>(
+          nd_range<1>(confirmRange, confirmRange), [=](nd_item<1> it) {
+            group<1> g = it.get_group();
+            joint_inclusive_scan(
+                g, global_ptr<const InputT>(in),
+                global_ptr<const InputT>(in) + N,
+                out.template get_multi_ptr<access::decorated::no>(), binary_op);
+          });
     });
   }
   emu::inclusive_scan(input.begin(), input.begin() + N, expected.begin(),
@@ -118,13 +122,15 @@ void test(const InputContainer &input, BinaryOperation binary_op,
     q.submit([&](handler &cgh) {
       accessor in{in_buf, cgh, sycl::read_only};
       accessor out{out_buf, cgh, sycl::write_only, sycl::no_init};
-      cgh.parallel_for<kernel_name3>(nd_range<1>(G, G), [=](nd_item<1> it) {
-        group<1> g = it.get_group();
-        joint_inclusive_scan(
-            g, global_ptr<const InputT>(in), global_ptr<const InputT>(in) + N,
-            out.template get_multi_ptr<access::decorated::no>(), binary_op,
-            init);
-      });
+      cgh.parallel_for<kernel_name3>(
+          nd_range<1>(confirmRange, confirmRange), [=](nd_item<1> it) {
+            group<1> g = it.get_group();
+            joint_inclusive_scan(
+                g, global_ptr<const InputT>(in),
+                global_ptr<const InputT>(in) + N,
+                out.template get_multi_ptr<access::decorated::no>(), binary_op,
+                init);
+          });
     });
   }
   emu::inclusive_scan(input.begin(), input.begin() + N, expected.begin(),
