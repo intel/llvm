f76567f43bd47961377664151062f11f968fabee
Lu, John <john.lu@intel.com>
2024-07-15 07:31:32 -0700

    Deal with SYCL_EXTERNAL header decls that have lost SYCL_EXTERNAL attribute in LLVM IR
    Signed-off-by: Lu, John <john.lu@intel.com>

--- llvm/lib/SYCLLowerIR/ModuleSplitter.cpp
+++ llvm/lib/SYCLLowerIR/ModuleSplitter.cpp
@@ -1313,7 +1313,11 @@ splitSYCLModule(std::unique_ptr<Module> M, ModuleSplitterSettings Settings) {
 
 bool canBeImportedFunction(const Function &F) {
   if (F.isIntrinsic() || F.getName().starts_with("__") ||
-      !llvm::sycl::utils::isSYCLExternalFunction(&F))
+      !( // SYCL_EXTERNAL property is not recorded for a declaration
+         // in a header file.  Thus SYCL IR that is a declaration
+         // will be considered as SYCL_EXTERNAL for the purposes of
+         // this function.
+          F.isDeclaration() || llvm::sycl::utils::isSYCLExternalFunction(&F)))
     return false;
 
   bool ReturnValue = true;
