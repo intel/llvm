9e3b5277508a70c5ea5f2544265d8105b8ae2b9a
Lu, John <john.lu@intel.com>
2024-07-15 13:12:11 -0700

    Add option to allow dependencies between device images
    Signed-off-by: Lu, John <john.lu@intel.com>

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -10704,6 +10704,11 @@ static void addArgs(ArgStringList &DstArgs, const llvm::opt::ArgList &Alloc,
 }
 
 static bool supportDynamicLinking(const llvm::opt::ArgList &TCArgs) {
+  if (TCArgs.hasFlag(options::OPT_fsycl_allow_split_with_dependencies,
+                     options::OPT_fno_sycl_allow_split_with_dependencies,
+                     false))
+    return true;
+
   // Enable when SYCL RT supports dynamic linking
   //  return (TCArgs.hasArg(options::OPT_shared));
   return false;
