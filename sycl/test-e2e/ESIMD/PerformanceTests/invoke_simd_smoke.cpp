8088c3e4872a7032d478c10e61209443350290bc
Fine, Gregory <gregory.fine@intel.com>
2024-07-15 18:34:18 -0700

    Adjust instruction count

--- sycl/test-e2e/ESIMD/PerformanceTests/invoke_simd_smoke.cpp
+++ sycl/test-e2e/ESIMD/PerformanceTests/invoke_simd_smoke.cpp
@@ -10,7 +10,7 @@
 
 // RUN: mkdir -p %t.dir && %{build} -fsycl -fno-sycl-device-code-split-esimd -Xclang -fsycl-allow-func-ptr -o %t.dir/exec.out
 // RUN: env  IGC_VCSaveStackCallLinkage=1 IGC_VCDirectCallsOnly=1 IGC_DumpToCustomDir=%t.dir IGC_ShaderDumpEnable=1 %{run} %t.dir/exec.out
-// RUN: python3 %S/instruction_count.py %t.dir 99 OCL_asmc2becd046944fa5f_simd16_entry_0001.asm
+// RUN: python3 %S/instruction_count.py %t.dir 197 OCL_asmc2becd046944fa5f_simd16_entry_0001.asm
 // RUN: echo "Baseline from driver version 1.3.29735"
 
 #include "../../InvokeSimd/invoke_simd_smoke.cpp"
