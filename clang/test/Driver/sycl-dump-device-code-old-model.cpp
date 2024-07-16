7515da8e2e94c4b45e3bafbf19d968b2b05031bc
Michael Toguchi <michael.d.toguchi@intel.com>
2024-07-16 11:25:53 -0700

    [NFC][SYCL] Update tests to reflect new offload model (#14539)
    Update existing SYCL based driver tests to use the new offload model and
    retain the equivalent old offloading model tests be creating copies.
    These tests use the --offload-new-driver and --no-offload-new-driver
    options to force a particular model to follow.

--- clang/test/Driver/sycl-dump-device-code-old-model.cpp
+++ clang/test/Driver/sycl-dump-device-code-old-model.cpp
@@ -4,31 +4,31 @@
 // in the user provided directory.
 
 // Linux
-// clang -fsycl -target x86_64-unknown-linux-gnu
-// RUN: %clang -fsycl -fno-sycl-instrument-device-code -fno-sycl-device-lib=all -target x86_64-unknown-linux-gnu -fsycl-dump-device-code=/user/input/path %s -### 2>&1 \
+// clang -fsycl --no-offload-new-driver -target x86_64-unknown-linux-gnu
+// RUN: %clang -fsycl --no-offload-new-driver -fno-sycl-instrument-device-code -fno-sycl-device-lib=all -target x86_64-unknown-linux-gnu -fsycl-dump-device-code=/user/input/path %s -### 2>&1 \
 // RUN: | FileCheck %s --check-prefixes=CHK-FSYCL-DUMP-DEVICE-CODE
 
-// clang -fsycl -fsycl-targets=spir64-unknown-unknown
-// RUN: %clang -fsycl  -fsycl-targets=spir64-unknown-unknown -target x86_64-unknown-linux-gnu -fsycl-dump-device-code=/user/input/path %s -### 2>&1 \
+// clang -fsycl --no-offload-new-driver -fsycl-targets=spir64-unknown-unknown
+// RUN: %clang -fsycl --no-offload-new-driver  -fsycl-targets=spir64-unknown-unknown -target x86_64-unknown-linux-gnu -fsycl-dump-device-code=/user/input/path %s -### 2>&1 \
 // RUN: | FileCheck %s --check-prefixes=CHK-FSYCL-DUMP-DEVICE-CODE
 
 // clang --driver-mode=g++
-// RUN: %clangxx -fsycl  -fsycl-targets=spir64-unknown-unknown -target x86_64-unknown-linux-gnu -fsycl-dump-device-code=/user/input/path %s -### 2>&1 \
+// RUN: %clangxx -fsycl --no-offload-new-driver  -fsycl-targets=spir64-unknown-unknown -target x86_64-unknown-linux-gnu -fsycl-dump-device-code=/user/input/path %s -### 2>&1 \
 // RUN: | FileCheck %s --check-prefixes=CHK-FSYCL-DUMP-DEVICE-CODE
 
 // Windows
-// RUN: %clang_cl -fsycl -fsycl-dump-device-code=/user/input/path %s -### 2>&1 \
+// RUN: %clang_cl -fsycl --no-offload-new-driver -fsycl-dump-device-code=/user/input/path %s -### 2>&1 \
 // RUN: | FileCheck %s --check-prefixes=CHK-FSYCL-DUMP-DEVICE-CODE-WIN
 
 // CHK-FSYCL-DUMP-DEVICE-CODE: llvm-foreach{{.*}} "--out-dir=/user/input/path{{(/|\\\\)}}" "--" "{{.*}}llvm-spirv"
 // CHK-FSYCL-DUMP-DEVICE-CODE-WIN: llvm-foreach{{.*}} "--out-dir=/user/input/path{{(/|\\\\)}}" "--" "{{.*}}llvm-spirv"
 
 // Linux
-// RUN: %clang -fsycl  -fsycl-targets=spir64-unknown-unknown -target x86_64-unknown-linux-gnu -fsycl-dump-device-code= %s -### 2>&1 \
+// RUN: %clang -fsycl --no-offload-new-driver  -fsycl-targets=spir64-unknown-unknown -target x86_64-unknown-linux-gnu -fsycl-dump-device-code= %s -### 2>&1 \
 // RUN: | FileCheck %s --check-prefixes=CHK-FSYCL-DUMP-DEVICE-CODE-CWD
 
 // Windows
-// RUN: %clang_cl -fsycl -fsycl-dump-device-code= %s -### 2>&1 \
+// RUN: %clang_cl -fsycl --no-offload-new-driver -fsycl-dump-device-code= %s -### 2>&1 \
 // RUN: | FileCheck %s --check-prefixes=CHK-FSYCL-DUMP-DEVICE-CODE-WIN-CWD
 
 // CHK-FSYCL-DUMP-DEVICE-CODE-CWD: llvm-foreach{{.*}} "--out-dir=.{{(/|\\\\)}}" "--" "{{.*}}llvm-spirv"
