202ab499a8857db72682a33f48929bda20d9faaf
Li, Ian <ian.li@intel.com>
2024-07-17 09:46:22 -0700

    Temporarily re-enable fpga_pipes.cpp

--- sycl/test-e2e/Basic/fpga_tests/fpga_pipes.cpp
+++ sycl/test-e2e/Basic/fpga_tests/fpga_pipes.cpp
@@ -1,6 +1,6 @@
 // REQUIRES: accelerator
 // https://github.com/intel/llvm/issues/14308
-// UNSUPPORTED: accelerator
+// Temporarily re-enabled for testing purposes
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 
