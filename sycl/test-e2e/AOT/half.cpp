e328ae7634854ecda36bd5417169886b2db782b7
Cai, Justin <justin.cai@intel.com>
2024-07-17 09:32:41 -0700

    Add windows conditional to half.cpp

--- sycl/test-e2e/AOT/half.cpp
+++ sycl/test-e2e/AOT/half.cpp
@@ -3,12 +3,12 @@
 
 // REQUIRES: ocloc, opencl-aot, any-device-is-cpu
 // RUN: %clangxx -fsycl -fsycl-targets=intel_gpu_tgllp -o %t.tgllp.out %s
-// RUN: %clangxx -fsycl -fsycl-targets=intel_gpu_cfl -o %t.cfl.out %s
 // RUN: %clangxx -fsycl -fsycl-targets=spir64_x86_64 -o %t.x86.out %s
 
-// ocloc on windows does not have support for PVC, so this command will
+// ocloc on windows does not have support for PVC/CFL, so this command will
 // result in an error when on windows. (In general, there is no support
-// for pvc on windows.)
+// for pvc/cfl on windows.)
+// RUN: %if !windows %{ %clangxx -fsycl -fsycl-targets=intel_gpu_cfl -o %t.cfl.out %s %}
 // RUN: %if !windows %{ %clangxx -fsycl -fsycl-targets=intel_gpu_pvc -o %t.pvc.out %s %}
 
 #include <sycl/detail/core.hpp>
