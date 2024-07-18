bc1f7625c8464f334a9e95a72e43ce01fd8674ac
Zhao, Yang2 <yang2.zhao@intel.com>
2024-07-17 20:00:14 -0700

    fix SanitizerInterceptor is always constructed

--- source/loader/layers/sanitizer/ur_sanddi.cpp
+++ source/loader/layers/sanitizer/ur_sanddi.cpp
@@ -1527,19 +1527,20 @@ ur_result_t context_t::init(ur_dditable_t *dditable,
     ur_result_t result = UR_RESULT_SUCCESS;
 
     if (enabledLayerNames.count("UR_LAYER_ASAN")) {
-        getContext()->enabledType = SanitizerType::AddressSanitizer;
+        enabledType = SanitizerType::AddressSanitizer;
+        interceptor = std::make_unique<SanitizerInterceptor>(logger);
     } else if (enabledLayerNames.count("UR_LAYER_MSAN")) {
-        getContext()->enabledType = SanitizerType::MemorySanitizer;
+        enabledType = SanitizerType::MemorySanitizer;
     } else if (enabledLayerNames.count("UR_LAYER_TSAN")) {
-        getContext()->enabledType = SanitizerType::ThreadSanitizer;
+        enabledType = SanitizerType::ThreadSanitizer;
     }
 
     // Only support AddressSanitizer now
-    if (getContext()->enabledType != SanitizerType::AddressSanitizer) {
+    if (enabledType != SanitizerType::AddressSanitizer) {
         return result;
     }
 
-    if (getContext()->enabledType == SanitizerType::AddressSanitizer) {
+    if (enabledType == SanitizerType::AddressSanitizer) {
         if (!(dditable->VirtualMem.pfnReserve && dditable->VirtualMem.pfnMap &&
               dditable->VirtualMem.pfnGranularityGetInfo)) {
             die("Some VirtualMem APIs are needed to enable UR_LAYER_ASAN");
