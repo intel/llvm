26d1ab1658733d34429650f79d5e39c5b477c063
Byoungro So <byoungro.so@intel.com>
2024-07-18 09:30:58 -0700

    Update sycl/include/sycl/kernel_bundle.hpp
    Co-authored-by: Steffen Larsen <steffen.larsen@intel.com>

--- sycl/include/sycl/kernel_bundle.hpp
+++ sycl/include/sycl/kernel_bundle.hpp
@@ -932,7 +932,7 @@ make_kernel_bundle_from_source(
     std::vector<std::pair<sycl::detail::string_view, sycl::detail::string_view>>
         IncludePairsVec);
 
-__SYCL_EXPORT inline kernel_bundle<bundle_state::ext_oneapi_source>
+inline kernel_bundle<bundle_state::ext_oneapi_source>
 make_kernel_bundle_from_source(
     const context &SyclContext, source_language Language,
     const std::vector<std::byte> &Bytes,
