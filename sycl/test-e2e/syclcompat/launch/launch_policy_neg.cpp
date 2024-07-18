c6a883e086f4898c75b96d56729eeaaa87b0f40d
Joe Todd <joe.todd@codeplay.com>
2024-07-18 13:47:30 +0100

    Isolate static_assert negative tests
    It's a headache to debug these CHECK-DAG tests when they go wrong, so
    I have isolated each case with an `ifdef` and we run each in turn.
    
    Also fix expected error messages for a case.
    
    Signed-off-by: Joe Todd <joe.todd@codeplay.com>

--- sycl/test-e2e/syclcompat/launch/launch_policy_neg.cpp
+++ sycl/test-e2e/syclcompat/launch/launch_policy_neg.cpp
@@ -20,7 +20,18 @@
  *     Negative tests for new launch_policy.
  **************************************************************************/
 
-// RUN: not %clangxx -std=c++20 -fsycl -fsycl-targets=%{sycl_triple} %s -o %t.out 2>&1 | FileCheck -vv %s
+// RUN: not %clangxx -std=c++20 -fsycl -fsycl-targets=%{sycl_triple} -DCHECK1 %s -o %t.out 2>&1 | FileCheck -vv %s --check-prefixes=CHECK1
+// RUN: not %clangxx -std=c++20 -fsycl -fsycl-targets=%{sycl_triple} -DCHECK2 %s -o %t.out 2>&1 | FileCheck -vv %s --check-prefixes=CHECK2
+// RUN: not %clangxx -std=c++20 -fsycl -fsycl-targets=%{sycl_triple} -DCHECK3 %s -o %t.out 2>&1 | FileCheck -vv %s --check-prefixes=CHECK3
+// RUN: not %clangxx -std=c++20 -fsycl -fsycl-targets=%{sycl_triple} -DCHECK4 %s -o %t.out 2>&1 | FileCheck -vv %s --check-prefixes=CHECK4
+// RUN: not %clangxx -std=c++20 -fsycl -fsycl-targets=%{sycl_triple} -DCHECK5 %s -o %t.out 2>&1 | FileCheck -vv %s --check-prefixes=CHECK5
+// RUN: not %clangxx -std=c++20 -fsycl -fsycl-targets=%{sycl_triple} -DCHECK6 %s -o %t.out 2>&1 | FileCheck -vv %s --check-prefixes=CHECK6
+// RUN: not %clangxx -std=c++20 -fsycl -fsycl-targets=%{sycl_triple} -DCHECK7 %s -o %t.out 2>&1 | FileCheck -vv %s --check-prefixes=CHECK7
+// RUN: not %clangxx -std=c++20 -fsycl -fsycl-targets=%{sycl_triple} -DCHECK8 %s -o %t.out 2>&1 | FileCheck -vv %s --check-prefixes=CHECK8
+// RUN: not %clangxx -std=c++20 -fsycl -fsycl-targets=%{sycl_triple} -DCHECK9 %s -o %t.out 2>&1 | FileCheck -vv %s --check-prefixes=CHECK9
+// RUN: not %clangxx -std=c++20 -fsycl -fsycl-targets=%{sycl_triple} -DCHECK10 %s -o %t.out 2>&1 | FileCheck -vv %s --check-prefixes=CHECK10
+// RUN: not %clangxx -std=c++20 -fsycl -fsycl-targets=%{sycl_triple} -DCHECK11 %s -o %t.out 2>&1 | FileCheck -vv %s --check-prefixes=CHECK11
+// RUN: not %clangxx -std=c++20 -fsycl -fsycl-targets=%{sycl_triple} -DCHECK12 %s -o %t.out 2>&1 | FileCheck -vv %s --check-prefixes=CHECK12
 
 #include <sycl/ext/oneapi/kernel_properties/properties.hpp>
 #include <sycl/detail/core.hpp>
@@ -69,97 +80,112 @@ dummy_properties(Props... props)
 void test_variadic_config_ctor() {
   std::cout << __PRETTY_FUNCTION__ << std::endl;
 
+#ifdef CHECK1
   // Missing range
   {
     compat_exp::launch_policy missing_range_config(
         compat_exp::kernel_properties{sycl_exp::sub_group_size<32>});
-    //CHECK-DAG: error: static assertion failed due to requirement 'syclcompat::detail::is_range_or_nd_range_v
+    //CHECK1: error: static assertion failed due to requirement 'syclcompat::detail::is_range_or_nd_range_v
   }
-
+#endif
+#ifdef CHECK2
   // Duplicate nd_range
   {
     sycl::nd_range<3> launch_range{{1,1,32},{1,1,32}};
     compat_exp::launch_policy duplicate_nd_range_config(launch_range, launch_range);
-    //CHECK-DAG: error: static assertion failed due to requirement 'std::conjunction_v<std::disjunction<
+    //CHECK2: error: static assertion failed{{.*Did you forget to wrap}}
   }
-
+#endif
+#ifdef CHECK3
   // Duplicate range
   {
     sycl::range<3> launch_range{1,1,32};
     compat_exp::launch_policy duplicate_nd_range_config(launch_range, launch_range);
-    //CHECK-DAG: error: static assertion failed due to requirement 'std::conjunction_v<std::disjunction<
+    //CHECK3: error: static assertion failed{{.*Did you forget to wrap}}
   }
-
+#endif
+#ifdef CHECK4
   // Unwrapped property
   {
     sycl::nd_range<3> launch_range{{1,1,32},{1,1,32}};
     compat_exp::launch_policy unwrapped_property_config(launch_range, {sycl_exp::sub_group_size<32>});
-    //CHECK-DAG: error: no viable constructor or deduction guide for deduction of template arguments of 'compat_exp::launch_policy'
+    //CHECK4: error: no viable constructor or deduction guide for deduction of template arguments of 'compat_exp::launch_policy'
   }
-
+#endif
+#ifdef CHECK5
   // Foreign object in ctor
   {
     dummy_properties foreign_object{sycl_exp::sub_group_size<32>};
     sycl::nd_range<3> launch_range{{1,1,32},{1,1,32}};
     compat_exp::launch_policy unwrapped_property_config(launch_range, foreign_object);
-    //CHECK-DAG: error: no viable constructor or deduction guide for deduction of template arguments of 'compat_exp::launch_policy'
+    //CHECK5: error: static assertion failed{{.*Did you forget to wrap}}
   }
+#endif
+#ifdef CHECK6
   // Local mem with sycl::range launch 1
   {
     sycl::range<3> launch_range{1, 1, 32};
     compat_exp::local_mem_size lmem_size(0);
     compat_exp::launch_policy range_and_local_mem_config_1(launch_range,
                                                          lmem_size);
-    //CHECK-DAG: error: static assertion failed due to requirement 'syclcompat::detail::is_nd_range_v<sycl::range<3>> || !true': sycl::range kernel launches are incompatible with local
+    //CHECK6: error: static assertion failed due to requirement 'syclcompat::detail::is_nd_range_v<sycl::range<3>> || !true': sycl::range kernel launches are incompatible with local
   }
+#endif
+#ifdef CHECK7
   // Local mem with sycl::range launch 2
   {
     syclcompat::dim3 launch_range{32, 1, 1};
     compat_exp::local_mem_size lmem_size(0);
     compat_exp::launch_policy range_and_local_mem_config_2(launch_range, compat_exp::kernel_properties{sycl_exp::sub_group_size<32>},
                                                          lmem_size);
-    //CHECK-DAG: error: static assertion failed due to requirement 'syclcompat::detail::is_nd_range_v<sycl::range<3>> || !true': sycl::range kernel launches are incompatible with local
+    //CHECK7: error: static assertion failed due to requirement 'syclcompat::detail::is_nd_range_v<sycl::range<3>> || !true': sycl::range kernel launches are incompatible with local
   }
+#endif
+#ifdef CHECK8
   // Duplicate local_mem spec
   {
     sycl::nd_range<3> launch_range{{1, 1, 32}, {1, 1, 32}};
     compat_exp::local_mem_size lmem_size(0);
     compat_exp::launch_policy duplicate_local_mem_config(launch_range, lmem_size, lmem_size);
-    //CHECK-DAG: error: static assertion failed due to requirement{{.*exactly once}}
+    //CHECK8: error: static assertion failed due to requirement{{.*exactly once}}
   }
-
+#endif
+#ifdef CHECK9
   // Duplicate kernel_properties spec
   {
     sycl::nd_range<3> launch_range{{1, 1, 32}, {1, 1, 32}};
     compat_exp::kernel_properties kernel_props{sycl_exp::sub_group_size<32>};
     compat_exp::launch_policy duplicate_kernel_properties_config(launch_range, kernel_props, kernel_props);
-    //CHECK-DAG: error: static assertion failed due to requirement{{.*type appears more than once}}
+    //CHECK9: error: static assertion failed due to requirement{{.*type appears more than once}}
   }
-
+#endif
+#ifdef CHECK10
   // Duplicate launch_properties spec
   {
     sycl::nd_range<3> launch_range{{1, 1, 32}, {1, 1, 32}};
     compat_exp::launch_properties launch_props{};
     compat_exp::local_mem_size lmem_size(0);
     compat_exp::launch_policy duplicate_launch_properties_config(launch_range, launch_props, lmem_size, launch_props);
-    //CHECK-DAG: error: static assertion failed due to requirement{{.*type appears more than once}}
+    //CHECK10: error: static assertion failed due to requirement{{.*type appears more than once}}
   }
-
+#endif
+#ifdef CHECK11
   // Missing kernel args
   {
     sycl::range<3> launch_range{1, 1, 32};
     compat_exp::launch_policy range_only(launch_range);
     compat_exp::launch<int_kernel>(range_only);
-    //CHECK-DAG: error: static assertion failed due to requirement 'syclcompat::args_compatible
+    //CHECK11: error: static assertion failed due to requirement 'syclcompat::args_compatible
   }
-
+#endif
+#ifdef CHECK12
   // Extra kernel args
   {
     sycl::nd_range<3> launch_range{{1, 1, 32}, {1, 1, 32}};
     compat_exp::launch_policy range_only(launch_range);
     int extra_arg = 1;
     compat_exp::launch<empty_kernel>(range_only, extra_arg);
-    //CHECK-DAG: error: static assertion failed due to requirement 'syclcompat::args_compatible
+    //CHECK12: error: static assertion failed due to requirement 'syclcompat::args_compatible
   }
-
+#endif
 }
