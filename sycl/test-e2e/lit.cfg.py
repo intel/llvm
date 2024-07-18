38836f57740fd663e3e915ed36f6539971acfdc0
Agarwal, Udit <udit.agarwal@intel.com>
2024-07-18 07:57:42 -0700

    Remove debug statements

--- sycl/test-e2e/lit.cfg.py
+++ sycl/test-e2e/lit.cfg.py
@@ -253,9 +253,6 @@ if sp[0] == 0:
     config.available_features.add("level_zero_dev_kit")
     config.substitutions.append(("%level_zero_options", level_zero_options))
 else:
-    lit_config.warning("Could not find L0 library and/or header files. Disabling level_zero_dev_kit LIT feature.")
-    lit_config.warning("L0 options: " + level_zero_options)
-    lit_config.warning("config.level_zero_libs_dir: " + config.level_zero_libs_dir)
     config.substitutions.append(("%level_zero_options", ""))
 
 # Check for sycl-preview library
@@ -337,7 +334,6 @@ if config.opencl_libs_dir:
             ("%opencl_lib", "-L" + config.opencl_libs_dir + " -lOpenCL")
         )
     config.available_features.add("opencl_icd")
-
 config.substitutions.append(("%opencl_include_dir", config.opencl_include_dir))
 
 if cl_options:
