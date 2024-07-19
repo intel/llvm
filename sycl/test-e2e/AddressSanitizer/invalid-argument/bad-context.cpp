c6286fa0fd7a866203987c5f278a8d9b29763a1e
Zhao, Yang2 <yang2.zhao@intel.com>
2024-07-19 00:19:09 -0700

    fix lit test

--- sycl/test-e2e/AddressSanitizer/invalid-argument/bad-context.cpp
+++ sycl/test-e2e/AddressSanitizer/invalid-argument/bad-context.cpp
@@ -1,5 +1,4 @@
-// UNSUPPORTED: true
-// REQUIRES: linux
+// REQUIRES: linux, (gpu && level_zero), cpu
 // RUN: %{build} %device_asan_flags -DMALLOC_DEVICE -O2 -g -o %t
 // RUN: env SYCL_PREFER_UR=1 %{run} not %t 2>&1 | FileCheck --check-prefixes CHECK,CHECK-DEVICE %s
 
