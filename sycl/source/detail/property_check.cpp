672d2770ba0c05127bdd12b8c4d54f333cf16587
Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>
2024-07-19 06:12:08 -0700

    fix format
    Signed-off-by: Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>

--- sycl/source/detail/property_check.cpp
+++ sycl/source/detail/property_check.cpp
@@ -1,4 +1,4 @@
-//==----------------- property_check.cpp - SYCL property check utils------==//
+//==----------------- property_check.cpp - SYCL property check utils--------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- sycl/source/detail/property_check.hpp
+++ sycl/source/detail/property_check.hpp
@@ -1,4 +1,4 @@
-//==----------------- property_check.hpp - SYCL property check utils------==//
+//==----------------- property_check.hpp - SYCL property check utils--------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
