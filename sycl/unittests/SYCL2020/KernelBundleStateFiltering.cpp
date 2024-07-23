57103f0240b7c5db00cbb9e25f6d9afed7262bdb
Alexey Sachkov <sachkov2011@gmail.com>
2024-07-23 20:31:46 +0200

    Fixes to unit-tests

--- sycl/unittests/SYCL2020/KernelBundleStateFiltering.cpp
+++ sycl/unittests/SYCL2020/KernelBundleStateFiltering.cpp
@@ -142,13 +142,14 @@ pi_result redefinedDevicesGet(pi_platform platform, pi_device_type device_type,
 }
 
 pi_result redefinedExtDeviceSelectBinary(pi_device device,
-                                         pi_device_binary *binaries,
+                                         pi_device_binary *pi_binaries,
                                          pi_uint32 num_binaries,
                                          pi_uint32 *selected_binary_ind) {
   EXPECT_EQ(num_binaries, 1U);
+  auto *binaries = reinterpret_cast<sycl_device_binary>(pi_binaries);
   // Treat image 3 as incompatible with one of the devices.
-  if (TrackedImages.count(binaries[0]->BinaryStart) != 0 &&
-      *binaries[0]->BinaryStart == 3 &&
+  if (TrackedImages.count(binaries[0].BinaryStart) != 0 &&
+      *binaries[0].BinaryStart == 3 &&
       device == reinterpret_cast<pi_device>(2)) {
     return PI_ERROR_INVALID_BINARY;
   }
