101f8abe4170a77970b16e40545846a8f3a172f7
Ben Tracy <ben.tracy@codeplay.com>
2024-07-23 12:07:38 +0100

    Fix formatting

--- sycl/source/detail/graph_impl.cpp
+++ sycl/source/detail/graph_impl.cpp
@@ -907,7 +907,7 @@ exec_graph_impl::enqueue(const std::shared_ptr<sycl::detail::queue_impl> &Queue,
       // If we have no requirements or dependent events for the command buffer,
       // enqueue it directly
       if (CGData.MRequirements.empty() && CGData.MEvents.empty()) {
-        if (NewEvent != nullptr){
+        if (NewEvent != nullptr) {
           NewEvent->setSubmissionTime();
           NewEvent->setHostEnqueueTime();
         }
