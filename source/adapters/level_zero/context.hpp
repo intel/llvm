5c9de5a2a084b3ca54c945271d032ae114df5324
Winston Zhang <winston.zhang@intel.com>
2024-07-22 17:04:24 -0700

    [L0] Refactored against in-order list to enable CB event by default
    Signed-off-by: Winston Zhang <winston.zhang@intel.com>

--- source/adapters/level_zero/context.hpp
+++ source/adapters/level_zero/context.hpp
@@ -217,6 +217,15 @@ struct ur_context_handle_t_ : _ur_object {
   // Add ur_event_handle_t to cache.
   void addEventToContextCache(ur_event_handle_t);
 
+  enum EventCacheType {
+    HostVisibleProfilingCacheType,
+    HostVisibleRegularCacheType,
+    HostInvisibleProfilingCacheType,
+    HostInvisibleRegularCacheType,
+    CounterBasedImmediateCacheType,
+    CounterBasedRegularCacheType
+  };
+
   enum ZeEventPoolCacheType {
     HostVisibleCacheType,
     HostInvisibleCacheType,
@@ -226,14 +235,6 @@ struct ur_context_handle_t_ : _ur_object {
     HostInvisibleCounterBasedImmediateCacheType
   };
 
-  enum EventCacheType {
-    HostVisibleProfilingCacheType,
-    HostVisibleRegularCacheType,
-    HostInvisibleProfilingCacheType,
-    HostInvisibleRegularCacheType,
-    CounterBasedImmediateCacheType,
-    CounterBasedRegularCacheType
-  };
 
   std::list<ze_event_pool_handle_t> *
   getZeEventPoolCache(bool HostVisible, bool WithProfiling,
