5f183613f302feaaadc412f50a0d7bee0b94c5a1
Garcia Orozco, David <david.garcia.orozco@intel.com>
2024-07-24 12:42:18 -0700

    add -Wno-error to test failing on hip

--- sycl/test-e2e/Scheduler/HostAccDestruction.cpp
+++ sycl/test-e2e/Scheduler/HostAccDestruction.cpp
@@ -1,4 +1,4 @@
-// RUN: %{build} -fsycl-dead-args-optimization -o %t.out
+// RUN: %{build} -Wno-error=unused-command-line-argument -fsycl-dead-args-optimization -o %t.out
 // RUN: env SYCL_PI_TRACE=2 %{run} %t.out 2>&1 | FileCheck %s
 
 // Windows doesn't yet have full shutdown().
