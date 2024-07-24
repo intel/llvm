0a6b353e4cc2e73047360c26233e3a110df66cb6
Alexey Sachkov <alexey.sachkov@intel.com>
2024-07-24 23:03:52 +0200

    Apply suggestions from code review

--- sycl/source/detail/compiler.hpp
+++ sycl/source/detail/compiler.hpp
@@ -1,4 +1,4 @@
-//==--------- compiler.h - Interface between compiler and runtime ----------==//
+//==-------- compiler.hpp - Interface between compiler and runtime ---------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
