6d4bcce102c1905578676f3146ee2d23a29d896f
Ejjeh, Adel <adel.ejjeh@intel.com>
2024-07-25 12:10:33 -0700

    Replace sycl.hpp include in e2e tests

--- sycl/test-e2e/TaskSequence/common.hpp
+++ sycl/test-e2e/TaskSequence/common.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
-#include <sycl/sycl.hpp>
+#include <sycl/detail/core.hpp>
+#include <sycl/ext/intel/experimental/pipe_properties.hpp>
+#include <sycl/ext/intel/experimental/pipes.hpp>
+#include <sycl/ext/intel/experimental/task_sequence.hpp>
+#include <sycl/ext/intel/experimental/task_sequence_properties.hpp>
 
 using namespace sycl;
 using namespace sycl::ext::oneapi::experimental;
