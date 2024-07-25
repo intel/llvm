6b07ff16c48d51a9cdc33cd3ab57012785b765e4
Sidorov, Dmitry <dmitry.sidorov@intel.com>
2024-07-25 04:46:23 -0700

    [SYCL] Disable SPV_INTEL_token_type extension
    It's not used right now.
    
    Signed-off-by: Sidorov, Dmitry <dmitry.sidorov@intel.com>

--- clang/test/Driver/sycl-spirv-ext-old-model.c
+++ clang/test/Driver/sycl-spirv-ext-old-model.c
@@ -51,7 +51,6 @@
 // CHECK-DEFAULT-SAME:,+SPV_INTEL_fpga_latency_control
 // CHECK-DEFAULT-SAME:,+SPV_KHR_shader_clock
 // CHECK-DEFAULT-SAME:,+SPV_INTEL_bindless_images
-// CHECK-DEFAULT-SAME:,+SPV_INTEL_token_type
 // CHECK-DEFAULT-SAME:,+SPV_INTEL_bfloat16_conversion
 // CHECK-DEFAULT-SAME:,+SPV_INTEL_joint_matrix
 // CHECK-DEFAULT-SAME:,+SPV_INTEL_hw_thread_queries
@@ -114,7 +113,6 @@
 // CHECK-CPU-SAME:,+SPV_INTEL_fpga_argument_interfaces
 // CHECK-CPU-SAME:,+SPV_INTEL_fpga_invocation_pipelining_attributes
 // CHECK-CPU-SAME:,+SPV_INTEL_fpga_latency_control
-// CHECK-CPU-SAME:,+SPV_INTEL_token_type
 // CHECK-CPU-SAME:,+SPV_INTEL_bfloat16_conversion
 // CHECK-CPU-SAME:,+SPV_INTEL_joint_matrix
 // CHECK-CPU-SAME:,+SPV_INTEL_hw_thread_queries
