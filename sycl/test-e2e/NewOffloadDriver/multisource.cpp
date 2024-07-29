b93d3379b877e78ddc9b3a304e801b400c741a6f
Garcia Orozco, David <david.garcia.orozco@intel.com>
2024-07-29 11:38:00 -0700

    Add -Wno-error flag missed in previous commit

--- sycl/test-e2e/NewOffloadDriver/multisource.cpp
+++ sycl/test-e2e/NewOffloadDriver/multisource.cpp
@@ -18,7 +18,7 @@
 // RUN: %{build} --offload-new-driver -c -o %t.init.o -DINIT_KERNEL
 // RUN: %{build} --offload-new-driver -c -o %t.calc.o -DCALC_KERNEL
 // RUN: %{build} --offload-new-driver -c -o %t.main.o -DMAIN_APP
-// RUN: %clangxx -fsycl -fsycl-targets=%{sycl_triple} --offload-new-driver %t.init.o %t.calc.o %t.main.o -o %t.fat
+// RUN: %clangxx -Wno-error=unused-command-line-argument -fsycl -fsycl-targets=%{sycl_triple} --offload-new-driver %t.init.o %t.calc.o %t.main.o -o %t.fat
 // RUN: %{run} %t.fat
 
 #include <sycl/detail/core.hpp>
