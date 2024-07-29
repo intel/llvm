30c37cd07eb099aa11a2e17261b754c901bdd344
Igor Chorążewicz <igor.chorazewicz@intel.com>
2024-07-29 06:21:11 -0700

    [UR] disable hwloc support for UMF (#14812)
    Hwloc package is not avalable on certain distros (e.g. RHEL). To avoid
    forcing users to install hwloc from sources, disable hwloc in UMF.
    
    Based on:
    https://github.com/oneapi-src/unified-memory-framework/pull/642
    
    ---------
    
    Co-authored-by: Piotr Balcer <piotr.balcer@intel.com>
    Co-authored-by: Dmitry Vodopyanov <dmitry.vodopyanov@intel.com>

--- sycl/cmake/modules/FetchUnifiedRuntime.cmake
+++ sycl/cmake/modules/FetchUnifiedRuntime.cmake
@@ -23,6 +23,9 @@ option(SYCL_PI_UR_USE_FETCH_CONTENT
 set(SYCL_PI_UR_SOURCE_DIR
   "" CACHE PATH "Path to root of Unified Runtime repository")
 
+option(SYCL_UMF_DISABLE_HWLOC
+  "Disable hwloc support in UMF" ON)
+
 # Here we override the defaults to disable building tests from unified-runtime
 set(UR_BUILD_EXAMPLES OFF CACHE BOOL "Build example applications." FORCE)
 set(UR_BUILD_TESTS OFF CACHE BOOL "Build unit tests." FORCE)
@@ -127,6 +130,8 @@ if(SYCL_PI_UR_USE_FETCH_CONTENT)
   if(WIN32)
     set(UMF_BUILD_SHARED_LIBRARY OFF CACHE INTERNAL "Build UMF shared library")
     set(UMF_LINK_HWLOC_STATICALLY ON CACHE INTERNAL "static HWLOC")
+  else()
+    set(UMF_DISABLE_HWLOC ${SYCL_UMF_DISABLE_HWLOC} CACHE INTERNAL "Disable hwloc for UMF")
   endif()
 
   fetch_adapter_source(level_zero
