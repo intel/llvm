607f10058f06e2ff960d4f70d26338b44bb4b3b9
Ross Brunton <ross@codeplay.com>
2024-08-06 14:25:38 +0100

    [XPTI] Rename `ur` to `ur.call` in XPTI

--- sycl/cmake/modules/FetchUnifiedRuntime.cmake
+++ sycl/cmake/modules/FetchUnifiedRuntime.cmake
@@ -117,13 +117,13 @@ if(SYCL_UR_USE_FETCH_CONTENT)
   endfunction()
 
   set(UNIFIED_RUNTIME_REPO "https://github.com/oneapi-src/unified-runtime.git")
-  # commit cabf128094eff9ff7b79bdff559640a8a111f0c3
-  # Merge: a96fcbc5 15bca3b6
+  # commit d0a50523006fa6f283da6a36811081add3bb22fc
+  # Merge: 804851e4 04deb8b3
   # Author: Omar Ahmed <omar.ahmed@codeplay.com>
-  # Date:   Mon Aug 19 16:20:45 2024 +0100
-  #     Merge pull request #1984 from rafbiels/rafbiels/cuda-stream-race-cond
-  #     Fix race condition in CUDA stream creation
-  set(UNIFIED_RUNTIME_TAG cabf128094eff9ff7b79bdff559640a8a111f0c3)
+  # Date:   Tue Aug 20 16:28:30 2024 +0100
+  #    Merge pull request #1940 from RossBrunton/ross/urcall
+  #    [XPTI] Use `ur.call` rather than `ur` in XPTI
+  set(UNIFIED_RUNTIME_TAG d0a50523006fa6f283da6a36811081add3bb22fc)
 
   set(UMF_BUILD_EXAMPLES OFF CACHE INTERNAL "EXAMPLES")
   # Due to the use of dependentloadflag and no installer for UMF and hwloc we need
