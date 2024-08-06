a3c859ebc3219fe20bc04cb34e93867185504ba1
Wu, Yingcong <yingcong.wu@intel.com>
2024-08-06 10:25:25 +0800

    exclude shadow from coredump

--- source/loader/layers/sanitizer/common.hpp
+++ source/loader/layers/sanitizer/common.hpp
@@ -142,6 +142,7 @@ bool IsInASanContext();
 
 uptr MmapNoReserve(uptr Addr, uptr Size);
 bool Munmap(uptr Addr, uptr Size);
+bool DontCoredumpRange(uptr Addr, uptr Size);
 
 void *GetMemFunctionPointer(const char *);
 
