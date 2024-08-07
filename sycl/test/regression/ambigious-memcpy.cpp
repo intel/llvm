87f8f3ea47d9c9ec5ea65471fca9bb168f2b262f
Agarwal, Udit <udit.agarwal@intel.com>
2024-08-07 15:24:55 -0700

    Address feedback

--- sycl/test/regression/ambigious-memcpy.cpp
+++ sycl/test/regression/ambigious-memcpy.cpp
@@ -1,16 +1,17 @@
-// detail::memcpy, even though in a different namespace, can cause ambiguity with libc's memcpy, due to argument dependent lookup (ADL). This compile-only test checks for ambigiouity in call to detail::memcpy.
+// detail::memcpy, even though in a different namespace, can cause ambiguity
+// with libc's memcpy, due to argument dependent lookup (ADL). This compile-only
+// test checks for ambigiouity in call to detail::memcpy.
 
-// RUN: %clang -fsycl -fsycl-device-only %s -o %s.out
+// RUN: %clang -fsycl -fsycl-device-only -fsyntax-only %s
 
 #include <sycl/vector.hpp>
 
-template <typename T>
-void foo(T *dst, T *src, size_t count) {
-	          memcpy(dst, src, count * sizeof(T));
+template <typename T> void foo(T *dst, T *src, size_t count) {
+  memcpy(dst, src, count * sizeof(T));
 }
 
 using T = sycl::vec<int, 1>;
 
 SYCL_EXTERNAL void bar(T *dst, T *src, size_t count) {
-	          foo(dst, src, count * sizeof(T));
+  foo(dst, src, count * sizeof(T));
 }
