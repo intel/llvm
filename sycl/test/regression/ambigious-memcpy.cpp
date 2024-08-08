1e5a4a8cb2939926ab8c045f8be7150034d3f625
Udit Agarwal <udit.agarwal@intel.com>
2024-08-08 06:55:55 -0700

    Address reviews
    Co-authored-by: Steffen Larsen <steffen.larsen@intel.com>

--- sycl/test/regression/ambigious-memcpy.cpp
+++ sycl/test/regression/ambigious-memcpy.cpp
@@ -1,8 +1,8 @@
+// RUN: %clang -fsycl -fsycl-device-only -fsyntax-only %s
+
 // detail::memcpy, even though in a different namespace, can cause ambiguity
 // with libc's memcpy, due to argument dependent lookup (ADL). This compile-only
-// test checks for ambigiouity in call to detail::memcpy.
-
-// RUN: %clang -fsycl -fsycl-device-only -fsyntax-only %s
+// test checks for ambiguity in call to detail::memcpy.
 
 #include <sycl/vector.hpp>
 
