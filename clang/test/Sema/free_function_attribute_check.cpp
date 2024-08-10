af526939d4697e4169f72d01337ac7cc05dcf289
Rajiv Deodhar <rajiv.deodhar@intel.com>
2024-08-10 10:28:30 -0700

    Comment correction.

--- clang/test/Sema/free_function_attribute_check.cpp
+++ clang/test/Sema/free_function_attribute_check.cpp
@@ -1,4 +1,4 @@
-//==---- free_function_implicit_sycl_extern.cpp ----------------------------==//
+//==---- free_function_attribute_check.cpp ---------------------------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
