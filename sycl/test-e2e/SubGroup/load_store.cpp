47a0be815eb8a37833b5e6144b3b88c6ca5bc416
Martin Grant <martin.morrisongrant@codeplay.com>
2024-08-12 11:14:17 +0100

    [SYCL][E2E] Re-enable test-e2e/SubGroup/load_store.cpp (#15010)
    Fixes #15006
    
    This test passes against all adapters both on my own machine and Github
    CI and should now be re-enabled.

--- sycl/test-e2e/SubGroup/load_store.cpp
+++ sycl/test-e2e/SubGroup/load_store.cpp
@@ -1,8 +1,6 @@
 // RUN: %{build} -fsycl-device-code-split=per_kernel -o %t.out
 // RUN: %{run} %t.out
-//
-// TODO: Flaky reenable, see https://github.com/intel/llvm/issues/14765
-// UNSUPPORTED: windows, linux
+
 //
 //==----------- load_store.cpp - SYCL sub_group load/store test ------------==//
 //
