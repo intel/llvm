79a6f1ed011f4b116a4caed0e992458280796804
PietroGhg <pietro.ghiglio@codeplay.com>
2024-08-13 08:42:54 +0100

    Update UR tag

--- sycl/cmake/modules/FetchUnifiedRuntime.cmake
+++ sycl/cmake/modules/FetchUnifiedRuntime.cmake
@@ -156,8 +156,8 @@ if(SYCL_UR_USE_FETCH_CONTENT)
   )
 
   fetch_adapter_source(native_cpu
-    ${UNIFIED_RUNTIME_REPO}
-    ${UNIFIED_RUNTIME_TAG}
+    "https://github.com/PietroGhg/unified-runtime.git"
+    pietro/lock_stop_thread
   )
 
   if(SYCL_PI_UR_OVERRIDE_FETCH_CONTENT_REPO)
