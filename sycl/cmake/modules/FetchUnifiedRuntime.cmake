1ebfd89926433fbeaff941476fc81c4d54466b83
Neil R. Spruit <neil.r.spruit@intel.com>
2024-08-13 09:01:48 -0700

    [UR][L0] fix Coverity issues for L0 Adapter (#15034)
    -pre-commit PR for:
    https://github.com/oneapi-src/unified-runtime/pull/1955
    
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>

--- sycl/cmake/modules/FetchUnifiedRuntime.cmake
+++ sycl/cmake/modules/FetchUnifiedRuntime.cmake
@@ -117,13 +117,13 @@ if(SYCL_UR_USE_FETCH_CONTENT)
   endfunction()
 
   set(UNIFIED_RUNTIME_REPO "https://github.com/oneapi-src/unified-runtime.git")
-  # commit 26f1dfc0b187763bc739537bdd0d8c76f32bd48e
-  # Merge: 89dc3a56 2dbe0800
+  # commit f8336050f43a6529636fd6b2075dfe09961564f5
+  # Merge: 6c98e0e8 b75f2bc2
   # Author: Omar Ahmed <omar.ahmed@codeplay.com>
-  # Date:   Tue Aug 13 11:12:55 2024 +0100
-  #     Merge pull request #1960 from Bensuo/ewan/imm_command_list_fix
-  #     Command-buffer immediate command-list CTS fix
-  set(UNIFIED_RUNTIME_TAG 26f1dfc0b187763bc739537bdd0d8c76f32bd48e)
+  # Date:   Tue Aug 13 13:07:23 2024 +0100
+  #     Merge pull request #1955 from nrspruit/fix_l0_coverity
+  #     [L0] fix Coverity issues for L0 Adapter
+  set(UNIFIED_RUNTIME_TAG f8336050f43a6529636fd6b2075dfe09961564f5)
 
   set(UMF_BUILD_EXAMPLES OFF CACHE INTERNAL "EXAMPLES")
   # Due to the use of dependentloadflag and no installer for UMF and hwloc we need
