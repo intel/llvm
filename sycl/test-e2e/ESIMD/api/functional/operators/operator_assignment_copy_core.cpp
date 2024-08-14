70425800319b571410051ab32e20f8e5e9b02775
fineg74 <61437305+fineg74@users.noreply.github.com>
2024-08-14 11:35:56 -0700

    [SYCL][ESIMD] Remove simd move test cases (#15062)
    This PR to remove tests requiring simd move constructor/assignment as it
    was decided not to implement it.
    
    Closes: https://github.com/intel/llvm/issues/14542

--- sycl/test-e2e/ESIMD/api/functional/operators/operator_assignment_copy_core.cpp
+++ sycl/test-e2e/ESIMD/api/functional/operators/operator_assignment_copy_core.cpp
@@ -8,11 +8,9 @@
 //===----------------------------------------------------------------------===//
 // RUN: %{build} -fsycl-device-code-split=per_kernel -o %t.out
 // RUN: %{run} %t.out
-// XFAIL: *
-// TODO Remove XFAIL once the simd vector provides move assignment operator
 //
-// Test for simd move and copy assignment operators.
-// The test creates source simd instance with reference data and invokes move or
+// Test for simd copy assignment operators.
+// The test creates source simd instance with reference data and invokes
 // copy assignment operator from source to destination simd instance. It is
 // expected for destination simd instance to store a bitwise same data as the
 // reference one.
@@ -21,23 +19,6 @@
 
 using namespace sycl::ext::intel::esimd;
 using namespace esimd_test::api::functional;
-
-// Descriptor class for the case of calling move assignment operator.
-struct move_assignment {
-  static std::string get_description() { return "move assignment operator"; }
-
-  static constexpr bool is_move_expected() { return true; }
-
-  template <typename DataT, int NumElems>
-  static bool run(const DataT *const ref_data, DataT *const out) {
-    simd<DataT, NumElems> source_simd;
-    source_simd.copy_from(ref_data);
-    simd<DataT, NumElems> simd_obj;
-    simd_obj = std::move(source_simd);
-    simd_obj.copy_to(out);
-    return simd_obj.get_test_proxy().was_move_destination();
-  }
-};
 
 // Descriptor class for the case of calling copy assignment operator.
 struct copy_assignment {
@@ -52,7 +33,7 @@ struct copy_assignment {
     simd<DataT, NumElems> simd_obj;
     simd_obj = source_simd;
     simd_obj.copy_to(out);
-    return simd_obj.get_test_proxy().was_move_destination();
+    return false;
   }
 };
 
@@ -62,11 +43,10 @@ int main(int, char **) {
 
   bool passed = true;
 
-  const auto types = get_tested_types<tested_types::all>();
+  const auto types = get_tested_types<tested_types::core>();
   const auto all_sizes = get_all_sizes();
 
-  const auto context =
-      unnamed_type_pack<move_assignment, copy_assignment>::generate();
+  const auto context = unnamed_type_pack<copy_assignment>::generate();
 
   passed &= for_all_combinations<operators::run_test>(types, all_sizes, context,
                                                       queue);
