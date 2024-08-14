f28c4fb1cfe0ef2cc80d001ca74f63587e3d3826
Uwe Dolinsky <uwe@codeplay.com>
2024-08-14 11:04:36 +0100

    [SYCLNATIVECPU] removed check for clang-rt

--- clang/test/Driver/sycl-native-cpu.cpp
+++ clang/test/Driver/sycl-native-cpu.cpp
@@ -27,6 +27,6 @@
 
 // Checking that coverage testing options are accepted by native_cpu, and that device and host compilation invocations receive the same options
 // RUN: %clangxx -fsycl -fsycl-targets=native_cpu -Werror -fno-profile-instr-generate -fprofile-instr-generate -fno-coverage-mapping -fcoverage-mapping -### %s 2>&1 | FileCheck %s --check-prefix=CHECK_COV_INVO
-// CHECK_COV_INVO:{{.*}}clang{{.*}}-fsycl-is-device{{.*}}"-fsycl-is-native-cpu" "-D" "__SYCL_NATIVE_CPU__"{{.*}}"-fprofile-instrument=clang"{{.*}}"--dependent-lib=clang_rt{{.*}}"{{.*}}"-fcoverage-mapping" "-fcoverage-compilation-dir={{.*}}"
-// CHECK_COV_INVO:{{.*}}clang{{.*}}"-fsycl-is-host"{{.*}}"-fprofile-instrument=clang"{{.*}}"--dependent-lib=clang_rt{{.*}}"{{.*}}"-fcoverage-mapping" "-fcoverage-compilation-dir={{.*}}"
+// CHECK_COV_INVO:{{.*}}clang{{.*}}-fsycl-is-device{{.*}}"-fsycl-is-native-cpu" "-D" "__SYCL_NATIVE_CPU__"{{.*}}"-fprofile-instrument=clang"{{.*}}"-fcoverage-mapping" "-fcoverage-compilation-dir={{.*}}"
+// CHECK_COV_INVO:{{.*}}clang{{.*}}"-fsycl-is-host"{{.*}}"-fprofile-instrument=clang"{{.*}}"-fcoverage-mapping" "-fcoverage-compilation-dir={{.*}}"
 
