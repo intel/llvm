087c01c45520fc62d6e3f0906d3e5b34e236d15c
omarahmed1111 <omar.ahmed@codeplay.com>
2024-08-16 10:58:33 +0100

    testing- fix coverity issues

--- sycl/cmake/modules/FetchUnifiedRuntime.cmake
+++ sycl/cmake/modules/FetchUnifiedRuntime.cmake
@@ -116,14 +116,14 @@ if(SYCL_UR_USE_FETCH_CONTENT)
       CACHE PATH "Path to external '${name}' adapter source dir" FORCE)
   endfunction()
 
-  set(UNIFIED_RUNTIME_REPO "https://github.com/oneapi-src/unified-runtime.git")
+  set(UNIFIED_RUNTIME_REPO "https://github.com/omarahmed1111/unified-runtime.git")
   # commit d8f1c98e48e98ea2f6a227af82366734fcde977e
   # Merge: 6e8efa3d 9e824480
   # Author: Omar Ahmed <omar.ahmed@codeplay.com>
   # Date:   Wed Aug 14 11:46:59 2024 +0100
   #     Merge pull request #1946 from callumfare/callum/update_ur_trace_env_var
   #     Update expected values of SYCL_UR_TRACE environment variable
-  set(UNIFIED_RUNTIME_TAG d8f1c98e48e98ea2f6a227af82366734fcde977e)
+  set(UNIFIED_RUNTIME_TAG e9c705d9ef13918e18e96b8d514607abcefda235)
 
   set(UMF_BUILD_EXAMPLES OFF CACHE INTERNAL "EXAMPLES")
   # Due to the use of dependentloadflag and no installer for UMF and hwloc we need
