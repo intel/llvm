ffae8c0c1e5d91c4dc6300b6e863f4b65c179e31
Hugh Delaney <hugh.delaney@codeplay.com>
2024-08-21 10:56:38 +0100

    Make external linkage only apply to AMDGCN/NVPTX
    Also make sure that the device global is only considered externally
    initialized on the device compilation pass.

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -6235,7 +6235,7 @@ void CodeGenModule::EmitGlobalVarDefinition(const VarDecl *D,
     getCUDARuntime().handleVarRegistration(D, *GV);
   }
 
-  if (LangOpts.isSYCL()) {
+  if (LangOpts.SYCLIsDevice) {
     const RecordDecl *RD = D->getType()->getAsRecordDecl();
     if (RD && RD->hasAttr<SYCLDeviceGlobalAttr>()) {
       // SYCL device globals are initialized externally
@@ -6243,7 +6243,8 @@ void CodeGenModule::EmitGlobalVarDefinition(const VarDecl *D,
       // Since static device global symbols need to cross host device boundary,
       // don't allow internal linkage as it might tamper with the device global
       // symbol
-      if (Linkage == llvm::GlobalValue::InternalLinkage)
+      if (Linkage == llvm::GlobalValue::InternalLinkage &&
+          (getTriple().isAMDGCN() || getTriple().isNVPTX()))
         Linkage = llvm::GlobalValue::ExternalLinkage;
     }
   }
