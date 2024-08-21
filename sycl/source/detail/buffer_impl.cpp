intel/llvm

commit 3dc75a75b624ae2d3fd2e2335a7c6ee054b75366

[SYCL] Host device & queue removal (internal part, not breaking ABI)  (#14370)
Host device support is deprecated long time ago. Although our internal
host task & host accessor implementation was still using it.
This change eliminates it and remove possibility to create host
queue/context and device. This brings the following changes:

- Commands & Events could not guarantee queried Context != nullptr and
Queue!= nullptr since for host task stuff no device queue/context is
involved. For host task we have submitted queue instance stored in event
to be able to report exceptions to user and to be able to properly
handle dependencies. Submitted queue for host task is guaranteed to be
not null.
- Connection command for cross context dependencies is now attached to
the queue of new command (dependency for which is being analyzed).
Previously it was also related to host queue only. No perf impact is
expected.
- Stream flush command is now submitted to the same queue as
corresponding kernel (previously it was submitted to the host queue).
This could bring negative perf impact for stream usage with in-order
queue but stream is not perf oriented feature.
ABI breaking changes to remove is_host methods and some SYCL_EXTERN
stuff will be submitted separately.

---------

Signed-off-by: Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>

sycl/source/detail/buffer_impl.cpp
@@ -24,8 +24,7 @@ void *buffer_impl::allocateMem(ContextImplPtr Context, bool InitFromUserData,
                                sycl::detail::pi::PiEvent &OutEventToWait) {
   bool HostPtrReadOnly = false;
   BaseT::determineHostPtr(Context, InitFromUserData, HostPtr, HostPtrReadOnly);
-
-  assert(!(nullptr == HostPtr && BaseT::useHostPtr() && Context->is_host()) &&
+  assert(!(nullptr == HostPtr && BaseT::useHostPtr() && !Context) &&
          "Internal error. Allocating memory on the host "
          "while having use_host_ptr property");
   return MemoryManager::allocateMemBuffer(
@@ -71,10 +70,13 @@ buffer_impl::getNativeVector(backend BackendName) const {
     sycl::detail::pi::PiMem NativeMem =
         pi::cast<sycl::detail::pi::PiMem>(Cmd->getMemAllocation());
     auto Ctx = Cmd->getWorkerContext();
-    auto Platform = Ctx->getPlatformImpl();
     // If Host Shared Memory is not supported then there is alloca for host that
-    // doesn't have platform
-    if (!Platform || (Platform->getBackend() != BackendName))
+    // doesn't have context and platform
+    if (!Ctx)
+      continue;
+    PlatformImplPtr Platform = Ctx->getPlatformImpl();
+    assert(Platform && "Platform must be present for device context");
+    if (Platform->getBackend() != BackendName)
       continue;
 
     auto Plugin = Platform->getPlugin();

sycl/source/detail/image_impl.cpp
@@ -471,6 +471,8 @@ bool image_impl::checkImageFormat(
 }
 
 std::vector<device> image_impl::getDevices(const ContextImplPtr Context) {
+  if (!Context)
+    return {};
   return Context->get_info<info::context::devices>();
 }
 

sycl/source/detail/sycl_mem_obj_t.cpp
@@ -33,12 +33,6 @@ SYCLMemObjT::SYCLMemObjT(pi_native_handle MemObject, const context &SyclContext,
       MUserPtr(nullptr), MShadowCopy(nullptr), MUploadDataFunctor(nullptr),
       MSharedPtrStorage(nullptr), MHostPtrProvided(true),
       MOwnNativeHandle(OwnNativeHandle) {
-  if (MInteropContext->is_host())
-    throw sycl::invalid_parameter_error(
-        "Creation of interoperability memory object using host context is "
-        "not allowed",
-        PI_ERROR_INVALID_CONTEXT);
-
   sycl::detail::pi::PiContext Context = nullptr;
   const PluginPtr &Plugin = getPlugin();
 
@@ -84,12 +78,6 @@ SYCLMemObjT::SYCLMemObjT(pi_native_handle MemObject, const context &SyclContext,
       MUserPtr(nullptr), MShadowCopy(nullptr), MUploadDataFunctor(nullptr),
       MSharedPtrStorage(nullptr), MHostPtrProvided(true),
       MOwnNativeHandle(OwnNativeHandle) {
-  if (MInteropContext->is_host())
-    throw sycl::invalid_parameter_error(
-        "Creation of interoperability memory object using host context is "
-        "not allowed",
-        PI_ERROR_INVALID_CONTEXT);
-
   sycl::detail::pi::PiContext Context = nullptr;
   const PluginPtr &Plugin = getPlugin();
 
@@ -191,19 +179,12 @@ void SYCLMemObjT::determineHostPtr(const ContextImplPtr &Context,
   // The data for the allocation can be provided via either the user pointer
   // (InitFromUserData, can be read-only) or a runtime-allocated read-write
   // HostPtr. We can have one of these scenarios:
-  // 1. The allocation is the first one and on host. InitFromUserData == true.
-  // 2. The allocation is the first one and isn't on host. InitFromUserData
+  // 1. The allocation is the first one and isn't on host. InitFromUserData
   // varies based on unified host memory support and whether or not the data can
   // be discarded.
-  // 3. The allocation is not the first one and is on host. InitFromUserData ==
-  // false, HostPtr == nullptr. This can only happen if the allocation command
-  // is not linked since it would be a no-op otherwise. Attempt to reuse the
-  // user pointer if it's read-write, but do not copy its contents if it's not.
-  // 4. The allocation is not the first one and not on host. InitFromUserData ==
+  // 2. The allocation is not the first one and not on host. InitFromUserData ==
   // false, HostPtr is provided if the command is linked. The host pointer is
   // guaranteed to be reused in this case.
-  if (Context->is_host() && !MOpenCLInterop && !MHostPtrReadOnly)
-    InitFromUserData = true;
 
   if (InitFromUserData) {
     assert(!HostPtr && "Cannot init from user data and reuse host ptr provided "
@@ -228,8 +209,9 @@ void SYCLMemObjT::detachMemoryObject(
       !MOwnNativeHandle ||
       (MInteropContext && !MInteropContext->isOwnedByRuntime());
 
-  if (MRecord && MRecord->MCurContext->isOwnedByRuntime() &&
-      !InteropObjectsUsed && (!MHostPtrProvided || MIsInternal)) {
+  if (MRecord && MRecord->MCurContext &&
+      MRecord->MCurContext->isOwnedByRuntime() && !InteropObjectsUsed &&
+      (!MHostPtrProvided || MIsInternal)) {
     bool okToDefer = GlobalHandler::instance().isOkToDefer();
     if (okToDefer)
       Scheduler::getInstance().deferMemObjRelease(Self);

commit 00147592d8d519e35f0bc873fac888c1d58110dd

[SYCL] Fix regression in update_host (#15153)
Restores missed context analysis for host pointer update.
Regression is caused by
https://github.com/intel/llvm/commit/3dc75a75b624ae2d3fd2e2335a7c6ee054b75366

---------

Signed-off-by: Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>

sycl/source/detail/buffer_impl.cpp
@@ -24,7 +24,7 @@ void *buffer_impl::allocateMem(ContextImplPtr Context, bool InitFromUserData,
                                void *HostPtr,
                                ur_event_handle_t &OutEventToWait) {
   bool HostPtrReadOnly = false;
-  BaseT::determineHostPtr(InitFromUserData, HostPtr, HostPtrReadOnly);
+  BaseT::determineHostPtr(Context, InitFromUserData, HostPtr, HostPtrReadOnly);
   assert(!(nullptr == HostPtr && BaseT::useHostPtr() && !Context) &&
          "Internal error. Allocating memory on the host "
          "while having use_host_ptr property");

sycl/source/detail/image_impl.cpp
@@ -327,7 +327,7 @@ void *image_impl::allocateMem(ContextImplPtr Context, bool InitFromUserData,
                               void *HostPtr,
                               ur_event_handle_t &OutEventToWait) {
   bool HostPtrReadOnly = false;
-  BaseT::determineHostPtr(InitFromUserData, HostPtr, HostPtrReadOnly);
+  BaseT::determineHostPtr(Context, InitFromUserData, HostPtr, HostPtrReadOnly);
 
   ur_image_desc_t Desc = getImageDesc(HostPtr != nullptr);
   assert(checkImageDesc(Desc, Context, HostPtr) &&

sycl/source/detail/sycl_mem_obj_t.cpp
@@ -107,7 +107,8 @@ SYCLMemObjT::SYCLMemObjT(ur_native_handle_t MemObject,
                sizeof(Context), &Context, nullptr);
 
   if (MInteropContext->getHandleRef() != Context)
-    throw sycl::exception(make_error_code(errc::invalid),
+    throw sycl::exception(
+        make_error_code(errc::invalid),
         "Input context must be the same as the context of cl_mem");
 
   if (MInteropContext->getBackend() == backend::opencl)
@@ -175,7 +176,8 @@ size_t SYCLMemObjT::getBufSizeForContext(const ContextImplPtr &Context,
 
 bool SYCLMemObjT::isInterop() const { return MOpenCLInterop; }
 
-void SYCLMemObjT::determineHostPtr(bool InitFromUserData, void *&HostPtr,
+void SYCLMemObjT::determineHostPtr(const ContextImplPtr &Context,
+                                   bool InitFromUserData, void *&HostPtr,
                                    bool &HostPtrReadOnly) {
   // The data for the allocation can be provided via either the user pointer
   // (InitFromUserData, can be read-only) or a runtime-allocated read-write
@@ -186,6 +188,8 @@ void SYCLMemObjT::determineHostPtr(bool InitFromUserData, void *&HostPtr,
   // 2. The allocation is not the first one and not on host. InitFromUserData ==
   // false, HostPtr is provided if the command is linked. The host pointer is
   // guaranteed to be reused in this case.
+  if (!Context && !MOpenCLInterop && !MHostPtrReadOnly)
+    InitFromUserData = true;
 
   if (InitFromUserData) {
     assert(!HostPtr && "Cannot init from user data and reuse host ptr provided "

sycl/source/detail/sycl_mem_obj_t.hpp
@@ -327,8 +327,8 @@ public:
  
 protected:
   // An allocateMem helper that determines which host ptr to use
-  void determineHostPtr(bool InitFromUserData, void *&HostPtr,
-                        bool &HostPtrReadOnly);
+  void determineHostPtr(const ContextImplPtr &Context, bool InitFromUserData,
+                        void *&HostPtr, bool &HostPtrReadOnly);
 
   // Allocator used for allocation memory on host.
   std::unique_ptr<SYCLMemObjAllocator> MAllocator;