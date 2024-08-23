86a22d8913c02f4b2c6ff6e0ee83f712adc9016f
premanandrao <premanand.m.rao@intel.com>
2024-08-23 14:28:58 -0400

    [SYCL] Disable clang pedantic diagnostics in header/footer (#15171)
    When -Wreserved-identifer or -Wold-style-cast is enabled, some of the
    constructs used in
    integration headers and footers for device_globals and host_pipes
    trigger a diagnostic.
    This change adds a pragma to the generated files that suppress the
    diagnostic prior to
    the construct and reenabled the diagnostic after.

--- clang/lib/Sema/SemaSYCL.cpp
+++ clang/lib/Sema/SemaSYCL.cpp
@@ -6047,6 +6047,23 @@ static void OutputStableNameInChars(raw_ostream &O, StringRef Name) {
   }
 }
 
+static void EmitPragmaDiagnosticPush(raw_ostream &O, StringRef DiagName) {
+  O << "\n";
+  O << "#ifdef __clang__\n";
+  O << "#pragma clang diagnostic push\n";
+  O << "#pragma clang diagnostic ignored \"" << DiagName.str() << "\"\n";
+  O << "#endif // defined(__clang__)\n";
+  O << "\n";
+}
+
+static void EmitPragmaDiagnosticPop(raw_ostream &O) {
+  O << "\n";
+  O << "#ifdef __clang__\n";
+  O << "#pragma clang diagnostic pop\n";
+  O << "#endif // defined(__clang__)\n";
+  O << "\n";
+}
+
 void SYCLIntegrationHeader::emit(raw_ostream &O) {
   O << "// This is auto-generated SYCL integration header.\n";
   O << "\n";
@@ -6145,6 +6162,9 @@ void SYCLIntegrationHeader::emit(raw_ostream &O) {
   // main() function.
 
   if (NeedToEmitDeviceGlobalRegistration) {
+    // Supress the reserved identifier diagnostic that clang generates
+    // for the construct below.
+    EmitPragmaDiagnosticPush(O, "-Wreserved-identifier");
     O << "namespace {\n";
 
     O << "class __sycl_device_global_registration {\n";
@@ -6156,12 +6176,16 @@ void SYCLIntegrationHeader::emit(raw_ostream &O) {
     O << "} // namespace\n";
 
     O << "\n";
+    EmitPragmaDiagnosticPop(O);
   }
 
   // Generate declaration of variable of type __sycl_host_pipe_registration
   // whose sole purpose is to run its constructor before the application's
   // main() function.
   if (NeedToEmitHostPipeRegistration) {
+    // Supress the reserved identifier diagnostic that clang generates
+    // for the construct below.
+    EmitPragmaDiagnosticPush(O, "-Wreserved-identifier");
     O << "namespace {\n";
 
     O << "class __sycl_host_pipe_registration {\n";
@@ -6173,6 +6197,7 @@ void SYCLIntegrationHeader::emit(raw_ostream &O) {
     O << "} // namespace\n";
 
     O << "\n";
+    EmitPragmaDiagnosticPop(O);
   }
 
 
@@ -6741,12 +6766,16 @@ bool SYCLIntegrationFooter::emit(raw_ostream &OS) {
     OS << "#include <sycl/detail/device_global_map.hpp>\n";
     DeviceGlobOS.flush();
     OS << "namespace sycl::detail {\n";
+    // Supress the old-style case diagnostic that clang generates
+    // for the construct below in DeviceGlobalsBuf.
+    EmitPragmaDiagnosticPush(OS, "-Wold-style-cast");
     OS << "namespace {\n";
     OS << "__sycl_device_global_registration::__sycl_device_global_"
           "registration() noexcept {\n";
     OS << DeviceGlobalsBuf;
     OS << "}\n";
     OS << "} // namespace (unnamed)\n";
+    EmitPragmaDiagnosticPop(OS);
     OS << "} // namespace sycl::detail\n";
 
     S.getSyclIntegrationHeader().addDeviceGlobalRegistration();
@@ -6756,12 +6785,16 @@ bool SYCLIntegrationFooter::emit(raw_ostream &OS) {
     OS << "#include <sycl/detail/host_pipe_map.hpp>\n";
     HostPipesOS.flush();
     OS << "namespace sycl::detail {\n";
+    // Supress the old-style case diagnostic that clang generates
+    // for the construct below in HostPipesBuf.
+    EmitPragmaDiagnosticPush(OS, "-Wold-style-cast");
     OS << "namespace {\n";
     OS << "__sycl_host_pipe_registration::__sycl_host_pipe_"
           "registration() noexcept {\n";
     OS << HostPipesBuf;
     OS << "}\n";
     OS << "} // namespace (unnamed)\n";
+    EmitPragmaDiagnosticPop(OS);
     OS << "} // namespace sycl::detail\n";
 
     S.getSyclIntegrationHeader().addHostPipeRegistration();
--- clang/test/CodeGenSYCL/host_pipe_int_footer_header.cpp
+++ clang/test/CodeGenSYCL/host_pipe_int_footer_header.cpp
@@ -23,7 +23,7 @@ void foo() {
 // CHECK-HEADER: namespace sycl {
 // CHECK-HEADER-NEXT: inline namespace _V1 {
 // CHECK-HEADER-NEXT: namespace detail {
-// CHECK-HEADER-NEXT: namespace {
+// CHECK-HEADER: namespace {
 // CHECK-HEADER-NEXT: class __sycl_host_pipe_registration {
 // CHECK-HEADER-NEXT: public:
 // CHECK-HEADER-NEXT: __sycl_host_pipe_registration() noexcept;
@@ -37,7 +37,7 @@ void foo() {
 // CHECK-FOOTER: #include <sycl/detail/defines_elementary.hpp>
 // CHECK-FOOTER: #include <sycl/detail/host_pipe_map.hpp>
 // CHECK-FOOTER-NEXT: namespace sycl::detail {
-// CHECK-FOOTER-NEXT: namespace {
+// CHECK-FOOTER: namespace {
 // CHECK-FOOTER-NEXT: __sycl_host_pipe_registration::__sycl_host_pipe_registration() noexcept {
 
 // CHECK-FOOTER: host_pipe_map::add((void *)&::sycl::ext::intel::experimental::host_pipe<HPInt, int>::__pipe, "THE_PREFIX____ZN4sycl3_V13ext5intel12experimental9host_pipeIZZZ3foovENKUlRNS0_7handlerEE_clES6_ENKUlvE_clEvE5HPIntiE6__pipeE");
