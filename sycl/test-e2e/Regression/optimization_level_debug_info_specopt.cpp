a142ad39c416047b687293147e97b7349394e0b7
Jinsong Ji <jinsong.ji@intel.com>
2024-08-23 07:32:18 -0700

    Use -ffp-model=fast isntead of Ofast

--- sycl/test-e2e/Regression/optimization_level_debug_info_specopt.cpp
+++ sycl/test-e2e/Regression/optimization_level_debug_info_specopt.cpp
@@ -1,4 +1,4 @@
-// RUN: %{build} %debug_option -O3 -ffast-math -o %t.out
+// RUN: %{build} %debug_option -ffp-model=fast -o %t.out
 // RUN: %{build} %debug_option -Os -o %t.out
 // RUN: %{build} %debug_option -Oz -o %t.out
 // RUN: %{build} %debug_option -Og -o %t.out
