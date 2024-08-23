b3b0c42e4810b3b46b9a1f8ad3a590769db4c6b2
Lorenc Bushi <113361374+lbushi25@users.noreply.github.com>
2024-08-23 16:25:35 -0400

    Update device_assert_win.hpp

--- sycl/include/sycl/detail/device_assert_win.hpp
+++ sycl/include/sycl/detail/device_assert_win.hpp
@@ -8,27 +8,32 @@
 
 #pragma once
 
-// Normal workflow for device assertions on Windows GPU does not work as
+// Normal workflow for device assertions on Windows does not work as
 // expected, most likely because the SPV_EXT_relaxed_printf_string_address_space
 // extension is not supported by IGC More specifically, the _wassert function
 // seems to allocate the file and expression buffers on private memory and not
 // constant memory which catches IGC by surprise. Therefore, we define this
 // header file to explicitly redefine assert on Windows so that it directly
 // calls __devicelib_assert_fail and not _wassert.
+// Unfortunately, for this workaround to work, it must not be succeeded 
+// by an include of <cassert> or <assert.h> because these two headers
+// do not have include guards and will redefine the assert macro.
 // TODO: Delete this header file and its inclusion in <sycl/sycl.hpp> once the
 // extension is supported by IGC.
 
 #ifdef __SYCL_DEVICE_ONLY__
 #include <CL/__spirv/spirv_vars.hpp>
 #include <cassert>
-#endif
 
-#if defined(_WIN32) && defined(__SYCL_DEVICE_ONLY__) && defined(assert)
+#if defined(_WIN32) && defined(assert)
 extern "C" __DPCPP_SYCL_EXTERNAL void
 __devicelib_assert_fail(const char *, const char *, int32_t, const char *,
                         uint64_t, uint64_t, uint64_t, uint64_t, uint64_t,
                         uint64_t);
 #undef assert
+#if defined(NDEBUG)
+#define assert(e) ((void)0)
+#else
 #define assert(e)                                                              \
   (e) ? void(0)                                                                \
       : __devicelib_assert_fail(                                               \
@@ -36,4 +41,6 @@ __devicelib_assert_fail(const char *, const char *, int32_t, const char *,
             __spirv_GlobalInvocationId_y(), __spirv_GlobalInvocationId_z(),    \
             __spirv_LocalInvocationId_x(), __spirv_LocalInvocationId_y(),      \
             __spirv_LocalInvocationId_z());
+#endif
+#endif
 #endif
