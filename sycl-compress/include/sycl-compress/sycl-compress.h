7d7edc6c17263b289d98a24d5e092b1101112424
Agarwal <udit.agarwal@intel.com>
2024-08-25 23:12:42 -0700

    Fix linkage error while windows build

--- sycl-compress/include/sycl-compress/sycl-compress.h
+++ sycl-compress/include/sycl-compress/sycl-compress.h
@@ -1,2 +1,10 @@
+#if defined(_MSC_VER)
+__declspec(dllexport)
+#endif
 char *compressBlob(const char *src, size_t srcSize, size_t &dstSize, int level);
+
+
+#if defined(_MSC_VER)
+__declspec(dllexport)
+#endif
 char *decompressBlob(const char *src, size_t srcSize, size_t &dstSize);
