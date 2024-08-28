bf1454ccf8cd7cb0ecb25a8064dedcd9bbd1644e
Artur Gainullin <artur.gainullin@intel.com>
2024-08-28 10:54:34 -0700

    Add test

---
+++ sycl/test-e2e/ThreadSafety/event.cpp
@@ -0,0 +1,63 @@
+// RUN: %{build} -o %t.out
+// RUN: %{run} %t.out
+
+// This test checks thread safety of sycl::event's handle.
+// To do that we create a host task and a kernel task which depends on the host
+// task. We yield in the main thread to let the host task to work and create the
+// event's handle and start checking the status of the event's handle in a loop
+// to catch the moment when event's handle is modified. If read and modification
+// if event's handle is not thread safe this results in a segfault.
+
+#include <iostream>
+#include <sycl/detail/core.hpp>
+#include <sycl/usm.hpp>
+#include <thread>
+
+int main() {
+  // Create a SYCL queue
+  sycl::queue queue;
+  if (!queue.get_device().has(sycl::aspect::usm_shared_allocations))
+    return 0;
+
+  // Define the size of the buffers
+  const size_t size = 1024;
+
+  // Allocate USM memory for source and destination buffers
+  int *src = sycl::malloc_shared<int>(size, queue);
+  int *dst = sycl::malloc_shared<int>(size, queue);
+
+  // Initialize the source buffer with some data
+  for (size_t i = 0; i < size; ++i) {
+    src[i] = i;
+  }
+
+  auto host_task_event = queue.submit([&](sycl::handler &cgh) {
+    cgh.host_task([=]() {
+      // Do some work in the host task
+      std::cout << "Host task is executing." << std::endl;
+      memcpy(dst, src, 1024 * sizeof(int));
+      std::cout << "Host task completed." << std::endl;
+    });
+  });
+
+  sycl::event kernel_event = queue.submit([&](sycl::handler &cgh) {
+    cgh.depends_on(host_task_event);
+    cgh.memcpy(dst, src, 1024 * sizeof(int));
+  });
+
+  // Let host task thread to work which will result in kernel_event's handle to
+  // be created at some random moment.
+  std::this_thread::yield();
+  for (int i = 0; i < 10000; i++) {
+    std::cout << (int64_t)kernel_event
+                     .get_info<sycl::info::event::command_execution_status>()
+              << std::endl;
+  }
+
+  kernel_event.wait();
+
+  // Free the USM memory
+  sycl::free(src, queue);
+  sycl::free(dst, queue);
+  return 0;
+}
