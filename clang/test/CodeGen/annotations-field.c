3e8e9cc67c758bd6877d1176c5fa7f989acf2831
Jinsong Ji <jinsong.ji@intel.com>
2024-08-29 12:48:12 -0700

    Fix conflict resoultion in annotations-field.c

--- clang/test/CodeGen/annotations-field.c
+++ clang/test/CodeGen/annotations-field.c
@@ -21,18 +21,14 @@ int main(int argc, char **argv) {
     struct foo f;
     f.v = argc;
 // CHECK: getelementptr inbounds nuw %struct.foo, ptr %{{.*}}, i32 0, i32 0
-// CHECK-NEXT: call ptr @llvm.ptr.annotation.p0.[[$MANGLE_AS]]({{.*}}str{{.*}}str{{.*}}i32 9, ptr[[$CONST_AS]] null)
-// CHECK-NEXT: call ptr @llvm.ptr.annotation.p0.[[$MANGLE_AS]]({{.*}}str{{.*}}str{{.*}}i32 9, ptr[[$CONST_AS]] null)
-
-// CHECK: getelementptr inbounds %struct.foo, ptr %{{.*}}, i32 0, i32 0
 // CHECK-NEXT: call ptr @llvm.ptr.annotation.p0.[[$MANGLE_AS]]({{.*}}str{{.*}}str{{.*}}i32 13, ptr[[$CONST_AS]] null)
 // CHECK-NEXT: call ptr @llvm.ptr.annotation.p0.[[$MANGLE_AS]]({{.*}}str{{.*}}str{{.*}}i32 13, ptr[[$CONST_AS]] null)
     f.w = 42;
-// CHECK: getelementptr inbounds %struct.foo, ptr %{{.*}}, i32 0, i32 1
+// CHECK: getelementptr inbounds nuw %struct.foo, ptr %{{.*}}, i32 0, i32 1
 // CHECK-NEXT: call ptr @llvm.ptr.annotation.p0.[[$MANGLE_AS]]({{.*}}str{{.*}}str{{.*}}i32 14, ptr[[$CONST_AS]] null)
 // CHECK-NEXT: call ptr @llvm.ptr.annotation.p0.[[$MANGLE_AS]]({{.*}}str{{.*}}str{{.*}}i32 14, ptr[[$CONST_AS]] null)
     f.f = 0;
-// CHECK: getelementptr inbounds %struct.foo, ptr %{{.*}}, i32 0, i32 2
+// CHECK: getelementptr inbounds nuw %struct.foo, ptr %{{.*}}, i32 0, i32 2
 // CHECK-NEXT: call ptr @llvm.ptr.annotation.p0.[[$MANGLE_AS]]({{.*}}str{{.*}}str{{.*}}i32 15, ptr[[$CONST_AS]] null)
 // CHECK-NEXT: call ptr @llvm.ptr.annotation.p0.[[$MANGLE_AS]]({{.*}}str{{.*}}str{{.*}}i32 15, ptr[[$CONST_AS]] null)
 
@@ -42,10 +38,10 @@ int main(int argc, char **argv) {
 // AMDGPU: call ptr @llvm.ptr.annotation.p0.p4(ptr addrspacecast (ptr addrspace(1) @gf to ptr), ptr addrspace(4) @.str{{.*}}, ptr addrspace(4) @.str{{.*}}, i32 13, ptr addrspace(4) null)
 // AMDGPU-NEXT: call ptr @llvm.ptr.annotation.p0.p4(ptr %{{.*}}, ptr addrspace(4) @.str{{.*}}, ptr addrspace(4) @.str{{.*}}, i32 13, ptr addrspace(4) null)
     gf.w = 42;
-// X86: call ptr @llvm.ptr.annotation.p0.p0(ptr getelementptr inbounds (%struct.foo, ptr @gf, i32 0, i32 1), {{.*}}str{{.*}}str{{.*}}i32 14, ptr null)
-// AMDGPU: call ptr @llvm.ptr.annotation.p0.p4(ptr getelementptr inbounds (%struct.foo, ptr addrspacecast (ptr addrspace(1) @gf to ptr), i32 0, i32 1), {{.*}}str{{.*}}str{{.*}}i32 14, ptr addrspace(4) null)
+// X86: call ptr @llvm.ptr.annotation.p0.p0(ptr getelementptr inbounds nuw (%struct.foo, ptr @gf, i32 0, i32 1), {{.*}}str{{.*}}str{{.*}}i32 14, ptr null)
+// AMDGPU: call ptr @llvm.ptr.annotation.p0.p4(ptr getelementptr inbounds nuw (%struct.foo, ptr addrspacecast (ptr addrspace(1) @gf to ptr), i32 0, i32 1), {{.*}}str{{.*}}str{{.*}}i32 14, ptr addrspace(4) null)
     gf.f = 0;
-// X86: call ptr @llvm.ptr.annotation.p0.p0(ptr getelementptr inbounds (%struct.foo, ptr @gf, i32 0, i32 2), {{.*}}str{{.*}}str{{.*}}i32 15, ptr null)
-// AMDGPU: call ptr @llvm.ptr.annotation.p0.p4(ptr getelementptr inbounds (%struct.foo, ptr addrspacecast (ptr addrspace(1) @gf to ptr), i32 0, i32 2), {{.*}}str{{.*}}str{{.*}}i32 15, ptr addrspace(4) null)
+// X86: call ptr @llvm.ptr.annotation.p0.p0(ptr getelementptr inbounds nuw (%struct.foo, ptr @gf, i32 0, i32 2), {{.*}}str{{.*}}str{{.*}}i32 15, ptr null)
+// AMDGPU: call ptr @llvm.ptr.annotation.p0.p4(ptr getelementptr inbounds nuw (%struct.foo, ptr addrspacecast (ptr addrspace(1) @gf to ptr), i32 0, i32 2), {{.*}}str{{.*}}str{{.*}}i32 15, ptr addrspace(4) null)
     return 0;
 }
