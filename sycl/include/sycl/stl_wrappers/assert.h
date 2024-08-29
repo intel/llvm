cbe38f1ad226cf51dc4bfee974acc2c6ec0411d0
Lorenc Bushi <lorenc.bushi@intel.com>
2024-08-29 17:17:16 -0400

    Update assert.h

--- sycl/include/sycl/stl_wrappers/assert.h
+++ sycl/include/sycl/stl_wrappers/assert.h
@@ -1,4 +1,4 @@
-/= = ----------------<assert.h> wrapper around STL--------------------------==//
+//== ----------------<assert.h> wrapper around STL--------------------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
