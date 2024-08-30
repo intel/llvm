0e043b8ec6a6e684125ff1b55a96905be0436f50
Li, Ian <ian.li@intel.com>
2024-08-29 17:51:13 -0700

    Fix uninitialized SYCLMemObjT MSizeInBytes

--- sycl/source/detail/sycl_mem_obj_t.cpp
+++ sycl/source/detail/sycl_mem_obj_t.cpp
@@ -106,6 +106,10 @@ SYCLMemObjT::SYCLMemObjT(ur_native_handle_t MemObject,
   Plugin->call(urMemGetInfo, MInteropMemObject, UR_MEM_INFO_CONTEXT,
                sizeof(Context), &Context, nullptr);
 
+  // Get the size of the buffer in bytes
+  Plugin->call(urMemGetInfo, MInteropMemObject, UR_MEM_INFO_SIZE,
+               sizeof(size_t), &MSizeInBytes, nullptr);
+
   if (MInteropContext->getHandleRef() != Context)
     throw sycl::exception(
         make_error_code(errc::invalid),
