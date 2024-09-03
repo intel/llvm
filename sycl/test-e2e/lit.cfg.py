f6f64954607cb0730d365b2a063ceeadb20ff0f9
Agarwal, Udit <udit.agarwal@intel.com>
2024-09-03 16:43:39 -0700

    Remove newly aded gpu-intel-pvc feature

--- sycl/test-e2e/lit.cfg.py
+++ sycl/test-e2e/lit.cfg.py
@@ -159,7 +159,6 @@ if lit_config.params.get("gpu-intel-pvc", False):
     config.available_features.add(
         "matrix-tf32"
     )  # PVC implies the support of TF32 matrix
-    config.available_features.add("gpu-intel-pvc")
 if lit_config.params.get("gpu-intel-pvc-vg", False):
     config.available_features.add("gpu-intel-pvc-vg")
     config.available_features.add(
