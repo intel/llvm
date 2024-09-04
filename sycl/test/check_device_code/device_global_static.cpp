d3c9529acf8ac3a9943cae73cdf544d857dedb90
Hugh Delaney <hugh.delaney@codeplay.com>
2024-09-04 16:47:35 +0100

    Check that `strings` can find the symbol
    Make sure that `strings` can successfully detect the symbol in an object
    file.

--- sycl/test/check_device_code/device_global_static.cpp
+++ sycl/test/check_device_code/device_global_static.cpp
@@ -2,15 +2,25 @@
 // device globals, is removed at some point in compilation. For SPIR-V this
 // symbol is removed at sycl-post-link and for NVPTX/AMDGCN it is removed at
 // lowering.
+//
+// It also checks that the symbol can be found in an object file for a given
+// triple, thus validating that `strings` can successfully be used to check for
+// the presence of the symbol.
 
 // UNSUPPORTED: windows
 
+// RUN: %clangxx -fsycl -fsycl-device-only %s -o %t
+// RUN: strings %t | grep "llvm.compiler.used"
 // RUN: %clangxx -fsycl %s -o %t
 // RUN: strings %t | not grep "llvm.compiler.used"
 
+// RUN: %if cuda %{ %clangxx -fsycl -fsycl-device-only -fsycl-targets=nvptx64-nvidia-cuda %s -o %t %}
+// RUN: %if cuda %{ strings %t | grep "llvm.compiler.used" %}
 // RUN: %if cuda %{ %clangxx -fsycl -fsycl-targets=nvptx64-nvidia-cuda %s -o %t %}
 // RUN: %if cuda %{ strings %t | not grep "llvm.compiler.used" %}
 
+// RUN: %if hip_amd %{ %clangxx -fsycl -fsycl-device-only -fsycl-targets=amd_gpu_gfx906 %s -o %t %}
+// RUN: %if hip_amd %{ strings %t | grep "llvm.compiler.used" %}
 // RUN: %if hip_amd %{ %clangxx -fsycl -fsycl-targets=amd_gpu_gfx906 %s -o %t %}
 // RUN: %if hip_amd %{ strings %t | not grep "llvm.compiler.used" %}
 
