a6fe5f3d6460bc912d217043cf45d093710544c4
Marcos Maronas <marcos.maronas@intel.com>
2024-09-05 03:01:12 -0700

    Filter out illegal attributes to align with Khronos behaviour.
    Fixes #10817.
    
    Signed-off-by: Marcos Maronas <marcos.maronas@intel.com>

--- llvm-spirv/lib/SPIRV/SPIRVReader.cpp
+++ llvm-spirv/lib/SPIRV/SPIRVReader.cpp
@@ -3042,12 +3042,15 @@ Function *SPIRVToLLVM::transFunction(SPIRVFunction *BF) {
     auto BA = BF->getArgument(I->getArgNo());
     mapValue(BA, &(*I));
     setName(&(*I), BA);
+    AttributeMask IllegalAttrs = AttributeFuncs::typeIncompatible(I->getType());
     BA->foreachAttr([&](SPIRVFuncParamAttrKind Kind) {
       // Skip this function parameter attribute as it will translated among
       // OpenCL metadata
       if (Kind == FunctionParameterAttributeRuntimeAlignedINTEL)
         return;
       Attribute::AttrKind LLVMKind = SPIRSPIRVFuncParamAttrMap::rmap(Kind);
+      if (IllegalAttrs.contains(LLVMKind))
+        return;
       Type *AttrTy = nullptr;
       switch (LLVMKind) {
       case Attribute::AttrKind::ByVal:
