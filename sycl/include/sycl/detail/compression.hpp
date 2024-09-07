ff53221c0e3f0511844fb38d4b3d50d3cc51e5c3
Agarwal, Udit <udit.agarwal@intel.com>
2024-09-07 16:49:30 -0700

    Add unit test for (de)compression

--- sycl/include/sycl/detail/compression.hpp
+++ sycl/include/sycl/detail/compression.hpp
@@ -7,6 +7,7 @@
 //===----------------------------------------------------------------------===//
 #pragma once
 
+#include <iostream>
 #include <memory>
 #include <zstd.h>
 
---
+++ sycl/unittests/compression/CompressionTests.cpp
@@ -0,0 +1,90 @@
+//==------- CompressionTests.cpp --- compression unit test ----------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include <sycl/detail/compression.hpp>
+
+#include <string>
+
+#include <gtest/gtest.h>
+
+using namespace sycl::detail;
+
+TEST(CompressionTest, SimpleCompression) {
+
+  // Data to compress.
+  std::string data = "Hello World! I'm about to get compressed :P";
+  size_t compressedDataSize = 0;
+
+  auto compressedData = ZSTDCompressor::CompressBlob(
+      data.c_str(), data.size(), compressedDataSize, /*Compression level*/ 3);
+
+  // Check if compression was successful.
+  EXPECT_NE(compressedData, nullptr);
+  EXPECT_GT(compressedDataSize, 0);
+
+  // Decompress the data.
+  size_t decompressedSize = 0;
+  auto decompressedData = ZSTDCompressor::DecompressBlob(
+      compressedData.get(), compressedDataSize, decompressedSize);
+
+  ASSERT_NE(decompressedData, nullptr);
+  ASSERT_GT(decompressedSize, 0);
+
+  // Check if decompressed data is same as original data.
+  std::string decompressedStr((char *)decompressedData.get(), decompressedSize);
+  ASSERT_EQ(data, decompressedStr);
+
+  // Check that error code is 0 after successful decompression.
+  int errorCode = ZSTDCompressor::GetLastError();
+  ASSERT_EQ(errorCode, 0);
+
+  // Check that error string is "No error detected" after successful
+  // decompression.
+  std::string errorString = ZSTDCompressor::GetErrorString(errorCode);
+  ASSERT_EQ(errorString, "No error detected");
+}
+
+// Test getting error code and error string.
+// Intentionally give incorrect input to decompress
+// to trigger an error.
+TEST(CompressionTest, NegativeErrorTest) {
+  std::string input = "Hello, World!";
+  size_t decompressedSize = 0;
+  auto compressedData = ZSTDCompressor::DecompressBlob(
+      input.c_str(), input.size(), decompressedSize);
+
+  int errorCode = ZSTDCompressor::GetLastError();
+  ASSERT_NE(errorCode, 0);
+
+  std::string errorString = ZSTDCompressor::GetErrorString(errorCode);
+  ASSERT_NE(errorString, "No error detected");
+}
+
+// Test passing empty input to (de)compress.
+// There should be no error and the output should be empty.
+TEST(CompressionTest, EmptyInputTest) {
+  std::string input = "";
+  size_t compressedSize = 0;
+  auto compressedData = ZSTDCompressor::CompressBlob(
+      input.c_str(), input.size(), compressedSize, 1);
+
+  ASSERT_NE(compressedData, nullptr);
+  ASSERT_GT(compressedSize, 0);
+  ASSERT_EQ(ZSTDCompressor::GetLastError(), 0);
+
+  size_t decompressedSize = 0;
+  auto decompressedData = ZSTDCompressor::DecompressBlob(
+      compressedData.get(), compressedSize, decompressedSize);
+
+  ASSERT_NE(decompressedData, nullptr);
+  ASSERT_EQ(decompressedSize, 0);
+  ASSERT_EQ(ZSTDCompressor::GetLastError(), 0);
+
+  std::string decompressedStr((char *)decompressedData.get(), decompressedSize);
+  ASSERT_EQ(input, decompressedStr);
+}
