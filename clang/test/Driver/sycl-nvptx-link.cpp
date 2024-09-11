5e54dc1a1c66ef7fab42c1ad260da14090120d71
Fraser Cormack <fraser@codeplay.com>
2024-09-11 16:31:23 +0100

    pass -nocudalib

--- clang/test/Driver/sycl-nvptx-link.cpp
+++ clang/test/Driver/sycl-nvptx-link.cpp
@@ -1,6 +1,6 @@
 // REQUIRES: nvptx-registered-target
 
-// RUN: %clang -### -fsycl -fsycl-targets=nvptx64-nvidia-cuda %s 2>&1 | FileCheck %s
+// RUN: %clang -### -nocudalib -fsycl -fsycl-targets=nvptx64-nvidia-cuda %s 2>&1 | FileCheck %s
 
 // Check that we correctly determine that the final link command links
 // devicelibs together, as far as the driver is concerned. This results in the
@@ -15,6 +15,5 @@
 // CHECK-SAME: -only-needed
 // CHECK-SAME: devicelib--cuda.bc
 // CHECK-SAME: libspirv-nvptx64-nvidia-cuda.bc
-// CHECK-SAME: libdevice
 
 void func(){}
