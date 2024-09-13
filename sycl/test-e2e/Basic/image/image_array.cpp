2bc8746c568c2665b518eaf689102bdeb39262c2
Larsen, Steffen <steffen.larsen@intel.com>
2024-09-13 06:35:37 -0700

    [SYCL][E2E] Enable passing UNSUPPORTED:gpu tests
    This commit enables a selection of tests with UNSUPPORTED:gpu that seems
    to have been fixed since their disablement.
    
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- sycl/test-e2e/Basic/image/image_array.cpp
+++ sycl/test-e2e/Basic/image/image_array.cpp
@@ -4,8 +4,6 @@
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 
-// UNSUPPORTED: gpu
-
 //==------------------- image.cpp - SYCL image basic test -----------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
