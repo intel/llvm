fd0f1e334766219575d7569fa0102fd413440e6c
Agarwal, Udit <udit.agarwal@intel.com>
2024-09-16 23:59:34 -0700

    Simply E2E test and fix failure on CUDA

--- sycl/test-e2e/Compression/compression_multiple_tu.cpp
+++ sycl/test-e2e/Compression/compression_multiple_tu.cpp
@@ -2,63 +2,51 @@
 // translation units, one compressed and one not compressed.
 // REQUIRES: zstd
 
-// RUN: %clangxx --offload-compress -DENABLE_KERNEL1 -fsycl -O0 -shared -fPIC %s -o %t_kernel1.so
-// RUN: %clangxx -DENABLE_KERNEL2 -fsycl -O0 -shared -fPIC %s -o %t_kernel2.so
+// RUN: %{build} --offload-compress -DENABLE_KERNEL1 -shared -fPIC -o %t_kernel1.so
+// RUN: %{build} -DENABLE_KERNEL2 -shared -fPIC -o %t_kernel2.so
 
-// RUN: %clangxx -fsycl %t_kernel1.so %t_kernel2.so %s -Wl,-rpath=%T -o %t_compress.out
+// RUN: %{build} %t_kernel1.so %t_kernel2.so -Wl,-rpath=%T -o %t_compress.out
 // RUN: %{run} %t_compress.out
 #if defined(ENABLE_KERNEL1) || defined(ENABLE_KERNEL2)
 #include <sycl/detail/core.hpp>
 
 using namespace sycl;
-
-class TestFnObj {
-public:
-  TestFnObj(buffer<int> &buf, handler &cgh)
-      : data(buf.get_access<access::mode::write>(cgh)) {}
-  accessor<int, 1, access::mode::write, access::target::device> data;
-  void operator()(id<1> item) const { data[item] = item[0]; }
-};
 #endif
-
-void kernel1();
-void kernel2();
 
 #ifdef ENABLE_KERNEL1
 void kernel1() {
-  static int data[10];
+  int data = -1;
   {
-    buffer<int> b(data, range<1>(10));
+    buffer<int> b(&data, range(1));
     queue q;
     q.submit([&](sycl::handler &cgh) {
-      TestFnObj kernel(b, cgh);
-      cgh.parallel_for(range<1>(10), kernel);
+      auto acc = accessor(b, cgh);
+      cgh.single_task([=]{ acc[0] = __builtin_abs(acc[0]);});
     });
   }
-  for (int i = 0; i < 10; i++) {
-    assert(data[i] == i);
-  }
+  assert(data == 1);
 }
 #endif
 
 #ifdef ENABLE_KERNEL2
 void kernel2() {
-  static int data[256];
+  int data = -2;
   {
-    buffer<int> b(data, range<1>(256));
+    buffer<int> b(&data, range(1));
     queue q;
-    q.submit([&](handler &cgh) {
-      TestFnObj kernel(b, cgh);
-      cgh.parallel_for(range<1>(256), kernel);
+    q.submit([&](sycl::handler &cgh) {
+      auto acc = accessor(b, cgh);
+      cgh.single_task([=]{ acc[0] = __builtin_abs(acc[0]);});
     });
   }
-  for (int i = 0; i < 256; i++) {
-    assert(data[i] == i);
-  }
+  assert(data == 2);
 }
 #endif
 
 #if not defined(ENABLE_KERNEL1) && not defined(ENABLE_KERNEL2)
+void kernel1();
+void kernel2();
+
 int main() {
   kernel1();
   kernel2();
