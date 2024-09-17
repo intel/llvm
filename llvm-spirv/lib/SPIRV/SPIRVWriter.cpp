22359e703e904be6c235fef200dd8c44d93fdc50
Marcos Maronas <marcos.maronas@intel.com>
2024-09-17 09:24:09 +0200

    Fix typo in intrinsic name in a comment. (#2719)
    Signed-off-by: Marcos Maronas <marcos.maronas@intel.com>
    
    Original commit:
    https://github.com/KhronosGroup/SPIRV-LLVM-Translator/commit/52ba01c7e56cf4a

--- llvm-spirv/lib/SPIRV/SPIRVWriter.cpp
+++ llvm-spirv/lib/SPIRV/SPIRVWriter.cpp
@@ -4762,7 +4762,7 @@ SPIRVValue *LLVMToSPIRVBase::transIntrinsicInst(IntrinsicInst *II,
   case Intrinsic::invariant_start:
   case Intrinsic::invariant_end:
   case Intrinsic::dbg_label:
-  // llvm.trap and llvm.debug.trap intrinsics are not implemented. But for now
+  // llvm.trap and llvm.debugtrap intrinsics are not implemented. But for now
   // don't crash. This change is pending the trap/abort intrinsic
   // implementation.
   case Intrinsic::trap:
