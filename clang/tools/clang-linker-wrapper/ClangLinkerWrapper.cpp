63ed85b2cb0e3775822c5211e0c8872e104de1b2
Harald van Dijk <harald.vandijk@codeplay.com>
2024-09-17 17:09:06 +0100

    More updates:
    - Update clang-offload-wrapper too.
    - Remove unused SYCL::Linker::constructLlcCommand.

--- clang/tools/clang-linker-wrapper/ClangLinkerWrapper.cpp
+++ clang/tools/clang-linker-wrapper/ClangLinkerWrapper.cpp
@@ -1149,7 +1149,7 @@ wrapSYCLBinariesFromFile(std::vector<module_split::SplitModule> &SplitModules,
   return OutputFilePath;
 }
 
-/// Run llc tool for SYCL offloading.
+/// Run clang tool for SYCL offloading.
 /// 'InputFile' is the wrapped input file.
 /// 'Args' encompasses all arguments required for linking and wrapping device
 /// code and will be parsed to generate options required to be passed into the
@@ -1162,21 +1162,29 @@ static Expected<StringRef> runCompile(StringRef &InputFile,
   if (!OutputFileOrErr)
     return OutputFileOrErr.takeError();
 
-  Expected<std::string> LLCPath =
-      findProgram("llc", {getMainExecutable("llc")});
-  if (!LLCPath)
-    return LLCPath.takeError();
+  Expected<std::string> ClangPath =
+      findProgram("clang", {getMainExecutable("clang")});
+  if (!ClangPath)
+    return ClangPath.takeError();
+
+  const llvm::Triple HostTriple(Args.getLastArgValue(OPT_host_triple_EQ));
 
   SmallVector<StringRef, 8> CmdArgs;
-  CmdArgs.push_back(*LLCPath);
+  CmdArgs.push_back(*ClangPath);
+
+  const std::string TargetStr = "--target=" + HostTriple.getTriple();
+  CmdArgs.push_back(TargetStr);
+
   // Checking for '-shared' linker option
-  if (Args.hasArg(OPT_shared))
-    CmdArgs.push_back("-relocation-model=pic");
-  CmdArgs.push_back("-filetype=obj");
+  if (Args.hasArg(OPT_shared)) {
+    if (!HostTriple.isOSWindows())
+      CmdArgs.push_back("-fPIC");
+  }
+  CmdArgs.push_back("-c");
   CmdArgs.push_back("-o");
   CmdArgs.push_back(*OutputFileOrErr);
   CmdArgs.push_back(InputFile);
-  if (Error Err = executeCommands(*LLCPath, CmdArgs))
+  if (Error Err = executeCommands(*ClangPath, CmdArgs))
     return std::move(Err);
   return *OutputFileOrErr;
 }
