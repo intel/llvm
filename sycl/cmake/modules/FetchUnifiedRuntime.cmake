09851167aa1bd63ce48d499e53e13f5b68c8964e
Yang Zhao <yang2.zhao@intel.com>
2024-09-19 19:29:12 +0800

    [DeviceSanitizer] Support nullpointer detection & enable GPU tests (#14891)
    UR: https://github.com/oneapi-src/unified-runtime/pull/1914
    
    ---------
    
    Co-authored-by: omarahmed1111 <omar.ahmed@codeplay.com>

--- sycl/cmake/modules/FetchUnifiedRuntime.cmake
+++ sycl/cmake/modules/FetchUnifiedRuntime.cmake
@@ -117,13 +117,13 @@ if(SYCL_UR_USE_FETCH_CONTENT)
   endfunction()
 
   set(UNIFIED_RUNTIME_REPO "https://github.com/oneapi-src/unified-runtime.git")
-  # commit 6298474e628889d3598b9416303a52e67a2b66aa
-  # Merge: 3cd6eaeb 4bb6a103
-  # Author: Piotr Balcer <piotr.balcer@intel.com>
-  # Date:   Wed Sep 18 09:20:05 2024 +0200
-  #   Merge pull request #2093 from lslusarczyk/memleak-fix
-  #   fixed issue #1990, L0 leaks checker counts successful create/destroy only
-  set(UNIFIED_RUNTIME_TAG 6298474e628889d3598b9416303a52e67a2b66aa)
+  # commit 4517290650a9938537666e6409fb8e0db73ff4d8
+  # Merge: 6298474e 3dbb7a2a
+  # Author: Omar Ahmed <omar.ahmed@codeplay.com>
+  # Date:   Wed Sep 18 08:48:03 2024 +0100
+  #     Merge pull request #1914 from AllanZyne/review/yang/dsan_nullpointer
+  #     [DeviceSanitizer] Support nullpointer detection
+  set(UNIFIED_RUNTIME_TAG 4517290650a9938537666e6409fb8e0db73ff4d8)
 
   set(UMF_BUILD_EXAMPLES OFF CACHE INTERNAL "EXAMPLES")
   # Due to the use of dependentloadflag and no installer for UMF and hwloc we need
