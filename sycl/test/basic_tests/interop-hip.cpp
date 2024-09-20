f585393f37b8be868e9ebb8b40be766c2c69b745
JackAKirk <jack.kirk@codeplay.com>
2024-09-20 03:03:41 -0700

    Use expected-no-diagnostics in test
    Signed-off-by: JackAKirk <jack.kirk@codeplay.com>

--- sycl/test/basic_tests/interop-hip.cpp
+++ sycl/test/basic_tests/interop-hip.cpp
@@ -1,6 +1,6 @@
 // REQUIRES: hip
-// RUN: %clangxx %fsycl-host-only -fsyntax-only -Xclang -verify -Xclang -verify-ignore-unexpected=note %s
-// RUN: %clangxx %fsycl-host-only -fsyntax-only -Xclang -verify -Xclang -verify-ignore-unexpected=note -D__SYCL_INTERNAL_API %s
+// RUN: %clangxx %fsycl-host-only -fsyntax-only -Xclang -verify %s
+// RUN: %clangxx %fsycl-host-only -fsyntax-only -Xclang -verify -D__SYCL_INTERNAL_API %s
 
 // Test for HIP interop API
 
@@ -51,6 +51,7 @@ int main() {
   // backend-defined and specified in the backend specification.
 
   hip_device = get_native<backend::ext_oneapi_hip>(Device);
+  // expected-no-diagnostics
   hip_context = get_native<backend::ext_oneapi_hip>(Context);
   hip_event = get_native<backend::ext_oneapi_hip>(Event);
   hip_queue = get_native<backend::ext_oneapi_hip>(Queue);
