b7cd07fdf45bc0b2e55af64c158f93a4b676c3b0
Zhao, Yang2 <yang2.zhao@intel.com>
2024-09-23 10:58:56 +0200

    change ur

--- sycl/cmake/modules/FetchUnifiedRuntime.cmake
+++ sycl/cmake/modules/FetchUnifiedRuntime.cmake
@@ -116,14 +116,14 @@ if(SYCL_UR_USE_FETCH_CONTENT)
       CACHE PATH "Path to external '${name}' adapter source dir" FORCE)
   endfunction()
 
-  set(UNIFIED_RUNTIME_REPO "https://github.com/oneapi-src/unified-runtime.git")
+  set(UNIFIED_RUNTIME_REPO "https://github.com/AllanZyne/unified-runtime.git")
   # commit 7384e2d7b908de0b2bca9f3c57827ea84698864e
   # Merge: ed85c3e2 a32549b4
   # Author: Omar Ahmed <omar.ahmed@codeplay.com>
   # Date:   Fri Sep 20 14:56:40 2024 +0100
   #   Merge pull request #2108 from pbalcer/fix-perf-tracing-logs
   #   fix perf regression in tracing layer
-  set(UNIFIED_RUNTIME_TAG 7384e2d7b908de0b2bca9f3c57827ea84698864e)
+  set(UNIFIED_RUNTIME_TAG review/yang/fix_invalid-argument)
 
   set(UMF_BUILD_EXAMPLES OFF CACHE INTERNAL "EXAMPLES")
   # Due to the use of dependentloadflag and no installer for UMF and hwloc we need
