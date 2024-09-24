df42f6577a39085227adcb929ea5a899987e4d81
Wu, Yingcong <yingcong.wu@intel.com>
2024-09-23 23:11:10 -0700

    fix typo

--- sycl/test-e2e/AddressSanitizer/common/options-invalid.cpp
+++ sycl/test-e2e/AddressSanitizer/common/options-invalid.cpp
@@ -3,23 +3,23 @@
 // RUN: %{run} %t
 
 // Invalid ur option format
-// RUN: env UR_LAYER_ASAN_OPTIONS=a:1,b:1 %{run} not %t 2>&1 | FileCheck %s  --check-prefixes INVLAID-FORMAT
-// INVLAID-FORMAT: <SANITIZER>[ERROR]: Wrong format of the UR_LAYER_ASAN_OPTIONS environment variable value
+// RUN: env UR_LAYER_ASAN_OPTIONS=a:1,b:1 %{run} not %t 2>&1 | FileCheck %s  --check-prefixes INVALID-FORMAT
+// INVALID-FORMAT: <SANITIZER>[ERROR]: Wrong format of the UR_LAYER_ASAN_OPTIONS environment variable value
 
 // Invalid bool option
-// RUN: env UR_LAYER_ASAN_OPTIONS=debug:yes %{run} not %t 2>&1 | FileCheck %s  --check-prefixes INVLAID-BOOL
-// INVLAID-BOOL: <SANITIZER>[ERROR]: "debug" is set to "yes", which is not an valid setting. Acceptable input are: for enable, use: "1" "true"; for disable, use: "0" "false".
+// RUN: env UR_LAYER_ASAN_OPTIONS=debug:yes %{run} not %t 2>&1 | FileCheck %s  --check-prefixes INVALID-BOOL
+// INVALID-BOOL: <SANITIZER>[ERROR]: "debug" is set to "yes", which is not an valid setting. Acceptable input are: for enable, use: "1" "true"; for disable, use: "0" "false".
 
 // Invalid quarantine_size_mb
-// RUN: env UR_LAYER_ASAN_OPTIONS=quarantine_size_mb:-1 %{run} not %t 2>&1 | FileCheck %s  --check-prefixes INVLAID-QUARANTINE
-// RUN: env UR_LAYER_ASAN_OPTIONS=quarantine_size_mb:4294967296 %{run} not %t 2>&1 | FileCheck %s  --check-prefixes INVLAID-QUARANTINE
-// INVLAID-QUARANTINE: <SANITIZER>[ERROR]: "quarantine_size_mb" should be an integer in range[0, 4294967295].
+// RUN: env UR_LAYER_ASAN_OPTIONS=quarantine_size_mb:-1 %{run} not %t 2>&1 | FileCheck %s  --check-prefixes INVALID-QUARANTINE
+// RUN: env UR_LAYER_ASAN_OPTIONS=quarantine_size_mb:4294967296 %{run} not %t 2>&1 | FileCheck %s  --check-prefixes INVALID-QUARANTINE
+// INVALID-QUARANTINE: <SANITIZER>[ERROR]: "quarantine_size_mb" should be an integer in range[0, 4294967295].
 
 // Invalid redzone and max_redzone
-// RUN: env UR_LAYER_ASAN_OPTIONS=redzone:abc %{run} not %t 2>&1 | FileCheck %s  --check-prefixes INVLAID-REDZONE
-// INVLAID-REDZONE: <SANITIZER>[ERROR]: "redzone" should be an integer in range[0, 16].
-// RUN: env UR_LAYER_ASAN_OPTIONS=max_redzone:abc %{run} not %t 2>&1 | FileCheck %s  --check-prefixes INVLAID-MAXREDZONE
-// INVLAID-MAXREDZONE: <SANITIZER>[ERROR]: "max_redzone" should be an integer in range[0, 2048].
+// RUN: env UR_LAYER_ASAN_OPTIONS=redzone:abc %{run} not %t 2>&1 | FileCheck %s  --check-prefixes INVALID-REDZONE
+// INVALID-REDZONE: <SANITIZER>[ERROR]: "redzone" should be an integer in range[0, 16].
+// RUN: env UR_LAYER_ASAN_OPTIONS=max_redzone:abc %{run} not %t 2>&1 | FileCheck %s  --check-prefixes INVALID-MAXREDZONE
+// INVALID-MAXREDZONE: <SANITIZER>[ERROR]: "max_redzone" should be an integer in range[0, 2048].
 
 
 
