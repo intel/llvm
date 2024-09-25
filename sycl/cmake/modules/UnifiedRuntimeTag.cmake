011c5558cf819be097fdf726b7cc398ff968c0bf
Martin Morrison-Grant <martin.morrisongrant@codeplay.com>
2024-09-25 12:32:24 +0100

    [UR] Bump tag to 2a08189 for UR#2112.

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,2 @@
-# commit fa8cc8ec16c1a2cf0926cc64026edc6a254ff0c2
-# Merge: 3d58884b 1984ceb1
-# Author: aarongreig <aaron.greig@codeplay.com>
-# Date:   Fri Nov 1 10:03:37 2024 +0000
-#     Merge pull request #1833 from ayylol/old-opencl-check
-#     [OpenCL] Add OpenCL version check for independent forward progress query
-set(UNIFIED_RUNTIME_TAG fa8cc8ec16c1a2cf0926cc64026edc6a254ff0c2)
+set(UNIFIED_RUNTIME_TAG 2a081891b69ce881be19ab61a92394c8473786da)
+
