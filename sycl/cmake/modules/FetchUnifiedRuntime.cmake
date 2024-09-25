2fe409d0710e4502db3618a2ef4f365a8b785805
Ross Brunton <ross@codeplay.com>
2024-09-25 14:57:16 +0100

    [UR] Fix level_zero for loop bug
    Pre-commit MR for:
    https://github.com/oneapi-src/unified-runtime/pull/2130

--- sycl/cmake/modules/FetchUnifiedRuntime.cmake
+++ sycl/cmake/modules/FetchUnifiedRuntime.cmake
@@ -116,14 +116,12 @@ if(SYCL_UR_USE_FETCH_CONTENT)
       CACHE PATH "Path to external '${name}' adapter source dir" FORCE)
   endfunction()
 
-  set(UNIFIED_RUNTIME_REPO "https://github.com/oneapi-src/unified-runtime.git")
-  # commit 7ecf64d60c31cd72bd88588498536d067bad59d6
-  # Merge: 17aa04d3 6eb5208b
-  # Author: aarongreig <aaron.greig@codeplay.com>
-  # Date:   Wed Sep 25 11:14:47 2024 +0100
-  #     Merge pull request #1996 from frasercrmck/ur-max-wg-size-props
-  #     Add two new properties to ur_kernel_group_info_t
-  set(UNIFIED_RUNTIME_TAG 7ecf64d60c31cd72bd88588498536d067bad59d6)
+  set(UNIFIED_RUNTIME_REPO "https://github.com/RossBrunton/unified-runtime.git")
+  # commit ee60403891e73c1150cdb70c383d54f545896575
+  # Author: Ross Brunton <ross@codeplay.com>
+  # Date:   Wed Sep 25 14:50:05 2024 +0100
+  # Fix `for` in level_zero EventWaitList handling
+  set(UNIFIED_RUNTIME_TAG ee60403891e73c1150cdb70c383d54f545896575)
 
   set(UMF_BUILD_EXAMPLES OFF CACHE INTERNAL "EXAMPLES")
   # Due to the use of dependentloadflag and no installer for UMF and hwloc we need
