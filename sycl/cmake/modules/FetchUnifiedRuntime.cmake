e7187aae9b0105ed3425fda972e0b552ec5fa98d
Ben Tracy <ben.tracy@codeplay.com>
2024-09-26 14:32:50 +0100

    [SYCL][Graph] Only get command handle for updatable command-buffers
    - Update UR tag to include changes to command-handles
    - Update calls to UrCommandBufferAppendKernelLaunch to only request command handles for updatable command-buffers
    - Only store command handles for updatable command-graphs
    - Update UR commit tag

--- sycl/cmake/modules/FetchUnifiedRuntime.cmake
+++ sycl/cmake/modules/FetchUnifiedRuntime.cmake
@@ -116,14 +116,14 @@ if(SYCL_UR_USE_FETCH_CONTENT)
       CACHE PATH "Path to external '${name}' adapter source dir" FORCE)
   endfunction()
 
-  set(UNIFIED_RUNTIME_REPO "https://github.com/oneapi-src/unified-runtime.git")
-  # commit b79ebe4e98789144bcdf3832088eb6e6b5ae6967
-  # Merge: 7b4bc761 fbb6e862
-  # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-  # Date:   Fri Oct 4 16:39:59 2024 +0100
-  #     Merge pull request #2018 from wenju-he/L0-bindless-image-device-query
-  #     [L0] Fix device query of bindless image support
-  set(UNIFIED_RUNTIME_TAG b79ebe4e98789144bcdf3832088eb6e6b5ae6967)
+  set(UNIFIED_RUNTIME_REPO "https://github.com/bensuo/unified-runtime.git")
+  # commit 22962057df1b9d538e08088a7b75d9d8e7c29f90 (HEAD, origin/main, origin/HEAD)
+  # Merge: e824ddc2 f0a1c433
+  # Author: aarongreig <aaron.greig@codeplay.com>
+  # Date:   Fri Sep 27 16:54:04 2024 +0100
+  #     Merge pull request #2017 from nrspruit/new_sysman_init
+  #     [L0] Use zesInit for SysMan API usage
+  set(UNIFIED_RUNTIME_TAG 508a4531367f57c9d0deb38f151423530a350e54)
 
   set(UMF_BUILD_EXAMPLES OFF CACHE INTERNAL "EXAMPLES")
   # Due to the use of dependentloadflag and no installer for UMF and hwloc we need
