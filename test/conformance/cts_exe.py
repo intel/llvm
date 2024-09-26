acb9ca3a3c92d340f604ea9aa2dd19016b793418
Ross Brunton <ross@codeplay.com>
2024-09-26 11:46:22 +0100

    Don't use empty match test files
    This patch removes any empty ".match" files, and instructs ctest to
    run the tests verbatim if there is no match file available.
    
    As well as providing more useful output, this also handles the case
    where the test startup and/or teardown causes the test runner to fail.
    Previously, this would be ignored (as it produced no output).
    
    I also modified cts_exe.py so that it uses the same parameters as
    ctest, so the same command invocation could be used for both.

--- test/conformance/cts_exe.py
+++ test/conformance/cts_exe.py
@@ -21,13 +21,13 @@ if __name__ == '__main__':
 
     parser = ArgumentParser()
     parser.add_argument("--test_command", help="Ctest test case")
-    parser.add_argument("--test_devices_count", type=str, help="Number of devices on which tests will be run")
-    parser.add_argument("--test_platforms_count", type=str, help="Number of platforms on which tests will be run")
+    parser.add_argument("--devices_count", type=str, help="Number of devices on which tests will be run")
+    parser.add_argument("--platforms_count", type=str, help="Number of platforms on which tests will be run")
     args = parser.parse_args()
 
     result = subprocess.Popen([args.test_command, '--gtest_brief=1',        # nosec B603
-                               f'--devices_count={args.test_devices_count}',
-                               f'--platforms_count={args.test_platforms_count}'],
+                               f'--devices_count={args.devices_count}',
+                               f'--platforms_count={args.platforms_count}'],
                                stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
 
     pat = re.compile(r'\[( )*FAILED( )*\]')
