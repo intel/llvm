73ce7a882469617f3bb730c7aefe96a4b5790110
Lorenc Bushi <lorenc.bushi@intel.com>
2024-10-01 12:23:06 -0700

    Merge the two tests into one

--- sycl/test/basic_tests/assert_header_with_c_linkage.cpp
+++ sycl/test/basic_tests/assert_header_with_c_linkage.cpp
@@ -1,13 +1,18 @@
-// RUN: %clangxx -fsycl -c %s
+// RUN: %clangxx -fsycl -DASSERT -c %s
+// RUN: %clangxx -fsycl -DCASSERT -c %s
 
-// Verify that compilation works when assert.h is wrapped by a C linkage
+// Verify that compilation works when assert.h/cassert is wrapped by a C linkage
 // specification.
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#if defined(ASSERT)
 #include <assert.h>
+#elif defined(CASSERT)
+#include <cassert>
+#endif
 
 #ifdef __cplusplus
 }
