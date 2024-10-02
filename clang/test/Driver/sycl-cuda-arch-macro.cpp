7062717b5bada638d67cbc8411a84d584c51cd0c
Georgi Mirazchiyski <georgi.mirazchiyski@codeplay.com>
2024-10-02 16:43:50 +0100

    Add clang Driver test

--- clang/test/Driver/sycl-cuda-arch-macro.cpp
+++ clang/test/Driver/sycl-cuda-arch-macro.cpp
@@ -1,6 +1,9 @@
 // Verify the __CUDA_ARCH__ macro has not been defined when offloading SYCL on NVPTX
 // RUN: %clangxx -E -dM -fsycl -fsycl-targets=nvptx64-nvidia-cuda -Xsycl-target-backend --offload-arch=sm_80 -nocudalib -fno-sycl-libspirv %s 2>&1 \
 // RUN: | FileCheck --check-prefix=CHECK-CUDA-ARCH-MACRO %s
+// Verify the __CUDA_ARCH__ macro has not been defined when offloading SYCL on AMDGPU
+// RUN: %clangxx -E -dM -fsycl -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx90a -nogpulib -fno-sycl-libspirv %s 2>&1 \
+// RUN: | FileCheck --check-prefix=CHECK-CUDA-ARCH-MACRO %s
 // CHECK-CUDA-ARCH-MACRO-NOT: #define __CUDA_ARCH__ {{[0-9]+}}
 
 // Verify that '-fcuda-is-device' is not supplied when offloading SYCL on NVPTX
