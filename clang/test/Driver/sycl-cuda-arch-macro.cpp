c27960458f0cbc8fa391f706230edc822325a40a
Georgi Mirazchiyski <georgi.mirazchiyski@codeplay.com>
2024-10-03 12:26:29 +0100

    Split NVPTX and AMDGPU filecheck tests

--- clang/test/Driver/sycl-cuda-arch-macro.cpp
+++ clang/test/Driver/sycl-cuda-arch-macro.cpp
@@ -1,10 +1,12 @@
 // Verify the __CUDA_ARCH__ macro has not been defined when offloading SYCL on NVPTX
 // RUN: %clangxx -E -dM -fsycl -fsycl-targets=nvptx64-nvidia-cuda -Xsycl-target-backend --offload-arch=sm_80 -nocudalib -fno-sycl-libspirv %s 2>&1 \
-// RUN: | FileCheck --check-prefix=CHECK-CUDA-ARCH-MACRO %s
+// RUN: | FileCheck --check-prefix=CHECK-CUDA-ARCH-MACRO-NVPTX %s
+// CHECK-CUDA-ARCH-MACRO-NVPTX-NOT: #define __CUDA_ARCH__ {{[0-9]+}}
+
 // Verify the __CUDA_ARCH__ macro has not been defined when offloading SYCL on AMDGPU
 // RUN: %clangxx -E -dM -fsycl -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx90a -nogpulib -fno-sycl-libspirv %s 2>&1 \
-// RUN: | FileCheck --check-prefix=CHECK-CUDA-ARCH-MACRO %s
-// CHECK-CUDA-ARCH-MACRO-NOT: #define __CUDA_ARCH__ {{[0-9]+}}
+// RUN: | FileCheck --check-prefix=CHECK-CUDA-ARCH-MACRO-AMDGPU %s
+// CHECK-CUDA-ARCH-MACRO-AMDGPU-NOT: #define __CUDA_ARCH__ {{[0-9]+}}
 
 // Verify that '-fcuda-is-device' is not supplied when offloading SYCL on NVPTX
 // RUN: %clangxx -### -fsycl -fsycl-targets=nvptx64-nvidia-cuda -Xsycl-target-backend --offload-arch=sm_80 -nocudalib -fno-sycl-libspirv %s 2>&1 \
