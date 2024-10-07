8a650c9fbdcfe963a3e36bbd582b51a9bb47b432
Aaron Greig <aaron.greig@codeplay.com>
2024-10-07 11:34:23 +0100

    [UR] Support device to device USM copies with the CL adapter

--- sycl/cmake/modules/FetchUnifiedRuntime.cmake
+++ sycl/cmake/modules/FetchUnifiedRuntime.cmake
@@ -116,12 +116,8 @@ if(SYCL_UR_USE_FETCH_CONTENT)
       CACHE PATH "Path to external '${name}' adapter source dir" FORCE)
   endfunction()
 
-  set(UNIFIED_RUNTIME_REPO "https://github.com/oneapi-src/unified-runtime.git")
-  # commit 568a96aabc6edabe8514ae163aecc64cd5a41878
-  # Author: Mateusz P. Nowak <112635238+mateuszpn@users.noreply.github.com>
-  # Date:   Tue Oct 15 13:57:26 2024 +0200
-  #     Benchmark updates for faster run and more reliable results (#2164)
-  set(UNIFIED_RUNTIME_TAG 568a96aabc6edabe8514ae163aecc64cd5a41878)
+  set(UNIFIED_RUNTIME_REPO "https://github.com/aarongreig/unified-runtime.git")
+  set(UNIFIED_RUNTIME_TAG aaron/supportCLInterDeviceCopy)
 
   set(UMF_BUILD_EXAMPLES OFF CACHE INTERNAL "EXAMPLES")
   # Due to the use of dependentloadflag and no installer for UMF and hwloc we need
