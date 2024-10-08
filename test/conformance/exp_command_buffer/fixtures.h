623e885868188f61c0ef099eba8f7a87a530591e
Ross Brunton <ross@codeplay.com>
2024-10-08 16:56:58 +0100

    Enable Wextra on Linux
    Also fixed some warnings:
    * A small fallthrough bug fix for level zero v2
    * Unset initializers in command buffer tests

--- test/conformance/exp_command_buffer/fixtures.h
+++ test/conformance/exp_command_buffer/fixtures.h
@@ -403,7 +403,8 @@ struct urCommandEventSyncUpdateTest : urCommandEventSyncTest {
 
         // Create a command-buffer with update enabled.
         ur_exp_command_buffer_desc_t desc{
-            UR_STRUCTURE_TYPE_EXP_COMMAND_BUFFER_DESC, nullptr, true};
+            UR_STRUCTURE_TYPE_EXP_COMMAND_BUFFER_DESC, nullptr, true, false,
+            false};
 
         ASSERT_SUCCESS(urCommandBufferCreateExp(context, device, &desc,
                                                 &updatable_cmd_buf_handle));
