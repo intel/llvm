35413eafcb239b19f16aacf95067327e3756cd08
jinge90 <ge.jin@intel.com>
2024-10-09 13:31:45 +0800

    add tests for key type i64/u64
    Signed-off-by: jinge90 <ge.jin@intel.com>

--- sycl/test-e2e/DeviceLib/group_sort/group_private_KV_sort_p1p1_p1.hpp
+++ sycl/test-e2e/DeviceLib/group_sort/group_private_KV_sort_p1p1_p1.hpp
@@ -415,3 +415,131 @@ __devicelib_default_work_group_private_sort_close_ascending_p1i32_p1i64_u32_p1i8
 __DPCPP_SYCL_EXTERNAL extern "C" void
 __devicelib_default_work_group_private_sort_close_descending_p1i32_p1i64_u32_p1i8(
     int32_t *keys, int64_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_ascending_p1u64_p1u8_u32_p1i8(
+    uint64_t *keys, uint8_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_descending_p1u64_p1u8_u32_p1i8(
+    uint64_t *keys, uint8_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_ascending_p1u64_p1i8_u32_p1i8(
+    uint64_t *keys, int8_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_descending_p1u64_p1i8_u32_p1i8(
+    uint64_t *keys, int8_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_ascending_p1u64_p1u16_u32_p1i8(
+    uint64_t *keys, uint16_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_descending_p1u64_p1u16_u32_p1i8(
+    uint64_t *keys, uint16_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_ascending_p1u64_p1i16_u32_p1i8(
+    uint64_t *keys, int16_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_descending_p1u64_p1i16_u32_p1i8(
+    uint64_t *keys, int16_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_ascending_p1u64_p1u32_u32_p1i8(
+    uint64_t *keys, uint32_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_descending_p1u64_p1u32_u32_p1i8(
+    uint64_t *keys, uint32_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_ascending_p1u64_p1i32_u32_p1i8(
+    uint64_t *keys, int32_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_descending_p1u64_p1i32_u32_p1i8(
+    uint64_t *keys, int32_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_ascending_p1u64_p1u64_u32_p1i8(
+    uint64_t *keys, uint64_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_descending_p1u64_p1u64_u32_p1i8(
+    uint64_t *keys, uint64_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_ascending_p1u64_p1i64_u32_p1i8(
+    uint64_t *keys, int64_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_descending_p1u64_p1i64_u32_p1i8(
+    uint64_t *keys, int64_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_ascending_p1i64_p1u8_u32_p1i8(
+    int64_t *keys, uint8_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_descending_p1i64_p1u8_u32_p1i8(
+    int64_t *keys, uint8_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_ascending_p1i64_p1i8_u32_p1i8(
+    int64_t *keys, int8_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_descending_p1i64_p1i8_u32_p1i8(
+    int64_t *keys, int8_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_ascending_p1i64_p1u16_u32_p1i8(
+    int64_t *keys, uint16_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_descending_p1i64_p1u16_u32_p1i8(
+    int64_t *keys, uint16_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_ascending_p1i64_p1i16_u32_p1i8(
+    int64_t *keys, int16_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_descending_p1i64_p1i16_u32_p1i8(
+    int64_t *keys, int16_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_ascending_p1i64_p1u32_u32_p1i8(
+    int64_t *keys, uint32_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_descending_p1i64_p1u32_u32_p1i8(
+    int64_t *keys, uint32_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_ascending_p1i64_p1i32_u32_p1i8(
+    int64_t *keys, int32_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_descending_p1i64_p1i32_u32_p1i8(
+    int64_t *keys, int32_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_ascending_p1i64_p1u64_u32_p1i8(
+    int64_t *keys, uint64_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_descending_p1i64_p1u64_u32_p1i8(
+    int64_t *keys, uint64_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_ascending_p1i64_p1i64_u32_p1i8(
+    int64_t *keys, int64_t *vals, uint32_t n, uint8_t *scratch);
+
+__DPCPP_SYCL_EXTERNAL extern "C" void
+__devicelib_default_work_group_private_sort_close_descending_p1i64_p1i64_u32_p1i8(
+    int64_t *keys, int64_t *vals, uint32_t n, uint8_t *scratch);
