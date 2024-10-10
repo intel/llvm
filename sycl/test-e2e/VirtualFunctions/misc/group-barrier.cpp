f92ac8537f1584ec1084fb932eb4824d68fde579
Alexey Sachkov <alexey.sachkov@intel.com>
2024-10-10 04:12:19 -0700

    Apply review comments to simplify the test

--- sycl/test-e2e/VirtualFunctions/misc/group-barrier.cpp
+++ sycl/test-e2e/VirtualFunctions/misc/group-barrier.cpp
@@ -34,13 +34,15 @@ class BaseOp {
 public:
   SYCL_EXT_ONEAPI_FUNCTION_PROPERTY(oneapi::indirectly_callable)
   virtual int apply(int *, sycl::group<1>) = 0;
+
+  virtual int computeReference(sycl::range<1> LocalRange, int Init) = 0;
 };
 
 class SumOp : public BaseOp {
 public:
   SYCL_EXT_ONEAPI_FUNCTION_PROPERTY(oneapi::indirectly_callable)
-  virtual int apply(int *LocalData, sycl::group<1> WG) {
-    LocalData[WG.get_local_id()] += WG.get_local_id();
+  int apply(int *LocalData, sycl::group<1> WG) override {
+    LocalData[WG.get_local_id()] = WG.get_local_id() + WG.get_group_id();
     sycl::group_barrier(WG);
     if (WG.leader()) {
       int Res = 0;
@@ -53,13 +55,26 @@ public:
 
     return LocalData[0];
   }
+
+  int computeReference(sycl::range<1> LocalRange, int WGID) override {
+    std::vector<int> LocalData(LocalRange.size());
+    for (size_t LID = 0; LID < LocalRange.size(); ++LID)
+      LocalData[LID] = LID + WGID;
+
+    int Res = 0;
+    for (size_t LID = 0; LID < LocalRange.size(); ++LID)
+      Res += LocalData[LID];
+
+    return Res;
+  }
 };
 
 class MultiplyOp : public BaseOp {
 public:
   SYCL_EXT_ONEAPI_FUNCTION_PROPERTY(oneapi::indirectly_callable)
-  virtual int apply(int *LocalData, sycl::group<1> WG) {
-    LocalData[WG.get_local_id()] += WG.get_local_id();
+  int apply(int *LocalData, sycl::group<1> WG) override {
+    // +1 to avoid multiplying by 0 below
+    LocalData[WG.get_local_id()] = WG.get_local_id() + WG.get_group_id() + 1;
     sycl::group_barrier(WG);
     if (WG.leader()) {
       int Res = 1;
@@ -72,6 +87,18 @@ public:
 
     return LocalData[0];
   }
+
+  int computeReference(sycl::range<1> LocalRange, int WGID) override {
+    std::vector<int> LocalData(LocalRange.size());
+    for (size_t LID = 0; LID < LocalRange.size(); ++LID)
+      LocalData[LID] = LID + WGID + 1;
+
+    int Res = 1;
+    for (size_t LID = 0; LID < LocalRange.size(); ++LID)
+      Res *= LocalData[LID];
+
+    return Res;
+  }
 };
 
 int main() try {
@@ -79,6 +106,7 @@ int main() try {
 
   sycl::queue q;
 
+  storage_t HostStorage;
   auto *DeviceStorage = sycl::malloc_shared<storage_t>(1, q);
   // Let's keep ranges small, or otherwise we will encounter integer overflow
   // (which is a UB) in MultiplyOp::apply.
@@ -87,10 +115,7 @@ int main() try {
 
   constexpr oneapi::properties props{oneapi::assume_indirect_calls};
   for (unsigned TestCase = 0; TestCase < 2; ++TestCase) {
-    std::vector<int> HostData(G.size());
-    std::iota(HostData.begin(), HostData.end(), 1);
-    std::vector<int> DeviceData = HostData;
-    sycl::buffer<int> DataStorage(DeviceData.data(), G);
+    sycl::buffer<int> DataStorage(G);
 
     q.submit([&](sycl::handler &CGH) {
        CGH.single_task([=]() {
@@ -102,70 +127,28 @@ int main() try {
       sycl::accessor DataAcc(DataStorage, CGH, sycl::read_write);
       sycl::local_accessor<int> LocalAcc(L, CGH);
       CGH.parallel_for(sycl::nd_range{G, L}, props, [=](auto It) {
-        LocalAcc[It.get_local_id()] = DataAcc[It.get_global_id()];
         auto *Ptr = DeviceStorage->getAs<BaseOp>();
         DataAcc[It.get_global_id()] = Ptr->apply(
             LocalAcc.get_multi_ptr<sycl::access::decorated::no>().get(),
             It.get_group());
       });
-    });
+    }).wait_and_throw();
 
-    // We can't call group_barrier on host and therefore here we have a
-    // reference function instead of calling the same methods on host.
-    //
-    // 'apply' function is written as a kernel, i.e. it describes a single
-    // work-item in an nd-range. Here we emulate that nd-range by looping over
-    // all work-groups and then over each work-item within that group.
+    auto *Ptr = HostStorage.construct</* ret type = */ BaseOp>(TestCase);
+    sycl::host_accessor HostAcc(DataStorage);
+
+    // All work-items in a group produce the same result, so we do verification
+    // per work-group.
     for (size_t WorkGroupID = 0; WorkGroupID < G.size() / L.size();
          ++WorkGroupID) {
-      // Equivalent of a local accessor (LocalData)
-      std::vector<int> LocalHostData(L.size());
-      // For each work-item within a group, LID - local id
-      for (size_t LID = 0; LID < L.size(); ++LID) {
-        // GID - global id
-        size_t GID = WorkGroupID * L.size() + LID;
-        LocalHostData[LID] = HostData[GID];
-
-        // Below (including other loops) is an equivalent of apply's body, but
-        // it combains both SumOp and MultiplyOp and hence conditions based on
-        // TestCase.
-        LocalHostData[LID] += LID;
-      }
-
-      // Group barrier is simulated by splitting work-group loop in two.
-      // Even though Res is a private variable in the kernel, here we have to
-      // declare it in an outer scope (making it local) so it survies our
-      // barriers emulation.
-      int Res = (TestCase == 0) ? 0 : 1;
-
-      for (size_t LID = 0; LID < L.size(); ++LID) {
-        if (LID == 0) { // if that is a group leader
-          for (size_t NestedLID = 0; NestedLID < L.size(); ++NestedLID) {
-            if (TestCase == 0)
-              Res += LocalHostData[NestedLID];
-            else
-              Res *= LocalHostData[NestedLID];
-          }
+      int Reference = Ptr->computeReference(L, WorkGroupID);
+      for (size_t I = 0; I < L.size(); ++I) {
+        size_t GID = WorkGroupID * L.size() + I;
+        if (HostAcc[GID] != Reference) {
+          std::cout << "Mismatch at index " << I << ": " << HostAcc[I]
+                    << " != " << Reference << std::endl;
+          assert(HostAcc[I] == Reference);
         }
-      }
-
-      // Group broadcast involves a barrier, so we once again splitting
-      // work-group loop.
-      for (size_t LID = 0; LID < L.size(); ++LID) {
-        // GID - global id
-        size_t GID = WorkGroupID * L.size() + LID;
-        // The broadcast itself: all work-items get result computed by a
-        // work-group leader.
-        HostData[GID] = Res;
-      }
-    }
-
-    sycl::host_accessor HostAcc(DataStorage);
-    for (size_t I = 0; I < HostData.size(); ++I) {
-      if (HostAcc[I] != HostData[I]) {
-        std::cout << "Mismatch at index " << I << ": " << HostAcc[I]
-                  << " != " << HostData[I] << std::endl;
-        assert(HostAcc[I] == HostData[I]);
       }
     }
   }
