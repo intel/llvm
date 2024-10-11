9baeb6f8aa5e713fcb527539f35df6dcd4567e54
Aaron Greig <aaron.greig@codeplay.com>
2024-10-11 16:07:50 +0100

    Update UR tag.

--- sycl/cmake/modules/FetchUnifiedRuntime.cmake
+++ sycl/cmake/modules/FetchUnifiedRuntime.cmake
@@ -117,13 +117,13 @@ if(SYCL_UR_USE_FETCH_CONTENT)
   endfunction()
 
   set(UNIFIED_RUNTIME_REPO "https://github.com/oneapi-src/unified-runtime.git")
-  # commit 53c461b257e7bbf5827147c3b5ca0b8ea45b434d
-  # Merge: 2c4bf63b d93b262b
+  # commit 4814e505335bfb968ea905d76e5b978f6f4d0002
+  # Merge: d977bd76 aa53a35d
   # Author: aarongreig <aaron.greig@codeplay.com>
-  # Date:   Fri Oct 11 11:01:37 2024 +0100
-  #     Merge pull request #2174 from ProGTX/peter/winfix
-  #     [NFC][CUDA] Include STL headers in program.cpp
-  set(UNIFIED_RUNTIME_TAG 53c461b257e7bbf5827147c3b5ca0b8ea45b434d)
+  # Date:   Fri Oct 11 16:05:19 2024 +0100
+  #     Merge pull request #2090 from RossBrunton/ross/harden
+  #     Hardening flags
+  set(UNIFIED_RUNTIME_TAG 4814e505335bfb968ea905d76e5b978f6f4d0002)
 
   set(UMF_BUILD_EXAMPLES OFF CACHE INTERNAL "EXAMPLES")
   # Due to the use of dependentloadflag and no installer for UMF and hwloc we need
