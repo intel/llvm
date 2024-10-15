604c6407a283f871ad4770c1b787b808168f2cbf
Lorenc Bushi <lorenc.bushi@intel.com>
2024-10-15 09:40:51 -0700

    Fix unit test implementation for backend kernel argument

--- sycl/unittests/Extensions/WorkGroupMemoryBackendArgument.cpp
+++ sycl/unittests/Extensions/WorkGroupMemoryBackendArgument.cpp
@@ -7,15 +7,40 @@
 //===----------------------------------------------------------------------===//
 
 #include <gtest/gtest.h>
-#include <helpers/TestKernel.hpp>
+#include <helpers/UrImage.hpp>
 #include <helpers/UrMock.hpp>
 #include <sycl/sycl.hpp>
 
 // Check that the work group memory object is mapped to exactly one backend
 // kernel argument.
-
+class WorkGroupMemoryKernel;
 namespace syclext = sycl::ext::oneapi::experimental;
-using arg_type = syclext::work_group_memory<int, syclext::empty_properties_t>;
+using arg_type = syclext::work_group_memory<int>;
+
+namespace sycl {
+inline namespace _V1 {
+namespace detail {
+template <> struct KernelInfo<WorkGroupMemoryKernel> {
+  static constexpr unsigned getNumParams() { return 1; }
+  static const detail::kernel_param_desc_t &getParamDesc(int) {
+    static detail::kernel_param_desc_t WorkGroupMemory = {
+        detail::kernel_param_kind_t::kind_work_group_memory, 0, 0};
+    return WorkGroupMemory;
+  }
+  static constexpr bool isESIMD() { return false; }
+  static constexpr bool callsThisItem() { return false; }
+  static constexpr bool callsAnyThisFreeFunction() { return false; }
+  static constexpr int64_t getKernelSize() { return sizeof(arg_type); }
+  static constexpr const char *getName() { return "WorkGroupMemoryKernel"; }
+};
+
+} // namespace detail
+} // namespace _V1
+} // namespace sycl
+
+static sycl::unittest::UrImage Img =
+    sycl::unittest::generateDefaultImage({"WorkGroupMemoryKernel"});
+static sycl::unittest::UrImageArray<1> ImgArray{&Img};
 
 static int urKernelSetArgLocalCalls = 0;
 inline ur_result_t redefined_urKernelSetArgLocal(void *) {
@@ -32,9 +57,9 @@ TEST(URArgumentTest, URArgumentTest) {
   sycl::queue q{dev};
   syclext::submit(q, [&](sycl::handler &cgh) {
     arg_type data{cgh};
-    const auto mykernel = [=](sycl::nd_item<1> it) { data = 42; };
-    syclext::nd_launch<TestKernel<sizeof(mykernel)>>(
-        cgh, sycl::nd_range<1>{1, 1}, mykernel);
+    const auto kernel = [=](sycl::nd_item<1> it) { data = 42; };
+    syclext::nd_launch<WorkGroupMemoryKernel>(cgh, sycl::nd_range<1>{1, 1},
+                                              kernel);
   });
   q.wait();
   ASSERT_EQ(urKernelSetArgLocalCalls, 1);
