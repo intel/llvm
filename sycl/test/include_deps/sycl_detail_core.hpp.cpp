09631466aa99562e49cfc2d3019e7ab9d8987c44
Victor Lomuller <victor@codeplay.com>
2024-10-21 16:39:21 +0100

    fix include test

--- sycl/test/include_deps/sycl_detail_core.hpp.cpp
+++ sycl/test/include_deps/sycl_detail_core.hpp.cpp
@@ -158,8 +158,7 @@
 // CHECK-NEXT: ext/oneapi/experimental/use_root_sync_prop.hpp
 // CHECK-NEXT: ext/oneapi/experimental/virtual_functions.hpp
 // CHECK-NEXT: ext/oneapi/kernel_properties/properties.hpp
-// CHECK-NEXT: ext/oneapi/work_group_static.hpp
-// CHECK-NEXT: ext/intel/usm_pointers.hpp
+// CHECK-NEXT: ext/oneapi/work_group_scratch_memory.hpp
 // CHECK-NEXT: kernel.hpp
 // CHECK-NEXT: kernel_bundle.hpp
 // CHECK-NEXT: ext/oneapi/experimental/free_function_traits.hpp
