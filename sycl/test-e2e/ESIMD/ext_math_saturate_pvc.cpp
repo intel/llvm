322a09df1ac2a24bcec2e11870bd39320297dce0
Sarnie, Nick <nick.sarnie@intel.com>
2024-10-22 08:18:51 -0700

    [SYCL][ESIMD][E2E] Add driver requirement to sqrt tests
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- sycl/test-e2e/ESIMD/ext_math_saturate_pvc.cpp
+++ sycl/test-e2e/ESIMD/ext_math_saturate_pvc.cpp
@@ -1,19 +1,21 @@
-//==-- ext_math_ieee_qsrt_div.cpp  - DPC++ ESIMD ieee sqrt/div  test -------==//
+//==---- ext_math_saturate_pvc.cpp  - DPC++ ESIMD extended math test ----==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-// REQUIRES: arch-intel_gpu_pvc
 // REQUIRES-INTEL-DRIVER: lin: 31294
-
 // DEFINE: %{mathflags} = %if cl_options %{/clang:-fno-fast-math%} %else %{-fno-fast-math%}
 // RUN: %{build} -fsycl-device-code-split=per_kernel %{mathflags} -o %t.out
 // RUN: %{run} %t.out
 
-// This test checks ieee_sqrt() and ieee_sqrt() with float and double types.
+// REQUIRES: arch-intel_gpu_pvc
 
-#define SKIP_NEW_GPU_DRIVER_VERSION_CHECK 1
-#define TEST_IEEE_DIV_REM 1
-#include "ext_math.cpp"
+// This test checks extended math operations called with saturation.
+// Combinations of
+// - argument type - half, float
+// - math function - sin, cos, ..., div_ieee, pow
+// - SYCL vs ESIMD APIs
+
+#include "ext_math_saturate.cpp"
