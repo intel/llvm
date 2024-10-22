439ec9e1ddb529610f2ab9397c08be65c3cbd239
Jinsong Ji <jinsong.ji@intel.com>
2024-10-22 17:19:34 -0400

    [UR] Split the UNIFIED_RUNTIME_TAG to its own file (#15809)
    With current repo setup, we need to update UNIFIED_RUNTIME_TAG
    constantly. This is creating conflicts when doing merge from other
    branches. To faciliate the automation to handle such conflicts,
    we would prefer to split the tag into into own file, the automation
    will then be triggereed whenever the file changes (implying UR tag
    change).
    
    ---------
    
    Co-authored-by: Udit Agarwal <udit.agarwal@intel.com>

---
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -0,0 +1,7 @@
+# commit c742ca49efb12380a35b8b0b467e6577ab8174ce
+# Merge: 3a8bf2c5 504d3b63
+# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
+# Date:   Mon Oct 21 11:55:23 2024 +0100
+#     Merge pull request #2131 from Bensuo/ben/command-handle-fix
+#     [EXP][CMDBUF] Make command handle behaviour consistent
+set(UNIFIED_RUNTIME_TAG c742ca49efb12380a35b8b0b467e6577ab8174ce)
