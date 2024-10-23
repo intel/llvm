e9ee0fcda31233d0b84031533829262890bed979
Sarnie, Nick <nick.sarnie@intel.com>
2024-10-23 08:37:12 -0700

    [SYCL] Move SYCL header E2E test to compile-time
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- sycl/test/no_sycl_hpp_in_e2e_tests.cpp
+++ sycl/test/no_sycl_hpp_in_e2e_tests.cpp
@@ -1,13 +1,12 @@
 // REQUIRES: linux
 //
-// RUN: grep -r -l 'sycl.hpp' %S | FileCheck  %s
-// RUN: grep -r -l 'sycl.hpp' %S | wc -l | FileCheck %s --check-prefix CHECK-NUM-MATCHES
+// RUN: grep -r -l 'sycl.hpp' %S/../test-e2e | FileCheck  %s
+// RUN: grep -r -l 'sycl.hpp' %S/../test-e2e | wc -l | FileCheck %s --check-prefix CHECK-NUM-MATCHES
 //
 // CHECK-DAG: README.md
-// CHECK-DAG: no_sycl_hpp_in_e2e_tests.cpp
 // CHECK-DAG: lit.cfg.py
 //
-// CHECK-NUM-MATCHES: 5
+// CHECK-NUM-MATCHES: 4
 //
 // This test verifies that `<sycl/sycl.hpp>` isn't used in E2E tests. Instead,
 // fine-grained includes should used, see
