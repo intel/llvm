c4985671aac67edd45e748850d8989c77d38ac77
Artur Gainullin <artur.gainullin@intel.com>
2024-10-22 23:04:53 -0700

    [SYCL] Support sycl::kernel_bundle for multi-device scenario (#15546)
    This PR includes:
    * Changes in the program manager methods to be able to properly
    create/build UR program for multiple devices. So far, we were mostly
    using only the first device in the vector to create/build UR program
    which made UR program unusable on other devices.
    
    * UR tag update brings the version of urProgramCreateWithBinary which
    allows to create UR program from multiple device binaries.
    
    * Our program cache key allowed only a single device. I have changed it
    to contain a set of devices. If UR program is created and built for a
    set of devices then the same UR program is usable whenver we have any
    subset of this set. That's why if we have a program built for a set of
    devices then add all subsets to the cache. Before we were adding a
    record to the cache for each device from the set which is incorrect. For
    example, if someone requests a UR program for {dev2, dev3} from the
    cache then it is expected that this UR progam must be usable to submit a
    kernel to dev3. But we could get a program for {dev1, dev2} from the
    cache which is unusable on dev3.

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,6 @@
-# commit c742ca49efb12380a35b8b0b467e6577ab8174ce
-# Merge: 3a8bf2c5 504d3b63
-# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Mon Oct 21 11:55:23 2024 +0100
-#     Merge pull request #2131 from Bensuo/ben/command-handle-fix
-#     [EXP][CMDBUF] Make command handle behaviour consistent
-set(UNIFIED_RUNTIME_TAG c742ca49efb12380a35b8b0b467e6577ab8174ce)
+# commit 3db3a5e2d935630f2ffddd93a72ae0aa9af89acb
+# Author: Artur Gainullin <artur.gainullin@intel.com>
+# Date:   Tue Oct 22 03:30:08 2024 -0700
+#
+#     Support UR program creation from multiple device binaries (#2147)
+set(UNIFIED_RUNTIME_TAG 3db3a5e2d935630f2ffddd93a72ae0aa9af89acb)
