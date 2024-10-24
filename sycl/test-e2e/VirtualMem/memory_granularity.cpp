2cb74a1868bd0611d8241ee20360f2ab2c039a92
Skrobot, Daniel <daniel.skrobot@intel.com>
2024-10-24 13:50:11 +0200

    formatting fixed

--- sycl/test-e2e/VirtualMem/memory_granularity.cpp
+++ sycl/test-e2e/VirtualMem/memory_granularity.cpp
@@ -12,41 +12,60 @@
 
 namespace syclext = sycl::ext::oneapi::experimental;
 
-template<typename... ArgsT> 
+template <typename... ArgsT>
 int CheckReturnedGranularitySize(std::string_view TestName, ArgsT... Args) {
-    if (syclext::get_mem_granularity(Args...) == 0){
-      std::cout << "Failed check: " << TestName << std::endl;
-      return 1;
-    }
-    return 0;
+  if (syclext::get_mem_granularity(Args...) == 0) {
+    std::cout << "Failed check: " << TestName << std::endl;
+    return 1;
+  }
+  return 0;
 }
 
 int main() {
   int Failed = 0;
   sycl::queue Q;
   sycl::context Context = Q.get_context();
-  sycl::device Device = Q.get_device(); 
+  sycl::device Device = Q.get_device();
 
-  Failed += CheckReturnedGranularitySize("returned granularity size must be greater then 0 for device and granularity_mode::recommended", Device, Context, syclext::granularity_mode::recommended);
-  Failed += CheckReturnedGranularitySize("returned granularity size must be greater then 0 for device and granularity_mode::minimum", Device, Context, syclext::granularity_mode::minimum);
-  Failed += CheckReturnedGranularitySize("returned granularity size must be greater then 0 for context and granularity_mode::recommended", Context, syclext::granularity_mode::recommended);
-  Failed += CheckReturnedGranularitySize("returned granularity size must be greater then 0 for context and granularity_mode::minimum", Context, syclext::granularity_mode::minimum);
+  Failed += CheckReturnedGranularitySize(
+      "returned granularity size must be greater then 0 for device and "
+      "granularity_mode::recommended",
+      Device, Context, syclext::granularity_mode::recommended);
+  Failed += CheckReturnedGranularitySize(
+      "returned granularity size must be greater then 0 for device and "
+      "granularity_mode::minimum",
+      Device, Context, syclext::granularity_mode::minimum);
+  Failed += CheckReturnedGranularitySize(
+      "returned granularity size must be greater then 0 for context and "
+      "granularity_mode::recommended",
+      Context, syclext::granularity_mode::recommended);
+  Failed += CheckReturnedGranularitySize(
+      "returned granularity size must be greater then 0 for context and "
+      "granularity_mode::minimum",
+      Context, syclext::granularity_mode::minimum);
 
   constexpr size_t NumberOfElements = 2;
-  std::array<size_t, NumberOfElements> RecommendedGranularities = {syclext::get_mem_granularity(Device, Context, syclext::granularity_mode::recommended),
-                                       syclext::get_mem_granularity(Context, syclext::granularity_mode::recommended)};
+  std::array<size_t, NumberOfElements> RecommendedGranularities = {
+      syclext::get_mem_granularity(Device, Context,
+                                   syclext::granularity_mode::recommended),
+      syclext::get_mem_granularity(Context,
+                                   syclext::granularity_mode::recommended)};
 
-  std::array<size_t, NumberOfElements> MinimumGranularities = {syclext::get_mem_granularity(Device, Context, syclext::granularity_mode::minimum),
-  syclext::get_mem_granularity(Context, syclext::granularity_mode::minimum)};
+  std::array<size_t, NumberOfElements> MinimumGranularities = {
+      syclext::get_mem_granularity(Device, Context,
+                                   syclext::granularity_mode::minimum),
+      syclext::get_mem_granularity(Context,
+                                   syclext::granularity_mode::minimum)};
 
   std::array<std::string_view, NumberOfElements> TestNames = {
-    "Failed check: granularity_mode::recommended size must be at least granularity_mode::minimum for device",
-    "Failed check: granularity_mode::recommended size must be at least granularity_mode::minimum for context"
-  };
+      "Failed check: granularity_mode::recommended size must be at least "
+      "granularity_mode::minimum for device",
+      "Failed check: granularity_mode::recommended size must be at least "
+      "granularity_mode::minimum for context"};
 
-  for (size_t Index=0; Index<NumberOfElements;++Index) {
-    if(RecommendedGranularities[Index]<MinimumGranularities[Index]){
-       std::cout << TestNames[Index]<<std::endl;
+  for (size_t Index = 0; Index < NumberOfElements; ++Index) {
+    if (RecommendedGranularities[Index] < MinimumGranularities[Index]) {
+      std::cout << TestNames[Index] << std::endl;
       ++Failed;
     }
   }
