ccde31ec616fa51b29e0d6f123bc9ad15bf9f0c6
Hugh Delaney <hugh.delaney@codeplay.com>
2024-10-25 16:38:46 +0100

    Put UR entry points in ur::level_zero
    Fixes missing symbol at linking for static build of L0 adapter.

--- source/adapters/level_zero/tensor_map.cpp
+++ source/adapters/level_zero/tensor_map.cpp
@@ -10,7 +10,9 @@
 
 #include <ur_api.h>
 
-UR_APIEXPORT ur_result_t UR_APICALL urTensorMapEncodeIm2ColExp(
+namespace ur::level_zero {
+
+ur_result_t urTensorMapEncodeIm2ColExp(
     ur_device_handle_t, ur_exp_tensor_map_data_type_flags_t, uint32_t, void *,
     const uint64_t *, const uint64_t *, const int *, const int *, uint32_t,
     uint32_t, const uint32_t *, ur_exp_tensor_map_interleave_flags_t,
@@ -18,7 +20,8 @@ UR_APIEXPORT ur_result_t UR_APICALL urTensorMapEncodeIm2ColExp(
     ur_exp_tensor_map_oob_fill_flags_t, ur_exp_tensor_map_handle_t *) {
   return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
 }
-UR_APIEXPORT ur_result_t UR_APICALL urTensorMapEncodeTiledExp(
+
+ur_result_t urTensorMapEncodeTiledExp(
     ur_device_handle_t, ur_exp_tensor_map_data_type_flags_t, uint32_t, void *,
     const uint64_t *, const uint64_t *, const uint32_t *, const uint32_t *,
     ur_exp_tensor_map_interleave_flags_t, ur_exp_tensor_map_swizzle_flags_t,
@@ -26,3 +29,4 @@ UR_APIEXPORT ur_result_t UR_APICALL urTensorMapEncodeTiledExp(
     ur_exp_tensor_map_handle_t *) {
   return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
 }
+} // namespace ur::level_zero
