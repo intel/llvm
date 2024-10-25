872f67134a90602900092f9b76b28135068e23fb
Lorenc Bushi <lorenc.bushi@intel.com>
2024-10-24 20:45:38 -0400

    Update work_group_memory_forward_decl.hpp

--- clang/test/SemaSYCL/Inputs/sycl/ext/oneapi/experimental/work_group_memory_forward_decl.hpp
+++ clang/test/SemaSYCL/Inputs/sycl/ext/oneapi/experimental/work_group_memory_forward_decl.hpp
@@ -1,2 +1,16 @@
 #pragma once
 // Dummy header file to avoid integration-header #include errors.
+// It shadows the file 
+// sycl/include/sycl/ext/oneapi/experimental/work_group_memory_forward_decl.hpp
+namespace sycl {
+inline namespace _V1 {
+namespace ext {
+namespace oneapi {
+namespace experimental {
+template <typename DataT, typename PropertiesT = int>
+class work_group_memory;
+} // namespace experimental
+} // namespace oneapi
+} // namespace ext
+} // namespace _V1
+} // namespace sycl
