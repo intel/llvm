a8d19fcd98e68c9a0fd1c66cec49c126079dacd1
Agarwal, Udit <udit.agarwal@intel.com>
2024-10-28 11:29:15 -0700

    add -fno-sycl-libspriv -nogpulib

--- clang/test/Driver/sycl-offload-wrapper-compression.cpp
+++ clang/test/Driver/sycl-offload-wrapper-compression.cpp
@@ -11,10 +11,7 @@
 // RUN: %clangxx -### -fsycl --offload-compression-level=3 %s 2>&1 | FileCheck %s --check-prefix=CHECK-NO-COMPRESS
 
 // For SYCL offloading to HIP, make sure we don't pass '--compress' to offload-bundler.
-// Clang throws an error in the following command "cannot find 'remangled-l64-signed_char.libspirv-amdgcn-amd-amdhsa.bc'" on some machine,
-// that's why appending '|| true'. This error should not affect the driver invocation, which this test intends to verify.
-
-// RUN: (%clangxx -### -fsycl --offload-compress -Xsycl-target-backend=amdgcn-amd-amdhsa --offload-arch=gfx1031 -fsycl-targets=amdgcn-amd-amdhsa %s &> %t.driver) || true
+// RUN: %clangxx -### -fsycl --offload-compress -Xsycl-target-backend=amdgcn-amd-amdhsa --offload-arch=gfx1031 -fsycl-targets=amdgcn-amd-amdhsa -fno-sycl-libspirv -nogpulib %s &> %t.driver
 // RUN: FileCheck %s --check-prefix=CHECK-NO-COMPRESS-BUNDLER --input-file=%t.driver
 
 // CHECK-NO-COMPRESS-NOT: {{.*}}clang-offload-wrapper{{.*}}"-offload-compress"{{.*}}
