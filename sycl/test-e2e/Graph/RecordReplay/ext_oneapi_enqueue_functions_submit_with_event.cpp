6a85cd3499e9d8178681c242ed9c7084cbb63b1f
Pablo Reble <pablo.reble@intel.com>
2024-10-28 13:22:10 -0700

    clang-format

--- sycl/test-e2e/Graph/RecordReplay/ext_oneapi_enqueue_functions_submit_with_event.cpp
+++ sycl/test-e2e/Graph/RecordReplay/ext_oneapi_enqueue_functions_submit_with_event.cpp
@@ -52,7 +52,9 @@ int main() {
 
   auto GraphExec = Graph.finalize();
 
-  exp_ext::submit_with_event(Queue, [&](handler &CGH){execute_graph(CGH, GraphExec);}).wait();
+  exp_ext::submit_with_event(Queue, [&](handler &CGH) {
+    execute_graph(CGH, GraphExec);
+  }).wait();
 
   free(PtrA, Queue);
   free(PtrB, Queue);
