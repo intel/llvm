0ff5854f27f2e050f827d5b645ff842e0ed42347
Daniel Skrobot <daniel.skrobot@intel.com>
2024-10-28 09:17:42 +0100

    Update sycl/test-e2e/VirtualMem/memory_granularity.cpp
    Co-authored-by: Alexey Sachkov <alexey.sachkov@intel.com>

--- sycl/test-e2e/VirtualMem/memory_granularity.cpp
+++ sycl/test-e2e/VirtualMem/memory_granularity.cpp
@@ -2,7 +2,7 @@
 
 // This test checks whether memory granularity returned is greater then 0 and
 // that the recommended granularity is the same size or greater than the minimum
-// granularity for virtul memory extension.
+// granularity for virtual memory extension.
 
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
