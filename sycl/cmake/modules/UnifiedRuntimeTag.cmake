b0efcb86811fa1d7e290f42286d70cf56d74e386
Aaron Greig <aaron.greig@codeplay.com>
2024-10-28 17:29:21 +0000

    [UR] Use extension version of clGetKernelSubGroupInfo when necessary.

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -2,4 +2,4 @@
 # Author: Maosu Zhao <maosu.zhao@intel.com>
 # Date:   Mon Oct 28 18:39:57 2024 +0800
 #     [DeviceSanitizer] Add a report flag to LaunchInfo (#2069)
-set(UNIFIED_RUNTIME_TAG 66ba7970a6badf781226b75a98c9585ef30ea93a)
+set(UNIFIED_RUNTIME_TAG aaron/tryUseExtensionSubgroupInfo)
