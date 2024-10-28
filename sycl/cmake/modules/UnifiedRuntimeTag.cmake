13e84cff559bfff8a47540ce29335313c12a0d9f
Maosu Zhao <maosu.zhao@intel.com>
2024-10-28 21:03:41 +0800

    [DeviceSanitizer] Only allows to report one error per each kernel launch (#15326)
    When error is detected, the corresponding work-item will exit without
    unpoisoning local/private shadow memory. Then other work-items are easy
    to cause false alarms. So, we only allows to report one error per each
    kernel launch by default.
    
    UR Part: https://github.com/oneapi-src/unified-runtime/pull/2069
    
    ---------
    
    Co-authored-by: Piotr Balcer <piotr.balcer@intel.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,5 @@
-# commit d3a518a2a621da6d78f8d25e8460fd3300fff705
-# Merge: ab0a706b 77975b87
-# Author: aarongreig <aaron.greig@codeplay.com>
-# Date:   Fri Oct 25 16:45:53 2024 +0100
-#     Merge pull request #2219 from kbenzie/benie/cl-adapter-opt-core-func
-#     [CL] Support using old ICD loaders
-set(UNIFIED_RUNTIME_TAG d3a518a2a621da6d78f8d25e8460fd3300fff705)
+# commit 66ba7970a6badf781226b75a98c9585ef30ea93a
+# Author: Maosu Zhao <maosu.zhao@intel.com>
+# Date:   Mon Oct 28 18:39:57 2024 +0800
+#     [DeviceSanitizer] Add a report flag to LaunchInfo (#2069)
+set(UNIFIED_RUNTIME_TAG 66ba7970a6badf781226b75a98c9585ef30ea93a)
