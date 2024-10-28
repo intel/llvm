54f24099f2e38553d18ec6b84cdd581f33e45ac7
Zahira Ammarguellat <zahira.ammarguellat@intel.com>
2024-10-28 14:32:20 -0700

    Fix format.

--- clang/lib/Driver/ToolChains/Clang.h
+++ clang/lib/Driver/ToolChains/Clang.h
@@ -57,8 +57,8 @@ private:
                         llvm::opt::ArgStringList &CmdArgs,
                         bool KernelOrKext) const;
   void AddSPIRTargetArgs(const llvm::opt::ArgList &Args,
-                          llvm::opt::ArgStringList &CmdArgs,
-                          const JobAction &JA, const Driver &D) const;
+                         llvm::opt::ArgStringList &CmdArgs, const JobAction &JA,
+                         const Driver &D) const;
   void AddARM64TargetArgs(const llvm::opt::ArgList &Args,
                           llvm::opt::ArgStringList &CmdArgs) const;
   void AddLoongArchTargetArgs(const llvm::opt::ArgList &Args,
