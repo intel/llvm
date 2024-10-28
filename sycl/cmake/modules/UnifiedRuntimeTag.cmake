5c9f65ec30acfccec65ef9b64978d75f97a1081c
Neil R. Spruit <neil.r.spruit@intel.com>
2024-10-28 14:24:59 -0700

    [L0] Enable Immediate Command Lists by default given Intel GPU DG2 or newer
    -pre-commit PR for https://github.com/oneapi-src/unified-runtime/pull/2259
    
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -2,4 +2,4 @@
 # Author: Maosu Zhao <maosu.zhao@intel.com>
 # Date:   Mon Oct 28 18:39:57 2024 +0800
 #     [DeviceSanitizer] Add a report flag to LaunchInfo (#2069)
-set(UNIFIED_RUNTIME_TAG 66ba7970a6badf781226b75a98c9585ef30ea93a)
+set(UNIFIED_RUNTIME_TAG 11d29e686cf8ae3542f73bdd86ed3b612c36de6c)
