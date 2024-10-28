c6f0f372d2dfcf1581063ac0fb009c3cdf163736
Skrobot, Daniel <daniel.skrobot@intel.com>
2024-10-28 09:37:49 +0100

    adjusted according to suggestions

--- sycl/test-e2e/VirtualMem/memory_granularity.cpp
+++ sycl/test-e2e/VirtualMem/memory_granularity.cpp
@@ -7,70 +7,36 @@
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 
-#include <string_view>
-
 #include <sycl/detail/core.hpp>
 
 #include <sycl/ext/oneapi/virtual_mem/virtual_mem.hpp>
 
+#include <assert.h>
+
 namespace syclext = sycl::ext::oneapi::experimental;
 
-template <typename... ArgsT>
-int CheckReturnedGranularitySize(std::string_view TestName, ArgsT... Args) {
-  if (syclext::get_mem_granularity(Args...) == 0) {
-    std::cout << "Failed check: " << TestName << std::endl;
-    return 1;
-  }
-  return 0;
-}
-
 int main() {
-  int Failed = 0;
   sycl::queue Q;
   sycl::context Context = Q.get_context();
   sycl::device Device = Q.get_device();
 
-  Failed += CheckReturnedGranularitySize(
-      "returned granularity size must be greater then 0 for device and "
-      "granularity_mode::recommended",
-      Device, Context, syclext::granularity_mode::recommended);
-  Failed += CheckReturnedGranularitySize(
-      "returned granularity size must be greater then 0 for device and "
-      "granularity_mode::minimum",
-      Device, Context, syclext::granularity_mode::minimum);
-  Failed += CheckReturnedGranularitySize(
-      "returned granularity size must be greater then 0 for context and "
-      "granularity_mode::recommended",
-      Context, syclext::granularity_mode::recommended);
-  Failed += CheckReturnedGranularitySize(
-      "returned granularity size must be greater then 0 for context and "
-      "granularity_mode::minimum",
-      Context, syclext::granularity_mode::minimum);
+  size_t DevRecommended = syclext::get_mem_granularity(Device, Context,
+                                   syclext::granularity_mode::recommended);
+  size_t ContextRecommended = syclext::get_mem_granularity(Context,
+                                   syclext::granularity_mode::recommended);
+  size_t DevMinimum = syclext::get_mem_granularity(Device, Context,
+                                   syclext::granularity_mode::minimum);
+  size_t ContextMinimum = syclext::get_mem_granularity(Context,
+                                   syclext::granularity_mode::minimum);
 
-  constexpr size_t NumberOfElements = 2;
-  std::array<size_t, NumberOfElements> RecommendedGranularities = {
-      syclext::get_mem_granularity(Device, Context,
-                                   syclext::granularity_mode::recommended),
-      syclext::get_mem_granularity(Context,
-                                   syclext::granularity_mode::recommended)};
-
-  std::array<size_t, NumberOfElements> MinimumGranularities = {
-      syclext::get_mem_granularity(Device, Context,
-                                   syclext::granularity_mode::minimum),
-      syclext::get_mem_granularity(Context,
-                                   syclext::granularity_mode::minimum)};
-
-  std::array<std::string_view, NumberOfElements> TestNames = {
-      "Failed check: granularity_mode::recommended size must be at least "
-      "granularity_mode::minimum for device",
-      "Failed check: granularity_mode::recommended size must be at least "
-      "granularity_mode::minimum for context"};
-
-  for (size_t Index = 0; Index < NumberOfElements; ++Index) {
-    if (RecommendedGranularities[Index] < MinimumGranularities[Index]) {
-      std::cout << TestNames[Index] << std::endl;
-      ++Failed;
-    }
-  }
-  return Failed;
+  assert(DevRecommended > 0 && "recommended granularity for device should be greater than 0");
+  assert(ContextRecommended > 0 && "recommended granularity for context should be greater than 0");
+  
+  assert(DevMinimum > 0 && "minimum granularity for device should be greater than 0");
+  assert(ContextMinimum> 0 && "minimum granularity for context should be greater than 0");
+  
+  assert(DevRecommended >= DevMinimum && "recommended granularity size must be at least minimum granularity for device");
+  assert(ContextRecommended >= ContextMinimum && "recommended granularity size must be at least minimum granularity for context");
+  
+  return 0;
 }
