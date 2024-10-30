cedd60211c5f8018aaed3457de04db5fa6397e3b
Aaron Greig <aaron.greig@codeplay.com>
2024-10-30 17:15:45 +0000

    [UR] Add  device info query for devicelib assert.

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,1 @@
-# commit 3d58884b4939d9bd095c917f8dd823ac8486684c
-# Merge: 6ade245e b0bd146a
-# Author: aarongreig <aaron.greig@codeplay.com>
-# Date:   Thu Oct 31 14:05:55 2024 +0000
-#     Merge pull request #2228 from nrspruit/copy_engine_refactor
-#     [L0] Refactor Copy Engine Usage checks for Performance
-set(UNIFIED_RUNTIME_TAG 3d58884b4939d9bd095c917f8dd823ac8486684c)
+set(UNIFIED_RUNTIME_TAG aaron/addAssertDeviceInfoQuery)
