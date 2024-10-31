535890ab3b1465944810c3d72d6fbb1dbe0394a4
Lorenc Bushi <lorenc.bushi@intel.com>
2024-10-31 12:33:02 -0700

    Remove work_group_memory_forward_decl.hpp

--- sycl/include/sycl/ext/oneapi/experimental/work_group_memory.hpp
+++ sycl/include/sycl/ext/oneapi/experimental/work_group_memory.hpp
@@ -8,8 +8,6 @@
 
 #pragma once
 
-#include <tuple>
-#include <sycl/ext/oneapi/experimental/work_group_memory_forward_decl.hpp>
 #include <type_traits>
 
 namespace sycl {
@@ -38,7 +36,7 @@ private:
 } // namespace detail
 namespace ext::oneapi::experimental {
 
-template <typename DataT, typename PropertyListT>
+template <typename DataT, typename PropertyListT = empty_properties_t>
 class __SYCL_SPECIAL_CLASS __SYCL_TYPE(work_group_memory) work_group_memory
     : public sycl::detail::work_group_memory_impl {
 public:
