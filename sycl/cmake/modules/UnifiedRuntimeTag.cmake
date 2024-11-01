1dd4ef88dfe9bc2697d98cf43a20a6624a421676
Neil R. Spruit <neil.r.spruit@intel.com>
2024-11-01 14:14:39 -0700

    [UR][L0] Update Level Zero Loader version to v1.18.5
    -pre-commit-pr for https://github.com/oneapi-src/unified-runtime/pull/2279
    
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Fri Nov 1 10:03:37 2024 +0000
 #     Merge pull request #1833 from ayylol/old-opencl-check
 #     [OpenCL] Add OpenCL version check for independent forward progress query
-set(UNIFIED_RUNTIME_TAG fa8cc8ec16c1a2cf0926cc64026edc6a254ff0c2)
+set(UNIFIED_RUNTIME_TAG 090feebf455335576a3ab59e07db451ee79f5675)
