e15a0ab7606ab37030749e91caec9d790dc738ed
Callum Fare <callum@codeplay.com>
2024-11-05 17:44:48 +0000

    [UR] Bump UR tag to get L0 default init refactor (#15987)
    Bump UR to pull in
    https://github.com/oneapi-src/unified-runtime/pull/2260

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit b0a9e2be61ad42d3447f1f246120ab25119a03e0
-# Merge: fa8cc8ec a0cf2ce2
+# commit f01741af022cfe82afcb026b9aa0be251eb6a497
+# Merge: 004d2474 85bb5f62
 # Author: Callum Fare <callum@codeplay.com>
-# Date:   Mon Nov 4 10:00:08 2024 +0000
-#     Merge pull request #2165 from aarongreig/aaron/makeUSMPoolsOptional
-#     Make USM pools optional with a device query to report support.
-set(UNIFIED_RUNTIME_TAG b0a9e2be61ad42d3447f1f246120ab25119a03e0)
+# Date:   Tue Nov 5 13:39:53 2024 +0000
+#     Merge pull request #2260 from nrspruit/refactor_l0_default_init
+#     [L0] Refactor to remove default constructor inits
+set(UNIFIED_RUNTIME_TAG f01741af022cfe82afcb026b9aa0be251eb6a497)
