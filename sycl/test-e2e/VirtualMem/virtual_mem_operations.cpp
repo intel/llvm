4be491809107ce8ab337debcbb686d11571fa89a
Skrobot, Daniel <daniel.skrobot@intel.com>
2024-11-05 13:21:04 +0100

    formatting fixed

--- sycl/test-e2e/VirtualMem/virtual_mem_operations.cpp
+++ sycl/test-e2e/VirtualMem/virtual_mem_operations.cpp
@@ -1,4 +1,5 @@
-// This test checks whether certain operations in virtual memory extension work as expectd.
+// This test checks whether certain operations in virtual memory extension work
+// as expectd.
 
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
@@ -8,103 +9,113 @@
 int main() {
 
   constexpr size_t NumberOfIterations = 3;
-  std::array<size_t, NumberOfIterations>  NumberOfElementsPerIteration{10,100,1000};
-  
+  std::array<size_t, NumberOfIterations> NumberOfElementsPerIteration{10, 100,
+                                                                      1000};
+
   sycl::queue Q;
   sycl::context Context = Q.get_context();
-  sycl::device Device = Q.get_device(); 
-  
-  //A check should be performed that we can successfully perform and immediately release a valid reservation.
-  for(const size_t RequiredNumElements: NumberOfElementsPerIteration){
+  sycl::device Device = Q.get_device();
+
+  // A check should be performed that we can successfully perform and
+  // immediately release a valid reservation.
+  for (const size_t RequiredNumElements : NumberOfElementsPerIteration) {
     size_t BytesRequired = RequiredNumElements * sizeof(int);
     size_t UsedGranularity = GetLCMGranularity(Device, Context);
     size_t AlignedByteSize = GetAlignedByteSize(BytesRequired, UsedGranularity);
-    uintptr_t VirtualMemoryPtr = syclext::reserve_virtual_mem(0, AlignedByteSize, Context);
+    uintptr_t VirtualMemoryPtr =
+        syclext::reserve_virtual_mem(0, AlignedByteSize, Context);
     syclext::free_virtual_mem(VirtualMemoryPtr, AlignedByteSize, Context);
   }
 
-
-  // A check should be performed that we can successfully map and immediately unmap a virtual memory range.
-  for(const size_t RequiredNumElements: NumberOfElementsPerIteration){
+  // A check should be performed that we can successfully map and immediately
+  // unmap a virtual memory range.
+  for (const size_t RequiredNumElements : NumberOfElementsPerIteration) {
     size_t BytesRequired = RequiredNumElements * sizeof(int);
     size_t UsedGranularity = GetLCMGranularity(Device, Context);
     size_t AlignedByteSize = GetAlignedByteSize(BytesRequired, UsedGranularity);
-    uintptr_t VirtualMemoryPtr = syclext::reserve_virtual_mem(0, AlignedByteSize, Context);
+    uintptr_t VirtualMemoryPtr =
+        syclext::reserve_virtual_mem(0, AlignedByteSize, Context);
     syclext::physical_mem PhysicalMem{Device, Context, AlignedByteSize};
-    void* MappedPtr =
-          PhysicalMem.map(VirtualMemoryPtr, AlignedByteSize,
-                               syclext::address_access_mode::read_write);
+    void *MappedPtr = PhysicalMem.map(VirtualMemoryPtr, AlignedByteSize,
+                                      syclext::address_access_mode::read_write);
     syclext::unmap(MappedPtr, AlignedByteSize, Context);
     syclext::free_virtual_mem(VirtualMemoryPtr, AlignedByteSize, Context);
   }
 
   {
-  // Check should be performed that methods get_context(), get_device() and size() return correct values (i.e. ones which were passed to physical_mem constructor).
-  size_t BytesRequired = NumberOfElementsPerIteration[2] * sizeof(int);
-  size_t UsedGranularity = GetLCMGranularity(Device, Context);
-  size_t AlignedByteSize = GetAlignedByteSize(BytesRequired, UsedGranularity);
-  
-  syclext::physical_mem PhysicalMem{Device, Context, AlignedByteSize};
+    // Check should be performed that methods get_context(), get_device() and
+    // size() return correct values (i.e. ones which were passed to physical_mem
+    // constructor).
+    size_t BytesRequired = NumberOfElementsPerIteration[2] * sizeof(int);
+    size_t UsedGranularity = GetLCMGranularity(Device, Context);
+    size_t AlignedByteSize = GetAlignedByteSize(BytesRequired, UsedGranularity);
 
-  PhysicalMem.get_context();
-  PhysicalMem.get_device();
-  PhysicalMem.size();
+    syclext::physical_mem PhysicalMem{Device, Context, AlignedByteSize};
 
-  assert(PhysicalMem.get_device() == Device &&
-         "device passed to physical_mem must be the same as returned from get_device()");
+    PhysicalMem.get_context();
+    PhysicalMem.get_device();
+    PhysicalMem.size();
 
-  assert(PhysicalMem.get_context() == Context &&
-         "context passed to physical_mem must be the same as returned from get_context()");
+    assert(PhysicalMem.get_device() == Device &&
+           "device passed to physical_mem must be the same as returned from "
+           "get_device()");
 
-  assert(PhysicalMem.size() == AlignedByteSize &&
-         "size in bytes passed to physical_mem must be the same as returned from size()");
+    assert(PhysicalMem.get_context() == Context &&
+           "context passed to physical_mem must be the same as returned from "
+           "get_context()");
+
+    assert(PhysicalMem.size() == AlignedByteSize &&
+           "size in bytes passed to physical_mem must be the same as returned "
+           "from size()");
   }
 
   {
-  // Check to see if value returned from a valid call to map() is the same as reinterpret_cast<void *>(ptr).
-  size_t BytesRequired = NumberOfElementsPerIteration[2] * sizeof(int);
-  size_t UsedGranularity = GetLCMGranularity(Device, Context);
-  size_t AlignedByteSize = GetAlignedByteSize(BytesRequired, UsedGranularity);
+    // Check to see if value returned from a valid call to map() is the same as
+    // reinterpret_cast<void *>(ptr).
+    size_t BytesRequired = NumberOfElementsPerIteration[2] * sizeof(int);
+    size_t UsedGranularity = GetLCMGranularity(Device, Context);
+    size_t AlignedByteSize = GetAlignedByteSize(BytesRequired, UsedGranularity);
 
-  uintptr_t VirtualMemoryPtr =
-      syclext::reserve_virtual_mem(0, AlignedByteSize, Context);
+    uintptr_t VirtualMemoryPtr =
+        syclext::reserve_virtual_mem(0, AlignedByteSize, Context);
 
-  syclext::physical_mem PhysicalMem{Device, Context, AlignedByteSize};
+    syclext::physical_mem PhysicalMem{Device, Context, AlignedByteSize};
 
-  void *MappedPtr =
-      PhysicalMem.map(VirtualMemoryPtr, AlignedByteSize,
-                              syclext::address_access_mode::read_write);
+    void *MappedPtr = PhysicalMem.map(VirtualMemoryPtr, AlignedByteSize,
+                                      syclext::address_access_mode::read_write);
 
-  assert(MappedPtr == reinterpret_cast<void*>(VirtualMemoryPtr) &&
-         "ptrs arent equal");
+    assert(MappedPtr == reinterpret_cast<void *>(VirtualMemoryPtr) &&
+           "ptrs arent equal");
 
-  syclext::unmap(MappedPtr, AlignedByteSize, Context);
-  syclext::free_virtual_mem(VirtualMemoryPtr, AlignedByteSize, Context);
+    syclext::unmap(MappedPtr, AlignedByteSize, Context);
+    syclext::free_virtual_mem(VirtualMemoryPtr, AlignedByteSize, Context);
   }
-  
-  // Check to see if can change access mode of a virtual memory range and immediately see it changed.
-   for(const size_t RequiredNumElements: NumberOfElementsPerIteration){
+
+  // Check to see if can change access mode of a virtual memory range and
+  // immediately see it changed.
+  for (const size_t RequiredNumElements : NumberOfElementsPerIteration) {
     size_t BytesRequired = RequiredNumElements * sizeof(int);
     size_t UsedGranularity = GetLCMGranularity(Device, Context);
     size_t AlignedByteSize = GetAlignedByteSize(BytesRequired, UsedGranularity);
-    uintptr_t VirtualMemoryPtr = syclext::reserve_virtual_mem(0, AlignedByteSize, Context);
+    uintptr_t VirtualMemoryPtr =
+        syclext::reserve_virtual_mem(0, AlignedByteSize, Context);
     syclext::physical_mem PhysicalMem{Device, Context, AlignedByteSize};
-    void* MappedPtr =
-          PhysicalMem.map(VirtualMemoryPtr, AlignedByteSize,
-                               syclext::address_access_mode::read_write);
-    
-    
+    void *MappedPtr = PhysicalMem.map(VirtualMemoryPtr, AlignedByteSize,
+                                      syclext::address_access_mode::read_write);
+
     syclext::set_access_mode(MappedPtr, AlignedByteSize,
-                           syclext::address_access_mode::read, Context);
-    
-    syclext::address_access_mode CurrentAccessMode = syclext::get_access_mode(MappedPtr, AlignedByteSize, Context);
+                             syclext::address_access_mode::read, Context);
+
+    syclext::address_access_mode CurrentAccessMode =
+        syclext::get_access_mode(MappedPtr, AlignedByteSize, Context);
 
     assert(CurrentAccessMode == syclext::address_access_mode::read &&
-         "access mode must be address_access_mode::read after change with set_access_mode()");
+           "access mode must be address_access_mode::read after change with "
+           "set_access_mode()");
 
     syclext::unmap(MappedPtr, AlignedByteSize, Context);
     syclext::free_virtual_mem(VirtualMemoryPtr, AlignedByteSize, Context);
   }
-  
+
   return 0;
 }
