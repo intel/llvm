5edd26155994964b7b17b6f8f43de99c7534d944
Fábio Mestre <fabio.m.mestre@gmail.com>
2024-11-06 15:29:08 +0000

    Update UnifiedRuntimeTag.cmake

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Tue Nov 5 13:39:53 2024 +0000
 #     Merge pull request #2260 from nrspruit/refactor_l0_default_init
 #     [L0] Refactor to remove default constructor inits
-set(UNIFIED_RUNTIME_TAG 6e3cd3ee78d60c0c4a6b6db2364594fd95d9e535)
+set(UNIFIED_RUNTIME_TAG 3a78b63a3ba2227b9a7c35eabb981489d727a66d)
