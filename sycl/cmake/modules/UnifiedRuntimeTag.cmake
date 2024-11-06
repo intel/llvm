cda38dedd238a0ce9acd3d860be58ae247429885
Benjamin Tracy <ben.tracy@codeplay.com>
2024-11-06 14:46:20 +0000

    [SYCL][Graph]Fix and add E2E tests for using local accessors in graphs (#15920)
    - Update UR tag for fix for updating local accessors on CUDA/HIP
    - Add e2e tests covering local accessor usage
    
    ---------
    
    Co-authored-by: Callum Fare <callum@codeplay.com>
    Co-authored-by: Ewan Crawford <ewan@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit f01741af022cfe82afcb026b9aa0be251eb6a497
-# Merge: 004d2474 85bb5f62
-# Author: Callum Fare <callum@codeplay.com>
-# Date:   Tue Nov 5 13:39:53 2024 +0000
-#     Merge pull request #2260 from nrspruit/refactor_l0_default_init
-#     [L0] Refactor to remove default constructor inits
-set(UNIFIED_RUNTIME_TAG f01741af022cfe82afcb026b9aa0be251eb6a497)
+# commit 3edf99755ce2af3b53102a7d8438e0fe969efac3
+# Merge: 5955bad3 0b968661
+# Author: Ross Brunton <ross@codeplay.com>
+# Date:   Wed Nov 6 11:07:29 2024 +0000
+#    Merge pull request #2082 from RossBrunton/ross/multiadapt
+#    [CI] Add "loader" support to conformance testing
+set(UNIFIED_RUNTIME_TAG 3edf99755ce2af3b53102a7d8438e0fe969efac3)
