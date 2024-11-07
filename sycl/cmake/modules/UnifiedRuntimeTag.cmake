dd2390c50259f24278a0ab866cf7898952a3c04a
Rafal Bielski <rafal.bielski@codeplay.com>
2024-11-07 09:34:45 +0000

    [UR][HIP] Set the right HIP device before creating base event counter (#15964)
    Update UR tag to include
    https://github.com/oneapi-src/unified-runtime/pull/2276
    
    ---------
    
    Co-authored-by: Callum Fare <callum@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 3edf99755ce2af3b53102a7d8438e0fe969efac3
-# Merge: 5955bad3 0b968661
-# Author: Ross Brunton <ross@codeplay.com>
-# Date:   Wed Nov 6 11:07:29 2024 +0000
-#    Merge pull request #2082 from RossBrunton/ross/multiadapt
-#    [CI] Add "loader" support to conformance testing
-set(UNIFIED_RUNTIME_TAG 3edf99755ce2af3b53102a7d8438e0fe969efac3)
+# commit 2858a8a28d0b6524a3b2b0e25a597d1c8295ce9d
+# Merge: a5a649f3 34b66fda
+# Author: Callum Fare <callum@codeplay.com>
+# Date:   Wed Nov 6 16:45:49 2024 +0000
+#     Merge pull request #2276 from rafbiels/rafbiels/fix-hip-evbase
+#     Set the right HIP device before creating base event counter
+set(UNIFIED_RUNTIME_TAG 2858a8a28d0b6524a3b2b0e25a597d1c8295ce9d)
