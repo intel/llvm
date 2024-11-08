dbadad3aebee253d2c7c5d6ae052bf6e31ac98a3
Neil R. Spruit <neil.r.spruit@intel.com>
2024-11-08 08:57:24 -0800

    [UR][L0] Implement Support for zeInitDrivers
    -pre-commit PR for https://github.com/oneapi-src/unified-runtime/pull/2303
    
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Fri Nov 22 15:27:05 2024 +0000
 #     Merge pull request #2370 from againull/multi_device_sanitizer
 #     [L0] Check that program is in exe state in urProgramGetGlobalVariablePointer
-set(UNIFIED_RUNTIME_TAG 2f479e01f62ed96a9466d19d9df1aabeda77b335)
+set(UNIFIED_RUNTIME_TAG 91b6db04e70f6554fd07314bdc392da1f7ee9d40)
