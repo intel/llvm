dc12f5a8082bc154bce4317d62ff888826618ced
Aaron Greig <aaron.greig@codeplay.com>
2024-11-08 13:21:43 +0000

    [UR] Explicitly specify USMFree as a non-blocking operation.

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,1 @@
-# commit c94dbc8fa25f62b812a3db707b78f1c217b33bc3
-# Merge: 9ae8e65b 5bc8c925
-# Author: Callum Fare <callum@codeplay.com>
-# Date:   Fri Nov 8 17:14:06 2024 +0000
-#     Merge pull request #2302 from callumfare/callum/revert_ur_2100
-#     Revert #2100 "Raise MSVC warning level from /W3 to /W4"
-set(UNIFIED_RUNTIME_TAG c94dbc8fa25f62b812a3db707b78f1c217b33bc3)
+set(UNIFIED_RUNTIME_TAG aaron/clarifyNonBlockingFree)
