54232144683124702e95f8026c11e9d90aaf8705
Kornev, Nikita <nikita.kornev@intel.com>
2024-11-11 11:08:33 -0800

    delete runx test

--- sycl/test/e2e_test_requirements/no-runx-without-tracker.cpp
+++
@@ -1,6 +0,0 @@
-// This test is intended to ensure that we have no tests marked as RUNx.
-// If it fails - please remove RUNx and provide a TODO with a tracker.
-//
-// REQUIRES: linux
-//
-// RUN: not grep -rI "RUNx:" %S/../../test-e2e
