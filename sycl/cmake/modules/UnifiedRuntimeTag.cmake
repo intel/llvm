f215f43c8b059e26088385babc080b66e673eb30
Callum Fare <callum@codeplay.com>
2024-11-11 10:04:15 +0000

    [UR] Bump UR tag to c94dbc8 (#16033)
    Pulls in https://github.com/oneapi-src/unified-runtime/pull/2302

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit ed9fe09f9987bbe86715f191e6dbe5695ebf0306
-# Merge: 09ae26af dc5246dd
+# commit c94dbc8fa25f62b812a3db707b78f1c217b33bc3
+# Merge: 9ae8e65b 5bc8c925
 # Author: Callum Fare <callum@codeplay.com>
-# Date:   Fri Nov 8 10:29:25 2024 +0000
-#     Merge pull request #2279 from nrspruit/update_loader_1_18
-#     [L0] Update Level Zero Loader version to v1.18.5
-set(UNIFIED_RUNTIME_TAG ed9fe09f9987bbe86715f191e6dbe5695ebf0306)
+# Date:   Fri Nov 8 17:14:06 2024 +0000
+#     Merge pull request #2302 from callumfare/callum/revert_ur_2100
+#     Revert #2100 "Raise MSVC warning level from /W3 to /W4"
+set(UNIFIED_RUNTIME_TAG c94dbc8fa25f62b812a3db707b78f1c217b33bc3)
