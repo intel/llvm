8965b8b6c58d67869697620ce836f62c57a322af
Callum Fare <callum@codeplay.com>
2024-11-11 13:37:46 +0000

    Bump UR tag

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,2 +1,7 @@
-set(UNIFIED_RUNTIME_TAG 2a081891b69ce881be19ab61a92394c8473786da)
-
+# commit 2eae687a4cf24ba02ee8e9ebb9552c1d392972ee
+# Merge: 1ba7f39a 2a081891
+# Author: Callum Fare <callum@codeplay.com>
+# Date:   Mon Nov 11 13:35:24 2024 +0000
+#     Merge pull request #2112 from martygrant/martin/context-cts-spec-gap
+#     Improvements to align CTS and Spec for Context
+set(UNIFIED_RUNTIME_TAG 2eae687a4cf24ba02ee8e9ebb9552c1d392972ee)
