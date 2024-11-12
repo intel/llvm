d4e0e6c980bcceeb9676d720edb849c197d561b8
Sidorov, Dmitry <dmitry.sidorov@intel.com>
2024-11-12 05:51:45 -0800

    [SYCL] Add -f-offload-fp32-prec-[div/sqrt] FE option handling in JIT
    It matches -cl-fp32-correctly-rounded-divide-[div/sqrt] option for OpenCL
    and -ze-fp32-correctly-rounded-divide-[div/sqrt] for L0.
    
    Signed-off-by: Sidorov, Dmitry <dmitry.sidorov@intel.com>

--- source/adapters/level_zero/platform.cpp
+++ source/adapters/level_zero/platform.cpp
@@ -196,6 +196,14 @@ ur_result_t urPlatformGetBackendOption(
     *PlatformOption = "-igc_opts 'PartitionUnit=1,SubroutineThreshold=50000'";
     return UR_RESULT_SUCCESS;
   }
+  if (pFrontendOption == "-foffload-fp32-prec-div") {
+    *ppPlatformOption = "-ze-fp32-correctly-rounded-divide-div";
+     return UR_RESULT_SUCCESS;
+  }
+  if (pFrontendOption == "-foffload-fp32-prec-sqrt") {
+    *ppPlatformOption = "-ze-fp32-correctly-rounded-divide-sqrt";
+    return UR_RESULT_SUCCESS;
+  }
   return UR_RESULT_ERROR_INVALID_VALUE;
 }
 
