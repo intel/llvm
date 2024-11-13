48b6502ecd2fff8d8c8748e07980a39e6f068553
Sarnie, Nick <nick.sarnie@intel.com>
2024-11-13 13:50:28 -0800

    add -fsycl to test
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- sycl/test-e2e/Matrix/runtime_query_hip_gfx90a.cpp
+++ sycl/test-e2e/Matrix/runtime_query_hip_gfx90a.cpp
@@ -7,7 +7,7 @@
 //===----------------------------------------------------------------------===//
 
 // REQUIRES: arch-amd_gpu_gfx90a
-// RUN: %clangxx -Xsycl-target-backend=amdgcn-amd-amdhsa --offload-arch=gfx90a -o %t.out
+// RUN: %clangxx -fsycl -Xsycl-target-backend=amdgcn-amd-amdhsa --offload-arch=gfx90a -o %t.out
 // RUN: %{run} %t.out
 
 #include <sycl/detail/core.hpp>
