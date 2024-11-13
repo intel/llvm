9082959fe2cddd7e59fdf8cbc1e4083ed6837b30
Maronas, Marcos <marcos.maronas@intel.com>
2024-11-13 13:45:52 +0100

    Fix build failure.

--- clang/tools/clang-linker-wrapper/ClangLinkerWrapper.cpp
+++ clang/tools/clang-linker-wrapper/ClangLinkerWrapper.cpp
@@ -676,8 +676,7 @@ getTripleBasedSYCLPostLinkOpts(const ArgList &Args,
   if ((!Args.hasFlag(OPT_no_sycl_remove_unused_external_funcs,
                      OPT_sycl_remove_unused_external_funcs, false) &&
        !SYCLNativeCPU) &&
-      !Args.hasFlag(OPT_sycl_allow_device_image_dependencies,
-                    OPT_no_sycl_allow_device_image_dependencies, false) &&
+      !Args.hasArg(OPT_sycl_allow_device_image_dependencies) &&
       !Triple.isNVPTX() && !Triple.isAMDGPU())
     PostLinkArgs.push_back("-emit-only-kernels-as-entry-points");
 
