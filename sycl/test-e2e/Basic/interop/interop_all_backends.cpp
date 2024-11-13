ebaf36e5682078a4a7308a9bab01bd637c945362
Klochkov, Denis <denis.klochkov@intel.com>
2024-11-13 00:45:27 -0800

    [SYCL][E2E] fix cuda test build option

--- sycl/test-e2e/Basic/interop/interop_all_backends.cpp
+++ sycl/test-e2e/Basic/interop/interop_all_backends.cpp
@@ -1,5 +1,5 @@
 // RUN: %if any-device-is-opencl %{ %{build} -o %t-opencl.out %}
-// RUN: %if any-device-is-cuda %{ %{build} -isystem %sycl_include -DBUILD_FOR_CUDA -DSYCL_EXT_ONEAPI_BACKEND_CUDA -DSYCL_EXT_ONEAPI_BACKEND_CUDA_EXPERIMENTAL -o %t-cuda.out %}
+// RUN: %if any-device-is-cuda %{ %{build} -isystem %sycl_include -DBUILD_FOR_CUDA -DSYCL_EXT_ONEAPI_BACKEND_CUDA -o %t-cuda.out %}
 // RUN: %if any-device-is-hip %{ %{build} -DBUILD_FOR_HIP -o %t-hip.out %}
 
 #include <sycl/backend.hpp>
