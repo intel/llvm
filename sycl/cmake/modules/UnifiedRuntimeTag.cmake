f989c3f9672101fb4790cfe7a7de1b229965aeb9
chedy.najjar <chedy.najjar@codeplay.com>
2024-11-13 15:04:56 +0000

    update tag

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Wed Nov 13 09:57:16 2024 +0000
 #     Merge pull request #2254 from PietroGhg/pietro/events_rr
 #     [NATIVECPU] Implement events on Native CPU
-set(UNIFIED_RUNTIME_TAG cd92e72bbc4ebddef63c63c0f7e66a410f4b9552)
+set(UNIFIED_RUNTIME_TAG 698cd9974c18874bf6a7e9ec78507c30b9deeca7)
