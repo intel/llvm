d8aceb12ff89e9a16f008b7922f6f94451910bed
JackAKirk <jack.kirk@codeplay.com>
2024-11-14 09:36:59 -0800

    Check default addrspace used for non cuda/hip
    Signed-off-by: JackAKirk <jack.kirk@codeplay.com>

--- sycl/test/check_device_code/device_global_const.cpp
+++ sycl/test/check_device_code/device_global_const.cpp
@@ -1,6 +1,7 @@
 // RUN: %clangxx -fsycl -fsycl-device-only %if cuda %{ -fsycl-targets=nvptx64-nvidia-cuda %} %if hip-amd %{ -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx90a %} -S -emit-llvm %s -o - | FileCheck %s  %if cuda || hip-amd %{ --check-prefixes=CHECK-CONST %}
 
-// Tests that const T device_global uses const address space for cuda/hip
+// Tests that const T device_global uses const address space for cuda/hip and
+// global address space otherwise
 
 #include <sycl/sycl.hpp>
 
@@ -13,6 +14,7 @@ int main() {
   queue Q;
   Q.single_task([]() {
     // CHECK-CONST: (ptr addrspace(4) @DeviceGlobalVar
+    // CHECK: (ptr addrspace(1) @DeviceGlobalVar
     volatile int ReadVal = DeviceGlobalVar;
   });
   return 0;
