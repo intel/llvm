3adb475178f1ba5ebad55c72fddfb7020bc6125a
Steffen Larsen <steffen.larsen@intel.com>
2024-11-14 13:51:28 +0100

    Revert "[DeviceASAN] Enable e2e test "private_nullptr.cpp" (#15995)"
    This reverts commit c0015204bfd926e979ec37395af03a3daca35b39.

--- sycl/test/e2e_test_requirements/no-xfail-without-tracker.cpp
+++ sycl/test/e2e_test_requirements/no-xfail-without-tracker.cpp
@@ -51,12 +51,13 @@
 // tests to match the required format and in that case you should just update
 // (i.e. reduce) the number and the list below.
 //
-// NUMBER-OF-XFAIL-WITHOUT-TRACKER: 141
+// NUMBER-OF-XFAIL-WITHOUT-TRACKER: 142
 //
 // List of improperly XFAIL-ed tests.
 // Remove the CHECK once the test has been properly XFAIL-ed.
 //
-// CHECK: Basic/aspects.cpp
+// CHECK: AddressSanitizer/nullpointer/private_nullptr.cpp
+// CHECK-NEXT: Basic/aspects.cpp
 // CHECK-NEXT: Basic/buffer/reinterpret.cpp
 // CHECK-NEXT: Basic/device_event.cpp
 // CHECK-NEXT: Basic/diagnostics/handler.cpp
