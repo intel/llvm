84f0f1182412015e448e6c42b06a7a4a71954db4
JackAKirk <jack.kirk@codeplay.com>
2024-11-14 09:38:47 -0800

    Improve comment
    Signed-off-by: JackAKirk <jack.kirk@codeplay.com>

--- sycl/test/check_device_code/device_global_const.cpp
+++ sycl/test/check_device_code/device_global_const.cpp
@@ -1,6 +1,6 @@
 // RUN: %clangxx -fsycl -fsycl-device-only %if cuda %{ -fsycl-targets=nvptx64-nvidia-cuda %} %if hip-amd %{ -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx90a %} -S -emit-llvm %s -o - | FileCheck %s  %if cuda || hip-amd %{ --check-prefixes=CHECK-CONST %}
 
-// Tests that const T device_global uses const address space for cuda/hip and
+// Tests that device_global<const T> uses const address space for cuda/hip and
 // global address space otherwise
 
 #include <sycl/sycl.hpp>
