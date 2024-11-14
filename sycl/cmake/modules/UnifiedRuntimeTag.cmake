37aa036d946353b15772e02a1e435dee69713dc3
Artur Gainullin <artur.gainullin@intel.com>
2024-11-14 07:37:07 -0800

    UR PR was merged - update tag

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,4 +1,4 @@
-# commit 6c54509c921c5f788ff35456b0fa8c882d9b4213 (HEAD -> main)
+# commit 3e1a0ea44842b10cfdc0e1e98af2eee8fcd8e937 (HEAD -> main)
 # Author: Artur Gainullin <artur.gainullin@intel.com>
 # Date:   Wed Nov 6 16:05:50 2024 -0800
 #
@@ -7,4 +7,4 @@
 #     Currently urProgramGetInfo will return UR_INVALID_PROGRAM is program is
 #     compiled only for a subset of associated devices, i.e. not all devices
 #     have level zero module and binaries. This PR fixes this behaviour.
-set(UNIFIED_RUNTIME_TAG 6c54509c921c5f788ff35456b0fa8c882d9b4213)
+set(UNIFIED_RUNTIME_TAG 3e1a0ea44842b10cfdc0e1e98af2eee8fcd8e937)
