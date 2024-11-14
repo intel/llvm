d64e9f12a6ffbd8b8d84593851deaccb58cfe675
JackAKirk <jack.kirk@codeplay.com>
2024-11-14 07:13:43 -0800

    Add device code test/Update ext spec with note
    Signed-off-by: JackAKirk <jack.kirk@codeplay.com>

---
+++ sycl/test/check_device_code/device_global_const.cpp
@@ -0,0 +1,19 @@
+// RUN: %clangxx -fsycl -fsycl-device-only %if cuda %{ -fsycl-targets=nvptx64-nvidia-cuda %} %if hip-amd %{ -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx90a %} -S -emit-llvm %s -o - | FileCheck %s  %if cuda || hip-amd %{ --check-prefixes=CHECK-CONST %}
+
+// Tests that const T device_global uses const address space for cuda/hip
+
+#include <sycl/sycl.hpp>
+
+using namespace sycl;
+using namespace sycl::ext::oneapi::experimental;
+
+device_global<const int> DeviceGlobalVar;
+
+int main() {
+  queue Q;
+  Q.single_task([]() {
+    // CHECK-CONST: (ptr addrspace(4) @DeviceGlobalVar
+    volatile int ReadVal = DeviceGlobalVar;
+  });
+  return 0;
+}
