a8a18e23cc5fbb617b418d952742cd53fb2e234b
Martin Morrison-Grant <martin.morrisongrant@codeplay.com>
2024-11-14 11:07:54 +0000

    [UR] Update tag to 1f68788 for UR#2326 - Index into property array in urDevicePartition for OpenCL.

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Wed Nov 20 14:49:17 2024 +0000
 #     Merge pull request #2258 from aarongreig/aaron/tryUseExtensionSubgroupInfo
 #     Use extension version of clGetKernelSubGroupInfo when necessary.
-set(UNIFIED_RUNTIME_TAG 9937d029c7fdcbf101e89f8515f640c145e059c5)
+set(UNIFIED_RUNTIME_TAG 1f68788020e2f13602cd209e6e59ede6626185d4)
