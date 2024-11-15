99e753bb58676dd0e4d273a6e433bb9c5444e44d
Hugh Delaney <hugh.delaney@codeplay.com>
2024-11-15 14:18:12 +0000

    Update REPO and TAG

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,1 @@
-# commit 098deca1f9f3b9f3f0563ee823ac424d8db30668
-# Merge: 58e4d76c2ace 73ba29bfe9df
-# Author: Martin Grant <martin.morrisongrant@codeplay.com>
-# Date:   Wed Dec 11 17:23:43 2024 +0000
-#     Merge pull request #2299 from cppchedy/chedy/fix-mipmap-leak
-#     [CUDA][Bindless] Fix memory leak in interop mapping
-set(UNIFIED_RUNTIME_TAG 098deca1f9f3b9f3f0563ee823ac424d8db30668)
+set(UNIFIED_RUNTIME_TAG 428a480f3942a69336172ecdcf53d22d5f13414d)
