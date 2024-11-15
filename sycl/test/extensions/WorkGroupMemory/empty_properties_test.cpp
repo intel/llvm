7ecab94d45b265458f8e42e28371b99ec557b551
Lorenc Bushi <lorenc.bushi@intel.com>
2024-11-15 11:54:33 -0800

    Fix error in test logic

--- sycl/test/extensions/WorkGroupMemory/empty_properties_test.cpp
+++ sycl/test/extensions/WorkGroupMemory/empty_properties_test.cpp
@@ -10,7 +10,7 @@ namespace syclexp = sycl::ext::oneapi::experimental;
 
 template <typename PropertyListT, typename... PropertyListTs>
 void test_properties() {
-  // expected-error-re@sycl/ext/oneapi/experimental/work_group_memory.hpp:* 2{{static assertion failed due to requirement 'std::is_same_v<{{.*}}, sycl::ext::oneapi::experimental::properties<{{.*}}>>'}}
+  // expected-error-re@sycl/ext/oneapi/experimental/work_group_memory.hpp:* 2{{static assertion failed due to requirement 'std::is_same_v<{{.*}}, sycl::ext::oneapi::experimental::properties<sycl::ext::oneapi::experimental::detail::properties_type_list<>>>'}}
   syclexp::work_group_memory<int, PropertyListT>{syclexp::indeterminate};
   if constexpr (sizeof...(PropertyListTs))
     test_properties<PropertyListTs...>();
