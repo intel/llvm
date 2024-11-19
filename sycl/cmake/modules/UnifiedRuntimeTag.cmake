94bb685fa2b9a9f42a9f28a963e3b2325758e05a
Victor Lomuller <victor@codeplay.com>
2024-11-19 14:11:12 +0000

    update ur tag

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Fri Nov 15 15:13:20 2024 +0000
 #     Merge pull request #2222 from RossBrunton/ross/cfi
 #     Enable -flto and -fsanitize=cfi in clang
-set(UNIFIED_RUNTIME_TAG ae9804cefc2d777360d652659f07d7e67d1fba45)
+set(UNIFIED_RUNTIME_TAG a64d6494956c3356c33d753370698d5f9eb0b8ac)
