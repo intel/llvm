b4ddbea9fab338bf9ec0fa3ce7cc6dd18cd2589b
Neil R. Spruit <neil.r.spruit@intel.com>
2024-11-19 09:08:49 -0800

    [UR][L0] Add support for the MCL 1.1 apis thru the spec extensions
    -pre-commit PR for https://github.com/oneapi-src/unified-runtime/pull/2351
    
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Wed Nov 20 14:49:17 2024 +0000
 #     Merge pull request #2258 from aarongreig/aaron/tryUseExtensionSubgroupInfo
 #     Use extension version of clGetKernelSubGroupInfo when necessary.
-set(UNIFIED_RUNTIME_TAG 9937d029c7fdcbf101e89f8515f640c145e059c5)
+set(UNIFIED_RUNTIME_TAG 3fc25b9cbded69d1bde8d2e677cc63b6ebefc223)
