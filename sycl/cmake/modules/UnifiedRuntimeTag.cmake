3c80bd95fd4a9acff5fab22a3cf0b594ac1748b9
Wenju He <wenju.he@intel.com>
2024-11-19 12:51:31 +0800

    [UR][CL] Fix macro to string conversion when getting function ptr

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,5 @@
-# commit 30391c65d2d2ccc7ee3688a14815804bfb7fdf05
-# Merge: 5e6d79b3 58dabfe8
-# Author: Callum Fare <callum@codeplay.com>
-# Date:   Fri Nov 15 15:13:20 2024 +0000
-#     Merge pull request #2222 from RossBrunton/ross/cfi
-#     Enable -flto and -fsanitize=cfi in clang
-set(UNIFIED_RUNTIME_TAG 30391c65d2d2ccc7ee3688a14815804bfb7fdf05)
+# commit 57fe505dce8213932861b6c4bd24e6be5adf6c85
+# Author: Wenju He <wenju.he@intel.com>
+# Date:   Tue Nov 19 12:11:02 2024 +0800
+#    [OpenCL] Fix macro to string conversion when getting function ptr
+set(UNIFIED_RUNTIME_TAG 57fe505dce8213932861b6c4bd24e6be5adf6c85)
