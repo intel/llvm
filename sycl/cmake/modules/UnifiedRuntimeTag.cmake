5fdc75fd2ca4c50e199f592a1fac2b050085c2f2
Callum Fare <callum@codeplay.com>
2024-11-19 14:54:55 +0000

    Bump UR tag

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,1 +1,7 @@
-set(UNIFIED_RUNTIME_TAG aaron/fixCoreFuncMacroWindows)
+# commit 1675f053176f0860388aa67fd009750a7e03b2c2
+# Merge: 0ea47d7c 22ca5ee2
+# Author: Callum Fare <callum@codeplay.com>
+# Date:   Tue Nov 19 14:41:15 2024 +0000
+#     Merge pull request #2337 from aarongreig/aaron/fixCoreFuncMacroWindows
+#     Fix the CL_CORE_FUNCTION macro on windows.
+set(UNIFIED_RUNTIME_TAG 1675f053176f0860388aa67fd009750a7e03b2c2)
