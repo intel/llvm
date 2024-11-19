f47c7e72699f880d38676a9e5ecba351db2ff2ac
Neil R. Spruit <neil.r.spruit@intel.com>
2024-11-19 07:16:31 -0800

    [UR][L0] fix Coverity issues in memory and timestamps
    -pre-commit PR for https://github.com/oneapi-src/unified-runtime/pull/2343
    
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Tue Nov 19 10:24:08 2024 +0000
 #     Merge pull request #1584 from zhaomaosu/simplify-device-global
 #     [DeviceSanitizer] Remove device global "__AsanDeviceGlobalCount"
-set(UNIFIED_RUNTIME_TAG 0ea47d7c70b9a21a3d90612a0a0e7525034e62f7)
+set(UNIFIED_RUNTIME_TAG c67652b5318fe6e2e2f31467f899c4d7b22fe952)
