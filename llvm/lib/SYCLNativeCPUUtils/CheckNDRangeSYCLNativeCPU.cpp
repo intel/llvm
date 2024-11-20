1d86faaf6e8ec07928ef0ab3896398f4f26e274a
PietroGhg <pietro.ghiglio@codeplay.com>
2024-11-20 15:08:09 +0000

    Check for function calls and local as global variables

--- llvm/lib/SYCLNativeCPUUtils/CheckNDRangeSYCLNativeCPU.cpp
+++ llvm/lib/SYCLNativeCPUUtils/CheckNDRangeSYCLNativeCPU.cpp
@@ -14,16 +14,22 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/SYCLLowerIR/CheckNDRangeSYCLNativeCPU.h"
+#include "llvm/ADT/PriorityWorklist.h"
+#include "llvm/ADT/SmallPtrSet.h"
 #include "llvm/IR/CallingConv.h"
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
 #include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Metadata.h"
+#include "llvm/SYCLLowerIR/UtilsSYCLNativeCPU.h"
+#include "llvm/Support/Casting.h"
 
 using namespace llvm;
 
-static std::array<const char *, 13> NdFunctions{
+static std::array<const char *, 13> NdBuiltins{
     "_Z23__spirv_WorkgroupSize_xv",     "_Z23__spirv_WorkgroupSize_yv",
     "_Z23__spirv_WorkgroupSize_zv",     "_Z23__spirv_NumWorkgroups_xv",
     "_Z23__spirv_NumWorkgroups_yv",     "_Z23__spirv_NumWorkgroups_zv",
@@ -42,6 +48,55 @@ static void addNDRangeMetadata(Function &F, bool Value) {
 PreservedAnalyses
 CheckNDRangeSYCLNativeCPUPass::run(Module &M, ModuleAnalysisManager &MAM) {
   bool ModuleChanged = false;
+  SmallPtrSet<Function *, 5> NdFuncs; // Functions that use NDRange features
+  SmallPtrSet<Function *, 5> Visited;
+  SmallPriorityWorklist<Function *, 5> WorkList;
+
+  // Add builtins to the set of functions that may use NDRange features
+  for (auto &FName : NdBuiltins) {
+    auto F = M.getFunction(FName);
+    if (F == nullptr)
+      continue;
+    WorkList.insert(F);
+    NdFuncs.insert(F);
+  }
+
+  // Add users of local AS global var to the set of functions that may use
+  // NDRange features
+  for (auto &GV : M.globals()) {
+    if (GV.getAddressSpace() != sycl::utils::SyclNativeCpuLocalAS)
+      continue;
+
+    for (auto U : GV.users()) {
+      if (auto I = dyn_cast<Instruction>(U)) {
+        auto F = I->getFunction();
+        if (F != nullptr && NdFuncs.insert(F).second) {
+          WorkList.insert(F);
+          NdFuncs.insert(F);
+        }
+      }
+    }
+  }
+
+  // Traverse the use chain to find Functions that may use NDRange features
+  // (or, recursively, Functions that call Functions that may use NDRange
+  // features)
+  while (!WorkList.empty()) {
+    auto F = WorkList.pop_back_val();
+
+    for (User *U : F->users()) {
+      if (auto CI = dyn_cast<CallInst>(U)) {
+        auto Caller = CI->getFunction();
+        if (!Caller)
+          continue;
+        if (!Visited.contains(Caller)) {
+          WorkList.insert(Caller);
+          NdFuncs.insert(Caller);
+        }
+      }
+    }
+    Visited.insert(F);
+  }
 
   for (auto &F : M) {
     if (F.getCallingConv() == llvm::CallingConv::SPIR_KERNEL) {
@@ -55,23 +110,11 @@ CheckNDRangeSYCLNativeCPUPass::run(Module &M, ModuleAnalysisManager &MAM) {
         }
       }
 
-      for (auto &BB : F) {
-        for (auto &I : BB) {
-          if (auto CI = dyn_cast<CallInst>(&I)) {
-            auto CalleeName = CI->getCalledFunction()->getName();
-            if (std::find(NdFunctions.begin(), NdFunctions.end(), CalleeName) !=
-                NdFunctions.end()) {
-              IsNDRange = true;
-              break;
-            }
-          }
-        }
-        if (IsNDRange) {
-          break;
-        }
-      }
+      // Check if the kernel calls one of the ND Range builtins
+      IsNDRange |= NdFuncs.contains(&F);
 
       addNDRangeMetadata(F, IsNDRange);
+      ModuleChanged = true;
     }
   }
   return ModuleChanged ? PreservedAnalyses::none() : PreservedAnalyses::all();
