71875dd4ec0cfd6063cbf68e1d18c1e312f71447
Georgi Mirazchiyski <georgi.mirazchiyski@codeplay.com>
2024-11-21 10:57:33 +0000

    [UR] Implement device info extensions for HW_THREADS_PER_EU and EU_SIMD_WIDTH on Cuda

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Wed Nov 20 14:49:17 2024 +0000
 #     Merge pull request #2258 from aarongreig/aaron/tryUseExtensionSubgroupInfo
 #     Use extension version of clGetKernelSubGroupInfo when necessary.
-set(UNIFIED_RUNTIME_TAG 9937d029c7fdcbf101e89f8515f640c145e059c5)
+set(UNIFIED_RUNTIME_TAG georgi/implement-ext-intel-hw-resident-threads-on-cuda)
