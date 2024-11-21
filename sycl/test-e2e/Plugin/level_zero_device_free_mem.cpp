8856ca4d07632f96d3c84d40085d98300505f5a4
Skrobot, Daniel <daniel.skrobot@intel.com>
2024-11-21 17:09:32 +0100

    fix

--- sycl/test-e2e/Plugin/level_zero_device_free_mem.cpp
+++ sycl/test-e2e/Plugin/level_zero_device_free_mem.cpp
@@ -1,4 +1,5 @@
 // REQUIRES: aspect-ext_intel_free_memory
+// REQUIRES: level_zero, level_zero_dev_kit
 //
 // RUN: %{build} %level_zero_options -o %t.out
 // RUN: env ZES_ENABLE_SYSMAN=1 %{run} %t.out 2>&1 | FileCheck %s
@@ -14,6 +15,7 @@
 using namespace sycl;
 
 int main() {
+  
   queue Queue;
   auto dev = Queue.get_device();
   std::cout << "Device: " << dev.get_info<info::device::name>() << std::endl;
