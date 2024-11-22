1aeb4559a46f7d3bb8ca0dbe9898a43471ce2ebb
Aaron Greig <aaron.greig@codeplay.com>
2024-11-22 11:31:18 +0000

    [UR] Pull in fixes for issues raised in latest coverity scan.

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,1 @@
-# commit 2f479e01f62ed96a9466d19d9df1aabeda77b335
-# Merge: b0f9293d b693389e
-# Author: Callum Fare <callum@codeplay.com>
-# Date:   Fri Nov 22 15:27:05 2024 +0000
-#     Merge pull request #2370 from againull/multi_device_sanitizer
-#     [L0] Check that program is in exe state in urProgramGetGlobalVariablePointer
-set(UNIFIED_RUNTIME_TAG 2f479e01f62ed96a9466d19d9df1aabeda77b335)
+set(UNIFIED_RUNTIME_TAG aaron/fixCoverity15-11)
