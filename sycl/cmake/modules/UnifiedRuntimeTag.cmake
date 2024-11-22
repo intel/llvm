1e1ffc019aeb389343abdc6e6a9e48b9d59242ab
Yang Zhao <yang2.zhao@intel.com>
2024-11-22 20:19:56 +0800

    [DeviceAsan][NFC] Code Restructure (#15843)
    UR: https://github.com/oneapi-src/unified-runtime/pull/2232
    
    Code refactoring for implementing MemorySanitizer
    
    ---------
    
    Co-authored-by: Piotr Balcer <piotr.balcer@intel.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,5 @@
-# commit 9937d029c7fdcbf101e89f8515f640c145e059c5
-# Merge: 9ac6d5d9 10b0e101
-# Author: Callum Fare <callum@codeplay.com>
-# Date:   Wed Nov 20 14:49:17 2024 +0000
-#     Merge pull request #2258 from aarongreig/aaron/tryUseExtensionSubgroupInfo
-#     Use extension version of clGetKernelSubGroupInfo when necessary.
-set(UNIFIED_RUNTIME_TAG 9937d029c7fdcbf101e89f8515f640c145e059c5)
+# commit b5294ee0762e9eb4919102e5663e724c152487bc
+# Author: Yang Zhao <yang2.zhao@intel.com>
+# Date:   Fri Nov 22 15:59:23 2024 +0800
+#    [DeviceASAN][NFC] Code Restructure (#2232)
+set(UNIFIED_RUNTIME_TAG b5294ee0762e9eb4919102e5663e724c152487bc)
