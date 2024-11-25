f4992941aa3fcf70034429b4ee56904858a24197
Aaron Greig <aaron.greig@codeplay.com>
2024-11-25 14:45:27 +0000

    Enable adapter tests to run on all discovered adapters.

--- test/conformance/source/main.cpp
+++ test/conformance/source/main.cpp
@@ -9,17 +9,14 @@ int main(int argc, char **argv) {
 #ifdef KERNELS_ENVIRONMENT
     auto *environment =
         new uur::KernelsEnvironment(argc, argv, KERNELS_DEFAULT_DIR);
-#endif
-#ifdef DEVICES_ENVIRONMENT
+#elif DEVICES_ENVIRONMENT
     auto *environment = new uur::DevicesEnvironment(argc, argv);
-#endif
-#ifdef PLATFORM_ENVIRONMENT
+#elif PLATFORM_ENVIRONMENT
     auto *environment = new uur::PlatformEnvironment(argc, argv);
+#else
+    auto *environment = new uur::AdapterEnvironment();
 #endif
     ::testing::InitGoogleTest(&argc, argv);
-#if defined(DEVICES_ENVIRONMENT) || defined(PLATFORM_ENVIRONMENT) ||           \
-    defined(KERNELS_ENVIRONMENT)
     ::testing::AddGlobalTestEnvironment(environment);
-#endif
     return RUN_ALL_TESTS();
 }
