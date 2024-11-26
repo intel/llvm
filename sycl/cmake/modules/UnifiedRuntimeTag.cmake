15853c7e961acc41805b92fb7ae031ea252454e5
Ben Tracy <ben.tracy@codeplay.com>
2024-11-26 15:01:04 +0000

    Bump UR tag

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,1 @@
-# commit 38ee6ce2a0400573c0c7c5da782bc32ff578fcc4
-# Merge: c7086f7f 91b6db04
-# Author: Callum Fare <callum@codeplay.com>
-# Date:   Mon Nov 25 11:04:37 2024 +0000
-#     Merge pull request #2303 from nrspruit/zeInitDrivers
-#     [L0] Implement Support for zeInitDrivers
-set(UNIFIED_RUNTIME_TAG 3a9480649ff21f07ee9bad167863832a0941acfe)
+set(UNIFIED_RUNTIME_TAG 6e0bdeb93483c165f3f0e926f6b2cd3edc00d0a7)
