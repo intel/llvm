aa8e9d97883c01972f21b7aceeac657df58fb1c0
Callum Fare <callum@codeplay.com>
2024-11-26 14:31:21 +0000

    Bump UR tag

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 9937d029c7fdcbf101e89f8515f640c145e059c5
-# Merge: 9ac6d5d9 10b0e101
+# commit e0f22b54080411537e8a12fe42f06dbac5e7a2d2
+# Merge: ab3cc0b7 6ba65403
 # Author: Callum Fare <callum@codeplay.com>
-# Date:   Wed Nov 20 14:49:17 2024 +0000
-#     Merge pull request #2258 from aarongreig/aaron/tryUseExtensionSubgroupInfo
-#     Use extension version of clGetKernelSubGroupInfo when necessary.
-set(UNIFIED_RUNTIME_TAG 6ba65403fbe6e25c2947e7c10a671b8d3f3ad208)
+# Date:   Tue Nov 26 13:32:11 2024 +0000
+#     Merge pull request #2351 from nrspruit/mcl_1_1
+#     [L0] Add support for the MCL 1.1 apis thru the spec extensions
+set(UNIFIED_RUNTIME_TAG e0f22b54080411537e8a12fe42f06dbac5e7a2d2)
