f018e768fd2814a6b761e2f8f669e585ca40186e
Ben Tracy <ben.tracy@codeplay.com>
2024-11-27 15:41:01 +0000

    [SYCL][Graph] Update UR tag for L0 kernel binary update (#16154)
    - Fix potential implicit conversion from queue to command_graph when
    using dynamic command groups
    - Remove XFAIL for dyn_cgf* tests on L0
    - Fix dyn_cgf_accessor_spv test and add new prebuilt spirv binary for it
    
    ---------
    
    Co-authored-by: Callum Fare <callum@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,5 +1,7 @@
-# commit f2af85f35957601dd91f81e8fad39fea413ccbf2
-# Author: Yang Zhao <yang2.zhao@intel.com>
-# Date:   Wed Nov 27 00:20:29 2024 +0800
-#     [DeviceSanitizer] Support "-fsanitize-ignorelist=" to disable sanitizing on some of kernels (#2055)
-set(UNIFIED_RUNTIME_TAG f2af85f35957601dd91f81e8fad39fea413ccbf2)
+# commit 0a90db9b2c36960c9b28ce18557ca15760724c4d
+# Merge: c4d9fdb4 6e0bdeb9
+# Author: Callum Fare <callum@codeplay.com>
+# Date:   Wed Nov 27 12:16:44 2024 +0000
+#     Merge pull request #2369 from Bensuo/ben/kernel-binary-update-l0
+#     [CMDBUF] Implement kernel binary update for L0 adapter
+set(UNIFIED_RUNTIME_TAG 0a90db9b2c36960c9b28ce18557ca15760724c4d)
