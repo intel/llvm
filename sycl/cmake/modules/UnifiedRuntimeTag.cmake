34554e684f1bd94e58b8bdaad75acc5dde26475f
Yang Zhao <yang2.zhao@intel.com>
2024-11-27 20:11:07 +0800

    [DeviceSanitizer] Support "-fsanitize-ignorelist=" to disable sanitizing on some of kernels (#15294)
    UR: https://github.com/oneapi-src/unified-runtime/pull/2055
    
    We reuse `-fsanitize-ignorelist=` to support kernel filtering.
    
    Usage: https://clang.llvm.org/docs/SanitizerSpecialCaseList.html
    
    We implement this feature by adding a device global
    "__AsanKernelMetadata" which records the sanitized kernel name, so that
    we can check if we need specially handle kernel in UR.
    
    ---------
    
    Co-authored-by: Callum Fare <callum@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,5 @@
-# commit e0f22b54080411537e8a12fe42f06dbac5e7a2d2
-# Merge: ab3cc0b7 6ba65403
-# Author: Callum Fare <callum@codeplay.com>
-# Date:   Tue Nov 26 13:32:11 2024 +0000
-#     Merge pull request #2351 from nrspruit/mcl_1_1
-#     [L0] Add support for the MCL 1.1 apis thru the spec extensions
-set(UNIFIED_RUNTIME_TAG e0f22b54080411537e8a12fe42f06dbac5e7a2d2)
+# commit f2af85f35957601dd91f81e8fad39fea413ccbf2
+# Author: Yang Zhao <yang2.zhao@intel.com>
+# Date:   Wed Nov 27 00:20:29 2024 +0800
+#     [DeviceSanitizer] Support "-fsanitize-ignorelist=" to disable sanitizing on some of kernels (#2055)
+set(UNIFIED_RUNTIME_TAG f2af85f35957601dd91f81e8fad39fea413ccbf2)
