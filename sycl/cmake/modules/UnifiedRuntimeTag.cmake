9b23258e5641691160c2c035f69e5d13c309f736
Callum Fare <callum@codeplay.com>
2024-11-27 12:19:21 +0000

    Bump UR tag

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,1 +1,7 @@
-set(UNIFIED_RUNTIME_TAG 6e0bdeb93483c165f3f0e926f6b2cd3edc00d0a7)
+# commit 0a90db9b2c36960c9b28ce18557ca15760724c4d
+# Merge: c4d9fdb4 6e0bdeb9
+# Author: Callum Fare <callum@codeplay.com>
+# Date:   Wed Nov 27 12:16:44 2024 +0000
+#     Merge pull request #2369 from Bensuo/ben/kernel-binary-update-l0
+#     [CMDBUF] Implement kernel binary update for L0 adapter
+set(UNIFIED_RUNTIME_TAG 0a90db9b2c36960c9b28ce18557ca15760724c4d)
