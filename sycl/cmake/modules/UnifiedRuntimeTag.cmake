51e6466fe29d3104dc4576dbb3694010468f9e30
Ewan Crawford <ewan@codeplay.com>
2024-11-29 12:13:06 +0000

    [SYCL][Graph] Bump UR with Coverity fixes
    Bump the UR tag to include coverity fixes
    for command-buffers https://github.com/oneapi-src/unified-runtime/pull/2379

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Wed Nov 27 16:04:19 2024 +0000
 #     Merge pull request #2261 from againull/againull/2d_block_exp
 #     Add new device descriptor to query 2D block array capabilities of the Intel GPU
-set(UNIFIED_RUNTIME_TAG db83117e830406b0d9950e24892dba868acba354)
+set(UNIFIED_RUNTIME_TAG "ben/coverity-fixes")
