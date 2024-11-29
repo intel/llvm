05f59b036c36dcbade703b189bba4b4df0c5a003
Aaron Greig <aaron.greig@codeplay.com>
2024-11-29 10:57:36 +0000

    [UR] Pull in native CPU adapter fix to allow paremeterized adapter tests.

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,1 @@
-# commit db83117e830406b0d9950e24892dba868acba354
-# Merge: 0a90db9b c79df596
-# Author: Callum Fare <callum@codeplay.com>
-# Date:   Wed Nov 27 16:04:19 2024 +0000
-#     Merge pull request #2261 from againull/againull/2d_block_exp
-#     Add new device descriptor to query 2D block array capabilities of the Intel GPU
-set(UNIFIED_RUNTIME_TAG db83117e830406b0d9950e24892dba868acba354)
+set(UNIFIED_RUNTIME_TAG aaron/parameterizeAdapterTests)
