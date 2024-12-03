b204187322e02c0c1afe0d041bc44efac8791128
Artur Gainullin <artur.gainullin@intel.com>
2024-12-02 16:51:09 -0800

    [UR] Update UR tag to include the fix to set the execution flag for all kernels

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,1 @@
-# commit eb076da108a49ef1426f38690547a71905f58015
-# Merge: d8d8ee90 46832dfd
-# Author: Callum Fare <callum@codeplay.com>
-# Date:   Fri Nov 29 15:54:31 2024 +0000
-#     Merge pull request #2396 from kswiecicki/init-results-fix
-#     [L0] Add nullopt check before init results access
-set(UNIFIED_RUNTIME_TAG eb076da108a49ef1426f38690547a71905f58015)
+set(UNIFIED_RUNTIME_TAG 4ae20657c66d5ad43153107f16d04ce736434587)
