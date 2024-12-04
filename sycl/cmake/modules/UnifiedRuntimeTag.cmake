4c76c239ec53285f464a974984980e47cca42aa8
Kenneth Benzie (Benie) <k.benzie@codeplay.com>
2024-12-04 18:19:23 +0000

    [UR] Bump tag to 3ad235de

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,1 +1,7 @@
-set(UNIFIED_RUNTIME_TAG 4ae20657c66d5ad43153107f16d04ce736434587)
+# commit 3ad235dee9588152a58980bb73c8827a3d03a9e8
+# Merge: c7cd88de 6390f637
+# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
+# Date:   Wed Dec 4 18:17:07 2024 +0000
+#     Merge pull request #2389 from againull/indirect_fix
+#     [L0] Set exec info for all L0 kernels in UR kernel
+set(UNIFIED_RUNTIME_TAG 3ad235dee9588152a58980bb73c8827a3d03a9e8)
