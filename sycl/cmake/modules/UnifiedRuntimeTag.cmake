02aca2a55ac261b30d35253232c900946cf30790
Neil R. Spruit <neil.r.spruit@intel.com>
2024-12-04 08:03:47 -0800

    [UR][L0] Disabling Driver In Order Lists by default
    -pre-commit PR https://github.com/oneapi-src/unified-runtime/pull/2416
    
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 3ad235dee9588152a58980bb73c8827a3d03a9e8
-# Merge: c7cd88de 6390f637
-# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Wed Dec 4 18:17:07 2024 +0000
-#     Merge pull request #2389 from againull/indirect_fix
-#     [L0] Set exec info for all L0 kernels in UR kernel
-set(UNIFIED_RUNTIME_TAG 3ad235dee9588152a58980bb73c8827a3d03a9e8)
+# commit b23a973a08194fbc462e968c7f0b2a04aded9b0e
+# Merge: 3ad235de 447b638b
+# Author: Neil R. Spruit <neil.r.spruit@intel.com>
+# Date:   Wed Dec 4 13:35:40 2024 -0800
+#   Merge pull request #2416 from nrspruit/disable_driver_inorderlists
+#   [L0] Disabling Driver In Order Lists by default
+set(UNIFIED_RUNTIME_TAG b23a973a08194fbc462e968c7f0b2a04aded9b0e)
