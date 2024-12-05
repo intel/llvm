859252ca3773a1987453f2b8517cec5d2c41bc57
Martin Morrison-Grant <martin.morrisongrant@codeplay.com>
2024-12-05 10:53:30 +0000

    [UR] Update tag to 5f4a5a2 for https://github.com/oneapi-src/unified-runtime/pull/2334.

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit bd39fbe70f22d8141481ab8debf49004b0afdca0
-# Merge: b23a973 dc3ca71
+# commit 5f4a5a27e8192ee41ce21c8fb140d770c779af78
+# Merge: 4111306dc9d2 039cf144f535
 # Author: Martin Grant <martin.morrisongrant@codeplay.com>
-# Date:   Thu Dec 5 09:12:06 2024 +0000
-#     Merge pull request #2361 from nrspruit/l0_external_semaphore
-#     [L0] Add Support for External Semaphores
-set(UNIFIED_RUNTIME_TAG bd39fbe70f22d8141481ab8debf49004b0afdca0)
+# Date:   Thu Dec 5 10:49:58 2024 +0000
+#     Merge pull request #2334 from winstonzhang-intel/interrupt-based
+#     [L0] Interrupt-based event implementation
+set(UNIFIED_RUNTIME_TAG 5f4a5a27e8192ee41ce21c8fb140d770c779af78)
