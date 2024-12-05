62b384ca05bbab445e408b60e9364bd8568953bf
Martin Morrison-Grant <martin.morrisongrant@codeplay.com>
2024-12-05 15:00:04 +0000

    [UR] Update tag to 2739808 for https://github.com/oneapi-src/unified-runtime/pull/2293.

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,1 +1,7 @@
-set(UNIFIED_RUNTIME_TAG yc-PR/241107-misc-minor-fix)
+# commit 27398080349f1d8d21d6a8680e234d29dcd14734 (HEAD, origin/main, origin/HEAD)
+# Merge: 572355db942d dc971af72a31
+# Author: Martin Grant <martin.morrisongrant@codeplay.com>
+# Date:   Thu Dec 5 14:57:07 2024 +0000
+#   Merge pull request #2293 from yingcong-wu/yc-PR/241107-misc-minor-fix
+#   [DeviceAsan] Serval bug fixes
+set(UNIFIED_RUNTIME_TAG 27398080349f1d8d21d6a8680e234d29dcd14734)
