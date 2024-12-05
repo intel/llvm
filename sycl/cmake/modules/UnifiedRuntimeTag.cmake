bee8a397ac7260e7fe2bcf9345564d3da0be082d
Maosu Zhao <maosu.zhao@intel.com>
2024-12-05 22:44:45 +0800

    [UR][DeviceASAN] Sync the latest changes in asan_libdevice.hpp (#15911)
    UR Part: https://github.com/oneapi-src/unified-runtime/pull/2249
    
    ---------
    
    Co-authored-by: Martin Morrison-Grant <martin.morrisongrant@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 5f4a5a27e8192ee41ce21c8fb140d770c779af78
-# Merge: 4111306dc9d2 039cf144f535
+# commit a172cde4bc2857dcf74b0b2907a5b0f90566e808 (HEAD, origin/main, origin/HEAD)
+# Merge: 1851eff47b0a cde0d4c820b0
 # Author: Martin Grant <martin.morrisongrant@codeplay.com>
-# Date:   Thu Dec 5 10:49:58 2024 +0000
-#     Merge pull request #2334 from winstonzhang-intel/interrupt-based
-#     [L0] Interrupt-based event implementation
-set(UNIFIED_RUNTIME_TAG 5f4a5a27e8192ee41ce21c8fb140d770c779af78)
+# Date:   Thu Dec 5 12:29:44 2024 +0000
+#   Merge pull request #2249 from zhaomaosu/use-device-usm-for-rtl-data
+#   [DeviceASAN] Use device usm to sync asan runtime data instead of shared usm
+set(UNIFIED_RUNTIME_TAG a172cde4bc2857dcf74b0b2907a5b0f90566e808)
