846839d2f11c00b8f1383f2a4ff47497039874ec
Zhao, Maosu <maosu.zhao@intel.com>
2024-12-05 00:53:22 -0800

    [DevASAN] Do allocation with USM pool to reduce memory overhead

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Wed Dec 4 18:17:07 2024 +0000
 #     Merge pull request #2389 from againull/indirect_fix
 #     [L0] Set exec info for all L0 kernels in UR kernel
-set(UNIFIED_RUNTIME_TAG 3ad235dee9588152a58980bb73c8827a3d03a9e8)
+set(UNIFIED_RUNTIME_TAG do-alloc-use-pool)
