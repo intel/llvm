590960a4205f9e712a155ecc8c3089f62df10c52
Neil R. Spruit <neil.r.spruit@intel.com>
2024-12-05 02:49:18 -0800

    [UR][L0] Add Support for External Semaphores (#16162)
    -pre-commit PR for
    https://github.com/oneapi-src/unified-runtime/pull/2361
    
    ---------
    
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>
    Co-authored-by: Martin Morrison-Grant <martin.morrisongrant@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit b23a973a08194fbc462e968c7f0b2a04aded9b0e
-# Merge: 3ad235de 447b638b
-# Author: Neil R. Spruit <neil.r.spruit@intel.com>
-# Date:   Wed Dec 4 13:35:40 2024 -0800
-#   Merge pull request #2416 from nrspruit/disable_driver_inorderlists
-#   [L0] Disabling Driver In Order Lists by default
-set(UNIFIED_RUNTIME_TAG b23a973a08194fbc462e968c7f0b2a04aded9b0e)
+# commit bd39fbe70f22d8141481ab8debf49004b0afdca0
+# Merge: b23a973 dc3ca71
+# Author: Martin Grant <martin.morrisongrant@codeplay.com>
+# Date:   Thu Dec 5 09:12:06 2024 +0000
+#     Merge pull request #2361 from nrspruit/l0_external_semaphore
+#     [L0] Add Support for External Semaphores
+set(UNIFIED_RUNTIME_TAG bd39fbe70f22d8141481ab8debf49004b0afdca0)
