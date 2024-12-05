df5fe9f4f61ede5adaeefbdcc4d6e0225ed0ab17
Martin Morrison-Grant <martin.morrisongrant@codeplay.com>
2024-12-05 12:37:57 +0000

    [UR] Update tag for https://github.com/oneapi-src/unified-runtime/pull/2249 and https://github.com/oneapi-src/unified-runtime/pull/1811.

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit dbd168cbed2d2590b47904728cd5762f1c2f4c6b (HEAD, origin/main, origin/HEAD)
-# Merge: 694c1b9a 27ad3f7d
-# Author: Piotr Balcer <piotr.balcer@intel.com>
-# Date:   Mon Oct 28 16:29:45 2024 +0100
-#   Merge pull request #2242 from nrspruit/sysman_env_disable
-#   [L0] Enable Sysman Thru Env by default and have zesInit be optional
-set(UNIFIED_RUNTIME_TAG use-device-usm-for-rtl-data)
+# commit a172cde4bc2857dcf74b0b2907a5b0f90566e808 (HEAD, origin/main, origin/HEAD)
+# Merge: 1851eff47b0a cde0d4c820b0
+# Author: Martin Grant <martin.morrisongrant@codeplay.com>
+# Date:   Thu Dec 5 12:29:44 2024 +0000
+#   Merge pull request #2249 from zhaomaosu/use-device-usm-for-rtl-data
+#   [DeviceASAN] Use device usm to sync asan runtime data instead of shared usm
+set(UNIFIED_RUNTIME_TAG a172cde4bc2857dcf74b0b2907a5b0f90566e808)
