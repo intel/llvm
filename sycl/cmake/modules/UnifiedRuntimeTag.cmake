ec36104e71ca0b35d4913e2b784396589dd24b05
Zhang, Winston <winston.zhang@intel.com>
2024-12-05 21:52:48 +0000

    [UR] getImmCmdList fix https://github.com/oneapi-src/unified-runtime/pull/2429
    Signed-off-by: Zhang, Winston <winston.zhang@intel.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Thu Dec 5 12:29:44 2024 +0000
 #   Merge pull request #2249 from zhaomaosu/use-device-usm-for-rtl-data
 #   [DeviceASAN] Use device usm to sync asan runtime data instead of shared usm
-set(UNIFIED_RUNTIME_TAG a172cde4bc2857dcf74b0b2907a5b0f90566e808)
+set(UNIFIED_RUNTIME_TAG 60e6f5e5e63b06e887aa2a692650b40f2fbc4b66)
