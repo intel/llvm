6df2d88c46e913b749fcb911e57702cc7e5b41dc
Lorenc Bushi <lorenc.bushi@intel.com>
2024-12-06 08:19:04 -0800

    Fix typos and remove the tests from the list of improperly XFAIL-ed tests

--- sycl/test-e2e/Basic/queue/release.cpp
+++ sycl/test-e2e/Basic/queue/release.cpp
@@ -1,8 +1,5 @@
 // RUN: %{build} -o %t.out
 // RUN: env SYCL_UR_TRACE=2 %{run} %t.out | FileCheck %s %if !windows %{--check-prefixes=CHECK-RELEASE%}
-//
-// XFAIL: hip_nvidia
-// XFAIL-TRACKER: https://github.com/intel/llvm/issues/16197
 
 #include <sycl/detail/core.hpp>
 int main() {
