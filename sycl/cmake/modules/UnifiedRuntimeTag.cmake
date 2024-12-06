6169cfad5b373a4b0d5e41dc4c25fdbf20448fdc
Martin Morrison-Grant <martin.morrisongrant@codeplay.com>
2024-12-06 12:07:47 +0000

    [UR] Update tag to d8fde1b for https://github.com/oneapi-src/unified-runtime/pull/2266.

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 2bea25d7d5404ce82f36caf91c359f65b25187d7
-# Merge: 0b5d8f9e e578228a
-# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Tue Dec 3 12:41:16 2024 +0000
-#     Merge pull request #2298 from Bensuo/ewan/cuda_update_local_size
-#     Improve CUDA/HIP local argument handling
-set(UNIFIED_RUNTIME_TAG reuse-shadow-memory)
+# commit d8fde1bdb3c60af0ee13cc44fab567a508644f77
+# Merge: d88481bf20a0 bc5105f1cf30
+# Author: Martin Grant <martin.morrisongrant@codeplay.com>
+# Date:   Fri Dec 6 12:05:32 2024 +0000
+#     Merge pull request #2266 from zhaomaosu/reuse-shadow-memory
+#     [DeviceASAN] Re-use shadow if required size is not larger than last one
+set(UNIFIED_RUNTIME_TAG d8fde1bdb3c60af0ee13cc44fab567a508644f77)
