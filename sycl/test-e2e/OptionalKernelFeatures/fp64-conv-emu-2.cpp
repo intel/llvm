6070ed4f940f157c3d0dd52d2fb0809141c5d538
Alexey Sachkov <alexey.sachkov@intel.com>
2024-12-06 19:08:05 +0100

    [SYCL][E2E] Expand fp64 emulation testing (#16259)
    Updated the test to cover more HW so that there is a less change that we
    will simply skip it.
    
    Note that FP64 emulation is only enabled by default on DG2. Therefore,
    we include HW which
    natively supports `fp64`, but can't include any HW without `fp64`
    support except for DG2 unless
    we tweak the environment to force-enable emulation on those platforms.

--- sycl/test-e2e/OptionalKernelFeatures/fp64-conv-emu-2.cpp
+++ sycl/test-e2e/OptionalKernelFeatures/fp64-conv-emu-2.cpp
@@ -1,8 +1,26 @@
-// REQUIRES: ocloc, linux, arch-intel_gpu_dg2_g10
+// -fsycl-fp64-conv-emu is not a language feature, but an implementation
+// feature that is only available for Intel GPUs. In this test we recreate a
+// user-provided scenario where an application has two kinds of kernels: ones
+// which require full fp64 support and ones which can be emulated.
+//
+// The test ensures that the application can be successfully AOT compiled and
+// ran on different HW (w/ and w/o native fp64 support), i.e. it also serves
+// as an integration test for two features: fp64 emulation and optional kernel
+// features AOT.
+//
+// REQUIRES: ocloc
+//
+// We require a certain HW here, because we specifically want to exercise AOT
+// compilation and not JIT fallback. However, to make this test run in more
+// environments (and therefore cover more scenarios), the list of HW is bigger
+// than just a single target.
+//
+// REQUIRES: arch-intel_gpu_dg2_g10 || arch-intel_gpu_dg2_g11 || arch-intel_gpu_dg2_g12 || arch-intel_gpu_pvc || arch-intel_gpu_mtl_h || arch-intel_gpu_mtl_u
+//
 // UNSUPPORTED: cuda, hip
 // UNSUPPORTED-REASON: FP64 emulation is an Intel specific feature.
 
-// RUN: %clangxx -fsycl -fsycl-targets=intel_gpu_dg2_g10 -fsycl-fp64-conv-emu -O0 %s -o %t.out
+// RUN: %clangxx -fsycl -fsycl-targets=intel_gpu_dg2_g10,intel_gpu_dg2_g11,intel_gpu_dg2_g12,intel_gpu_pvc,intel_gpu_mtl_h,intel_gpu_mtl_u -fsycl-fp64-conv-emu -O0 %s -o %t.out
 // RUN: %{run} %t.out
 
 #include <sycl/detail/core.hpp>
