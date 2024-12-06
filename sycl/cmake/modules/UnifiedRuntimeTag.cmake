2d68e6bf28ec4cd8ed2e2a4b502b8f4b86b5e8c4
Maosu Zhao <maosu.zhao@intel.com>
2024-12-06 23:10:31 +0800

    [DeviceASAN] Re-use shadow if required size is not larger than last one (#16258)
    UR Part: https://github.com/oneapi-src/unified-runtime/pull/2266
    
    ---------
    
    Co-authored-by: Martin Morrison-Grant <martin.morrisongrant@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 72e80a42cc8e5b11d43dd9d34b40d470e1476181 (HEAD, origin/main, origin/HEAD)
-# Merge:  6e5d0e6b9a47 9c7e56cc765b
+# commit d8fde1bdb3c60af0ee13cc44fab567a508644f77
+# Merge: d88481bf20a0 bc5105f1cf30
 # Author: Martin Grant <martin.morrisongrant@codeplay.com>
-# Date:   Fri Dec 6 10:11:15 2024 +0000
-#   Merge pull request #2316 from 0x12CC/coop_kernel_query
-#   Change `urSuggestMaxCooperativeGroupCountExp` to accept ND size parameter
-set(UNIFIED_RUNTIME_TAG 72e80a42cc8e5b11d43dd9d34b40d470e1476181)
+# Date:   Fri Dec 6 12:05:32 2024 +0000
+#     Merge pull request #2266 from zhaomaosu/reuse-shadow-memory
+#     [DeviceASAN] Re-use shadow if required size is not larger than last one
+set(UNIFIED_RUNTIME_TAG d8fde1bdb3c60af0ee13cc44fab567a508644f77)
