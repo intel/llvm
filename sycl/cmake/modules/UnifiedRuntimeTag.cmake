689da90b1336c671724d2163f34ddf961489b9b7
Michael Aziz <michael.aziz@intel.com>
2024-12-06 07:04:31 -0500

    Update the `kernel_queue_specific::max_num_work_group` query (#16051)
    Update calls to `urKernelSuggestMaxCooperativeGroupCountExp` to match
    the changes in https://github.com/oneapi-src/unified-runtime/pull/2316.
    This PR also implements the `range<1>` and `range<2>` overloads of the
    `ext_oneapi_get_info` API from
    [sycl_ext_oneapi_launch_queries](https://github.com/intel/llvm/blob/sycl/sycl/doc/extensions/proposed/sycl_ext_oneapi_launch_queries.asciidoc).
    
    ---------
    
    Signed-off-by: Michael Aziz <michael.aziz@intel.com>
    Co-authored-by: Martin Morrison-Grant <martin.morrisongrant@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 0a90db9b2c36960c9b28ce18557ca15760724c4d
-# Merge: c4d9fdb4 6e0bdeb9
-# Author: Callum Fare <callum@codeplay.com>
-# Date:   Wed Nov 27 12:16:44 2024 +0000
-#     Merge pull request #2369 from Bensuo/ben/kernel-binary-update-l0
-#     [CMDBUF] Implement kernel binary update for L0 adapter
-set(UNIFIED_RUNTIME_TAG 0a90db9b2c36960c9b28ce18557ca15760724c4d)
+# commit 72e80a42cc8e5b11d43dd9d34b40d470e1476181 (HEAD, origin/main, origin/HEAD)
+# Merge:  6e5d0e6b9a47 9c7e56cc765b
+# Author: Martin Grant <martin.morrisongrant@codeplay.com>
+# Date:   Fri Dec 6 10:11:15 2024 +0000
+#   Merge pull request #2316 from 0x12CC/coop_kernel_query
+#   Change `urSuggestMaxCooperativeGroupCountExp` to accept ND size parameter
+set(UNIFIED_RUNTIME_TAG 72e80a42cc8e5b11d43dd9d34b40d470e1476181)
