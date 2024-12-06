6347914485a8c27fb96229de41cc5782576e5918
Wu Yingcong <yingcong.wu@intel.com>
2024-12-06 18:09:52 +0800

    [DeviceAsan] bugfixes for UR (#16257)
    UR PR: https://github.com/oneapi-src/unified-runtime/pull/2293
    
    ---------
    
    Co-authored-by: Martin Morrison-Grant <martin.morrisongrant@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit a172cde4bc2857dcf74b0b2907a5b0f90566e808 (HEAD, origin/main, origin/HEAD)
-# Merge: 1851eff47b0a cde0d4c820b0
+# commit 27398080349f1d8d21d6a8680e234d29dcd14734 (HEAD, origin/main, origin/HEAD)
+# Merge: 572355db942d dc971af72a31
 # Author: Martin Grant <martin.morrisongrant@codeplay.com>
-# Date:   Thu Dec 5 12:29:44 2024 +0000
-#   Merge pull request #2249 from zhaomaosu/use-device-usm-for-rtl-data
-#   [DeviceASAN] Use device usm to sync asan runtime data instead of shared usm
-set(UNIFIED_RUNTIME_TAG a172cde4bc2857dcf74b0b2907a5b0f90566e808)
+# Date:   Thu Dec 5 14:57:07 2024 +0000
+#   Merge pull request #2293 from yingcong-wu/yc-PR/241107-misc-minor-fix
+#   [DeviceAsan] Serval bug fixes
+set(UNIFIED_RUNTIME_TAG 27398080349f1d8d21d6a8680e234d29dcd14734)
