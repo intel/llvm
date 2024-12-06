a6d8889d3152c4f12a0bbb6aa30c57ef33a556f3
Wu Yingcong <yingcong.wu@intel.com>
2024-12-06 18:09:52 +0800

    [DeviceAsan] bugfixes for UR (#16257)
    UR PR: https://github.com/oneapi-src/unified-runtime/pull/2293
    
    ---------
    
    Co-authored-by: Martin Morrison-Grant <martin.morrisongrant@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 0a90db9b2c36960c9b28ce18557ca15760724c4d
-# Merge: c4d9fdb4 6e0bdeb9
-# Author: Callum Fare <callum@codeplay.com>
-# Date:   Wed Nov 27 12:16:44 2024 +0000
-#     Merge pull request #2369 from Bensuo/ben/kernel-binary-update-l0
-#     [CMDBUF] Implement kernel binary update for L0 adapter
-set(UNIFIED_RUNTIME_TAG 0a90db9b2c36960c9b28ce18557ca15760724c4d)
+# commit 27398080349f1d8d21d6a8680e234d29dcd14734 (HEAD, origin/main, origin/HEAD)
+# Merge: 572355db942d dc971af72a31
+# Author: Martin Grant <martin.morrisongrant@codeplay.com>
+# Date:   Thu Dec 5 14:57:07 2024 +0000
+#   Merge pull request #2293 from yingcong-wu/yc-PR/241107-misc-minor-fix
+#   [DeviceAsan] Serval bug fixes
+set(UNIFIED_RUNTIME_TAG 27398080349f1d8d21d6a8680e234d29dcd14734)
