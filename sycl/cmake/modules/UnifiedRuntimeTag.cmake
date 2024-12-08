ae07302a99184f2f77d45dc31946acfbb00481bf
Ben Ashbaugh <ben.ashbaugh@intel.com>
2024-12-08 11:27:23 -0800

    add a few missing Intel GPU device queries, fix device ID query

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit aa72577da55571b15028dd1710e02a7ea4f62338
-# Merge: 89def64486dd 3d2b7210e352
-# Author: Martin Grant <martin.morrisongrant@codeplay.com>
-# Date:   Fri Dec 6 15:24:04 2024 +0000
-#     Merge pull request #2352 from aarongreig/aaron/fixCoverity15-11
-#     Fix a number of issues from the latest coverity scan.
-set(UNIFIED_RUNTIME_TAG aa72577da55571b15028dd1710e02a7ea4f62338)
+# commit f74b2d7e4fb19aee4c4794cdb2ebc1d87dce02b0
+# Author: Ben Ashbaugh <ben.ashbaugh@intel.com>
+# Date:   Sat Dec 7 16:46:06 2024 -0800
+
+#     add a few missing Intel GPU device queries
+#     also, fix the device ID query
+set(UNIFIED_RUNTIME_TAG f74b2d7e4fb19aee4c4794cdb2ebc1d87dce02b0)
