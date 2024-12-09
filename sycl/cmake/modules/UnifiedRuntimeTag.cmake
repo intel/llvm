2431c73ecc80eeb915d73d604207991a1a99bd2d
Wu, Yingcong <yingcong.wu@intel.com>
2024-12-08 19:39:10 -0800

    use ur pr

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Fri Dec 6 15:24:04 2024 +0000
 #     Merge pull request #2352 from aarongreig/aaron/fixCoverity15-11
 #     Fix a number of issues from the latest coverity scan.
-set(UNIFIED_RUNTIME_TAG aa72577da55571b15028dd1710e02a7ea4f62338)
+set(UNIFIED_RUNTIME_TAG yc-test/1209-cif-syclos-CI)
