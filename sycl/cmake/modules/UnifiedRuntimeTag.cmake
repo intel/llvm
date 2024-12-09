d98db0df87f23bae8385bd11fab93ffe86f632bf
Kenneth Benzie (Benie) <k.benzie@codeplay.com>
2024-12-09 15:00:56 +0000

    [UR] Bump main tag to 5c10466f

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,1 +1,7 @@
-set(UNIFIED_RUNTIME_TAG b7b6b55769c2f4e8ffb077a9f945e371232148f2)
+# commit 5c10466f349b0ff303bd14b015f64bbcd0b78d7d
+# Merge: ffcc53b6 b7b6b557
+# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
+# Date:   Mon Dec 9 14:58:34 2024 +0000
+#     Merge pull request #2410 from steffenlarsen/steffen/fix_profiling_tag_eviction_and_caching
+#     [L0] Fix cached and evicted timestamp recordings
+set(UNIFIED_RUNTIME_TAG 5c10466f349b0ff303bd14b015f64bbcd0b78d7d)
