8c4df603c6f7d541001ca801b2d277e0432c8dde
Ewan Crawford <ewan@codeplay.com>
2024-12-09 12:57:58 +0000

    [SYCL][Graph] Bump UR with Coverity fixes (#16215)
    Bump the UR tag to include coverity fixes for command-buffers
    https://github.com/oneapi-src/unified-runtime/pull/2379
    
    ---------
    
    Co-authored-by: Martin Morrison-Grant <martin.morrisongrant@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit aa72577da55571b15028dd1710e02a7ea4f62338
-# Merge: 89def64486dd 3d2b7210e352
+# commit e37f75ffad5ad22e96c46eb70ab1757be7cc5afc
+# Merge: daa0b110464c 323b37c55504
 # Author: Martin Grant <martin.morrisongrant@codeplay.com>
-# Date:   Fri Dec 6 15:24:04 2024 +0000
-#     Merge pull request #2352 from aarongreig/aaron/fixCoverity15-11
-#     Fix a number of issues from the latest coverity scan.
-set(UNIFIED_RUNTIME_TAG aa72577da55571b15028dd1710e02a7ea4f62338)
+# Date:   Fri Dec 6 18:03:11 2024 +0000
+#     Merge pull request #2379 from Bensuo/ben/coverity-fixes
+#     Fix command_buffer coverity issues
+set(UNIFIED_RUNTIME_TAG e37f75ffad5ad22e96c46eb70ab1757be7cc5afc)
