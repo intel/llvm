ea90e8a20cd8531dc032c36e08f67b9042fd32d1
Li, Ian <ian.li@intel.com>
2024-12-10 13:41:12 -0800

    Bump UR

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,5 +1,6 @@
-# commit 2143085f88c9a6746bde14ef827a36b13981da9d
+# commit c1dcb31365ffd58fc4712b7bb4be4719280aab07
+# Merge: 2143085f 8f33719f
 # Author: Li, Ian <ian.li@intel.com>
-# Date:   Tue Dec 10 11:38:49 2024 -0800
-#     Add handling for unsupported prefetch combinations
-set(UNIFIED_RUNTIME_TAG 2143085f88c9a6746bde14ef827a36b13981da9d)
+# Date:   Tue Dec 10 13:00:46 2024 -0800
+#     Merge branch 'main' of https://github.com/oneapi-src/unified-runtime into ianayl/2way-prefetch
+set(UNIFIED_RUNTIME_TAG c1dcb31365ffd58fc4712b7bb4be4719280aab07)
