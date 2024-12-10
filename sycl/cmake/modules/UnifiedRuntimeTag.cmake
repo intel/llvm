92baea662e0ea27e661d830c2041c1ebcdb71395
Steffen Larsen <steffen.larsen@intel.com>
2024-12-10 15:01:30 +0100

    [SYCL][UR][L0] Fix issue with event caching causing profiling tag conflicts (#16233)
    This commit adds a test and changes to a UR version that fixes an issue
    where the L0 would report old timestamp recordings for events produced
    for profiling tags, due to reusing of dead events.
    
    ---------
    
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>
    Co-authored-by: Kenneth Benzie (Benie) <k.benzie@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit e37f75ffad5ad22e96c46eb70ab1757be7cc5afc
-# Merge: daa0b110464c 323b37c55504
-# Author: Martin Grant <martin.morrisongrant@codeplay.com>
-# Date:   Fri Dec 6 18:03:11 2024 +0000
-#     Merge pull request #2379 from Bensuo/ben/coverity-fixes
-#     Fix command_buffer coverity issues
-set(UNIFIED_RUNTIME_TAG e37f75ffad5ad22e96c46eb70ab1757be7cc5afc)
+# commit 5c10466f349b0ff303bd14b015f64bbcd0b78d7d
+# Merge: ffcc53b6 b7b6b557
+# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
+# Date:   Mon Dec 9 14:58:34 2024 +0000
+#     Merge pull request #2410 from steffenlarsen/steffen/fix_profiling_tag_eviction_and_caching
+#     [L0] Fix cached and evicted timestamp recordings
+set(UNIFIED_RUNTIME_TAG 5c10466f349b0ff303bd14b015f64bbcd0b78d7d)
