fe4c6204a5da6a642613a6b6d6bbb54452a22e7c
Victor Lomuller <victor@codeplay.com>
2024-12-11 13:00:52 +0000

    fix work_group_scrach_memory (#16325)
    Fixes https://github.com/intel/llvm/issues/16285, local testing was done
    using arc only initially and didn't exibit any of the seen behaviour
    elsewhere.
    
    Tested locally with OpenCL CPU and OpenCL gpu (PVC).
    
    also partially addresses https://github.com/intel/llvm/issues/16072
    
    ---------
    
    Signed-off-by: Victor Lomuller <victor@codeplay.com>

--- sycl/test-e2e/WorkGroupMemory/Dynamic/dynamic_alloc_local_accessor.cpp
+++ sycl/test-e2e/WorkGroupMemory/Dynamic/dynamic_alloc_local_accessor.cpp
@@ -35,8 +35,8 @@ int main() {
     sycl_ext::properties properties{static_size};
     auto LocalAccessor =
         sycl::local_accessor<int>(WgSize * RepeatWG * sizeof(int), Cgh);
-    Cgh.parallel_for(nd_range<1>(range<1>(Size), range<1>(WgSize)), properties,
-                     [=](nd_item<1> Item) {
+    Cgh.parallel_for(nd_range<1>(range<1>(WgSize * WgCount), range<1>(WgSize)),
+                     properties, [=](nd_item<1> Item) {
                        int *Ptr = reinterpret_cast<int *>(
                            sycl_ext::get_work_group_scratch_memory());
                        size_t GroupOffset =
