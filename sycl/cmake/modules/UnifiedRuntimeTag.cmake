cc941cad264e78529896dd25dc74fc4a281557f4
Neil R. Spruit <neil.r.spruit@intel.com>
2024-12-11 09:46:42 -0800

    [UR][L0] Fix Device Info Reporting for vector width to match spec
    -pre-commit PR for https://github.com/oneapi-src/unified-runtime/pull/2447
    
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Wed Dec 11 15:14:14 2024 +0000
 #     Merge pull request #2436 from igchor/umf_params_bump
 #     [UMF] bump UMF version to v0.10.0 and adjust code
-set(UNIFIED_RUNTIME_TAG 58e4d76c2ace154c053b51592f2645fc43437646)
+set(UNIFIED_RUNTIME_TAG 0185baf440f6402ef7cc2de464190c16b48dd4ae)
