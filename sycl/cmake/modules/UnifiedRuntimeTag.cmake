8106796a40934746f7ce71566e45a51ebc35a22b
Martin Grant <martin.morrisongrant@codeplay.com>
2024-12-11 11:02:47 +0000

    [UR] Update tag to 45f3d8  (#16327)
    https://github.com/oneapi-src/unified-runtime/pull/2430

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 870f0ed76aa302c23b4d72aa90d0b24b1faf1aa7
-# Merge: e1cc9b2c b532350f
-# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Tue Dec 10 18:05:02 2024 +0000
-#     Merge pull request #2420 from yingcong-wu/yc-PR/1204-error-on-unsupported-feature
-#     [DevAsan] Report error when using unsupported extension API
-set(UNIFIED_RUNTIME_TAG 870f0ed76aa302c23b4d72aa90d0b24b1faf1aa7)
+# commit 45f3d8ad95d7acada8e3bc4aa49bfae07dabcba5
+# Merge: 8f33719fa4fc 08acf05f2d66
+# Author:  Martin Grant <martin.morrisongrant@codeplay.com>
+# Date:   Wed Dec 11 09:12:33 2024 +0000
+#     Merge pull request #2430 from nrspruit/fix_inorder_initList
+#     [UR] Fix correct usage of In Order sync list given counting events
+set(UNIFIED_RUNTIME_TAG 45f3d8ad95d7acada8e3bc4aa49bfae07dabcba5)
