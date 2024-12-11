bb652803674fac255d786ba8d5af0c7d3b6bce82
Wu Yingcong <yingcong.wu@intel.com>
2024-12-11 15:44:46 +0800

    [DeviceAsan] Report error when using unsupported API (#16281)
    UR PR: https://github.com/oneapi-src/unified-runtime/pull/2420
    
    ---------
    
    Co-authored-by: Kenneth Benzie (Benie) <k.benzie@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit e1cc9b2cfce7f329f0f411b5b84837b511b86a2d
-# Merge: 5a1a81b6 b209eba8
+# commit 870f0ed76aa302c23b4d72aa90d0b24b1faf1aa7
+# Merge: e1cc9b2c b532350f
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Tue Dec 10 14:29:41 2024 +0000
-#     Merge pull request #2394 from zhaomaosu/do-alloc-use-pool
-#     [DevASAN] Do allocation with USM pool to reduce memory overhead
-set(UNIFIED_RUNTIME_TAG e1cc9b2cfce7f329f0f411b5b84837b511b86a2d)
+# Date:   Tue Dec 10 18:05:02 2024 +0000
+#     Merge pull request #2420 from yingcong-wu/yc-PR/1204-error-on-unsupported-feature
+#     [DevAsan] Report error when using unsupported extension API
+set(UNIFIED_RUNTIME_TAG 870f0ed76aa302c23b4d72aa90d0b24b1faf1aa7)
