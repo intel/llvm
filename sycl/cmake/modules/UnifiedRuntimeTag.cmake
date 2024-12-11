f189391725150e77ae4b12d3a58bf68c17f0b788
Zhao, Yang2 <yang2.zhao@intel.com>
2024-12-11 04:08:53 +0100

    fix build

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
 # commit 3db3a5e2d935630f2ffddd93a72ae0aa9af89acb
 # Author: Artur Gainullin <artur.gainullin@intel.com>
 # Date:   Tue Oct 22 03:30:08 2024 -0700
 #
 #     Support UR program creation from multiple device binaries (#2147)
 set(UNIFIED_RUNTIME_TAG review/yang/restructure_asan_msan)
-=======
-# commit e1cc9b2cfce7f329f0f411b5b84837b511b86a2d
-# Merge: 5a1a81b6 b209eba8
-# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Tue Dec 10 14:29:41 2024 +0000
-#     Merge pull request #2394 from zhaomaosu/do-alloc-use-pool
-#     [DevASAN] Do allocation with USM pool to reduce memory overhead
-set(UNIFIED_RUNTIME_TAG e1cc9b2cfce7f329f0f411b5b84837b511b86a2d)
->>>>>>> sycl
