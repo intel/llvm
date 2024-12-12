c9e33e898f6b511c0a76f71af2ff6f9d3cbcf140
Neil R. Spruit <neil.r.spruit@intel.com>
2024-12-12 09:05:30 -0800

    [UR][L0] Update L0 Init checking to print details in error log
    -pre-commit PR for https://github.com/oneapi-src/unified-runtime/pull/2449
    
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Fri Dec 13 14:20:15 2024 +0000
 #     Merge pull request #2454 from Bensuo/l0_cmd-buf_multi-device
 #     Fix L0 command-buffer consumption of multi-device kernels
-set(UNIFIED_RUNTIME_TAG b7047f6c36ec17b8560c2f1cd9ac9521715a9127)
+set(UNIFIED_RUNTIME_TAG 64ae812e9dc0c0369094ed7874c58bf85e5301ca)
