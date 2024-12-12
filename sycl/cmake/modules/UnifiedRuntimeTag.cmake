5e10744da44f279914e139ddd5dd5114193533c3
cppchedy <chedy.najjar@codeplay.com>
2024-12-12 08:37:44 +0000

    [SYCL][Bindless][E2E] fix double free in vulkan interop unsampled test (#16027)
    * fix double free in vulkan interop unsampled test
    
    ---------
    
    Co-authored-by: Peter Žužek <peter@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 58e4d76c2ace154c053b51592f2645fc43437646
-# Merge: e23a75034bb2 bc5d6a695dc8
+# commit 098deca1f9f3b9f3f0563ee823ac424d8db30668
+# Merge: 58e4d76c2ace 73ba29bfe9df
 # Author: Martin Grant <martin.morrisongrant@codeplay.com>
-# Date:   Wed Dec 11 15:14:14 2024 +0000
-#     Merge pull request #2436 from igchor/umf_params_bump
-#     [UMF] bump UMF version to v0.10.0 and adjust code
-set(UNIFIED_RUNTIME_TAG 58e4d76c2ace154c053b51592f2645fc43437646)
+# Date:   Wed Dec 11 17:23:43 2024 +0000
+#     Merge pull request #2299 from cppchedy/chedy/fix-mipmap-leak
+#     [CUDA][Bindless] Fix memory leak in interop mapping
+set(UNIFIED_RUNTIME_TAG 098deca1f9f3b9f3f0563ee823ac424d8db30668)
