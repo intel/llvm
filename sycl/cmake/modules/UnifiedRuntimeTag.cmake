c4404a2c9ce6fd6f3937deaeb4b1700fdfe6cfea
Zhang, Winston <winston.zhang@intel.com>
2024-12-12 00:34:22 +0000

    [UR] Fixed event leak when outevent is given and completed
    Signed-off-by: Zhang, Winston <winston.zhang@intel.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,1 @@
-# commit b7047f6c36ec17b8560c2f1cd9ac9521715a9127
-# Merge: 73e5f3c6ff2d fcddf077c290
-# Author: Martin Grant <martin.morrisongrant@codeplay.com>
-# Date:   Fri Dec 13 14:20:15 2024 +0000
-#     Merge pull request #2454 from Bensuo/l0_cmd-buf_multi-device
-#     Fix L0 command-buffer consumption of multi-device kernels
-set(UNIFIED_RUNTIME_TAG b7047f6c36ec17b8560c2f1cd9ac9521715a9127)
+set(UNIFIED_RUNTIME_TAG fbd879482c8bce659e98e1448a2c134e3bcc8b18)
