1f36c3a601e798f9930c91fd98e2ee20fd163119
Ewan Crawford <ewan@codeplay.com>
2024-12-12 10:56:17 +0000

    [SYCL][Graph] Fix L0 multi-device kernel bundles
    Bumps UR commit to https://github.com/oneapi-src/unified-runtime/pull/2454
    which contains a fix for using kernel bundles associated
    with a multi-device context in a SYCL-Graph

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Wed Dec 11 17:23:43 2024 +0000
 #     Merge pull request #2299 from cppchedy/chedy/fix-mipmap-leak
 #     [CUDA][Bindless] Fix memory leak in interop mapping
-set(UNIFIED_RUNTIME_TAG 098deca1f9f3b9f3f0563ee823ac424d8db30668)
+set(UNIFIED_RUNTIME_TAG "l0_cmd-buf_multi-device")
