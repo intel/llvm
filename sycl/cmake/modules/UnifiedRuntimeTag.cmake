0e39d49906f7b01d136695b1e2fbdd1024098754
Wu Yingcong <yingcong.wu@intel.com>
2024-12-13 17:55:22 +0800

    update UR

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,1 @@
-# commit aa72577da55571b15028dd1710e02a7ea4f62338
-# Merge: 89def64486dd 3d2b7210e352
-# Author: Martin Grant <martin.morrisongrant@codeplay.com>
-# Date:   Fri Dec 6 15:24:04 2024 +0000
-#     Merge pull request #2352 from aarongreig/aaron/fixCoverity15-11
-#     Fix a number of issues from the latest coverity scan.
-set(UNIFIED_RUNTIME_TAG aa72577da55571b15028dd1710e02a7ea4f62338)
+set(UNIFIED_RUNTIME_TAG yc/1213-omp-global-fix )
