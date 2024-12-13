dfbc1383b5b086ea1d4f63bce51a8dd8e5f1f0bc
aelovikov-intel <andrei.elovikov@intel.com>
2024-12-13 08:49:42 -0800

    Update sycl/test/basic_tests/vectors/swizzle.cpp

--- sycl/test/basic_tests/vectors/swizzle.cpp
+++ sycl/test/basic_tests/vectors/swizzle.cpp
@@ -32,7 +32,6 @@ int main() {
   {
     // expected-error-re@+1 {{no template named 'swizzle' in {{.*}}}}
     auto tmp = (sw + sw).swizzle<1, 0>();
-    std::cout << tmp[0] << " " << tmp[1] << std::endl;
 
     assert(tmp[0] == 6);
     assert(tmp[1] == 4);
