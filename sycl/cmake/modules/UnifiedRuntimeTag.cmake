dff7e07a7ce70f6b7483f3752aa1ac8f6f09e44f
Neil R. Spruit <neil.r.spruit@intel.com>
2024-12-13 11:50:45 -0800

    [UR][L0] Fix external semaphore import function call to match the header
    -pre-commit PR for https://github.com/oneapi-src/unified-runtime/pull/2467
    
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Fri Dec 13 14:20:15 2024 +0000
 #     Merge pull request #2454 from Bensuo/l0_cmd-buf_multi-device
 #     Fix L0 command-buffer consumption of multi-device kernels
-set(UNIFIED_RUNTIME_TAG b7047f6c36ec17b8560c2f1cd9ac9521715a9127)
+set(UNIFIED_RUNTIME_TAG be27d8f06e559890ca47c131b3346e0e2a551057)
