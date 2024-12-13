039cf388f569ed315e84453b1a2469222a319adb
Zhao, Yang2 <yang2.zhao@intel.com>
2024-12-13 08:13:51 +0100

    triggle ci

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -2,6 +2,6 @@
 # Merge: c7cd88de 6390f637
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
 # Date:   Tue Dec 3 12:41:16 2024 +0000
-#     Merge pull request #2298 from Bensuo/ewan/cuda_update_local_size
-#     Improve CUDA/HIP local argument handling
+#      Merge pull request #2298 from Bensuo/ewan/cuda_update_local_size
+#      Improve CUDA/HIP local argument handling
 set(UNIFIED_RUNTIME_TAG review/yang/fix_metadata_assert)
