deeb35a545099f25d76667e820aa527c74dd4cd2
Martin Morrison-Grant <martin.morrisongrant@codeplay.com>
2024-12-13 11:34:04 +0000

    [UR] Update tag to 4739d4c for https://github.com/oneapi-src/unified-runtime/pull/2437.

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 6d4eec8cdcfe8a5d359ed05092797c429c2ca878
-# Merge: 40d28e7bd84a 800b452d67c5
+# commit 4739d4cc30534d80fba4c876bfbafa51d3cad53a
+# Merge: b0d133d5d5e8 44b4ff98ae79
 # Author: Martin Grant <martin.morrisongrant@codeplay.com>
-# Date:   Thu Dec 12 16:00:13 2024 +0000
-#     Merge pull request #2272 from martygrant/martin/virtual-memory-cts-spec-gap
-#     Improvements to align CTS and Spec for Virtual Memory
-set(UNIFIED_RUNTIME_TAG 6d4eec8cdcfe8a5d359ed05092797c429c2ca878)
+# Date:   Fri Dec 13 11:30:56 2024 +0000
+#     Merge pull request #2437 from bashbaug/add-opencl-device-queries
+#     add a few missing Intel GPU device queries, fix device ID query
+set(UNIFIED_RUNTIME_TAG 4739d4cc30534d80fba4c876bfbafa51d3cad53a)
