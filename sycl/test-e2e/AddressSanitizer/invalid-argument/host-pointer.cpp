fd3a11535bd144781050c4c719389e1e53b3868d
aarongreig <aaron.greig@codeplay.com>
2024-12-16 15:26:06 +0000

    [SYCL] Remove uses of unimplemented SYCL_PREFER_UR env var. (#16213)
    Also remove the documentation for same.
    
    Since the PI port this env var no longer does anything.

--- sycl/test-e2e/AddressSanitizer/invalid-argument/host-pointer.cpp
+++ sycl/test-e2e/AddressSanitizer/invalid-argument/host-pointer.cpp
@@ -1,6 +1,6 @@
 // REQUIRES: linux, cpu || (gpu && level_zero)
 // RUN: %{build} %device_asan_flags -O2 -g -o %t
-// RUN: env SYCL_PREFER_UR=1 UR_LAYER_ASAN_OPTIONS="detect_kernel_arguments:1" %{run} %if gpu %{ not %} %t 2>&1 | FileCheck --check-prefixes %if cpu %{ CHECK-CPU %} %if gpu %{ CHECK-GPU %} %s
+// RUN: env UR_LAYER_ASAN_OPTIONS="detect_kernel_arguments:1" %{run} %if gpu %{ not %} %t 2>&1 | FileCheck --check-prefixes %if cpu %{ CHECK-CPU %} %if gpu %{ CHECK-GPU %} %s
 
 #include <sycl/detail/core.hpp>
 #include <sycl/usm.hpp>
