68acb46fdc92ec77a03ac5f69a53fd4624e4e49b
roy1994 <dev.mancitrus@outlook.com>
2024-12-16 17:52:17 +0000

    [SYCL][HIP] fix: support nearbyint and rint fp64

--- libdevice/cmath_wrapper_fp64.cpp
+++ libdevice/cmath_wrapper_fp64.cpp
@@ -190,6 +190,16 @@ DEVICE_EXTERN_C_INLINE
 double rint(double x) { return __nv_rint(x); }
 #endif // __NVPTX__
 
+#ifdef __AMDGCN__
+extern "C" SYCL_EXTERNAL double __ocml_nearbyint_f64(double);
+DEVICE_EXTERN_C_INLINE
+double nearbyintf(double x) { return __ocml_nearbyint_f64(x); }
+
+extern "C" SYCL_EXTERNAL double __ocml_rint_f64(double);
+DEVICE_EXTERN_C_INLINE
+double rintf(double x) { return __ocml_rint_f64(x); }
+#endif // __AMDGCN__
+
 #if defined(_MSC_VER)
 #include <math.h>
 // FLOAT PROPERTIES
