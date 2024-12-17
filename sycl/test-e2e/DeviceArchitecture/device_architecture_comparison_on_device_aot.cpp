45b0c0ae35eed6a570498fa7af8d3dec74d1b8f9
Agarwal, Udit <udit.agarwal@intel.com>
2024-12-17 14:10:57 -0800

    One more xfail

--- sycl/test-e2e/DeviceArchitecture/device_architecture_comparison_on_device_aot.cpp
+++ sycl/test-e2e/DeviceArchitecture/device_architecture_comparison_on_device_aot.cpp
@@ -1,5 +1,8 @@
 // REQUIRES: arch-intel_gpu_pvc, ocloc
 
+// XFAIL: arch-intel_gpu_pvc && !igc-dev
+// XFAIL-TRACKER: https://github.com/intel/llvm/issues/16401
+
 // RUN: %clangxx -fsycl -fsycl-targets=intel_gpu_pvc %s -o %t.out
 // RUN: %{run} %t.out
 
