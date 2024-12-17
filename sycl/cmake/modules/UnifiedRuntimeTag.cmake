7866c2eeda5071971184fdea2450034f12cd27f4
Kenneth Benzie (Benie) <k.benzie@codeplay.com>
2024-12-17 16:11:09 +0000

    [UR] Bump main tag to 501057c6

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,1 +1,7 @@
-set(UNIFIED_RUNTIME_TAG aaron/optionalInfoQueries)
+# commit 501057c67bed70cc3eff09d203dc4b3db18408c2
+# Merge: 39df0317 c0a372bd
+# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
+# Date:   Tue Dec 17 16:07:54 2024 +0000
+#     Merge pull request #1332 from aarongreig/aaron/optionalInfoQueries
+#     Explicitly define which info queries are optional.
+set(UNIFIED_RUNTIME_TAG 501057c67bed70cc3eff09d203dc4b3db18408c2)
