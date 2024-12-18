caaeaf1e542ce5e9ec3994f23a172caed625665d
Garcia Orozco, David <david.garcia.orozco@intel.com>
2024-12-18 10:43:26 -0800

    Format

--- sycl/test-e2e/format.py
+++ sycl/test-e2e/format.py
@@ -245,9 +245,9 @@ class SYCLEndToEndTest(lit.formats.ShTest):
                 and test.config.fallback_build_run_only
             )
             if not ignore_line_filtering and (
-                (is_run_line and test.config.test_mode == "build-only") or
-                (not is_run_line and test.config.test_mode == "run-only")
-                ):
+                (is_run_line and test.config.test_mode == "build-only")
+                or (not is_run_line and test.config.test_mode == "run-only")
+            ):
                 directive.command = ""
 
             if "%{run}" not in directive.command:
