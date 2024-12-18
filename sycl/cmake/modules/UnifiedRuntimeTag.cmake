5ba99edaf9be8e9b5456cca76b1813d70ae965ab
Kenneth Benzie (Benie) <k.benzie@codeplay.com>
2024-12-18 09:31:41 +0000

    [UR] Bump main tag to 607e88d0

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 501057c67bed70cc3eff09d203dc4b3db18408c2
-# Merge: 39df0317 c0a372bd
+# commit 607e88d09139337e3fe508016c4238f05cb2a8b6
+# Merge: c36a0b62 3ab955f2
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Tue Dec 17 16:07:54 2024 +0000
-#     Merge pull request #1332 from aarongreig/aaron/optionalInfoQueries
-#     Explicitly define which info queries are optional.
-set(UNIFIED_RUNTIME_TAG 501057c67bed70cc3eff09d203dc4b3db18408c2)
+# Date:   Wed Dec 18 09:30:22 2024 +0000
+#     Merge pull request #2376 from aarongreig/aaron/parameterizeAdapterTests
+#     Enable adapter tests to run on all discovered adapters.
+set(UNIFIED_RUNTIME_TAG 607e88d09139337e3fe508016c4238f05cb2a8b6)
