36c12252534d8776f59a907ee5dbce21ddc44520
Luke Drummond <luke.drummond@codeplay.com>
2024-12-18 18:29:27 +0000

    Get rid of extra-confounding test helper code
    I can't work out why all this complexity could be needed. My guess is
    there was a plan to mould this further into something useful, but we're
    not using it and it's fairly confusing to boot. DELETE.

--- test/adapters/cuda/fixtures.h
+++ test/adapters/cuda/fixtures.h
@@ -8,36 +8,12 @@
 #include <cuda.h>
 #include <uur/fixtures.h>
 
-namespace uur {
-struct ResultCuda {
-
-    constexpr ResultCuda(CUresult result) noexcept : value(result) {}
-
-    inline bool operator==(const ResultCuda &rhs) const noexcept {
-        return rhs.value == value;
-    }
-
-    CUresult value;
-};
-
-} // namespace uur
-
-#ifndef ASSERT_EQ_RESULT_CUDA
-#define ASSERT_EQ_RESULT_CUDA(EXPECTED, ACTUAL)                                \
-    ASSERT_EQ(uur::ResultCuda(EXPECTED), uur::ResultCuda(ACTUAL))
-#endif // ASSERT_EQ_RESULT_CUDA
-
 #ifndef ASSERT_SUCCESS_CUDA
-#define ASSERT_SUCCESS_CUDA(ACTUAL) ASSERT_EQ_RESULT_CUDA(CUDA_SUCCESS, ACTUAL)
+#define ASSERT_SUCCESS_CUDA(ACTUAL) ASSERT_EQ(CUDA_SUCCESS, (ACTUAL))
 #endif // ASSERT_SUCCESS_CUDA
 
-#ifndef EXPECT_EQ_RESULT_CUDA
-#define EXPECT_EQ_RESULT_CUDA(EXPECTED, ACTUAL)                                \
-    EXPECT_EQ(uur::ResultCuda(EXPECTED), uur::ResultCuda(ACTUAL))
-#endif // EXPECT_EQ_RESULT_CUDA
-
 #ifndef EXPECT_SUCCESS_CUDA
-#define EXPECT_SUCCESS_CUDA(ACTUAL) EXPECT_EQ_RESULT_CUDA(CUDA_SUCCESS, ACTUAL)
+#define EXPECT_SUCCESS_CUDA(ACTUAL) EXPECT_EQ(CUDA_SUCCESS, (ACTUAL))
 #endif // EXPECT_EQ_RESULT_CUDA
 
 #endif // UR_TEST_CONFORMANCE_ADAPTERS_CUDA_FIXTURES_H_INCLUDED
