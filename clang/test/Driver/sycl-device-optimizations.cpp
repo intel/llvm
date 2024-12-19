9a8613ab8ad0d45314553b4ed76ef26540d5325b
Srividya Sundaram <srividya.sundaram@intel.com>
2024-12-19 09:18:47 -0800

    [SYCL] Set -O0 as the default SYCL device optimization if -g is passed (#16408)
    Set `O0` as the default SYCL device optimization if `-g` is passed and
    optimization level is not provided explicitly.
    Use of `'-g'` without any optimization-level option will turn off
    compiler optimizations similar to use of `'-O0'`.

--- clang/test/Driver/sycl-device-optimizations.cpp
+++ clang/test/Driver/sycl-device-optimizations.cpp
@@ -53,3 +53,59 @@
 // RUN:   %clang -### -fsycl --offload-new-driver %s 2>&1 \
 // RUN:   | FileCheck -check-prefix=CHECK-NO-THRESH %s
 // CHECK-NO-THRESH-NOT: "-mllvm" "-inline-threshold
+
+/// Check that optimizations for sycl device are disabled with -g passed:
+// RUN:   %clang -### -fsycl -g %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-DEBUG %s
+// RUN:   %clang_cl -### -fsycl -g %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-DEBUG %s
+// CHECK-DEBUG: clang{{.*}} "-fsycl-is-device{{.*}}" "-O0"
+// CHECK-DEBUG: sycl-post-link{{.*}} "-O0"
+// CHECK-DEBUG-NOT: "-O2"
+
+/// Check that optimizations for sycl device are enabled with -g and O2 passed:
+// RUN:   %clang -### -fsycl -O2 -g %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-G-O2 %s
+// For clang_cl, -O2 maps to -O3
+// RUN:   %clang_cl -### -fsycl -O2 -g %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-G-O3 %s
+// CHECK-G-O2: clang{{.*}} "-fsycl-is-device{{.*}}" "-O2"
+// CHECK-G-O2: sycl-post-link{{.*}} "-O2"
+// CHECK-G-O2-NOT: "-O0"
+// CHECK-G-O3: clang{{.*}} "-fsycl-is-device{{.*}}" "-O3"
+// CHECK-G-O3: sycl-post-link{{.*}} "-O3"
+// CHECK-G-O3-NOT: "-O0"
+
+/// Check that -O2 is passed for FPGA
+// RUN:   %clang -### -fintelfpga -fsycl-early-optimizations %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-FPGA %s
+// RUN:   %clang_cl -### -fintelfpga -fsycl-early-optimizations %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-FPGA %s
+// CHECK-FPGA: clang{{.*}} "-fsycl-is-device{{.*}}" "-O2"
+// CHECK-FPGA: sycl-post-link{{.*}} "-O2"
+// CHECK-FPGA-NOT: "-O0"
+
+/// Check that -O2 preserves for FPGA when it's explicitly passed
+// RUN:   %clang -### -O2 -fintelfpga -fsycl-early-optimizations %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-FPGA-O2 %s
+// For clang_cl, -O2 maps to -O3
+// RUN:   %clang_cl -### -O2 -fintelfpga -fsycl-early-optimizations %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-FPGA-O3 %s
+// CHECK-FPGA-O2: clang{{.*}} "-fsycl-is-device{{.*}}" "-O2"
+// CHECK-FPGA-O2: sycl-post-link{{.*}} "-O2"
+// CHECK-FPGA-O2-NOT: "-O0"
+// CHECK-FPGA-O3: clang{{.*}} "-fsycl-is-device{{.*}}" "-O3"
+// CHECK-FPGA-O3: sycl-post-link{{.*}} "-O3"
+// CHECK-FPGA-O3-NOT: "-O0"
+
+/// Check that -O0 is passed for FPGA when -g is explicitly passed
+// RUN:   %clang -### -fintelfpga -g %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-FPGA-O0 %s
+// RUN:   %clang_cl -### -fintelfpga -g %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=CHECK-FPGA-O0 %s
+// CHECK-FPGA-O0: clang{{.*}} "-fsycl-is-device{{.*}}" "-O0"
+// CHECK-FPGA-O0: sycl-post-link{{.*}} "-O0"
+// CHECK-FPGA-O0-NOT: "-O2"
+
+
+
