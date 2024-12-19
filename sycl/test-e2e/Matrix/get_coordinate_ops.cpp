7f465fc2275c3c83be2df97c028b9962d2d14166
YixingZhang007 <yixing.zhang@intel.com>
2024-12-19 09:28:22 -0500

    [SYCL][E2E]  Update get_coordinate tests to verify both row and columns (#15880)
    Improving Test Coverage and Cleanliness:
    - Implement validation for the `get_coordinate` function to check both
    row and column indices for all matrix types in `get_coordinate_ops.cpp`.
    - Remove the original tests: `get_coord_int8_matA.cpp`,
    `get_coord_int8_matB.cpp`, and `get_coord_float_matC.cpp`. These tests
    have been consolidated and are now included in `get_coordinate_ops.cpp`.

--- sycl/test-e2e/Matrix/get_coordinate_ops.cpp
+++ sycl/test-e2e/Matrix/get_coordinate_ops.cpp
@@ -1,20 +1,18 @@
-//==----------- get_coord_int8_matA.cpp  - DPC++ joint_matrix---------==//
+//==----------- get_coordinate_ops.cpp - DPC++ joint_matrix---------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-// SG size = 32 is not currently supported for SYCL Joint Matrix by IGC on DG2
-// UNSUPPORTED: gpu-intel-dg2
 // REQUIRES: aspect-ext_intel_matrix
-// REQUIRES-INTEL-DRIVER: lin: 27501, win: 101.4943
+
+// XFAIL: !igc-dev
+// XFAIL-TRACKER: GSD-6376
+// REQUIRES-INTEL-DRIVER: lin: 30049
 
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 
-#include "../common.hpp"
-
-#define SG_SZ 32
-
-#include "../get_coord_int8_matA_impl.hpp"
+#include "common.hpp"
+#include "get_coordinate_ops_impl.hpp"
