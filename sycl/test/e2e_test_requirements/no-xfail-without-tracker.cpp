1368b9151322a9a483ac00dbe51ab6c17c8af155
Lorenc Bushi <lorenc.bushi@intel.com>
2024-12-19 12:03:08 -0800

    Modify logic in counting of improperly XFAIL-ed tests

--- sycl/test/e2e_test_requirements/no-xfail-without-tracker.cpp
+++ sycl/test/e2e_test_requirements/no-xfail-without-tracker.cpp
@@ -20,31 +20,10 @@
 // - in the result, search for "XFAIL" again, but invert the result - this
 //   allows us to get the line *after* XFAIL
 // - in those lines, check that XFAIL-TRACKER is present and correct. Once
-//   again, invert the search to get all "bad" lines and save the test names in
-//   the temp file
-// - make a final count of how many ill-formatted directives there are and
-//   verify that against the reference
-// - ...and check if the list of improperly XFAIL-ed tests needs to be updated.
+//   again, invert the search to get all "bad" lines. Because there should be no 
+//   bad lines, check that the search fails using the negation operator !.
 //
 // RUN: grep -rI "XFAIL:" %S/../../test-e2e \
 // RUN: -A 1 --include=*.cpp --no-group-separator | \
 // RUN: grep -v "XFAIL:" | \
-// RUN: grep -Pv "XFAIL-TRACKER:\s+(?:https://github.com/[\w\d-]+/[\w\d-]+/issues/[\d]+)|(?:[\w]+-[\d]+)" > %t
-// RUN: cat %t | wc -l | FileCheck %s --check-prefix NUMBER-OF-XFAIL-WITHOUT-TRACKER
-//
-// The number below is a number of tests which are *improperly* XFAIL-ed, i.e.
-// we either don't have a tracker associated with a failure listed in those
-// tests, or it is listed in a wrong format.
-// Note: strictly speaking, that is not amount of files, but amount of XFAIL
-// directives. If a test contains several XFAIL directives, some of them may be
-// valid and other may not.
-//
-// That number *must not* increase. Any PR which causes this number to grow
-// should be rejected and it should be updated to either keep the number as-is
-// or have it reduced (preferably, down to zero).
-//
-// If you see this test failed for your patch, it means that you either
-// introduced XFAIL directive to a test improperly, or broke the format of an
-// existing XFAIL-ed tests.
-//
-// NUMBER-OF-XFAIL-WITHOUT-TRACKER: 0
+// RUN: ! grep -Pv "XFAIL-TRACKER:\s+(?:https://github.com/[\w\d-]+/[\w\d-]+/issues/[\d]+)|(?:[\w]+-[\d]+)"
