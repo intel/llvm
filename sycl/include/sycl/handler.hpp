dac7175cb34f1a6dc1348d6c0e1e7325fdaea5c6
Alexey Sachkov <alexey.sachkov@intel.com>
2024-12-20 02:12:32 -0800

    Dummy change to trigger full pre-commit

--- sycl/include/sycl/handler.hpp
+++ sycl/include/sycl/handler.hpp
@@ -1,4 +1,4 @@
-//==-------- handler.hpp --- SYCL command group handler --------------------==//
+ //==-------- handler.hpp --- SYCL command group handler --------------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
