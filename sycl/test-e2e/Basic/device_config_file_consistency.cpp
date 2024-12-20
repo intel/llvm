d7264c9a46e9bf0f9e4c85e164546e3e539b0b4a
Cai, Justin <justin.cai@intel.com>
2024-12-20 11:23:50 -0800

    Make style consistent

--- sycl/test-e2e/Basic/device_config_file_consistency.cpp
+++ sycl/test-e2e/Basic/device_config_file_consistency.cpp
@@ -7,7 +7,7 @@
 
 // UNSUPPORTED: accelerator
 // UNSUPPORTED-INTENDED: Accelerator is not supported by
-// sycl_ext_oneapi_device_architecture
+// sycl_ext_oneapi_device_architecture.
 // REQUIRES: device-config-file
 // RUN: %{build} -o %t.out %device_config_file_include_flag
 // RUN: %{run} %t.out
@@ -23,8 +23,8 @@ using namespace sycl;
 
 const char *getArchName(const device &Device) {
   namespace syclex = sycl::ext::oneapi::experimental;
-  auto arch = Device.get_info<syclex::info::device::architecture>();
-  switch (arch) {
+  auto Arch = Device.get_info<syclex::info::device::architecture>();
+  switch (Arch) {
 #define __SYCL_ARCHITECTURE(ARCH, VAL)                                         \
   case syclex::architecture::ARCH:                                             \
     return #ARCH;
@@ -36,14 +36,14 @@ const char *getArchName(const device &Device) {
   return "unknown";
 }
 
-// checks if a container contains a specific element
+// Checks if a container contains a specific element
 template <typename Container, typename T>
-bool contains(const Container &c, const T &elem) {
-  return std::find(c.begin(), c.end(), elem) != c.end();
+bool contains(const Container &C, const T &Elem) {
+  return std::find(C.begin(), C.end(), Elem) != C.end();
 }
 
-std::string_view getAspectName(aspect asp) {
-  switch (asp) {
+std::string_view getAspectName(aspect Asp) {
+  switch (Asp) {
 #define __SYCL_ASPECT(ASPECT, ID)                                              \
   case aspect::ASPECT:                                                         \
     return #ASPECT;
@@ -53,9 +53,9 @@ std::string_view getAspectName(aspect asp) {
   return "unknown";
 }
 
-aspect getAspectByName(std::string_view name) {
+aspect getAspectByName(std::string_view Name) {
 #define __SYCL_ASPECT(ASPECT, ID)                                              \
-  if (name == #ASPECT)                                                         \
+  if (Name == #ASPECT)                                                         \
     return aspect::ASPECT;
 #include <sycl/info/aspects.def>
   throw std::invalid_argument("Unknown aspect name");
@@ -63,77 +63,77 @@ aspect getAspectByName(std::string_view name) {
 
 int main() {
   // Get the device arch
-  queue q;
-  auto dev = q.get_device();
-  auto device_name = getArchName(dev);
+  queue Q;
+  auto Dev = Q.get_device();
+  auto DeviceName = getArchName(Dev);
 
-  auto TargetInfo = DeviceConfigFile::TargetTable.find(device_name);
+  auto TargetInfo = DeviceConfigFile::TargetTable.find(DeviceName);
   if (TargetInfo == DeviceConfigFile::TargetTable.end()) {
-    std::cout << "No aspects found for device " << device_name << std::endl;
+    std::cout << "No aspects found for device " << DeviceName << "\n";
     return 1;
   }
 
   // Check aspects consistency
-  int nAspectInconsistencies = 0;
-  std::cout << "Checking consistency of aspects for device " << device_name
+  int NAspectInconsistencies = 0;
+  std::cout << "Checking consistency of aspects for device " << DeviceName
             << "...\n";
 
-  auto supportedAspects = dev.get_info<info::device::aspects>();
-  auto deviceConfigAspectNames = TargetInfo->second.aspects;
-  std::vector<aspect> deviceConfigAspects;
-  for (auto aspectName : deviceConfigAspectNames) {
-    deviceConfigAspects.push_back(getAspectByName(aspectName));
+  auto SupportedAspects = Dev.get_info<info::device::aspects>();
+  auto DeviceConfigAspectNames = TargetInfo->second.aspects;
+  std::vector<aspect> DeviceConfigAspects;
+  for (auto AspectName : DeviceConfigAspectNames) {
+    DeviceConfigAspects.push_back(getAspectByName(AspectName));
   }
 
-  for (auto asp : deviceConfigAspects) {
-    if (!contains(supportedAspects, asp)) {
-      std::cout << "error: " << device_name << " does not support aspect "
-                << getAspectName(asp)
+  for (auto Asp : DeviceConfigAspects) {
+    if (!contains(SupportedAspects, Asp)) {
+      std::cout << "error: " << DeviceName << " does not support aspect "
+                << getAspectName(Asp)
                 << " but it is declared in the device config file\n";
-      ++nAspectInconsistencies;
+      ++NAspectInconsistencies;
     }
   }
-  for (auto asp : supportedAspects) {
-    if (!contains(deviceConfigAspects, asp)) {
-      std::cout << "note: the device " << device_name << " supports aspect "
-                << getAspectName(asp)
+  for (auto Asp : SupportedAspects) {
+    if (!contains(DeviceConfigAspects, Asp)) {
+      std::cout << "note: the device " << DeviceName << " supports aspect "
+                << getAspectName(Asp)
                 << " but it is not declared in the device config file\n";
       // Not necessarily an error, so we won't increment n_fail
     }
   }
 
-  if (nAspectInconsistencies == 0)
+  if (NAspectInconsistencies == 0)
     std::cout << "All aspects are consistent\n";
 
   // Check sub-group sizes consistency
-  int nSubGroupSizeInconsistencies = 0;
+  int NSubGroupSizeInconsistencies = 0;
   std::cout << "Checking consistency of sub-group sizes for device "
-            << device_name << "...\n";
+            << DeviceName << "...\n";
 
-  auto supportedSubGroupSizes = dev.get_info<info::device::sub_group_sizes>();
-  auto deviceConfigSubGroupSizes = TargetInfo->second.subGroupSizes;
+  auto SupportedSubGroupSizes = Dev.get_info<info::device::sub_group_sizes>();
+  auto DeviceConfigSubGroupSizes = TargetInfo->second.subGroupSizes;
 
-  for (auto size : deviceConfigSubGroupSizes) {
-    if (!contains(supportedSubGroupSizes, size)) {
-      std::cout << "error: " << device_name
-                << " does not support sub-group size " << size
+  for (auto Size : DeviceConfigSubGroupSizes) {
+    if (!contains(SupportedSubGroupSizes, Size)) {
+      std::cout << "error: " << DeviceName
+                << " does not support sub-group size " << Size
                 << " but it is declared in the device config file\n";
-      ++nSubGroupSizeInconsistencies;
+      ++NSubGroupSizeInconsistencies;
     }
   }
-  for (auto size : supportedSubGroupSizes) {
-    if (!contains(deviceConfigSubGroupSizes, size)) {
-      std::cout << "note: the device " << device_name
-                << " supports sub-group size " << size
+  for (auto Size : SupportedSubGroupSizes) {
+    if (!contains(DeviceConfigSubGroupSizes, Size)) {
+      std::cout << "note: the device " << DeviceName
+                << " supports sub-group size " << Size
                 << " but it is not declared in the device config file\n";
       // Not necessarily an error, so we won't increment n_fail
     }
   }
 
-  if (nSubGroupSizeInconsistencies == 0)
+  if (NSubGroupSizeInconsistencies == 0)
     std::cout << "All sub-group sizes are consistent\n";
 
-  return nAspectInconsistencies + nSubGroupSizeInconsistencies;
+  return NAspectInconsistencies + NSubGroupSizeInconsistencies;
 }
 
 #undef __SYCL_ASPECT_DEPRECATED_ALIAS
