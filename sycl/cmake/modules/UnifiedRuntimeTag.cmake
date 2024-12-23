dd81737134a1ddc86b8189081387986b37de5f6f
Mestre, Fabio <fabio.mestre@intel.com>
2024-12-23 19:04:13 +0100

    Bump UR to remove handle translation when using MCL

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Thu Dec 19 11:26:01 2024 +0000
 #     Merge pull request #2277 from igchor/cooperative_fix
 #     [Spec] fix urKernelSuggestMaxCooperativeGroupCountExp
-set(UNIFIED_RUNTIME_TAG ea0f3a1f5f15f9af7bf40bd13669afeb9ada569c)
+set(UNIFIED_RUNTIME_TAG b1ae104b6d1b7d23d225bc08759591d947c4eee8)
