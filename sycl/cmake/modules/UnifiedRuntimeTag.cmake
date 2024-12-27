b64bd0bbbf2fbd75ef3bfa3c8cbeeb5c853bfda6
Yang Zhao <yang2.zhao@intel.com>
2024-12-28 05:51:11 +0800

    [DeviceMSAN] Fix MemToShadow algorithm and VA reservation (#16475)
    UR: https://github.com/oneapi-src/unified-runtime/pull/2507

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit bb64b3e9f6d30b232b204590b94250ce2da6967b
-# Merge: d23739306f2c e625bfdaa99b
-# Author: Martin Grant <martin.morrisongrant@codeplay.com>
-# Date:   Martin Grant <martin.morrisongrant@codeplay.com>
-#     Merge pull request #2471 from Bensuo/ben/l0-include-fix
-#     [L0] Fix issue with test include path
-set(UNIFIED_RUNTIME_TAG bb64b3e9f6d30b232b204590b94250ce2da6967b)
+# commit fe6c83a3a40e69eaa0c4829c8ba99b03e05bb4e2
+# Merge: 93fc1331 cf0c9482
+# Author: aarongreig <aaron.greig@codeplay.com>
+# Date:   Fri Dec 27 17:39:52 2024 +0000
+#     Merge pull request #2507 from AllanZyne/review/yang/fix_msan_shadow
+#     [DeviceMSAN] Fix MemToShadow algorithm and VA reservation
+set(UNIFIED_RUNTIME_TAG fe6c83a3a40e69eaa0c4829c8ba99b03e05bb4e2)
