c189f49c30d55e00136c1cd92c60446426b9e09e
Udit Kumar Agarwal <udit.agarwal@intel.com>
2025-01-03 10:42:19 -0800

    Fix `unused_function` error in post-commit
    Failure: https://github.com/intel/llvm/actions/runs/12601531257/job/35122811530#step:12:83

--- sycl/source/detail/os_util.cpp
+++ sycl/source/detail/os_util.cpp
@@ -58,7 +58,7 @@ namespace sycl {
 inline namespace _V1 {
 namespace detail {
 
-#if defined(__INTEL_PREVIEW_BREAKING_CHANGES)
+#if defined(__INTEL_PREVIEW_BREAKING_CHANGES) && defined(__SYCL_RT_OS_LINUX)
 static std::string getDirName(const char *Path)
 #else
 std::string OSUtil::getDirName(const char *Path)
