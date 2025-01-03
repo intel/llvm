d8ce8d4db9318c3f87cd8dd61cb97d5aa1fb0bc5
Martin Morrison-Grant <martin.morrisongrant@codeplay.com>
2025-01-03 10:10:48 +0000

    [UR] Update tag to ad288bb for:
    - https://github.com/oneapi-src/unified-runtime/pull/2501
    - https://github.com/oneapi-src/unified-runtime/pull/2502

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 13f5e52ac047bb81f478f0dbe9416a8bc179130a
-# Merge: fe6c83a3 fa1e678a
-# Author: aarongreig <aaron.greig@codeplay.com>
-# Date:   Mon Dec 30 15:14:19 2024 +0000
-#     Merge pull request #2484 from zhaomaosu/move-clean-shadow-to-launchinfo
-#     [DevMSAN] Move clean shadow into launch info
-set(UNIFIED_RUNTIME_TAG 13f5e52ac047bb81f478f0dbe9416a8bc179130a)
+# commit ad288bb36c45e24a35a75c665d382ce18935fcf7
+# Merge: db491abc6aba f8b04f4216e8
+# Author: Martin Grant <martin.morrisongrant@codeplay.com>
+# Date:   Fri Jan 3 10:08:59 2025 +0000
+#     Merge pull request #2502 from ldrumm/luke/tensormap-version
+#     CUTensorMap is only in CUDA v12
+set(UNIFIED_RUNTIME_TAG ad288bb36c45e24a35a75c665d382ce18935fcf7)
