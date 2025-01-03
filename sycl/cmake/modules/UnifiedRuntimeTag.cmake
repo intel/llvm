37a2af34b376a1a508421bcd977f17dfd50e1486
Zhao, Yang2 <yang2.zhao@intel.com>
2025-01-03 09:33:15 +0100

    update ur tag

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Fri Dec 27 17:39:52 2024 +0000
 #     Merge pull request #2507 from AllanZyne/review/yang/fix_msan_shadow
 #     [DeviceMSAN] Fix MemToShadow algorithm and VA reservation
-set(UNIFIED_RUNTIME_TAG fe6c83a3a40e69eaa0c4829c8ba99b03e05bb4e2)
+set(UNIFIED_RUNTIME_TAG review/yang/fix_msan_usm)
