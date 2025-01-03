66d98a8a22d4401f0f77c1be5f30c8483636a197
Martin Morrison-Grant <martin.morrisongrant@codeplay.com>
2025-01-03 14:38:00 +0000

    [UR] Update tag to f0b35200c for https://github.com/oneapi-src/unified-runtime/pull/2503

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit ad288bb36c45e24a35a75c665d382ce18935fcf7
-# Merge: db491abc6aba f8b04f4216e8
+# commit f0b35200c07e6a15766ba75d01a3c176d42a2429
+# Merge: 545a01bae624 ceb0465d4090
 # Author: Martin Grant <martin.morrisongrant@codeplay.com>
-# Date:   Fri Jan 3 10:08:59 2025 +0000
-#     Merge pull request #2502 from ldrumm/luke/tensormap-version
-#     CUTensorMap is only in CUDA v12
-set(UNIFIED_RUNTIME_TAG ad288bb36c45e24a35a75c665d382ce18935fcf7)
+# Date:   Mon Jan 6 09:51:26 2025 +0000
+#     Merge pull request #2519 from oneapi-src/revert-2481-fabio/fix_potential_race_condition
+#     Revert "Fix potential deadlock in the WaitEvent path of CmdBuffers"
+set(UNIFIED_RUNTIME_TAG f0b35200c07e6a15766ba75d01a3c176d42a2429)
