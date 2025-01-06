oneapi-src/unified-runtime

Improve command-buffer update error code wording
The `UR_DEVICE_INFO_COMMAND_BUFFER_UPDATE_SUPPORT_EXP` query no longer
exists, so error defined for trying to make an updatable command-buffer
when a device doesn't support it needs reworded to say if no
capabilities are supported.

Additionally, for each individual update capability, specify that
an error is thrown if update is attempted using that characteristic.

UR CTS added to verify this.

test/conformance/exp_command_buffer/update/invalid_update.cpp
@@ -224,3 +224,254 @@ TEST_P(InvalidUpdateTest, InvalidDimensions) {
         UR_RESULT_ERROR_INVALID_VALUE,
         urCommandBufferUpdateKernelLaunchExp(command_handle, &update_desc));
 }
+
+// Tests that an error is thrown when trying to update a kernel capability
+// that isn't supported.
+struct urInvalidUpdateCommandBufferExpExecutionTest
+    : uur::urKernelExecutionTest {
+    void SetUp() override {
+        program_name = "fill_usm";
+        UUR_RETURN_ON_FATAL_FAILURE(uur::urKernelExecutionTest::SetUp());
+
+        UUR_RETURN_ON_FATAL_FAILURE(
+            uur::command_buffer::checkCommandBufferSupport(device));
+
+        ASSERT_SUCCESS(urDeviceGetInfo(
+            device, UR_DEVICE_INFO_COMMAND_BUFFER_UPDATE_CAPABILITIES_EXP,
+            sizeof(update_capability_flags), &update_capability_flags,
+            nullptr));
+
+        if (0 == update_capability_flags) {
+            GTEST_SKIP() << "Test requires update support from device";
+        }
+
+        ur_device_usm_access_capability_flags_t shared_usm_flags;
+        ASSERT_SUCCESS(
+            uur::GetDeviceUSMSingleSharedSupport(device, shared_usm_flags));
+        if (!(shared_usm_flags & UR_DEVICE_USM_ACCESS_CAPABILITY_FLAG_ACCESS)) {
+            GTEST_SKIP() << "Shared USM is not supported.";
+        }
+
+        // Allocate USM pointer to fill
+        ASSERT_SUCCESS(urUSMSharedAlloc(context, device, nullptr, nullptr,
+                                        allocation_size, &shared_ptr));
+        ASSERT_NE(shared_ptr, nullptr);
+        std::memset(shared_ptr, 0, allocation_size);
+
+        // Index 0 is output
+        ASSERT_SUCCESS(urKernelSetArgPointer(kernel, 0, nullptr, shared_ptr));
+        // Index 1 is input scalar
+        ASSERT_SUCCESS(
+            urKernelSetArgValue(kernel, 1, sizeof(val), nullptr, &val));
+
+        // Create a command-buffer with update enabled.
+        ur_exp_command_buffer_desc_t desc{
+            UR_STRUCTURE_TYPE_EXP_COMMAND_BUFFER_DESC, nullptr, true, false,
+            false};
+
+        ASSERT_SUCCESS(urCommandBufferCreateExp(context, device, &desc,
+                                                &updatable_cmd_buf_handle));
+        ASSERT_NE(updatable_cmd_buf_handle, nullptr);
+
+        // Append kernel command to command-buffer
+        ASSERT_SUCCESS(urCommandBufferAppendKernelLaunchExp(
+            updatable_cmd_buf_handle, kernel, n_dimensions, &global_offset,
+            &global_size, &local_size, 0, nullptr, 0, nullptr, 0, nullptr,
+            nullptr, nullptr, &command_handle));
+        ASSERT_NE(command_handle, nullptr);
+
+        ASSERT_SUCCESS(urCommandBufferFinalizeExp(updatable_cmd_buf_handle));
+
+        ASSERT_SUCCESS(urKernelCreate(program, kernel_name.data(), &kernel_2));
+    }
+
+    void TearDown() override {
+        if (updatable_cmd_buf_handle) {
+            EXPECT_SUCCESS(urCommandBufferReleaseExp(updatable_cmd_buf_handle));
+        }
+
+        if (shared_ptr) {
+            EXPECT_SUCCESS(urUSMFree(context, shared_ptr));
+        }
+
+        if (command_handle) {
+            EXPECT_SUCCESS(urCommandBufferReleaseCommandExp(command_handle));
+        }
+
+        if (kernel_2) {
+            ASSERT_SUCCESS(urKernelRelease(kernel_2));
+        }
+
+        UUR_RETURN_ON_FATAL_FAILURE(urKernelExecutionTest::TearDown());
+    }
+
+    ur_exp_command_buffer_handle_t updatable_cmd_buf_handle = nullptr;
+    ur_exp_command_buffer_command_handle_t command_handle = nullptr;
+    ur_kernel_handle_t kernel_2 = nullptr;
+    static constexpr uint32_t val = 42;
+    static constexpr size_t local_size = 4;
+    static constexpr size_t global_size = 32;
+    static constexpr size_t global_offset = 0;
+    static constexpr uint32_t n_dimensions = 1;
+    static constexpr size_t allocation_size = sizeof(val) * global_size;
+    void *shared_ptr = nullptr;
+    ur_device_command_buffer_update_capability_flags_t update_capability_flags =
+        0;
+};
+
+UUR_INSTANTIATE_DEVICE_TEST_SUITE_P(
+    urInvalidUpdateCommandBufferExpExecutionTest);
+
+// Test error reported if device doesn't support updating kernel args
+TEST_P(urInvalidUpdateCommandBufferExpExecutionTest, KernelArg) {
+    if (update_capability_flags &
+        UR_DEVICE_COMMAND_BUFFER_UPDATE_CAPABILITY_FLAG_KERNEL_ARGUMENTS) {
+        GTEST_SKIP() << "Test requires device to not support kernel arg update "
+                        "capability";
+    }
+
+    uint32_t new_val = 33;
+    ur_exp_command_buffer_update_value_arg_desc_t new_input_desc = {
+        UR_STRUCTURE_TYPE_EXP_COMMAND_BUFFER_UPDATE_VALUE_ARG_DESC, // stype
+        nullptr,                                                    // pNext
+        1,                                                          // argIndex
+        sizeof(new_val),                                            // argSize
+        nullptr,  // pProperties
+        &new_val, // hArgValue
+    };
+
+    ur_exp_command_buffer_update_kernel_launch_desc_t update_desc = {
+        UR_STRUCTURE_TYPE_EXP_COMMAND_BUFFER_UPDATE_KERNEL_LAUNCH_DESC, // stype
+        nullptr,                                                        // pNext
+        nullptr,         // hNewKernel
+        0,               // numNewMemObjArgs
+        0,               // numNewPointerArgs
+        1,               // numNewValueArgs
+        n_dimensions,    // newWorkDim
+        nullptr,         // pNewMemObjArgList
+        nullptr,         // pNewPointerArgList
+        &new_input_desc, // pNewValueArgList
+        nullptr,         // pNewGlobalWorkOffset
+        nullptr,         // pNewGlobalWorkSize
+        nullptr,         // pNewLocalWorkSize
+    };
+
+    ur_result_t result =
+        urCommandBufferUpdateKernelLaunchExp(command_handle, &update_desc);
+    ASSERT_EQ(UR_RESULT_ERROR_UNSUPPORTED_FEATURE, result);
+}
+
+TEST_P(urInvalidUpdateCommandBufferExpExecutionTest, GlobalSize) {
+    if (update_capability_flags &
+        UR_DEVICE_COMMAND_BUFFER_UPDATE_CAPABILITY_FLAG_GLOBAL_WORK_SIZE) {
+        GTEST_SKIP() << "Test requires device to not support global work size "
+                        "update capability.";
+    }
+
+    auto new_global_size = global_size * 2;
+    ur_exp_command_buffer_update_kernel_launch_desc_t update_desc = {
+        UR_STRUCTURE_TYPE_EXP_COMMAND_BUFFER_UPDATE_KERNEL_LAUNCH_DESC, // stype
+        nullptr,                                                        // pNext
+        nullptr,          // hNewKernel
+        0,                // numNewMemObjArgs
+        0,                // numNewPointerArgs
+        0,                // numNewValueArgs
+        n_dimensions,     // newWorkDim
+        nullptr,          // pNewMemObjArgList
+        nullptr,          // pNewPointerArgList
+        nullptr,          // pNewValueArgList
+        nullptr,          // pNewGlobalWorkOffset
+        &new_global_size, // pNewGlobalWorkSize
+        nullptr,          // pNewLocalWorkSize
+    };
+
+    ur_result_t result =
+        urCommandBufferUpdateKernelLaunchExp(command_handle, &update_desc);
+    ASSERT_EQ(UR_RESULT_ERROR_UNSUPPORTED_FEATURE, result);
+}
+
+TEST_P(urInvalidUpdateCommandBufferExpExecutionTest, GlobalOffset) {
+    if (update_capability_flags &
+        UR_DEVICE_COMMAND_BUFFER_UPDATE_CAPABILITY_FLAG_GLOBAL_WORK_OFFSET) {
+        GTEST_SKIP() << "Test requires device to not support global work "
+                        "offset update capability.";
+    }
+
+    size_t new_global_offset = 1;
+    ur_exp_command_buffer_update_kernel_launch_desc_t update_desc = {
+        UR_STRUCTURE_TYPE_EXP_COMMAND_BUFFER_UPDATE_KERNEL_LAUNCH_DESC, // stype
+        nullptr,                                                        // pNext
+        nullptr,            // hNewKernel
+        0,                  // numNewMemObjArgs
+        0,                  // numNewPointerArgs
+        0,                  // numNewValueArgs
+        n_dimensions,       // newWorkDim
+        nullptr,            // pNewMemObjArgList
+        nullptr,            // pNewPointerArgList
+        nullptr,            // pNewValueArgList
+        &new_global_offset, // pNewGlobalWorkOffset
+        nullptr,            // pNewGlobalWorkSize
+        nullptr,            // pNewLocalWorkSize
+    };
+
+    ur_result_t result =
+        urCommandBufferUpdateKernelLaunchExp(command_handle, &update_desc);
+    ASSERT_EQ(UR_RESULT_ERROR_UNSUPPORTED_FEATURE, result);
+}
+
+TEST_P(urInvalidUpdateCommandBufferExpExecutionTest, LocalSize) {
+    if (update_capability_flags &
+        UR_DEVICE_COMMAND_BUFFER_UPDATE_CAPABILITY_FLAG_LOCAL_WORK_SIZE) {
+        GTEST_SKIP() << "Test requires device to not support local work size "
+                        "update capability.";
+    }
+
+    size_t new_local_size = 2;
+    ur_exp_command_buffer_update_kernel_launch_desc_t update_desc = {
+        UR_STRUCTURE_TYPE_EXP_COMMAND_BUFFER_UPDATE_KERNEL_LAUNCH_DESC, // stype
+        nullptr,                                                        // pNext
+        nullptr,         // hNewKernel
+        0,               // numNewMemObjArgs
+        0,               // numNewPointerArgs
+        0,               // numNewValueArgs
+        n_dimensions,    // newWorkDim
+        nullptr,         // pNewMemObjArgList
+        nullptr,         // pNewPointerArgList
+        nullptr,         // pNewValueArgList
+        nullptr,         // pNewGlobalWorkOffset
+        nullptr,         // pNewGlobalWorkSize
+        &new_local_size, // pNewLocalWorkSize
+    };
+
+    ur_result_t result =
+        urCommandBufferUpdateKernelLaunchExp(command_handle, &update_desc);
+    ASSERT_EQ(UR_RESULT_ERROR_UNSUPPORTED_FEATURE, result);
+}
+
+TEST_P(urInvalidUpdateCommandBufferExpExecutionTest, Kernel) {
+    if (update_capability_flags &
+        UR_DEVICE_COMMAND_BUFFER_UPDATE_CAPABILITY_FLAG_KERNEL_HANDLE) {
+        GTEST_SKIP() << "Test requires device to not support kernel handle "
+                        "update capability.";
+    }
+
+    ur_exp_command_buffer_update_kernel_launch_desc_t update_desc = {
+        UR_STRUCTURE_TYPE_EXP_COMMAND_BUFFER_UPDATE_KERNEL_LAUNCH_DESC, // stype
+        nullptr,                                                        // pNext
+        kernel_2,     // hNewKernel
+        0,            // numNewMemObjArgs
+        0,            // numNewPointerArgs
+        0,            // numNewValueArgs
+        n_dimensions, // newWorkDim
+        nullptr,      // pNewMemObjArgList
+        nullptr,      // pNewPointerArgList
+        nullptr,      // pNewValueArgList
+        nullptr,      // pNewGlobalWorkOffset
+        nullptr,      // pNewGlobalWorkSize
+        nullptr,      // pNewLocalWorkSize
+    };
+
+    ur_result_t result =
+        urCommandBufferUpdateKernelLaunchExp(command_handle, &update_desc);
+    ASSERT_EQ(UR_RESULT_ERROR_UNSUPPORTED_FEATURE, result);
+}

intel/llvm

test/conformance/exp_command_buffer/update/invalid_update.cpp
@@ -224,3 +224,252 @@ TEST_P(InvalidUpdateTest, InvalidDimensions) {
         UR_RESULT_ERROR_INVALID_VALUE,
         urCommandBufferUpdateKernelLaunchExp(command_handle, &update_desc));
 }
+
+// Tests that an error is thrown when trying to update a kernel capability
+// that isn't supported.
+struct InvalidUpdateCommandBufferExpExecutionTest : uur::urKernelExecutionTest {
+    void SetUp() override {
+        program_name = "fill_usm";
+        UUR_RETURN_ON_FATAL_FAILURE(uur::urKernelExecutionTest::SetUp());
+
+        UUR_RETURN_ON_FATAL_FAILURE(
+            uur::command_buffer::checkCommandBufferSupport(device));
+
+        ASSERT_SUCCESS(urDeviceGetInfo(
+            device, UR_DEVICE_INFO_COMMAND_BUFFER_UPDATE_CAPABILITIES_EXP,
+            sizeof(update_capability_flags), &update_capability_flags,
+            nullptr));
+
+        if (0 == update_capability_flags) {
+            GTEST_SKIP() << "Test requires update support from device";
+        }
+
+        ur_device_usm_access_capability_flags_t shared_usm_flags;
+        ASSERT_SUCCESS(
+            uur::GetDeviceUSMSingleSharedSupport(device, shared_usm_flags));
+        if (!(shared_usm_flags & UR_DEVICE_USM_ACCESS_CAPABILITY_FLAG_ACCESS)) {
+            GTEST_SKIP() << "Shared USM is not supported.";
+        }
+
+        // Allocate USM pointer to fill
+        ASSERT_SUCCESS(urUSMSharedAlloc(context, device, nullptr, nullptr,
+                                        allocation_size, &shared_ptr));
+        ASSERT_NE(shared_ptr, nullptr);
+        std::memset(shared_ptr, 0, allocation_size);
+
+        // Index 0 is output
+        ASSERT_SUCCESS(urKernelSetArgPointer(kernel, 0, nullptr, shared_ptr));
+        // Index 1 is input scalar
+        ASSERT_SUCCESS(
+            urKernelSetArgValue(kernel, 1, sizeof(val), nullptr, &val));
+
+        // Create a command-buffer with update enabled.
+        ur_exp_command_buffer_desc_t desc{
+            UR_STRUCTURE_TYPE_EXP_COMMAND_BUFFER_DESC, nullptr, true, false,
+            false};
+
+        ASSERT_SUCCESS(urCommandBufferCreateExp(context, device, &desc,
+                                                &updatable_cmd_buf_handle));
+        ASSERT_NE(updatable_cmd_buf_handle, nullptr);
+
+        // Append kernel command to command-buffer
+        ASSERT_SUCCESS(urCommandBufferAppendKernelLaunchExp(
+            updatable_cmd_buf_handle, kernel, n_dimensions, &global_offset,
+            &global_size, &local_size, 0, nullptr, 0, nullptr, 0, nullptr,
+            nullptr, nullptr, &command_handle));
+        ASSERT_NE(command_handle, nullptr);
+
+        ASSERT_SUCCESS(urCommandBufferFinalizeExp(updatable_cmd_buf_handle));
+
+        ASSERT_SUCCESS(urKernelCreate(program, kernel_name.data(), &kernel_2));
+    }
+
+    void TearDown() override {
+        if (updatable_cmd_buf_handle) {
+            EXPECT_SUCCESS(urCommandBufferReleaseExp(updatable_cmd_buf_handle));
+        }
+
+        if (shared_ptr) {
+            EXPECT_SUCCESS(urUSMFree(context, shared_ptr));
+        }
+
+        if (command_handle) {
+            EXPECT_SUCCESS(urCommandBufferReleaseCommandExp(command_handle));
+        }
+
+        if (kernel_2) {
+            ASSERT_SUCCESS(urKernelRelease(kernel_2));
+        }
+
+        UUR_RETURN_ON_FATAL_FAILURE(urKernelExecutionTest::TearDown());
+    }
+
+    ur_exp_command_buffer_handle_t updatable_cmd_buf_handle = nullptr;
+    ur_exp_command_buffer_command_handle_t command_handle = nullptr;
+    ur_kernel_handle_t kernel_2 = nullptr;
+    static constexpr uint32_t val = 42;
+    static constexpr size_t local_size = 4;
+    static constexpr size_t global_size = 32;
+    static constexpr size_t global_offset = 0;
+    static constexpr uint32_t n_dimensions = 1;
+    static constexpr size_t allocation_size = sizeof(val) * global_size;
+    void *shared_ptr = nullptr;
+    ur_device_command_buffer_update_capability_flags_t update_capability_flags =
+        0;
+};
+
+UUR_INSTANTIATE_DEVICE_TEST_SUITE_P(InvalidUpdateCommandBufferExpExecutionTest);
+
+// Test error reported if device doesn't support updating kernel args
+TEST_P(InvalidUpdateCommandBufferExpExecutionTest, KernelArg) {
+    if (update_capability_flags &
+        UR_DEVICE_COMMAND_BUFFER_UPDATE_CAPABILITY_FLAG_KERNEL_ARGUMENTS) {
+        GTEST_SKIP() << "Test requires device to not support kernel arg update "
+                        "capability";
+    }
+
+    uint32_t new_val = 33;
+    ur_exp_command_buffer_update_value_arg_desc_t new_input_desc = {
+        UR_STRUCTURE_TYPE_EXP_COMMAND_BUFFER_UPDATE_VALUE_ARG_DESC, // stype
+        nullptr,                                                    // pNext
+        1,                                                          // argIndex
+        sizeof(new_val),                                            // argSize
+        nullptr,  // pProperties
+        &new_val, // hArgValue
+    };
+
+    ur_exp_command_buffer_update_kernel_launch_desc_t update_desc = {
+        UR_STRUCTURE_TYPE_EXP_COMMAND_BUFFER_UPDATE_KERNEL_LAUNCH_DESC, // stype
+        nullptr,                                                        // pNext
+        nullptr,         // hNewKernel
+        0,               // numNewMemObjArgs
+        0,               // numNewPointerArgs
+        1,               // numNewValueArgs
+        n_dimensions,    // newWorkDim
+        nullptr,         // pNewMemObjArgList
+        nullptr,         // pNewPointerArgList
+        &new_input_desc, // pNewValueArgList
+        nullptr,         // pNewGlobalWorkOffset
+        nullptr,         // pNewGlobalWorkSize
+        nullptr,         // pNewLocalWorkSize
+    };
+
+    ur_result_t result =
+        urCommandBufferUpdateKernelLaunchExp(command_handle, &update_desc);
+    ASSERT_EQ(UR_RESULT_ERROR_UNSUPPORTED_FEATURE, result);
+}
+
+TEST_P(InvalidUpdateCommandBufferExpExecutionTest, GlobalSize) {
+    if (update_capability_flags &
+        UR_DEVICE_COMMAND_BUFFER_UPDATE_CAPABILITY_FLAG_GLOBAL_WORK_SIZE) {
+        GTEST_SKIP() << "Test requires device to not support global work size "
+                        "update capability.";
+    }
+
+    auto new_global_size = global_size * 2;
+    ur_exp_command_buffer_update_kernel_launch_desc_t update_desc = {
+        UR_STRUCTURE_TYPE_EXP_COMMAND_BUFFER_UPDATE_KERNEL_LAUNCH_DESC, // stype
+        nullptr,                                                        // pNext
+        nullptr,          // hNewKernel
+        0,                // numNewMemObjArgs
+        0,                // numNewPointerArgs
+        0,                // numNewValueArgs
+        n_dimensions,     // newWorkDim
+        nullptr,          // pNewMemObjArgList
+        nullptr,          // pNewPointerArgList
+        nullptr,          // pNewValueArgList
+        nullptr,          // pNewGlobalWorkOffset
+        &new_global_size, // pNewGlobalWorkSize
+        nullptr,          // pNewLocalWorkSize
+    };
+
+    ur_result_t result =
+        urCommandBufferUpdateKernelLaunchExp(command_handle, &update_desc);
+    ASSERT_EQ(UR_RESULT_ERROR_UNSUPPORTED_FEATURE, result);
+}
+
+TEST_P(InvalidUpdateCommandBufferExpExecutionTest, GlobalOffset) {
+    if (update_capability_flags &
+        UR_DEVICE_COMMAND_BUFFER_UPDATE_CAPABILITY_FLAG_GLOBAL_WORK_OFFSET) {
+        GTEST_SKIP() << "Test requires device to not support global work "
+                        "offset update capability.";
+    }
+
+    size_t new_global_offset = 1;
+    ur_exp_command_buffer_update_kernel_launch_desc_t update_desc = {
+        UR_STRUCTURE_TYPE_EXP_COMMAND_BUFFER_UPDATE_KERNEL_LAUNCH_DESC, // stype
+        nullptr,                                                        // pNext
+        nullptr,            // hNewKernel
+        0,                  // numNewMemObjArgs
+        0,                  // numNewPointerArgs
+        0,                  // numNewValueArgs
+        n_dimensions,       // newWorkDim
+        nullptr,            // pNewMemObjArgList
+        nullptr,            // pNewPointerArgList
+        nullptr,            // pNewValueArgList
+        &new_global_offset, // pNewGlobalWorkOffset
+        nullptr,            // pNewGlobalWorkSize
+        nullptr,            // pNewLocalWorkSize
+    };
+
+    ur_result_t result =
+        urCommandBufferUpdateKernelLaunchExp(command_handle, &update_desc);
+    ASSERT_EQ(UR_RESULT_ERROR_UNSUPPORTED_FEATURE, result);
+}
+
+TEST_P(InvalidUpdateCommandBufferExpExecutionTest, LocalSize) {
+    if (update_capability_flags &
+        UR_DEVICE_COMMAND_BUFFER_UPDATE_CAPABILITY_FLAG_LOCAL_WORK_SIZE) {
+        GTEST_SKIP() << "Test requires device to not support local work size "
+                        "update capability.";
+    }
+
+    size_t new_local_size = 2;
+    ur_exp_command_buffer_update_kernel_launch_desc_t update_desc = {
+        UR_STRUCTURE_TYPE_EXP_COMMAND_BUFFER_UPDATE_KERNEL_LAUNCH_DESC, // stype
+        nullptr,                                                        // pNext
+        nullptr,         // hNewKernel
+        0,               // numNewMemObjArgs
+        0,               // numNewPointerArgs
+        0,               // numNewValueArgs
+        n_dimensions,    // newWorkDim
+        nullptr,         // pNewMemObjArgList
+        nullptr,         // pNewPointerArgList
+        nullptr,         // pNewValueArgList
+        nullptr,         // pNewGlobalWorkOffset
+        nullptr,         // pNewGlobalWorkSize
+        &new_local_size, // pNewLocalWorkSize
+    };
+
+    ur_result_t result =
+        urCommandBufferUpdateKernelLaunchExp(command_handle, &update_desc);
+    ASSERT_EQ(UR_RESULT_ERROR_UNSUPPORTED_FEATURE, result);
+}
+
+TEST_P(InvalidUpdateCommandBufferExpExecutionTest, Kernel) {
+    if (update_capability_flags &
+        UR_DEVICE_COMMAND_BUFFER_UPDATE_CAPABILITY_FLAG_KERNEL_HANDLE) {
+        GTEST_SKIP() << "Test requires device to not support kernel handle "
+                        "update capability.";
+    }
+
+    ur_exp_command_buffer_update_kernel_launch_desc_t update_desc = {
+        UR_STRUCTURE_TYPE_EXP_COMMAND_BUFFER_UPDATE_KERNEL_LAUNCH_DESC, // stype
+        nullptr,                                                        // pNext
+        kernel_2,     // hNewKernel
+        0,            // numNewMemObjArgs
+        0,            // numNewPointerArgs
+        0,            // numNewValueArgs
+        n_dimensions, // newWorkDim
+        nullptr,      // pNewMemObjArgList
+        nullptr,      // pNewPointerArgList
+        nullptr,      // pNewValueArgList
+        nullptr,      // pNewGlobalWorkOffset
+        nullptr,      // pNewGlobalWorkSize
+        nullptr,      // pNewLocalWorkSize
+    };
+
+    ur_result_t result =
+        urCommandBufferUpdateKernelLaunchExp(command_handle, &update_desc);
+    ASSERT_EQ(UR_RESULT_ERROR_UNSUPPORTED_FEATURE, result);
+}