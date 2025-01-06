intel/llvm

commit 52015d8246abdbf3e8e4b6b1bf492aaadce844f8

[SYCL] Implement eviction in persistent cache (#16289)
This PR implements eviction for persistent cache.
Eviction is disabled by default and can be controlled by the user via
`SYCL_CACHE_MAX_SIZE` variable.

Here's how eviction works:
1. File size: A file called, `cache_size.txt`, is stored at the root of
persistent cache. Every time a process adds a new entry to cache, it
will also update the `cache_size.txt` file. This file is used to track
the size of persistent cache. For backwards compatibility, if SYCL RT
does not find `cache_size.txt` in the cache root, it will create once.
All access to `cache_size.txt` are done using `LockCacheItem`, to
prevent data races.
2. When adding a new entry to cache, SYCL RT will check the
`cache_size.txt` file and if the cache size exceeds the threshold,
eviction is triggered.
3. When a cache entry is created/accessed, SYCL RT create a file in that
cache entry to store the access time. This file is later read during
eviction.
4. During eviction, SYCL RT will determine the last access time of each
cache entry and items are evicted based on the LRU policy.

sycl/include/sycl/detail/os_util.hpp
@@ -12,7 +12,8 @@
 
 #include <sycl/detail/export.hpp> // for __SYCL_EXPORT
 
-#include <cstdlib>    // for size_t
+#include <cstdlib> // for size_t
+#include <functional>
 #include <string>     // for string
 #include <sys/stat.h> // for stat
 
@@ -89,6 +90,20 @@ public:
 #endif
   }
 };
+
+// These functions are not a part of OSUtils class to prevent
+// exporting them as ABI. They are only used in persistent cache
+// implementation and should not be exposed to the end users.
+// Get size of directory in bytes.
+size_t getDirectorySize(const std::string &Path);
+
+// Get size of file in bytes.
+size_t getFileSize(const std::string &Path);
+
+// Function to recursively iterate over the directory and execute
+// 'Func' on each regular file.
+void fileTreeWalk(const std::string Path,
+                  std::function<void(const std::string)> Func);
 
 } // namespace detail
 } // namespace _V1

sycl/source/detail/os_util.cpp
@@ -7,14 +7,20 @@
 //===----------------------------------------------------------------------===//
 
 #include <sycl/detail/os_util.hpp>
+#include <sycl/exception.hpp>
 
 #include <cassert>
 #include <limits>
 
-#if __GNUC__ && __GNUC__ < 8
-// Don't include <filesystem> for GCC versions less than 8
+// For GCC versions less than 8, use experimental/filesystem.
+#if defined(__has_include) && __has_include(<filesystem>)
+#include <filesystem>
+namespace fs = std::filesystem;
+#elif defined(__has_include) && __has_include(<experimental/filesystem>)
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
 #else
-#include <filesystem> // C++ 17 std::create_directories
+#error "OSUtils requires C++ filesystem support"
 #endif
 
 #if defined(__SYCL_RT_OS_LINUX)
@@ -275,6 +281,45 @@ int OSUtil::makeDir(const char *Dir) {
   std::filesystem::create_directories(path.make_preferred());
 #endif
   return 0;
+}
+
+// Get size of file in bytes.
+size_t getFileSize(const std::string &Path) {
+  return static_cast<size_t>(fs::file_size(Path));
+}
+
+// Function to recursively iterate over the directory and execute
+// 'Func' on each regular file.
+void fileTreeWalk(const std::string Path,
+                  std::function<void(const std::string)> Func) {
+
+  std::error_code EC;
+  for (auto It = fs::recursive_directory_iterator(Path, EC);
+       It != fs::recursive_directory_iterator(); It.increment(EC)) {
+
+    // Errors can happen if a file was removed/added during the iteration.
+    if (EC)
+      throw sycl::exception(
+          make_error_code(errc::runtime),
+          "Failed to do File Tree Walk. Ensure that the directory is not "
+          "getting updated while FileTreeWalk is in progress.: " +
+              Path + "\n" + EC.message());
+
+    if (fs::is_regular_file(It->path()))
+      Func(It->path().string());
+  }
+}
+
+// Get size of a directory in bytes.
+size_t getDirectorySize(const std::string &Path) {
+  size_t DirSizeVar = 0;
+
+  auto CollectFIleSize = [&DirSizeVar](const std::string Path) {
+    DirSizeVar += getFileSize(Path);
+  };
+  fileTreeWalk(Path, CollectFIleSize);
+
+  return DirSizeVar;
 }
 
 } // namespace detail

sycl/source/detail/persistent_device_code_cache.cpp
@@ -12,6 +12,7 @@
 #include <detail/program_manager/program_manager.hpp>
 
 #include <cerrno>
+#include <chrono>
 #include <cstdio>
 #include <fstream>
 #include <optional>
@@ -178,6 +179,246 @@ getProgramBinaryData(const ur_program_handle_t &NativePrg,
   return Result;
 }
 
+// Save the current time in a file.
+void PersistentDeviceCodeCache::saveCurrentTimeInAFile(std::string FileName) {
+  // Lock the file to prevent concurrent writes.
+  LockCacheItem Lock{FileName};
+  if (Lock.isOwned()) {
+    try {
+      std::ofstream FileStream{FileName, std::ios::trunc};
+      FileStream << std::chrono::high_resolution_clock::now()
+                        .time_since_epoch()
+                        .count();
+      FileStream.close();
+    } catch (std::exception &e) {
+      throw sycl::exception(make_error_code(errc::runtime),
+                            "Failed to save current time in a file: " +
+                                FileName + "\n" + std::string(e.what()));
+    }
+  }
+}
+
+// Check if cache_size.txt file is present in the cache root directory.
+// If not, create it and populate it with the size of the cache directory.
+void PersistentDeviceCodeCache::repopulateCacheSizeFile(
+    const std::string &CacheRoot) {
+
+  // No need to store cache size if eviction is disabled.
+  if (!isEvictionEnabled())
+    return;
+
+  const std::string CacheSizeFileName = "cache_size.txt";
+  const std::string CacheSizeFile = CacheRoot + "/" + CacheSizeFileName;
+
+  // If the cache size file is not present, calculate the size of the cache size
+  // directory and write it to the file.
+  if (!OSUtil::isPathPresent(CacheSizeFile)) {
+    PersistentDeviceCodeCache::trace(
+        "Cache size file not present. Creating one.");
+
+    // Take the lock to write the cache size to the file.
+    {
+      LockCacheItem Lock{CacheSizeFile};
+      if (!Lock.isOwned()) {
+        // If some other process is writing the cache size, do not write it.
+        PersistentDeviceCodeCache::trace("Didnot create the cache size file. "
+                                         "Some other process is creating one.");
+
+        // Stall until the other process creates the file. Stalling is important
+        // to prevent race between one process that's calculating the directory
+        // size and another process that's trying to create a new cache entry.
+        while (!OSUtil::isPathPresent(CacheSizeFile))
+          continue;
+      } else {
+        // Calculate the size of the cache directory.
+        // During directory size calculation, do not add anything
+        // in the cache. Otherwise, we'll get a std::fs_error.
+        size_t CacheSize = getDirectorySize(CacheRoot);
+
+        std::ofstream FileStream{CacheSizeFile};
+        FileStream << CacheSize;
+        FileStream.close();
+        PersistentDeviceCodeCache::trace("Cache size file created.");
+      }
+    }
+  }
+}
+
+void PersistentDeviceCodeCache::evictItemsFromCache(
+    const std::string &CacheRoot, size_t CacheSize, size_t MaxCacheSize) {
+  PersistentDeviceCodeCache::trace("Cache eviction triggered.");
+
+  // EVict half of the cache.
+  constexpr float HowMuchCacheToEvict = 0.5;
+
+  // Create a file eviction_in_progress.lock to indicate that eviction is in
+  // progress. This file is used to prevent two processes from evicting the
+  // cache at the same time.
+  LockCacheItem Lock{CacheRoot + EvictionInProgressFileSuffix};
+  if (!Lock.isOwned()) {
+    // If some other process is evicting the cache, return.
+    PersistentDeviceCodeCache::trace(
+        "Another process is evicting the cache. Returning.");
+    return;
+  }
+
+  // Get the list of all files in the cache directory along with their last
+  // modification time.
+  std::vector<std::pair<uint64_t, std::string>> FilesWithAccessTime;
+
+  auto CollectFileAccessTime = [&FilesWithAccessTime](const std::string File) {
+    if (File.find(CacheEntryAccessTimeSuffix) != std::string::npos) {
+      std::ifstream FileStream{File};
+      uint64_t AccessTime;
+      FileStream >> AccessTime;
+      FilesWithAccessTime.push_back({AccessTime, File});
+    }
+  };
+
+  // fileTreeWalk can throw if any new file is created or removed during the
+  // iteration. Retry in that case. When eviction is in progress, we don't
+  // insert any new item but processes can still read the cache. Reading from
+  // cache can create/remove .lock file which can cause the exception.
+  while (true) {
+    try {
+      fileTreeWalk(CacheRoot, CollectFileAccessTime);
+      break;
+    } catch (...) {
+      FilesWithAccessTime.clear();
+      // If the cache directory is removed during the iteration, retry.
+      continue;
+    }
+  }
+
+  // Sort the files in the cache directory based on their last access time.
+  std::sort(FilesWithAccessTime.begin(), FilesWithAccessTime.end(),
+            [](const std::pair<uint64_t, std::string> &A,
+               const std::pair<uint64_t, std::string> &B) {
+              return A.first < B.first;
+            });
+
+  // Evict files from the cache directory until the cache size is less than the
+  // threshold.
+  size_t CurrCacheSize = CacheSize;
+  for (const auto &File : FilesWithAccessTime) {
+
+    int pos = File.second.find(CacheEntryAccessTimeSuffix);
+    const std::string FileNameWOExt = File.second.substr(0, pos);
+    const std::string BinFile = FileNameWOExt + ".bin";
+    const std::string SrcFile = FileNameWOExt + ".src";
+
+    while (OSUtil::isPathPresent(BinFile) || OSUtil::isPathPresent(SrcFile)) {
+
+      // Lock to prevent race between writer and eviction thread.
+      LockCacheItem Lock{FileNameWOExt};
+      if (Lock.isOwned()) {
+        // Remove the file and subtract its size from the cache size.
+        auto RemoveFileAndSubtractSize = [&CurrCacheSize](
+                                             const std::string &FileName) {
+          // If the file is not present, return.
+          if (!OSUtil::isPathPresent(FileName))
+            return;
+
+          auto FileSize = getFileSize(FileName);
+          if (std::remove(FileName.c_str())) {
+            throw sycl::exception(make_error_code(errc::runtime),
+                                  "Failed to evict cache entry: " + FileName);
+          } else {
+            PersistentDeviceCodeCache::trace("File removed: " + FileName);
+            CurrCacheSize -= FileSize;
+          }
+        };
+
+        // If removal fails due to a race, retry.
+        // Races are rare, but can happen if another process is reading the
+        // file. Locking down the entire cache and blocking all readers would be
+        // inefficient.
+        try {
+          RemoveFileAndSubtractSize(SrcFile);
+          RemoveFileAndSubtractSize(BinFile);
+        } catch (...) {
+          continue;
+        }
+      }
+    }
+
+    // If the cache size is less than the threshold, break.
+    if (CurrCacheSize <= (size_t)(HowMuchCacheToEvict * MaxCacheSize))
+      break;
+  }
+
+  // Update the cache size file with the new cache size.
+  {
+    const std::string CacheSizeFileName = "cache_size.txt";
+    const std::string CacheSizeFile = CacheRoot + "/" + CacheSizeFileName;
+    while (true) {
+      LockCacheItem Lock{CacheSizeFile};
+      if (!Lock.isOwned()) {
+        // If some other process is writing the cache size, spin lock.
+        continue;
+      } else {
+        std::fstream FileStream;
+        FileStream.open(CacheSizeFile, std::ios::out | std::ios::trunc);
+        FileStream << CurrCacheSize;
+        FileStream.close();
+
+        PersistentDeviceCodeCache::trace(
+            "Updating the cache size file after eviction. New size: " +
+            std::to_string(CurrCacheSize));
+        break;
+      }
+    }
+  }
+}
+
+// Update the cache size file and trigger cache eviction if needed.
+void PersistentDeviceCodeCache::updateCacheFileSizeAndTriggerEviction(
+    const std::string &CacheRoot, size_t ItemSize) {
+
+  // No need to store cache size if eviction is disabled.
+  if (!isEvictionEnabled())
+    return;
+
+  const std::string CacheSizeFileName = "cache_size.txt";
+  const std::string CacheSizeFile = CacheRoot + "/" + CacheSizeFileName;
+  size_t CurrentCacheSize = 0;
+  // Read the cache size from the file.
+  while (true) {
+    LockCacheItem Lock{CacheSizeFile};
+    if (!Lock.isOwned()) {
+      // If some other process is writing the cache size, spin lock.
+      continue;
+    } else {
+      PersistentDeviceCodeCache::trace("Updating the cache size file.");
+      std::fstream FileStream;
+      FileStream.open(CacheSizeFile, std::ios::in);
+
+      // Read the cache size from the file;
+      std::string line;
+      if (std::getline(FileStream, line)) {
+        CurrentCacheSize = std::stoull(line);
+      }
+      FileStream.close();
+
+      CurrentCacheSize += ItemSize;
+
+      // Write the updated cache size to the file.
+      FileStream.open(CacheSizeFile, std::ios::out | std::ios::trunc);
+      FileStream << CurrentCacheSize;
+      FileStream.close();
+      break;
+    }
+  }
+
+  // Check if the cache size exceeds the threshold and trigger cache eviction if
+  // needed.
+  size_t MaxCacheSize = SYCLConfig<SYCL_CACHE_MAX_SIZE>::getProgramCacheSize();
+  if (CurrentCacheSize > MaxCacheSize) {
+    // Trigger cache eviction.
+    evictItemsFromCache(CacheRoot, CurrentCacheSize, MaxCacheSize);
+  }
+}
+
 /* Stores built program in persistent cache. We will put the binary for each
  * device in the list to a separate file.
  */
@@ -190,8 +431,21 @@ void PersistentDeviceCodeCache::putItemToDisc(
   if (!areImagesCacheable(Imgs))
     return;
 
+  repopulateCacheSizeFile(getRootDir());
+
+  // Do not insert any new item if eviction is in progress.
+  // Since evictions are rare, we can afford to spin lock here.
+  const std::string EvictionInProgressFile =
+      getRootDir() + EvictionInProgressFileSuffix;
+  // Stall until the other process finishes eviction.
+  while (OSUtil::isPathPresent(EvictionInProgressFile))
+    continue;
+
   std::vector<const RTDeviceBinaryImage *> SortedImgs = getSortedImages(Imgs);
   auto BinaryData = getProgramBinaryData(NativePrg, Devices);
+
+  // Total size of the item that we just wrote to the cache.
+  size_t TotalSize = 0;
   for (size_t DeviceIndex = 0; DeviceIndex < Devices.size(); DeviceIndex++) {
     // If we don't have binary for the device, skip it.
     if (BinaryData[DeviceIndex].empty())
@@ -202,9 +456,10 @@ void PersistentDeviceCodeCache::putItemToDisc(
     if (DirName.empty())
       return;
 
+    std::string FileName;
     try {
       OSUtil::makeDir(DirName.c_str());
-      std::string FileName = getUniqueFilename(DirName);
+      FileName = getUniqueFilename(DirName);
       LockCacheItem Lock{FileName};
       if (Lock.isOwned()) {
         std::string FullFileName = FileName + ".bin";
@@ -212,6 +467,12 @@ void PersistentDeviceCodeCache::putItemToDisc(
         trace("device binary has been cached: " + FullFileName);
         writeSourceItem(FileName + ".src", Devices[DeviceIndex], SortedImgs,
                         SpecConsts, BuildOptionsString);
+
+        // Update Total cache size after adding the new items.
+        TotalSize += getFileSize(FileName + ".src");
+        TotalSize += getFileSize(FileName + ".bin");
+
+        saveCurrentTimeInAFile(FileName + CacheEntryAccessTimeSuffix);
       } else {
         PersistentDeviceCodeCache::trace("cache lock not owned " + FileName);
       }
@@ -225,6 +486,10 @@ void PersistentDeviceCodeCache::putItemToDisc(
           std::strerror(errno));
     }
   }
+
+  // Update the cache size file and trigger cache eviction if needed.
+  if (TotalSize)
+    updateCacheFileSizeAndTriggerEviction(getRootDir(), TotalSize);
 }
 
 void PersistentDeviceCodeCache::putCompiledKernelToDisc(
@@ -298,6 +563,12 @@ std::vector<std::vector<char>> PersistentDeviceCodeCache::getItemFromDisc(
         try {
           std::string FullFileName = FileName + ".bin";
           Binaries[DeviceIndex] = readBinaryDataFromFile(FullFileName);
+
+          // Explicitly update the access time of the file. This is required for
+          // eviction.
+          if (isEvictionEnabled())
+            saveCurrentTimeInAFile(FileName + CacheEntryAccessTimeSuffix);
+
           FileNames += FullFileName + ";";
           break;
         } catch (...) {
@@ -423,8 +694,8 @@ PersistentDeviceCodeCache::readBinaryDataFromFile(const std::string &FileName) {
 }
 
 /* Writing cache item key sources to be used for reliable identification
- * Format: Four pairs of [size, value] for device, build options, specialization
- * constant values, device code SPIR-V images.
+ * Format: Four pairs of [size, value] for device, build options,
+ * specialization constant values, device code SPIR-V images.
  */
 void PersistentDeviceCodeCache::writeSourceItem(
     const std::string &FileName, const device &Device,

sycl/unittests/kernel-and-program/PersistentDeviceCodeCache.cpp
@@ -135,6 +135,12 @@ public:
     SYCLCachePersistentChanged = true;
   }
 
+  // Set SYCL_CACHE_MAX_SIZE.
+  void SetDiskCacheEvictionEnv(const char *NewValue) {
+    set_env("SYCL_CACHE_MAX_SIZE", NewValue);
+    sycl::detail::SYCLConfig<sycl::detail::SYCL_CACHE_MAX_SIZE>::reset();
+  }
+
   void AppendToSYCLCacheDirEnv(const char *SubDir) {
     std::string NewSYCLCacheDirPath{RootSYCLCacheDir};
     if (NewSYCLCacheDirPath.back() != '\\' && NewSYCLCacheDirPath.back() != '/')
@@ -142,6 +148,24 @@ public:
     NewSYCLCacheDirPath += SubDir;
     set_env("SYCL_CACHE_DIR", NewSYCLCacheDirPath.c_str());
     sycl::detail::SYCLConfig<sycl::detail::SYCL_CACHE_DIR>::reset();
+  }
+
+  // Get the list of binary files in the cache directory.
+  std::vector<std::string> getBinaryFileNames(std::string CachePath) {
+
+    std::vector<std::string> FileNames;
+    std::error_code EC;
+    for (llvm::sys::fs::directory_iterator DirIt(CachePath, EC);
+         DirIt != llvm::sys::fs::directory_iterator(); DirIt.increment(EC)) {
+      // Check if the file is a binary file.
+      std::string filename = DirIt->path();
+      if (filename.find(".bin") != std::string::npos) {
+        // Just return the file name without the path.
+        FileNames.push_back(filename.substr(filename.find_last_of("/\\") + 1));
+      }
+    }
+
+    return FileNames;
   }
 
   void ResetSYCLCacheDirEnv() {
@@ -169,6 +193,9 @@ public:
       SetSYCLCachePersistentEnv(SYCLCachePersistentBefore
                                     ? SYCLCachePersistentBefore->c_str()
                                     : nullptr);
+
+    // Reset SYCL_CACHE_MAX_SIZE.
+    SetDiskCacheEvictionEnv(nullptr);
     ResetSYCLCacheDirEnv();
   }
 
@@ -539,6 +566,92 @@ TEST_P(PersistentDeviceCodeCache, AccessDeniedForCacheDir) {
   ASSERT_NO_ERROR(llvm::sys::fs::remove_directories(ItemDir));
 }
 #endif //_WIN32
+
+// Unit tests for testing eviction in persistent cache.
+TEST_P(PersistentDeviceCodeCache, BasicEviction) {
+
+  // Cleanup the cache directory.
+  std::string CacheRoot = detail::PersistentDeviceCodeCache::getRootDir();
+  ASSERT_NO_ERROR(llvm::sys::fs::remove_directories(CacheRoot));
+  ASSERT_NO_ERROR(llvm::sys::fs::create_directories(CacheRoot));
+
+  // Disable eviction for the time being.
+  SetDiskCacheEvictionEnv("9000000");
+
+  std::string BuildOptions{"--eviction"};
+  // Put 3 items to the cache.
+  detail::PersistentDeviceCodeCache::putItemToDisc({Dev}, {&Img}, {},
+                                                   BuildOptions, NativeProg);
+
+  std::string ItemDir = detail::PersistentDeviceCodeCache::getCacheItemPath(
+      Dev, {&Img}, {}, BuildOptions);
+  size_t SizeOfOneEntry = (size_t)(detail::getDirectorySize(ItemDir));
+
+  detail::PersistentDeviceCodeCache::putItemToDisc({Dev}, {&Img}, {},
+                                                   BuildOptions, NativeProg);
+
+  detail::PersistentDeviceCodeCache::putItemToDisc({Dev}, {&Img}, {},
+                                                   BuildOptions, NativeProg);
+
+  // Retrieve 0.bin from the cache.
+  auto Res = detail::PersistentDeviceCodeCache::getItemFromDisc(
+      {Dev}, {&Img}, {}, BuildOptions);
+
+  // Get the number of binary files in the cached item folder.
+  auto BinFiles = getBinaryFileNames(ItemDir);
+  EXPECT_EQ(BinFiles.size(), static_cast<size_t>(3))
+      << "Missing binary files. Eviction should not have happened.";
+
+  // Set SYCL_CACHE_MAX_SIZE.
+  SetDiskCacheEvictionEnv(std::to_string(3 * SizeOfOneEntry).c_str());
+
+  // Put 4th item to the cache. This should trigger eviction. Three of the
+  // items should be evicted as we evict till the size of cache is less than
+  // the half of cache size.
+  detail::PersistentDeviceCodeCache::putItemToDisc({Dev}, {&Img}, {},
+                                                   BuildOptions, NativeProg);
+
+  // We should have two binary files: 0.bin, 3.bin.
+  BinFiles = getBinaryFileNames(ItemDir);
+  EXPECT_EQ(BinFiles.size(), static_cast<size_t>(1))
+      << "Eviction failed. Wrong number of binary files in the cache.";
+
+  // Check that 1.bin, 2.bin, and 0.bin was evicted.
+  for (const auto &File : BinFiles) {
+    EXPECT_NE(File, "1.bin")
+        << "Eviction failed. 1.bin should have been evicted.";
+    EXPECT_NE(File, "2.bin")
+        << "Eviction failed. 2.bin should have been evicted.";
+    EXPECT_NE(File, "0.bin")
+        << "Eviction failed. 0.bin should have been evicted.";
+  }
+
+  ASSERT_NO_ERROR(llvm::sys::fs::remove_directories(ItemDir));
+}
+
+// Unit test for testing size file creation and update, concurrently.
+TEST_P(PersistentDeviceCodeCache, ConcurentReadWriteCacheFileSize) {
+  // Cleanup the cache directory.
+  std::string CacheRoot = detail::PersistentDeviceCodeCache::getRootDir();
+  ASSERT_NO_ERROR(llvm::sys::fs::remove_directories(CacheRoot));
+  ASSERT_NO_ERROR(llvm::sys::fs::create_directories(CacheRoot));
+
+  // Insanely large value (1GB) to not trigger eviction. This test just
+  // checks for deadlocks/crashes when updating the size file concurrently.
+  SetDiskCacheEvictionEnv("1000000000");
+  ConcurentReadWriteCache(1, 100);
+}
+
+// Unit test for adding and evicting cache, concurrently.
+TEST_P(PersistentDeviceCodeCache, ConcurentReadWriteCacheEviction) {
+  // Cleanup the cache directory.
+  std::string CacheRoot = detail::PersistentDeviceCodeCache::getRootDir();
+  ASSERT_NO_ERROR(llvm::sys::fs::remove_directories(CacheRoot));
+  ASSERT_NO_ERROR(llvm::sys::fs::create_directories(CacheRoot));
+
+  SetDiskCacheEvictionEnv("1000");
+  ConcurentReadWriteCache(2, 100);
+}
 
 INSTANTIATE_TEST_SUITE_P(PersistentDeviceCodeCacheImpl,
                          PersistentDeviceCodeCache,

commit 7af800e17a32961da6099a2cb11864ade57cd1d3

[SYCL] Fix flaky failure of concurrent cache eviction UT (#16522)
Regression after: https://github.com/intel/llvm/pull/16289
fixes https://github.com/intel/llvm/issues/16515

**Problem**
The exception is thrown when one process tries to calculate cache size
and another process simultaneously inserted/removed any item from the
cache. This might cause std::filesystem::recursive_directory iterator to
throw std::filesystem_error.

**Solution**
We catch the exception and just skip over to the next item. This means
that while calculating the size of cache we might not consider the size
of any item added/removed from cache in the meanwhile. We calculate the
cache size only once (for all processes/threads using the same cache)
and that too the first-time persistent cache is used by any process. So,
this race is rather rare and just ignoring the exception would work.

sycl/include/sycl/detail/os_util.hpp
@@ -95,7 +95,7 @@ public:
 // exporting them as ABI. They are only used in persistent cache
 // implementation and should not be exposed to the end users.
 // Get size of directory in bytes.
-size_t getDirectorySize(const std::string &Path);
+size_t getDirectorySize(const std::string &Path, bool ignoreErrors = false);
 
 // Get size of file in bytes.
 size_t getFileSize(const std::string &Path);
@@ -103,7 +103,8 @@ size_t getFileSize(const std::string &Path);
 // Function to recursively iterate over the directory and execute
 // 'Func' on each regular file.
 void fileTreeWalk(const std::string Path,
-                  std::function<void(const std::string)> Func);
+                  std::function<void(const std::string)> Func,
+                  bool ignoreErrors = false);
 
 } // namespace detail
 } // namespace _V1

sycl/source/detail/os_util.cpp
@@ -248,33 +248,45 @@ size_t getFileSize(const std::string &Path) {
 // Function to recursively iterate over the directory and execute
 // 'Func' on each regular file.
 void fileTreeWalk(const std::string Path,
-                  std::function<void(const std::string)> Func) {
+                  std::function<void(const std::string)> Func,
+                  bool ignoreErrors) {
 
   std::error_code EC;
   for (auto It = fs::recursive_directory_iterator(Path, EC);
        It != fs::recursive_directory_iterator(); It.increment(EC)) {
 
     // Errors can happen if a file was removed/added during the iteration.
-    if (EC)
-      throw sycl::exception(
-          make_error_code(errc::runtime),
-          "Failed to do File Tree Walk. Ensure that the directory is not "
-          "getting updated while FileTreeWalk is in progress.: " +
-              Path + "\n" + EC.message());
+    if (EC) {
+      if (ignoreErrors) {
+        EC.clear();
+        continue;
+      } else
+        throw sycl::exception(
+            make_error_code(errc::runtime),
+            "Failed to do File Tree Walk. Ensure that the directory is not "
+            "getting updated while FileTreeWalk is in progress.: " +
+                Path + "\n" + EC.message());
+    }
 
-    if (fs::is_regular_file(It->path()))
-      Func(It->path().string());
+    try {
+      if (fs::is_regular_file(It->path()))
+        Func(It->path().string());
+    } catch (...) {
+      // Ignore errors if ignoreErrors is set to true.
+      if (!ignoreErrors)
+        throw;
+    }
   }
 }
 
 // Get size of a directory in bytes.
-size_t getDirectorySize(const std::string &Path) {
+size_t getDirectorySize(const std::string &Path, bool ignoreErrors) {
   size_t DirSizeVar = 0;
 
   auto CollectFIleSize = [&DirSizeVar](const std::string Path) {
     DirSizeVar += getFileSize(Path);
   };
-  fileTreeWalk(Path, CollectFIleSize);
+  fileTreeWalk(Path, CollectFIleSize, ignoreErrors);
 
   return DirSizeVar;
 }

sycl/source/detail/persistent_device_code_cache.cpp
@@ -241,7 +241,7 @@ void PersistentDeviceCodeCache::repopulateCacheSizeFile(
         // Calculate the size of the cache directory.
         // During directory size calculation, do not add anything
         // in the cache. Otherwise, we'll get a std::fs_error.
-        size_t CacheSize = getDirectorySize(CacheRoot);
+        size_t CacheSize = getDirectorySize(CacheRoot, /*Ignore Error*/ true);
 
         std::ofstream FileStream{CacheSizeFile};
         FileStream << CacheSize;

sycl/unittests/kernel-and-program/PersistentDeviceCodeCache.cpp
@@ -653,6 +653,39 @@ TEST_P(PersistentDeviceCodeCache, ConcurentReadWriteCacheEviction) {
   ConcurentReadWriteCache(2, 100);
 }
 
+// Unit test for ensuring that os_utils::getDirectorySize is thread-safe.
+TEST_P(PersistentDeviceCodeCache, ConcurentDirectorySizeCalculation) {
+  // Cleanup the cache directory.
+  std::string CacheRoot = detail::PersistentDeviceCodeCache::getRootDir();
+  ASSERT_NO_ERROR(llvm::sys::fs::remove_directories(CacheRoot));
+  ASSERT_NO_ERROR(llvm::sys::fs::create_directories(CacheRoot));
+
+  // Spawn multiple threads to calculate the size of the directory concurrently
+  // and adding/removing file simultaneously.
+  constexpr size_t ThreadCount = 50;
+  Barrier b(ThreadCount);
+  {
+    auto testLambda = [&](std::size_t threadId) {
+      b.wait();
+      // Create a file named: test_file_<thread_id>.txt
+      std::string FileName =
+          CacheRoot + "/test_file_" + std::to_string(threadId) + ".txt";
+      std::ofstream FileStream(FileName,
+                               std::ofstream::out | std::ofstream::trunc);
+      FileStream << "Test file for thread: " << threadId;
+      FileStream.close();
+
+      // Calculate the size of the directory.
+      [[maybe_unused]] auto i = detail::getDirectorySize(CacheRoot, true);
+
+      // Remove the created file.
+      ASSERT_NO_ERROR(llvm::sys::fs::remove(FileName));
+    };
+
+    ThreadPool MPool(ThreadCount, testLambda);
+  }
+}
+
 INSTANTIATE_TEST_SUITE_P(PersistentDeviceCodeCacheImpl,
                          PersistentDeviceCodeCache,
                          ::testing::Values(SYCL_DEVICE_BINARY_TYPE_SPIRV,