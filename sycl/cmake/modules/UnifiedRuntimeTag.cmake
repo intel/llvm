5e6b4aa82f7401b5c5c6c4a99914d7c2d5036f0a
Neil R. Spruit <neil.r.spruit@intel.com>
2025-01-06 14:24:32 -0800

    [UR][L0]: Fix Out Event in Enqueue Wait Events to prevent reuse
    -pre-commit PR for https://github.com/oneapi-src/unified-runtime/pull/2524
    
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Mon Jan 6 15:24:08 2025 +0000
 #     Merge pull request #2513 from AllanZyne/review/yang/fix_msan_usm
 #     [DeviceMSAN] Fix "urEnqueueUSM" APIs
-set(UNIFIED_RUNTIME_TAG 533ab9b08dfbf061585eb3487aed94ca45d0b331)
+set(UNIFIED_RUNTIME_TAG da04d13807044aaf17615b66577fb0e832011ab1)
