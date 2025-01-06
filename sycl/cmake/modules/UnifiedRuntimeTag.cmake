d5b969d149b9b1247d6b551950976cee5ecd5862
Yang Zhao <yang2.zhao@intel.com>
2025-01-06 23:23:15 +0800

    [DeviceMSAN] Fix empty kernel (#16478)
    UR: https://github.com/oneapi-src/unified-runtime/pull/2508
    
    ---------
    
    Co-authored-by: Kenneth Benzie (Benie) <k.benzie@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit f0b35200c07e6a15766ba75d01a3c176d42a2429
-# Merge: 545a01bae624 ceb0465d4090
-# Author: Martin Grant <martin.morrisongrant@codeplay.com>
-# Date:   Mon Jan 6 09:51:26 2025 +0000
-#     Merge pull request #2519 from oneapi-src/revert-2481-fabio/fix_potential_race_condition
-#     Revert "Fix potential deadlock in the WaitEvent path of CmdBuffers"
-set(UNIFIED_RUNTIME_TAG f0b35200c07e6a15766ba75d01a3c176d42a2429)
+# commit 75745a910cb7197de6e214c1f23c544895afbbb7
+# Merge: 0eb08b67 5a7d8fa0
+# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
+# Date:   Mon Jan 6 11:21:29 2025 +0000
+#     Merge pull request #2508 from AllanZyne/review/yang/fix_msan_empty_kernel
+#     [DeviceMSAN] Fix empty kernel
+set(UNIFIED_RUNTIME_TAG 75745a910cb7197de6e214c1f23c544895afbbb7)
