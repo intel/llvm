33a431a5699b0991194d334cb3ebe0cd60c6fb59
Maosu Zhao <maosu.zhao@intel.com>
2025-01-06 16:06:14 +0800

    update tag

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Fri Jan 3 10:08:59 2025 +0000
 #     Merge pull request #2502 from ldrumm/luke/tensormap-version
 #     CUTensorMap is only in CUDA v12
-set(UNIFIED_RUNTIME_TAG ad288bb36c45e24a35a75c665d382ce18935fcf7)
+set(UNIFIED_RUNTIME_TAG fix-buffer-shadow)
