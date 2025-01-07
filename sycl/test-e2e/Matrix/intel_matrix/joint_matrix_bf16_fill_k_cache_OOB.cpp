f796b836f07bc3e267dcac0b4805550d28ac5e20
Garcia Orozco, David <david.garcia.orozco@intel.com>
2025-01-07 08:19:56 -0800

    Move rest of tests that use `REQUIRES: aspect-ext_intel_matrix`

---
+++ sycl/test-e2e/Matrix/intel_matrix/joint_matrix_bf16_fill_k_cache_OOB.cpp
@@ -0,0 +1,20 @@
+//==--- joint_matrix_bf16_fill_k_cache_OOB.cpp  - DPC++ joint_matrix--------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+// REQUIRES: gpu
+// UNSUPPORTED: gpu-intel-dg2
+
+// RUN: %{build} -o %t_gpu_vnni.out %fp-model-precise -DOOB -DVNNI
+// RUN: %{run} %t_gpu_vnni.out
+
+// RUN: %{build} -o %t_gpu.out %fp-model-precise -DOOB
+// RUN: %{run} %t_gpu.out
+
+// -ffp-model=precise is added to not depend on compiler defaults.
+
+#include "../common.hpp"
+#include "../joint_matrix_bf16_fill_k_cache_impl.hpp"
