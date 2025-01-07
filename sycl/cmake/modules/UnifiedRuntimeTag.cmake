78d8feb10341cb9214b636cc49329e3e31b19459
Skrobot, Daniel <daniel.skrobot@intel.com>
2025-01-07 15:12:16 +0100

    [UR] Addition of num_compute_units query

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
 # commit eaea885d5477c8936209175a5b00062ca44f5765
 # Merge: af4ab49c 2a03334c
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Thu Jan 16 14:30:47 2025 +0000
-#     Merge pull request #2569 from zhaomaosu/asan-only-warn-host-ptr
-#     [DevASAN] Only report warning if passing host ptr to kernel
-set(UNIFIED_RUNTIME_TAG eaea885d5477c8936209175a5b00062ca44f5765)
+# Date:   Tue Jan 7 10:40:35 2025 +0000
+#     Merge pull request #2524 from nrspruit/fix_enqueue_wait_out_event
+#     [L0]: Fix Out Event in Enqueue Wait Events to prevent reuse
+set(UNIFIED_RUNTIME_TAG e6d65d72ef7980f376ea192e34d2b6bb8fb30050)
