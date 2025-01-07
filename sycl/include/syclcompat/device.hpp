2f987a01af7d14c76c9210af95cea46e85ad6743
Deepak Raj H R <deepak.raj.h.r@intel.com>
2025-01-07 11:52:31 +0530

    Add support for device attribute MAX_PITCH and ASYNC_ENGINE_COUNT

--- sycl/include/syclcompat/device.hpp
+++ sycl/include/syclcompat/device.hpp
@@ -444,6 +444,13 @@ public:
     return get_device_info().get_local_mem_size();
   }
 
+  // For CU_DEVICE_ATTRIBUTE_MAX_PITCH, NVidia GPUs return MAX_INT 
+  // indicating there is no limit (like Intel GPUs)
+  int get_max_pitch() const { return INT_MAX; }
+
+  // CU_DEVICE_ATTRIBUTE_ASYNC_ENGINE_COUNT is irrelevant in SYCL
+  int get_async_engine_count() const { return 0; }
+
   /// Get the number of bytes of free and total memory on the SYCL device.
   /// \param [out] free_memory The number of bytes of free memory on the SYCL
   /// device.
