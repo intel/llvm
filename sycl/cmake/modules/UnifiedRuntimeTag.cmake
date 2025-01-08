bd2fcdc7201c4a2f720a32c25e66ee2abe42d100
Ewan Crawford <ewan@codeplay.com>
2025-01-08 12:44:17 +0000

    [SYCL][Graph] UR command-buffer spec improvements (#16383)
    Bump UR tag to [Improve specification of command-buffer update
    errors](https://github.com/oneapi-src/unified-runtime/pull/2462)
    
    ---------
    
    Co-authored-by: Kenneth Benzie (Benie) <k.benzie@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 3472b5bda88fc06560561d7538a9c21d02f749e7
-# Merge: 33c94851 da04d138
+# commit b2ac58f27c63b8ff714e8b0c39b79aaab05a3faf
+# Merge: 3472b5bd ead3d07d
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Tue Jan 7 10:40:35 2025 +0000
-#     Merge pull request #2524 from nrspruit/fix_enqueue_wait_out_event
-#     [L0]: Fix Out Event in Enqueue Wait Events to prevent reuse
-set(UNIFIED_RUNTIME_TAG da04d13807044aaf17615b66577fb0e832011ab1)
+# Date:   Wed Jan 8 10:49:59 2025 +0000
+#     Merge pull request #2462 from Bensuo/cmd-buf_update_errors
+#     Improve specification of command-buffer update errors
+set(UNIFIED_RUNTIME_TAG b2ac58f27c63b8ff714e8b0c39b79aaab05a3faf)
