063e01a6426571151b649e43615231820aaf3518
Steffen Larsen <steffen.larsen@intel.com>
2025-01-08 08:42:28 +0100

    Revert "[SYCL] Refactor `sycl::vec`'s operators implementation (#16529)"
    This reverts commit 16c2c2161c9a67a1519a719f867cb5ad1bdd4533.

--- sycl/include/sycl/detail/type_traits/vec_marray_traits.hpp
+++ sycl/include/sycl/detail/type_traits/vec_marray_traits.hpp
@@ -31,24 +31,14 @@ template <typename T> constexpr bool is_vec_v = is_vec<T>::value;
 
 template <typename T, typename = void>
 struct is_ext_vector : std::false_type {};
-template <typename T, typename = void>
-struct is_valid_type_for_ext_vector : std::false_type {};
 #if defined(__has_extension)
 #if __has_extension(attribute_ext_vector_type)
 template <typename T, int N>
-using ext_vector = T __attribute__((ext_vector_type(N)));
-template <typename T, int N>
-struct is_ext_vector<ext_vector<T, N>> : std::true_type {};
-template <typename T>
-struct is_valid_type_for_ext_vector<T, std::void_t<ext_vector<T, 2>>>
-    : std::true_type {};
+struct is_ext_vector<T __attribute__((ext_vector_type(N)))> : std::true_type {};
 #endif
 #endif
 template <typename T>
 inline constexpr bool is_ext_vector_v = is_ext_vector<T>::value;
-template <typename T>
-inline constexpr bool is_valid_type_for_ext_vector_v =
-    is_valid_type_for_ext_vector<T>::value;
 
 template <typename> struct is_swizzle : std::false_type {};
 template <typename VecT, typename OperationLeftT, typename OperationRightT,
