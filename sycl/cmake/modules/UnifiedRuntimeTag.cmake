023fbbbfc63217da21b1a075c39ea706f6f20be0
Isaac Ault <isaac.ault@codeplay.com>
2025-01-08 21:20:51 +1000

    Update UR repo and tag.

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,5 @@
-# commit 3472b5bda88fc06560561d7538a9c21d02f749e7
-# Merge: 33c94851 da04d138
-# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Tue Jan 7 10:40:35 2025 +0000
-#     Merge pull request #2524 from nrspruit/fix_enqueue_wait_out_event
-#     [L0]: Fix Out Event in Enqueue Wait Events to prevent reuse
-set(UNIFIED_RUNTIME_TAG da04d13807044aaf17615b66577fb0e832011ab1)
+# commit 568ac3fe5fc56dd066541543acc14aaf5e7c0cfc
+# Author: Isaac Ault <isaac.ault@codeplay.com>
+# Date:   Wed Jan 8 07:38:41 2025 +1000
+#     Isolate fixed sub-group kernel for CTS.
+set(UNIFIED_RUNTIME_TAG 568ac3fe5fc56dd066541543acc14aaf5e7c0cfc)
