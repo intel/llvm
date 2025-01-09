c6b1edfb7512c893b50f49943abde916a71d6993
Ross Brunton <ross@codeplay.com>
2025-01-09 20:57:12 +0000

    [UR] Use reference counting on factories (#15296)
    Pre-commit MR for
    https://github.com/oneapi-src/unified-runtime/pull/2048

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,8 @@
-# commit 19182934ba5b15ebf26ae9ec7c06df1b9269725a
-# Merge: 3e62cc91 9c2b0400
-# Author: Isaac Ault <isaac.ault@codeplay.com>
-# Date:   Wed Jan 8 22:24:12 2025 +0000
-#     Merge pull request #2444 from isaacault/kernel-cts
-#     Reduce gap between Kernel CTS and Specification.
-set(UNIFIED_RUNTIME_TAG 19182934ba5b15ebf26ae9ec7c06df1b9269725a)
+# commit 7eae5c80a9e969bc12fda57c9cc0a0970f0cd17f
+# Merge: 9c652ffb b78cfa71
+# Author: Ross Brunton <ross@codeplay.com>
+# Date:   Thu Jan 9 17:28:00 2025 +0000
+# Merge pull request #2048 from RossBrunton/ross/refc
+#
+# Use reference counting on factories
+set(UNIFIED_RUNTIME_TAG 7eae5c80a9e969bc12fda57c9cc0a0970f0cd17f)
