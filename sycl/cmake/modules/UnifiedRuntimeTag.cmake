bfd4ad6adaa9ad2259ced629ace98d8986ff47a1
Przemek Malon <przemek.malon@codeplay.com>
2025-01-09 05:59:29 +0000

    [SYCL][Bindless][E2E] Test for host USM backed images
    This patch adds a test for an image backed by a host USM allocation.

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,8 +1,17 @@
-# commit 7eae5c80a9e969bc12fda57c9cc0a0970f0cd17f
-# Merge: 9c652ffb b78cfa71
-# Author: Ross Brunton <ross@codeplay.com>
-# Date:   Thu Jan 9 17:28:00 2025 +0000
-# Merge pull request #2048 from RossBrunton/ross/refc
+# commit 70053658b3f741f41cf9b62e063e8c4fe51957e0
+# Author: Przemek Malon <przemek.malon@codeplay.com>
+# Date:   Wed Jan 8 19:53:17 2025 +0000
+# Enable creation of images backed by host USM
 #
-# Use reference counting on factories
-set(UNIFIED_RUNTIME_TAG 7eae5c80a9e969bc12fda57c9cc0a0970f0cd17f)
+#   Small patch to enable bindless images backed by host USM in the CUDA
+#   adapter.
+#
+#   Host and Device USM pointers are usable across the host and device
+#   for all versions of CUDA that we support. There is no need to provide
+#   the `CU_MEMHOSTALLOC_DEVICEMAP` flag during allocation, or calling
+#   `cuMemHostGetDevicePointer` to retrieve a device usable address.
+#
+#   Passing a `CU_MEMHOSTALLOC_WRITECOMBINED` flag to the host USM
+#   allocation will enhance performance in certain scenarios, however, an
+#   extension allowing this is not yet available.
+set(UNIFIED_RUNTIME_TAG 70053658b3f741f41cf9b62e063e8c4fe51957e0)
