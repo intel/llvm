704c7a1c7b347f8e2121a5dc0f17df88fb568e54
Zhao, Yang2 <yang2.zhao@intel.com>
2025-01-09 07:11:22 +0100

    update ur tag

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Tue Jan 7 10:40:35 2025 +0000
 #     Merge pull request #2524 from nrspruit/fix_enqueue_wait_out_event
 #     [L0]: Fix Out Event in Enqueue Wait Events to prevent reuse
-set(UNIFIED_RUNTIME_TAG da04d13807044aaf17615b66577fb0e832011ab1)
+set(UNIFIED_RUNTIME_TAG review/yang/msan_device_global)
