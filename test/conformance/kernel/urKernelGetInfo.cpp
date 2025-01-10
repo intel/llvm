aarongreig/unified-runtime

Port remaining tests and replace match files with KNOWN_FAILURE macro

test/conformance/kernel/urKernelGetInfo.cpp
@@ -4,6 +4,7 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <uur/fixtures.h>
+#include <uur/known_failure.h>
 
 using urKernelGetInfoTest = uur::urKernelTestWithParam<ur_kernel_info_t>;
 
@@ -16,10 +17,15 @@ UUR_DEVICE_TEST_SUITE_P(
     uur::deviceTestWithParamPrinter<ur_kernel_info_t>);
 
 using urKernelGetInfoSingleTest = uur::urKernelExecutionTest;
-UUR_INSTANTIATE_KERNEL_TEST_SUITE_P(urKernelGetInfoSingleTest);
+UUR_INSTANTIATE_DEVICE_TEST_SUITE_P(urKernelGetInfoSingleTest);
 
 TEST_P(urKernelGetInfoTest, Success) {
     auto property_name = getParam();
+
+    if (property_name == UR_KERNEL_INFO_NUM_REGS) {
+        UUR_KNOWN_FAILURE_ON(uur::HIP{}, uur::OpenCL{});
+    }
+
     size_t property_size = 0;
     std::vector<char> property_value;
     ASSERT_SUCCESS_OR_OPTIONAL_QUERY(

intel/llvm

test/conformance/kernel/urKernelGetInfo.cpp
@@ -5,9 +5,10 @@
 
 #include "ur_api.h"
 #include <uur/fixtures.h>
+#include <uur/known_failure.h>
 
 using urKernelGetInfoTest = uur::urKernelTest;
-UUR_INSTANTIATE_KERNEL_TEST_SUITE_P(urKernelGetInfoTest);
+UUR_INSTANTIATE_DEVICE_TEST_SUITE_P(urKernelGetInfoTest);
 
 TEST_P(urKernelGetInfoTest, FunctionName) {
     auto property_name = UR_KERNEL_INFO_FUNCTION_NAME;
@@ -114,6 +115,7 @@ TEST_P(urKernelGetInfoTest, Attributes) {
 }
 
 TEST_P(urKernelGetInfoTest, NumRegs) {
+    UUR_KNOWN_FAILURE_ON(uur::HIP{}, uur::OpenCL{});
     auto property_name = UR_KERNEL_INFO_NUM_REGS;
     size_t property_size = 0;
 