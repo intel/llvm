ac7f169d932856883a90cf734bc65fffecca9ade
Nikita Kornev <nikita.kornev@intel.com>
2025-01-10 17:48:06 +0100

    [sycl-rel] Turn off OnlineCompiler tests (#16569)
    https://github.com/intel/llvm/issues/16406

--- sycl/test-e2e/OnlineCompiler/online_compiler_L0.cpp
+++ sycl/test-e2e/OnlineCompiler/online_compiler_L0.cpp
@@ -1,4 +1,6 @@
 // REQUIRES: level_zero, level_zero_dev_kit, cm-compiler
+// XFAIL: gpu && !(arch-intel_gpu_pvc && igc-dev)
+// XFAIL-TRACKER: https://github.com/intel/llvm/issues/16406
 
 // RUN: %{build} -DRUN_KERNELS %level_zero_options -o %t.out
 // RUN: %{run} %t.out
