eb3a7bafbc4274a5f7ed7a75131f28d0bf8e13bc
Ben Tracy <ben.tracy@codeplay.com>
2025-01-10 15:56:48 +0000

    [SYCL] Update UR tag for L0 synchronize fix
    - Removes extra waits when updating graphs on L0

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,9 +1,1 @@
-# commit ad88f0a1e3a22807866af4f6dad53e8986733abb
-# Merge: 68aed2d5 599a28e1
-# Author: Ross Brunton <ross@codeplay.com>
-# Date:   Tue Jan 14 10:28:07 2025 +0000
-
-#    Merge pull request #2527 from RossBrunton/ross/wrapper
-#    
-#    Wrap urEventSetCallback when ran through loader
-set(UNIFIED_RUNTIME_TAG ad88f0a1e3a22807866af4f6dad53e8986733abb)
+set(UNIFIED_RUNTIME_TAG fe926aa6d666a5e25a2b44e584508257d6f3cbc5)
