9b06de759c5e3c38dc63a5f842918c6e54d06b8a
Victor Lomuller <victor@codeplay.com>
2025-01-13 15:16:07 +0000

    handle format

--- libclc/libspirv/include/libspirv/conversion/GenericCastToPtrExplicit.h
+++ libclc/libspirv/include/libspirv/conversion/GenericCastToPtrExplicit.h
@@ -7,21 +7,18 @@
 //===----------------------------------------------------------------------===//
 
 #define GenericCastToPtrExplicit_To(ADDRSPACE, NAME)                           \
-  _CLC_DECL _CLC_OVERLOAD                                                      \
-      ADDRSPACE void *__spirv_GenericCastToPtrExplicit_To##NAME(               \
-          generic void *, int);                                                \
-  _CLC_DECL _CLC_OVERLOAD                                                      \
-      ADDRSPACE const void *__spirv_GenericCastToPtrExplicit_To##NAME(         \
-          generic const void *, int);                                          \
-  _CLC_DECL _CLC_OVERLOAD                                                      \
-      ADDRSPACE volatile void *__spirv_GenericCastToPtrExplicit_To##NAME(      \
-          generic volatile void *, int);                                       \
+  _CLC_DECL _CLC_OVERLOAD ADDRSPACE void                                       \
+      *__spirv_GenericCastToPtrExplicit_To##NAME(generic void *, int);         \
+  _CLC_DECL _CLC_OVERLOAD ADDRSPACE const void                                 \
+      *__spirv_GenericCastToPtrExplicit_To##NAME(generic const void *, int);   \
+  _CLC_DECL _CLC_OVERLOAD ADDRSPACE volatile void *                            \
+      __spirv_GenericCastToPtrExplicit_To##NAME(generic volatile void *, int); \
   _CLC_DECL _CLC_OVERLOAD ADDRSPACE const volatile void *                      \
       __spirv_GenericCastToPtrExplicit_To##NAME(generic const volatile void *, \
-                                                int);
+                                                int)
 
-GenericCastToPtrExplicit_To(global, Global)
-GenericCastToPtrExplicit_To(local, Local)
-GenericCastToPtrExplicit_To(private, Private)
+GenericCastToPtrExplicit_To(global, Global);
+GenericCastToPtrExplicit_To(local, Local);
+GenericCastToPtrExplicit_To(private, Private);
 
 #undef GenericCastToPtrExplicit_To
