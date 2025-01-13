957ee3991e64afc8906e6826bbb3b212e2dc0041
Sarnie, Nick <nick.sarnie@intel.com>
2025-01-13 11:13:04 -0800

    Disabling failing igc-dev pvc tests
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- sycl/test-e2e/OnlineCompiler/online_compiler_L0.cpp
+++ sycl/test-e2e/OnlineCompiler/online_compiler_L0.cpp
@@ -1,5 +1,5 @@
 // REQUIRES: level_zero, level_zero_dev_kit, cm-compiler
-// XFAIL: gpu && !(arch-intel_gpu_pvc && igc-dev)
+// XFAIL: gpu && igc-dev
 // XFAIL-TRACKER: https://github.com/intel/llvm/issues/16406
 // RUN: %{build} -Wno-error=deprecated-declarations -DRUN_KERNELS %level_zero_options -o %t.out
 // RUN: %{run} %t.out
