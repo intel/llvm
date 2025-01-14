17ccdaa40b9efd61d5bc092e7a71a611eda5c1da
Skrobot, Daniel <daniel.skrobot@intel.com>
2025-01-14 17:20:05 +0100

    fix

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,6 +1,6 @@
 # commit bbe28e9712bae50a779b7e10c937345badd97619
 # Merge: 503c16ec ef70004f
-# Author: Daniel Skrobot <ross@codeplay.com>
+# Author: Skrobot, Daniel <daniel.skrobot@intel.com>
 # Date:   Tue Jan 14 17:10:34 2025 +0100
 
 #    Addition of num_compute_units query for device info
