758a2bd05e61701677fb104d7bc832e35de1f848
Skrobot, Daniel <daniel.skrobot@intel.com>
2025-01-14 17:18:55 +0100

    UNIFIED_RUNTIME_TAG update

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit eaea885d5477c8936209175a5b00062ca44f5765
-# Merge: af4ab49c 2a03334c
-# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Tue Jan 7 10:40:35 2025 +0000
-#     Merge pull request #2524 from nrspruit/fix_enqueue_wait_out_event
-#     [L0]: Fix Out Event in Enqueue Wait Events to prevent reuse
-set(UNIFIED_RUNTIME_TAG b8216ab9a35d3a4a8bf68cf48533e6dc7297dd17)
+# commit bbe28e9712bae50a779b7e10c937345badd97619
+# Merge: 503c16ec ef70004f
+# Author: Daniel Skrobot <ross@codeplay.com>
+# Date:   Tue Jan 14 17:10:34 2025 +0100
+
+#    Addition of num_compute_units query for device info
+set(UNIFIED_RUNTIME_TAG bbe28e9712bae50a779b7e10c937345badd97619)
