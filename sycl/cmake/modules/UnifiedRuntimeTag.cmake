356d1e90e873e070215a45a3c8dbc480dd7c4087
Kenneth Benzie (Benie) <k.benzie@codeplay.com>
2025-01-15 15:01:06 +0000

    [UR] Bump main tag to 9e48f543

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit b2ac58f27c63b8ff714e8b0c39b79aaab05a3faf
-# Merge: 3472b5bd ead3d07d
-# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Tue Jan 7 10:40:35 2025 +0000
-#     Merge pull request #2524 from nrspruit/fix_enqueue_wait_out_event
-#     [L0]: Fix Out Event in Enqueue Wait Events to prevent reuse
-set(UNIFIED_RUNTIME_TAG review/yang/msan_device_global)
+# commit 9e48f543b8dd39d45563169433bb529583625dfe
+# Merge: 6a3fece6 1a1108b3
+# Author: Martin Grant <martin.morrisongrant@codeplay.com>
+# Date:   Wed Jan 15 14:33:29 2025 +0000
+#     Merge pull request #2540 from martygrant/martin/program-info-unswitch
+#     Move urProgramGetInfo success test from a switch to individual tests.
+set(UNIFIED_RUNTIME_TAG 9e48f543b8dd39d45563169433bb529583625dfe)
