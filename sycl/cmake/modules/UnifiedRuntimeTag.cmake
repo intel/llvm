ada16682e8c332768da64357a1867c61d47e183c
Maosu Zhao <maosu.zhao@intel.com>
2025-01-17 00:01:26 +0800

    [DevASAN] Only report warning if passing host ptr to kernel (#16654)
    UR Part: https://github.com/oneapi-src/unified-runtime/pull/2569
    
    ---------
    
    Co-authored-by: Kenneth Benzie (Benie) <k.benzie@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 023a84744b7db460d42225c8b5ea4d6def085c81
-# Merge: 9b57d7b7 0607d2c8
+# commit eaea885d5477c8936209175a5b00062ca44f5765
+# Merge: af4ab49c 2a03334c
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Thu Jan 16 10:17:25 2025 +0000
-#     Merge pull request #2546 from lukaszstolarczuk/bump-umf-to-latest-main
-#     [main] Bump UMF to v0.10.1
-set(UNIFIED_RUNTIME_TAG 023a84744b7db460d42225c8b5ea4d6def085c81)
+# Date:   Thu Jan 16 14:30:47 2025 +0000
+#     Merge pull request #2569 from zhaomaosu/asan-only-warn-host-ptr
+#     [DevASAN] Only report warning if passing host ptr to kernel
+set(UNIFIED_RUNTIME_TAG eaea885d5477c8936209175a5b00062ca44f5765)
