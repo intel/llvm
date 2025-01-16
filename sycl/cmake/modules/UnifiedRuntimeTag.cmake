087f9bff262ee1aa6ce094db64a4cc0d12948be3
Skrobot, Daniel <daniel.skrobot@intel.com>
2025-01-16 18:23:43 +0100

    fix

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,6 @@
 # commit 3e3282c01f3703b76a107ed34593ba46c4444298
-Merge: 2e7eb6ad bfc0676a
-Author: Daniel Skrobot <daniel.skrobot@intel.com>
-Date:   Thu Jan 16 18:00:16 2025 +0100
-
-    Addition of num_compute_units query
+# Merge: 2e7eb6ad bfc0676a
+# Author: Daniel Skrobot <daniel.skrobot@intel.com>
+# Date:   Thu Jan 16 18:00:16 2025 +0100
+#     Addition of num_compute_units query
 set(UNIFIED_RUNTIME_TAG 3e3282c01f3703b76a107ed34593ba46c4444298)
