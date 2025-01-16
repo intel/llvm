f00f0ea95d62656099c6f1442e9c2236cd7c8987
Konrad Kusiak <konrad.kusiak@codeplay.com>
2025-01-16 09:53:41 +0000

    Added UNSUPPORTED-INTENDED line

--- sycl/test-e2e/Graph/Explicit/work_group_static_memory.cpp
+++ sycl/test-e2e/Graph/Explicit/work_group_static_memory.cpp
@@ -5,8 +5,9 @@
 // Extra run to check for immediate-command-list in Level Zero
 // RUN: %if level_zero %{env SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=1 %{l0_leak_check} %{run} %t.out 2>&1 | FileCheck %s --implicit-check-not=LEAK %}
 //
-// sycl_ext_oneapi_work_group_static is not supported on AMD
 // UNSUPPORTED: hip
+// UNSUPPORTED-INTENDED: sycl_ext_oneapi_work_group_static is not supported on
+// AMD
 
 #define GRAPH_E2E_EXPLICIT
 
