9616ef65fa791102f0ce280f21c5b774dcd0765a
Hu, Peisen <peisen.hu@intel.com>
2025-01-15 18:25:18 -0800

    [SYCL] Rewrite multiple-translation-units tests
    Signed-off-by: Hu, Peisen <peisen.hu@intel.com>

--- sycl/test-e2e/VirtualFunctions/multiple-translation-units/Inputs/call.cpp
+++ sycl/test-e2e/VirtualFunctions/multiple-translation-units/Inputs/call.cpp
@@ -1,17 +1,27 @@
 #include "declarations.hpp"
 
+template <typename T1, typename T2> struct KernelFunctor {
+  T1 mDeviceStorage;
+  T2 mDataAcc;
+  KernelFunctor(T1 &DeviceStorage, T2 &DataAcc)
+      : mDeviceStorage(DeviceStorage), mDataAcc(DataAcc) {}
+
+  void operator()() const {
+    auto *Ptr = mDeviceStorage->getAs<BaseIncrement>();
+    Ptr->increment(mDataAcc.get_multi_ptr<sycl::access::decorated::no>().get());
+  }
+  auto get(oneapi::properties_tag) const {
+    return oneapi::properties{oneapi::assume_indirect_calls};
+  }
+};
+
 int call(sycl::queue Q, storage_t *DeviceStorage, int Init) {
   int Data = Init;
   {
     sycl::buffer<int> DataStorage(&Data, sycl::range{1});
-    constexpr oneapi::properties props{oneapi::assume_indirect_calls};
     Q.submit([&](sycl::handler &CGH) {
       sycl::accessor DataAcc(DataStorage, CGH, sycl::write_only);
-      CGH.single_task(props, [=]() {
-        auto *Ptr = DeviceStorage->getAs<BaseIncrement>();
-        Ptr->increment(
-            DataAcc.get_multi_ptr<sycl::access::decorated::no>().get());
-      });
+      CGH.single_task(KernelFunctor(DeviceStorage, DataAcc));
     });
   }
 
