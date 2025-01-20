c62b2bdf30852b357237fdb40b27c7547c207eea
Skrobot, Daniel <daniel.skrobot@intel.com>
2025-01-20 16:20:23 +0100

    fix

--- tools/urinfo/urinfo.hpp
+++ tools/urinfo/urinfo.hpp
@@ -327,6 +327,8 @@ inline void printDeviceInfos(ur_device_handle_t hDevice,
   std::cout << prefix;
   printDeviceInfo<ur_bool_t>(hDevice, UR_DEVICE_INFO_USM_POOL_SUPPORT);
   std::cout << prefix;
+  printDeviceInfo<uint32_t>(hDevice, UR_DEVICE_INFO_NUM_COMPUTE_UNITS);
+  std::cout << prefix;
   printDeviceInfo<ur_bool_t>(hDevice,
                              UR_DEVICE_INFO_COMMAND_BUFFER_SUPPORT_EXP);
   std::cout << prefix;
