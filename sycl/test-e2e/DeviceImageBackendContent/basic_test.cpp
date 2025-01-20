e8be85939eacb0d25f2f6be83e76e9431dd19a47
Lorenc Bushi <lorenc.bushi@intel.com>
2025-01-19 18:47:59 -0800

    Enhance tests

--- sycl/test-e2e/DeviceImageBackendContent/basic_test.cpp
+++ sycl/test-e2e/DeviceImageBackendContent/basic_test.cpp
@@ -7,6 +7,7 @@
 #endif
 #include <sycl/detail/core.hpp>
 #include <sycl/kernel_bundle.hpp>
+#include <type_traits>
 
 class kernel;
 
@@ -28,38 +29,41 @@ int main() {
   sycl::kernel_id id = sycl::get_kernel_id<kernel>();
   auto bundle =
       sycl::get_kernel_bundle<sycl::bundle_state::executable>(ctxt, {id});
+  assert(!bundle.empty());
   sycl::backend backend;
   std::vector<std::byte> bytes;
 #ifdef __cpp_lib_span
   std::span<std::byte> bytes_view;
 #endif
-  int runs = 0;
   for (const auto &img : bundle) {
-    if (img.has_kernel(id, d)) {
-      // Check that all 3 functions of the api compile.
-      // Furthermore, check that the backend corresponds to the backend of the
-      // bundle Check that the view of the content is indeed equal to the
-      // content.
-      ++runs;
-      backend = img.ext_oneapi_get_backend();
-      assert(backend == bundle.get_backend());
-      bytes = img.ext_oneapi_get_backend_content();
+    // Check that all 3 functions of the api return correct types and compile.
+    // Furthermore, check that the backend corresponds to the backend of the
+    // bundle Check that the view of the content is indeed equal to the
+    // content.
+    static_assert(std::is_same_v<decltype(img.ext_oneapi_get_backend()),
+                                 decltype(backend)>);
+    static_assert(std::is_same_v<decltype(img.ext_oneapi_get_backend_content()),
+                                 decltype(bytes)>);
+    backend = img.ext_oneapi_get_backend();
+    assert(backend == bundle.get_backend());
+    bytes = img.ext_oneapi_get_backend_content();
 #ifdef __cpp_lib_span
-      bytes_view = img.ext_oneapi_get_backend_content_view();
-      assert(bytes_view.size() == bytes.size());
-      for (size_t i = 0; i < bytes.size(); ++i) {
-        assert(bytes[i] == bytes_view[i]);
-      }
-#endif
+    static_assert(
+        std ::is_same_v<decltype(img.ext_oneapi.get_backend_content_view()),
+                        decltype(bytes_view)>);
+    bytes_view = img.ext_oneapi_get_backend_content_view();
+    assert(bytes_view.size() == bytes.size());
+    for (size_t i = 0; i < bytes.size(); ++i) {
+      assert(bytes[i] == bytes_view[i]);
     }
+#endif
   }
-  assert(runs); // check that the inner loop ran at least once
 
 #ifdef TEST_API_VIOLATION
   // Check that the ext_oneapi_get_backend_content and the
-  // ext_oneapi_get_backend_content_view of the content functions are only
+  // ext_oneapi_get_backend_content_view of the content functions are not
   // available
-  // when the image is in the executable state.
+  // when the image is not in the executable state.
 
   auto input_bundle =
       sycl::get_kernel_bundle<sycl::bundle_state::input>(ctxt, {id});
