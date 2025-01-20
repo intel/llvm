08a117890ee24db77eca0e60cc57f0adbac27426
Wu, Yingcong <yingcong.wu@intel.com>
2025-01-20 07:42:13 +0100

    update tag

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Fri Jan 17 10:28:34 2025 +0000
 #     Merge pull request #2561 from Bensuo/ben/cmd-buffer-l0-fence
 #     [L0][CMDBUF] Optimize fence/event waits during update
-set(UNIFIED_RUNTIME_TAG 222e4b1d51536bb38e03e2000a79679af0a44a6d)
+set(UNIFIED_RUNTIME_TAG yc-test-main/0120-on-shadow-per-type)
