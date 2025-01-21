9c65739ea12b12633701c6b438ad50bf4ed0fc32
Ross Brunton <ross@codeplay.com>
2025-01-21 13:28:52 +0000

    [UR] Bump with DEVICE_INFO_PROGRAM_SET_SPECIALIZATION_CONSTANTS (#16659)
    UR: https://github.com/oneapi-src/unified-runtime/pull/2539

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,8 @@
-# commit 8b7a99578966eb691a961d9620ea38d235196b2f
-# Merge: ed095412 7b0e3b19
-# Author: Martin Grant <martin.morrisongrant@codeplay.com>
-# Date:   Mon Jan 20 09:27:22 2025 +0000
-#     Merge pull request #2582 from przemektmalon/przemek/intel-host-usm-support
-#     Enable Host USM backed images on Level Zero
-set(UNIFIED_RUNTIME_TAG 8b7a99578966eb691a961d9620ea38d235196b2f)
+# commit b074893e854d28141cd67bc5935ed87e47eb3bb6
+# Merge: 71a5eab0 128ea023
+# Author: Ross Brunton <ross@codeplay.com>
+# Date:   Tue Jan 21 11:21:50 2025 +0000
+#     Merge pull request #2539 from RossBrunton/ross/specconst
+#
+#     Added `DEVICE_INFO_PROGRAM_SET_SPECIALIZATION_CONSTANTS`
+set(UNIFIED_RUNTIME_TAG b074893e854d28141cd67bc5935ed87e47eb3bb6)
