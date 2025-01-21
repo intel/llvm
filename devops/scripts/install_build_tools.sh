f717a34bba5dbbbac11d8acfb4b670410d1f8309
Udit Kumar Agarwal <udit.agarwal@intel.com>
2025-01-21 13:04:10 -0800

    Update install_build_tools.sh

--- devops/scripts/install_build_tools.sh
+++ devops/scripts/install_build_tools.sh
@@ -25,5 +25,13 @@ apt update && apt install -yqq \
       curl \
       libhwloc-dev \
       libzstd-dev \
-      time \
-      spirv-tools
+      time
+
+# Add LLVM's GPG key to obtain latest release of spriv-tool.
+# Same as what's done in SPRIV-LLVM-TRANSLATOR:
+# https://github.com/KhronosGroup/SPIRV-LLVM-Translator/blob/cec12d6cf46306d0a015e883d5adb5a8200df1c0/.github/workflows/check-out-of-tree-build.yml#L59
+curl -L "https://apt.llvm.org/llvm-snapshot.gpg.key" | sudo apt-key add -
+curl -L "https://packages.lunarg.com/lunarg-signing-key-pub.asc" | sudo apt-key add -
+echo "deb https://apt.llvm.org/jammy/ llvm-toolchain-jammy main" | sudo tee -a /etc/apt/sources.list
+echo "deb https://packages.lunarg.com/vulkan jammy main" | sudo tee -a /etc/apt/sources.list
+apt update && apt install -yqq spirv-tools
