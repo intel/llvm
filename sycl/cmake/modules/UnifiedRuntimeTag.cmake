1ae5121e04291272a8de9048301bb74a4cb4fa9d
Skrobot, Daniel <daniel.skrobot@intel.com>
2025-01-21 12:50:26 +0100

    added unittest for num_compute_units

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,6 +1,5 @@
-# commit bbb172e7df29cefc1fd809f75f0295aefb0eeed8
-# Merge: ac2780e0 222e4b1d
-# Author: Daniel Skrobot <daniel.skrobot@intel.com>
-# Date:   Fri Jan 17 11:37:50 2025 +0100
+# commit 46b4a751fba98b4b9443244ff40dfa7c214a955f
+# Author: Skrobot, Daniel <daniel.skrobot@intel.com>
+# Date:   Mon Jan 20 16:20:23 2025 +0100
 #     Addition of num_compute_units query
-set(UNIFIED_RUNTIME_TAG bbb172e7df29cefc1fd809f75f0295aefb0eeed8)
+set(UNIFIED_RUNTIME_TAG 46b4a751fba98b4b9443244ff40dfa7c214a955f)
