d136bf371ec3594963605c7ed3384474c18c28c0
Zhao, Yang2 <yang2.zhao@intel.com>
2025-01-20 23:02:50 -0800

    Revert "update ur tag"
    This reverts commit 5e1925a7cacbcf5b490b452d8eab9a26f57b0e34.

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Fri Jan 17 10:28:34 2025 +0000
 #     Merge pull request #2561 from Bensuo/ben/cmd-buffer-l0-fence
 #     [L0][CMDBUF] Optimize fence/event waits during update
-set(UNIFIED_RUNTIME_TAG review/yang/fix_kernel_release)
+set(UNIFIED_RUNTIME_TAG 222e4b1d51536bb38e03e2000a79679af0a44a6d)
