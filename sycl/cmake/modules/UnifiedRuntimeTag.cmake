a5ff8ec283023c7cc1ea7d89584a8ff003e1284a
Aaron Greig <aaron.greig@codeplay.com>
2025-01-22 10:26:32 +0000

    [UR] Make adapters check native properties before dereferencing.

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,8 @@
+set(UNIFIED_RUNTIME_REPO "https://github.com/aarongreig/unified-runtime.git")
 # commit 871061f1aa3b8ade57e0a2ed63d8000e257548cc
 # Merge: 262ec93e 7cca93f9
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
 # Date:   Tue Jan 21 13:26:45 2025 +0000
 #     Merge pull request #2588 from kbenzie/benie/ci-delete-prerelease
 #     Remove the prerelease.yml job
-set(UNIFIED_RUNTIME_TAG 871061f1aa3b8ade57e0a2ed63d8000e257548cc)
+set(UNIFIED_RUNTIME_TAG aaron/clarifyIsNativeHandleOwned)
