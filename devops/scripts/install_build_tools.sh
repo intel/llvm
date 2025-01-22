9c57858ad9e566bd8c8899721b3e59ced38440d2
Udit Kumar Agarwal <udit.agarwal@intel.com>
2025-01-22 12:50:31 -0800

    Revert "[CI] Install `spirv-tools` on Linux docker containers (#16724)"
    This reverts commit 34ef8668fcdc385780edefb44c80fd9254d183a3.

--- devops/scripts/install_build_tools.sh
+++ devops/scripts/install_build_tools.sh
@@ -26,11 +26,3 @@ apt update && apt install -yqq \
       libhwloc-dev \
       libzstd-dev \
       time
-
-# To obtain latest release of spriv-tool.
-# Same as what's done in SPRIV-LLVM-TRANSLATOR:
-# https://github.com/KhronosGroup/SPIRV-LLVM-Translator/blob/cec12d6cf46306d0a015e883d5adb5a8200df1c0/.github/workflows/check-out-of-tree-build.yml#L59
-. /etc/os-release
-curl -L "https://packages.lunarg.com/lunarg-signing-key-pub.asc" | apt-key add -
-echo "deb https://packages.lunarg.com/vulkan $VERSION_CODENAME main" | tee -a /etc/apt/sources.list
-apt update && apt install -yqq spirv-tools
