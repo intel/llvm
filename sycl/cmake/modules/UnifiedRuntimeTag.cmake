cc9b99da66b13d6426535c478ed0b44f87de5cf3
Fábio Mestre <fabio.mestre@codeplay.com>
2025-01-22 15:14:25 +0000

    [UR] Fix kernel arguments being overwritten in the CUDA and HIP adapters

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Tue Jan 21 15:49:41 2025 +0000
 #     Merge pull request #2593 from RossBrunton/ross/formatall
 #     Ensure all files are clang formatted
-set(UNIFIED_RUNTIME_TAG f058cb230c65fe8094f74043d0c9afd5ba0e8325)
+set(UNIFIED_RUNTIME_TAG 9de10cd9547db008ef4347f86dc0bf9198a8fb97)
