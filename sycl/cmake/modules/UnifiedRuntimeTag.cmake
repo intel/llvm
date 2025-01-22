696514238e2eeeb61c4551b8c4190d5cc1dc57ea
Yang Zhao <yang2.zhao@intel.com>
2025-01-22 20:36:11 +0800

    [DeviceASAN] Fix kernel release order (#16688)
    UR: https://github.com/oneapi-src/unified-runtime/pull/2586
    
    ---------
    
    Co-authored-by: Kenneth Benzie (Benie) <k.benzie@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 871061f1aa3b8ade57e0a2ed63d8000e257548cc
-# Merge: 262ec93e 7cca93f9
-# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Tue Jan 21 13:26:45 2025 +0000
-#     Merge pull request #2588 from kbenzie/benie/ci-delete-prerelease
-#     Remove the prerelease.yml job
-set(UNIFIED_RUNTIME_TAG 871061f1aa3b8ade57e0a2ed63d8000e257548cc)
+# commit f058cb230c65fe8094f74043d0c9afd5ba0e8325
+# Merge: 871061f1 3bf76246
+# Author: Ross Brunton <ross@codeplay.com>
+# Date:   Tue Jan 21 15:49:41 2025 +0000
+#     Merge pull request #2593 from RossBrunton/ross/formatall
+#     Ensure all files are clang formatted
+set(UNIFIED_RUNTIME_TAG f058cb230c65fe8094f74043d0c9afd5ba0e8325)
