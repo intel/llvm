8da711e3f7a8533b7f3c9ff1431d00005fec0af2
Skrobot, Daniel <daniel.skrobot@intel.com>
2025-01-22 17:33:56 +0100

    added check testing presence of extension macro

--- sycl/test-e2e/NumComputeUnits/num_compute_units.cpp
+++ sycl/test-e2e/NumComputeUnits/num_compute_units.cpp
@@ -4,10 +4,12 @@
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 
+#include <cassert>
 #include <sycl/detail/core.hpp>
 
 int main() {
 
+#ifdef SYCL_EXT_ONEAPI_NUM_COMPUTE_UNITS
   sycl::queue Queue;
   sycl::device Device = Queue.get_device();
 
@@ -16,6 +18,10 @@ int main() {
 
   assert(NumberComputeUnits >= 1 &&
          "The minimum value for number of compute units in the device is 1");
+
+#else
+  static_assert(false, "SYCL_EXT_ONEAPI_NUM_COMPUTE_UNITS not defined");
+#endif
 
   return 0;
 }
