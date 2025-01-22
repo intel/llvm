b564c42fa17b5e9f75950c5323301ed2b99a11d6
Kenneth Benzie (Benie) <k.benzie@codeplay.com>
2025-01-22 15:56:43 +0000

    [UR] Bump bump main tag to 3f6dbf3e

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,8 +1,7 @@
-set(UNIFIED_RUNTIME_REPO "https://github.com/aarongreig/unified-runtime.git")
-# commit 871061f1aa3b8ade57e0a2ed63d8000e257548cc
-# Merge: 262ec93e 7cca93f9
+# commit 3f6dbf3e4bec2ba7bfe7e4cebe998b51852baadc
+# Merge: bf7a6548 dc294e4a
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Tue Jan 21 13:26:45 2025 +0000
-#     Merge pull request #2588 from kbenzie/benie/ci-delete-prerelease
-#     Remove the prerelease.yml job
-set(UNIFIED_RUNTIME_TAG aaron/clarifyIsNativeHandleOwned)
+# Date:   Wed Jan 22 15:54:22 2025 +0000
+#     Merge pull request #2193 from aarongreig/aaron/clarifyIsNativeHandleOwned
+#     Clarify spec around isNativeHandleOwned.
+set(UNIFIED_RUNTIME_TAG 3f6dbf3e4bec2ba7bfe7e4cebe998b51852baadc)
