cff43417c19c72935b3b11610c309ec7a971467c
Garcia Orozco, David <david.garcia.orozco@intel.com>
2025-01-23 09:04:37 -0800

    Move E2EExpr unit tests back into E2EExpr.py

--- sycl/test/e2e_test_requirements/check_e2eexpr_logic.cpp
+++ sycl/test/e2e_test_requirements/check_e2eexpr_logic.cpp
@@ -3,6 +3,6 @@
 // statements. This test runs the unit tests related to these expressions.
 //
 // REQUIRES: linux
-// DEFINE: %{e2e_folder}=%S/../../test-e2e
+// DEFINE: %{E2EExpr}=%S/../../test-e2e/E2EExpr.py
 // DEFINE: %{lit_source}=%S/../../../llvm/utils/lit
-// RUN: env PYTHONPATH=%{lit_source}:%{e2e_folder} python %S/E2EExpr_tests.py
+// RUN: env PYTHONPATH=%{lit_source} python %{E2EExpr}
