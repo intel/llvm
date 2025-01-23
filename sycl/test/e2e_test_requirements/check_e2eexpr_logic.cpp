d1f8409b0f0c7d6ab8fa2fbcbae250e89e975eb6
Garcia Orozco, David <david.garcia.orozco@intel.com>
2025-01-23 08:18:07 -0800

    Add requires linux on E2EExpr tests

--- sycl/test/e2e_test_requirements/check_e2eexpr_logic.cpp
+++ sycl/test/e2e_test_requirements/check_e2eexpr_logic.cpp
@@ -2,6 +2,7 @@
 // boolean state to handle missing run-time features in REQUIRES/UNSUPPORTED 
 // statements. This test runs the unit tests related to these expressions.
 //
+// REQUIRES: linux
 // DEFINE: %{e2e_folder}=%S/../../test-e2e
 // DEFINE: %{lit_source}=%S/../../../llvm/utils/lit
 // RUN: env PYTHONPATH=%{lit_source}:%{e2e_folder} python %S/E2EExpr_tests.py
