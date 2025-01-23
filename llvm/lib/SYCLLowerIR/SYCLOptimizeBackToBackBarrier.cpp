b7162befe09902cf9c8c077d1833729d045b90e8
Sidorov, Dmitry <dmitry.sidorov@intel.com>
2025-01-23 07:43:51 -0800

    make some decisions depend on the execution scope
    Signed-off-by: Sidorov, Dmitry <dmitry.sidorov@intel.com>

--- llvm/lib/SYCLLowerIR/SYCLOptimizeBackToBackBarrier.cpp
+++ llvm/lib/SYCLLowerIR/SYCLOptimizeBackToBackBarrier.cpp
@@ -22,9 +22,55 @@ static constexpr char CONTROL_BARRIER[] = "_Z22__spirv_ControlBarrieriii";
 static constexpr char ITT_BARRIER[] = "__itt_offload_wg_barrier_wrapper";
 static constexpr char ITT_RESUME[] = "__itt_offload_wi_resume_wrapper";
 
+// Known scopes in SPIR-V.
+enum class Scope {
+  CrossDevice = 0,
+  Device = 1,
+  Workgroup = 2,
+  Subgroup = 3,
+  Invocation = 4
+};
+
+enum class CompareRes {
+  BIGGER = 0,
+  SMALLER = 1,
+  EQUAL = 2,
+  UNKNOWN = 3
+};
+
+// This map is added in case of any future scopes are added to SPIR-V and/or
+// SYCL.
+const std::unordered_map<uint64_t, uint64_t> ScopeWeights = {
+  {static_cast<uint64_t>(Scope::CrossDevice), 1000},
+  {static_cast<uint64_t>(Scope::Device), 800},
+  {static_cast<uint64_t>(Scope::Workgroup), 600},
+  {static_cast<uint64_t>(Scope::Subgroup), 400},
+  {static_cast<uint64_t>(Scope::Invocation), 10}
+};
+
+inline CompareRes compareScopesWithWeights(const uint64_t LHS,
+                                           const uint64_t RHS) {
+  auto LHSIt = ScopeWeights.find(LHS);
+  auto RHSIt = ScopeWeights.find(RHS);
+
+  if (LHSIt == ScopeWeights.end() || RHSIt == ScopeWeights.end())
+    return CompareRes::UNKNOWN;
+
+  const uint64_t LHSWeight = LHSIt->second;
+  const uint64_t RHSWeight = RHSIt->second;
+
+  if (LHSWeight > RHSWeight)
+    return CompareRes::BIGGER;
+  if (LHSWeight < RHSWeight)
+    return CompareRes::SMALLER;
+  return CompareRes::EQUAL;
+}
+
 // The function removes back-to-back ControlBarrier calls in case if they
-// have the same arguments. It also cleans up ITT annotations surrounding
-// the barrier call.
+// have the same memory scope and memory semantics arguments. When two
+// back-to-back ControlBarriers are having different execution scope arguments -
+// pick the one with the 'bigger' scope.
+// It also cleans up ITT annotations surrounding the removed barrier call.
 bool processControlBarrier(Function *F) {
   BasicBlock *PrevBB = nullptr;
   llvm::SmallPtrSet<Instruction *, 8> ToErase;
@@ -50,20 +96,43 @@ bool processControlBarrier(Function *F) {
       auto *Cand = dyn_cast<CallInst>(&*It);
       if (!Cand)
         break;
+      CallInst *CIToRemove = Cand;
       StringRef CandName = Cand->getCalledFunction()->getName();
       if (CandName == ITT_RESUME || CandName == ITT_BARRIER) {
         ToEraseLocalITT.insert(Cand);
         continue;
       } else if (CandName == CONTROL_BARRIER) {
         bool EqualOps = true;
-        for (unsigned I = 0; I != CI->getNumOperands(); ++I) {
+        const auto *ExecutionScopeCI = CI->getOperand(0);
+        const auto *ExecutionScopeCand = Cand->getOperand(0);
+        if (ExecutionScopeCI != ExecutionScopeCand) {
+          if (isa<ConstantInt>(ExecutionScopeCI) &&
+              isa<ConstantInt>(ExecutionScopeCand)) {
+            const auto ConstScopeCI =
+                cast<ConstantInt>(ExecutionScopeCI)->getZExtValue();
+            const auto ConstScopeCand =
+                cast<ConstantInt>(ExecutionScopeCand)->getZExtValue();
+            // Pick ControlBarrier with the 'bigger' execution scope.
+            const auto Compare = compareScopesWithWeights(ConstScopeCI,
+                                                          ConstScopeCand);
+            if (Compare == CompareRes::SMALLER)
+              CIToRemove = CI;
+            else if (Compare == CompareRes::UNKNOWN)
+              // Unknown scopes = unknown rules. Keep ControlBarrier call.
+              EqualOps = false;
+          } else
+            EqualOps = false;
+        }
+        // TODO: may be handle a case with not-matching memory scope and
+        // memory semantic arguments in a smart way.
+        for (unsigned I = 1; I != CI->getNumOperands(); ++I) {
           if (CI->getOperand(I) != Cand->getOperand(I)) {
             EqualOps = false;
             break;
           }
         }
         if (EqualOps) {
-          ToErase.insert(Cand);
+          ToErase.insert(CIToRemove);
           for (auto *ITT : ToEraseLocalITT)
             ToErase.insert(ITT);
           ToEraseLocalITT.clear();
--- llvm/test/SYCLLowerIR/SYCLOptimizeBackToBackBarrier/remove-back-to-back-barrier.ll
+++ llvm/test/SYCLLowerIR/SYCLOptimizeBackToBackBarrier/remove-back-to-back-barrier.ll
@@ -2,19 +2,36 @@
 ; The test checks if back-to-back __spirv_ControlBarrier and ITT annotations are
 ; removed.
 
-; CHECK: define spir_func void @_Z3foov()
+; CHECK: define spir_func void @_Z3foov(i32 [[#Arg1:]], i32 [[#Arg2:]])
 ; CHECK-NEXT: call spir_func void @__itt_offload_wg_barrier_wrapper()
 ; CHECK-NEXT: call void @_Z22__spirv_ControlBarrieriii(i32 noundef 3, i32 noundef 1, i32 noundef 912)
 ; CHECK-NEXT: call spir_func void @__itt_offload_wi_resume_wrapper()
 ; CHECK-NEXT: call spir_func void @__itt_offload_wg_barrier_wrapper()
 ; CHECK-NEXT: call void @_Z22__spirv_ControlBarrieriii(i32 noundef 3, i32 noundef 2, i32 noundef 912)
 ; CHECK-NEXT: call spir_func void @__itt_offload_wi_resume_wrapper()
+; CHECK-NEXT: call spir_func void @__itt_offload_wg_barrier_wrapper()
+; CHECK-NEXT: call void @_Z22__spirv_ControlBarrieriii(i32 noundef 64, i32 noundef 2, i32 noundef 912)
+; CHECK-NEXT: call spir_func void @__itt_offload_wi_resume_wrapper()
+; CHECK-NEXT: call spir_func void @__itt_offload_wg_barrier_wrapper()
+; CHECK-NEXT: call void @_Z22__spirv_ControlBarrieriii(i32 %0, i32 noundef 2, i32 noundef 912)
+; CHECK-NEXT: call spir_func void @__itt_offload_wi_resume_wrapper()
+; CHECK-NEXT: call spir_func void @__itt_offload_wg_barrier_wrapper()
+; CHECK-NEXT: call void @_Z22__spirv_ControlBarrieriii(i32 %[[#Arg1]], i32 noundef 2, i32 noundef 912)
+; CHECK-NEXT: call spir_func void @__itt_offload_wi_resume_wrapper()
 ; CHECK-NEXT: ret void
 
 target datalayout = "e-i64:64-v16:16-v24:32-v32:32-v48:64-v96:128-v192:256-v256:256-v512:512-v1024:1024"
 target triple = "spirv64-unknown-unknown"
 
-define spir_func void @_Z3foov() {
+define spir_func void @_Z3foov(i32 %0, i32 %1) {i
+  call spir_func void @__itt_offload_wg_barrier_wrapper()
+  call void @_Z22__spirv_ControlBarrieriii(i32 noundef 4, i32 noundef 1, i32 noundef 912)
+  call spir_func void @__itt_offload_wi_resume_wrapper()
+
+  call spir_func void @__itt_offload_wg_barrier_wrapper()
+  call void @_Z22__spirv_ControlBarrieriii(i32 noundef 2, i32 noundef 1, i32 noundef 912)
+  call spir_func void @__itt_offload_wi_resume_wrapper()
+
   call spir_func void @__itt_offload_wg_barrier_wrapper()
   call void @_Z22__spirv_ControlBarrieriii(i32 noundef 3, i32 noundef 1, i32 noundef 912)
   call spir_func void @__itt_offload_wi_resume_wrapper()
@@ -30,6 +47,23 @@ define spir_func void @_Z3foov() {
   call spir_func void @__itt_offload_wg_barrier_wrapper()
   call void @_Z22__spirv_ControlBarrieriii(i32 noundef 3, i32 noundef 2, i32 noundef 912)
   call spir_func void @__itt_offload_wi_resume_wrapper()
+
+  call spir_func void @__itt_offload_wg_barrier_wrapper()
+  call void @_Z22__spirv_ControlBarrieriii(i32 noundef 64, i32 noundef 2, i32 noundef 912)
+  call spir_func void @__itt_offload_wi_resume_wrapper()
+
+  call spir_func void @__itt_offload_wg_barrier_wrapper()
+  call void @_Z22__spirv_ControlBarrieriii(i32 %0, i32 noundef 2, i32 noundef 912)
+  call spir_func void @__itt_offload_wi_resume_wrapper()
+
+  call spir_func void @__itt_offload_wg_barrier_wrapper()
+  call void @_Z22__spirv_ControlBarrieriii(i32 %0, i32 noundef 2, i32 noundef 912)
+  call spir_func void @__itt_offload_wi_resume_wrapper()
+
+  call spir_func void @__itt_offload_wg_barrier_wrapper()
+  call void @_Z22__spirv_ControlBarrieriii(i32 %1, i32 noundef 2, i32 noundef 912)
+  call spir_func void @__itt_offload_wi_resume_wrapper()
+
   ret void
 }
 
