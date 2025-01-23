02993de84ac169d19fc0d2ae060949db716260d1
Kenneth Benzie (Benie) <k.benzie@codeplay.com>
2025-01-23 14:01:09 +0000

    [UR] Bump main tag to c6859445

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 8b7a99578966eb691a961d9620ea38d235196b2f
-# Merge: ed095412 7b0e3b19
-# Author: Martin Grant <martin.morrisongrant@codeplay.com>
-# Date:   Mon Jan 20 09:27:22 2025 +0000
-#     Merge pull request #2582 from przemektmalon/przemek/intel-host-usm-support
-#     Enable Host USM backed images on Level Zero
-set(UNIFIED_RUNTIME_TAG b1b0c60c42466e1e76666b1c9475d56d952e3f59)
+# commit c6859445e01d433ec1cf3d87a244c5cf697fb290
+# Merge: d3e97040 b1b0c60c
+# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
+# Date:   Thu Jan 23 13:59:15 2025 +0000
+#     Merge pull request #2589 from Bensuo/fabio/fix_potential_race_condition
+#     Fix potential deadlock in the WaitEvent path of CmdBuffers
+set(UNIFIED_RUNTIME_TAG c6859445e01d433ec1cf3d87a244c5cf697fb290)
