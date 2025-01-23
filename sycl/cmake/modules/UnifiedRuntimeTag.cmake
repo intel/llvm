c5c7e2082f76d950f3db104df65681ad59e44343
Martin Morrison-Grant <martin.morrisongrant@codeplay.com>
2025-01-23 10:42:26 +0000

    [UR] Update tag for Improvements to align CTS and Spec for Device - https://github.com/oneapi-src/unified-runtime/pull/2597

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
 # commit e2df8acd37c3c159364cfdf9dfdc1be35b71779e
 # Merge: 992ff37f 3a31ffee
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Fri Jan 31 10:15:03 2025 +0000
-#     Merge pull request #2575 from DBDuncan/duncan/extend-copies
-#     [CUDA][Bindless] Add support for device to device pitched copies and host to host copies
-set(UNIFIED_RUNTIME_TAG e2df8acd37c3c159364cfdf9dfdc1be35b71779e)
+# Date:   Thu Jan 23 16:07:06 2025 +0000
+#     Merge pull request #2559 from Bensuo/fix_kernel_arg_indices
+#     [CUDA][HIP] Fix kernel arguments being overwritten when added out of order
+set(UNIFIED_RUNTIME_TAG martin/device-cts-spec-gap-redo)
