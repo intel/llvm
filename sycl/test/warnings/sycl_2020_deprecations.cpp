c6e45cf42a89b99711f12ed73700bea5a7973b9a
HPS-1 <65581261+HPS-1@users.noreply.github.com>
2025-01-23 10:55:17 -0500

    [SYCL] Deprecate current implementations of get_backend_info() (#16700)
    The `get_backend_info()` functions implemented in
    https://github.com/intel/llvm/pull/12906 are returning some info
    descriptors that are not backend-specific info descriptors but SYCL core
    info descriptors. This leads to problems as described in
    https://github.com/intel/llvm/pull/16272 . Therefore, its current
    implementations are being deprecated, and removed under the
    `__INTEL_PREVIEW_BREAKING_CHANGES` flag.
    
    ---------
    
    Signed-off-by: Hu, Peisen <peisen.hu@intel.com>

--- sycl/test/warnings/sycl_2020_deprecations.cpp
+++ sycl/test/warnings/sycl_2020_deprecations.cpp
@@ -137,7 +137,7 @@ int main() {
   using PIUS = sycl::info::device::preferred_interop_user_sync;
   // expected-warning@+1{{'image_max_array_size' is deprecated: support for image arrays has been removed in SYCL 2020}}
   using IMAS = sycl::info::device::image_max_array_size;
-  // expected-warning@+1{{'opencl_c_version' is deprecated: use device::get_backend_info instead}}
+  // expected-warning@+1{{'opencl_c_version' is deprecated: use device::get_info instead}}
   using OCV = sycl::info::device::opencl_c_version;
 
   // expected-warning@+1{{'extensions' is deprecated: deprecated in SYCL 2020, use device::get_info() with info::device::aspects instead}}
