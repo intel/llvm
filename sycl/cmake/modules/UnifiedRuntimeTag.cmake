3e0cfcf4a148e5d3201edd222211f8c559f853de
Lukasz Dorau <lukasz.dorau@intel.com>
2025-01-24 08:42:07 +0100

    Use UMF CUDA provider in Unified Runtime
    Signed-off-by: Lukasz Dorau <lukasz.dorau@intel.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,9 +1,1 @@
-# commit 14f4a3ba70b91b3adc411ec6bfc8ae86e948a990
-# Merge: 4643d1c0 3ed86fae
-# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Wed Jan 29 13:55:27 2025 +0000
-#
-#    Merge pull request #2578 from Bensuo/ewan/remove_command_ref_counting
-#
-#    Remove command-buffer command handle ref counting
-set(UNIFIED_RUNTIME_TAG 14f4a3ba70b91b3adc411ec6bfc8ae86e948a990)
+set(UNIFIED_RUNTIME_TAG 8e4b5c3d31c25764f4d16f3167fec91b6068e192)
