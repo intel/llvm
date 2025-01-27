58c43f2eee16bdd00132d4e6fa15f9392542e243
Garcia Orozco, David <david.garcia.orozco@intel.com>
2025-01-27 06:08:48 -0800

    Remove `REQUIRES: build-and-run-mode` in bindless_images tests

--- sycl/test-e2e/bindless_images/vulkan_interop/mipmaps.cpp
+++ sycl/test-e2e/bindless_images/vulkan_interop/mipmaps.cpp
@@ -1,6 +1,5 @@
 // REQUIRES: cuda
 // REQUIRES: vulkan
-// REQUIRES: build-and-run-mode
 
 // RUN: %{build} %link-vulkan -o %t.out
 // RUN: %{run} %t.out
--- sycl/test-e2e/bindless_images/vulkan_interop/unsampled_images.cpp
+++ sycl/test-e2e/bindless_images/vulkan_interop/unsampled_images.cpp
@@ -1,6 +1,5 @@
 // REQUIRES: cuda || (windows && level_zero && aspect-ext_oneapi_bindless_images)
 // REQUIRES: vulkan
-// REQUIRES: build-and-run-mode
 
 // RUN: %{build} %link-vulkan -o %t.out %if any-device-is-level_zero %{ -Wno-ignored-attributes -DTEST_L0_SUPPORTED_VK_FORMAT %}
 // RUN: %{run} env NEOReadDebugKeys=1 UseBindlessMode=1 UseExternalAllocatorForSshAndDsh=1 %t.out
