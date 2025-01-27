c7b3197c32c88fea4d3c8b596949f9e5a23c4c77
przemektmalon <przemek.malon@codeplay.com>
2025-01-27 14:09:16 +0000

    [SYCL][Bindless][E2E] Test for host USM backed images (#16607)
    This patch adds a test for an image backed by a host USM allocation.
    
    Related UR PR: https://github.com/oneapi-src/unified-runtime/pull/2551

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit b841691699393dd2375e987c3d38d5f59c3e35cf
-# Merge: c6859445 9de10cd9
+# commit 0bb6789f0113ea937d861fd67fd677b91ecdeb8b
+# Merge: e370a2b9 eeff9f4a
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Thu Jan 23 16:07:06 2025 +0000
-#     Merge pull request #2559 from Bensuo/fix_kernel_arg_indices
-#     [CUDA][HIP] Fix kernel arguments being overwritten when added out of order
-set(UNIFIED_RUNTIME_TAG b841691699393dd2375e987c3d38d5f59c3e35cf)
+# Date:   Mon Jan 27 10:40:02 2025 +0000
+#     Merge pull request #2551 from przemektmalon/przemek/bindless-images-host-usm
+#     Enable creation of bindless images backed by host USM
+set(UNIFIED_RUNTIME_TAG 0bb6789f0113ea937d861fd67fd677b91ecdeb8b)
