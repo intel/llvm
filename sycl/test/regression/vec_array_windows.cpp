62bcf3539c974e6e21739ed615e5a69c989ab5e6
Agarwal <udit.agarwal@intel.com>
2025-01-27 11:41:46 -0800

    Add regression test for sycl::vec compilation error in windows, debug

---
+++ sycl/test/regression/vec_array_windows.cpp
@@ -0,0 +1,14 @@
+// Test to isolate sycl::vec regression after
+// https://github.com/intel/llvm/pull/14130. This PR caused sycl::vec to use
+// std::array as its underlying storage. However, operations on std::array
+// may emit debug-mode-only functions, on which the device compiler may fail.
+// Examples of such failure: CMPLRLLVM-52910, CMPLRLLVM-64902.
+
+// REQUIRES: windows
+
+// RUN: not %clangxx -fsycl -D_DEBUG -I %sycl_include %s -fsycl-device-only 2>&1 | FileCheck %s
+
+#include <sycl/sycl.hpp>
+
+// CHECK: error: SYCL kernel cannot call a variadic function
+SYCL_EXTERNAL auto GetFirstElement(sycl::vec<int, 3> v) { return v[0]; }
