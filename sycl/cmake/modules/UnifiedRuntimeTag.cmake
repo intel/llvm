a91672b8e93a542c5db4123d61623d3bda676558
Duncan Brawley <duncan.brawley@codeplay.com>
2025-01-27 15:22:25 +0000

    Update UR commit

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,4 @@
 # Date:   Mon Jan 27 10:40:02 2025 +0000
 #     Merge pull request #2551 from przemektmalon/przemek/bindless-images-host-usm
 #     Enable creation of bindless images backed by host USM
-set(UNIFIED_RUNTIME_TAG 0bb6789f0113ea937d861fd67fd677b91ecdeb8b)
+set(UNIFIED_RUNTIME_TAG 3a31ffee22b078a70ee7918c1c783a13d1f3aa60)
