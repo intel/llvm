d2dd7ff4a0303b5c9ad6fde0487fb32663777ca3
Larsen, Steffen <steffen.larsen@intel.com>
2025-01-28 06:32:38 -0800

    [SYCL][Docs] Rename restrict property to unaliased
    This commit renames the `restrict` kernel argument property to
    `unaliased` to avoid conflicting with common code patterns in C/C++
    projects.
    
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- clang/test/CodeGenSYCL/sycl_unaliased_property.cpp
+++ clang/test/CodeGenSYCL/sycl_unaliased_property.cpp
@@ -4,7 +4,7 @@ struct __attribute__((sycl_special_class))
       [[__sycl_detail__::sycl_type(annotated_arg)]]
     AnnotatedIntPtr {
   void __init([[__sycl_detail__::add_ir_attributes_kernel_parameter(
-                  "sycl-restrict", nullptr)]]
+                  "sycl-unaliased", nullptr)]]
               __attribute__((opencl_global)) int* InPtr) {
     Ptr = InPtr;
   }
@@ -32,48 +32,48 @@ int main() {
     int *b;
     int *c;
     kernel<class kernel_restrict1>([a, b, c]() { c[0] = a[0] + b[0]; });
-    // CHECK-DAG: define {{.*}}spir_kernel {{.*}}kernel_restrict1(ptr addrspace(1) noalias noundef align 4 "sycl-restrict" %{{.*}}, ptr addrspace(1) noundef align 4 %{{.*}}, ptr addrspace(1) noundef align 4 %{{.*}})
+    // CHECK-DAG: define {{.*}}spir_kernel {{.*}}kernel_restrict1(ptr addrspace(1) noalias noundef align 4 "sycl-unaliased" %{{.*}}, ptr addrspace(1) noundef align 4 %{{.*}}, ptr addrspace(1) noundef align 4 %{{.*}})
   }
   {
     int *a;
     AnnotatedIntPtr b;
     int *c;
     kernel<class kernel_restrict2>([a, b, c]() { c[0] = a[0] + b[0]; });
-    // CHECK-DAG: define {{.*}}spir_kernel {{.*}}kernel_restrict2(ptr addrspace(1) noundef align 4 %{{.*}}, ptr addrspace(1) noalias noundef align 4 "sycl-restrict" %{{.*}}, ptr addrspace(1) noundef align 4 %{{.*}})
+    // CHECK-DAG: define {{.*}}spir_kernel {{.*}}kernel_restrict2(ptr addrspace(1) noundef align 4 %{{.*}}, ptr addrspace(1) noalias noundef align 4 "sycl-unaliased" %{{.*}}, ptr addrspace(1) noundef align 4 %{{.*}})
   }
   {
     int *a;
     int *b;
     AnnotatedIntPtr c;
     kernel<class kernel_restrict3>([a, b, c]() { c[0] = a[0] + b[0]; });
-    // CHECK-DAG: define {{.*}}spir_kernel {{.*}}kernel_restrict3(ptr addrspace(1) noundef align 4 %{{.*}}, ptr addrspace(1) noundef align 4 %{{.*}}, ptr addrspace(1) noalias noundef align 4 "sycl-restrict" %{{.*}})
+    // CHECK-DAG: define {{.*}}spir_kernel {{.*}}kernel_restrict3(ptr addrspace(1) noundef align 4 %{{.*}}, ptr addrspace(1) noundef align 4 %{{.*}}, ptr addrspace(1) noalias noundef align 4 "sycl-unaliased" %{{.*}})
   }
   {
     AnnotatedIntPtr a;
     AnnotatedIntPtr b;
     int *c;
     kernel<class kernel_restrict4>([a, b, c]() { c[0] = a[0] + b[0]; });
-    // CHECK-DAG: define {{.*}}spir_kernel {{.*}}kernel_restrict4(ptr addrspace(1) noalias noundef align 4 "sycl-restrict" %{{.*}}, ptr addrspace(1) noalias noundef align 4 "sycl-restrict" %{{.*}}, ptr addrspace(1) noundef align 4 %{{.*}})
+    // CHECK-DAG: define {{.*}}spir_kernel {{.*}}kernel_restrict4(ptr addrspace(1) noalias noundef align 4 "sycl-unaliased" %{{.*}}, ptr addrspace(1) noalias noundef align 4 "sycl-unaliased" %{{.*}}, ptr addrspace(1) noundef align 4 %{{.*}})
   }
   {
     AnnotatedIntPtr a;
     int *b;
     AnnotatedIntPtr c;
     kernel<class kernel_restrict5>([a, b, c]() { c[0] = a[0] + b[0]; });
-    // CHECK-DAG: define {{.*}}spir_kernel {{.*}}kernel_restrict5(ptr addrspace(1) noalias noundef align 4 "sycl-restrict" %{{.*}}, ptr addrspace(1) noundef align 4 %{{.*}}, ptr addrspace(1) noalias noundef align 4 "sycl-restrict" %{{.*}})
+    // CHECK-DAG: define {{.*}}spir_kernel {{.*}}kernel_restrict5(ptr addrspace(1) noalias noundef align 4 "sycl-unaliased" %{{.*}}, ptr addrspace(1) noundef align 4 %{{.*}}, ptr addrspace(1) noalias noundef align 4 "sycl-unaliased" %{{.*}})
   }
   {
     int *a;
     AnnotatedIntPtr b;
     AnnotatedIntPtr c;
     kernel<class kernel_restrict6>([a, b, c]() { c[0] = a[0] + b[0]; });
-    // CHECK-DAG: define {{.*}}spir_kernel {{.*}}kernel_restrict6(ptr addrspace(1) noundef align 4 %{{.*}}, ptr addrspace(1) noalias noundef align 4 "sycl-restrict" %{{.*}}, ptr addrspace(1) noalias noundef align 4 "sycl-restrict" %{{.*}})
+    // CHECK-DAG: define {{.*}}spir_kernel {{.*}}kernel_restrict6(ptr addrspace(1) noundef align 4 %{{.*}}, ptr addrspace(1) noalias noundef align 4 "sycl-unaliased" %{{.*}}, ptr addrspace(1) noalias noundef align 4 "sycl-unaliased" %{{.*}})
   }
   {
     AnnotatedIntPtr a;
     AnnotatedIntPtr b;
     AnnotatedIntPtr c;
     kernel<class kernel_restrict7>([a, b, c]() { c[0] = a[0] + b[0]; });
-    // CHECK-DAG: define {{.*}}spir_kernel {{.*}}kernel_restrict7(ptr addrspace(1) noalias noundef align 4 "sycl-restrict" %{{.*}}, ptr addrspace(1) noalias noundef align 4 "sycl-restrict" %{{.*}}, ptr addrspace(1) noalias noundef align 4 "sycl-restrict" %{{.*}})
+    // CHECK-DAG: define {{.*}}spir_kernel {{.*}}kernel_restrict7(ptr addrspace(1) noalias noundef align 4 "sycl-unaliased" %{{.*}}, ptr addrspace(1) noalias noundef align 4 "sycl-unaliased" %{{.*}}, ptr addrspace(1) noalias noundef align 4 "sycl-unaliased" %{{.*}})
   }
 }
