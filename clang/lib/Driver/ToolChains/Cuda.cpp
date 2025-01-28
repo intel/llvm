87c57b3cc246f1ede036e2fc4a11f993434f57b4
Ji, Jinsong <jinsong.ji@intel.com>
2025-01-28 14:14:10 -0800

    [SYCL][CUDA] include PTX in new driver
    Somehow we still need ptx for sycl .
    
    Fix failures
    https://github.com/intel/llvm/actions/runs/13014198774/job/36299123697
    
    ```
    error: nvFatbinAddCubin(handle, file.image, file.size, arch,
    identName) failed with error fatbinary elf mismatch: elf size doesn't
    match user-specified size
    ```

--- clang/lib/Driver/ToolChains/Cuda.cpp
+++ clang/lib/Driver/ToolChains/Cuda.cpp
@@ -517,7 +517,7 @@ void NVPTX::Assembler::ConstructJob(Compilation &C, const JobAction &JA,
 static bool shouldIncludePTX(const ArgList &Args, StringRef InputArch) {
   // The new driver does not include PTX by default to avoid overhead.
   bool includePTX = !Args.hasFlag(options::OPT_offload_new_driver,
-                                  options::OPT_no_offload_new_driver, true);
+                                  options::OPT_no_offload_new_driver, false); // INTEL
   for (Arg *A : Args.filtered(options::OPT_cuda_include_ptx_EQ,
                               options::OPT_no_cuda_include_ptx_EQ)) {
     A->claim();
