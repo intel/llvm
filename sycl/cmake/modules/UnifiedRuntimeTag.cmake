cf19f7758c6ed3b6bdbf0c2fc4cefbe08cbaabd3
Kenneth Benzie (Benie) <k.benzie@codeplay.com>
2025-01-28 12:34:13 +0000

    [UR] fix parseDisjointPoolConfig and add tests (#16791)
    https://github.com/oneapi-src/unified-runtime/pull/2574

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 0bb6789f0113ea937d861fd67fd677b91ecdeb8b
-# Merge: e370a2b9 eeff9f4a
+# commit 095e8464124a48c8ed4b995403e754254c072143
+# Merge: 0bb6789f 07001aa7
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Mon Jan 27 10:40:02 2025 +0000
-#     Merge pull request #2551 from przemektmalon/przemek/bindless-images-host-usm
-#     Enable creation of bindless images backed by host USM
-set(UNIFIED_RUNTIME_TAG 0bb6789f0113ea937d861fd67fd677b91ecdeb8b)
+# Date:   Mon Jan 27 14:53:22 2025 +0000
+#     Merge pull request #2574 from bratpiorka/rrudnick_fix_usm_pool_config_parse
+#     fix parseDisjointPoolConfig and add tests
+set(UNIFIED_RUNTIME_TAG 095e8464124a48c8ed4b995403e754254c072143)
