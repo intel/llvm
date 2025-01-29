13823934e92609b46a3998032a7440252e80ebdb
Alexandr Konovalov <alexandr.konovalov@intel.com>
2025-01-29 14:04:51 +0100

    Small fixes.

--- sycl/unittests/scheduler/EnqueueWithDependsOnDeps.cpp
+++ sycl/unittests/scheduler/EnqueueWithDependsOnDeps.cpp
@@ -323,6 +323,7 @@ TEST_F(DependsOnTests, ShortcutFunctionWithWaitList) {
                                            &redefinedextUSMEnqueueMemcpy);
   sycl::queue Queue = detail::createSyclObjFromImpl<queue>(QueueDevImpl);
 
+  // Mock up an incomplete host task
   auto HostTaskEvent =
       Queue.submit([&](sycl::handler &cgh) { cgh.host_task([=]() {}); });
   std::shared_ptr<detail::event_impl> HostTaskEventImpl =
@@ -332,8 +333,6 @@ TEST_F(DependsOnTests, ShortcutFunctionWithWaitList) {
   ASSERT_NE(Cmd, nullptr);
   Cmd->MIsBlockable = true;
   Cmd->MEnqueueStatus = detail::EnqueueResultT::SyclEnqueueBlocked;
-  // for the test functionality, depenent task HostTaskEvent must be treated as
-  // incompleted
   HostTaskEventImpl->setStateIncomplete();
 
   auto SingleTaskEvent = Queue.submit([&](sycl::handler &cgh) {
