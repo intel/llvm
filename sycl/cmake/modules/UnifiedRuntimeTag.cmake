5ec444f9d57573a4f10263e1263d9ae514aca0cb
Łukasz Ślusarczyk <lukasz.slusarczyk@intel.com>
2025-01-29 15:50:35 +0100

    update tag with UR logging

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
 # commit 064d3560cbcc24a40380b57adb3690b2bc256d6e
 # Merge: 739d8a75 e1bbdad9
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Wed Feb 12 10:20:12 2025 +0000
-#     Merge pull request #2672 from lukaszstolarczuk/umf-with-icx-build-0.11.0-dev2
-#     common: Bump UMF version to v0.11.0-dev2
-set(UNIFIED_RUNTIME_TAG 064d3560cbcc24a40380b57adb3690b2bc256d6e)
+# Date:   Tue Jan 28 15:16:58 2025 +0000
+#     Merge pull request #2594 from kbenzie/benie/cl-core-functions-no-dlopen
+#     Fix invalid use of dlopen()
+set(UNIFIED_RUNTIME_TAG 77d1c2ee4916fad4f8968b538f31dfc1e862e795)
