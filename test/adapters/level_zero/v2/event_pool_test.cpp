a8ff1dcb2099e7914cb5a989365afc8d2606e31f
Ross Brunton <ross@codeplay.com>
2025-01-29 12:05:46 +0000

    Remove virtual from L0 V2 queues

--- test/adapters/level_zero/v2/event_pool_test.cpp
+++ test/adapters/level_zero/v2/event_pool_test.cpp
@@ -16,6 +16,7 @@
 #include "event_provider.hpp"
 #include "event_provider_counter.hpp"
 #include "event_provider_normal.hpp"
+#include "queue_handle.hpp"
 #include "uur/fixtures.h"
 #include "ze_api.h"
 
@@ -164,7 +165,7 @@ TEST_P(EventPoolTest, Basic) {
       auto pool = cache->borrow(device->Id.value(), getParam().flags);
 
       first = pool->allocate();
-      first->resetQueueAndCommand(queue, UR_COMMAND_KERNEL_LAUNCH);
+      first->resetQueueAndCommand(&queue->get(), UR_COMMAND_KERNEL_LAUNCH);
       zeFirst = first->getZeEvent();
 
       urEventRelease(first);
@@ -175,7 +176,7 @@ TEST_P(EventPoolTest, Basic) {
       auto pool = cache->borrow(device->Id.value(), getParam().flags);
 
       second = pool->allocate();
-      first->resetQueueAndCommand(queue, UR_COMMAND_KERNEL_LAUNCH);
+      first->resetQueueAndCommand(&queue->get(), UR_COMMAND_KERNEL_LAUNCH);
       zeSecond = second->getZeEvent();
 
       urEventRelease(second);
@@ -195,7 +196,8 @@ TEST_P(EventPoolTest, Threaded) {
         std::vector<ur_event_handle_t> events;
         for (int i = 0; i < 100; ++i) {
           events.push_back(pool->allocate());
-          events.back()->resetQueueAndCommand(queue, UR_COMMAND_KERNEL_LAUNCH);
+          events.back()->resetQueueAndCommand(&queue->get(),
+                                              UR_COMMAND_KERNEL_LAUNCH);
         }
         for (int i = 0; i < 100; ++i) {
           urEventRelease(events[i]);
@@ -214,7 +216,7 @@ TEST_P(EventPoolTest, ProviderNormalUseMostFreePool) {
   std::list<ur_event_handle_t> events;
   for (int i = 0; i < 128; ++i) {
     auto event = pool->allocate();
-    event->resetQueueAndCommand(queue, UR_COMMAND_KERNEL_LAUNCH);
+    event->resetQueueAndCommand(&queue->get(), UR_COMMAND_KERNEL_LAUNCH);
     events.push_back(event);
   }
   auto frontZeHandle = events.front()->getZeEvent();
@@ -224,7 +226,7 @@ TEST_P(EventPoolTest, ProviderNormalUseMostFreePool) {
   }
   for (int i = 0; i < 8; ++i) {
     auto e = pool->allocate();
-    e->resetQueueAndCommand(queue, UR_COMMAND_KERNEL_LAUNCH);
+    e->resetQueueAndCommand(&queue->get(), UR_COMMAND_KERNEL_LAUNCH);
     events.push_back(e);
   }
 
