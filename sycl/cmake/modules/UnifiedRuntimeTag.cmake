a04d0665efbfe9a08bd87d78ed782cdabdffcee7
Konrad Kusiak <konrad.kusiak@codeplay.com>
2025-01-29 14:15:43 +0000

    Updated the test and UR tag

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,5 @@
 # Date:   Tue Jan 28 15:16:58 2025 +0000
 #     Merge pull request #2594 from kbenzie/benie/cl-core-functions-no-dlopen
 #     Fix invalid use of dlopen()
-set(UNIFIED_RUNTIME_TAG 3a1b4c7b9ba952fad6f6ad36c01101bbf368347b)
+set(UNIFIED_RUNTIME_REPO "https://github.com/konradkusiak97/unified-runtime.git")
+set(UNIFIED_RUNTIME_TAG addInOrderCopyEngineSupport)
