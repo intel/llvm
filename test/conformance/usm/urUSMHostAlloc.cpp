ee8efc492afedaf50c5b1663ebfab37b0d40d7e0
Martin Morrison-Grant <martin.morrisongrant@codeplay.com>
2025-01-29 16:12:39 +0000

    Improvements to align CTS and Spec for USM: - Additional error case checking - More validation for urUSMPoolRelease - Refactored urUSMDevice/Shared/HostAlloc test structs into a shared parent struct - Added ur_usm_advice_flags_t param to parameterised Alloc tests

--- test/conformance/usm/urUSMHostAlloc.cpp
+++ test/conformance/usm/urUSMHostAlloc.cpp
@@ -6,41 +6,18 @@
 
 #include "helpers.h"
 #include <cstring>
+#include <limits>
 #include <uur/fixtures.h>
 #include <uur/known_failure.h>
 
-struct urUSMHostAllocTest
-    : uur::urQueueTestWithParam<uur::USMDeviceAllocParams> {
+struct urUSMHostAllocTest : uur::urUSMAllocTest {
   void SetUp() override {
-    UUR_RETURN_ON_FATAL_FAILURE(
-        uur::urQueueTestWithParam<uur::USMDeviceAllocParams>::SetUp());
-    ur_device_usm_access_capability_flags_t hostUSMSupport = 0;
-    ASSERT_SUCCESS(uur::GetDeviceUSMHostSupport(device, hostUSMSupport));
-    if (!hostUSMSupport) {
-      GTEST_SKIP() << "Device USM is not supported.";
-    }
-
-    if (usePool) {
-      ur_bool_t poolSupport = false;
-      ASSERT_SUCCESS(uur::GetDeviceUSMPoolSupport(device, poolSupport));
-      if (!poolSupport) {
-        GTEST_SKIP() << "USM pools are not supported.";
-      }
-      ur_usm_pool_desc_t pool_desc = {};
-      ASSERT_SUCCESS(urUSMPoolCreate(context, &pool_desc, &pool));
+    UUR_RETURN_ON_FATAL_FAILURE(urUSMAllocTest::SetUp());
+    ASSERT_SUCCESS(uur::GetDeviceUSMHostSupport(device, USMSupport));
+    if (!USMSupport) {
+      GTEST_SKIP() << "Host USM is not supported.";
     }
   }
-
-  void TearDown() override {
-    if (pool) {
-      ASSERT_SUCCESS(urUSMPoolRelease(pool));
-    }
-    UUR_RETURN_ON_FATAL_FAILURE(
-        uur::urQueueTestWithParam<uur::USMDeviceAllocParams>::TearDown());
-  }
-
-  ur_usm_pool_handle_t pool = nullptr;
-  bool usePool = std::get<0>(getParam()).value;
 };
 
 // The 0 value parameters are not relevant for urUSMHostAllocTest tests, they
@@ -50,20 +27,14 @@ UUR_DEVICE_TEST_SUITE_WITH_PARAM(
     urUSMHostAllocTest,
     testing::Combine(
         testing::ValuesIn(uur::BoolTestParam::makeBoolParam("UsePool")),
-        testing::Values(0), testing::Values(0)),
+        testing::Values(0), testing::Values(0),
+        ::testing::ValuesIn(uur::usm_alloc_test_parameters)),
     uur::printUSMAllocTestString<urUSMHostAllocTest>);
 
 TEST_P(urUSMHostAllocTest, Success) {
   UUR_KNOWN_FAILURE_ON(uur::NativeCPU{});
 
-  ur_device_usm_access_capability_flags_t hostUSMSupport = 0;
-  ASSERT_SUCCESS(uur::GetDeviceUSMHostSupport(device, hostUSMSupport));
-  if (!hostUSMSupport) {
-    GTEST_SKIP() << "Host USM is not supported.";
-  }
-
-  size_t allocation_size = sizeof(int);
-  int *ptr = nullptr;
+  allocation_size = sizeof(int);
   ASSERT_SUCCESS(urUSMHostAlloc(context, nullptr, pool, sizeof(int),
                                 reinterpret_cast<void **>(&ptr)));
   ASSERT_NE(ptr, nullptr);
@@ -77,7 +48,7 @@ TEST_P(urUSMHostAllocTest, Success) {
   EXPECT_SUCCESS(urQueueFlush(queue));
   ASSERT_SUCCESS(urEventWait(1, &event));
   EXPECT_SUCCESS(urEventRelease(event));
-  ASSERT_EQ(*ptr, 0);
+  ASSERT_EQ(*reinterpret_cast<int *>(ptr), 0);
 
   // Set 1, in all bytes of int
   pattern = 1;
@@ -86,23 +57,25 @@ TEST_P(urUSMHostAllocTest, Success) {
   EXPECT_SUCCESS(urQueueFlush(queue));
   ASSERT_SUCCESS(urEventWait(1, &event));
   EXPECT_SUCCESS(urEventRelease(event));
+
   // replicate it on host
   int set_data = 0;
   std::memset(&set_data, 1, allocation_size);
-  ASSERT_EQ(*ptr, set_data);
+  ASSERT_EQ(*reinterpret_cast<int *>(ptr), set_data);
 
   ASSERT_SUCCESS(urUSMFree(context, ptr));
 }
 
 TEST_P(urUSMHostAllocTest, SuccessWithDescriptors) {
-  ur_usm_host_desc_t usm_host_desc{UR_STRUCTURE_TYPE_USM_HOST_DESC, nullptr,
-                                   /* host flags */ 0};
+  const ur_usm_host_desc_t usm_host_desc{UR_STRUCTURE_TYPE_USM_HOST_DESC,
+                                         nullptr,
+                                         /* host flags */ 0};
 
-  ur_usm_desc_t usm_desc{UR_STRUCTURE_TYPE_USM_DESC, &usm_host_desc,
-                         /* mem advice flags */ UR_USM_ADVICE_FLAG_DEFAULT,
-                         /* alignment */ 0};
-  void *ptr = nullptr;
-  size_t allocation_size = sizeof(int);
+  const ur_usm_desc_t usm_desc{UR_STRUCTURE_TYPE_USM_DESC, &usm_host_desc,
+                               advice_flags, alignment};
+
+  allocation_size = sizeof(int);
+
   ASSERT_SUCCESS(
       urUSMHostAlloc(context, &usm_desc, pool, allocation_size, &ptr));
   ASSERT_NE(ptr, nullptr);
@@ -118,7 +91,6 @@ TEST_P(urUSMHostAllocTest, SuccessWithDescriptors) {
 }
 
 TEST_P(urUSMHostAllocTest, InvalidNullHandleContext) {
-  void *ptr = nullptr;
   ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_NULL_HANDLE,
                    urUSMHostAlloc(nullptr, nullptr, pool, sizeof(int), &ptr));
 }
@@ -132,17 +104,36 @@ TEST_P(urUSMHostAllocTest, InvalidNullPtrMem) {
 TEST_P(urUSMHostAllocTest, InvalidUSMSize) {
   UUR_KNOWN_FAILURE_ON(uur::CUDA{}, uur::HIP{}, uur::NativeCPU{});
 
-  void *ptr = nullptr;
   ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_USM_SIZE,
-                   urUSMHostAlloc(context, nullptr, pool, -1, &ptr));
+                   urUSMHostAlloc(context, nullptr, pool, 0, &ptr));
+
+  // TODO: Producing error X from case "size is greater than
+  // UR_DEVICE_INFO_MAX_MEM_ALLOC_SIZE" is currently unreliable due to
+  // implementation issues for UR_DEVICE_INFO_MAX_MEM_ALLOC_SIZE
+  // https://github.com/oneapi-src/unified-runtime/issues/2665
 }
 
-TEST_P(urUSMHostAllocTest, InvalidValueAlignPowerOfTwo) {
-  void *ptr = nullptr;
+TEST_P(urUSMHostAllocTest, InvalidValue) {
   ur_usm_desc_t desc = {};
   desc.stype = UR_STRUCTURE_TYPE_USM_DESC;
   desc.align = 5;
+  desc.hints = UR_USM_ADVICE_FLAG_DEFAULT;
+  desc.pNext = nullptr;
   ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_VALUE,
+                   urUSMHostAlloc(context, &desc, pool, sizeof(int), &ptr));
+
+  desc.align = std::numeric_limits<uint32_t>::max();
+  ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_VALUE,
+                   urUSMHostAlloc(context, &desc, pool, sizeof(int), &ptr));
+}
+
+TEST_P(urUSMHostAllocTest, InvalidEnumeration) {
+  ur_usm_desc_t desc = {};
+  desc.stype = UR_STRUCTURE_TYPE_USM_DESC;
+  desc.align = 5;
+  desc.hints = UR_USM_ADVICE_FLAG_FORCE_UINT32;
+  desc.pNext = nullptr;
+  ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_ENUMERATION,
                    urUSMHostAlloc(context, &desc, pool, sizeof(int), &ptr));
 }
 
@@ -152,21 +143,18 @@ UUR_DEVICE_TEST_SUITE_WITH_PARAM(
     urUSMHostAllocAlignmentTest,
     testing::Combine(
         testing::ValuesIn(uur::BoolTestParam::makeBoolParam("UsePool")),
-        testing::Values(4, 8, 16, 32, 64), testing::Values(8, 512, 2048)),
+        testing::Values(4, 8, 16, 32, 64), testing::Values(8, 512, 2048),
+        ::testing::ValuesIn(uur::usm_alloc_test_parameters)),
     uur::printUSMAllocTestString<urUSMHostAllocAlignmentTest>);
 
 TEST_P(urUSMHostAllocAlignmentTest, SuccessAlignedAllocations) {
-  uint32_t alignment = std::get<1>(getParam());
-  size_t allocation_size = std::get<2>(getParam());
+  const ur_usm_host_desc_t usm_host_desc{UR_STRUCTURE_TYPE_USM_HOST_DESC,
+                                         nullptr,
+                                         /* host flags */ 0};
 
-  ur_usm_host_desc_t usm_host_desc{UR_STRUCTURE_TYPE_USM_HOST_DESC, nullptr,
-                                   /* host flags */ 0};
+  const ur_usm_desc_t usm_desc{UR_STRUCTURE_TYPE_USM_DESC, &usm_host_desc,
+                               advice_flags, alignment};
 
-  ur_usm_desc_t usm_desc{UR_STRUCTURE_TYPE_USM_DESC, &usm_host_desc,
-                         /* mem advice flags */ UR_USM_ADVICE_FLAG_DEFAULT,
-                         alignment};
-
-  void *ptr = nullptr;
   ASSERT_SUCCESS(
       urUSMHostAlloc(context, &usm_desc, pool, allocation_size, &ptr));
   ASSERT_NE(ptr, nullptr);
