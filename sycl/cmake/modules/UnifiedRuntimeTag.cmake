83e9cf7e0e8ae1ae82116ded2b329388721d5732
Kenneth Benzie (Benie) <k.benzie@codeplay.com>
2025-01-29 15:57:12 +0000

    [UR] Bump main tag to 8e6ea72e

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit martin/memimage-info-unswitch
-# Merge: 6a3fece6 1a1108b3
-# Author: Martin Grant <martin.morrisongrant@codeplay.com>
-# Date:   Wed Jan 15 14:33:29 2025 +0000
-#     Merge pull request #2540 from martygrant/martin/program-info-unswitch
-#     Move urProgramGetInfo success test from a switch to individual tests.
-set(UNIFIED_RUNTIME_TAG martin/memimage-info-unswitch-redo)
+# commit 8e6ea72e44dab71e976b3daf439073c4ef9a847b
+# Merge: 1f6945ce c57c181d
+# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
+# Date:   Wed Jan 29 15:54:56 2025 +0000
+#     Merge pull request #2619 from martygrant/martin/memimage-info-unswitch-redo
+#     Move urMemImageGetInfo success test from a switch to individual test
+set(UNIFIED_RUNTIME_TAG 8e6ea72e44dab71e976b3daf439073c4ef9a847b)
