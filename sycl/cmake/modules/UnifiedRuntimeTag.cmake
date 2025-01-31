ca53d2dea52d160d2b71953bcc2fe1099b54939d
Duncan Brawley <duncan.brawley@codeplay.com>
2025-01-31 10:56:49 +0000

    Update UR link and commit

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 8e6ea72e44dab71e976b3daf439073c4ef9a847b
-# Merge: 1f6945ce c57c181d
+# commit e2df8acd37c3c159364cfdf9dfdc1be35b71779e
+# Merge: 992ff37f 3a31ffee
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Wed Jan 29 15:54:56 2025 +0000
-#     Merge pull request #2619 from martygrant/martin/memimage-info-unswitch-redo
-#     Move urMemImageGetInfo success test from a switch to individual test
-set(UNIFIED_RUNTIME_TAG 8e6ea72e44dab71e976b3daf439073c4ef9a847b)
+# Date:   Fri Jan 31 10:15:03 2025 +0000
+#     Merge pull request #2575 from DBDuncan/duncan/extend-copies
+#     [CUDA][Bindless] Add support for device to device pitched copies and host to host copies
+set(UNIFIED_RUNTIME_TAG e2df8acd37c3c159364cfdf9dfdc1be35b71779e)
