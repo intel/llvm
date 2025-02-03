4f3edc98691cc092bd785148dc3cf906e0822651
Kenneth Benzie (Benie) <k.benzie@codeplay.com>
2025-02-03 10:14:11 +0000

    [UR] Bump main tag to b487b627

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 78e1b33271d28d26845a4bfae7ae3b72c14e0e63
-# Merge: 902bb2e2 94b32ac2
-# Author: Ross Brunton <ross@codeplay.com>
-# Date:   Tue Jan 28 11:02:44 2025 +0000
-#     Merge pull request #2624 from RossBrunton/ross/msanfix
-#     Assert that Device is valid for memory poisoning
-set(UNIFIED_RUNTIME_TAG 21bf4befe863a3fe6a86c2bf43bb1c81cff2de9d)
+# commit b487b62738b24de5fbb664231b9a4934181d785e
+# Merge: af4f3310 21bf4bef
+# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
+# Date:   Mon Feb 3 10:09:15 2025 +0000
+#     Merge pull request #2612 from nrspruit/destroy_wait_always
+#     [L0]: fix missing destroy of event given enqueue wait out event
+set(UNIFIED_RUNTIME_TAG b487b62738b24de5fbb664231b9a4934181d785e)
