479da1d689647a793d221e61a498719e7addeedd
Petr Kurapov <petr.a.kurapov@intel.com>
2025-02-04 16:00:28 +0100

    [UR] Bump tag to 08d36b76 (#16810)
    Pulls in changes from:
    
    * https://github.com/oneapi-src/unified-runtime/pull/2614
    * https://github.com/oneapi-src/unified-runtime/pull/2596
    * https://github.com/oneapi-src/unified-runtime/pull/2620
    
    ---------
    
    Co-authored-by: Kenneth Benzie (Benie) <k.benzie@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit b487b62738b24de5fbb664231b9a4934181d785e
-# Merge: af4f3310 21bf4bef
+# commit 08d36b76a5b1c4f080e3301507a39525ab5ab365
+# Merge: 4c504dbc e6b61c67
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Mon Feb 3 10:09:15 2025 +0000
-#     Merge pull request #2612 from nrspruit/destroy_wait_always
-#     [L0]: fix missing destroy of event given enqueue wait out event
-set(UNIFIED_RUNTIME_TAG b487b62738b24de5fbb664231b9a4934181d785e)
+# Date:   Tue Feb 4 13:14:19 2025 +0000
+#     Merge pull request #2614 from kurapov-peter/spills
+#     Add UR_KERNEL_INFO_SPILL_MEM_SIZE kernel info prop
+set(UNIFIED_RUNTIME_TAG 08d36b76a5b1c4f080e3301507a39525ab5ab365)
