c2a5680ad022f14675cb90fbb03cbd227150a189
Kenneth Benzie (Benie) <k.benzie@codeplay.com>
2025-02-04 15:49:25 +0000

    [UR] Bump main tag to f07688db

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,9 +1,7 @@
-#commit 3dbf8b247a6f42bfed1db6e6bdfdfd0b0f1067fc 
-#Author: Zhang, Winston <winston.zhang@intel.com>
-#Date:   Wed Jan 29 16:52:47 2025 -0800
-#
-#[L0] MAX_COMPUTE_UNITS using ze_eu_count_ext_t
-#
-#For some recovery SKUs, MAX_COMPUTE_COUNT calculation does not provide the correct number of EUs. Now we will  use ze_eu_count_t when
-#available.
-set(UNIFIED_RUNTIME_TAG 3dbf8b247a6f42bfed1db6e6bdfdfd0b0f1067fc)
+# commit f07688dbc20c73d7e480cb62d7dc0ce7dc822bd3
+# Merge: 7d864b6c 3dbf8b24
+# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
+# Date:   Tue Feb 4 15:45:49 2025 +0000
+#     Merge pull request #2618 from winstonzhang-intel/max_eu_count_calculation
+#     [L0] MAX_COMPUTE_UNITS using ze_eu_count_ext_t
+set(UNIFIED_RUNTIME_TAG f07688dbc20c73d7e480cb62d7dc0ce7dc822bd3)
