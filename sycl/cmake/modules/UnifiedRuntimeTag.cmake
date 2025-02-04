1408bf66b16c84af3de6d09ec3968a1525f29db0
Lukasz Dorau <lukasz.dorau@intel.com>
2025-02-04 08:40:42 +0100

    TEST: Use UMF CUDA provider in Unified Runtime

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -4,4 +4,8 @@
 # Date:   Tue Feb 4 15:45:49 2025 +0000
 #     Merge pull request #2618 from winstonzhang-intel/max_eu_count_calculation
 #     [L0] MAX_COMPUTE_UNITS using ze_eu_count_ext_t
-set(UNIFIED_RUNTIME_TAG f07688dbc20c73d7e480cb62d7dc0ce7dc822bd3)
+# set(UNIFIED_RUNTIME_TAG f07688dbc20c73d7e480cb62d7dc0ce7dc822bd3)
+#
+# + Test "Use UMF CUDA provider in UR" by ldorau
+#
+set(UNIFIED_RUNTIME_TAG 93c4da6f0eba70f20ba3731880ee353952824d2a)
