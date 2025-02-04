2029e2bab87bc59b5e84dc361d5652e793b486c1
Petr Kurapov <petr.a.kurapov@intel.com>
2025-02-04 15:21:43 +0000

    Update UR tag

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,14 +1,7 @@
 # commit d03f19a88e42cb98be9604ff24b61190d1e48727
 # Merge: 3ce6fcc9 84454b0e
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-<<<<<<< HEAD
 # Date:   Thu Feb 13 11:43:34 2025 +0000
 #     Merge pull request #2680 from ldorau/Set_UMF_CUDA_INCLUDE_DIR_to_not_fetch_cudart_from_gitlab
 #     Do not fetch cudart from gitlab for UMF
 set(UNIFIED_RUNTIME_TAG d03f19a88e42cb98be9604ff24b61190d1e48727)
-=======
-# Date:   Mon Jan 27 10:40:02 2025 +0000
-#     Merge pull request #2551 from przemektmalon/przemek/bindless-images-host-usm
-#     Enable creation of bindless images backed by host USM
-set(UNIFIED_RUNTIME_TAG e6b61c6768f9e12b46ed921a4bdf3283de53772f)
->>>>>>> a7fa67b9ba93 (Specify UR version to inlude upcoming changes)
