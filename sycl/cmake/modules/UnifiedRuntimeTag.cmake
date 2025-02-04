d3e825ca30585c61b1318105775498d3132dec4e
Neil R. Spruit <neil.r.spruit@intel.com>
2025-02-04 04:35:29 -0800

    [UR][L0]: fix missing destroy of event given enqueue wait out event (#16759)
    -pre-commit PR for
    https://github.com/oneapi-src/unified-runtime/pull/2612
    
    ---------
    
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>
    Co-authored-by: Kenneth Benzie (Benie) <k.benzie@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit e2df8acd37c3c159364cfdf9dfdc1be35b71779e
-# Merge: 992ff37f 3a31ffee
+# commit b487b62738b24de5fbb664231b9a4934181d785e
+# Merge: af4f3310 21bf4bef
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Fri Jan 31 10:15:03 2025 +0000
-#     Merge pull request #2575 from DBDuncan/duncan/extend-copies
-#     [CUDA][Bindless] Add support for device to device pitched copies and host to host copies
-set(UNIFIED_RUNTIME_TAG e2df8acd37c3c159364cfdf9dfdc1be35b71779e)
+# Date:   Mon Feb 3 10:09:15 2025 +0000
+#     Merge pull request #2612 from nrspruit/destroy_wait_always
+#     [L0]: fix missing destroy of event given enqueue wait out event
+set(UNIFIED_RUNTIME_TAG b487b62738b24de5fbb664231b9a4934181d785e)
