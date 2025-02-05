martygrant/unified-runtime

Improvements to align CTS and Spec for USM: - Additional error case checking - More validation for urUSMPoolRelease - Refactored urUSMDevice/Shared/HostAlloc test structs into a shared parent struct - Added ur_usm_advice_flags_t param to parameterised Alloc tests

test/conformance/usm/helpers.h
@@ -11,7 +11,40 @@
 
 namespace uur {
 
-using USMDeviceAllocParams = std::tuple<uur::BoolTestParam, uint32_t, size_t>;
+using USMDeviceAllocParams =
+    std::tuple<uur::BoolTestParam, uint32_t, size_t, ur_usm_advice_flags_t>;
+
+struct urUSMAllocTest : uur::urQueueTestWithParam<uur::USMDeviceAllocParams> {
+  void SetUp() override {
+    UUR_RETURN_ON_FATAL_FAILURE(
+        uur::urQueueTestWithParam<uur::USMDeviceAllocParams>::SetUp());
+    if (usePool) {
+      ur_bool_t poolSupport = false;
+      ASSERT_SUCCESS(uur::GetDeviceUSMPoolSupport(device, poolSupport));
+      if (!poolSupport) {
+        GTEST_SKIP() << "USM pools are not supported.";
+      }
+      ur_usm_pool_desc_t pool_desc = {};
+      ASSERT_SUCCESS(urUSMPoolCreate(context, &pool_desc, &pool));
+    }
+  }
+
+  void TearDown() override {
+    if (pool) {
+      ASSERT_SUCCESS(urUSMPoolRelease(pool));
+    }
+    UUR_RETURN_ON_FATAL_FAILURE(
+        uur::urQueueTestWithParam<uur::USMDeviceAllocParams>::TearDown());
+  }
+
+  ur_usm_pool_handle_t pool = nullptr;
+  const bool usePool = std::get<0>(getParam()).value;
+  ur_device_usm_access_capability_flags_t USMSupport = 0;
+  const uint32_t alignment = std::get<1>(getParam());
+  size_t allocation_size = std::get<2>(getParam());
+  const ur_usm_advice_flags_t advice_flags = std::get<3>(getParam());
+  void *ptr = nullptr;
+};
 
 template <typename T>
 inline std::string printUSMAllocTestString(
@@ -24,19 +57,35 @@ inline std::string printUSMAllocTestString(
   const auto &BoolParam = std::get<0>(usmDeviceAllocParams);
 
   std::stringstream ss;
-  ss << BoolParam.name << (BoolParam.value ? "Enabled" : "Disabled");
-
-  const auto alignment = std::get<1>(usmDeviceAllocParams);
-  const auto size = std::get<2>(usmDeviceAllocParams);
-  if (alignment && size > 0) {
-    ss << "_";
-    ss << std::get<1>(usmDeviceAllocParams);
-    ss << "_";
-    ss << std::get<2>(usmDeviceAllocParams);
-  }
+  ss << BoolParam.name << (BoolParam.value ? "Enabled" : "Disabled"); // UsePool
+  ss << "_";
+  ss << std::get<1>(usmDeviceAllocParams); // alignment
+  ss << "_";
+  ss << std::get<2>(usmDeviceAllocParams); // size
+  ss << "_";
+  ss << std::get<3>(usmDeviceAllocParams); // ur_usm_advice_flags_t
 
   return platform_device_name + "__" + ss.str();
 }
+
+static std::vector<ur_usm_advice_flags_t> usm_alloc_test_parameters{
+    UR_USM_ADVICE_FLAG_DEFAULT,
+    UR_USM_ADVICE_FLAG_SET_READ_MOSTLY,
+    UR_USM_ADVICE_FLAG_CLEAR_READ_MOSTLY,
+    UR_USM_ADVICE_FLAG_SET_PREFERRED_LOCATION,
+    UR_USM_ADVICE_FLAG_CLEAR_PREFERRED_LOCATION,
+    UR_USM_ADVICE_FLAG_SET_NON_ATOMIC_MOSTLY,
+    UR_USM_ADVICE_FLAG_CLEAR_NON_ATOMIC_MOSTLY,
+    UR_USM_ADVICE_FLAG_BIAS_CACHED,
+    UR_USM_ADVICE_FLAG_BIAS_UNCACHED,
+    UR_USM_ADVICE_FLAG_SET_ACCESSED_BY_DEVICE,
+    UR_USM_ADVICE_FLAG_CLEAR_ACCESSED_BY_DEVICE,
+    UR_USM_ADVICE_FLAG_SET_ACCESSED_BY_HOST,
+    UR_USM_ADVICE_FLAG_CLEAR_ACCESSED_BY_HOST,
+    UR_USM_ADVICE_FLAG_SET_PREFERRED_LOCATION_HOST,
+    UR_USM_ADVICE_FLAG_CLEAR_PREFERRED_LOCATION_HOST,
+    UR_USM_ADVICE_FLAG_SET_NON_COHERENT_MEMORY,
+    UR_USM_ADVICE_FLAG_CLEAR_NON_COHERENT_MEMORY};
 
 } // namespace uur
 

test/conformance/usm/urUSMPoolRelease.cpp
@@ -6,15 +6,31 @@
 
 #include <uur/fixtures.h>
 
-using urUSMPoolDestroyTest = uur::urUSMPoolTest;
-UUR_INSTANTIATE_DEVICE_TEST_SUITE_P(urUSMPoolDestroyTest);
+using urUSMPoolReleaseTest = uur::urUSMPoolTest;
+UUR_INSTANTIATE_DEVICE_TEST_SUITE_P(urUSMPoolReleaseTest);
 
-TEST_P(urUSMPoolDestroyTest, Success) {
-  ASSERT_SUCCESS(urUSMPoolRelease(pool));
+TEST_P(urUSMPoolReleaseTest, Success) {
+  uint32_t prevRefCount = 0;
+  ASSERT_SUCCESS(uur::GetObjectReferenceCount(pool, prevRefCount));
+
+  ASSERT_SUCCESS(urUSMPoolRetain(pool));
+
+  uint32_t refCount = 0;
+  ASSERT_SUCCESS(uur::GetObjectReferenceCount(pool, refCount));
+
+  ASSERT_LT(prevRefCount, refCount);
+
+  EXPECT_SUCCESS(urUSMPoolRelease(pool));
+
+  uint32_t afterRefCount = 0;
+  ASSERT_SUCCESS(uur::GetObjectReferenceCount(pool, afterRefCount));
+
+  ASSERT_LT(afterRefCount, refCount);
+
   pool = nullptr; // prevent double-delete
 }
 
-TEST_P(urUSMPoolDestroyTest, InvalidNullHandleContext) {
+TEST_P(urUSMPoolReleaseTest, InvalidNullHandle) {
   ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_NULL_HANDLE,
                    urUSMPoolRelease(nullptr));
 }

intel/llvm

test/conformance/usm/helpers.h
@@ -11,32 +11,81 @@
 
 namespace uur {
 
-using USMDeviceAllocParams = std::tuple<uur::BoolTestParam, uint32_t, size_t>;
+using USMAllocTestParams =
+    std::tuple<uur::BoolTestParam, uint32_t, size_t, ur_usm_advice_flags_t>;
+
+struct urUSMAllocTest : uur::urQueueTestWithParam<uur::USMAllocTestParams> {
+  void SetUp() override {
+    UUR_RETURN_ON_FATAL_FAILURE(
+        uur::urQueueTestWithParam<uur::USMAllocTestParams>::SetUp());
+    if (usePool) {
+      ur_bool_t poolSupport = false;
+      ASSERT_SUCCESS(uur::GetDeviceUSMPoolSupport(device, poolSupport));
+      if (!poolSupport) {
+        GTEST_SKIP() << "USM pools are not supported.";
+      }
+      ur_usm_pool_desc_t pool_desc = {};
+      ASSERT_SUCCESS(urUSMPoolCreate(context, &pool_desc, &pool));
+    }
+  }
+
+  void TearDown() override {
+    if (pool) {
+      ASSERT_SUCCESS(urUSMPoolRelease(pool));
+    }
+    UUR_RETURN_ON_FATAL_FAILURE(
+        uur::urQueueTestWithParam<uur::USMAllocTestParams>::TearDown());
+  }
+
+  ur_usm_pool_handle_t pool = nullptr;
+  const bool usePool = std::get<0>(getParam()).value;
+  ur_device_usm_access_capability_flags_t USMSupport = 0;
+  const uint32_t alignment = std::get<1>(getParam());
+  size_t allocation_size = std::get<2>(getParam());
+  const ur_usm_advice_flags_t advice_flags = std::get<3>(getParam());
+  void *ptr = nullptr;
+};
 
 template <typename T>
 inline std::string printUSMAllocTestString(
     const testing::TestParamInfo<typename T::ParamType> &info) {
-  // ParamType will be std::tuple<ur_device_handle_t, USMDeviceAllocParams>
+  // ParamType will be std::tuple<ur_device_handle_t, USMAllocTestParams>
   const auto device_handle = std::get<0>(info.param).device;
   const auto platform_device_name =
       uur::GetPlatformAndDeviceName(device_handle);
-  const auto &usmDeviceAllocParams = std::get<1>(info.param);
-  const auto &BoolParam = std::get<0>(usmDeviceAllocParams);
+  const auto &USMAllocTestParams = std::get<1>(info.param);
+  const auto &BoolParam = std::get<0>(USMAllocTestParams);
 
   std::stringstream ss;
-  ss << BoolParam.name << (BoolParam.value ? "Enabled" : "Disabled");
-
-  const auto alignment = std::get<1>(usmDeviceAllocParams);
-  const auto size = std::get<2>(usmDeviceAllocParams);
-  if (alignment && size > 0) {
-    ss << "_";
-    ss << std::get<1>(usmDeviceAllocParams);
-    ss << "_";
-    ss << std::get<2>(usmDeviceAllocParams);
-  }
+  ss << BoolParam.name << (BoolParam.value ? "Enabled" : "Disabled"); // UsePool
+  ss << "_";
+  ss << std::get<1>(USMAllocTestParams); // alignment
+  ss << "_";
+  ss << std::get<2>(USMAllocTestParams); // size
+  ss << "_";
+  ss << std::get<3>(USMAllocTestParams); // ur_usm_advice_flags_t
 
   return platform_device_name + "__" + ss.str();
 }
+
+static std::vector<ur_usm_advice_flags_t> usm_alloc_test_parameters{
+    UR_USM_ADVICE_FLAG_DEFAULT,
+    UR_USM_ADVICE_FLAG_SET_READ_MOSTLY,
+    UR_USM_ADVICE_FLAG_CLEAR_READ_MOSTLY,
+    UR_USM_ADVICE_FLAG_SET_PREFERRED_LOCATION,
+    UR_USM_ADVICE_FLAG_CLEAR_PREFERRED_LOCATION,
+    UR_USM_ADVICE_FLAG_SET_NON_ATOMIC_MOSTLY,
+    UR_USM_ADVICE_FLAG_CLEAR_NON_ATOMIC_MOSTLY,
+    UR_USM_ADVICE_FLAG_BIAS_CACHED,
+    UR_USM_ADVICE_FLAG_BIAS_UNCACHED,
+    UR_USM_ADVICE_FLAG_SET_ACCESSED_BY_DEVICE,
+    UR_USM_ADVICE_FLAG_CLEAR_ACCESSED_BY_DEVICE,
+    UR_USM_ADVICE_FLAG_SET_ACCESSED_BY_HOST,
+    UR_USM_ADVICE_FLAG_CLEAR_ACCESSED_BY_HOST,
+    UR_USM_ADVICE_FLAG_SET_PREFERRED_LOCATION_HOST,
+    UR_USM_ADVICE_FLAG_CLEAR_PREFERRED_LOCATION_HOST,
+    UR_USM_ADVICE_FLAG_SET_NON_COHERENT_MEMORY,
+    UR_USM_ADVICE_FLAG_CLEAR_NON_COHERENT_MEMORY};
 
 } // namespace uur
 

test/conformance/usm/urUSMPoolRelease.cpp
@@ -6,15 +6,31 @@
 
 #include <uur/fixtures.h>
 
-using urUSMPoolDestroyTest = uur::urUSMPoolTest;
-UUR_INSTANTIATE_DEVICE_TEST_SUITE(urUSMPoolDestroyTest);
+using urUSMPoolReleaseTest = uur::urUSMPoolTest;
+UUR_INSTANTIATE_DEVICE_TEST_SUITE(urUSMPoolReleaseTest);
 
-TEST_P(urUSMPoolDestroyTest, Success) {
-  ASSERT_SUCCESS(urUSMPoolRelease(pool));
+TEST_P(urUSMPoolReleaseTest, Success) {
+  uint32_t prevRefCount = 0;
+  ASSERT_SUCCESS(uur::GetObjectReferenceCount(pool, prevRefCount));
+
+  ASSERT_SUCCESS(urUSMPoolRetain(pool));
+
+  uint32_t refCount = 0;
+  ASSERT_SUCCESS(uur::GetObjectReferenceCount(pool, refCount));
+
+  ASSERT_LT(prevRefCount, refCount);
+
+  EXPECT_SUCCESS(urUSMPoolRelease(pool));
+
+  uint32_t afterRefCount = 0;
+  ASSERT_SUCCESS(uur::GetObjectReferenceCount(pool, afterRefCount));
+
+  ASSERT_LT(afterRefCount, refCount);
+
   pool = nullptr; // prevent double-delete
 }
 
-TEST_P(urUSMPoolDestroyTest, InvalidNullHandleContext) {
+TEST_P(urUSMPoolReleaseTest, InvalidNullHandle) {
   ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_NULL_HANDLE,
                    urUSMPoolRelease(nullptr));
 }