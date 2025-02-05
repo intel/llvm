23b2457304bda9926eaa2b6201e9f2d8c5a590be
Lukasz Dorau <lukasz.dorau@intel.com>
2025-02-05 16:38:46 +0100

    Use UMF CUDA provider in Unified Runtime (#16761)
    Co-authored-by: Kenneth Benzie (Benie) <k.benzie@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit f07688dbc20c73d7e480cb62d7dc0ce7dc822bd3
-# Merge: 7d864b6c 3dbf8b24
+# commit 87d4a32666f2950e7cfa016ff181311c2e0ebbca
+# Merge: f07688db 8e4b5c3d
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Tue Feb 4 15:45:49 2025 +0000
-#     Merge pull request #2618 from winstonzhang-intel/max_eu_count_calculation
-#     [L0] MAX_COMPUTE_UNITS using ze_eu_count_ext_t
-set(UNIFIED_RUNTIME_TAG f07688dbc20c73d7e480cb62d7dc0ce7dc822bd3)
+# Date:   Tue Feb 4 15:45:58 2025 +0000
+#     Merge pull request #2480 from ldorau/Add_UMF_CUDA_provider
+#     Use UMF CUDA memory provider in UR
+set(UNIFIED_RUNTIME_TAG 87d4a32666f2950e7cfa016ff181311c2e0ebbca)
