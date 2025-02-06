1fe1862f18f658ee9be9d81996341de2f68a5425
Harald van Dijk <harald.vandijk@codeplay.com>
2025-02-06 17:55:46 +0000

    [SYCL][NativeCPU] Handle null phEvent. (#16868)
    Matching PR: https://github.com/oneapi-src/unified-runtime/pull/2655
    
    ---------
    
    Co-authored-by: Kenneth Benzie (Benie) <k.benzie@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 0e6adfb64039d3dd5fc8363245fcac60cbdcb8bd
-# Merge: 42044a83 d0c87169
+# commit 9d6542bac16db69d907002cb0f8c724c7a50d2dc
+# Merge: 0e6adfb6 04882ba3
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Wed Feb 5 15:39:15 2025 +0000
-#     Merge pull request #2496 from GeorgeWeb/georgi/bindless-hip
-#     [UR][Bindless] Initial implementation of bindless images for HIP
-set(UNIFIED_RUNTIME_TAG 0e6adfb64039d3dd5fc8363245fcac60cbdcb8bd)
+# Date:   Thu Feb 6 11:30:48 2025 +0000
+#     Merge pull request #2655 from hvdijk/nativecpu-null-phevent
+#     [NativeCPU] Handle null phEvent.
+set(UNIFIED_RUNTIME_TAG 9d6542bac16db69d907002cb0f8c724c7a50d2dc)
