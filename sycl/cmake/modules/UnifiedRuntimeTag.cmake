900976edbd9b1e371211c0e40f930315987e8a5b
Kenneth Benzie (Benie) <k.benzie@codeplay.com>
2025-02-06 18:02:27 +0000

    [UR] Bump main tag to cc60d087

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,2 +1,7 @@
-set(UNIFIED_RUNTIME_REPO "https://github.com/Bensuo/unified-runtime.git")
-set(UNIFIED_RUNTIME_TAG "cmd-buf_enqueue_refactor")
+# commit cc60d0872f1ffe0656bc9ec25db5b5cbb3cbafd6
+# Merge: 0c11a742 ec5a3bd1
+# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
+# Date:   Thu Feb 6 17:59:43 2025 +0000
+#     Merge pull request #2606 from Bensuo/cmd-buf_enqueue_refactor
+#     Rename urCommandBufferEnqueueExp to urEnqueueCommandBufferExp
+set(UNIFIED_RUNTIME_TAG cc60d0872f1ffe0656bc9ec25db5b5cbb3cbafd6)
