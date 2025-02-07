2f75188a06b6af08d5605b54fbc36051b8d87c01
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2025-02-07 14:59:04 +0100

    [UR] Bump UMF to v0.11.0-dev2
    It includes fix for icx compiler

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,6 @@
-# commit 9d6542bac16db69d907002cb0f8c724c7a50d2dc
-# Merge: 0e6adfb6 04882ba3
-# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Thu Feb 6 11:30:48 2025 +0000
-#     Merge pull request #2655 from hvdijk/nativecpu-null-phevent
-#     [NativeCPU] Handle null phEvent.
-set(UNIFIED_RUNTIME_TAG 9d6542bac16db69d907002cb0f8c724c7a50d2dc)
+# commit b0b4b2255f23beb519645ec624ab12a5d5b2069a (HEAD -> umf-with-icx-build-0.11.0-dev2, origin/umf-with-icx-build-0.11.0-dev2)
+# Author: Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
+# Date:   Fri Feb 7 14:53:40 2025 +0100
+# common: Bump UMF version to v0.11.0-dev2
+# It includes fix for icx compiler
+set(UNIFIED_RUNTIME_TAG b0b4b2255f23beb519645ec624ab12a5d5b2069a)
