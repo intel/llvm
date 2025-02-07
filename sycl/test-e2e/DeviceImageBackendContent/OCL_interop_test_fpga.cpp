495b8f7305d2ff36e4b2042d770a9cf34cc165f3
Lorenc Bushi <lorenc.bushi@intel.com>
2025-02-07 15:02:30 -0800

    More refactoring

--- sycl/test-e2e/DeviceImageBackendContent/OCL_interop_test_fpga.cpp
+++ sycl/test-e2e/DeviceImageBackendContent/OCL_interop_test_fpga.cpp
@@ -1,3 +1,4 @@
 // REQUIRES: opencl, opencl_icd, opencl-aot, accelerator, aspect-usm_shared_allocations
-// RUN: %clangxx -fsycl -fno-sycl-dead-args-optimization %opencl_lib -fsycl-targets=spir64_fpga %S/OCL_common.cpp -o %t.out
+// RUN: %{build} -fno-sycl-dead-args-optimization %opencl_lib -fsycl-targets=spir64_fpga -o %t.out
 // RUN: %{run} %t.out
+#include "OCL_common.hpp"
--- sycl/test-e2e/DeviceImageBackendContent/OCL_interop_test_no_fpga.cpp
+++ sycl/test-e2e/DeviceImageBackendContent/OCL_interop_test_no_fpga.cpp
@@ -1,3 +1,4 @@
 // REQUIRES: opencl, opencl_icd, aspect-usm_shared_allocations, !accelerator
-// RUN: %clangxx -fsycl -fno-sycl-dead-args-optimization %opencl_lib %S/OCL_common.cpp -o %t.out
+// RUN: %{build} -fno-sycl-dead-args-optimization %opencl_lib -o %t.out
 // RUN: %{run} %t.out
+#include "OCL_common.hpp"
