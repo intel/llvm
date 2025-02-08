17607c3a03e924db28274ab3c25df20860c2b46c
Lorenc Bushi <lorenc.bushi@intel.com>
2025-02-07 17:51:59 -0800

    Split OCL interop tests for each device type

--- sycl/test-e2e/DeviceImageBackendContent/OCL_interop_cpu.cpp
+++ sycl/test-e2e/DeviceImageBackendContent/OCL_interop_cpu.cpp
@@ -1,4 +1,4 @@
-// REQUIRES: opencl, opencl_icd, aspect-usm_shared_allocations, cpu
+// REQUIRES: opencl, opencl_icd, aspect-usm_shared_allocations, opencl-aot, cpu
 // RUN: %{build} -fno-sycl-dead-args-optimization -fsycl-targets=spir64_x86_64 %opencl_lib -o %t.out
 // RUN: %{run} %t.out
 #include "OCL_common.hpp"
