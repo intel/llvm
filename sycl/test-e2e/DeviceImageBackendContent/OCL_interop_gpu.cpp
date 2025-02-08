4662cd573870635510b87772691c2cdc53983876
Lorenc Bushi <lorenc.bushi@intel.com>
2025-02-07 21:12:02 -0500

    Update OCL_interop_gpu.cpp

--- sycl/test-e2e/DeviceImageBackendContent/OCL_interop_gpu.cpp
+++ sycl/test-e2e/DeviceImageBackendContent/OCL_interop_gpu.cpp
@@ -1,4 +1,4 @@
-// REQUIRES: opencl, opencl_icd, aspect-usm_shared_allocations, ocloc, gpu
-// RUN: %{build} -fno-sycl-dead-args-optimization -fsycl-targets=spir64_gen %opencl_lib -o %t.out
+// REQUIRES: opencl, opencl_icd, aspect-usm_shared_allocations, gpu
+// RUN: %{build} -fno-sycl-dead-args-optimization %opencl_lib -o %t.out
 // RUN: %{run} %t.out
 #include "OCL_common.hpp"
