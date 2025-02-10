b9775b10c551ac818839888201dbf2636e13fd72
Lukasz Dorau <lukasz.dorau@intel.com>
2025-02-10 11:16:50 +0100

    Do not fetch cudart from gitlab for UMF
    Signed-off-by: Lukasz Dorau <lukasz.dorau@intel.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,1 @@
-# commit 9d6542bac16db69d907002cb0f8c724c7a50d2dc
-# Merge: 0e6adfb6 04882ba3
-# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Thu Feb 6 11:30:48 2025 +0000
-#     Merge pull request #2655 from hvdijk/nativecpu-null-phevent
-#     [NativeCPU] Handle null phEvent.
-set(UNIFIED_RUNTIME_TAG 9d6542bac16db69d907002cb0f8c724c7a50d2dc)
+set(UNIFIED_RUNTIME_TAG 4b5799ac0ec43b21aa07c787d47df0aa139abad4)
