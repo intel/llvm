22d85eeef4854cacd73e8bc6d12c10a814963893
Lorenc Bushi <lorenc.bushi@intel.com>
2025-02-09 20:44:50 -0800

    Remove GPU test

--- sycl/test-e2e/DeviceImageBackendContent/OCL_interop_gpu.cpp
+++
@@ -1,4 +0,0 @@
-// REQUIRES: opencl, opencl_icd, aspect-usm_shared_allocations, gpu
-// RUN: %{build} -fno-sycl-dead-args-optimization %opencl_lib -o %t.out
-// RUN: %{run} %t.out
-#include "OCL_common.hpp"
