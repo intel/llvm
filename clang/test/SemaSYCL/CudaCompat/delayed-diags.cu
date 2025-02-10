662ea1ebcf540a3f2e69b79ce4eb2a7c66c608cb
Victor Lomuller <victor@codeplay.com>
2025-02-10 21:45:48 +0000

    test printf

--- clang/test/SemaSYCL/CudaCompat/delayed-diags.cu
+++ clang/test/SemaSYCL/CudaCompat/delayed-diags.cu
@@ -1,9 +1,11 @@
 // RUN: %clang_cc1 %s -fsycl-is-host -fsycl-cuda-compatibility \
 // RUN:   -internal-isystem %S/../../SemaCUDA/Inputs \
+// RUN:   -internal-isystem %S/../Inputs \
 // RUN:   -fsycl-targets=nvptx64-nvidia-cuda -triple x86_64-unknown-linux \
 // RUN:   -emit-llvm -o - -verify -verify-ignore-unexpected=note
 // RUN: %clang_cc1 %s -fsycl-is-device -fsycl-cuda-compatibility \
 // RUN:   -internal-isystem %S/../../SemaCUDA/Inputs \
+// RUN:   -internal-isystem %S/../Inputs \
 // RUN:   -fsycl-targets=nvptx64-nvidia-cuda -triple x86_64-unknown-linux\
 // RUN:   -emit-llvm -o - -verify -verify-ignore-unexpected=note
 
@@ -11,6 +13,7 @@
 // expected-no-diagnostics
 
 #include "cuda.h"
+#include "sycl.hpp"
 
 struct Foo {
   Foo();
@@ -19,4 +22,16 @@ struct Foo {
 Foo& get() {
   static Foo f;
   return f;
+}
+
+int printf(const char *restrict, ...) { return 0; }
+
+void print() {
+  sycl::queue q;
+  q.submit([&](sycl::handler &h) {
+    h.single_task<class use_image1d_r>(
+        [=] {
+          printf("hello");
+        });
+  });
 }
