79c0975475e08091fbfd86a5b466fb82b5633754
Kornev, Nikita <nikita.kornev@intel.com>
2025-02-11 13:37:50 +0100

    update tests

--- sycl/test-e2e/OptionalKernelFeatures/is_compatible/is_compatible_spir64_fpga.cpp
+++
@@ -1,7 +0,0 @@
-// REQUIRES: opencl-aot, any-device-is-gpu, any-device-is-cpu
-
-// RUN: %clangxx -fsycl -fsycl-targets=spir64_fpga %S/Inputs/is_compatible_with_env.cpp -o %t.out
-
-// RUN: env ONEAPI_DEVICE_SELECTOR=opencl:fpga %{run-unfiltered-devices} %t.out
-// RUN: env ONEAPI_DEVICE_SELECTOR=*:gpu %{run-unfiltered-devices} not %t.out
-// RUN: env ONEAPI_DEVICE_SELECTOR=opencl:cpu %{run-unfiltered-devices} not %t.out
--- sycl/test-e2e/OptionalKernelFeatures/is_compatible/is_compatible_spir64_gen.cpp
+++ sycl/test-e2e/OptionalKernelFeatures/is_compatible/is_compatible_spir64_gen.cpp
@@ -1,4 +1,4 @@
-// REQUIRES: ocloc, any-device-is-cpu, level_zero
+// REQUIRES: ocloc, any-device-is-cpu, any-device-is-level_zero
 
 // RUN: %clangxx -fsycl -fsycl-targets=spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts %S/Inputs/is_compatible_with_env.cpp -o %t.out
 
