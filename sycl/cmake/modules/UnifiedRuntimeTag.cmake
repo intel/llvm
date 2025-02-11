5550792feb7dca48774275f603ca7264cb553e88
Kenneth Benzie (Benie) <k.benzie@codeplay.com>
2025-02-11 12:10:27 +0000

    [UR] Bump main tag to 98756a20

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit bdbe7b47a8d72710da60a409d6e21f64080d6f05
-# Merge: e55fe50f 508e6b9b
+# commit 98756a20d7792636c87b6193fe1ff47ec4f78914
+# Merge: 34c21d7f 8b22ed83
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Mon Feb 10 11:10:34 2025 +0000
-#     Merge pull request #2679 from yingcong-wu/yc/0208-ur-dasan-bugfix-main
-#     [DeviceASAN][bugfix] Not allow concurrent kernel launches across different queue
-set(UNIFIED_RUNTIME_TAG bdbe7b47a8d72710da60a409d6e21f64080d6f05)
+# Date:   Tue Feb 11 12:09:29 2025 +0000
+#     Merge pull request #2688 from Bensuo/revert_2606
+#     Revert "Merge pull request #2606 from Bensuo/cmd-buf_enqueue_refactor"
+set(UNIFIED_RUNTIME_TAG 98756a20d7792636c87b6193fe1ff47ec4f78914)
