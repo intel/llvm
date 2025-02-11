a8f30a9212d92068a7649c61a75432f4a66e3ab8
Wu Yingcong <yingcong.wu@intel.com>
2025-02-11 21:37:02 +0800

    [DeviceASAN][bugfix] Not allow concurrent kernel launches across different queue (#16935)
    UR PR: https://github.com/oneapi-src/unified-runtime/pull/2679
    
    ---------
    
    Co-authored-by: Kenneth Benzie (Benie) <k.benzie@codeplay.com>

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 9d6542bac16db69d907002cb0f8c724c7a50d2dc
-# Merge: 0e6adfb6 04882ba3
+# commit 98756a20d7792636c87b6193fe1ff47ec4f78914
+# Merge: 34c21d7f 8b22ed83
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Thu Feb 6 11:30:48 2025 +0000
-#     Merge pull request #2655 from hvdijk/nativecpu-null-phevent
-#     [NativeCPU] Handle null phEvent.
-set(UNIFIED_RUNTIME_TAG 9d6542bac16db69d907002cb0f8c724c7a50d2dc)
+# Date:   Tue Feb 11 12:09:29 2025 +0000
+#     Merge pull request #2688 from Bensuo/revert_2606
+#     Revert "Merge pull request #2606 from Bensuo/cmd-buf_enqueue_refactor"
+set(UNIFIED_RUNTIME_TAG 98756a20d7792636c87b6193fe1ff47ec4f78914)
