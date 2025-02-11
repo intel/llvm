15012f8f3f5d1396e7c729e8e0c19735778ff1a7
Kenneth Benzie (Benie) <k.benzie@codeplay.com>
2025-02-11 13:41:59 +0000

    [UR] Bump main tag to a0d3cb5d

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,6 +1,1 @@
-# commit b0b4b2255f23beb519645ec624ab12a5d5b2069a (HEAD -> umf-with-icx-build-0.11.0-dev2, origin/umf-with-icx-build-0.11.0-dev2)
-# Author: Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
-# Date:   Fri Feb 7 14:53:40 2025 +0100
-# common: Bump UMF version to v0.11.0-dev2
-# It includes fix for icx compiler
-set(UNIFIED_RUNTIME_TAG b0b4b2255f23beb519645ec624ab12a5d5b2069a)
+set(UNIFIED_RUNTIME_TAG a0d3cb5d1eb1d8a50b1c09326ca2b5e2aaea33e3b0b4b2255f23beb519645ec624ab12a5d5b2069a)
