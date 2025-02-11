c159601f1877f80a3629a96e400fe89bbcfa08ee
Kenneth Benzie (Benie) <k.benzie@codeplay.com>
2025-02-11 10:33:19 +0000

    [UR] Noop PR to test CI

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -2,6 +2,8 @@
 # Merge: 0e6adfb6 04882ba3
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
 # Date:   Thu Feb 6 11:30:48 2025 +0000
+#
 #     Merge pull request #2655 from hvdijk/nativecpu-null-phevent
+#
 #     [NativeCPU] Handle null phEvent.
 set(UNIFIED_RUNTIME_TAG 9d6542bac16db69d907002cb0f8c724c7a50d2dc)
