b3775458fafa7621de2475ae3cf9d319df6990c2
Rafal Rudnicki <rafal.rudnicki@intel.com>
2025-02-12 17:31:34 +0000

    disjoint_c

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,2 @@
-# commit 064d3560cbcc24a40380b57adb3690b2bc256d6e
-# Merge: 739d8a75 e1bbdad9
-# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Wed Feb 12 10:20:12 2025 +0000
-#     Merge pull request #2672 from lukaszstolarczuk/umf-with-icx-build-0.11.0-dev2
-#     common: Bump UMF version to v0.11.0-dev2
-set(UNIFIED_RUNTIME_TAG 064d3560cbcc24a40380b57adb3690b2bc256d6e)
+
+set(UNIFIED_RUNTIME_TAG 6b925efe7fb17d5a202124b2af746994b36381cb)
