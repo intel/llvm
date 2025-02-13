53d7551bf46a306c06ecabc47b91f0a032b0f857
Garcia Orozco, David <david.garcia.orozco@intel.com>
2025-02-13 06:23:32 -0800

    Add more diagnostics

--- sycl/test-e2e/lit.cfg.py
+++ sycl/test-e2e/lit.cfg.py
@@ -322,7 +322,7 @@ if cl_options:
 config.substitutions.append(("%level_zero_options", level_zero_options))
 
 sp = subprocess.getstatusoutput(
-    config.dpcpp_compiler + " -fsycl  "
+    config.dpcpp_compiler + "-v -fsycl  "
     + os.path.join(config.sycl_obj_root, check_l0_file)
     + level_zero_options
 )
@@ -352,9 +352,10 @@ with open_check_file(check_preview_breaking_changes_file) as fp:
 
 sp = subprocess.getstatusoutput(
     config.dpcpp_compiler
-    + " -fsycl -fpreview-breaking-changes "
+    + "-v -fsycl -fpreview-breaking-changes "
     + os.path.join(config.sycl_obj_root, check_preview_breaking_changes_file)
 )
+print("PREVIEW BREAKING CHANGES:", sp[1])
 if sp[0] == 0:
     config.available_features.add("preview-breaking-changes-supported")
 
@@ -420,7 +421,7 @@ if cl_options:
 config.substitutions.append(("%cuda_options", cuda_options))
 
 sp = subprocess.getstatusoutput(
-    config.dpcpp_compiler + " -fsycl  " 
+    config.dpcpp_compiler + "-v -fsycl  "
     + os.path.join(config.sycl_obj_root, check_cuda_file)
     + cuda_options
 )
