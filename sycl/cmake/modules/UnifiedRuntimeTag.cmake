63b1e52acf74f25cdd97ad3bac3bbc3ec78b435f
Kenneth Benzie (Benie) <k.benzie@codeplay.com>
2025-02-13 16:15:07 +0000

    [UR] Bump main tag to 7e38d0ab

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 8b7a99578966eb691a961d9620ea38d235196b2f
-# Merge: ed095412 7b0e3b19
-# Author: Martin Grant <martin.morrisongrant@codeplay.com>
-# Date:   Mon Jan 20 09:27:22 2025 +0000
-#     Merge pull request #2582 from przemektmalon/przemek/intel-host-usm-support
-#     Enable Host USM backed images on Level Zero
-set(UNIFIED_RUNTIME_TAG review/yang/fix_asan_free)
+# commit 7e38d0abec315b828c45e43a3dc374a25fc2b552
+# Merge: 4ed3ba47 08504e89
+# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
+# Date:   Thu Feb 13 16:10:30 2025 +0000
+#     Merge pull request #2592 from AllanZyne/review/yang/fix_asan_free
+#     [DeviceASAN] Fix throw "UR_RESULT_ERROR_INVALID_ARGUMENT" exception when catching free related error
+set(UNIFIED_RUNTIME_TAG 7e38d0abec315b828c45e43a3dc374a25fc2b552)
