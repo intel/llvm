4e5ba0ce1ce24730b3bf839a7217c6e8c936f72a
Victor Lomuller <victor@codeplay.com>
2025-02-17 13:13:41 +0000

    address comments

--- clang/test/SemaSYCL/CudaCompat/delayed-diags.cu
+++ clang/test/SemaSYCL/CudaCompat/delayed-diags.cu
@@ -2,13 +2,18 @@
 // RUN:   -internal-isystem %S/../../SemaCUDA/Inputs \
 // RUN:   -internal-isystem %S/../Inputs \
 // RUN:   -fsycl-targets=nvptx64-nvidia-cuda -triple x86_64-unknown-linux \
-// RUN:   -emit-llvm -o - -verify -verify-ignore-unexpected=note
+// RUN:   -emit-llvm -o - -verify
 // RUN: %clang_cc1 %s -fsycl-is-device -fsycl-cuda-compatibility \
 // RUN:   -internal-isystem %S/../../SemaCUDA/Inputs \
 // RUN:   -internal-isystem %S/../Inputs \
 // RUN:   -fsycl-targets=nvptx64-nvidia-cuda -triple x86_64-unknown-linux\
-// RUN:   -emit-llvm -o - -verify -verify-ignore-unexpected=note
+// RUN:   -emit-llvm -o - -verify
 
+// Check that delayed diagnostics are done
+// according to the SYCL logic and not the CUDA one.
+// fsycl-cuda-compatibility doesn't enable the CUDA device mode,
+// this leaves the diagnostic logic for CUDA it is processing the host side
+// and triggers SYCL's delayed diagnostics.
 
 // expected-no-diagnostics
 
@@ -20,6 +25,7 @@ struct Foo {
 };
 
 Foo& get() {
+  // Checks no error is raised for non const static variable.
   static Foo f;
   return f;
 }
--- clang/test/SemaSYCL/CudaCompat/stub.debug.cpp
+++ clang/test/SemaSYCL/CudaCompat/stub.debug.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 %s -fsycl-is-host -aux-triple nvptx64-nvidia-cuda -debug-info-kind=constructor -dwarf-version=5 -fsycl-cuda-compatibility -emit-llvm -O3 -o %t
+// RUN: %clang_cc1 %s -fsycl-is-host -aux-triple nvptx64-nvidia-cuda -debug-info-kind=constructor -dwarf-version=5 -fsycl-cuda-compatibility -emit-llvm -o %t
 
 __attribute__((device)) void callee() {}
 
