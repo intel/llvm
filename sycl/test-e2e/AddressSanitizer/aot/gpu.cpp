fb40b3ae796a4b5b94f21a420827506350cd54f9
Wu, Yingcong <yingcong.wu@intel.com>
2025-02-18 09:38:16 +0100

    use flag

--- sycl/test-e2e/AddressSanitizer/aot/gpu.cpp
+++ sycl/test-e2e/AddressSanitizer/aot/gpu.cpp
@@ -1,14 +1,14 @@
 // REQUIRES: linux, ocloc, gpu && level_zero
 // REQUIRES: build-and-run-mode
 
-// RUN: %clangxx  %device_asan_flags -O0 -g  -fsycl -fsycl-targets=spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts %S/Inputs/host-usm-oob.cpp -o %t.out
+// RUN: %clangxx  %device_asan_aot_flags -O0 -g %S/Inputs/host-usm-oob.cpp -o %t.out
 // RUN: %{run} not %t.out 2>&1 | FileCheck %S/Inputs/host-usm-oob.cpp
 
-// RUN: %clangxx  %device_asan_flags -O1 -g  -fsycl -fsycl-targets=spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts %S/Inputs/host-usm-oob.cpp -o %t.out
+// RUN: %clangxx  %device_asan_aot_flags -O1 -g %S/Inputs/host-usm-oob.cpp -o %t.out
 // RUN: %{run} not %t.out 2>&1 | FileCheck %S/Inputs/host-usm-oob.cpp
 
-// RUN: %clangxx  %device_asan_flags -O2 -g  -fsycl -fsycl-targets=spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts %S/Inputs/host-usm-oob.cpp -o %t.out
+// RUN: %clangxx  %device_asan_aot_flags -O2 -g %S/Inputs/host-usm-oob.cpp -o %t.out
 // RUN: %{run} not %t.out 2>&1 | FileCheck %S/Inputs/host-usm-oob.cpp
 
-// RUN: %clangxx  %device_asan_flags -O3 -g  -fsycl -fsycl-targets=spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts %S/Inputs/host-usm-oob.cpp -o %t.out
+// RUN: %clangxx  %device_asan_aot_flags -O3 -g %S/Inputs/host-usm-oob.cpp -o %t.out
 // RUN: %{run} not %t.out 2>&1 | FileCheck %S/Inputs/host-usm-oob.cpp
