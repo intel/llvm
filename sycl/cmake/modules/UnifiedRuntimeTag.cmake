7aa7cbe346e4f1a4343ce68bc7daeaf87de13ea8
Zhao, Yang2 <yang2.zhao@intel.com>
2025-02-19 08:23:30 +0100

    revert ur tag

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,7 @@
-# commit 7e38d0abec315b828c45e43a3dc374a25fc2b552
-# Merge: 4ed3ba47 08504e89
+# commit d03f19a88e42cb98be9604ff24b61190d1e48727
+# Merge: 3ce6fcc9 84454b0e
 # Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Thu Feb 13 16:10:30 2025 +0000
-#     Merge pull request #2592 from AllanZyne/review/yang/fix_asan_free
-#     [DeviceASAN] Fix throw "UR_RESULT_ERROR_INVALID_ARGUMENT" exception when catching free related error
-set(UNIFIED_RUNTIME_TAG 7e38d0abec315b828c45e43a3dc374a25fc2b552)
+# Date:   Thu Feb 13 11:43:34 2025 +0000
+#     Merge pull request #2680 from ldorau/Set_UMF_CUDA_INCLUDE_DIR_to_not_fetch_cudart_from_gitlab
+#     Do not fetch cudart from gitlab for UMF
+set(UNIFIED_RUNTIME_TAG d03f19a88e42cb98be9604ff24b61190d1e48727)
