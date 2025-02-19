ff85f3639fc69a8ff00e7b2cbf73233b9ff84b31
Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>
2025-02-19 08:24:13 -0800

    enable 2 more containers
    Signed-off-by: Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>

--- sycl/unittests/program_manager/Cleanup.cpp
+++ sycl/unittests/program_manager/Cleanup.cpp
@@ -124,8 +124,13 @@ generateImage(const std::string& ImageId) {
 
   std::initializer_list<std::string> KernelNames{ generateRefName(ImageId, "Kernel"), generateRefName(ImageId, "__sycl_service_kernel__") };
   const std::vector<std::string> ExportedSymbols{generateRefName(ImageId, "Exported")};
-  const std::vector<std::string> &ImportedSymbols{generateRefName(ImageId, "Imported")};
+  const std::vector<std::string> ImportedSymbols{generateRefName(ImageId, "Imported")};
+  const std::vector<std::string> ImplicitLocalArg{KernelNames.begin()[0]};
   const std::string &VirtualFunctions{generateRefName(ImageId, "VF")};
+  std::vector<unsigned char> KernelEAM{0b0000001};
+  sycl::unittest::MockProperty EAMKernelPOI = sycl::unittest::makeKernelParamOptInfo(
+      KernelNames.begin()[0], 1, KernelEAM);
+  std::vector<sycl::unittest::MockProperty> ImgKPOI{std::move(EAMKernelPOI)};
 
     PropSet.insert(__SYCL_PROPERTY_SET_SYCL_EXPORTED_SYMBOLS,
                    createPropertySet(ExportedSymbols));
@@ -136,6 +141,10 @@ generateImage(const std::string& ImageId) {
     PropSet.insert(__SYCL_PROPERTY_SET_SYCL_VIRTUAL_FUNCTIONS,
                   createVFPropertySet(VirtualFunctions));
     setKernelUsesAssert(std::vector<std::string>{KernelNames.begin()[0]}, PropSet);
+
+    PropSet.insert(__SYCL_PROPERTY_SET_SYCL_IMPLICIT_LOCAL_ARG,
+                   createPropertySet(ImplicitLocalArg));
+      PropSet.insert(__SYCL_PROPERTY_SET_KERNEL_PARAM_OPT_INFO, std::move(ImgKPOI));
 
   std::vector<unsigned char> Bin{0};
 
@@ -190,10 +199,10 @@ void checkAllInvolvedContainers(ProgramManagerExposed& PM, size_t ExpectedCount,
 
    // EXPECT_EQ(PM.getVFSet2BinImage().size(), ExpectedCount) << Comment;
     EXPECT_EQ(PM.getNativePrograms().size(), 0u) << Comment;
-    EXPECT_EQ(PM.getEliminatedKernelArgMask().size(), 0u) << Comment;
+    EXPECT_EQ(PM.getEliminatedKernelArgMask().size(), ExpectedCount) << Comment;
 
     EXPECT_EQ(PM.getKernelUsesAssert().size(), ExpectedCount) << Comment;
-    EXPECT_EQ(PM.getKernelImplicitLocalArgPos().size(), 0u) << Comment;
+    EXPECT_EQ(PM.getKernelImplicitLocalArgPos().size(), ExpectedCount) << Comment;
 }
 
 TEST(ImageRemoval, Base) {
