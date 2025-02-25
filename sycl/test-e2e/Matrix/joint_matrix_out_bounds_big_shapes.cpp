53e7052f8a8b67ff6bc7d05bf1634f2ede1542e3
Plyakhin, Yury <yury.plyakhin@intel.com>
2025-02-24 16:38:24 -0800

    format

--- sycl/test-e2e/Matrix/joint_matrix_out_bounds_big_shapes.cpp
+++ sycl/test-e2e/Matrix/joint_matrix_out_bounds_big_shapes.cpp
@@ -1,4 +1,4 @@
-//==----joint_matrix_out_bounds_big_shapes.cpp  - DPC++ joint_matrix---------==//
+//==----joint_matrix_out_bounds_big_shapes.cpp - DPC++ joint_matrix---------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
