5ab196d70301511fca7d4baa53c4e5964a1fde5b
David Garcia Orozco <david.garcia.orozco@intel.com>
2025-03-04 07:21:38 -0700

    [SYCL][E2E] Clean-up `test-mode`s by removing workarounds (#17023)
    - Removes the `build-and-run-mode` feature from e2e lit infrastructure,
    as well as the remaining `REQUIRES: build-and-run-mode` markups found in
    tests.
    - Removes the `run-only` mode ignore line filtering workaround on
    `REQUIRES: build-and-run-mode`.
    - Adds `build-mode` to remove `test-mode-run-only` mode, this can be
    tested for by checking `run-mode && !build-mode`.
    - Marks CPU aot compilation lines with `%{run-aux}`, as those need to be
    compiled on a system with the same CPU isa as the running system.

--- sycl/test-e2e/DeviceCodeSplit/aot-cpu.cpp
+++ sycl/test-e2e/DeviceCodeSplit/aot-cpu.cpp
@@ -1,6 +1,6 @@
 // REQUIRES: opencl-aot, cpu
-// REQUIRES: build-and-run-mode
 
-// RUN: %clangxx -fsycl -fsycl-device-code-split=per_source -fsycl-targets=spir64_x86_64 -I %S/Inputs -o %t.out %S/split-per-source-main.cpp %S/Inputs/split-per-source-second-file.cpp \
-// RUN: -fsycl-dead-args-optimization
+// CPU AOT targets host isa, so we compile on the run system instead.
+// RUN: %{run-aux} %clangxx -fsycl -fsycl-device-code-split=per_source -fsycl-targets=spir64_x86_64 -I %S/Inputs -o %t.out \
+// RUN:   %S/split-per-source-main.cpp %S/Inputs/split-per-source-second-file.cpp -fsycl-dead-args-optimization
 // RUN: %{run} %t.out
