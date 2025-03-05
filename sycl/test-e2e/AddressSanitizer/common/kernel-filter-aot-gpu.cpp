c73e30a0eafd71a41a91a1e77c973a67c4607ac3
Wu, Yingcong <yingcong.wu@intel.com>
2025-03-05 16:45:07 +0800

    fix wrong flag

--- sycl/test-e2e/AddressSanitizer/common/kernel-filter-aot-gpu.cpp
+++ sycl/test-e2e/AddressSanitizer/common/kernel-filter-aot-gpu.cpp
@@ -1,5 +1,5 @@
 // REQUIRES: linux, ocloc, gpu && level_zero
 // REQUIRES: (arch-intel_gpu_pvc || gpu-intel-dg2)
 
-// RUN: %{run-aux} %{build} %device_asan_aot_flag %S/kernel-filter.cpp -O2 -fsanitize-ignorelist=%p/ignorelist.txt -o %t1
+// RUN: %{run-aux} %{build} %device_asan_aot_flags %S/kernel-filter.cpp -O2 -fsanitize-ignorelist=%p/ignorelist.txt -o %t1
 // RUN: %{run} %t1 2>&1 | FileCheck %S/kernel-filter.cpp
