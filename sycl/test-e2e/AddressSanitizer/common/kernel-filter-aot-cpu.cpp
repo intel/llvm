0eda25a326258d4605c93d961636925fa87f70ed
Wu, Yingcong <yingcong.wu@intel.com>
2025-03-05 16:07:41 +0800

    follow new usage

--- sycl/test-e2e/AddressSanitizer/common/kernel-filter-aot-cpu.cpp
+++ sycl/test-e2e/AddressSanitizer/common/kernel-filter-aot-cpu.cpp
@@ -1,5 +1,4 @@
 // REQUIRES: linux, opencl-aot, cpu
-// REQUIRES: build-and-run-mode
 
-// RUN: %{build} %device_asan_cpu_aot_flag %S/kernel-filter.cpp -O2 -fsanitize-ignorelist=%p/ignorelist.txt -o %t1
+// RUN: %{run-aux} %{build} %device_asan_aot_flags %S/kernel-filter.cpp -O2 -fsanitize-ignorelist=%p/ignorelist.txt -o %t1
 // RUN: %{run} %t1 2>&1 | FileCheck %S/kernel-filter.cpp
--- sycl/test-e2e/AddressSanitizer/common/kernel-filter-aot-gpu.cpp
+++ sycl/test-e2e/AddressSanitizer/common/kernel-filter-aot-gpu.cpp
@@ -1,6 +1,5 @@
 // REQUIRES: linux, ocloc, gpu && level_zero
-// REQUIRES: build-and-run-mode
 // REQUIRES: (arch-intel_gpu_pvc || gpu-intel-dg2)
 
-// RUN: %{build} %device_asan_gpu_aot_flag %S/kernel-filter.cpp -O2 -fsanitize-ignorelist=%p/ignorelist.txt -o %t1
+// RUN: %{run-aux} %{build} %device_asan_aot_flag %S/kernel-filter.cpp -O2 -fsanitize-ignorelist=%p/ignorelist.txt -o %t1
 // RUN: %{run} %t1 2>&1 | FileCheck %S/kernel-filter.cpp
