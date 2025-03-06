5cf627b8f8458c8b65fba9f69fb59c4455f63a09
Zhao, Yang2 <yang2.zhao@intel.com>
2025-03-06 08:28:56 +0100

    fix invalid test

--- sycl/test-e2e/AddressSanitizer/common/kernel-filter-aot-cpu.cpp
+++ sycl/test-e2e/AddressSanitizer/common/kernel-filter-aot-cpu.cpp
@@ -1,4 +1,4 @@
 // REQUIRES: linux, opencl-aot, cpu
 
-// RUN: %{run-aux} %{build} %device_asan_aot_flags %S/kernel-filter.cpp -O2 -fsanitize-ignorelist=%p/ignorelist.txt -o %t1
-// RUN: %{run} %t1 2>&1 | FileCheck %S/kernel-filter.cpp
+// RUN: %{run-aux} %{build} %device_asan_aot_flags %S/kernel-filter.cpp -g -O2 -fsanitize-ignorelist=%p/ignorelist.txt -o %t1
+// RUN: %{run} not %t1 2>&1 | FileCheck %S/kernel-filter.cpp
