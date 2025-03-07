43a3123d10ebb798cf58bfad8ad6d53844ce0e3c
Aaron Greig <aaron.greig@codeplay.com>
2025-03-07 12:26:56 +0000

    Add missing newline

--- unified-runtime/source/adapters/native_cpu/device.cpp
+++ unified-runtime/source/adapters/native_cpu/device.cpp
@@ -451,6 +451,7 @@ UR_APIEXPORT ur_result_t UR_APICALL urDeviceGetInfo(ur_device_handle_t hDevice,
 
   case UR_DEVICE_INFO_USM_CONTEXT_MEMCPY_SUPPORT_EXP:
     return ReturnValue(false);
+
   default:
     DIE_NO_IMPLEMENTATION;
   }
