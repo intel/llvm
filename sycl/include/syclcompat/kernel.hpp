a0e67814c6f6cf57f9f45e17073364fe146b3965
intwanghao <hao3.wang@intel.com>
2025-03-07 13:07:15 +0800

    fix
    Signed-off-by: intwanghao <hao3.wang@intel.com>

--- sycl/include/syclcompat/kernel.hpp
+++ sycl/include/syclcompat/kernel.hpp
@@ -54,6 +54,8 @@
 #include <sycl/nd_range.hpp>
 #include <sycl/queue.hpp>
 
+#include <syclcompat/util.hpp>
+
 namespace syclcompat {
 
 typedef void (*kernel_functor)(sycl::queue &, const sycl::nd_range<3> &,
