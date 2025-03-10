998d80f2ac1432f701ac7bbd0e22e140ac1fd02b
sumesh-s-mcw <sumesh.suresh@multicorewareinc.com>
2025-03-10 21:13:06 +0530

    Corrected subnormal checking logic issubnormal(V) ==> unsigned(abs(V) -1) (#3036)
    Corrected sunormal checking logic in is_fpclass intrinsic issubnormal(V)
    ==> unsigned(abs(V) - 1) < (all mantissa bits set)
    corrected the testfile to check the corrected logic
    
    Original commit:
    https://github.com/KhronosGroup/SPIRV-LLVM-Translator/commit/a3a498db77dc532

--- llvm-spirv/lib/SPIRV/SPIRVWriter.cpp
+++ llvm-spirv/lib/SPIRV/SPIRVWriter.cpp
@@ -5035,7 +5035,7 @@ SPIRVValue *LLVMToSPIRVBase::transIntrinsicInst(IntrinsicInst *II,
     const APInt Inf = APFloat::getInf(Semantics).bitcastToAPInt();
     const APInt AllOneMantissa =
         APFloat::getLargest(Semantics).bitcastToAPInt() & ~Inf;
-
+    const APInt OneValue = APInt(BitSize, 1);
     // Some checks can be inverted tests for simple cases, for example
     // simultaneous check for inf, normal, subnormal and zero is a check for
     // non nan.
@@ -5144,8 +5144,10 @@ SPIRVValue *LLVMToSPIRVBase::transIntrinsicInst(IntrinsicInst *II,
           BM->addUnaryInst(OpBitcast, OpSPIRVTy, InputFloat, BB);
       auto *MantissaConst = transValue(
           Constant::getIntegerValue(IntOpLLVMTy, AllOneMantissa), BB);
+      auto *ConstOne =
+          transValue(Constant::getIntegerValue(IntOpLLVMTy, OneValue), BB);
       auto *MinusOne =
-          BM->addBinaryInst(OpISub, OpSPIRVTy, BitCastToInt, MantissaConst, BB);
+          BM->addBinaryInst(OpISub, OpSPIRVTy, BitCastToInt, ConstOne, BB);
       auto *TestIsSubnormal =
           BM->addCmpInst(OpULessThan, ResTy, MinusOne, MantissaConst, BB);
       if (FPClass & fcPosSubnormal && FPClass & fcNegSubnormal)
