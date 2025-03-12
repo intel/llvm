433fb102d797393cff4242f8c36406f542e60084
Wu, Yingcong <yingcong.wu@intel.com>
2025-03-12 12:17:20 +0100

    save

--- unified-runtime/source/loader/layers/sanitizer/sanitizer_common/linux/backtrace.cpp
+++ unified-runtime/source/loader/layers/sanitizer/sanitizer_common/linux/backtrace.cpp
@@ -21,6 +21,13 @@ StackTrace GetCurrentBacktrace() {
   BacktraceFrame Frames[MAX_BACKTRACE_FRAMES];
   int FrameCount = backtrace(Frames, MAX_BACKTRACE_FRAMES);
 
+  // The Frames contain the return addresses, which is one instruction after the
+  // call instruction. Adjust the addresses so that symbolizer would give more
+  // precise result.
+  for (int I = 0; I < FrameCount; I++) {
+    (uintptr_t &)Frames[I] = (uintptr_t &)Frames[I] - 1;
+  }
+
   StackTrace Stack;
   Stack.stack =
       std::vector<BacktraceFrame>(&Frames[0], &Frames[FrameCount - 1]);
