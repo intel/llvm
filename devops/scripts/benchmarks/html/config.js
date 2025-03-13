a3edf7aff115c3ebb64c90afe042a177ad4ea2c4
Li, Ian <ian.li@intel.com>
2025-03-13 12:47:26 -0700

    Add benchmarking results to sycl-docs.yml

--- devops/scripts/benchmarks/html/config.js
+++ devops/scripts/benchmarks/html/config.js
@@ -1,2 +1,2 @@
-//remoteDataUrl = 'https://example.com/data.json';
+remoteDataUrl = 'https://raw.githubusercontent.com/intel/llvm-ci-perf-results/refs/heads/unify-ci/UR_DNP_INTEL_06_03/data.json';
 //defaultCompareNames = ['baseline'];
