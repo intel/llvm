db6d7d3585e9626971740f597344dd86ff076dd8
jinge90 <ge.jin@intel.com>
2025-03-17 15:32:38 +0800

    regenerate ur enum for bf16 convert
    Signed-off-by: jinge90 <ge.jin@intel.com>

--- unified-runtime/include/ur_print.hpp
+++ unified-runtime/include/ur_print.hpp
@@ -2885,9 +2885,6 @@ inline std::ostream &operator<<(std::ostream &os, enum ur_device_info_t value) {
   case UR_DEVICE_INFO_KERNEL_SET_SPECIALIZATION_CONSTANTS:
     os << "UR_DEVICE_INFO_KERNEL_SET_SPECIALIZATION_CONSTANTS";
     break;
-  case UR_DEVICE_INFO_BFLOAT16_CONVERSIONS_INTEL:
-    os << "UR_DEVICE_INFO_BFLOAT16_CONVERSIONS_INTEL";
-    break;
   case UR_DEVICE_INFO_MEMORY_BUS_WIDTH:
     os << "UR_DEVICE_INFO_MEMORY_BUS_WIDTH";
     break;
@@ -2935,6 +2932,9 @@ inline std::ostream &operator<<(std::ostream &os, enum ur_device_info_t value) {
     break;
   case UR_DEVICE_INFO_USE_NATIVE_ASSERT:
     os << "UR_DEVICE_INFO_USE_NATIVE_ASSERT";
+    break;
+  case UR_DEVICE_INFO_BFLOAT16_CONVERSIONS_INTEL:
+    os << "UR_DEVICE_INFO_BFLOAT16_CONVERSIONS_INTEL";
     break;
   case UR_DEVICE_INFO_COMMAND_BUFFER_SUPPORT_EXP:
     os << "UR_DEVICE_INFO_COMMAND_BUFFER_SUPPORT_EXP";
@@ -4411,19 +4411,6 @@ inline ur_result_t printTagged(std::ostream &os, const void *ptr,
 
     os << ")";
   } break;
-  case UR_DEVICE_INFO_BFLOAT16_CONVERSIONS_INTEL: {
-    const ur_bool_t *tptr = (const ur_bool_t *)ptr;
-    if (sizeof(ur_bool_t) > size) {
-      os << "invalid size (is: " << size
-         << ", expected: >=" << sizeof(ur_bool_t) << ")";
-      return UR_RESULT_ERROR_INVALID_SIZE;
-    }
-    os << (const void *)(tptr) << " (";
-
-    os << *tptr;
-
-    os << ")";
-  } break;
   case UR_DEVICE_INFO_MEMORY_BUS_WIDTH: {
     const uint32_t *tptr = (const uint32_t *)ptr;
     if (sizeof(uint32_t) > size) {
@@ -4622,6 +4609,19 @@ inline ur_result_t printTagged(std::ostream &os, const void *ptr,
     os << ")";
   } break;
   case UR_DEVICE_INFO_USE_NATIVE_ASSERT: {
+    const ur_bool_t *tptr = (const ur_bool_t *)ptr;
+    if (sizeof(ur_bool_t) > size) {
+      os << "invalid size (is: " << size
+         << ", expected: >=" << sizeof(ur_bool_t) << ")";
+      return UR_RESULT_ERROR_INVALID_SIZE;
+    }
+    os << (const void *)(tptr) << " (";
+
+    os << *tptr;
+
+    os << ")";
+  } break;
+  case UR_DEVICE_INFO_BFLOAT16_CONVERSIONS_INTEL: {
     const ur_bool_t *tptr = (const ur_bool_t *)ptr;
     if (sizeof(ur_bool_t) > size) {
       os << "invalid size (is: " << size
