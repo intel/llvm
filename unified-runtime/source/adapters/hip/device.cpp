27c9cb6b7f1e9efd5eecd6b751d7fc769efdb9b2
Aaron Greig <aaron.greig@codeplay.com>
2025-03-17 14:58:06 +0000

    actually fix hip this time

--- unified-runtime/source/adapters/hip/device.cpp
+++ unified-runtime/source/adapters/hip/device.cpp
@@ -1070,8 +1070,6 @@ UR_APIEXPORT ur_result_t UR_APICALL urDeviceGetInfo(ur_device_handle_t hDevice,
   }
   case UR_DEVICE_INFO_COMMAND_BUFFER_EVENT_SUPPORT_EXP:
     return ReturnValue(false);
-  case UR_DEVICE_INFO_LOW_POWER_EVENTS_EXP:
-    return ReturnValue(false);
   case UR_DEVICE_INFO_USM_CONTEXT_MEMCPY_SUPPORT_EXP:
     return ReturnValue(false);
   case UR_DEVICE_INFO_COMMAND_BUFFER_SUBGRAPH_SUPPORT_EXP:
