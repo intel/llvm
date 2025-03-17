e0340230dc0af930321a76816db85b188e7926ac
Aaron Greig <aaron.greig@codeplay.com>
2025-03-17 14:33:18 +0000

    Fix bad merge.

--- unified-runtime/source/adapters/hip/device.cpp
+++ unified-runtime/source/adapters/hip/device.cpp
@@ -1076,7 +1076,7 @@ UR_APIEXPORT ur_result_t UR_APICALL urDeviceGetInfo(ur_device_handle_t hDevice,
     return ReturnValue(false);
   case UR_DEVICE_INFO_COMMAND_BUFFER_SUBGRAPH_SUPPORT_EXP:
     return ReturnValue(true);
-  case UR_DEVICE_INFO_LOW_POWER_EVENTS_EXP: {
+  case UR_DEVICE_INFO_LOW_POWER_EVENTS_EXP:
     return ReturnValue(false);
   case UR_DEVICE_INFO_USE_NATIVE_ASSERT:
     return ReturnValue(true);
