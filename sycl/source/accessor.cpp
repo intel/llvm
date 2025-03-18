intel/llvm

commit 78613dc04cfe766998306dc25ad2401fbdf8a95c

[SYCL] Reduce heap allocations from `sycl::local_accessor` (#17147)
Ideally, there should be zero but we can't fix that without ABI break
(both present, and potential future one for unanticipated future
changes).

Reduce what we can now:
- don't allocate memory on host (no host device, APIs accessing memory
must be called from inside "command")
- Use `std::make_shared` to fold two allocation into one for pImpl idiom

sycl/source/accessor.cpp
@@ -56,10 +56,10 @@ AccessorBaseHost::AccessorBaseHost(id<3> Offset, range<3> AccessRange,
                                    bool IsSubBuffer,
                                    const property_list &PropertyList) {
   verifyAccessorProps(PropertyList);
-  impl = std::shared_ptr<AccessorImplHost>(
-      new AccessorImplHost(Offset, AccessRange, MemoryRange, AccessMode,
-                           (detail::SYCLMemObjI *)SYCLMemObject, Dims, ElemSize,
-                           false, OffsetInBytes, IsSubBuffer, PropertyList));
+  impl = std::make_shared<AccessorImplHost>(
+      Offset, AccessRange, MemoryRange, AccessMode,
+      (detail::SYCLMemObjI *)SYCLMemObject, Dims, ElemSize, false,
+      OffsetInBytes, IsSubBuffer, PropertyList);
 }
 
 AccessorBaseHost::AccessorBaseHost(id<3> Offset, range<3> AccessRange,
@@ -119,19 +119,14 @@ const sycl::range<3> &LocalAccessorBaseHost::getSize() const {
   return impl->MSize;
 }
 void *LocalAccessorBaseHost::getPtr() {
-  // Const cast this in order to call the const getPtr.
-  return const_cast<const LocalAccessorBaseHost *>(this)->getPtr();
+  // Must not be/isn't called, user-facing APIs do error-checking.
+  std::abort();
+  return nullptr;
 }
 void *LocalAccessorBaseHost::getPtr() const {
-  char *ptr = impl->MMem.data();
-
-  // Align the pointer to MElemSize.
-  size_t val = reinterpret_cast<size_t>(ptr);
-  if (val % impl->MElemSize != 0) {
-    ptr += impl->MElemSize - val % impl->MElemSize;
-  }
-
-  return ptr;
+  // Must not be/isn't called, user-facing APIs do error-checking.
+  std::abort();
+  return nullptr;
 }
 const property_list &LocalAccessorBaseHost::getPropList() const {
   return impl->MPropertyList;

sycl/include/sycl/accessor.hpp
@@ -2224,8 +2224,6 @@ protected:
     const auto *this_const = this;
     (void)getSize();
     (void)this_const->getSize();
-    (void)getPtr();
-    (void)this_const->getPtr();
 #endif
   }
 

sycl/source/detail/accessor_impl.hpp
@@ -134,13 +134,11 @@ public:
   LocalAccessorImplHost(sycl::range<3> Size, int Dims, int ElemSize,
                         const property_list &PropertyList)
       : MSize(Size), MDims(Dims), MElemSize(ElemSize),
-        MMem(Size[0] * Size[1] * Size[2] * ElemSize + ElemSize),
         MPropertyList(PropertyList) {}
 
   sycl::range<3> MSize;
   int MDims;
   int MElemSize;
-  std::vector<char> MMem;
   property_list MPropertyList;
 };
 

commit 536596b77979aaa0b31b01989829c9f489e1b14a

[SYCL] Restore ABI-combatibility for `local_accessor` ctor
Initially broken in https://github.com/intel/llvm/pull/17147.

sycl/source/accessor.cpp
@@ -119,13 +119,25 @@ const sycl::range<3> &LocalAccessorBaseHost::getSize() const {
   return impl->MSize;
 }
 void *LocalAccessorBaseHost::getPtr() {
+  // `local_accessor_base::GDBMethodsAnchor` was/is inline and used to call
+  // `(void)getPtr()` inside. As such, binaries compiled with older toolchain
+  // are calling this method from the `sycl::local_accessor` ctor on host and we
+  // cannot "abort" for them.
+#ifdef __INTEL_PREVIEW_BREAKING_CHANGES
   // Must not be/isn't called, user-facing APIs do error-checking.
   std::abort();
+#endif
   return nullptr;
 }
 void *LocalAccessorBaseHost::getPtr() const {
+  // `local_accessor_base::GDBMethodsAnchor` was/is inline and used to call
+  // `(void)getPtr()` inside. As such, binaries compiled with older toolchain
+  // are calling this method from the `sycl::local_accessor` ctor on host and we
+  // cannot "abort" for them.
+#ifdef __INTEL_PREVIEW_BREAKING_CHANGES
   // Must not be/isn't called, user-facing APIs do error-checking.
   std::abort();
+#endif
   return nullptr;
 }
 const property_list &LocalAccessorBaseHost::getPropList() const {