90c2f8b4daceffdb9952f22831f712b615ca5573
Vladislav Dzhidzhoev <vdzhidzhoev@accesssoftek.com>
2025-03-18 14:00:34 +0100

    [llvm-objdump] Pass MCSubtargetInfo to findPltEntries (NFC) (#131773)
    It allows access to subtarget features, collected in llvm-objdump.cpp,
    from findPltEntries, which will be used in
    https://github.com/llvm/llvm-project/pull/130764.

--- llvm/lib/Object/ELFObjectFile.cpp
+++ llvm/lib/Object/ELFObjectFile.cpp
@@ -782,7 +782,8 @@ void ELFObjectFileBase::setARMSubArch(Triple &TheTriple) const {
   TheTriple.setArchName(Triple);
 }
 
-std::vector<ELFPltEntry> ELFObjectFileBase::getPltEntries() const {
+std::vector<ELFPltEntry>
+ELFObjectFileBase::getPltEntries(const MCSubtargetInfo &STI) const {
   std::string Err;
   const auto Triple = makeTriple();
   const auto *T = TargetRegistry::lookupTarget(Triple.str(), Err);
@@ -840,7 +841,7 @@ std::vector<ELFPltEntry> ELFObjectFileBase::getPltEntries() const {
       llvm::append_range(
           PltEntries,
           MIA->findPltEntries(Section.getAddress(),
-                              arrayRefFromStringRef(*PltContents), Triple));
+                              arrayRefFromStringRef(*PltContents), STI));
     }
   }
 
