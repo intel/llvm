37e9bbf0f5d4e01b3050e33284944dde6a856be4
Nick Sarnie <nick.sarnie@intel.com>
2025-03-18 16:47:55 +0000

    Update sycl/test-e2e/Matrix/SG32/joint_matrix_out_bounds.cpp
    Co-authored-by: Yury Plyakhin <yury.plyakhin@intel.com>

--- sycl/test-e2e/Matrix/SG32/joint_matrix_out_bounds.cpp
+++ sycl/test-e2e/Matrix/SG32/joint_matrix_out_bounds.cpp
@@ -18,7 +18,7 @@
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 
-// XFAIL: !(linux && arch-intel_gpu_bmg_g21)
+// XFAIL: gpu && !(linux && arch-intel_gpu_bmg_g21)
 // XFAIL-TRACKER: GSD-4181
 
 #include "common.hpp"
