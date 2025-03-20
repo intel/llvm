8e553afe9b08369fb024fb183eea36b51128fac4
Martin Morrison-Grant <martin.morrisongrant@codeplay.com>
2025-03-20 10:47:23 +0000

    update adapters emitting warnings to use logger instead of setErrorMessage and link up a logger callback from SYCL RT to simply print to stdout, this is setup during adapter initialisation in sycl/source/detail.cpp
    also remove setErrorMessage from OpenCL and Native CPU adapters where there was no usage

--- unified-runtime/source/adapters/native_cpu/common.cpp
+++ unified-runtime/source/adapters/native_cpu/common.cpp
@@ -15,21 +15,6 @@
 thread_local ur_result_t ErrorMessageCode = UR_RESULT_SUCCESS;
 thread_local char ErrorMessage[MaxMessageSize]{};
 
-// Utility function for setting a message and warning
-[[maybe_unused]] void setErrorMessage(const char *pMessage,
-                                      ur_result_t ErrorCode) {
-  assert(strlen(pMessage) < MaxMessageSize);
-  // Copy at most MaxMessageSize - 1 bytes to ensure the resultant string is
-  // always null terminated.
-  strncpy(ErrorMessage, pMessage, MaxMessageSize - 1);
-  ErrorMessageCode = ErrorCode;
-}
-
-ur_result_t urGetLastResult(ur_platform_handle_t, const char **ppMessage) {
-  *ppMessage = &ErrorMessage[0];
-  return ErrorMessageCode;
-}
-
 void detail::ur::die(const char *pMessage) {
   logger::always("ur_die: {}", pMessage);
   std::terminate();
