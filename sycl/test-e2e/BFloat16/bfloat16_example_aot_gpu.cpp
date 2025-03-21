a070d08d7b2b7652fcbc59107558141fa84e7672
Sarnie, Nick <nick.sarnie@intel.com>
2025-03-21 10:49:53 -0700

    [SYCL][E2E] Enable XPASSing bfloat16 AOT test
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- sycl/test-e2e/BFloat16/bfloat16_example_aot_gpu.cpp
+++ sycl/test-e2e/BFloat16/bfloat16_example_aot_gpu.cpp
@@ -5,7 +5,7 @@
 
 // REQUIRES: opencl-aot, ocloc, gpu-intel-gen12, any-device-is-gpu
 
-// XFAIL: linux
+// XFAIL: !gpu-intel-gen12
 // XFAIL-TRACKER: https://github.com/intel/llvm/issues/17305
 
 // RUN: %clangxx -fsycl -fsycl-targets=spir64_gen -Xsycl-target-backend "-device gen12lp" %s -o %t.out
