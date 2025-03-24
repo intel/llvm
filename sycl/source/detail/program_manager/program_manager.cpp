de9839e70034e6e55bf6ffd89ac117397c7959e3
Ptak, Slawomir <slawomir.ptak@intel.com>
2025-03-24 11:08:00 +0000

    Use platform local var instead of a temp object

--- sycl/source/detail/program_manager/program_manager.cpp
+++ sycl/source/detail/program_manager/program_manager.cpp
@@ -369,7 +369,8 @@ static void appendCompileOptionsFromImage(std::string &CompileOpts,
 
   appendCompileOptionsForGRFSizeProperties(CompileOpts, Img, isEsimdImage);
 
-  const auto &PlatformImpl = detail::getSyclObjImpl(Devs[0].get_platform());
+  platform Platform = Devs[0].get_platform();
+  const auto &PlatformImpl = detail::getSyclObjImpl(Platform);
 
   // Add optimization flags.
   auto str = getUint32PropAsOptStr(Img, "optLevel");
