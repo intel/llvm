76be4d820122dbd90b6f8286bf4dd3e13370cfc8
Winston Zhang <winston.zhang@intel.com>
2025-03-24 14:51:52 -0700

    Apply suggestions from code review
    Co-authored-by: Peter Žužek <peterzuzek@gmail.com>

--- unified-runtime/source/adapters/level_zero/device.cpp
+++ unified-runtime/source/adapters/level_zero/device.cpp
@@ -1115,7 +1115,7 @@ ur_result_t urDeviceGetInfo(
     return ReturnValue(false);
   }
   case UR_DEVICE_INFO_EXTERNAL_MEMORY_IMPORT_SUPPORT_EXP: {
-    // L0 does not support importing external memory.
+    // L0 supports importing external memory.
     return ReturnValue(true);
   }
   case UR_DEVICE_INFO_EXTERNAL_SEMAPHORE_IMPORT_SUPPORT_EXP: {
