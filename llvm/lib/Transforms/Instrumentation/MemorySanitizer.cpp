6c21a0025b15c2dc188b3c2431b53bc592193904
Zhao, Yang2 <yang2.zhao@intel.com>
2025-03-25 09:26:49 +0100

    add test

--- llvm/lib/Transforms/Instrumentation/MemorySanitizer.cpp
+++ llvm/lib/Transforms/Instrumentation/MemorySanitizer.cpp
@@ -5921,7 +5921,7 @@ struct MemorySanitizerVisitor : public InstVisitor<MemorySanitizerVisitor> {
 
     // The caller may still expect information passed over TLS if we pass our
     // check
-    if (StoreShadow) {
+    if (StoreShadow && !SpirOrSpirv) {
       IRB.CreateAlignedStore(Shadow, ShadowPtr, kShadowTLSAlignment);
       if (MS.TrackOrigins && StoreOrigin)
         IRB.CreateStore(getOrigin(RetVal), getOriginPtrForRetval());
