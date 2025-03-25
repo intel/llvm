4612758aa404fe03be9a5fa494e4630d82ce3576
Ptak, Slawomir <slawomir.ptak@intel.com>
2025-03-25 14:17:16 +0000

    Remove unused graph_impl function.

--- sycl/source/detail/graph_impl.hpp
+++ sycl/source/detail/graph_impl.hpp
@@ -919,10 +919,6 @@ public:
   /// @return Device associated with graph.
   sycl::device getDevice() const { return MDevice; }
 
-  const DeviceImplPtr &getDeviceImplPtr() const {
-    return getSyclObjImpl(MDevice);
-  }
-
   /// List of root nodes.
   std::set<std::weak_ptr<node_impl>, std::owner_less<std::weak_ptr<node_impl>>>
       MRoots;
