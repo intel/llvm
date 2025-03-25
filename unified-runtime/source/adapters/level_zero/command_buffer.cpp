998843a0f9a89db633abe47b681d50c2cc8c6249
Zhang, Winston <winston.zhang@intel.com>
2025-03-25 22:25:35 +0000

    Enable command buffer in order default
    Signed-off-by: Zhang, Winston <winston.zhang@intel.com>

--- unified-runtime/source/adapters/level_zero/command_buffer.cpp
+++ unified-runtime/source/adapters/level_zero/command_buffer.cpp
@@ -76,10 +76,7 @@ bool checkCounterBasedEventsSupport(ur_device_handle_t Device) {
     return std::atoi(UrRet) != 0;
   }();
 
-  return Device->ImmCommandListUsed &&
-         Device->Platform->allowDriverInOrderLists(
-             true /*Only Allow Driver In Order List if requested*/) &&
-         useDriverCounterBasedEvents &&
+  return Device->ImmCommandListUsed && useDriverCounterBasedEvents &&
          Device->Platform->ZeDriverEventPoolCountingEventsExtensionFound;
 }
 
@@ -649,9 +646,12 @@ ur_result_t createMainCommandList(ur_context_handle_t Context,
  */
 bool canBeInOrder(ur_context_handle_t Context,
                   const ur_exp_command_buffer_desc_t *CommandBufferDesc) {
-  bool CanUseDriverInOrderLists =
-      Context->getPlatform()->allowDriverInOrderLists(
-          true /*Only Allow Driver In Order List if requested*/);
+  const char *UrRet = std::getenv("UR_L0_USE_DRIVER_INORDER_LISTS");
+  // In-order command-lists are not available in old driver version.
+  bool DriverInOrderRequested = UrRet ? std::atoi(UrRet) != 0 : false;
+  bool CompatibleDriver = Context->getPlatform()->isDriverVersionNewerOrSimilar(
+      1, 3, L0_DRIVER_INORDER_MIN_VERSION);
+  bool CanUseDriverInOrderLists = CompatibleDriver && DriverInOrderRequested;
   return CanUseDriverInOrderLists ? CommandBufferDesc->isInOrder : false;
 }
 
