3100ac24eecab39386ea2267c3cb700510415712
Kornev, Nikita <nikita.kornev@intel.com>
2025-03-25 12:58:36 +0100

    also disable amdgcn test on win

--- sycl/test-e2e/OptionalKernelFeatures/is_compatible/is_compatible_amdgcn.cpp
+++ sycl/test-e2e/OptionalKernelFeatures/is_compatible/is_compatible_amdgcn.cpp
@@ -1,3 +1,7 @@
+// UNSUPPORTED: windows
+// UNSUPPORTED-TRACKER: https://github.com/intel/llvm/issues/17515
+// There are no ROCm libs on win machines, so the compilation fails.
+
 // RUN: %clangxx -fsycl -Xsycl-target-backend=amdgcn-amd-amdhsa --offload-arch=gfx906 -fsycl-targets=amdgcn-amd-amdhsa %S/Inputs/is_compatible_with_env.cpp -o %t.out
 
 // RUN: %if !hip %{ not %} %{run} %t.out
