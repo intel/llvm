250815d792b605cfac45d36a13ea0c737543af23
Kornev, Nikita <nikita.kornev@intel.com>
2025-03-26 11:05:15 +0100

    fix amd test

--- sycl/test-e2e/OptionalKernelFeatures/is_compatible/is_compatible_amdgcn.cpp
+++ sycl/test-e2e/OptionalKernelFeatures/is_compatible/is_compatible_amdgcn.cpp
@@ -2,6 +2,6 @@
 // UNSUPPORTED-TRACKER: https://github.com/intel/llvm/issues/17515
 // There are no ROCm libs on win machines, so the compilation fails.
 
-// RUN: %clangxx -fsycl -Xsycl-target-backend=amdgcn-amd-amdhsa --offload-arch=gfx906 -fsycl-targets=amdgcn-amd-amdhsa %S/Inputs/is_compatible_with_env.cpp -o %t.out
+// RUN: %clangxx -fsycl -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend=amdgcn-amd-amdhsa --offload-arch=gfx1030 %S/Inputs/is_compatible_with_env.cpp -o %t.out
 
 // RUN: %if !hip %{ not %} %{run} %t.out
