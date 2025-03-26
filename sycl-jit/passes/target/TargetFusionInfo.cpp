13010bedb081999e40369d665b9e7451843b8049
Jakub Chlanda <jakub@codeplay.com>
2025-03-26 09:31:06 +0000

    PR feedback

--- sycl-jit/passes/target/TargetFusionInfo.cpp
+++ sycl-jit/passes/target/TargetFusionInfo.cpp
@@ -458,7 +458,7 @@ public:
     SmallPtrSet<Constant *, 8> DeletedFuncs{Funcs.begin(), Funcs.end()};
     SmallVector<MDNode *> ValidKernels;
     auto *OldAnnotations = LLVMMod->getNamedMetadata(MDName);
-    assert(OldAnnotations && "Failed to retreive old annotations");
+    assert(OldAnnotations && "Failed to retrieve old annotations");
     for (auto *Op : OldAnnotations->operands()) {
       if (auto *TOp = dyn_cast<MDTuple>(Op)) {
         if (auto *COp = dyn_cast_if_present<ConstantAsMetadata>(
