df2fcb2076ed3baf2bce22837e88cdd5d61a0bae
Klochkov, Denis <denis.klochkov@intel.com>
2025-03-27 14:38:18 +0100

    [SYCL] update formatting

--- sycl/test/basic_tests/logical_operations_void_types.cpp
+++ sycl/test/basic_tests/logical_operations_void_types.cpp
@@ -6,11 +6,14 @@
 #include <sycl/functional.hpp>
 #include <type_traits>
 
-
 const auto logicalAndVoid = sycl::logical_and<void>();
 const auto logicalOrVoid = sycl::logical_or<void>();
 
 // expected-error@+1 {{}}
-static_assert(std::is_same_v<decltype(logicalAndVoid(static_cast<void>(1), static_cast<void>(2))), bool> == true);
+static_assert(std::is_same_v<decltype(logicalAndVoid(static_cast<void>(1),
+                                                     static_cast<void>(2))),
+                             bool> == true);
 // expected-error@+1 {{}}
-static_assert(std::is_same_v<decltype(logicalOrVoid(static_cast<void>(1), static_cast<void>(2))), bool> == true);
+static_assert(std::is_same_v<decltype(logicalOrVoid(static_cast<void>(1),
+                                                    static_cast<void>(2))),
+                             bool> == true);
