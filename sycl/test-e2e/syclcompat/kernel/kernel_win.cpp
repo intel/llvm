ee4da028255f90d8bb60a5c6dd3ec5d188a4eff9
Garcia Orozco, David <david.garcia.orozco@intel.com>
2025-03-31 07:42:36 -0700

    Use run-aux instead of xfailing

--- sycl/test-e2e/syclcompat/kernel/kernel_win.cpp
+++ sycl/test-e2e/syclcompat/kernel/kernel_win.cpp
@@ -1,10 +1,10 @@
 // REQUIRES: windows
 
-// UNSUPPORTED: gpu-intel-gen12 && run-mode && !build-mode
-// UNSUPPORTED-TRACKER: https://github.com/intel/llvm/issues/17561
-
 // DEFINE: %{sharedflag} = %if cl_options %{/clang:-shared%} %else %{-shared%}
 
-// RUN: %clangxx %{sharedflag} -fsycl %{sycl_target_opts} %S\Inputs\kernel_module.cpp -o %t.dll
-// RUN: %clangxx -DTEST_SHARED_LIB='"%/t.dll"' -fsycl %{sycl_target_opts} %S\Inputs\kernel_function.cpp -o %t.out
+// This test is sensitive to the absolute path of the dll file produced, so we
+// run the test completely on the run system to avoid issues.
+
+// RUN: %{run-aux} %clangxx %{sharedflag} -fsycl %{sycl_target_opts} %S\Inputs\kernel_module.cpp -o %t.dll
+// RUN: %{run-aux} %clangxx -DTEST_SHARED_LIB='"%/t.dll"' -fsycl %{sycl_target_opts} %S\Inputs\kernel_function.cpp -o %t.out
 // RUN: %{run} %t.out
