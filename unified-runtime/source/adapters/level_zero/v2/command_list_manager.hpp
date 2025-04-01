d1388fcf76c69f8080b46b9374ed7954cd79472e
Igor Chorazewicz <igor.chorazewicz@intel.com>
2025-04-01 18:45:45 +0000

    [SYCL][UR][L0 v2] fix urQueueCreateWithNativeHandle
    getSignalEvent in ur_command_list_manager returned nullptr
    when queue was not set. This was the case when initializing
    the command list manager in queue ctor called from
    urQueueCreateWithNativeHandle.
    
    Fix this by passing queue to the command list manager and
    removing the default nullptr value from the ctor (to avoid
    this issue in future).
    
    Also, event if queue is nullptr, return the event instead
    of nullptr from getSignalEvent. This event is mostly functional,
    it just cannot be used for profiling.

--- unified-runtime/source/adapters/level_zero/v2/command_list_manager.hpp
+++ unified-runtime/source/adapters/level_zero/v2/command_list_manager.hpp
@@ -36,8 +36,7 @@ struct ur_command_list_manager {
   ur_command_list_manager(ur_context_handle_t context,
                           ur_device_handle_t device,
                           v2::raii::command_list_unique_handle &&commandList,
-                          v2::event_flags_t flags = v2::EVENT_FLAGS_COUNTER,
-                          ur_queue_t_ *queue = nullptr);
+                          v2::event_flags_t flags, ur_queue_t_ *queue);
   ur_command_list_manager(ur_command_list_manager &&src) = default;
   ~ur_command_list_manager();
 
