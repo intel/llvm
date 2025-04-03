e1b5fa110d3926e3848d9cf439962f698d3c3920
Chris Perkins <chris.perkins@intel.com>
2025-04-02 18:32:48 -0700

    if the EnqueueStatus isn't reset, the Command/Requirement cannot be re-enqueued. But many errors/exceptions are addressable and operations should be able to resume after.  The fix here is straightforward. We also improve the FailedDependency unit test.

--- sycl/source/detail/scheduler/graph_processor.cpp
+++ sycl/source/detail/scheduler/graph_processor.cpp
@@ -84,6 +84,10 @@ bool Scheduler::GraphProcessor::enqueueCommand(
     return false;
   }
 
+  // Reset enqueue status if reattempting
+  if (Cmd->MEnqueueStatus == EnqueueResultT::SyclEnqueueFailed)
+    Cmd->MEnqueueStatus = EnqueueResultT::SyclEnqueueReady;
+
   // Recursively enqueue all the implicit + explicit backend level dependencies
   // first and exit immediately if any of the commands cannot be enqueued.
   for (const EventImplPtr &Event : Cmd->getPreparedDepsEvents()) {
