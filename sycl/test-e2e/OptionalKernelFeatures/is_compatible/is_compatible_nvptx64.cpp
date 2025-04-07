e328c6310c1bb8e24fff60c47a196916b87511a3
Kornev, Nikita <nikita.kornev@intel.com>
2025-04-07 15:49:49 +0200

    restore unsupported

--- sycl/test-e2e/OptionalKernelFeatures/is_compatible/is_compatible_nvptx64.cpp
+++ sycl/test-e2e/OptionalKernelFeatures/is_compatible/is_compatible_nvptx64.cpp
@@ -1,5 +1,9 @@
 // REQUIRES: cuda_dev_kit
 
+// UNSUPPORTED: windows
+// UNSUPPORTED-TRACKER: https://github.com/intel/llvm/issues/17515
+// CUDA libs are not installed correctly.
+
 // RUN: %clangxx -fsycl -fsycl-targets=nvptx64-nvidia-cuda %S/Inputs/is_compatible_with_env.cpp -o %t.out
 
 // RUN: %if !cuda %{ not %} %{run} %t.out
