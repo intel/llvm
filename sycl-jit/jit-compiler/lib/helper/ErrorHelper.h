2672b1e7546637379342f92e84a726c0297bb00b
Julian Oppermann <julian.oppermann@codeplay.com>
2025-04-08 20:16:58 +0200

    Add license headers
    Signed-off-by: Julian Oppermann <julian.oppermann@codeplay.com>

--- sycl-jit/jit-compiler/lib/helper/ErrorHelper.h
+++ sycl-jit/jit-compiler/lib/helper/ErrorHelper.h
@@ -1,3 +1,11 @@
+//==--- ErrorHelper.h - Helper to wrap LLVM errors in result objects -------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #pragma once
 
 #include <llvm/Support/Error.h>
