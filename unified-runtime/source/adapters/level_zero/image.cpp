cff04e687c8704f57e1444882ffa5b3537d1bc97
Zhang, Winston <winston.zhang@intel.com>
2025-04-07 21:08:34 -0700

    With changes and pitched/unpitched usm to unpitched/pitched usm copy cases
    Signed-off-by: Zhang, Winston <winston.zhang@intel.com>

--- unified-runtime/source/adapters/level_zero/image.cpp
+++ unified-runtime/source/adapters/level_zero/image.cpp
@@ -538,7 +538,6 @@ ur_result_t urBindlessImagesImageCopyExp(
                   WaitList.Length, WaitList.ZeEventList));
     }
   } else if (imageCopyFlags == UR_EXP_IMAGE_COPY_FLAG_DEVICE_TO_DEVICE) {
-    /*
     if (pSrcImageDesc->rowPitch != 0 && pDstImageDesc->rowPitch != 0) {
       // Copy from pitched USM memory to pitched USM memory
       uint32_t SrcRowPitch = pSrcImageDesc->rowPitch;
@@ -561,7 +560,6 @@ ur_result_t urBindlessImagesImageCopyExp(
                  (ZeCommandList, pDst, &ZeDstRegion, DstRowPitch, DstSlicePitch,
                   pSrc, &ZeSrcRegion, SrcRowPitch, SrcSlicePitch, ZeEvent,
                   WaitList.Length, WaitList.ZeEventList));
-
     } else if (pSrcImageDesc->rowPitch == 0 && pDstImageDesc->rowPitch != 0) {
       // Handle the case where source row pitch is zero and destination row
       // pitch is non-zero
@@ -578,22 +576,20 @@ ur_result_t urBindlessImagesImageCopyExp(
           "source row pitch is non-zero. Potential misconfiguration detected.");
       return UR_RESULT_ERROR_INVALID_ARGUMENT;
     } else {
-      */
-    // Copy from Non-USM memory to Non-USM memory
-    // Copy from Non-USM/pitched USM memory to pitched USM/Non-USM memory
-    ze_image_region_t DstRegion;
-    UR_CALL(getImageRegionHelper(ZeImageDesc, &pCopyRegion->dstOffset,
-                                 &pCopyRegion->copyExtent, DstRegion));
-    ze_image_region_t SrcRegion;
-    UR_CALL(getImageRegionHelper(ZeImageDesc, &pCopyRegion->srcOffset,
-                                 &pCopyRegion->copyExtent, SrcRegion));
-    auto *UrImageDst = static_cast<_ur_image *>(pDst);
-    auto *UrImageSrc = static_cast<const _ur_image *>(pSrc);
-    ZE2UR_CALL(zeCommandListAppendImageCopyRegion,
-               (ZeCommandList, UrImageDst->ZeImage, UrImageSrc->ZeImage,
-                &DstRegion, &SrcRegion, ZeEvent, WaitList.Length,
-                WaitList.ZeEventList));
-    //}
+      // Copy from Non-USM memory to Non-USM memory
+      ze_image_region_t DstRegion;
+      UR_CALL(getImageRegionHelper(ZeImageDesc, &pCopyRegion->dstOffset,
+                                   &pCopyRegion->copyExtent, DstRegion));
+      ze_image_region_t SrcRegion;
+      UR_CALL(getImageRegionHelper(ZeImageDesc, &pCopyRegion->srcOffset,
+                                   &pCopyRegion->copyExtent, SrcRegion));
+      auto *UrImageDst = static_cast<_ur_image *>(pDst);
+      auto *UrImageSrc = static_cast<const _ur_image *>(pSrc);
+      ZE2UR_CALL(zeCommandListAppendImageCopyRegion,
+                 (ZeCommandList, UrImageDst->ZeImage, UrImageSrc->ZeImage,
+                  &DstRegion, &SrcRegion, ZeEvent, WaitList.Length,
+                  WaitList.ZeEventList));
+    }
   } else {
     logger::error("urBindlessImagesImageCopyExp: unexpected imageCopyFlags");
     return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
