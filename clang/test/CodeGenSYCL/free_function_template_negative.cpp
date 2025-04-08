fae0cff588af6b7b34389cb2ab4a985c371c92a5
Klochkov, Denis <denis.klochkov@intel.com>
2025-04-08 15:28:44 +0200

    [SYCL] do not emit error twice

--- clang/test/CodeGenSYCL/free_function_template_negative.cpp
+++ clang/test/CodeGenSYCL/free_function_template_negative.cpp
@@ -1,11 +1,9 @@
-// RUN: %clang_cc1 -fsycl-is-device -fsyntax-only -verify -internal-isystem %S/Inputs -sycl-std=2020 -fsycl-int-header=%t.h %s
+// RUN: %clang_cc1 -fsycl-is-device -internal-isystem %S/Inputs -triple spir64-unknown-unknown -sycl-std=2020 -fsycl-int-header=%t.h %s
 
 #include "sycl.hpp"
 
-// expected-error@+1 {{free function can not be variadic template function}}
+// expected-error@+1 {{free function can not be a variadic template function}}
 template<typename ...Ts>
 [[__sycl_detail__::add_ir_attributes_function("sycl-nd-range-kernel", 2)]] void
 templated_variadic(Ts... args) {
 }
-
-template void templated_variadic(int, int);
