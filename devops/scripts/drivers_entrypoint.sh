556e715b2a376143359133161bff92a206d50e17
opensource-krzysztof <152877665+opensource-krzysztof@users.noreply.github.com>
2025-04-09 07:57:13 +0200

    Update drivers_entrypoint.sh

--- devops/scripts/drivers_entrypoint.sh
+++ devops/scripts/drivers_entrypoint.sh
@@ -2,5 +2,13 @@
 
 source /runtimes/oneapi-tbb/env/vars.sh
 
+
+wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor | sudo tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
+sudo apt-get update
+sudo apt-get install -y aptitute
+sudo aptitude install -y intel-oneapi-runtime-opencl intel-oneapi-base-toolkit
+        
+
 exec "$@"
 
