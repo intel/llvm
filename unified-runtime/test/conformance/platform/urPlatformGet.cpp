f2907d8a75cae0810b4874c7b754f085f5c18c17
Isaac Ault <isaac.ault@codeplay.com>
2025-04-09 10:51:36 +1000

    Update urPlatformGetTest to leverage AdapterTest parameterisation.

--- unified-runtime/test/conformance/platform/urPlatformGet.cpp
+++ unified-runtime/test/conformance/platform/urPlatformGet.cpp
@@ -6,32 +6,25 @@
 
 #include <uur/fixtures.h>
 
-struct urPlatformGetTest : ::testing::Test {
-  std::vector<ur_adapter_handle_t> &adapters =
-      uur::PlatformEnvironment::instance->adapters;
-};
+struct urPlatformGetTest = uur::urAdapterTest;
 
 TEST_F(urPlatformGetTest, Success) {
-  for (auto adapter : adapters) {
-    uint32_t count;
-    ASSERT_SUCCESS(urPlatformGet(adapter, 0, nullptr, &count));
-    ASSERT_NE(count, 0);
-    std::vector<ur_platform_handle_t> platforms(count);
-    ASSERT_SUCCESS(urPlatformGet(adapter, count, platforms.data(), nullptr));
-    for (auto platform : platforms) {
-      ASSERT_NE(nullptr, platform);
-    }
+  uint32_t count;
+  ASSERT_SUCCESS(urPlatformGet(adapter, 0, nullptr, &count));
+  ASSERT_NE(count, 0);
+  std::vector<ur_platform_handle_t> platforms(count);
+  ASSERT_SUCCESS(urPlatformGet(adapter, count, platforms.data(), nullptr));
+  for (auto platform : platforms) {
+    ASSERT_NE(nullptr, platform);
   }
 }
 
 TEST_F(urPlatformGetTest, InvalidNumEntries) {
-  for (auto adapter : adapters) {
-    uint32_t count;
-    ASSERT_SUCCESS(urPlatformGet(adapter, 0, nullptr, &count));
-    std::vector<ur_platform_handle_t> platforms(count);
-    ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_SIZE,
-                     urPlatformGet(adapter, 0, platforms.data(), nullptr));
-  }
+  uint32_t count;
+  ASSERT_SUCCESS(urPlatformGet(adapter, 0, nullptr, &count));
+  std::vector<ur_platform_handle_t> platforms(count);
+  ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_SIZE,
+                   urPlatformGet(adapter, 0, platforms.data(), nullptr));
 }
 
 TEST_F(urPlatformGetTest, InvalidNullPointer) {
@@ -41,8 +34,6 @@ TEST_F(urPlatformGetTest, InvalidNullPointer) {
 }
 
 TEST_F(urPlatformGetTest, NullArgs) {
-  for (auto adapter : adapters) {
-    ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_VALUE,
-                     urPlatformGet(adapter, 0, nullptr, nullptr));
-  }
+  ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_VALUE,
+                   urPlatformGet(adapter, 0, nullptr, nullptr));
 }
