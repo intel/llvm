7ed1c44ce75682c28169dbed82971ac519eb77e4
Skrobot, Daniel <daniel.skrobot@intel.com>
2025-04-10 14:19:36 +0200

    [SYCL] Add test for free function kernels extension that checks emitted diagnostics if kernel violates restrictions

---
+++ sycl/test/extensions/free_function_kernels/free_function_kernels_diagnostics.cpp
@@ -0,0 +1,29 @@
+// RUN: %clangxx -fsyntax-only -fsycl-device-only -Xclang -verify -Xclang -verify-ignore-unexpected=note %s
+#include <sycl/sycl.hpp>
+
+namespace syclexp = sycl::ext::oneapi::experimental;
+
+// expected-error@+2 {{'int &' cannot be used as the type of a kernel parameter}}
+SYCL_EXT_ONEAPI_FUNCTION_PROPERTY(syclexp::single_task_kernel)
+void singleTaskKernelReference(int &Ref) {}
+
+// expected-error@+2 {{'int &' cannot be used as the type of a kernel parameter}}
+SYCL_EXT_ONEAPI_FUNCTION_PROPERTY(syclexp::nd_range_kernel<2>)
+void ndRangeKernelReference(int &Ref) {}
+
+// Diagnostic for these violations of the restrictions haven't been implemented yet.
+// TODO: Add expected error when it will be implemented.
+SYCL_EXT_ONEAPI_FUNCTION_PROPERTY(syclexp::single_task_kernel)
+void singleTaskKernelVariadic(...) {}
+
+// TODO: Add expected error when it will be implemented.
+SYCL_EXT_ONEAPI_FUNCTION_PROPERTY(syclexp::nd_range_kernel<3>)
+void ndRangeKernelVariadic(...) {}
+
+// TODO: Add expected error when it will be implemented.
+SYCL_EXT_ONEAPI_FUNCTION_PROPERTY(syclexp::single_task_kernel)
+void singleTaskKernelDefaultValues(int Value = 1) {}
+
+// TODO: Add expected error when it will be implemented.
+SYCL_EXT_ONEAPI_FUNCTION_PROPERTY(syclexp::nd_range_kernel<1>)
+void ndRangeKernelDefaultValues(int Value = 1) {}
