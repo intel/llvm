64bc6e86b47391add7a47ce9f4eb291db24f42e2
Julian Oppermann <julian.oppermann@codeplay.com>
2025-04-10 15:12:49 +0100

    Fix header comments
    Signed-off-by: Julian Oppermann <julian.oppermann@codeplay.com>

--- sycl-jit/jit-compiler/lib/translation/Translation.h
+++ sycl-jit/jit-compiler/lib/translation/Translation.h
@@ -1,4 +1,4 @@
-//==--- Translation.cpp - Translate SYCL images between different formats --==//
+//==--- Translation.h - Translate SYCL images between different formats ----==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
