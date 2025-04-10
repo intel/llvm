87acf739b472e5115ac0ab19be619adffbdad7d2
Martin Grant <martin.morrisongrant@codeplay.com>
2025-04-10 16:58:18 +0100

    [UR] Logger callback function sink (#17095)
    Migrated from https://github.com/oneapi-src/unified-runtime/pull/1748
    
    This PR implements
    https://github.com/oneapi-src/unified-runtime/issues/1330 through a new
    logger sink: a user configurable callback. It introduces some spec
    additions:
    
    - `typedef void (*ur_logger_output_callback_t)(ur_logger_level_t level,
    const char *pLoggerMsg, void *pUserData)`
    - `urSetLoggerCallback(ur_adapter_handle_t hAdapter,
    ur_logger_output_callback_t pfnLoggerCallback, void *pUserData,
    ur_logger_level_t level`
    )`
    - `urSetLoggerCallbackLevel(ur_adapter_handle_t hAdapter,
    ur_logger_level_t level)`
    - `typedef enum ur_logger_level_t` (moved the logger::level enum into
    the spec)
    
    This new logger sink will only be constructed once a user makes a call
    to `urSetLoggerCallback` (which is called from the UR
    `urAdapterSetLoggerCallback` entry point), supplying their own callback
    function. They can set the minimum logging level through
    `urSetLoggerCallbackLevel`. Any subsequent logging calls will
    additionally make a call to the supplied callback where the log level,
    message and user data will be sent.
    
    A callback has been setup in the SYCL RT in `sycl/source/detail/ur.cpp`
    to print logs to `std::err`:
    ```
    void urLoggerCallback([[maybe_unused]] ur_logger_level_t level, const char *msg,
                          [[maybe_unused]] void *userData) {
      std::cerr << msg << std::endl;
    }
    ```
    
    A new test suite `LoggerWithCallbackSink` has been added to test this
    new functionality.
    
    ---------
    
    Co-authored-by: Kenneth Benzie (Benie) <k.benzie83@gmail.com>

--- unified-runtime/include/ur_ddi.h
+++ unified-runtime/include/ur_ddi.h
@@ -24,6 +24,43 @@ extern "C" {
 #endif
 
 ///////////////////////////////////////////////////////////////////////////////
+/// @brief Function-pointer for urAdapterSetLoggerCallback
+typedef ur_result_t(UR_APICALL *ur_pfnAdapterSetLoggerCallback_t)(
+    ur_adapter_handle_t, ur_logger_callback_t, void *, ur_logger_level_t);
+
+///////////////////////////////////////////////////////////////////////////////
+/// @brief Function-pointer for urAdapterSetLoggerCallbackLevel
+typedef ur_result_t(UR_APICALL *ur_pfnAdapterSetLoggerCallbackLevel_t)(
+    ur_adapter_handle_t, ur_logger_level_t);
+
+///////////////////////////////////////////////////////////////////////////////
+/// @brief Table of Adapter functions pointers
+typedef struct ur_adapter_dditable_t {
+  ur_pfnAdapterSetLoggerCallback_t pfnSetLoggerCallback;
+  ur_pfnAdapterSetLoggerCallbackLevel_t pfnSetLoggerCallbackLevel;
+} ur_adapter_dditable_t;
+
+///////////////////////////////////////////////////////////////////////////////
+/// @brief Exported function for filling application's Adapter table
+///        with current process' addresses
+///
+/// @returns
+///     - ::UR_RESULT_SUCCESS
+///     - ::UR_RESULT_ERROR_UNINITIALIZED
+///     - ::UR_RESULT_ERROR_INVALID_NULL_POINTER
+///     - ::UR_RESULT_ERROR_UNSUPPORTED_VERSION
+UR_DLLEXPORT ur_result_t UR_APICALL urGetAdapterProcAddrTable(
+    /// [in] API version requested
+    ur_api_version_t version,
+    /// [in,out] pointer to table of DDI function pointers
+    ur_adapter_dditable_t *pDdiTable);
+
+///////////////////////////////////////////////////////////////////////////////
+/// @brief Function-pointer for urGetAdapterProcAddrTable
+typedef ur_result_t(UR_APICALL *ur_pfnGetAdapterProcAddrTable_t)(
+    ur_api_version_t, ur_adapter_dditable_t *);
+
+///////////////////////////////////////////////////////////////////////////////
 /// @brief Function-pointer for urPlatformGet
 typedef ur_result_t(UR_APICALL *ur_pfnPlatformGet_t)(ur_adapter_handle_t,
                                                      uint32_t,
@@ -1966,6 +2003,7 @@ typedef ur_result_t(UR_APICALL *ur_pfnGetDeviceProcAddrTable_t)(
 ///////////////////////////////////////////////////////////////////////////////
 /// @brief Container for all DDI tables
 typedef struct ur_dditable_t {
+  ur_adapter_dditable_t Adapter;
   ur_platform_dditable_t Platform;
   ur_context_dditable_t Context;
   ur_event_dditable_t Event;
--- unified-runtime/source/adapters/cuda/adapter.cpp
+++ unified-runtime/source/adapters/cuda/adapter.cpp
@@ -26,7 +26,7 @@ public:
     this->ostream = &std::cerr;
   }
 
-  virtual void print([[maybe_unused]] logger::Level level,
+  virtual void print([[maybe_unused]] ur_logger_level_t level,
                      const std::string &msg) override {
     std::cerr << msg << std::endl;
   }
@@ -39,7 +39,7 @@ public:
 // https://github.com/oneapi-src/unified-runtime/issues/1330
 ur_adapter_handle_t_::ur_adapter_handle_t_()
     : logger(logger::get_logger("cuda",
-                                /*default_log_level*/ logger::Level::ERR)) {
+                                /*default_log_level*/ UR_LOGGER_LEVEL_ERROR)) {
   Platform = std::make_unique<ur_platform_handle_t_>();
 
   if (std::getenv("UR_LOG_CUDA") == nullptr &&
@@ -113,6 +113,24 @@ UR_APIEXPORT ur_result_t UR_APICALL urAdapterGetInfo(ur_adapter_handle_t,
   default:
     return UR_RESULT_ERROR_INVALID_ENUMERATION;
   }
+
+  return UR_RESULT_SUCCESS;
+}
+
+UR_APIEXPORT ur_result_t UR_APICALL urAdapterSetLoggerCallback(
+    ur_adapter_handle_t, ur_logger_callback_t pfnLoggerCallback,
+    void *pUserData, ur_logger_level_t level = UR_LOGGER_LEVEL_QUIET) {
+
+  ur::cuda::adapter->logger.setCallbackSink(pfnLoggerCallback, pUserData,
+                                            level);
+
+  return UR_RESULT_SUCCESS;
+}
+
+UR_APIEXPORT ur_result_t UR_APICALL urAdapterSetLoggerCallbackLevel(
+    ur_adapter_handle_t, ur_logger_level_t level = UR_LOGGER_LEVEL_QUIET) {
+
+  ur::cuda::adapter->logger.setCallbackLevel(level);
 
   return UR_RESULT_SUCCESS;
 }
