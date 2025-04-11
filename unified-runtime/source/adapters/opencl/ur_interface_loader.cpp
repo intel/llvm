c80b35f63c073efc3eaf4656dd1e7975a3ec66e8
Aaron Greig <aaron.greig@codeplay.com>
2025-04-11 13:28:27 +0100

    Fix missed conflict.

--- unified-runtime/source/adapters/opencl/ur_interface_loader.cpp
+++ unified-runtime/source/adapters/opencl/ur_interface_loader.cpp
@@ -427,22 +427,6 @@ UR_DLLEXPORT ur_result_t UR_APICALL urGetKernelExpProcAddrTable(
   return UR_RESULT_SUCCESS;
 }
 
-<<<<<<< HEAD
-=======
-UR_DLLEXPORT ur_result_t UR_APICALL urGetProgramExpProcAddrTable(
-    ur_api_version_t version, ur_program_exp_dditable_t *pDdiTable) {
-  auto result = validateProcInputs(version, pDdiTable);
-  if (UR_RESULT_SUCCESS != result) {
-    return result;
-  }
-
-  pDdiTable->pfnBuildExp = urProgramBuildExp;
-  pDdiTable->pfnCompileExp = urProgramCompileExp;
-  pDdiTable->pfnLinkExp = urProgramLinkExp;
-
-  return UR_RESULT_SUCCESS;
-}
-
 UR_DLLEXPORT ur_result_t UR_APICALL urGetAdapterProcAddrTable(
     ur_api_version_t version, ur_adapter_dditable_t *pDdiTable) {
   auto result = validateProcInputs(version, pDdiTable);
@@ -455,7 +439,6 @@ UR_DLLEXPORT ur_result_t UR_APICALL urGetAdapterProcAddrTable(
   return UR_RESULT_SUCCESS;
 }
 
->>>>>>> sycl
 #if defined(__cplusplus)
 } // extern "C"
 #endif
