d5fad51498923dfd41f42c1b5822521601fe4f66
Mikołaj Komar <mikolaj.komar@intel.com>
2025-04-11 10:23:58 +0000

    Add descriptors instead of parameter list

--- unified-runtime/source/adapters/level_zero/v2/command_list_cache.hpp
+++ unified-runtime/source/adapters/level_zero/v2/command_list_cache.hpp
@@ -26,6 +26,30 @@ using command_list_unique_handle =
                     std::function<void(::ze_command_list_handle_t)>>;
 } // namespace raii
 
+struct supported_extensions_descriptor_t {
+  supported_extensions_descriptor_t(
+      bool ZeCopyOffloadExtensionSupported,
+      bool ZeMutableCmdListExtentionSupported)
+      : ZeCopyOffloadExtensionSupported(ZeCopyOffloadExtensionSupported),
+        ZeMutableCmdListExtentionSupported(ZeMutableCmdListExtentionSupported) {}
+  bool ZeCopyOffloadExtensionSupported;
+  bool ZeMutableCmdListExtentionSupported;
+};
+
+struct command_list_desc_t {
+  command_list_desc_t() = default;
+  command_list_desc_t(bool IsInOrder,
+                      uint32_t Ordinal, bool CopyOffloadEnable, bool Mutable = false)
+      : IsInOrder(IsInOrder), Ordinal(Ordinal),
+        CopyOffloadEnable(CopyOffloadEnable), Mutable(Mutable) {}
+  bool IsInOrder;
+  uint32_t Ordinal;
+  bool CopyOffloadEnable;
+  // The mutable command list is not supported by immediate command lists
+  // and because of that this field will be ignored in immediate command list case
+  bool Mutable;
+};
+
 struct immediate_command_list_descriptor_t {
   ze_device_handle_t ZeDevice;
   bool IsInOrder;
@@ -56,18 +80,15 @@ struct command_list_descriptor_hash_t {
 
 struct command_list_cache_t {
   command_list_cache_t(ze_context_handle_t ZeContext,
-                       bool ZeCopyOffloadExtensionSupported,
-                       bool ZeMutableCmdListExtentionSupported);
+    supported_extensions_descriptor_t SupportedExtensions);
 
   raii::command_list_unique_handle
-  getImmediateCommandList(ze_device_handle_t ZeDevice, bool IsInOrder,
-                          uint32_t Ordinal, bool CopyOffloadEnable,
+  getImmediateCommandList(ze_device_handle_t ZeDevice, command_list_desc_t Desc,
                           ze_command_queue_mode_t Mode,
                           ze_command_queue_priority_t Priority,
                           std::optional<uint32_t> Index = std::nullopt);
   raii::command_list_unique_handle
-  getRegularCommandList(ze_device_handle_t ZeDevice, bool IsInOrder,
-                        uint32_t Ordinal, bool CopyOffloadEnable, bool Mutable);
+  getRegularCommandList(ze_device_handle_t ZeDevice, command_list_desc_t Desc);
 
   // For testing purposes
   size_t getNumImmediateCommandLists();
