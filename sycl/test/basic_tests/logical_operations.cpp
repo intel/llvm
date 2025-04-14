89462dffc292ab476707e4e85d2813701955470a
dklochkov-intel <denis.klochkov@intel.com>
2025-04-14 11:54:46 +0200

    [SYCL] avoid true in test expressions
    Co-authored-by: Steffen Larsen <steffen.larsen@intel.com>

--- sycl/test/basic_tests/logical_operations.cpp
+++ sycl/test/basic_tests/logical_operations.cpp
@@ -9,11 +9,11 @@ int main() {
   const auto logicalAnd = sycl::logical_and<int>();
   const auto logicalOr = sycl::logical_or<int>();
 #ifdef __INTEL_PREVIEW_BREAKING_CHANGES
-  static_assert(std::is_same_v<decltype(logicalAnd(1, 2)), bool> == true);
-  static_assert(std::is_same_v<decltype(logicalOr(1, 2)), bool> == true);
+  static_assert(std::is_same_v<decltype(logicalAnd(1, 2)), bool>);
+  static_assert(std::is_same_v<decltype(logicalOr(1, 2)), bool>);
 #else
-  static_assert(std::is_same_v<decltype(logicalAnd(1, 2)), int> == true);
-  static_assert(std::is_same_v<decltype(logicalOr(1, 2)), int> == true);
+  static_assert(std::is_same_v<decltype(logicalAnd(1, 2)), int>);
+  static_assert(std::is_same_v<decltype(logicalOr(1, 2)), int>);
 #endif
   return 0;
 }
