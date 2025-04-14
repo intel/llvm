da3f5fc0f73fd5c489ca1380242bed5f6311f15a
Ross Brunton <ross@codeplay.com>
2025-04-14 12:19:54 +0100

    [UR] Move conformance tests over to lit
    Conformance tests are now ran by LLVM's lit framework rather than ctest.
    A new `check-unified-runtime-conformance` target is available, which
    builds and runs all conformance tests. The `test` target will still run
    them.
    
    Some additional housekeeping was done:
    * `add_conformance_test_with_x_environment` was renamed to
      `add_conformance_x_test`.
    * Conformance tests now explicitly respect `ONEAPI_DEVICE_SELECTOR`.
    * If unspecified, `ONEAPI_DEVICE_SELECTOR` will now be populated with
      the list of built targets. Notably, this will exclude adapters which
      exist in the build directory but were not specified using a
      `BUILD_ADAPTER` cmake option (e.g, from a previous build before a
      reconfigure).
    * References to `UR_CTS_ADAPTER_PLATFORM` and `UR_CTS_BACKEND` are
      removed (they never did anything anyway).
    * Github build jobs have been updated.
    * The generated gtest binaries have been moved from `build/bin` to
      `build/test/conformance`
    * `requirements_testing.txt` now imports psutil for using timeouts.
    * Output format has been changed to show a progress bar and print
      the test output in real time.
    * Testing now respects the "device" filters from
      `ONEAPI_DEVICE_SELECTOR` (previously, the filter was only used to
      select the platform. `opencl:cpu` now works as expected.

--- unified-runtime/test/conformance/lit.cfg.py
+++ unified-runtime/test/conformance/lit.cfg.py
@@ -7,5 +7,68 @@ SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 """
 
-# Disable lit testing (for now)
-config.suffixes = []
+import lit.formats
+import subprocess
+from os import path
+
+
+class NopTestFormat(lit.formats.TestFormat):
+    def getTestsForPath(*args, **kwargs):
+        return
+        yield
+
+    def getTestsInDirectory(*args, **kwargs):
+        return
+        yield
+
+
+config.name = "Unified Runtime Conformance"
+config.test_format = lit.formats.GoogleTest("", "-test")
+config.test_source_root = config.binary_dir
+config.test_exec_root = config.binary_dir
+
+if config.using_l0_v2:
+    config.environment["UR_LOADER_USE_LEVEL_ZERO_V2"] = "1"
+else:
+    config.environment["UR_LOADER_USE_LEVEL_ZERO_V2"] = "0"
+
+if "UR_CTS_ALSO_RUN_KNOWN_FAILURES" in os.environ:
+    config.environment["UR_CTS_ALSO_RUN_KNOWN_FAILURES"] = os.environ[
+        "UR_CTS_ALSO_RUN_KNOWN_FAILURES"
+    ]
+
+if "selector" in lit_config.params:
+    # Specified via passing `-Dselector="backend:*"` to lit
+    config.environment["ONEAPI_DEVICE_SELECTOR"] = lit_config.params["selector"]
+elif os.environ.get("ONEAPI_DEVICE_SELECTOR", ""):
+    lit_config.warning(
+        "ONEAPI_DEVICE_SELECTOR set in environment, using that for UR conformance tests"
+    )
+    config.environment["ONEAPI_DEVICE_SELECTOR"] = os.environ["ONEAPI_DEVICE_SELECTOR"]
+else:
+    ods = ""
+    for a in config.adapters_built:
+        if a != "mock":
+            ods += f"{a}:*;"
+    config.environment["ONEAPI_DEVICE_SELECTOR"] = ods
+
+selector = config.environment["ONEAPI_DEVICE_SELECTOR"]
+if selector == "":
+    lit_config.warning(
+        "No device selector is set (are any adapters enabled?), skipping conformance tests"
+    )
+    config.test_format = NopTestFormat()
+else:
+    lit_config.note(
+        f"Running Unified Runtime conformance tests with ONEAPI_DEVICE_SELECTOR='{selector}':"
+    )
+    urinfo = subprocess.run(
+        path.join(config.runtime_dir, "urinfo"),
+        text=True,
+        env=config.environment,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    for l in urinfo.stdout.split("\n"):
+        if l:
+            lit_config.note(f"  {l}")
