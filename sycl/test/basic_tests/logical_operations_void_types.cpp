fe3b943eb8a5f3c0af277cf0ffb5ec0d8ab3c33c
Klochkov, Denis <denis.klochkov@intel.com>
2025-04-14 11:02:00 +0200

    [SYCL] simplify expression to test logfical operations with void types

--- sycl/test/basic_tests/logical_operations_void_types.cpp
+++ sycl/test/basic_tests/logical_operations_void_types.cpp
@@ -2,7 +2,6 @@
 // RUN: %clangxx -fsycl -fsyntax-only -Xclang -verify -Xclang -verify-ignore-unexpected=note %s
 // RUN: %clangxx -fsycl -fpreview-breaking-changes -fsyntax-only -Xclang -verify -Xclang -verify-ignore-unexpected=note %s
 
-#include <cassert>
 #include <sycl/functional.hpp>
 #include <type_traits>
 
@@ -10,10 +9,6 @@ const auto logicalAndVoid = sycl::logical_and<void>();
 const auto logicalOrVoid = sycl::logical_or<void>();
 
 // expected-error@+1 {{}}
-static_assert(std::is_same_v<decltype(logicalAndVoid(static_cast<void>(1),
-                                                     static_cast<void>(2))),
-                             bool> == true);
+logicalAndVoid(static_cast<void>(1), static_cast<void>(2));
 // expected-error@+1 {{}}
-static_assert(std::is_same_v<decltype(logicalOrVoid(static_cast<void>(1),
-                                                    static_cast<void>(2))),
-                             bool> == true);
+logicalOrVoid(static_cast<void>(1), static_cast<void>(2));
