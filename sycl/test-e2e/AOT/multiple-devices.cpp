e856d63d8bc91643fb6429f802d10f1fdf811c24
KornevNikita <nikita.kornev@intel.com>
2025-04-15 02:04:45 -0700

    add run-aux for cpu aot runs

--- sycl/test-e2e/AOT/multiple-devices.cpp
+++ sycl/test-e2e/AOT/multiple-devices.cpp
@@ -12,8 +12,8 @@
 // RUN: %clangxx -fsycl -fsycl-targets=spir64,spir64_x86_64,spir64_gen %S/Inputs/aot.cpp -c -o %t.o
 
 // CPU, GPU
-// RUN: %clangxx -fsycl -fsycl-targets=spir64_x86_64,spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts %t.o -o %t_cpu_gpu.out
-// RUN: %if gpu || cpu %{ %{run} %t_cpu_gpu.out %}
+// RUN: %{run-aux} %clangxx -fsycl -fsycl-targets=spir64_x86_64,spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts %t.o -o %t_cpu_gpu.out
+// RUN: %{run} %t_cpu_gpu.out
 
 // No AOT-compiled image for CPU
 // RUN: %clangxx -fsycl -fsycl-targets=spir64,spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts %t.o -o %t_spv_gpu.out
@@ -21,11 +21,11 @@
 // RUN: env ONEAPI_DEVICE_SELECTOR="*:gpu" %{run-unfiltered-devices} %t_spv_gpu.out
 
 // No AOT-compiled image for GPU
-// RUN: %clangxx -fsycl -fsycl-targets=spir64,spir64_x86_64 %t.o -o %t_spv_cpu.out
+// RUN: %{run-aux} %clangxx -fsycl -fsycl-targets=spir64,spir64_x86_64 %t.o -o %t_spv_cpu.out
 // Check that execution on AOT-compatible devices is unaffected
 // RUN: env ONEAPI_DEVICE_SELECTOR="*:cpu" %{run-unfiltered-devices} %t_spv_cpu.out
 
 // No AOT-compiled image for FPGA
-// RUN: %clangxx -fsycl -fsycl-targets=spir64,spir64_x86_64,spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts %t.o -o %t_spv_cpu_gpu.out
+// RUN: %{run-aux} %clangxx -fsycl -fsycl-targets=spir64,spir64_x86_64,spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts %t.o -o %t_spv_cpu_gpu.out
 // Check that execution on AOT-compatible devices is unaffected
 // RUN: %{run} %t_spv_cpu_gpu.out
