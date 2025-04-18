997f12033d0c2ebf672efff43f50a82164b672c5
Harald van Dijk <harald.vandijk@codeplay.com>
2025-04-18 14:28:05 +0100

    Require native_cpu for new test.

--- sycl/test/native_cpu/target-features.cpp
+++ sycl/test/native_cpu/target-features.cpp
@@ -1,3 +1,4 @@
+// REQUIRES: native_cpu
 // RUN: %clang_cc1 -triple native_cpu -aux-triple x86_64-unknown-linux-gnu -fsycl-is-device -fsycl-is-native-cpu -emit-llvm -o - %s | FileCheck %s --check-prefixes=CHECK,NOAVX
 // RUN: %clang_cc1 -triple native_cpu -aux-triple x86_64-unknown-linux-gnu -aux-target-cpu skylake -fsycl-is-device -fsycl-is-native-cpu -emit-llvm -o - %s | FileCheck %s --check-prefixes=CHECK,AVX
 // RUN: %clang_cc1 -triple native_cpu -aux-triple x86_64-unknown-linux-gnu -aux-target-feature +avx -fsycl-is-device -fsycl-is-native-cpu -emit-llvm -o - %s | FileCheck %s --check-prefixes=CHECK,AVX
