9b7e9f0100a8ec57d0c0ca3dfe9dbe3a581deb0e
jinge90 <ge.jin@intel.com>
2025-04-22 16:39:01 +0800

    add device tsan aot e2e tests
    Signed-off-by: jinge90 <ge.jin@intel.com>

---
+++ sycl/test-e2e/ThreadSanitizer/aot/cpu.cpp
@@ -0,0 +1,13 @@
+// REQUIRES: linux, opencl-aot, cpu
+
+// RUN: %{run-aux} %{build} %device_tsan_aot_flags -O0 -g %S/Inputs/usm_data_race.cpp -o %t.out
+// RUN: %{run} %t.out 2>&1 | FileCheck %S/Inputs/usm_data_race.cpp
+
+// RUN: %{run-aux} %{build} %device_tsan_aot_flags -O1 -g %S/Inputs/usm_data_race.cpp -o %t.out
+// RUN: %{run} %t.out 2>&1 | FileCheck %S/Inputs/usm_data_race.cpp
+
+// RUN: %{run-aux} %{build} %device_tsan_aot_flags -O2 -g %S/Inputs/usm_data_race.cpp -o %t.out
+// RUN: %{run} %t.out 2>&1 | FileCheck %S/Inputs/usm_data_race.cpp
+
+// RUN: %{run-aux} %{build} %device_tsan_aot_flags -O3 -g %S/Inputs/usm_data_race.cpp -o %t.out
+// RUN: %{run} %t.out 2>&1 | FileCheck %S/Inputs/usm_data_race.cpp
