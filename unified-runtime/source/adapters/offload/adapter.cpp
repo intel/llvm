13c1cb56af6c95b54d32880265eee7c52b4e18db
Ross Brunton <ross@codeplay.com>
2025-04-24 12:49:35 +0100

    Add "offload" as an adapter to the various registries
    This adds "offload" to several locations, meaning that:
    * It will be initialised by the loader and iterated like other adapters.
    * ONEAPI_DEVICE_SELECTOR="offload:*" works (note that this is an
      extension to the ONEAPI_DEVICE_SELECTOR format).
    * Platforms and adapters now report themselves as "OFFLOAD" rather than
      "CUDA" or "HIP".

--- unified-runtime/source/adapters/offload/adapter.cpp
+++ unified-runtime/source/adapters/offload/adapter.cpp
@@ -8,9 +8,9 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include <OffloadAPI.h>
 #include <atomic>
 #include <cstdint>
-#include <OffloadAPI.h>
 #include <unordered_set>
 
 #include "adapter.hpp"
@@ -88,7 +88,7 @@ UR_APIEXPORT ur_result_t UR_APICALL urAdapterGetInfo(ur_adapter_handle_t,
 
   switch (propName) {
   case UR_ADAPTER_INFO_BACKEND:
-    return ReturnValue(UR_BACKEND_CUDA); // TODO: Return a proper value
+    return ReturnValue(UR_BACKEND_OFFLOAD);
   case UR_ADAPTER_INFO_REFERENCE_COUNT:
     return ReturnValue(Adapter.RefCount.load());
   default:
