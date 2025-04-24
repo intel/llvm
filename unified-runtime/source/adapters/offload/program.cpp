1e649801b6dccbdb8ae8c6cb8db94b8e7deeda15
Ross Brunton <ross@codeplay.com>
2025-04-24 12:01:48 +0100

    Support compiling on non-CUDA
    This makes the dependency on cudadrv optional and ifdefs away the cubin
    workaround if it isn't. This isn't sufficient to have HIP devices
    compile kernels, but does allow libur_adapter_offload to be built on
    said hosts.
    
    In addition, an unused variable error was fixed.

--- unified-runtime/source/adapters/offload/program.cpp
+++ unified-runtime/source/adapters/offload/program.cpp
@@ -1,11 +1,62 @@
 #include <OffloadAPI.h>
 #include <ur/ur.hpp>
 #include <ur_api.h>
-#include <cuda.h>
 
 #include "context.hpp"
 #include "program.hpp"
 #include "ur2offload.hpp"
+
+#ifdef UR_CUDA_ENABLED
+#include <cuda.h>
+#endif
+
+namespace {
+// Workaround for Offload not supporting PTX binaries. Force CUDA programs
+// to be linked so they end up as CUBIN.
+#ifdef UR_CUDA_ENABLED
+ur_result_t ProgramCreateCudaWorkaround(ur_context_handle_t hContext,
+                                        const uint8_t *Binary, size_t Length,
+                                        ur_program_handle_t *phProgram) {
+  uint8_t *RealBinary;
+  size_t RealLength;
+  CUlinkState State;
+  cuLinkCreate(0, nullptr, nullptr, &State);
+
+  cuLinkAddData(State, CU_JIT_INPUT_PTX, (char *)(Binary), Length, nullptr, 0,
+                nullptr, nullptr);
+
+  void *CuBin = nullptr;
+  size_t CuBinSize = 0;
+  cuLinkComplete(State, &CuBin, &CuBinSize);
+  RealBinary = (uint8_t *)CuBin;
+  RealLength = CuBinSize;
+  fprintf(stderr, "Performed CUDA bin workaround (size = %lu)\n", RealLength);
+
+  ur_program_handle_t Program = new ur_program_handle_t_();
+  auto Res =
+      olCreateProgram(reinterpret_cast<ol_device_handle_t>(hContext->Device),
+                      RealBinary, RealLength, &Program->OffloadProgram);
+
+  // Program owns the linked module now
+  cuLinkDestroy(State);
+  (void)State;
+
+  if (Res != OL_SUCCESS) {
+    delete Program;
+    return offloadResultToUR(Res);
+  }
+
+  *phProgram = Program;
+
+  return UR_RESULT_SUCCESS;
+}
+#else
+ur_result_t ProgramCreateCudaWorkaround(ur_context_handle_t, const uint8_t *,
+                                        size_t, ur_program_handle_t *) {
+  return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
+}
+#endif
+} // namespace
 
 UR_APIEXPORT ur_result_t UR_APICALL urProgramCreateWithBinary(
     ur_context_handle_t hContext, uint32_t numDevices,
@@ -15,45 +66,23 @@ UR_APIEXPORT ur_result_t UR_APICALL urProgramCreateWithBinary(
     return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
   }
 
-  // Workaround for Offload not supporting PTX binaries. Force CUDA programs
-  // to be linked so they end up as CUBIN.
-  uint8_t *RealBinary;
-  size_t RealLength;
   ur_platform_handle_t DevicePlatform;
-  bool DidLink = false;
-  CUlinkState State;
   urDeviceGetInfo(phDevices[0], UR_DEVICE_INFO_PLATFORM,
                   sizeof(ur_platform_handle_t), &DevicePlatform, nullptr);
   ur_backend_t PlatformBackend;
   urPlatformGetInfo(DevicePlatform, UR_PLATFORM_INFO_BACKEND,
                     sizeof(ur_backend_t), &PlatformBackend, nullptr);
   if (PlatformBackend == UR_BACKEND_CUDA) {
-    cuLinkCreate(0, nullptr, nullptr, &State);
-
-    cuLinkAddData(State, CU_JIT_INPUT_PTX, (char *)(ppBinaries[0]), pLengths[0],
-                  nullptr, 0, nullptr, nullptr);
-
-    void *CuBin = nullptr;
-    size_t CuBinSize = 0;
-    cuLinkComplete(State, &CuBin, &CuBinSize);
-    RealBinary = (uint8_t *)CuBin;
-    RealLength = CuBinSize;
-    DidLink = true;
-    fprintf(stderr, "Performed CUDA bin workaround (size = %lu)\n", RealLength);
-  } else {
-    RealBinary = const_cast<uint8_t *>(ppBinaries[0]);
-    RealLength = pLengths[0];
+    return ProgramCreateCudaWorkaround(hContext, ppBinaries[0], pLengths[0],
+                                       phProgram);
   }
+
+  auto *RealBinary = const_cast<uint8_t *>(ppBinaries[0]);
 
   ur_program_handle_t Program = new ur_program_handle_t_();
   auto Res =
       olCreateProgram(reinterpret_cast<ol_device_handle_t>(hContext->Device),
-                      RealBinary, RealLength, &Program->OffloadProgram);
-
-  // Program owns the linked module now
-  if (DidLink) {
-    cuLinkDestroy(State);
-  }
+                      RealBinary, pLengths[0], &Program->OffloadProgram);
 
   if (Res != OL_SUCCESS) {
     delete Program;
@@ -79,7 +108,6 @@ UR_APIEXPORT ur_result_t UR_APICALL urProgramBuildExp(ur_program_handle_t,
   // Do nothing, program is built upon creation
   return UR_RESULT_SUCCESS;
 }
-
 
 UR_APIEXPORT ur_result_t UR_APICALL
 urProgramRetain(ur_program_handle_t hProgram) {
