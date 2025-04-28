3ed7c4ef9c5a247335050f070b7dda0e2047b064
Gojska, Grzegorz <grzegorz.gojska@intel.com>
2025-04-28 16:53:25 +0200

    changed Bfloat16 to float casting

--- sycl/include/sycl/ext/oneapi/matrix/matrix-intel.hpp
+++ sycl/include/sycl/ext/oneapi/matrix/matrix-intel.hpp
@@ -270,7 +270,9 @@ public:
                             sycl::ext::oneapi::bfloat16, NumRows, NumCols,
                             spv_matrix_use_traits<Use>::value,
                             spv_scope_traits<Group>::value>(&M.spvm, idx);
-  return __imf_bfloat162float(*ExtractP);
+  float* tmp;
+  BF16VecToFloatVec<1>(ExtractP, tmp);
+  return *tmp;//*ExtractP->to_float(*ExtractP);
 
 #else
     throw exception(make_error_code(errc::runtime),
