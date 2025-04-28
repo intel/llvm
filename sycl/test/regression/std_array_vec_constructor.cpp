b0fdd94a698ae0d75e25fc8acf8674ed7d46e5cb
Agarwal, Udit <udit.agarwal@intel.com>
2025-04-28 14:16:29 -0700

    Wrap around crtdbg.h instead

--- sycl/test/regression/std_array_vec_constructor.cpp
+++ sycl/test/regression/std_array_vec_constructor.cpp
@@ -2,15 +2,12 @@
 // the constructor.
 // REQUIRES: windows
 
-// RUN: %clangxx -O0 -fsycl -D_DEBUG -shared %s -nostdlib -Xclang --dependent-lib=msvcrtd -fms-runtime-lib=dll_dbg  &> %t.compile.log
-// RUN: FileCheck %s -input-file=%t.compile.log
+// RUN: %clangxx -O0 -fsycl -D_DEBUG -shared %s -nostdlib -Xclang --dependent-lib=msvcrtd -fms-runtime-lib=dll_dbg
 
 #include <sycl/sycl.hpp>
 
 using namespace sycl::ext::oneapi::experimental;
 
-// CHECK-NOT: UnsupportedVarArgFunction: Variadic functions other than 'printf' are not supported in SPIR-V.
-// CHECK-NOT: clang{{.*}} error: llvm-spirv command failed with exit code 23{{.*}}
 auto Reproducer(sycl::queue q, sampled_image_handle imgHanlde) {
   return q.submit([&](sycl::handler &cg) {
     cg.parallel_for(
