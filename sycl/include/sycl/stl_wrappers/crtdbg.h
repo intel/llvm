5c98abc391145f51d4ed69154ea349ab476cca7e
Agarwal, Udit <udit.agarwal@intel.com>
2025-04-28 15:08:53 -0700

    Formatting
    Signed-off-by: Agarwal, Udit <udit.agarwal@intel.com>

--- sycl/include/sycl/stl_wrappers/crtdbg.h
+++ sycl/include/sycl/stl_wrappers/crtdbg.h
@@ -7,24 +7,25 @@
 //===----------------------------------------------------------------------===//
 
 // When std::array is used in device code, MSVC's STL uses _CrtDbgReport
-// function with variable arguments, when compiled in debug mode (-D_DEBUG flag).
+// function with variable arguments, when compiled in debug mode (-D_DEBUG
+// flag).
 
-// Variable argument functions are not supported in SPIR-V, and the compilation fails with
-// llvm-spirv error:
-// UnsupportedVarArgFunction: Variadic functions other than 'printf' are not supported in SPIR-V.
+// Variable argument functions are not supported in SPIR-V, and the compilation
+// fails with llvm-spirv error: UnsupportedVarArgFunction: Variadic functions
+// other than 'printf' are not supported in SPIR-V.
 
-// As a workaround, in this wrapper, we define our own variable templated _CrtDbgReport
-// which overrides the variable argument _CrtDbgReport function declaration in crtdbg.h.
+// As a workaround, in this wrapper, we define our own variable templated
+// _CrtDbgReport which overrides the variable argument _CrtDbgReport function
+// declaration in crtdbg.h.
 
 // The variable templated _CrtDbgReport function has to be declared before the
-// crtdbg.h header is included, and that's why we have this STL wrapper instead of
-// declaring the _CrtDbgReport function in SYCL headers.
+// crtdbg.h header is included, and that's why we have this STL wrapper instead
+// of declaring the _CrtDbgReport function in SYCL headers.
 
 #pragma once
 
 #if defined(__SYCL_DEVICE_ONLY__)
-template <typename... Ts>
-int _CrtDbgReport(Ts...) { return 0; }
+template <typename... Ts> int _CrtDbgReport(Ts...) { return 0; }
 #endif
 
 #if defined(__has_include_next)
