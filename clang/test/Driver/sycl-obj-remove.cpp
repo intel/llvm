805770e2b3b48db075c92b7c403c8b48973a4c27
Michael D Toguchi <michael.d.toguchi@intel.com>
2025-04-29 10:42:46 -0700

    Update test, clean up predefine macro name, add -c and general test

--- clang/test/Driver/sycl-obj-remove.cpp
+++ clang/test/Driver/sycl-obj-remove.cpp
@@ -2,15 +2,25 @@
 
 // REQUIRES: system-linux
 
+/// Force failure during device compilation
 // RUN: touch %t.o
-// RUN: not %clangxx -fsycl -Xsycl-target-frontend -DCOMPILE_HOST_FAIL=1 -o %t.o %s
+// RUN: not %clangxx -fsycl -Xsycl-target-frontend -DCOMPILE_FAIL=1 -c -o %t.o %s
 // RUN: not ls %t.o
 
 // RUN: touch %t.o
-// RUN: not %clangxx --offload-new-driver -fsycl -Xsycl-target-frontend -DCOMPILE_HOST_FAIL=1 -o %t.o %s
+// RUN: not %clangxx --offload-new-driver -fsycl -Xsycl-target-frontend -DCOMPILE_FAIL=1 -c -o %t.o %s
+// RUN: not ls %t.o
+
+/// Force failure during compilation
+// RUN: touch %t.o
+// RUN: not %clangxx -fsycl -DCOMPILE_FAIL=1 -c -o %t.o %s
+// RUN: not ls %t.o
+
+// RUN: touch %t.o
+// RUN: not %clangxx --offload-new-driver -fsycl -DCOMPILE_FAIL=1 -c -o %t.o %s
 // RUN: not ls %t.o
 
 void func(){};
-#ifdef COMPILE_HOST_FAIL
+#ifdef COMPILE_FAIL
 #error FAIL
-#endif // COMPILE_HOST_FAIL
+#endif // COMPILE_FAIL
