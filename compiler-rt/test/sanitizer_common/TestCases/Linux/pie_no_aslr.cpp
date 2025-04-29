d9ddaa5f74744a997fd3fc4f8ac1be07f7deff16
Wu, Yingcong <yingcong.wu@intel.com>
2025-04-29 08:18:42 +0200

    disable test

--- compiler-rt/test/sanitizer_common/TestCases/Linux/pie_no_aslr.cpp
+++ compiler-rt/test/sanitizer_common/TestCases/Linux/pie_no_aslr.cpp
@@ -1,3 +1,6 @@
+// There is a bug with Linux kernel with la57 https://lore.kernel.org/all/20230803151609.22141-1-kirill.shutemov@linux.intel.com/ , some of our machines are still affected, disable this test for now.
+// UNSUPPORTED: true
+
 // RUN: %clangxx %s -pie -fPIE -o %t && %run setarch x86_64 -R %t
 // REQUIRES: x86_64-target-arch
 
