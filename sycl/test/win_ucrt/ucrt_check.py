344c638446c25dbdbfafd84f01e3efef3fe68771
Larsen, Steffen <steffen.larsen@intel.com>
2025-04-29 03:52:14 -0700

    Fix ucrt check and ignore OpenCL.dll
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- sycl/test/win_ucrt/ucrt_check.py
+++ sycl/test/win_ucrt/ucrt_check.py
@@ -11,26 +11,36 @@ import subprocess
 import sys
 
 
+# Some libraries are excluded from the check:
+#  * OpenCL.dll: The OpenCL ICD loader is ignored as it is generally not part of
+#                the compiler release packages.
+exclude_list = {"OpenCL.dll"}
+
 def check_file(filepath):
     filename, file_ext = os.path.splitext(entry.name)
 
     # Only consider .dll or .lib files.
     if not (file_ext == ".dll" or file_ext == ".lib"):
-        return
+        return 0
 
     has_debug_postfix = filename.endswith("d") or filename.endswith("d-preview")
     dep_output = subprocess.run(
         ["dumpbin", "/dependents", filepath], shell=False, capture_output=True
     )
 
-    if str(dep_output.stdout).find("ucrtbased.dll"):
+    if str(dep_output.stdout).find("ucrtbased.dll") != -1:
         if not has_debug_postfix:
             print("Unexpected use of ucrtbased.dll:", filepath)
-            sys.exit(1)
-    elif str(dep_output.stdout).find("ucrtbase.dll"):
+            return 1
+    elif str(dep_output.stdout).find("ucrtbase.dll") != -1:
         if has_debug_postfix:
             print("Unexpected use of ucrtbase.dll:", filepath)
-            sys.exit(1)
+            return 1
+    elif str(dep_output.stdout).find("api-ms-win-crt-") != -1:
+        if has_debug_postfix:
+            print("Unexpected use of api-ms-win-crt-*.dll: ", filepath)
+            return 1
+    return 0
 
 
 if __name__ == "__main__":
@@ -39,7 +49,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Scan the path for library files.
+    failures = 0
     with os.scandir(args.target_path) as it:
         for entry in it:
             if entry.is_file():
-                check_file(entry.name)
+                failures += check_file(entry.name)
+    if failures > 0:
+        exit(failures)
