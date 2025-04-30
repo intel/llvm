39425c995c5d84ecb7b37337ca146eed29938854
Callum Fare <callum@codeplay.com>
2025-04-30 17:36:25 +0100

    Add missing license text

--- unified-runtime/source/adapters/offload/event.cpp
+++ unified-runtime/source/adapters/offload/event.cpp
@@ -1,3 +1,13 @@
+//===----------- event.cpp - LLVM Offload Adapter  ------------------------===//
+//
+// Copyright (C) 2025 Intel Corporation
+//
+// Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM
+// Exceptions. See LICENSE.TXT
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include <OffloadAPI.h>
 #include <ur_api.h>
 
