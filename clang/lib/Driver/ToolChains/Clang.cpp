382b3581d3f709e04d805aea855dfb9b7817e7d4
Michael Toguchi <michael.d.toguchi@intel.com>
2025-05-02 01:44:08 -0700

    [Driver][SYCL] Do not set workaround predefines with /MDd (#18252)
    We are currently setting 2 pre-defines which workaound a compilation
    issue: -D_CONTAINER_DEBUG_LEVEL=0 and -D_ITERATOR_DEBUG_LEVEL=0. We can
    no longer rely on these macros as _CONTAINER_DEBUG_LEVEL will be going
    away with a future release of Visual Studio.
    
    The issues that this worked around are detailed in our release notes as
    a known issue:
    https://github.com/intel/llvm/blob/aa2c876f529f942943afaf9395694fe07a7f6121/sycl/ReleaseNotes.md?plain=1#L2700
    
    Remove these workarounds in preparation for the upcoming MSVS release.

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -5346,11 +5346,6 @@ static void ProcessVSRuntimeLibrary(const ToolChain &TC, const ArgList &Args,
       CmdArgs.push_back("-D_MT");
     if (Defines & addDLL && !isSPIROrSPIRV)
       CmdArgs.push_back("-D_DLL");
-    // for /MDd with spir targets
-    if ((Defines & addDLL) && (Defines & addDEBUG) && isSPIROrSPIRV) {
-      CmdArgs.push_back("-D_CONTAINER_DEBUG_LEVEL=0");
-      CmdArgs.push_back("-D_ITERATOR_DEBUG_LEVEL=0");
-    }
   };
   StringRef FlagForCRT;
   switch (RTOptionID) {
