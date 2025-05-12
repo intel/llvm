560faf12d11f6bdf45ea0970db6c50f77e3bb94a
Li, Ian <ian.li@intel.com>
2025-05-12 12:12:46 -0700

    Add test to check that std::array does not cause problems in device code

---
+++ sycl/test-e2e/Regression/std_array_in_device_code.cpp
@@ -0,0 +1,24 @@
+// REQUIRES: system-windows
+
+// RUN: clang-cl -fsycl -o %t.exe %s /Od /MDd /Zi /EHsc 2>&1 | FileCheck --allow-empty %s
+ 
+// Check that std::array in device code does not result in compilation errors:
+
+// CHECK-NOT: error: SYCL kernel cannot call an undefined function without SYCL_EXTERNAL attribute
+// CHECK-NOT: note: '_invalid_parameter' declared here
+// CHECK-NOT: Undefined function _invalid_parameter found in
+
+// RUN: %t.exe
+
+#include <sycl/sycl.hpp>
+
+int main() {
+  sycl::queue q;
+
+  q.single_task([=]() {
+    std::array<int, 5> arr = {1, 2, 0, 4, 5};
+    arr[2] = 3;
+  });
+
+  return 0;
+}
