0c2d5fe2cd0a91db457a568502e4be9e760ec01f
Gojska, Grzegorz <grzegorz.gojska@intel.com>
2025-05-14 19:42:17 +0200

    added missing test files

---
+++ sycl/test-e2e/Matrix/SG32/joint_matrix_bfloat16_accumulator.cpp
@@ -0,0 +1,32 @@
+//==-------- SG32/joint_matrix_bfloat16_accumulator.cpp  - DPC++ joint_matrix----------- ----==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+// SG size = 32 is not currently supported for SYCL Joint Matrix by IGC on DG2
+// UNSUPPORTED: gpu-intel-dg2
+
+// UNSUPPORTED: target-nvidia, target-amd
+// UNSUPPORTED-INTENDED: aspect-ext_intel_matrix isn't currently supported for
+// other triples
+
+// REQUIRES: aspect-ext_intel_matrix
+// REQUIRES-INTEL-DRIVER: lin: 27501, win: 101.4943
+
+// XFAIL: gpu
+
+// RUN: %{build} -o %t.out
+// RUN: %{run} %t.out
+// RUN: %if gpu %{ env IGC_JointMatrixLoadStoreOpt=2 %{run} %t.out %}
+// RUN: %if gpu %{ env IGC_JointMatrixLoadStoreOpt=1 %{run} %t.out %}
+// RUN: %if gpu %{ env IGC_JointMatrixLoadStoreOpt=0 %{run} %t.out %}
+
+
+#include "common.hpp"
+
+#define SG_SZ 32
+#define ACC_BFLOAT16 true
+
+#include "joint_matrix_bfloat16_impl.hpp"
