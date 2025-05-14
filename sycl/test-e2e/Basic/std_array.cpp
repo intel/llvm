intel/llvm

commit 570f74da299e14fe9d66ae379cfd975a79a079ff

[SYCL] Provide `_invalid_parameter` symbol in device code on Windows (#18400)
Provides a fix for #18426: Provides symbols necessary for `std::array`
to work in device code in debug mode on Windows.

---------

Co-authored-by: aelovikov-intel <andrei.elovikov@intel.com>

sycl/test-e2e/Basic/std_array.cpp
@@ -1,10 +1,9 @@
+// Check that std::array is supported on device in debug mode on Windows.
+
 // REQUIRES: windows
 
-// RUN: not clang-cl -fsycl -o %t.exe %s /Od /MDd /Zi /EHsc 2>&1 | FileCheck %s
-
-// FIXME: This code should have compiled cleanly.
-// CHECK: error: SYCL kernel cannot call an undefined function without SYCL_EXTERNAL attribute
-// CHECK: note: '_invalid_parameter' declared here
+// RUN: %clangxx --driver-mode=cl -fsycl -o %t.exe %s /Od /MDd /Zi /EHsc
+// RUN: %{run} %t.exe
 
 #include <sycl/queue.hpp>
 

sycl/include/sycl/stl_wrappers/corecrt.h
@@ -1,0 +1,51 @@
+//==---------------- Wrapper around corecrt.h ------------------------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// When std::array is used in device code, MSVC's STL uses a _invalid_parameter
+// function. This causes issue when _invalid_parameter is invoked from device
+// code:
+
+// 1. `_invalid_parameter` is provided via ucrtbased.dll at runtime: DLLs are
+//    not loaded for device code, thus causing undefined symbol errors.
+
+// 2. MSVC's STL never defined the function as SYCL_EXTERNAL, errors are thrown
+//    when device code tries to invoke `_invalid_parameter`.
+
+// As a workaround, this wrapper wraps around corecrt.h and defines a custom
+// _invalid_parameter for device code compilation.
+
+// This new SYCL_EXTERNAL definition of _invalid_parameter has to be declared
+// before corecrt.h is included: Thus, we have this STL wrapper instead of
+// declaring _invalid_parameter function in SYCL headers.
+
+#pragma once
+
+#if defined(__SYCL_DEVICE_ONLY__) && defined(_DEBUG)
+
+#include <cstdint>                            // For uintptr_t
+#include <sycl/detail/defines_elementary.hpp> // For __DPCPP_SYCL_EXTERNAL
+
+extern "C" __DPCPP_SYCL_EXTERNAL void __cdecl _invalid_parameter(
+    wchar_t const *, wchar_t const *, wchar_t const *, unsigned int,
+    uintptr_t) {
+  // Do nothing when called in device code
+}
+
+#endif
+
+#if defined(__has_include_next)
+// GCC/clang support go through this path.
+#include_next <corecrt.h>
+#else
+// MSVC doesn't support "#include_next", so we have to be creative.
+// Our header is located in "stl_wrappers/corecrt.h" so it won't be picked by
+// the aforementioned include. MSVC's installation, on the other hand, has the
+// layout where the following would result in the <corecrt.h> we want. This is
+// obviously hacky, but the best we can do...
+#include <../ucrt/corecrt.h>
+#endif

commit cbdd143e2dde0b417f5989f9cdfa3ecadf9804a6

[SYCL-E2E] Disable Basic/std_array.cpp for GEN12 on Windows
Post-commit failure introduced by
https://github.com/intel/llvm/pull/18400 and tracked in
https://github.com/intel/llvm/issues/18458.

sycl/test-e2e/Basic/std_array.cpp
@@ -1,6 +1,8 @@
 // Check that std::array is supported on device in debug mode on Windows.
 
 // REQUIRES: windows
+// UNSUPPORTED: gpu-intel-gen12
+// UNSUPPORTED-TRACKER: https://github.com/intel/llvm/issues/18458
 
 // RUN: %clangxx --driver-mode=cl -fsycl -o %t.exe %s /Od /MDd /Zi /EHsc
 // RUN: %{run} %t.exe