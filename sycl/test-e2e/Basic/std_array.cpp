7f68d89cd14d04af028affae351326b4a99be93b
Li, Ian <ian.li@intel.com>
2025-05-15 09:07:20 -0700

    funny

--- sycl/test-e2e/Basic/std_array.cpp
+++ sycl/test-e2e/Basic/std_array.cpp
@@ -2,7 +2,7 @@
 
 // REQUIRES: windows
 
-// RUN: %clangxx --driver-mode=cl -fsycl -o %t.exe %s /Od /MDd /Zi /EHsc
+// RUN: %clangxx --driver-mode=cl -fsycl -o %t.exe %s 
 // RUN: %{run} %t.exe
 
 #include <sycl/queue.hpp>
@@ -12,11 +12,11 @@ int main() {
 
   q.single_task([=]() {
     int tmp = 5;
-    tmp += 5;
     // std::array<int, 5> arr;// = {1, 2, 0, 4, 5};
 	//int tmp = arr[0];
     // arr[2] = 3;
   });
 
+///Od /MDd /Zi /EHsc
   return 0;
 }
