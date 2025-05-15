660a3013abd677440aafafd4cb7790f44db7aa5e
Nicolas Miller <nicolas.miller@codeplay.com>
2025-05-15 16:44:59 +0100

    [UR] Fix test

--- sycl/test-e2e/Adapters/max-local-mem-size.cpp
+++ sycl/test-e2e/Adapters/max-local-mem-size.cpp
@@ -1,11 +1,13 @@
-// REQUIRES: cuda
+// REQUIRES: cuda || hip
+
+// DEFINE: %{env_var} = %if cuda %{UR_CUDA_MAX_LOCAL_MEM_SIZE %} %else %{UR_HIP_MAX_LOCAL_MEM_SIZE%}
 
 // RUN: %{build} -o %t.out
-// RUN: %{run} SYCL_PI_CUDA_MAX_LOCAL_MEM_SIZE=0 %t.out 2>&1 | FileCheck --check-prefixes=CHECK-ZERO %s
-// RUN: %{run} SYCL_PI_CUDA_MAX_LOCAL_MEM_SIZE=100000000 %t.out 2>&1 | FileCheck --check-prefixes=CHECK-OVERALLOCATE %s
+// RUN: %{run} %{env_var}=0 %t.out 2>&1 | FileCheck --check-prefixes=CHECK-ZERO %s
+// RUN: %{run} %{env_var}=100000000 %t.out 2>&1 | FileCheck --check-prefixes=CHECK-OVERALLOCATE %s
 
-//==---------------------- cuda-max-local-mem-size.cpp --------------------===//
-//==--- SYCL test to test SYCL_PI_CUDA_MAX_LOCAL_MEM_SIZE env var----------===//
+//==------------------------ max-local-mem-size.cpp -----------------------===//
+//==--- SYCL test to test UR_{CUDA,HIP}_MAX_LOCAL_MEM_SIZE env var---------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -29,6 +31,6 @@ int main() {
   } catch (const std::exception &e) {
     std::puts(e.what());
   }
-  // CHECK-ZERO: Local memory for kernel exceeds the amount requested using SYCL_PI_CUDA_MAX_LOCAL_MEM_SIZE
+  // CHECK-ZERO: Local memory for kernel exceeds the amount requested
   // CHECK-OVERALLOCATE: Excessive allocation of local memory on the device
 }
