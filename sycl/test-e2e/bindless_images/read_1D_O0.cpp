5f0230dab198b991f95f05f847b848b4baffddb8
JackAKirk <jack.kirk@codeplay.com>
2025-05-15 12:22:09 +0100

    Add l0 failing O0 bindless tests
    Signed-off-by: JackAKirk <jack.kirk@codeplay.com>

--- sycl/test-e2e/bindless_images/read_1D_O0.cpp
+++ sycl/test-e2e/bindless_images/read_1D_O0.cpp
@@ -1,6 +1,7 @@
 // REQUIRES: aspect-ext_oneapi_bindless_images
+// XFAIL: level_zero
 
-// RUN: %{build} -o %t.out
+// RUN: %{build} %O0 -o %t.out
 // RUN: %{run-unfiltered-devices} env NEOReadDebugKeys=1 UseBindlessMode=1 UseExternalAllocatorForSshAndDsh=1 %t.out
 
 // Uncomment to print additional test information
@@ -9,3 +10,4 @@
 #include "read_1D.hpp"
 
 int main() { return test(); }
+
--- sycl/test-e2e/bindless_images/sampling_1D_O0.cpp
+++ sycl/test-e2e/bindless_images/sampling_1D_O0.cpp
@@ -1,11 +1,15 @@
 // REQUIRES: aspect-ext_oneapi_bindless_images
+// XFAIL: level_zero
 
-// RUN: %{build} -o %t.out
+// RUN: %{build} %O0 -o %t.out
 // RUN: %{run-unfiltered-devices} env NEOReadDebugKeys=1 UseBindlessMode=1 UseExternalAllocatorForSshAndDsh=1 %t.out
+
+// UNSUPPORTED: hip
+// UNSUPPORTED-TRACKER: https://github.com/intel/llvm/issues/17212
 
 // Uncomment to print additional test information
 // #define VERBOSE_PRINT
 
-#include "read_1D.hpp"
+#include "sampling_1D.hpp"
 
 int main() { return test(); }
