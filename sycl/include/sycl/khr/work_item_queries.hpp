bfb2d89ea0c522943a8dedd078a2c33cab3be906
Michael Aziz <michael.aziz@intel.com>
2025-05-16 13:50:48 -0400

    Add license to extension header

--- sycl/include/sycl/khr/work_item_queries.hpp
+++ sycl/include/sycl/khr/work_item_queries.hpp
@@ -1,3 +1,10 @@
+//===-- work_item_queries.hpp --- KHR work item queries extension ---------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 #pragma once
 
 #ifdef __DPCPP_ENABLE_UNFINISHED_KHR_EXTENSIONS
