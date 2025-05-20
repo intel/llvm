402359fd1206b8448a20f2d2689788e34c59d064
Wenju He <wenju.he@intel.com>
2025-05-19 22:17:47 -0700

    [NFC][libspirv] Re-use clc shuffle/shuffle2 implementations for __spirv_ocl_shuffle/shuffle2
    llvm-diff on libspirv-amdgcn--amdhsa.bc and libspirv-nvptx64--nvidiacl.bc
    only shows __clc_get_el_* symbols are removed.

--- libclc/libspirv/include/libspirv/misc/shuffle2.h
+++ libclc/libspirv/include/libspirv/misc/shuffle2.h
@@ -1,4 +1,4 @@
-//===----------------- generic/lib/misc/shuffle.cl ------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,14 +6,14 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include <clc/misc/clc_shuffle.h>
-#include <libspirv/spirv.h>
+#define __CLC_FUNCTION __spirv_ocl_shuffle2
 
-#define FUNCTION __spirv_ocl_shuffle
-#define __CLC_FUNCTION(x) __clc_shuffle
-
-#define __CLC_BODY <clc/misc/shuffle_def.inc>
+// Integer-type decls
+#define __CLC_BODY <clc/misc/shuffle_decl.inc>
 #include <clc/integer/gentype.inc>
 
-#define __CLC_BODY <clc/misc/shuffle_def.inc>
+// Floating-point decls
+#define __CLC_BODY <clc/misc/shuffle_decl.inc>
 #include <clc/math/gentype.inc>
+
+#undef __CLC_FUNCTION
