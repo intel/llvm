c5469cda52b487853980cb4fd6627d53cad7667a
Sarnie, Nick <nick.sarnie@intel.com>
2025-05-20 11:06:10 -0700

    fix tests
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- sycl/test-e2e/Matrix/joint_matrix_bf16_fill_k_cache_arg_dim.cpp
+++ sycl/test-e2e/Matrix/joint_matrix_bf16_fill_k_cache_arg_dim.cpp
@@ -8,7 +8,7 @@
 // UNSUPPORTED: target-nvidia, target-amd
 // UNSUPPORTED-INTENDED: aspect-ext_intel_matrix isn't currently supported for
 // other triples
-// XFAIL: run-mode && gpu
+// XFAIL: run-mode && gpu && !arch-intel_gpu_bmg_g21
 // XFAIL-TRACKER: CMPLRLLVM-66371
 
 // REQUIRES: aspect-ext_intel_matrix
@@ -22,8 +22,6 @@
 // test.
 // XFAIL: gpu-intel-dg2 && run-mode
 // XFAIL-TRACKER: GSD-10510
-// XFAIL: arch-intel_gpu_bmg_g21
-// XFAIL-TRACKER: https://github.com/intel/llvm/issues/16922
 
 #include "common.hpp"
 #include "joint_matrix_bf16_fill_k_cache_impl.hpp"
