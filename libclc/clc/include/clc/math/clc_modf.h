bdfe2d9ba4097ae707bd034ec3a894628f2fb961
Fraser Cormack <fraser@codeplay.com>
2025-05-22 14:10:19 +0100

    [libclc] Reduce diff with upstream in CLC headers
    In a recent sycl-web PR I mistakenly introduced extra includes to some
    CLC functions to account for the generic address space. This was because
    the macro defines weren't at that time being pulled in by
    math/gentype.inc (or any CLC headers) as they were in my upstream work.
    
    After pulling in the upstream generic address space support, the defines
    are now in a common place across the whole of libclc and are
    automatically included by all of these files. Keeping the redundant
    includes around only serves to increases our diff with upstream.
    
    This commit takes the opportunity to clean up some whitespace diffs at
    the same time.

--- libclc/clc/include/clc/math/clc_modf.h
+++ libclc/clc/include/clc/math/clc_modf.h
@@ -9,8 +9,6 @@
 #ifndef __CLC_MATH_CLC_MODF_H__
 #define __CLC_MATH_CLC_MODF_H__
 
-#include <clc/internal/clc.h>
-
 #define __CLC_FUNCTION __clc_modf
 #define __CLC_BODY <clc/math/unary_decl_with_ptr.inc>
 #include <clc/math/gentype.inc>
