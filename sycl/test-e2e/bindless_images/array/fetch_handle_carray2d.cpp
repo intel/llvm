7d07c29dd3bc9f40662357ce40380005e18d55d0
JackAKirk <jack.kirk@codeplay.com>
2025-05-29 15:17:20 +0100

    Fix CI failures, address review comments
    Signed-off-by: JackAKirk <jack.kirk@codeplay.com>

--- sycl/test-e2e/bindless_images/array/fetch_handle_carray2d.cpp
+++ sycl/test-e2e/bindless_images/array/fetch_handle_carray2d.cpp
@@ -1,12 +1,17 @@
 // REQUIRES: aspect-ext_oneapi_bindless_images
 // XFAIL: level_zero
+// XFAIL-TRACKER: https://github.com/intel/llvm/issues/18727
 // UNSUPPORTED: hip
 // UNSUPPORTED-INTENDED: Undetermined issue in 'create_image' in this test.
 
 // RUN: %{build} -o %t.out
 // RUN: %{run} env NEOReadDebugKeys=1 UseBindlessMode=1 UseExternalAllocatorForSshAndDsh=1 %t.out
 
-#include <sycl/sycl.hpp>
+#include <sycl/detail/core.hpp>
+#include <sycl/ext/oneapi/bindless_images.hpp>
+#include <sycl/usm.hpp>
+
+namespace syclexp = sycl::ext::oneapi::experimental;
 
 int main() {
 
@@ -21,56 +26,72 @@ int main() {
   }
 
   // Image descriptor - can use the same for both images
-  sycl::ext::oneapi::experimental::image_descriptor desc(
-      sycl::range{width}, 1, sycl::image_channel_type::fp32);
+  syclexp::image_descriptor desc(sycl::range{width}, 1,
+                                 sycl::image_channel_type::fp32);
 
   // Extension: returns the device pointer to the allocated memory
-  sycl::ext::oneapi::experimental::image_mem imgMemoryIn(desc, q);
-  sycl::ext::oneapi::experimental::image_mem imgMemoryOut(desc, q);
+  syclexp::image_mem imgMemoryIn(desc, q);
+  syclexp::image_mem imgMemoryOut(desc, q);
 
-  // Extension: create the image and return the handle
-  sycl::ext::oneapi::experimental::unsampled_image_handle imgIn =
-      sycl::ext::oneapi::experimental::create_image(imgMemoryIn, desc, q);
-  sycl::ext::oneapi::experimental::unsampled_image_handle imgOut =
-      sycl::ext::oneapi::experimental::create_image(imgMemoryOut, desc, q);
-
-  void *imageHandlePtrGen = static_cast<void *>(sycl::malloc_device(
-      sizeof(sycl::ext::oneapi::experimental::unsampled_image_handle), q));
-  q.memcpy(static_cast<void *>(imageHandlePtrGen),
-           static_cast<const void *>(&imgIn),
-           sizeof(sycl::ext::oneapi::experimental::unsampled_image_handle));
-  q.wait_and_throw();
   q.ext_oneapi_copy(dataIn.data(), imgMemoryIn.get_handle(), desc);
   q.wait_and_throw();
-  void *imageHandlePtrPtrGen = static_cast<void *>(sycl::malloc_device(
-      sizeof(sycl::ext::oneapi::experimental::unsampled_image_handle *), q));
+  // Extension: create the image and return the handle
+  syclexp::unsampled_image_handle imgIn =
+      syclexp::create_image(imgMemoryIn, desc, q);
+  syclexp::unsampled_image_handle imgOut =
+      syclexp::create_image(imgMemoryOut, desc, q);
+
+  // Copy the input data to the image_mem of the device unsampled_image_handle
+  q.ext_oneapi_copy(dataIn.data(), imgMemoryIn.get_handle(), desc);
+  q.wait_and_throw();
+
+  // Allocate an unsampled_image_handle manually instead of using create_image
+  // The implied purpose of this appears to be that a valid device void* is
+  // returned which points to the image_handle
+  void *imageHandlePtrGen =
+      sycl::malloc_device(sizeof(syclexp::unsampled_image_handle), q);
+
+  // Copy the create_image returned device unsampled_image_handle to the contents
+  // of the void* pointing to the manually created unsampled_image_handle
+  q.memcpy(static_cast<void *>(imageHandlePtrGen),
+           static_cast<const void *>(&imgIn),
+           sizeof(syclexp::unsampled_image_handle));
+
+  q.wait_and_throw();
+
+  // Allocate a device generic pointer pointing to an unsampled_image_handle*
+  void *imageHandlePtrPtrGen =
+      sycl::malloc_device(sizeof(syclexp::unsampled_image_handle *), q);
+
+  // Copy the address of the manually allocated unsampled_image_handle to the
+  // contents of the generic device pointer allocated above
   q.memcpy(static_cast<void *>(imageHandlePtrPtrGen),
            static_cast<const void *>(&imageHandlePtrGen),
-           sizeof(sycl::ext::oneapi::experimental::unsampled_image_handle *));
+           sizeof(syclexp::unsampled_image_handle *));
+
   q.wait_and_throw();
 
   q.submit([&](sycl::handler &cgh) {
-    cgh.parallel_for(sycl::nd_range<1>{{width}, {width}}, [=](sycl::nd_item<1>
-                                                                  it) {
-      sycl::ext::oneapi::experimental::unsampled_image_handle *
-          *imageHandlePtrPtr = static_cast<
-              sycl::ext::oneapi::experimental::unsampled_image_handle **>(
-              imageHandlePtrPtrGen);
-      sycl::ext::oneapi::experimental::unsampled_image_handle *imageHandlePtr =
-          static_cast<
-              sycl::ext::oneapi::experimental::unsampled_image_handle *>(
-              imageHandlePtrPtr[0]);
-      sycl::ext::oneapi::experimental::unsampled_image_handle imageHandle =
-          imageHandlePtr[0];
+    cgh.parallel_for(
+        sycl::nd_range<1>{{width}, {width}}, [=](sycl::nd_item<1> it) {
+          syclexp::unsampled_image_handle **imageHandlePtrPtr =
+              static_cast<syclexp::unsampled_image_handle **>(
+                  imageHandlePtrPtrGen);
+          // Dereference the generic pointer to the unsampled_image_handle
+          // pointer
+          syclexp::unsampled_image_handle *imageHandlePtr =
+              static_cast<syclexp::unsampled_image_handle *>(
+                  imageHandlePtrPtr[0]);
+          // Dereference the unsampled_image_handle pointer
+          syclexp::unsampled_image_handle imageHandle = imageHandlePtr[0];
 
-      size_t dim0 = it.get_local_id(0);
-      // Extension: read image data from handle
-      float pixel = sycl::ext::oneapi::experimental::fetch_image<float>(
-          imageHandle, int(dim0));
+          size_t dim0 = it.get_local_id(0);
+          // Extension: read image data from handle
+          float pixel = syclexp::fetch_image<float>(imageHandle, int(dim0));
 
-      // Extension: write to image data using handle
-      sycl::ext::oneapi::experimental::write_image(imgOut, int(dim0), pixel);
-    });
+          // Extension: write to image data using handle
+          syclexp::write_image(imgOut, int(dim0), pixel);
+        });
   });
 
   q.wait_and_throw();
@@ -82,8 +103,8 @@ int main() {
   q.wait_and_throw();
 
   // Cleanup
-  sycl::ext::oneapi::experimental::destroy_image_handle(imgIn, q);
-  sycl::ext::oneapi::experimental::destroy_image_handle(imgOut, q);
+  syclexp::destroy_image_handle(imgIn, q);
+  syclexp::destroy_image_handle(imgOut, q);
   sycl::free(imageHandlePtrGen, q);
   sycl::free(imageHandlePtrPtrGen, q);
 
