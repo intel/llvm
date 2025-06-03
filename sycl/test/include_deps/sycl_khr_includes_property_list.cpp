d4fb7e07383d0611e938e8b4602d47f7dda99b7b
Alexey Sachkov <alexey.sachkov@intel.com>
2025-06-03 11:47:59 +0200

    Add content for the remaining headers

--- sycl/test/include_deps/sycl_khr_includes_property_list.cpp
+++ sycl/test/include_deps/sycl_khr_includes_property_list.cpp
@@ -5,4 +5,12 @@
 // CHECK-LABEL: Dependencies for <sycl/khr/includes/property_list>:
 // CHECK-NEXT: /dev/null: /dev/null
 // CHECK-NEXT: khr/includes/property_list
+// CHECK-NEXT: property_list.hpp
+// CHECK-NEXT: detail/property_helper.hpp
+// CHECK-NEXT: detail/property_list_base.hpp
+// CHECK-NEXT: exception.hpp
+// CHECK-NEXT: detail/defines_elementary.hpp
+// CHECK-NEXT: detail/export.hpp
+// CHECK-NEXT: detail/string.hpp
+// CHECK-NEXT: properties/property_traits.hpp
 // CHECK-EMPTY:
--- sycl/test/include_deps/sycl_khr_includes_span.cpp
+++ sycl/test/include_deps/sycl_khr_includes_span.cpp
@@ -5,4 +5,11 @@
 // CHECK-LABEL: Dependencies for <sycl/khr/includes/span>:
 // CHECK-NEXT: /dev/null: /dev/null
 // CHECK-NEXT: khr/includes/span
+// CHECK-NEXT: sycl_span.hpp
+// CHECK-NEXT: stl_wrappers/cassert
+// CHECK-NEXT: stl_wrappers/assert.h
+// CHECK-NEXT: __spirv/spirv_vars.hpp
+// CHECK-NEXT: __spirv/spirv_types.hpp
+// CHECK-NEXT: detail/defines.hpp
+// CHECK-NEXT: detail/defines_elementary.hpp
 // CHECK-EMPTY:
