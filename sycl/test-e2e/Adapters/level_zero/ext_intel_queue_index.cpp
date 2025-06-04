c716b2f3810a3544455ae5e7e26c31894650d096
Li, Ian <ian.li@intel.com>
2025-06-04 13:28:36 -0700

    Use CCS patition of 2 to see if it fixes BMG tests

--- sycl/test-e2e/Adapters/level_zero/ext_intel_queue_index.cpp
+++ sycl/test-e2e/Adapters/level_zero/ext_intel_queue_index.cpp
@@ -4,7 +4,7 @@
 // XFAIL: gpu-intel-pvc-1T
 // XFAIL-TRACKER: https://github.com/intel/llvm/issues/15699
 
-// XFAIL: linux && run-mode && (arch-intel_gpu_bmg_g21 || gpu-intel-dg2) && !igc-dev
+// XFAIL: linux && run-mode && gpu-intel-dg2 && !igc-dev
 // XFAIL-TRACKER: https://github.com/intel/llvm/issues/18576
 
 // RUN: %{build} -o %t.out
@@ -18,7 +18,7 @@
 // In the interim, these are the environment vars that must be set to get cslice
 // or the extra level of partition_by_affinity_domain with the "EXPOSE_" env
 // var.
-// DEFINE: %{setup_env} = env ZE_FLAT_DEVICE_HIERARCHY=COMPOSITE ZE_AFFINITY_MASK=0 ZEX_NUMBER_OF_CCS=0:4
+// DEFINE: %{setup_env} = env ZE_FLAT_DEVICE_HIERARCHY=COMPOSITE ZE_AFFINITY_MASK=0 ZEX_NUMBER_OF_CCS=0:2
 
 // RUN: %{setup_env} env UR_L0_DEBUG=1 %{run} %t.out 2>&1 | FileCheck %s --check-prefixes=CHECK-PVC
 //
--- sycl/test-e2e/Adapters/sycl-ls-uuid-subdevs.cpp
+++ sycl/test-e2e/Adapters/sycl-ls-uuid-subdevs.cpp
@@ -2,13 +2,13 @@
  * devices. */
 // REQUIRES:  gpu, level_zero
 
-// XFAIL: linux && run-mode && (arch-intel_gpu_bmg_g21 || gpu-intel-dg2) && !igc-dev
+// XFAIL: linux && run-mode && gpu-intel-dg2 && !igc-dev
 // XFAIL-TRACKER: https://github.com/intel/llvm/issues/18576
 
 // As of now, ZEX_NUMBER_OF_CCS is not working with FLAT hierachy,
 // which is the new default on PVC.
 
-// RUN: %{run-unfiltered-devices} env ONEAPI_DEVICE_SELECTOR="level_zero:*" env ZE_FLAT_DEVICE_HIERARCHY=COMPOSITE env ZEX_NUMBER_OF_CCS=0:4 sycl-ls --verbose | \
+// RUN: %{run-unfiltered-devices} env ONEAPI_DEVICE_SELECTOR="level_zero:*" env ZE_FLAT_DEVICE_HIERARCHY=COMPOSITE env ZEX_NUMBER_OF_CCS=0:2 sycl-ls --verbose | \
 // RUN: FileCheck %s
 
 // CHECK: {{.*}}UUID              : {{.*}}
