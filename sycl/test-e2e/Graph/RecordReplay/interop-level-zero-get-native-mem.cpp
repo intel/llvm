8d62e2ac2b018faf4bb20649a7262a50e08b99b1
andrei.elovikov <aeloviko@smtp.intel.com>
2025-06-04 11:31:50 -0700

    clang-format

--- sycl/test-e2e/Graph/RecordReplay/interop-level-zero-get-native-mem.cpp
+++ sycl/test-e2e/Graph/RecordReplay/interop-level-zero-get-native-mem.cpp
@@ -1,6 +1,7 @@
 // REQUIRES: level_zero, level_zero_dev_kit
 // UNSUPPORTED: ze_debug
-// UNSUPPORTED-INTENDED: Leaks detection is done at UR level and doesn't account for native L0 API calls.
+// UNSUPPORTED-INTENDED: Leaks detection is done at UR level and doesn't account
+// for native L0 API calls.
 // UNSUPPORTED: linux && gpu-intel-dg2 && run-mode && !igc-dev
 // UNSUPPORTED-TRACKER: https://github.com/intel/llvm/issues/18273
 // RUN: %{build} %level_zero_options -o %t.out
