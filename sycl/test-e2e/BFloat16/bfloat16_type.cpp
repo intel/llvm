3a55c3cdcb60f12f3be8aad1db77d477a83a1016
Alexey Sachkov <alexey.sachkov@intel.com>
2025-06-05 12:19:56 +0200

    Simplify bfloat16 test

--- sycl/test-e2e/BFloat16/bfloat16_type.cpp
+++ sycl/test-e2e/BFloat16/bfloat16_type.cpp
@@ -13,14 +13,12 @@
 
 #include "bfloat16_type.hpp"
 
-#include <sycl/platform.hpp>
-
 int main() {
 
 #ifdef USE_CUDA_SM80
   // Special build for SM80 CUDA.
   sycl::device Dev{default_selector_v};
-  if (Dev.get_platform().get_backend() != backend::ext_oneapi_cuda) {
+  if (Dev.get_backend() != backend::ext_oneapi_cuda) {
     std::cout << "Test skipped; CUDA run was not run with CUDA device."
               << std::endl;
     return 0;
