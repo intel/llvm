a1f7e52ca2e7d7473386e883d7981d3b2527aaf0
Callum Fare <callum@codeplay.com>
2025-06-06 15:47:34 +0100

    Don't pass the host device around

--- unified-runtime/source/adapters/offload/context.hpp
+++ unified-runtime/source/adapters/offload/context.hpp
@@ -20,12 +20,9 @@
 struct ur_context_handle_t_ : RefCounted {
   ur_context_handle_t_(ur_device_handle_t hDevice) : Device{hDevice} {
     urDeviceRetain(Device);
-    // For convenience, store the host device in the context
-    OffloadHost = Adapter.HostDevice;
   }
   ~ur_context_handle_t_() { urDeviceRelease(Device); }
 
   ur_device_handle_t Device;
   std::unordered_map<void *, ol_alloc_type_t> AllocTypeMap;
-  ol_device_handle_t OffloadHost;
 };
