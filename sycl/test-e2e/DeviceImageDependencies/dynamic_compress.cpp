5286ad12b00d91af0735d8c684b786582501667b
Agarwal, Udit <udit.agarwal@intel.com>
2025-06-10 23:01:44 +0200

    Add test for linking uncompressed device images with a compressed one.

---
+++ sycl/test-e2e/DeviceImageDependencies/dynamic_compress.cpp
@@ -0,0 +1,25 @@
+// Test device image linking when using dynamic libraries when one of
+// the device image is compressed and the other is not.
+// Modeled after regression seen in CMPLRLLVM-67948.
+
+// UNSUPPORTED: target-nvidia || target-amd
+// REQUIRES: zstd
+
+// DEFINE: %{dynamic_lib_options} = -fsycl %fPIC %shared_lib -fsycl-allow-device-image-dependencies -I %S/Inputs %if windows %{-DMAKE_DLL %}
+// DEFINE: %{dynamic_lib_suffix} = %if windows %{dll%} %else %{so%}
+
+// RUN: %clangxx %{dynamic_lib_options} %S/Inputs/d.cpp                                    -o %T/libdevice_d.%{dynamic_lib_suffix}
+// RUN: %clangxx %{dynamic_lib_options} %S/Inputs/c.cpp %if windows %{%T/libdevice_d.lib%} -o %T/libdevice_c.%{dynamic_lib_suffix}
+// RUN: %clangxx %{dynamic_lib_options} %S/Inputs/b.cpp %if windows %{%T/libdevice_c.lib%} -o %T/libdevice_b.%{dynamic_lib_suffix}
+// RUN: %clangxx %{dynamic_lib_options} %S/Inputs/a.cpp %if windows %{%T/libdevice_b.lib%} -o %T/libdevice_a.%{dynamic_lib_suffix}
+
+// RUN: %clangxx -fsycl --offload-compress %{sycl_target_opts} -fsycl-allow-device-image-dependencies -fsycl-device-code-split=per_kernel %S/Inputs/basic.cpp -o %t.out            \
+// RUN: %if windows                                                                       \
+// RUN:   %{%T/libdevice_a.lib%}                                                          \
+// RUN: %else                                                                             \
+// RUN:   %{-L%T -ldevice_a -ldevice_b -ldevice_c -ldevice_d -Wl,-rpath=%T%}
+
+// RUN: not %{run} %t.out &> FileCheck %s
+
+// CHECK: terminate called after throwing an instance of 'sycl::_V1::exception'
+// CHECK:   what():  No device image found for external symbol _Z6levelAi
