42b108268e8e98eeb1227b3c7041d01f716ee765
JackAKirk <jack.kirk@codeplay.com>
2025-06-11 13:51:57 +0100

    Add XFAIL trackers
    Signed-off-by: JackAKirk <jack.kirk@codeplay.com>

--- sycl/test-e2e/bindless_images/read_1D_O0.cpp
+++ sycl/test-e2e/bindless_images/read_1D_O0.cpp
@@ -1,6 +1,6 @@
 // REQUIRES: aspect-ext_oneapi_bindless_images
 // XFAIL: level_zero && windows
-
+// XFAIL-TRACKER: https://github.com/intel/llvm/issues/18919
 // RUN: %{build} %O0 -o %t.out
 // RUN: %{run-unfiltered-devices} env NEOReadDebugKeys=1 UseBindlessMode=1 UseExternalAllocatorForSshAndDsh=1 %t.out
 
@@ -10,4 +10,3 @@
 #include "read_1D.hpp"
 
 int main() { return test(); }
-
