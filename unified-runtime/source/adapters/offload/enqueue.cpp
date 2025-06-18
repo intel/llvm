d50a581a2a15fef82bdcd72ba539edc18e83801e
Ross Brunton <ross@codeplay.com>
2025-06-18 13:43:32 +0100

    [UR][Offload] Dynamically allocate adapter object and check for errors
    We allow `urAdapterRelease` to be called in a global destructor. To avoid
    any issues with destructor ordering, use `urAdapterGet` and
    `urAdapterRelease` to manage the offload adapter's lifetime rather than
    global init/fini.
    
    In addition, `adapter::init()` now actually checks and returns any
    error from iterating devices.

--- unified-runtime/source/adapters/offload/enqueue.cpp
+++ unified-runtime/source/adapters/offload/enqueue.cpp
@@ -111,7 +111,7 @@ UR_APIEXPORT ur_result_t UR_APICALL urEnqueueMemBufferRead(
 
   void *DevPtr = std::get<BufferMem>(hBuffer->Mem).Ptr;
 
-  olMemcpy(hQueue->OffloadQueue, pDst, Adapter.HostDevice, DevPtr + offset,
+  olMemcpy(hQueue->OffloadQueue, pDst, Adapter->HostDevice, DevPtr + offset,
            hQueue->OffloadDevice, size, phEvent ? &EventOut : nullptr);
 
   if (blockingRead) {
@@ -143,7 +143,7 @@ UR_APIEXPORT ur_result_t UR_APICALL urEnqueueMemBufferWrite(
 
   auto Res =
       olMemcpy(hQueue->OffloadQueue, DevPtr + offset, hQueue->OffloadDevice,
-               pSrc, Adapter.HostDevice, size, phEvent ? &EventOut : nullptr);
+               pSrc, Adapter->HostDevice, size, phEvent ? &EventOut : nullptr);
   if (Res) {
     return offloadResultToUR(Res);
   }
