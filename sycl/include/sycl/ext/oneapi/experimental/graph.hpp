c9d6bb880e69955816a6f3642dca18a62a923ec0
Konrad Kusiak <konrad.kusiak@codeplay.com>
2025-06-19 21:10:55 +0100

    Further splitted command_graph into modifiable and executable

--- sycl/include/sycl/ext/oneapi/experimental/graph.hpp
+++ sycl/include/sycl/ext/oneapi/experimental/graph.hpp
@@ -9,5 +9,7 @@
 #pragma once
 
 #include "graph/command_graph.hpp"
+#include "graph/executable_graph.hpp"
 #include "graph/graph_dynamic.hpp"
 #include "graph/graph_node.hpp"
+#include "graph/modifiable_graph.hpp"
