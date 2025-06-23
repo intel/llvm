97cd7055d871ab3b37e1a2f05c9b829a09382787
Zhang, Winston <winston.zhang@intel.com>
2025-06-23 16:43:53 -0700

    [UR][L0] urDevice when numdevices is not zero
    When NumDevices is not zero and when phDevice is null, the user cannot
    query the number of devices. This addresses that issue.
    
    Signed-off-by: Zhang, Winston <winston.zhang@intel.com>

--- unified-runtime/source/adapters/level_zero/device.cpp
+++ unified-runtime/source/adapters/level_zero/device.cpp
@@ -167,7 +167,7 @@ ur_result_t urDeviceGet(
     std::copy_n(MatchedDevices.begin(), N, Devices);
 
   if (NumDevices) {
-    if (*NumDevices == 0)
+    if (*NumDevices == 0 || (NumEntries < ZeDeviceCount && !Devices))
       *NumDevices = ZeDeviceCount;
     else
       *NumDevices = N;
