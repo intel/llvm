7b31fe66cb52abd88562efcf2f53b9b6b6e642ca
Jakub Chlanda <jakub@codeplay.com>
2025-06-24 12:53:52 +0000

    NOT

--- sycl/test-e2e/KernelCompiler/sycl_cache.cpp
+++ sycl/test-e2e/KernelCompiler/sycl_cache.cpp
@@ -24,7 +24,7 @@
 // RUN: %{run-aux} %if cuda %{ echo "WOOF run-aux CUDA"; %}
 // RUN: %{run-aux} %if hip %{ echo "WOOF run-aux HIP"; %}
 // RUN: %{run-aux} %if (cuda || hip) %{ echo "WOOF run-aux CUDA || HIP"; %}
-// RUN: %{run-aux} %if !(cuda || hip) %{ echo "WOOF run-aux !CUDA || HIP"; %}
+// RUN: %{run-aux} %if !(cuda || hip) %{ echo "WOOF run-aux NOT CUDA || HIP"; %}
 //
 // RUN: %{run} %if cuda %{ echo "WOOF run CUDA"; %}
 // RUN: %{run} %if hip %{ echo "WOOF run HIP"; %}
