445ae42cc743dc0c59ad48cddb38e92895c4ba6a
Zhao, Yang2 <yang2.zhao@intel.com>
2025-06-25 04:45:33 +0200

    fix format

--- libdevice/include/sanitizer_utils.hpp
+++ libdevice/include/sanitizer_utils.hpp
@@ -1,4 +1,4 @@
-//==------------------ sanitizer_utils.hpp - utils for sanitizers -------------------==//
+//==------------- sanitizer_utils.hpp - utils for sanitizers ---------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
