22c339a76f92447420e932bdba21bf703a839f80
Sergey Semenov <sergey.semenov@intel.com>
2025-06-26 07:59:40 -0700

    [SYCL][NFC] Fix copy instead of move Coverity hits

--- sycl/source/detail/memory_manager.cpp
+++ sycl/source/detail/memory_manager.cpp
@@ -1050,7 +1050,7 @@ void MemoryManager::memset_2d_usm(void *DstMem, queue_impl &Queue, size_t Pitch,
         sycl::make_error_code(errc::invalid),
         "NULL pointer argument in 2D memory memset operation.");
   MemoryManager::fill_2d_usm(DstMem, Queue, Pitch, Width, Height,
-                             {static_cast<unsigned char>(Value)}, DepEvents,
+                             {static_cast<unsigned char>(Value)}, std::move(DepEvents),
                              OutEvent);
 }
 
@@ -1151,7 +1151,7 @@ getOrBuildProgramForDeviceGlobal(queue_impl &Queue,
   device_image_plain DeviceImage =
       PM.getDeviceImageFromBinaryImage(&Img, Context, Device);
   device_image_plain BuiltImage =
-      PM.build(std::move(DeviceImage), {Device}, {});
+      PM.build(std::move(DeviceImage), {std::move(Device)}, {});
   return getSyclObjImpl(BuiltImage)->get_ur_program_ref();
 }
 
