8477da70585bfbaef96d4a53188398515e8b2367
Nicolas Miller <nicolas.miller@codeplay.com>
2025-06-27 09:10:28 +0100

    [SYCL] Add missing copyright notices

--- sycl/include/sycl/stl_wrappers/cmath-fallback.h
+++ sycl/include/sycl/stl_wrappers/cmath-fallback.h
@@ -1,3 +1,11 @@
+//==------------- cmath-fallback.h -----------------------------------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef __CMATH_FALLBACK_H__
 #define __CMATH_FALLBACK_H__
 
