f5bc8389104b6e4851c7b6d5c14829e590d286b1
Larsen, Steffen <steffen.larsen@intel.com>
2025-06-30 22:04:32 -0700

    Remove req after fix
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- sycl/test-e2e/SYCLBIN/dae_input.cpp
+++ sycl/test-e2e/SYCLBIN/dae_input.cpp
@@ -10,11 +10,6 @@
 
 // -- Test for using a kernel from a SYCLBIN with a dead argument.
 
-// Due to the regression in https://github.com/intel/llvm/issues/18432 it will
-// fail to build the SYCLBIN with nvptx targets. Once this is fixed,
-// %{sycl_target_opts} should be added to the SYCLBIN generation run-line.
-// REQUIRES: target-spir
-
 // RUN: %clangxx --offload-new-driver -fsyclbin=input %S/Inputs/dae_kernel.cpp -o %t.syclbin
 // RUN: %{build} -o %t.out
 // RUN: %{l0_leak_check} %{run} %t.out %t.syclbin
