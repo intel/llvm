47ea7b4dadda258451e3d7b0e009ef13795faa44
Dubinov, Igor <igor.dubinov@intel.com>
2025-07-02 09:26:32 +0200

    code format

--- sycl/test-e2e/Adapters/level_zero/interop-buffer.cpp
+++ sycl/test-e2e/Adapters/level_zero/interop-buffer.cpp
@@ -47,7 +47,7 @@ int main() {
 
     // Initializing Level Zero driver is required if this test is linked
     // statically with Level Zero loader, otherwise the driver will not be
-  // initialized.
+    // initialized.
     ze_result_t result = zeInit(ZE_INIT_FLAG_GPU_ONLY);
     assert(result == ZE_RESULT_SUCCESS);
 
