05698abd208d992afc6c7dda5011917906404783
Agarwal, Udit <udit.agarwal@intel.com>
2025-07-04 00:17:35 +0200

    Fix test

--- sycl/test/warnings/no_fsycl_flag.cpp
+++ sycl/test/warnings/no_fsycl_flag.cpp
@@ -1,7 +1,6 @@
 // Test to verify that a warning is thrown when the -fsycl flag is not used and
 // <sycl/sycl.hpp> file is included.
-// RUN: %clangxx -I %sycl_include -fsyntax-only %s 2>&1 | FileCheck %s --check-prefix=CHECK-WARNING
-// RUN: %clangxx -I %sycl_include -fsyntax-only -DSYCL_DISABLE_FSYCL_SYCLHPP_WARNING %s 2>&1 | FileCheck %s --implicit-check-not=CHECK-WARNING
+// RUN: %clangxx -std=c++17 -I %sycl_include -fsyntax-only %s -Xclang -verify -Xclang -verify-ignore-unexpected=note,warning
 
-// CHECK-WARNING: You are including <sycl/sycl.hpp> without -fsycl flag, which is errorenous for device code compilation.
+// expected-warning@sycl/sycl.hpp:27 {{You are including <sycl/sycl.hpp> without -fsycl flag, which is errorenous for device code compilation. This warning can be disabled by setting SYCL_DISABLE_FSYCL_SYCLHPP_WARNING macro.}}
 #include <sycl/sycl.hpp>
