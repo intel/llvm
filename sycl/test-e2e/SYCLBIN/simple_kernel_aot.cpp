e36d77ba678ec3c58670aaae45e7d2075138300f
Dmitry Vodopyanov <dmitry.vodopyanov@intel.com>
2025-07-04 18:21:47 +0200

    Update tests

--- sycl/test-e2e/SYCLBIN/simple_kernel_aot.cpp
+++ sycl/test-e2e/SYCLBIN/simple_kernel_aot.cpp
@@ -35,7 +35,8 @@ void TestKernel(int *Ptr, int Size) {
 // CHECK-INPUT-BMG-G21-GPU-DEVICE-ARCH-NEXT: Native device code image 0:
 // CHECK-INPUT-BMG-G21-GPU-DEVICE-ARCH-NEXT:   Metadata:
 // CHECK-INPUT-BMG-G21-GPU-DEVICE-ARCH-NEXT:     SYCLBIN/native device code image metadata:
-// CHECK-INPUT-BMG-G21-GPU-DEVICE-ARCH-NEXT:        arch: bmg_g21
+// CHECK-INPUT-BMG-G21-GPU-DEVICE-ARCH-NEXT:        arch:{{.*}}
+// CHECK-INPUT-BMG-G21-GPU-DEVICE-ARCH-NEXT:        target:{{.*}}spir64_gen-unknown
 // CHECK-INPUT-BMG-G21-GPU-DEVICE-ARCH-NEXT:   Raw native device code image bytes: <Binary blob of {{.*}} bytes>
 
 // CHECK-INPUT-ALL-GPU-DEVICE-ARCHS:      Version: {{[1-9]+}}
@@ -51,6 +52,7 @@ void TestKernel(int *Ptr, int Size) {
 // CHECK-INPUT-ALL-GPU-DEVICE-ARCHS-NEXT:   Metadata:
 // CHECK-INPUT-ALL-GPU-DEVICE-ARCHS-NEXT:     SYCLBIN/native device code image metadata:
 // CHECK-INPUT-ALL-GPU-DEVICE-ARCHS-NEXT:        arch:{{.*}}
+// CHECK-INPUT-ALL-GPU-DEVICE-ARCHS-NEXT:        target:{{.*}}spir64_gen-unknown
 // CHECK-INPUT-ALL-GPU-DEVICE-ARCHS-NEXT:   Raw native device code image bytes: <Binary blob of {{.*}} bytes>
 
 // CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH:      Version: {{[1-9]+}}
@@ -66,19 +68,21 @@ void TestKernel(int *Ptr, int Size) {
 // CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH-NEXT:   Metadata:
 // CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH-NEXT:     SYCLBIN/native device code image metadata:
 // CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH-NEXT:        arch:{{.*}}
+// CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH-NEXT:        target:{{.*}}spir64_gen-unknown
 // CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH-NEXT:   Raw native device code image bytes: <Binary blob of {{.*}} bytes>
 
-// CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH:      Version: {{[1-9]+}}
-// CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH-NEXT: Global metadata:
-// CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH-NEXT:   SYCLBIN/global metadata:
-// CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH-NEXT:     state: 2
-// CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH-NEXT: Number of Abstract Modules: 1
-// CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH-NEXT: Abstract Module 0:
-// CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH-NEXT:   Metadata:
-// CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH:      Number of IR Modules: 0
-// CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH-NEXT: Number of Native Device Code Images: 1
-// CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH-NEXT: Native device code image 0:
-// CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH-NEXT:   Metadata:
-// CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH-NEXT:     SYCLBIN/native device code image metadata:
-// CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH-NEXT:        arch:{{.*}}
-// CHECK-OBJECT-BMG-G21-GPU-DEVICE-ARCH-NEXT:   Raw native device code image bytes: <Binary blob of {{.*}} bytes>
+// CHECK-EXECUTABLE-BMG-G21-GPU-DEVICE-ARCH:      Version: {{[1-9]+}}
+// CHECK-EXECUTABLE-BMG-G21-GPU-DEVICE-ARCH-NEXT: Global metadata:
+// CHECK-EXECUTABLE-BMG-G21-GPU-DEVICE-ARCH-NEXT:   SYCLBIN/global metadata:
+// CHECK-EXECUTABLE-BMG-G21-GPU-DEVICE-ARCH-NEXT:     state: 2
+// CHECK-EXECUTABLE-BMG-G21-GPU-DEVICE-ARCH-NEXT: Number of Abstract Modules: 1
+// CHECK-EXECUTABLE-BMG-G21-GPU-DEVICE-ARCH-NEXT: Abstract Module 0:
+// CHECK-EXECUTABLE-BMG-G21-GPU-DEVICE-ARCH-NEXT:   Metadata:
+// CHECK-EXECUTABLE-BMG-G21-GPU-DEVICE-ARCH:      Number of IR Modules: 0
+// CHECK-EXECUTABLE-BMG-G21-GPU-DEVICE-ARCH-NEXT: Number of Native Device Code Images: 1
+// CHECK-EXECUTABLE-BMG-G21-GPU-DEVICE-ARCH-NEXT: Native device code image 0:
+// CHECK-EXECUTABLE-BMG-G21-GPU-DEVICE-ARCH-NEXT:   Metadata:
+// CHECK-EXECUTABLE-BMG-G21-GPU-DEVICE-ARCH-NEXT:     SYCLBIN/native device code image metadata:
+// CHECK-EXECUTABLE-BMG-G21-GPU-DEVICE-ARCH-NEXT:        arch:{{.*}}
+// CHECK-EXECUTABLE-BMG-G21-GPU-DEVICE-ARCH-NEXT:        target:{{.*}}spir64_gen-unknown
+// CHECK-EXECUTABLE-BMG-G21-GPU-DEVICE-ARCH-NEXT:   Raw native device code image bytes: <Binary blob of {{.*}} bytes>
