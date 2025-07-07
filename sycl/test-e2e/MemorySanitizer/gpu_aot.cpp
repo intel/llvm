393936cc737653204a78e05f409e8ab2da161b3b
jinge90 <ge.jin@intel.com>
2025-07-07 16:18:40 +0800

    fix lit failure
    Signed-off-by: jinge90 <ge.jin@intel.com>

--- sycl/test-e2e/MemorySanitizer/gpu_aot.cpp
+++ sycl/test-e2e/MemorySanitizer/gpu_aot.cpp
@@ -2,10 +2,10 @@
 // REQUIRES: arch-intel_gpu_pvc
 
 // RUN: %{run-aux} %{build} %device_msan_aot_flags -O0 -g %S/check_divide.cpp -o %t.out
-// RUN: %{run} %t.out 2>&1 | FileCheck %S/check_divide.cpp
+// RUN: %{run} not %t.out 2>&1 | FileCheck %S/check_divide.cpp
 
 // RUN: %{run-aux} %{build} %device_msan_aot_flags -O1 -g %S/check_divide.cpp -o %t.out
-// RUN: %{run} %t.out 2>&1 | FileCheck %S/check_divide.cpp
+// RUN: %{run} not %t.out 2>&1 | FileCheck %S/check_divide.cpp
 
 // RUN: %{run-aux} %{build} %device_msan_aot_flags -O2 -g %S/check_divide.cpp -o %t.out
-// RUN: %{run} %t.out 2>&1 | FileCheck %S/check_divide.cpp
+// RUN: %{run} not %t.out 2>&1 | FileCheck %S/check_divide.cpp
