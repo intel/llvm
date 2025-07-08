f780d86cc908856bd4d18ce8760bafab46d4547a
Li, Ian <ian.li@intel.com>
2025-07-08 08:17:54 -0700

    Revert test

--- devops/scripts/benchmarks/main.py
+++ devops/scripts/benchmarks/main.py
@@ -660,7 +660,6 @@ if __name__ == "__main__":
         detect_res = DetectVersion.init(detect_ver_path)
 
     benchmark_filter = re.compile(args.filter) if args.filter else None
-    parser.error("dummy error")
 
     main(
         args.benchmark_directory,
