intel/llvm

commit 79d1e54955791ea59c135b8cb24e8450e6c3c1df

[SYCL][NFC] Throw warning when sycl.hpp is included without `-fsycl` flag. (#19279)
To improve user experience.

Fixes https://github.com/intel/llvm/issues/12052

sycl/include/sycl/sycl.hpp
@@ -8,6 +8,30 @@
 
 #pragma once
 
+// Throw warning when including sycl.hpp without using -fsycl flag.
+// Warning can be disabled by defining SYCL_DISABLE_FSYCL_SYCLHPP_WARNING macro.
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
+#ifdef _MSC_VER
+#define WARNING(msg)                                                           \
+  __pragma(message(__FILE__ "(" TOSTRING(__LINE__) "): warning: " msg))
+#elif defined(__GNUC__) || defined(__clang__)
+#define WARNING(msg) _Pragma(TOSTRING(GCC warning msg))
+#else
+#define WARNING(msg) // Unsupported compiler
+#endif
+
+#if !defined(SYCL_LANGUAGE_VERSION) &&                                         \
+    !defined(SYCL_DISABLE_FSYCL_SYCLHPP_WARNING)
+WARNING("You are including <sycl/sycl.hpp> without -fsycl flag, \
+which is errorenous for device code compilation. This warning \
+can be disabled by setting SYCL_DISABLE_FSYCL_SYCLHPP_WARNING macro.")
+#endif
+#undef WARNING
+#undef TOSTRING
+#undef STRINGIFY
+
 #include <sycl/detail/core.hpp>
 
 #include <sycl/accessor_image.hpp>

sycl/test/warnings/no_fsycl_flag.cpp
@@ -1,0 +1,6 @@
+// Test to verify that a warning is thrown when the -fsycl flag is not used and
+// <sycl/sycl.hpp> file is included.
+// RUN: %clangxx -std=c++17 -I %sycl_include -fsyntax-only %s -Xclang -verify -Xclang -verify-ignore-unexpected=note,warning
+
+// expected-warning@sycl/sycl.hpp:27 {{You are including <sycl/sycl.hpp> without -fsycl flag, which is errorenous for device code compilation. This warning can be disabled by setting SYCL_DISABLE_FSYCL_SYCLHPP_WARNING macro.}}
+#include <sycl/sycl.hpp>

sycl/tools/sycl-ls/CMakeLists.txt
@@ -9,7 +9,7 @@ if (WIN32 AND "${build_type_lower}" MATCHES "debug")
 endif()
 
 # Disable aspect::image & deprecation warnings.
-target_compile_definitions(sycl-ls PRIVATE SYCL_DISABLE_IMAGE_ASPECT_WARNING SYCL2020_DISABLE_DEPRECATION_WARNINGS)
+target_compile_definitions(sycl-ls PRIVATE SYCL_DISABLE_IMAGE_ASPECT_WARNING SYCL2020_DISABLE_DEPRECATION_WARNINGS SYCL_DISABLE_FSYCL_SYCLHPP_WARNING)
 
 target_link_libraries(sycl-ls
   PRIVATE

commit 8e1b9a9e12f363e3b003c46429c0c2782a8d47f2

[SYCL] Don't use common macro names in sycl.hpp (#19393)
fixes https://github.com/intel/llvm/issues/19392 and
https://github.com/intel/llvm/issues/19390
These macro names are clashing with macros that OneDNN defined.
Regression after https://github.com/intel/llvm/pull/19279

sycl/include/sycl/sycl.hpp
@@ -10,27 +10,27 @@
 
 // Throw warning when including sycl.hpp without using -fsycl flag.
 // Warning can be disabled by defining SYCL_DISABLE_FSYCL_SYCLHPP_WARNING macro.
-#define STRINGIFY(x) #x
-#define TOSTRING(x) STRINGIFY(x)
+#define __SYCL_STRINGIFY(x) #x
+#define __SYCL_TOSTRING(x) __SYCL_STRINGIFY(x)
 
 #ifdef _MSC_VER
-#define WARNING(msg)                                                           \
-  __pragma(message(__FILE__ "(" TOSTRING(__LINE__) "): warning: " msg))
+#define __SYCL_WARNING(msg)                                                    \
+  __pragma(message(__FILE__ "(" __SYCL_TOSTRING(__LINE__) "): warning: " msg))
 #elif defined(__GNUC__) || defined(__clang__)
-#define WARNING(msg) _Pragma(TOSTRING(GCC warning msg))
+#define __SYCL_WARNING(msg) _Pragma(__SYCL_TOSTRING(GCC warning msg))
 #else
-#define WARNING(msg) // Unsupported compiler
+#define __SYCL_WARNING(msg) // Unsupported compiler
 #endif
 
 #if !defined(SYCL_LANGUAGE_VERSION) &&                                         \
     !defined(SYCL_DISABLE_FSYCL_SYCLHPP_WARNING)
-WARNING("You are including <sycl/sycl.hpp> without -fsycl flag, \
+__SYCL_WARNING("You are including <sycl/sycl.hpp> without -fsycl flag, \
 which is errorenous for device code compilation. This warning \
 can be disabled by setting SYCL_DISABLE_FSYCL_SYCLHPP_WARNING macro.")
 #endif
-#undef WARNING
-#undef TOSTRING
-#undef STRINGIFY
+#undef __SYCL_WARNING
+#undef __SYCL_TOSTRING
+#undef __SYCL_STRINGIFY
 
 #include <sycl/detail/core.hpp>
 