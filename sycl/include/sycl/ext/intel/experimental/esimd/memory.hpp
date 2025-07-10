88accefe2685d08ea9d1fddc95c3cdf9ad0f5219
Sidorov, Dmitry <dmitry.sidorov@intel.com>
2025-07-10 07:37:30 -0700

    format
    Signed-off-by: Sidorov, Dmitry <dmitry.sidorov@intel.com>

--- sycl/include/sycl/ext/intel/experimental/esimd/memory.hpp
+++ sycl/include/sycl/ext/intel/experimental/esimd/memory.hpp
@@ -2480,13 +2480,9 @@ lsc_atomic_update(AccessorTy acc, __ESIMD_NS::simd<uint32_t, N> offsets,
 /// @{
 
 /// Get HW Thread ID
-__ESIMD_API int32_t get_hw_thread_id() {
-  return 0;
-}
+__ESIMD_API int32_t get_hw_thread_id() { return 0; }
 /// Get subdevice ID
-__ESIMD_API int32_t get_subdevice_id() {
-  return 0;
-}
+__ESIMD_API int32_t get_subdevice_id() { return 0; }
 
 /// @} sycl_esimd_hw_thread_queries
 
