fba70423b14081d24df8fc134422e77037e52c08
Nicolas Miller <nicolas.miller@codeplay.com>
2025-07-14 07:29:34 +0100

    [SYCL] Update copyright header

--- sycl/include/sycl/stl_wrappers/__sycl_cmath_wrapper_impl.hpp
+++ sycl/include/sycl/stl_wrappers/__sycl_cmath_wrapper_impl.hpp
@@ -1,4 +1,4 @@
-//==------------- cmath-fallback.h -----------------------------------------==//
+//==------------- __sycl_cmath_wrapper_impl.hpp ----------------------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
