7551b7fe242115bbf46a41cf2137e6cd0015e91c
Sarnie, Nick <nick.sarnie@intel.com>
2025-07-15 11:52:33 -0700

    [SYCL][E2E][Matrix] Remove XFAIL from tests passing on PVC dev-igc
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- sycl/test-e2e/Matrix/joint_matrix_bf16_fill_k_cache_prefetch.cpp
+++ sycl/test-e2e/Matrix/joint_matrix_bf16_fill_k_cache_prefetch.cpp
@@ -9,9 +9,7 @@
 
 // REQUIRES: aspect-ext_intel_matrix
 // UNSUPPORTED: gpu-intel-dg2
-
-// XFAIL: run-mode && igc-dev
-// XFAIL-TRACKER: CMPLRLLVM-66371
+// UNSUPPORTED-TRACKER: CMPLRLLVM-66371
 
 // RUN: %{build} -o %t_vnni.out -DPREFETCH -DVNNI %fp-model-precise
 // RUN: %{run} %t_vnni.out
