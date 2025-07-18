b38b4dd6b090f9585f09f535adc0adf0759e2687
Duncan Brawley <duncan.brawley@codeplay.com>
2025-07-18 17:48:18 +0100

    Specify that LUID and node mask are windows only

--- unified-runtime/include/ur_api.h
+++ unified-runtime/include/ur_api.h
@@ -2312,9 +2312,9 @@ typedef enum ur_device_info_t {
   /// [::ur_kernel_launch_properties_flags_t] Bitfield of supported kernel
   /// launch properties.
   UR_DEVICE_INFO_KERNEL_LAUNCH_CAPABILITIES = 128,
-  /// [uint8_t[]][optional-query] return device LUID
+  /// [uint8_t[]][optional-query] return device Windows LUID
   UR_DEVICE_INFO_LUID = 129,
-  /// [uint32_t][optional-query] return device node mask
+  /// [uint32_t][optional-query] return device Windows node mask
   UR_DEVICE_INFO_NODE_MASK = 130,
   /// [::ur_bool_t] Returns true if the device supports the use of
   /// command-buffers.
