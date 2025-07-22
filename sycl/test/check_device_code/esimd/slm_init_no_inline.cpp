e5f7f15f12ae4ce0f402b88585a77d9c2de902e2
Marcos Maronas <marcos.maronas@intel.com>
2025-07-22 17:25:01 +0200

    [SYCL][ESIMD] Run all passes with O0 opt level.

--- sycl/test/check_device_code/esimd/slm_init_no_inline.cpp
+++ sycl/test/check_device_code/esimd/slm_init_no_inline.cpp
@@ -1,5 +1,6 @@
 // RUN: %clangxx -fsycl -O0 -fsycl-device-only -Xclang -emit-llvm -o %t.comp.ll %s
-// RUN: sycl-post-link -ir-output-only -lower-esimd -S %t.comp.ll -O0 -o %t.out.ll
+// -O0 lowering, requires `-force-disable-opt` to disable all optimizations.
+// RUN: sycl-post-link -ir-output-only -lower-esimd -S %t.comp.ll -O0 -force-disable-opt -o %t.out.ll
 // RUN: FileCheck --input-file=%t.out.ll %s
 
 // This test verifies that calls the call of slm_init<N>() that is originally
