9e1f16d4738be31cc66f7b3216a16524deab7744
Klochkov, Denis <denis.klochkov@intel.com>
2025-07-22 10:41:12 +0200

    [SYCL] fix syntax

--- sycl/source/detail/kernel_bundle_impl.hpp
+++ sycl/source/detail/kernel_bundle_impl.hpp
@@ -27,8 +27,8 @@
 #include <cstdint>
 #include <cstring>
 #include <memory>
-#include <unordered_set>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 #include "split_string.hpp"
