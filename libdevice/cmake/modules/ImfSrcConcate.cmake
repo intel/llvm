5690656a8a36e2d36f97782e85797bc4941c2454
Sarnie, Nick <nick.sarnie@intel.com>
2025-07-24 11:41:36 -0700

    Revert "test"
    This reverts commit 26c5c3fa791c3e16bc41107ba79d851e6d96d1b6.

--- libdevice/cmake/modules/ImfSrcConcate.cmake
+++ libdevice/cmake/modules/ImfSrcConcate.cmake
@@ -1,4 +1,4 @@
-set(imf_fp32_fallback_src_list imf_utils/integer_misc.cpp 
+set(imf_fp32_fallback_src_list imf_utils/integer_misc.cpp
                                imf_utils/half_convert.cpp
                                imf_utils/float_convert.cpp
                                imf_utils/simd_emulate.cpp
