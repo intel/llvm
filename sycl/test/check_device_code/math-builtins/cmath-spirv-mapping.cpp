cdd9785989fab40431a701b5825b619003e898b4
Alexey Bader <alexey.bader@intel.com>
2025-07-24 13:09:02 -0700

    Revert changes in cmath-spirv-mapping.cpp test.
    A dozen of math functions are mapped to llvm intrinsics instead of
    spir-v functions. I don't want to mix in these changes into my PR.

--- sycl/test/check_device_code/math-builtins/cmath-spirv-mapping.cpp
+++ sycl/test/check_device_code/math-builtins/cmath-spirv-mapping.cpp
@@ -1,9 +1,8 @@
-// REQUIRES: cuda && hip
+// REQUIRES: cuda
 // Note: This isn't really target specific and should be switched to spir when
 // it's enabled for it.
 
-// RUN: %clangxx -fsycl-device-only -fsycl-targets=nvptx64-nvidia-cuda -S -emit-llvm %s -o - | FileCheck %s
-// RUN: %clangxx -fsycl-device-only -fsycl-targets=amdgcn-amd-amdhsa -S -emit-llvm %s -o - | FileCheck %s
+// RUN: %clangxx -fsycl -fsycl-targets=nvptx64-nvidia-cuda -S -Xclang -emit-llvm -fsycl-device-only %s -o - | FileCheck %s
 
 #include <cmath>
 
