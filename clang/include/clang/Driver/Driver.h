0d9f59785ffd0487fee9ce8bfd79d8b39f7a8a4f
Maksimova, Viktoria <viktoria.maksimova@intel.com>
2025-07-25 08:05:34 -0700

    Revert "[Clang] Rework creating offloading toolchains (#125556)"
    This reverts commit a7d93653a6712d8a374a2776853057b03181c12a.

--- clang/include/clang/Driver/Driver.h
+++ clang/include/clang/Driver/Driver.h
@@ -357,9 +357,6 @@ public:
   phases::ID getFinalPhase(const llvm::opt::DerivedArgList &DAL,
                            llvm::opt::Arg **FinalPhaseArg = nullptr) const;
 
-  llvm::Expected<std::unique_ptr<llvm::MemoryBuffer>>
-  executeProgram(llvm::ArrayRef<llvm::StringRef> Args) const;
-
 private:
   /// Certain options suppress the 'no input files' warning.
   LLVM_PREFERRED_TYPE(bool)
@@ -371,6 +368,10 @@ private:
   /// created targeting that triple. The driver owns all the ToolChain objects
   /// stored in it, and will clean them up when torn down.
   mutable llvm::StringMap<std::unique_ptr<ToolChain>> ToolChains;
+
+  /// The associated offloading architectures with each toolchain.
+  llvm::DenseMap<const ToolChain *, llvm::SmallVector<llvm::StringRef>>
+      OffloadArchs;
 
 private:
   /// TranslateInputArgs - Create a new derived argument list from the input
@@ -540,7 +541,8 @@ public:
   /// empty string.
   llvm::SmallVector<StringRef>
   getOffloadArchs(Compilation &C, const llvm::opt::DerivedArgList &Args,
-                  Action::OffloadKind Kind, const ToolChain &TC) const;
+                  Action::OffloadKind Kind, const ToolChain *TC,
+                  bool SpecificToolchain = true) const;
 
   /// Check that the file referenced by Value exists. If it doesn't,
   /// issue a diagnostic and return false.
