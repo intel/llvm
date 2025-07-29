4d18c85f32760b36bc55c6b22a8d52bd7b7b83b2
Steffen Larsen <steffen.larsen@intel.com>
2025-07-29 17:23:15 +0200

    Apply suggestions from code review

--- sycl/test-e2e/NonUniformGroups/fragment_algorithms.cpp
+++ sycl/test-e2e/NonUniformGroups/fragment_algorithms.cpp
@@ -1,7 +1,7 @@
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 //
-// CPU AOT targets host isa, so we compile on the run system instead.
+// Test CPU AOT as well when possible.
 // RUN: %if any-device-is-cpu && opencl-aot %{ %{run-aux} %clangxx -fsycl -fsycl-targets=spir64_x86_64 -o %t.x86.out %s %}
 // RUN: %if cpu && opencl-aot %{ %{run} %t.x86.out %}
 //
