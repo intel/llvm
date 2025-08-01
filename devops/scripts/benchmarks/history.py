1aa59442ae3232c7f518f7f9683f2f75591ad471
Mateusz P. Nowak <mateusz.p.nowak@intel.com>
2025-08-01 16:21:28 +0200

    [UR][Benchmarks] platform info added to bench results (#19640)
    Benchmarks webpage displays platform information for selected runs
    
    ---------
    
    Signed-off-by: Mateusz P. Nowak <mateusz.p.nowak@intel.com>

--- devops/scripts/benchmarks/history.py
+++ devops/scripts/benchmarks/history.py
@@ -9,6 +9,7 @@ from pathlib import Path
 import socket
 
 from utils.result import Result, BenchmarkRun
+from utils.platform import get_platform_info
 from options import Compare, options
 from datetime import datetime, timezone, timedelta
 from utils.utils import run
@@ -182,6 +183,9 @@ class BenchmarkHistory:
         else:
             compute_runtime = "unknown"
 
+        # Get platform information
+        platform_info = get_platform_info()
+
         return BenchmarkRun(
             name=name,
             git_hash=git_hash,
@@ -190,6 +194,7 @@ class BenchmarkHistory:
             results=results,
             hostname=hostname,
             compute_runtime=compute_runtime,
+            platform=platform_info,
         )
 
     def save(self, save_name, results: list[Result]):
@@ -246,6 +251,7 @@ class BenchmarkHistory:
             git_hash="average",
             date=first_run.date,  # should this be different?
             hostname=first_run.hostname,
+            platform=first_run.platform,
         )
 
         return average_benchmark_run
