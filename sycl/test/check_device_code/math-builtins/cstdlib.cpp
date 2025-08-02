de7845124c3cdb80c2ef85b46bc5d00e53cd160a
Alexey Bader <alexey.bader@intel.com>
2025-08-01 19:01:19 -0700

    Extend the test and disable checks for global name space overloads on Windows.

--- sycl/test/check_device_code/math-builtins/cstdlib.cpp
+++ sycl/test/check_device_code/math-builtins/cstdlib.cpp
@@ -4,10 +4,25 @@
 
 #include <cstdlib>
 
+int i = 1;
+long l = 1;
+long long ll = 1;
+
 // Unqualified calls should resolve to the `int` overloads
-static_assert(std::is_same_v<decltype(div(1, 1)), div_t>);
-static_assert(std::is_same_v<decltype(div(1l, 1l)), div_t>);
-static_assert(std::is_same_v<decltype(div(1ll, 1ll)), div_t>);
-static_assert(std::is_same_v<decltype(abs(1)), int>);
-static_assert(std::is_same_v<decltype(abs(1l)), int>);
-static_assert(std::is_same_v<decltype(abs(1ll)), int>);
+static_assert(std::is_same_v<decltype(abs(i)), int>);
+static_assert(std::is_same_v<decltype(div(i, i)), div_t>);
+// NOTE: Windows Universal C Runtime defines C++ overloads for `long` and
+// `long long` types in global name space. See
+// https://github.com/huangqinjin/ucrt/blob/d6e817a4cc90f6f1fe54f8a0aa4af4fff0bb647d/include/stdlib.h#L360-L383.
+#ifndef _WIN32
+static_assert(std::is_same_v<decltype(abs(l)), int>);
+static_assert(std::is_same_v<decltype(abs(ll)), int>);
+static_assert(std::is_same_v<decltype(div(i, l)), div_t>);
+static_assert(std::is_same_v<decltype(div(i, ll)), div_t>);
+static_assert(std::is_same_v<decltype(div(l, i)), div_t>);
+static_assert(std::is_same_v<decltype(div(l, l)), div_t>);
+static_assert(std::is_same_v<decltype(div(l, ll)), div_t>);
+static_assert(std::is_same_v<decltype(div(ll, i)), div_t>);
+static_assert(std::is_same_v<decltype(div(ll, l)), div_t>);
+static_assert(std::is_same_v<decltype(div(ll, ll)), div_t>);
+#endif // _WIN32
