dcb478896200e1e3b70b89bf5c3032cc7325adac
Sergey Semenov <sergey.semenov@intel.com>
2025-08-05 16:46:54 -0700

    Debug kernel_lin.cpp test

--- sycl/test-e2e/syclcompat/kernel/kernel_lin.cpp
+++ sycl/test-e2e/syclcompat/kernel/kernel_lin.cpp
@@ -4,4 +4,5 @@
 
 // RUN: %clangxx -fPIC -shared -fsycl %{sycl_target_opts} %S/Inputs/kernel_module.cpp -o %t.so
 // RUN: %clangxx -DTEST_SHARED_LIB='"%t.so"' -ldl -fsycl %{sycl_target_opts} %S/Inputs/kernel_function.cpp -o %t.out
+// RUN: %clangxx -DTEST_SHARED_LIB='"kernel_lin.cpp.tmp.so"' -ldl -fsycl %{sycl_target_opts} %S/Inputs/kernel_function.cpp -o %t2.out
 // RUN: %{run} %t.out
