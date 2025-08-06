22d66d0a7353b46abf9c4b0626be5bc29f41c435
Agarwal, Udit <udit.agarwal@intel.com>
2025-08-06 16:51:36 +0200

    Make check-sycl-unittests check both prevew and non-preview versions

--- sycl/cmake/modules/AddSYCLUnitTest.cmake
+++ sycl/cmake/modules/AddSYCLUnitTest.cmake
@@ -29,20 +29,19 @@ function(add_sycl_unittest_internal test_dirname link_variant is_preview)
     endif()
   endif()
 
-  # Set unittest_target based on whether this is a preview build
+  # This is done to ensure that preview tests are kept in a seperate
+  # directory, so that they do not interfere with the non-preview tests.
+  # Chaning CMAKE_CURRENT_BINARY_DIR should not affect this variable in its
+  # parent scope.
   if (${is_preview})
-    set(unittest_target "SYCLUnitTestsPreview")
-    set(check_target_dep "check-sycl-unittests-preview")
-  else()
-    set(unittest_target "SYCLUnitTests")
-    set(check_target_dep "check-sycl-unittests")
+    set(CMAKE_CURRENT_BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/Preview")
   endif()
 
   if ("${link_variant}" MATCHES "SHARED")
     set(SYCL_LINK_LIBS ${sycl_so_target})
-    add_unittest(${unittest_target} ${test_dirname} ${ARGN})
+    add_unittest(SYCLUnitTests ${test_dirname} ${ARGN})
   else()
-    add_unittest(${unittest_target} ${test_dirname}
+    add_unittest(SYCLUnitTests ${test_dirname}
                 $<TARGET_OBJECTS:${sycl_obj_target}> ${ARGN})
     target_compile_definitions(${test_dirname}
                                PRIVATE __SYCL_BUILD_SYCL_DLL)
@@ -63,6 +62,7 @@ function(add_sycl_unittest_internal test_dirname link_variant is_preview)
   if (${is_preview})
     target_compile_definitions(${test_dirname}
         PRIVATE __INTEL_PREVIEW_BREAKING_CHANGES)
+    set(sycl_cache_suffix "_preview")
   endif()
 
   if (SYCL_ENABLE_XPTI_TRACING)
@@ -79,9 +79,10 @@ function(add_sycl_unittest_internal test_dirname link_variant is_preview)
         LLVM_PROFILE_FILE="${SYCL_COVERAGE_PATH}/${test_dirname}.profraw"
         SYCL_CONFIG_FILE_NAME=null.cfg
         SYCL_DEVICELIB_NO_FALLBACK=1
-        SYCL_CACHE_DIR="${CMAKE_BINARY_DIR}/sycl_cache"
+        SYCL_CACHE_DIR="${CMAKE_BINARY_DIR}/sycl_cache${sycl_cache_suffix}"
         "PATH=${CMAKE_BINARY_DIR}/bin;$ENV{PATH}"
         ${CMAKE_CURRENT_BINARY_DIR}/${test_dirname}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
         DEPENDS
         ${test_dirname}
     )
@@ -91,37 +92,26 @@ function(add_sycl_unittest_internal test_dirname link_variant is_preview)
         LLVM_PROFILE_FILE="${SYCL_COVERAGE_PATH}/${test_dirname}.profraw"
         SYCL_CONFIG_FILE_NAME=null.cfg
         SYCL_DEVICELIB_NO_FALLBACK=1
-        SYCL_CACHE_DIR="${CMAKE_BINARY_DIR}/sycl_cache"
+        SYCL_CACHE_DIR="${CMAKE_BINARY_DIR}/sycl_cache${sycl_cache_suffix}"
         "LD_LIBRARY_PATH=${SYCL_BINARY_DIR}/unittests/lib:${CMAKE_BINARY_DIR}/lib:$ENV{LD_LIBRARY_PATH}"
         ${CMAKE_CURRENT_BINARY_DIR}/${test_dirname}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
         DEPENDS
         ${test_dirname}
     )
   endif()
 
-  add_dependencies(${check_target_dep} check-sycl-${test_dirname})
+  add_dependencies(check-sycl-unittests check-sycl-${test_dirname})
 
   if(WIN32)
     # Windows doesn't support LD_LIBRARY_PATH, so instead we copy the mock OpenCL binary next to the test and ensure
     # that the test itself links to OpenCL (rather than through ur_adapter_opencl.dll)
     set(mock_ocl ${CMAKE_CURRENT_BINARY_DIR}/OpenCL.dll)
 
-    # An error is thrown if both the preview and non-preview versions try to
-    # copy OpenCL.dll to the test binary directory. Ninja complains that there
-    # are multiple rules for building the same file (listed in the "BYPRODUCTS" field).
-    # To fix this error, we use a different "BYPRODUCTS" name for
-    # preview and non-preview version. We don't use the "BYPRODUCTS" field
-    # in any following commands, so the name doesn't really matter.
-    if (${is_preview})
-      set(byproducts_suffix "_preview")
-    else()
-      set(byproducts_suffix "")
-    endif()
-
     add_custom_command(TARGET ${test_dirname} POST_BUILD
       COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:mockOpenCL> ${mock_ocl}
       DEPENDS mockOpenCL
-      BYPRODUCTS ${mock_ocl}${byproducts_suffix}
+      BYPRODUCTS ${mock_ocl}
       COMMAND_EXPAND_LISTS
       )
   endif()
