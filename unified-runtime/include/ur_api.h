8dfd953a8bcf647821325efbae1a82fde278407f
Ross Brunton <ross@codeplay.com>
2025-08-14 10:32:11 +0100

    [UR] Allow `urProgramGetFunctionPointer` to return `UNSUPPORTED`
    This is primarily for liboffload which doesn't support it yet, and
    probably never will for AMD devices (which don't seem to have this
    functionality). In theory it will also affect level zero, at least
    according to a comment, but urProgramGetFunctionPointer seems to be
    implemented there.

--- unified-runtime/include/ur_api.h
+++ unified-runtime/include/ur_api.h
@@ -5722,6 +5722,8 @@ UR_APIEXPORT ur_result_t UR_APICALL urProgramRelease(
 ///     - ::UR_RESULT_ERROR_FUNCTION_ADDRESS_NOT_AVAILABLE
 ///         + If `pFunctionName` could be located, but its address couldn't be
 ///         retrieved.
+///     - ::UR_RESULT_ERROR_UNSUPPORTED_FEATURE
+///         + If the backend does not support querying function pointers.
 UR_APIEXPORT ur_result_t UR_APICALL urProgramGetFunctionPointer(
     /// [in] handle of the device to retrieve pointer for.
     ur_device_handle_t hDevice,
