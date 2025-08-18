9766fb1f898964fa42de0721270b6d6dbf24d3dd
Zhao, Maosu <maosu.zhao@intel.com>
2025-08-18 03:41:38 +0200

    set spec constant id to INT_MAX - 1

--- unified-runtime/source/loader/layers/sanitizer/sanitizer_common/sanitizer_libdevice.hpp
+++ unified-runtime/source/loader/layers/sanitizer/sanitizer_common/sanitizer_libdevice.hpp
@@ -22,7 +22,7 @@ namespace ur_sanitizer_layer {
 enum class DeviceType : uint32_t { UNKNOWN = 0, CPU, GPU_PVC, GPU_DG2 };
 
 // Try to use a larger ID number to avoid conflict with user ID.
-constexpr int SpecConstantDeviceTyID = 99;
+constexpr int SpecConstantDeviceTyID = INT32_MAX - 1;
 
 inline const char *ToString(DeviceType Type) {
   switch (Type) {
