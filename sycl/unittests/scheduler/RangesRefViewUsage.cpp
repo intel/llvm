858bf21676caf7a04aed08e849e7ae2518b949ec
Alexandr Konovalov <alexandr.konovalov@intel.com>
2025-08-19 18:25:16 +0200

    Decrease dumpilcation is the test.

--- sycl/unittests/scheduler/RangesRefViewUsage.cpp
+++ sycl/unittests/scheduler/RangesRefViewUsage.cpp
@@ -9,104 +9,46 @@
 
 #include <gtest/gtest.h>
 
+template <int dims>
+void TestNDRangesRefView(sycl::range<dims> global, sycl::range<dims> local,
+                         sycl::id<dims> offset) {
+  {
+    sycl::nd_range<dims> nd_range{global, local, offset};
+    sycl::detail::ranges_ref_view r{nd_range};
+    ASSERT_EQ(r.Dims, size_t{dims});
+    for (int d = 0; d < dims; d++) {
+      ASSERT_EQ(r.GlobalSize[d], global[d]);
+      ASSERT_EQ(r.LocalSize[d], local[d]);
+      ASSERT_EQ(r.GlobalOffset[d], offset[d]);
+    }
+  }
+  {
+    sycl::detail::ranges_ref_view r{global, local};
+    ASSERT_EQ(r.Dims, size_t{dims});
+    for (int d = 0; d < dims; d++) {
+      ASSERT_EQ(r.GlobalSize[d], global[d]);
+      ASSERT_EQ(r.LocalSize[d], local[d]);
+    }
+    ASSERT_EQ(r.GlobalOffset, nullptr);
+  }
+  {
+    sycl::detail::ranges_ref_view r{global};
+    ASSERT_EQ(r.Dims, size_t{dims});
+    for (int d = 0; d < dims; d++) {
+      ASSERT_EQ(r.GlobalSize[d], global[d]);
+    }
+    ASSERT_EQ(r.LocalSize, nullptr);
+    ASSERT_EQ(r.GlobalOffset, nullptr);
+  }
+}
+
+
 TEST(RangesRefUsage, RangesRefUsage) {
-  sycl::detail::ranges_ref_view r0;
-  ASSERT_EQ(r0.Dims, size_t{0});
-
-  {
-    sycl::range<1> global_range{1024};
-    sycl::range<1> local_range{64};
-    sycl::id<1> offset{10};
-    sycl::nd_range<1> nd_range{global_range, local_range, offset};
-
-    {
-      sycl::detail::ranges_ref_view r{nd_range};
-      ASSERT_EQ(r.Dims, size_t{1});
-      ASSERT_EQ(*r.GlobalSize, global_range[0]);
-      ASSERT_EQ(*r.LocalSize, local_range[0]);
-      ASSERT_EQ(*r.GlobalOffset, offset[0]);
-    }
-    {
-      sycl::detail::ranges_ref_view r{global_range, local_range};
-      ASSERT_EQ(r.Dims, size_t{1});
-      ASSERT_EQ(*r.GlobalSize, global_range[0]);
-      ASSERT_EQ(*r.LocalSize, local_range[0]);
-    }
-    {
-      sycl::detail::ranges_ref_view r{global_range};
-      ASSERT_EQ(r.Dims, size_t{1});
-      ASSERT_EQ(*r.GlobalSize, global_range[0]);
-      ASSERT_EQ(r.LocalSize, nullptr);
-    }
-  }
-  {
-    sycl::range<2> global_range{1024, 512};
-    sycl::range<2> local_range{64, 32};
-    sycl::id<2> offset{10, 20};
-    sycl::nd_range<2> nd_range{global_range, local_range, offset};
-
-    {
-      sycl::detail::ranges_ref_view r{nd_range};
-      ASSERT_EQ(r.Dims, size_t{2});
-      ASSERT_EQ(r.GlobalSize[0], global_range[0]);
-      ASSERT_EQ(r.GlobalSize[1], global_range[1]);
-      ASSERT_EQ(r.LocalSize[0], local_range[0]);
-      ASSERT_EQ(r.LocalSize[1], local_range[1]);
-      ASSERT_EQ(r.GlobalOffset[0], offset[0]);
-      ASSERT_EQ(r.GlobalOffset[1], offset[1]);
-    }
-    {
-      sycl::detail::ranges_ref_view r{global_range, local_range};
-      ASSERT_EQ(r.Dims, size_t{2});
-      ASSERT_EQ(r.GlobalSize[0], global_range[0]);
-      ASSERT_EQ(r.GlobalSize[1], global_range[1]);
-      ASSERT_EQ(r.LocalSize[0], local_range[0]);
-      ASSERT_EQ(r.LocalSize[1], local_range[1]);
-    }
-    {
-      sycl::detail::ranges_ref_view r{global_range};
-      ASSERT_EQ(r.Dims, size_t{2});
-      ASSERT_EQ(r.GlobalSize[0], global_range[0]);
-      ASSERT_EQ(r.GlobalSize[1], global_range[1]);
-      ASSERT_EQ(r.LocalSize, nullptr);
-    }
-  }
-  {
-    sycl::range<3> global_range{1024, 512, 256};
-    sycl::range<3> local_range{64, 32, 16};
-    sycl::id<3> offset{10, 20, 30};
-    sycl::nd_range<3> nd_range{global_range, local_range, offset};
-
-    {
-      sycl::detail::ranges_ref_view r{nd_range};
-      ASSERT_EQ(r.Dims, size_t{3});
-      ASSERT_EQ(r.GlobalSize[0], global_range[0]);
-      ASSERT_EQ(r.GlobalSize[1], global_range[1]);
-      ASSERT_EQ(r.GlobalSize[2], global_range[2]);
-      ASSERT_EQ(r.LocalSize[0], local_range[0]);
-      ASSERT_EQ(r.LocalSize[1], local_range[1]);
-      ASSERT_EQ(r.LocalSize[2], local_range[2]);
-      ASSERT_EQ(r.GlobalOffset[0], offset[0]);
-      ASSERT_EQ(r.GlobalOffset[1], offset[1]);
-      ASSERT_EQ(r.GlobalOffset[2], offset[2]);
-    }
-    {
-      sycl::detail::ranges_ref_view r{global_range, local_range};
-      ASSERT_EQ(r.Dims, size_t{3});
-      ASSERT_EQ(r.GlobalSize[0], global_range[0]);
-      ASSERT_EQ(r.GlobalSize[1], global_range[1]);
-      ASSERT_EQ(r.GlobalSize[2], global_range[2]);
-      ASSERT_EQ(r.LocalSize[0], local_range[0]);
-      ASSERT_EQ(r.LocalSize[1], local_range[1]);
-      ASSERT_EQ(r.LocalSize[2], local_range[2]);
-    }
-    {
-      sycl::detail::ranges_ref_view r{global_range};
-      ASSERT_EQ(r.Dims, size_t{3});
-      ASSERT_EQ(r.GlobalSize[0], global_range[0]);
-      ASSERT_EQ(r.GlobalSize[1], global_range[1]);
-      ASSERT_EQ(r.GlobalSize[2], global_range[2]);
-      ASSERT_EQ(r.LocalSize, nullptr);
-    }
-  }
+  TestNDRangesRefView(sycl::range<1>{1024}, sycl::range<1>{64},
+      sycl::id<1>{10});
+  TestNDRangesRefView(
+      sycl::range<2>{1024, 512}, sycl::range<2>{64, 32}, sycl::id<2>{10, 5});
+  TestNDRangesRefView(
+      sycl::range<3>{1024, 512, 256}, sycl::range<3>{64, 32, 16},
+      sycl::id<3>{10, 5, 2});
 }
