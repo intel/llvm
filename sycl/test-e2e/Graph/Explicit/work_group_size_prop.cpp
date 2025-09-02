e658e51a9a04a8901e2f5e5b1bd569ba1b341055
Kornev, Nikita <nikita.kornev@intel.com>
2025-09-02 18:00:01 +0200

    Try to re-enable some Graph tests

--- sycl/test-e2e/Graph/Explicit/work_group_size_prop.cpp
+++ sycl/test-e2e/Graph/Explicit/work_group_size_prop.cpp
@@ -2,14 +2,6 @@
 // RUN: %{run} %t.out
 // Extra run to check for leaks in Level Zero using UR_L0_LEAKS_DEBUG
 // RUN: %if level_zero %{%{l0_leak_check} %{run} %t.out 2>&1 | FileCheck %s --implicit-check-not=LEAK %}
-//
-
-// Intended OpenCL fail due to backend:
-// The OpenCL emulation layer does not return `CL_INVALID_WORK_GROUP_SIZE` as it
-// should. So the Sycl graph support cannot correctly catch the error and throw
-// the appropriate exception for negative test. An issue has been reported
-// https://github.com/bashbaug/SimpleOpenCLSamples/issues/95
-// UNSUPPORTED: opencl
 
 #define GRAPH_E2E_EXPLICIT
 
