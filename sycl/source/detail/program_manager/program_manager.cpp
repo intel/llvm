fcd35c06ce1e421f01d3d6b096fd57ca7ff57d6b
Sergey Semenov <sergey.semenov@intel.com>
2025-09-10 15:54:42 +0200

    [SYCL] Fix build with older gcc (#20035)
    Works around an internal compiler error that's observed in Jenkins
    precommit.
    
    ```
    sycl/source/detail/program_manager/program_manager.cpp:1845:75: internal compiler error: in replace_placeholders_r, at cp/tree.c:2804
    KernelName, CompileTimeKernelInfoTy{detail::string_view(KernelName)});
    ```

--- sycl/source/detail/program_manager/program_manager.cpp
+++ sycl/source/detail/program_manager/program_manager.cpp
@@ -1841,8 +1841,9 @@ DeviceKernelInfo &ProgramManager::getOrCreateDeviceKernelInfo(
 DeviceKernelInfo &
 ProgramManager::getOrCreateDeviceKernelInfo(KernelNameStrRefT KernelName) {
   std::lock_guard<std::mutex> Guard(m_DeviceKernelInfoMapMutex);
-  auto Result = m_DeviceKernelInfoMap.try_emplace(
-      KernelName, CompileTimeKernelInfoTy{std::string_view(KernelName)});
+  CompileTimeKernelInfoTy DefaultCompileTimeInfo{std::string_view(KernelName)};
+  auto Result =
+      m_DeviceKernelInfoMap.try_emplace(KernelName, DefaultCompileTimeInfo);
   return Result.first->second;
 }
 
