ff01eb74b9da60a13c2b6b907010dec88b646634
Zhang, Yixing <yixing.zhang@intel.com>
2025-09-15 13:44:07 -0700

    enable test for AddressSanitizer

--- sycl/test-e2e/AddressSanitizer/aot/gpu.cpp
+++ sycl/test-e2e/AddressSanitizer/aot/gpu.cpp
@@ -1,5 +1,6 @@
 // REQUIRES: linux, gpu && level_zero
 // REQUIRES: (arch-intel_gpu_pvc || gpu-intel-dg2)
+// UNSUPPORTED: spirv-backend
 
 // RUN: %{run-aux} %{build} %device_asan_aot_flags -O0 -g %S/Inputs/host-usm-oob.cpp -o %t.out
 // RUN: %{run} not %t.out 2>&1 | FileCheck %S/Inputs/host-usm-oob.cpp
