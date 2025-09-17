2029603066d18df2f8f8ffbf6c81eed5752bf981
Lukasz Dorau <lukasz.dorau@intel.com>
2025-09-17 09:10:23 +0000

    [UR] Initialize RefCount with 0, not with 1
    Initialize RefCount with 0, not with 1.
    
    Signed-off-by: Lukasz Dorau <lukasz.dorau@intel.com>

--- unified-runtime/source/common/ur_ref_count.hpp
+++ unified-runtime/source/common/ur_ref_count.hpp
@@ -18,14 +18,14 @@ namespace ur {
 
 class RefCount {
 public:
-  RefCount(uint32_t count = 1) : Count(count) {}
+  RefCount(uint32_t count = 0) : Count(count) {}
   RefCount(const RefCount &) = delete;
   RefCount &operator=(const RefCount &) = delete;
 
   uint32_t getCount() const noexcept { return Count.load(); }
   uint32_t retain() { return ++Count; }
   bool release() { return --Count == 0; }
-  void reset(uint32_t value = 1) { Count = value; }
+  void reset(uint32_t value = 0) { Count = value; }
 
 private:
   std::atomic_uint32_t Count;
