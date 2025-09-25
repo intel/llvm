d19becd5bdea94e81937c6fb20e6010cdb3a3427
Feng Zou <feng.zou@intel.com>
2025-09-25 21:51:22 +0800

    [NFC] XFAIL ProgramManager/uneven_kernel_split.cpp Basic/backend_info.cpp KernelParams/has-special-captures.cpp

--- sycl/test-e2e/ProgramManager/uneven_kernel_split.cpp
+++ sycl/test-e2e/ProgramManager/uneven_kernel_split.cpp
@@ -3,6 +3,9 @@
 // UNSUPPORTED: linux
 // UNSUPPORTED-TRACKER: https://github.com/intel/llvm/issues/17305
 
+// XFAIL: windows
+// XFAIL-TRACKER: https://github.com/intel/llvm/issues/20127
+
 // RUN: %clangxx -fsycl -fsycl-targets=spir64_x86_64 -I %S/Inputs/ %S/uneven_kernel_split.cpp -c -o %t.o
 // RUN: %clangxx -fsycl -fsycl-targets=spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts -I %S/Inputs/ %S/Inputs/gpu_kernel1.cpp -c -o %t1.o
 // RUN: %clangxx -fsycl -fsycl-targets=spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts -I %S/Inputs/ %S/Inputs/gpu_kernel2.cpp -c -o %t2.o
