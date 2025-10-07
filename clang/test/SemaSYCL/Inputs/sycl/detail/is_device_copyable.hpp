43076a20c4038a1ea235069dd96a8ec82735a935
Lorenc Bushi <lorenc.bushi@intel.com>
2025-10-07 08:20:48 -0700

    Fix more pre-commit failures

---
+++ clang/test/SemaSYCL/Inputs/sycl/detail/is_device_copyable.hpp
@@ -0,0 +1,14 @@
+//==------------ is_device_copyable.hpp - ----------------------------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+#pragma once
+
+namespace sycl {
+inline namespace _V1 {
+template <typename T> struct is_device_copyable;
+} // namespace _V1
+} // namespace sycl
