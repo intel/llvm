bb8c5224969ed6722842778c8ce9418dd0e827e8
Marcos Maronas <marcos.maronas@intel.com>
2025-10-15 20:49:36 +0200

    Use %t.dir for DeviceImageDependencies tests

--- sycl/test-e2e/DeviceImageDependencies/singleDynamicLibrary.cpp
+++ sycl/test-e2e/DeviceImageDependencies/singleDynamicLibrary.cpp
@@ -1,21 +1,20 @@
 // Test -fsycl-allow-device-image-dependencies with a single dynamic library on Windows
 // and Linux.
 
-// DEFINE: %{tdir} = %t/..
-// RUN: mkdir -p %{tdir}
+// RUN: mkdir -p %t.dir
 // RUN: %clangxx -fsycl %fPIC %shared_lib -fsycl-allow-device-image-dependencies -I %S/Inputs \
 // RUN:    %S/Inputs/a.cpp                                                              \
 // RUN:    %S/Inputs/b.cpp                                                              \
 // RUN:    %S/Inputs/c.cpp                                                              \
 // RUN:    %S/Inputs/d.cpp                                                              \
 // RUN:    %S/Inputs/wrapper.cpp                                                        \
-// RUN:    -o %if windows %{%{tdir}/device_single.dll%} %else %{%{tdir}/libdevice_single.so%}
+// RUN:    -o %if windows %{%t.dir/device_single.dll%} %else %{%t.dir/libdevice_single.so%}
 
 // RUN: %{build} -I%S/Inputs -o %t.out           \
 // RUN: %if windows                              \
-// RUN:   %{%{tdir}/device_single.lib%}               \
+// RUN:   %{%t.dir/device_single.lib%}               \
 // RUN: %else                                    \
-// RUN:   %{-L%{tdir} -ldevice_single -Wl,-rpath=%{tdir}%}
+// RUN:   %{-L%t.dir -ldevice_single -Wl,-rpath=%t.dir%}
 
 // RUN: %{run} %t.out
 
