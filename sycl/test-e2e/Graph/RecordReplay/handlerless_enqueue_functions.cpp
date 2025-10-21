61e8133dc4e9b3a6fa681769ecd0c8afb3348ffa
Matthew Michel <matthew.michel@intel.com>
2025-10-21 08:47:35 -0700

    Add leak check to test header

--- sycl/test-e2e/Graph/RecordReplay/handlerless_enqueue_functions.cpp
+++ sycl/test-e2e/Graph/RecordReplay/handlerless_enqueue_functions.cpp
@@ -1,5 +1,7 @@
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
+// Extra run to check for leaks in Level Zero using UR_L0_LEAKS_DEBUG
+// RUN: %if level_zero %{%{l0_leak_check} %{run} %t.out 2>&1 | FileCheck %s --implicit-check-not=LEAK %}
 
 // Test recording of handlerless SYCL queue APIs (memset, memcpy, memadvise,
 // parallel_for/nd_launch, prefetch, single_task) inside a single function call
