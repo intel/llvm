4f7b179582ab33481299c2e51b268498316d4784
Lorenc Bushi <lorenc.bushi@intel.com>
2025-10-28 18:00:46 -0400

    [SYCLLowerIR] Fix a bug in hierarchical parallelism implementation (#20484)
    When inside a `parallel_for_work_group` context, calls to functions that
    contain calls to `parallel_for_work_item` are being lowered incorrectly
    into IR in that they are being put under the work-group leader branch in
    the IR which is semantically incorrect as this function should be called
    in every work item. This manifests when we have an indirect function
    call to `parallel_for_work_item` together with at least one other direct
    call to `parallel_for_work_item` in the same `parallel_for_work_group`
    context and it leads to a program that hangs. This PR fixes the issue
    and adds a couple of other tests to check this behavior.

--- sycl/test-e2e/HierPar/hier_par_indirect.cpp
+++ sycl/test-e2e/HierPar/hier_par_indirect.cpp
@@ -19,12 +19,36 @@ void __attribute__((noinline)) foo(sycl::group<1> work_group) {
   work_group.parallel_for_work_item([&](sycl::h_item<1> index) {});
 }
 
+void __attribute__((noinline)) bar(sycl::group<1> work_group) {
+  foo(work_group);
+}
+
 int main(int argc, char **argv) {
   sycl::queue q;
+
+  // Try a single indirect call, two indirect calls and an indirect call
+  // accompanied by multiple parallel_for_work_item calls in the same work_group
+  // scope.
   q.submit([&](sycl::handler &cgh) {
      cgh.parallel_for_work_group(sycl::range<1>{1}, sycl::range<1>{128},
                                  ([=](sycl::group<1> wGroup) { foo(wGroup); }));
    }).wait();
+  q.submit([&](sycl::handler &cgh) {
+     cgh.parallel_for_work_group(
+         sycl::range<1>{1}, sycl::range<1>{128}, ([=](sycl::group<1> wGroup) {
+           foo(wGroup); // 1-layer indirect call
+           bar(wGroup); // 2-layer indirect call since bar calls foo
+         }));
+   }).wait();
+  q.submit([&](sycl::handler &cgh) {
+     cgh.parallel_for_work_group(
+         sycl::range<1>{1}, sycl::range<1>{128}, ([=](sycl::group<1> wGroup) {
+           wGroup.parallel_for_work_item([&](sycl::h_item<1> index) {});
+           foo(wGroup);
+           wGroup.parallel_for_work_item([&](sycl::h_item<1> index) {});
+         }));
+   }).wait();
+
   std::cout << "test passed" << std::endl;
   return 0;
 }
