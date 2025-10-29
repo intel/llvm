756a1090be4e38f5802a81b4c56fa652219b3cc8
Larsen, Steffen <steffen.larsen@intel.com>
2025-10-29 05:10:59 -0700

    Fix wrong if-else order
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- sycl/test/regression/bit_cast_win.cpp
+++ sycl/test/regression/bit_cast_win.cpp
@@ -1,4 +1,4 @@
-// DEFINE: %{md_flag} = %if debug_sycl_library %{/MD%} %else %{/MDd%}
+// DEFINE: %{md_flag} = %if debug_sycl_library %{/MDd%} %else %{/MD%}
 // RUN: %clangxx %fsycl -fsycl-host-compiler=cl -fsycl-host-compiler-options='/std:c++17 /Zc:__cplusplus %{md_flag}' %s -o %t.out
 // UNSUPPORTED: linux
 
