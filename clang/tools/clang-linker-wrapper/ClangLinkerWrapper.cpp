ff35d3874e2bbce9ddbd4ee9b8d03e2781324b39
y <yixing.zhang@intel.com>
2025-11-07 14:38:40 -0800

    add to the last commit, now AOT/double.cpp and AOT/reqd-sg-size.cpp also pass

--- clang/tools/clang-linker-wrapper/ClangLinkerWrapper.cpp
+++ clang/tools/clang-linker-wrapper/ClangLinkerWrapper.cpp
@@ -730,9 +730,10 @@ runSYCLPostLinkTool(ArrayRef<StringRef> InputFiles, const ArgList &Args) {
     return TempFileOrErr.takeError();
   std::string OutputPathWithArch = TempFileOrErr->str();
   StringRef Arch = Args.getLastArgValue(OPT_arch_EQ);
-  if (Triple.getSubArch() == llvm::Triple::SPIRSubArch_gen && Arch.data()) {
-    std::string OutputPathWithArch = "intel_gpu_" + Arch.str() + "," + OutputPathWithArch;
-  }
+  if (Triple.getSubArch() == llvm::Triple::SPIRSubArch_gen && Arch.data())
+    OutputPathWithArch = "intel_gpu_" + Arch.str() + "," + OutputPathWithArch;
+  else if (Triple.getSubArch() == llvm::Triple::SPIRSubArch_x86_64)
+    OutputPathWithArch = "spir64_x86_64," + OutputPathWithArch;
 
   SmallVector<StringRef, 8> CmdArgs;
   CmdArgs.push_back(*SYCLPostLinkPath);
