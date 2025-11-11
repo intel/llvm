8a17b980bc181bfe88738f5c1cb55f7dd923a868
y <yixing.zhang@intel.com>
2025-11-11 09:04:03 -0800

    Add the test for optional kernel for sycl-post-link

--- sycl/test-e2e/NewOffloadDriver/aot-gpu.cpp
+++ sycl/test-e2e/NewOffloadDriver/aot-gpu.cpp
@@ -9,3 +9,12 @@
 // RUN:   %S/Inputs/split-per-source-second-file.cpp \
 // RUN:   -fsycl-dead-args-optimization --offload-new-driver
 // RUN: %{run} %t.out
+//
+// Check that target is passed to sycl-post-link for filtering
+// RUN: %clangxx -fsycl -fsycl-targets=intel_gpu_pvc,intel_gpu_dg1,spir64_x86_64 \
+// RUN: -Xsycl-target-backend=spir64_gen --offload-new-driver \
+// RUN: -v %S/Inputs/aot.cpp 2>&1 | FileCheck %s --check-prefix=CHECK_TOOLS_FILTER
+//
+// CHECK_TOOLS_FILTER: sycl-post-link{{.*}} -o intel_gpu_pvc,{{.*}}
+// CHECK_TOOLS_FILTER: sycl-post-link{{.*}} -o intel_gpu_dg1,{{.*}}
+// CHECK_TOOLS_FILTER: sycl-post-link{{.*}} -o spir64_x86_64,{{.*}}
