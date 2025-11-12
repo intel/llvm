30e02002f7427967778326a893692c33a21cfb9b
Cai, Justin <justin.cai@intel.com>
2025-11-12 19:27:40 +0000

    fix missed merge conflict

--- clang/test/Driver/sycl-ftarget-compile-fast.cpp
+++ clang/test/Driver/sycl-ftarget-compile-fast.cpp
@@ -7,19 +7,8 @@
 // RUN:     -fsycl-targets=spir64_gen -ftarget-compile-fast %s 2>&1 \
 // RUN:   | FileCheck -check-prefix=TARGET_COMPILE_FAST_GEN %s
 
-<<<<<<< HEAD
-
-// Due to how clang-offload-packager works, if we want a value in a key-value pair to
-// have a comma, we need to specify the key twice, once for each part of the value
-// separated by the comma. clang-offload-packager will then combine the two parts into a single
-// value with a comma in between.
-// TARGET_COMPILE_FAST_GEN: clang-offload-packager
-// TARGET_COMPILE_FAST_GEN: compile-opts={{.*}}-options -igc_opts 'PartitionUnit=1
-// TARGET_COMPILE_FAST_GEN-SAME: compile-opts=SubroutineThreshold=50000'
-=======
 // TARGET_COMPILE_FAST_GEN: llvm-offload-binary
 // TARGET_COMPILE_FAST_GEN: compile-opts={{.*}}-options -igc_opts 'PartitionUnit=1,SubroutineThreshold=50000'
->>>>>>> intel/sycl
 
 // RUN: %clang -### -target x86_64-unknown-linux-gnu -fsycl --offload-new-driver \
 // RUN:    -ftarget-compile-fast %s 2>&1 \
