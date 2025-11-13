046ccde3196246e1f68dbd1cfe911984d6d3f1c2
Zhiming Wang <zhiming.wang@intel.com>
2025-11-13 16:43:55 +0800

    Fix failed lit test case

--- sycl/source/detail/graph/graph_impl.cpp
+++ sycl/source/detail/graph/graph_impl.cpp
@@ -1268,7 +1268,7 @@ exec_graph_impl::enqueue(sycl::detail::queue_impl &Queue,
 }
 
 void exec_graph_impl::duplicateNodes() {
-  if (MGraphImpl->IsLinearRecorded()) {
+  if (MGraphImpl->isLinearRecorded() && !MGraphImpl->hasSubGraph()) {
     MNodeStorage = MGraphImpl->MNodeStorage;
     return;
   }
