19dca2b095850378a9b06b3cc1d9fecd2dbb5fe9
Larsen, Steffen <steffen.larsen@intel.com>
2025-11-14 04:37:11 -0800

    Change copyright header
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- sycl/include/sycl/ext/oneapi/experimental/syclbin_properties.hpp
+++ sycl/include/sycl/ext/oneapi/experimental/syclbin_properties.hpp
@@ -1,4 +1,4 @@
-//==-------- syclbin_properties.hpp - SYCLBIN and tooling properties -------==//
+//==------------------------------------------------------------------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
