b269b180570b8df5b3f95d6368d5c37605cc149b
Chris Perkins <chris.perkins@intel.com>
2025-11-18 15:25:18 -0800

    can't reproduce the failure seen https://github.com/intel/llvm/issues/20006 . Wanting to try it on CI again to see if still relevant.

--- sycl/test-e2e/bindless_images/copies/copy_subregion_2D.cpp
+++ sycl/test-e2e/bindless_images/copies/copy_subregion_2D.cpp
@@ -1,7 +1,5 @@
 // REQUIRES: aspect-ext_oneapi_bindless_images
-// UNSUPPORTED: gpu
-// UNSUPPORTED-INTENDED: sporadic failure in CI
-//                       https://github.com/intel/llvm/issues/20006
+
 // XFAIL: linux && arch-intel_gpu_acm_g10 && level_zero_v2_adapter
 // XFAIL-TRACKER: https://github.com/intel/llvm/issues/20004
 // XFAIL: hip
