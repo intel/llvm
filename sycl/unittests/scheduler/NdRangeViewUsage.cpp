a99febc09a407568e39dc8f9553ed2dd0825ffc3
Ptak, Slawomir <slawomir.ptak@intel.com>
2025-11-18 12:26:40 +0000

    Unit test comment alignment

--- sycl/unittests/scheduler/NdRangeViewUsage.cpp
+++ sycl/unittests/scheduler/NdRangeViewUsage.cpp
@@ -1,4 +1,4 @@
-//==---- NdRangeViewUsage.cpp --- Check nd_range_view ------------------==//
+//==---- NdRangeViewUsage.cpp --- Check nd_range_view ----------------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
