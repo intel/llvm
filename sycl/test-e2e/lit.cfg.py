e95960f25e32b8c71fb8ae6d6bc7d4658790b659
Sabianin, Maksim <maksim.sabianin@intel.com>
2025-11-20 12:38:49 -0800

    uncomment non-NewOffloadModel cases

--- sycl/test-e2e/lit.cfg.py
+++ sycl/test-e2e/lit.cfg.py
@@ -987,7 +987,7 @@ if config.test_mode != "build-only":
 # Check for enabled NewOffloadModel
 if lit_config.params.get("enable_new_offload_model", "False") != "False":
     config.available_features.add("new-offload-model")
-    config.cxx_flags += "--offload-new-driver "
+    config.cxx_flags += " --offload-new-driver "
 
 # That has to be executed last so that all device-independent features have been
 # discovered already.
