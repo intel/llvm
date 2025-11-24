c9f98c5d9802673f3fa4ff85c954e7877ca45b27
Artur Gainullin <artur.gainullin@intel.com>
2025-11-24 11:04:20 -0800

    update handler size

--- sycl/test/abi/symbol_size_alignment.cpp
+++ sycl/test/abi/symbol_size_alignment.cpp
@@ -55,7 +55,7 @@ int main() {
 #ifdef _MSC_VER
   check<handler, 208, 8>();
 #else
-  check<handler, 216, 8>();
+  check<handler, 176, 8>();
 #endif
   check<image<1>, 16, 8>();
   check<kernel, 16, 8>();
