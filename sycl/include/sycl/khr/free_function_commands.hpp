c18c8ead5809a385764750924c03e45b40d1b7aa
Kornev, Nikita <nikita.kornev@intel.com>
2025-11-24 11:11:37 +0100

    upd header

--- sycl/include/sycl/khr/free_function_commands.hpp
+++ sycl/include/sycl/khr/free_function_commands.hpp
@@ -1,9 +1,15 @@
-//== free_function_commands.hpp - sycl_khr_free_function_commands extension ==//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the implementation of the sycl_khr_free_function_commands
+/// extension.
+///
 //===----------------------------------------------------------------------===//
 #pragma once
 
