2c7915888704a75b855dd92512a1c1f65f9f5c18
Kornev, Nikita <nikita.kornev@intel.com>
2025-11-26 13:58:40 +0100

    fix-win-precommit

--- sycl/test-e2e/Basic/sycl_ext_oneapi_platform_device_index.cpp
+++ sycl/test-e2e/Basic/sycl_ext_oneapi_platform_device_index.cpp
@@ -1,6 +1,8 @@
 // RUN: %{build} %level_zero_options %opencl_lib -o %t.out
 // RUN: %{run} %t.out
 
+#include "../helpers.hpp"
+
 #include <level_zero/ze_api.h>
 #include <sycl/detail/core.hpp>
 #include <sycl/ext/oneapi/backend/level_zero.hpp>
@@ -41,7 +43,7 @@ int main() {
 
   // Check if the index matches the index of the device’s underlying handle
   // within the list of handles returned from zeDeviceGet/clGetDeviceIDs.
-  const char *selector = std::getenv("ONEAPI_DEVICE_SELECTOR");
+  const char *selector = env::getVal("ONEAPI_DEVICE_SELECTOR");
   if (selector) {
     assert(orig_dev_index_within_plt == ext_oneapi_index_within_platform &&
            "The index returned from device::ext_oneapi_index_within_platform "
