48e9075cdbc1eb8a00e3a878d382b6fd1a26128e
Mateusz P. Nowak <mateusz.p.nowak@intel.com>
2025-12-01 14:44:28 +0000

    Use counter-based events

--- unified-runtime/source/adapters/level_zero/v2/event_provider_counter.cpp
+++ unified-runtime/source/adapters/level_zero/v2/event_provider_counter.cpp
@@ -22,9 +22,14 @@ namespace v2 {
 
 provider_counter::provider_counter(ur_platform_handle_t platform,
                                    ur_context_handle_t context,
-                                   ur_device_handle_t device) {
+                                   queue_type queueType,
+                                   ur_device_handle_t device,
+                                   event_flags_t flags)
+    : queueType(queueType), flags(flags) {
+  assert(flags & EVENT_FLAGS_COUNTER);
+
   ZE2UR_CALL_THROWS(zeDriverGetExtensionFunctionAddress,
-                    (platform->ZeDriver, "zexCounterBasedEventCreate",
+                    (platform->ZeDriver, "zexCounterBasedEventCreate2",
                      (void **)&this->eventCreateFunc));
   ZE2UR_CALL_THROWS(zelLoaderTranslateHandle,
                     (ZEL_HANDLE_CONTEXT, context->getZeHandle(),
@@ -34,17 +39,41 @@ provider_counter::provider_counter(ur_platform_handle_t platform,
       (ZEL_HANDLE_DEVICE, device->ZeDevice, (void **)&translatedDevice));
 }
 
+static zex_counter_based_event_exp_flags_t createZeFlags(queue_type queueType,
+                                                         event_flags_t flags) {
+  zex_counter_based_event_exp_flags_t zeFlags =
+      ZEX_COUNTER_BASED_EVENT_FLAG_HOST_VISIBLE;
+  if (flags & EVENT_FLAGS_PROFILING_ENABLED) {
+    zeFlags |= ZEX_COUNTER_BASED_EVENT_FLAG_KERNEL_TIMESTAMP;
+  }
+
+  if (queueType == QUEUE_IMMEDIATE) {
+    zeFlags |= ZEX_COUNTER_BASED_EVENT_FLAG_IMMEDIATE;
+  } else {
+    zeFlags |= ZEX_COUNTER_BASED_EVENT_FLAG_NON_IMMEDIATE;
+  }
+
+  return zeFlags;
+}
+
 raii::cache_borrowed_event provider_counter::allocate() {
   if (freelist.empty()) {
-    ZeStruct<ze_event_desc_t> desc;
-    desc.index = 0;
-    desc.signal = ZE_EVENT_SCOPE_FLAG_HOST;
-    desc.wait = 0;
+    zex_counter_based_event_desc_t desc = {};
+    desc.stype = ZEX_STRUCTURE_COUNTER_BASED_EVENT_DESC;
+    desc.flags = createZeFlags(queueType, flags);
+    desc.signalScope = ZE_EVENT_SCOPE_FLAG_HOST;
+
+    uint32_t equivalentFlags = ZE_EVENT_POOL_FLAG_HOST_VISIBLE;
+    if (flags & EVENT_FLAGS_PROFILING_ENABLED) {
+      equivalentFlags |= ZE_EVENT_POOL_FLAG_KERNEL_TIMESTAMP;
+    }
+    UR_LOG(DEBUG, "ze_event_pool_desc_t flags set to: {}", equivalentFlags);
+
     ze_event_handle_t handle;
 
     // TODO: allocate host and device buffers to use here
-    ZE2UR_CALL_THROWS(eventCreateFunc, (translatedContext, translatedDevice,
-                                        nullptr, nullptr, 0, &desc, &handle));
+    ZE2UR_CALL_THROWS(eventCreateFunc,
+                      (translatedContext, translatedDevice, &desc, &handle));
 
     freelist.emplace_back(handle);
   }
@@ -57,8 +86,6 @@ raii::cache_borrowed_event provider_counter::allocate() {
       [this](ze_event_handle_t handle) { freelist.push_back(handle); });
 }
 
-event_flags_t provider_counter::eventFlags() const {
-  return EVENT_FLAGS_COUNTER;
-}
+event_flags_t provider_counter::eventFlags() const { return flags; }
 
 } // namespace v2
