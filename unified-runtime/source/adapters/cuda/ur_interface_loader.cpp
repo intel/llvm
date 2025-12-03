2342a56bbbb4fbf2c3aa1d569ef786ccf411ee84
Krzysztof Święcicki <krzysztof.swiecicki@intel.com>
2025-12-03 16:12:23 +0100

    [UR] Cleanup the record & replay spec (#20806)

--- unified-runtime/source/adapters/cuda/ur_interface_loader.cpp
+++ unified-runtime/source/adapters/cuda/ur_interface_loader.cpp
@@ -259,7 +259,6 @@ UR_APIEXPORT ur_result_t UR_APICALL urGetQueueExpProcAddrTable(
   pDdiTable->pfnBeginGraphCaptureExp = urQueueBeginGraphCaptureExp;
   pDdiTable->pfnBeginCaptureIntoGraphExp = urQueueBeginCaptureIntoGraphExp;
   pDdiTable->pfnEndGraphCaptureExp = urQueueEndGraphCaptureExp;
-  pDdiTable->pfnAppendGraphExp = urQueueAppendGraphExp;
   pDdiTable->pfnIsGraphCaptureEnabledExp = urQueueIsGraphCaptureEnabledExp;
 
   return UR_RESULT_SUCCESS;
@@ -496,6 +495,7 @@ UR_DLLEXPORT ur_result_t UR_APICALL urGetEnqueueExpProcAddrTable(
   pDdiTable->pfnUSMFreeExp = urEnqueueUSMFreeExp;
   pDdiTable->pfnCommandBufferExp = urEnqueueCommandBufferExp;
   pDdiTable->pfnKernelLaunchWithArgsExp = urEnqueueKernelLaunchWithArgsExp;
+  pDdiTable->pfnGraphExp = urEnqueueGraphExp;
 
   return UR_RESULT_SUCCESS;
 }
