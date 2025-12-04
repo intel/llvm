44e3a0506d1dc484f6cc2fe38e6baec6da760e72
Michael Aziz <michael.aziz@intel.com>
2025-12-04 11:56:12 -0800

    Add `clang_cl` test case
    Signed-off-by: Michael Aziz <michael.aziz@intel.com>

--- clang/test/Driver/sycl-profile-update.cpp
+++ clang/test/Driver/sycl-profile-update.cpp
@@ -1,3 +1,4 @@
 // Ensure that the profile update mode is set to 'atomic' when compiling SYCL code.
-// RUN: %clang -### -fsycl -fprofile-instr-generate -fcoverage-mapping %s 2>&1 | FileCheck %s
+// RUN: %clangxx -### -fsycl -fprofile-instr-generate -fcoverage-mapping %s 2>&1 | FileCheck %s
+// RUN: %clang_cl -### -fsycl -fprofile-instr-generate -fcoverage-mapping %s 2>&1 | FileCheck %s
 // CHECK: "-fprofile-update=atomic"
