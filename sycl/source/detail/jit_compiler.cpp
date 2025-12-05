90afa9ad47c223dbe69c6cc65f9ffe7fd42cbc57
Artur Gainullin <artur.gainullin@intel.com>
2025-12-05 07:48:30 -0800

    Apply suggestion from @steffenlarsen
    Co-authored-by: Steffen Larsen <steffen.larsen@intel.com>

--- sycl/source/detail/jit_compiler.cpp
+++ sycl/source/detail/jit_compiler.cpp
@@ -137,7 +137,7 @@ static ::jit_compiler::BinaryFormat getTargetFormat(queue_impl &Queue) {
   default:
     throw sycl::exception(
         sycl::make_error_code(sycl::errc::feature_not_supported),
-        "Backend unsupported by jit compiler");
+        "Backend unsupported by JIT compiler");
   }
 }
 #endif // _WIN32
