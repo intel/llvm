3954a936c9f7a339dc7cc1bac7257911189576cb
Adam Fidel <adam.fidel@intel.com>
2025-12-05 11:45:37 -0800

    Add WCL XFAIL with tracker

--- sycl/test-e2e/Graph/Update/update_with_indices_accessor_double_update.cpp
+++ sycl/test-e2e/Graph/Update/update_with_indices_accessor_double_update.cpp
@@ -3,8 +3,9 @@
 // Extra run to check for leaks in Level Zero using UR_L0_LEAKS_DEBUG
 // RUN: %if level_zero %{%{l0_leak_check} %{run} %t.out 2>&1 | FileCheck %s --implicit-check-not=LEAK %}
 //
-// XFAIL: arch-intel_gpu_ptl_u || arch-intel_gpu_ptl_h
+// XFAIL: arch-intel_gpu_ptl_u || arch-intel_gpu_ptl_h || arch-intel_gpu_wcl
 // XFAIL-TRACKER: CMPLRTST-27275
+// XFAIL-TRACKER: CMPLRLLVM-72055
 
 // Tests updating a graph node accessor argument multiple times before the graph
 // is updated, using index-based explicit update
