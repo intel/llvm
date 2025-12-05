928886634102273ae52b909db54754b94b97567f
Mateusz P. Nowak <mateusz.p.nowak@intel.com>
2025-12-05 09:01:18 +0000

    fix lit to use arch and v2

--- sycl/test-e2e/format.py
+++ sycl/test-e2e/format.py
@@ -342,8 +342,14 @@ class SYCLEndToEndTest(lit.formats.ShTest):
                 elif "level_zero_v1" in full_dev_name:
                     expanded += " env UR_LOADER_USE_LEVEL_ZERO_V2=0"
 
+                # If ZE_AFFINITY_MASK is set in local config, it filters devices so we should use :0
+                device_selector = parsed_dev_name
+                if test.config.ze_affinity_mask is not None:
+                    backend, _ = parsed_dev_name.split(":", 1)
+                    device_selector = f"{backend}:0"
+
                 expanded += " ONEAPI_DEVICE_SELECTOR={} {}".format(
-                    parsed_dev_name, test.config.run_launcher
+                    device_selector, test.config.run_launcher
                 )
                 cmd = directive.command.replace("%{run}", expanded)
                 # Expand device-specific condtions (%if ... %{ ... %}).
@@ -353,6 +359,11 @@ class SYCLEndToEndTest(lit.formats.ShTest):
                     "linux",
                     "windows",
                     "preview-breaking-changes-supported",
+                    # the following entries are used by architecture-based filtering
+                    # (:arch- device, not :gpu or :cpu)
+                    "cpu",
+                    "gpu",
+                    "accelerator",
                 ]:
                     if cond_features in test.config.available_features:
                         conditions[cond_features] = True
