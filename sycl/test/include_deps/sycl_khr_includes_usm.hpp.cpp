66dabed8856783d08dd107f9d6a8f9a90c4c8a0e
Ptak, Slawomir <slawomir.ptak@intel.com>
2025-12-16 11:32:52 +0000

    Move range_rounding.hpp to detail

--- sycl/test/include_deps/sycl_khr_includes_usm.hpp.cpp
+++ sycl/test/include_deps/sycl_khr_includes_usm.hpp.cpp
@@ -160,6 +160,7 @@
 // CHECK-NEXT: ext/oneapi/kernel_properties/properties.hpp
 // CHECK-NEXT: ext/oneapi/work_group_scratch_memory.hpp
 // CHECK-NEXT: detail/sycl_local_mem_builtins.hpp
+// CHECK-NEXT: detail/range_rounding.hpp
 // CHECK-NEXT: detail/reduction_forward.hpp
 // CHECK-NEXT: detail/ur.hpp
 // CHECK-NEXT: ur_api_funcs.def
@@ -169,7 +170,6 @@
 // CHECK-NEXT: ext/oneapi/experimental/free_function_traits.hpp
 // CHECK-NEXT: ext/oneapi/experimental/raw_kernel_arg.hpp
 // CHECK-NEXT: kernel.hpp
-// CHECK-NEXT: range_rounding.hpp
 // CHECK-NEXT: sampler.hpp
 // CHECK-NEXT: sycl_span.hpp
 // CHECK-NEXT: usm/usm_pointer_info.hpp
