ba918a6c96837dbf6bbb2dc04651fd74c4ee08a0
Maksim Sabianin <maksim.sabianin@intel.com>
2025-12-18 21:02:30 +0100

    [CI][SYCL] Enable E2E testing for New Offloading Model (#20634)
    This patch has the following changes:
    * `--param enable_new_offload_model` is added to SYCL E2E LIT test
    configuration
    * NewOffloadModel mode run is added to sycl-linux-precommit
    * NewOffloadModel mode run is added to sycl-nightly for linux and
    windows.

--- sycl/test-e2e/Compression/compression.cpp
+++ sycl/test-e2e/Compression/compression.cpp
@@ -1,3 +1,6 @@
+// XFAIL: new-offload-model && target-amd
+// XFAIL-TRACKER: https://github.com/intel/llvm/issues/20797
+
 // End-to-End test for testing device image compression.
 // REQUIRES: zstd
 
