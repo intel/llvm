9ccd3175d73616cf6ecfb937dbebd7ff4490f0f1
Artur Gainullin <artur.gainullin@intel.com>
2025-12-31 19:34:31 +0100

    Fix

--- sycl/test-e2e/Basic/built-ins/const_vec_common.cpp
+++ sycl/test-e2e/Basic/built-ins/const_vec_common.cpp
@@ -1,6 +1,6 @@
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
-// RUN: %{ %{build} -D__SYCL_USE_LIBSYCL8_VEC_IMPL=1 -o %t2.out
+// RUN: %{build} -D__SYCL_USE_LIBSYCL8_VEC_IMPL=1 -o %t2.out
 // RUN: %{run} %t2.out
 
 #include "vec_common.hpp"
