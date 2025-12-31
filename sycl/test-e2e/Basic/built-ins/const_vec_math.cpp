e177ba0565ef9bb130a7307ecece84f91e0ceccf
Artur Gainullin <artur.gainullin@intel.com>
2025-12-31 18:52:41 +0100

    Add option

--- sycl/test-e2e/Basic/built-ins/const_vec_math.cpp
+++ sycl/test-e2e/Basic/built-ins/const_vec_math.cpp
@@ -2,7 +2,7 @@
 
 // RUN: %{build} %{mathflags} -o %t.out
 // RUN: %{run} %t.out
-// RUN: %{build} %{mathflags} -o %t2.out
+// RUN: %{build} -D__SYCL_USE_LIBSYCL8_VEC_IMPL=1 %{mathflags} -o %t2.out
 // RUN: %{run} %t2.out
 
 #include "vec_math.hpp"
