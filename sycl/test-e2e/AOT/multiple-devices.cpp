8355e49740808f62076937511b4a9af2250cda56
Yixing Zhang <yixing.zhang@intel.com>
2026-01-01 08:39:59 -0800

    Resolve comments

--- sycl/test-e2e/AOT/multiple-devices.cpp
+++ sycl/test-e2e/AOT/multiple-devices.cpp
@@ -5,21 +5,24 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-// End-to-End test for testing appropriate AOT-compiled device image got
-// selected correctly from a fat binary built for multiple device targets
+// End-to-end test for verifying that the targets specified at link command
+// line are used to select images from the fat binary built for multiple
+// targets, and that compilation occurs for those targets.
 
-// UNSUPPORTED: target={{.*}}
-
-// This test should not be supported because device names should be specified
-// when compiling AOT fat binaries for GPU targets.
+// UNSUPPORTED-INTENDED:  device names are expected to be specified when
+// compiling AOT fat binaries. This test should be removed, when old offload
+// model support is removed.
+// UNSUPPORTED-TRACKER: https://github.com/intel/llvm/issues/20988
 //
-// Explanation: In the old offloading model, when compiling a fat binary for
-// multiple device targets A, B, C and linking only with targets A and B,
-// the AOT images for only targets A and B are selected from the fat binary.
-// However, in the new offloading model, the AOT images for all targets A, B, C
-// will be selected during linking even though only A and B are specified.
-// Therefore, when compiling AOT fat binaries for GPU, the device name must be
-// specified, otherwise linking will fail due to missing device names.
+// Explanation: Old offloading model was accepting compilation of a fat binary
+// for AOT target without specifying specific device. In that case specific
+// device to AOT-compile for was specified at link command line and compilation
+// was happening only for targets specified in link command line, ignoring
+// targets specified in compile command line, which doesn't look correct. In the
+// new offloading model, the AOT images for all targets specified in compile
+// command line are honored and AOT-compiled at link stage. To make it correct,
+// if AOT target is specified at compile time, specific device should also be
+// provided at compilation command line.
 //
 // A modified test can be found at NewOffloadDriver/aot-multiple-device.cpp
 
@@ -28,21 +31,26 @@
 // Produce a fat object for all targets (generic SPIR-V, CPU, GPU)
 // RUN: %clangxx -fsycl -fsycl-targets=spir64,spir64_x86_64,spir64_gen %S/Inputs/aot.cpp -c -o %t.o
 
-// AOT image selection with CPU and GPU targets available
-// RUN: %{run-aux} %clangxx -fsycl -fsycl-targets=spir64_x86_64,spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts %t.o -o %t_cpu_gpu.out
+// Verify that AOT compilation occurs for the device targets (GPU, CPU)
+// specified in the link command line. Note that generic SPIR-V compilation
+// is enabled by default even when only GPU or CPU targets are specified.
+// RUN: %{run-aux} %clangxx -fsycl -fsycl-targets=spir64_x86_64,spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts -v %t.o -o %t_cpu_gpu.out 2>&1 | FileCheck %s --check-prefix=CHECK-AOT-CPU --check-prefix=CHECK-AOT-GPU --check-prefix=CHECK-GENERIC
 // RUN: %{run} %t_cpu_gpu.out
 
-// AOT image selection with generic SPIR-V and GPU target available
-// RUN: %clangxx -fsycl -fsycl-targets=spir64,spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts %t.o -o %t_spv_gpu.out
+// Verify that AOT compilation occurs for the device targets (GPU) specified in
+// the link command line.
+// RUN: %clangxx -fsycl -fsycl-targets=spir64,spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts -v %t.o -o %t_spv_gpu.out 2>&1 | FileCheck %s --check-prefix=CHECK-GENERIC --check-prefix=CHECK-AOT-GPU --check-prefix=CHECK-NO-AOT-CPU
 // Check that execution on AOT-compatible devices is unaffected
 // RUN: env ONEAPI_DEVICE_SELECTOR="*:gpu" %{run-unfiltered-devices} %t_spv_gpu.out
 
-// AOT image selection with generic SPIR-V and CPU target available
-// RUN: %{run-aux} %clangxx -fsycl -fsycl-targets=spir64,spir64_x86_64 %t.o -o %t_spv_cpu.out
+// Verify that AOT compilation occurs for the device targets (CPU) specified in
+// the link command line.
+// RUN: %{run-aux} %clangxx -fsycl -fsycl-targets=spir64,spir64_x86_64 -v %t.o -o %t_spv_cpu.out 2>&1 | FileCheck %s --check-prefix=CHECK-GENERIC --check-prefix=CHECK-AOT-CPU --check-prefix=CHECK-NO-AOT-GPU
 // Check that execution on AOT-compatible devices is unaffected
 // RUN: env ONEAPI_DEVICE_SELECTOR="*:cpu" %{run-unfiltered-devices} %t_spv_cpu.out
 
-// AOT image selection with all targets available (SPIR-V, CPU, GPU)
-// RUN: %{run-aux} %clangxx -fsycl -fsycl-targets=spir64,spir64_x86_64,spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts %t.o -o %t_spv_cpu_gpu.out
-// Check that execution on AOT-compatible devices is unaffected
-// RUN: %{run} %t_spv_cpu_gpu.out
+// CHECK-GENERIC-DAG: clang-offload-wrapper{{.*}} -target=spir64{{[^_]}}
+// CHECK-AOT-CPU-DAG: clang-offload-wrapper{{.*}} -target=spir64_x86_64
+// CHECK-AOT-GPU-DAG: clang-offload-wrapper{{.*}} -target=spir64_gen
+// CHECK-NO-AOT-CPU-NOT: clang-offload-wrapper{{.*}} -target=spir64_x86_64
+// CHECK-NO-AOT-GPU-NOT: clang-offload-wrapper{{.*}} -target=spir64_gen
--- sycl/test-e2e/NewOffloadDriver/aot-multiple-device.cpp
+++ sycl/test-e2e/NewOffloadDriver/aot-multiple-device.cpp
@@ -5,11 +5,10 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-// End-to-End test for testing that AOT-compiled device images for multiple
-// devices are handled properly during linking. In the new offloading model,
-// when compiling a fat binary for multiple device targets A, B, C and linking
-// only with targets A and B, the AOT images for all targets A, B, C will be
-// selected during linking even though only A and B are specified.
+// End-to-end test for verifying that when compiling a fat binary for multiple
+// device targets, all targets from the fat binary are processed during linking,
+// regardless of which specific targets are specified in the link command
+// line.
 
 // This test is copied from AOT/multiple_devices.cpp
 // and modified to test with the New Offloading Model.
@@ -20,28 +19,21 @@
 // It is required to specify the device name when compiling AOT for GPU.
 // RUN: %{run-aux} %clangxx --offload-new-driver -fsycl -fsycl-targets=spir64,spir64_x86_64,spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts %S/Inputs/aot.cpp -c -o %t.o
 
-// AOT image selection with CPU and GPU targets available.
 // RUN: %{run-aux} %clangxx --offload-new-driver -fsycl -fsycl-targets=spir64_x86_64,spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts -v %t.o -o %t_cpu_gpu.out 2>&1 | FileCheck %s --check-prefix=CHECK-DEVICE
 // RUN: %{run} %t_cpu_gpu.out
 
-// AOT image selection with generic SPIR-V and GPU target available.
 // Check that all targets compiled in the fat binary are selected during
 // linking (including CPU even though it is not specified during linking).
 // RUN: %{run-aux} %clangxx --offload-new-driver -fsycl -fsycl-targets=spir64,spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts -v %t.o -o %t_spv_gpu.out 2>&1 | FileCheck %s --check-prefix=CHECK-DEVICE
 // Check that execution on AOT-compatible devices is unaffected
 // RUN: env ONEAPI_DEVICE_SELECTOR="*:gpu" %{run-unfiltered-devices} %t_spv_gpu.out
 
-// AOT image selection with generic SPIR-V and CPU target available.
 // Check that all targets compiled in the fat binary are selected during
 // linking (including GPU even though it is not specified during linking).
 // RUN: %{run-aux} %clangxx --offload-new-driver -fsycl -fsycl-targets=spir64,spir64_x86_64 -v %t.o -o %t_spv_cpu.out 2>&1 | FileCheck %s --check-prefix=CHECK-DEVICE
 // Check that execution on AOT-compatible devices is unaffected.
 // RUN: env ONEAPI_DEVICE_SELECTOR="*:cpu" %{run-unfiltered-devices} %t_spv_cpu.out
 
-// AOT image selection with all targets available (SPIR-V, CPU, GPU).
-// RUN: %{run-aux} %clangxx --offload-new-driver -fsycl -fsycl-targets=spir64,spir64_x86_64,spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts -v %t.o -o %t_spv_cpu_gpu.out 2>&1 | FileCheck %s --check-prefix=CHECK-DEVICE
-// Check that execution on AOT-compatible devices is unaffected.
-// RUN: %{run} %t_spv_cpu_gpu.out
-
-// CHECK-DEVICE-DAG: {{.*}}ocloc{{.*}} -device {{.*}}
-// CHECK-DEVICE-DAG: opencl-aot{{.*}} --device=cpu
+// CHECK-DEVICE-DAG: spirv-to-ir-wrapper{{.*}}spir64_gen-unknown-unknown{{.*}}
+// CHECK-DEVICE-DAG: spirv-to-ir-wrapper{{.*}}spir64_x86_64-unknown-unknown{{.*}}
+// CHECK-DEVICE-DAG: spirv-to-ir-wrapper{{.*}}spir64-unknown-unknown{{.*}}
