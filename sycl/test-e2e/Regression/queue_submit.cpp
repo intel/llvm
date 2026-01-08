bd43daa9542704606084c240f0dd2c2ba3e26d26
Agarwal, Udit <udit.agarwal@intel.com>
2026-01-08 18:00:37 +0100

    ADdress feedback

--- sycl/test-e2e/Regression/queue_submit.cpp
+++ sycl/test-e2e/Regression/queue_submit.cpp
@@ -15,27 +15,24 @@
 
 class kernel_set_value;
 
-void submit(sycl::queue *queue, sycl::kernel *kernel) {
+void submit(sycl::queue &queue, sycl::kernel &kernel) {
   int data[DIMS];
   try {
     sycl::buffer<int, 1> result_buf{data, sycl::range<1>{DIMS}};
-    queue->submit([&](sycl::handler &cgh) {
+    queue.submit([&](sycl::handler &cgh) {
       auto result_acc =
           result_buf.get_access<sycl::access::mode::discard_write>(cgh);
       cgh.set_arg(0, result_acc);
-      cgh.parallel_for(sycl::range<1>{DIMS}, *kernel);
+      cgh.parallel_for(sycl::range<1>{DIMS}, kernel);
     });
-    queue->wait_and_throw();
+    queue.wait_and_throw();
   } catch (sycl::exception &e) {
     std::cerr << "Exception thrown: " << e.what() << "\n";
     return;
   }
 
-  for (int i = 0; i < DIMS; i++) {
-    if (data[i] != i) {
-      std::cerr << "data[" << i << "] != " << i << " (got " << data[i] << ")\n";
-    }
-  }
+  for (int i = 0; i < DIMS; i++)
+    assert(data[i] == i);
 }
 
 void run_test(size_t numThreads) {
@@ -62,7 +59,7 @@ void run_test(size_t numThreads) {
   // Spawn multiple threads submitting the same kernel to the same queue.
   std::vector<std::thread> threads;
   for (size_t i = 0; i < numThreads; ++i)
-    threads.push_back(std::thread(&submit, &queue, &kernel));
+    threads.push_back(std::thread(&submit, std::ref(queue), std::ref(kernel)));
 
   for (auto &t : threads)
     t.join();
