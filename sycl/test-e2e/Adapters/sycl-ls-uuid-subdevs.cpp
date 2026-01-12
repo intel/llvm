5d0df6e35e0dc4296021a51f74820429bcfe2275
Kornev, Nikita <nikita.kornev@intel.com>
2026-01-12 16:08:28 +0100

    [sycl-rel-6_3] Update some E2E tests

--- sycl/test-e2e/Adapters/sycl-ls-uuid-subdevs.cpp
+++ sycl/test-e2e/Adapters/sycl-ls-uuid-subdevs.cpp
@@ -1,14 +1,10 @@
 /* Test to check that sycl-ls is outputting UUID and number of sub and sub-sub
  * devices. */
-// REQUIRES:  gpu, level_zero
+// REQUIRES:  gpu, level_zero, gpu-intel-pvc
 
-// XFAIL: linux && run-mode && (arch-intel_gpu_bmg_g21 || gpu-intel-dg2) && !igc-dev
-// XFAIL-TRACKER: https://github.com/intel/llvm/issues/18576
-
-// XFAIL: windows && arch-intel_gpu_bmg_g21
-// XFAIL-TRACKER: https://github.com/intel/llvm/issues/18576
 // As of now, ZEX_NUMBER_OF_CCS is not working with FLAT hierachy,
 // which is the new default on PVC.
+// PVC is the only HW now that supports 4 CCS.
 
 // RUN: %{run-unfiltered-devices} env ONEAPI_DEVICE_SELECTOR="level_zero:*" env ZE_FLAT_DEVICE_HIERARCHY=COMPOSITE env ZEX_NUMBER_OF_CCS=0:4 sycl-ls --verbose | \
 // RUN: FileCheck %s
