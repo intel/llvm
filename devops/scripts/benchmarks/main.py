9fb430a73d1d8ee5743c4b95853e252acf4df684
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2026-01-14 23:09:17 +0100

    [Bench] Remove unused TestSuite (#21052)
    Before, it was run (manually?) to verify benchmark's structures.
    We have proper integration tests now, run on PRs updating benchmarks.

--- devops/scripts/benchmarks/main.py
+++ devops/scripts/benchmarks/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2024-2025 Intel Corporation
+# Copyright (C) 2024-2026 Intel Corporation
 # Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM Exceptions.
 # See LICENSE.TXT
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
@@ -16,7 +16,6 @@ from benches.velocity import VelocityBench
 from benches.syclbench import *
 from benches.llamacpp import *
 from benches.umf import *
-from benches.test import TestSuite
 from benches.benchdnn import OneDnnBench
 from benches.base import TracingType
 from options import Compare, options
@@ -276,7 +275,6 @@ def main(directory, additional_env_vars, compare_names, filter, execution_stats)
         UMFSuite(),
         GromacsBench(),
         OneDnnBench(),
-        TestSuite(),
     ]
 
     # Collect metadata from all benchmarks without setting them up
@@ -486,7 +484,7 @@ def main(directory, additional_env_vars, compare_names, filter, execution_stats)
             html_path = os.path.normpath(
                 os.path.join(os.path.dirname(__file__), "html")
             )
-        log.info(f"Generating HTML with benchmark results in {html_path}...")
+        log.info(f"Generating HTML with benchmark results in '{html_path}'...")
 
         generate_html(history, compare_names, html_path, metadata)
         log.info(f"HTML with benchmark results has been generated")
