f01abea5085f37b584c728cd404af2de76f8a885
Maksim Sabianin <maksim.sabianin@intel.com>
2026-01-16 17:45:58 +0100

    [CI] Reapply "[SYCL] Enable E2E testing for New Offloading Model" (#20634)" (#21036)
    Reapply of #20634
    
    This patch has the following changes:
    * `--param enable_new_offload_model` is added to SYCL E2E LIT test
    configuration
    * NewOffloadModel mode run is added to sycl-linux-precommit and
    sycl-windows-precommit.
    * Failing tests in NewOffloadModel mode are marked with
    xfail/unsupported marker.

--- sycl/test-e2e/OptionalKernelFeatures/is_compatible/is_compatible_amdgcn.cpp
+++ sycl/test-e2e/OptionalKernelFeatures/is_compatible/is_compatible_amdgcn.cpp
@@ -1,5 +1,8 @@
 // REQUIRES: hip_dev_kit
 
+// XFAIL: new-offload-model
+// XFAIL-TRACKER: https://github.com/intel/llvm/issues/20797
+
 // RUN: %clangxx %hip_options -fsycl -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend=amdgcn-amd-amdhsa --offload-arch=gfx1030 %S/Inputs/is_compatible_with_env.cpp -o %t.out
 
 // RUN: %if !hip %{ not %} %{run} %t.out
