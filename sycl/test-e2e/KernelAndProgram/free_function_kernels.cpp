4178b9b1890292a9456e9638ad421a574ab5b565
Skrobot, Daniel <daniel.skrobot@intel.com>
2026-01-26 17:57:56 +0100

    mark test as unsuported on windows

--- sycl/test-e2e/KernelAndProgram/free_function_kernels.cpp
+++ sycl/test-e2e/KernelAndProgram/free_function_kernels.cpp
@@ -1,6 +1,8 @@
 // REQUIRES: aspect-usm_shared_allocations
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
+// UNSUPPORTED: windows
+// UNSUPPORTED-TRACKER: CMPLRLLVM-73028
 // This test tests free function kernel code generation and execution.
 
 #include <iostream>
