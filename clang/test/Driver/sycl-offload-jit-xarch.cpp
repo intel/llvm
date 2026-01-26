435b3dc927b40d39ec922c101fa8cafe03d8592b
srividya sundaram <srividya.sundaram@intel.com>
2026-01-26 15:47:32 -0800

    Add check for CLW.

--- clang/test/Driver/sycl-offload-jit-xarch.cpp
+++ clang/test/Driver/sycl-offload-jit-xarch.cpp
@@ -5,7 +5,7 @@
 // RUN: %clang -fsycl --offload-new-driver -Xarch_spir64 -O3 -### %s 2>&1 \
 // RUN: | FileCheck -check-prefixes=SYCL-DEVICE-O3 %s
 // SYCL-DEVICE-O3: "-triple" "spir64-unknown-unknown" "-O3"{{.*}} "-fsycl-is-device"
-
+// CLW-O3: {{"[^"]*clang-linker-wrapper[^"]*".* "--device-compiler=spirv64-unknown-unknown=-O3"}}
 
 // Verify that `-Xarch_spir64` forwards libraries to the device linker.
 // RUN: %clang -fsycl --offload-new-driver -Xarch_spir64 -Wl,-lfoo -### %s 2>&1 \
