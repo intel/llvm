0d48d73d28a7d1028b756d8c4aadcd8ab603d798
sys_ce_bb <sys_ce_bb@intel.com>
2026-01-28 14:54:13 -0800

    [NFC][Driver] Require shell for spirv-tools-err.c
    The env PATH='' won't work on windows.

--- clang/test/Driver/spirv-tools-err.c
+++ clang/test/Driver/spirv-tools-err.c
@@ -1,5 +1,6 @@
 // UNSUPPORTED: spirv-tools
 // REQUIRES: spirv-registered-target
+// REQUIRES: shell
 
 // RUN: env PATH='' not %clang -o /dev/null --target=spirv64 --save-temps -v %s 2>&1 | FileCheck %s
 
