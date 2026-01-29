ae5e695b901a88e79ed9b28c7d1aa7549eb110cb
Nick Sarnie <nick.sarnie@intel.com>
2026-01-30 08:21:38 +0900

    [SYCL][E2E] Fix nightly interop failures (#21169)
    https://github.com/intel/llvm/actions/runs/21464844352
    
    Two XPASS on AWS but fails on our runners for some reason. BMG times out
    so probably sporadic.
    
    Signed-off-by: Nick Sarnie <nick.sarnie@intel.com>

--- sycl/test-e2e/bindless_images/vulkan_interop/unsampled_images.cpp
+++ sycl/test-e2e/bindless_images/vulkan_interop/unsampled_images.cpp
@@ -8,6 +8,9 @@
 // XFAIL: linux && gpu-intel-dg2
 // XFAIL-TRACKER: https://github.com/intel/llvm/issues/21136
 
+// UNSUPPORTED: cuda
+// UNSUPPORTED-TRACKER: https://github.com/intel/llvm/issues/21131
+
 // RUN: %{build} %link-vulkan -o %t.out %if target-spir %{ -Wno-ignored-attributes -DTEST_L0_SUPPORTED_VK_FORMAT %}
 // RUN: %{run} env NEOReadDebugKeys=1 UseBindlessMode=1 UseExternalAllocatorForSshAndDsh=1 %t.out
 
