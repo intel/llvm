54260eb8e861b43fffa1e8156a991f2cc4501b5e
travisle2 <travis.le@intel.com>
2026-02-03 10:30:42 -0800

    [SYCL][E2E] Move instructions that link Shared Library to Run in Linux (#21182)
    The shared libraries' path may be invalid when the workspace location is
    changed. This update moves linking shared libraries to the run phase.

--- sycl/test-e2e/Compression/compression_multiple_tu.cpp
+++ sycl/test-e2e/Compression/compression_multiple_tu.cpp
@@ -9,7 +9,7 @@
 // RUN: %{build} --offload-compress -DENABLE_KERNEL1 -shared -fPIC -o %t.dir/kernel1.so
 // RUN: %{build} -DENABLE_KERNEL2 -shared -fPIC -o %t.dir/kernel2.so
 
-// RUN: %{build} %t.dir/kernel1.so %t.dir/kernel2.so -o %t_compress.out
+// RUN: %{run-aux} %{build} %t.dir/kernel1.so %t.dir/kernel2.so -o %t_compress.out
 // RUN: %{run} %t_compress.out
 #if defined(ENABLE_KERNEL1) || defined(ENABLE_KERNEL2)
 #include <sycl/builtins.hpp>
