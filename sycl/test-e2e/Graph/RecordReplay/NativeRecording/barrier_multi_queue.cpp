7cde912de8e4171fdd961bfb3969edf6bf7494f3
Matthew Michel <matthew.michel@intel.com>
2026-02-12 14:37:34 -0600

    Move native recording tests into new directory (#2)

--- sycl/test-e2e/Graph/RecordReplay/NativeRecording/barrier_multi_queue.cpp
+++ sycl/test-e2e/Graph/RecordReplay/NativeRecording/barrier_multi_queue.cpp
@@ -5,4 +5,4 @@
 // Extra run to check for leaks in Level Zero using UR_L0_LEAKS_DEBUG
 // RUN: %if level_zero %{env SYCL_GRAPH_ENABLE_NATIVE_RECORDING=1 %{l0_leak_check} %{run} %t.out 2>&1 | FileCheck %s --implicit-check-not=LEAK %}
 
-#include "../Inputs/barrier_multi_queue.cpp"
+#include "../../Inputs/barrier_multi_queue.cpp"
