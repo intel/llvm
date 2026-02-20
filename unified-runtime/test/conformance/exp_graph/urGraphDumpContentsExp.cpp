9265d4edec09990d98de5a7e9e7a90a99f8df936
Matthew Michel <matthew.michel@intel.com>
2026-02-20 14:13:50 -0800

    Add support for urGraphDumpContentsExp with L0v2 adapter

---
+++ unified-runtime/test/conformance/exp_graph/urGraphDumpContentsExp.cpp
@@ -0,0 +1,71 @@
+// Copyright (C) 2026 Intel Corporation
+// Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM
+// Exceptions. See LICENSE.TXT
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "fixtures.h"
+#include <cstdio>
+#include <fstream>
+#include <iterator>
+#include <string>
+
+using urGraphDumpContentsExpTest = uur::urGraphPopulatedExpTest;
+
+UUR_DEVICE_TEST_SUITE_WITH_QUEUE_TYPES(
+    urGraphDumpContentsExpTest,
+    ::testing::Values(0 /* In-Order */,
+                      UR_QUEUE_FLAG_OUT_OF_ORDER_EXEC_MODE_ENABLE));
+
+TEST_P(urGraphDumpContentsExpTest, Success) {
+  const char *filePath = "test_graph_dump.dot";
+
+  ASSERT_SUCCESS(urGraphDumpContentsExp(graph, filePath));
+
+  std::ifstream file(filePath);
+  ASSERT_TRUE(file.good());
+
+  std::string content((std::istreambuf_iterator<char>(file)),
+                      std::istreambuf_iterator<char>());
+  ASSERT_NE(content.find("digraph"), std::string::npos);
+
+  file.close();
+  std::remove(filePath);
+}
+
+TEST_P(urGraphDumpContentsExpTest, SuccessEmptyGraph) {
+  const char *filePath = "test_empty_graph_dump.dot";
+
+  ur_exp_graph_handle_t emptyGraph = nullptr;
+  ASSERT_SUCCESS(urGraphCreateExp(context, &emptyGraph));
+  ASSERT_SUCCESS(urGraphDumpContentsExp(graph, filePath));
+
+  std::ifstream file(filePath);
+  ASSERT_TRUE(file.good());
+
+  std::string content((std::istreambuf_iterator<char>(file)),
+                      std::istreambuf_iterator<char>());
+  ASSERT_NE(content.find("digraph"), std::string::npos);
+
+  file.close();
+
+  std::remove(filePath);
+  ASSERT_SUCCESS(urGraphDestroyExp(emptyGraph));
+}
+
+TEST_P(urGraphDumpContentsExpTest, InvalidNullHandleGraph) {
+  const char *filePath = "test_graph_dump.dot";
+  ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_NULL_HANDLE,
+                   urGraphDumpContentsExp(nullptr, filePath));
+}
+
+TEST_P(urGraphDumpContentsExpTest, InvalidNullPointerFilePath) {
+  ASSERT_EQ_RESULT(UR_RESULT_ERROR_INVALID_NULL_POINTER,
+                   urGraphDumpContentsExp(graph, nullptr));
+}
+
+TEST_P(urGraphDumpContentsExpTest, InvalidFilePath) {
+  const char *invalidPath = "/invalid/path/that/does/not/exist/graph.dot";
+  ASSERT_EQ_RESULT(UR_RESULT_ERROR_UNKNOWN,
+                   urGraphDumpContentsExp(graph, invalidPath));
+}
