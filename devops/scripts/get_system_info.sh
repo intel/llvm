942c4ab2ca06355f6ca9c829bc19be30009d7347
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2026-02-24 16:16:42 +0100

    [CI] Move get_system_info.sh script from UR to devops

--- devops/scripts/get_system_info.sh
+++ devops/scripts/get_system_info.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#  Copyright (C) 2023 Intel Corporation
+#  Copyright (C) 2023-2026 Intel Corporation
 #  Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM Exceptions.
 #  See LICENSE.TXT
 #  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
