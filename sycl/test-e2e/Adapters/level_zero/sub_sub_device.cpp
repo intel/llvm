016f545d08286ecbaf2490a61ef2863e2df1b217
Chris Perkins <chris.perkins@intel.com>
2026-02-25 12:31:12 -0800

    maybe this?

--- sycl/test-e2e/Adapters/level_zero/sub_sub_device.cpp
+++ sycl/test-e2e/Adapters/level_zero/sub_sub_device.cpp
@@ -1,6 +1,6 @@
 // REQUIRES:  arch-intel_gpu_pvc, level_zero
 
-// RUN: %{build} %level_zero_options -o %t.out
+// RUN: %{build} -o %t.out
 
 // TODO - at this time ZEX_NUMBER_OF_CCS is not working with FLAT hierachy,
 // which is the new default on PVC.  Once it is supported, we'll test on both.
@@ -8,11 +8,11 @@
 // conjunction with ZEX_NUMBER_OF_CCS
 // DEFINE: %{setup_env} = env ZE_FLAT_DEVICE_HIERARCHY=COMPOSITE ZE_AFFINITY_MASK=0 ZEX_NUMBER_OF_CCS=0:4
 
-// RUN: %{setup_env} env UR_L0_DEBUG=1 %{run} %t.out 2>&1 | FileCheck %s
+// The output from UR_L0_DEBUG=1 is 2GB for some reason. So trying to filter it.
+// RUN: %{setup_env} env UR_L0_DEBUG=1 %{run} %t.out 2>&1 | grep "getZeQueue" | FileCheck %s
+
 // RUN: %{setup_env} %{run} %t.out
 
-// Check that queues created on sub-sub-devices are going to specific compute
-// engines:
 // CHECK: [getZeQueue]: create queue ordinal = 0, index = 0 (round robin in [0, 0])
 // CHECK: [getZeQueue]: create queue ordinal = 0, index = 1 (round robin in [1, 1])
 // CHECK: [getZeQueue]: create queue ordinal = 0, index = 2 (round robin in [2, 2])
