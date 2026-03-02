f68e03a2682ecf1d7ff36d951e00f2a1cc72ad9e
Artur Gainullin <artur.gainullin@intel.com>
2026-03-02 11:51:13 -0800

    [SYCL][XPTI] Optimize instrumentationGetKernelName by caching demangled name in DeviceKernelInfo (#21384)
    Store the demangled kernel name lazily in DeviceKernelInfo instead of
    demangling same kernel name repeteadly.

--- sycl/source/detail/device_kernel_info.cpp
+++ sycl/source/detail/device_kernel_info.cpp
@@ -8,6 +8,15 @@
 #include <detail/device_kernel_info.hpp>
 #include <detail/program_manager/program_manager.hpp>
 
+#ifdef __has_include
+#if __has_include(<cxxabi.h>)
+#define __SYCL_ENABLE_GNU_DEMANGLING
+#include <cstdlib>
+#include <cxxabi.h>
+#include <memory>
+#endif
+#endif
+
 namespace sycl {
 inline namespace _V1 {
 namespace detail {
@@ -48,6 +57,26 @@ void DeviceKernelInfo::setImplicitLocalArgPos(int Pos) {
   assert(!MImplicitLocalArgPos.has_value() || MImplicitLocalArgPos == Pos);
   MImplicitLocalArgPos = Pos;
 }
+
+std::string_view DeviceKernelInfo::getDemangledName() const {
+  std::call_once(MDemangledNameInitFlag, [&]() {
+#ifdef __SYCL_ENABLE_GNU_DEMANGLING
+    int Status = -1; // some arbitrary value to eliminate the compiler warning
+    char *Demangled =
+        abi::__cxa_demangle(Name.data(), nullptr, nullptr, &Status);
+    if (Status == 0 && Demangled) {
+      std::unique_ptr<char, void (*)(void *)> Guard(Demangled, std::free);
+      MDemangledName = std::string(Guard.get());
+    } else {
+      MDemangledName = std::string(Name);
+    }
+#else
+    MDemangledName = std::string(Name);
+#endif
+  });
+  return MDemangledName;
+}
+
 } // namespace detail
 } // namespace _V1
 } // namespace sycl
