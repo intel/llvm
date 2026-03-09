a07f2055c1b6e94c95e661235f961b79b933ad60
Kornev, Nikita <nikita.kornev@intel.com>
2026-03-09 13:44:06 +0100

    format

--- sycl/include/sycl/ext/oneapi/experimental/enqueue_functions.hpp
+++ sycl/include/sycl/ext/oneapi/experimental/enqueue_functions.hpp
@@ -199,15 +199,15 @@ template <typename T>
 void host_task(sycl::queue Q, T &&hostTaskCallable,
                const sycl::detail::code_location &CodeLoc =
                    sycl::detail::code_location::current()) {
-  submit(std::move(Q),
-         [&](sycl::handler &cgh) {
-           cgh.host_task(std::forward<T>(hostTaskCallable));
-         },
-         CodeLoc);
+  submit(
+      std::move(Q),
+      [&](sycl::handler &cgh) {
+        cgh.host_task(std::forward<T>(hostTaskCallable));
+      },
+      CodeLoc);
 }
 
-template <typename T>
-void host_task(handler &CGH, T&& hostTaskCallable) {
+template <typename T> void host_task(handler &CGH, T &&hostTaskCallable) {
   CGH.host_task(std::forward<T>(hostTaskCallable));
 }
 
