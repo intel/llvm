266927d371e15e19db0001eaf2583954ee2426cc
Michael D Toguchi <michael.d.toguchi@intel.com>
2026-03-09 15:34:16 -0700

    Mark tests as Linux only, cannot mix Windows .bc files with compile

--- clang/test/Driver/sycl-spirv-ext.cpp
+++ clang/test/Driver/sycl-spirv-ext.cpp
@@ -1,4 +1,5 @@
 // Generate .o file as SYCL device library file.
+// REQUIRES: system-linux
 //
 // RUN: touch %t.devicelib.cpp
 // RUN: %clang %t.devicelib.cpp -fsycl -fsycl-targets=spir64-unknown-unknown -c --offload-new-driver -o %t_1.devicelib.o
@@ -7,7 +8,7 @@
 
 /// Check llvm-spirv extensions that are set
 
-// RUN: %clang -target x86_64-unknown-linux-gnu -fsycl --offload-new-driver \ 
+// RUN: %clang -target x86_64-unknown-linux-gnu -fsycl --offload-new-driver \
 // RUN:   -fsycl-targets=spir64-unknown-unknown -c %s -o %t_1.o
 // RUN: clang-linker-wrapper -sycl-device-libraries=%t_1.devicelib.o \
 // RUN:   "--host-triple=x86_64-unknown-linux-gnu" "--linker-path=/usr/bin/ld" \
