9a9ec208b786c97ea9f1203cbfbaab578dd56ec2
Chris Perkins <chris.perkins@intel.com>
2026-03-10 13:48:36 -0700

    fix

--- sycl/test-e2e/SpecConstants/2020/native_specialization_constant.cpp
+++ sycl/test-e2e/SpecConstants/2020/native_specialization_constant.cpp
@@ -7,7 +7,7 @@
 // RUN: %{build} -DJIT -o %t1.out
 // RUN: %{run} %t1.out
 
-// RUN: %if any-device-is-gpu %{ %{run-aux} %clangxx -fsycl -fsycl-targets=spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts %s -o %t2.out %}
+// RUN: %if gpu %{ %{run-aux} %clangxx -fsycl -fsycl-targets=spir64_gen -Xsycl-target-backend=spir64_gen %gpu_aot_target_opts %s -o %t2.out %}
 // RUN: %if gpu %{ %{run} %t2.out %}
 
 // RUN: %if any-device-is-cpu %{ %{run-aux} %clangxx -fsycl -fsycl-targets=spir64_x86_64 %s -o %t3.out %}
