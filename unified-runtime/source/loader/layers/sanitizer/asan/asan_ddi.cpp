bdbe295c5959fbe603d4eab3151b87fbbf473def
Lukasz Dorau <lukasz.dorau@intel.com>
2026-03-19 08:41:51 +0000

    [UR] Do not set kernel args in intercept functions for urEnqueueKernelLaunchWithArgsExp()
    Signed-off-by: Lukasz Dorau <lukasz.dorau@intel.com>

--- unified-runtime/source/loader/layers/sanitizer/asan/asan_ddi.cpp
+++ unified-runtime/source/loader/layers/sanitizer/asan/asan_ddi.cpp
@@ -1744,63 +1744,6 @@ __urdlllocal ur_result_t UR_APICALL urEnqueueKernelLaunchWithArgsExp(
   std::memcpy(KernelInfo.ArgProps.data(), pArgs,
               numArgs * sizeof(ur_exp_kernel_arg_properties_t));
 
-  // We need to set all the args now rather than letting LaunchWithArgs handle
-  // them. This is because some implementations of
-  // urKernelGetSuggestedLocalWorkSize, which is used in preLaunchKernel, rely
-  // on all the args being set.
-  for (uint32_t ArgPropIndex = 0; ArgPropIndex < numArgs; ArgPropIndex++) {
-    switch (pArgs[ArgPropIndex].type) {
-    case UR_EXP_KERNEL_ARG_TYPE_LOCAL: {
-      UR_CALL(ur_sanitizer_layer::asan::urKernelSetArgLocal(
-          hKernel, pArgs[ArgPropIndex].index, pArgs[ArgPropIndex].size,
-          nullptr));
-      KernelInfo.ArgProps[ArgPropIndex].size =
-          KernelInfo.LocalArgs[ArgPropIndex].SizeWithRedZone;
-      break;
-    }
-    case UR_EXP_KERNEL_ARG_TYPE_POINTER: {
-      UR_CALL(ur_sanitizer_layer::asan::urKernelSetArgPointer(
-          hKernel, pArgs[ArgPropIndex].index, nullptr,
-          pArgs[ArgPropIndex].value.pointer));
-      break;
-    }
-    case UR_EXP_KERNEL_ARG_TYPE_VALUE: {
-      UR_CALL(ur_sanitizer_layer::asan::urKernelSetArgValue(
-          hKernel, pArgs[ArgPropIndex].index, pArgs[ArgPropIndex].size, nullptr,
-          pArgs[ArgPropIndex].value.value));
-      break;
-    }
-    case UR_EXP_KERNEL_ARG_TYPE_MEM_OBJ: {
-      ur_kernel_arg_mem_obj_properties_t Properties = {
-          UR_STRUCTURE_TYPE_KERNEL_ARG_MEM_OBJ_PROPERTIES, nullptr,
-          pArgs[ArgPropIndex].value.memObjTuple.flags};
-      UR_CALL(ur_sanitizer_layer::asan::urKernelSetArgMemObj(
-          hKernel, pArgs[ArgPropIndex].index, &Properties,
-          pArgs[ArgPropIndex].value.memObjTuple.hMem));
-      if (std::shared_ptr<MemBuffer> MemBuffer =
-              getAsanInterceptor()->getMemBuffer(
-                  pArgs[ArgPropIndex].value.memObjTuple.hMem)) {
-        char *Handle = nullptr;
-        UR_CALL(MemBuffer->getHandle(GetDevice(hQueue), Handle));
-        KernelInfo.ArgProps[ArgPropIndex].type =
-            ur_exp_kernel_arg_type_t::UR_EXP_KERNEL_ARG_TYPE_POINTER;
-        KernelInfo.ArgProps[ArgPropIndex].value.pointer = Handle;
-      }
-      break;
-    }
-    case UR_EXP_KERNEL_ARG_TYPE_SAMPLER: {
-      auto pfnKernelSetArgSampler =
-          getContext()->urDdiTable.Kernel.pfnSetArgSampler;
-      UR_CALL(pfnKernelSetArgSampler(hKernel, pArgs[ArgPropIndex].index,
-                                     nullptr,
-                                     pArgs[ArgPropIndex].value.sampler));
-      break;
-    }
-    default:
-      return UR_RESULT_ERROR_INVALID_ENUMERATION;
-    }
-  }
-
   LaunchInfo LaunchInfo(GetContext(hQueue), GetDevice(hQueue), pGlobalWorkSize,
                         pLocalWorkSize, pGlobalWorkOffset, workDim);
   UR_CALL(LaunchInfo.Data.syncToDevice(hQueue));
