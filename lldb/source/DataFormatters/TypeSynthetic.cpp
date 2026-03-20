intel/llvm

commit 65d84ea12725d543fc09686231c3971642585a39

[lldb] Update the calling convention of BytecodeSyntheticChildren::Update (#182155)
This changes the inputs to `update`. It's now the data stack that was
the result of `@init`. This makes `update` more predictable, as its
inputs are the same between the first call and the Nth call.

lldb/source/DataFormatters/TypeSynthetic.cpp
@@ -258,9 +258,9 @@ std::string ScriptedSyntheticChildren::GetDescription() {
 BytecodeSyntheticChildren::FrontEnd::FrontEnd(
     ValueObject &backend, SyntheticBytecodeImplementation &impl)
     : SyntheticChildrenFrontEnd(backend), m_impl(impl) {
-  FormatterBytecode::DataStack data(backend.GetSP());
+  FormatterBytecode::DataStack data = {backend.GetSP()};
   if (!m_impl.init) {
-    m_self = std::move(data);
+    m_init_results = std::move(data);
     return;
   }
 
@@ -274,15 +274,15 @@ BytecodeSyntheticChildren::FrontEnd::FrontEnd(
   }
 
   if (data.size() > 0)
-    m_self = std::move(data);
+    m_init_results = std::move(data);
 }
 
 lldb::ChildCacheState BytecodeSyntheticChildren::FrontEnd::Update() {
   if (!m_impl.update)
-    return ChildCacheState::eRefetch;
+    return ChildCacheState::eReuse;
 
   FormatterBytecode::ControlStack control = {m_impl.update->getBuffer()};
-  FormatterBytecode::DataStack data = m_self;
+  FormatterBytecode::DataStack data = m_init_results;
   llvm::Error error = FormatterBytecode::Interpret(
       control, data, FormatterBytecode::sig_update);
   if (error) {

lldb/include/lldb/DataFormatters/TypeSynthetic.h
@@ -525,6 +525,7 @@ private:
 
   private:
     const SyntheticBytecodeImplementation &m_impl;
+    FormatterBytecode::DataStack m_init_results;
     FormatterBytecode::DataStack m_self;
   };
 

commit 7a5431eee3c233fa30679a5d2a48b19d8343f435

[lldb][bytecode] Fix Update() and failing test (#187795)
Fixes a breakage from https://github.com/llvm/llvm-project/pull/182155

lldb/source/DataFormatters/TypeSynthetic.cpp
@@ -278,8 +278,10 @@ BytecodeSyntheticChildren::FrontEnd::FrontEnd(
 }
 
 lldb::ChildCacheState BytecodeSyntheticChildren::FrontEnd::Update() {
-  if (!m_impl.update)
+  if (!m_impl.update) {
+    m_self = m_init_results;
     return ChildCacheState::eReuse;
+  }
 
   FormatterBytecode::ControlStack control = {m_impl.update->getBuffer()};
   FormatterBytecode::DataStack data = m_init_results;

lldb/test/API/functionalities/data-formatter/bytecode-synthetic/main.cpp
@@ -15,14 +15,14 @@ int main(int argc, char **argv) {
 
 __attribute__((used, section("__DATA_CONST,__lldbformatters"))) unsigned char
     _Account_synthetic[] =
-        "\x01"                      // version
-        "\x15"                      // remaining record size
-        "\x07"                      // type name size
-        "Account"                   // type name
-        "\x00"                      // flags
-        "\x02"                      // sig_get_num_children
-        "\x02"                      // program size
-        "\x20\x01"                  // `return 1`
-        "\x04"                      // sig_get_child_at_index
-        "\x06"                      // program size
-        "\x02\x20\x00\x23\x11\x60"; // `return self.valobj.GetChildAtIndex(0)`
+        "\x01"          // version
+        "\x12"          // remaining record size
+        "\x07"          // type name size
+        "Account"       // type name
+        "\x00"          // flags
+        "\x02"          // sig_get_num_children
+        "\x02"          // program size
+        "\x20\x01"      // `return 1`
+        "\x04"          // sig_get_child_at_index
+        "\x03"          // program size
+        "\x23\x11\x60"; // `return self.valobj.GetChildAtIndex(idx)`