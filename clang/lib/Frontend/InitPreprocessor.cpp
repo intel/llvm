49127beec496cdbfb6236a1ec03efd6dfcb0973f
Khatavkar, Yashasvi <yashasvi.khatavkar@intel.com>
2026-03-20 06:22:43 -0700

    Revert the changes for __clang_version__

--- clang/lib/Frontend/InitPreprocessor.cpp
+++ clang/lib/Frontend/InitPreprocessor.cpp
@@ -875,16 +875,17 @@ static void InitializePredefinedMacros(const TargetInfo &TI,
   Builder.defineMacro("__clang_major__", TOSTR(CLANG_VERSION_MAJOR));
   Builder.defineMacro("__clang_minor__", TOSTR(CLANG_VERSION_MINOR));
   Builder.defineMacro("__clang_patchlevel__", TOSTR(CLANG_VERSION_PATCHLEVEL));
+#undef TOSTR
+#undef TOSTR2
   Builder.defineMacro("__clang_version__",
-                      "\"" + getDPCPPFullCPPVersion() + "\"");
+                      "\"" CLANG_VERSION_STRING " "
+                      + getClangFullRepositoryVersion() + "\"");
 
   // DPC++ version macros - Intel's SYCL compiler
   Builder.defineMacro("__DPCPP__", "1");
   Builder.defineMacro("__dpcpp_major__", TOSTR(DPCPP_VERSION_MAJOR));
   Builder.defineMacro("__dpcpp_minor__", TOSTR(DPCPP_VERSION_MINOR));
   Builder.defineMacro("__dpcpp_patchlevel__", TOSTR(DPCPP_VERSION_PATCH));
-#undef TOSTR
-#undef TOSTR2
 
   if (LangOpts.GNUCVersion != 0) {
     // Major, minor, patch, are given two decimal places each, so 4.2.1 becomes
