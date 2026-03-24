405706ddb21d669bd8b55756ef03f88e9d1818d2
Matthew Michel <matthew.michel@intel.com>
2026-03-24 14:34:07 -0700

    Fix incorrect usage of FileCheck in NativeRecording/debug_print_graph.cpp

--- sycl/test-e2e/Graph/RecordReplay/NativeRecording/debug_print_graph.cpp
+++ sycl/test-e2e/Graph/RecordReplay/NativeRecording/debug_print_graph.cpp
@@ -1,7 +1,7 @@
 // REQUIRES: level_zero_v2_adapter && arch-intel_gpu_bmg_g21
 
 // RUN: %{build} -o %t.out
-// RUN: %if level_zero %{%{l0_leak_check} %{run} %t.out 2>&1 | FileCheck %s --implicit-check-not=LEAK --input-file graph_native.dot %}
+// RUN: %if level_zero %{%{l0_leak_check} %{run} %t.out ; FileCheck %s --implicit-check-not=LEAK --input-file graph_native.dot %}
 
 // Test for native recording with debug print graph functionality. SYCL graph
 // does not control the output format, so we only verify high level details of
