5786b285635ae8cacac788d6651b49ba66c9a455
Wenju He <wenju.he@intel.com>
2026-03-30 20:47:14 -0700

    move SPIR mangling utils into MangleUtils.cpp

---
+++ llvm/include/llvm/SYCLLowerIR/MangleUtils.h
@@ -0,0 +1,186 @@
+//===- MangleUtils.h - SPIR mangling helpers for SYCL -------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_SYCLLOWERIR_MANGLEUTILS_H
+#define LLVM_SYCLLOWERIR_MANGLEUTILS_H
+
+#include "llvm/ADT/ArrayRef.h"
+#include "llvm/ADT/IntrusiveRefCntPtr.h"
+#include "llvm/ADT/SmallString.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/StringRef.h"
+
+namespace llvm {
+
+namespace SPIR {
+
+enum MangleError { MANGLE_SUCCESS };
+
+enum TypePrimitiveEnum {
+  PRIMITIVE_BOOL,
+  PRIMITIVE_SCHAR,
+  PRIMITIVE_UCHAR,
+  PRIMITIVE_CHAR,
+  PRIMITIVE_USHORT,
+  PRIMITIVE_SHORT,
+  PRIMITIVE_UINT,
+  PRIMITIVE_INT,
+  PRIMITIVE_ULONG,
+  PRIMITIVE_LONG,
+  PRIMITIVE_ULONGLONG,
+  PRIMITIVE_LONGLONG,
+  PRIMITIVE_FLOAT16,
+  PRIMITIVE_HALF,
+  PRIMITIVE_FLOAT,
+  PRIMITIVE_DOUBLE,
+  PRIMITIVE_VOID,
+  PRIMITIVE_NUM
+};
+
+enum TypeEnum {
+  TYPE_ID_PRIMITIVE,
+  TYPE_ID_POINTER,
+  TYPE_ID_VECTOR,
+  TYPE_ID_TEMPLATE_PARAMETER,
+  TYPE_ID_STRUCTURE
+};
+
+enum TypeAttributeEnum {
+  ATTR_RESTRICT,
+  ATTR_VOLATILE,
+  ATTR_CONST,
+  ATTR_PRIVATE,
+  ATTR_GLOBAL,
+  ATTR_CONSTANT,
+  ATTR_LOCAL,
+  ATTR_GENERIC,
+  ATTR_GENERIC_EXPLICIT,
+  ATTR_NONE,
+  ATTR_NUM = ATTR_NONE
+};
+
+struct TypeVisitor;
+struct PrimitiveType;
+struct VectorType;
+struct PointerType;
+struct TemplateParameterType;
+struct UserDefinedType;
+
+struct ParamType : public RefCountedBase<ParamType> {
+  ParamType(TypeEnum TypeId) : TypeId(TypeId) {}
+  virtual ~ParamType() {}
+  virtual MangleError accept(TypeVisitor *) const = 0;
+  TypeEnum getTypeId() const { return TypeId; }
+
+protected:
+  TypeEnum TypeId;
+};
+
+typedef IntrusiveRefCntPtr<ParamType> RefParamType;
+
+struct PrimitiveType : public ParamType {
+  static const TypeEnum EnumTy;
+  PrimitiveType(TypePrimitiveEnum P)
+      : ParamType(TYPE_ID_PRIMITIVE), Primitive(P) {}
+  MangleError accept(TypeVisitor *Visitor) const override;
+  TypePrimitiveEnum getPrimitive() const { return Primitive; }
+
+private:
+  TypePrimitiveEnum Primitive;
+};
+
+struct PointerType : public ParamType {
+  static const TypeEnum EnumTy;
+  PointerType(const RefParamType Type)
+      : ParamType(TYPE_ID_POINTER), PType(Type), AddressSpace(ATTR_PRIVATE) {
+    Qualifiers[0] = Qualifiers[1] = Qualifiers[2] = false;
+  }
+  MangleError accept(TypeVisitor *Visitor) const override;
+  const RefParamType &getPointee() const { return PType; }
+  void setAddressSpace(TypeAttributeEnum Attr) { AddressSpace = Attr; }
+  TypeAttributeEnum getAddressSpace() const { return AddressSpace; }
+  void setQualifier(TypeAttributeEnum Qual, bool Enabled);
+  bool hasQualifier(TypeAttributeEnum Qual) const;
+
+private:
+  RefParamType PType;
+  bool Qualifiers[3];
+  TypeAttributeEnum AddressSpace;
+};
+
+struct VectorType : public ParamType {
+  static const TypeEnum EnumTy;
+  VectorType(const RefParamType Type, int Len)
+      : ParamType(TYPE_ID_VECTOR), PType(Type), Len(Len) {}
+  MangleError accept(TypeVisitor *Visitor) const override;
+  const RefParamType &getScalarType() const { return PType; }
+  int getLength() const { return Len; }
+
+private:
+  RefParamType PType;
+  int Len;
+};
+
+struct TemplateParameterType : public ParamType {
+  static const TypeEnum EnumTy;
+  TemplateParameterType(unsigned Index)
+      : ParamType(TYPE_ID_TEMPLATE_PARAMETER), Index(Index) {}
+  MangleError accept(TypeVisitor *Visitor) const override;
+  unsigned getIndex() const { return Index; }
+
+private:
+  unsigned Index;
+};
+
+struct UserDefinedType : public ParamType {
+  static const TypeEnum EnumTy;
+  UserDefinedType(StringRef Name) : ParamType(TYPE_ID_STRUCTURE), Name(Name) {}
+  MangleError accept(TypeVisitor *Visitor) const override;
+  StringRef getName() const { return Name; }
+
+private:
+  SmallString<32> Name;
+};
+
+struct TypeVisitor {
+  virtual ~TypeVisitor() {}
+  virtual MangleError visit(const PrimitiveType *) = 0;
+  virtual MangleError visit(const VectorType *) = 0;
+  virtual MangleError visit(const PointerType *) = 0;
+  virtual MangleError visit(const TemplateParameterType *) = 0;
+  virtual MangleError visit(const UserDefinedType *) = 0;
+};
+
+template <typename T> const T *dynCast(const ParamType *PType) {
+  return (T::EnumTy == PType->getTypeId()) ? static_cast<const T *>(PType)
+                                           : nullptr;
+}
+
+template <typename T> T *dynCast(ParamType *PType) {
+  return (T::EnumTy == PType->getTypeId()) ? static_cast<T *>(PType) : nullptr;
+}
+
+class NameMangler {
+public:
+  MangleError mangleTemplateName(StringRef Name,
+                                 ArrayRef<RefParamType> TemplateArgs,
+                                 SmallVectorImpl<char> &MangledName);
+
+  MangleError mangle(StringRef Name, ArrayRef<RefParamType> Params,
+                     SmallVectorImpl<char> &MangledName);
+
+  MangleError mangle(StringRef Name, ArrayRef<RefParamType> TemplateArgs,
+                     ArrayRef<RefParamType> Params,
+                     SmallVectorImpl<char> &MangledName);
+};
+
+} // namespace SPIR
+
+} // namespace llvm
+
+#endif // LLVM_SYCLLOWERIR_MANGLEUTILS_H
---
+++ llvm/lib/SYCLLowerIR/MangleUtils.cpp
@@ -0,0 +1,347 @@
+//===- MangleUtils.cpp - SPIR mangling helpers for SYCL -----------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/SYCLLowerIR/MangleUtils.h"
+#include "llvm/ADT/StringMap.h"
+#include "llvm/Support/raw_ostream.h"
+
+namespace llvm {
+namespace SPIR {
+
+// Mangling utilities.
+// clang-format off
+#define PRIMITIVE_TYPES_MAP(X) \
+  X(PRIMITIVE_BOOL,         "b")                                               \
+  X(PRIMITIVE_SCHAR,        "a")                                               \
+  X(PRIMITIVE_UCHAR,        "h")                                               \
+  X(PRIMITIVE_CHAR,         "c")                                               \
+  X(PRIMITIVE_USHORT,       "t")                                               \
+  X(PRIMITIVE_SHORT,        "s")                                               \
+  X(PRIMITIVE_UINT,         "j")                                               \
+  X(PRIMITIVE_INT,          "i")                                               \
+  X(PRIMITIVE_ULONG,        "m")                                               \
+  X(PRIMITIVE_LONG,         "l")                                               \
+  X(PRIMITIVE_ULONGLONG,    "y")                                               \
+  X(PRIMITIVE_LONGLONG,     "x")                                               \
+  X(PRIMITIVE_FLOAT16,      "DF16_")                                           \
+  X(PRIMITIVE_HALF,         "Dh")                                              \
+  X(PRIMITIVE_FLOAT,        "f")                                               \
+  X(PRIMITIVE_DOUBLE,       "d")                                               \
+  X(PRIMITIVE_VOID,         "v")
+// clang-format on
+
+StringRef mangledPrimitiveString(TypePrimitiveEnum T) {
+  switch (T) {
+#define TYPE_ENUM_CASE(Enum, Mangled)                                          \
+  case Enum:                                                                   \
+    return Mangled;
+    PRIMITIVE_TYPES_MAP(TYPE_ENUM_CASE)
+#undef TYPE_ENUM_CASE
+  default:
+    return {};
+  }
+}
+
+#undef PRIMITIVE_TYPES_MAP
+
+void appendTemplateParameterMangling(unsigned Index, raw_ostream &Stream) {
+  Stream << 'T';
+  if (Index > 0)
+    Stream << (Index - 1);
+  Stream << '_';
+}
+
+SmallString<8> getLeafTypeMangling(const ParamType *Type) {
+  if (const PrimitiveType *Prim = dynCast<PrimitiveType>(Type)) {
+    SmallString<8> Buffer;
+    Buffer += mangledPrimitiveString(Prim->getPrimitive());
+    return Buffer;
+  }
+  if (const TemplateParameterType *TemplateParam =
+          dynCast<TemplateParameterType>(Type)) {
+    SmallString<8> Buffer;
+    raw_svector_ostream Stream(Buffer);
+    appendTemplateParameterMangling(TemplateParam->getIndex(), Stream);
+    return Buffer;
+  }
+  return {};
+}
+
+// Address space mangling:
+// - Private (AS0) is explicit (U3AS0) - matches SYCL conventions.
+// clang-format off
+#define ATTRIBUTE_TYPES_MAP(X)                                                 \
+  X(ATTR_RESTRICT,          "r")                                               \
+  X(ATTR_VOLATILE,          "V")                                               \
+  X(ATTR_CONST,             "K")                                               \
+  X(ATTR_PRIVATE,           "U3AS0")                                           \
+  X(ATTR_GLOBAL,            "U3AS1")                                           \
+  X(ATTR_CONSTANT,          "U3AS2")                                           \
+  X(ATTR_LOCAL,             "U3AS3")                                           \
+  X(ATTR_GENERIC,           "")                                                \
+  X(ATTR_GENERIC_EXPLICIT,  "U3AS4")
+// clang-format on
+
+StringRef getMangledAttribute(TypeAttributeEnum Attribute) {
+  switch (Attribute) {
+#define ATTR_ENUM_CASE(Enum, Mangled)                                          \
+  case Enum:                                                                   \
+    return Mangled;
+    ATTRIBUTE_TYPES_MAP(ATTR_ENUM_CASE)
+#undef ATTR_ENUM_CASE
+  default:
+    return {};
+  }
+}
+
+#undef ATTRIBUTE_TYPES_MAP
+
+// Type implementations
+const TypeEnum PrimitiveType::EnumTy = TYPE_ID_PRIMITIVE;
+const TypeEnum PointerType::EnumTy = TYPE_ID_POINTER;
+const TypeEnum VectorType::EnumTy = TYPE_ID_VECTOR;
+const TypeEnum TemplateParameterType::EnumTy = TYPE_ID_TEMPLATE_PARAMETER;
+const TypeEnum UserDefinedType::EnumTy = TYPE_ID_STRUCTURE;
+
+MangleError PrimitiveType::accept(TypeVisitor *Visitor) const {
+  return Visitor->visit(this);
+}
+
+MangleError PointerType::accept(TypeVisitor *Visitor) const {
+  return Visitor->visit(this);
+}
+
+void PointerType::setQualifier(TypeAttributeEnum Qual, bool Enabled) {
+  if (Qual == ATTR_RESTRICT)
+    Qualifiers[0] = Enabled;
+  else if (Qual == ATTR_VOLATILE)
+    Qualifiers[1] = Enabled;
+  else if (Qual == ATTR_CONST)
+    Qualifiers[2] = Enabled;
+}
+
+bool PointerType::hasQualifier(TypeAttributeEnum Qual) const {
+  if (Qual == ATTR_RESTRICT)
+    return Qualifiers[0];
+  if (Qual == ATTR_VOLATILE)
+    return Qualifiers[1];
+  if (Qual == ATTR_CONST)
+    return Qualifiers[2];
+  return false;
+}
+
+MangleError VectorType::accept(TypeVisitor *Visitor) const {
+  return Visitor->visit(this);
+}
+
+MangleError TemplateParameterType::accept(TypeVisitor *Visitor) const {
+  return Visitor->visit(this);
+}
+
+MangleError UserDefinedType::accept(TypeVisitor *Visitor) const {
+  return Visitor->visit(this);
+}
+
+class MangleVisitor : public TypeVisitor {
+public:
+  MangleVisitor(SmallVectorImpl<char> &Buffer, raw_ostream &Stream,
+                unsigned InitialSeqId = 0)
+      : Buffer(Buffer), Stream(Stream), SeqId(InitialSeqId) {}
+
+  MangleError visit(const PrimitiveType *T) override {
+    Stream << mangledPrimitiveString(T->getPrimitive());
+    return MANGLE_SUCCESS;
+  }
+
+  MangleError visit(const PointerType *P) override {
+    size_t Pos = Buffer.size();
+    SmallString<8> AttrMangling = getPointerAttributesManglingWithMode(P);
+    SmallString<8> PtrTypePrefix("P");
+    PtrTypePrefix += AttrMangling;
+    if (!mangleSubstitution(P, PtrTypePrefix)) {
+      Stream << PtrTypePrefix;
+      MangleError Err =
+          P->getPointee()->accept(static_cast<TypeVisitor *>(this));
+      if (!AttrMangling.empty())
+        recordSubstitution(currentBuffer().substr(Pos + 1));
+      recordSubstitution(currentBuffer().substr(Pos));
+      return Err;
+    }
+    return MANGLE_SUCCESS;
+  }
+
+  MangleError visit(const VectorType *V) override {
+    size_t Index = Buffer.size();
+    SmallString<16> TypeStorage;
+    raw_svector_ostream TypeStream(TypeStorage);
+    TypeStream << "Dv" << V->getLength() << '_';
+    StringRef TypeStr(TypeStorage);
+    if (!mangleSubstitution(V, TypeStr)) {
+      Stream << TypeStr;
+      MangleError Err = V->getScalarType()->accept(this);
+      recordSubstitution(currentBuffer().substr(Index));
+      return Err;
+    }
+    return MANGLE_SUCCESS;
+  }
+
+  MangleError visit(const TemplateParameterType *T) override {
+    appendTemplateParameterMangling(T->getIndex(), Stream);
+    return MANGLE_SUCCESS;
+  }
+
+  MangleError visit(const UserDefinedType *U) override {
+    size_t Index = Buffer.size();
+    StringRef Name = U->getName();
+    if (!mangleSubstitution(U, Name)) {
+      Stream << Name.size() << Name;
+      recordSubstitution(currentBuffer().substr(Index));
+    }
+    return MANGLE_SUCCESS;
+  }
+
+private:
+  StringRef currentBuffer() const {
+    return StringRef(Buffer.data(), Buffer.size());
+  }
+
+  SmallString<8> getPointerAttributesManglingWithMode(const PointerType *P) {
+    SmallString<8> QualStr;
+    // Handle address space.
+    TypeAttributeEnum AS = P->getAddressSpace();
+    // Normal mangling (generic is implicit).
+    QualStr += getMangledAttribute(AS);
+    // Handle qualifiers.
+    if (P->hasQualifier(ATTR_RESTRICT))
+      QualStr += 'r';
+    if (P->hasQualifier(ATTR_VOLATILE))
+      QualStr += 'V';
+    if (P->hasQualifier(ATTR_CONST))
+      QualStr += 'K';
+    return QualStr;
+  }
+
+  void mangleSequenceID(unsigned SeqID) {
+    if (SeqID == 1)
+      Stream << '0';
+    else if (SeqID > 1) {
+      SmallString<8> Bstr;
+      constexpr StringRef Charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      SeqID--;
+      for (; SeqID != 0; SeqID /= 36)
+        Bstr.push_back(Charset[SeqID % 36]);
+      for (char C : llvm::reverse(Bstr))
+        Stream << C;
+    }
+    Stream << '_';
+  }
+
+  bool mangleSubstitution(const ParamType *Type, StringRef TypeStr) {
+    if (currentBuffer().find(TypeStr) == StringRef::npos)
+      return false;
+
+    SmallString<32> ThisTypeStr(TypeStr);
+    if (const PointerType *P = dynCast<PointerType>(Type)) {
+      ThisTypeStr += getPointeeMangling(P->getPointee());
+    } else if (const VectorType *PVec = dynCast<VectorType>(Type)) {
+      SmallString<8> NType = getLeafTypeMangling(PVec->getScalarType().get());
+      if (!NType.empty())
+        ThisTypeStr += NType;
+    }
+    auto It = Substitutions.find(StringRef(ThisTypeStr));
+    if (It == Substitutions.end())
+      return false;
+
+    unsigned SeqID = It->getValue();
+    Stream << 'S';
+    mangleSequenceID(SeqID);
+    return true;
+  }
+
+  SmallString<32> getPointeeMangling(RefParamType Pointee) {
+    // Generates a simplified mangling of the pointee type for substitution
+    // matching, without recursive substitutions.
+    SmallString<32> Mangling;
+    raw_svector_ostream ManglingStream(Mangling);
+
+    while (const PointerType *P = SPIR::dynCast<PointerType>(Pointee.get())) {
+      ManglingStream << 'P' << getPointerAttributesManglingWithMode(P);
+      Pointee = P->getPointee();
+    }
+
+    if (const UserDefinedType *U =
+            SPIR::dynCast<UserDefinedType>(Pointee.get())) {
+      StringRef Name = U->getName();
+      ManglingStream << Name.size() << Name;
+    } else {
+      SmallString<8> LeafMangling = getLeafTypeMangling(Pointee.get());
+      if (!LeafMangling.empty())
+        ManglingStream << LeafMangling;
+    }
+    return Mangling;
+  }
+  void recordSubstitution(StringRef Str) { Substitutions[Str] = SeqId++; }
+
+  SmallVectorImpl<char> &Buffer;
+  raw_ostream &Stream;
+  unsigned SeqId;
+  StringMap<unsigned> Substitutions;
+};
+
+MangleError
+NameMangler::mangleTemplateName(StringRef Name,
+                                ArrayRef<RefParamType> TemplateArgs,
+                                SmallVectorImpl<char> &MangledName) {
+  MangledName.clear();
+  raw_svector_ostream Stream(MangledName);
+  Stream << "_Z" << Name.size() << Name;
+  if (!TemplateArgs.empty()) {
+    Stream << 'I';
+    MangleVisitor Visitor(MangledName, Stream, 1);
+    for (const auto &TemplateArg : TemplateArgs) {
+      MangleError Err = TemplateArg->accept(&Visitor);
+      if (Err != MANGLE_SUCCESS)
+        return Err;
+    }
+    Stream << 'E';
+  }
+  return MANGLE_SUCCESS;
+}
+
+MangleError NameMangler::mangle(StringRef Name, ArrayRef<RefParamType> Params,
+                                SmallVectorImpl<char> &MangledName) {
+  return mangle(Name, {}, Params, MangledName);
+}
+
+MangleError NameMangler::mangle(StringRef Name,
+                                ArrayRef<RefParamType> TemplateArgs,
+                                ArrayRef<RefParamType> Params,
+                                SmallVectorImpl<char> &MangledName) {
+  MangledName.clear();
+  raw_svector_ostream Stream(MangledName);
+  Stream << "_Z" << Name.size() << Name;
+  MangleVisitor Visitor(MangledName, Stream, 0);
+  if (!TemplateArgs.empty()) {
+    Stream << 'I';
+    for (const auto &TemplateArg : TemplateArgs) {
+      MangleError Err = TemplateArg->accept(&Visitor);
+      if (Err != MANGLE_SUCCESS)
+        return Err;
+    }
+    Stream << 'E';
+  }
+  for (const auto &P : Params) {
+    MangleError Err = P->accept(&Visitor);
+    if (Err != MANGLE_SUCCESS)
+      return Err;
+  }
+  return MANGLE_SUCCESS;
+}
+
+} // namespace SPIR
+} // namespace llvm
