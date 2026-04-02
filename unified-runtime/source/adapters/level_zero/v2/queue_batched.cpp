12c6f65dd7122883f760dd5c5da7e010943c1d85
Krzysztof Filipek <krzysztof.filipek@intel.com>
2026-04-02 08:56:00 +0200

    Remove obsolete enqueueKernelLaunch()

--- unified-runtime/source/adapters/level_zero/v2/queue_batched.cpp
+++ unified-runtime/source/adapters/level_zero/v2/queue_batched.cpp
@@ -179,28 +179,6 @@ ur_result_t ur_queue_batched_t::markIssuedCommandInBatch(
   return UR_RESULT_SUCCESS;
 }
 
-ur_result_t ur_queue_batched_t::enqueueKernelLaunch(
-    ur_kernel_handle_t hKernel, uint32_t workDim,
-    const size_t *pGlobalWorkOffset, const size_t *pGlobalWorkSize,
-    const size_t *pLocalWorkSize,
-    const ur_kernel_launch_ext_properties_t *launchPropList,
-    uint32_t numEventsInWaitList, const ur_event_handle_t *phEventWaitList,
-    ur_event_handle_t *phEvent) {
-
-  wait_list_view waitListView =
-      wait_list_view(phEventWaitList, numEventsInWaitList, this);
-
-  TRACK_SCOPE_LATENCY("ur_queue_batched_t::enqueueKernelLaunch");
-  auto lockedBatch = currentCmdLists.lock();
-  markIssuedCommandInBatch(lockedBatch);
-
-  UR_CALL(lockedBatch->getListManager().appendKernelLaunch(
-      hKernel, workDim, pGlobalWorkOffset, pGlobalWorkSize, pLocalWorkSize,
-      launchPropList, waitListView, getEvent(lockedBatch, phEvent)));
-
-  return UR_RESULT_SUCCESS;
-}
-
 ur_result_t batch_manager::hostSynchronize() {
   TRACK_SCOPE_LATENCY("ur_queue_batched_t::hostSynchronize");
 
