21d07534740f50778fd49e7bea3a73f93419bf27
Michael D Toguchi <michael.d.toguchi@intel.com>
2026-04-21 11:28:30 -0700

    clang format

--- libdevice/nativecpu_utils.cpp
+++ libdevice/nativecpu_utils.cpp
@@ -28,8 +28,7 @@ using __nativecpu_state = native_cpu::state;
 #define DEVICE_EXTERN_C                                                        \
   extern "C" SYCL_EXTERNAL __attribute__((libclc_call, weak))
 #define DEVICE_EXTERNAL_C DEVICE_EXTERN_C
-#define DEVICE_EXTERNAL                                                        \
-  SYCL_EXTERNAL __attribute__((libclc_call, weak))
+#define DEVICE_EXTERNAL SYCL_EXTERNAL __attribute__((libclc_call, weak))
 
 // Several functions are used implicitly by WorkItemLoopsPass and
 // PrepareSYCLNativeCPUPass and need to be marked as used to prevent them being
