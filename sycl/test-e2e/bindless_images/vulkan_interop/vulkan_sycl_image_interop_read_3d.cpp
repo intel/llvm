68cfeda5d9b295b07ec5de8a14cb6e0422dab68c
Chris Perkins <chris.perkins@intel.com>
2026-04-22 11:06:24 -0700

    [SYCL][bindless-image][E2E]  coord order doc and clarify (#21506)
    Our existing bindless image implementation uses `sycl::range` in a way
    that is not 100% aligned with the (admitedly unclear) spec. Though, it
    is very much aligned with how image coordinates and dimensions are
    handled generally.
    
    Documenting and commenting accordingly.
    
    ---------
    
    Signed-off-by: Chris Perkins <chris.perkins@intel.com>

--- sycl/test-e2e/bindless_images/vulkan_interop/vulkan_sycl_image_interop_read_3d.cpp
+++ sycl/test-e2e/bindless_images/vulkan_interop/vulkan_sycl_image_interop_read_3d.cpp
@@ -178,6 +178,8 @@ int runTest(
                                             ? syclOverride.value()
                                             : getSyclChannelType<T>();
 
+    // bindless image use (x,y,z) order,
+    // differening from SYCL 2020 "fastest incrementing" convention.
     syclexp::image_descriptor imgDesc(sycl::range<3>(width, height, depth),
                                       channels, syclType);
 
@@ -221,11 +223,13 @@ int runTest(
          h.depends_on(dependencyEvent);
        sycl::accessor outAcc(checkBuf, h, sycl::write_only);
 
+       // ranges for parallel_for use "fastest incrementing" order (z,y,x),
+       // but bindless images ranges use (x,y,z) order.
        h.parallel_for(
-           sycl::range<3>(width, height, depth), [=](sycl::item<3> item) {
-             int x = item.get_id(0);
+           sycl::range<3>(depth, height, width), [=](sycl::item<3> item) {
+             int x = item.get_id(2);
              int y = item.get_id(1);
-             int z = item.get_id(2);
+             int z = item.get_id(0);
              size_t baseIdx = (z * width * height + y * width + x) * channels;
 
              // Special handling for unorm8 (normalized 0.0-1.0)
