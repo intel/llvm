66156bb9fc9f6711f740207608a3122e0206c5a8
Rafal Rudnicki <rafal.rudnicki@intel.com>
2026-04-24 09:02:27 +0000

    [SYCL][E2E] Split WG Memory basic usage test

---
+++ sycl/test-e2e/WorkGroupMemory/basic_usage_test_ptr.cpp
@@ -0,0 +1,26 @@
+// Pointer-types version of the basic usage test.
+
+// UNSUPPORTED: hip
+// UNSUPPORTED-TRACKER: https://github.com/intel/llvm/issues/17339
+
+// XFAIL: spirv-backend
+// XFAIL-TRACKER: https://github.com/intel/llvm/issues/18230
+
+// RUN: %{build} -o %t.out
+// RUN: %{run} %t.out
+
+#include <sycl/queue.hpp>
+
+#include "./basic_usage_common.hpp"
+
+sycl::queue q;
+
+int main() {
+  test_ptr<float *>();
+  test_ptr<int *>();
+  test_ptr<char *>();
+  test_ptr<uint16_t *>();
+  if (q.get_device().has(sycl::aspect::fp16))
+    test_ptr<sycl::half *>();
+  return 0;
+}
