41f8fc5d31d592895e357d55194fc43ef730e9c6
Maksimova, Viktoria <viktoria.maksimova@intel.com>
2026-04-29 05:28:51 -0700

    Revert "Revert "add support for SPV_EXT_optnone (#2951)""
    Re-add SPV_EXT_optnone support and migrate from the deprecated
    SPV_INTEL_optnone extension.
    The INTEL variant remains registered for backward compatibility in the
    reader, but the writer now exclusively uses SPV_EXT_optnone.
    
    This reverts commit 2bae4010f11c4d0f423f2021ef10d44b6ba33560.

--- llvm-spirv/lib/SPIRV/libSPIRV/SPIRVNameMapEnum.h
+++ llvm-spirv/lib/SPIRV/libSPIRV/SPIRVNameMapEnum.h
@@ -639,7 +639,7 @@ template <> inline void SPIRVMap<Capability, std::string>::init() {
   add(CapabilityAtomicFloat32AddEXT, "AtomicFloat32AddEXT");
   add(CapabilityAtomicFloat64AddEXT, "AtomicFloat64AddEXT");
   add(CapabilityLongCompositesINTEL, "LongCompositesINTEL");
-  add(CapabilityOptNoneINTEL, "OptNoneINTEL");
+  add(CapabilityOptNoneEXT, "OptNoneEXT");
   add(CapabilityAtomicFloat16AddEXT, "AtomicFloat16AddEXT");
   add(internal::CapabilityAtomicBFloat16AddINTEL, "AtomicBFloat16AddINTEL");
   add(internal::CapabilityAtomicBFloat16MinMaxINTEL,
@@ -674,7 +674,6 @@ template <> inline void SPIRVMap<Capability, std::string>::init() {
   add(CapabilityTernaryBitwiseFunctionINTEL, "TernaryBitwiseFunctionINTEL");
   add(CapabilityFMAKHR, "FMAKHR");
   // From spirv_internal.hpp
-  add(internal::CapabilityOptNoneINTEL, "OptNoneINTEL");
   add(internal::CapabilityTokenTypeINTEL, "TokenTypeINTEL");
   add(internal::CapabilityHWThreadQueryINTEL, "HWThreadQueryINTEL");
   add(internal::CapabilityGlobalVariableDecorationsINTEL,
