61d0195bf9dd3b49b8393c01a7bae27228b51f6e
Ptak, Slawomir <slawomir.ptak@intel.com>
2026-05-05 09:48:16 +0000

    Fix header name.

--- sycl/include/sycl/ext/oneapi/experimental/detail/ipc_common.hpp
+++ sycl/include/sycl/ext/oneapi/experimental/detail/ipc_common.hpp
@@ -1,4 +1,4 @@
-//==------- ipc_memory.hpp ------- SYCL inter-process common ---------------==//
+//==------- ipc_common.hpp ------- SYCL inter-process common ---------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
