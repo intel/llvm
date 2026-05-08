999b4f5e40fa8d4f6341c0afc548c53d728648e2
Meszaros, Gergely <gergely.meszaros@intel.com>
2026-05-08 16:06:37 +0000

    Use %sycl_triple in test

--- sycl/test/regression/include_cmath_no_rdc.cpp
+++ sycl/test/regression/include_cmath_no_rdc.cpp
@@ -1,9 +1,5 @@
-// REQUIRES: cuda || hip
-
-// RUN: %if cuda %{ %clangxx -fsycl -fsycl-targets=nvptx64-nvidia-cuda -fsycl-device-only -fno-sycl-rdc %s %}
-// RUN: %if cuda %{ %clangxx -fsycl -fsycl-targets=nvptx64-nvidia-cuda -fsycl-device-only -fsycl-rdc    %s %}
-// RUN: %if hip  %{ %clangxx -fsycl -fsycl-targets=amdgcn-amd-amdhsa   -fsycl-device-only -fno-sycl-rdc %s %}
-// RUN: %if hip  %{ %clangxx -fsycl -fsycl-targets=amdgcn-amd-amdhsa   -fsycl-device-only -fsycl-rdc    %s %}
+// RUN: %clangxx -fsycl -fsycl-rdc    -fsycl-targets=%sycl_triple -fsycl-device-only -fsyntax-only %s
+// RUN: %clangxx -fsycl -fno-sycl-rdc -fsycl-targets=%sycl_triple -fsycl-device-only -fsyntax-only %s
 
 #include <cmath>
 
