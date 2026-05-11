d0f66dbca674bc415c40517ebdfe49fac813498e
Konstantinos Parasyris <konstantinos.parasyris@intel.com>
2026-05-11 09:33:28 -0700

    [SYCL] Split detail builtins implementation into internal headers (#21835)
    Refactors builtins.hpp into a thin hub plus builtin_helpers.hpp and
    per-category internal headers.
    Intended to improve header organization and introduce opportunities for
    reducing compile-time cost.
    
    Implements the` builtin_*.hpp` files listed in
    https://github.com/KhronosGroup/SYCL-Docs/pull/814

---
+++ sycl/include/sycl/khr/includes/builtins_relational.hpp
@@ -0,0 +1,18 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef __SYCL_KHR_INCLUDES_BUILTINS_RELATIONAL
+#define __SYCL_KHR_INCLUDES_BUILTINS_RELATIONAL
+
+#include "version.hpp"
+
+#include <sycl/detail/defines_elementary.hpp>
+
+#include <sycl/detail/builtins/relational_functions.hpp>
+
+#endif // __SYCL_KHR_INCLUDES_BUILTINS_RELATIONAL
