a5ebd4ec4e95a3e80e774e07301152eaad5283e7
Chris Perkins <chris.perkins@intel.com>
2026-05-11 17:36:35 -0700

    clang-format to the rescue

--- clang/lib/Basic/Targets/SPIR.h
+++ clang/lib/Basic/Targets/SPIR.h
@@ -396,10 +396,10 @@ public:
                                     : CCCR_Warning;
   }
 
-  bool initFeatureMap(llvm::StringMap<bool> &Features, DiagnosticsEngine &Diags,
-                      StringRef CPU,
-                      const std::vector<std::string> &FeaturesVec)
-      const override;
+  bool
+  initFeatureMap(llvm::StringMap<bool> &Features, DiagnosticsEngine &Diags,
+                 StringRef CPU,
+                 const std::vector<std::string> &FeaturesVec) const override;
 };
 
 // x86-64 SPIR64 Windows Visual Studio target
