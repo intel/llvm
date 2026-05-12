ae9dafe70b761a98a9931b3e7ef0b71beef5c90d
y <konstantinos.parasyris@intel.com>
2026-05-12 15:53:54 -0700

    Fix comment

--- clang/test/CodeGenSYCL/free_function_kernels_restrict.cpp
+++ clang/test/CodeGenSYCL/free_function_kernels_restrict.cpp
@@ -1,24 +1,21 @@
-// RUN: %clangxx -fsycl-device-only -S -Xclang -emit-llvm -Xclang -disable-llvm-passes %s -o - | FileCheck %s --check-prefix CHECK-IR
+// RUN: %clang_cc1 -internal-isystem %S/Inputs -fsycl-is-device -triple spir64-unknown-unknown -sycl-std=2020 -emit-llvm %s -o - | FileCheck %s --check-prefix CHECK-IR
 
-#include <sycl/ext/oneapi/experimental/free_function_traits.hpp>
-#include <sycl/sycl.hpp>
-
-namespace syclexp = sycl::ext::oneapi::experimental;
+#include "sycl.hpp"
 
 // CHECK-IR-DAG: define dso_local spir_func noundef i32 @_Z15helper_restrictPKi(ptr addrspace(4) noalias
-SYCL_EXTERNAL int helper_restrict(const int *__restrict__ input) {
+__attribute__((sycl_device)) int helper_restrict(const int *__restrict__ input) {
   return input[0];
 }
 
 // CHECK-IR-DAG: define dso_local spir_func void @_Z25helper_const_ptr_restrictPiPKi(ptr addrspace(4) noalias{{[^,]*}}, ptr addrspace(4) noalias
-SYCL_EXTERNAL void
+__attribute__((sycl_device)) void
 helper_const_ptr_restrict(int *const __restrict__ output,
                           const int *const __restrict__ input) {
   output[0] = input[0];
 }
 
 // CHECK-IR: define dso_local spir_kernel void @_Z43__sycl_kernel_free_function_kernel_restrictPiPKiS_S_S1_PKv(ptr addrspace(1) noalias{{[^,]*}}, ptr addrspace(1) noalias{{[^,]*}}, ptr addrspace(1) {{[^,]*}}, ptr addrspace(1) noalias{{[^,]*}}, ptr addrspace(1) noalias{{[^,]*}}, ptr addrspace(1) noalias
-SYCL_EXT_ONEAPI_FUNCTION_PROPERTY(syclexp::single_task_kernel)
+[[__sycl_detail__::add_ir_attributes_function("sycl-single-task-kernel", 0)]]
 void free_function_kernel_restrict(int *__restrict__ output,
                                    const int *__restrict__ input, int *plain,
                                    int *const __restrict__ output_const,
