36cbf42a423d4f8cc67b44a0e92157418be1f37e
Mariya Podchishchaeva <mariya.podchishchaeva@intel.com>
2026-05-13 18:12:21 +0200

    [clang][SYCL] Fix libclc_call calling convention (#21951)
    We have device-specific calling convention libclc_call here in the
    downstream added by 5b5fd84692bf14b8ff6eeb605c40bab0320dcfe6 which was
    not expected by the upstream patch
    94ca49099ef77751a33e4babe41b2ae03ff228e1, so we ended up ignoring the
    calling convention attribute because the host target doesn't support it.
    This makes a little tweak in SemaDeclAttr to save the calling convention
    and also fixes sycl-cconv.cpp which was strangely half-merged after
    94ca49099ef77751a33e4babe41b2ae03ff228e1.

--- clang/test/SemaSYCL/sycl-cconv.cpp
+++ clang/test/SemaSYCL/sycl-cconv.cpp
@@ -1,24 +1,31 @@
 // RUN: %clang_cc1 -isystem %S/Inputs/ -fsycl-is-device -triple spirv64 -aux-triple x86_64-pc-windows-msvc -fsyntax-only -verify %s
-// RUN: %clang_cc1 -isystem %S/Inputs/ -fsycl-is-device -triple spirv64 -fsyntax-only -verify %s
+// RUN: %clang_cc1 -isystem %S/Inputs/ -fsycl-is-device -triple spirv64 -fsyntax-only -verify=expected,no-aux %s
 
 // Check that there is no error/warning emitted for cdecl functions compiled for
 // SYCL device. Make sure variadic calls from within device code are diagnosed.
 
+// no-aux-warning@+2 {{'__cdecl' calling convention is not supported for this target}}
+// no-aux-error@+1 {{variadic function cannot use spir_function calling convention}}
 __inline __cdecl int printf(char const* const _Format, ...) { return 0; }
 
 // FIXME: that should be diagnosed.
 [[clang::sycl_external]] int foo(int, ...) { return 0; }
 
+// no-aux-warning@+1 {{'__cdecl' calling convention is not supported for this target}}
 __inline __cdecl int moo() { return 0; }
 
 void bar() {
   printf("hello\n");
 }
 
+// Check some weird calling convention that is not supported even by x86_64 aux.
+// no-aux-warning@+1 {{'__swiftasynccall__' calling convention is not supported for this target}}
+void __attribute__((__swiftasynccall__)) g(void) {}
+
 template <typename name, typename Func>
-// expected-no-warning@+1
+// no-aux-warning@+1 {{'__cdecl' calling convention is not supported for this target}}
 __cdecl __attribute__((sycl_kernel)) void kernel_single_task(const Func &kernelFunc) {
-  // expected-error@+1 2{{SYCL kernel cannot call a variadic function}}
+  // expected-error@+1 2{{SYCL device code does not support variadic functions}}
   printf("cannot call from here\n");
   // expected-no-error@+1
   moo();
@@ -26,9 +33,26 @@ __cdecl __attribute__((sycl_kernel)) void kernel_single_task(const Func &kernelF
   kernelFunc();
 }
 
+template<typename KN, typename...Args>
+void sycl_kernel_launch(Args ...args) {}
+
+template<typename KN, typename K>
+[[clang::sycl_kernel_entry_point(KN)]]
+// no-aux-warning@+1 {{'__cdecl' calling convention is not supported for this target}}
+__cdecl void sycl_entry_point(K k) {
+  k(); // expected-note {{called by}}
+}
+
 int main() {
   //expected-error@+1 {{SYCL device code does not support variadic functions}}
   sycl_entry_point<class kn>([]() { printf("world\n");
+     moo();
+  //expected-error@+1 {{SYCL device code does not support variadic functions}}
+     foo(1,2); });
+
+  //expected-note@+2 {{in instantiation of}}
+  //expected-error@+1 {{SYCL device code does not support variadic functions}}
+  kernel_single_task<class kn>([]() { printf("world\n");
      moo();
   //expected-error@+1 {{SYCL device code does not support variadic functions}}
      foo(1,2); });
