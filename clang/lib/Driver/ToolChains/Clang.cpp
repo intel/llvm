0419ff6415bc9bbea6d1b7e1d746eea1469953ee
Maksimova, Viktoria <viktoria.maksimova@intel.com>
2026-05-13 03:22:36 -0700

    switch to EXT from INTEL in driver

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -10952,7 +10952,7 @@ static void getTripleBasedSPIRVTransOpts(Compilation &C,
       ",+SPV_KHR_uniform_group_instructions"
       ",+SPV_INTEL_masked_gather_scatter"
       ",+SPV_INTEL_tensor_float32_conversion"
-      ",+SPV_INTEL_optnone"
+      ",+SPV_EXT_optnone"
       ",+SPV_KHR_non_semantic_info"
       ",+SPV_KHR_cooperative_matrix"
       ",+SPV_EXT_shader_atomic_float16_add"
