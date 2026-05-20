e27c0234bd3b60ce3050c49b5334fe1c77aac32c
Katarzyna Kaczmarska <katarzynax.e.kaczmarska@intel.com>
2026-05-20 10:12:59 +0200

    [UR][CI] Use lit.local.cfg.py for ci-validation like other .test suites

--- unified-runtime/test/adapters/ci-validation/lit.local.cfg.py
+++ unified-runtime/test/adapters/ci-validation/lit.local.cfg.py
@@ -7,10 +7,4 @@ SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 """
 
-import lit.formats
-
-config.name = "Unified Runtime CI Validation"
-config.test_format = lit.formats.ShTest(True)
 config.suffixes = [".test"]
-config.test_source_root = config.binary_dir
-config.test_exec_root = config.binary_dir
