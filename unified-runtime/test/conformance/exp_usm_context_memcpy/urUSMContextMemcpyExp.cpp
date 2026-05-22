177edf20321132e90473f3b2a1f1824cdf503e40
Katarzyna Kaczmarska <katarzynax.e.kaczmarska@intel.com>
2026-05-22 09:43:08 +0200

    [TEST] Reproduce urUSMContextMemcpyExp CUDA failure
    This commit sets up CI to reproduce the sporadic failure in
    urUSMContextMemcpyExpTestDevice::Success on CUDA.
    
    Changes:
    - Remove UUR_KNOWN_FAILURE_ON(uur::CUDA{}) from test
    - Add loop with 20 iterations to increase probability of catching
      the race condition (flaky ~1/3-5 runs)
    - Re-initialize allocations between iterations
    - Comment out non-CUDA jobs in ur-precommit.yml to minimize CI run
      time and focus only on CUDA testing

--- unified-runtime/test/conformance/exp_usm_context_memcpy/urUSMContextMemcpyExp.cpp
+++ unified-runtime/test/conformance/exp_usm_context_memcpy/urUSMContextMemcpyExp.cpp
@@ -81,10 +81,19 @@ UUR_INSTANTIATE_DEVICE_TEST_SUITE_MULTI_QUEUE(urUSMContextMemcpyExpTestDevice);
 
 TEST_P(urUSMContextMemcpyExpTestDevice, Success) {
   // https://github.com/intel/llvm/issues/19688
-  UUR_KNOWN_FAILURE_ON(uur::CUDA{});
-  ASSERT_SUCCESS(
-      urUSMContextMemcpyExp(context, dst_ptr, src_ptr, allocation_size));
-  verifyData();
+  // Testing without xfail to reproduce sporadic failure
+  // Run multiple iterations to increase chance of catching race condition
+  constexpr int NumIterations = 20;
+  for (int i = 0; i < NumIterations; ++i) {
+    ASSERT_SUCCESS(
+        urUSMContextMemcpyExp(context, dst_ptr, src_ptr, allocation_size));
+    verifyData();
+
+    // Re-initialize for next iteration
+    if (i < NumIterations - 1) {
+      initAllocations();
+    }
+  }
 }
 
 // Arbitrarily do the negative tests with device allocations. These are mostly a
