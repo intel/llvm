814058dcff22812024c77e418b8ee2cf808dcafa
Dmitry Vodopyanov <dmitry.vodopyanov@intel.com>
2026-05-29 21:38:41 +0200

    Make it only for Linux`

--- cmake/Modules/DPCPPVersion.cmake
+++ cmake/Modules/DPCPPVersion.cmake
@@ -4,9 +4,14 @@ if(NOT DEFINED DPCPP_VERSION_MAJOR)
   set(DPCPP_VERSION_MAJOR 7)
 endif()
 
-# Install directory for all DPC++ compiler tools.  Placing them under a
-# versioned subdirectory avoids conflicts with a system LLVM installation.
-set(DPCPP_INSTALL_INTERNAL_BINDIR "lib/dpcpp-${DPCPP_VERSION_MAJOR}/bin")
+# Install directory for all DPC++ compiler tools.  On Linux, tools are placed
+# under a versioned subdirectory to avoid conflicts with a system LLVM
+# installation.  On other platforms the standard bin/ directory is used.
+if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+  set(DPCPP_INSTALL_INTERNAL_BINDIR "lib/dpcpp-${DPCPP_VERSION_MAJOR}/bin")
+else()
+  set(DPCPP_INSTALL_INTERNAL_BINDIR "bin")
+endif()
 if(NOT DEFINED DPCPP_VERSION_MINOR)
   set(DPCPP_VERSION_MINOR 1)
 endif()
