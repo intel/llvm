2986427615f9c69bbbfe8ee33fd71d65a10163bc
Kornev, Nikita <nikita.kornev@intel.com>
2026-06-01 19:22:34 +0200

    fix-target

--- sycl/test-e2e/AOT/CompatTargets/bmg.cpp
+++ sycl/test-e2e/AOT/CompatTargets/bmg.cpp
@@ -1,4 +1,4 @@
 // REQUIRES: arch-intel_gpu_bmg_g21 || arch-intel_gpu_bmg_g31 || arch-intel_gpu_lnl_m
 
-// RUN: %{build} -fsycl-targets=intel_gpu_ptl %S/../Inputs/simple.cpp -o %t.out
+// RUN: %{build} -fsycl-targets=intel_gpu_bmg %S/../Inputs/simple.cpp -o %t.out
 // RUN: %{run} %t.out
--- sycl/test-e2e/AOT/CompatTargets/mtl.cpp
+++ sycl/test-e2e/AOT/CompatTargets/mtl.cpp
@@ -1,4 +1,4 @@
 // REQUIRES: arch-intel_gpu_mtl_u || arch-intel_gpu_mtl_h
 
-// RUN: %{build} -fsycl-targets=intel_gpu_ptl %S/../Inputs/simple.cpp -o %t.out
+// RUN: %{build} -fsycl-targets=intel_gpu_mtl %S/../Inputs/simple.cpp -o %t.out
 // RUN: %{run} %t.out
