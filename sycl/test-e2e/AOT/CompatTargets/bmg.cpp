e7011809653100ccd363557534a5594fded8e51a
Kornev, Nikita <nikita.kornev@intel.com>
2026-06-02 12:37:08 +0200

    choose device

--- sycl/test-e2e/AOT/CompatTargets/bmg.cpp
+++ sycl/test-e2e/AOT/CompatTargets/bmg.cpp
@@ -1,4 +1,30 @@
 // REQUIRES: arch-intel_gpu_bmg_g21 || arch-intel_gpu_bmg_g31 || arch-intel_gpu_lnl_m
 
-// RUN: %{build} -fsycl-targets=intel_gpu_bmg %S/../Inputs/simple.cpp -o %t.out
+// RUN: %{build} -fsycl-targets=intel_gpu_bmg -o %t.out
 // RUN: %{run} %t.out
+
+#include <sycl/detail/core.hpp>
+
+int choose_bmg(const sycl::device& d) {
+  auto ver = d.get_info<sycl::info::device::version>();
+  return ver == "20.4.4" || ver == "20.2.0" || ver == "20.1.0";
+}
+
+int main() {
+  sycl::device dev { choose_bmg };
+  sycl::queue q(dev);
+
+  int data = 0;
+  {
+    sycl::buffer<int, 1> buf(&data, sycl::range<1>(1));
+
+    q.submit([&](sycl::handler &h) {
+       auto acc = buf.get_access<sycl::access::mode::write>(h);
+       h.single_task([=]() { acc[0] = 42; });
+     }).wait();
+  }
+
+  assert(data == 42);
+
+  return 0;
+}
--- sycl/test-e2e/AOT/CompatTargets/ptl.cpp
+++ sycl/test-e2e/AOT/CompatTargets/ptl.cpp
@@ -1,4 +1,31 @@
 // REQUIRES: arch-intel_gpu_ptl_u || arch-intel_gpu_ptl_h
 
-// RUN: %{build} -fsycl-targets=intel_gpu_ptl %S/../Inputs/simple.cpp -o %t.out
+// RUN: %{build} -fsycl-targets=intel_gpu_ptl -o %t.out
 // RUN: %{run} %t.out
+
+#include <sycl/detail/core.hpp>
+
+int choose_ptl(const sycl::device& d) {
+  auto ver = d.get_info<sycl::info::device::version>();
+  return ver == "30.0.4" || ver == "30.1.0" || ver == "30.3.1" ||
+         ver == "30.4.4" || ver == "30.5.4";
+}
+
+int main() {
+  sycl::device dev { choose_ptl };
+  sycl::queue q(dev);
+
+  int data = 0;
+  {
+    sycl::buffer<int, 1> buf(&data, sycl::range<1>(1));
+
+    q.submit([&](sycl::handler &h) {
+       auto acc = buf.get_access<sycl::access::mode::write>(h);
+       h.single_task([=]() { acc[0] = 42; });
+     }).wait();
+  }
+
+  assert(data == 42);
+
+  return 0;
+}
