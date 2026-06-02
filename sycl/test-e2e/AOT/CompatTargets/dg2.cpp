534e0cb98fa95fbfeb2b28cea6c6de5ff672f1de
Kornev, Nikita <nikita.kornev@intel.com>
2026-06-02 15:11:13 +0200

    dg2 -> acm

--- sycl/test-e2e/AOT/CompatTargets/dg2.cpp
+++ sycl/test-e2e/AOT/CompatTargets/dg2.cpp
@@ -1,4 +1,4 @@
-// REQUIRES: arch-intel_gpu_dg2_g10 || arch-intel_gpu_dg2_g11 || arch-intel_gpu_dg2_g12
+// REQUIRES: arch-intel_gpu_acm_g10 || arch-intel_gpu_acm_g11 || arch-intel_gpu_acm_g12
 
 // RUN: %{build} -fsycl-targets=intel_gpu_dg2 %S/../Inputs/simple.cpp -o %t.out
 // RUN: %{run} %t.out
