2630e63f40352b6645de8faf80d371445eeb26f6
y <konstantinos.parasyris@intel.com>
2026-06-03 11:20:29 -0700

    Fix comments

--- sycl/test-e2e/SYCLBIN/Inputs/aot_object_with_imports.cpp
+++ sycl/test-e2e/SYCLBIN/Inputs/aot_object_with_imports.cpp
@@ -4,16 +4,17 @@
 // (AOT) image whose intrinsic state is object because it still has
 // unresolved cross-image dependencies.
 
-#include <sycl/sycl.hpp>
+#include <sycl/ext/oneapi/free_function_kernel_properties.hpp>
+#include <sycl/khr/work_item_queries.hpp>
 
-namespace syclext = sycl::ext::oneapi;
+namespace syclkhr = sycl::khr;
 namespace syclexp = sycl::ext::oneapi::experimental;
 
 SYCL_EXTERNAL void TestFunc(int *Ptr, int Size);
 
 extern "C" SYCL_EXTERNAL SYCL_EXT_ONEAPI_FUNCTION_PROPERTY(
     (syclexp::nd_range_kernel<1>)) void AOTKernel(int *Ptr, int Size) {
-  size_t I = syclext::this_work_item::get_nd_item<1>().get_global_linear_id();
+  size_t I = syclkhr::this_nd_item<1>().get_global_linear_id();
   if (static_cast<int>(I) < Size)
     TestFunc(Ptr, Size);
 }
--- sycl/test/e2e_test_requirements/no_sycl_hpp_in_e2e_tests.cpp
+++ sycl/test/e2e_test_requirements/no_sycl_hpp_in_e2e_tests.cpp
@@ -6,8 +6,8 @@
 // CHECK-DAG: README.md
 // CHECK-DAG: lit.cfg.py
 //
-// CHECK-NUM-MATCHES: 30
-// localdisk2/kparasyr/LLVM-FORK/llvm/sycl/test/e2e_test_requirements/no_sycl_hpp_in_e2e_tests.cpp/
-//  This test verifies that `<sycl/sycl.hpp>` isn't used in E2E tests. Instead,
-//  fine-grained includes should used, see
-//  https://github.com/intel/llvm/tree/sycl/sycl/test-e2e#sycldetailcorehpp
+// CHECK-NUM-MATCHES: 29
+
+// This test verifies that `<sycl/sycl.hpp>` isn't used in E2E tests. Instead,
+// fine-grained includes should used, see
+// https://github.com/intel/llvm/tree/sycl/sycl/test-e2e#sycldetailcorehpp
