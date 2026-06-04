597a5136ba80998210336040d54d4dfb87afae0c
Matthew Michel <matthew.michel@intel.com>
2026-06-04 16:15:05 -0700

    [SYCL][GRAPH][E2E] Add new driver regression tests

---
+++ sycl/test-e2e/Graph/Error/invalid_event_wait_recording.cpp
@@ -0,0 +1,9 @@
+// RUN: %{build} -o %t.out
+// RUN: %{run} %t.out
+// Extra run to check for leaks in Level Zero using UR_L0_LEAKS_DEBUG
+// RUN: %if level_zero %{%{l0_leak_check} %{run} %t.out 2>&1 | FileCheck %s --implicit-check-not=LEAK %}
+
+#define GRAPH_E2E_RECORD_REPLAY
+#define GRAPH_E2E_NATIVE_RECORDING
+
+#include "../../Inputs/exception_recording_event_wait.cpp"
---
+++ sycl/test-e2e/Graph/Inputs/exception_recording_queue_wait.cpp
@@ -0,0 +1,38 @@
+// Tests that waiting on a Queue in recording mode throws.
+
+#include "../graph_common.hpp"
+
+#include <sycl/properties/all_properties.hpp>
+
+int main() {
+  queue Queue{property::queue::in_order{}};
+
+#ifdef GRAPH_E2E_NATIVE_RECORDING
+  exp_ext::command_graph Graph{
+      Queue.get_context(),
+      Queue.get_device(),
+      {exp_ext::property::graph::enable_native_recording{}}};
+#else
+  exp_ext::command_graph Graph{Queue.get_context(), Queue.get_device()};
+#endif
+  Graph.begin_recording(Queue);
+
+  // TODO: The SYCL graph spec mandates errc::invalid but L0 graph's generic
+  // error codes make it difficult to determine the root cause. Once L0 graph
+  // expands their error codes we can add our handling and return the correct
+  // SYCL code along with a descriptive message to the user.
+#ifdef GRAPH_E2E_NATIVE_RECORDING
+  if (!expectException([&]() { Queue.wait(); },
+                       "event wait during graph recording", errc::runtime)) {
+    return 1;
+  }
+#else
+  if (!expectException([&]() { Queue.wait(); },
+                       "event wait during graph recording", errc::invalid)) {
+    return 1;
+  }
+#endif
+  Graph.end_recording();
+
+  return 0;
+}
