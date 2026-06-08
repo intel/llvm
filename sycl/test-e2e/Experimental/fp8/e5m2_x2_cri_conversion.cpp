9e4b9be5e856f169a89bf7b011a032e9cfe89f48
Klochkov, Denis <denis.klochkov@intel.com>
2026-06-08 14:40:29 +0200

    [SYCL][TESTE2E] run fp8 tests only on cri device

--- sycl/test-e2e/Experimental/fp8/e5m2_x2_cri_conversion.cpp
+++ sycl/test-e2e/Experimental/fp8/e5m2_x2_cri_conversion.cpp
@@ -1,3 +1,4 @@
+// REQUIRES: intel_feature_gpu_cri
 // RUN: %{build} -Xclang -freg-struct-return -Xspirv-translator=spir64 --spirv-ext=+SPV_INTEL_fp_conversions,+SPV_EXT_float8,+SPV_KHR_bfloat16 -o %t.out
 // RUN: %{run} SYCL_UR_TRACE=1 %t.out
 
