7b10d480c13756b50b4d90a05afbe1a19a8ce229
Michael D Toguchi <michael.d.toguchi@intel.com>
2026-06-08 16:25:35 -0700

    -fsycl-rdc is the default

--- sycl/test-e2e/SYCLBIN/link_object_fast_link.cpp
+++ sycl/test-e2e/SYCLBIN/link_object_fast_link.cpp
@@ -4,11 +4,11 @@
 
 // REQUIRES: target-spir
 
-// RUN: %clangxx --offload-new-driver -fsycl-rdc -fsyclbin=object %{syclbin_exec_opts} -fsycl-allow-device-image-dependencies %S/Inputs/exporting_function.cpp -o %t.export_w_aot.syclbin
-// RUN: %clangxx --offload-new-driver -fsycl-rdc -fsyclbin=object %{syclbin_exec_opts} -fsycl-allow-device-image-dependencies %S/Inputs/importing_kernel.cpp -o %t.import_w_aot.syclbin
+// RUN: %clangxx --offload-new-driver -fsyclbin=object %{syclbin_exec_opts} -fsycl-allow-device-image-dependencies %S/Inputs/exporting_function.cpp -o %t.export_w_aot.syclbin
+// RUN: %clangxx --offload-new-driver -fsyclbin=object %{syclbin_exec_opts} -fsycl-allow-device-image-dependencies %S/Inputs/importing_kernel.cpp -o %t.import_w_aot.syclbin
 // RUN: %clangxx --offload-new-driver -fsyclbin=object -fsycl-allow-device-image-dependencies %S/Inputs/exporting_function.cpp -o %t.export.syclbin
 // RUN: %clangxx --offload-new-driver -fsyclbin=object -fsycl-allow-device-image-dependencies %S/Inputs/importing_kernel.cpp -o %t.import.syclbin
-// RUN: %{build} -o %t.out
+// RUN: %{build} -o %t.ou t
 
 // RUN: %{run} %t.out %t.export.syclbin %t.import.syclbin
 // RUN: %{run} %t.out %t.export_w_aot.syclbin %t.import_w_aot.syclbin
