be98f79b6171b11b83170c619d406ed6e933cbe9
Lorenc Bushi <lorenc.bushi@intel.com>
2026-06-16 12:03:56 -0700

    Print default arguments of templates when printing kernel names for lambdas/functors in integration header

--- clang/lib/Sema/SemaSYCL.cpp
+++ clang/lib/Sema/SemaSYCL.cpp
@@ -6442,6 +6442,32 @@ public:
     TA.print(Policy, OS, false /* IncludeType */);
   }
 
+  void VisitDeclarationTemplateArgument(const TemplateArgument &TA) {
+    // A non-type template argument that refers to a function template
+    // specialization (e.g. a free function kernel pointer such as
+    // `&vec_add_kernel<int>`) is printed by TemplateArgument::print using only
+    // the function's qualified name, dropping its template arguments. This
+    // produces an ambiguous `<overloaded function type>` initializer in the
+    // integration header. Emit the template arguments explicitly so the
+    // resulting kernel name type names the exact specialization.
+    const ValueDecl *VD = TA.getAsDecl();
+    const auto *FD = dyn_cast<FunctionDecl>(VD);
+    const TemplateArgumentList *TAL =
+        FD ? FD->getTemplateSpecializationArgs() : nullptr;
+    if (!TAL) {
+      VisitTemplateArgument(TA);
+      return;
+    }
+
+    const QualType ParamType = TA.getParamTypeForDecl();
+    if (ParamType->isPointerType() || ParamType->isMemberPointerType())
+      OS << "&";
+    VD->printQualifiedName(OS, Policy);
+    OS << "<";
+    printTemplateArgs(TAL->asArray());
+    OS << ">";
+  }
+
   void VisitTypeTemplateArgument(const TemplateArgument &TA) {
     Policy.SuppressTagKeyword = true;
     QualType T = TA.getAsType();
@@ -7419,6 +7445,13 @@ void SYCLIntegrationHeader::emit(raw_ostream &O) {
   O << "} // namespace _V1\n";
   O << "} // namespace sycl\n";
 
+  // The rest of this function only applies to free-function kernels. However,
+  // in RTC mode, we do not need integration header information for
+  // free-function kernels, so we can return early here.
+  if (S.getLangOpts().SYCLRTCMode) {
+    return;
+  }
+
   unsigned ShimCounter = 1;
   int FreeFunctionCount = 0;
   // Structs with special types inside need some special code generation in the
@@ -7637,9 +7670,6 @@ void SYCLIntegrationHeader::emit(raw_ostream &O) {
     O << "} // namespace\n";
   }
 
-
- PrintingPolicy NewPolicy(LO);
-
   O << "// Specializations of KernelInfo for kernel function types:\n";
   O << "namespace sycl {\n";
   O << "inline namespace _V1 {\n";
@@ -7662,8 +7692,10 @@ void SYCLIntegrationHeader::emit(raw_ostream &O) {
       OutputStableNameInChars(O, K.StableName);
       O << "> {\n";
     } else {
+      Policy.SuppressDefaultTemplateArgs = false;
+      Policy.EnforceDefaultTemplateArgs = true;
       O << "template <> struct KernelInfo<";
-      SYCLKernelNameTypePrinter Printer(O, NewPolicy);
+      SYCLKernelNameTypePrinter Printer(O, Policy);
       Printer.Visit(K.NameType);
       O << "> {\n";
     }
@@ -7696,7 +7728,7 @@ void SYCLIntegrationHeader::emit(raw_ostream &O) {
     O << "  static constexpr const char* getFunctionName() {\n";
     O << "#ifndef NDEBUG\n";
     O << "    return \"";
-    SYCLKernelNameTypePrinter Printer(O, NewPolicy);
+    SYCLKernelNameTypePrinter Printer(O, Policy);
     Printer.Visit(K.NameType);
     O << "\";\n";
     O << "#else\n";
