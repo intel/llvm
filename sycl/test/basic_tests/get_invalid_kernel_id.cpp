b1407f4249fb19b35e5de6a25332a42b83398deb
Sergey Semenov <sergey.semenov@intel.com>
2026-06-17 10:14:38 -0700

    Fix get_invalid_kernel_id on Windows

--- sycl/test/basic_tests/get_invalid_kernel_id.cpp
+++ sycl/test/basic_tests/get_invalid_kernel_id.cpp
@@ -1,10 +1,11 @@
-// RUN: not %clangxx -fsycl %s 2>&1 | FileCheck %s
+// RUN: not %clangxx -fsycl %s 2>&1 | FileCheck %s --check-prefix %if windows %{CHECK-WINDOWS%} %else %{CHECK-LINUX%}
 
 #include <sycl/sycl.hpp>
 
 class NotAKernel;
 
 int main() {
-  // CHECK: undefined reference to `sycl::_V1::detail::getKernelNameHelper(sycl::_V1::detail::KernelIdentity<NotAKernel>)
+  // CHECK-LINUX: undefined reference to `sycl::_V1::detail::getKernelNameHelper(sycl::_V1::detail::KernelIdentity<NotAKernel>)
+  // CHECK-WINDOWS: unresolved external symbol "char const * __cdecl sycl::_V1::detail::getKernelNameHelper(struct sycl::_V1::detail::KernelIdentity<class NotAKernel>)"
   sycl::get_kernel_id<NotAKernel>();
 }
