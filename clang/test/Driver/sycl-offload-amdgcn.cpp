c6d3bc99ee89dd5ef594cdcde797e5a3d7702914
Wenju He <wenju.he@intel.com>
2026-06-22 06:02:36 +0200

    fix sycl-offload-amdgcn.cpp

--- clang/test/Driver/sycl-offload-amdgcn.cpp
+++ clang/test/Driver/sycl-offload-amdgcn.cpp
@@ -15,17 +15,18 @@
 // CHK-MULTI-ARCH: error: missing AMDGPU architecture for SYCL offloading; specify it with '-Xsycl-target-backend=amdgcn-amd-amdhsa --offload-arch=<arch-name>'
 
 /// Check action graph.
-// RUN: %clangxx -### -std=c++11 -target x86_64-unknown-linux-gnu -fsycl \
+// RUN: %clangxx -### -std=c++17 -target x86_64-unknown-linux-gnu -fsycl \
 // RUN: -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx906 -nogpulib\
 // RUN: -fsycl-libspirv-path=%S/Inputs/SYCL/libspirv.bc %s 2>&1 \
 // RUN: | FileCheck -check-prefix=CHK-ACTIONS %s
-// CHK-ACTIONS: "-cc1" "-triple" "amdgcn-amd-amdhsa" "-aux-triple" "x86_64-unknown-linux-gnu"{{.*}} "-fsycl-is-device"{{.*}} "-Wno-sycl-strict"{{.*}} "-sycl-std=2020" {{.*}} "-internal-isystem" "{{.*}}bin{{[/\\]+}}..{{[/\\]+}}include{{[/\\]+}}sycl{{[/\\]+}}stl_wrappers"{{.*}} "-mlink-builtin-bitcode" "{{.*}}libspirv.bc"{{.*}} "-target-cpu" "gfx906"{{.*}} "-std=c++11"{{.*}}
+// CHK-ACTIONS: "-cc1" "-triple" "amdgcn-amd-amdhsa" "-aux-triple" "x86_64-unknown-linux-gnu"{{.*}} "-fsycl-is-device"{{.*}} "-Wno-sycl-strict"{{.*}} "-sycl-std=2020" {{.*}} "-internal-isystem" "{{.*}}bin{{[/\\]+}}..{{[/\\]+}}include{{[/\\]+}}sycl{{[/\\]+}}stl_wrappers"{{.*}} "-mlink-builtin-bitcode" "{{.*}}libspirv.bc"{{.*}} "-target-cpu" "gfx906"{{.*}} "-std=c++17"{{.*}}
 // CHK-ACTIONS-NOT: "-mllvm -sycl-opt"
 // CHK-ACTIONS: clang-offload-wrapper"{{.*}} "-host=x86_64-unknown-linux-gnu" "-compile-opts=--offload-arch=gfx906" "-target=amdgcn" "-kind=sycl"{{.*}}
 
 /// Check phases w/out specifying a compute capability.
-// RUN: %clangxx -ccc-print-phases -std=c++11 -target x86_64-unknown-linux-gnu -fsycl \
+// RUN: %clangxx -ccc-print-phases -std=c++17 -target x86_64-unknown-linux-gnu -fsycl \
 // RUN: -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx906 %s 2>&1 \
+// RUN: --sysroot=%S/Inputs/SYCL \
 // RUN: | FileCheck -check-prefix=CHK-PHASES-NO-CC %s
 // CHK-PHASES-NO-CC: 0: input, "{{.*}}", c++, (host-sycl)
 // CHK-PHASES-NO-CC: 1: preprocessor, {0}, c++-cpp-output, (host-sycl)
@@ -55,11 +56,11 @@
 // RUN: %clangxx -### -target x86_64-unknown-linux-gnu -fsycl -nogpulib \
 // RUN:   -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend \
 // RUN:   --offload-arch=gfx906 %s -L%S/Inputs/SYCL -llin64 2>&1 \
-// RUN: | FileCheck -check-prefix=CHK-ARCHIVE %s
+// RUN:   -fsycl-libspirv-path=%S/Inputs/SYCL/libspirv.bc | FileCheck -check-prefix=CHK-ARCHIVE %s
 // RUN: %clangxx -### -target x86_64-unknown-linux-gnu -fsycl -nogpulib \
 // RUN:   -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend \
 // RUN:   --offload-arch=gfx906 %s %S/Inputs/SYCL/liblin64.a 2>&1 \
-// RUN: | FileCheck -check-prefix=CHK-ARCHIVE %s
+// RUN:   -fsycl-libspirv-path=%S/Inputs/SYCL/libspirv.bc | FileCheck -check-prefix=CHK-ARCHIVE %s
 // CHK-ARCHIVE: clang-offload-bundler{{.*}} "-input={{.*}}/Inputs/SYCL/liblin64.a"
 // CHK-ARCHIVE: llvm-link{{.*}}
 // CHK-ARCHIVE-NOT: clang-offload-bundler{{.*}} "-unbundle"
