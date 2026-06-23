194000dad76fbfebba2d3966acd453e642a5130e
Nick Sarnie <nick.sarnie@intel.com>
2026-06-23 11:09:36 -0700

    format
    Signed-off-by: Nick Sarnie <nick.sarnie@intel.com>

--- sycl/test-e2e/KernelAndProgram/level-zero-static-link-flow.cpp
+++ sycl/test-e2e/KernelAndProgram/level-zero-static-link-flow.cpp
@@ -4,7 +4,6 @@
 // UNSUPPORTED: windows && (gpu-intel-gen12 || arch-intel_gpu_bmg_g21)
 // UNSUPPORTED-TRACKER: https://github.com/intel/llvm/issues/21556
 
-
 // UNSUPPORTED: linux && run-mode && !gpu-intel-dg2
 // UNSUPPORTED-TRACKER: https://github.com/intel/llvm/issues/22405
 
