0a42f64adb6c0c3cab02b967ab3e0da4e1991d5d
kweronsx <krzysztofx.weronski@intel.com>
2026-06-24 20:39:43 +0200

    [UR] Refactor reusable event API to pass device as urEventCreateExp parameter (#22387)
    Remove hDevice data member from ur_exp_event_desc_t.
    Insert hDevice as parameter of urEventCreateExp.

--- unified-runtime/source/adapters/hip/event.cpp
+++ unified-runtime/source/adapters/hip/event.cpp
@@ -314,7 +314,8 @@ UR_APIEXPORT ur_result_t UR_APICALL urEventCreateWithNativeHandle(
   return UR_RESULT_SUCCESS;
 }
 
-UR_APIEXPORT ur_result_t UR_APICALL urEventCreateExp(
-    ur_context_handle_t, const ur_exp_event_desc_t *, ur_event_handle_t *) {
+UR_APIEXPORT ur_result_t UR_APICALL
+urEventCreateExp(ur_context_handle_t, ur_device_handle_t,
+                 const ur_exp_event_desc_t *, ur_event_handle_t *) {
   return UR_RESULT_ERROR_UNSUPPORTED_FEATURE;
 }
