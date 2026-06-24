3aaa0c9688ea3b0ac0cc4407b22ae91289aa2bfb
dklochkov-intel <denis.klochkov@intel.com>
2026-06-24 13:00:36 +0200

    [SYCL] optimize fp8 load store (#22321)
    When size == 2 of FP8 data type, it was used memcpy to store and copy
    data. However, if it is guaranteed that fp8 data type sizeof is 2 bytes,
    then we can optimize it to have one ir operation, This PR adds alignment
    for data types which are supported by hardware.

--- sycl/test-e2e/Experimental/fp8/e4m3_x2_cri_conversion.cpp
+++ sycl/test-e2e/Experimental/fp8/e4m3_x2_cri_conversion.cpp
@@ -337,6 +337,9 @@ template <typename T> int test_carray_conversion(sycl::queue &queue) {
 }
 
 int main() {
+  static_assert(alignof(fp8_e4m3_x2) == 2);
+  static_assert(sizeof(fp8_e4m3_x2) == 2);
+
   auto async_handler = [](sycl::exception_list exceptions) {
     for (const std::exception_ptr &e : exceptions) {
       try {
