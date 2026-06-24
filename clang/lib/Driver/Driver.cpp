b0496df40a12c9fed614b4b3cca0fd1ec6ce2f4c
Wenju He <wenju.he@intel.com>
2026-06-24 13:09:33 +0200

    drop driver diagnose

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -3736,10 +3736,7 @@ void Driver::BuildInputs(const ToolChain &TC, DerivedArgList &Args,
         Diag(clang::diag::err_drv_unknown_language) << A->getValue();
         InputType = types::TY_Object;
       }
-      // Reject C input with -fsycl; SYCL requires C++.
-      if (IsSYCL && (InputType == types::TY_C || InputType == types::TY_PP_C ||
-                     InputType == types::TY_CHeader))
-        Diag(clang::diag::err_drv_argument_not_allowed_with) << "C" << "-fsycl";
+
       // If the user has put -fmodule-header{,=} then we treat C++ headers as
       // header unit inputs.  So we 'promote' -xc++-header appropriately.
       if (InputType == types::TY_CXXHeader && hasHeaderMode())
