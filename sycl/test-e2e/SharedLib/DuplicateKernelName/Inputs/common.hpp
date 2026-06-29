037026aa99032e7e0b9c0e259987b7744a540a70
Sergey Semenov <sergey.semenov@intel.com>
2026-06-29 09:06:40 -0700

    clang-format

--- sycl/test-e2e/SharedLib/DuplicateKernelName/Inputs/common.hpp
+++ sycl/test-e2e/SharedLib/DuplicateKernelName/Inputs/common.hpp
@@ -9,7 +9,7 @@
 #define API_EXPORT
 #endif
 
- inline void enqueueWithKernelId(sycl::queue &Q, sycl::kernel_id Id, int *Ptr) {
+inline void enqueueWithKernelId(sycl::queue &Q, sycl::kernel_id Id, int *Ptr) {
   sycl::kernel_bundle KernelBundle =
       sycl::get_kernel_bundle<sycl::bundle_state::executable>(Q.get_context(),
                                                               {Q.get_device()});
