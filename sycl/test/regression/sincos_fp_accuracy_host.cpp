4207747d63d473280b2dea00d5f897cefe1060a6
jinge90 <ge.jin@intel.com>
2026-07-02 15:55:03 +0800

    add clang codegen test
    Signed-off-by: jinge90 <ge.jin@intel.com>

--- sycl/test/regression/sincos_fp_accuracy_host.cpp
+++ sycl/test/regression/sincos_fp_accuracy_host.cpp
@@ -1,23 +1,16 @@
 // Verifies that sycl::sincos does not crash the compiler during host
 // compilation with -ffp-accuracy=high. The SYCL sincos has a different
-// signature (float sincos(float, float*)) from the GNU sincos 
+// signature (float sincos(float, float*)) from the GNU sincos
 // (void sincos(float, float*, float*)) and must not be replaced with the
 // fpbuiltin_sincos intrinsic.
 
 // RUN: %clangxx -fsycl -ffp-accuracy=high -fno-math-errno %s -S -emit-llvm \
 // RUN: -o - | FileCheck %s
-#include <sycl/sycl.hpp>
+#include <sycl/builtins.hpp>
 
-// CHECK-NOT: call void @llvm.fpbuiltin.sincos
-// CHECK: call {{.*}}float @_ZN4sycl{{.*}}sincos_impl{{.*}}(float {{.*}}, ptr {{.*}})
-
-void test(sycl::queue &q) {
-  q.submit([&](sycl::handler &cgh) {
-    cgh.parallel_for<class SinCosKernel>(sycl::range<1>(4), [=](sycl::id<1> idx) {
-      float x = 1.0f;
-      float c;
-      float s = sycl::sincos(x, &c);
-    });
-  });
+// CHECK-NOT: call {{.*}}@llvm.fpbuiltin.sincos
+// CHECK: call {{.*}}float {{.*}}sycl{{.*}}sincos_impl{{.*}}(float {{.*}}, ptr {{.*}})
+float host_sincos_sycl(float x, float *c) {
+  float s = sycl::sincos(x, c);
+  return s;
 }
-
