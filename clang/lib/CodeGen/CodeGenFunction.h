d09048a46401b8e8385196679a9515516e16288a
Sirraide <aeternalmail@gmail.com>
2026-07-11 00:01:08 +0200

    [Clang] [C++26] Expansion Statements (P1306R5) (#169680)
    This implements C++26's expansion statements, i.e.
    [P1306R5](https://wg21.link/P1306R5).
    
    An expansion statement is implemented using a number of new AST
    nodes. Since an expansion statement is a template, we need it to be
    a dependent DeclContext, but only nodes that inherit from Decl can
    be DeclContexts, for which reason this introduces a CXXExpansionStmtDecl.
    This holds a synthesised constant template parameter used as the
    expansion index as well as a 'CXXExpansionStmtPattern' and a
    'CXXExpansionStmtInstantiation.
    
    These two AST nodes are similar to the semantic vs syntactic form
    of an InitListExpr: the 'Pattern' represents an unexpanded expansion
    statement, and the resulting AST after expansion is stored in an
    'Instantiation'.
    
    Since expansion statements require us to perform template instantiation
    *inside* a function without instantiating the rest of a function, I had to
    remove a number of assertions in the template instantiation engine (e.g.
    the assertion that a function parameter must always have an instantiation 
    by the time we encounter a DeclRefExpr to it no longer holds).
    
    For more implementation details, see the corresponding doc comments
    in DeclTemplate.h and StmtCXX.h.
    
    There is some follow up work to be done after this lands, which is
    tracked in #207776.

--- clang/lib/CodeGen/CodeGenFunction.h
+++ clang/lib/CodeGen/CodeGenFunction.h
@@ -3749,6 +3749,9 @@ public:
   void EmitCXXForRangeStmt(const CXXForRangeStmt &S,
                            ArrayRef<const Attr *> Attrs = {});
 
+  void
+  EmitCXXExpansionStmtInstantiation(const CXXExpansionStmtInstantiation &S);
+
   /// Controls insertion of cancellation exit blocks in worksharing constructs.
   class OMPCancelStackRAII {
     CodeGenFunction &CGF;
