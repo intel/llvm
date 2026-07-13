1f6ecb005f42f1544e0aebe558db64e0dc8ca1da
Wenju He <wenju.he@intel.com>
2026-07-13 11:34:09 +0200

    Revert "remove colon"
    This reverts commit 546fc6743528017b526eade3a40e7833a2c255a1.

--- sycl/test/basic_tests/get_invalid_kernel_id.cpp
+++ sycl/test/basic_tests/get_invalid_kernel_id.cpp
@@ -5,7 +5,7 @@
 class NotAKernel;
 
 int main() {
-  // CHECK-LINUX: {{undefined reference to|undefined symbol}} {{.?}}sycl::_V1::detail::getKernelNameHelper(sycl::_V1::detail::KernelIdentity<NotAKernel>)
+  // CHECK-LINUX: {{undefined reference to|undefined symbol:}} {{.?}}sycl::_V1::detail::getKernelNameHelper(sycl::_V1::detail::KernelIdentity<NotAKernel>)
   // CHECK-WINDOWS: unresolved external symbol "char const * __cdecl sycl::_V1::detail::getKernelNameHelper(struct sycl::_V1::detail::KernelIdentity<class NotAKernel>)"
   sycl::get_kernel_id<NotAKernel>();
 }
