7697e32e3c8c1e2fbeeed6bf05daec6fdd299f7b
Chris Perkins <chris.perkins@intel.com>
2026-07-20 14:50:26 -0700

    a bit more guarding

--- unified-runtime/source/adapters/level_zero/common.cpp
+++ unified-runtime/source/adapters/level_zero/common.cpp
@@ -216,10 +216,6 @@ template <>
 ze_structure_type_t getZeStructureType<ze_image_pitched_exp_desc_t>() {
   return ZE_STRUCTURE_TYPE_PITCHED_IMAGE_EXP_DESC;
 }
-template <>
-ze_structure_type_t getZeStructureType<ze_custom_pitch_exp_desc_t>() {
-  return ZE_STRUCTURE_TYPE_CUSTOM_PITCH_EXP_DESC;
-}
 template <> ze_structure_type_t getZeStructureType<ze_module_desc_t>() {
   return ZE_STRUCTURE_TYPE_MODULE_DESC;
 }
