3df272b6ac65a862f5b18c91ff70aa0edfd0fb42
Grzegorz Mianowski <grzegorzx.mianowski@intel.com>
2026-07-20 09:48:19 +0200

    Remove checking LuidResult and NodeMaskResult

--- unified-runtime/source/adapters/cuda/device.cpp
+++ unified-runtime/source/adapters/cuda/device.cpp
@@ -1229,11 +1229,6 @@ UR_APIEXPORT ur_result_t UR_APICALL urDeviceGetInfo(ur_device_handle_t hDevice,
     // std::array<std::byte, 8>. For details about this extension,
     // see sycl/doc/extensions/supported/sycl_ext_intel_device_info.md.
     std::array<char, 8> LUID{};
-    auto LuidResult = cuDeviceGetLuid(LUID.data(), nullptr, hDevice->get());
-    if (LuidResult == CUDA_ERROR_NOT_SUPPORTED) {
-      return UR_RESULT_ERROR_UNSUPPORTED_ENUMERATION;
-    }
-    UR_CHECK_ERROR(LuidResult);
 
     bool isAllZeros = true;
     for (char num : LUID) {
@@ -1256,11 +1251,6 @@ UR_APIEXPORT ur_result_t UR_APICALL urDeviceGetInfo(ur_device_handle_t hDevice,
     // uint32_t. For details about this extension,
     // see sycl/doc/extensions/supported/sycl_ext_intel_device_info.md.
     uint32_t nodeMask = 0;
-    auto NodeMaskResult = cuDeviceGetLuid(nullptr, &nodeMask, hDevice->get());
-    if (NodeMaskResult == CUDA_ERROR_NOT_SUPPORTED) {
-      return UR_RESULT_ERROR_UNSUPPORTED_ENUMERATION;
-    }
-    UR_CHECK_ERROR(NodeMaskResult);
 
     // If nodeMask has not changed, return unsupported.
     if (nodeMask == 0) {
