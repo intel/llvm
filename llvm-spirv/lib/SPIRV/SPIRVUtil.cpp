8dcabda6f64c13751260fc11487f87d606698537
Mateusz Chudyk <mateusz.chudyk@intel.com>
2026-07-22 02:57:27 +0200

    Fix OpGroupNonUniformRotateKHR mangling (#3863)
    According to
    https://github.khronos.org/SPIRV-Registry/extensions/KHR/SPV_KHR_subgroup_rotate.html
    both Delta and ClusterSize must be a scalar of integer type, whose
    Signedness operand is 0.
    
    Original commit:
    https://github.com/KhronosGroup/SPIRV-LLVM-Translator/commit/a37897185f29584

--- llvm-spirv/lib/SPIRV/SPIRVUtil.cpp
+++ llvm-spirv/lib/SPIRV/SPIRVUtil.cpp
@@ -2530,6 +2530,7 @@ public:
       addUnsignedArg(2);
       break;
     case OpGroupNonUniformRotateKHR:
+      addUnsignedArg(2);
       if (ArgTys.size() == 4)
         addUnsignedArg(3);
       break;
