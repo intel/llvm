4aafa75dcfb92a720a2ab65c506d55b47049ec5d
Dmitry Vodopyanov <dmitry.vodopyanov@intel.com>
2026-07-23 15:51:55 +0200

    Remove xfail from e2e test

--- sycl/test-e2e/FreeFunctionKernels/enqueue_functions_sub_group_size.cpp
+++ sycl/test-e2e/FreeFunctionKernels/enqueue_functions_sub_group_size.cpp
@@ -2,9 +2,6 @@
 // UNSUPPORTED: cuda, hip
 // UNSUPPORTED-INTENDED: Device incompatible error
 
-// XFAIL: target-native_cpu
-// XFAIL-TRACKER: https://github.com/intel/llvm/issues/20142
-
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 
