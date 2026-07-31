ffddb5cbc7e4d7511ed18dfc9e5d770126515488
Nikita Kornev <nikita.kornev@intel.com>
2026-07-31 12:08:58 +0200

    [SYCL] Update Graph tests to use access_mode (#22826)
    access::mode is deprecated in favor of access_mode.
    PR to add deprecations: https://github.com/intel/llvm/pull/22803

--- sycl/test-e2e/Graph/Inputs/spec_constants_handler_api.cpp
+++ sycl/test-e2e/Graph/Inputs/spec_constants_handler_api.cpp
@@ -74,11 +74,11 @@ bool test_default_values(sycl::queue Queue) {
 
     add_node(Graph, Queue, ([&](sycl::handler &CGH) {
                auto IntAcc =
-                   IntBuffer.get_access<sycl::access::mode::write>(CGH);
+                   IntBuffer.get_access<sycl::access_mode::write>(CGH);
                auto IntAcc2 =
-                   IntBuffer2.get_access<sycl::access::mode::write>(CGH);
+                   IntBuffer2.get_access<sycl::access_mode::write>(CGH);
                auto FloatAcc =
-                   FloatBuffer.get_access<sycl::access::mode::write>(CGH);
+                   FloatBuffer.get_access<sycl::access_mode::write>(CGH);
 
                CGH.single_task<TestDefaultValuesKernel>(
                    [=](sycl::kernel_handler KH) {
@@ -173,10 +173,9 @@ bool test_set_and_get_on_device(sycl::queue Queue) {
 
     add_node(
         Graph, Queue, ([&](sycl::handler &CGH) {
-          auto IntAcc = IntBuffer.get_access<sycl::access::mode::write>(CGH);
-          auto IntAcc2 = IntBuffer2.get_access<sycl::access::mode::write>(CGH);
-          auto FloatAcc =
-              FloatBuffer.get_access<sycl::access::mode::write>(CGH);
+          auto IntAcc = IntBuffer.get_access<sycl::access_mode::write>(CGH);
+          auto IntAcc2 = IntBuffer2.get_access<sycl::access_mode::write>(CGH);
+          auto FloatAcc = FloatBuffer.get_access<sycl::access_mode::write>(CGH);
 
           CGH.set_specialization_constant<IntId>(NewIntValue);
           CGH.set_specialization_constant<IntId2>(NewIntValue2);
