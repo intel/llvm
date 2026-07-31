ffd5c9767200642a3ff182ddc1a9bde8d615b714
Nikita Kornev <nikita.kornev@intel.com>
2026-07-31 14:23:21 +0200

    [SYCL] Update Adapters tests to use access_mode (#22822)
    access::mode is deprecated in favor of access_mode.
    PR to add deprecations: https://github.com/intel/llvm/pull/22803

--- sycl/test-e2e/Adapters/level_zero/interop-buffer-ownership.cpp
+++ sycl/test-e2e/Adapters/level_zero/interop-buffer-ownership.cpp
@@ -88,8 +88,7 @@ void test_copyback_and_free(
           BufferInteropInput, Context);
 
       auto Event = Queue1.submit([&](sycl::handler &CGH) {
-        auto Acc =
-            BufferInterop.get_access<sycl::access::mode::read_write>(CGH);
+        auto Acc = BufferInterop.get_access<sycl::access_mode::read_write>(CGH);
         CGH.single_task<class SimpleKernel6>([=]() {
           for (int i = 0; i < 12; i++) {
             Acc[i] = 99;
@@ -100,8 +99,7 @@ void test_copyback_and_free(
 
       // Submit in a different context
       Queue2.submit([&](sycl::handler &CGH) {
-        auto Acc =
-            BufferInterop.get_access<sycl::access::mode::read_write>(CGH);
+        auto Acc = BufferInterop.get_access<sycl::access_mode::read_write>(CGH);
         CGH.single_task<class SimpleKernel7>([=]() {
           for (int i = 0; i < 12; i++) {
             Acc[i] *= 2;
