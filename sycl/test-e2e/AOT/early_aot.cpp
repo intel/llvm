31cd694528fa7fc74e72d9919ef08bce8b936412
Balaji V. Iyer <balaji.iyer@intel.com>
2026-08-03 15:40:16 -0700

    Fix conflict resolution: restore XFAIL/UNSUPPORTED markers
    Restore test-directive headers on two files that were lost during the
    rebase conflict resolution when the origin/sycl side of the header was
    kept instead of the PR side:
    
    - sycl/test-e2e/AOT/early_aot.cpp: XFAIL: new-offload-model + tracker
    - sycl/test-e2e/ProgramManager/uneven_kernel_split.cpp:
      UNSUPPORTED: linux + XFAIL: windows && !new-offload-model + trackers

--- sycl/test-e2e/AOT/early_aot.cpp
+++ sycl/test-e2e/AOT/early_aot.cpp
@@ -1,9 +1,8 @@
 // Test early-AOT behaviors with -fsycl -fno-sycl-rdc.  This targets spir64_gen
 
 // REQUIRES: ocloc, gpu, target-spir
-
-// Note: New Offload Model temporarily requires -fno-sycl-rdc to be specified
-// at the linking step. Old Offload Model requires it at the compilation step.
+// XFAIL: new-offload-model
+// XFAIL-TRACKER: CMPLRLLVM-51875
 
 // Build the early AOT device binaries
 // RUN: %clangxx -fsycl -fsycl-targets=%{intel_gpu_aot_targets} -fno-sycl-rdc -c -DADD_CPP %s -o %t_add.o
