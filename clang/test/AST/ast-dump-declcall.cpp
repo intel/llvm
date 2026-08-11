6442e7f69d5c0106183890767d4317f2feb66fe8
Konstantinos Parasyris <konstantinos.parasyris@intel.com>
2026-08-11 12:21:56 -0700

    [clang][test] Check the devirtualized marker in declcall AST dump

--- clang/test/AST/ast-dump-declcall.cpp
+++ clang/test/AST/ast-dump-declcall.cpp
@@ -7,3 +7,9 @@ auto p = declcall(f(0));
 // CHECK: CXXDeclcallExpr {{.*}} 'int (*)(int)'
 // CHECK-NEXT: ImplicitCastExpr {{.*}} 'int (*)(int)' <FunctionToPointerDecay>
 // CHECK-NEXT: DeclRefExpr {{.*}} 'int (int)' {{.*}} Function {{.*}} 'f' 'int (int)'
+
+// A devirtualized member-pointer value is dumped with a "(devirtualized)"
+// marker.
+struct B { virtual int g(int); };
+constexpr auto pm = declcall(((B *)0)->B::g(0));
+// CHECK: value: MemberPointer {{.*}}g (devirtualized)
