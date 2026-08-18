c3e76baf640cd377a5b2021ec5b7819f1d4ebb03
Kornev, Nikita <nikita.kornev@intel.com>
2026-08-18 13:44:21 +0200

    use-lit-local-cfg

--- sycl/test-e2e/Tracing/task_execution.cpp
+++ sycl/test-e2e/Tracing/task_execution.cpp
@@ -37,7 +37,7 @@ int main() {
     // CHECK-NEXT: [SYCL] Task begin (event={{.*}},instanceID={{.*}})
     // CHECK-DAG:          enqueue_kernel_data : {{.*}}
     // CHECK-DAG:          sym_column_no : {{.*}}
-    // CHECK-DAG:          sym_line_no : 37
+    // CHECK-DAG:          sym_line_no : 36
     // CHECK-DAG:          sym_source_file_name : {{.*}}task_execution.cpp
     // CHECK-DAG:          queue_id : 1
     // CHECK-DAG:          sym_function_name : typeinfo name for main::E2ETestKernel
--- sycl/test-e2e/Tracing/usm/queue_copy_released_pointer.cpp
+++ sycl/test-e2e/Tracing/usm/queue_copy_released_pointer.cpp
@@ -1,5 +1,3 @@
-// UNSUPPORTED: windows
-// UNSUPPORTED-INTENDED: sycl-trace is unsupported on Windows.
 // RUN: %{build} -o %t.out
 // RUN: not --crash env SYCL_TRACE_TERMINATE_ON_WARNING=1 %{run} sycl-trace --verify %t.out | FileCheck %s
 
