cac536c4523b5556d4ca449ee905a7720fb56552
Skrobot, Daniel <daniel.skrobot@intel.com>
2026-08-24 03:26:02 -0700

    fixed code formatting

--- sycl/test-e2e/bindless_images/aligned_alloc_device_2d.cpp
+++ sycl/test-e2e/bindless_images/aligned_alloc_device_2d.cpp
@@ -4,8 +4,8 @@
 // REQUIRES: aspect-ext_oneapi_bindless_images_2d_usm
 // REQUIRES: aspect-ext_oneapi_bindless_sampled_image_fetch_2d_usm
 
-// Support for ZE_STRUCTURE_TYPE_CUSTOM_PITCH_EXP_DESC was added in Level Zero build 38646.
-// REQUIRES-INTEL-DRIVER: lin: 38646
+// Support for ZE_STRUCTURE_TYPE_CUSTOM_PITCH_EXP_DESC was added in Level Zero
+// build 38646. REQUIRES-INTEL-DRIVER: lin: 38646
 
 // XFAIL: hip
 // XFAIL-TRACKER: https://github.com/intel/llvm/issues/19957
