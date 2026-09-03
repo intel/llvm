d8231b63bbe6a60281e6addbc2480cb3c98026cc
Katarzyna Kaczmarska <katarzynax.e.kaczmarska@intel.com>
2026-09-03 15:11:47 +0200

    Reduce root and models __init__.py to minimal facades

--- devops/scripts/ur_test_tools/models/__init__.py
+++ devops/scripts/ur_test_tools/models/__init__.py
@@ -1,19 +1,1 @@
 """Models package - Data structures for UR test tools."""
-
-from .config import (
-    TestConfig,
-    TestExecutionContext,
-)
-from .test_results import (
-    TestStatus,
-    TestResult,
-    TestRunResult,
-)
-
-__all__ = [
-    "TestConfig",
-    "TestExecutionContext",
-    "TestStatus",
-    "TestResult",
-    "TestRunResult",
-]
