e1e4bc8dae81d030eaadbfd5c6cfd83b9785e483
Hugh Delaney <hdelaney@qti.qualcomm.com>
2026-09-08 18:32:00 +0100

    [SYCL][Windows] Don't do backend calls in DLL_PROCESS_DETACH (#22277)
    In Windows, ExitProcess kills spawned threads before calling
    DLL_PROCESS_DETACH. This can lead to issues if the backend relies on
    threads.
    
    
    https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-exitprocess
    
    This MS devblog recommends that DLL_PROCESS_DETACH should be as minimal
    as possible.
    
    https://devblogs.microsoft.com/oldnewthing/20120105-00/?p=8683
    
    Additionally, if helper threads are forcibly terminated this can prevent
    the stdout buffer from being flushed, so add explicit flushing of the
    stdout buffer after the threads are killed.
    
    Only cleanup early if L0 is not being used, otherwise
    `UR_L0_LEAKS_DEBUG` will break.
    
    Also extends the functionality to other libraries that may contain SYCL
    objects through the `Managed` class.

--- sycl/test-e2e/Adapters/adapter-release.cpp
+++ sycl/test-e2e/Adapters/adapter-release.cpp
@@ -1,3 +1,8 @@
+// UNSUPPORTED: windows && (opencl || cuda)
+// UNSUPPORTED-INTENDED: Only L0 does adapter release on Windows. Windows
+// recommends not doing cleanup at unloading, however L0 still requires this in
+// order to enable UR_L0_LEAKS_DEBUG on Windows.
+
 // ensure that urAdapterRelease is called
 
 // RUN: env SYCL_UR_TRACE=2 %{run-unfiltered-devices} sycl-ls 2>&1| FileCheck %s
