6fc2a65d3553d017dc70a59c7eac7eca9088f7d7
Krzysztof Filipek <krzysztof.filipek@intel.com>
2026-09-09 15:28:59 +0200

    [SYCL][Graph] Restrict whole_update_host_task leak check to L0v2 adapter

--- sycl/test-e2e/Graph/Update/RecordReplay/whole_update_host_task.cpp
+++ sycl/test-e2e/Graph/Update/RecordReplay/whole_update_host_task.cpp
@@ -1,7 +1,9 @@
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
-// Extra run to check for leaks in Level Zero using UR_L0_LEAKS_DEBUG
-// RUN: %if level_zero && !system-windows %{%{l0_leak_check} %{run} %t.out 2>&1 | FileCheck %s --implicit-check-not=LEAK %}
+// Extra run to check for leaks in Level Zero using UR_L0_LEAKS_DEBUG.
+// The leak check is restricted to the v2 adapter: on the v1 adapter it
+// sporadically reports a leak (https://github.com/intel/llvm/issues/22555).
+// RUN: %if level_zero_v2_adapter && !system-windows %{%{l0_leak_check} %{run} %t.out 2>&1 | FileCheck %s --implicit-check-not=LEAK %}
 
 // REQUIRES: aspect-usm_shared_allocations
 
