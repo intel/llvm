3176fe4f079e549fc9e7475b3f26d4df20fc76e1
Lukasz Dorau <lukasz.dorau@intel.com>
2026-09-18 07:41:00 +0000

    Fix Adapters/max_malloc.cpp - 4

--- sycl/test-e2e/Adapters/max_malloc.cpp
+++ sycl/test-e2e/Adapters/max_malloc.cpp
@@ -1,6 +1,6 @@
 // REQUIRES: level_zero, level_zero_dev_kit
 // RUN: %{build} %level_zero_options -o %t.out
-// RUN: env UR_L0_ENABLE_RELAXED_ALLOCATION_LIMITS=1 SYCL_PROGRAM_COMPILE_OPTIONS=-ze-intel-greater-than-4GB-buffer-required %{run} %t.out
+// RUN: env SYCL_UR_L0_RESTRICT_USM_RESIDENCY_TO_P2P=1 UR_L0_ENABLE_RELAXED_ALLOCATION_LIMITS=1 SYCL_PROGRAM_COMPILE_OPTIONS=-ze-intel-greater-than-4GB-buffer-required %{run} %t.out
 
 #include <iostream>
 #include <sycl/detail/core.hpp>
