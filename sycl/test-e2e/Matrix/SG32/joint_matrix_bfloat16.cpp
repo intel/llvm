58ab1e2bde2fc813c4102baf0c2bdd200a8b77b8
Dounia Khaldi <dounia.khaldi@intel.com>
2026-09-23 08:24:36 -0500

    [SYCL][E2E] Add XFAIL to Joint Matrix test failing with LNL Windows machine O0

--- sycl/test-e2e/Matrix/SG32/joint_matrix_bfloat16.cpp
+++ sycl/test-e2e/Matrix/SG32/joint_matrix_bfloat16.cpp
@@ -14,6 +14,9 @@
 // REQUIRES: aspect-ext_intel_matrix
 // REQUIRES-INTEL-DRIVER: lin: 27501, win: 101.4943
 
+// XFAIL: windows && intel_gpu_lnl_m && O0
+// XFAIL-TRACKER: CMPLRLLVM-72111
+
 // RUN: %{build} -o %t.out
 // RUN: %{run} %t.out
 // RUN: %if gpu %{ env IGC_JointMatrixLoadStoreOpt=2 %{run} %t.out %}
