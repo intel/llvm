1b6aa65ee9f5a697593dee4f03aad2a1580129bf
Alexey Bader <alexey.bader@intel.com>
2026-09-25 10:36:15 -0700

    [clang][test] Move SYCL linker wrapper checks to OffloadTools

--- clang/test/OffloadTools/clang-linker-wrapper/linker-wrapper-sycl-spirv-default-options.cpp
+++ clang/test/OffloadTools/clang-linker-wrapper/linker-wrapper-sycl-spirv-default-options.cpp
@@ -1,5 +1,6 @@
-// Generate .bc file as SYCL device library file.
-//
+// REQUIRES: system-linux, x86-registered-target, spirv-to-ir-wrapper, sycl-post-link
+
+// Create SYCL device library .bc file.
 // RUN: touch %t.devicelib.bc
 
 // Test for default llvm-spirv options
@@ -8,8 +9,8 @@
 // RUN:   --no-offloadlib -fno-sycl-instrument-device-code \
 // RUN:   -fsycl-targets=spir64-unknown-unknown -c %s -o %t.o
 // RUN: clang-linker-wrapper --bitcode-library=spir64-unknown-unknown=%t.devicelib.bc \
-// RUN:   "--host-triple=x86_64-unknown-linux-gnu" "--linker-path=/usr/bin/ld" \
-// RUN:   "--" "-o" "a.out" %t.o --dry-run 2>&1 | FileCheck  %s
+// RUN:   --host-triple=x86_64-unknown-linux-gnu --linker-path=/usr/bin/ld \
+// RUN:   -- -o a.out %t.o --dry-run 2>&1 | FileCheck %s
 
 // CHECK: llvm-spirv{{.*}}-spirv-debug-info-version=nonsemantic-shader-200
 // CHECK-NOT: ocl-100
--- clang/test/OffloadTools/clang-linker-wrapper/linker-wrapper-sycl-spirv-ext.cpp
+++ clang/test/OffloadTools/clang-linker-wrapper/linker-wrapper-sycl-spirv-ext.cpp
@@ -1,5 +1,5 @@
+// REQUIRES: system-linux, x86-registered-target, spirv-to-ir-wrapper, sycl-post-link
 // Generate .bc file as SYCL device library file.
-// REQUIRES: system-linux
 //
 // RUN: touch %t_1.devicelib.bc
 // RUN: touch %t_2.devicelib.bc
@@ -11,22 +11,22 @@
 // RUN:   --no-offloadlib -fno-sycl-instrument-device-code \
 // RUN:   -fsycl-targets=spir64-unknown-unknown -c %s -o %t_1.o
 // RUN: clang-linker-wrapper --bitcode-library=spir64-unknown-unknown=%t_1.devicelib.bc \
-// RUN:   "--host-triple=x86_64-unknown-linux-gnu" "--linker-path=/usr/bin/ld" \
-// RUN:   "--" "-o" "a.out" %t_1.o --dry-run 2>&1 \
+// RUN:   --host-triple=x86_64-unknown-linux-gnu --linker-path=/usr/bin/ld \
+// RUN:   -- -o a.out %t_1.o --dry-run 2>&1 \
 // RUN:   | FileCheck -check-prefix=CHECK-DEFAULT %s
 // RUN: %clang -target x86_64-unknown-linux-gnu -fsycl --offload-new-driver \
 // RUN:   --no-offloadlib -fno-sycl-instrument-device-code \
 // RUN:   -fsycl-targets=spir64_gen-unknown-unknown -c %s -o %t_2.o
 // RUN: clang-linker-wrapper --bitcode-library=spir64_gen-unknown-unknown=%t_2.devicelib.bc \
-// RUN:   "--host-triple=x86_64-unknown-linux-gnu" "--linker-path=/usr/bin/ld" \
-// RUN:   "--" "-o" "a.out" %t_2.o --dry-run 2>&1 \
+// RUN:   --host-triple=x86_64-unknown-linux-gnu --linker-path=/usr/bin/ld \
+// RUN:   -- -o a.out %t_2.o --dry-run 2>&1 \
 // RUN:   | FileCheck -check-prefix=CHECK-DEFAULT %s
 // RUN: %clang -target x86_64-unknown-linux-gnu -fsycl --offload-new-driver \
 // RUN:   --no-offloadlib -fno-sycl-instrument-device-code \
 // RUN:   -fsycl-targets=spir64_x86_64-unknown-unknown -c %s -o %t_3.o
 // RUN: clang-linker-wrapper --bitcode-library=spir64_x86_64-unknown-unknown=%t_3.devicelib.bc \
-// RUN:   "--host-triple=x86_64-unknown-linux-gnu" "--linker-path=/usr/bin/ld" \
-// RUN:   "--" "-o" "a.out" %t_3.o --dry-run 2>&1 \
+// RUN:   --host-triple=x86_64-unknown-linux-gnu --linker-path=/usr/bin/ld \
+// RUN:   -- -o a.out %t_3.o --dry-run 2>&1 \
 // RUN:   | FileCheck -check-prefix=CHECK-DEFAULT %s
 
 // CHECK-DEFAULT: llvm-spirv{{.*}}-spirv-ext=-all
